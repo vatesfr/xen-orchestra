1b6ae84a8a1785cdead49c9247af08da98ddfc0e
Julien Fontanet <julien.fontanet@isonoe.net>
2013-01-03 11:13:09 +0100

    License uniformization + COPYING file.

--- src/lib/TemplateUtils.php
+++ src/lib/TemplateUtils.php
@@ -1,24 +1,25 @@
 <?php
 /**
- * This file is a part of IoViz.
+ * This file is a part of Xen Orchestra Web.
  *
- * IoViz is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Xen Orchestra Web is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
  *
- * IoViz is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * Xen Orchestra Web is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with IoViz. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Xen Orchestra Web. If not, see
+ * <http://www.gnu.org/licenses/>.
  *
  * @author Julien Fontanet <julien.fontanet@vates.fr>
- * @license http://www.gnu.org/licenses/gpl-3.0-standalone.html GPLv3
+ * @license http://www.gnu.org/licenses/agpl-3.0-standalone.html GNU AGPLv3
  *
- * @package IoViz
+ * @package Xen Orchestra Web
  */
 
 /**
