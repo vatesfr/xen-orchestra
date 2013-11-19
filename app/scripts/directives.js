577ff28387af39c90af4a0bdb1bb3439aae6fdb9
Olivier Lambert <olivier.lambert@vates.fr>
2013-11-19 17:06:37 +0100

    oops remove a useless console log in custom directive

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -2,7 +2,6 @@ angular.module('xoWebApp').directive('stopEvent', function () {
     return {
         restrict: 'A',
         link: function (scope, element, attr) {
-            console.log(attr);
             element.bind(attr.stopEvent, function (e) {
                 e.stopPropagation();
             });
