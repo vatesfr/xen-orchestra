abc4a6c8670b65ca683838ec932f19f1ff1e85ad
Julien Fontanet <julien.fontanet@isonoe.net>
2014-03-25 15:33:16 +0100

    First working version.

--- config.js
+++ config.js
@@ -2,10 +2,47 @@
 
 //====================================================================
 
-var xdg = require('xdg').basedir;
+var fs = require('fs');
+
+//--------------------------------------------------------------------
+
+var _ = require('lodash');
+var l33t = require('l33teral');
+var mkdirp = require('mkdirp');
+var Promise = require('bluebird');
+var xdg = require('xdg');
 
 //====================================================================
 
-var Config = function () {
-  this.data =
+var configPath = xdg.basedir.configPath('xo-cli');
+var configFile = configPath +'/config.json';
+
+var mkdirp = Promise.promisify(mkdirp);
+var readFile = Promise.promisify(fs.readFile);
+var writeFile = Promise.promisify(fs.writeFile);
+
+//====================================================================
+
+var load = exports.load = function () {
+  return readFile(configFile).then(JSON.parse).catch(function () {
+    return {};
+  });
+};
+
+exports.get = function (path) {
+  return load().then(function (config) {
+    return l33t(config).tap(path);
+  });
+};
+
+var save = exports.save = function (config) {
+  return mkdirp(configPath).then(function () {
+    return writeFile(configFile, JSON.stringify(config));
+  });
+};
+
+exports.set = function (data) {
+  return load().then(function (config) {
+    return save(_.extend(config, data));
+  });
 };
