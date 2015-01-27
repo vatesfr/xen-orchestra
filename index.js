6bb0ca22d005da98de5bc105135c9a9f74ebdb42
Julien Fontanet <julien.fontanet@isonoe.net>
2015-01-27 17:44:26 +0100

    Enable ES6.

--- index.js
+++ index.js
@@ -2,5 +2,10 @@
 
 //====================================================================
 
+// Some modules are written in CoffeeScript.
 require('coffee-script/register');
+
+// Some modules are written in ES6.
+require('6to5/register');
+
 module.exports = require('./src/main');
