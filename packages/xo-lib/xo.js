d76cb440f9a5b23c111430dd482be2e09e58a69c
Julien Fontanet <julien.fontanet@isonoe.net>
2015-03-04 17:41:30 +0100

    Move BackOff to its own module.

--- packages/xo-lib/xo.js
+++ packages/xo-lib/xo.js
@@ -7,6 +7,7 @@ var isString = require('lodash.isstring');
 var startsWith = require('lodash.startsWith');
 
 var Api = require('./api');
+var BackOff = require('./back-off');
 var ConnectionError = require('./connection-error');
 var createCollection = require('./collection');
 
@@ -28,67 +29,6 @@ function makeStandaloneDeferred() {
 function noop() {}
 
 //====================================================================
-
-function returnThis() {
-  /* jshint validthis: true */
-
-  return this;
-}
-
-// Returns an iterator to the Fibonacci sequence.
-function fibonacci(start) {
-  var prev = 0;
-  var curr = start || 1;
-
-  var iterator = {
-    next: function () {
-      var tmp = curr;
-      curr += prev;
-      prev = tmp;
-
-      return {
-        done: false,
-        value: prev,
-      };
-    },
-  };
-
-  // Make the iterator a true iterable (ES6).
-  if (typeof Symbol !== 'undefined') {
-    iterator[Symbol.iterator] = returnThis;
-  }
-
-  return iterator;
-}
-
-//--------------------------------------------------------------------
-
-function BackOff(generator, opts) {
-  if (!opts) {
-    opts = {};
-  }
-
-  this._attempts = 0;
-  this._generator = generator;
-  this._iterator = generator();
-  this._maxAttempts = opts.maxAttempts || Infinity;
-}
-
-BackOff.prototype.wait = function () {
-  var maxAttempts = this._maxAttempts;
-  if (this._attempts++ > maxAttempts) {
-    return Bluebird.reject(new Error(
-      'maximum attempts reached (' + maxAttempts +')'
-    ));
-  }
-
-  return Bluebird.delay(this._iterator.next().value);
-};
-
-BackOff.prototype.reset = function () {
-  this._attempts = 0;
-  this._iterator = this._generator();
-};
 
 //====================================================================
 
