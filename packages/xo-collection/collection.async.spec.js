cbd93f450e63706af65c7d188d3420841db0e7a9
Fabrice Marsaud <fabrice.marsaud@vates.fr>
2015-04-02 15:32:08 +0200

    Implementation seems ok. Deeper tests to come

---
+++ packages/xo-collection/collection.async.spec.js
@@ -0,0 +1,132 @@
+/* eslint-env mocha */
+
+import Collection, {DuplicateEntry, NoSuchEntry} from './collection'
+
+import eventToPromise from 'event-to-promise'
+import sinon from 'sinon'
+
+import chai from 'chai'
+const expect = chai.expect
+import dirtyChai from 'dirty-chai'
+chai.use(dirtyChai)
+
+describe('Collection', function () {
+  beforeEach(function (done) {
+    this.col = new Collection()
+    this.col.add('bar', 0)
+
+    process.nextTick(done)
+  })
+
+  describe('#add()', function () {
+    it('adds item to the collection', function () {
+      const spy = sinon.spy()
+      this.col.on('add', spy)
+
+      this.col.add('foo', true)
+
+      expect(this.col.get('foo')).to.equal(true)
+
+      // No sync events.
+      sinon.assert.notCalled(spy)
+
+      // Async event.
+      return eventToPromise(this.col, 'add').then(function (added) {
+        expect(added).to.have.all.keys('foo')
+        expect(added.foo).to.equal(true)
+      })
+    })
+
+    it('throws an exception if the item already exists', function () {
+      expect(() => this.col.add('bar', true)).to.throw(DuplicateEntry)
+    })
+  })
+
+  describe('#update()', function () {
+    it('updates an item of the collection', function () {
+      const spy = sinon.spy()
+      this.col.on('update', spy)
+
+      this.col.update('bar', 1)
+      expect(this.col.get('bar')).to.equal(1) // Will be forgotten by de-duplication
+      this.col.update('bar', 2)
+      expect(this.col.get('bar')).to.equal(2)
+
+      // No sync events.
+      sinon.assert.notCalled(spy)
+
+      // Async event.
+      return eventToPromise(this.col, 'update').then(function (updated) {
+        expect(updated).to.have.all.keys('bar')
+        expect(updated.bar).to.equal(2)
+      })
+    })
+
+    it('throws an exception if the item does not exist', function () {
+      expect(() => this.col.update('baz', true)).to.throw(NoSuchEntry)
+    })
+  })
+
+  describe('#remove()', function () {
+    it('removes an item of the collection', function () {
+      const spy = sinon.spy()
+      this.col.on('remove', spy)
+
+      this.col.update('bar', 1)
+      expect(this.col.get('bar')).to.equal(1) // Will be forgotten by de-duplication
+      this.col.remove('bar')
+
+      // No sync events.
+      sinon.assert.notCalled(spy)
+
+      // Async event.
+      return eventToPromise(this.col, 'remove').then(function (removed) {
+        expect(removed).to.have.all.keys('bar')
+        expect(removed.bar).to.equal(null)
+      })
+    })
+
+    it('throws an exception if the item does not exist', function () {
+      expect(() => this.col.remove('baz', true)).to.throw(NoSuchEntry)
+    })
+  })
+
+  describe('#set()', function () {
+    it('adds item if collection has not key', function () {
+      const spy = sinon.spy()
+      this.col.on('add', spy)
+
+      this.col.set('foo', true)
+
+      expect(this.col.get('foo')).to.equal(true)
+
+      // No sync events.
+      sinon.assert.notCalled(spy)
+
+      // Async events.
+      return eventToPromise(this.col, 'add').then(function (added) {
+        expect(added).to.have.all.keys('foo')
+        expect(added.foo).to.equal(true)
+      })
+    })
+
+    it('updates item if collection has key', function () {
+      const spy = sinon.spy()
+      this.col.on('udpate', spy)
+
+      this.col.set('bar', 1)
+
+      expect(this.col.get('bar')).to.equal(1)
+
+      // No sync events.
+      sinon.assert.notCalled(spy)
+
+      // Async events.
+      return eventToPromise(this.col, 'update').then(function (updated) {
+        expect(updated).to.have.all.keys('bar')
+        expect(updated.bar).to.equal(1)
+      })
+    })
+  })
+
+})
--- packages/xo-collection/collection.js
+++ packages/xo-collection/collection.js
@@ -1,273 +1,197 @@
-import makeError from 'make-error';
-import events from 'events';
-import _forEach from 'lodash.foreach';
-
-const AlreadyBuffering = makeError('AlreadyBuffering');
-const NotBuffering = makeError('NotBuffering');
-const IllegalAdd = makeError('IllegalAdd');
-const DuplicateEntry = makeError('DuplicateEntry');
-const NoSuchEntry = makeError('NoSuchEntry');
-
-class Collection extends events.EventEmitter {
-	
-	constructor () {
-
-		super();
-
-		this._map = {};
-		this._buffering = false;
-		this._size = 0;
-
-	}
-
-	bufferChanges () {
-
-		if (this._buffering) {
-			throw new AlreadyBuffering('Already buffering'); // FIXME Really ?...
-		}
-
-		this._buffering = true;
-		this._buffer = {};
-
-		return () => {
-
-			if (!this._buffering) {
-				throw new NotBuffering('Nothing to flush'); // FIXME Really ?
-			}
-
-			this._buffering = false; // FIXME Really ?
-
-			let data = {
-				add: {data: {}},
-				update: {data: {}},
-				remove: {data: {}}
-			};
-
-			for (let key in this._buffer) {
-				data[this._buffer[key]].data[key] = this.has(key) ?
-					this.get(key) :
-					null; // 'remove' case
-				data[this._buffer[key]].has = true;
-			}
-
-			['add', 'update', 'remove'].forEach(action => {
-				if (data[action].has) {
-					this.emit(action, data[action].data);
-				}
-			});
-
-			delete this._buffer;
-
-		};
-
-	}
-
-	_touch (action, key, value) {
-
-		if (this._buffering) {
-
-			switch(action) {
-
-				case 'add':
-					this._buffer[key] = this._buffer[key] ? 'update' : 'add';
-					break;
-				case 'update':
-					this._buffer[key] = this._buffer[key] || 'update';
-					break;
-				case 'remove':
-					switch(this._buffer[key]) {
-						case undefined:
-						case 'update':
-							this._buffer[key] = 'remove';
-							break;
-						case 'add':
-							delete this._buffer[key];
-							break;
-
-					}
-					break;
-			}
-
-
-		} else {
-
-			this.emit(action, {key: value});
-
-		}
-
-	}
-
-	getId (item) {
-
-		return item.id;
-
-	}
-
-	has (key) {
-
-		return Object.hasOwnProperty.call(this._map, key);
-
-	}
-
-	resolveEntry (keyOrObjectWithId, valueIfKey = null) {
-
-		let value;
-		let key = (undefined !== keyOrObjectWithId) ?
-			this.getId(keyOrObjectWithId) :
-			undefined;
-
-		if (undefined === key) {
-			if (arguments.length < 2) {
-				throw new IllegalAdd();
-			} else {
-				key = keyOrObjectWithId;
-				value = valueIfKey;
-			}
-		} else {
-			value = keyOrObjectWithId;
-		}
-
-		return [key, value];
-
-	}
-
-	_assertHas(key) {
-
-		if (!this.has(key)) {
-			throw new NoSuchEntry();
-		}
-
-	}
-
-	_assertHasNot(key) {
-
-		if (this.has(key)) {
-			throw new DuplicateEntry();
-		}
-
-	}
-
-	add (keyOrObjectWithId, valueIfKey = null) {
-
-		const [key, value] = this.resolveEntry.apply(this, arguments);
-
-		this._assertHasNot(key);
-
-		this._map[key] = value;
-
-		this._size++;
-		this._touch('add', key, value);
-
-		return this;
-
-	}
-
-	set (keyOrObjectWithId, valueIfKey = null) {
-
-		const [key, value] = this.resolveEntry.apply(this, arguments);
-
-		const action = this.has(key) ? 'update' : 'add';
-		this._map[key] = value;
-		if ('add' === action) {
-			this._size++;
-		}
-
-		this._touch(action, key, value);
-
-		return this;
-
-	}
-
-	get (key) {
-
-		this._assertHas(key);
-
-		return this._map[key];
-	}
-
-	update (keyOrObjectWithId, valueIfKey = null) {
-
-		const [key, value] = this.resolveEntry.apply(this, arguments);
-
-		this._assertHas(key);
-
-		this._map[key] = value;
-		this._touch('update', key, value);
-
-		return this;
-
-	}
-
-	touch (keyOrObjectWithId) {
-
-		const [key] = this.resolveEntry(keyOrObjectWithId, null);
-
-		this._assertHas(key);
-
-		if (!this._buffering) { // FIXME Really ?
-			// TODO throw something
-		}
-
-		this._touch('update', key);
-		return this.get(key);
-
-	}
-
-	remove (keyOrObjectWithId) {
-
-		const [key] = this.resolveEntry(keyOrObjectWithId, null);
-
-		this._assertHas(key);
-
-		const oldValue = this.get(key);
-		delete this._map[key];
-		this._size--;
-
-		// FIXME do we "emit" null in place of oldValue to harmonize with flush remove events ?
-		this._touch('remove', key, oldValue);
-
-		return this;
-
-	}
-
-	clear () {
-
-		if (this._size > 0) { // FIXME Really ?
-			if (!this._buffering) {
-				this.emit('remove', this._map);
-			} else {
-				for (let key in this._map) {
-					this._touch('remove', key);
-				}
-			}
-		}
-
-		this._map = {};
-		this._size = 0;
-
-		return this;
-
-	}
-
-	get size () {
-
-		return this._size;
-
-	}
-
-	get all () {
-
-		return this._map;
-
-	}
-
+import events from 'events'
+import makeError from 'make-error'
+
+export const DuplicateEntry = makeError('DuplicateEntry')
+export const BufferAlreadyFlushed = makeError('BufferAlreadyFlushed')
+export const IllegalAdd = makeError('IllegalAdd')
+export const NoSuchEntry = makeError('NoSuchEntry')
+
+export default class Collection extends events.EventEmitter {
+  constructor () {
+    super()
+
+    this._buffering = 0
+    this._map = {}
+    this._size = 0
+  }
+
+  bufferChanges () {
+    if (this._buffering++ === 0) {
+      this._buffer = {}
+    }
+
+    let called = false
+    return () => {
+      if (called) {
+        throw new BufferAlreadyFlushed('Buffer flush already requested')
+      }
+      called = true
+
+      if (--this._buffering > 0) {
+        return
+      }
+
+      let data = {
+        add: {data: {}},
+        remove: {data: {}},
+        update: {data: {}}
+      }
+
+      for (let key in this._buffer) {
+        data[this._buffer[key]].data[key] = this.has(key) ?
+          this.get(key) :
+          null // "remove" case
+        data[this._buffer[key]].has = true
+      }
+
+      ['add', 'update', 'remove'].forEach(action => {
+        if (data[action].has) {
+          this.emit(action, data[action].data)
+        }
+      })
+
+      delete this._buffer
+    }
+  }
+
+  _touch (action, key) {
+    if (this._buffering === 0) {
+      process.nextTick(this.bufferChanges())
+    }
+    switch (action) {
+      case 'add':
+        this._buffer[key] = this._buffer[key] ? 'update' : 'add'
+        break
+      case 'remove':
+        switch (this._buffer[key]) {
+          case undefined:
+          case 'update':
+            this._buffer[key] = 'remove'
+            break
+          case 'add':
+            delete this._buffer[key]
+            break
+        }
+        break
+      case 'update':
+        this._buffer[key] = this._buffer[key] || 'update'
+        break
+    }
+  }
+
+  getId (item) {
+    return item.id
+  }
+
+  has (key) {
+    return Object.hasOwnProperty.call(this._map, key)
+  }
+
+  resolveEntry (keyOrObjectWithId, valueIfKey = null) {
+    let value
+    let key = (undefined !== keyOrObjectWithId) ?
+      this.getId(keyOrObjectWithId) :
+      undefined
+
+    if (undefined === key) {
+      if (arguments.length < 2) {
+        throw new IllegalAdd('Missing value, or object value does not provide id/key')
+      } else {
+        key = keyOrObjectWithId
+        value = valueIfKey
+      }
+    } else {
+      value = keyOrObjectWithId
+    }
+
+    return [key, value]
+  }
+
+  _assertHas (key) {
+    if (!this.has(key)) {
+      throw new NoSuchEntry('No ' + key + ' entry')
+    }
+  }
+
+  _assertHasNot (key) {
+    if (this.has(key)) {
+      throw new DuplicateEntry('Attempt to duplicate ' + key + ' entry')
+    }
+  }
+
+  add (keyOrObjectWithId, valueIfKey = null) {
+    const [key, value] = this.resolveEntry.apply(this, arguments)
+    this._assertHasNot(key)
+
+    this._map[key] = value
+    this._size++
+    this._touch('add', key)
+
+    return this
+  }
+
+  set (keyOrObjectWithId, valueIfKey = null) {
+    const [key, value] = this.resolveEntry.apply(this, arguments)
+
+    const action = this.has(key) ? 'update' : 'add'
+    this._map[key] = value
+    if (action === 'add') {
+      this._size++
+    }
+    this._touch(action, key)
+
+    return this
+  }
+
+  get (key) {
+    this._assertHas(key)
+    return this._map[key]
+  }
+
+  update (keyOrObjectWithId, valueIfKey = null) {
+    const [key, value] = this.resolveEntry.apply(this, arguments)
+    this._assertHas(key)
+
+    this._map[key] = value
+    this._touch('update', key)
+
+    return this
+  }
+
+  touch (keyOrObjectWithId) {
+    const [key] = this.resolveEntry(keyOrObjectWithId, null)
+    this._assertHas(key)
+
+    this._touch('update', key)
+
+    return this.get(key)
+  }
+
+  remove (keyOrObjectWithId) {
+    const [key] = this.resolveEntry(keyOrObjectWithId, null)
+    this._assertHas(key)
+
+    delete this._map[key]
+    this._size--
+    this._touch('remove', key)
+
+    return this
+  }
+
+  clear () {
+    for (let key in this._map) {
+      delete this._map[key]
+      this._size--
+      this._touch('remove', key)
+    }
+    return this
+  }
+
+  get size () {
+    return this._size
+  }
+
+  get all () {
+    return this._map
+  }
 }
-
-export default {
-	Collection,
-	AlreadyBuffering,
-	NotBuffering,
-	IllegalAdd,
-	DuplicateEntry,
-	NoSuchEntry
-};
