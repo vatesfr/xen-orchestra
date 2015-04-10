25d29fb3894398d2d2ec195703779444494ea069
Julien Fontanet <julien.fontanet@isonoe.net>
2015-04-10 17:52:30 +0200

    server: CoffeeScript → ES6.

---
+++ src/api/server.js
@@ -0,0 +1,121 @@
+import {coroutine, wait} from '../fibers-utils'
+import {NoSuchObject, NotImplemented} from '../api-errors'
+
+// ===================================================================
+
+// FIXME: We are storing passwords which is bad!
+//        Could we use tokens instead?
+
+export const add = coroutine(function ({host, username, password}) {
+  const server = wait(this.servers.add({
+    host: host,
+    username: username,
+    password: password
+  }))
+  return server.get('id')
+})
+
+add.description = 'Add a new Xen server to XO'
+
+add.permission = 'admin'
+
+add.params = {
+  host: {
+    type: 'string'
+  },
+  username: {
+    type: 'string'
+  },
+  password: {
+    type: 'string'
+  }
+}
+
+// -------------------------------------------------------------------
+
+export const remove = coroutine(function ({id}) {
+  if (!wait(this.servers.remove(id))) {
+    throw new NoSuchObject()
+  }
+})
+
+remove.permission = 'admin'
+
+remove.params = {
+  id: {
+    type: 'string'
+  }
+}
+
+// -------------------------------------------------------------------
+
+export const getAll = coroutine(function () {
+  const servers = wait(this.servers.get())
+
+  for (let i = 0, n = servers.length; i < n; ++i) {
+    servers[i] = this.getServerPublicProperties(servers[i])
+  }
+
+  return servers
+})
+
+getAll.permission = 'admin'
+
+// -------------------------------------------------------------------
+
+export const set = coroutine(function ({id, host, username, password}) {
+  const server = wait(this.servers.first(id))
+  if (!server) {
+    throw new NoSuchObject()
+  }
+  if (host != null) {
+    server.set({
+      host: host
+    })
+  }
+  if (username != null) {
+    server.set({
+      username: username
+    })
+  }
+  if (password != null) {
+    server.set({
+      password: password
+    })
+  }
+  wait(this.servers.update(server))
+})
+
+set.permission = 'admin'
+
+set.params = {
+  id: {
+    type: 'string'
+  },
+  host: {
+    type: 'string',
+    optional: true
+  },
+  username: {
+    type: 'string',
+    optional: true
+  },
+  password: {
+    type: 'string',
+    optional: true
+  }
+}
+
+// -------------------------------------------------------------------
+
+export const connect = function () {
+  throw new NotImplemented()
+}
+
+set.permission = 'admin'
+
+// -------------------------------------------------------------------
+
+export const disconnect = function () {
+  throw new NotImplemented()
+}
