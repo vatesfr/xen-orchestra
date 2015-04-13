783ab0b6111f127aa1b15f4f6e891bd35ed7013d
Julien Fontanet <julien.fontanet@isonoe.net>
2015-04-13 20:23:43 +0200

    Initial view implementation (fix #3).

---
+++ packages/xo-collection/src/view.example.js
@@ -0,0 +1,40 @@
+import Collection from './'
+import View from './view'
+
+const users = new Collection()
+users.getId = (user) => user.name
+
+const activeUsers = new View(users, 'active')
+activeUsers.on('add', console.log)
+activeUsers.on('update', console.log)
+activeUsers.on('remove', console.log)
+
+users.add({
+  name: 'bob'
+})
+
+users.add({
+  name: 'clara',
+  active: true
+})
+
+users.add({
+  name: 'ophelia'
+})
+
+users.add({
+  name: 'Steve',
+  active: true
+})
+
+setTimeout(function () {
+  console.log('-----')
+
+  users.set({
+    name: 'ophelia',
+    active: true
+  })
+  users.set({
+    name: 'Steve'
+  })
+}, 10)
