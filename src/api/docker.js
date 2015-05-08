89d485e1884774b03c4056e96d781dbf8d8bc137
Olivier Lambert <olivier.lambert@vates.fr>
2015-05-08 16:12:15 +0200

    remove typo in id

--- src/api/docker.js
+++ src/api/docker.js
@@ -1,21 +1,21 @@
 export async function start ({vm, container}) {
-  await this.getXAPI(vm).startDockerContainer(vm.$id, container)
+  await this.getXAPI(vm).startDockerContainer(vm.id, container)
 }
 
 export async function stop ({vm, container}) {
-  await this.getXAPI(vm).stopDockerContainer(vm.$id, container)
+  await this.getXAPI(vm).stopDockerContainer(vm.id, container)
 }
 
 export async function restart ({vm, container}) {
-  await this.getXAPI(vm).restartDockerContainer(vm.$id, container)
+  await this.getXAPI(vm).restartDockerContainer(vm.id, container)
 }
 
 export async function pause ({vm, container}) {
-  await this.getXAPI(vm).pauseDockerContainer(vm.$id, container)
+  await this.getXAPI(vm).pauseDockerContainer(vm.id, container)
 }
 
 export async function unpause ({vm, container}) {
-  await this.getXAPI(vm).unpauseDockerContainer(vm.$id, container)
+  await this.getXAPI(vm).unpauseDockerContainer(vm.id, container)
 }
 
 for (let fn of [start, stop, restart, pause, unpause]) {
