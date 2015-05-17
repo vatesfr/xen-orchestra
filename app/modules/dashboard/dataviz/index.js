b530ab2ef6f796a7fa83d4b109c568f7409331bf
Olivier Lambert <olivier.lambert@vates.fr>
2015-05-17 17:20:09 +0200

    clean copy/pasted code

--- app/modules/dashboard/dataviz/index.js
+++ app/modules/dashboard/dataviz/index.js
@@ -20,108 +20,10 @@ export default angular.module('dashboard.dataviz', [
     $stateProvider.state('dashboard.dataviz', {
       controller: 'Dataviz as ctrl',
       url: '/dataviz',
-      resolve: {
-        servers(xo) {
-          return xo.server.getAll();
-        },
-      },
       template: view,
     });
   })
-  .controller('Dataviz', function ($scope, $interval, servers, xoApi, xo, notify) {
-    this.servers = servers;
-    const selected = this.selectedServers = {};
-    const newServers = this.newServers = [];
-
-    const refreshServers = () => {
-      xo.server.getAll().then(servers => {
-        this.servers = servers;
-      });
-    };
-
-    const interval = $interval(refreshServers, 10e3)
-    $scope.$on('$destroy', () => {
-      $interval.cancel(interval)
-    })
-
-    this.connectServer = (id) => {
-      notify.info ({
-          title: 'Server connect',
-          message: 'Connecting the server...'
-      });
-      xo.server.connect(id).catch(error => {
-        notify.error({
-          title: 'Server connection error',
-          message: error.message
-        });
-      });
-    };
-
-    this.disconnectServer = (id) => {
-      notify.info ({
-          title: 'Server disconnect',
-          message: 'Disconnecting the server...'
-      });
-      xo.server.disconnect(id);
-    };
-
-    this.addServer = () => {
-      newServers.push({
-        // Fake (unique) id needed by Angular.JS
-        id: Math.random(),
-        status: 'connecting'
-      });
-    };
-
-    this.addServer();
-    this.saveServers = () => {
-      const newServers = this.newServers;
-      const servers = this.servers;
-      const updateServers = [];
-
-      for (let i = 0, len = servers.length; i < len; i++) {
-        const server = servers[i];
-        const {id} = server;
-        if (selected[id]) {
-          delete selected[id];
-          xo.server.remove(id);
-        }
-        else {
-          if (!server.password) {
-            delete server.password;
-          }
-          xo.server.set(server);
-          delete server.password;
-          updateServers.push(server);
-        }
-      }
-      for (let i = 0, len = newServers.length; i < len; i++) {
-        const server = newServers[i];
-        const {host, username, password} = server;
-        if (!host) {
-          continue;
-        }
-        xo.server.add({
-          host,
-          username,
-          password,
-          autoConnect: false,
-        }).then(function(id) {
-          server.id = id;
-          xo.server.connect(id).catch(error => {
-            notify.error({
-              title: 'Server connection error',
-              message: error.message
-            });
-          });
-        });
-        delete server.password;
-        updateServers.push(server);
-      }
-      this.servers = updateServers;
-      this.newServers.length = 0;
-      this.addServer();
-    };
+  .controller('Dataviz', function ($scope, $interval, xoApi, xo, notify) {
   })
   .name
 ;
--- app/modules/dashboard/health/index.js
+++ app/modules/dashboard/health/index.js
@@ -1,9 +1,6 @@
 import angular from 'angular'
 import uiRouter from 'angular-ui-router'
 
-import ansiUp from 'ansi_up'
-import updater from '../../updater'
-import {AuthenticationFailed} from '../../updater'
 import xoApi from 'xo-api'
 import xoServices from 'xo-services'
 
@@ -12,72 +9,16 @@ import view from './view'
 export default angular.module('dashboard.health', [
   uiRouter,
 
-  updater,
   xoApi,
   xoServices
 ])
   .config(function ($stateProvider) {
     $stateProvider.state('dashboard.health', {
       controller: 'Health as ctrl',
-      url: '/update',
-      onExit: updater => {
-        updater.removeAllListeners('end')
-      },
+      url: '/health',
       template: view
     })
   })
-  .filter('ansitohtml', function ($sce) {
-    return function (input) {
-      return $sce.trustAsHtml(ansiUp.ansi_to_html(input))
-    }
-  })
-  .controller('SettingsUpdate', function (xoApi, xo, updater, notify) {
-    this.updater = updater
-
-    this.updater.isRegistered()
-    .then(() => this.updater.on('end', () => {
-      if (this.updater.state === 'registerNeeded' && this.updater.registerState !== 'unregistered' && this.updater.registerState !== 'error') {
-        this.updater.isRegistered()
-      }
-    }))
-    .catch(err => console.error(err))
-
-    this.registerXoa = (email, password) => {
-      this.regPwd = ''
-      this.updater.register(email, password)
-      .then(() => this.updater.update())
-      .catch(AuthenticationFailed, () => {})
-    }
-
-    this.update = () => {
-      this.updater.log('info', 'Start updating...')
-      this.updater.update()
-      .catch(error => notify.error({
-        title: 'XOA Updater',
-        message: error.message
-      }))
-    }
-
-    this.upgrade = () => {
-      this.updater.log('info', 'Start upgrading...')
-      this.updater.upgrade()
-      .catch(error => notify.error({
-        title: 'XOA Updater',
-        message: error.message
-      }))
-    }
-
-    this.trial = () => {
-      this.updater.requestTrial()
-      .then(() => this.update())
-      .catch(error => notify.error({
-        title: 'Trial request',
-        message: error.message
-      }))
-    }
-
-    this.valid = trial => {
-      return trial && trial.end && Date.now() < trial.end
-    }
+  .controller('Health', function (xoApi, xo, notify) {
   })
   .name
