1733290c028b2fbba78c6c110e24188b39dc7556
Varchar38 <mylene.barreyre@gmail.com>
2015-06-16 11:10:22 +0200

    Tests role

---
+++ src/role.spec.js
@@ -0,0 +1,51 @@
+/* eslint-env mocha */
+
+// Doc: https://github.com/moll/js-must/blob/master/doc/API.md#must
+import expect from 'must'
+
+// ===================================================================
+
+import {Xo} from 'xo-lib'
+
+// ===================================================================
+
+describe('role', function () {
+  let xo
+  before(async function () {
+    xo = new Xo('localhost:9000')
+
+    await xo.signIn({
+      email: 'admin@admin.net',
+      password: 'admin'
+    })
+  })
+
+// ==================================================================
+  describe('.getAll()', function () {
+
+    it(' returns all the roles', async function () {
+
+      const role = await xo.call('role.getAll')
+
+      // FIXME: use permutationOf but figure out how not to compare objects by
+      // equality.
+      expect(role).to.eql([
+        {
+          id: 'viewer',
+          name: 'Viewer',
+          permissions: ['view']
+        },
+        {
+          id: 'operator',
+          name: 'Operator',
+          permissions: ['view', 'operate']
+        },
+        {
+          id: 'admin',
+          name: 'Admin',
+          permissions: ['view', 'operate', 'administrate']
+        }
+      ])
+    })
+  })
+})
