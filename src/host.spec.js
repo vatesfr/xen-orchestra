ae31ebdc3319a861696287685f1d4980617dc851
Varchar38 <mylene.barreyre@gmail.com>
2015-06-18 17:03:48 +0200

    Tests host and vm

--- src/host.spec.js
+++ src/host.spec.js
@@ -2,19 +2,65 @@
 
 // Doc: https://github.com/moll/js-must/blob/master/doc/API.md#must
 
-// import {getConnection} from './util'
+import expect from 'must'
+import eventToPromise from 'event-to-promise'
+import {getConnection} from './util'
+import {forEach} from 'lodash'
 
 // ===================================================================
 
 describe('host', function () {
-  /*let xo
+  let xo
+  let serverId
   before(async function () {
+    this.timeout(30e3)
+
     xo = await getConnection()
-  })*/
+
+    serverId = await xo.call('server.add', {
+      host: '192.168.1.3',
+      username: 'root',
+      password: 'qwerty'
+    }).catch(() => {})
+
+    await eventToPromise(xo.objects, 'finish')
+  })
+
+  after(async function () {
+    await xo.call('server.remove', {
+      id: serverId
+    })
+  })
+
+  function getAllHosts () {
+    return xo.objects.indexes.type.host
+  }
+
+  function getOneHost () {
+    const hosts = getAllHosts()
+    for (const id in hosts) {
+      return hosts[id]
+    }
+
+    throw new Error('no hosts found')
+  }
 
 // ===================================================================
+
   describe('.set()', function () {
-    it('changes properties of the host')
+    this.timeout(20e3)
+    it('changes properties of the host', async function () {
+      let host = getOneHost()
+
+      await xo.call('host.set', {
+        id: host.id,
+        name_label: 'labTest'
+      })
+
+      // Waits for the host to be udated.
+      host = await xo.waitObject(host.id)
+      expect(host.name_label).to.be.equal('labTest')
+    })
   })
 
   // ------------------------------------------------------------------
@@ -49,14 +95,26 @@ describe('host', function () {
 
   // ------------------------------------------------------------------
 
-  describe('.enable()', function () {
-    it('enables to create VM on the host')
-  })
+  describe('.disable(), .enable()', function () {
+    this.timeout(20e3)
+    it('enable or disable to create VM on the host', async function () {
+      let host = getOneHost()
 
-  // ------------------------------------------------------------------
+      await xo.call('host.disable', {
+        id: host.id
+      })
+      host = await xo.waitObject(host.id)
 
-  describe('.disable()', function () {
-    it('disable to create VM on the host')
+      expect(host.enabled).to.be.false()
+
+      await xo.call('host.enable', {
+        id: host.id
+      })
+      host = await xo.waitObject(host.id)
+
+      expect(host.enabled).to.be.true()
+
+    })
   })
 
   // ------------------------------------------------------------------
@@ -75,7 +133,16 @@ describe('host', function () {
   // ------------------------------------------------------------------
 
   describe('.stats()', function () {
-    it('returns an array with statistics of the host')
-  })
+    it('returns an array with statistics of the host', async function () {
+      const host = getOneHost()
+      const stats = await xo.call('host.stats', {
+        host: host.id
+      })
+      expect(stats).to.be.an.object()
 
+      forEach(stats, function (array, key) {
+        expect(array).to.be.an.array()
+      })
+    })
+  })
 })
--- src/vm.spec.js
+++ src/vm.spec.js
@@ -5,23 +5,61 @@
 
 // ===================================================================
 
-// import {find} from 'lodash'
-import {Xo} from 'xo-lib'
+import {getConnection} from './util'
 
 // ===================================================================
 
 describe('vm', function () {
   let xo
   before(async function () {
-    xo = new Xo('localhost:9000')
-
-    await xo.signIn({
-      email: 'admin@admin.net',
-      password: 'admin'
-    })
+    xo = await getConnection()
   })
 
   // =================================================================
+
+  describe('.create()', function () {
+    describe('.createHVM()', function () {
+      it('creates a VM with the Other Config template, three disks, two interfaces and a ISO mounted, and return its UUID')
+      it('creates a VM with the Other Config template, no disk, no network and a ISO mounted, and return its UUID')
+    })
+    describe('.createPV()', function () {
+      it('creates a VM with the Debian 7 64 bits template, network install, one disk, one network, and return its UUID')
+      it('creates a VM with the CentOS 7 64 bits template, two disks, two networks and a ISO mounted and return its UUID')
+    })
+  })
+
+  // ------------------------------------------------------------------
+
+  describe('.delete()', function () {
+    it('deletes a VM')
+    it('')
+  })
+
+  // ------------------------------------------------------------------
+
+  describe('.ejectCd()', function () {
+    it('')
+  })
+
+  // -------------------------------------------------------------------
+
+  describe('.insertCd()', function () {
+    it('')
+  })
+
+  // -------------------------------------------------------------------
+
+  describe('.migrate', function () {
+    it('migrates the VM on an other host')
+  })
+
+  // -------------------------------------------------------------------
+
+  describe('.migratePool()', function () {
+    it('migrates the VM on an other host which is in an other pool')
+  })
+
+  // -------------------------------------------------------------------
 
   describe('.set()', function () {
     it('sets a VM name and returns true')
@@ -30,7 +68,7 @@ describe('vm', function () {
     it('sets a VM RAM amount and returns true')
   })
 
-  // =================================================================
+  // --------------------------------------------------------------------
 
   describe('.cycle()', function () {
     it('starts a VM and returns true')
@@ -42,23 +80,63 @@ describe('vm', function () {
     it('converts a VM and returns true')
   })
 
-  // =================================================================
+  // ---------------------------------------------------------------------
 
   describe('.snapshot()', function () {
     it('snapshots a VM and returns its snapshot UUID')
     it('reverts a VM snapshot and returns true')
   })
 
-  // =================================================================
+  // ---------------------------------------------------------------------
 
-  describe('.createHVM()', function () {
-    it('creates a VM with the Other Config template, three disks, two interfaces and a ISO mounted, and return its UUID')
-    it('creates a VM with the Other Config template, no disk, no network and a ISO mounted, and return its UUID')
+  describe('.revert()', function () {
+    it('')
   })
-  // =================================================================
 
-  describe('.createPV()', function () {
-    it('creates a VM with the Debian 7 64 bits template, network install, one disk, one network, and return its UUID')
-    it('creates a VM with the CentOS 7 64 bits template, two disks, two networks and a ISO mounted and return its UUID')
+  // ---------------------------------------------------------------------
+
+  describe('.handleExport()', function () {
+    it('')
+  })
+
+  // --------------------------------------------------------------------
+
+  describe('.import()', function () {
+    it('')
+  })
+
+  // ---------------------------------------------------------------------
+
+  describe('.attachDisk()', function () {
+    it('')
+  })
+
+  // ---------------------------------------------------------------------
+
+  describe('.createInterface()', function () {
+    it('')
+  })
+
+  // ---------------------------------------------------------------------
+
+  describe('.attachPci()', function () {
+    it('')
+  })
+
+  // ---------------------------------------------------------------------
+
+  describe('.detachPci()', function () {
+    it('')
+  })
+
+  // ---------------------------------------------------------------------
+
+  describe('.stats()', function () {
+    it('returns an array with statistics of the VM')
+  })
+
+  // ---------------------------------------------------------------------
+  describe('.bootOrder()', function () {
+    it('')
   })
 })
