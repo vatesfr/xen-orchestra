bcd44e4b2dcdbf8573de0d17a7406a7393206d7b
Varchar38 <mylene.barreyre@gmail.com>
2015-06-18 16:58:01 +0200

    Tests for disk and docker

---
+++ src/disk.spec.js
@@ -0,0 +1,23 @@
+/* eslint-env mocha */
+
+// Doc: https://github.com/moll/js-must/blob/master/doc/API.md#must
+import expect from 'must'
+
+// ===================================================================
+
+import {getConnection} from './util'
+
+// ===================================================================
+
+describe('disk', function () {
+  let xo
+  before(async function () {
+    xo = await getConnection()
+  })
+
+// ===================================================================
+
+  describe('.create()', function () {
+    it('create a new disk on a SR')
+  })
+})
---
+++ src/docker.spec.js
@@ -0,0 +1,59 @@
+/* eslint-env mocha */
+
+// Doc: https://github.com/moll/js-must/blob/master/doc/API.md#must
+import expect from 'must'
+
+// ===================================================================
+
+import {getConnection} from './util'
+
+// ===================================================================
+
+describe('docker', function () {
+  let xo
+  before(async function () {
+    xo = await getConnection()
+  })
+
+// ===================================================================
+
+  describe('.register()', async function () {
+    it('registers the VM for Docker management')
+  })
+
+// -------------------------------------------------------------------
+
+  describe('.deregister()', async function () {
+    it('deregister the VM for Docker management')
+  })
+
+// -------------------------------------------------------------------
+
+  describe('.start()', async function () {
+    it('starts the Docker')
+  })
+
+// -------------------------------------------------------------------
+
+  describe('.stop()', async function () {
+    it('stops the Docker')
+  })
+
+// -------------------------------------------------------------------
+
+  describe('.restart()', async function () {
+    it('restarts the Docker')
+  })
+
+// -------------------------------------------------------------------
+
+  describe('.pause()', async function () {
+    it('pauses the Docker')
+  })
+
+// -------------------------------------------------------------------
+
+  describe('.unpause()', async function () {
+    it('unpauses the Docker')
+  })
+})
