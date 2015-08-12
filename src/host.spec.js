2d639e191ab4b46469477cd7279902382085825b
Varchar38 <mylene.barreyre@gmail.com>
2015-08-12 16:33:26 +0200

    Add functions on host

--- src/host.spec.js
+++ src/host.spec.js
@@ -4,34 +4,29 @@
 
 import expect from 'must'
 import eventToPromise from 'event-to-promise'
-import {getConfig, getOneHost, getMainConnection, waitObjectState} from './util'
-import {forEach} from 'lodash'
+import {getAllHosts, getConfig, getMainConnection, getVmToMigrateId, waitObjectState} from './util'
+import {find, forEach} from 'lodash'
 
 // ===================================================================
 
 describe('host', function () {
   let xo
   let serverId
-  let host
+  let hostId
 
   // -----------------------------------------------------------------
 
   before(async function () {
-    this.timeout(5e3)
+    this.timeout(10e3)
     let config
-
     ;[xo, config] = await Promise.all([
       getMainConnection(),
       getConfig()
     ])
-    serverId = await xo.call('server.add', config.xenServer1).catch(() => {})
+    serverId = await xo.call('server.add', config.xenServer2).catch(() => {})
     await eventToPromise(xo.objects, 'finish')
-  })
 
-  // -------------------------------------------------------------------
-
-  beforeEach(async function () {
-    host = getOneHost(xo)
+    hostId = getHost(config.host1)
   })
 
   // -------------------------------------------------------------------
@@ -42,24 +37,41 @@ describe('host', function () {
     })
   })
 
+  // -------------------------------------------------------------------
+
+  function getHost (name_label) {
+    const hosts = getAllHosts(xo)
+    const host = find(hosts, {name_label: name_label})
+    return host.id
+  }
+
 // ===================================================================
 
   describe('.set()', function () {
+    let name_label
+    let name_description
+
+    beforeEach(async function () {
+      // get values to set them at the end of the test
+      const host = xo.objects.all[hostId]
+      name_label = host.name_label
+      name_description = host.name_description
+    })
     afterEach(async function () {
       await xo.call('host.set', {
-        id: host.id,
-        name_label: 'lab3',
-        name_description: 'Default install of XenServer'
+        id: hostId,
+        name_label: name_label,
+        name_description: name_description
       })
     })
 
     it('changes properties of the host', async function () {
       await xo.call('host.set', {
-        id: host.id,
+        id: hostId,
         name_label: 'labTest',
         name_description: 'description'
       })
-      await waitObjectState(xo, host.id, host => {
+      await waitObjectState(xo, hostId, host => {
         expect(host.name_label).to.be.equal('labTest')
         expect(host.name_description).to.be.equal('description')
       })
@@ -69,7 +81,20 @@ describe('host', function () {
   // ------------------------------------------------------------------
 
   describe('.restart()', function () {
-    it('restart the host')
+    this.timeout(330e3)
+    it('restart the host', async function () {
+      await xo.call('host.restart', {id: hostId})
+
+      await waitObjectState(xo, hostId, host => {
+        expect(host.current_operations)
+      })
+      await waitObjectState(xo, hostId, host => {
+        expect(host.power_state).to.be.equal('Halted')
+      })
+      await waitObjectState(xo, hostId, host => {
+        expect(host.power_state).to.be.equal('Running')
+      })
+    })
   })
 
   // ------------------------------------------------------------------
@@ -81,13 +106,60 @@ describe('host', function () {
   // ------------------------------------------------------------------
 
   describe('.start()', function () {
-    it('start the host')
+    this.timeout(300e3)
+    beforeEach(async function () {
+      try {
+        await xo.call('host.stop', {id: hostId})
+      } catch(_) {}
+
+      // test if the host is shutdown
+      await waitObjectState(xo, hostId, host => {
+        expect(host.power_state).to.be.equal('Halted')
+      })
+    })
+
+    it('start the host', async function () {
+      await xo.call('host.start', {id: hostId})
+      await waitObjectState(xo, hostId, host => {
+        expect(host.power_state).to.be.equal('Running')
+      })
+    })
   })
 
   // ------------------------------------------------------------------
 
   describe('.stop()', function () {
-    it('stop the host')
+    this.timeout(300e3)
+    let vmId
+
+    before(async function () {
+      vmId = await getVmToMigrateId(xo)
+      try {
+        await xo.call('vm.start', {id: vmId})
+      } catch(_) {}
+      try {
+        await xo.call('vm.migrate', {
+          vm: vmId,
+          host: hostId
+        })
+      } catch(_) {}
+    })
+    afterEach(async function () {
+      await xo.call('host.start', {id: hostId})
+    })
+
+    it('stop the host and shutdown its VMs', async function () {
+      await xo.call('host.stop', {id: hostId})
+      await Promise.all([
+        waitObjectState(xo, vmId, vm => {
+          expect(vm.$container).not.to.be.equal(hostId)
+          expect(vm.power_state).to.be.equal('Halted')
+        }),
+        waitObjectState(xo, hostId, host => {
+          expect(host.power_state).to.be.equal('Halted')
+        })
+      ])
+    })
   })
 
   // ------------------------------------------------------------------
@@ -101,14 +173,13 @@ describe('host', function () {
   describe('.disable(), ', function () {
     afterEach(async function () {
       await xo.call('host.enable', {
-        id: host.id
+        id: hostId
       })
     })
+
     it('disables to create VM on the host', async function () {
-      await xo.call('host.disable', {
-        id: host.id
-      })
-      await waitObjectState(xo, host.id, host => {
+      await xo.call('host.disable', {id: hostId})
+      await waitObjectState(xo, hostId, host => {
         expect(host.enabled).to.be.false()
       })
     })
@@ -118,24 +189,28 @@ describe('host', function () {
 
   describe('.enable()', async function () {
     beforeEach(async function () {
-      await xo.call('host.disable', {id: host.id})
+      await xo.call('host.disable', {id: hostId})
     })
-    it('enables to create VM on the host', async function () {
-      await xo.call('host.enable', {
-        id: host.id
-      })
 
-      await waitObjectState(xo, host.id, host => {
+    it('enables to create VM on the host', async function () {
+      await xo.call('host.enable', {id: hostId})
+
+      await waitObjectState(xo, hostId, host => {
         expect(host.enabled).to.be.true()
       })
     })
   })
 
   // -----------------------------------------------------------------
+  describe('.createNetwork()', function () {
+    it('create a network')
+  })
+
+  // -----------------------------------------------------------------
 
   describe('.listMissingPatches()', function () {
     it('returns an array of missing patches in the host')
-    it('returns a empty arry if up-to-date')
+    it('returns a empty array if up-to-date')
   })
 
   // ------------------------------------------------------------------
@@ -149,7 +224,7 @@ describe('host', function () {
   describe('.stats()', function () {
     it('returns an array with statistics of the host', async function () {
       const stats = await xo.call('host.stats', {
-        host: host.id
+        host: hostId
       })
       expect(stats).to.be.an.object()
 
