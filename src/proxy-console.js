dfd8133431a89a11c72cec1a9ec095d853d7e951
Julien Fontanet <julien.fontanet@isonoe.net>
2015-09-02 16:15:28 +0200

    Handle errors in consoles proxy.

--- src/proxy-console.js
+++ src/proxy-console.js
@@ -1,6 +1,9 @@
+import createDebug from 'debug'
 import partialStream from 'partial-stream'
 import {connect} from 'tls'
 import {parse} from 'url'
+
+const debug = createDebug('xo:proxy-console')
 
 export default function proxyConsole (ws, vmConsole, sessionId) {
   const url = parse(vmConsole.location)
@@ -20,11 +23,16 @@ export default function proxyConsole (ws, vmConsole, sessionId) {
 
     socket.pipe(partialStream('\r\n\r\n', headers => {
       // TODO: check status code 200.
+      debug('connected')
     })).on('data', data => {
       // Encode to base 64.
       ws.send(data.toString('base64'))
     }).on('end', () => {
       ws.close()
+      debug('disconnected')
+    }).on('error', error => {
+      ws.close()
+      debug('%s', error.stack || error.message || error)
     })
 
     ws
