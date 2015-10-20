d4510c2afedb8987bdea558bd330d593e52afb99
Julien Fontanet <julien.fontanet@isonoe.net>
2015-10-20 18:02:57 +0200

    Put schemas in a dedicated directory.

---
+++ src/schemas/plugin.js
@@ -0,0 +1,41 @@
+export default {
+  $schema: 'http://json-schema.org/draft-04/schema#',
+  type: 'object',
+  properties: {
+    id: {
+      type: 'string',
+      description: 'unique identifier for this plugin'
+    },
+    name: {
+      type: 'string',
+      description: 'unique human readable name for this plugin'
+    },
+    autoload: {
+      type: 'boolean',
+      description: 'whether this plugin is loaded on startup'
+    },
+    loaded: {
+      type: 'boolean',
+      description: 'whether or not this plugin is currently loaded'
+    },
+    unloadable: {
+      type: 'boolean',
+      default: 'true',
+      description: 'whether or not this plugin can be unloaded'
+    },
+    configuration: {
+      type: 'object',
+      description: 'current configuration of this plugin (not present if none)'
+    },
+    configurationSchema: {
+      $ref: 'http://json-schema.org/draft-04/schema#',
+      description: 'configuration schema for this plugin (not present if not configurable)'
+    }
+  },
+  required: [
+    'id',
+    'name',
+    'autoload',
+    'loaded'
+  ]
+}
