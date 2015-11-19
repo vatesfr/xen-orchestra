12c4680501e471420e284e616e313def64cf85b9
wescoeur <ronan.abhamon@gmail.com>
2015-11-19 17:22:55 +0100

    Initial commit

---
+++ packages/xo-server-backup-reports/src/index.js
@@ -0,0 +1,67 @@
+import forEach from 'lodash.foreach'
+
+class BackupReportsXoPlugin {
+  constructor (xo) {
+    this._xo = xo
+  }
+
+  configure (conf) {
+    this._conf = conf
+  }
+
+  load () {
+    this._xo.on('job:terminated', status => {
+      let nSuccess = 0
+      let nCalls = 0
+
+      // Backup status for each vm
+      forEach(status.calls, call => {
+        let vmStatus
+
+        if (call.error) {
+          vmStatus = 'Fail'
+        } else {
+          nSuccess++
+          vmStatus = 'Success'
+        }
+
+        nCalls++
+
+        const vm = this._xo.getObject(call.params.id)
+
+        console.log(
+          `VM Name: ${vm.name_label}
+          UUID: ${call.params.id}
+          Status: ${vmStatus}
+          Start time: ${call.start}
+          End time: ${call.end}
+          Duration: ${call.end - call.start}
+          `
+        )
+      })
+
+      const globalStatus = nSuccess === nCalls
+            ? 'Success'
+            : 'Fail'
+
+      // Global status
+      console.log(
+        `Global status: ${globalStatus}
+        Start time: ${status.start}
+        End time: ${status.end}
+        Duration: ${status.end - status.start}
+        Successful backed up VM number: ${nSuccess}
+        Failed backed up VM: ${nCalls - nSuccess}
+        `
+      )
+    })
+  }
+
+  unload () {
+
+  }
+}
+
+// ===================================================================
+
+export default ({xo}) => new BackupReportsXoPlugin(xo)
