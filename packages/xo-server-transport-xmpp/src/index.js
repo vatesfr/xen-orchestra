74a31f3301df7789edc2675f02ede1ec3e570dc1
wescoeur <ronan.abhamon@gmail.com>
2015-11-27 16:47:16 +0100

    Add schema

--- packages/xo-server-transport-xmpp/src/index.js
+++ packages/xo-server-transport-xmpp/src/index.js
@@ -1,3 +1,55 @@
+export const configurationSchema = {
+  type: 'object',
+
+  properties: {
+    transport: {
+      type: 'object',
+
+      properties: {
+        host: {
+          type: 'string',
+          description: 'host where the XMPP server is located'
+        },
+        port: {
+          type: 'number',
+          description: 'port of the XMPP server (default to 5222)'
+        },
+        user: {
+          type: 'string',
+          description: 'name to use to authenticate'
+        },
+        pass: {
+          type: 'string',
+          description: 'password to use to authenticate'
+        }
+      },
+
+      additionalProperties: false,
+      required: ['user', 'pass']
+    },
+
+    accountOptions: {
+      type: 'object',
+
+      properties: {
+        register: {
+          type: 'boolean',
+          description: 'create a new account if necessary',
+          default: true
+        }
+      },
+
+      additionalProperties: false,
+      required: ['register']
+    }
+  },
+
+  additionalProperties: false,
+  required: ['transport', 'accountOptions']
+}
+
+// ===================================================================
+
 class TransportXmppPlugin {
   constructor (xo) {
 
