a4fa670dc5c4443d9a90e84d4caa1c7a7170785d
Fabrice Marsaud <fabrice.marsaud@vates.fr>
2016-01-26 16:01:03 +0100

    Initial commit

---
+++ packages/xo-remote-parser/README.md
@@ -0,0 +1,52 @@
+# ${pkg.name} [![Build Status](https://travis-ci.org/${pkg.shortGitHubPath}.png?branch=master)](https://travis-ci.org/${pkg.shortGitHubPath})
+
+> ${pkg.description}
+
+## Install
+
+Installation of the [npm package](https://npmjs.org/package/${pkg.name}):
+
+```
+> npm install --save ${pkg.name}
+```
+
+## Usage
+
+**TODO**
+
+## Development
+
+### Installing dependencies
+
+```
+> npm install
+```
+
+### Compilation
+
+The sources files are watched and automatically recompiled on changes.
+
+```
+> npm run dev
+```
+
+### Tests
+
+```
+> npm run test-dev
+```
+
+## Contributions
+
+Contributions are *very* welcomed, either on the documentation or on
+the code.
+
+You may:
+
+- report any [issue](${pkg.bugs})
+  you've encountered;
+- fork and create a pull request.
+
+## License
+
+${pkg.license} © [${pkg.author.name}](${pkg.author.url})
---
+++ packages/xo-remote-parser/src/index.js
@@ -0,0 +1,50 @@
+import filter from 'lodash.filter'
+import map from 'lodash.map'
+import trim from 'lodash.trim'
+
+const sanitizePath = (...paths) => filter(map(paths, s => s && filter(map(s.split('/'), trim)).join('/'))).join('/')
+
+export const parse = (remote) => {
+  const [type, rest] = remote.url.split('://')
+  if (type === 'file') {
+    remote.type = 'local'
+    remote.path = `/${rest}` // FIXME the heading slash has been forgotten on client side first implementation
+  } else if (type === 'nfs') {
+    remote.type = 'nfs'
+    const [host, share] = rest.split(':')
+    remote.path = `/tmp/xo-server/mounts/${remote.id}`
+    remote.host = host
+    remote.share = share
+  } else if (type === 'smb') {
+    remote.type = 'smb'
+    const [auth, smb] = rest.split('@')
+    const [username, password] = auth.split(':')
+    const [domain, sh] = smb.split('\\\\')
+    const [host, path] = sh.split('\0')
+    remote.host = host
+    remote.path = path
+    remote.domain = domain
+    remote.username = username
+    remote.password = password
+  }
+  return remote
+}
+
+export const compose = ({type, host, path, username, password, domain}) => {
+  let url = `${type}://`
+  if (type === 'nfs') {
+    url += `${host}:`
+  }
+  if (type === 'smb') {
+    url += `${username}:${password}@${domain}\\\\${host}`
+  }
+  path = sanitizePath(path)
+  if (type === 'smb') {
+    path = path.split('/')
+    path = '\0' + path.join('\\') // FIXME saving with the windows fashion \ was a bad idea :,(
+  } else {
+    type === 'smb' && (path = `/${path}`) // FIXME file type should have a / too, but it has been forgotten on client side first implementation...
+  }
+  url += path
+  return url
+}
