f4ea39b602a4074a2bacd414671d5376430f6303
Julien Fontanet <julien.fontanet@isonoe.net>
2016-02-03 10:02:39 +0100

    Initial commit.

---
+++ packages/xo-acl-resolver/README.md
@@ -0,0 +1,52 @@
+# xo-acl-resolver [![Build Status](https://travis-ci.org/vatesfr/xo-acl-resolver.png?branch=master)](https://travis-ci.org/vatesfr/xo-acl-resolver)
+
+> [Xen-Orchestra](http://xen-orchestra.com/) internal: do ACLs resolution.
+
+## Install
+
+Installation of the [npm package](https://npmjs.org/package/xo-acl-resolver):
+
+```
+> npm install --save xo-acl-resolver
+```
+
+## Usage
+
+**TODO**
+
+## Development
+
+### Installing dependencies
+
+```
+> npm install
+```
+
+### Compilation
+
+The sources files are watched and automatically recompiled on changes.
+
+```
+> npm run dev
+```
+
+### Tests
+
+```
+> npm run test-dev
+```
+
+## Contributions
+
+Contributions are *very* welcomed, either on the documentation or on
+the code.
+
+You may:
+
+- report any [issue](https://github.com/vatesfr/xo-acl-resolver/issues)
+  you've encountered;
+- fork and create a pull request.
+
+## License
+
+ISC © [Vates SAS](https://vates.fr)
---
+++ packages/xo-acl-resolver/src/index.js
@@ -0,0 +1,118 @@
+// These global variables are not a problem because the algorithm is
+// synchronous.
+let permissionsByObject
+let getObject
+
+// -------------------------------------------------------------------
+
+const authorized = () => true // eslint-disable-line no-unused-vars
+const forbiddden = () => false // eslint-disable-line no-unused-vars
+
+const and = (...checkers) => (object, permission) => { // eslint-disable-line no-unused-vars
+  for (const checker of checkers) {
+    if (!checker(object, permission)) {
+      return false
+    }
+  }
+  return true
+}
+
+const or = (...checkers) => (object, permission) => { // eslint-disable-line no-unused-vars
+  for (const checker of checkers) {
+    if (checker(object, permission)) {
+      return true
+    }
+  }
+  return false
+}
+
+// -------------------------------------------------------------------
+
+const checkMember = memberName => (object, permission) => {
+  const member = object[memberName]
+  return checkAuthorization(member, permission)
+}
+
+const checkSelf = ({ id }, permission) => {
+  const permissionsForObject = permissionsByObject[id]
+
+  return (
+    permissionsForObject &&
+    permissionsForObject[permission]
+  )
+}
+
+// ===================================================================
+
+const checkAuthorizationByTypes = {
+  host: or(checkSelf, checkMember('$pool')),
+
+  message: checkMember('$object'),
+
+  network: or(checkSelf, checkMember('$pool')),
+
+  SR: or(checkSelf, checkMember('$pool')),
+
+  task: checkMember('$host'),
+
+  VBD: checkMember('VDI'),
+
+  // Access to a VDI is granted if the user has access to the
+  // containing SR or to a linked VM.
+  VDI (vdi, permission) {
+    // Check authorization for the containing SR.
+    if (checkAuthorization(vdi.$SR, permission)) {
+      return true
+    }
+
+    // Check authorization for each of the connected VMs.
+    for (const { VM: vm } of vdi.$VBDs) {
+      if (checkAuthorization(vm, permission)) {
+        return true
+      }
+    }
+
+    return false
+  },
+
+  VIF: or(checkMember('$network'), checkMember('$VM')),
+
+  VM: or(checkSelf, checkMember('$container')),
+
+  'VM-snapshot': checkMember('$snapshot_of'),
+
+  'VM-template': authorized
+}
+
+// Hoisting is important for this function.
+function checkAuthorization (objectId, permission) {
+  const object = getObject(objectId)
+  const checker = checkAuthorizationByTypes[object.type] || checkSelf
+
+  return checker(object, permission)
+}
+
+// -------------------------------------------------------------------
+
+export default (
+  permissionsByObject_,
+  getObject_,
+  permissions
+) => {
+  // Assign global variables.
+  permissionsByObject = permissionsByObject_
+  getObject = getObject_
+
+  try {
+    for (const [objectId, permission] of permissions) {
+      if (!checkAuthorization(objectId, permission)) {
+        return false
+      }
+    }
+
+    return true
+  } finally {
+    // Free the global variables.
+    permissionsByObject = getObject = null
+  }
+}
