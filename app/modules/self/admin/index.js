510f6e982d50f9850cb93867befe0b6414c6414f
Fabrice Marsaud <fabrice.marsaud@vates.fr>
2016-02-08 12:11:46 +0100

    constraint reolutions

--- app/modules/self/admin/index.js
+++ app/modules/self/admin/index.js
@@ -3,9 +3,11 @@ import uiBootstrap from 'angular-ui-bootstrap'
 import uiRouter from 'angular-ui-router'
 
 import assign from 'lodash.assign'
+import differenceBy from 'lodash.differenceby'
 import filter from 'lodash.filter'
 import forEach from 'lodash.foreach'
 import intersection from 'lodash.intersection'
+import map from 'lodash.map'
 
 import view from './view'
 
@@ -18,56 +20,103 @@ export default angular.module('self.admin', [
   .config(function ($stateProvider) {
     $stateProvider.state('self.admin', {
       url: '/admin',
+      resolve: {
+        users (xo) {
+          return xo.user.getAll()
+        },
+        groups (xo) {
+          return xo.group.getAll()
+        }
+      },
       controller: 'AdminCtrl as ctrl',
       template: view
     })
   })
-  .controller('AdminCtrl', function (xoApi, $scope) {
-    this.containers = {}
+  .controller('AdminCtrl', function (xoApi, $scope, users, groups) {
+    this.sizeUnits = ['MiB', 'GiB', 'TiB']
+    this.memoryUnit = this.sizeUnits[0]
+    this.diskUnit = this.sizeUnits[1]
+
     this.srs = {}
     this.networks = {}
     this.templates = {}
+    this.eligibleHosts = {}
+    let eligibleHosts = {}
+
+    this.pools = xoApi.getView('pool').all
     const hosts = xoApi.getView('host').all
-    const pools = xoApi.getView('pool').all
     const srs = xoApi.getView('SR').all
     const networks = xoApi.getView('network').all
     const vmTemplatesByContainer = xoApi.getIndex('vmTemplatesByContainer')
-    this._net = networks
-    this._hosts = hosts
-    const assemble = () => this.containers = assign(hosts, pools)
 
-    $scope.$watchCollection(() => hosts, assemble)
-    $scope.$watchCollection(() => pools, assemble)
+    users.push(...groups)
+    this.subjects = users
 
-    this.toggleType = (toggle, type) => {
-      const selectedContainers = this.selectedContainers && this.selectedContainers.slice() || []
-      if (toggle) {
-        const objects = filter(this.containers, {type})
-        forEach(objects, object => { selectedContainers.indexOf(object) === -1 && selectedContainers.push(object) })
-        this.selectedContainers = selectedContainers
+    const filterSrs = () => filter(srs, sr => {
+      let found = false
+      forEach(this.selectedPools, pool => !(found = sr.$poolId === pool.id))
+      return found
+    })
+
+    const filterNetworks = () => filter(networks, network => {
+      let found = false
+      forEach(this.selectedPools, pool => !(found = network.$poolId === pool.id))
+      return found
+    })
+
+    $scope.$watchCollection(() => this.selectedPools, () => {
+      this.srs = filterSrs()
+      this.networks = filterNetworks()
+      eligibleHosts = filter(hosts, host => {
+        let found = false
+        forEach(this.selectedPools, pool => !(found = host.$poolId === pool.id))
+        return found
+      })
+      const vmTemplates = {}
+      forEach(this.selectedPools, pool => assign(vmTemplates, vmTemplatesByContainer[pool.id]))
+      this.vmTemplates = vmTemplates
+    })
+
+    $scope.$watchCollection(() => this.selectedSrs, () => {
+      const keptHosts = filter(eligibleHosts, host => {
+        let kept = false
+        forEach(this.selectedSrs, sr => !(kept = intersection(sr.$PBDs, host.$PBDs).length > 0))
+        return kept
+      })
+      this.eligibleHosts = keptHosts
+      this.excludedHosts = differenceBy(map(hosts), keptHosts, item => item && item.id)
+      if (!this.selectedSrs || !this.selectedSrs.length) {
+        this.networks = filterNetworks()
       } else {
-        const keptObjects = []
-        for (let index in this.selectedContainers) {
-          const object = this.selectedContainers[index]
-          if (object.type !== type) {
-            keptObjects.push(object)
-          }
-        }
-        this.selectedContainers = keptObjects
+        const keptNetworks = filter(networks, network => {
+          let kept = false
+          forEach(this.eligibleHosts, host => {
+            return !(kept = intersection(network.PIFs, host.PIFs).length > 0)
+          })
+          return kept
+        })
+        this.networks = keptNetworks
       }
-    }
+    })
 
-    $scope.$watchCollection(() => this.selectedContainers, () => {
-      this.srs = filter(srs, sr => {
-        let found = false
-        forEach(this.selectedContainers, container => !(found = (sr.$container === container.id || sr.$poolId === container.id)))
-        return found
+    $scope.$watchCollection(() => this.selectedNetworks, () => {
+      const keptHosts = filter(eligibleHosts, host => {
+        let kept = false
+        forEach(this.selectedNetworks, network => !(kept = intersection(network.PIFs, host.PIFs).length > 0))
+        return kept
       })
-      this.networks = filter(networks, network => {
-        let found = false
-        forEach(this.selectedContainers, container => !(found = (network.$poolId === container.id || intersection(network.PIFs, container.PIFs).length > 0)))
-        return found
-      })
+      this.eligibleHosts = keptHosts
+      this.excludedHosts = differenceBy(map(hosts), keptHosts, item => item && item.id)
+      if (!this.selectedNetworks || !this.selectedNetworks.length) {
+        this.srs = filterSrs()
+      } else {
+        const keptSrs = filter(srs, sr => {
+          let kept = false
+          forEach(this.eligibleHosts, host => !(kept = intersection(sr.$PBDs, host.$PBDs).length > 0))
+          return kept
+        })
+        this.srs = keptSrs
+      }
     })
   })
 
