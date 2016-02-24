1de33cd4ca11c5582e0add7831802d4c46dd9b42
Pierre <pierre.donias@gmail.com>
2016-02-24 11:22:24 +0100

    Self service dashboard (Fix #741)
    Fixed no limit condition and icons.

---
+++ app/modules/self/dashboard/index.js
@@ -0,0 +1,61 @@
+import angular from 'angular'
+import uiBootstrap from 'angular-ui-bootstrap'
+import uiRouter from 'angular-ui-router'
+
+import view from './view'
+
+// ====================================================================
+
+export default angular.module('self.dashboard', [
+  uiRouter,
+  uiBootstrap
+])
+  .config(function ($stateProvider) {
+    $stateProvider.state('self.dashboard', {
+      url: '/dashboard',
+      resolve: {
+        users (xo) {
+          return xo.user.getAll()
+        },
+        groups (xo) {
+          return xo.group.getAll()
+        }
+      },
+      controller: 'DashboardCtrl as ctrl',
+      template: view
+    })
+  })
+  .controller('DashboardCtrl', function (xo, xoApi, $scope, $window, users, groups, sizeToBytesFilter, bytesToSizeFilter) {
+    $window.bytesToSize = bytesToSizeFilter //  FIXME dirty workaround to custom a Chart.js tooltip template
+
+    this.resourceSets = {}
+    const loadSets = () => {
+      xo.resourceSet.getAll()
+      .then(sets => {
+        this.resourceSets = sets
+        this.resourceSet = this.resourceSets[0]
+      })
+    }
+    loadSets()
+
+    $scope.$watch('ctrl.resourceSet', (resourceSet) => {
+      if (!resourceSet) {
+        return
+      }
+      this.cpusStats = [0, 0]
+      this.memoryStats = [0, 0]
+      this.diskStats = [0, 0]
+      if (resourceSet.limits.cpus) {
+        this.cpusStats = [resourceSet.limits.cpus.total - resourceSet.limits.cpus.available, resourceSet.limits.cpus.available]
+      }
+      if (resourceSet.limits.memory) {
+        this.memoryStats = [resourceSet.limits.memory.total - resourceSet.limits.memory.available, resourceSet.limits.memory.available]
+      }
+      if (resourceSet.limits.disk) {
+        this.diskStats = [resourceSet.limits.disk.total - resourceSet.limits.disk.available, resourceSet.limits.disk.available]
+      }
+    })
+  })
+
+  // A module exports its name.
+  .name
