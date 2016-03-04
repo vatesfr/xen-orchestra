bff42954d1ba12417082298922bfafbe780bde2a
Pierre <pierre.donias@gmail.com>
2016-03-04 12:45:30 +0100

    `createNetwork` on pool: network creation on pool.master

--- src/api/pool.js
+++ src/api/pool.js
@@ -143,3 +143,41 @@ getLicenseState.params = {
 getLicenseState.resolve = {
   pool: ['pool', 'pool', 'administrate']
 }
+
+// -------------------------------------------------------------------
+
+export async function createNetwork ({ pool, name, description, pif, mtu, vlan }) {
+  const xapi = this.getXapi(pool.master)
+
+  description = description || 'Created with Xen Orchestra'
+
+  const network_ref = await xapi.call('network.create', {
+    name_label: name,
+    name_description: description,
+    MTU: mtu || '1500',
+    other_config: {}
+  })
+
+  if (pif !== null) {
+    vlan = vlan || '0'
+    pif = this.getObject(pif, 'PIF')
+    await xapi.call('pool.create_VLAN_from_PIF', pif._xapiRef, network_ref, vlan)
+  }
+
+  return true
+}
+
+createNetwork.params = {
+  pool: { type: 'string' },
+  name: { type: 'string' },
+  description: { type: 'string', optional: true },
+  pif: { type: 'string', optional: true },
+  mtu: { type: 'string', optional: true },
+  vlan: { type: 'string', optional: true }
+}
+
+createNetwork.resolve = {
+  pool: ['pool', 'pool', 'administrate']
+}
+createNetwork.permission = 'admin'
+exports.createNetwork = createNetwork
