65807bf35dc70c948a9321888c8126c16606bb56
Pierre <pierre.donias@gmail.com>
2016-03-09 11:43:19 +0100

    Fixes.

--- src/api/network.js
+++ src/api/network.js
@@ -1,4 +1,3 @@
-<<<<<<< 91431201771f54434ebda173c26bda1c3df405d4
 export async function create ({ pool, name, description, pif, mtu = 1500, vlan = 0 }) {
   return this.getXapi(pool).createNetwork({
     name,
@@ -7,20 +6,6 @@ export async function create ({ pool, name, description, pif, mtu = 1500, vlan =
     mtu: +mtu,
     vlan: +vlan
   })
-=======
-export async function create ({ pool, name, description, pif, mtu, vlan }) {
-  if (pif) {
-    pif = this.getObject(pif, 'PIF')
-  }
-  const params = {
-    name,
-    description,
-    pif: pif && pif._xapiRef,
-    mtu: mtu || '1500',
-    vlan: vlan || '0'
-  }
-  return this.getXapi(pool).createNetwork(params)
->>>>>>> `network.create` instead of `createNetwork` for host and pool
 }
 
 create.params = {
@@ -28,20 +13,11 @@ create.params = {
   name: { type: 'string' },
   description: { type: 'string', optional: true },
   pif: { type: 'string', optional: true },
-<<<<<<< 91431201771f54434ebda173c26bda1c3df405d4
   mtu: { type: ['integer', 'string'], optional: true },
   vlan: { type: ['integer', 'string'], optional: true }
-=======
-  mtu: { type: 'string', optional: true },
-  vlan: { type: 'string', optional: true }
->>>>>>> `network.create` instead of `createNetwork` for host and pool
 }
 
 create.resolve = {
   pool: ['pool', 'pool', 'administrate']
 }
 create.permission = 'admin'
-<<<<<<< 91431201771f54434ebda173c26bda1c3df405d4
-=======
-exports.create = create
->>>>>>> `network.create` instead of `createNetwork` for host and pool
