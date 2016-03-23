269bf4feec863c24f72fd87ef1c3545a4f7bffdc
Julien Fontanet <julien.fontanet@isonoe.net>
2016-03-23 18:40:47 +0100

    Various updates.

--- src/xo-app/home/index.js
+++ src/xo-app/home/index.js
@@ -4,28 +4,52 @@ import flow from 'lodash/flow'
 import map from 'lodash/map'
 import React, { Component } from 'react'
 import sortBy from 'lodash/fp/sortBy'
-import { connectStore } from 'utils'
+import { connectStore, createFilter } from 'utils'
 import { createSelector } from 'reselect'
 import { Link } from 'react-router'
 
 @connectStore({
   vms: createSelector(
-    (state) => state.objects,
+    (state, props) => state.objects,
     flow(filter({ type: 'VM' }), sortBy('name_label'))
   )
 })
 export default class extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      filter: ''
+    }
+
+    this.getFilteredVms = createFilter(
+      () => this.props.vms,
+      () => this.state.filter,
+      (vm) => vm.name_label
+    )
+  }
+
   render () {
     const { vms } = this.props
+    const filteredVms = this.getFilteredVms()
 
     return <div>
       <h1>{_('homePage')}</h1>
       {vms.length
-        ? <ul>
-          {map(vms, (vm) => <li>
-            <Link to={`/vms/${vm.id}`}>{vm.name_label}</Link> ({vm.power_state})
-          </li>)}
-        </ul>
+        ? <div>
+          <p>
+            <input type='text' onChange={(event) => {
+              this.setState({
+                filter: event.target.value
+              })
+            }} />
+          </p>
+          <ul>
+            {map(filteredVms, (vm) => <li key={vm.id}>
+              <Link to={`/vms/${vm.id}`}>{vm.name_label}</Link> ({vm.power_state})
+            </li>)}
+          </ul>
+        </div>
         : <p>There are no VMs</p>
       }
     </div>
