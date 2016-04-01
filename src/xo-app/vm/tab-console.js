63602216eb09e3135790ad1c5c5e3ef18be9be35
Olivier Lambert <olivier.lambert@vates.fr>
2016-04-01 14:38:58 +0200

    using icon component

--- src/xo-app/vm/tab-console.js
+++ src/xo-app/vm/tab-console.js
@@ -1,4 +1,5 @@
 import _ from 'messages'
+import Icon from 'icon'
 import NoVnc from 'react-novnc'
 import React from 'react'
 import { resolveUrl } from 'xo'
@@ -17,25 +18,25 @@ export default ({
   <Row className='text-xs-center'>
     <Col smallSize={3}>
       <p>
-        <i className='xo-icon-cpu fa-3x'>&nbsp;</i>
+        <Icon icon='cpu' size={2} />&nbsp;
         {statsOverview && <CpuSparkLines data={statsOverview} />}
       </p>
     </Col>
     <Col smallSize={3}>
       <p>
-        <i className='xo-icon-memory fa-3x'>&nbsp;</i>
+        <Icon icon='memory' size={2} />&nbsp;
         {statsOverview && <MemorySparkLines data={statsOverview} />}
       </p>
     </Col>
     <Col smallSize={3}>
       <p>
-        <i className='xo-icon-disk fa-3x'>&nbsp;</i>
+        <Icon icon='disk' size={2} />&nbsp;
         {statsOverview && <XvdSparkLines data={statsOverview} />}
       </p>
     </Col>
     <Col smallSize={3}>
       <p>
-        <i className='xo-icon-network fa-3x'>&nbsp;</i>
+        <Icon icon='network' size={2} />&nbsp;
        {statsOverview && <VifSparkLines data={statsOverview} />}
       </p>
     </Col>
@@ -51,7 +52,7 @@ export default ({
         </select>
         <span className='input-group-btn'>
           <button className='btn btn-secondary'>
-            <i className='xo-icon-vm-eject'></i>
+            <Icon icon='vm-eject' />
           </button>
         </span>
       </div>
@@ -61,19 +62,19 @@ export default ({
         <input type='text' className='form-control'></input>
         <span className='input-group-btn'>
           <button className='btn btn-secondary'>
-            <i className='xo-icon-clipboard'>&nbsp;</i>{_('copyToClipboardLabel')}
+            <Icon icon='clipboard' /> {_('copyToClipboardLabel')}
           </button>
         </span>
       </div>
     </Col>
     <Col smallSize={2}>
-      <button className='btn btn-secondary'><i className='xo-icon-vm-keyboard'>&nbsp;</i>{_('ctrlAltDelButtonLabel')}</button>
+      <button className='btn btn-secondary'><Icon icon='vm-keyboard' /> {_('ctrlAltDelButtonLabel')}</button>
     </Col>
   </Row>
   <Row className='console'>
     <Col smallSize={12}>
       <NoVnc url={resolveUrl(`consoles/${vm.id}`)} />
-      <p><em><i className='xo-icon-info'>&nbsp;</i>{_('tipLabel')} {_('tipConsoleLabel')}</em></p>
+      <p><em><Icon icon='info' /> {_('tipLabel')} {_('tipConsoleLabel')}</em></p>
     </Col>
   </Row>
 </div>
--- src/xo-app/vm/tab-snapshots.js
+++ src/xo-app/vm/tab-snapshots.js
@@ -15,10 +15,10 @@ export default ({
       <Col smallSize={6} className='text-xs-center'>
         <br/>
         <h4>{_('noSnapshot')}</h4>
-        <p><em><i className='xo-icon-info'>&nbsp;</i>{_('tipLabel')} {_('tipCreateSnapshotLabel')}</em></p>
+        <p><em><Icon icon='info' size={1} /> {_('tipLabel')} {_('tipCreateSnapshotLabel')}</em></p>
       </Col>
       <Col smallSize={6} className='text-xs-center'>
-        <p><button type='button' className='btn btn-lg btn-secondary btn-huge'><i className='xo-icon-vm-snapshot'></i></button></p>
+        <p><button type='button' className='btn btn-lg btn-secondary btn-huge'><Icon icon='vm-snapshot' size={1} /> </button></p>
       </Col>
     </Row>
     : [<Row>
