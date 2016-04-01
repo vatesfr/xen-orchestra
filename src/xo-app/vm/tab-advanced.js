e21934fd551f506ccc7efceeed094cf327e83144
Olivier Lambert <olivier.lambert@vates.fr>
2016-04-01 16:59:46 +0200

    more work on VM advanced tab

--- src/xo-app/vm/tab-advanced.js
+++ src/xo-app/vm/tab-advanced.js
@@ -1,5 +1,6 @@
 import _ from 'messages'
 import CopyToClipboard from 'react-copy-to-clipboard'
+import isEmpty from 'lodash/isEmpty'
 import React from 'react'
 import { Row, Col } from 'grid'
 import { normalizeXenToolsStatus, osFamily } from 'utils'
@@ -10,100 +11,103 @@ export default ({
   <Row>
     <Col smallSize={12}>
       <h3>{_('xenSettingsLabel')}</h3>
-      <table className='table table-hover'>
-        <tr>
-          <th>{_('uuid')}</th>
-          <td className='copy-to-clipboard'>
-            {vm.uuid}&nbsp;
-            <CopyToClipboard text={vm.uuid}>
-              <button className='btn btn-sm btn-secondary btn-copy-to-clipboard'>
-                <i className='xo-icon-clipboard'></i>
-              </button>
-            </CopyToClipboard>
-          </td>
-        </tr>
-        <tr>
-          <th>{_('virtualizationMode')}</th>
-          <td>
-            {vm.virtualizationMode === 'pv'
-              ? _('paraVirtualizedMode')
-              : _('hardwareVirtualizedMode')
-            }
-          </td>
-        </tr>
-        <tr>
-        {vm.PV_args
-          ? [
-            <th key={0}>{_('pvArgsLabel')}</th>,
-            <td key={1}>{vm.PV_args}</td>
-          ]
-          : null
-        }
-        </tr>
-        <tr>
-          <th>{_('cpuWeightLabel')}</th>
-          {vm.cpuWeight
-            ? <td>{vm.cpuWeight}</td>
-            : <td>{_('defaultCpuWeight')}</td>
+      <table className='table'>
+        <tbody>
+          <tr>
+            <th>{_('uuid')}</th>
+            <td className='copy-to-clipboard'>
+              {vm.uuid}&nbsp;
+              <CopyToClipboard text={vm.uuid}>
+                <button className='btn btn-sm btn-secondary btn-copy-to-clipboard'>
+                  <i className='xo-icon-clipboard'></i>
+                </button>
+              </CopyToClipboard>
+            </td>
+          </tr>
+          <tr>
+            <th>{_('virtualizationMode')}</th>
+            <td>
+              {vm.virtualizationMode === 'pv'
+                ? _('paraVirtualizedMode')
+                : _('hardwareVirtualizedMode')
+              }
+            </td>
+          </tr>
+          {vm.PV_args
+            ? [
+              <tr>,
+                <th key={0}>{_('pvArgsLabel')}</th>,
+                <td key={1}>{vm.PV_args}</td>,
+              </tr>
+            ]
+            : null
           }
-        </tr>
-        <tr>
-          <th>{_('autoPowerOn')}</th>
-          <td>{vm.auto_poweron ? _('enabledAutoPowerOn') : _('disabledAutoPowerOn')}</td>
-        </tr>
-        <tr>
-          <th>{_('ha')}</th>
-          <td>{vm.high_availability ? _('enabledHa') : _('disabledHa')}</td>
-        </tr>
+          <tr>
+            <th>{_('cpuWeightLabel')}</th>
+            {vm.cpuWeight
+              ? <td>{vm.cpuWeight}</td>
+              : <td>{_('defaultCpuWeight')}</td>
+            }
+          </tr>
+          <tr>
+            <th>{_('autoPowerOn')}</th>
+            <td>{vm.auto_poweron ? _('enabledAutoPowerOn') : _('disabledAutoPowerOn')}</td>
+          </tr>
+          <tr>
+            <th>{_('ha')}</th>
+            <td>{vm.high_availability ? _('enabledHa') : _('disabledHa')}</td>
+          </tr>
+        </tbody>
       </table>
-      <hr/>
+      <br/>
       <h3>{_('vmLimitsLabel')}</h3>
-      <Row>
-        <Col smallSize={6}>
-          <dl className='dl-horizontal'>
-            <dt>{_('vmCpuLimitsLabel')}</dt>
-            <dd>TODO</dd>
-          </dl>
-        </Col>
-        <Col smallSize={6}>
-          <dl className='dl-horizontal'>
-            <dt>{_('vmMemoryLimitsLabel')}</dt>
-            <dd>TODO</dd>
-          </dl>
-        </Col>
-      </Row>
-      <hr/>
+      <table className='table table-hover'>
+        <tbody>
+          <tr>
+            <th>{_('vmCpuLimitsLabel')}</th>
+            <td></td>
+          </tr>
+          <tr>
+            <th>{_('vmMemoryLimitsLabel')}</th>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+      <br/>
       <h3>{_('guestOsLabel')}</h3>
-      <Row>
-        <Col smallSize={4}>
-          <dl className='dl-horizontal'>
-            <dt>{_('xenToolsStatus')}</dt>
-            <dd>{_('xenToolsStatusValue', { status: normalizeXenToolsStatus(vm.xenTools) })}</dd>
-          </dl>
-        </Col>
-        <Col smallSize={4}>
-          <dl className='dl-horizontal'>
-            <dt>{_('osName')}</dt>
-            <dd><i className={'icon-' + osFamily(vm.os_version.distro)} />{vm.os_version ? vm.os_version.name : _('unknownOsName')}</dd>
-          </dl>
-        </Col>
-        <Col smallSize={4}>
-          <dl className='dl-horizontal'>
-            <dt>{_('osKernel')}</dt>
-            <dd>{vm.os_version ? vm.os_version.uname ? vm.os_version.uname : _('unknownOsKernel') : _('unknownOsKernel')}</dd>
-          </dl>
-        </Col>
-      </Row>
-      <hr/>
+      <table className='table table-hover'>
+        <tbody>
+          <tr>
+            <th>{_('xenToolsStatus')}</th>
+            <td>{_('xenToolsStatusValue', { status: normalizeXenToolsStatus(vm.xenTools) })}</td>
+          </tr>
+          <tr>
+            <th>{_('osName')}</th>
+            <td>{isEmpty(vm.os_version)
+                ? _('unknownOsName')
+                : [
+                  <i className={'icon-' + osFamily(vm.os_version.distro)} />,
+                  ' ' + vm.os_version.name
+                ]
+                }
+            </td>
+          </tr>
+          <tr>
+            <th>{_('osKernel')}</th>
+            <td>{vm.os_version ? vm.os_version.uname ? vm.os_version.uname : _('unknownOsKernel') : _('unknownOsKernel')}</td>
+          </tr>
+        </tbody>
+      </table>
+      <br/>
       <h3>{_('miscLabel')}</h3>
-      <Row>
-        <Col smallSize={12}>
-          <dl className='dl-horizontal'>
-            <dt>{_('originalTemplate')}</dt>
-            <dd>{vm.other.base_template_name ? vm.other.base_template_name : _('unknownOriginalTemplate')}</dd>
-          </dl>
-        </Col>
-      </Row>
+      <table className='table table-hover'>
+        <tbody>
+          <tr>
+            <th>{_('originalTemplate')}</th>
+            <td>{vm.other.base_template_name ? vm.other.base_template_name : _('unknownOriginalTemplate')}</td>
+          </tr>
+        </tbody>
+      </table>
     </Col>
   </Row>
 </div>
