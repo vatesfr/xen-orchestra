890d733bf8b89391024160c55f27fd6fb224d8a1
Olivier Lambert <olivier.lambert@vates.fr>
2016-04-12 15:19:05 +0200

    snapshot name edition

--- src/xo-app/vm/tab-snapshots.js
+++ src/xo-app/vm/tab-snapshots.js
@@ -3,8 +3,10 @@ import Icon from 'icon'
 import isEmpty from 'lodash/isEmpty'
 import map from 'lodash/map'
 import React from 'react'
+import xo from 'xo'
 import { FormattedRelative, FormattedTime } from 'react-intl'
 import { Row, Col } from 'grid'
+import { Text } from 'editable'
 
 export default ({
   snapshots,
@@ -39,7 +41,11 @@ export default ({
             {map(snapshots, (snapshot) =>
               <tr key={snapshot.id}>
                 <td><FormattedTime value={snapshot.snapshot_time * 1000} minute='numeric' hour='numeric' day='numeric' month='long' year='numeric'/> (<FormattedRelative value={snapshot.snapshot_time * 1000}/>)</td>
-                <td>{snapshot.name_label}</td>
+                <td>
+                  <Text onChange={(value) => xo.call('vm.set', { id: snapshot.id, name_label: value })}>
+                    {snapshot.name_label}
+                  </Text>
+                </td>
                 <td><i className='xo-icon-export xo-icon-action-row'></i> <i className='xo-icon-snapshot-revert xo-icon-action-row'></i> <i className='xo-icon-snapshot-delete xo-icon-action-row'></i></td>
               </tr>
             )}
