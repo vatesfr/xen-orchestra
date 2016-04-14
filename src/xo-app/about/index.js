afdab8dcdea56772bafcd9415b9b4a635164d1c7
Julien Fontanet <julien.fontanet@isonoe.net>
2016-04-14 18:54:14 +0200

    Fix About page.

--- src/xo-app/about/index.js
+++ src/xo-app/about/index.js
@@ -1,6 +1,5 @@
 import _ from 'messages'
 import React from 'react'
-import { routes } from 'utils'
 
-const About = routes(() => <h1>{_('aboutPage')}</h1>)
+const About = () => <h1>{_('aboutPage')}</h1>
 export default About
