f161d7da055d9df699ea04f54b61ed37a8f96292
Pierre <pierre.donias@gmail.com>
2016-04-19 14:12:39 +0200

    [WIP] Sticky header

--- src/xo-app/navbar/index.js
+++ src/xo-app/navbar/index.js
@@ -4,9 +4,10 @@ export default class Navbar extends Component {
   render () {
     const { children } = this.props
     return <div>
-      <nav className='xo-navbar'>
+      <nav style={{maxHeight: '10em'}} className='xo-navbar'>
         {children}
       </nav>
+      <div style={{height: '10em'}}></div>
     </div>
   }
 }
