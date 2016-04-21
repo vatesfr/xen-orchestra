f388a4fa5151f6109a0f7631051051e4e2353225
Pierre <pierre.donias@gmail.com>
2016-04-21 15:41:23 +0200

    Vertical list wizard.

--- src/xo-app/new/vm/index.js
+++ src/xo-app/new/vm/index.js
@@ -14,9 +14,9 @@ export default class NewVm extends Component {
   render () {
     return <div>
       <h1>{_('newVmCreateNewVmOn')}lab1</h1>
-      <form className='blocks'>
-      {/* INFOS */ }
-        <Block icon='infos' title='newVmInfoPanel'>
+      <form>
+        {/* INFOS */ }
+        <Step icon='infos' title='newVmInfoPanel'>
           <FormItem label='newVmNameLabel'>
             <input className='form-control' type='text'/>
           </FormItem>
@@ -30,18 +30,18 @@ export default class NewVm extends Component {
           <FormItem label='newVmDescriptionLabel'>
             <input className='form-control' type='text'/>
           </FormItem>
-        </Block>
+        </Step>
         {/* PERFORMANCES */ }
-        <Block icon='perf' title='newVmPerfPanel'>
+        <Step icon='perf' title='newVmPerfPanel'>
           <FormItem label='newVmVcpusLabel'>
             <input className='form-control' type='number'/>
           </FormItem>
           <FormItem label='newVmRamLabel'>
             <input className='form-control' type='text'/>
           </FormItem>
-        </Block>
+        </Step>
         {/* INSTALL SETTINGS */ }
-        <Block icon='install-settings' title='newVmInstallSettingsPanel'>
+        <Step icon='install-settings' title='newVmInstallSettingsPanel'>
           <FormItem label='newVmIsoDvdLabel'>
             <select className='form-control'>
               {map(['ISO 1', 'ISO 2', 'DVD 1', 'DVD 2'], (iso, index) =>
@@ -59,85 +59,66 @@ export default class NewVm extends Component {
           <FormItem label='newVmPvArgsLabel'>
             <input className='form-control' type='text'/>
           </FormItem>
-        </Block>
+        </Step>
         {/* INTERFACES */ }
-        <Block icon='interfaces' title='newVmInterfacesPanel'>
-          <div style={{margin: '0.5em'}}>
-            <Row>
-              <Col size={10}>
-                <div className='form-inline'>
-                  <div className='form-group'>
-                    <label>{_('newVmMacLabel')}</label>&nbsp;&nbsp;
-                    <input className='form-control' type='text'/>
-                  </div>
-                  &nbsp;&nbsp;
-                  <div className='form-group'>
-                    <label>{_('newVmNetworkLabel')}</label>&nbsp;&nbsp;
-                    <select className='form-control'>
-                      {map(['Network 1', 'Network 2', 'Network 3'], (network, index) =>
-                        <option key={index} value={index}>{network}</option>
-                    )}
-                    </select>
-                  </div>
-                </div>
-              </Col>
-              <Col size={1}>
-                <Button bsStyle='secondary'>
-                  <Icon icon='new-vm-remove' />
-                </Button>
-              </Col>
-            </Row>
+        <Step icon='interfaces' title='newVmInterfacesPanel' column>
+          <div>
+            <div>
+              <label>{_('newVmMacLabel')}</label>&nbsp;&nbsp;
+              <input className='form-control' type='text'/>
+            </div>
+            <div>
+              <label>{_('newVmNetworkLabel')}</label>&nbsp;&nbsp;
+              <select className='form-control'>
+                {map(['Network 1', 'Network 2', 'Network 3'], (network, index) =>
+                  <option key={index} value={index}>{network}</option>
+              )}
+              </select>
+            </div>
+            <Button bsStyle='secondary'>
+              <Icon icon='new-vm-remove' />
+            </Button>
           </div>
           <Button bsStyle='secondary'>
             <Icon icon='new-vm-add' />&nbsp;
             {_('newVmAddInterface')}
           </Button>
-        </Block>
+        </Step>
         {/* DISKS */ }
-        <Block icon='disks' full title='newVmDisksPanel'>
-          <div style={{margin: '0.5em'}}>
-            <Row>
-              <Col size={10}>
-                <div className='form-inline'>
-                  <div className='form-group'>
-                    <label>{_('newVmSrLabel')}</label>&nbsp;&nbsp;
-                    <input className='form-control' type='text'/>
-                  </div>
-                  &nbsp;&nbsp;
-                  <div className='form-group checkbox'>
-                    <label>
-                      <input type='checkbox'/>&nbsp;
-                      {_('newVmBootableLabel')}
-                    </label>
-                  </div>
-                  &nbsp;&nbsp;&nbsp;&nbsp;
-                  <div className='form-group'>
-                    <label>{_('newVmNameLabel')}</label>&nbsp;&nbsp;
-                    <input className='form-control' type='text'/>
-                  </div>
-                  &nbsp;&nbsp;
-                  <div className='form-group'>
-                    <label>{_('newVmDescriptionLabel')}</label>&nbsp;&nbsp;
-                    <input className='form-control' type='text'/>
-                  </div>
-                </div>
-              </Col>
-              <Col size={1}>
-                <Button bsStyle='secondary'>
-                  <Icon icon='new-vm-remove' />
-                </Button>
-              </Col>
-            </Row>
+        <Step icon='disks' title='newVmDisksPanel' column>
+          <div>
+            <div>
+              <label>{_('newVmSrLabel')}</label>&nbsp;&nbsp;
+              <input className='form-control' type='text'/>
+            </div>
+            &nbsp;&nbsp;
+            <div className='checkbox'>
+              <label>
+                <input type='checkbox'/>&nbsp;
+                {_('newVmBootableLabel')}
+              </label>
+            </div>
+            <div>
+              <label>{_('newVmNameLabel')}</label>&nbsp;&nbsp;
+              <input className='form-control' type='text'/>
+            </div>
+            <div>
+              <label>{_('newVmDescriptionLabel')}</label>&nbsp;&nbsp;
+              <input className='form-control' type='text'/>
+            </div>
+            <Button bsStyle='secondary'>
+              <Icon icon='new-vm-remove' />
+            </Button>
           </div>
           <Button bsStyle='secondary'>
             <Icon icon='new-vm-add' />&nbsp;
             {_('newVmAddDisk')}
           </Button>
-        </Block>
+        </Step>
         {/* SUMMARY */ }
-        <Block icon='summary' full title='newVmSummaryPanel'>
+        <Step icon='summary' title='newVmSummaryPanel'>
           Summary panel.
-        </Block>
+        </Step>
         <div style={{width: '100%'}}>
           <Row>
             <Col size={1} offset={3}>
@@ -159,28 +140,26 @@ export default class NewVm extends Component {
   }
 }
 
-const Block = ({ icon, title, full, children }) => <div className={classNames(
-  'block',
-  full ? 'full' : 'half'
-)}>
-  <div className='card-new-vm'>
-    <div className='card-header-new-vm'>
-      {icon && <Icon icon={`new-vm-${icon}`} />} {_(title)}
-    </div>
-    <div className='card-block-new-vm'>
-      {children}
-    </div>
+const Step = ({ icon, title, column, children }) => <Row className='wizard-step'>
+  <div style={{flex: '0 0 15em'}}>
+    <h4>{icon && <Icon icon={`new-vm-${icon}`} />} {_(title)}</h4>
   </div>
-</div>
-Block.propTypes = {
+  <div style={{flex: '0.9 1 50%'}} className={classNames(
+      'wizard-block',
+      column && 'column'
+  )}>
+    {children}
+  </div>
+</Row>
+Step.propTypes = {
   icon: PropTypes.string,
   title: PropTypes.string.isRequired
 }
 
-const FormItem = ({ label, children }) => <Row className='form-group'>
-  <Col largeSize={3}><label className='form-control-label'>{_(label)}</label></Col>
-  <Col largeSize={9}>{children}</Col>
-</Row>
+const FormItem = ({ label, children }) => <div style={{flex: '1'}} className='form-group'>
+  <label style={{whiteSpace: 'nowrap'}}>{_(label)}</label>&nbsp;&nbsp;
+  {children}
+</div>
 FormItem.propTypes = {
   label: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired
