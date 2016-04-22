9dc2c4d39ffb6a89957b1894642e4c152a9302dc
Pierre <pierre.donias@gmail.com>
2016-04-22 18:04:45 +0200

    [WIP] Generic page header

--- src/xo-app/index.js
+++ src/xo-app/index.js
@@ -11,6 +11,7 @@ import {
 } from 'utils'
 
 import About from './about'
+import Header from './header'
 import Home from './home'
 import Host from './host'
 import SignIn from './sign-in'
@@ -35,6 +36,10 @@ import Settings from './settings'
   children: propTypes.node.isRequired
 })
 export default class XoApp extends Component {
+  componentWillReceiveProps () {
+    this.menuHeight = document.getElementById('xo-menu-content').offsetHeight
+  }
+
   render () {
     const {
       children,
@@ -44,11 +49,21 @@ export default class XoApp extends Component {
 
     return <div className='xo-main'>
       <Menu />
-      {
-        user == null
-          ? <SignIn onSubmit={signIn} />
-          : children
-      }
+      <div className='xo-body' style={{minHeight: this.menuHeight}}>
+        <Header>
+          This is the header
+          {/* TODO Add header
+          {children.header()}
+          */}
+        </Header>
+        <div className='xo-content'>
+          {
+            user == null
+              ? <SignIn onSubmit={signIn} />
+              : children
+          }
+        </div>
+      </div>
     </div>
   }
 }
