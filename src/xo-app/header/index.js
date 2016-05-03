0cd14dee4e05f6a6dcd94db50eeee8780863d1f0
Pierre <pierre.donias@gmail.com>
2016-05-03 12:13:18 +0200

    Header: removed useless div

--- src/xo-app/header/index.js
+++ src/xo-app/header/index.js
@@ -3,10 +3,8 @@ import React, { Component } from 'react'
 export default class Header extends Component {
   render () {
     const { children } = this.props
-    return <div>
-      <nav className='xo-header'>
-        {children}
-      </nav>
-    </div>
+    return <nav className='xo-header'>
+      {children}
+    </nav>
   }
 }
