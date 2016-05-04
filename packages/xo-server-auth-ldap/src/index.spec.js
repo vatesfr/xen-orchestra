9328518bbccf74beda342fc1a2c45cb53721fcbe
Julien Fontanet <julien.fontanet@isonoe.net>
2016-05-04 16:14:35 +0200

    chore: various updates

---
+++ packages/xo-server-auth-ldap/src/index.spec.js
@@ -0,0 +1,17 @@
+/* eslint-env mocha */
+
+import expect from 'must'
+
+// ===================================================================
+
+import myLib from './'
+
+// ===================================================================
+
+describe.skip('myLib', () => {
+  it('does something', () => {
+    // TODO: some real tests.
+
+    expect(myLib).to.exists()
+  })
+})
