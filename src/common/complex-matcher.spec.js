570f56a4ccd867430d4d2bdf38d178b767f02203
Julien Fontanet <julien.fontanet@isonoe.net>
2016-05-08 00:25:16 +0200

    chore(complex-matcher): test parse() and toString()

---
+++ src/common/complex-matcher.spec.js
@@ -0,0 +1,33 @@
+import test from 'ava'
+
+import invoke from './invoke'
+import {
+  parse,
+  toString
+} from './complex-matcher'
+
+const pattern = 'foo !bar name:|(wonderwoman batman)'
+const ast = invoke(() => {
+  const and = (...children) => ({ type: 'and', children })
+  const or = (...children) => ({ type: 'or', children })
+  const not = child => ({ type: 'not', child })
+  const property = (name, child) => ({ type: 'property', name, child })
+  const string = value => ({ type: 'string', value })
+
+  return and(
+    string('foo'),
+    not(string('bar')),
+    property('name', or(
+      string('wonderwoman'),
+      string('batman')
+    ))
+  )
+})
+
+test('parse', t => {
+  t.deepEqual(parse(pattern), ast)
+})
+
+test('toString', t => {
+  t.is(pattern, toString(ast))
+})
