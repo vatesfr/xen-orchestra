2f3e463acafcb2a5ce40dbcef5f25a13483be606
Pierre Donias <pierre.donias@e.ujf-grenoble.fr>
2016-05-11 15:13:12 +0200

    feat: move tabs in header for host and VM views (#927)
    Fixes #926

---
+++ src/xo-app/page/index.js
@@ -0,0 +1,31 @@
+import Header from '../header'
+import React from 'react'
+
+const Page = ({ children, header }) => {
+  return (
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      flex: '1',
+      maxHeight: '100vh',
+      margin: '-1em' /* To offset the padding applied to the wrapper */
+    }}>
+      <Header style={{
+        backgroundColor: '#eee',
+        padding: '0.6em',
+        paddingBottom: '0',
+        flexShrink: '0'
+      }}>
+        {header}
+      </Header>
+      <div style={{
+        flex: '1',
+        overflowY: 'auto',
+        padding: '1em'
+      }}>
+        {children}
+      </div>
+    </div>
+  )
+}
+export { Page as default }
