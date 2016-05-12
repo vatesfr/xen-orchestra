1f194f168036cf5c4f5f68595bcc35c544ce5ea9
Julien Fontanet <julien.fontanet@isonoe.net>
2016-05-12 11:29:27 +0200

    chore(ActionRow): reimplemented on top of ActionButton

--- src/common/action-row.js
+++ src/common/action-row.js
@@ -1,65 +1,14 @@
-import Icon from 'icon'
-import React, { Component } from 'react'
-import { autobind, propTypes } from 'utils'
-import { Button } from 'react-bootstrap-4/lib'
+import React from 'react'
 
-@propTypes({
-  btnStyle: propTypes.string,
-  handler: propTypes.func.isRequired,
-  icon: propTypes.string.isRequired
-})
-export default class ActionRow extends Component {
-  constructor () {
-    super()
+import ActionButton from './action-button'
 
-    this.state = {}
-  }
-
-  @autobind
-  async _execute () {
-    if (this.state.working) {
-      return
-    }
-
-    const { handler } = this.props
-    try {
-      this.setState({
-        error: null,
-        working: true
-      })
-      await handler()
-      this.setState({
-        working: false
-      })
-    } catch (error) {
-      this.setState({
-        error,
-        working: false
-      })
-    }
-  }
-
-  render () {
-    const {
-      props: {
-        btnStyle,
-        children,
-        icon
-      },
-      state: { error, working }
-    } = this
-
-    return <Button
-      bsSize='small'
-      bsStyle={error ? 'warning' : btnStyle}
-      disabled={working}
-      onClick={this._execute}
-      style={{ marginLeft: '0.5em' }}
-
-    >
-      <Icon icon={working ? 'loading' : icon} fixedWidth />
-      {children && ' '}
-      {children}
-    </Button>
-  }
-}
+const ActionRow = props => (
+  <ActionButton
+    {...props}
+    size='small'
+    style={{
+      marginLeft: '0.5em'
+    }}
+  />
+)
+export { ActionRow as default }
