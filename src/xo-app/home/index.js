909eb0c4ddaff820e24cd0e1daa494ff8878eee8
Pierre <pierre.donias@gmail.com>
2016-05-20 16:32:47 +0200

    Home: use Icon even as a placeholder.

--- src/xo-app/home/index.js
+++ src/xo-app/home/index.js
@@ -285,7 +285,7 @@ export default class Home extends Component {
   _sortByRam = () => this.setState({ sortBy: 'memory.size', sortOrder: 'desc' })
   _sortByVcpus = () => this.setState({ sortBy: 'CPUs.number', sortOrder: 'desc' })
 
-  _tick = isCriteria => isCriteria ? <Icon icon='success' fixedWidth /> : <i className='fa fa-fw' />
+  _tick = isCriteria => <Icon icon={isCriteria && 'success'} fixedWidth />
 
   _updateSelectedPools = pools => { this.setState({ selectedPools: pools }) }
   _updateSelectedHosts = hosts => { this.setState({ selectedHosts: hosts }) }
