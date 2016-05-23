92ee050bba26420a58bdffb250b8b11874e606b3
Olivier Lambert <olivier.lambert@vates.fr>
2016-05-23 13:41:06 +0200

    fix(home): make keys of isSelected once

--- src/xo-app/home/index.js
+++ src/xo-app/home/index.js
@@ -328,6 +328,7 @@ export default class Home extends Component {
       return <p>There are no VMs</p>
     }
 
+    const selectedVmsIds = keys(this._isSelected)
     const { pools, hosts, tags } = this.props
     const { activePage, sortBy } = this.state
     const filteredVms = this.getFilteredVms()
@@ -395,21 +396,21 @@ export default class Home extends Component {
           <Col mediumSize={8} className='text-xs-right hidden-sm-down'>
           {this.state.displayActions
             ? <div className='btn-group'>
-              <ActionButton btnStyle='secondary' handler={stopVms} handlerParam={keys(this._isSelected)} icon='vm-stop' />
-              <ActionButton btnStyle='secondary' handler={startVms} handlerParam={keys(this._isSelected)} icon='vm-start' />
-              <ActionButton btnStyle='secondary' handler={restartVms} handlerParam={keys(this._isSelected)} icon='vm-reboot' />
-              <ActionButton btnStyle='secondary' handler={migrateVms} handlerParam={keys(this._isSelected)} icon='vm-migrate' />
+              <ActionButton btnStyle='secondary' handler={stopVms} handlerParam={selectedVmsIds} icon='vm-stop' />
+              <ActionButton btnStyle='secondary' handler={startVms} handlerParam={selectedVmsIds} icon='vm-start' />
+              <ActionButton btnStyle='secondary' handler={restartVms} handlerParam={selectedVmsIds} icon='vm-reboot' />
+              <ActionButton btnStyle='secondary' handler={migrateVms} handlerParam={selectedVmsIds} icon='vm-migrate' />
               <DropdownButton bsStyle='secondary' id='advanced' title={_('homeMore')}>
-                <MenuItem onClick={() => { restartVms(keys(this._isSelected), true) }}>
+                <MenuItem onClick={() => { restartVms(selectedVmsIds, true) }}>
                   <Icon icon='vm-force-reboot' fixedWidth /> {_('forceRebootVmLabel')}
                 </MenuItem>
-                <MenuItem onClick={() => { stopVms(keys(this._isSelected), true) }}>
+                <MenuItem onClick={() => { stopVms(selectedVmsIds, true) }}>
                   <Icon icon='vm-force-shutdown' fixedWidth /> {_('forceShutdownVmLabel')}
                 </MenuItem>
-                <MenuItem onClick={() => { snapshotVms(keys(this._isSelected)) }}>
+                <MenuItem onClick={() => { snapshotVms(selectedVmsIds) }}>
                   <Icon icon='vm-snapshot' fixedWidth /> {_('snapshotVmLabel')}
                 </MenuItem>
-                <MenuItem onClick={() => { deleteVms(keys(this._isSelected)) }}>
+                <MenuItem onClick={() => { deleteVms(selectedVmsIds) }}>
                   <Icon icon='vm-delete' fixedWidth /> {_('vmRemoveButton')}
                 </MenuItem>
               </DropdownButton>
