52a241f300c3f91dbbe8938e050d3e79125cfbc8
Julien Fontanet <julien.fontanet@isonoe.net>
2016-05-30 10:41:11 +0200

    chore(ActionToggle): use btnStyle prop

--- src/common/action-toggle.js
+++ src/common/action-toggle.js
@@ -1,12 +1,11 @@
 import ActionButton from 'action-button'
-import classnames from 'classnames'
 import React from 'react'
 import { propTypes } from 'utils'
 
 const ActionToggle = ({ className, value, ...props }) =>
   <ActionButton
     {...props}
-    className={classnames(value && 'btn-success', className)}
+    btnStyle={value ? 'success' : null}
     icon={value ? 'toggle-on' : 'toggle-off'}
   />
 
