0341b926b9696aac8358bfec0c182b1295dad3e7
Julien Fontanet <julien.fontanet@isonoe.net>
2016-06-03 10:12:03 +0200

    chore(xo-app/page): move styles to CSS module

--- src/xo-app/page/index.js
+++ src/xo-app/page/index.js
@@ -1,26 +1,14 @@
 import React from 'react'
 
+import styles from './index.css'
+
 const Page = ({ children, header }) => {
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      flex: '1',
-      maxHeight: '100vh',
-      margin: '-1em' /* To offset the padding applied to the wrapper */
-    }}>
-      <nav className='page-header' style={{
-        padding: '0.6em',
-        paddingBottom: 0,
-        flexShrink: 0
-      }}>
+    <div className={styles.container}>
+      <nav className={'page-header ' + styles.header}>
         {header}
       </nav>
-      <div style={{
-        flex: '1',
-        overflowY: 'auto',
-        padding: '1em'
-      }}>
+      <div className={styles.content}>
         {children}
       </div>
     </div>
