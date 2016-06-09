7fec6fc679f521995b2e046b0d75e81cea058bcc
wescoeur <ronan.abhamon@gmail.com>
2016-06-09 15:26:49 +0200

    Use Page instead of Card.

--- src/xo-app/vm-import/index.js
+++ src/xo-app/vm-import/index.js
@@ -6,20 +6,27 @@ import React from 'react'
 import _ from 'messages'
 import filter from 'lodash/filter'
 import map from 'lodash/map'
-import { Col, Row } from 'grid'
+import { Container, Col, Row } from 'grid'
 import { importVms } from 'xo'
-import {
-  Card,
-  CardBlock,
-  CardHeader
-} from 'card'
 import {
   SelectPool,
   SelectSr
 } from 'select-objects'
 import { formatSize } from 'utils'
 
+import Page from '../page'
+
 import styles from './index.css'
+
+const HEADER = (
+  <Container>
+    <Row>
+      <Col mediumSize={3}>
+        <h2><Icon icon='import' /> {_('newImport')}</h2>
+      </Col>
+    </Row>
+  </Container>
+)
 
 export default class Import extends Component {
   constructor (props) {
@@ -55,78 +62,73 @@ export default class Import extends Component {
     const { files, srPredicate } = this.state
 
     return (
-      <Card>
-        <CardHeader>
-          <Icon icon='import' /> {_('newImport')}
-        </CardHeader>
-        <CardBlock>
-          <form id='import-form'>
-            <Row className='form-group'>
-              <label className='col-sm-2 form-control-label'>{_('vmImportToPool')}</label>
+      <Page header={HEADER}>
+        <form id='import-form'>
+          <Row className='form-group'>
+            <label className='col-sm-2 form-control-label'>{_('vmImportToPool')}</label>
+            <Col mediumSize={10}>
+              <SelectPool onChange={this._handleSelectedPool} required />
+            </Col>
+          </Row>
+          <Row className='form-group'>
+            <label className='col-sm-2 form-control-label'>{_('vmImportToSr')}</label>
+            <Col mediumSize={10}>
+              <SelectSr
+                disabled={!srPredicate}
+                predicate={srPredicate}
+                ref='selectSr'
+                required
+              />
+            </Col>
+          </Row>
+          <Dropzone onDrop={this._onDrop} className={styles.dropzone} activeClassName={styles.activeDropzone}>
+            <div className={styles.dropzoneText}>{_('importVmsList')}</div>
+          </Dropzone>
+          <hr />
+          <h5><span>{_('vmsToImport')}</span></h5>
+          {files.length ? (
+            <Row className={styles.filesRow}>
               <Col mediumSize={10}>
-                <SelectPool onChange={this._handleSelectedPool} required />
+                <ul className='list-group'>
+                  {map(files, (file, key) => (
+                    <li key={key} className='list-group-item'>
+                      {file.name}
+                      <span className='pull-xs-right'>{`(${formatSize(file.size)})`}</span>
+                    </li>
+                  ))}
+                </ul>
+              </Col>
+              <Col mediumSize={2} className={styles.cleanButtonContainer}>
+                <button
+                  className='btn btn-secondary'
+                  onClick={this._onCleanSelectedVms}
+                  type='button'
+                >
+                  {_('importVmsCleanList')}
+                </button>
               </Col>
             </Row>
-            <Row className='form-group'>
-              <label className='col-sm-2 form-control-label'>{_('vmImportToSr')}</label>
-              <Col mediumSize={10}>
-                <SelectSr
-                  disabled={!srPredicate}
-                  predicate={srPredicate}
-                  ref='selectSr'
-                  required
-                />
-              </Col>
-            </Row>
-            <Dropzone onDrop={this._onDrop} className={styles.dropzone} activeClassName={styles.activeDropzone}>
-              <div className={styles.dropzoneText}>{_('importVmsList')}</div>
-            </Dropzone>
-            <hr />
-            <h5><span>{_('vmsToImport')}</span></h5>
-            {files.length ? (
-              <Row className={styles.filesRow}>
-                <Col mediumSize={10}>
-                  <ul className='list-group'>
-                    {map(files, (file, key) => (
-                      <li key={key} className='list-group-item'>
-                        {file.name}
-                        <span className='pull-xs-right'>{`(${formatSize(file.size)})`}</span>
-                      </li>
-                    ))}
-                  </ul>
-                </Col>
-                <Col mediumSize={2} className={styles.cleanButtonContainer}>
-                  <button
-                    className='btn btn-secondary'
-                    onClick={this._onCleanSelectedVms}
-                    type='button'
-                  >
-                    {_('importVmsCleanList')}
-                  </button>
-                </Col>
-              </Row>
-            ) : <p>{_('noSelectedVms')}</p>}
-            <hr />
-            <div className='form-group pull-xs-right'>
-              <div className='btn-toolbar'>
-                <div className='btn-group'>
-                  <ActionButton
-                    btnStyle='primary'
-                    disabled={!files.length}
-                    form='import-form'
-                    handler={this._import}
-                    icon='import'
-                    redirectOnSuccess='/'
-                    type='submit'
-                  >
-                    {_('newImport')}
-                  </ActionButton>
-                </div>
+          ) : <p>{_('noSelectedVms')}</p>}
+          <hr />
+          <div className='form-group pull-xs-right'>
+            <div className='btn-toolbar'>
+              <div className='btn-group'>
+                <ActionButton
+                  btnStyle='primary'
+                  disabled={!files.length}
+                  form='import-form'
+                  handler={this._import}
+                  icon='import'
+                  redirectOnSuccess='/'
+                  type='submit'
+                >
+                  {_('newImport')}
+                </ActionButton>
               </div>
             </div>
-          </form>
-        </CardBlock>
-      </Card>
+          </div>
+        </form>
+      </Page>
     )
   }
 }
