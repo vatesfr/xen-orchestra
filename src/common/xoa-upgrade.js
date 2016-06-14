05766003c46645249325f8c0f4109519bffd1329
Olivier Lambert <olivier.lambert@vates.fr>
2016-06-14 10:25:37 +0200

    another PR fix

--- src/common/xoa-upgrade.js
+++ src/common/xoa-upgrade.js
@@ -5,27 +5,6 @@ import { Card, CardHeader, CardBlock } from 'card'
 import { getXoaPlan, propTypes } from 'utils'
 
 export const Upgrade = propTypes({
-  available: propTypes.number,
-  place: propTypes.string
-})(({
-  available,
-  place
-}) => (
-  <Card>
-    <CardHeader>Upgrade needed</CardHeader>
-    <CardBlock className='text-xs-center'>
-      <p>This feature is available starting from {getXoaPlan(available)} Edition</p>
-      <a href={`https://xen-orchestra.com/#!/pricing?pk_campaign=xoa_${getXoaPlan()}_upgrade&pk_kwd=${place}`} className='btn btn-primary btn-lg'>
-        <Icon icon='plan-upgrade' /> Upgrade now!
-      </a> Or&nbsp;
-      <Link className='btn btn-success btn-lg' to={'/xoa-update'}>
-        <Icon icon='plan-trial' /> Try it for free!
-      </Link>
-    </CardBlock>
-  </Card>
-))
-
-export const ModalUpgrade = propTypes({
   available: propTypes.number.isRequired,
   place: propTypes.string.isRequired
 })(({
@@ -40,10 +19,10 @@ export const ModalUpgrade = propTypes({
         <a href={`https://xen-orchestra.com/#!/pricing?pk_campaign=xoa_${getXoaPlan()}_upgrade&pk_kwd=${place}`} className='btn btn-primary btn-lg'>
           <Icon icon='plan-upgrade' /> Upgrade now!
         </a> Or&nbsp;
+        <Link className='btn btn-success btn-lg' to={'/xoa-update'}>
+          <Icon icon='plan-trial' /> Try it for free!
+        </Link>
       </p>
-      <Link className='btn btn-success btn-lg' to={'/xoa-update'}>
-        <Icon icon='plan-trial' /> Try it for free!
-      </Link>
     </CardBlock>
   </Card>
 ))
