332b093ee99827d4307e35dcbffebe4d76bdf498
Olivier Lambert <olivier.lambert@vates.fr>
2016-06-15 13:10:53 +0200

    feat(copyVM): include Edition check

--- src/common/xo/copy-vm-modal/index.js
+++ src/common/xo/copy-vm-modal/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 import _, { messages } from '../../messages'
 import SingleLineRow from '../../single-line-row'
+import Upgrade from 'xoa-upgrade'
 import { Col } from '../../grid'
 import { SelectSr } from '../../select-objects'
 import { Toggle } from '../../form'
@@ -26,37 +27,39 @@ class CopyVmModalBody extends Component {
 
   render () {
     const { formatMessage } = this.props.intl
-    return <div>
-      <SingleLineRow>
-        <Col size={6}>{_('copyVmSelectSr')}</Col>
-        <Col size={6}>
-          <SelectSr
-            onChange={this._onChangeSr}
-          />
-        </Col>
-      </SingleLineRow>
-      &nbsp;
-      <SingleLineRow>
-        <Col size={6}>{_('copyVmName')}</Col>
-        <Col size={6}>
-          <input
-            className='form-control'
-            onChange={this._onChangeName}
-            placeholder={formatMessage(messages.copyVmNamePlaceholder)}
-            type='text'
-          />
-        </Col>
-      </SingleLineRow>
-      &nbsp;
-      <SingleLineRow>
-        <Col size={6}>{_('copyVmCompress')}</Col>
-        <Col size={6}>
-          <Toggle
-            onChange={this._onChangeCompress}
-          />
-        </Col>
-      </SingleLineRow>
-    </div>
+    return process.env.XOA_PLAN > 2
+      ? <div>
+        <SingleLineRow>
+          <Col size={6}>{_('copyVmSelectSr')}</Col>
+          <Col size={6}>
+            <SelectSr
+              onChange={this._onChangeSr}
+            />
+          </Col>
+        </SingleLineRow>
+        &nbsp;
+        <SingleLineRow>
+          <Col size={6}>{_('copyVmName')}</Col>
+          <Col size={6}>
+            <input
+              className='form-control'
+              onChange={this._onChangeName}
+              placeholder={formatMessage(messages.copyVmNamePlaceholder)}
+              type='text'
+            />
+          </Col>
+        </SingleLineRow>
+        &nbsp;
+        <SingleLineRow>
+          <Col size={6}>{_('copyVmCompress')}</Col>
+          <Col size={6}>
+            <Toggle
+              onChange={this._onChangeCompress}
+            />
+          </Col>
+        </SingleLineRow>
+      </div>
+      : <div><Upgrade place='vmCopy' available={3} /></div>
   }
 }
 export default injectIntl(CopyVmModalBody, { withRef: true })
