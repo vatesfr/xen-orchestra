85d118862810b59c0bcb6e388a6301feadebf6c8
ABHAMON Ronan <ronan.abhamon@gmail.com>
2016-06-15 11:28:44 +0200

    feat(react-novnc): can send Ctrl+Alt+Del (#1068)

--- src/xo-app/host/tab-console.js
+++ src/xo-app/host/tab-console.js
@@ -1,4 +1,5 @@
 import _ from 'messages'
+import Component from 'base-component'
 import Icon from 'icon'
 import NoVnc from 'react-novnc'
 import React from 'react'
@@ -12,68 +13,84 @@ import {
   LoadSparkLines
 } from 'xo-sparklines'
 
-export default ({
-  vmController,
-  host,
-  statsOverview
-}) => <Container>
-  {statsOverview && <Row className='text-xs-center'>
-    <Col mediumSize={3}>
-      <Icon icon='cpu' size={2} />
-      {' '}
-      <CpuSparkLines data={statsOverview} />
-    </Col>
-    <Col mediumSize={3}>
-      <Icon icon='memory' size={2} />
-      {' '}
-      <MemorySparkLines data={statsOverview} />
-    </Col>
-    <Col mediumSize={3}>
-      <Icon icon='network' size={2} />
-      {' '}
-      <PifSparkLines data={statsOverview} />
-    </Col>
-    <Col mediumSize={3}>
-      <Icon icon='disk' size={2} />
-      {' '}
-      <LoadSparkLines data={statsOverview} />
-    </Col>
-  </Row>}
-  <br />
-  <Row>
-    <Col mediumSize={5}>
-      {/* TODO: insert real ISO selector, CtrlAltSuppr button and Clipboard */}
-      <div className='input-group'>
-        <select className='form-control'>
-          <option>-- CD Drive (empty) --</option>
-          <option>Debian-8.iso</option>
-          <option>Windows7.iso</option>
-        </select>
-        <span className='input-group-btn'>
-          <button className='btn btn-secondary'>
-            <Icon icon='vm-eject' />
-          </button>
-        </span>
-      </div>
-    </Col>
-    <Col mediumSize={5}>
-      <div className='input-group'>
-        <input type='text' className='form-control'></input>
-        <span className='input-group-btn'>
-          <button className='btn btn-secondary'>
-            <Icon icon='clipboard' /> {_('copyToClipboardLabel')}
-          </button>
-        </span>
-      </div>
-    </Col>
-    <Col mediumSize={2}>
-      <button className='btn btn-secondary'><Icon icon='vm-keyboard' /> {_('ctrlAltDelButtonLabel')}</button>
-    </Col>
-  </Row>
-  <Row className='console'>
-    <Col>
-      <NoVnc url={resolveUrl(`consoles/${vmController.id}`)} />
-      <p><em><Icon icon='info' /> {_('tipLabel')} {_('tipConsoleLabel')}</em></p>
-    </Col>
-  </Row>
-</Container>
+export default class extends Component {
+  _sendCtrlAltDel = () => {
+    this.refs.noVnc.sendCtrlAltDel()
+  }
+
+  render () {
+    const {
+      vmController,
+      statsOverview
+    } = this.props
+
+    return (
+      <Container>
+        {statsOverview && <Row className='text-xs-center'>
+          <Col mediumSize={3}>
+            <Icon icon='cpu' size={2} />
+            {' '}
+            <CpuSparkLines data={statsOverview} />
+          </Col>
+          <Col mediumSize={3}>
+            <Icon icon='memory' size={2} />
+            {' '}
+            <MemorySparkLines data={statsOverview} />
+          </Col>
+          <Col mediumSize={3}>
+            <Icon icon='network' size={2} />
+            {' '}
+            <PifSparkLines data={statsOverview} />
+          </Col>
+          <Col mediumSize={3}>
+            <Icon icon='disk' size={2} />
+            {' '}
+            <LoadSparkLines data={statsOverview} />
+          </Col>
+        </Row>}
+        <br />
+        <Row>
+          <Col mediumSize={5}>
+            {/* TODO: insert real ISO selector, CtrlAltSuppr button and Clipboard */}
+            <div className='input-group'>
+              <select className='form-control'>
+                <option>-- CD Drive (empty) --</option>
+                <option>Debian-8.iso</option>
+                <option>Windows7.iso</option>
+              </select>
+              <span className='input-group-btn'>
+                <button className='btn btn-secondary'>
+                  <Icon icon='vm-eject' />
+                </button>
+              </span>
+            </div>
+          </Col>
+          <Col mediumSize={5}>
+            <div className='input-group'>
+              <input type='text' className='form-control'></input>
+              <span className='input-group-btn'>
+                <button className='btn btn-secondary'>
+                  <Icon icon='clipboard' /> {_('copyToClipboardLabel')}
+                </button>
+              </span>
+            </div>
+          </Col>
+          <Col mediumSize={2}>
+            <button
+              className='btn btn-secondary'
+              onClick={this._sendCtrlAltDel}
+            >
+              <Icon icon='vm-keyboard' /> {_('ctrlAltDelButtonLabel')}
+            </button>
+          </Col>
+        </Row>
+        <Row className='console'>
+          <Col>
+            <NoVnc ref='noVnc' url={resolveUrl(`consoles/${vmController.id}`)} />
+            <p><em><Icon icon='info' /> {_('tipLabel')} {_('tipConsoleLabel')}</em></p>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+}
--- src/xo-app/vm/tab-console.js
+++ src/xo-app/vm/tab-console.js
@@ -1,4 +1,5 @@
 import _ from 'messages'
+import Component from 'base-component'
 import Icon from 'icon'
 import NoVnc from 'react-novnc'
 import React from 'react'
@@ -11,76 +12,97 @@ import {
   XvdSparkLines
 } from 'xo-sparklines'
 
-export default ({
-  statsOverview,
-  vm
-}) => vm.power_state !== 'Running' ? <p>
-  Console is only available for running VMs.
-</p> : <Container>
-  {statsOverview && <Row className='text-xs-center'>
-    <Col mediumSize={3}>
-      <p>
-        <Icon icon='cpu' size={2} />
-        {' '}
-        <CpuSparkLines data={statsOverview} />
-      </p>
-    </Col>
-    <Col mediumSize={3}>
-      <p>
-        <Icon icon='memory' size={2} />
-        {' '}
-        <MemorySparkLines data={statsOverview} />
-      </p>
-    </Col>
-    <Col mediumSize={3}>
-      <p>
-        <Icon icon='network' size={2} />
-        {' '}
-        <VifSparkLines data={statsOverview} />
-      </p>
-    </Col>
-    <Col mediumSize={3}>
-      <p>
-        <Icon icon='disk' size={2} />
-        {' '}
-        <XvdSparkLines data={statsOverview} />
-      </p>
-    </Col>
-  </Row>}
-  <Row>
-    <Col mediumSize={5}>
-      {/* TODO: insert real ISO selector, CtrlAltSuppr button and Clipboard */}
-      <div className='input-group'>
-        <select className='form-control'>
-          <option>-- CD Drive (empty) --</option>
-          <option>Debian-8.iso</option>
-          <option>Windows7.iso</option>
-        </select>
-        <span className='input-group-btn'>
-          <button className='btn btn-secondary'>
-            <Icon icon='vm-eject' />
-          </button>
-        </span>
-      </div>
-    </Col>
-    <Col mediumSize={5}>
-      <div className='input-group'>
-        <input type='text' className='form-control'></input>
-        <span className='input-group-btn'>
-          <button className='btn btn-secondary'>
-            <Icon icon='clipboard' /> {_('copyToClipboardLabel')}
-          </button>
-        </span>
-      </div>
-    </Col>
-    <Col mediumSize={2}>
-      <button className='btn btn-secondary'><Icon icon='vm-keyboard' /> {_('ctrlAltDelButtonLabel')}</button>
-    </Col>
-  </Row>
-  <Row className='console'>
-    <Col>
-      <NoVnc url={resolveUrl(`consoles/${vm.id}`)} />
-      <p><em><Icon icon='info' /> {_('tipLabel')} {_('tipConsoleLabel')}</em></p>
-    </Col>
-  </Row>
-</Container>
+export default class TabConsole extends Component {
+  _sendCtrlAltDel = () => {
+    this.refs.noVnc.sendCtrlAltDel()
+  }
+
+  render () {
+    const {
+      statsOverview,
+      vm
+    } = this.props
+
+    if (vm.power_state !== 'Running') {
+      return (
+        <p>Console is only available for running VMs.</p>
+      )
+    }
+
+    return (
+      <Container>
+        {statsOverview && <Row className='text-xs-center'>
+          <Col mediumSize={3}>
+            <p>
+              <Icon icon='cpu' size={2} />
+              {' '}
+              <CpuSparkLines data={statsOverview} />
+            </p>
+          </Col>
+          <Col mediumSize={3}>
+            <p>
+              <Icon icon='memory' size={2} />
+              {' '}
+              <MemorySparkLines data={statsOverview} />
+            </p>
+          </Col>
+          <Col mediumSize={3}>
+            <p>
+              <Icon icon='network' size={2} />
+              {' '}
+              <VifSparkLines data={statsOverview} />
+            </p>
+          </Col>
+          <Col mediumSize={3}>
+            <p>
+              <Icon icon='disk' size={2} />
+              {' '}
+              <XvdSparkLines data={statsOverview} />
+            </p>
+          </Col>
+        </Row>}
+        <Row>
+          <Col mediumSize={5}>
+            {/* TODO: insert real ISO selector, CtrlAltSuppr button and Clipboard */}
+            <div className='input-group'>
+              <select className='form-control'>
+                <option>-- CD Drive (empty) --</option>
+                <option>Debian-8.iso</option>
+                <option>Windows7.iso</option>
+              </select>
+              <span className='input-group-btn'>
+                <button className='btn btn-secondary'>
+                  <Icon icon='vm-eject' />
+                </button>
+              </span>
+            </div>
+          </Col>
+          <Col mediumSize={5}>
+            <div className='input-group'>
+              <input type='text' className='form-control'></input>
+              <span className='input-group-btn'>
+                <button className='btn btn-secondary'>
+                  <Icon icon='clipboard' /> {_('copyToClipboardLabel')}
+                </button>
+              </span>
+            </div>
+          </Col>
+          <Col mediumSize={2}>
+            <button
+              className='btn btn-secondary'
+              onClick={this._sendCtrlAltDel}
+            >
+              <Icon icon='vm-keyboard' /> {_('ctrlAltDelButtonLabel')}
+            </button>
+          </Col>
+        </Row>
+        <Row className='console'>
+          <Col>
+            <NoVnc ref='noVnc' url={resolveUrl(`consoles/${vm.id}`)} />
+            <p><em><Icon icon='info' /> {_('tipLabel')} {_('tipConsoleLabel')}</em></p>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+}
