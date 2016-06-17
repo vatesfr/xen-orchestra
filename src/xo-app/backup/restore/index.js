fd2352f611c9095059c72dc06cfcf247ac3d3212
Fabrice Marsaud <fabrice.marsaud@vates.fr>
2016-06-17 17:55:24 +0200

    Reworked restore UI

--- src/xo-app/backup/restore/index.js
+++ src/xo-app/backup/restore/index.js
@@ -1,20 +1,25 @@
 import ActionButton from 'action-button'
+import ActionRowButton from 'action-row-button'
+import filter from 'lodash/filter'
 import find from 'lodash/find'
 import forEach from 'lodash/forEach'
 import Link from 'react-router/lib/Link'
 import map from 'lodash/map'
 import moment from 'moment'
 import orderBy from 'lodash/orderBy'
-import Upgrade from 'xoa-upgrade'
 import React, { Component } from 'react'
-import renderXoItem, { renderXoItemFromId } from 'render-xo-item'
+import reduce from 'lodash/reduce'
 import size from 'lodash/size'
-import { connectStore, formatSize } from 'utils'
+import SortedTable from 'sorted-table'
+import Upgrade from 'xoa-upgrade'
+import { alert } from 'modal'
+import { connectStore } from 'utils'
+import { Container } from 'grid'
 import { createGetObjectsOfType } from 'selectors'
-import { DropdownButton, MenuItem } from 'react-bootstrap-4/lib'
 import { FormattedDate } from 'react-intl'
 import { info, error } from 'notification'
-import { Container, Row, Col } from 'grid'
+import { SelectPlainObject } from 'form/select-plain-object'
+import { SelectSr } from 'select-objects'
 
 import {
   importBackup,
@@ -23,22 +28,14 @@ import {
   subscribeRemotes
 } from 'xo'
 
-/**
- * Groups backup files by tag, vmId
- */
-const deltaBuilder = (backups, tag, id, value) => {
-  let deltaBackup = backups[tag]
-    ? backups[tag]
-    : backups[tag] = {}
+const isEmptyRemote = remote => !remote.backups || !size(remote.backups)
 
-  deltaBackup = deltaBackup[id]
-    ? deltaBackup[id]
-    : deltaBackup[id] = []
-
-  deltaBackup.push(value)
-}
-
-const isEmptyRemote = remote => !remote.backups || !(size(remote.backups.delta) || size(remote.backups.other))
+const backupOptionRenderer = backup => <span>
+    {backup.type === 'delta' && <span><span className='tag tag-info'>delta</span>{' '}</span>}
+    {backup.tag}
+    {' '}
+  <FormattedDate value={new Date(backup.date)} month='long' day='numeric' year='numeric' hour='2-digit' minute='2-digit' second='2-digit' />
+</span>
 
 @connectStore(() => ({
   writableSrs: createGetObjectsOfType('SR').filter(
@@ -68,6 +65,7 @@ export default class Restore extends Component {
   }
 
   _list = async id => {
+    const parseDate = date => +moment(date, 'YYYYMMDDTHHmmssZ').format('x')
     let files
     try {
       files = await listRemote(id)
@@ -78,66 +76,55 @@ export default class Restore extends Component {
     const { remotes } = this.state
     const remote = find(remotes, {id})
     if (remote) {
-      const backups = remote.backups = {
-        delta: {},
-        other: []
-      }
-
+      const backups = []
+      const lastVmbackups = {}
       forEach(files, file => {
-        const arr = /^vm_delta_(.*)_([^\/]+)\/([^_]+)_(.*)$/.exec(file)
-
-        if (arr) {
-          const [ , tag, id, date, name ] = arr
-          const value = {
+        let backup
+        const deltaInfo = /^vm_delta_(.*)_([^\/]+)\/([^_]+)_(.*)$/.exec(file)
+        if (deltaInfo) {
+          const [ , tag, id, date, name ] = deltaInfo
+          backup = {
+            type: 'delta',
+            date: parseDate(date),
+            id,
+            name,
             path: file,
-            date: +moment(date, 'YYYYMMDDTHHmmssZ').format('x'),
-            name
+            tag,
+            remote
           }
-          deltaBuilder(backups.delta, tag, id, value)
         } else {
-          backups.other.push(file)
+          const backupInfo = /^([^_]+)_([^_]+)_(.*)\.xva$/.exec(file)
+          if (backupInfo) {
+            const [ , date, tag, name ] = backupInfo
+            backup = {
+              type: 'simple',
+              date: parseDate(date),
+              name,
+              path: file,
+              tag,
+              remote
+            }
+          }
         }
+        backup.label = backupOptionRenderer(backup)
+        backups.push(backup)
+        lastVmbackups[backup.name] || (lastVmbackups[backup.name] = [])
+        lastVmbackups[backup.name].push(backup)
       })
-
-      forEach(remote.backups.delta, (byId, tag) => {
-        forEach(byId, (bks, id) => {
-          byId[id] = orderBy(bks, ['date'], ['asc'])
-        })
-      })
+      for (let vm in lastVmbackups) {
+        const bks = lastVmbackups[vm]
+        lastVmbackups[vm] = reduce(bks, (last, b) => b.date > last.date ? b : last)
+      }
+      remote.backups = backups
+      remote.lastVmbackups = map(lastVmbackups)
     }
     this.setState({remotes})
-  }
-
-  _notifyImportStart () {
-    info('VM import', 'Starting your backup import')
-  }
-
-  _importDeltaBackup = async (remote, sr, filePath) => {
-    this._notifyImportStart()
-    try {
-      await importDeltaBackup({remote, sr, filePath})
-    } catch (err) {
-      error('VM import', err.message || String(err))
-    }
-  }
-
-  _importBackup = async (remote, sr, file) => {
-    this._notifyImportStart()
-    try {
-      await importBackup({remote, sr, file})
-    } catch (err) {
-      error('VM import', err.message || String(err))
-    }
   }
 
   render () {
     const {
       remotes
     } = this.state
-
-    const {
-      writableSrs
-    } = this.props
 
     return process.env.XOA_PLAN > 1
       ? <Container>
@@ -152,60 +139,96 @@ export default class Restore extends Component {
             <span className='pull-right'>
               <ActionButton disabled={!r.enabled} icon='refresh' btnStyle='default' handler={this._list} handlerParam={r.id} />
             </span>
-            {r.backups &&
-              <div>
-                {isEmptyRemote(r) && <span>No backups available</span>}
-                {map(r.backups.delta, (backups, tag) =>
-                  <Row key={tag}>
-                    <Col smallSize={2}>{tag}</Col>
-                    <Col smallSize={10}>
-                      {map(backups, (backups, id) =>
-                        <div key={id} className='clearfix'>
-                          {renderXoItemFromId(id)}
-                          <span className='pull-right'>
-                            {map(backups, (b, k) =>
-                              <span key={k}>
-                                <FormattedDate value={new Date(b.date)} month='long' day='numeric' year='numeric' hour='2-digit' minute='2-digit' second='2-digit' />
-                                {' '}
-                                <DropdownButton id='filter' bsStyle='info' title='Import to'>
-                                  {map(writableSrs, (sr, key) =>
-                                    <MenuItem key={key} onClick={() => this._importDeltaBackup(r.id, sr.id, b.path)}>
-                                      {renderXoItem(sr)} ({formatSize(sr.size - sr.physical_usage)} free)
-                                    </MenuItem>
-                                  )}
-                                </DropdownButton>
-                                <br />
-                                <br />
-                              </span>
-                            )}
-                          </span>
-                        </div>
-                      )}
-                    </Col>
-                  </Row>
-                )}
-                {map(r.backups.other, (b, k) =>
-                  <div key={k}>
-                    {b}
-                    <span className='pull-right'>
-                      <DropdownButton id='filter' bsStyle='info' title='Import to '>
-                        {map(writableSrs, (sr, key) =>
-                          <MenuItem key={key} onClick={() => this._importBackup(r.id, sr.id, b)}>
-                            {renderXoItem(sr)} ({formatSize(sr.size - sr.physical_usage)} free)
-                          </MenuItem>
-                        )}
-                      </DropdownButton>
-                    </span>
-                    <br />
-                    <br />
-                  </div>
-                )}
-              </div>
-            }
+            {r.lastVmbackups && <div>
+              <br />
+              {isEmptyRemote(r) && <span>No backups available</span>}
+              {!isEmptyRemote(r) && <SortedTable collection={r.lastVmbackups} columns={BK_COLUMNS} />}
+            </div>}
             <hr />
           </div>
         )}
       </Container>
       : <Container><Upgrade place='restoreBackup' available={2} /></Container>
+  }
+}
+
+const openImportModal = backup => alert(`Import a ${backup.name} Backup`, <BackupImport vmName={backup.name} remote={backup.remote} />)
+
+const BK_COLUMNS = [
+  {
+    name: 'VM name',
+    itemRenderer: bk => bk.name,
+    sortCriteria: bk => bk.name
+  },
+  {
+    name: '',
+    itemRenderer: bk => <ActionRowButton icon='import' handler={openImportModal} handlerParam={bk} />
+  },
+  {
+    name: 'Backup Tag',
+    itemRenderer: bk => bk.tag,
+    sortCriteria: bk => bk.tag
+  },
+  {
+    name: 'Last Backup date',
+    itemRenderer: bk => <FormattedDate value={bk.date} month='long' day='numeric' year='numeric' hour='2-digit' minute='2-digit' second='2-digit' />,
+    sortCriteria: bk => bk.date
+  },
+  {
+    name: 'Backup Type',
+    itemRenderer: bk => bk.type,
+    sortCriteria: bk => bk.type
+  }
+]
+
+const srWritablePredicate = sr => sr.content_type !== 'iso'
+const notifyImportStart = () => info('VM import', 'Starting your backup import')
+
+@connectStore(() => ({
+  writableSrs: createGetObjectsOfType('SR').filter(
+    [ sr => sr.content_type !== 'iso' ]
+  ).sort()
+}))
+class BackupImport extends Component {
+  constructor (props) {
+    super(props)
+    const { vmName, remote } = props
+    this.options = filter(remote.backups, b => b.name === vmName)
+  }
+
+  _import = () => {
+    const { sr, backup } = this.refs
+    const { remote } = this.props
+    const methods = {
+      delta: this._importDeltaBackup,
+      simple: this._importBackup
+    }
+    methods[backup.type](remote, sr, backup.path)
+  }
+
+  _importDeltaBackup = async (remote, sr, filePath) => {
+    notifyImportStart()
+    try {
+      await importDeltaBackup({remote, sr, filePath})
+    } catch (err) {
+      error('VM import', err.message || String(err))
+    }
+  }
+
+  _importBackup = async (remote, sr, file) => {
+    notifyImportStart()
+    try {
+      await importBackup({remote, sr, file})
+    } catch (err) {
+      error('VM import', err.message || String(err))
+    }
+  }
+
+  render () {
+    return <div>
+      <SelectSr ref='sr' predicate={srWritablePredicate} />
+      <SelectPlainObject ref='backup' options={this.options} optionKey='path' />
+      <ActionButton icon='import' handler={this._import}>Import</ActionButton>
+    </div>
   }
 }
