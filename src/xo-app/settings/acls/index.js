1d6d8ccb28f7bbcb83ec318ae61878f9a8915834
Olivier Lambert <olivier.lambert@vates.fr>
2016-06-25 01:09:06 +0200

    fix(ACLs): are available from Enterprise (#1117)

--- src/xo-app/settings/acls/index.js
+++ src/xo-app/settings/acls/index.js
@@ -165,7 +165,7 @@ export default class Acls extends Component {
       subjects
     } = this.state
 
-    return process.env.XOA_PLAN > 3
+    return process.env.XOA_PLAN > 2
       ? <Container>
         <form>
           <div className='form-group'>
