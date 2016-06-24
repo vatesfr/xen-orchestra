48e972eb29a53c1e793f0f419c0297ed4ad46b21
wescoeur <ronan.abhamon@gmail.com>
2016-06-24 13:57:16 +0200

    Fixes.

--- src/common/tab-button.js
+++ src/common/tab-button.js
@@ -1,8 +1,8 @@
 import _ from 'intl'
 import ActionButton from 'action-button'
+import Icon from 'icon'
 import React from 'react'
-
-export TabButtonLink from 'tab-button-link'
+import { Link } from 'react-router'
 
 const COMMON_PROPS = {
   size: 'large',
@@ -21,3 +21,25 @@ const TabButton = ({
   ><span className='hidden-md-down'>{_(labelId)}</span></ActionButton>
 )
 export { TabButton as default }
+
+export const TabButtonLink = ({
+  labelId,
+  icon,
+  ...props
+}) => (
+  <Link
+    {...props}
+    {...COMMON_PROPS}
+    className='btn btn-lg btn-primary'
+  >
+    <span className='hidden-md-down'>
+      {icon && (
+        <span>
+          <Icon icon={icon} />
+          {' '}
+        </span>
+      )}
+      {_(labelId)}
+    </span>
+  </Link>
+)
