8257714cdb13d55220287eb959d225a29776d2e1
Julien Fontanet <julien.fontanet@isonoe.net>
2016-06-28 15:30:43 +0200

    feat(get-event-value): works with checkbox/radio/select

--- src/common/get-event-value.js
+++ src/common/get-event-value.js
@@ -1,7 +1,18 @@
 // If the param is an event, returns the value of it's target,
 // otherwise returns the param.
-const getEventValue = event => event && event.target
-  ? event.target.value
-  : event
+const getEventValue = event => {
+  let target
+  if (!event || !(target = event.target)) {
+    return event
+  }
+
+  let type
+  return target.nodeName.toLowerCase() === 'input' && (
+    (type = target.type.toLowerCase()) === 'checkbox' ||
+    type === 'radio'
+  )
+    ? target.checked
+    : target.value
+}
 
 export { getEventValue as default }
