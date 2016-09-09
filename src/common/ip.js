a2b7506c78399eeab3a576fef174831a8ea1d6c7
Pierre Donias <pierre.donias@gmail.com>
2016-09-09 12:48:57 +0200

    fix(common/ip): copy code from ip-range-generator package

--- src/common/ip.js
+++ src/common/ip.js
@@ -3,11 +3,50 @@ import forEach from 'lodash/forEach'
 import isArray from 'lodash/isArray'
 import isIp from 'is-ip'
 import some from 'lodash/some'
-import range from 'ip-range-generator'
 
 export { isIp }
 export const isIpV4 = isIp.v4
 export const isIpV6 = isIp.v6
+
+// Source: https://github.com/ezpaarse-project/ip-range-generator/blob/master/index.js
+
+const ipv4 = /^(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(?:\.(?!$)|$)){4}$/
+
+function ip2hex (ip) {
+  let parts = ip.split('.').map(str => parseInt(str, 10))
+  let n = 0
+
+  n += parts[3]
+  n += parts[2] * 256      // 2^8
+  n += parts[1] * 65536    // 2^16
+  n += parts[0] * 16777216 // 2^24
+
+  return n
+}
+
+function assertIpv4 (str, msg) {
+  if (!ipv4.test(str)) { throw new Error(msg) }
+}
+
+function *range (ip1, ip2) {
+  assertIpv4(ip1, 'argument "ip1" must be a valid IPv4 address')
+  assertIpv4(ip2, 'argument "ip2" must be a valid IPv4 address')
+
+  let hex = ip2hex(ip1)
+  let hex2 = ip2hex(ip2)
+
+  if (hex > hex2) {
+    let tmp = hex
+    hex = hex2
+    hex2 = tmp
+  }
+
+  for (let i = hex; i <= hex2; i++) {
+    yield `${(i >> 24) & 0xff}.${(i >> 16) & 0xff}.${(i >> 8) & 0xff}.${i & 0xff}`
+  }
+}
+
+// -----------------------------------------------------------------------------
 
 export const getNextIpV4 = ip => {
   const splitIp = ip.split('.')
@@ -76,6 +115,8 @@ export const parseIpPattern = pattern => {
     const ipRange = rawIpRange.split('-')
     if (ipRange.length < 2) {
       ips.push(ipRange[0])
+    } else if (!isIpV4(ipRange[0]) || !isIpV4(ipRange[1])) {
+      ips.push(rawIpRange)
     } else {
       ips.push(...range(ipRange[0], ipRange[1]))
     }
