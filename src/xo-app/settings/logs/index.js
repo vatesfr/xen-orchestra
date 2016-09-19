94975f5ea68ac1da9c69ef51adea93449e0cda5e
Julien Fontanet <julien.fontanet@isonoe.net>
2016-09-19 10:58:11 +0200

    feat(settings/logs): group action buttons
    Fixes #1547

--- src/xo-app/settings/logs/index.js
+++ src/xo-app/settings/logs/index.js
@@ -3,79 +3,96 @@ import ActionRowButton from 'action-row-button'
 import BaseComponent from 'base-component'
 import Copiable from 'copiable'
 import find from 'lodash/find'
-import forEach from 'lodash/forEach'
+import isEmpty from 'lodash/isEmpty'
 import map from 'lodash/map'
 import React from 'react'
-import size from 'lodash/size'
 import SortedTable from 'sorted-table'
-import TabButton from 'tab-button'
-import Tooltip from 'tooltip'
-import { addSubscriptions } from 'utils'
-import { subscribeApiLogs, subscribeUsers, deleteApiLog } from 'xo'
-import { FormattedDate } from 'react-intl'
-import { alert, confirm } from 'modal'
 import styles from './index.css'
+import TabButton from 'tab-button'
+import { addSubscriptions } from 'utils'
+import { alert, confirm } from 'modal'
+import { ButtonGroup } from 'react-bootstrap-4/lib'
+import { createSelector } from 'selectors'
+import { FormattedDate } from 'react-intl'
+import { subscribeApiLogs, subscribeUsers, deleteApiLog } from 'xo'
 
 @addSubscriptions({
   logs: subscribeApiLogs,
   users: subscribeUsers
 })
 export default class Logs extends BaseComponent {
-  _showError = log => alert(_('logError'), <Copiable tagName='pre'>{`${log.data.method}
-${JSON.stringify(log.data.params, null, 2)}
-${JSON.stringify(log.data.error, null, 2)}`}</Copiable>)
-  _showParams = log => alert(_('logParams'), <Copiable tagName='pre'>{JSON.stringify(log.data.params, null, 2)}</Copiable>)
+  _columns = [
+    {
+      name: _('logUser'),
+      itemRenderer: log => {
+        if (log.data.userId == null) {
+          return _('noUser')
+        }
+        if (!this.props.users) {
+          return '...'
+        }
+        const user = find(this.props.users, user => user.id === log.data.userId)
+        return user ? user.email : _('unknownUser')
+      },
+      sortCriteria: log => log.data.userId
+    },
+    {
+      name: _('logMessage'),
+      itemRenderer: log => <pre className={styles.widthLimit}>{log.data.error && log.data.error.message}</pre>,
+      sortCriteria: log => log.data.error && log.data.error.message
+    },
+    {
+      name: _('logTime'),
+      itemRenderer: log => <span>
+        {log.time && <FormattedDate value={new Date(log.time)} month='long' day='numeric' year='numeric' hour='2-digit' minute='2-digit' second='2-digit' />}
+      </span>,
+      sortCriteria: log => log.time,
+      sortOrder: 'desc'
+    },
+    {
+      name: '',
+      itemRenderer: log => <div className='text-xs-right'><ButtonGroup>
+        <ActionRowButton
+          handler={this._showError}
+          handlerParam={log}
+          icon='preview'
+          tooltip={_('logDisplayDetails')}
+        />
+        <ActionRowButton
+          btnStyle='danger'
+          handler={deleteApiLog}
+          handlerParam={log.id}
+          icon='delete'
+          tooltip={_('logDelete')}
+        />
+      </ButtonGroup></div>
+    }
+  ]
+
   _deleteAllLogs = () =>
     confirm({
       title: _('logDeleteAllTitle'),
       body: _('logDeleteAllMessage')
     }).then(() =>
-      forEach(this.props.logs, (log, id) => deleteApiLog(id))
+      Promise.all(map(this.props.logs, (log, id) => deleteApiLog(id)))
     )
+
+  _getLogs = createSelector(
+    () => this.props.logs,
+    logs => map(logs, (log, id) => ({ ...log, id }))
+  )
+
+  _showError = log => alert(_('logError'), <Copiable tagName='pre'>{`${log.data.method}
+${JSON.stringify(log.data.params, null, 2)}
+${JSON.stringify(log.data.error, null, 2)}`}</Copiable>)
+
   render () {
-    const columns = [
-      {
-        name: '',
-        itemRenderer: log => <Tooltip content={_('logDisplayDetails')}><ActionRowButton icon='preview' handler={this._showError} handlerParam={log} /></Tooltip>
-      },
-      {
-        name: _('logUser'),
-        itemRenderer: log => {
-          if (log.data.userId == null) {
-            return _('noUser')
-          }
-          if (!this.props.users) {
-            return '...'
-          }
-          const user = find(this.props.users, user => user.id === log.data.userId)
-          return user ? user.email : _('unknownUser')
-        },
-        sortCriteria: log => log.data.userId
-      },
-      {
-        name: _('logMessage'),
-        itemRenderer: log => <pre className={styles.widthLimit}>{log.data.error && log.data.error.message}</pre>,
-        sortCriteria: log => log.data.error && log.data.error.message
-      },
-      {
-        name: _('logTime'),
-        itemRenderer: log => <span>
-          {log.time && <FormattedDate value={new Date(log.time)} month='long' day='numeric' year='numeric' hour='2-digit' minute='2-digit' second='2-digit' />}
-        </span>,
-        sortCriteria: log => log.time,
-        sortOrder: 'desc'
-      },
-      {
-        itemRenderer: log => <ActionRowButton btnStyle='default' handler={deleteApiLog} handlerParam={log.id} icon='delete' />
-      }
-    ]
-    const { logs } = this.props
-    if (!logs) {
-      return <h3>{_('loadingLogs')}</h3>
-    }
+    const logs = this._getLogs()
+
     return <div>
-      {size(logs)
-        ? <div>
+      {isEmpty(logs)
+        ? <p>{_('noLogs')}</p>
+        : <div>
           <span className='pull-xs-right'>
             <TabButton
               btnStyle='danger'
@@ -85,9 +102,13 @@ ${JSON.stringify(log.data.error, null, 2)}`}</Copiable>)
             />
           </span>
           {' '}
-          <SortedTable collection={map(logs, (log, id) => ({ ...log, id }))} columns={columns} defaultColumn={4} />
+          <SortedTable
+            collection={logs}
+            columns={this._columns}
+            defaultColumn={2}
+          />
         </div>
-        : <p>{_('noLogs')}</p>}
+      }
     </div>
   }
 }
