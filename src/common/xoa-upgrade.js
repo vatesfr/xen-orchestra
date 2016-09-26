05a55e5eb26087b2a6d8e6c289079ad787896a8b
Pierre Donias <pierre.donias@gmail.com>
2016-09-26 10:31:23 +0200

    fix(xoa-upgrade): more suitable message for non-admin users (#1591)
    Fixes #1564

--- src/common/xoa-upgrade.js
+++ src/common/xoa-upgrade.js
@@ -5,28 +5,37 @@ import Icon from './icon'
 import Link from './link'
 import propTypes from './prop-types'
 import { Card, CardHeader, CardBlock } from './card'
-import { getXoaPlan } from './utils'
+import { connectStore, getXoaPlan } from './utils'
+import { isAdmin } from 'selectors'
 
 const Upgrade = propTypes({
   available: propTypes.number.isRequired,
   place: propTypes.string.isRequired
-})(({
+})(connectStore({
+  isAdmin
+}))(({
   available,
+  isAdmin,
   place
 }) => (
   <Card>
     <CardHeader>{_('upgradeNeeded')}</CardHeader>
-    <CardBlock className='text-xs-center'>
-      <p>{_('availableIn', {plan: getXoaPlan(available)})}</p>
-      <p>
-        <a href={`https://xen-orchestra.com/#!/pricing?pk_campaign=xoa_${getXoaPlan()}_upgrade&pk_kwd=${place}`} className='btn btn-primary btn-lg'>
-          <Icon icon='plan-upgrade' /> {_('upgradeNow')}
-        </a> {_('or')}&nbsp;
-        <Link className='btn btn-success btn-lg' to={'/xoa-update'}>
-          <Icon icon='plan-trial' /> {_('tryIt')}
-        </Link>
-      </p>
-    </CardBlock>
+    {isAdmin
+      ? <CardBlock className='text-xs-center'>
+        <p>{_('availableIn', {plan: getXoaPlan(available)})}</p>
+        <p>
+          <a href={`https://xen-orchestra.com/#!/pricing?pk_campaign=xoa_${getXoaPlan()}_upgrade&pk_kwd=${place}`} className='btn btn-primary btn-lg'>
+            <Icon icon='plan-upgrade' /> {_('upgradeNow')}
+          </a> {_('or')}&nbsp;
+          <Link className='btn btn-success btn-lg' to={'/xoa-update'}>
+            <Icon icon='plan-trial' /> {_('tryIt')}
+          </Link>
+        </p>
+      </CardBlock>
+      : <CardBlock className='text-xs-center'>
+        <p>{_('notAvailable')}</p>
+      </CardBlock>
+    }
   </Card>
 ))
 
