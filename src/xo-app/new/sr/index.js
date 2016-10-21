vatesfr/xen-orchestra

commit e79096626ae28473fd965d2fb833d528eb0119ff

feat(intl): more translatable messages (#1641)

src/xo-app/new/sr/index.js
@@ -465,7 +465,7 @@ export default class New extends Component {
                 <input
                   id='srName'
                   className='form-control'
-                  placeholder='storage name'
+                  placeholder={formatMessage(messages.newSrNamePlaceHolder)}
                   ref='name'
                   onBlur={this._handleNameChange}
                   required
@@ -475,7 +475,7 @@ export default class New extends Component {
                 <input
                   id='srDescription'
                   className='form-control'
-                  placeholder='storage description'
+                  placeholder={formatMessage(messages.newSrDescPlaceHolder)}
                   ref='description'
                   onBlur={this._handleDescriptionChange}
                   required
@@ -509,7 +509,7 @@ export default class New extends Component {
                         <input
                           id='srServer'
                           className='form-control'
-                          placeholder='address'
+                          placeholder={formatMessage(messages.newSrAddressPlaceHolder)}
                           ref='server'
                           required
                           type='text'
@@ -547,7 +547,7 @@ export default class New extends Component {
                         <input
                           id='srServer'
                           className='form-control'
-                          placeholder='address'
+                          placeholder={formatMessage(messages.newSrAddressPlaceHolder)}
                           ref='server'
                           required
                           type='text'
@@ -556,7 +556,7 @@ export default class New extends Component {
                         <input
                           id='srServer'
                           className='form-control'
-                          placeholder='[port]'
+                          placeholder={formatMessage(messages.newSrPortPlaceHolder)}
                           ref='port'
                           type='text'
                         />
@@ -569,13 +569,13 @@ export default class New extends Component {
                             id='srServerUser'
                             className='form-control'
                             placeholder='user'
-                            ref='username'
+                            ref={formatMessage(messages.newSrUsernamePlaceHolder)}
                             required
                             type='text'
                           />
                           <label>{_('newSrPassword')}</label>
                           <Password
-                            placeholder='password'
+                            placeholder={formatMessage(messages.newSrPasswordPlaceHolder)}
                             ref='password'
                             required
                           />
@@ -607,7 +607,7 @@ export default class New extends Component {
                       <input
                         id='srServer'
                         className='form-control'
-                        placeholder='address'
+                        placeholder={formatMessage(messages.newSrAddressPlaceHolder)}
                         ref='server'
                         required
                         type='text'
@@ -616,14 +616,14 @@ export default class New extends Component {
                       <input
                         id='srServerUser'
                         className='form-control'
-                        placeholder='user'
+                        placeholder={formatMessage(messages.newSrUsernamePlaceHolder)}
                         ref='username'
                         required
                         type='text'
                       />
                       <label>{_('newSrPassword')}</label>
                       <Password
-                        placeholder='password'
+                        placeholder={formatMessage(messages.newSrPasswordPlaceHolder)}
                         ref='password'
                         required
                       />
@@ -635,7 +635,7 @@ export default class New extends Component {
                       <input
                         id='srDevice'
                         className='form-control'
-                        placeholder='Device, e.g /dev/sda...'
+                        placeholder={formatMessage(messages.newSrLvmDevicePlaceHolder)}
                         ref='device'
                         required
                         type='text'
@@ -648,7 +648,7 @@ export default class New extends Component {
                       <input
                         id='srPath'
                         className='form-control'
-                        placeholder=''
+                        placeholder={formatMessage(messages.newSrLocalPathPlaceHolder)}
                         ref='localPath'
                         required
                         type='text'

src/xo-app/backup/new/index.js
@@ -25,6 +25,7 @@ import {
 } from 'xo'
 
 // ===================================================================
+// FIXME: missing most of translation. Can't be done in a dumb way, some of the word are keyword for XO-Server parameters...
 
 const NO_SMART_SCHEMA = {
   type: 'object',

src/xo-app/dashboard/stats/index.js
@@ -305,6 +305,7 @@ class SelectMetric extends Component {
               <button
                 className='btn btn-secondary'
                 onClick={this._resetSelection}
+                tooltip={_('dashboardStatsButtonRemoveAll')}
                 type='button'
               >
                 <Icon icon='remove' />
@@ -312,6 +313,7 @@ class SelectMetric extends Component {
               <button
                 className='btn btn-secondary'
                 onClick={this._selectAllHosts}
+                tooltip={_('dashboardStatsButtonAddAllHost')}
                 type='button'
               >
                 <Icon icon='host' />
@@ -319,6 +321,7 @@ class SelectMetric extends Component {
               <button
                 className='btn btn-secondary'
                 onClick={this._selectAllVms}
+                tooltip={_('dashboardStatsButtonAddAllVM')}
                 type='button'
               >
                 <Icon icon='vm' />

src/xo-app/dashboard/visualizations/index.js
@@ -21,6 +21,7 @@ import {
 // ===================================================================
 
 // Columns order is defined by the attributes declaration order.
+// FIXME translation
 const DATA_LABELS = {
   nVCpus: 'vCPUs number',
   ram: 'RAM quantity',

src/xo-app/settings/acls/index.js
@@ -207,7 +207,7 @@ export default class Acls extends Component {
           </div>
           <ButtonGroup className='p-b-1'>
             {map(TYPES, type =>
-              <ActionButton key={type} btnStyle={isAllSelected[type] ? 'success' : 'secondary'} size='small' icon={type.toLowerCase()} handler={this._toggleAll} handlerParam={type} />
+              <ActionButton tooltip={_('settingsAclsButtonTooltip'+type)} key={type} btnStyle={isAllSelected[type] ? 'success' : 'secondary'} size='small' icon={type.toLowerCase()} handler={this._toggleAll} handlerParam={type} />
             )}
           </ButtonGroup>
           <div className='form-group'>

src/common/intl/locales/fr.js
@@ -12,6 +12,7 @@ export default {
   statusDisconnected: 'Déconnecté',
   statusLoading: 'Chargement…',
   errorPageNotFound: 'Page introuvable',
+  errorNoSuchItem: 'aucune correspondance',
 
   editableLongClickPlaceholder: 'Clic long pour éditer',
   editableClickPlaceholder: 'Cliquer pour éditer',
@@ -253,7 +254,6 @@ export default {
   remoteName: 'Nom',
   remotePath: 'Chemin',
   remoteState: 'État',
-  remoteError: 'Erreur',
   remoteDevice: 'Équipement',
   remoteShare: 'Partage',
   remoteAuth: 'Accès',
@@ -296,6 +296,14 @@ export default {
   newSrInUse: 'utilisé',
   newSrSize: 'Taille',
   newSrCreate: 'Créer',
+  newSrNamePlaceHolder: 'Nom de l\'emplacement',
+  newSrDescPlaceHolder: 'Description de l\'emplacement',
+  newSrAddressPlaceHolder: 'Adresse',
+  newSrPortPlaceHolder: '[port]',
+  newSrUsernamePlaceHolder: 'Nom d\'utilisateur',
+  newSrPasswordPlaceHolder: 'Mot de passe',
+  newSrLvmDevicePlaceHolder: 'Matériel, par exemple /dev/sda...',
+  newSrLocalPathPlaceHolder: '/chemin/du/répertoire',
 
   // ----- Acls, Users, Groups ------
   subjectName: 'Utilisateurs/Groupes',
@@ -595,6 +603,13 @@ export default {
   vbdNoVbd: 'Pas de disques',
   vbdConnect: 'Connecter un VBD',
   vbdDisconnect: 'Déconnecter un VBD',
+  vdbBootable: 'Bootable',
+  vdbReadonly: 'Lecture seule',
+  vdbCreate: 'Créer',
+  vdbNamePlaceHolder: 'Nom du disque',
+  vdbSizePlaceHolder: 'Taille',
+  saveBootOption: 'Enregistrer',
+  resetBootOption: 'Réinitialiser',
 
   // ----- VM network tab -----
   vifCreateDeviceButton: 'Nouvelle interface',
@@ -617,6 +632,7 @@ export default {
   vifLockedNetworkNoIps: 'Le réseau est verrouillé et aucune IP n\'est autorisé sur cette interface',
   vifUnLockedNetwork: 'Réseau non verrouillé',
   vifUnknownNetwork: 'Réseau inconnu',
+  vifCreate: 'Créer',
 
   // ----- VM snapshot tab -----
   noSnapshots: 'Pas d\'instantané',
@@ -705,6 +721,9 @@ export default {
   srUsageStatePanel: 'Utilisation du stockage',
   srTopUsageStatePanel: 'Top 5 d\'utilisation des SR (en %)',
   vmsStates: '{running} en cours ({halted} stoppés)',
+  dashboardStatsButtonRemoveAll: 'Vider la sélection',
+  dashboardStatsButtonAddAllHost: 'Ajouter tout les hôtes',
+  dashboardStatsButtonAddAllVM: 'Ajouter toutes les VMs',
 
   // --- Stats board --
   weekHeatmapData: '{value} {date, date, medium}',
@@ -858,6 +877,7 @@ export default {
   restoreBackups: 'Restauration de sauvegardes',
   noRemotes: 'Pas d\'emplacements',
   remoteEnabled: 'activé',
+  remoteError: 'Erreur',
   noBackup: 'Pas de sauvegarde disponible',
   backupVmNameColumn: 'Nom de la VM',
   backupTagColumn: 'Tag de sauvegarde',
@@ -1135,5 +1155,13 @@ export default {
   shortcut_NAV_DOWN: 'Élément suivant',
   shortcut_NAV_UP: 'Élément précédent',
   shortcut_SELECT: 'Sélectionné l\'élément',
-  shortcut_JUMP_INTO: 'Ouvrir'
+  shortcut_JUMP_INTO: 'Ouvrir',
+
+  // ----- Settings/ACLs -----
+  settingsAclsButtonTooltipVM: 'VM',
+  settingsAclsButtonTooltiphost: 'Hôtes',
+  settingsAclsButtonTooltippool: 'Pool',
+  settingsAclsButtonTooltipSR: 'SR',
+  settingsAclsButtonTooltipnetwork: 'Réseaux'
+
 }

src/common/intl/messages.js
@@ -9,6 +9,7 @@ var messages = {
   statusDisconnected: 'Disconnected',
   statusLoading: 'Loading…',
   errorPageNotFound: 'Page not found',
+  errorNoSuchItem: 'no such item',
 
   editableLongClickPlaceholder: 'Long click to edit',
   editableClickPlaceholder: 'Click to edit',
@@ -292,6 +293,14 @@ var messages = {
   newSrInUse: 'in use',
   newSrSize: 'Size',
   newSrCreate: 'Create',
+  newSrNamePlaceHolder: 'Storage name',
+  newSrDescPlaceHolder: 'Storage description',
+  newSrAddressPlaceHolder: 'Address',
+  newSrPortPlaceHolder: '[port]',
+  newSrUsernamePlaceHolder: 'Username',
+  newSrPasswordPlaceHolder: 'Password',
+  newSrLvmDevicePlaceHolder: 'Device, e.g /dev/sda...',
+  newSrLocalPathPlaceHolder: '/path/to/directory',
 
   // ----- Acls, Users, Groups ------
   subjectName: 'Users/Groups',
@@ -591,6 +600,13 @@ var messages = {
   vbdNoVbd: 'No disks',
   vbdConnect: 'Connect VBD',
   vbdDisconnect: 'Disconnect VBD',
+  vdbBootable: 'Bootable',
+  vdbReadonly: 'Readonly',
+  vdbCreate: 'Create',
+  vdbNamePlaceHolder: 'Disk name',
+  vdbSizePlaceHolder: 'Size',
+  saveBootOption: 'Save',
+  resetBootOption: 'Reset',
 
   // ----- VM network tab -----
   vifCreateDeviceButton: 'New device',
@@ -613,6 +629,7 @@ var messages = {
   vifLockedNetworkNoIps: 'Network locked and no IPs are allowed for this interface',
   vifUnLockedNetwork: 'Network not locked',
   vifUnknownNetwork: 'Unknown network',
+  vifCreate: 'Create',
 
   // ----- VM snapshot tab -----
   noSnapshots: 'No snapshots',
@@ -704,6 +721,9 @@ var messages = {
   srUsageStatePanel: 'Storage Usage',
   srTopUsageStatePanel: 'Top 5 SR Usage (in %)',
   vmsStates: '{running} running ({halted} halted)',
+  dashboardStatsButtonRemoveAll: 'Clear selection',
+  dashboardStatsButtonAddAllHost: 'Add all hosts',
+  dashboardStatsButtonAddAllVM: 'Add all VMs',
 
   // --- Stats board --
   weekHeatmapData: '{value} {date, date, medium}',
@@ -1135,7 +1155,14 @@ var messages = {
   shortcut_NAV_DOWN: 'Next item',
   shortcut_NAV_UP: 'Previous item',
   shortcut_SELECT: 'Select item',
-  shortcut_JUMP_INTO: 'Open'
+  shortcut_JUMP_INTO: 'Open',
+
+  // ----- Settings/ACLs -----
+  settingsAclsButtonTooltipVM: 'VM',
+  settingsAclsButtonTooltiphost: 'Hosts',
+  settingsAclsButtonTooltippool: 'Pool',
+  settingsAclsButtonTooltipSR: 'SR',
+  settingsAclsButtonTooltipnetwork: 'Network'
 
 }
 forEach(messages, function (message, id) {

src/common/render-xo-item.js
@@ -1,3 +1,4 @@
+import _ from 'intl'
 import React from 'react'
 
 import Icon from './icon'
@@ -217,7 +218,7 @@ const GenericXoItem = connectStore(() => {
   })
 })(({ xoItem, ...props }) => xoItem
   ? renderXoItem(xoItem, props)
-  : <span className='text-muted'>no such item</span>
+  : <span className='text-muted'>{_('errorNoSuchItem')}</span>
 )
 
 export const renderXoItemFromId = (id, props) => <GenericXoItem {...props} id={id} />

commit f953c89979491b072e7849eb3aec2bdb0b6bfce4

fix(new/sr): fix username handling  (#1683)
Regression was introduced in e79096626ae28473fd965d2fb833d528eb0119ff

src/xo-app/new/sr/index.js
@@ -568,8 +568,8 @@ export default class New extends Component {
                           <input
                             id='srServerUser'
                             className='form-control'
-                            placeholder='user'
-                            ref={formatMessage(messages.newSrUsernamePlaceHolder)}
+                            placeholder={formatMessage(messages.newSrUsernamePlaceHolder)}
+                            ref='username'
                             required
                             type='text'
                           />