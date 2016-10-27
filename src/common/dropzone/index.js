bed2205f374b76a9837bb0f393a23677c4e52254
Pierre Donias <pierre.donias@gmail.com>
2016-10-27 14:26:25 +0200

    Add missing files.

---
+++ src/common/dropzone/index.js
@@ -0,0 +1,20 @@
+import Component from 'base-component'
+import propTypes from 'prop-types'
+import React from 'react'
+import ReactDropzone from 'react-dropzone'
+
+import styles from './index.css'
+
+@propTypes({
+  onDrop: propTypes.func,
+  message: propTypes.node
+})
+export default class Dropzone extends Component {
+  render () {
+    const { onDrop, message } = this.props
+
+    return <ReactDropzone onDrop={onDrop} className={styles.dropzone} activeClassName={styles.activeDropzone}>
+      <div className={styles.dropzoneText}>{message}</div>
+    </ReactDropzone>
+  }
+}
