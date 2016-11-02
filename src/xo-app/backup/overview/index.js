cde56b498a2c2e2790c15cf63d54dfac23eb21ba
Pierre Donias <pierre.donias@gmail.com>
2016-11-02 14:18:21 +0100

    feat(backup/overview): display jobs in a SortedTable
    Fixes #1726

--- src/xo-app/backup/overview/index.js
+++ src/xo-app/backup/overview/index.js
@@ -1,15 +1,19 @@
 import _ from 'intl'
 import ActionRowButton from 'action-row-button'
 import ActionToggle from 'action-toggle'
+import Component from 'base-component'
 import filter from 'lodash/filter'
 import forEach from 'lodash/forEach'
+import get from 'lodash/get'
 import Icon from 'icon'
 import Link from 'link'
 import LogList from '../../logs'
 import map from 'lodash/map'
 import orderBy from 'lodash/orderBy'
-import React, { Component } from 'react'
+import React from 'react'
+import SortedTable from 'sorted-table'
 import { ButtonGroup } from 'react-bootstrap-4/lib'
+import { createSelector } from 'selectors'
 import {
   Card,
   CardHeader,
@@ -35,6 +39,59 @@ const jobKeyToLabel = {
   rollingSnapshot: _('rollingSnapshot')
 }
 
+const JOB_COLUMNS = [
+  {
+    name: _('job'),
+    itemRenderer: ({ jobId, jobLabel }) => <span>{jobId.slice(0, 5)} ({jobLabel})</span>,
+    sortCriteria: 'jobId'
+  },
+  {
+    name: _('jobTag'),
+    itemRenderer: ({ scheduleTag }) => scheduleTag
+  },
+  {
+    name: _('jobScheduling'),
+    itemRenderer: ({ schedule }) => schedule.cron,
+    sortCriteria: ({ schedule }) => schedule.cron
+  },
+  {
+    name: _('jobTimezone'),
+    itemRenderer: ({ schedule }) => schedule.timezone || _('jobServerTimezone'),
+    sortCriteria: ({ schedule }) => schedule.timezone
+  },
+  {
+    name: _('jobState'),
+    itemRenderer: ({ schedule, scheduleToggleValue }) => <ActionToggle
+      value={scheduleToggleValue}
+      handler={scheduleToggleValue ? disableSchedule : enableSchedule}
+      handlerParam={schedule.id}
+      size='small'
+    />,
+    sortCriteria: 'scheduleToggleValue'
+  },
+  {
+    itemRenderer: ({ schedule }) => <fieldset className='pull-right'>
+      <Link className='btn btn-sm btn-primary mr-1' to={`/backup/${schedule.id}/edit`}>
+        <Icon icon='edit' />
+      </Link>
+      <ButtonGroup>
+        <ActionRowButton
+          icon='delete'
+          btnStyle='danger'
+          handler={deleteBackupSchedule}
+          handlerParam={schedule}
+        />
+        <ActionRowButton
+          icon='run-schedule'
+          btnStyle='warning'
+          handler={runJob}
+          handlerParam={schedule.job}
+        />
+      </ButtonGroup>
+    </fieldset>
+  }
+]
+
 // ===================================================================
 
 export default class Overview extends Component {
@@ -59,7 +116,7 @@ export default class Overview extends Component {
     const unsubscribeSchedules = subscribeSchedules(schedules => {
       // Get only backup jobs.
       schedules = filter(schedules, schedule => {
-        const job = this._getScheduleJob(schedule)
+        const job = this.state.jobs && this.state.jobs[schedule.job]
         return job && jobKeyToLabel[job.key]
       })
 
@@ -81,49 +138,30 @@ export default class Overview extends Component {
     }
   }
 
-  _getScheduleJob (schedule) {
-    const { jobs } = this.state || {}
-    return jobs[schedule.job]
-  }
+  _getScheduleCollection = createSelector(
+    () => this.state.schedules,
+    () => this.state.scheduleTable,
+    () => this.state.jobs,
+    (schedules, scheduleTable, jobs) => {
+      if (!schedules || !jobs) {
+        return []
+      }
 
-  _getJobLabel (job = {}) {
-    return jobKeyToLabel[job.key] || _('unknownSchedule')
-  }
+      return map(schedules, schedule => {
+        const job = jobs[schedule.job]
+        const { items } = job.paramsVector
 
-  _getScheduleTag (schedule, job = {}) {
-    try {
-      const { paramsVector } = job
-      const values = paramsVector.items
-
-      // Old versions of XenOrchestra uses values[0]
-      return (
-        values[0].values[0].tag ||
-        values[1].values[0].tag
-      )
-    } catch (_) {}
-
-    return schedule.id
-  }
-
-  _getScheduleToggle (schedule) {
-    const { id } = schedule
-
-    return (
-      <ActionToggle
-        value={this.state.scheduleTable[id]}
-        handler={this._updateScheduleState}
-        handlerParam={id}
-        size='small'
-      />
-    )
-  }
-
-  _updateScheduleState = id => {
-    const enabled = this.state.scheduleTable[id]
-    const method = enabled ? disableSchedule : enableSchedule
-
-    return method(id)
-  }
+        return {
+          jobId: job.id,
+          jobLabel: jobKeyToLabel[job.key] || _('unknownSchedule'),
+          // Old versions of XenOrchestra use items[0]
+          scheduleTag: get(items, '[0].values[0].tag') || get(items, '[1].values[0].tag') || schedule.id,
+          schedule,
+          scheduleToggleValue: scheduleTable && scheduleTable[schedule.id]
+        }
+      })
+    }
+  )
 
   render () {
     const {
@@ -138,53 +176,7 @@ export default class Overview extends Component {
           </CardHeader>
           <CardBlock>
             {schedules.length ? (
-              <table className='table'>
-                <thead className='thead-default'>
-                  <tr>
-                    <th>{_('job')}</th>
-                    <th>{_('jobTag')}</th>
-                    <th className='hidden-xs-down'>{_('jobScheduling')}</th>
-                    <th className='hidden-xs-down'>{_('jobTimezone')}</th>
-                    <th>{_('jobState')}</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {map(schedules, (schedule, key) => {
-                    const job = this._getScheduleJob(schedule)
-
-                    return (
-                      <tr key={key}>
-                        <td>{job.id.slice(0, 5)} ({this._getJobLabel(job)})</td>
-                        <td>{this._getScheduleTag(schedule, job)}</td>
-                        <td className='hidden-xs-down'>{schedule.cron}</td>
-                        <td className='hidden-xs-down'>{schedule.timezone || _('jobServerTimezone')}</td>
-                        <td>
-                          {this._getScheduleToggle(schedule)}
-                          <fieldset className='pull-right'>
-                            <Link className='btn btn-sm btn-primary mr-1' to={`/backup/${schedule.id}/edit`}>
-                              <Icon icon='edit' />
-                            </Link>
-                            <ButtonGroup>
-                              <ActionRowButton
-                                icon='delete'
-                                btnStyle='danger'
-                                handler={deleteBackupSchedule}
-                                handlerParam={schedule}
-                              />
-                              <ActionRowButton
-                                icon='run-schedule'
-                                btnStyle='warning'
-                                handler={runJob}
-                                handlerParam={schedule.job}
-                              />
-                            </ButtonGroup>
-                          </fieldset>
-                        </td>
-                      </tr>
-                    )
-                  })}
-                </tbody>
-              </table>
+              <SortedTable columns={JOB_COLUMNS} collection={this._getScheduleCollection()} />
             ) : <p>{_('noScheduledJobs')}</p>}
           </CardBlock>
         </Card>
