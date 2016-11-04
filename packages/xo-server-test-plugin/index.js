8496a9bebd9e001b42fc64fb1ef79e1e5e3ae194
Badr AZIZBI <azizbibadr@gmail.com>
2016-11-04 13:42:23 +0100

    Initial commit

--- packages/xo-server-test-plugin/index.js
+++ packages/xo-server-test-plugin/index.js
@@ -15,6 +15,18 @@ exports.configurationSchema = {
   required: ['foo']
 }
 
+// This (optional) schema is used to test the configuration 
+// of the plugin.
+exports.testSchema = {
+  type: 'object',
+  properties: {
+    test: {
+       type: 'string'
+    }
+  },
+  required: ['test']
+}
+
 // The default export is just a factory function which will create an
 // instance of the plugin.
 // Usually it will be called only once, at startup.
@@ -22,7 +34,6 @@ exports.configurationSchema = {
 // Its only parameter is an object which currently only contains a
 // `xo` property: the instance of the currently running xo-server.
 exports.default = function (opts) {
-
   // For simplicity's sake, this plugin returns a plain object, but
   // usually it returns a new instance of an existing class.
   return {
@@ -52,6 +63,15 @@ exports.default = function (opts) {
     // Note: will only be called if the plugin is currently loaded.
     unload: function () {
       console.log('stub unloaded')
+    },
+
+    // This (optional) method is called to test the configuration of the plugin.
+    // Note 1: will only be called if the plugin has been successfully configured.
+    // Note 2: before being called, the test configuration is validated
+    // against the provided test configuration schema.
+    // Note 3: will only be called if the test option is activated.
+    test: function(data){
+      console.log('the configuration is valid')
     }
   }
 }
