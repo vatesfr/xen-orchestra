6613ba02aba4c8e1139b9c451942806e640e07a9
Pierre Donias <pierre.donias@gmail.com>
2016-11-22 16:41:07 +0100

    feat(dashboard/health): better formating of alert messages (#1781)
    Fixes #1776

--- src/xo-app/dashboard/health/index.js
+++ src/xo-app/dashboard/health/index.js
@@ -1,18 +1,23 @@
 import _ from 'intl'
 import ActionRowButton from 'action-row-button'
+import Component from 'base-component'
+import get from 'lodash/get'
 import Icon from 'icon'
 import isEmpty from 'lodash/isEmpty'
 import Link from 'link'
 import map from 'lodash/map'
+import mapValues from 'lodash/mapValues'
 import SortedTable from 'sorted-table'
 import TabButton from 'tab-button'
 import Tooltip from 'tooltip'
 import Upgrade from 'xoa-upgrade'
-import React, { Component } from 'react'
+import React from 'react'
+import xml2js from 'xml2js'
 import { Card, CardHeader, CardBlock } from 'card'
 import { confirm } from 'modal'
 import { deleteMessage, deleteVdi, deleteOrphanedVdis, deleteVm, isSrWritable } from 'xo'
 import { FormattedRelative, FormattedTime } from 'react-intl'
+import { fromCallback } from 'promise-toolbox'
 import { Container, Row, Col } from 'grid'
 import {
   createGetObject,
@@ -39,11 +44,21 @@ const VmColContainer = connectStore(() => ({
 
 const AlarmColObject = connectStore(() => ({
   object: createGetObject()
-}))(({ object }) => object.type === 'VM-controller' ? <Link to={`hosts/${object.$container}`}>{object.name_label}</Link> : <Link to={`vms/${object.id}`}>{object.name_label}</Link>)
+}))(({ object }) => {
+  if (!object) {
+    return null
+  }
+  return object.type === 'VM-controller' ? <Link to={`hosts/${object.$container}`}>{object.name_label}</Link> : <Link to={`vms/${object.id}`}>{object.name_label}</Link>
+})
 
 const AlarmColPool = connectStore(() => ({
   pool: createGetObject()
-}))(({ pool }) => <Link to={`pools/${pool.id}`}>{pool.name_label}</Link>)
+}))(({ pool }) => {
+  if (!pool) {
+    return null
+  }
+  return <Link to={`pools/${pool.id}`}>{pool.name_label}</Link>
+})
 
 const SR_COLUMNS = [
   {
@@ -161,7 +176,19 @@ const ALARM_COLUMNS = [
   },
   {
     name: _('alarmContent'),
-    itemRenderer: message => message.body,
+    itemRenderer: ({ formatted, body }) => formatted
+      ? <div>
+        <Row>
+          <Col mediumSize={6}><strong>{formatted.name}</strong></Col>
+          <Col mediumSize={6}>{formatted.value}</Col>
+        </Row>
+        <br />
+        {map(formatted.alarmAttributes, (value, label) => <Row>
+          <Col mediumSize={6}>{label}</Col>
+          <Col mediumSize={6}>{value}</Col>
+        </Row>)}
+      </div>
+      : <pre style={{ whiteSpace: 'pre-wrap' }}>{body}</pre>,
     sortCriteria: message => message.body
   },
   {
@@ -211,6 +238,46 @@ const ALARM_COLUMNS = [
   }
 })
 export default class Health extends Component {
+  componentWillReceiveProps (props) {
+    if (props.alertMessages !== this.props.alertMessages) {
+      Promise.all(
+        map(props.alertMessages, ({ body }) => {
+          const matches = /^value: ([0-9\.]+) config: (.*)$/.exec(body)
+          if (!matches) {
+            return
+          }
+
+          const [ , value, xml ] = matches
+          return fromCallback(cb =>
+            xml2js.parseString(xml, cb)
+          ).then(
+            result => {
+              const object = mapValues(result && result.variable, value => get(value, '[0].$.value'))
+              if (!object || !object.name) {
+                return
+              }
+
+              const { name, ...alarmAttributes } = object
+
+              return { name, value, alarmAttributes }
+            },
+            noop
+          )
+        })
+      ).then(
+        formattedMessages => {
+          this.setState({
+            messages: map(formattedMessages, (formattedMessage, index) => ({
+              formatted: formattedMessage,
+              ...props.alertMessages[index]
+            }))
+          })
+        },
+        noop
+      )
+    }
+  }
+
   _deleteOrphanedVdis = () =>
     deleteOrphanedVdis(this.props.vdiOrphaned)
 
@@ -323,7 +390,7 @@ export default class Health extends Component {
                     </Row>
                     <Row>
                       <Col>
-                        <SortedTable collection={this.props.alertMessages} columns={ALARM_COLUMNS} />
+                        <SortedTable collection={this.state.messages} columns={ALARM_COLUMNS} />
                       </Col>
                     </Row>
                   </div>
