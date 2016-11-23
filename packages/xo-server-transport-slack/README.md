2de4d36a0f494cbdffee40c5812ae61572782c76
badrAZ <azizbibadr@gmail.com>
2016-11-23 09:37:39 +0100

    implementation (#2)

--- packages/xo-server-transport-slack/README.md
+++ packages/xo-server-transport-slack/README.md
@@ -12,11 +12,50 @@ Installation of the [npm package](https://npmjs.org/package/xo-server-transport-
 
 ## Usage
 
-**TODO**
+Like all other xo-server plugins, it can be configured directly via
+the web iterface, see [the plugin documentation](https://xen-orchestra.com/docs/plugins.html). You can also test the configuration plugin if it works.
+
+### Slack
+
+#### Generate the Webhook
+
+1. Log in your Slack team account
+2. Click on the **Main menu** at the top and choose **Apps & Integrations**
+
+   ![Apps & Integrations](image/DocImg1.png)
+3. Search **Incoming WebHooks**
+
+   ![Incoming WebHooks](image/DocImg2.png)
+4. Click on **Add Configuration**
+
+   ![Add Configuration](image/DocImg3.png)
+5. Choose the default channel and click on **Add Incoming WebHooks integration**
+
+   ![Add Incoming WebHooks integration](image/DocImg4.png)
+6. Modify the default settings and click on **Save Settings**
+
+   ![Save Settings](image/DocImg5.png)
+
+### Testing the plugin
+
+#### Slack
+
+![Slack configuration](image/DocImg6.png)
+
+![Slack](image/DocImg7.png)
+
+#### Mattermost
+
+![Mattermost configuration](image/DocImg8.png)
+
+![Mattermost](image/DocImg9.png)
 
 ## Development
 
-```
+### `Xo#sendSlackMessage({ message }) `
+
+This xo method is called to send the message passed in parameter to Slack or Mattermost.
+
 # Install dependencies
 > npm install
 
--- packages/xo-server-transport-slack/src/index.js
+++ packages/xo-server-transport-slack/src/index.js
@@ -1,27 +1,85 @@
-export const configurationSchema = null
+import Slack from 'slack-node'
+import { promisify } from 'promise-toolbox'
 
-export const testSchema = null
+// ===================================================================
 
-class XoServerTransportNagios {
+const logAndRethrow = error => {
+  console.error('[WARN] plugin transport-slack:', error && error.stack || error)
+
+  throw error
+}
+
+// ===================================================================
+
+export const configurationSchema = {
+  type: 'object',
+  properties: {
+    webhookUri: {
+      type: 'string',
+      description: 'The Mattermost or Slack webhook URL.'
+    },
+    channel: {
+      type: 'string',
+      description: 'Channel, private group, or IM channel to send message to.'
+    },
+    username: {
+      type: 'string',
+      description: 'Bot username.'
+    },
+    icon_emoji: {
+      type: 'string',
+      description: 'The bot icon. It can be a slack emoji or a URL image.'
+    }
+  },
+  additionalProperties: false,
+  required: ['webhookUri', 'channel']
+}
+
+// ===================================================================
+
+class XoServerTransportSlack {
   constructor ({ xo }) {
-    this._xo = xo
+    this._sendSlack = ::this._sendSlack
+    this._set = ::xo.defineProperty
+    this._unset = null
+
+   // Defined in configure().
+    this._conf = null
+    this._send = null
   }
 
-  configure (configuration) {
-    throw new Error('not implemented')
+  configure ({
+    webhookUri,
+    ...conf
+  }) {
+    const slack = new Slack()
+    slack.setWebhook(webhookUri)
+    this._conf = conf
+    this._send = promisify(slack.webhook)
   }
 
   load () {
-    throw new Error('not implemented')
+    this._unset = this._set('sendSlackMessage', this._sendSlack)
   }
 
   unload () {
-    throw new Error('not implemented')
+    this._unset()
   }
 
   test () {
-    throw new Error('not implemented')
+    return this._sendSlack({
+      message: `Hi there,
+
+The transport-slack plugin for Xen Orchestra server seems to be working fine, nicely done :)`
+    })
+  }
+
+  _sendSlack ({
+    message
+  }) {
+    // TODO: handle errors
+    return this._send({ ...this._conf, text: message }).catch(logAndRethrow)
   }
 }
 
-export default opts => new XoServerTransportNagios(opts)
+export default opts => new XoServerTransportSlack(opts)
