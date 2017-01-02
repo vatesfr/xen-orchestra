77d43b22808c98b36dbba3942a52916b3280be81
Julien Fontanet <julien.fontanet@isonoe.net>
2017-01-02 16:35:41 +0100

    fix(settings/logs): correctly refresh users

--- src/xo-app/settings/logs/index.js
+++ src/xo-app/settings/logs/index.js
@@ -16,60 +16,60 @@ import { createSelector } from 'selectors'
 import { FormattedDate } from 'react-intl'
 import { subscribeApiLogs, subscribeUsers, deleteApiLog } from 'xo'
 
+const COLUMNS = [
+  {
+    name: _('logUser'),
+    itemRenderer: (log, { users }) => {
+      if (log.data.userId == null) {
+        return _('noUser')
+      }
+      if (!users) {
+        return '...'
+      }
+      const user = find(users, user => user.id === log.data.userId)
+      return user ? user.email : _('unknownUser')
+    },
+    sortCriteria: log => log.data.userId
+  },
+  {
+    name: _('logMessage'),
+    itemRenderer: log => <pre className={styles.widthLimit}>{log.data.error && log.data.error.message}</pre>,
+    sortCriteria: log => log.data.error && log.data.error.message
+  },
+  {
+    default: true,
+    name: _('logTime'),
+    itemRenderer: log => <span>
+      {log.time && <FormattedDate value={new Date(log.time)} month='long' day='numeric' year='numeric' hour='2-digit' minute='2-digit' second='2-digit' />}
+    </span>,
+    sortCriteria: log => log.time,
+    sortOrder: 'desc'
+  },
+  {
+    name: '',
+    itemRenderer: (log, { showError }) => <div className='text-xs-right'><ButtonGroup>
+      <ActionRowButton
+        handler={showError}
+        handlerParam={log}
+        icon='preview'
+        tooltip={_('logDisplayDetails')}
+      />
+      <ActionRowButton
+        btnStyle='danger'
+        handler={deleteApiLog}
+        handlerParam={log.id}
+        icon='delete'
+        tooltip={_('logDelete')}
+      />
+    </ButtonGroup></div>
+  }
+]
+
 @addSubscriptions({
   logs: subscribeApiLogs,
   users: subscribeUsers
 })
 export default class Logs extends BaseComponent {
-  _columns = [
-    {
-      name: _('logUser'),
-      itemRenderer: log => {
-        if (log.data.userId == null) {
-          return _('noUser')
-        }
-        if (!this.props.users) {
-          return '...'
-        }
-        const user = find(this.props.users, user => user.id === log.data.userId)
-        return user ? user.email : _('unknownUser')
-      },
-      sortCriteria: log => log.data.userId
-    },
-    {
-      name: _('logMessage'),
-      itemRenderer: log => <pre className={styles.widthLimit}>{log.data.error && log.data.error.message}</pre>,
-      sortCriteria: log => log.data.error && log.data.error.message
-    },
-    {
-      default: true,
-      name: _('logTime'),
-      itemRenderer: log => <span>
-        {log.time && <FormattedDate value={new Date(log.time)} month='long' day='numeric' year='numeric' hour='2-digit' minute='2-digit' second='2-digit' />}
-      </span>,
-      sortCriteria: log => log.time,
-      sortOrder: 'desc'
-    },
-    {
-      name: '',
-      itemRenderer: log => <div className='text-xs-right'><ButtonGroup>
-        <ActionRowButton
-          handler={this._showError}
-          handlerParam={log}
-          icon='preview'
-          tooltip={_('logDisplayDetails')}
-        />
-        <ActionRowButton
-          btnStyle='danger'
-          handler={deleteApiLog}
-          handlerParam={log.id}
-          icon='delete'
-          tooltip={_('logDelete')}
-        />
-      </ButtonGroup></div>
-    }
-  ]
-
   _deleteAllLogs = () =>
     confirm({
       title: _('logDeleteAllTitle'),
@@ -86,6 +86,12 @@ export default class Logs extends BaseComponent {
   _showError = log => alert(_('logError'), <Copiable tagName='pre'>{`${log.data.method}
 ${JSON.stringify(log.data.params, null, 2)}
 ${JSON.stringify(log.data.error, null, 2)}`}</Copiable>)
+
+  _getData = createSelector(
+    () => this.props.users,
+    () => this._showError,
+    (users, showError) => ({ users, showError })
+  )
 
   render () {
     const logs = this._getLogs()
@@ -105,7 +111,8 @@ ${JSON.stringify(log.data.error, null, 2)}`}</Copiable>)
           {' '}
           <SortedTable
             collection={logs}
-            columns={this._columns}
+            columns={COLUMNS}
+            userData={this._getData()}
           />
         </div>
       }
