d50e1b4e02a6f87c16b87961f4432d7cf4ce1753
badrAZ <azizbibadr@gmail.com>
2017-01-12 17:07:29 +0100

    fix: role tests (#30)

--- src/role.spec.js
+++ src/role.spec.js
@@ -1,28 +1,19 @@
 /* eslint-env jest */
 
-// Doc: https://github.com/moll/js-must/blob/master/doc/API.md#must
-import expect from 'must'
-
-// ===================================================================
-
-import {getMainConnection} from './util'
+import {
+  xo
+} from './util'
 
 // ===================================================================
 
 describe('role', () => {
-  let xo
-  beforeAll(async () => {
-    xo = await getMainConnection()
-  })
-
-// ==================================================================
   describe('.getAll()', () => {
     it(' returns all the roles', async () => {
       const role = await xo.call('role.getAll')
 
       // FIXME: use permutationOf but figure out how not to compare objects by
       // equality.
-      expect(role).to.eql([
+      expect(role).toEqual([
         {
           id: 'viewer',
           name: 'Viewer',
