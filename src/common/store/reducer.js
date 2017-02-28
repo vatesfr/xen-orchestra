079310c67e49e96b8ea90ab055f80463e0127457
Julien Fontanet <julien.fontanet@isonoe.net>
2017-02-28 17:01:36 +0100

    fix(store/reducer/object): missing part of previous fix

--- src/common/store/reducer.js
+++ src/common/store/reducer.js
@@ -105,7 +105,7 @@ export default {
         const previous = all[id]
 
         if (object) {
-          const { type } = object.type
+          const { type } = object
 
           all[id] = object
           get(type)[id] = object
