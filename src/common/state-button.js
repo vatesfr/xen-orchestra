5a84e6e81886eef769ef7755960439b0cd82924c
Julien Fontanet <julien.fontanet@isonoe.net>
2017-03-22 18:16:00 +0100

    fix: ThemeProvider at application level

--- src/common/state-button.js
+++ src/common/state-button.js
@@ -1,36 +1,25 @@
 import React from 'react'
-import styled, { ThemeProvider } from 'styled-components'
+import styled from 'styled-components'
 
 import ActionButton from './action-button'
-import themes from './themes'
 import propTypes from './prop-types'
 
 const Button = styled(ActionButton)`
-  background-color: ${props => props.theme.bgColor}
-  border: 2px solid ${props => props.theme.color}
-  color: ${props => props.theme.color}
+  background-color: ${p => p.theme[`${p.enabled ? 'enabled' : 'disabled'}StateBg`]}
+  border: 2px solid ${p => p.theme[`${p.enabled ? 'enabled' : 'disabled'}StateColor`]}
+  color: ${p => p.theme[`${p.enabled ? 'enabled' : 'disabled'}StateColor`]}
 `
-const enabledTheme = {
-  color: themes.base.enabledStateColor,
-  bgColor: themes.base.enabledStateBg
-}
-
-const disabledTheme = {
-  color: themes.base.disabledStateColor,
-  bgColor: themes.base.disabledStateBg
-}
 
 const StateButton = ({value, state, ...props}) =>
-  <ThemeProvider theme={value ? enabledTheme : disabledTheme}>
-    <Button
-      {...props}
-      icon={value ? 'running' : 'halted'}
-      size='small'
-    >
-      { state }
-    </Button>
-  </ThemeProvider>
+  <Button
+    {...props}
+    enabled={value}
+    icon={value ? 'running' : 'halted'}
+    size='small'
+  >
+    { state }
+  </Button>
 
 export default propTypes({
-  value: propTypes.bool
+  value: propTypes.bool.isRequired
 })(StateButton)
