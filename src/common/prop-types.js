61179ec67d22591227a9cc35a017b4465f2ab4c5
Julien Fontanet <julien.fontanet@isonoe.net>
2017-05-09 14:31:43 +0200

    feat(prop-types): can also be used to set context types

--- src/common/prop-types.js
+++ src/common/prop-types.js
@@ -1,22 +1,33 @@
 import assign from 'lodash/assign'
 import { PropTypes } from 'react'
 
-// Decorators to help declaring on React components without using the
-// tedious static properties syntax.
+// Decorators to help declaring properties and context types on React
+// components without using the tedious static properties syntax.
 //
 // ```js
 // @propTypes({
 //   children: propTypes.node.isRequired
+// }, {
+//   store: propTypes.object.isRequired
 // })
 // class MyComponent extends React.Component {}
 // ```
-const propTypes = types => target => {
-  target.propTypes = {
-    ...target.propTypes,
-    ...types
+const propTypes = (propTypes, contextTypes) => target => {
+  if (propTypes !== undefined) {
+    target.propTypes = {
+      ...target.propTypes,
+      ...propTypes
+    }
+  }
+  if (contextTypes !== undefined) {
+    target.contextTypes = {
+      ...target.contextTypes,
+      ...contextTypes
+    }
   }
 
   return target
 }
 assign(propTypes, PropTypes)
+
 export { propTypes as default }
