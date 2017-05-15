923166b4e3c4f396282fa9fc939d8b8727a7fdd6
Julien Fontanet <julien.fontanet@isonoe.net>
2017-05-15 16:30:10 +0200

    feat(Icon): pass extraneous props down

--- src/common/icon.js
+++ src/common/icon.js
@@ -2,14 +2,16 @@ import classNames from 'classnames'
 import isInteger from 'lodash/isInteger'
 import React, { PropTypes } from 'react'
 
-const Icon = ({ className, icon, size = 1, fixedWidth }) => (
-  <i className={classNames(
-      className,
-      icon ? `xo-icon-${icon}` : 'fa', // Without icon prop, is a placeholder.
-      isInteger(size) ? `fa-${size}x` : `fa-${size}`,
-      fixedWidth && 'fa-fw'
-  )} />
-)
+const Icon = ({ icon, size = 1, fixedWidth, ...props }) => {
+  props.className = classNames(
+    props.className,
+    icon !== undefined ? `xo-icon-${icon}` : 'fa', // Without icon prop, is a placeholder.
+    isInteger(size) ? `fa-${size}x` : `fa-${size}`,
+    fixedWidth && 'fa-fw'
+  )
+
+  return <i {...props} />
+}
 Icon.propTypes = {
   fixedWidth: PropTypes.bool,
   icon: PropTypes.string,
