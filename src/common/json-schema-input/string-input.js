1e5dc9efe748ac513292accd9733e2fe57c94946
Julien Fontanet <julien.fontanet@isonoe.net>
2017-06-02 16:21:30 +0200

    fix(json-schema-input/string): consider empty value as undefined (#2192)

--- src/common/json-schema-input/string-input.js
+++ src/common/json-schema-input/string-input.js
@@ -1,8 +1,9 @@
 import React from 'react'
-
 import uncontrollableInput from 'uncontrollable-input'
+
 import Combobox from '../combobox'
 import Component from '../base-component'
+import getEventValue from '../get-event-value'
 import propTypes from '../prop-types-decorator'
 
 import { PrimitiveInputWrapper } from './helpers'
@@ -14,23 +15,30 @@ import { PrimitiveInputWrapper } from './helpers'
 })
 @uncontrollableInput()
 export default class StringInput extends Component {
+  // the value of this input  is undefined not '' when empty to make
+  // it homogenous with when the user has never touched this input
+  _onChange = event => {
+    const value = getEventValue(event)
+    this.props.onChange(value !== '' ? value : undefined)
+  }
+
   render () {
     const { required, schema } = this.props
     const {
       disabled,
-      onChange,
       password,
       placeholder = schema.default,
       value,
       ...props
     } = this.props
+    delete props.onChange
 
     return (
       <PrimitiveInputWrapper {...props}>
         <Combobox
-          value={value || ''}
+          value={value !== undefined ? value : ''}
           disabled={disabled}
-          onChange={onChange}
+          onChange={this._onChange}
           options={schema.defaults}
           placeholder={placeholder || schema.default}
           required={required}
