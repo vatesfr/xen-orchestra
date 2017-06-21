1dff6f81fccd26a9c0e3a50e3ee33e79954ab393
Julien Fontanet <julien.fontanet@isonoe.net>
2017-06-21 14:33:15 +0200

    feat(examples): also accept UUID and name labels

--- packages/xen-api/examples/utils.js
+++ packages/xen-api/examples/utils.js
@@ -1,6 +1,8 @@
 const { createReadStream, createWriteStream, statSync } = require('fs')
 const { PassThrough } = require('stream')
 
+const { isOpaqueRef } = require('../')
+
 exports.createInputStream = path => {
   if (path === undefined || path === '-') {
     return process.stdin
@@ -23,3 +25,17 @@ exports.createOutputStream = path => {
   stream.pipe(process.stdout)
   return stream
 }
+
+exports.resolveRef = (xapi, type, refOrUuidOrNameLabel) =>
+  isOpaqueRef(refOrUuidOrNameLabel)
+    ? refOrUuidOrNameLabel
+    : xapi.call(`${type}.get_by_uuid`, refOrUuidOrNameLabel).catch(
+      () => xapi.call(`${type}.get_by_name_label`, refOrUuidOrNameLabel).then(
+        refs => {
+          if (refs.length === 1) {
+            return refs[0]
+          }
+          throw new Error(`no single match for ${type} with name label ${refOrUuidOrNameLabel}`)
+        }
+      )
+    )
