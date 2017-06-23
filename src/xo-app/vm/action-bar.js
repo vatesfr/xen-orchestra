eba4a18a91a5952829ec68b00ea2c04c52fb6cfe
badrAZ <azizbibadr@gmail.com>
2017-06-23 12:00:07 +0200

    fix: a user with ACLs(admin) on a pool should be able to migrate a self service VM
    Fixes #2212

--- src/xo-app/vm/action-bar.js
+++ src/xo-app/vm/action-bar.js
@@ -31,7 +31,7 @@ const vmActionBarByState = {
           handler: restartVm,
           pending: includes(vm.current_operations, 'clean_reboot')
         },
-        (isAdmin || !vm.resourceSet) && {
+        {
           icon: 'vm-migrate',
           label: 'migrateVmLabel',
           handler: migrateVm,
