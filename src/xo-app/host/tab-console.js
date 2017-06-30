cb3b662ebb067aac5b32631f7cd727d53787320d
Olivier Lambert <olivier.lambert@vates.fr>
2017-06-30 17:28:45 +0200

    pr review fixes

--- src/xo-app/host/tab-console.js
+++ src/xo-app/host/tab-console.js
@@ -92,7 +92,6 @@ export default class extends Component {
         <Row className='console'>
           <Col>
             <NoVnc ref='noVnc' url={resolveUrl(`consoles/${vmController.id}`)} onClipboardChange={this._getRemoteClipboard} />
-            <p><em><Icon icon='info' /> {_('tipLabel')} {_('tipConsoleLabel')}</em></p>
           </Col>
         </Row>
       </Container>
