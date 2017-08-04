761a2ed8723c5f2774ad0e1af61df614aa3a68fb
badrAZ <azizbibadr@gmail.com>
2017-08-04 16:34:58 +0200

    collection can be an object

--- src/common/no-objects.js
+++ src/common/no-objects.js
@@ -6,12 +6,15 @@ import propTypes from './prop-types-decorator'
 const NoObjects = ({ children, className, collection, message, predicate }) => !predicate(collection)
   ? <h2 className={className}><img src='assets/loading.svg' /></h2>
   : isEmpty(collection)
-    ? <p className={className}> {message} </p>
-    : <div> {children} </div>
+    ? <p className={className}>{message}</p>
+    : <div>{children}</div>
 
 propTypes(NoObjects)({
   children: propTypes.node.isRequired,
-  collection: propTypes.array.isRequired,
+  collection: propTypes.oneOfType([
+    propTypes.array,
+    propTypes.object
+  ]).isRequired,
   message: propTypes.node.isRequired,
   className: propTypes.string,
   predicate: propTypes.func.isRequired
