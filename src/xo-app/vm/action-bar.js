3f29dd129f2464b008e65f1aa0f4d81a20a5ccf3
badrAZ <azizbibadr@gmail.com>
2017-08-08 14:53:36 +0200

    fix(vm/action-bar): no migrate when VM is halted (#2291)
    Fixes #2233

--- src/xo-app/vm/action-bar.js
+++ src/xo-app/vm/action-bar.js
@@ -82,12 +82,6 @@ const vmActionBarByState = {
         pending={includes(vm.current_operations, 'clone')}
       />}
       {!isSelfUser && <Action
-        handler={migrateVm}
-        icon='vm-migrate'
-        label={_('migrateVmLabel')}
-        pending={includes(vm.current_operations, 'pool_migrate')}
-      />}
-      {!isSelfUser && <Action
         handler={snapshotVm}
         icon='vm-snapshot'
         label={_('snapshotVmLabel')}
