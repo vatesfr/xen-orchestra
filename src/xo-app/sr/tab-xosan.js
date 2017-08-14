ce1f70709fcfafd11c70400234a93f55ab9b15fb
Nicolas Raynaud <github@nraynaud.com>
2017-08-14 16:04:04 -0700

    XOSAN: remove warning

--- src/xo-app/sr/tab-xosan.js
+++ src/xo-app/sr/tab-xosan.js
@@ -161,7 +161,7 @@ export default class TabXosan extends Component {
         <div key={node.config.brickName}>
           <h3>Brick {node.config.brickName}</h3>
           <div style={{ marginLeft: '15px' }}>
-            <Row><Col size={2}>Virtual Machine: </Col><Col size={4}>{vms[node.config.vm.id] != null && <Link
+            <Row><Col size={2}>Virtual Machine: </Col><Col size={4}>{vms[node.config.vm.id] !== undefined && <Link
               to={`/vms/${node.config.vm.id}`}>{vms[node.config.vm.id].name_label}</Link>}</Col></Row>
             <Row><Col size={2}>Underlying Storage: </Col><Col size={4}><Link
               to={`/srs/${node.config.underlyingSr}`}>{srs[node.config.underlyingSr].name_label}</Link> -
