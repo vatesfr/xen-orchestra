5e0caa0952597e2534a81587fbb675a3c73e16ef
Julien Fontanet <julien.fontanet@isonoe.net>
2017-09-27 10:32:14 +0200

    chore(SelectLun): cleaner code

--- src/xo-app/new/sr/index.js
+++ src/xo-app/new/sr/index.js
@@ -2,7 +2,6 @@ import _, { messages } from 'intl'
 import ActionButton from 'action-button'
 import Component from 'base-component'
 import filter from 'lodash/filter'
-import find from 'lodash/find'
 import Icon from 'icon'
 import includes from 'lodash/includes'
 import info, { error } from 'notification'
@@ -96,37 +95,29 @@ class SelectIqn extends Component {
   options: propTypes.array.isRequired
 })
 class SelectLun extends Component {
-  _computeOptions (props = this.props) {
-    this.setState({
-      options: map(props.options, lun => ({
-        value: lun.id,
-        label: `LUN ${lun.id}: ${lun.serial} - ${formatSize(+lun.size)} - (${lun.vendor})`
-      }))
-    })
-  }
+  _getOptions = createSelector(
+    () => this.props.options,
+    options => map(options, (lun, index) => ({
+      label: `LUN ${lun.id}: ${lun.serial} - ${formatSize(+lun.size)} - (${lun.vendor})`,
+      value: index
+    }))
+  )
 
-  _handleChange = value => {
-    const { onChange, options } = this.props
-    value = value.value
-    this.setState({ value }, () => onChange(find(options, { id: value })))
-  }
-
-  componentWillMount () {
-    this._computeOptions()
-  }
-
-  componentWillReceiveProps (props) {
-    this._computeOptions(props)
+  _handleChange = ({ value }) => {
+    const { props } = this
+    this.setState(
+      { value },
+      () => props.onChange(props.options[value])
+    )
   }
 
   render () {
-    const { state } = this
     return (
       <Select
         clearable={false}
         onChange={this._handleChange}
-        options={state.options}
-        value={state.value}
+        options={this._getOptions()}
+        value={this.state.value}
       />
     )
   }
