0c8fc9fd585118bee1e6ee65cfdd8f979050555e
Zakariae <ejjelthizakariae@gmail.com>
2017-10-30 11:46:51 +0100

    tab-logs v1

--- src/xo-app/host/tab-network.js
+++ src/xo-app/host/tab-network.js
@@ -8,6 +8,7 @@ import map from 'lodash/map'
 import pick from 'lodash/pick'
 import SingleLineRow from 'single-line-row'
 import some from 'lodash/some'
+import SortedTable from 'sorted-table'
 import StateButton from 'state-button'
 import TabButton from 'tab-button'
 import Tooltip from 'tooltip'
@@ -26,13 +27,16 @@ import {
   editNetwork,
   editPif,
   getIpv4ConfigModes,
-  reconfigurePifIp,
+  reconfigurePifIp
 } from 'xo'
 
 const EDIT_BUTTON_STYLE = { color: '#999', cursor: 'pointer' }
 
-const _toggleDefaultLockingMode = (component, tooltip) =>
-  tooltip ? <Tooltip content={tooltip}>{component}</Tooltip> : component
+const _toggleDefaultLockingMode = (component, tooltip) => tooltip
+  ? <Tooltip content={tooltip}>
+    {component}
+  </Tooltip>
+  : component
 
 class ConfigureIpModal extends Component {
   constructor (props) {
@@ -51,64 +55,61 @@ class ConfigureIpModal extends Component {
   render () {
     const { ip, netmask, dns, gateway } = this.state
 
-    return (
-      <div>
-        <SingleLineRow>
-          <Col size={6}>{_('staticIp')}</Col>
-          <Col size={6}>
-            <input
-              className='form-control'
-              onChange={this.linkState('ip')}
-              value={ip}
-            />
-          </Col>
-        </SingleLineRow>
-        &nbsp;
-        <SingleLineRow>
-          <Col size={6}>{_('netmask')}</Col>
-          <Col size={6}>
-            <input
-              className='form-control'
-              onChange={this.linkState('netmask')}
-              value={netmask}
-            />
-          </Col>
-        </SingleLineRow>
-        &nbsp;
-        <SingleLineRow>
-          <Col size={6}>{_('dns')}</Col>
-          <Col size={6}>
-            <input
-              className='form-control'
-              onChange={this.linkState('dns')}
-              value={dns}
-            />
-          </Col>
-        </SingleLineRow>
-        &nbsp;
-        <SingleLineRow>
-          <Col size={6}>{_('gateway')}</Col>
-          <Col size={6}>
-            <input
-              className='form-control'
-              onChange={this.linkState('gateway')}
-              value={gateway}
-            />
-          </Col>
-        </SingleLineRow>
-      </div>
-    )
+    return <div>
+      <SingleLineRow>
+        <Col size={6}>{_('staticIp')}</Col>
+        <Col size={6}>
+          <input className='form-control' onChange={this.linkState('ip')} value={ip} />
+        </Col>
+      </SingleLineRow>
+      &nbsp;
+      <SingleLineRow>
+        <Col size={6}>{_('netmask')}</Col>
+        <Col size={6}>
+          <input className='form-control' onChange={this.linkState('netmask')} value={netmask} />
+        </Col>
+      </SingleLineRow>
+      &nbsp;
+      <SingleLineRow>
+        <Col size={6}>{_('dns')}</Col>
+        <Col size={6}>
+          <input className='form-control' onChange={this.linkState('dns')} value={dns} />
+        </Col>
+      </SingleLineRow>
+      &nbsp;
+      <SingleLineRow>
+        <Col size={6}>{_('gateway')}</Col>
+        <Col size={6}>
+          <input className='form-control' onChange={this.linkState('gateway')} value={gateway} />
+        </Col>
+      </SingleLineRow>
+    </div>
   }
 }
 
 @connectStore(() => ({
-  vifsByNetwork: createGetObjectsOfType('VIF').groupBy('$network'),
+  vifsByNetwork: createGetObjectsOfType('VIF').groupBy('$network')
 }))
-class PifItem extends Component {
-  state = { configModes: [] }
+class PifItemVlan extends Component {
+  _editPif = vlan =>
+    editPif(this.props.pif, { vlan })
+  render () {
+    const {pif} = this.props
+    return <div>{pif.vlan === -1
+      ? 'None'
+      : <Number value={pif.vlan} onChange={this._editPif}>
+        {pif.vlan}
+      </Number>
+    }</div>
+  }
+}
 
-  componentWillMount () {
-    getIpv4ConfigModes().then(configModes => this.setState({ configModes }))
+class PifItemIp extends Component {
+  state = { configModes: [] }
+  componentDidMount () {
+    getIpv4ConfigModes().then(configModes =>
+      this.setState({ configModes })
+    )
   }
 
   _configIp = mode => {
@@ -116,162 +117,219 @@ class PifItem extends Component {
       return confirm({
         icon: 'ip',
         title: _('pifConfigureIp'),
-        body: <ConfigureIpModal pif={this.props.pif} />,
-      }).then(params => {
-        if (!params.ip || !params.netmask) {
-          error(_('configIpErrorTitle'), _('configIpErrorMessage'))
-          return
-        }
-        return reconfigurePifIp(this.props.pif, { mode, ...params })
-      }, noop)
+        body: <ConfigureIpModal pif={this.props.pif} />
+      }).then(
+        params => {
+          if (!params.ip || !params.netmask) {
+            error(_('configIpErrorTitle'), _('configIpErrorMessage'))
+            return
+          }
+          return reconfigurePifIp(this.props.pif, { mode, ...params })
+        },
+        noop
+      )
     }
     return reconfigurePifIp(this.props.pif, { mode })
   }
   _onEditIp = () => this._configIp('Static')
-
-  _editPif = vlan => editPif(this.props.pif, { vlan })
-
   render () {
-    const { networks, pif, vifsByNetwork } = this.props
-    const { configModes } = this.state
-
-    const pifInUse = some(vifsByNetwork[pif.$network], vif => vif.attached)
-
-    return (
-      <tr>
-        <td>{pif.device}</td>
-        <td>{networks[pif.$network].name_label}</td>
-        <td>
-          {pif.vlan === -1 ? (
-            'None'
-          ) : (
-            <Number value={pif.vlan} onChange={this._editPif}>
-              {pif.vlan}
-            </Number>
-          )}
-        </td>
-        <td>
-          {pif.ip}{' '}
-          {pif.ip && (
-            <a
-              className='hidden-md-down'
-              onClick={this._onEditIp}
-              style={EDIT_BUTTON_STYLE}
-            >
-              <Icon icon='edit' size='1' fixedWidth />
-            </a>
-          )}
-        </td>
-        <td>
-          <Select
-            onChange={this._configIp}
-            options={configModes}
-            value={pif.mode}
-          >
-            {pif.mode}
-          </Select>
-        </td>
-        <td>
-          <pre>{pif.mac}</pre>
-        </td>
-        <td>{pif.mtu}</td>
-        <td className='text-xs-center'>
-          {_toggleDefaultLockingMode(
-            <Toggle
-              disabled={pifInUse}
-              onChange={() =>
-                editNetwork(pif.$network, {
-                  defaultIsLocked: !networks[pif.$network].defaultIsLocked,
-                })
-              }
-              value={networks[pif.$network].defaultIsLocked}
-            />,
-            pifInUse && _('pifInUse')
-          )}
-        </td>
-        <td>
-          <StateButton
-            disabledLabel={_('pifDisconnected')}
-            disabledHandler={connectPif}
-            disabledTooltip={_('connectPif')}
-            enabledLabel={_('pifConnected')}
-            enabledHandler={disconnectPif}
-            enabledTooltip={_('disconnectPif')}
-            disabled={pif.attached && (pif.management || pif.disallowUnplug)}
-            handlerParam={pif}
-            state={pif.attached}
-          />{' '}
-          <Tooltip
-            content={
-              pif.carrier
-                ? _('pifPhysicallyConnected')
-                : _('pifPhysicallyDisconnected')
-            }
-          >
-            <Icon
-              icon='network'
-              size='lg'
-              className={pif.carrier ? 'text-success' : 'text-muted'}
-            />
-          </Tooltip>
-        </td>
-        <td className='text-xs-right'>
-          <ActionRowButton
-            disabled={pif.physical || pif.disallowUnplug || pif.management}
-            handler={deletePif}
-            handlerParam={pif}
-            icon='delete'
-            tooltip={_('deletePif')}
-          />
-        </td>
-      </tr>
-    )
+    const {pif} = this.props
+    return <div>
+      {pif.ip}
+      {' '}
+      {pif.ip && <a className='hidden-md-down' onClick={this._onEditIp} style={EDIT_BUTTON_STYLE}>
+        <Icon icon='edit' size='1' fixedWidth />
+      </a>}
+    </div>
   }
 }
 
-export default ({ host, networks, pifs, vifsByNetwork }) => (
-  <Container>
-    <Row>
-      <Col className='text-xs-right'>
-        <TabButton
-          btnStyle='primary'
-          handler={createNetwork}
-          handlerParam={host}
-          icon='add'
-          labelId='networkCreateButton'
+class PifItemMode extends Component {
+  state = { configModes: [] }
+  componentDidMount () {
+    getIpv4ConfigModes().then(configModes =>
+      this.setState({ configModes })
+    )
+  }
+  _configIp = mode => {
+    if (mode === 'Static') {
+      return confirm({
+        icon: 'ip',
+        title: _('pifConfigureIp'),
+        body: <ConfigureIpModal pif={this.props.pif} />
+      }).then(
+        params => {
+          if (!params.ip || !params.netmask) {
+            error(_('configIpErrorTitle'), _('configIpErrorMessage'))
+            return
+          }
+          return reconfigurePifIp(this.props.pif, { mode, ...params })
+        },
+        noop
+      )
+    }
+    return reconfigurePifIp(this.props.pif, { mode })
+  }
+  render () {
+    const {pif} = this.props
+    const { configModes } = this.state
+    return <Select
+      onChange={this._configIp}
+      options={configModes}
+      value={pif.mode}
+    >
+      {pif.mode}
+    </Select>
+  }
+}
+@connectStore(() => ({
+  vifsByNetwork: createGetObjectsOfType('VIF').groupBy('$network')
+}))
+class PifItemInUse extends Component {
+  render () {
+    const {networks, pif, vifsByNetwork} = this.props
+    const pifInUse = some(vifsByNetwork[pif.$network], vif => vif.attached)
+    return <div className='text-xs-center'>
+      {_toggleDefaultLockingMode(
+        <Toggle
+          disabled={pifInUse}
+          onChange={() => editNetwork(pif.$network, { defaultIsLocked: !networks[pif.$network].defaultIsLocked })}
+          value={networks[pif.$network].defaultIsLocked}
+        />,
+        pifInUse && _('pifInUse')
+      )}
+    </div>
+  }
+}
+
+const COLUMNS = [
+  {
+    itemRenderer: (pif, networks) => {
+      return pif.device
+    },
+    default: true,
+    name: _('pifDeviceLabel'),
+    sortCriteria: 'device'
+  },
+  {
+    itemRenderer: (pif, networks) => {
+      return networks[pif.$network].name_label
+    },
+    name: _('pifNetworkLabel'),
+    sortCriteria: (pif, networks) => networks[pif.$network].name_label
+  },
+  {
+    itemRenderer: (pif, networks) => {
+      return <PifItemVlan key={pif.id} pif={pif} networks={networks} />
+    },
+    name: _('pifVlanLabel'),
+    sortCriteria: 'vlan'
+  },
+  {
+    itemRenderer: (pif, networks) => {
+      return <PifItemIp key={pif.id} pif={pif} networks={networks} />
+    },
+    name: _('pifAddressLabel'),
+    sortCriteria: 'ip'
+  },
+  {
+    itemRenderer: (pif, networks) => {
+      return <PifItemMode key={pif.id} pif={pif} networks={networks} />
+    },
+    name: _('pifModeLabel'),
+    sortCriteria: 'mode'
+  },
+  {
+    itemRenderer: (pif, networks) => {
+      return pif.mac
+    },
+    name: _('pifMacLabel'),
+    sortCriteria: 'mac'
+  },
+  {
+    itemRenderer: (pif, networks) => {
+      return pif.mtu
+    },
+    name: _('pifMtuLabel'),
+    sortCriteria: 'mtu'
+  },
+  {
+    itemRenderer: (pif, networks) => {
+      return <PifItemInUse key={pif.id} pif={pif} networks={networks} />
+    },
+    name: _('defaultLockingMode')
+  },
+  {
+    itemRenderer: (pif, networks) => {
+      return <div>
+        <StateButton
+          disabledLabel={_('pifDisconnected')}
+          disabledHandler={connectPif}
+          disabledTooltip={_('connectPif')}
+
+          enabledLabel={_('pifConnected')}
+          enabledHandler={disconnectPif}
+          enabledTooltip={_('disconnectPif')}
+
+          disabled={pif.attached && (pif.management || pif.disallowUnplug)}
+          handlerParam={pif}
+          state={pif.attached}
         />
-      </Col>
-    </Row>
-    <Row>
-      <Col>
-        {!isEmpty(pifs) ? (
-          <span>
-            <table className='table'>
-              <thead className='thead-default'>
-                <tr>
-                  <th>{_('pifDeviceLabel')}</th>
-                  <th>{_('pifNetworkLabel')}</th>
-                  <th>{_('pifVlanLabel')}</th>
-                  <th>{_('pifAddressLabel')}</th>
-                  <th>{_('pifModeLabel')}</th>
-                  <th>{_('pifMacLabel')}</th>
-                  <th>{_('pifMtuLabel')}</th>
-                  <th>{_('defaultLockingMode')}</th>
-                  <th>{_('pifStatusLabel')}</th>
-                  <th className='text-xs-right'>{_('pifAction')}</th>
-                </tr>
-              </thead>
-              <tbody>
-                {map(pifs, pif => (
-                  <PifItem key={pif.id} pif={pif} networks={networks} />
-                ))}
-              </tbody>
-            </table>
-          </span>
-        ) : (
-          <h4 className='text-xs-center'>{_('pifNoInterface')}</h4>
-        )}
-      </Col>
-    </Row>
-  </Container>
-)
+        {' '}
+        <Tooltip content={pif.carrier ? _('pifPhysicallyConnected') : _('pifPhysicallyDisconnected')}>
+          <Icon
+            icon='network'
+            size='lg'
+            className={pif.carrier ? 'text-success' : 'text-muted'}
+          />
+        </Tooltip>
+      </div>
+    },
+    name: _('pifStatusLabel')
+  }
+]
+const INDIVIDUAL_ACTIONS = [
+  {
+    handler: deletePif,
+    icon: 'delete',
+    label: _('deletePif')
+  }
+]
+
+export default({
+  host,
+  networks,
+  pifs,
+  vifsByNetwork
+}) => <Container>
+  <Row>
+    <Col className='text-xs-right'>
+      <TabButton
+        btnStyle='primary'
+        handler={createNetwork}
+        handlerParam={host}
+        icon='add'
+        labelId='networkCreateButton'
+      />
+    </Col>
+  </Row>
+  <Row>
+    <Col>
+      {!isEmpty(pifs)
+        ? <span>
+          <Row>
+            <Col>
+              <SortedTable
+                collection={pifs}
+                columns={COLUMNS}
+                individualActions={INDIVIDUAL_ACTIONS}
+                userData={networks}
+              />
+            </Col>
+          </Row>
+        </span>
+        : <h4 className='text-xs-center'>{_('pifNoInterface')}</h4>
+      }
+    </Col>
+  </Row>
+</Container>
--- src/xo-app/pool/tab-logs.js
+++ src/xo-app/pool/tab-logs.js
@@ -1,14 +1,60 @@
 import _ from 'intl'
 import ActionRow from 'action-row-button'
 import React, { Component } from 'react'
-import TabButton from 'tab-button'
+import SortedTable from 'sorted-table'
 import { deleteMessage } from 'xo'
 import { createPager, createSelector } from 'selectors'
 import { FormattedRelative, FormattedTime } from 'react-intl'
 import { Container, Row, Col } from 'grid'
-import { ceil, isEmpty, map } from 'lodash'
+import {
+  ceil,
+  isEmpty,
+  map
+} from 'lodash'
 
 const LOGS_PER_PAGE = 10
+
+const COLUMNS = [
+  {
+    itemRenderer: log =>
+      <div>
+        <FormattedTime value={log.time * 1000} minute='numeric' hour='numeric' day='numeric' month='long' year='numeric' /> (<FormattedRelative value={log.time * 1000} />)
+      </div>,
+    name: _('logDate'),
+    default: true,
+    sortCriteria: 'name_label'
+  },
+  {
+    itemRenderer: log => { return log.name },
+    name: _('logName'),
+    sortCriteria: 'name'
+  },
+  {
+    itemRenderer: log => { return log.body },
+    name: _('logDate'),
+    default: true,
+    sortCriteria: 'body'
+  },
+  {
+    itemRenderer: log =>
+      <td>
+        <ActionRow
+          btnStyle='danger'
+          handler={deleteMessage}
+          handlerParam={log}
+          icon='delete'
+        />
+      </td>,
+    name: _('logAction'),
+    sortCriteria: 'name_description'
+  }
+]
+const INDIVIDUAL_ACTIONS = [
+  {
+    icon: 'delete',
+    handler: deleteMessage
+  }
+]
 
 export default class TabLogs extends Component {
   constructor () {
@@ -21,102 +67,40 @@ export default class TabLogs extends Component {
     )
 
     this.getNPages = createSelector(
-      () => (this.props.logs ? this.props.logs.length : 0),
+      () => this.props.logs ? this.props.logs.length : 0,
       nLogs => ceil(nLogs / LOGS_PER_PAGE)
     )
 
     this.state = {
-      page: 1,
+      page: 1
     }
   }
 
   _deleteAllLogs = () => map(this.props.logs, deleteMessage)
-  _nextPage = () =>
-    this.setState({ page: Math.min(this.state.page + 1, this.getNPages()) })
-  _previousPage = () =>
-    this.setState({ page: Math.max(this.state.page - 1, 1) })
+  _nextPage = () => this.setState({ page: Math.min(this.state.page + 1, this.getNPages()) })
+  _previousPage = () => this.setState({ page: Math.max(this.state.page - 1, 1) })
 
   render () {
     const logs = this.getLogs()
     const { page } = this.state
 
-    return (
-      <Container>
-        {isEmpty(logs) ? (
-          <Row>
-            <Col mediumSize={6} className='text-xs-center'>
-              <br />
-              <h4>{_('noLogs')}</h4>
-            </Col>
-          </Row>
-        ) : (
-          <div>
-            <Row>
-              <Col className='text-xs-right'>
-                <TabButton
-                  btnStyle='secondary'
-                  disabled={page === 1}
-                  handler={this._previousPage}
-                  icon='previous'
-                />
-                <TabButton
-                  btnStyle='secondary'
-                  disabled={page === this.getNPages()}
-                  handler={this._nextPage}
-                  icon='next'
-                />
-                <TabButton
-                  btnStyle='danger'
-                  handler={this._removeAllLogs} // FIXME: define this method
-                  icon='delete'
-                  labelId='logRemoveAll'
-                />
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <table className='table'>
-                  <thead className='thead-default'>
-                    <tr>
-                      <th>{_('logDate')}</th>
-                      <th>{_('logName')}</th>
-                      <th>{_('logContent')}</th>
-                      <th>{_('logAction')}</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {map(logs, log => (
-                      <tr key={log.id}>
-                        <td>
-                          <FormattedTime
-                            value={log.time * 1000}
-                            minute='numeric'
-                            hour='numeric'
-                            day='numeric'
-                            month='long'
-                            year='numeric'
-                          />{' '}
-                          (<FormattedRelative value={log.time * 1000} />)
-                        </td>
-                        <td>{log.name}</td>
-                        <td>{log.body}</td>
-                        <td>
-                          <ActionRow
-                            btnStyle='danger'
-                            handler={deleteMessage}
-                            handlerParam={log}
-                            icon='delete'
-                          />
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </Col>
-            </Row>
-          </div>
-        )}
-      </Container>
-    )
+    return <Container>
+      {isEmpty(logs)
+        ? <Row>
+          <Col mediumSize={6} className='text-xs-center'>
+            <br />
+            <h4>{_('noLogs')}</h4>
+          </Col>
+        </Row>
+        : <Row>
+          <SortedTable
+            collection={logs}
+            columns={COLUMNS}
+            individualActions={INDIVIDUAL_ACTIONS}
+            groupedActions={GROUPED_ACTIONS}
+              />
+        </Row>
+      }
+    </Container>
   }
 }
