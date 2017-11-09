64018ad47b53a542a157317a316d00576f7fa806
Pierre Donias <pierre.donias@gmail.com>
2017-11-09 14:50:21 +0100

    resolveIds → resolveId

--- src/common/xo/index.js
+++ src/common/xo/index.js
@@ -1159,7 +1159,7 @@ export const deleteVdis = vdis => (
     title: _('deleteVdisModalTitle', { nVdis: vdis.length }),
     body: _('deleteVdisModalMessage', { nVdis: vdis.length })
   }).then(
-    () => Promise.all(map(resolveIds(vdis), id => _call('vdi.delete', { id }))),
+    () => Promise.all(map(vdis, vdi => _call('vdi.delete', { id: resolveId(vdi) }))),
     noop
   )
 )
