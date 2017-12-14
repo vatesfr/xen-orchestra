18ca6b935c24198e78ead40452f7a9740f26404d
Rajaa.BARHTAOUI <rajaa.barhtaoui@gmail.com>
2017-12-14 16:07:32 +0100

    feat(host/network): use SortedTable (#2512)
    See #2416

--- src/common/intl/messages.js
+++ src/common/intl/messages.js
@@ -1407,6 +1407,9 @@ const messages = {
   disconnectPifConfirm: 'Are you sure you want to disconnect this PIF?',
   deletePif: 'Delete PIF',
   deletePifConfirm: 'Are you sure you want to delete this PIF?',
+  deletePifs: 'Delete PIFs',
+  deletePifsConfirm:
+    'Are you sure you want to delete {nPifs, number} PIF{nPifs, plural, one {} other {s}}?',
   pifConnected: 'Connected',
   pifDisconnected: 'Disconnected',
   pifPhysicallyConnected: 'Physically connected',
