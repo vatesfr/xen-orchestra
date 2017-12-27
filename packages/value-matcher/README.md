22e3037e8592cd6b127358a8b94463631d0c8e1f
Julien Fontanet <julien.fontanet@isonoe.net>
2017-12-27 15:21:27 +0100

    feat(value-matcher): new package (#30)

---
+++ packages/value-matcher/README.md
@@ -0,0 +1,66 @@
+# value-matcher [![Build Status](https://travis-ci.org/vatefr/xen-orchestra.png?branch=master)](https://travis-ci.org/vatefr/xen-orchestra)
+
+> ${pkg.description}
+
+## Install
+
+Installation of the [npm package](https://npmjs.org/package/value-matcher):
+
+```
+> npm install --save value-matcher
+```
+
+## Usage
+
+```js
+import { createPredicate } from 'value-matcher'
+
+[
+  { user: 'sam', age: 65, active: false },
+  { user: 'barney', age: 36, active: true },
+  { user: 'fred', age: 40, active: false },
+].filter(createPredicate({
+  __or: [
+    { user: 'sam' },
+    { active: true },
+  ],
+}))
+// [
+//   { user: 'sam', age: 65, active: false },
+//   { user: 'barney', age: 36, active: true },
+// ]
+```
+
+## Development
+
+```
+# Install dependencies
+> yarn
+
+# Run the tests
+> yarn test
+
+# Continuously compile
+> yarn dev
+
+# Continuously run the tests
+> yarn dev-test
+
+# Build for production (automatically called by npm install)
+> yarn build
+```
+
+## Contributions
+
+Contributions are *very* welcomed, either on the documentation or on
+the code.
+
+You may:
+
+- report any [issue](https://github.com/vatesfr/xo-web/issues)
+  you've encountered;
+- fork and create a pull request.
+
+## License
+
+ISC © [Vates SAS](https://vates.fr)
---
+++ packages/value-matcher/src/index.js
@@ -0,0 +1,63 @@
+// @flow
+
+// eslint-disable-next-line no-use-before-define
+export type Pattern = OrPattern | NotPattern | ObjectPattern | ArrayPattern | ValuePattern
+
+// one of the pattern must match
+type OrPattern = {| __or: Array<Pattern> |}
+
+// the pattern must not  match
+type NotPattern = {| __not: Pattern |}
+
+// value is an object with properties matching the patterns
+type ObjectPattern = { [string]: Pattern }
+
+// value is an array and each patterns must match a different item
+type ArrayPattern = Array<Pattern>
+
+// value equals the pattern
+type ValuePattern = bool | number | string
+
+const match = (pattern: Pattern, value: any) => {
+  if (Array.isArray(pattern)) {
+    return Array.isArray(value) && pattern.every((subpattern, i) =>
+      // FIXME: subpatterns should match different subvalues
+      value.some(subvalue => match(subpattern, subvalue))
+    )
+  }
+
+  if (pattern !== null && typeof pattern === 'object') {
+    const keys = Object.keys(pattern)
+    const { length } = keys
+
+    if (length === 1) {
+      const [ key ] = keys
+      if (key === '__or') {
+        const orPattern: OrPattern = (pattern: any)
+        return orPattern.__or.some(subpattern => match(subpattern, value))
+      }
+      if (key === '__not') {
+        const notPattern: NotPattern = (pattern: any)
+        return !match(notPattern.__not, value)
+      }
+    }
+
+    if (value === null || typeof value !== 'object') {
+      return false
+    }
+
+    const objectPattern: ObjectPattern = (pattern: any)
+    for (let i = 0; i < length; ++i) {
+      const key = keys[i]
+      const subvalue = value[key]
+      if (subvalue === undefined || !match(objectPattern[key], subvalue)) {
+        return false
+      }
+    }
+    return true
+  }
+
+  return pattern === value
+}
+
+export const createPredicate = (pattern: Pattern) => (value: any) => match(pattern, value)
