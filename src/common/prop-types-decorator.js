d7f8d12d888718be09788d638bbb30b0debc679b
Julien Fontanet <julien.fontanet@isonoe.net>
2018-01-15 12:48:22 +0100

    chore(prop-types-decorator): deprecate

--- src/common/prop-types-decorator.js
+++ src/common/prop-types-decorator.js
@@ -1,6 +1,18 @@
 import assign from 'lodash/assign'
 import PropTypes from 'prop-types'
 
+// Deprecated because :
+// - unnecessary
+// - not standard in the React ecosystem
+if (__DEV__) {
+  console.warn(`DEPRECATED: use prop-types directly:
+class MyComponent extends React.Component {
+  static propTypes = {
+    foo: PropTypes.string.isRequired
+  }
+}`)
+}
+
 // Decorators to help declaring properties and context types on React
 // components without using the tedious static properties syntax.
 //
