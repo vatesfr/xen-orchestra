a7fa9c5bb80f98dec6c4d12fcbb134930bb1af59
Rajaa Barhtaoui <rajaa.barhtaoui@gmail.com>
2018-01-19 10:19:28 +0100

    Revert "for test"
    This reverts commit 3d9c6b43c16cf34f102bbd06350d9ea6b7f46daa.

--- src/xo-app/tasks/index.js
+++ src/xo-app/tasks/index.js
@@ -188,28 +188,20 @@ export default class Tasks extends Component {
       >
         <Container>
           <Row className='mb-1'>
-            <Col mediumSize={8}>
-              <SelectPool
-                multi
-                value={state.pools}
-                onChange={this.linkState('pools')}
-              />
-            </Col>
-            <Col mediumSize={4}>
-              <div ref={container => this.setState({ container })} />
-            </Col>
-          </Row>
-          <div>
-            <SortedTable
-              collection={_getTasks()}
-              columns={COLUMNS}
-              groupedActions={GROUPED_ACTIONS}
-              individualActions={INDIVIDUAL_ACTIONS}
-              stateUrlParam='s'
-              userData={{ pools }}
-              filterContainer={() => this.state.container}
+            <SelectPool
+              multi
+              value={state.pools}
+              onChange={this.linkState('pools')}
             />
-          </div>
+          </Row>
+          <SortedTable
+            collection={_getTasks()}
+            columns={COLUMNS}
+            groupedActions={GROUPED_ACTIONS}
+            individualActions={INDIVIDUAL_ACTIONS}
+            stateUrlParam='s'
+            userData={{ pools }}
+          />
         </Container>
       </Page>
     )
