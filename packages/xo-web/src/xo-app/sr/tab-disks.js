e3c1a4ebb9b620168550023948d5137a46f8e7a8
Rajaa Barhtaoui <rajaa.barhtaoui@gmail.com>
2018-01-31 10:55:06 +0100

    Allow forget action in SR/Disks

--- packages/xo-web/src/xo-app/sr/tab-disks.js
+++ packages/xo-web/src/xo-app/sr/tab-disks.js
@@ -1,15 +1,16 @@
 import _ from 'intl'
+import ActionRowButton from 'action-row-button'
 import Component from 'base-component'
 import Icon from 'icon'
 import Link from 'link'
 import React from 'react'
 import renderXoItem, { renderXoUnknownItem } from 'render-xo-item'
 import SortedTable from 'sorted-table'
-import { concat, isEmpty } from 'lodash'
+import { concat, find, isEmpty, map } from 'lodash'
 import { connectStore, formatSize } from 'utils'
 import { Container, Row, Col } from 'grid'
-import { createGetObject, createSelector } from 'selectors'
-import { deleteVdi, deleteVdis, editVdi } from 'xo'
+import { createGetObjectsOfType, createSelector } from 'selectors'
+import { deleteVbd, deleteVdi, deleteVdis, editVdi } from 'xo'
 import { Text } from 'editable'
 
 // ===================================================================
@@ -42,45 +43,6 @@ const COLUMNS = [
     ),
   },
   {
-    name: _('vdiVm'),
-    component: connectStore(() => {
-      const getObject = createGetObject((_, id) => id)
-
-      return {
-        vm: (state, { item: { $VBDs: [vbdId] } }) => {
-          if (vbdId === undefined) {
-            return null
-          }
-
-          const vbd = getObject(state, vbdId)
-          if (vbd != null) {
-            return getObject(state, vbd.VM)
-          }
-        },
-      }
-    })(({ vm }) => {
-      if (vm === null) {
-        return null // no attached VM
-      }
-
-      if (vm === undefined) {
-        return renderXoUnknownItem()
-      }
-
-      let link
-      const { type } = vm
-      if (type === 'VM') {
-        link = `/vms/${vm.id}`
-      } else if (type === 'VM-snapshot') {
-        const id = vm.$snapshot_of
-        link = id !== undefined ? `/vms/${id}/snapshots` : '/dashboard/health'
-      }
-
-      const item = renderXoItem(vm)
-      return link === undefined ? item : <Link to={link}>{item}</Link>
-    }),
-  },
-  {
     name: _('vdiTags'),
     itemRenderer: vdi => vdi.tags,
   },
@@ -89,6 +51,74 @@ const COLUMNS = [
     itemRenderer: vdi => formatSize(vdi.size),
     sortCriteria: vdi => vdi.size,
   },
+  {
+    name: _('vdiVm'),
+    component: connectStore(() => {
+      const getVbds = createGetObjectsOfType('VBD').pick(
+        (_, props) => props.item.$VBDs
+      )
+      const getVMsId = createSelector(getVbds, vbds => map(vbds, vbd => vbd.VM))
+      const getVMs = createGetObjectsOfType('VM').pick(getVMsId)
+
+      return (state, props) => ({
+        vms: getVMs(state, props),
+        vbds: getVbds(state, props),
+        vdi: props.item,
+      })
+    })(({ vdi, vbds, vms }) => {
+      if (vms === null) {
+        return null // no attached VM
+      }
+
+      if (vms === undefined) {
+        return renderXoUnknownItem()
+      }
+
+      return (
+        <Container>
+          {map(vms, vm => {
+            if (vm === null) {
+              return null // no attached VM
+            }
+
+            if (vm === undefined) {
+              return renderXoUnknownItem()
+            }
+            let link
+            const { type } = vm
+            if (type === 'VM') {
+              link = `/vms/${vm.id}`
+            } else if (type === 'VM-snapshot') {
+              const id = vm.$snapshot_of
+              link =
+                id !== undefined ? `/vms/${id}/snapshots` : '/dashboard/health'
+            }
+
+            const item = renderXoItem(vm)
+            const vbd = find(vbds, { VDI: vdi.id, VM: vm.id })
+
+            return (
+              <Row>
+                <Col mediumSize={8}>
+                  {link === undefined ? item : <Link to={link}>{item}</Link>}{' '}
+                </Col>
+                <Col mediumSize={2}>
+                  <ActionRowButton
+                    btnStyle='danger'
+                    disabled={!(vbd !== undefined && vbd.attached)}
+                    handler={deleteVbd}
+                    handlerParam={vbd}
+                    icon='vdi-forget'
+                    tooltip={_('vdiForget')}
+                  />
+                </Col>
+              </Row>
+            )
+          })}
+        </Container>
+      )
+    }),
+  },
 ]
 
 const GROUPED_ACTIONS = [
@@ -96,6 +126,7 @@ const GROUPED_ACTIONS = [
     handler: deleteVdis,
     icon: 'delete',
     label: _('deleteSelectedVdis'),
+    level: 'danger',
   },
 ]
 
@@ -128,6 +159,7 @@ export default class SrDisks extends Component {
 
   render () {
     const vdis = this._getAllVdis()
+
     return (
       <Container>
         <Row>
