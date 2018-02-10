22997cd9032ffd72cce2549f504724f91f617a9a
Julien Fontanet <julien.fontanet@isonoe.net>
2018-02-10 23:09:29 +0100

    chore: improve Babel 7 configs
    - easier to customize
    - all target configuration is done in package.json

--- packages/value-matcher/.babelrc.js
+++ packages/value-matcher/.babelrc.js
@@ -6,35 +6,42 @@ const __TEST__ = NODE_ENV === 'test'
 
 const pkg = require('./package')
 
-let nodeCompat = (pkg.engines || {}).node
-if (nodeCompat === undefined) {
-  nodeCompat = '6'
-} else {
-  const trimChars = '^=>~'
-  while (trimChars.includes(nodeCompat[0])) {
-    nodeCompat = nodeCompat.slice(1)
-  }
+const plugins = {
+  lodash: {},
 }
+
+const presets = {
+  '@babel/preset-env': {
+    debug: !__TEST__,
+    loose: true,
+    shippedProposals: true,
+    targets: __PROD__
+      ? (() => {
+        let node = (pkg.engines || {}).node
+        if (node !== undefined) {
+          const trimChars = '^=>~'
+          while (trimChars.includes(node[0])) {
+            node = node.slice(1)
+          }
+          return { node: node }
+        }
+      })()
+      : { browsers: '', node: 'current' },
+    useBuiltIns: '@babel/polyfill' in (pkg.dependencies || {}) && 'usage',
+  },
+}
+
+Object.keys(pkg.devDependencies || {}).forEach(name => {
+  if (!(name in presets) && /@babel\/plugin-.+/.test(name)) {
+    plugins[name] = {}
+  } else if (!(name in presets) && /@babel\/preset-.+/.test(name)) {
+    presets[name] = {}
+  }
+})
 
 module.exports = {
   comments: !__PROD__,
   ignore: __TEST__ ? undefined : [/\.spec\.js$/],
-  presets: [
-    [
-      '@babel/env',
-      {
-        debug: !__TEST__,
-        loose: true,
-        shippedProposals: true,
-        targets: __PROD__
-          ? {
-            browsers: '>2%',
-            node: nodeCompat,
-          }
-          : { node: 'current' },
-        useBuiltIns: '@babel/polyfill' in (pkg.dependencies || {}) && 'usage',
-      },
-    ],
-    '@babel/flow',
-  ],
+  plugins: Object.keys(plugins).map(plugin => [plugin, plugins[plugin]]),
+  presets: Object.keys(presets).map(preset => [preset, presets[preset]]),
 }
