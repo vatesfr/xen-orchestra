bb68dba229549a266e4f3bff753f0000509dd632
Badr <azizbibadr@gmail.com>
2018-02-23 12:20:54 +0100

    code improvement

--- packages/xo-server/src/test-stats.js
+++ packages/xo-server/src/test-stats.js
@@ -3,16 +3,10 @@ import { map, forEach, endsWith, find } from 'lodash'
 
 // -------------------------------------------------------------------
 
-const computeValues = (dataRow, legendIndex, type) => {
-  const quotient = {
-    memory: 1024,
-    memoryFree: 1024,
-    cpu: 100
-  }
-  return map(dataRow, ({ values }) => values[legendIndex] * (quotient[type] || 1))
-}
+const computeValues = (dataRow, legendIndex, convertQuotient = 1) =>
+  map(dataRow, ({ values }) => values[legendIndex] * convertQuotient)
 
-const initializeProperty = (obj, targetPath) => {
+const getValuesFromDepth = (obj, targetPath) => {
   const splitedPath = targetPath.split('.')
   forEach(splitedPath, (path, key) => {
     if (obj[path] === undefined) {
@@ -28,89 +22,93 @@ const initializeProperty = (obj, targetPath) => {
 
 const STATS = {
   host: {
-    init (uuid) {
-      let host
-      this._hosts[uuid] = host = this._hosts[uuid] || {}
-      return host
+    getCollection (uuid) {
+      return this._hosts[uuid] !== undefined
+        ? this._hosts[uuid]
+        : (this._hosts[uuid] = {})
     },
     metrics: {
       load: {
-        init: collection =>
-          initializeProperty(collection, 'load'),
+        getValues: collection => getValuesFromDepth(collection, 'load'),
         test: metricType => metricType === 'loadavg',
       },
       memoryFree: {
-        init: collection =>
-          initializeProperty(collection, 'memoryFree'),
+        getValues: collection => getValuesFromDepth(collection, 'memoryFree'),
         test: metricType => metricType === 'memory_free_kib',
+        convertQuotient: 1024,
       },
       memory: {
-        init: collection =>
-          initializeProperty(collection, 'memory'),
+        getValues: collection => getValuesFromDepth(collection, 'memory'),
         test: metricType => metricType === 'memory_total_kib',
+        convertQuotient: 1024,
       },
       cpus: {
-        init: (collection, cpuKey) =>
-          initializeProperty(collection, `cpus.${cpuKey}`),
+        getValues: (collection, cpuKey) =>
+          getValuesFromDepth(collection, `cpus.${cpuKey}`),
         test: metricType => /^cpu([0-9]+)$/.exec(metricType),
+        convertQuotient: 100,
       },
       pifs: {
         rx: {
-          init: (collection, pifKey) =>
-            initializeProperty(collection, `pifs.rx.${pifKey}`),
+          getValues: (collection, pifKey) =>
+            getValuesFromDepth(collection, `pifs.rx.${pifKey}`),
           test: metricType => /^pif_eth([0-9]+)_rx$/.exec(metricType),
         },
         tx: {
-          init: (collection, pifKey) =>
-            initializeProperty(collection, `pifs.tx.${pifKey}`),
+          getValues: (collection, pifKey) =>
+            getValuesFromDepth(collection, `pifs.tx.${pifKey}`),
           test: metricType => /^pif_eth([0-9]+)_tx$/.exec(metricType),
-        }
-      }
+        },
+      },
     },
   },
   vm: {
-    init (uuid) {
-      let vm
-      this._vms[uuid] = vm = this._vms[uuid] || {}
-      return vm
+    getCollection (uuid) {
+      return this._vms[uuid] !== undefined
+        ? this._vms[uuid]
+        : (this._vms[uuid] = {})
     },
     metrics: {
       memoryFree: {
-        init: collection => initializeProperty(collection, 'memoryFree'),
+        getValues: collection => getValuesFromDepth(collection, 'memoryFree'),
         test: metricType => metricType === 'memory_internal_free',
+        convertQuotient: 1024,
       },
       memory: {
-        init: collection => initializeProperty(collection, 'memory'),
+        getValues: collection => getValuesFromDepth(collection, 'memory'),
         test: metricType => endsWith(metricType, 'memory'),
+        convertQuotient: 1024,
       },
       cpus: {
-        init: (collection, cpuKey) => initializeProperty(collection, `cpus.${cpuKey}`),
+        getValues: (collection, cpuKey) =>
+          getValuesFromDepth(collection, `cpus.${cpuKey}`),
         test: metricType => /^cpu([0-9]+)$/.exec(metricType),
+        convertQuotient: 100,
       },
       vifs: {
         rx: {
-          init: (collection, vifKey) =>
-            initializeProperty(collection, `vifs.rx.${vifKey}`),
-          test:  metricType => /^vif_([0-9]+)_rx$/.exec(metricType),
+          getValues: (collection, vifKey) =>
+            getValuesFromDepth(collection, `vifs.rx.${vifKey}`),
+          test: metricType => /^vif_([0-9]+)_rx$/.exec(metricType),
         },
         tx: {
-          init: (collection, vifKey) =>
-            initializeProperty(collection, `vifs.tx.${vifKey}`),
+          getValues: (collection, vifKey) =>
+            getValuesFromDepth(collection, `vifs.tx.${vifKey}`),
           test: metricType => /^vif_([0-9]+)_tx$/.exec(metricType),
-        }
+        },
       },
       xvds: {
         r: {
-          init: (collection,  xvdKey) =>
-            initializeProperty(collection, `xvds.r.${xvdKey}`),
+          getValues: (collection, xvdKey) =>
+            getValuesFromDepth(collection, `xvds.r.${xvdKey}`),
           test: metricType => /^vbd_xvd(.)_read$/.exec(metricType),
         },
         w: {
-          init: (collection,  xvdKey) =>
-            initializeProperty(collection, `xvds.w.${xvdKey}`),
+          getValues: (collection, xvdKey) =>
+            getValuesFromDepth(collection, `xvds.w.${xvdKey}`),
           test: metricType => /^vbd_xvd(.)_write$/.exec(metricType),
-        }
-      }
+        },
+      },
     },
   },
 }
@@ -130,16 +128,22 @@ export default class XapiStats {
         legend
       )
 
-      const statsByType = STATS[type]
-      if (statsByType === undefined || (vmId !== undefined && vmId !== uuid)) {
+      const stats = STATS[type]
+      if (stats === undefined || (vmId !== undefined && vmId !== uuid)) {
         return
       }
 
       let testResult
       let metricInDeep
-      let metric = find(statsByType.metrics, metric => metric.test !== undefined
-          ? (testResult = metric.test(metricType))
-          : (metricInDeep = find(metric, metricInDeep => (testResult = metricInDeep.test(metricType))))
+      let metric = find(
+        stats.metrics,
+        metric =>
+          metric.test !== undefined
+            ? (testResult = metric.test(metricType))
+            : (metricInDeep = find(
+              metric,
+              metricInDeep => (testResult = metricInDeep.test(metricType))
+            ))
       )
 
       if (metric === undefined) {
@@ -148,9 +152,11 @@ export default class XapiStats {
         metric = metricInDeep
       }
 
-      const collection = statsByType.init.call(this, uuid)
-      const metricValues = metric.init(collection, testResult[1])
-      metricValues.push(...computeValues(json.data, index, type))
+      const collection = stats.getCollection.call(this, uuid)
+      const metricValues = metric.getValues(collection, testResult[1])
+      metricValues.push(
+        ...computeValues(json.data, index, metric.convertQuotient)
+      )
     })
 
     return vmId !== undefined ? this._vms[vmId] : this._hosts[hostId]
