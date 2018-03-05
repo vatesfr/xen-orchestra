57ddcd1feb25202b27b600deea7ed751e6c96184
Badr <azizbibadr@gmail.com>
2018-03-05 10:13:15 +0100

    fix: use freactal in backup's edition and remove comments

--- packages/xo-web/src/xo-app/backup-ng/edit.js
+++ packages/xo-web/src/xo-app/backup-ng/edit.js
@@ -1,38 +1,26 @@
-import _ from 'intl'
 import addSubscriptions from 'add-subscriptions'
 import React from 'react'
-import Component from 'base-component'
-// import { injectState, provideState } from '@julien-f/freactal'
-import { Debug } from 'utils'
+import { injectState, provideState } from '@julien-f/freactal'
 import { subscribeBackupNgJobs, subscribeSchedules } from 'xo'
 import { find, groupBy } from 'lodash'
 
 import New from './new'
 
-/*export default [
+export default [
   addSubscriptions({
     jobs: subscribeBackupNgJobs,
-    schedules: subscribeSchedules,
+    schedulesByJob: cb =>
+      subscribeSchedules(schedules => {
+        cb(groupBy(schedules, 'jobId'))
+      }),
   }),
   provideState({
     computed: {
-      value: ({ jobs, schedules }) => { console.log(jobs)},
+      job: (_, { jobs, routeParams: { id } }) => find(jobs, { id }),
+      schedules: (_, { schedulesByJob, routeParams: { id } }) =>
+        schedulesByJob && schedulesByJob[id],
     },
   }),
   injectState,
-  props => ({ state }) => <New value={state.value} />,
-].reduceRight((value, decorator) => decorator(value))*/
-
-@addSubscriptions({
-  jobs: subscribeBackupNgJobs,
-  schedulesByJob: cb =>
-    subscribeSchedules(schedules => {
-      cb(groupBy(schedules, 'jobId'))
-    })
-})
-export default class Edit extends Component {
-  render () {
-    const { routeParams: { id }, jobs, schedulesByJob } = this.props
-    return <New job={find(jobs, { id })} schedules={schedulesByJob && schedulesByJob[id]} />
-  }
-}
+  ({ state: { job, schedules } }) => <New job={job} schedules={schedules} />,
+].reduceRight((value, decorator) => decorator(value))
