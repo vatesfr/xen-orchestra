5e7512fb7002162bcdc7b2771f5f8f2d243bf1f9
Rajaa Barhtaoui <rajaa.barhtaoui@gmail.com>
2018-03-26 10:47:30 +0200

    fixes

--- packages/xo-web/src/common/shortcuts.js
+++ packages/xo-web/src/common/shortcuts.js
@@ -1,7 +1,7 @@
 import Component from 'base-component'
 import React from 'react'
 import { Shortcuts as ReactShortcuts } from 'react-shortcuts'
-import { forEach, noop, remove } from 'lodash'
+import { forEach, remove } from 'lodash'
 
 let enabled = true
 const instances = []
@@ -27,20 +27,18 @@ export default class Shortcuts extends Component {
   componentWillUnmount () {
     remove(instances, this)
   }
-  /* if we press unprintable keys when an input is focused,
-       handler passing to ReactShortcuts fire the first.
-    https://github.com/avocode/react-shortcuts/issues/13#issuecomment-255868423
-  */
+  _handler = (action, event) => {
+    // if we press unprintable keys when an input is focused,
+    //   the handler passed to ReactShortcuts fires first.
+    //  https://github.com/avocode/react-shortcuts/issues/13#issuecomment-255868423
+    if (event.target.tagName === 'INPUT') return
+    this.props.handler(action, event)
+  }
+  _handler = this._handler.bind(this)
+
   render () {
     return enabled ? (
-      <ReactShortcuts
-        {...this.props}
-        handler={(action, event) =>
-          event.target.tagName === 'INPUT'
-            ? noop
-            : this.props.handler(action, event)
-        }
-      />
+      <ReactShortcuts {...this.props} handler={this._handler} />
     ) : null
   }
 }
