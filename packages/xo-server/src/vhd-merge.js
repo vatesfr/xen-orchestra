96616d8707bedb9983bd718dc6e8f0afbd7e59ea
Julien Fontanet <julien.fontanet@isonoe.net>
2018-05-03 18:37:09 +0200

    chore(vhd-merge): dont floor sectors per block

--- packages/xo-server/src/vhd-merge.js
+++ packages/xo-server/src/vhd-merge.js
@@ -299,9 +299,9 @@ export class Vhd {
 
     // Compute the number of sectors in one block.
     // Default: One block contains 4096 sectors of 512 bytes.
-    const sectorsPerBlock = (this.sectorsPerBlock = Math.floor(
-      header.blockSize / VHD_SECTOR_SIZE
-    ))
+    const sectorsPerBlock = (this.sectorsPerBlock =
+      header.blockSize / VHD_SECTOR_SIZE)
+    assert(Number.isInteger(sectorsPerBlock))
 
     // Compute bitmap size in sectors.
     // Default: 1.
