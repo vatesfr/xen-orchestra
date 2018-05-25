b471eb0c2aa52ab4f523b2ab55bfc453bc07707f
badrAZ <azizbibadr@gmail.com>
2018-05-25 12:04:31 +0200

    move the log consolidation to xo-mixins/backups-ng-logs.js

---
+++ packages/xo-server/src/xo-mixins/backups-ng-logs.js
@@ -0,0 +1,275 @@
+import { forEach, get, groupBy, isEmpty } from 'lodash'
+
+import { type CallJob } from './jobs'
+import { type BackupJob, Mode, ReportWhen } from './backups-ng'
+
+type TaskStatus = 'failure' | 'interrupted' | 'pending' | 'skipped' | 'success'
+
+type ConsolidatedOperationTask = {|
+  parentId: string,
+  id: string,
+  start: string,
+  message: 'transger' | 'merge',
+  status: TaskStatus,
+  result?:
+    | {|
+        size: number,
+      |}
+    | Object,
+  end?: number,
+|}
+
+type ConsolidatedSubTask = {
+  data?: {
+    type: 'SR' | 'remote',
+    id: string,
+  },
+  message: 'snapshot' | 'export',
+  id: string,
+  parentId: string,
+  start: number,
+  status: TaskStatus,
+  result?: string | Object,
+  end?: number,
+  tasks?: Array<ConsolidatedOperationTask>,
+}
+
+type ConsolidatedTask = {
+  data: {|
+    type: 'VM',
+    id: string,
+  |},
+  parentId: string,
+  id: string,
+  start: number,
+  status: TaskStatus,
+  message: string,
+  end?: number,
+  tasks?: Array<ConsolidatedSubTask>,
+  transfer?: {|
+    duration: number,
+    size: number,
+  |},
+  merge?: {|
+    duration: number,
+    size: number,
+  |},
+  result?: Object,
+}
+
+type ConsolidatedBackupNgLog = {|
+  data?: {|
+    reportWhen?: ReportWhen,
+    mode?: Mode,
+  |},
+  end?: number,
+  error?: Object,
+  id: string,
+  jobId: string,
+  start: number,
+  status: TaskStatus,
+  tasks: Array<ConsolidatedTask>,
+|}
+
+const NO_VMS_MATCH_THIS_PATTERN = 'no VMs match this pattern'
+const UNHEALTHY_VDI_CHAIN_ERROR = 'unhealthy VDI chain'
+const NO_SUCH_OBJECT_ERROR = 'no such object'
+const NO_DISKS_FOUND = 'no disks found'
+
+const isSkippedError = error =>
+  error.message === UNHEALTHY_VDI_CHAIN_ERROR ||
+  error.message === NO_SUCH_OBJECT_ERROR ||
+  error.message === NO_DISKS_FOUND
+
+export default class BackupNgLogs {
+  _app: {
+    getLogs: (namespace: string) => Promise<{ [id: string]: Object }>,
+    getJob: ((id: string, 'backup') => Promise<BackupJob>) &
+      ((id: string, 'call') => Promise<CallJob>),
+  }
+
+  constructor (app: any) {
+    this._app = app
+  }
+
+  async getBackupNgLogs (
+    runId?: string
+  ): Promise<$Dict<ConsolidatedBackupNgLog>> {
+    const rawLogs = await this._app.getLogs('jobs')
+    let groupedLogs = {}
+    for (const id in rawLogs) {
+      const { data, time, message } = rawLogs[id]
+
+      switch (data.event) {
+        case 'job.start':
+          if (data.type === 'backup' || data.key === undefined) {
+            groupedLogs[id] = {
+              id,
+              jobId: data.jobId,
+              data: data.data,
+              start: time,
+            }
+          }
+          break
+        case 'job.end':
+          const jobLog = groupedLogs[data.runJobId]
+          if (jobLog !== undefined) {
+            jobLog.end = time
+            jobLog.error = data.error
+          }
+          break
+        case 'task.start':
+          const parentLog = groupedLogs[data.parentId]
+          if (parentLog !== undefined) {
+            groupedLogs[id] = {
+              parentId: data.parentId,
+              data: data.data,
+              id,
+              start: time,
+              message,
+            }
+          }
+          break
+        case 'task.end':
+          const task = groupedLogs[data.taskId]
+          if (task !== undefined) {
+            // work-around
+            if (
+              time === task.start &&
+              (message === 'merge' || message === 'transfer')
+            ) {
+              delete groupedLogs[data.taskId]
+            } else {
+              task.status = data.status
+              task.result = data.result
+              task.end = time
+            }
+          }
+          break
+        case 'jobCall.start':
+          if (groupedLogs[data.runJobId] !== undefined) {
+            groupedLogs[id] = {
+              parentId: data.runJobId,
+              data: {
+                type: 'VM',
+                id: data.params.id,
+              },
+              id,
+              start: time,
+            }
+          }
+          break
+        case 'jobCall.end':
+          const call = groupedLogs[data.runCallId]
+          if (call !== undefined) {
+            call.status = data.error === undefined ? 'success' : 'failure'
+            call.result = data.error
+            const returnedValue = data.returnedValue
+            if (returnedValue !== undefined) {
+              if (returnedValue.transferDuration > 0) {
+                call.transfer = {
+                  duration: returnedValue.transferDuration,
+                  size: returnedValue.transferSize,
+                }
+              }
+              if (returnedValue.mergeDuration > 0) {
+                call.merge = {
+                  duration: returnedValue.mergeDuration,
+                  size: returnedValue.mergeSize,
+                }
+              }
+            }
+            call.end = time
+          }
+      }
+    }
+
+    if (isEmpty(groupedLogs)) {
+      return {}
+    }
+
+    groupedLogs = groupBy(groupedLogs, log => log.parentId || 'roots')
+    const consolidatedLogs: $Dict<ConsolidatedBackupNgLog> = {}
+    for (const jobLog of groupedLogs['roots']) {
+      const jobLogId = jobLog.id
+      consolidatedLogs[jobLogId] = jobLog
+      let job
+      try {
+        job = await this._app.getJob(jobLog.jobId, 'backup')
+      } catch (e) {}
+      const taskWithNoEndStatus =
+        get(job, 'runId') === jobLog.id ? 'pending' : 'interrupted'
+
+      if (jobLog.error !== undefined) {
+        jobLog.status =
+          jobLog.error.message === NO_VMS_MATCH_THIS_PATTERN
+            ? 'skipped'
+            : 'failure'
+        continue
+      }
+
+      let jobFailed = false
+      let hasTaskSkipped = false
+      jobLog.tasks = []
+      forEach(groupedLogs[jobLogId], taskLog => {
+        const taskLogId = taskLog.id
+        jobLog.tasks.push(taskLog)
+
+        if (taskLog.result !== undefined) {
+          if (isSkippedError(taskLog.result)) {
+            taskLog.status = 'skipped'
+            hasTaskSkipped = true
+          } else {
+            taskLog.status = 'failure'
+            jobFailed = true
+          }
+          return
+        }
+
+        let taskFailed = false
+        taskLog.tasks = []
+        forEach(groupedLogs[taskLogId], subTaskLog => {
+          const subTaskLogId = subTaskLog.id
+          taskLog.tasks.push(subTaskLog)
+
+          if (subTaskLog.end === undefined) {
+            subTaskLog.status = taskWithNoEndStatus
+          }
+
+          if (subTaskLog.status === 'failure') {
+            jobFailed = true
+            taskFailed = true
+            return
+          }
+
+          if (subTaskLog.message !== 'snapshot') {
+            subTaskLog.tasks = []
+            forEach(groupedLogs[subTaskLogId], operationLog => {
+              subTaskLog.tasks.push(operationLog)
+              if (operationLog.end === undefined) {
+                operationLog.status = taskWithNoEndStatus
+              }
+            })
+          }
+        })
+
+        if (taskLog.end === undefined) {
+          taskLog.status = taskWithNoEndStatus
+        } else {
+          taskLog.status = taskFailed ? 'failure' : 'success'
+        }
+      })
+      if (jobLog.end === undefined) {
+        jobLog.status = taskWithNoEndStatus
+      } else {
+        jobLog.status = jobFailed
+          ? 'failure'
+          : hasTaskSkipped
+            ? 'skipped'
+            : 'success'
+      }
+    }
+
+    return runId !== undefined ? consolidatedLogs[runId] : consolidatedLogs
+  }
+}
--- packages/xo-server/src/xo-mixins/backups-ng/index.js
+++ packages/xo-server/src/xo-mixins/backups-ng/index.js
@@ -7,18 +7,7 @@ import limitConcurrency from 'limit-concurrency-decorator'
 import { type Pattern, createPredicate } from 'value-matcher'
 import { type Readable, PassThrough } from 'stream'
 import { basename, dirname } from 'path'
-import {
-  forEach,
-  get,
-  groupBy,
-  isEmpty,
-  last,
-  mapValues,
-  noop,
-  some,
-  sum,
-  values,
-} from 'lodash'
+import { isEmpty, last, mapValues, noop, some, sum, values } from 'lodash'
 import { fromEvent as pFromEvent, timeout as pTimeout } from 'promise-toolbox'
 import Vhd, {
   chainVhd,
@@ -44,8 +33,8 @@ import {
 
 import { translateLegacyJob } from './migration'
 
-type Mode = 'full' | 'delta'
-type ReportWhen = 'always' | 'failure' | 'never'
+export type Mode = 'full' | 'delta'
+export type ReportWhen = 'always' | 'failure' | 'never'
 
 type Settings = {|
   concurrency?: number,
@@ -94,76 +83,6 @@ type MetadataFull = {|
   xva: string,
 |}
 type Metadata = MetadataDelta | MetadataFull
-
-type TaskStatus = 'failure' | 'interrupted' | 'pending' | 'skipped' | 'success'
-
-type ConsolidatedOperationTask = {|
-  parentId: string,
-  id: string,
-  start: string,
-  message: 'transger' | 'merge',
-  status: TaskStatus,
-  result?:
-    | {|
-        size: number,
-      |}
-    | Object,
-  end?: number,
-|}
-
-type ConsolidatedSubTask = {
-  data?: {
-    type: 'SR' | 'remote',
-    id: string,
-  },
-  message: 'snapshot' | 'export',
-  id: string,
-  jobId: string,
-  scheduleId: string,
-  mode: Mode,
-  start: number,
-  status: TaskStatus,
-  result?: string | Object,
-  end?: number,
-  tasks?: Array<ConsolidatedOperationTask>,
-}
-
-type ConsolidatedTask = {
-  data: {|
-    type: 'VM',
-    id: string,
-  |},
-  parentId: string,
-  id: string,
-  start: number,
-  status: TaskStatus,
-  message: string,
-  end?: number,
-  tasks?: Array<ConsolidatedSubTask>,
-  transfer?: {|
-    duration: number,
-    size: number,
-  |},
-  merge?: {|
-    duration: number,
-    size: number,
-  |},
-  result?: Object,
-}
-
-type ConsolidatedBackupNgLog = {|
-  data?: {|
-    reportWhen?: ReportWhen,
-    mode?: Mode,
-  |},
-  end?: number,
-  error?: Object,
-  id: string,
-  jobId: string,
-  start: number,
-  status: TaskStatus,
-  tasks: Array<ConsolidatedTask>,
-|}
 
 const compareSnapshotTime = (a: Vm, b: Vm): number =>
   a.snapshot_time < b.snapshot_time ? -1 : 1
@@ -440,16 +359,6 @@ const wrapTaskFn = <T>(
       throw result
     }
   }
-
-const NO_VMS_MATCH_THIS_PATTERN = 'no VMs match this pattern'
-const UNHEALTHY_VDI_CHAIN_ERROR = 'unhealthy VDI chain'
-const NO_SUCH_OBJECT_ERROR = 'no such object'
-const NO_DISKS_FOUND = 'no disks found'
-
-const isSkippedError = error =>
-  error.message === UNHEALTHY_VDI_CHAIN_ERROR ||
-  error.message === NO_SUCH_OBJECT_ERROR ||
-  error.message === NO_DISKS_FOUND
 
 // File structure on remotes:
 //
@@ -827,7 +736,7 @@ export default class BackupNg {
         vbd => vbd.type === 'Disk' && vbd.VDI !== 'OpaqueRef:NULL'
       )
     ) {
-      throw new Error(NO_DISKS_FOUND)
+      throw new Error('no disks found')
     }
 
     const snapshots = vm.$snapshots
@@ -1390,187 +1299,5 @@ export default class BackupNg {
     }
 
     return backups.sort(compareTimestamp)
-  }
-
-  async getBackupNgLogs (
-    runId?: string
-  ): Promise<$Dict<ConsolidatedBackupNgLog>> {
-    const rawLogs = await this._app.getLogs('jobs')
-    let groupedLogs = {}
-    for (const id in rawLogs) {
-      const { data, time, message } = rawLogs[id]
-
-      switch (data.event) {
-        case 'job.start':
-          if (data.type === 'backup' || data.key === undefined) {
-            groupedLogs[id] = {
-              id,
-              jobId: data.jobId,
-              scheduleId: data.scheduleId,
-              data: data.data,
-              start: time,
-            }
-          }
-          break
-        case 'job.end':
-          const jobLog = groupedLogs[data.runJobId]
-          if (jobLog !== undefined) {
-            jobLog.end = time
-            jobLog.error = data.error
-          }
-          break
-        case 'task.start':
-          const parentLog = groupedLogs[data.parentId]
-          if (parentLog !== undefined) {
-            groupedLogs[id] = {
-              parentId: data.parentId,
-              data: data.data,
-              id,
-              start: time,
-              message,
-            }
-          }
-          break
-        case 'task.end':
-          const task = groupedLogs[data.taskId]
-          if (task !== undefined) {
-            // work-around
-            if (
-              time === task.start &&
-              (message === 'merge' || message === 'transfer')
-            ) {
-              delete groupedLogs[data.taskId]
-            } else {
-              task.status = data.status
-              task.result = data.result
-              task.end = time
-            }
-          }
-          break
-        case 'jobCall.start':
-          if (groupedLogs[data.runJobId] !== undefined) {
-            groupedLogs[id] = {
-              parentId: data.runJobId,
-              data: {
-                type: 'VM',
-                id: data.params.id,
-              },
-              id,
-              start: time,
-            }
-          }
-          break
-        case 'jobCall.end':
-          const call = groupedLogs[data.runCallId]
-          if (call !== undefined) {
-            call.status = data.error === undefined ? 'success' : 'failure'
-            call.result = data.error
-            const returnedValue = data.returnedValue
-            if (returnedValue !== undefined) {
-              if (returnedValue.transferDuration > 0) {
-                call.transfer = {
-                  duration: returnedValue.transferDuration,
-                  size: returnedValue.transferSize,
-                }
-              }
-              if (returnedValue.mergeDuration > 0) {
-                call.merge = {
-                  duration: returnedValue.mergeDuration,
-                  size: returnedValue.mergeSize,
-                }
-              }
-            }
-            call.end = time
-          }
-      }
-    }
-
-    if (isEmpty(groupedLogs)) {
-      return {}
-    }
-
-    groupedLogs = groupBy(groupedLogs, log => log.parentId || 'roots')
-    const consolidatedLogs: $Dict<ConsolidatedBackupNgLog> = {}
-    for (const jobLog of groupedLogs['roots']) {
-      const jobLogId = jobLog.id
-      consolidatedLogs[jobLogId] = jobLog
-      let job
-      try {
-        job = await this._app.getJob(jobLog.jobId, 'backup')
-      } catch (e) {}
-      const taskWithNoEndStatus =
-        get(job, 'runId') === jobLog.id ? 'pending' : 'interrupted'
-
-      if (jobLog.error !== undefined) {
-        jobLog.status =
-          jobLog.error.message === NO_VMS_MATCH_THIS_PATTERN
-            ? 'skipped'
-            : 'failure'
-        continue
-      }
-
-      let jobFailed = false
-      let hasTaskSkipped = false
-      jobLog.tasks = []
-      forEach(groupedLogs[jobLogId], taskLog => {
-        const taskLogId = taskLog.id
-        jobLog.tasks.push(taskLog)
-
-        if (taskLog.result !== undefined) {
-          if (isSkippedError(taskLog.result)) {
-            taskLog.status = 'skipped'
-            hasTaskSkipped = true
-          } else {
-            taskLog.status = 'failure'
-            jobFailed = true
-          }
-          return
-        }
-
-        let taskFailed = false
-        taskLog.tasks = []
-        forEach(groupedLogs[taskLogId], subTaskLog => {
-          const subTaskLogId = subTaskLog.id
-          taskLog.tasks.push(subTaskLog)
-
-          if (subTaskLog.end === undefined) {
-            subTaskLog.status = taskWithNoEndStatus
-          }
-
-          if (subTaskLog.status === 'failure') {
-            jobFailed = true
-            taskFailed = true
-            return
-          }
-
-          if (subTaskLog.message !== 'snapshot') {
-            subTaskLog.tasks = []
-            forEach(groupedLogs[subTaskLogId], operationLog => {
-              subTaskLog.tasks.push(operationLog)
-              if (operationLog.end === undefined) {
-                operationLog.status = taskWithNoEndStatus
-              }
-            })
-          }
-        })
-
-        if (taskLog.end === undefined) {
-          taskLog.status = taskWithNoEndStatus
-        } else {
-          taskLog.status = taskFailed ? 'failure' : 'success'
-        }
-      })
-      if (jobLog.end === undefined) {
-        jobLog.status = taskWithNoEndStatus
-      } else {
-        jobLog.status = jobFailed
-          ? 'failure'
-          : hasTaskSkipped
-            ? 'skipped'
-            : 'success'
-      }
-    }
-
-    return runId !== undefined ? consolidatedLogs[runId] : consolidatedLogs
   }
 }
