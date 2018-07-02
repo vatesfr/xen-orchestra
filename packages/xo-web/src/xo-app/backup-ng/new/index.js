f42108b581a8dfd59de276a00803f3700420da35
badrAZ <azizbibadr@gmail.com>
2018-07-02 14:23:49 +0200

    fix errors

--- packages/xo-web/src/xo-app/backup-ng/new/index.js
+++ packages/xo-web/src/xo-app/backup-ng/new/index.js
@@ -712,46 +712,47 @@ export default [
                     </Link>
                   </CardHeader>
                   <CardBlock>
-                    <FormGroup>
-                      <label>
-                        <strong>{_('backupTargetRemotes')}</strong>
-                      </label>
-                      <FormFeedback
-                        component={SelectRemote}
-                        message={_('missingRemotes')}
-                        onChange={effects.addRemote}
-                        predicate={state.remotePredicate}
-                        error={
-                          state.showErrors ? state.missingRemotes : undefined
-                        }
-                        value={null}
-                      />
-                      {isEmpty(remotesById) && (
-                        <span className='text-warning'>
-                          <Icon icon='alarm' /> {_('createRemoteMessage')}
-                        </span>
-                      )}
-                      <br />
-                      <Ul>
-                        {map(state.remotes, (id, key) => (
-                          <Li key={id}>
-                            {remotesById !== undefined &&
-                              renderXoItem({
-                                type: 'remote',
-                                value: remotesById[id],
-                              })}
-                            <ActionButton
-                              btnStyle='danger'
-                              className='pull-right'
-                              handler={effects.deleteRemote}
-                              handlerParam={key}
-                              icon='delete'
-                              size='small'
-                            />
-                          </Li>
-                        ))}
-                      </Ul>
-                    </FormGroup>
+                    {isEmpty(remotesById) ? (
+                      <span className='text-warning'>
+                        <Icon icon='alarm' /> {_('createRemoteMessage')}
+                      </span>
+                    ) : (
+                      <FormGroup>
+                        <label>
+                          <strong>{_('backupTargetRemotes')}</strong>
+                        </label>
+                        <FormFeedback
+                          component={SelectRemote}
+                          message={_('missingRemotes')}
+                          onChange={effects.addRemote}
+                          predicate={state.remotePredicate}
+                          error={
+                            state.showErrors ? state.missingRemotes : undefined
+                          }
+                          value={null}
+                        />
+                        <br />
+                        <Ul>
+                          {map(state.remotes, (id, key) => (
+                            <Li key={id}>
+                              {remotesById !== undefined &&
+                                renderXoItem({
+                                  type: 'remote',
+                                  value: remotesById[id],
+                                })}
+                              <ActionButton
+                                btnStyle='danger'
+                                className='pull-right'
+                                handler={effects.deleteRemote}
+                                handlerParam={key}
+                                icon='delete'
+                                size='small'
+                              />
+                            </Li>
+                          ))}
+                        </Ul>
+                      </FormGroup>
+                    )}
                   </CardBlock>
                 </Card>
               )}
