vatesfr/xen-orchestra

commit 0c514198bb30c6f6dbdeae65183cf47b08f0cfc3

feat: ability to save cloud configs (#3054)
Fixes #2984

packages/xo-web/src/xo-app/new-vm/index.js
@@ -13,7 +13,10 @@ import store from 'store'
 import Tags from 'tags'
 import Tooltip from 'tooltip'
 import Wizard, { Section } from 'wizard'
-import { alert } from 'modal'
+import {
+  AvailableTemplateVars,
+  DEFAULT_CLOUD_CONFIG_TEMPLATE,
+} from 'cloud-config'
 import { Container, Row, Col } from 'grid'
 import { injectIntl } from 'react-intl'
 import {
@@ -48,6 +51,7 @@ import {
   XEN_DEFAULT_CPU_WEIGHT,
 } from 'xo'
 import {
+  SelectCloudConfig,
   SelectHost,
   SelectIp,
   SelectNetwork,
@@ -88,32 +92,6 @@ import styles from './index.css'
 
 const NB_VMS_MIN = 2
 const NB_VMS_MAX = 100
-
-const AVAILABLE_TEMPLATE_VARS = {
-  '{name}': 'templateNameInfo',
-  '%': 'templateIndexInfo',
-}
-
-const showAvailableTemplateVars = () =>
-  alert(
-    _('availableTemplateVarsTitle'),
-    <ul>
-      {map(AVAILABLE_TEMPLATE_VARS, (value, key) => (
-        <li key={key}>{_.keyValue(key, _(value))}</li>
-      ))}
-    </ul>
-  )
-
-const AvailableTemplateVarsInfo = () => (
-  <Tooltip content={_('availableTemplateVarsInfo')}>
-    <a
-      className={classNames('text-info', styles.availableTemplateVars)}
-      onClick={showAvailableTemplateVars}
-    >
-      <Icon icon='info' />
-    </a>
-  </Tooltip>
-)
 
 /* eslint-disable camelcase */
 
@@ -313,12 +291,12 @@ export default class NewVm extends BaseComponent {
   _reset = () => {
     this._replaceState({
       bootAfterCreate: true,
-      configDrive: false,
       CPUs: '',
       cpuCap: '',
       cpuWeight: '',
       existingDisks: {},
       fastClone: true,
+      installMethod: 'noConfigDrive',
       multipleVms: false,
       name_label: '',
       name_description: '',
@@ -362,7 +340,7 @@ export default class NewVm extends BaseComponent {
 
     let cloudConfig
     let cloudConfigs
-    if (state.configDrive) {
+    if (state.installMethod !== 'noConfigDrive') {
       const hostname = state.name_label
         .replace(/^\s+|\s+$/g, '')
         .replace(/\s+/g, '-')
@@ -376,7 +354,9 @@ export default class NewVm extends BaseComponent {
           ''
         )}`
       } else {
-        const replacer = this._buildTemplate(state.customConfig)
+        const replacer = this._buildTemplate(
+          defined(state.customConfig, DEFAULT_CLOUD_CONFIG_TEMPLATE)
+        )
         cloudConfig = replacer(this.state.state, 0)
         if (state.multipleVms) {
           const seqStart = state.seqStart
@@ -519,10 +499,8 @@ export default class NewVm extends BaseComponent {
       // installation
       installMethod:
         (template.install_methods != null && template.install_methods[0]) ||
-        'SSH',
+        'noConfigDrive',
       sshKeys: this.props.userSshKeys && this.props.userSshKeys.length && [0],
-      customConfig:
-        '#cloud-config\n#hostname: {name}%\n#ssh_authorized_keys:\n#  - ssh-rsa <myKey>\n#packages:\n#  - htop\n',
       // interfaces
       VIFs,
       // disks
@@ -1017,6 +995,12 @@ export default class NewVm extends BaseComponent {
 
   // INSTALL SETTINGS ------------------------------------------------------------
 
+  _onChangeCloudConfig = cloudConfig => {
+    this._setState({
+      customConfig: get(() => cloudConfig.template),
+    })
+  }
+
   _renderInstallSettings = () => {
     const { template } = this.state.state
     if (!template) {
@@ -1024,7 +1008,6 @@ export default class NewVm extends BaseComponent {
     }
     const {
       cloudConfig,
-      configDrive,
       customConfig,
       installIso,
       installMethod,
@@ -1043,36 +1026,36 @@ export default class NewVm extends BaseComponent {
         {this._isDiskTemplate ? (
           <SectionContent key='diskTemplate' column>
             <LineItem>
-              <div className={styles.configDrive}>
-                <span className={styles.configDriveToggle}>
-                  {_('newVmConfigDrive')}
-                </span>
+              <label>
+                <input
+                  checked={installMethod === 'noConfigDrive'}
+                  name='installMethod'
+                  onChange={this._linkState('installMethod')}
+                  type='radio'
+                  value='noConfigDrive'
+                />
                 &nbsp;
-                <span className={styles.configDriveToggle}>
-                  <Toggle
-                    value={configDrive}
-                    onChange={this._linkState('configDrive')}
-                  />
-                </span>
-              </div>
+                {_('noConfigDrive')}
+              </label>
             </LineItem>
+            <br />
             <LineItem>
-              <span>
+              <label>
                 <input
                   checked={installMethod === 'SSH'}
-                  disabled={!configDrive}
                   name='installMethod'
                   onChange={this._linkState('installMethod')}
                   type='radio'
                   value='SSH'
-                />{' '}
-                <span>{_('newVmSshKey')}</span>
-              </span>
+                />
+                &nbsp;
+                {_('newVmSshKey')}
+              </label>
               &nbsp;
               <span className={classNames('input-group', styles.fixedWidth)}>
                 <DebounceInput
                   className='form-control'
-                  disabled={!configDrive || installMethod !== 'SSH'}
+                  disabled={installMethod !== 'SSH'}
                   onChange={this._linkState('newSshKey')}
                   value={newSshKey}
                 />
@@ -1086,7 +1069,7 @@ export default class NewVm extends BaseComponent {
                 this.props.userSshKeys.length > 0 && (
                   <span className={styles.fixedWidth}>
                     <SelectSshKey
-                      disabled={!configDrive || installMethod !== 'SSH'}
+                      disabled={installMethod !== 'SSH'}
                       onChange={this._onChangeSshKeys}
                       multi
                       value={sshKeys || []}
@@ -1094,29 +1077,37 @@ export default class NewVm extends BaseComponent {
                   </span>
                 )}
             </LineItem>
+            <br />
             <LineItem>
-              <input
-                checked={installMethod === 'customConfig'}
-                disabled={!configDrive}
-                name='installMethod'
-                onChange={this._linkState('installMethod')}
-                type='radio'
-                value='customConfig'
-              />
-              &nbsp;
-              <span>
-                {_('newVmCustomConfig')}
+              <label>
+                <input
+                  checked={installMethod === 'customConfig'}
+                  name='installMethod'
+                  onChange={this._linkState('installMethod')}
+                  type='radio'
+                  value='customConfig'
+                />
                 &nbsp;
-                <AvailableTemplateVarsInfo />
-              </span>
+                {_('newVmCustomConfig')}
+              </label>
               &nbsp;
-              <DebounceTextarea
-                className={classNames('form-control', styles.customConfig)}
-                disabled={!configDrive || installMethod !== 'customConfig'}
-                onChange={this._linkState('customConfig')}
-                value={customConfig}
-              />
+              <AvailableTemplateVars />
+              &nbsp;
+              <span className={styles.inlineSelect}>
+                <SelectCloudConfig
+                  disabled={installMethod !== 'customConfig'}
+                  onChange={this._onChangeCloudConfig}
+                />
+              </span>
             </LineItem>
+            <br />
+            <DebounceTextarea
+              className='form-control'
+              disabled={installMethod !== 'customConfig'}
+              onChange={this._linkState('customConfig')}
+              rows={7}
+              value={defined(customConfig, DEFAULT_CLOUD_CONFIG_TEMPLATE)}
+            />
           </SectionContent>
         ) : (
           <SectionContent>
@@ -1212,7 +1203,6 @@ export default class NewVm extends BaseComponent {
   }
   _isInstallSettingsDone = () => {
     const {
-      configDrive,
       customConfig,
       installIso,
       installMethod,
@@ -1222,7 +1212,11 @@ export default class NewVm extends BaseComponent {
     } = this.state.state
     switch (installMethod) {
       case 'customConfig':
-        return customConfig || !configDrive
+        return (
+          customConfig === undefined ||
+          customConfig.trim() !== '' ||
+          installMethod === 'noConfigDrive'
+        )
       case 'ISO':
         return installIso
       case 'network':
@@ -1230,9 +1224,11 @@ export default class NewVm extends BaseComponent {
       case 'PXE':
         return true
       case 'SSH':
-        return !isEmpty(sshKeys) || !configDrive
+        return !isEmpty(sshKeys) || installMethod === 'noConfigDrive'
       default:
-        return template && this._isDiskTemplate && !configDrive
+        return (
+          template && this._isDiskTemplate && installMethod === 'noConfigDrive'
+        )
     }
   }
 
@@ -1283,7 +1279,7 @@ export default class NewVm extends BaseComponent {
 
   _renderDisks = () => {
     const {
-      state: { configDrive, existingDisks, VDIs },
+      state: { installMethod, existingDisks, VDIs },
     } = this.state
     const { pool } = this.props
     let i = 0
@@ -1346,7 +1342,7 @@ export default class NewVm extends BaseComponent {
                   <SizeInput
                     className={styles.sizeInput}
                     onChange={this._linkState(`existingDisks.${index}.size`)}
-                    readOnly={!configDrive}
+                    readOnly={installMethod === 'noConfigDrive'}
                     value={defined(disk.size, null)}
                   />
                 </Item>
@@ -1567,7 +1563,7 @@ export default class NewVm extends BaseComponent {
                 value={namePattern}
               />
               &nbsp;
-              <AvailableTemplateVarsInfo />
+              <AvailableTemplateVars />
             </Item>
             <Item label={_('newVmFirstIndex')}>
               <DebounceInput

packages/xo-web/src/common/xo/index.js
@@ -2393,6 +2393,39 @@ export const setIpPool = (ipPool, { name, addresses, networks }) =>
     networks: resolveIds(networks),
   })::tap(subscribeIpPools.forceRefresh)
 
+// Cloud configs --------------------------------------------------------------------
+
+export const subscribeCloudConfigs = createSubscription(() =>
+  _call('cloudConfig.getAll')
+)
+
+export const createCloudConfig = props =>
+  _call('cloudConfig.create', props)::tap(subscribeCloudConfigs.forceRefresh)
+
+export const deleteCloudConfigs = ids => {
+  const { length } = ids
+  if (length === 0) {
+    return
+  }
+
+  const vars = { nCloudConfigs: length }
+  return confirm({
+    title: _('confirmDeleteCloudConfigsTitle', vars),
+    body: <p>{_('confirmDeleteCloudConfigsBody', vars)}</p>,
+  }).then(
+    () =>
+      Promise.all(
+        ids.map(id => _call('cloudConfig.delete', { id: resolveId(id) }))
+      )::tap(subscribeCloudConfigs.forceRefresh),
+    noop
+  )
+}
+
+export const editCloudConfig = (cloudConfig, props) =>
+  _call('cloudConfig.update', { ...props, id: resolveId(cloudConfig) })::tap(
+    subscribeCloudConfigs.forceRefresh
+  )
+
 // XO SAN ----------------------------------------------------------------------
 
 export const getVolumeInfo = (xosanSr, infoType) =>

packages/xo-web/src/xo-app/backup-ng/new/index.js
@@ -6,7 +6,12 @@ import renderXoItem, { renderXoItemFromId } from 'render-xo-item'
 import Select from 'form/select'
 import Tooltip from 'tooltip'
 import Upgrade from 'xoa-upgrade'
-import { addSubscriptions, resolveId, resolveIds } from 'utils'
+import {
+  addSubscriptions,
+  generateRandomId,
+  resolveId,
+  resolveIds,
+} from 'utils'
 import { Card, CardBlock, CardHeader } from 'card'
 import { constructSmartPattern, destructSmartPattern } from 'smart-backup'
 import { Container, Col, Row } from 'grid'
@@ -35,15 +40,7 @@ import {
 
 import Schedules from './schedules'
 import SmartBackup from './smart-backup'
-import {
-  FormFeedback,
-  FormGroup,
-  getRandomId,
-  Input,
-  Number,
-  Ul,
-  Li,
-} from './utils'
+import { FormFeedback, FormGroup, Input, Number, Ul, Li } from './utils'
 
 // ===================================================================
 
@@ -158,7 +155,7 @@ const getInitialState = () => ({
   deltaMode: false,
   drMode: false,
   editionMode: undefined,
-  formId: getRandomId(),
+  formId: generateRandomId(),
   name: '',
   newSchedules: {},
   offlineSnapshot: false,
@@ -476,7 +473,7 @@ export default [
             editionMode: undefined,
             newSchedules: {
               ...state.newSchedules,
-              [getRandomId()]: {
+              [generateRandomId()]: {
                 cron,
                 timezone,
                 exportRetention,

packages/xo-web/src/xo-app/menu/index.js
@@ -286,6 +286,11 @@ export default class Menu extends Component {
           },
           { to: '/settings/ips', icon: 'ip', label: 'settingsIpsPage' },
           {
+            to: '/settings/cloud-configs',
+            icon: 'template',
+            label: 'settingsCloudConfigsPage',
+          },
+          {
             to: '/settings/config',
             icon: 'menu-settings-config',
             label: 'settingsConfigPage',

packages/xo-web/src/xo-app/settings/cloud-configs/index.js
@@ -1,0 +1,192 @@
+import _ from 'intl'
+import ActionButton from 'action-button'
+import defined from 'xo-defined'
+import React from 'react'
+import SortedTable from 'sorted-table'
+import { addSubscriptions, generateRandomId } from 'utils'
+import {
+  AvailableTemplateVars,
+  DEFAULT_CLOUD_CONFIG_TEMPLATE,
+} from 'cloud-config'
+import { Container, Col } from 'grid'
+import { find } from 'lodash'
+import { injectState, provideState } from '@julien-f/freactal'
+import { Text } from 'editable'
+import { Textarea as DebounceTextarea } from 'debounce-input-decorator'
+import {
+  createCloudConfig,
+  deleteCloudConfigs,
+  editCloudConfig,
+  subscribeCloudConfigs,
+} from 'xo'
+
+// ===================================================================
+
+const COLUMNS = [
+  {
+    itemRenderer: _ => _.id.slice(4, 8),
+    name: _('formId'),
+    sortCriteria: _ => _.id.slice(4, 8),
+  },
+  {
+    itemRenderer: ({ id, name }) => (
+      <Text value={name} onChange={name => editCloudConfig(id, { name })} />
+    ),
+    sortCriteria: 'name',
+    name: _('formName'),
+    default: true,
+  },
+]
+
+const ACTIONS = [
+  {
+    handler: deleteCloudConfigs,
+    icon: 'delete',
+    individualLabel: _('deleteCloudConfig'),
+    label: _('deleteSelectedCloudConfigs'),
+    level: 'danger',
+  },
+]
+
+const INDIVIDUAL_ACTIONS = [
+  {
+    handler: (cloudConfig, { populateForm }) => populateForm(cloudConfig),
+    icon: 'edit',
+    label: _('editCloudConfig'),
+    level: 'primary',
+  },
+]
+
+const initialParams = {
+  cloudConfigToEditId: undefined,
+  name: '',
+  template: undefined,
+}
+
+export default [
+  addSubscriptions({
+    cloudConfigs: subscribeCloudConfigs,
+  }),
+  provideState({
+    initialState: () => ({
+      formId: generateRandomId(),
+      inputNameId: generateRandomId(),
+      inputTemplateId: generateRandomId(),
+      ...initialParams,
+    }),
+    effects: {
+      setInputValue: (_, { target: { name, value } }) => state => ({
+        ...state,
+        [name]: value,
+      }),
+      reset: () => state => ({
+        ...state,
+        ...initialParams,
+      }),
+      createCloudConfig: ({ reset }) => async ({
+        name,
+        template = DEFAULT_CLOUD_CONFIG_TEMPLATE,
+      }) => {
+        await createCloudConfig({ name, template })
+        reset()
+      },
+      editCloudConfig: ({ reset }) => async (
+        { name, template, cloudConfigToEditId },
+        { cloudConfigs }
+      ) => {
+        const oldCloudConfig = find(cloudConfigs, { id: cloudConfigToEditId })
+        if (
+          oldCloudConfig.name !== name ||
+          oldCloudConfig.template !== template
+        ) {
+          await editCloudConfig(cloudConfigToEditId, { name, template })
+        }
+        reset()
+      },
+      populateForm: (_, { id, name, template }) => state => ({
+        ...state,
+        name,
+        cloudConfigToEditId: id,
+        template,
+      }),
+    },
+    computed: {
+      isInvalid: ({ name, template }) =>
+        name.trim() === '' ||
+        (template !== undefined && template.trim() === ''),
+    },
+  }),
+  injectState,
+  ({ state, effects, cloudConfigs }) => (
+    <Container>
+      <Col mediumSize={6}>
+        <form id={state.formId}>
+          <div className='form-group'>
+            <label htmlFor={state.inputNameId}>
+              <strong>{_('formName')}</strong>{' '}
+            </label>
+            <input
+              className='form-control'
+              id={state.inputNameId}
+              name='name'
+              onChange={effects.setInputValue}
+              type='text'
+              value={state.name}
+            />
+          </div>{' '}
+          <div className='form-group'>
+            <label htmlFor={state.inputTemplateId}>
+              <strong>{_('settingsCloudConfigTemplate')}</strong>{' '}
+            </label>{' '}
+            <AvailableTemplateVars />
+            <DebounceTextarea
+              className='form-control'
+              id={state.inputTemplateId}
+              name='template'
+              onChange={effects.setInputValue}
+              rows={12}
+              value={defined(state.template, DEFAULT_CLOUD_CONFIG_TEMPLATE)}
+            />
+          </div>{' '}
+          {state.cloudConfigToEditId !== undefined ? (
+            <ActionButton
+              btnStyle='primary'
+              disabled={state.isInvalid}
+              form={state.formId}
+              handler={effects.editCloudConfig}
+              icon='edit'
+            >
+              {_('formEdit')}
+            </ActionButton>
+          ) : (
+            <ActionButton
+              btnStyle='success'
+              disabled={state.isInvalid}
+              form={state.formId}
+              handler={effects.createCloudConfig}
+              icon='add'
+            >
+              {_('formCreate')}
+            </ActionButton>
+          )}
+          <ActionButton
+            className='pull-right'
+            handler={effects.reset}
+            icon='cancel'
+          >
+            {_('formCancel')}
+          </ActionButton>
+        </form>
+      </Col>
+      <Col mediumSize={6}>
+        <SortedTable
+          actions={ACTIONS}
+          collection={cloudConfigs}
+          columns={COLUMNS}
+          data-populateForm={effects.populateForm}
+          individualActions={INDIVIDUAL_ACTIONS}
+        />
+      </Col>
+    </Container>
+  ),
+].reduceRight((value, decorator) => decorator(value))

packages/xo-web/src/xo-app/settings/index.js
@@ -7,6 +7,7 @@ import { Container, Row, Col } from 'grid'
 import { NavLink, NavTabs } from 'nav'
 
 import Acls from './acls'
+import CloudConfigs from './cloud-configs'
 import Config from './config'
 import Groups from './groups'
 import Ips from './ips'
@@ -50,6 +51,9 @@ const HEADER = (
           <NavLink to='/settings/ips'>
             <Icon icon='ip' /> {_('settingsIpsPage')}
           </NavLink>
+          <NavLink to='/settings/cloud-configs'>
+            <Icon icon='template' /> {_('settingsCloudConfigsPage')}
+          </NavLink>
           <NavLink to='/settings/config'>
             <Icon icon='menu-settings-config' /> {_('settingsConfigPage')}
           </NavLink>
@@ -61,6 +65,7 @@ const HEADER = (
 
 const Settings = routes('servers', {
   acls: Acls,
+  'cloud-configs': CloudConfigs,
   config: Config,
   groups: Groups,
   ips: Ips,

packages/xo-server/src/api/cloud-config.js
@@ -1,0 +1,41 @@
+export function getAll () {
+  return this.getAllCloudConfigs()
+}
+
+getAll.permission = 'admin'
+getAll.description = 'Gets all existing cloud configs templates'
+
+export function create (props) {
+  return this.createCloudConfig(props)
+}
+
+create.permission = 'admin'
+create.description = 'Creates a new cloud config template'
+create.params = {
+  name: { type: 'string' },
+  template: { type: 'string' },
+}
+
+export function update (props) {
+  return this.updateCloudConfig(props)
+}
+
+update.permission = 'admin'
+update.description = 'Modifies an existing cloud config template'
+update.params = {
+  id: { type: 'string' },
+  name: { type: 'string', optional: true },
+  template: { type: 'string', optional: true },
+}
+
+function delete_ ({ id }) {
+  return this.deleteCloudConfig(id)
+}
+
+delete_.permission = 'admin'
+delete_.description = 'Deletes an existing cloud config template'
+delete_.params = {
+  id: { type: 'string' },
+}
+
+export { delete_ as delete }

packages/xo-server/src/xo-mixins/cloud-configs.js
@@ -1,0 +1,71 @@
+// @flow
+import { noSuchObject } from 'xo-common/api-errors'
+
+import Collection from '../collection/redis'
+import patch from '../patch'
+
+type CloudConfig = {|
+  id: string,
+  name: string,
+  template: string,
+|}
+
+class CloudConfigs extends Collection {
+  get (properties) {
+    return super.get(properties)
+  }
+}
+
+export default class {
+  _app: any
+  _db: {|
+    add: Function,
+    first: Function,
+    get: Function,
+    remove: Function,
+    update: Function,
+  |}
+
+  constructor (app: any) {
+    this._app = app
+    const db = (this._db = new CloudConfigs({
+      connection: app._redis,
+      prefix: 'xo:cloudConfig',
+    }))
+
+    app.on('clean', () => db.rebuildIndexes())
+    app.on('start', () =>
+      app.addConfigManager(
+        'cloudConfigs',
+        () => db.get(),
+        cloudConfigs => db.update(cloudConfigs)
+      )
+    )
+  }
+
+  createCloudConfig (cloudConfig: $Diff<CloudConfig, {| id: string |}>) {
+    return this._db.add(cloudConfig).properties
+  }
+
+  async updateCloudConfig ({ id, name, template }: $Shape<CloudConfig>) {
+    const cloudConfig = await this.getCloudConfig(id)
+    patch(cloudConfig, { name, template })
+    return this._db.update(cloudConfig)
+  }
+
+  deleteCloudConfig (id: string) {
+    return this._db.remove(id)
+  }
+
+  getAllCloudConfigs (): Promise<Array<CloudConfig>> {
+    return this._db.get()
+  }
+
+  async getCloudConfig (id: string): Promise<CloudConfig> {
+    const cloudConfig = await this._db.first(id)
+    if (cloudConfig === null) {
+      throw noSuchObject(id, 'cloud config')
+    }
+    return cloudConfig.properties
+  }
+}

commit 16f8029e6936133fb027fa807e2dbc3a4a564ced

fix(xo-web/new-vm): only create cloud config drive when enabled (#3162)
This bug has been introduced by the PR: #3054

Fixes #3161

packages/xo-web/src/xo-app/new-vm/index.js
@@ -353,7 +353,7 @@ export default class NewVm extends BaseComponent {
           }),
           ''
         )}`
-      } else {
+      } else if (state.installMethod === 'customConfig') {
         const replacer = this._buildTemplate(
           defined(state.customConfig, DEFAULT_CLOUD_CONFIG_TEMPLATE)
         )