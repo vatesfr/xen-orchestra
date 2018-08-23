vatesfr/xen-orchestra

commit ec869ffdd37b1de54e4e8a7701e1a25bb439b4f0

feat(xo-web/backup-ng/new): tip when thick-provisioned SR (#3333)
Fixes #3291

packages/xo-web/src/xo-app/backup-ng/new/index.js
@@ -11,6 +11,7 @@ import Upgrade from 'xoa-upgrade'
 import { Card, CardBlock, CardHeader } from 'card'
 import { constructSmartPattern, destructSmartPattern } from 'smart-backup'
 import { Container, Col, Row } from 'grid'
+import { createGetObjectsOfType } from 'selectors'
 import { flatten, includes, isEmpty, keyBy, map, mapValues, some } from 'lodash'
 import { injectState, provideState } from '@julien-f/freactal'
 import { Map } from 'immutable'
@@ -18,6 +19,7 @@ import { Number } from 'form'
 import { SelectRemote, SelectSr, SelectVm } from 'select-objects'
 import {
   addSubscriptions,
+  connectStore,
   generateRandomId,
   resolveId,
   resolveIds,
@@ -44,6 +46,9 @@ import {
 } from './../utils'
 
 // ===================================================================
+
+const SR_BACKEND_FAILURE_LINK =
+  'https://xen-orchestra.com/docs/backup_troubleshooting.html#srbackendfailure44-insufficient-space'
 
 const normalizeTagValues = values => resolveIds(values).map(value => [value])
 
@@ -161,6 +166,9 @@ export default [
         cb(keyBy(remotes, 'id'))
       }),
   }),
+  connectStore(() => ({
+    srsById: createGetObjectsOfType('SR'),
+  })),
   provideState({
     initialState: getInitialState,
     effects: {
@@ -557,7 +565,7 @@ export default [
     },
   }),
   injectState,
-  ({ state, effects, remotesById, job = {} }) => {
+  ({ state, effects, remotesById, srsById, job = {} }) => {
     const { propSettings, settings = propSettings } = state
     const { concurrency, reportWhen = 'failure', offlineSnapshot, timeout } =
       settings.get('') || {}
@@ -785,7 +793,23 @@ export default [
                       <Ul>
                         {map(state.srs, (id, key) => (
                           <Li key={id}>
-                            {renderXoItemFromId(id)}
+                            {renderXoItemFromId(id)}{' '}
+                            {srsById !== undefined &&
+                              state.crMode &&
+                              srsById[id].SR_type === 'lvm' && (
+                                <Tooltip
+                                  content={_('crOnThickProvisionedSrWarning')}
+                                >
+                                  <a
+                                    className='text-info'
+                                    href={SR_BACKEND_FAILURE_LINK}
+                                    rel='noopener noreferrer'
+                                    target='_blank'
+                                  >
+                                    <Icon icon='info' />
+                                  </a>
+                                </Tooltip>
+                              )}
                             <div className='pull-right'>
                               <DeleteOldBackupsFirst
                                 handler={effects.setTargetDeleteFirst}

packages/xo-web/src/common/intl/messages.js
@@ -407,6 +407,8 @@ const messages = {
   backupTargetRemotes: 'Target remotes (for Export)',
   backupTargetSrs: 'Target SRs (for Replication)',
   localRemoteWarningTitle: 'Local remote selected',
+  crOnThickProvisionedSrWarning:
+    'Tip: using a thin-provisioned storage will consume less space. Please click on the icon to get more information',
   localRemoteWarningMessage:
     'Warning: local remotes will use limited XOA disk space. Only for advanced users.',
   backupVersionWarning:

commit ecc33f46abcd80b8f2ee2b15db0e85acee881c56

fix(xo-web/backup-ng/new): fix selected SRs issues (#3352)
Issue introduced in ec869ffd

packages/xo-web/src/xo-app/backup-ng/new/index.js
@@ -773,9 +773,9 @@ export default [
                         {map(state.srs, (id, key) => (
                           <Li key={id}>
                             {renderXoItemFromId(id)}{' '}
-                            {srsById !== undefined &&
+                            {!isEmpty(srsById) &&
                               state.crMode &&
-                              srsById[id].SR_type === 'lvm' && (
+                              get(() => srsById[id].SR_type) === 'lvm' && (
                                 <Tooltip
                                   content={_('crOnThickProvisionedSrWarning')}
                                 >