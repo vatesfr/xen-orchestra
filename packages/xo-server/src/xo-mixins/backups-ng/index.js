vatesfr/xen-orchestra

commit b3004a38aa82bdad50c9d01a972d112f764f943e

chore: update promise-toolbox to v0.10 (#3392)

packages/xo-server/src/xo-mixins/backups-ng/index.js
@@ -19,7 +19,7 @@ import {
   sum,
   values,
 } from 'lodash'
-import { fromEvent as pFromEvent, ignoreErrors } from 'promise-toolbox'
+import { pFromEvent, ignoreErrors } from 'promise-toolbox'
 import Vhd, {
   chainVhd,
   createSyntheticStream as createVhdReadStream,

packages/xen-api/src/index.js
@@ -21,12 +21,12 @@ import {
 import {
   Cancel,
   cancelable,
-  catchPlus as pCatch,
   defer,
-  delay as pDelay,
   fromEvents,
-  lastly,
-  timeout as pTimeout,
+  pCatch,
+  pDelay,
+  pFinally,
+  pTimeout,
   TimeoutError,
 } from 'promise-toolbox'
 
@@ -428,7 +428,7 @@ export class Xapi extends EventEmitter {
             this._sessionCall('task.cancel', [taskRef]).catch(noop)
           })
 
-          return lastly.call(this.watchTask(taskRef), () => {
+          return pFinally.call(this.watchTask(taskRef), () => {
             this._sessionCall('task.destroy', [taskRef]).catch(noop)
           })
         })

packages/xo-server/src/xapi/index.js
@@ -7,10 +7,10 @@ import tarStream from 'tar-stream'
 import vmdkToVhd from 'xo-vmdk-to-vhd'
 import {
   cancelable,
-  catchPlus as pCatch,
   defer,
   fromEvent,
   ignoreErrors,
+  pCatch,
 } from 'promise-toolbox'
 import { PassThrough } from 'stream'
 import { forbiddenOperation } from 'xo-common/api-errors'
@@ -1548,23 +1548,21 @@ export default class Xapi extends XapiBase {
 
     return host === undefined
       ? this.call(
-        'VM.start',
-        vm.$ref,
-        false, // Start paused?
-        false // Skip pre-boot checks?
-      )
-      : this.call(
-        'VM.start_on',
-        vm.$ref,
-        host.$ref,
-        false,
-        false
-      )
+          'VM.start',
+          vm.$ref,
+          false, // Start paused?
+          false // Skip pre-boot checks?
+        )
+      : this.call('VM.start_on', vm.$ref, host.$ref, false, false)
   }
 
   async startVm (vmId, hostId, force) {
     try {
-      await this._startVm(this.getObject(vmId), hostId && this.getObject(hostId), force)
+      await this._startVm(
+        this.getObject(vmId),
+        hostId && this.getObject(hostId),
+        force
+      )
     } catch (e) {
       if (e.code === 'OPERATION_BLOCKED') {
         throw forbiddenOperation('Start', e.params[1])

packages/xo-web/src/common/xo/index.js
@@ -1,7 +1,10 @@
 import asap from 'asap'
 import cookies from 'cookies-js'
 import fpSortBy from 'lodash/fp/sortBy'
+import pFinally from 'promise-toolbox/finally'
 import React from 'react'
+import reflect from 'promise-toolbox/reflect'
+import tap from 'promise-toolbox/tap'
 import URL from 'url-parse'
 import Xo from 'xo-lib'
 import { createBackoff } from 'jsonrpc-websocket-client'
@@ -20,7 +23,6 @@ import {
   sortBy,
   throttle,
 } from 'lodash'
-import { lastly, reflect, tap } from 'promise-toolbox'
 import {
   forbiddenOperation,
   noHostsAvailable,
@@ -480,7 +482,7 @@ export const editServer = (server, props) =>
   )
 
 export const connectServer = server =>
-  _call('server.connect', { id: resolveId(server) })::lastly(
+  _call('server.connect', { id: resolveId(server) })::pFinally(
     subscribeServers.forceRefresh
   )
 

packages/xo-web/src/xo-app/dashboard/health/index.js
@@ -1,5 +1,6 @@
 import _ from 'intl'
 import Component from 'base-component'
+import fromCallback from 'promise-toolbox/fromCallback'
 import Icon from 'icon'
 import Link from 'link'
 import NoObjects from 'no-objects'
@@ -9,7 +10,6 @@ import Tooltip from 'tooltip'
 import Upgrade from 'xoa-upgrade'
 import xml2js from 'xml2js'
 import { SelectPool } from 'select-objects'
-import { fromCallback } from 'promise-toolbox'
 import { Container, Row, Col } from 'grid'
 import { Card, CardHeader, CardBlock } from 'card'
 import { FormattedRelative, FormattedTime } from 'react-intl'

packages/xo-web/src/xo-app/new/sr/index.js
@@ -3,6 +3,7 @@ import ActionButton from 'action-button'
 import Component from 'base-component'
 import filter from 'lodash/filter'
 import Icon from 'icon'
+import ignoreErrors from 'promise-toolbox/ignoreErrors'
 import includes from 'lodash/includes'
 import info, { error } from 'notification'
 import isEmpty from 'lodash/isEmpty'
@@ -16,7 +17,6 @@ import Wizard, { Section } from 'wizard'
 import { confirm } from 'modal'
 import { connectStore, formatSize } from 'utils'
 import { Container, Row, Col } from 'grid'
-import { ignoreErrors } from 'promise-toolbox'
 import { injectIntl } from 'react-intl'
 import { Password, Select, Toggle } from 'form'
 import { SelectHost } from 'select-objects'

packages/xo-web/src/xo-app/settings/plugins/index.js
@@ -7,13 +7,13 @@ import GenericInput from 'json-schema-input'
 import Icon from 'icon'
 import isEmpty from 'lodash/isEmpty'
 import map from 'lodash/map'
+import pFinally from 'promise-toolbox/finally'
 import React from 'react'
 import size from 'lodash/size'
 import { addSubscriptions } from 'utils'
 import { alert } from 'modal'
 import { createSelector } from 'reselect'
 import { generateUiSchema } from 'xo-json-schema-input'
-import { lastly } from 'promise-toolbox'
 import { Row, Col } from 'grid'
 import {
   configurePlugin,
@@ -55,7 +55,7 @@ class Plugin extends Component {
     const method = event.target.checked
       ? enablePluginAutoload
       : disablePluginAutoload
-    method(this.props.id)::lastly(() => {
+    method(this.props.id)::pFinally(() => {
       this._updateAutoload = false
     })
   }

packages/xo-web/src/xo-app/vm-import/ova/index.js
@@ -1,7 +1,7 @@
 import find from 'lodash/find'
 import forEach from 'lodash/forEach'
+import fromEvent from 'promise-toolbox/fromEvent'
 import xml2js from 'xml2js'
-import { fromEvent } from 'promise-toolbox'
 import { ensureArray } from 'utils'
 import { readVmdkGrainTable } from 'xo-vmdk-to-vhd'
 

commit 1807917204496771e4ce783dd5ec4a438e54c366

fix(xo-server/backup NG): CancelToken#fork() no longer exists
Issue introduced in b3004a38a

packages/xo-server/src/xo-mixins/backups-ng/index.js
@@ -19,7 +19,7 @@ import {
   sum,
   values,
 } from 'lodash'
-import { pFromEvent, ignoreErrors } from 'promise-toolbox'
+import { CancelToken, pFromEvent, ignoreErrors } from 'promise-toolbox'
 import Vhd, {
   chainVhd,
   createSyntheticStream as createVhdReadStream,
@@ -495,7 +495,7 @@ export default class BackupNg {
           let vmCancel
           try {
             cancelToken.throwIfRequested()
-            vmCancel = cancelToken.fork()
+            vmCancel = CancelToken.race([cancelToken]).token
 
             // $FlowFixMe injected $defer param
             const p = this._backupVm(