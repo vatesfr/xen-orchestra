8bdccb76af3e30138c646d5d8259ccd58f04e1e6
badrAZ <azizbibadr@gmail.com>
2018-09-14 17:20:06 +0200

    use computed

--- packages/xo-web/src/xo-app/logs/log-alert-header.js
+++ packages/xo-web/src/xo-app/logs/log-alert-header.js
@@ -8,6 +8,7 @@ import React from 'react'
 import ReportBugButton, { CAN_REPORT_BUG } from 'report-bug-button'
 import Tooltip from 'tooltip'
 import { get } from 'xo-defined'
+import { injectState, provideState } from '@julien-f/freactal'
 import { subscribeBackupNgLogs } from 'xo'
 
 export default [
@@ -17,32 +18,35 @@ export default [
         cb(logs[id])
       }),
   })),
-  ({ log = {}, jobs }) => {
-    const formattedLog = JSON.stringify(log, null, 2)
-    return (
-      <span>
-        {get(() => jobs[log.jobId].name) || 'Job'} (
-        {get(() => log.jobId.slice(4, 8))}){' '}
-        <span style={{ fontSize: '0.5em' }} className='text-muted'>
-          {log.id}
-        </span>{' '}
-        <ButtonGroup>
-          <Tooltip content={_('copyToClipboard')}>
-            <CopyToClipboard text={formattedLog}>
-              <Button size='small'>
-                <Icon icon='clipboard' />
-              </Button>
-            </CopyToClipboard>
-          </Tooltip>
-          {CAN_REPORT_BUG && (
-            <ReportBugButton
-              message={`\`\`\`json\n${formattedLog}\n\`\`\``}
-              size='small'
-              title='Backup job failed'
-            />
-          )}
-        </ButtonGroup>
-      </span>
-    )
-  },
+  provideState({
+    computed: {
+      formattedLog: (_, { log }) => JSON.stringify(log, null, 2),
+    },
+  }),
+  injectState,
+  ({ state, log = {}, jobs }) => (
+    <span>
+      {get(() => jobs[log.jobId].name) || 'Job'} (
+      {get(() => log.jobId.slice(4, 8))}){' '}
+      <span style={{ fontSize: '0.5em' }} className='text-muted'>
+        {log.id}
+      </span>{' '}
+      <ButtonGroup>
+        <Tooltip content={_('copyToClipboard')}>
+          <CopyToClipboard text={state.formattedLog}>
+            <Button size='small'>
+              <Icon icon='clipboard' />
+            </Button>
+          </CopyToClipboard>
+        </Tooltip>
+        {CAN_REPORT_BUG && (
+          <ReportBugButton
+            message={`\`\`\`json\n${state.formattedLog}\n\`\`\``}
+            size='small'
+            title='Backup job failed'
+          />
+        )}
+      </ButtonGroup>
+    </span>
+  ),
 ].reduceRight((value, decorator) => decorator(value))
