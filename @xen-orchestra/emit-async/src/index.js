8689cff26bbe5e1569efcaac112d6c90abb02e1a
Julien Fontanet <julien.fontanet@isonoe.net>
2018-09-17 10:42:22 +0200

    feat(emit-async): handle async listeners (#3416)
    Extracted from xo-server.

---
+++ @xen-orchestra/emit-async/src/index.js
@@ -0,0 +1,26 @@
+export default function emitAsync (event) {
+  let opts
+  let i = 1
+
+  // an option object has been passed as first param
+  if (typeof event !== 'string') {
+    opts = event
+    event = arguments[i++]
+  }
+
+  const n = arguments.length - i
+  const args = new Array(n)
+  for (let j = 0; j < n; ++j) {
+    args[j] = arguments[j + i]
+  }
+
+  const onError = opts != null && opts.onError
+
+  return Promise.all(
+    this.listeners(event).map(listener =>
+      new Promise(resolve => {
+        resolve(listener.apply(this, args))
+      }).catch(onError)
+    )
+  )
+}
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -19,6 +19,7 @@
 
 ### Released packages
 
+- @xen-orchestra/emit-async v0.0.0
 - xo-server v5.27.0
 - xo-web v5.27.0
 
