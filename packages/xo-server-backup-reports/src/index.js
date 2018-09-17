d6ea0ea0d0dc60b239d0fe4d39b7b667fe096d8a
badrAZ <azizbibadr@gmail.com>
2018-09-17 15:05:13 +0200

    feat(xo-server-backup-report): ability to send a report

--- packages/xo-server-backup-reports/src/index.js
+++ packages/xo-server-backup-reports/src/index.js
@@ -30,6 +30,20 @@ export const configurationSchema = {
   },
 }
 
+export const testSchema = {
+  type: 'object',
+
+  properties: {
+    runId: {
+      type: 'string',
+      description: "job's runId",
+    },
+  },
+
+  additionalProperties: false,
+  required: ['runId'],
+}
+
 // ===================================================================
 
 const ICON_FAILURE = '🚨'
@@ -116,6 +130,10 @@ class BackupReportsXoPlugin {
 
   load () {
     this._xo.on('job:terminated', this._report)
+  }
+
+  test ({ runId }) {
+    return this._backupNgListener(undefined, undefined, undefined, runId)
   }
 
   unload () {
