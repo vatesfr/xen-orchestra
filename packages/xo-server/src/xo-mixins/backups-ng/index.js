a0ac3afc9c1b563fc04071d4d321874cc582a855
badrAZ <azizbibadr@gmail.com>
2018-09-26 14:48:39 +0200

    remove "runningRestores"

--- packages/xo-server/src/xo-mixins/backups-ng/index.js
+++ packages/xo-server/src/xo-mixins/backups-ng/index.js
@@ -24,7 +24,7 @@ import {
   sum,
   values,
 } from 'lodash'
-import { CancelToken, pFromEvent, pFinally, ignoreErrors } from 'promise-toolbox'
+import { CancelToken, pFromEvent, ignoreErrors } from 'promise-toolbox'
 import Vhd, {
   chainVhd,
   createSyntheticStream as createVhdReadStream,
@@ -451,16 +451,10 @@ export default class BackupNg {
     worker: $Dict<any>,
   }
   _logger: Logger
-  _runningRestores: Array<string>
-
-  get runningRestores () {
-    return this._runningRestores
-  }
 
   constructor (app: any) {
     this._app = app
     this._logger = undefined
-    this._runningRestores = []
 
     app.on('start', () => {
       const executor: Executor = async ({
@@ -677,9 +671,8 @@ export default class BackupNg {
         logger,
         message: 'restore',
       },
-      taskId => {
-       this._runningRestores.push(taskId)
-        return importer(
+      taskId =>
+        importer(
           handler,
           metadataFilename,
           metadata,
@@ -687,10 +680,7 @@ export default class BackupNg {
           xapi.getObject(srId),
           taskId,
           logger
-        )::pFinally(() => {
-          delete this._runningRestores[taskId]
-        })
-      }
+        )
     )()
   }
 
