vatesfr/xen-orchestra

commit aa5b3dc4260d52d887f9fc9b59621eb51f4868b4

chore(xo-web/backup-ng): timeout is in hours (#3553)
Fixes #3550

packages/xo-web/src/xo-app/backup-ng/new/index.js
@@ -1,4 +1,4 @@
-import _ from 'intl'
+import _, { messages } from 'intl'
 import ActionButton from 'action-button'
 import defined, { get } from '@xen-orchestra/defined'
 import Icon from 'icon'
@@ -15,6 +15,7 @@ import { createGetObjectsOfType } from 'selectors'
 import { error } from 'notification'
 import { flatten, includes, isEmpty, map, mapValues, some } from 'lodash'
 import { form } from 'modal'
+import { injectIntl } from 'react-intl'
 import { injectState, provideState } from '@julien-f/freactal'
 import { Map } from 'immutable'
 import { Number } from 'form'
@@ -604,8 +605,10 @@ export default [
         ),
     },
   }),
+  injectIntl,
   injectState,
-  ({ state, effects, remotes, srsById, job = {} }) => {
+  ({ state, effects, remotes, srsById, job = {}, intl }) => {
+    const { formatMessage } = intl
     const { propSettings, settings = propSettings } = state
     const { concurrency, reportWhen = 'failure', offlineSnapshot, timeout } =
       settings.get('') || {}
@@ -869,6 +872,7 @@ export default [
                       id={state.inputTimeoutId}
                       onChange={effects.setTimeout}
                       value={timeout && timeout / 1e3}
+                      placeholder={formatMessage(messages.timeoutUnit)}
                     />
                   </FormGroup>
                   <FormGroup>

packages/xo-web/src/common/intl/messages.js
@@ -408,7 +408,8 @@ const messages = {
   offlineSnapshot: 'Offline snapshot',
   offlineSnapshotInfo: 'Shutdown VMs before snapshotting them',
   timeout: 'Timeout',
-  timeoutInfo: 'Number of seconds after which a job is considered failed',
+  timeoutInfo: 'Number of hours after which a job is considered failed',
+  timeoutUnit: 'in hours',
   dbAndDrRequireEntreprisePlan: 'Delta Backup and DR require Entreprise plan',
   crRequiresPremiumPlan: 'CR requires Premium plan',
   smartBackupModeTitle: 'Smart mode',

commit f311a50c0be87877aff3b96f80e2182430bd8257

fix(xo-web/backup-ng/new): fix timeout unit
Fixes issue introduced in aa5b3dc42

packages/xo-web/src/xo-app/backup-ng/index.js
@@ -200,7 +200,7 @@ class JobsTable extends React.Component {
                 <Li>{_.keyValue(_('concurrency'), concurrency)}</Li>
               )}
               {timeout > 0 && (
-                <Li>{_.keyValue(_('timeout'), timeout / 1e3)} hours</Li>
+                <Li>{_.keyValue(_('timeout'), timeout / 3600e3)} hours</Li>
               )}
               {offlineSnapshot && (
                 <Li>

packages/xo-web/src/xo-app/backup-ng/new/index.js
@@ -525,7 +525,7 @@ export default [
       setTimeout: ({ setGlobalSettings }, value) => () => {
         setGlobalSettings({
           name: 'timeout',
-          value: value && value * 1e3,
+          value: value && value * 3600e3,
         })
       },
       setOfflineSnapshot: (
@@ -892,7 +892,7 @@ export default [
                         <Number
                           id={state.inputTimeoutId}
                           onChange={effects.setTimeout}
-                          value={timeout && timeout / 1e3}
+                          value={timeout && timeout / 3600e3}
                           placeholder={formatMessage(messages.timeoutUnit)}
                         />
                       </FormGroup>