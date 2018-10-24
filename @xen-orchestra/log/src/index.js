b534bfb68690b200b0489ba2d4e4535b33f6ce20
Enishowk <kenshineuh@gmail.com>
2018-10-24 19:47:07 +0200

    fix: error

--- @xen-orchestra/log/src/index.js
+++ @xen-orchestra/log/src/index.js
@@ -38,10 +38,10 @@ for (const name in LEVELS) {
   const level = LEVELS[name]
 
   prototype[name.toLowerCase()] = function (message, data) {
-    if (message instanceof Error || typeof message === 'object') {
-      const { error } = message
+    if (!data && (message instanceof Error || typeof message === 'object')) {
+      const error = message
       data = error
-      message = error.message
+      message = error.message || 'an error as occured'
     }
     global[symbol](new Log(data, level, this._namespace, message, new Date()))
   }
