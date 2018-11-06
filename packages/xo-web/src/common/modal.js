581667e6df4b02db7f88caaa7eb1769c52179448
Rajaa Barhtaoui <rajaa.barhtaoui@gmail.com>
2018-11-06 15:46:08 +0100

    fix(xo-web/StrongConfirm): input lose focus when using Dropdown

--- packages/xo-web/src/common/modal.js
+++ packages/xo-web/src/common/modal.js
@@ -195,7 +195,13 @@ class StrongConfirm extends Component {
 
   _focusAndAddEventListener = ref => {
     if (ref !== null) {
-      ref.focus()
+      /*
+      * input lose focus when using Dropdown of react-bootstrap and to resolve this we used setTimeout
+      * See https://github.com/react-bootstrap/react-bootstrap/issues/2553#issuecomment-324356126
+      */
+      setTimeout(() => {
+        ref.focus()
+      })
       ref.addEventListener('keydown', this._handleKeyDown)
       this.componentWillUnmount = () =>
         ref.removeEventListener('keydown', this._handleKeyDown)
