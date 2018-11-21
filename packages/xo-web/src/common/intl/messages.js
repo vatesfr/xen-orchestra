ab22c00d23bfadeefc6dadafd324c2560f2c2a45
Pierre Donias <pierre.donias@gmail.com>
2018-11-21 11:28:16 +0100

    fix(xo-web/xoa/licenses): tip: XOA license mgmt not supported
    Fixes #3713

--- packages/xo-web/src/common/intl/messages.js
+++ packages/xo-web/src/common/intl/messages.js
@@ -2048,6 +2048,8 @@ const messages = {
   licensesManage: 'Manage the licenses',
   newLicense: 'New license',
   refreshLicenses: 'Refresh',
+  xoaLicenseNotShown:
+    'Your XOA license is not shown here yet (current license: {plan})',
   xosanLicenseRestricted: 'Limited size because XOSAN is in trial',
   xosanAdminNoLicenseDisclaimer:
     'You need a license on this SR to manage the XOSAN.',
