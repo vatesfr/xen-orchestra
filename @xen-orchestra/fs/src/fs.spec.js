49a04ebf1a4c87544c2613a123d2f1bec9b4e90d
Enishowk <kenshineuh@gmail.com>
2018-11-26 18:30:45 +0100

    update unit test

--- @xen-orchestra/fs/src/fs.spec.js
+++ @xen-orchestra/fs/src/fs.spec.js
@@ -9,6 +9,7 @@ import { random } from 'lodash'
 import { tmpdir } from 'os'
 
 import { getHandler } from '.'
+import fs from 'fs-extra'
 
 // https://gist.github.com/julien-f/3228c3f34fdac01ade09
 const unsecureRandomBytes = n => {
@@ -289,4 +290,56 @@ handlers.forEach(url => {
       })
     })
   })
+
+  describe('outputFile()', () => {
+    it(`should put the correct content in file`, async () => {
+      const content = 'unit test'
+      await handler.outputFile(pathFile, content)
+      const contentFile = fs.readFileSync(pathFile, 'utf8')
+
+      await expect(contentFile).toEqual(content)
+    })
+  })
+
+  describe('readFile()', () => {
+    it(`should read the correct content in file`, async () => {
+      const content = await handler.readFile(pathFile, 'utf8')
+      const contentFile = fs.readFileSync(pathFile, 'utf8')
+
+      await expect(content).toEqual(contentFile)
+    })
+  })
+
+  describe('list()', () => {
+    it(`should list the content of folder`, async () => {
+      const content = await handler.list(pathFolder)
+      const contentFolder = fs.readdirSync(pathFolder)
+
+      await expect(content).toEqual(contentFolder)
+    })
+  })
+
+  describe('getSize()', () => {
+    it(`should return the correct size`, async () => {
+      const fileSize = await handler.getSize(pathFile)
+      const stats = await fs.statSync(pathFile)
+
+      expect(fileSize).toEqual(stats.size)
+    })
+  })
+
+  // describe('createReadStream()', () => {
+  //   it(`should return correct stream`, async () => {
+  //     const stream = await handler.createReadStream(pathFile)
+  //     const readStream = fs.createReadStream(pathFile)
+  //     await expect(stream).toEqual(readStream)
+  //   })
+  // })
+  // describe('openFile()', () => {
+  //   it(`should return the correct file descriptor`, async () => {
+  //     const content = await handler.openFile(pathFile, 'r')
+  //     const fd = fs.openSync(pathFile, 'r')
+  //     await expect(content.fd).toEqual(fd)
+  //   })
+  // })
 })
