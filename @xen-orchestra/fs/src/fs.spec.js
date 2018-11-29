66d8ecf5de3cca9fb34523e59ecfbe69398b9d8e
Enishowk <kenshineuh@gmail.com>
2018-11-29 16:36:47 +0100

    update test

--- @xen-orchestra/fs/src/fs.spec.js
+++ @xen-orchestra/fs/src/fs.spec.js
@@ -1,143 +1,120 @@
 /* eslint-env jest */
 
 import { getHandler } from '.'
-import { sep } from 'path'
 import fs from 'fs-extra'
-import os from 'os'
+import { tmpdir } from 'os'
 
-const tmpDir = os.tmpdir()
-const handlerList = [
-  { url: 'file://' + tmpDir },
-  // {
-  //   url: 'smb://login:pass@WORKGROUP\\\\ip\\smb\u0000',
-  //   id: '12345',
-  // },
-  // {
-  //   url: 'nfs://ip:/tmp/test',
-  //   id: '12345',
-  // },
-]
+// https://gist.github.com/julien-f/3228c3f34fdac01ade09
+const unsecureRandomBytes = n => {
+  const bytes = Buffer.alloc(n)
 
-const testDir = `${tmpDir}${sep}tests_${Date.now()}`
-const pathFile = `${testDir}${sep}file`
+  const odd = n & 1
+  for (let i = 0, m = n - odd; i < m; i += 2) {
+    bytes.writeUInt16BE((Math.random() * 65536) | 0, i)
+  }
 
-handlerList.forEach(element => {
-  let handler
+  if (odd) {
+    bytes.writeUInt8((Math.random() * 256) | 0, n - 1)
+  }
 
-  beforeAll(async () => {
-    handler = getHandler(element)
-    await handler.outputFile(pathFile, '')
-  })
+  return bytes
+}
 
-  afterAll(async () => {
-    if (fs.existsSync(testDir)) {
-      await handler.rmdir(testDir, { recursive: true })
-    }
-    handler = null
-  })
+const TEST_DATA = unsecureRandomBytes(1024)
 
-  describe('test()', () => {
-    it(`handler doesn't crash`, async () => {
-      const result = await handler.test()
+;[`file://${tmpdir()}`].forEach(url => {
+  describe(url, () => {
+    let handler
+    const testDir = `xo-fs-tests-${Date.now()}`
 
-      expect(result).toMatchSnapshot()
+    beforeAll(() => {
+      handler = getHandler({ url })
     })
-  })
-
-  describe('outputFile()', () => {
-    it(`should put the correct content in file`, async () => {
-      const content = 'unit test'
-      await handler.outputFile(pathFile, content)
-      const contentFile = fs.readFileSync(pathFile, 'utf8')
-
-      await expect(contentFile).toEqual(content)
-    })
-  })
-
-  describe('readFile()', () => {
-    it(`should read the correct content in file`, async () => {
-      const content = await handler.readFile(pathFile, 'utf8')
-      const contentFile = fs.readFileSync(pathFile, 'utf8')
-
-      await expect(content).toEqual(contentFile)
-    })
-  })
-
-  describe('list()', () => {
-    it(`should list the content of folder`, async () => {
-      const content = await handler.list(testDir)
-      const contentFolder = fs.readdirSync(testDir)
-
-      await expect(content).toEqual(contentFolder)
-    })
-  })
-
-  describe('createReadStream()', () => {
-    it(`should return a stream`, async () => {
-      const stream = await handler.createReadStream(pathFile)
-      const readStream = fs.createReadStream(pathFile)
-
-      await expect(stream.path).toEqual(readStream.path)
-    })
-  })
-
-  describe('getSize()', () => {
-    it(`should return the correct size`, async () => {
-      const fileSize = await handler.getSize(pathFile)
-      const stats = await fs.statSync(pathFile)
-
-      expect(fileSize).toEqual(stats.size)
-    })
-  })
-
-  describe('rename()', () => {
     afterAll(() => {
-      fs.unlinkSync(`${testDir}${sep}file2`)
+      handler = undefined
     })
-
-    it(`should rename the file`, async () => {
-      await handler.rename(pathFile, `${testDir}${sep}file2`)
-      await expect(fs.existsSync(`${testDir}${sep}file2`)).toBe(true)
-    })
-  })
-
-  describe('unlink()', () => {
-    it(`should remove the file`, async () => {
-      await handler.unlink(pathFile)
-
-      expect(fs.existsSync(pathFile)).toBe(false)
-    })
-  })
-
-  describe('rmdir()', () => {
-    const testRmDir = `${tmpDir}${sep}tests_rmdir_${Date.now()}`
-    const pathFile = `${testRmDir}${sep}file_recursive`
 
     beforeEach(async () => {
-      await handler.outputFile(pathFile, '')
+      await handler.outputFile(`${testDir}/file`, TEST_DATA)
     })
-
     afterEach(async () => {
-      if (fs.existsSync(testRmDir)) {
-        await handler.rmdir(testRmDir, { recursive: true })
-      }
+      await handler.rmdir(testDir, { recursive: true }).catch(error => {
+        if (error.code !== 'ENOENT') {
+          throw error
+        }
+      })
     })
 
-    it(`should remove folder resursively`, async () => {
-      await handler.rmdir(testRmDir, { recursive: true })
-
-      expect(fs.existsSync(testRmDir)).toBe(false)
+    describe('test handler', () => {
+      it(`handler doesn't crash`, async () => {
+        expect(await handler.test()).toMatchSnapshot()
+      })
     })
 
-    it(`should throw an error when recursive is false`, async () => {
-      let error
-      try {
-        await handler.rmdir(testRmDir)
-      } catch (e) {
-        error = e
-      }
+    describe('outputFile', () => {
+      it('writes data to a file', async () => {
+        expect(await handler.readFile(`${testDir}/file`)).toEqual(TEST_DATA)
+      })
+    })
 
-      await expect(error.code).toEqual('ENOTEMPTY')
+    describe('list', () => {
+      it(`should list the content of folder`, async () => {
+        const content = await handler.list(testDir)
+        const contentFolder = await fs.readdir(`${tmpdir}/${testDir}`)
+
+        await expect(content).toEqual(contentFolder)
+      })
+    })
+
+    describe('createReadStream', () => {
+      it(`should return a stream`, async () => {
+        const stream = await handler.createReadStream(`${testDir}/file`)
+        const readStream = fs.createReadStream(`${tmpdir}/${testDir}/file`)
+
+        await expect(stream.path).toEqual(readStream.path)
+      })
+    })
+    describe('getSize', () => {
+      it(`should return the correct size`, async () => {
+        const fileSize = await handler.getSize(`${testDir}/file`)
+        const stats = await fs.statSync(`${tmpdir}/${testDir}/file`)
+
+        expect(fileSize).toEqual(stats.size)
+      })
+    })
+
+    describe('rename', () => {
+      it(`should rename the file`, async () => {
+        await handler.rename(`${testDir}/file`, `${testDir}/file2`)
+        await expect(fs.existsSync(`${tmpdir}/${testDir}/file2`)).toBe(true)
+      })
+    })
+
+    describe('unlink', () => {
+      it(`should remove the file`, async () => {
+        await handler.unlink(`${testDir}/file`)
+
+        expect(fs.existsSync(`${tmpdir}/${testDir}/file`)).toBe(false)
+      })
+    })
+
+    describe('rmdir', () => {
+      it(`should remove folder resursively`, async () => {
+        await handler.rmdir(testDir, { recursive: true })
+
+        expect(fs.existsSync(`${tmpdir}/${testDir}`)).toBe(false)
+      })
+
+      it(`should throw an error when recursive is false`, async () => {
+        let error
+        try {
+          await handler.rmdir(testDir)
+        } catch (e) {
+          error = e
+        }
+
+        await expect(error.code).toEqual('ENOTEMPTY')
+      })
     })
   })
 })
