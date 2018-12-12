b7e58eeb3fa7cb090b9d815924a2f69d2c4a63d1
Julien Fontanet <julien.fontanet@isonoe.net>
2018-12-12 11:17:42 +0100

    chore(eslint): use plugin:node/recommended

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,16 @@
 module.exports = {
-  env: { es6: true },
-
   // use standard configuration and disable rules handled by prettier
-  extends: ["standard", "prettier"],
+  extends: [
+    // standard configuration
+    "standard",
+
+    // https://github.com/mysticatea/eslint-plugin-node#-rules
+    "plugin:node/recommended",
+
+    // disable rules handled by prettier
+    "prettier",
+    "prettier/standard",
+  ],
 
   rules: {
     // prefer let/const over var
@@ -12,5 +20,12 @@ module.exports = {
     //
     // should be included in standard: https://github.com/standard/eslint-config-standard/pull/133/
     "prefer-const": "error",
+
+    // detect incorrect import
+    "node/no-extraneous-import": "error",
+    "node/no-missing-import": "error",
+
+    // we are using Babel
+    "node/no-unsupported-features/es-syntax": "off",
   },
 };
