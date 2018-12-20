b5d3deb916239f1a707966cc342dfe7dedee71e6
badrAZ <azizbibadr@gmail.com>
2018-12-20 16:56:40 +0100

    missing file

---
+++ packages/xo-web/src/common/xo/snapshot-vm-modal/index.js
@@ -0,0 +1,23 @@
+import Component from 'base-component'
+import React from 'react'
+
+import _ from '../../intl'
+
+export default class SnapshotVmModalBody extends Component {
+  get value() {
+    return this.state.saveMemory
+  }
+
+  render() {
+    return (
+      <label>
+        <input
+          type='checkbox'
+          onChange={this.linkState('saveMemory')}
+          checked={this.state.saveMemory}
+        />{' '}
+        {_('snapshotVmModalSaveMemory')}
+      </label>
+    )
+  }
+}
