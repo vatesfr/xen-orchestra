4b4e5835b6a33c275640e790c424e9e095508b29
Enishowk <kenshineuh@gmail.com>
2019-01-04 18:49:21 +0100

    check if mount.cifs is available

--- @xen-orchestra/fs/src/index.js
+++ @xen-orchestra/fs/src/index.js
@@ -1,16 +1,17 @@
 // @flow
+import execa from 'execa'
 
 import type RemoteHandler from './abstract'
 import RemoteHandlerLocal from './local'
 import RemoteHandlerNfs from './nfs'
 import RemoteHandlerSmb from './smb'
+import RemoteHandlerSmbMount from './smb-mount'
 
 export type { default as RemoteHandler } from './abstract'
 export type Remote = { url: string }
 
 const HANDLERS = {
   file: RemoteHandlerLocal,
-  smb: RemoteHandlerSmb,
   nfs: RemoteHandlerNfs,
 }
 
@@ -18,7 +19,18 @@ export const getHandler = (remote: Remote, ...rest: any): RemoteHandler => {
   // FIXME: should be done in xo-remote-parser.
   const type = remote.url.split('://')[0]
 
-  const Handler = HANDLERS[type]
+  let Handler
+  if (type === 'smb') {
+    try {
+      execa.sync('mount.cifs', ['-V'])
+      Handler = RemoteHandlerSmbMount
+    } catch (error) {
+      Handler = RemoteHandlerSmb
+    }
+  } else {
+    Handler = HANDLERS[type]
+  }
+
   if (!Handler) {
     throw new Error('Unhandled remote type')
   }
