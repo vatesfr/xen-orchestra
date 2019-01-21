82bbd38f48cb52248a81763b1008b035a9f78ab6
Pierre Donias <pierre.donias@gmail.com>
2019-01-21 11:33:07 +0100

    implem

--- packages/xo-web/src/xo-app/xoa/notifications/index.js
+++ packages/xo-web/src/xo-app/xoa/notifications/index.js
@@ -1,47 +1,80 @@
-import React from 'react'
+import ActionButton from 'action-button'
 import Component from 'base-component'
+import cookies from 'cookies-js'
+import Icon from 'icon'
+import marked from 'marked'
+import React from 'react'
 import SortedTable from 'sorted-table'
+import updater from 'xoa-updater'
+import { alert } from 'modal'
 import { FormattedDate } from 'react-intl'
 
 const COLUMNS = [
   {
     default: true,
     name: 'Date',
-    itemRenderer: ({ date }) => (
-      <FormattedDate
-        value={new Date(date)}
-        month='long'
-        day='numeric'
-        year='numeric'
-        hour='2-digit'
-        minute='2-digit'
-        second='2-digit'
-      />
+    itemRenderer: ({ created, level }) => (
+      <span>
+        <Icon icon={level === 'warning' ? 'info' : 'alarm'} />{' '}
+        <FormattedDate
+          value={new Date(created)}
+          month='long'
+          day='numeric'
+          year='numeric'
+          hour='2-digit'
+          minute='2-digit'
+          second='2-digit'
+        />
+      </span>
     ),
-    sortCriteria: 'date',
+    sortCriteria: 'created',
     sortOrder: 'desc',
   },
   {
-    name: 'Content',
-    itemRenderer: ({ content }) => content,
+    name: '',
+    itemRenderer: ({ id }) => !cookies.get(`notification:${id}`) && 'NEW',
   },
 ]
 
-const collection = [
-  {
-    date: 1536242182676,
-    content: 'Hey there!',
-  },
-]
+const Notification = ({ notification: { message } }) => (
+  <div dangerouslySetInnerHTML={{ __html: marked(message) }} />
+)
 
 export default class Notifications extends Component {
-  render () {
+  _getNotifications = () =>
+    updater._call('getMessages').then(messages => this.setState({ messages }))
+
+  _showMessage = async notification => {
+    cookies.set(`notification:${notification.id}`, 'dismissed')
+    await alert(
+      <span>
+        <Icon icon='notification' /> Notification
+      </span>,
+      <Notification notification={notification} />
+    )
+    await this._getNotifications()
+  }
+
+  componentDidMount = this._getNotifications
+
+  render() {
     return (
-      <SortedTable
-        columns={COLUMNS}
-        collection={collection}
-        stateUrlParam='s'
-      />
+      <div>
+        <ActionButton
+          btnStyle='primary'
+          className='mb-1'
+          handler={this._getNotifications}
+          icon='refresh'
+        >
+          Refresh
+        </ActionButton>
+        <SortedTable
+          columns={COLUMNS}
+          collection={this.state.messages}
+          rowAction={this._showMessage}
+          stateUrlParam='s'
+        />
+      </div>
     )
   }
 }
