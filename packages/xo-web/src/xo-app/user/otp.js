e9dba228a492381ceeec346eda407afa07d2d52f
Enishowk <kenshineuh@gmail.com>
2019-01-25 16:11:22 +0100

    chore: Otp with Reaclette

--- packages/xo-web/src/xo-app/user/otp.js
+++ packages/xo-web/src/xo-app/user/otp.js
@@ -1,69 +1,59 @@
 import authenticator from 'otplib/authenticator'
-import Component from 'base-component'
 import crypto from 'crypto'
+import decorate from 'apply-decorators'
 import qrcode from 'qrcode'
 import React from 'react'
 import { addOtp, removeOtp } from 'xo'
+import { injectState, provideState } from 'reaclette'
 
 import { Container, Row, Col } from '../../common/grid'
 import { Toggle } from '../../common/form'
 
 authenticator.options = { crypto }
 
-export default class Otp extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      secret: props.user.preferences.otp || authenticator.generateSecret(),
-      qrcode: undefined,
-    }
-  }
-
-  componentDidMount() {
-    const { secret } = this.state
-    const { user } = this.props
-    const otpauth = authenticator.keyuri(user.email, 'XenOrchestra', secret)
-
-    qrcode.toDataURL(otpauth).then(url => {
-      this.setState({ qrcode: url })
-    })
-  }
-
-  _handleOtp = isChecked => {
-    const { secret } = this.state
-
-    if (isChecked) {
-      addOtp(secret)
-    } else {
-      removeOtp()
-    }
-  }
-
-  render() {
-    const { secret, qrcode } = this.state
-    const { user } = this.props
-
-    return (
-      <Container>
-        <Row>
-          <Col smallSize={2}>
-            <strong>Authentification OTP</strong>
-          </Col>
-          <Col smallSize={10}>
-            <Toggle
-              value={user.preferences.otp || false}
-              onChange={value => this._handleOtp(value)}
-            />
-            {user.preferences.otp !== undefined && (
-              <div>
-                <div>{secret}</div>
-                <img src={qrcode} alt='qrcode' />
-              </div>
-            )}
-          </Col>
-        </Row>
-      </Container>
-    )
-  }
-}
+export default decorate([
+  provideState({
+    initialState: ({ user }) => ({
+      secret: user.preferences.otp || authenticator.generateSecret(),
+    }),
+    effects: {
+      _handleOtp: (_, isChecked) => ({ secret }, _) => {
+        if (isChecked) {
+          addOtp(secret)
+        } else {
+          removeOtp()
+        }
+      },
+    },
+    computed: {
+      async qrcode({ secret }, { user }) {
+        const otpauth = authenticator.keyuri(user.email, 'XenOrchestra', secret)
+        return qrcode.toDataURL(otpauth).then(url => url)
+      },
+    },
+  }),
+  injectState,
+  ({ state, effects, user }) => (
+    <Container>
+      <Row>
+        <Col smallSize={2}>
+          <strong>Authentification OTP</strong>
+        </Col>
+        <Col smallSize={10}>
+          <Toggle
+            value={user.preferences.otp || false}
+            onChange={effects._handleOtp}
+          />
+          {user.preferences.otp !== undefined && (
+            <div>
+              <div>{state.secret}</div>
+              {state.qrcode !== undefined && (
+                <img src={state.qrcode} alt='qrcode' />
+              )}
+            </div>
+          )}
+        </Col>
+      </Row>
+    </Container>
+  ),
+])
