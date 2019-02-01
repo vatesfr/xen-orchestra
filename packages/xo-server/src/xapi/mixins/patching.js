e85e1e14e934552dce01a1c56b2624d8528a1eb3
Pierre Donias <pierre.donias@gmail.com>
2019-02-01 15:19:53 +0100

    Ignore paid patches for free-license hosts

--- packages/xo-server/src/xapi/mixins/patching.js
+++ packages/xo-server/src/xapi/mixins/patching.js
@@ -216,7 +216,6 @@ export default {
   // list patches:
   //   - not installed on the host
   //   - not conflicting with any of the installed patches
-  // TODO: ignore paid patches for free license hosts
   // TODO: handle upgrade patches
   async _listInstallablePatches(host) {
     const all = await this._listPatches(host)
@@ -225,6 +224,10 @@ export default {
     const installable = { __proto__: null }
     forEach(all, (patch, uuid) => {
       if (installed[uuid]) {
+        return
+      }
+
+      if (host.license_params.sku_type === 'free' && patch.paid) {
         return
       }
 
