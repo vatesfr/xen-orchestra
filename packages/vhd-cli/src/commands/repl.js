8150d3110ca06d9ff03b092ad7a97000ac2c12c3
Julien Fontanet <julien.fontanet@isonoe.net>
2019-03-05 11:46:14 +0100

    fix(vhd-cli/repl): various fixes

--- packages/vhd-cli/src/commands/repl.js
+++ packages/vhd-cli/src/commands/repl.js
@@ -1,18 +1,20 @@
 import { asCallback, fromCallback, fromEvent } from 'promise-toolbox'
 import { getHandler } from '@xen-orchestra/fs'
-import { resolve } from 'path'
+import { relative } from 'path'
 import { start as createRepl } from 'repl'
-import * as Vhd from 'vhd-lib'
+import Vhd, * as vhdLib from 'vhd-lib'
 
 export default async args => {
-  const handler = getHandler({ url: 'file://' + process.cwd() })
+  const cwd = process.cwd()
+  const handler = getHandler({ url: 'file://' + cwd })
   await handler.sync()
   try {
     const repl = createRepl({
       prompt: 'vhd> ',
     })
-    Object.assign(repl.context, Vhd)
-    repl.context.open = path => new Vhd.Vhd(handler, resolve(path))
+    Object.assign(repl.context, vhdLib)
+    repl.context.handler = handler
+    repl.context.open = path => new Vhd(handler, relative(cwd, path))
 
     // Make the REPL waits for promise completion.
     repl.eval = (evaluate => (cmd, context, filename, cb) => {
