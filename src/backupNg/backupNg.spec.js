437b0b0240e3cdb94658c3e169a9926aa36baa0e
heafalan <natacha.heafala@gmail.com>
2019-03-05 13:27:02 +0100

    feat: test backupNg.createJob with schedules (#81)
    See #76

--- src/backupNg/backupNg.spec.js
+++ src/backupNg/backupNg.spec.js
@@ -30,5 +30,40 @@ describe("backupNg", () => {
       expect(omit(backupNg, "id", "userId")).toMatchSnapshot();
       expect(backupNg.userId).toBe(xo._user.id);
     });
+
+    it("creates a new backup job with schedules", async () => {
+      const scheduleTempId = "scheduleTempId";
+      const { id: jobId } = await xo.createTempBackupNgJob({
+        ...defaultBackupNg,
+        schedules: {
+          [scheduleTempId]: {
+            name: "scheduleTest",
+            cron: "0 * * * * *",
+          },
+        },
+        settings: {
+          ...defaultBackupNg.settings,
+          [scheduleTempId]: { snapshotRetention: 1 },
+        },
+      });
+
+      const backupNgJob = await xo.call("backupNg.getJob", { id: jobId });
+
+      let scheduleId;
+      for (const key in backupNgJob.settings) {
+        if (key !== "") {
+          expect(backupNgJob.settings[key]).toEqual({ snapshotRetention: 1 });
+          scheduleId = key;
+        }
+      }
+
+      expect(typeof backupNgJob).toBe("object");
+      expect(omit(backupNgJob, "id", "userId", "settings")).toMatchSnapshot();
+      expect(backupNgJob.userId).toBe(xo._user.id);
+
+      const schedule = await xo.call("schedule.get", { id: scheduleId });
+      expect(omit(schedule, "id", "jobId")).toMatchSnapshot();
+      expect(schedule.jobId).toBe(jobId);
+    });
   });
 });
