c933f0ed243b15abf445ca160716a4090bca4e34
Pierre Donias <pierre.donias@gmail.com>
2019-03-22 16:41:38 +0100

    Missing file

---
+++ packages/xo-server/src/xo-mixins/patches.js
@@ -0,0 +1,18 @@
+import { differenceBy } from 'lodash'
+
+export default class {
+  constructor(xo) {
+    this._xo = xo
+  }
+
+  getPatchesDifference(hostA, hostB) {
+    const patchesA = this._xo
+      .getObject(hostA)
+      .patches.map(patchId => this._xo.getObject(patchId))
+    const patchesB = this._xo
+      .getObject(hostB)
+      .patches.map(patchId => this._xo.getObject(patchId))
+
+    return differenceBy(patchesA, patchesB, 'name').map(patch => patch.name)
+  }
+}
