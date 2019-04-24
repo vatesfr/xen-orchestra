vatesfr/xen-orchestra

commit 7acd90307bfdcd4ec67860f2c66c61c087123185

feat(xo-web/new-vm): network config box for cloud-init (#4150)
Fixes #3872

packages/xo-web/src/common/cloud-config.js
@@ -21,6 +21,37 @@ const showAvailableTemplateVars = () =>
     </ul>
   )
 
+const showNetworkConfigInfo = () =>
+  alert(
+    _('newVmNetworkConfigLabel'),
+    <div>
+      <p>
+        {_('newVmNetworkConfigInfo', {
+          noCouldDatasourceLink: (
+            <a
+              href='https://cloudinit.readthedocs.io/en/latest/topics/datasources/nocloud.html#datasource-nocloud'
+              target='_blank'
+            >
+              {_('newVmNoCloudDatasource')}
+            </a>
+          ),
+        })}
+      </p>
+      <p>
+        {_('newVmNetworkConfigDocLink', {
+          networkConfigDocLink: (
+            <a
+              href='https://cloudinit.readthedocs.io/en/latest/topics/network-config-format-v1.html'
+              target='_blank'
+            >
+              {_('newVmNetworkConfigDoc')}
+            </a>
+          ),
+        })}
+      </p>
+    </div>
+  )
+
 export const AvailableTemplateVars = () => (
   <Tooltip content={_('availableTemplateVarsInfo')}>
     <a
@@ -33,5 +64,26 @@ export const AvailableTemplateVars = () => (
   </Tooltip>
 )
 
+export const NetworkConfigInfo = () => (
+  <Tooltip content={_('newVmNetworkConfigTooltip')}>
+    <a
+      className='text-info'
+      style={{ cursor: 'pointer' }}
+      onClick={showNetworkConfigInfo}
+    >
+      <Icon icon='info' />
+    </a>
+  </Tooltip>
+)
+
 export const DEFAULT_CLOUD_CONFIG_TEMPLATE =
   '#cloud-config\n#hostname: {name}%\n#ssh_authorized_keys:\n#  - ssh-rsa <myKey>\n#packages:\n#  - htop\n'
+
+// SOURCE: https://cloudinit.readthedocs.io/en/latest/topics/network-config-format-v1.html
+export const DEFAULT_NETWORK_CONFIG_TEMPLATE = `#network:
+#  version: 1
+#  config:
+#  - type: physical
+#    name: eth0
+#    subnets:
+#      - type: dhcp`

packages/xo-web/src/common/intl/messages.js
@@ -1350,6 +1350,15 @@ const messages = {
   newVmHideAdvanced: 'Hide advanced settings',
   newVmShare: 'Share this VM',
   newVmSrsNotOnSameHost: 'The SRs must either be on the same host or shared',
+  newVmNetworkConfigLabel: 'Network config',
+  newVmNetworkConfigInfo:
+    'Network configuration is only compatible with the {noCouldDatasourceLink}.',
+  newVmNetworkConfigDocLink: 'See {networkConfigDocLink}.',
+  newVmNetworkConfigTooltip:
+    'Click here to get more information about network config',
+  newVmUserConfigLabel: 'User config',
+  newVmNoCloudDatasource: 'NoCould datasource',
+  newVmNetworkConfigDoc: 'Network config documentation',
 
   // ----- Self -----
   resourceSets: 'Resource sets',

packages/xo-server/src/api/vm.js
@@ -193,6 +193,11 @@ create.params = {
     optional: true,
   },
 
+  networkConfig: {
+    type: 'string',
+    optional: true,
+  },
+
   coreOs: {
     type: 'boolean',
     optional: true,

packages/xo-server/src/xapi/mixins/vm.js
@@ -52,6 +52,7 @@ export default {
 
       coreOs = false,
       cloudConfig = undefined,
+      networkConfig = undefined,
 
       vgpuType = undefined,
       gpuGroup = undefined,
@@ -241,10 +242,16 @@ export default {
         }
       })
 
-      const method = coreOs
-        ? 'createCoreOsCloudInitConfigDrive'
-        : 'createCloudInitConfigDrive'
-      await this[method](vm.$id, srRef, cloudConfig)
+      if (coreOs) {
+        await this.createCoreOsCloudInitConfigDrive(vm.$id, srRef, cloudConfig)
+      } else {
+        await this.createCloudInitConfigDrive(
+          vm.$id,
+          srRef,
+          cloudConfig,
+          networkConfig
+        )
+      }
     }
 
     // wait for the record with all the VBDs and VIFs

packages/xo-web/src/xo-app/new-vm/index.js
@@ -17,6 +17,8 @@ import Wizard, { Section } from 'wizard'
 import {
   AvailableTemplateVars,
   DEFAULT_CLOUD_CONFIG_TEMPLATE,
+  DEFAULT_NETWORK_CONFIG_TEMPLATE,
+  NetworkConfigInfo,
 } from 'cloud-config'
 import { confirm } from 'modal'
 import { Container, Row, Col } from 'grid'
@@ -398,6 +400,7 @@ export default class NewVm extends BaseComponent {
 
     let cloudConfig
     let cloudConfigs
+    let networkConfig
     if (state.installMethod !== 'noConfigDrive') {
       if (state.installMethod === 'SSH') {
         const format = hostname =>
@@ -434,8 +437,12 @@ export default class NewVm extends BaseComponent {
             replacer(state, i + +seqStart)
           )
         }
+        networkConfig = defined(
+          state.networkConfig,
+          DEFAULT_NETWORK_CONFIG_TEMPLATE
+        )
       }
-    } else if (state.template.name_label === 'CoreOS') {
+    } else if (this._isCoreOs()) {
       cloudConfig = state.cloudConfig
       if (state.multipleVms) {
         cloudConfigs = new Array(state.nbVms).fill(state.cloudConfig)
@@ -488,7 +495,8 @@ export default class NewVm extends BaseComponent {
       bootAfterCreate: state.bootAfterCreate,
       share: state.share,
       cloudConfig,
-      coreOs: state.template.name_label === 'CoreOS',
+      networkConfig: this._isCoreOs() ? undefined : networkConfig,
+      coreOs: this._isCoreOs(),
       tags: state.tags,
       vgpuType: get(() => state.vgpuType.id),
       gpuGroup: get(() => state.vgpuType.gpuGroup),
@@ -588,7 +596,7 @@ export default class NewVm extends BaseComponent {
       }),
     })
 
-    if (template.name_label === 'CoreOS') {
+    if (this._isCoreOs()) {
       getCloudInitConfig(template.id).then(
         cloudConfig =>
           this._setState({ cloudConfig, coreOsDefaultTemplateError: false }),
@@ -736,6 +744,11 @@ export default class NewVm extends BaseComponent {
     },
     () => this.state.state.CPUs,
     getCoresPerSocketPossibilities
+  )
+
+  _isCoreOs = createSelector(
+    () => this.state.template,
+    template => template && template.name_label === 'CoreOS'
   )
 
   // On change -------------------------------------------------------------------
@@ -1117,6 +1130,7 @@ export default class NewVm extends BaseComponent {
     const {
       cloudConfig,
       customConfig,
+      networkConfig,
       installIso,
       installMethod,
       installNetwork,
@@ -1208,13 +1222,39 @@ export default class NewVm extends BaseComponent {
               </span>
             </LineItem>
             <br />
-            <DebounceTextarea
-              className='form-control'
-              disabled={installMethod !== 'customConfig'}
-              onChange={this._linkState('customConfig')}
-              rows={7}
-              value={defined(customConfig, DEFAULT_CLOUD_CONFIG_TEMPLATE)}
-            />
+            <LineItem>
+              <Item>
+                <label className='text-muted'>
+                  {_('newVmUserConfigLabel')}
+                  <br />
+                  <DebounceTextarea
+                    className='form-control'
+                    disabled={installMethod !== 'customConfig'}
+                    onChange={this._linkState('customConfig')}
+                    rows={7}
+                    value={defined(customConfig, DEFAULT_CLOUD_CONFIG_TEMPLATE)}
+                  />
+                </label>
+              </Item>
+              {!this._isCoreOs() && (
+                <Item>
+                  <label className='text-muted'>
+                    {_('newVmNetworkConfigLabel')} <NetworkConfigInfo />
+                    <br />
+                    <DebounceTextarea
+                      className='form-control'
+                      disabled={installMethod !== 'customConfig'}
+                      onChange={this._linkState('networkConfig')}
+                      rows={7}
+                      value={defined(
+                        networkConfig,
+                        DEFAULT_NETWORK_CONFIG_TEMPLATE
+                      )}
+                    />
+                  </label>
+                </Item>
+              )}
+            </LineItem>
           </SectionContent>
         ) : (
           <SectionContent>
@@ -1294,7 +1334,7 @@ export default class NewVm extends BaseComponent {
             )}
           </SectionContent>
         )}
-        {template.name_label === 'CoreOS' && (
+        {this._isCoreOs() && (
           <div>
             <label>{_('newVmCloudConfig')}</label>{' '}
             {!coreOsDefaultTemplateError ? (

commit e8bfedc5c5b90bd3882489af5398a9cbd577ea3c

fix(xo-web/new-vm): typos
Introduced by 7acd90307bfdcd4ec67860f2c66c61c087123185

packages/xo-web/src/common/cloud-config.js
@@ -27,7 +27,7 @@ const showNetworkConfigInfo = () =>
     <div>
       <p>
         {_('newVmNetworkConfigInfo', {
-          noCouldDatasourceLink: (
+          noCloudDatasourceLink: (
             <a
               href='https://cloudinit.readthedocs.io/en/latest/topics/datasources/nocloud.html#datasource-nocloud'
               target='_blank'

packages/xo-web/src/common/intl/messages.js
@@ -1352,12 +1352,12 @@ const messages = {
   newVmSrsNotOnSameHost: 'The SRs must either be on the same host or shared',
   newVmNetworkConfigLabel: 'Network config',
   newVmNetworkConfigInfo:
-    'Network configuration is only compatible with the {noCouldDatasourceLink}.',
+    'Network configuration is only compatible with the {noCloudDatasourceLink}.',
   newVmNetworkConfigDocLink: 'See {networkConfigDocLink}.',
   newVmNetworkConfigTooltip:
     'Click here to get more information about network config',
   newVmUserConfigLabel: 'User config',
-  newVmNoCloudDatasource: 'NoCould datasource',
+  newVmNoCloudDatasource: 'NoCloud datasource',
   newVmNetworkConfigDoc: 'Network config documentation',
 
   // ----- Self -----