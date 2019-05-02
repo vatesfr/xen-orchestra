1bd68f387a794797771347372b28f62f937f16a2
badrAZ <azizbibadr@gmail.com>
2019-05-02 12:08:31 +0200

    fix(Backup-ng): report sent even though "Never" is selected
    Fixes #4092

--- packages/xo-server-backup-reports/src/index.js
+++ packages/xo-server-backup-reports/src/index.js
@@ -245,6 +245,8 @@ class BackupReportsXoPlugin {
       if (
         !force &&
         (reportWhen === 'never' ||
+          // Introduced by: https://github.com/vatesfr/xen-orchestra/pull/2869/commits/753ee994f2948bbaca9d3161eaab82329a682773#diff-9c044ab8a42ed6576ea927a64c1ec3ebR105
+          reportWhen === 'Never' ||
           (reportWhen === 'failure' && log.status === 'success'))
       ) {
         return
