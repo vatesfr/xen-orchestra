vatesfr/xen-orchestra

commit 59e68682bde8ea4445182bbd24a5c4fba83a6384

fix(xo-web): lock admin pages (#4220)
Related to xoa-support#1460

packages/xo-web/src/xo-app/backup-ng/index.js
@@ -12,9 +12,9 @@ import React from 'react'
 import SortedTable from 'sorted-table'
 import StateButton from 'state-button'
 import Tooltip from 'tooltip'
+import { adminOnly, connectStore, routes } from 'utils'
 import { Card, CardHeader, CardBlock } from 'card'
 import { confirm } from 'modal'
-import { connectStore, routes } from 'utils'
 import { constructQueryString } from 'smart-backup'
 import { Container, Row, Col } from 'grid'
 import { createGetLoneSnapshots, createSelector } from 'selectors'
@@ -420,18 +420,20 @@ const ChooseBackupType = () => (
   </Container>
 )
 
-export default routes('overview', {
-  ':id/edit': Edit,
-  new: ChooseBackupType,
-  'new/vms': NewVmBackup,
-  'new/metadata': NewMetadataBackup,
-  overview: Overview,
-  restore: Restore,
-  'restore/metadata': RestoreMetadata,
-  'file-restore': FileRestore,
-  health: Health,
-})(({ children }) => (
-  <Page header={HEADER} title='backupPage' formatTitle>
-    {children}
-  </Page>
-))
+export default adminOnly(
+  routes('overview', {
+    ':id/edit': Edit,
+    new: ChooseBackupType,
+    'new/vms': NewVmBackup,
+    'new/metadata': NewMetadataBackup,
+    overview: Overview,
+    restore: Restore,
+    'restore/metadata': RestoreMetadata,
+    'file-restore': FileRestore,
+    health: Health,
+  })(({ children }) => (
+    <Page header={HEADER} title='backupPage' formatTitle>
+      {children}
+    </Page>
+  ))
+)

packages/xo-web/src/xo-app/backup/index.js
@@ -3,7 +3,7 @@ import Icon from 'icon'
 import Link from 'link'
 import Page from '../page'
 import React from 'react'
-import { routes } from 'utils'
+import { adminOnly, routes } from 'utils'
 import { Container, Row, Col } from 'grid'
 import { NavLink, NavTabs } from 'nav'
 
@@ -62,16 +62,18 @@ const HEADER = (
   </Container>
 )
 
-const Backup = routes('overview', {
-  ':id/edit': Edit,
-  new: DEVELOPMENT ? New : DeprecatedMsg,
-  overview: Overview,
-  restore: MovingRestoreMessage,
-  'file-restore': MovingFileRestoreMessage,
-})(({ children }) => (
-  <Page header={HEADER} title='backupPage' formatTitle>
-    {children}
-  </Page>
-))
+const Backup = adminOnly(
+  routes('overview', {
+    ':id/edit': Edit,
+    new: DEVELOPMENT ? New : DeprecatedMsg,
+    overview: Overview,
+    restore: MovingRestoreMessage,
+    'file-restore': MovingFileRestoreMessage,
+  })(({ children }) => (
+    <Page header={HEADER} title='backupPage' formatTitle>
+      {children}
+    </Page>
+  ))
+)
 
 export default Backup

packages/xo-web/src/xo-app/jobs/index.js
@@ -4,7 +4,7 @@ import Page from '../page'
 import React from 'react'
 import { Container, Row, Col } from 'grid'
 import { NavLink, NavTabs } from 'nav'
-import { routes } from 'utils'
+import { adminOnly, routes } from 'utils'
 
 import Edit from './edit'
 import New from './new'
@@ -37,16 +37,18 @@ const HEADER = (
   </Container>
 )
 
-const Jobs = routes('overview', {
-  ':id/edit': Edit,
-  new: New,
-  overview: Overview,
-  schedules: Schedules,
-  'schedules/:id/edit': EditSchedule,
-})(({ children }) => (
-  <Page header={HEADER} title='jobsPage' formatTitle>
-    {children}
-  </Page>
-))
+const Jobs = adminOnly(
+  routes('overview', {
+    ':id/edit': Edit,
+    new: New,
+    overview: Overview,
+    schedules: Schedules,
+    'schedules/:id/edit': EditSchedule,
+  })(({ children }) => (
+    <Page header={HEADER} title='jobsPage' formatTitle>
+      {children}
+    </Page>
+  ))
+)
 
 export default Jobs

packages/xo-web/src/xo-app/settings/index.js
@@ -2,7 +2,7 @@ import _ from 'intl'
 import Icon from 'icon'
 import Page from '../page'
 import React from 'react'
-import { routes } from 'utils'
+import { adminOnly, routes } from 'utils'
 import { Container, Row, Col } from 'grid'
 import { NavLink, NavTabs } from 'nav'
 
@@ -63,21 +63,23 @@ const HEADER = (
   </Container>
 )
 
-const Settings = routes('servers', {
-  acls: Acls,
-  'cloud-configs': CloudConfigs,
-  config: Config,
-  groups: Groups,
-  ips: Ips,
-  logs: Logs,
-  plugins: Plugins,
-  remotes: Remotes,
-  servers: Servers,
-  users: Users,
-})(({ children }) => (
-  <Page header={HEADER} title='settingsPage' formatTitle>
-    {children}
-  </Page>
-))
+const Settings = adminOnly(
+  routes('servers', {
+    acls: Acls,
+    'cloud-configs': CloudConfigs,
+    config: Config,
+    groups: Groups,
+    ips: Ips,
+    logs: Logs,
+    plugins: Plugins,
+    remotes: Remotes,
+    servers: Servers,
+    users: Users,
+  })(({ children }) => (
+    <Page header={HEADER} title='settingsPage' formatTitle>
+      {children}
+    </Page>
+  ))
+)
 
 export default Settings

packages/xo-web/src/xo-app/new/sr/index.js
@@ -15,7 +15,7 @@ import store from 'store'
 import trim from 'lodash/trim'
 import Wizard, { Section } from 'wizard'
 import { confirm } from 'modal'
-import { connectStore, formatSize } from 'utils'
+import { adminOnly, connectStore, formatSize } from 'utils'
 import { Container, Row, Col } from 'grid'
 import { injectIntl } from 'react-intl'
 import { Password, Select, Toggle } from 'form'
@@ -216,6 +216,7 @@ const getSrPath = id => (id !== undefined ? `/srs/${id}` : undefined)
 
 // ===================================================================
 
+@adminOnly
 @injectIntl
 @connectStore(() => ({
   hosts: createGetObjectsOfType('host'),

packages/xo-web/src/xo-app/self/index.js
@@ -25,7 +25,12 @@ import Upgrade from 'xoa-upgrade'
 import { Container, Row, Col } from 'grid'
 import { injectIntl } from 'react-intl'
 import { SizeInput } from 'form'
-import { addSubscriptions, connectStore, resolveIds } from 'utils'
+import {
+  addSubscriptions,
+  adminOnly,
+  connectStore,
+  resolveIds
+} from 'utils'
 import {
   createGetObjectsOfType,
   createSelector,
@@ -700,6 +705,7 @@ class ResourceSet extends Component {
 
 const compareName = (a, b) => (a.name < b.name ? -1 : 1)
 
+@adminOnly
 @addSubscriptions({ resourceSets: subscribeResourceSets })
 @connectStore({ resolvedResourceSets: getResolvedResourceSets })
 export default class Self extends Component {

packages/xo-web/src/xo-app/xoa/licenses/index.js
@@ -6,7 +6,13 @@ import Link from 'link'
 import React from 'react'
 import renderXoItem from 'render-xo-item'
 import SortedTable from 'sorted-table'
-import { addSubscriptions, connectStore, getXoaPlan, ShortDate } from 'utils'
+import {
+  addSubscriptions,
+  adminOnly,
+  connectStore,
+  getXoaPlan,
+  ShortDate,
+} from 'utils'
 import { Container, Row, Col } from 'grid'
 import { createSelector, createGetObjectsOfType } from 'selectors'
 import { find, forEach } from 'lodash'
@@ -77,6 +83,7 @@ const getBoundXosanRenderer = (boundObjectId, xosanSrs) => {
   return () => <Link to={`srs/${sr.id}`}>{renderXoItem(sr)}</Link>
 }
 
+@adminOnly
 @connectStore({
   xosanSrs: createGetObjectsOfType('SR').filter([
     ({ SR_type }) => SR_type === 'xosan', // eslint-disable-line camelcase

packages/xo-web/src/xo-app/xoa/update/index.js
@@ -9,7 +9,7 @@ import Icon from 'icon'
 import React from 'react'
 import Tooltip from 'tooltip'
 import xoaUpdater, { exposeTrial, isTrialRunning } from 'xoa-updater'
-import { addSubscriptions, connectStore } from 'utils'
+import { addSubscriptions, adminOnly, connectStore } from 'utils'
 import { Card, CardBlock, CardHeader } from 'card'
 import { confirm } from 'modal'
 import { Container, Row, Col } from 'grid'
@@ -69,6 +69,7 @@ const helper = (obj1, obj2, prop) =>
   defined(() => obj1[prop], () => obj2[prop], '')
 
 const Updates = decorate([
+  adminOnly,
   addSubscriptions({
     backupNgJobs: subscribeBackupNgJobs,
     jobs: subscribeJobs,

commit 465c3973ddb602a51c2431d2cdef668e2adbe6cc

fix(xo-web): adminOnly breaks the routes
Introduced by 59e68682bde8ea4445182bbd24a5c4fba83a6384

packages/xo-web/src/xo-app/backup-ng/index.js
@@ -420,18 +420,18 @@ const ChooseBackupType = () => (
   </Container>
 )
 
-export default adminOnly(
-  routes('overview', {
-    ':id/edit': Edit,
-    new: ChooseBackupType,
-    'new/vms': NewVmBackup,
-    'new/metadata': NewMetadataBackup,
-    overview: Overview,
-    restore: Restore,
-    'restore/metadata': RestoreMetadata,
-    'file-restore': FileRestore,
-    health: Health,
-  })(({ children }) => (
+export default routes('overview', {
+  ':id/edit': Edit,
+  new: ChooseBackupType,
+  'new/vms': NewVmBackup,
+  'new/metadata': NewMetadataBackup,
+  overview: Overview,
+  restore: Restore,
+  'restore/metadata': RestoreMetadata,
+  'file-restore': FileRestore,
+  health: Health,
+})(
+  adminOnly(({ children }) => (
     <Page header={HEADER} title='backupPage' formatTitle>
       {children}
     </Page>

packages/xo-web/src/xo-app/backup/index.js
@@ -62,14 +62,14 @@ const HEADER = (
   </Container>
 )
 
-const Backup = adminOnly(
-  routes('overview', {
-    ':id/edit': Edit,
-    new: DEVELOPMENT ? New : DeprecatedMsg,
-    overview: Overview,
-    restore: MovingRestoreMessage,
-    'file-restore': MovingFileRestoreMessage,
-  })(({ children }) => (
+const Backup = routes('overview', {
+  ':id/edit': Edit,
+  new: DEVELOPMENT ? New : DeprecatedMsg,
+  overview: Overview,
+  restore: MovingRestoreMessage,
+  'file-restore': MovingFileRestoreMessage,
+})(
+  adminOnly(({ children }) => (
     <Page header={HEADER} title='backupPage' formatTitle>
       {children}
     </Page>

packages/xo-web/src/xo-app/jobs/index.js
@@ -37,14 +37,14 @@ const HEADER = (
   </Container>
 )
 
-const Jobs = adminOnly(
-  routes('overview', {
-    ':id/edit': Edit,
-    new: New,
-    overview: Overview,
-    schedules: Schedules,
-    'schedules/:id/edit': EditSchedule,
-  })(({ children }) => (
+const Jobs = routes('overview', {
+  ':id/edit': Edit,
+  new: New,
+  overview: Overview,
+  schedules: Schedules,
+  'schedules/:id/edit': EditSchedule,
+})(
+  adminOnly(({ children }) => (
     <Page header={HEADER} title='jobsPage' formatTitle>
       {children}
     </Page>

packages/xo-web/src/xo-app/settings/index.js
@@ -63,19 +63,19 @@ const HEADER = (
   </Container>
 )
 
-const Settings = adminOnly(
-  routes('servers', {
-    acls: Acls,
-    'cloud-configs': CloudConfigs,
-    config: Config,
-    groups: Groups,
-    ips: Ips,
-    logs: Logs,
-    plugins: Plugins,
-    remotes: Remotes,
-    servers: Servers,
-    users: Users,
-  })(({ children }) => (
+const Settings = routes('servers', {
+  acls: Acls,
+  'cloud-configs': CloudConfigs,
+  config: Config,
+  groups: Groups,
+  ips: Ips,
+  logs: Logs,
+  plugins: Plugins,
+  remotes: Remotes,
+  servers: Servers,
+  users: Users,
+})(
+  adminOnly(({ children }) => (
     <Page header={HEADER} title='settingsPage' formatTitle>
       {children}
     </Page>