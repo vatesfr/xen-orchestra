4e3da99ef96bba7d6b325d56f5ada16b44af0951
badrAZ <azizbibadr@gmail.com>
2019-05-22 14:50:51 +0200

    feat(proxy): ability to run a backup throught a proxy
    Fixes #1201
    
    [xo-proxy specification](https://github.com/vatesfr/xen-orchestra/blob/xo-proxy/@xen-orchestra/proxy/docs/api.md)
    
    - [ ] proxy API (b8c16a97b0d9ee80c9bea41bb023e766f84c9ac4)
    - [ ] update remote create/edit API and UI (c72084227afc63d99b9b895e28dda8ada8e0dbb5)
    - [ ] update backup NG create/edit API and UI (f75ecb97355c356723694b7edf753c896774224f)
    - [ ] update metadata backup create/edit API and UI (653477b17078e7efc1773bee8d6df1ceece32648)
    - [ ] ensure all remote API methods are working via the proxy

---
+++ packages/xo-server/src/xo-mixins/proxies.js
@@ -0,0 +1,74 @@
+import synchronized from 'decorator-synchronized'
+import { noSuchObject } from 'xo-common/api-errors'
+
+import Collection from '../collection/redis'
+import patch from '../patch'
+
+const sync = synchronized.withKey((arg1, arg2) => arg1.address ?? arg2.address)
+
+export default class Proxy {
+  constructor(app) {
+    const db = (this._db = new Collection({
+      connection: app._redis,
+      indexes: ['address'],
+      prefix: 'xo:proxy',
+    }))
+
+    app.on('clean', () => db.rebuildIndexes())
+    app.on('start', () =>
+      app.addConfigManager(
+        'proxies',
+        () => db.get(),
+        proxies => db.update(proxies)
+      )
+    )
+  }
+
+  async _throwIfRegistered(address) {
+    if (await this._db.exists({ address })) {
+      throw new Error(
+        `A proxy with the address (${address}) is already registered`
+      )
+    }
+  }
+
+  @sync
+  async registerProxy({ address, authenticationToken, name }) {
+    await this._throwIfRegistered(address)
+
+    const { properties } = await this._db.add({
+      address,
+      authenticationToken,
+      name,
+    })
+    return properties
+  }
+
+  unregisterProxy(id) {
+    return this._db.remove(id)
+  }
+
+  async getProxy(id) {
+    const proxy = await this._db.first(id)
+    if (proxy === undefined) {
+      throw noSuchObject(id, 'proxy')
+    }
+    return proxy.properties
+  }
+
+  getAllProxies() {
+    return this._db.get()
+  }
+
+  @sync
+  async updateProxy(id, { address, authenticationToken, name }) {
+    const proxy = await this.getProxy(id)
+    if (address != null && proxy.address !== address) {
+      await this._throwIfRegistered(address)
+    }
+
+    patch(proxy, { address, authenticationToken, name })
+    const { properties } = await this._db.update(proxy)
+    return properties
+  }
+}
