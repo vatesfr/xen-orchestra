c4b0831b5ca7e11c2a94b8d459ada5cc46852ca6
Mohamedox <ghemid_m_dz@yahoo.fr>
2019-05-29 16:49:23 +0200

    fix

--- packages/xo-web/src/xo-app/home/index.js
+++ packages/xo-web/src/xo-app/home/index.js
@@ -179,11 +179,6 @@ const OPTIONS = {
         labelId: 'snapshotVmLabel',
       },
       {
-        handler: snapshotVms,
-        icon: 'backup',
-        labelId: 'backupLabel',
-      },
-      {
         handler: deleteVms,
         icon: 'vm-delete',
         labelId: 'vmRemoveButton',
