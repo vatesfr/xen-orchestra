8e73dd40e1a3f53ee5522c2a4da35fb2b1633c0b
Julien Fontanet <julien.fontanet@isonoe.net>
2019-07-08 16:51:33 +0200

    feat(proxy): dynamic mixin construction (#4338)

--- @xen-orchestra/proxy/src/main/index.js
+++ @xen-orchestra/proxy/src/main/index.js
@@ -1,13 +1,43 @@
-import { camelCase } from 'lodash'
+import camelCase from 'lodash/camelCase'
 
 import mixins from './mixins'
 
+const { defineProperties, defineProperty, keys } = Object
+const noop = Function.prototype
+
+const MIXIN_CYCLIC_DESCRIPTOR = {
+  configurable: true,
+  get() {
+    throw new Error('cyclic dependency')
+  },
+}
+
 export default class App {
   constructor(opts) {
-    Object.keys(mixins).forEach(mixin => {
-      Object.defineProperty(this, camelCase(mixin), {
-        value: new mixins[mixin](this, opts),
-      })
+    // add lazy property for each of the mixin, this allows mixins to depend on
+    // one another without any special ordering
+    const descriptors = {}
+    keys(mixins).forEach(name => {
+      const Mixin = mixins[name]
+      name = camelCase(name)
+
+      descriptors[name] = {
+        configurable: true,
+        get: () => {
+          defineProperty(this, name, MIXIN_CYCLIC_DESCRIPTOR)
+          const instance = new Mixin(this, opts)
+          defineProperty(this, name, {
+            value: instance,
+          })
+          return instance
+        },
+      }
+    })
+    defineProperties(this, descriptors)
+
+    // access all mixin properties to trigger their creation
+    keys(descriptors).forEach(name => {
+      noop(this[name])
     })
   }
 }
