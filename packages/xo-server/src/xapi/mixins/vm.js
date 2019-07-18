vatesfr/xen-orchestra

commit 3196c7ca09d212330058716af5d17c11260d1355

chore(xo-server): use Xapi's setters (#4229)

packages/xo-server/src/xapi/mixins/vm.js
@@ -115,9 +115,7 @@ export default {
             order = 'ncd'
           }
 
-          this._setObjectProperties(vm, {
-            HVM_boot_params: { ...bootParams, order },
-          })
+          vm.set_HVM_boot_params({ ...bootParams, order })
         }
       } else {
         // PV
@@ -263,13 +261,8 @@ export default {
   _editVm: makeEditObject({
     affinityHost: {
       get: 'affinity',
-      set(value, vm) {
-        return this._setObjectProperty(
-          vm,
-          'affinity',
-          value ? this.getObject(value).$ref : NULL_REF
-        )
-      },
+      set: (value, vm) =>
+        vm.set_affinity(value ? this.getObject(value).$ref : NULL_REF),
     },
 
     autoPoweron: {
@@ -289,14 +282,13 @@ export default {
         if (virtualizationMode !== 'pv' && virtualizationMode !== 'hvm') {
           throw new Error(`The virtualization mode must be 'pv' or 'hvm'`)
         }
-        return this._set('domain_type', virtualizationMode)::pCatch(
-          { code: 'MESSAGE_METHOD_UNKNOWN' },
-          () =>
-            this._set(
-              'HVM_boot_policy',
+        return vm
+          .set_domain_type(virtualizationMode)
+          ::pCatch({ code: 'MESSAGE_METHOD_UNKNOWN' }, () =>
+            vm.set_HVM_boot_policy(
               virtualizationMode === 'hvm' ? 'Boot order' : ''
             )
-        )
+          )
       },
     },
 
@@ -320,9 +312,7 @@ export default {
       get: vm => +vm.VCPUs_at_startup,
       set: [
         'VCPUs_at_startup',
-        function(value, vm) {
-          return isVmRunning(vm) && this._set('VCPUs_number_live', value)
-        },
+        (value, vm) => isVmRunning(vm) && vm.set_VCPUs_number_live(value),
       ],
     },
 
@@ -355,9 +345,7 @@ export default {
     },
 
     highAvailability: {
-      set(ha, vm) {
-        return this.call('VM.set_ha_restart_priority', vm.$ref, ha)
-      },
+      set: (ha, vm) => vm.set_ha_restart_priority(ha),
     },
 
     memoryMin: {
@@ -461,9 +449,7 @@ export default {
 
     startDelay: {
       get: vm => +vm.start_delay,
-      set(startDelay, vm) {
-        return this.call('VM.set_start_delay', vm.$ref, startDelay)
-      },
+      set: (startDelay, vm) => vm.set_start_delay(startDelay),
     },
   }),
 

packages/xo-server/src/api/vm.js
@@ -731,7 +731,7 @@ export async function convertToTemplate({ vm }) {
   // Convert to a template requires pool admin permission.
   await this.checkPermissions(this.user.id, [[vm.$pool, 'administrate']])
 
-  await this.getXapi(vm).call('VM.set_is_a_template', vm._xapiRef, true)
+  await this.getXapiObject(vm).set_is_a_template(true)
 }
 
 convertToTemplate.params = {
@@ -1412,15 +1412,11 @@ stats.resolve = {
 // -------------------------------------------------------------------
 
 export async function setBootOrder({ vm, order }) {
-  const xapi = this.getXapi(vm)
-
-  order = { order }
-  if (vm.virtualizationMode === 'hvm') {
-    await xapi.call('VM.set_HVM_boot_params', vm._xapiRef, order)
-    return
+  if (vm.virtualizationMode !== 'hvm') {
+    throw invalidParameters('You can only set the boot order on a HVM guest')
   }
 
-  throw invalidParameters('You can only set the boot order on a HVM guest')
+  await this.getXapiObject(vm).set_HVM_boot_params({ order })
 }
 
 setBootOrder.params = {

packages/xo-server/src/api/vbd.js
@@ -48,8 +48,7 @@ connect.resolve = {
 
 export async function set({ position, vbd }) {
   if (position !== undefined) {
-    const xapi = this.getXapi(vbd)
-    await xapi.call('VBD.set_userdevice', vbd._xapiRef, String(position))
+    await this.getXapiObject(vbd).set_userdevice(String(position))
   }
 }
 
@@ -67,9 +66,7 @@ set.resolve = {
 // -------------------------------------------------------------------
 
 export async function setBootable({ vbd, bootable }) {
-  const xapi = this.getXapi(vbd)
-
-  await xapi.call('VBD.set_bootable', vbd._xapiRef, bootable)
+  await this.getXapiObject(vbd).set_bootable(bootable)
 }
 
 setBootable.params = {

packages/xo-server/src/api/xosan.js
@@ -1132,7 +1132,7 @@ async function _prepareGlusterVm(
     name_description: 'Xosan VM storage',
     memory: memorySize,
   })
-  await xapi.call('VM.set_xenstore_data', newVM.$ref, xenstoreData)
+  await newVM.set_xenstore_data(xenstoreData)
   const rootDisk = newVM.$VBDs
     .map(vbd => vbd && vbd.$VDI)
     .find(vdi => vdi && vdi.name_label === 'xosan_root')

packages/xo-server/src/xapi/index.js
@@ -229,14 +229,6 @@ export default class Xapi extends XapiBase {
 
   // =================================================================
 
-  _setObjectProperty(object, name, value) {
-    return this.call(
-      `${object.$type}.set_${camelToSnakeCase(name)}`,
-      object.$ref,
-      prepareXapiParam(value)
-    )
-  }
-
   _setObjectProperties(object, props) {
     const { $ref: ref, $type: type } = object
 
@@ -256,30 +248,32 @@ export default class Xapi extends XapiBase {
   }
 
   async setHostProperties(id, { nameLabel, nameDescription }) {
-    await this._setObjectProperties(this.getObject(id), {
-      nameLabel,
-      nameDescription,
-    })
+    const host = this.getObject(id)
+    await Promise.all([
+      nameDescription !== undefined &&
+        host.set_name_description(nameDescription),
+      nameLabel !== undefined && host.set_name_label(nameLabel),
+    ])
   }
 
   async setPoolProperties({ autoPoweron, nameLabel, nameDescription }) {
     const { pool } = this
 
     await Promise.all([
-      this._setObjectProperties(pool, {
-        nameLabel,
-        nameDescription,
-      }),
+      nameDescription !== undefined &&
+        pool.set_name_description(nameDescription),
+      nameLabel !== undefined && pool.set_name_label(nameLabel),
       autoPoweron != null &&
         pool.update_other_config('autoPoweron', autoPoweron ? 'true' : null),
     ])
   }
 
   async setSrProperties(id, { nameLabel, nameDescription }) {
-    await this._setObjectProperties(this.getObject(id), {
-      nameLabel,
-      nameDescription,
-    })
+    const sr = this.getObject(id)
+    await Promise.all([
+      nameDescription !== undefined && sr.set_name_description(nameDescription),
+      nameLabel !== undefined && sr.set_name_label(nameLabel),
+    ])
   }
 
   async setNetworkProperties(
@@ -292,11 +286,11 @@ export default class Xapi extends XapiBase {
     }
     const network = this.getObject(id)
     await Promise.all([
-      this._setObjectProperties(network, {
-        defaultLockingMode,
-        nameDescription,
-        nameLabel,
-      }),
+      defaultLockingMode !== undefined &&
+        network.set_default_locking_mode(defaultLockingMode),
+      nameDescription !== undefined &&
+        network.set_name_description(nameDescription),
+      nameLabel !== undefined && network.set_name_label(nameLabel),
       network.update_other_config(
         'automatic',
         automatic === undefined ? undefined : automatic ? 'true' : null
@@ -320,10 +314,8 @@ export default class Xapi extends XapiBase {
 
   // =================================================================
 
-  async setDefaultSr(srId) {
-    this._setObjectProperties(this.pool, {
-      default_SR: this.getObject(srId).$ref,
-    })
+  setDefaultSr(srId) {
+    return this.pool.set_default_SR(this.getObject(srId).$ref)
   }
 
   // =================================================================
@@ -449,7 +441,7 @@ export default class Xapi extends XapiBase {
 
   async setRemoteSyslogHost(hostId, syslogDestination) {
     const host = this.getObject(hostId)
-    await this.call('host.set_logging', host.$ref, {
+    await host.set_logging({
       syslog_destination: syslogDestination,
     })
     await this.call('host.syslog_reconfigure', host.$ref)
@@ -544,12 +536,7 @@ export default class Xapi extends XapiBase {
     stream = stream.pipe(sizeStream)
 
     const onVmCreation =
-      nameLabel !== undefined
-        ? vm =>
-            targetXapi._setObjectProperties(vm, {
-              nameLabel,
-            })
-        : null
+      nameLabel !== undefined ? vm => vm.set_name_label(nameLabel) : null
 
     const vm = await targetXapi._getOrWaitObject(
       await targetXapi._importVm(stream, sr, onVmCreation)
@@ -693,7 +680,7 @@ export default class Xapi extends XapiBase {
     }
 
     await Promise.all([
-      this.call('VM.set_is_a_template', vm.$ref, false),
+      vm.set_is_a_template(false),
       vm.update_blocked_operations('destroy', null),
       vm.update_other_config('default_template', null),
     ])
@@ -1040,9 +1027,7 @@ export default class Xapi extends XapiBase {
     $defer.onFailure(() => this._deleteVm(vm))
 
     await Promise.all([
-      this._setObjectProperties(vm, {
-        name_label: `[Importing…] ${name_label}`,
-      }),
+      vm.set_name_label(`[Importing…] ${name_label}`),
       vm.update_blocked_operations('start', 'Importing…'),
       vm.update_other_config(TAG_COPY_SRC, delta.vm.uuid),
     ])
@@ -1163,9 +1148,7 @@ export default class Xapi extends XapiBase {
     }
 
     await Promise.all([
-      this._setObjectProperties(vm, {
-        name_label,
-      }),
+      vm.set_name_label(name_label),
       // FIXME: move
       vm.update_blocked_operations(
         'start',
@@ -1400,9 +1383,7 @@ export default class Xapi extends XapiBase {
         'start',
         'OVA import in progress...'
       ),
-      this._setObjectProperties(vm, {
-        name_label: `[Importing...] ${nameLabel}`,
-      }),
+      vm.set_name_label(`[Importing...] ${nameLabel}`),
     ])
 
     // 2. Create VDIs & Vifs.
@@ -1463,7 +1444,7 @@ export default class Xapi extends XapiBase {
     // Enable start and restore the VM name label after import.
     await Promise.all([
       this.removeForbiddenOperationFromVm(vm.$id, 'start'),
-      this._setObjectProperties(vm, { name_label: nameLabel }),
+      vm.set_name_label(nameLabel),
     ])
     return vm
   }
@@ -1672,19 +1653,13 @@ export default class Xapi extends XapiBase {
 
         const cdDrive = this._getVmCdDrive(vm)
         forEach(vm.$VBDs, vbd => {
-          promises.push(
-            this._setObjectProperties(vbd, {
-              bootable: vbd === cdDrive,
-            })
-          )
+          promises.push(vbd.set_bootable(vbd === cdDrive))
 
           bootables.push([vbd, Boolean(vbd.bootable)])
         })
 
         promises.push(
-          this._setObjectProperties(vm, {
-            PV_bootloader: 'eliloader',
-          }),
+          vm.set_PV_bootloader('eliloader'),
           vm.update_other_config({
             'install-distro':
               template && template.other_config['install-distro'],
@@ -1696,12 +1671,10 @@ export default class Xapi extends XapiBase {
 
         await this._startVm(vm)
       } finally {
-        this._setObjectProperties(vm, {
-          PV_bootloader: bootloader,
-        })::ignoreErrors()
+        vm.set_PV_bootloader(bootloader)::ignoreErrors()
 
         forEach(bootables, ([vbd, bootable]) => {
-          this._setObjectProperties(vbd, { bootable })::ignoreErrors()
+          vbd.set_bootable(bootable)::ignoreErrors()
         })
       }
     }
@@ -1929,7 +1902,7 @@ export default class Xapi extends XapiBase {
       }
 
       if (bootable !== Boolean(cdDrive.bootable)) {
-        await this._setObjectProperties(cdDrive, { bootable })
+        await cdDrive.set_bootable(bootable)
       }
     } else {
       await this.createVbd({
@@ -1951,7 +1924,7 @@ export default class Xapi extends XapiBase {
       await this.call('VBD.unplug_force', vbd.$ref)
     } catch (error) {
       if (error.code === 'VBD_NOT_UNPLUGGABLE') {
-        await this.call('VBD.set_unpluggable', vbd.$ref, true)
+        await vbd.set_unpluggable(true)
         return this.call('VBD.unplug_force', vbd.$ref)
       }
     }
@@ -2006,7 +1979,7 @@ export default class Xapi extends XapiBase {
     )
 
     if (nameLabel) {
-      await this.call('VDI.set_name_label', snap.$ref, nameLabel)
+      await snap.set_name_label(nameLabel)
     }
 
     return snap

packages/xo-server/src/xapi/mixins/networking.js
@@ -37,7 +37,7 @@ export default {
               : 'locked'
 
           if (lockingMode !== vif.locking_mode) {
-            return this._set('locking_mode', lockingMode)
+            return vif.set_locking_mode(lockingMode)
           }
         },
       ],
@@ -53,7 +53,7 @@ export default {
               : 'locked'
 
           if (lockingMode !== vif.locking_mode) {
-            return this._set('locking_mode', lockingMode)
+            return vif.set_locking_mode(lockingMode)
           }
         },
       ],

packages/xo-server/src/xapi/utils.js
@@ -148,8 +148,8 @@ export const makeEditObject = specs => {
 
     if (set === true) {
       const prop = camelToSnakeCase(name)
-      return function(value) {
-        return this._set(prop, value)
+      return function(value, obj) {
+        return this.setField(obj.$type, obj.$ref, prop, value)
       }
     }
 
@@ -157,8 +157,8 @@ export const makeEditObject = specs => {
       const index = set.indexOf('.')
       if (index === -1) {
         const prop = camelToSnakeCase(set)
-        return function(value) {
-          return this._set(prop, value)
+        return function(value, obj) {
+          return this.setField(obj.$type, obj.$ref, prop, value)
         }
       }
 
@@ -255,16 +255,6 @@ export const makeEditObject = specs => {
     const limits = checkLimits && {}
     const object = this.getObject(id)
 
-    const _objectRef = object.$ref
-    const _setMethodPrefix = `${object.$type}.set_`
-
-    // Context used to execute functions.
-    const context = {
-      __proto__: this,
-      _set: (prop, value) =>
-        this.call(_setMethodPrefix + prop, _objectRef, prepareXapiParam(value)),
-    }
-
     const set = (value, name) => {
       if (value === undefined) {
         return
@@ -293,7 +283,7 @@ export const makeEditObject = specs => {
         }
       }
 
-      const cb = () => spec.set.call(context, value, object)
+      const cb = () => spec.set.call(this, value, object)
 
       const { constraints } = spec
       if (constraints) {

packages/xo-server/src/xo-mixins/backups-ng/index.js
@@ -449,9 +449,7 @@ const disableVmHighAvailability = async (xapi: Xapi, vm: Vm) => {
   }
 
   return Promise.all([
-    xapi._setObjectProperties(vm, {
-      haRestartPriority: '',
-    }),
+    vm.set_ha_restart_priority(''),
     xapi.addTag(vm.$ref, 'HA disabled'),
   ])
 }
@@ -1258,11 +1256,11 @@ export default class BackupNg {
                       result: () => ({ size: xva.size }),
                     },
                     xapi._importVm($cancelToken, fork, sr, vm =>
-                      xapi._setObjectProperties(vm, {
-                        nameLabel: `${metadata.vm.name_label} - ${
+                      vm.set_name_label(
+                        `${metadata.vm.name_label} - ${
                           job.name
-                        } - (${safeDateFormat(metadata.timestamp)})`,
-                      })
+                        } - (${safeDateFormat(metadata.timestamp)})`
+                      )
                     )
                   )
                 )

commit a721d559665dbe99942bb8ebc91cb60408ea1615

fix(xo-server/vm.set): setting VCPUs_live
Caused by 3196c7ca09d212330058716af5d17c11260d1355

Fixes xoa-support#1632

packages/xo-server/src/xapi/mixins/vm.js
@@ -306,7 +306,9 @@ export default {
       get: vm => +vm.VCPUs_at_startup,
       set: [
         'VCPUs_at_startup',
-        (value, vm) => isVmRunning(vm) && vm.set_VCPUs_number_live(value),
+        (value, vm) =>
+          isVmRunning(vm) &&
+          vm.$xapi.call('VM.set_VCPUs_number_live', vm.$ref, String(value)),
       ],
     },
 