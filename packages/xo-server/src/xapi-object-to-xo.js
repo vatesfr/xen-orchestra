1b26dd48cf552aff9c23c95614c276e0ac1c69b4
badrAZ <azizbibadr@gmail.com>
2019-08-27 11:50:58 +0200

    use sensitive values

--- packages/xo-server/src/xapi-object-to-xo.js
+++ packages/xo-server/src/xapi-object-to-xo.js
@@ -1,3 +1,4 @@
+import * as sensitiveValues from './sensitive-values'
 import ensureArray from './_ensureArray'
 import {
   extractProperty,
@@ -479,20 +480,16 @@ const TRANSFORMS = {
   // -----------------------------------------------------------------
 
   pbd(obj) {
-    const deviceConfig = obj.device_config
     return {
       type: 'PBD',
 
       attached: Boolean(obj.currently_attached),
       host: link(obj, 'host'),
       SR: link(obj, 'SR'),
-      device_config:
-        deviceConfig.cifspassword !== undefined
-          ? {
-              ...deviceConfig,
-              cifspassword: '* obfuscated *',
-            }
-          : deviceConfig,
+      deviceConfig: sensitiveValues.replace(
+        obj.device_config,
+        '* obfuscated *'
+      ),
       otherConfig: obj.other_config,
     }
   },
