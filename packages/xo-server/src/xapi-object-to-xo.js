vatesfr/xen-orchestra

commit 77c62d6e7d8de35f4d0b71b39678b4a17d5d4a63

fix(xo-server/xapi-object-to-xo): obfuscate cifs password from PBD device_config (#4401)
Fixes #4384

packages/xo-server/src/xapi-object-to-xo.js
@@ -1,3 +1,4 @@
+import * as sensitiveValues from './sensitive-values'
 import ensureArray from './_ensureArray'
 import {
   extractProperty,
@@ -485,7 +486,10 @@ const TRANSFORMS = {
       attached: Boolean(obj.currently_attached),
       host: link(obj, 'host'),
       SR: link(obj, 'SR'),
-      device_config: obj.device_config,
+      deviceConfig: sensitiveValues.replace(
+        obj.device_config,
+        '* obfuscated *'
+      ),
       otherConfig: obj.other_config,
     }
   },

packages/xo-server/src/sensitive-values.js
@@ -26,7 +26,12 @@ export const merge = (newValue, oldValue) => {
 
 export const obfuscate = value => replace(value, OBFUSCATED_VALUE)
 
-const SENSITIVE_PARAMS = { __proto__: null, password: true, token: true }
+const SENSITIVE_PARAMS = {
+  __proto__: null,
+  cifspassword: true,
+  password: true,
+  token: true,
+}
 
 export function replace(value, replacement) {
   function helper(value, name) {

commit ea6d12a614923809482d3784d096d713c4e11e82

fix(xo-server/xapi-object-to-xo): fix incorrect PBD entry name
Introduced by 77c62d6e7d8de35f4d0b71b39678b4a17d5d4a63

packages/xo-server/src/xapi-object-to-xo.js
@@ -486,7 +486,7 @@ const TRANSFORMS = {
       attached: Boolean(obj.currently_attached),
       host: link(obj, 'host'),
       SR: link(obj, 'SR'),
-      deviceConfig: sensitiveValues.replace(
+      device_config: sensitiveValues.replace(
         obj.device_config,
         '* obfuscated *'
       ),