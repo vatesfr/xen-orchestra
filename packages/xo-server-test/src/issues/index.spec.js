03c0894f7c68279083b81e3102bb9fe203385da8
badrAZ <azizbibadr@gmail.com>
2019-09-12 10:42:10 +0200

    issues folder

---
+++ packages/xo-server-test/src/issues/index.spec.js
@@ -0,0 +1,27 @@
+/* eslint-env jest */
+
+import config from '../_config'
+import xo from '../_xoConnection'
+
+describe('issues', () => {
+  test('4507', async () => {
+    await xo.createTempServer(config.servers.default)
+
+    const props = {
+      coresPerSocket: 1,
+      cpuCap: 1,
+    }
+    const vm = await xo.createTempVm(props)
+    expect(vm).toMatchObject(props)
+
+    await xo.call('vm.set', {
+      coresPerSocket: null,
+      cpuCap: null,
+      id: vm.id,
+    })
+    await xo.waitObjectState(vm.id, vm => {
+      expect(vm.coresPerSocket).toBe(undefined)
+      expect(vm.cpuCap).toBe(undefined)
+    })
+  })
+})
