da060cbe928495ab207865ac562037bcd33755ff
badrAZ <azizbibadr@gmail.com>
2019-09-12 09:34:22 +0200

    improvements

--- packages/xo-server-test/src/vm/index.spec.js
+++ packages/xo-server-test/src/vm/index.spec.js
@@ -1,33 +1,33 @@
 /* eslint-env jest */
 
-import { mapValues } from 'lodash'
-
 import config from '../_config'
 import xo from '../_xoConnection'
 
 describe('vm', () => {
   describe('.set() :', () => {
-    const PROPERTIES_TO_SET_TO_NULL = {
-      coresPerSocket: 1,
-      cpuCap: 1,
-    }
     it('set properties to null', async () => {
-      const id = await xo.createTempVm({
+      await xo.createTempServer(config.servers.default)
+      const vm = await xo.createTempVm({
+        coresPerSocket: 1,
+        cpuCap: 1,
         name_label: 'XO Test',
-        template: config.templates.default,
-        ...PROPERTIES_TO_SET_TO_NULL,
+        template: config.templates.templateWithoutDisks,
       })
-      expect(xo.objects.all[id]).toMatchObject(PROPERTIES_TO_SET_TO_NULL)
+
+      expect(vm).toMatchObject({
+        coresPerSocket: 1,
+        cpuCap: 1,
+      })
 
       await xo.call('vm.set', {
-        id,
-        ...mapValues(PROPERTIES_TO_SET_TO_NULL, _ => null),
+        coresPerSocket: null,
+        cpuCap: null,
+        id: vm.id,
       })
-      await xo.waitObjectState(id, vm => {
-        Object.keys(PROPERTIES_TO_SET_TO_NULL).forEach(prop => {
-          expect(vm[prop]).toBe(undefined)
-        })
+      await xo.waitObjectState(vm.id, vm => {
+        expect(vm.coresPerSocket).toBe(undefined)
+        expect(vm.cpuCap).toBe(undefined)
       })
-    }, 6e3)
+    })
   })
 })
