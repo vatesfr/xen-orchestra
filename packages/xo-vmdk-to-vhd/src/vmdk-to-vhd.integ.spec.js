4dfee3e2566a4ee73a3f14f6602767e48406ae5a
Nicolas Raynaud <github@nraynaud.com>
2019-09-25 15:33:35 +0200

    fix execa use in tests.

--- packages/xo-vmdk-to-vhd/src/vmdk-to-vhd.integ.spec.js
+++ packages/xo-vmdk-to-vhd/src/vmdk-to-vhd.integ.spec.js
@@ -51,14 +51,18 @@ test('VMDK to VHD can convert a random data file with VMDKDirectParser', async (
   const reconvertedFromVmdk = 'from-vhd-by-vbox.raw'
   const dataSize = 100 * 1024 * 1024 // this number is an integer head/cylinder/count equation solution
   try {
-    await execa.shell(
-      'base64 /dev/urandom | head -c ' + dataSize + ' > ' + inputRawFileName
+    await execa(
+      'base64 /dev/urandom | head -c ' + dataSize + ' > ' + inputRawFileName,
+      [],
+      { shell: true }
     )
-    await execa.shell(
+    await execa(
       'python /usr/share/pyshared/VMDKstream.py ' +
         inputRawFileName +
         ' ' +
-        vmdkFileName
+        vmdkFileName,
+      [],
+      { shell: true }
     )
     const result = await readVmdkGrainTable(createFileAccessor(vmdkFileName))
     const pipe = (
