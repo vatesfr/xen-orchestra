6908f579d9d4bec6df193bc417caa0121dbac7cf
BARHTAOUI <rajaa.barhtaoui@gmail.com>
2019-10-04 16:33:29 +0200

    server side fixed

--- packages/xo-server/src/api/xoa.js
+++ packages/xo-server/src/api/xoa.js
@@ -1,11 +1,7 @@
 import execa from 'execa'
 
-export async function getXoaCheck() {
-  let stdout
-  try {
-    stdout = await execa.stdout('xoa', ['check'])
-  } catch (err) {
-    return err
-  }
-  return stdout
+export async function check() {
+  return execa
+    .stdout('xoa', ['check'], { env: { FORCE_COLOR: true } })
+    .catch(err => err.stderr.concat(err.stdout))
 }
