b3daef0c26479f414290d4e6062f321d83345e1a
BARHTAOUI <rajaa.barhtaoui@gmail.com>
2019-10-09 10:39:34 +0200

    remove api/xoa

--- packages/xo-server/src/api/xoa.js
+++
@@ -1,7 +0,0 @@
-import execa from 'execa'
-
-export async function check() {
-  return execa
-    .stdout('xoa', ['check'], { env: { FORCE_COLOR: true } })
-    .catch(err => err.stderr.concat(err.stdout))
-}
