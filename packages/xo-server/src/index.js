vatesfr/xen-orchestra

commit 21901f2a75719013ed2f7c47a0130edf58c5a6ef

chore: dont wrap unnecessary with `fromCallback`

packages/xo-server/src/index.js
@@ -467,7 +467,7 @@ const setUpProxies = (express, opts, xo) => {
   const webSocketServer = new WebSocket.Server({
     noServer: true,
   })
-  xo.on('stop', () => fromCallback(cb => webSocketServer.close(cb)))
+  xo.on('stop', fromCallback.call(webSocketServer, 'close'))
 
   express.on('upgrade', (req, socket, head) => {
     const { url } = req
@@ -508,7 +508,7 @@ const setUpApi = (webServer, xo, config) => {
 
     noServer: true,
   })
-  xo.on('stop', () => fromCallback(cb => webSocketServer.close(cb)))
+  xo.on('stop', fromCallback.call(webSocketServer, 'close'))
 
   const onConnection = (socket, upgradeReq) => {
     const { remoteAddress } = upgradeReq.socket
@@ -577,7 +577,7 @@ const setUpConsoleProxy = (webServer, xo) => {
   const webSocketServer = new WebSocket.Server({
     noServer: true,
   })
-  xo.on('stop', () => fromCallback(cb => webSocketServer.close(cb)))
+  xo.on('stop', fromCallback.call(webSocketServer, 'close'))
 
   webServer.on('upgrade', async (req, socket, head) => {
     const matches = CONSOLE_PROXY_PATH_RE.exec(req.url)
@@ -670,7 +670,7 @@ export default async function main(args) {
   const xo = new Xo(config)
 
   // Register web server close on XO stop.
-  xo.on('stop', () => fromCallback(cb => webServer.stop(cb)))
+  xo.on('stop', fromCallback.call(webServer, 'stop'))
 
   // Connects to all registered servers.
   await xo.start()

packages/xo-cli/src/index.js
@@ -386,7 +386,7 @@ async function call(args) {
         printProgress
       )
 
-      return fromCallback(cb => pump(response, progress, output, cb))
+      return fromCallback(pump, response, progress, output)
     }
 
     if (key === '$sendTo') {

packages/xo-web/src/xo-app/dashboard/health/index.js
@@ -432,7 +432,7 @@ export default class Health extends Component {
         }
 
         const [, value, xml] = matches
-        return fromCallback(cb => xml2js.parseString(xml, cb)).then(result => {
+        return fromCallback(xml2js.parseString, xml).then(result => {
           const object = mapValues(result && result.variable, value =>
             get(value, '[0].$.value')
           )

@xen-orchestra/fs/src/abstract.js
@@ -389,7 +389,7 @@ export default class RemoteHandlerAbstract {
   async test(): Promise<Object> {
     const SIZE = 1024 * 1024 * 10
     const testFileName = normalizePath(`${Date.now()}.test`)
-    const data = await fromCallback(cb => randomBytes(SIZE, cb))
+    const data = await fromCallback(randomBytes, SIZE)
     let step = 'write'
     try {
       const writeStart = process.hrtime()

@xen-orchestra/fs/src/fs.spec.js
@@ -86,7 +86,7 @@ handlers.forEach(url => {
     describe('#createOutputStream()', () => {
       it('creates parent dir if missing', async () => {
         const stream = await handler.createOutputStream('dir/file')
-        await fromCallback(cb => pipeline(createTestDataStream(), stream, cb))
+        await fromCallback(pipeline, createTestDataStream(), stream)
         await expect(await handler.readFile('dir/file')).toEqual(TEST_DATA)
       })
     })
@@ -106,7 +106,7 @@ handlers.forEach(url => {
     describe('#createWriteStream()', () => {
       testWithFileDescriptor('file', 'wx', async ({ file, flags }) => {
         const stream = await handler.createWriteStream(file, { flags })
-        await fromCallback(cb => pipeline(createTestDataStream(), stream, cb))
+        await fromCallback(pipeline, createTestDataStream(), stream)
         await expect(await handler.readFile('file')).toEqual(TEST_DATA)
       })
 

packages/xo-server-auth-ldap/src/test-cli.js
@@ -16,7 +16,7 @@ const CACHE_FILE = './ldap.cache.conf'
 execPromise(async args => {
   const config = await promptSchema(
     configurationSchema,
-    await fromCallback(cb => readFile(CACHE_FILE, 'utf-8', cb)).then(
+    await fromCallback(readFile, CACHE_FILE, 'utf-8').then(
       JSON.parse,
       () => ({})
     )

packages/xo-server/src/_XenStore.js
@@ -1,5 +1,4 @@
 import fromCallback from 'promise-toolbox/fromCallback'
 import { execFile } from 'child_process'
 
-export const read = key =>
-  fromCallback(cb => execFile('xenstore-read', [key], cb))
+export const read = key => fromCallback(execFile, 'xenstore-read', [key])

packages/xo-server/src/api/backup-ng.js
@@ -168,7 +168,7 @@ runJob.params = {
 async function handleGetAllLogs(req, res) {
   const logs = await this.getBackupNgLogs()
   res.set('Content-Type', 'application/json')
-  return fromCallback(cb => pipeline(createNdJsonStream(logs), res, cb))
+  return fromCallback(pipeline, createNdJsonStream(logs), res)
 }
 
 export function getAllLogs({ ndjson = false }) {

commit cca945e05be193bf47881e36359ddbdf3837e6c6

fix(xo-server): remove overzealous changes
Bug introduced in 21901f2a7

As usual, thanks @Danp2 for your report :-)

packages/xo-server/src/index.js
@@ -467,7 +467,7 @@ const setUpProxies = (express, opts, xo) => {
   const webSocketServer = new WebSocket.Server({
     noServer: true,
   })
-  xo.on('stop', fromCallback.call(webSocketServer, 'close'))
+  xo.on('stop', () => fromCallback.call(webSocketServer, 'close'))
 
   express.on('upgrade', (req, socket, head) => {
     const { url } = req
@@ -508,7 +508,7 @@ const setUpApi = (webServer, xo, config) => {
 
     noServer: true,
   })
-  xo.on('stop', fromCallback.call(webSocketServer, 'close'))
+  xo.on('stop', () => fromCallback.call(webSocketServer, 'close'))
 
   const onConnection = (socket, upgradeReq) => {
     const { remoteAddress } = upgradeReq.socket
@@ -577,7 +577,7 @@ const setUpConsoleProxy = (webServer, xo) => {
   const webSocketServer = new WebSocket.Server({
     noServer: true,
   })
-  xo.on('stop', fromCallback.call(webSocketServer, 'close'))
+  xo.on('stop', () => fromCallback.call(webSocketServer, 'close'))
 
   webServer.on('upgrade', async (req, socket, head) => {
     const matches = CONSOLE_PROXY_PATH_RE.exec(req.url)
@@ -670,7 +670,7 @@ export default async function main(args) {
   const xo = new Xo(config)
 
   // Register web server close on XO stop.
-  xo.on('stop', fromCallback.call(webServer, 'stop'))
+  xo.on('stop', () => fromCallback.call(webServer, 'stop'))
 
   // Connects to all registered servers.
   await xo.start()