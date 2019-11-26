688b65ccde1579e247ad07a654b1c3e425ce5ca6
Julien Fontanet <julien.fontanet@isonoe.net>
2019-11-26 11:29:55 +0100

    feat(xo-server/backups-ng): limit number of gc-ed deltas (#4674)
    Merging multiple VHDs is currently a slow process which could be optimized by doing a single merge of a synthetic delta.
    
    Until this is implement, the number of garbage collected deltas should be limited to avoid taking too much time (and possibly interrupted jobs) in case of an important retention change.

--- packages/xo-server/src/xo-mixins/backups-ng/index.js
+++ packages/xo-server/src/xo-mixins/backups-ng/index.js
@@ -1594,6 +1594,21 @@ export default class BackupNg {
                   _ => _.mode === 'delta' && _.scheduleId === scheduleId
                 )
               ): any)
+
+              // FIXME: implement optimized multiple VHDs merging with synthetic
+              // delta
+              //
+              // For the time being, limit the number of deleted backups by run
+              // because it can take a very long time and can lead to
+              // interrupted backup with broken VHD chain.
+              //
+              // The old backups will be eventually merged in future runs of the
+              // job.
+              const { maxMergedDeltasPerRun } = this._backupOptions
+              if (oldBackups.length > maxMergedDeltasPerRun) {
+                oldBackups.length = maxMergedDeltasPerRun
+              }
+
               const deleteOldBackups = () =>
                 wrapTask(
                   {
