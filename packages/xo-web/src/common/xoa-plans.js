26bc05f3c2c59d78be197f2ff68465515f72f937
Pierre Donias <pierre.donias@gmail.com>
2020-01-13 10:20:39 +0100

    Simplify XOA plan management

--- packages/xo-web/src/common/xoa-plans.js
+++ packages/xo-web/src/common/xoa-plans.js
@@ -1,7 +1,53 @@
-export const CURRENT = +process.env.XOA_PLAN
+const FREE = {
+  valueOf: () => 1,
+  name: 'Free',
+}
+const STARTER = {
+  valueOf: () => 2,
+  name: 'Starter',
+}
+export const ENTERPRISE = {
+  valueOf: () => 3,
+  name: 'Enterprise',
+}
+export const PREMIUM = {
+  valueOf: () => 4,
+  name: 'Premium',
+}
+export const SOURCES = {
+  valueOf: () => 5,
+  name: 'Community',
+}
 
-export const FREE = 1
-export const STARTER = 2
-export const ENTERPRISE = 3
-export const PREMIUM = 4
-export const SOURCES = 5
+export const getXoaPlan = (plan = +process.env.XOA_PLAN) => {
+  switch (plan) {
+    case +FREE:
+      return FREE
+    case +STARTER:
+      return STARTER
+    case +ENTERPRISE:
+      return ENTERPRISE
+    case +PREMIUM:
+      return PREMIUM
+    case +SOURCES:
+      return SOURCES
+  }
+  return { name: 'Unknown' }
+}
+
+export const CURRENT = getXoaPlan()
+
+export const idToPlan = id => {
+  switch (id) {
+    case 'free':
+      return FREE
+    case 'starter':
+      return STARTER
+    case 'enterprise':
+      return ENTERPRISE
+    case 'premium':
+      return PREMIUM
+    case 'sb-premium':
+      return PREMIUM
+  }
+}
