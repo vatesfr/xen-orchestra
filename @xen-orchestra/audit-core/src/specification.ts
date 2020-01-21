ef1238036ffafd4724abb4770242fab6ecb4e5bd
badrAZ <azizbibadr@gmail.com>
2020-01-21 15:15:06 +0100

    feat(audit-core): initial commit (#4733)

---
+++ @xen-orchestra/audit-core/src/specification.ts
@@ -0,0 +1,25 @@
+class Storage {
+  acquire: () => Promise<() => undefined>
+  del: (id: string) => Promise<void>
+  get: (id: string) => Promise<Record | void>
+  getLastId: () => Promise<string | void>
+  put: (record: Record) => Promise<void>
+  setLastId: (id: string) => Promise<void>
+}
+
+interface Record {
+  data: object
+  event: string
+  id: string
+  previousId: string
+  subject: object
+  time: number
+}
+
+export class AuditCore {
+  constructor(storage: Storage) {}
+  public add(subject: any, event: string, data: any): Promise<Record> {}
+  public checkIntegrity(oldest: string, newest: string): Promise<void> {}
+  public getFrom(newest?: string): AsyncIterator {}
+  public deleteFrom(newest?: string): Promise<void> {}
+}
