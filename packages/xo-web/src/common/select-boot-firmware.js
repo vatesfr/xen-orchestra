420aca11c59b264a8360976ff231a9d47e7c0ee4
BARHTAOUI <rajaa.barhtaoui@gmail.com>
2020-02-04 13:35:54 +0100

    fix(xo-web/new/vm): display confirm modal for CH/XCP-ng 8.0 hosts
    fix #4749

--- packages/xo-web/src/common/select-boot-firmware.js
+++ packages/xo-web/src/common/select-boot-firmware.js
@@ -1,48 +1,60 @@
 import _ from 'intl'
+import decorate from 'apply-decorators'
 import PropTypes from 'prop-types'
 import React from 'react'
 import { confirm } from 'modal'
+import { createGetObjectsOfType } from 'selectors'
 import { injectState, provideState } from 'reaclette'
-import { noop } from 'utils'
+import { satisfies as versionSatisfies } from 'semver'
 
-// https://docs.citrix.com/en-us/citrix-hypervisor/whats-new/experimental.html
+import { connectStore, noop } from './utils'
+
 // XAPI values should be lowercased
 const VM_BOOT_FIRMWARES = ['bios', 'uefi']
 
-const withState = provideState({
-  effects: {
-    handleBootFirmwareChange(__, { target: { value } }) {
-      if (value !== '') {
-        // TODO: Confirm should be removed once the feature is stabilized
-        confirm({
-          title: _('vmBootFirmware'),
-          body: _('vmBootFirmwareWarningMessage'),
-        }).then(() => this.props.onChange(value), noop)
-      } else {
-        this.props.onChange(value)
-      }
+const SelectBootFirmware = decorate([
+  connectStore({
+    host: createGetObjectsOfType('host').find((_, { host: id }) => ({ id })),
+  }),
+  provideState({
+    effects: {
+      handleBootFirmwareChange(__, { target: { value } }) {
+        if (
+          value !== '' &&
+          versionSatisfies(this.props.host.version, '8.0.0')
+        ) {
+          // Guest UEFI boot is provided in CH/XCP-ng 8.0 as an experimental feature.
+          confirm({
+            title: _('vmBootFirmware'),
+            body: _('vmBootFirmwareWarningMessage'),
+          }).then(() => this.props.onChange(value), noop)
+        } else {
+          this.props.onChange(value)
+        }
+      },
     },
-  },
-})
-
-const SelectBootFirmware = ({ effects, value }) => (
-  <select
-    className='form-control'
-    onChange={effects.handleBootFirmwareChange}
-    value={value}
-  >
-    <option value=''>{_('vmDefaultBootFirmwareLabel')}</option>
-    {VM_BOOT_FIRMWARES.map(val => (
-      <option key={val} value={val}>
-        {val}
-      </option>
-    ))}
-  </select>
-)
+  }),
+  injectState,
+  ({ effects, value }) => (
+    <select
+      className='form-control'
+      onChange={effects.handleBootFirmwareChange}
+      value={value}
+    >
+      <option value=''>{_('vmDefaultBootFirmwareLabel')}</option>
+      {VM_BOOT_FIRMWARES.map(val => (
+        <option key={val} value={val}>
+          {val}
+        </option>
+      ))}
+    </select>
+  ),
+])
 
 SelectBootFirmware.propTypes = {
+  host: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
   value: PropTypes.string.isRequired,
 }
 
-export default withState(injectState(SelectBootFirmware))
+export default SelectBootFirmware
--- packages/xo-web/src/xo-app/new-vm/index.js
+++ packages/xo-web/src/xo-app/new-vm/index.js
@@ -1671,7 +1671,7 @@ export default class NewVm extends BaseComponent {
       showAdvanced,
       tags,
     } = this.state.state
-    const { isAdmin } = this.props
+    const { isAdmin, pool } = this.props
     const { formatMessage } = this.props.intl
     const isHvm = this._isHvm()
     const _copyHostBiosStrings =
@@ -1898,6 +1898,7 @@ export default class NewVm extends BaseComponent {
             <SectionContent>
               <Item label={_('vmBootFirmware')}>
                 <SelectBootFirmware
+                  host={pool.master}
                   onChange={this._handleBootFirmware}
                   value={hvmBootFirmware}
                 />
