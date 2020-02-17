50aabe345a9aa9797a4a946788630f9bead55502
BenjiReis <benjamin.reis@vates.fr>
2020-02-17 13:37:50 +0100

    remove fix for another PR

--- packages/xo-server-sdn-controller/src/index.js
+++ packages/xo-server-sdn-controller/src/index.js
@@ -840,7 +840,7 @@ class SDNController extends EventEmitter {
       return
     }
 
-    this._newHosts.splice(this._newHosts.indexOf(newHost), 1)
+    this._newHosts = this._newHosts.slice(this._newHosts.indexOf(newHost), 1)
 
     log.debug('Sync pool certificates', {
       newHost: host.name_label,
