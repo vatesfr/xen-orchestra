8a4a937d6d7b2a8322ffd343d00cf0088c1e285b
BARHTAOUI <rajaa.barhtaoui@gmail.com>
2020-03-10 16:04:49 +0100

    more changes

--- packages/xo-web/src/xo-app/vm/tab-backups.js
+++ packages/xo-web/src/xo-app/vm/tab-backups.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { adminOnly } from 'utils'
 
-import JobsTable from '../backup/overview/tab-jobs'
+import Overview from '../backup/overview'
 
-const BackupTab = adminOnly(({ vm }) => <JobsTable vm={vm.id} />)
+const BackupTab = adminOnly(({ vm }) => <Overview vm={vm.id} />)
+
 export default BackupTab
