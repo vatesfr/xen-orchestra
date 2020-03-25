33b5e174cb45a4c0236b1be206c4f4984fcc2f43
Julien Fontanet <julien.fontanet@isonoe.net>
2020-03-25 15:40:07 +0100

    test: resume VM and plug VBDs

--- packages/xo-server/src/xapi/index.js
+++ packages/xo-server/src/xapi/index.js
@@ -1052,6 +1052,8 @@ export default class Xapi extends XapiBase {
     // 2. Delete all VBDs which may have been created by the import.
     await asyncMap(vm.$VBDs, vbd => this._deleteVbd(vbd))::ignoreErrors()
 
+    const newVbds = []
+
     // 3. Create VDIs & VBDs.
     //
     // TODO: move all VDIs creation before the VM and simplify the code
@@ -1089,13 +1091,15 @@ export default class Xapi extends XapiBase {
         $defer.onFailure(() => this._deleteVdi(newVdi.$ref))
       }
 
-      await asyncMap(vbds[vdiRef], vbd =>
-        this.createVbd({
-          ...vbd,
-          vdi: newVdi,
-          vm,
-        })
-      )
+      await asyncMap(vbds[vdiRef], async vbd => {
+        newVbds.push(
+          await this.createVbd({
+            ...vbd,
+            vdi: newVdi,
+            vm,
+          })
+        )
+      })
 
       return newVdi
     })::pAll()
@@ -1175,11 +1179,16 @@ export default class Xapi extends XapiBase {
       // FIXME: move
       vm.update_blocked_operations(
         'start',
-        disableStartAfterImport
-          ? 'Do not start this VM, clone it if you want to use it.'
-          : null
+        null
+        // disableStartAfterImport
+        //   ? 'Do not start this VM, clone it if you want to use it.'
+        //   : null
       ),
     ])
+
+    await vm.$call('resume', false, false)
+
+    await Promise.all(newVbds.map(vbd => vbd.$call('plug')))
 
     return { transferSize, vm }
   }
