6e15f2f404dd2819b7355e95d593d2af84af6514
Nicolas Raynaud <github@nraynaud.com>
2020-04-07 09:48:11 +0200

    Update packages/xo-server/src/api/xosan.js
    Co-Authored-By: Julien Fontanet <julien.fontanet@isonoe.net>

--- packages/xo-server/src/api/xosan.js
+++ packages/xo-server/src/api/xosan.js
@@ -861,8 +861,8 @@ async function umountDisk(localEndpoint, diskMountPoint) {
 }
 
 async function createNewDisk(xapi, sr, vm, diskSize) {
-  // VDI size must be between 1 MB and 2088960 MB
-  const vdiMax = 2088960 * Math.pow(2, 20)
+  // https://github.com/xapi-project/sm/blob/c9e959b70b43930b33f3a5abe5afe0a36dc95f06/drivers/vhdutil.py#L31
+  const vdiMax = 2040 * 1024 * 1024 * 1024
   const createVdiSize = Math.min(vdiMax, diskSize)
   const extensionSize = diskSize - createVdiSize
   const newDisk = await xapi.createVdi(
