6f6b9206e6262ad2d62004fadf6827c59a49c5a1
badrAZ <azizbibadr@gmail.com>
2020-04-17 16:19:19 +0200

    throw if the remote proxy and the backup proxy are different

--- packages/xo-server/src/xo-mixins/backups-ng/index.js
+++ packages/xo-server/src/xo-mixins/backups-ng/index.js
@@ -619,6 +619,17 @@ export default class BackupNg {
         const jobId = job.id
 
         const remoteIds = unboxIdsFromPattern(job.remotes)
+        for (const id of remoteIds) {
+          const remote = await app.getRemote(id)
+          if (remote.proxy !== job.proxy) {
+            throw new Error(
+              job.proxy !== undefined
+                ? `The remote ${remote.name} should be linked to the proxy ${job.proxy}`
+                : `The remote ${remote.name} should not be linked to a proxy`
+            )
+          }
+        }
+
         const srIds = unboxIdsFromPattern(job.srs)
 
         if (job.proxy !== undefined) {
