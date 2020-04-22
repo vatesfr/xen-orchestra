71a580a83407e8e19782e89cb52d750bd5f7b7ad
badrAZ <azizbibadr@gmail.com>
2020-04-22 09:46:30 +0200

    update implementation

--- packages/xo-server/src/xo-mixins/backups-ng/index.js
+++ packages/xo-server/src/xo-mixins/backups-ng/index.js
@@ -619,20 +619,6 @@ export default class BackupNg {
         const jobId = job.id
 
         const remoteIds = unboxIdsFromPattern(job.remotes)
-
-        await Promise.all(
-          remoteIds.map(async id => {
-            const remote = await app.getRemote(id)
-            if (remote.proxy !== job.proxy) {
-              throw new Error(
-                job.proxy !== undefined
-                  ? `The remote ${remote.name} must be linked to the proxy ${job.proxy}`
-                  : `The remote ${remote.name} must not be linked to a proxy`
-              )
-            }
-          })
-        )
-
         const srIds = unboxIdsFromPattern(job.srs)
 
         if (job.proxy !== undefined) {
@@ -652,7 +638,14 @@ export default class BackupNg {
           const xapis = {}
           await waitAll([
             asyncMap(remoteIds, async id => {
-              remotes[id] = await app.getRemoteWithCredentials(id)
+              const remote = await app.getRemoteWithCredentials(id)
+              if (remote.proxy !== job.proxy) {
+                throw new Error(
+                  `The remote ${remote.name} must be linked to the proxy ${job.proxy}`
+                )
+              }
+
+              remotes[id] = remote
             }),
             asyncMap([...servers], async id => {
               const {
@@ -695,10 +688,19 @@ export default class BackupNg {
           }
         })
         const remotes = await Promise.all(
-          remoteIds.map(async id => ({
-            id,
-            handler: await app.getRemoteHandler(id),
-          }))
+          remoteIds.map(async id => {
+            const remote = await app.getRemote(id)
+            if (remote.proxy !== undefined) {
+              throw new Error(
+                `The remote ${remote.name} must not be linked to a proxy`
+              )
+            }
+
+            return {
+              id,
+              handler: await app.getRemoteHandler(id),
+            }
+          })
         )
 
         const settings = merge(job.settings, data?.settings)
