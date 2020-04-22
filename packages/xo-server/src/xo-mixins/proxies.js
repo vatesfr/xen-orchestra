9a15f057ed6fbc785f2da6813786472d6ae28d54
badrAZ <azizbibadr@gmail.com>
2020-04-22 14:49:39 +0200

    move `extractIp` to its own module

--- packages/xo-server/src/xo-mixins/proxies.js
+++ packages/xo-server/src/xo-mixins/proxies.js
@@ -16,31 +16,8 @@ import Collection from '../collection/redis'
 import parseDuration from '../_parseDuration'
 import patch from '../patch'
 import readChunk from '../_readStreamChunk'
+import { extractIpFromVmNetworks as extractIp } from '../_extractIpFromVmNetworks'
 import { generateToken } from '../utils'
-
-// See: https://github.com/xapi-project/xen-api/blob/324bc6ee6664dd915c0bbe57185f1d6243d9ed7e/ocaml/xapi/xapi_guest_agent.ml#L59-L81
-//
-// Returns <min(n)>/ipv4/<min(m)> || <min(n)>/ipv6/<min(m)> || undefined
-// where n corresponds to the network interface and m to its IP
-const IPV4_KEY_RE = /^\d+\/ipv4\/\d+$/
-const IPV6_KEY_RE = /^\d+\/ipv6\/\d+$/
-const extractIp = networks => {
-  if (networks === undefined) {
-    return
-  }
-
-  let ipv6
-  for (const key of Object.keys(networks).sort()) {
-    if (IPV4_KEY_RE.test(key)) {
-      return networks[key]
-    }
-
-    if (ipv6 === undefined && IPV6_KEY_RE.test(key)) {
-      ipv6 = networks[key]
-    }
-  }
-  return ipv6
-}
 
 const extractProperties = _ => _.properties
 const omitToken = proxy => omit(proxy, 'authenticationToken')
