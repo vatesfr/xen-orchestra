d9fa0a0beb2bd66112ed80f68b5ff9b2512646d8
BARHTAOUI <rajaa.barhtaoui@gmail.com>
2020-05-26 14:56:15 +0200

    minor change

--- packages/xo-web/src/xo-app/home/index.js
+++ packages/xo-web/src/xo-app/home/index.js
@@ -1258,7 +1258,9 @@ export default class Home extends Component {
       areObjectsFetched,
       isAdmin,
       isPoolAdmin,
-      location,
+      location: {
+        query: { backup },
+      },
       noResourceSets,
       type,
     } = this.props
@@ -1309,7 +1311,7 @@ export default class Home extends Component {
           name='Home'
           targetNodeSelector='body'
         />
-        {type !== 'VM' || location.query.backup === undefined ? (
+        {type !== 'VM' || backup === undefined ? (
           <div>
             <div className={styles.itemContainer}>
               {isEmpty(filteredItems) ? (
@@ -1354,7 +1356,7 @@ export default class Home extends Component {
         ) : (
           <BackedUpVms
             itemsPerPage={homeItemsPerPage}
-            showBackedUpVms={location.query.backup === 'true'}
+            showBackedUpVms={backup === 'true'}
             vms={filteredItems}
           />
         )}
