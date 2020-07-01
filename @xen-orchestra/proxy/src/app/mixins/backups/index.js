1cb8a9cf3249ac7b601c49c16ee528ee955f59d9
Julien Fontanet <julien.fontanet@isonoe.net>
2020-07-01 14:40:17 +0200

    fix(proxy/backup.importVmBackup): pass whole SR record to importDeltaVm

--- @xen-orchestra/proxy/src/app/mixins/backups/index.js
+++ @xen-orchestra/proxy/src/app/mixins/backups/index.js
@@ -118,7 +118,7 @@ export default class Backups {
 
                 vmRef = await importDeltaVm(
                   await adapter.readDeltaVmBackup(metadata),
-                  srRef
+                  await xapi.getRecord('SR', srRef)
                 )
               }
 
