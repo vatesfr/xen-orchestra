3f4d6181cfb64c21ffbb6bb4574ad5185d248146
badrAZ <azizbibadr@gmail.com>
2020-07-31 16:13:00 +0200

    update implementation

--- packages/xo-web/src/xo-app/proxies/deploy-proxy.js
+++ packages/xo-web/src/xo-app/proxies/deploy-proxy.js
@@ -298,13 +298,13 @@ const deployProxy = async ({ proxy } = {}) => {
     try {
       license = await createProxyTrialLicense()
     } catch (error) {
-      alert(
+      await alert(
         _('trialStartButton'),
         <span className='text-danger'>
           <Icon icon='alarm' /> {error.message}
         </span>
       )
-      throw error
+      throw undefined
     }
   }
 
