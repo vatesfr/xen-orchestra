17d8f897d4bb9b214578e77bc643ceb626bfd796
badrAZ <azizbibadr@gmail.com>
2020-09-01 16:43:33 +0200

    Update packages/xo-server-usage-report/src/index.js
    Co-authored-by: Julien Fontanet <julien.fontanet@isonoe.net>

--- packages/xo-server-usage-report/src/index.js
+++ packages/xo-server-usage-report/src/index.js
@@ -417,7 +417,7 @@ function computeGlobalVmsStats({ haltedVms, vmsStats, xo }) {
         tag => tag === 'Disaster Recovery' || tag === 'Continuous Replication'
       )
 
-    // Exclude replicated VMs from the VMs evolution because they can be added then deleted by the backup due to its retention
+    // Exclude replicated VMs because they keep being created/destroyed due to the implementation
     if (!isReplication) {
       allVms.push({
         uuid: vm.uuid,
