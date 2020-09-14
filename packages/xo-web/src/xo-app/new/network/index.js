b3c436e5ff38a7b2bf334d12c7c011dbd91969c2
badrAZ <azizbibadr@gmail.com>
2020-09-14 11:03:50 +0200

    fix(xo-web/new/network): omit bond slave PIFs from selection
    See https://xcp-ng.org/forum/topic/3524/old-networks-not-removed-after-creating-bond/8

--- packages/xo-web/src/xo-app/new/network/index.js
+++ packages/xo-web/src/xo-app/new/network/index.js
@@ -163,7 +163,9 @@ const NewNetwork = decorate([
           ({ pool }) => pool !== undefined && pool.id === host.$pool
         ),
       pifPredicate: (_, { pool }) => pif =>
-        pif.vlan === -1 && pif.$host === (pool && pool.master),
+        pif.vlan === -1 &&
+        !pif.isBondSlave &&
+        pif.$host === (pool && pool.master),
       pifPredicateSdnController: (_, { pool }) => pif =>
         canSupportPrivateNetwork(pool, pif),
       networkPifPredicate: ({ networks }) => (pif, key) =>
