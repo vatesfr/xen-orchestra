vatesfr/xen-orchestra

commit 9148510c046ea64db8497e6a94e9a5a8def8cd97

feat(proxy/backups): use XAPI connection as resource (#5442)

@xen-orchestra/proxy/src/app/mixins/backups/_Backup.js
@@ -51,35 +51,40 @@ export class Backup {
       ...settings[schedule.id],
     }
 
-    const srs = await Promise.all(extractIdsFromSimplePattern(job.srs).map(_ => this._getRecord('SR', _)))
+    await using(
+      extractIdsFromSimplePattern(job.srs).map(_ => this._getRecord('SR', _)),
+      async srs => {
+        const remoteAdapters = extractIdsFromSimplePattern(job.remotes).map(id => this._getAdapter(id))
+        await using(remoteAdapters, async adapters => {
+          const vmIds = extractIdsFromSimplePattern(job.vms)
 
-    const remoteAdapters = extractIdsFromSimplePattern(job.remotes).map(id => this._getAdapter(id))
-    await using(remoteAdapters, async adapters => {
-      const vmIds = extractIdsFromSimplePattern(job.vms)
+          Task.info('vms', { vms: vmIds })
 
-      Task.info('vms', { vms: vmIds })
+          const remoteAdapters = {}
+          adapters.forEach(({ adapter, remoteId }) => {
+            remoteAdapters[remoteId] = adapter
+          })
 
-      const remoteAdapters = {}
-      adapters.forEach(({ adapter, remoteId }) => {
-        remoteAdapters[remoteId] = adapter
-      })
-
-      const handleVm = vmUuid =>
-        Task.run({ name: 'backup VM', data: { type: 'VM', id: vmUuid } }, async () =>
-          new VmBackup({
-            getSnapshotNameLabel,
-            job,
-            // remotes,
-            remoteAdapters,
-            schedule,
-            settings: { ...scheduleSettings, ...settings[vmUuid] },
-            srs,
-            vm: await this._getRecord('VM', vmUuid),
-          }).run()
-        ).catch(noop) // errors are handled by logs
-      const { concurrency } = scheduleSettings
-      await asyncMap(vmIds, concurrency === 0 ? handleVm : limitConcurrency(concurrency)(handleVm))
-    })
+          const handleVm = vmUuid =>
+            Task.run({ name: 'backup VM', data: { type: 'VM', id: vmUuid } }, () =>
+              using(this._getRecord('VM', vmUuid), vm =>
+                new VmBackup({
+                  getSnapshotNameLabel,
+                  job,
+                  // remotes,
+                  remoteAdapters,
+                  schedule,
+                  settings: { ...scheduleSettings, ...settings[vmUuid] },
+                  srs,
+                  vm,
+                }).run()
+              )
+            ).catch(noop) // errors are handled by logs
+          const { concurrency } = scheduleSettings
+          await asyncMap(vmIds, concurrency === 0 ? handleVm : limitConcurrency(concurrency)(handleVm))
+        })
+      }
+    )
   }
 
   @decorateWith(disposable)
@@ -91,13 +96,14 @@ export class Backup {
     }
   }
 
-  async _getRecord(type, uuid) {
+  @decorateWith(disposable)
+  async *_getRecord(type, uuid) {
     const xapiId = this._recordToXapi[uuid]
     if (xapiId === undefined) {
       throw new Error('no XAPI associated to ' + uuid)
     }
 
-    const xapi = await this._getConnectedXapi(xapiId)
+    const xapi = yield this._getConnectedXapi(xapiId)
     return xapi.getRecordByUuid(type, uuid)
   }
 }

@xen-orchestra/proxy/src/app/mixins/backups/index.js
@@ -2,17 +2,21 @@ import assert from 'assert'
 import defer from 'golike-defer'
 import fromCallback from 'promise-toolbox/fromCallback'
 import fromEvent from 'promise-toolbox/fromEvent'
-import ignoreErrors from 'promise-toolbox/ignoreErrors'
 import mapValues from 'lodash/mapValues'
 import pDefer from 'promise-toolbox/defer'
 import using from 'promise-toolbox/using'
 import { createLogger } from '@xen-orchestra/log/dist'
+import { decorateWith } from '@vates/decorate-with'
 import { execFile } from 'child_process'
 import { formatFilenameDate } from '@xen-orchestra/backups/filenameDate'
 import { formatVmBackup } from '@xen-orchestra/backups/formatVmBackup'
+import { parseDuration } from '@vates/parse-duration'
 import { Xapi } from '@xen-orchestra/xapi'
 import { ZipFile } from 'yazl'
 
+import { debounceResource } from '../../_debounceResource'
+import { decorateResult } from '../../_decorateResult'
+import { deduped } from '../../_deduped'
 import { disposable } from '../../_disposable'
 
 import { Backup } from './_Backup'
@@ -25,7 +29,11 @@ const noop = Function.prototype
 const { warn } = createLogger('xo:proxy:backups')
 
 export default class Backups {
-  constructor(app, { config: { backups: config, xapiOptions: globalXapiOptions } }) {
+  constructor(app, { config: { backups: config, resourceDebounce, xapiOptions: globalXapiOptions } }) {
+    this._app = app
+    this._globalXapiOptions = globalXapiOptions
+    this._resourceDebounce = resourceDebounce
+
     // clean any LVM volumes that might have not been properly
     // unmounted
     app.hooks.on('start', async () => {
@@ -33,54 +41,14 @@ export default class Backups {
       await fromCallback(execFile, 'pvscan', ['--cache'])
     })
 
-    const createXapi = ({ credentials: { username: user, password }, ...opts }) =>
-      new Xapi({
-        ...globalXapiOptions,
-        ...opts,
-        auth: {
-          user,
-          password,
-        },
-      })
+    let run = ({ xapis, ...rest }) =>
+      new Backup({
+        ...rest,
+        app,
+        config,
+        getConnectedXapi: id => this.getXapi(xapis[id]),
+      }).run()
 
-    let run = async ({ xapis: xapisOptions, ...rest }) => {
-      const xapis = []
-      async function createConnectedXapi(id) {
-        const xapi = createXapi(xapisOptions[id])
-        xapis.push(xapi)
-        await xapi.connect()
-        await xapi.objectsFetched
-        return xapi
-      }
-      async function disconnectAllXapis() {
-        const promises = xapis.map(xapi => xapi.disconnect())
-        xapis.length = 0
-        await Promise.all(promises)
-      }
-      app.hooks.on('stop', disconnectAllXapis)
-
-      const connectedXapis = { __proto__: null }
-      function getConnectedXapi(id) {
-        let connectedXapi = connectedXapis[id]
-        if (connectedXapi === undefined) {
-          connectedXapi = createConnectedXapi(id)
-          connectedXapis[id] = connectedXapi
-        }
-        return connectedXapi
-      }
-
-      try {
-        await new Backup({
-          ...rest,
-          app,
-          config,
-          getConnectedXapi,
-        }).run()
-      } finally {
-        app.hooks.removeListener('stop', disconnectAllXapis)
-        ignoreErrors.call(disconnectAllXapis())
-      }
-    }
     const runningJobs = { __proto__: null }
     run = (run => {
       return async function (params) {
@@ -182,11 +150,7 @@ export default class Backups {
         ],
         importVmBackup: [
           defer(($defer, { backupId, remote, srUuid, xapi: xapiOpts }) =>
-            using(app.remotes.getAdapter(remote), async adapter => {
-              const xapi = createXapi(xapiOpts)
-              await xapi.connect()
-              $defer.call(xapi, 'disconnect')
-
+            using(app.remotes.getAdapter(remote), this.getXapi(xapiOpts), async (adapter, xapi) => {
               const srRef = await xapi.call('SR.get_by_uuid', srUuid)
 
               const metadata = await adapter.readVmBackupMetadata(backupId)
@@ -362,5 +326,31 @@ export default class Backups {
         ],
       },
     })
+  }
+
+  // FIXME: invalidate cache on options change
+  @decorateResult(function (resource) {
+    return debounceResource(resource, this._app.hooks, parseDuration(this._resourceDebounce))
+  })
+  @decorateWith(deduped, ({ url }) => [url])
+  @decorateWith(disposable)
+  async *getXapi({ credentials: { username: user, password }, ...opts }) {
+    const xapi = new Xapi({
+      ...this._globalXapiOptions,
+      ...opts,
+      auth: {
+        user,
+        password,
+      },
+    })
+
+    await xapi.connect()
+    try {
+      await xapi.objectsFetched
+
+      yield xapi
+    } finally {
+      await xapi.disconnect()
+    }
   }
 }

commit b8a3eb62420eea74331d04bf3168e5edbf657f95

feat(proxy): use the new resource management API (#5458)
Also fix a bug where backups without replications were no longer working introduced in 9148510c0

@xen-orchestra/proxy/src/app/_debounceResource.js
@@ -1,25 +1,27 @@
-import Resource from 'promise-toolbox/_Resource'
+import Disposable from 'promise-toolbox/Disposable'
 
-export function debounceResource(resource, hooks, delay = 0) {
+export async function debounceResource(pDisposable, hooks, delay = 0) {
   if (delay === 0) {
-    return resource
+    return pDisposable
   }
 
-  let timeoutId, value
-  const dispose = () => {
-    const { d } = resource
-    if (d !== undefined) {
-      resource.d = undefined
+  const disposable = await pDisposable
+
+  let timeoutId
+  const disposeWrapper = () => {
+    if (timeoutId !== undefined) {
+      clearTimeout(timeoutId)
+      timeoutId = undefined
       hooks.removeListener('stop', onStop)
-      return d(value)
+
+      return disposable.dispose()
     }
   }
 
   const onStop = () => {
     const shouldDisposeNow = timeoutId !== undefined
     if (shouldDisposeNow) {
-      clearTimeout(timeoutId)
-      return dispose()
+      return disposeWrapper()
     } else {
       // will dispose ASAP
       delay = 0
@@ -27,9 +29,7 @@ export function debounceResource(resource, hooks, delay = 0) {
   }
   hooks.on('stop', onStop)
 
-  return new Resource(resource.p, v => {
-    // onStop doesn't have access to the value
-    value = v
-    timeoutId = setTimeout(dispose, delay)
+  return new Disposable(disposable.value, () => {
+    timeoutId = setTimeout(disposeWrapper, delay)
   })
 }

@xen-orchestra/proxy/src/app/_deduped.js
@@ -1,11 +1,13 @@
+import Disposable from 'promise-toolbox/Disposable'
 import ensureArray from 'ensure-array'
-import Resource from 'promise-toolbox/_Resource'
 import { MultiKeyMap } from '@vates/multi-key-map'
 
-function State(resource) {
+function State(pGetNewDisposable) {
+  this.pGetNewDisposable = pGetNewDisposable
   this.users = 0
-  this.resource = resource
 }
+
+const call = fn => fn()
 
 export const deduped = (factory, keyFn = (...args) => args) =>
   (function () {
@@ -14,21 +16,26 @@ export const deduped = (factory, keyFn = (...args) => args) =>
       const keys = ensureArray(keyFn.apply(this, arguments))
       let state = states.get(keys)
       if (state === undefined) {
-        const resource = factory.apply(this, arguments)
-        resource.p.catch(() => {
+        const pDisposable = factory.apply(this, arguments)
+        pDisposable.catch(() => {
           states.delete(keys)
         })
 
-        state = new State(resource)
+        state = new State(
+          pDisposable.then(({ value, dispose }) => {
+            const disposeWrapper = () => {
+              if (--state.users === 0) {
+                states.delete(keys)
+                return dispose()
+              }
+            }
+            return () => new Disposable(value, disposeWrapper)
+          })
+        )
         states.set(keys, state)
       }
 
       ++state.users
-      return new Resource(state.resource.p, value => {
-        if (--state.users === 0) {
-          states.delete(keys)
-          return state.resource.d(value)
-        }
-      })
+      return state.pGetNewDisposable.then(call)
     }
   })()

@xen-orchestra/proxy/src/app/_disposable.js
@@ -1,39 +1,0 @@
-import defer from 'promise-toolbox/defer'
-import Resource from 'promise-toolbox/_Resource'
-
-async function helper(gen, resolve, disposers) {
-  let resourceValue
-  while (true) {
-    const { value } = await gen.next(resourceValue)
-    if (value instanceof Resource) {
-      resourceValue = await value.p
-      disposers.push(value.d)
-    } else {
-      return resolve(value)
-    }
-  }
-}
-
-// inspired by https://github.com/tc39/proposal-explicit-resource-management
-export const disposable = factory =>
-  function () {
-    const gen = factory.apply(this, arguments)
-
-    const { promise, reject, resolve } = defer()
-    const disposers = []
-    const dispose = async () => {
-      let disposer
-      while ((disposer = disposers.pop()) !== undefined) {
-        await disposer()
-      }
-    }
-    helper(gen, resolve, disposers).catch(error => {
-      reject(error)
-      return dispose()
-    })
-
-    return new Resource(promise, async value => {
-      await gen.return(value)
-      await dispose()
-    })
-  }

@xen-orchestra/proxy/src/app/mixins/appliance.js
@@ -1,3 +1,4 @@
+import Disposable from 'promise-toolbox/Disposable'
 import fromCallback from 'promise-toolbox/fromCallback'
 import fromEvent from 'promise-toolbox/fromEvent'
 import JsonRpcWebsocketClient from 'jsonrpc-websocket-client'
@@ -8,23 +9,16 @@ import { execFile, spawn } from 'child_process'
 import { readFile } from 'fs-extra'
 
 import { deduped } from '../_deduped'
-import { disposable } from '../_disposable'
 
 const TUNNEL_SERVICE = 'xoa-support-tunnel.service'
 
 const { debug, warn } = createLogger('xo:proxy:appliance')
 
-const getUpdater = deduped(
-  disposable(async function* () {
-    const updater = new JsonRpcWebsocketClient('ws://localhost:9001')
-    await updater.open()
-    try {
-      yield updater
-    } finally {
-      await updater.close()
-    }
-  })
-)
+const getUpdater = deduped(async function () {
+  const updater = new JsonRpcWebsocketClient('ws://localhost:9001')
+  await updater.open()
+  return new Disposable(updater, () => updater.close())
+})
 
 const callUpdate = params =>
   using(

@xen-orchestra/proxy/src/app/mixins/backups/_Backup.js
@@ -1,11 +1,10 @@
 import asyncMap from '@xen-orchestra/async-map'
+import Disposable from 'promise-toolbox/Disposable'
 import limitConcurrency from 'limit-concurrency-decorator'
 import using from 'promise-toolbox/using'
 import { compileTemplate } from '@xen-orchestra/template'
 import { decorateWith } from '@vates/decorate-with'
 import { extractIdsFromSimplePattern } from '@xen-orchestra/backups/extractIdsFromSimplePattern'
-
-import { disposable } from '../../_disposable'
 
 import { Task } from './_Task'
 import { VmBackup } from './_VmBackup'
@@ -87,7 +86,7 @@ export class Backup {
     )
   }
 
-  @decorateWith(disposable)
+  @decorateWith(Disposable.factory)
   *_getAdapter(remoteId) {
     const adapter = yield this._app.remotes.getAdapter(this._remotes[remoteId])
     return {
@@ -96,7 +95,7 @@ export class Backup {
     }
   }
 
-  @decorateWith(disposable)
+  @decorateWith(Disposable.factory)
   async *_getRecord(type, uuid) {
     const xapiId = this._recordToXapi[uuid]
     if (xapiId === undefined) {

@xen-orchestra/proxy/src/app/mixins/backups/_RemoteAdapter.js
@@ -1,4 +1,5 @@
 import asyncMap from '@xen-orchestra/async-map'
+import Disposable from 'promise-toolbox/Disposable'
 import fromCallback from 'promise-toolbox/fromCallback'
 import pump from 'pump'
 import using from 'promise-toolbox/using'
@@ -13,7 +14,6 @@ import { readdir, stat } from 'fs-extra'
 import { debounceResource } from '../../_debounceResource'
 import { decorateResult } from '../../_decorateResult'
 import { deduped } from '../../_deduped'
-import { disposable } from '../../_disposable'
 
 import { BACKUP_DIR } from './_getVmBackupDir'
 import { listPartitions, LVM_PARTITION_TYPE } from './_listPartitions'
@@ -117,7 +117,7 @@ export class RemoteAdapter {
 
   @decorateResult(getDebouncedResource)
   @decorateWith(deduped, (devicePath, pvId, vgName) => [devicePath, pvId, vgName])
-  @decorateWith(disposable)
+  @decorateWith(Disposable.factory)
   async *_getLvmLogicalVolumes(devicePath, pvId, vgName) {
     yield this._getLvmPhysicalVolume(devicePath, pvId && (await this._findPartition(devicePath, pvId)))
 
@@ -131,7 +131,7 @@ export class RemoteAdapter {
 
   @decorateResult(getDebouncedResource)
   @decorateWith(deduped, (devicePath, partition) => [devicePath, partition?.id])
-  @decorateWith(disposable)
+  @decorateWith(Disposable.factory)
   async *_getLvmPhysicalVolume(devicePath, partition) {
     const args = []
     if (partition !== undefined) {
@@ -154,7 +154,7 @@ export class RemoteAdapter {
 
   @decorateResult(getDebouncedResource)
   @decorateWith(deduped, (devicePath, partition) => [devicePath, partition?.id])
-  @decorateWith(disposable)
+  @decorateWith(Disposable.factory)
   async *_getPartition(devicePath, partition) {
     const options = ['loop', 'ro']
 
@@ -207,7 +207,7 @@ export class RemoteAdapter {
     })
   }
 
-  @decorateWith(disposable)
+  @decorateWith(Disposable.factory)
   async *usePartitionFiles(diskId, partitionId, paths) {
     const path = yield this.getPartition(diskId, partitionId)
 
@@ -257,7 +257,7 @@ export class RemoteAdapter {
 
   @decorateResult(getDebouncedResource)
   @decorateWith(deduped, diskId => [diskId])
-  @decorateWith(disposable)
+  @decorateWith(Disposable.factory)
   async *getDisk(diskId) {
     const handler = this._handler
 
@@ -294,7 +294,7 @@ export class RemoteAdapter {
   // - `<partitionId>`: partitioned disk
   // - `<pvId>/<vgName>/<lvName>`: LVM on a partitioned disk
   // - `/<vgName>/lvName>`: LVM on a raw disk
-  @decorateWith(disposable)
+  @decorateWith(Disposable.factory)
   async *getPartition(diskId, partitionId) {
     const devicePath = yield this.getDisk(diskId)
     if (partitionId === undefined) {

@xen-orchestra/proxy/src/app/mixins/backups/index.js
@@ -1,5 +1,6 @@
 import assert from 'assert'
 import defer from 'golike-defer'
+import Disposable from 'promise-toolbox/Disposable'
 import fromCallback from 'promise-toolbox/fromCallback'
 import fromEvent from 'promise-toolbox/fromEvent'
 import mapValues from 'lodash/mapValues'
@@ -17,7 +18,6 @@ import { ZipFile } from 'yazl'
 import { debounceResource } from '../../_debounceResource'
 import { decorateResult } from '../../_decorateResult'
 import { deduped } from '../../_deduped'
-import { disposable } from '../../_disposable'
 
 import { Backup } from './_Backup'
 import { importDeltaVm } from './_deltaVm'
@@ -131,16 +131,16 @@ export default class Backups {
         fetchPartitionFiles: [
           ({ disk: diskId, remote, partition: partitionId, paths }) => {
             const { promise, reject, resolve } = pDefer()
-            using(app.remotes.getAdapter(remote), adapter =>
-              using(adapter.usePartitionFiles(diskId, partitionId, paths), async files => {
-                const zip = new ZipFile()
-                files.forEach(({ realPath, metadataPath }) => zip.addFile(realPath, metadataPath))
-                zip.end()
-                const { outputStream } = zip
-                resolve(outputStream)
-                await fromEvent(outputStream, 'end')
-              })
-            ).catch(error => {
+            using(async function* () {
+              const adapter = yield app.remotes.getAdapter(remote)
+              const files = yield adapter.usePartitionFiles(diskId, partitionId, paths)
+              const zip = new ZipFile()
+              files.forEach(({ realPath, metadataPath }) => zip.addFile(realPath, metadataPath))
+              zip.end()
+              const { outputStream } = zip
+              resolve(outputStream)
+              await fromEvent(outputStream, 'end')
+            }).catch(error => {
               warn(error)
               reject(error)
             })
@@ -272,7 +272,7 @@ export default class Backups {
       },
     })
 
-    const getPartition = disposable(function* ({ disk, partition, remote }) {
+    const getPartition = Disposable.factory(function* ({ disk, partition, remote }) {
       const adapter = yield app.remotes.getAdapter(remote)
       return yield adapter.getPartition(disk, partition)
     })
@@ -294,13 +294,12 @@ export default class Backups {
       backup: {
         mountPartition: [
           async props => {
-            const resource = getPartition(props)
-            const path = await resource.p
+            const { value: path, dispose } = await getPartition(props)
 
             if (partitionDisposers[path] === undefined) {
               partitionDisposers[path] = []
             }
-            partitionDisposers[path].push(resource.d)
+            partitionDisposers[path].push(dispose)
 
             return path
           },
@@ -341,7 +340,7 @@ export default class Backups {
     return debounceResource(resource, this._app.hooks, parseDuration(this._resourceDebounce))
   })
   @decorateWith(deduped, ({ url }) => [url])
-  @decorateWith(disposable)
+  @decorateWith(Disposable.factory)
   async *getXapi({ credentials: { username: user, password }, ...opts }) {
     const xapi = new Xapi({
       ...this._globalXapiOptions,

@xen-orchestra/proxy/src/app/mixins/remotes.js
@@ -1,3 +1,4 @@
+import Disposable from 'promise-toolbox/Disposable'
 import fromCallback from 'promise-toolbox/fromCallback'
 import tmp from 'tmp'
 import using from 'promise-toolbox/using'
@@ -9,7 +10,6 @@ import { rmdir } from 'fs-extra'
 import { debounceResource } from '../_debounceResource'
 import { decorateResult } from '../_decorateResult'
 import { deduped } from '../_deduped'
-import { disposable } from '../_disposable'
 
 import { RemoteAdapter } from './backups/_RemoteAdapter'
 
@@ -48,21 +48,16 @@ export default class Remotes {
   // FIXME: invalidate cache on remote option change
   @decorateResult(getDebouncedResource)
   @decorateWith(deduped, remote => [remote.url])
-  @decorateWith(disposable)
-  async *getHandler(remote) {
+  async getHandler(remote) {
     const handler = getHandler(remote, this._config.remoteOptions)
     await handler.sync()
-    try {
-      yield handler
-    } finally {
-      await handler.forget()
-    }
+    return new Disposable(handler, () => handler.forget())
   }
 
   // FIXME: invalidate cache on remote option change
   @decorateResult(getDebouncedResource)
   @decorateWith(deduped, remote => [remote.url])
-  @decorateWith(disposable)
+  @decorateWith(Disposable.factory)
   *getAdapter(remote) {
     return new RemoteAdapter(yield this.getHandler(remote), {
       app: this._app,
@@ -70,13 +65,8 @@ export default class Remotes {
     })
   }
 
-  @decorateWith(disposable)
-  async *getTempMountDir() {
+  async getTempMountDir() {
     const mountDir = await fromCallback(tmp.dir)
-    try {
-      yield mountDir
-    } finally {
-      await rmdir(mountDir)
-    }
+    return new Disposable(mountDir, () => rmdir(mountDir))
   }
 }