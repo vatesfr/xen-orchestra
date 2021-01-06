97b620f98f4219be71117e34264e32c8484718ec
Julien Fontanet <julien.fontanet@isonoe.net>
2021-01-06 11:50:36 +0100

    fix(xo-server): rm xenStore entries after use (#5482)
    This should already be done by the deploy scripts but in case it's not, xo-server should do it.

--- packages/xo-server/src/_XenStore.js
+++ packages/xo-server/src/_XenStore.js
@@ -2,3 +2,4 @@ import fromCallback from 'promise-toolbox/fromCallback'
 import { execFile } from 'child_process'
 
 export const read = key => fromCallback(execFile, 'xenstore-read', [key])
+export const rm = key => fromCallback(execFile, 'xenstore-rm', [key])
