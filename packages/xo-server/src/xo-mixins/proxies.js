24e68724e61c9c61e8f274a4c433da7f9671535d
badrAZ <azizbibadr@gmail.com>
2021-02-05 17:52:56 +0100

    update implementation

--- packages/xo-server/src/xo-mixins/proxies.js
+++ packages/xo-server/src/xo-mixins/proxies.js
@@ -153,7 +153,7 @@ export default class Proxy {
         ids.push(id)
       }
       if (ids.length !== 0) {
-        throw incorrectState({ actual: ids, expected: [], property: 'runningJobs' })
+        throw incorrectState({ actual: ids, expected: [], object: 'runningJobs' })
       }
     }
 
