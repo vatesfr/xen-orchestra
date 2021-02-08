bc96ca3bcb076f362fc20ebc1cf9dcaa07ed159a
Julien Fontanet <julien.fontanet@isonoe.net>
2021-02-08 15:49:44 +0100

    chore(proxy/_getTmpDir): own implementation
    Does not depend on `tmp` anymore.

--- @xen-orchestra/proxy/src/app/mixins/backups/_getTmpDir.js
+++ @xen-orchestra/proxy/src/app/mixins/backups/_getTmpDir.js
@@ -1,9 +1,20 @@
 import Disposable from 'promise-toolbox/Disposable'
-import fromCallback from 'promise-toolbox/fromCallback'
-import tmp from 'tmp'
-import { rmdir } from 'fs-extra'
+import { join } from 'path'
+import { mkdir, rmdir } from 'fs-extra'
+import { tmpdir } from 'os'
+
+const MAX_ATTEMPTS = 3
 
 export const getTmpDir = async () => {
-  const tmpDir = await fromCallback(tmp.dir)
-  return new Disposable(tmpDir, () => rmdir(tmpDir))
+  for (let i = 0; true; ++i) {
+    const path = join(tmpdir(), Math.random().toString(36).slice(2))
+    try {
+      await mkdir(path)
+      return new Disposable(path, () => rmdir(path))
+    } catch (error) {
+      if (i === MAX_ATTEMPTS) {
+        throw error
+      }
+    }
+  }
 }
