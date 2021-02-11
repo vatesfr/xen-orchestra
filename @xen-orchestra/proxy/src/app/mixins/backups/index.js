0618e3f6b6ffc36db6867f827f887122b2eed837
badrAZ <azizbibadr@gmail.com>
2021-02-11 09:48:05 +0100

    update implementation

--- @xen-orchestra/proxy/src/app/mixins/backups/index.js
+++ @xen-orchestra/proxy/src/app/mixins/backups/index.js
@@ -61,7 +61,7 @@ export default class Backups {
         // don't change config during backup execution
         config: app.config.get('backups'),
 
-        // we passe getAdapter in order to mutualize the adapter resources usage
+        // passe getAdapter in order to mutualize the adapter resources usage
         getAdapter: this.getAdapter.bind(this),
         getConnectedXapi: id => this.getXapi(xapis[id]),
       }).run()
