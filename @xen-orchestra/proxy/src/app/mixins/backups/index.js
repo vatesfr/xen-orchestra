45d6b350d248a0c7d726e1edae8f20ad9269c0a7
badrAZ <azizbibadr@gmail.com>
2021-02-11 10:50:57 +0100

    fix(proxy/backups): fix resource generator function not called with backup context
    Introduced by c19326ed501629afdefe954a7ac375f9328aea63

--- @xen-orchestra/proxy/src/app/mixins/backups/index.js
+++ @xen-orchestra/proxy/src/app/mixins/backups/index.js
@@ -142,19 +142,21 @@ export default class Backups {
         fetchPartitionFiles: [
           ({ disk: diskId, remote, partition: partitionId, paths }) => {
             const { promise, reject, resolve } = pDefer()
-            using(async function* () {
-              const adapter = yield this.getAdapter(remote)
-              const files = yield adapter.usePartitionFiles(diskId, partitionId, paths)
-              const zip = new ZipFile()
-              files.forEach(({ realPath, metadataPath }) => zip.addFile(realPath, metadataPath))
-              zip.end()
-              const { outputStream } = zip
-              resolve(outputStream)
-              await fromEvent(outputStream, 'end')
-            }).catch(error => {
-              warn(error)
-              reject(error)
-            })
+            using
+              .call(this, async function* () {
+                const adapter = yield this.getAdapter(remote)
+                const files = yield adapter.usePartitionFiles(diskId, partitionId, paths)
+                const zip = new ZipFile()
+                files.forEach(({ realPath, metadataPath }) => zip.addFile(realPath, metadataPath))
+                zip.end()
+                const { outputStream } = zip
+                resolve(outputStream)
+                await fromEvent(outputStream, 'end')
+              })
+              .catch(error => {
+                warn(error)
+                reject(error)
+              })
             return promise
           },
           {
