bb1fa6aa71896f7077e88a6bfe7e5980bcfff756
badrAZ <azizbibadr@gmail.com>
2021-02-19 17:01:09 +0100

    update implementation

--- @xen-orchestra/proxy/src/app/_decorateResult.js
+++ @xen-orchestra/proxy/src/app/_decorateResult.js
@@ -1,14 +1,10 @@
-// TODO: Move to @Vates
-
 // Applies passed transformer to the result returned by the method
 //
 // @decorateResult(transform)
 // method () {}
-const decorateResult = fn => (target, name, descriptor) => ({
+export const decorateResult = fn => (target, name, descriptor) => ({
   ...descriptor,
   value: function () {
     return fn.call(this, descriptor.value.apply(this, arguments), ...arguments)
   },
 })
-
-exports.decorateResult = decorateResult
