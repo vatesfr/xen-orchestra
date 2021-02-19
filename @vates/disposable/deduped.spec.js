b692db8b3671a6374d51478197e35cf69a62a7ec
badrAZ <azizbibadr@gmail.com>
2021-02-19 16:08:46 +0100

    update implementation

--- @vates/disposable/deduped.spec.js
+++ @vates/disposable/deduped.spec.js
@@ -19,4 +19,25 @@ describe('deduped()', () => {
     expect(v1).toBe(value)
     expect(v2).toBe(value)
   })
+
+  it('only disposes the source disposable when its all copies dispose', async () => {
+    const dispose = jest.fn()
+    const getResource = async () => ({
+      value: '',
+      dispose,
+    })
+
+    const dedupedGetResource = deduped(getResource)
+
+    const { dispose: d1 } = await dedupedGetResource()
+    const { dispose: d2 } = await dedupedGetResource()
+
+    d1()
+
+    expect(dispose).not.toHaveBeenCalled()
+
+    d2()
+
+    expect(dispose).toHaveBeenCalledTimes(1)
+  })
 })
