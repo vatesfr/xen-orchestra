faeae7a08888ce16f38cb8f9ec79097605dc079c
badrAZ <azizbibadr@gmail.com>
2021-02-23 13:50:06 +0100

    update implementation

--- packages/xo-server/src/xo-mixins/backups-remote-adapter.js
+++ packages/xo-server/src/xo-mixins/backups-remote-adapter.js
@@ -1,3 +1,8 @@
+import Disposable from 'promise-toolbox/Disposable'
+
+import { compose } from '@vates/compose'
+import { decorateWith } from '@vates/decorate-with'
+import { deduped } from '@vates/disposable/deduped'
 import { RemoteAdapter } from '@xen-orchestra/backups/RemoteAdapter'
 
 export default class BackupsRemoteAdapter {
@@ -6,6 +11,12 @@ export default class BackupsRemoteAdapter {
     this._config = backup
   }
 
+  // FIXME: invalidate cache on remote option change
+  @decorateWith(compose, function (resource) {
+    return this._app.debounceResource(resource)
+  })
+  @decorateWith(deduped, remote => [remote.url])
+  @decorateWith(Disposable.factory)
   async getBackupsRemoteAdapter(remote) {
     const app = this._app
     return new RemoteAdapter(await app.getRemoteHandler(remote), {
