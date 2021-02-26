vatesfr/xen-orchestra

commit 062fb3ba3074b47734220abdf35d55c65c8867b4

feat(xo-web/home/vm): ability to choose network for bulk migration within a pool (#5427)
See xoa-support#3118

packages/xo-web/src/common/xo/migrate-vms-modal/index.js
@@ -161,15 +161,10 @@ export default class MigrateVmsModalBody extends BaseComponent {
       mapVmsMapVifsNetworks[vm.id] = mapVifsNetworks
     })
 
-    // Map VM --> migration network
-    const mapVmsMigrationNetwork = mapValues(doNotMigrateVmVdis, doNotMigrateVdis =>
-      doNotMigrateVdis ? undefined : migrationNetworkId
-    )
-
     return {
       mapVmsMapVdisSrs,
       mapVmsMapVifsNetworks,
-      mapVmsMigrationNetwork,
+      migrationNetwork: migrationNetworkId,
       targetHost: host.id,
       vms,
     }
@@ -231,7 +226,8 @@ export default class MigrateVmsModalBody extends BaseComponent {
     vms => createCompare([pool => some(vms, vm => vm.$pool === pool.id)])
   )
 
-  _selectMigrationNetwork = migrationNetwork => this.setState({ migrationNetworkId: migrationNetwork.id })
+  _selectMigrationNetwork = migrationNetwork =>
+    this.setState({ migrationNetworkId: migrationNetwork == null ? undefined : migrationNetwork.id })
   _selectNetwork = network => this.setState({ networkId: network.id })
   _selectSr = sr => this.setState({ srId: sr.id })
   _toggleSmartVifMapping = () => this.setState({ smartVifMapping: !this.state.smartVifMapping })
@@ -263,7 +259,7 @@ export default class MigrateVmsModalBody extends BaseComponent {
             </Col>
           </SingleLineRow>
         </div>
-        {intraPool === false && (
+        {host !== undefined && (
           <div style={LINE_STYLE}>
             <SingleLineRow>
               <Col size={6}>{_('migrateVmSelectMigrationNetwork')}</Col>
@@ -271,10 +267,12 @@ export default class MigrateVmsModalBody extends BaseComponent {
                 <SelectNetwork
                   onChange={this._selectMigrationNetwork}
                   predicate={this._getMigrationNetworkPredicate()}
+                  required={!intraPool}
                   value={migrationNetworkId}
                 />
               </Col>
             </SingleLineRow>
+            {intraPool && <i>{_('optionalEntry')}</i>}
           </div>
         )}
         {host && (!intraPool || !noVdisMigration) && (

packages/xo-web/src/common/xo/index.js
@@ -1260,13 +1260,13 @@ export const migrateVms = vms =>
       return error(_('migrateVmNoTargetHost'), _('migrateVmNoTargetHostMessage'))
     }
 
-    const { mapVmsMapVdisSrs, mapVmsMapVifsNetworks, mapVmsMigrationNetwork, targetHost, vms } = params
+    const { mapVmsMapVdisSrs, mapVmsMapVifsNetworks, migrationNetwork, targetHost, vms } = params
     Promise.all(
       map(vms, ({ id }) =>
         _call('vm.migrate', {
           mapVdisSrs: mapVmsMapVdisSrs[id],
           mapVifsNetworks: mapVmsMapVifsNetworks[id],
-          migrationNetwork: mapVmsMigrationNetwork[id],
+          migrationNetwork,
           targetHost,
           vm: id,
         })

commit b9ce0bd99d23586474e2ad4a8088672bb176475e

fix(xo-web): fix 'mapValues is defined but never used' error (#5618)
Introduced by 062fb3ba3074b47734220abdf35d55c65c8867b4

packages/xo-web/src/common/xo/migrate-vms-modal/index.js
@@ -6,7 +6,6 @@ import filter from 'lodash/filter'
 import find from 'lodash/find'
 import isEmpty from 'lodash/isEmpty'
 import map from 'lodash/map'
-import mapValues from 'lodash/mapValues'
 import React from 'react'
 import some from 'lodash/some'
 import store from 'store'