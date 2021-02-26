vatesfr/xen-orchestra

commit 90cafa126fa127e5ca016cb106be559ab326c6c9

feat(xo-web/migrateVm): show error when no main SR selected (#5568)
See xoa-support#3355

packages/xo-web/src/common/xo/index.js
@@ -1247,8 +1247,16 @@ export const migrateVm = async (vm, host) => {
     return
   }
 
-  if (!params.targetHost) {
+  const { mapVdisSrs, migrationNetwork, sr, targetHost } = params
+
+  if (!targetHost) {
     return error(_('migrateVmNoTargetHost'), _('migrateVmNoTargetHostMessage'))
+  }
+
+  // Workaround to prevent VM's VDIs from unexpectedly migrating to the default SR
+  // if migration network is defined, the SR is required.
+  if (migrationNetwork !== undefined && sr === undefined) {
+    return error(_('migrateVmNoSr'), _('migrateVmNoSrMessage'))
   }
 
   try {

packages/xo-web/src/common/intl/messages.js
@@ -1718,6 +1718,8 @@ const messages = {
   migrateVmNetwork: 'Network',
   migrateVmNoTargetHost: 'No target host',
   migrateVmNoTargetHostMessage: 'A target host is required to migrate a VM',
+  migrateVmNoSr: 'SR required',
+  migrateVmNoSrMessage: 'A destination SR is required',
   migrateVmNoDefaultSrError: 'No default SR',
   migrateVmNotConnectedDefaultSrError: 'Default SR not connected to host',
   chooseSrForEachVdisModalSelectSr: 'For each VDI, select an SR (optional)',

commit c82cee25a58c4d9c8ab3758b86833240fa2f110f

fix(xo-web): fix 'mapVdisSrs is assigned a value but never used' error (#5617)
Introduced by 90cafa126fa127e5ca016cb106be559ab326c6c9

packages/xo-web/src/common/xo/index.js
@@ -1247,7 +1247,7 @@ export const migrateVm = async (vm, host) => {
     return
   }
 
-  const { mapVdisSrs, migrationNetwork, sr, targetHost } = params
+  const { migrationNetwork, sr, targetHost } = params
 
   if (!targetHost) {
     return error(_('migrateVmNoTargetHost'), _('migrateVmNoTargetHostMessage'))