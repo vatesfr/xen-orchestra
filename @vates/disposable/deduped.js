vatesfr/xen-orchestra

commit 43aad3d1174d999518f1ccce5d62ef2c411ebab4

feat(disposable/deduped): works with sync factories

@vates/disposable/deduped.js
@@ -1,8 +1,8 @@
 const ensureArray = require('ensure-array')
 const { MultiKeyMap } = require('@vates/multi-key-map')
 
-function State(pGetNewDisposable) {
-  this.pGetNewDisposable = pGetNewDisposable
+function State(factory) {
+  this.factory = factory
   this.users = 0
 }
 
@@ -15,29 +15,38 @@ exports.deduped = (factory, keyFn = (...args) => args) =>
       const keys = ensureArray(keyFn.apply(this, arguments))
       let state = states.get(keys)
       if (state === undefined) {
-        const pDisposable = factory.apply(this, arguments)
-        pDisposable.catch(() => {
-          states.delete(keys)
-        })
+        const result = factory.apply(this, arguments)
 
-        state = new State(
-          pDisposable.then(({ value, dispose }) => {
-            const disposeWrapper = () => {
+        const createFactory = ({ value, dispose }) => {
+          const wrapper = {
+            dispose() {
               if (--state.users === 0) {
                 states.delete(keys)
                 return dispose()
               }
-            }
-            return () => ({
-              dispose: disposeWrapper,
-              value,
-            })
+            },
+            value,
+          }
+
+          return () => {
+            ++state.users
+            return wrapper
+          }
+        }
+
+        if (typeof result.then !== 'function') {
+          state = new State(createFactory(result))
+        } else {
+          result.catch(() => {
+            states.delete(keys)
           })
-        )
+          const pFactory = result.then(createFactory)
+          state = new State(() => pFactory.then(call))
+        }
+
         states.set(keys, state)
       }
 
-      ++state.users
-      return state.pGetNewDisposable.then(call)
+      return state.factory()
     }
   })()

@vates/disposable/deduped.spec.js
@@ -40,4 +40,24 @@ describe('deduped()', () => {
 
     expect(dispose).toHaveBeenCalledTimes(1)
   })
+
+  it('works with sync factory', () => {
+    const value = {}
+    const dispose = jest.fn()
+    const dedupedGetResource = deduped(() => ({ value, dispose }))
+
+    const d1 = dedupedGetResource()
+    expect(d1.value).toBe(value)
+
+    const d2 = dedupedGetResource()
+    expect(d2.value).toBe(value)
+
+    d1.dispose()
+
+    expect(dispose).not.toHaveBeenCalled()
+
+    d2.dispose()
+
+    expect(dispose).toHaveBeenCalledTimes(1)
+  })
 })

commit a4b209c6549eb03e5d85607c4cc32328d54f8a0d

fix(disposable/deduped): race condition when disposed during acquisition
Introduced in 43aad3d11

@vates/disposable/deduped.js
@@ -29,7 +29,6 @@ exports.deduped = (factory, keyFn = (...args) => args) =>
           }
 
           return () => {
-            ++state.users
             return wrapper
           }
         }
@@ -47,6 +46,7 @@ exports.deduped = (factory, keyFn = (...args) => args) =>
         states.set(keys, state)
       }
 
+      ++state.users
       return state.factory()
     }
   })()

@vates/disposable/deduped.spec.js
@@ -60,4 +60,17 @@ describe('deduped()', () => {
 
     expect(dispose).toHaveBeenCalledTimes(1)
   })
+
+  it('no race condition on dispose before async acquisition', async () => {
+    const dispose = jest.fn()
+    const dedupedGetResource = deduped(async () => ({ value: 42, dispose }))
+
+    const d1 = await dedupedGetResource()
+
+    dedupedGetResource()
+
+    d1.dispose()
+
+    expect(dispose).not.toHaveBeenCalled()
+  })
 })