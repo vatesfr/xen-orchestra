bf6010cea2c6114f342e1ba4a76f6f05cb06492d
Pierre Donias <pierre.donias@gmail.com>
2021-03-16 17:33:10 +0100

    feat(xo-lite): Reaclette types

---
+++ @xen-orchestra/lite/src/components/Button.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { withState } from 'reaclette'
+
+interface ParentState {}
+
+interface State {}
+
+interface Props {
+  label: string
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void
+}
+
+interface ParentEffects {}
+
+interface Effects {}
+
+interface Computed {}
+
+const Button = withState<State, Props, Effects, Computed, ParentState, ParentEffects>({}, ({ label, onClick }) => (
+  <button onClick={onClick}>{label}</button>
+))
+
+export default Button
