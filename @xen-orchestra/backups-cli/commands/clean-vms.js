09eba89f853a0b548b1f232c634899f002ca9a86
badrAZ <azizbibadr@gmail.com>
2021-03-22 16:44:18 +0100

    update implementation

--- @xen-orchestra/backups-cli/commands/clean-vms.js
+++ @xen-orchestra/backups-cli/commands/clean-vms.js
@@ -7,7 +7,6 @@ let merge, remove
 
 const asyncMap = require('lodash/curryRight')(require('@xen-orchestra/async-map').asyncMap)
 const getopts = require('getopts')
-const lockfile = require('proper-lockfile')
 const { RemoteAdapter } = require('@xen-orchestra/backups/RemoteAdapter')
 const { resolve } = require('path')
 
@@ -31,16 +30,10 @@ module.exports = async function main(args) {
   ;({ remove, merge } = opts)
   await asyncMap(opts._, async vmDir => {
     vmDir = resolve(vmDir)
-
-    // TODO: implement this in `xo-server`, not easy because not compatible with
-    // `@xen-orchestra/fs`.
-    const release = await lockfile.lock(vmDir)
     try {
       await adapter.cleanVm(vmDir, { remove, merge, onLog: log => console.warn(log) })
     } catch (error) {
       console.error('adapter.cleanVm', vmDir, error)
-    } finally {
-      await release()
     }
   })
 }
