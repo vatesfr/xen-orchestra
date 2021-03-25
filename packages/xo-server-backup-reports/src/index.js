b1ef59b95d5df034ff6e6d6975ea3fce797fea33
badrAZ <azizbibadr@gmail.com>
2021-03-25 09:34:40 +0100

    update implementation

--- packages/xo-server-backup-reports/src/index.js
+++ packages/xo-server-backup-reports/src/index.js
@@ -375,7 +375,7 @@ class BackupReportsXoPlugin {
       })
     }
 
-    const failedVmsText = []
+    const failedTasksText = []
     const skippedVmsText = []
     const successfulVmsText = []
     const interruptedVmsText = []
@@ -394,7 +394,7 @@ class BackupReportsXoPlugin {
       const { type, id } = taskLog.data ?? {}
       if (taskLog.message === 'get SR record' || taskLog.message === 'get remote adapter') {
         ++nFailures
-        failedVmsText.push(
+        failedTasksText.push(
           // It will ensure that it will never be in a nested list
           ''
         )
@@ -402,20 +402,20 @@ class BackupReportsXoPlugin {
         try {
           if (type === 'SR') {
             const { name_label: name, uuid } = xo.getObject(id)
-            failedVmsText.push(`### ${name}`, '', `- **UUID**: ${uuid}`)
+            failedTasksText.push(`### ${name}`, '', `- **UUID**: ${uuid}`)
             nagiosText.push(`[(${type} failed) ${name} : ${taskLog.result.message} ]`)
           } else {
             const { name } = await xo.getRemote(id)
-            failedVmsText.push(`### ${name}`, '', `- **UUID**: ${id}`)
+            failedTasksText.push(`### ${name}`, '', `- **UUID**: ${id}`)
             nagiosText.push(`[(${type} failed) ${name} : ${taskLog.result.message} ]`)
           }
         } catch (error) {
           logger.warn(error)
-          failedVmsText.push(`### ${UNKNOWN_ITEM}`, '', `- **UUID**: ${id}`)
+          failedTasksText.push(`### ${UNKNOWN_ITEM}`, '', `- **UUID**: ${id}`)
           nagiosText.push(`[(${type} failed) ${id} : ${taskLog.result.message} ]`)
         }
 
-        failedVmsText.push(
+        failedTasksText.push(
           `- **Type**: ${type}`,
           ...getTemporalDataMarkdown(taskLog.end, taskLog.start, formatDate),
           ...getWarningsMarkdown(taskLog.warnings),
@@ -548,14 +548,14 @@ class BackupReportsXoPlugin {
           nagiosText.push(`[(Skipped) ${vm !== undefined ? vm.name_label : 'undefined'} : ${taskLog.result.message} ]`)
         } else {
           ++nFailures
-          failedVmsText.push(...text, `- **Error**: ${taskLog.result.message}`)
+          failedTasksText.push(...text, `- **Error**: ${taskLog.result.message}`)
 
           nagiosText.push(`[(Failed) ${vm !== undefined ? vm.name_label : 'undefined'} : ${taskLog.result.message} ]`)
         }
       } else {
         if (taskLog.status === 'failure') {
           ++nFailures
-          failedVmsText.push(...text, ...subText)
+          failedTasksText.push(...text, ...subText)
           nagiosText.push(`[${vm !== undefined ? vm.name_label : 'undefined'}: (failed)[${failedSubTasks.toString()}]]`)
         } else if (taskLog.status === 'interrupted') {
           ++nInterrupted
@@ -567,8 +567,8 @@ class BackupReportsXoPlugin {
       }
     }
 
-    const nVms = log.tasks.length
-    const nSuccesses = nVms - nFailures - nSkipped - nInterrupted
+    const nTasks = log.tasks.length
+    const nSuccesses = nTasks - nFailures - nSkipped - nInterrupted
     const markdown = [
       `##  Global status: ${log.status}`,
       '',
@@ -576,7 +576,7 @@ class BackupReportsXoPlugin {
       `- **Run ID**: ${log.id}`,
       `- **mode**: ${mode}`,
       ...getTemporalDataMarkdown(log.end, log.start, formatDate),
-      `- **Successes**: ${nSuccesses} / ${nVms}`,
+      `- **Successes**: ${nSuccesses} / ${nTasks}`,
       globalTransferSize !== 0 && `- **Transfer size**: ${formatSize(globalTransferSize)}`,
       globalMergeSize !== 0 && `- **Merge size**: ${formatSize(globalMergeSize)}`,
       ...getWarningsMarkdown(log.warnings),
@@ -584,7 +584,7 @@ class BackupReportsXoPlugin {
     ]
 
     if (nFailures !== 0) {
-      markdown.push('---', '', `## ${nFailures} Failure${nFailures === 1 ? '' : 's'}`, '', ...failedVmsText)
+      markdown.push('---', '', `## ${nFailures} Failure${nFailures === 1 ? '' : 's'}`, '', ...failedTasksText)
     }
 
     if (nSkipped !== 0) {
