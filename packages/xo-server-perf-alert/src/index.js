adf16d180f64bf542d625e1fc71e184dd5c3355f
BARHTAOUI <rajaa.barhtaoui@gmail.com>
2021-03-29 13:10:17 +0200

    oneOf

--- packages/xo-server-perf-alert/src/index.js
+++ packages/xo-server-perf-alert/src/index.js
@@ -196,6 +196,7 @@ export const configurationSchema = {
             enum: [60, 600],
           },
         },
+        oneOf: [{ required: ['uuids'] }, { properties: { smartMode: { const: true } }, required: ['smartMode'] }],
       },
     },
     vmMonitors: {
@@ -246,6 +247,7 @@ export const configurationSchema = {
             enum: [60, 600],
           },
         },
+        oneOf: [{ required: ['uuids'] }, { properties: { smartMode: { const: true } }, required: ['smartMode'] }],
       },
     },
     srMonitors: {
@@ -288,6 +290,7 @@ export const configurationSchema = {
             default: 80,
           },
         },
+        oneOf: [{ required: ['uuids'] }, { properties: { smartMode: { const: true } }, required: ['smartMode'] }],
       },
     },
     toEmails: {
