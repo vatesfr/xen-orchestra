06a8424bd2b2c12fcfa731bd132e6e58541fa85c
mathieuRA <contact@mathieu-raisin.fr>
2021-03-31 09:25:06 +0200

    feat(xo-lite): possibility to scale the VM console

---
+++ @xen-orchestra/lite/src/components/RangeInput.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+
+interface PropsRangeInput {
+  defaultValue: number
+  max: number
+  min: number
+  onChange: (value: number) => void
+  step: number
+}
+const RangeInput: React.FC<PropsRangeInput> = ({defaultValue, max,min, onChange, step}) => {
+  const [currentValue, setCurrentValue] = React.useState(defaultValue)
+  const input = React.createRef<HTMLInputElement>()
+
+  const _onChange = React.useCallback(() => {
+    if(input.current === null){
+      return
+    }
+    setCurrentValue(+input.current.value)
+    onChange(currentValue)
+  },[input])
+
+  return <input max={max} min={min} onChange={_onChange} ref={input} step={step} type="range" value={currentValue} />
+}
+
+export default RangeInput
