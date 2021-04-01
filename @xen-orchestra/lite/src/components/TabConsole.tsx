877a2791f3c697f333477bc84fa52630524a0e39
mathieuRA <contact@mathieu-raisin.fr>
2021-04-01 17:48:45 +0200

    use % value instead of px value

--- @xen-orchestra/lite/src/components/TabConsole.tsx
+++ @xen-orchestra/lite/src/components/TabConsole.tsx
@@ -29,32 +29,33 @@ const TabConsole = withState<State, Props, Effects, Computed, ParentState, Paren
   {
     initialState: () => ({
       consoleSize: {
-        height: 768,
-        width: 1024,
+        // Value in percent
+        height: 100,
+        width: 100,
       },
     }),
     effects: {
       scaleConsole: function (value) {
         this.state.consoleSize = {
-          height: 768 * value,
-          width: 1024 * value,
+          height: value,
+          width: value,
         }
         // With "scaleViewport", the canvas occupies all
-        // available space of his container.
+        // available space of its container.
         // But when the size of the container is changed,
-        // the canvas size aren't updated
+        // the canvas size isn't updated
         // Issue https://github.com/novnc/noVNC/issues/1364
         // PR https://github.com/novnc/noVNC/pull/1365
         window.dispatchEvent(new UIEvent('resize'))
       },
     },
   },
-  ({ effects, vmId }) => {
+  ({ state, effects, vmId }) => {
     return (
-      <>
-        <RangeInput defaultValue={1} max={3} min={0.1} onChange={effects.scaleConsole} step={0.1} />
-        <Console vmId={vmId} />
-      </>
+      <div style={{ height: '100vh' }}>
+        <RangeInput defaultValue={100} max={100} min={1} onChange={effects.scaleConsole} step={1} />
+        <Console vmId={vmId} scale={state.consoleSize} />
+      </div>
     )
   }
 )
