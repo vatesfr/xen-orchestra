b4fbe8df07f5bcf65e5082cb640dd893720527cc
Julien Fontanet <julien.fontanet@isonoe.net>
2021-04-09 11:16:17 +0200

    feat(xo-server/api): explicitely allow $type and enumNames in schemas

--- packages/xo-server/src/xo-mixins/plugins.js
+++ packages/xo-server/src/xo-mixins/plugins.js
@@ -15,7 +15,7 @@ export default class {
     this._ajv = new Ajv({
       strict: 'log',
       useDefaults: true,
-    })
+    }).addVocabulary(['$type', 'enumNames'])
     this._plugins = { __proto__: null }
 
     this._pluginsMetadata = new PluginsMetadata({
