vatesfr/xen-orchestra

commit 9226c6cac193f9ed9dd9a4fd1e2c7a9833e7f6c6

fix(xo-server/api): don't log `host.stats` errors (#5553)
See xoa-support#3323

This avoids flooding the logs with ECONNREFUSED errors when the host's toolstack
is restarted

packages/xo-server/src/xo-mixins/api.js
@@ -342,10 +342,13 @@ export default class Api {
         Date.now() - startTime
       )}] =!> ${error}`
 
-      // 2020-07-10: Work-around: many kinds of error can be triggered by this
-      // method, which can generates a lot of logs due to the fact that xo-web
-      // uses 5s active subscriptions to call it
-      if (name !== 'pool.listMissingPatches') {
+      // 2020-07-10: Work-around: many kinds of error can be triggered by
+      // 'pool.listMissingPatches' method, which can generates a lot of logs due to the fact that xo-web
+      // uses 5s active subscriptions to call it.
+      // 2021-02-11: Work-around: ECONNREFUSED error can be triggered by
+      // 'host.stats' method because there is no connection to the host during a
+      // toolstack restart and xo-web may call it often
+      if (name !== 'pool.listMissingPatches' || name !== 'host.stats') {
         this._logger.error(message, {
           ...data,
           duration: Date.now() - startTime,

commit eaecba7ec8c422e631cfd4f4665b669239f995c5

fix(xo-server/api): dont log pool.listMissingPatches & host.stats errors
Introduced by 9226c6cac

packages/xo-server/src/xo-mixins/api.js
@@ -348,7 +348,7 @@ export default class Api {
       // 2021-02-11: Work-around: ECONNREFUSED error can be triggered by
       // 'host.stats' method because there is no connection to the host during a
       // toolstack restart and xo-web may call it often
-      if (name !== 'pool.listMissingPatches' || name !== 'host.stats') {
+      if (name !== 'pool.listMissingPatches' && name !== 'host.stats') {
         this._logger.error(message, {
           ...data,
           duration: Date.now() - startTime,