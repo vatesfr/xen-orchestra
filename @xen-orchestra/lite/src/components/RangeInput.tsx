7bb1969ff12e06cbf06389b9c899ccaa6c7946ec
mathieuRA <contact@mathieu-raisin.fr>
2021-04-12 12:29:01 +0200

    fixes

--- @xen-orchestra/lite/src/components/RangeInput.tsx
+++ @xen-orchestra/lite/src/components/RangeInput.tsx
@@ -1,53 +1,33 @@
 import React from 'react'
 import { withState } from 'reaclette'
 
-type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
-
-interface InputAttributes extends React.AllHTMLAttributes<HTMLInputElement> {
-  max: number
-  min: number
-  step: number
-}
-
 interface ParentState {}
 
 interface State {
-  currentValue: number
   input: React.RefObject<HTMLInputElement>
 }
 
-interface Props {
-  defaultValue: number
-  inputAttribues: Omit<InputAttributes, 'onChange'>
-  onChange: (value: number) => void
+interface Props extends React.AllHTMLAttributes<HTMLInputElement> {
+  max: number
+  min: number
+  onChange: React.ChangeEventHandler<HTMLInputElement>
+  step: number
+  value: number
 }
 
 interface ParentEffects {}
 
-interface Effects {
-  _onChange: React.ChangeEventHandler<HTMLInputElement>
-}
+interface Effects {}
 
 interface Computed {}
 
 const RangeInput = withState<State, Props, Effects, Computed, ParentState, ParentEffects>(
   {
-    initialState: props => ({
-      currentValue: props.defaultValue,
+    initialState: () => ({
       input: React.createRef(),
     }),
-    effects: {
-      _onChange: function (e) {
-        const { value } = e.currentTarget
-
-        this.state.currentValue = +value
-        this.props.onChange(+value)
-      },
-    },
   },
-  ({ effects, inputAttribues, state }) => (
-    <input onChange={effects._onChange} ref={state.input} type='range' value={state.currentValue} {...inputAttribues} />
-  )
+  ({ effects, resetState, state, ...props }) => <input ref={state.input} type='range' {...props} />
 )
 
 export default RangeInput
