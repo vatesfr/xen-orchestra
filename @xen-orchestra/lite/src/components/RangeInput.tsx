e359ab739ebbc642aa514a33e406f2cadb345276
mathieuRA <contact@mathieu-raisin.fr>
2021-04-15 16:42:06 +0200

    remove required props

--- @xen-orchestra/lite/src/components/RangeInput.tsx
+++ @xen-orchestra/lite/src/components/RangeInput.tsx
@@ -2,13 +2,7 @@ import React from 'react'
 
 type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
 
-interface Props extends Omit<React.ComponentPropsWithoutRef<'input'>, 'type'> {
-  max: number
-  min: number
-  onChange: React.ChangeEventHandler<HTMLInputElement>
-  step: number
-  value: number
-}
+interface Props extends Omit<React.ComponentPropsWithoutRef<'input'>, 'type'> {}
 
 const RangeInput: React.FC<Props> = React.memo(props => <input {...props} type='range' />)
 
