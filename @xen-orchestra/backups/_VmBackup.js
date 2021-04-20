vatesfr/xen-orchestra

commit 7aa10ef4be26b913c76f1157bcbcac1605f400c5

fix(backups): don't snapshot in case of offline backup (#5739)
Introduced by 0811da90146647aa2c0894d39b4bc41e39859453

@xen-orchestra/backups/_VmBackup.js
@@ -1,3 +1,4 @@
+const assert = require('assert')
 const findLast = require('lodash/findLast')
 const ignoreErrors = require('promise-toolbox/ignoreErrors')
 const keyBy = require('lodash/keyBy')
@@ -114,7 +115,10 @@ exports.VmBackup = class VmBackup {
 
     const settings = this._settings
 
-    const doSnapshot = this._isDelta || vm.power_state === 'Running' || settings.snapshotRetention !== 0
+    assert.notStrictEqual(settings.offlineBackup, settings.snapshotRetention !== 0)
+
+    const doSnapshot =
+      this._isDelta || (!settings.offlineBackup && vm.power_state === 'Running') || settings.snapshotRetention !== 0
     if (doSnapshot) {
       await Task.run({ name: 'snapshot' }, async () => {
         if (!settings.bypassVdiChainsCheck) {

commit 2aca775907be287ab210e1c4f330c747dc5ac681

fix(backups/VmBackup#_snapshot): dont fail on !offlineBackup && !snapshotRetention
Introduced by 7aa10ef4be26b913c76f1157bcbcac1605f400c5

@xen-orchestra/backups/_VmBackup.js
@@ -1,4 +1,3 @@
-const assert = require('assert')
 const findLast = require('lodash/findLast')
 const ignoreErrors = require('promise-toolbox/ignoreErrors')
 const keyBy = require('lodash/keyBy')
@@ -114,8 +113,6 @@ exports.VmBackup = class VmBackup {
     const xapi = this._xapi
 
     const settings = this._settings
-
-    assert.notStrictEqual(settings.offlineBackup, settings.snapshotRetention !== 0)
 
     const doSnapshot =
       this._isDelta || (!settings.offlineBackup && vm.power_state === 'Running') || settings.snapshotRetention !== 0