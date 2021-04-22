091c9c5ff6875f2f0b6d9450ce060430cd8744f2
Julien Fontanet <julien.fontanet@isonoe.net>
2021-04-22 15:21:49 +0200

    WiP: feat(vhd-lib/mergeVhd): continuable

--- packages/vhd-lib/src/merge.js
+++ packages/vhd-lib/src/merge.js
@@ -5,6 +5,7 @@ import concurrency from 'limit-concurrency-decorator'
 import noop from './_noop'
 
 import Vhd from './vhd'
+import { basename, dirname } from 'path'
 import { DISK_TYPE_DIFFERENCING, DISK_TYPE_DYNAMIC } from './_constants'
 
 // Merge vhd child into vhd parent.
@@ -17,6 +18,8 @@ export default concurrency(2)(async function merge(
   childPath,
   { onProgress = noop } = {}
 ) {
+  const mergeStatePath = dirname(parentPath) + '/' + '.' + basename(parentPath) + '.merge.json'
+
   const parentFd = await parentHandler.openFile(parentPath, 'r+')
   try {
     const parentVhd = new Vhd(parentHandler, parentFd)
@@ -24,14 +27,40 @@ export default concurrency(2)(async function merge(
     try {
       const childVhd = new Vhd(childHandler, childFd)
 
+      let mergeState = await parentHandler.readFile(mergeStatePath).catch(error => {
+        if (error.code !== 'ENOENT') {
+          throw error
+        }
+        // no merge state in case of missing file
+      })
+
       // Reading footer and header.
-      await Promise.all([parentVhd.readHeaderAndFooter(), childVhd.readHeaderAndFooter()])
+      await Promise.all([
+        parentVhd.readHeaderAndFooter(
+          // dont check VHD is complete if recovering a merge
+          mergeState === undefined
+        ),
 
-      assert(childVhd.header.blockSize === parentVhd.header.blockSize)
+        childVhd.readHeaderAndFooter(),
+      ])
 
-      const parentDiskType = parentVhd.footer.diskType
-      assert(parentDiskType === DISK_TYPE_DIFFERENCING || parentDiskType === DISK_TYPE_DYNAMIC)
-      assert.strictEqual(childVhd.footer.diskType, DISK_TYPE_DIFFERENCING)
+      if (mergeState !== undefined) {
+        // ensure the correct merge will be continued
+        assert.strictEqual(parentVhd.header.checksum, mergeState.parent.header.checksum)
+        assert.strictEqual(childVhd.header.checksum, mergeState.child.header.checksum)
+      } else {
+        assert.strictEqual(childVhd.header.blockSize, parentVhd.header.blockSize)
+
+        const parentDiskType = parentVhd.footer.diskType
+        assert(parentDiskType === DISK_TYPE_DIFFERENCING || parentDiskType === DISK_TYPE_DYNAMIC)
+        assert.strictEqual(childVhd.footer.diskType, DISK_TYPE_DIFFERENCING)
+
+        mergeState = {
+          child: { header: childVhd.header.checksum },
+          parent: { header: parentVhd.header.checksum },
+          currentBlock: 0,
+        }
+      }
 
       // Read allocation table of child/parent.
       await Promise.all([parentVhd.readBlockAllocationTable(), childVhd.readBlockAllocationTable()])
