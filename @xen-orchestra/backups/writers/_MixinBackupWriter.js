29fd2ff5e99d941f7e80e89f13b546d3d23b01fd
Julien Fontanet <julien.fontanet@isonoe.net>
2021-04-26 09:23:20 +0200

    feat(backups): lock VM dir during backup (#5746)
    May fix xoa-support#3387

--- @xen-orchestra/backups/writers/_MixinBackupWriter.js
+++ @xen-orchestra/backups/writers/_MixinBackupWriter.js
@@ -11,15 +11,25 @@ exports.MixinBackupWriter = (BaseClass = Object) =>
 
       this._adapter = rest.backup.remoteAdapters[remoteId]
       this._remoteId = remoteId
+      this._lock = undefined
     }
 
     async beforeBackup() {
+      const adapter = this._adapter
+      const vmBackupDir = getVmBackupDir(this._backup.vm.uuid)
+
       try {
-        await this._adapter.cleanVm(getVmBackupDir(this._backup.vm.uuid), { remove: true, merge: true, onLog: debug })
+        await adapter.cleanVm(vmBackupDir, { remove: true, merge: true, onLog: debug })
       } catch (error) {
         if (error?.code !== 'ENOENT') {
           throw error
         }
       }
+
+      this._lock = await adapter.handler.lock(vmBackupDir)
+    }
+
+    async afterBackup() {
+      await this._lock.dispose()
     }
   }
