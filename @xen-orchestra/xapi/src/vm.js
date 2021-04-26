157c81b0e9f1b61b43c3ed1763097b3e7f19b427
badrAZ <azizbibadr@gmail.com>
2021-04-26 17:29:14 +0200

    fix(@xen-orchestra/xapi#VM_import): ensure onVmCreation is called (#5747)
    It was not called if the import task was not received (for instance because the import was very fast).

--- @xen-orchestra/xapi/src/vm.js
+++ @xen-orchestra/xapi/src/vm.js
@@ -1,5 +1,6 @@
 const CancelToken = require('promise-toolbox/CancelToken.js')
 const groupBy = require('lodash/groupBy.js')
+const ignoreErrors = require('promise-toolbox/ignoreErrors')
 const pickBy = require('lodash/pickBy.js')
 const omit = require('lodash/omit.js')
 const pCatch = require('promise-toolbox/catch.js')
@@ -425,16 +426,29 @@ module.exports = class Vm {
       query.sr_id = srRef
     }
     if (onVmCreation != null) {
-      this.waitObject(
+      const original = onVmCreation
+      onVmCreation = vm => {
+        if (onVmCreation !== undefined) {
+          onVmCreation = undefined
+          stopWatch()
+          return original(vm)
+        }
+      }
+
+      const stopWatch = this.waitObject(
         obj => obj != null && obj.current_operations != null && taskRef in obj.current_operations,
         onVmCreation
       )
     }
     try {
-      return await this.putResource(stream, '/import/', {
+      const ref = await this.putResource(stream, '/import/', {
         query,
         task: taskRef,
       }).then(extractOpaqueRef)
+      if (onVmCreation != null) {
+        ignoreErrors.call(this.getRecord('VM', ref).then(onVmCreation))
+      }
+      return ref
     } catch (error) {
       // augment the error with as much relevant info as possible
       const [poolMaster, sr] = await Promise.all([
