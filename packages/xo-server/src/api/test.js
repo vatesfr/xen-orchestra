vatesfr/xen-orchestra

commit bdb0ca836c1b23ebf95d8a025f6d18f7f1eeb1f2

feat(xo-server): remove legacy backups (#5735)
BREAKING: all `backup.*` API methods removed

packages/xo-server/src/xapi/index.js
@@ -22,7 +22,6 @@ import { filter, find, flatMap, flatten, groupBy, identity, includes, isEmpty, n
 import { Ref } from 'xen-api'
 import { satisfies as versionSatisfies } from 'semver'
 
-import createSizeStream from '../size-stream.js'
 import ensureArray from '../_ensureArray.js'
 import fatfsBuffer, { init as fatfsBufferInit } from '../fatfs-buffer.js'
 import { asyncMapValues } from '../_asyncMapValues.js'
@@ -411,19 +410,15 @@ export default class Xapi extends XapiBase {
     }
 
     const sr = targetXapi.getObject(targetSrId)
-    let stream = await this.exportVm(vmId, {
+    const stream = await this.exportVm(vmId, {
       compress,
     })
-
-    const sizeStream = createSizeStream()
-    stream = stream.pipe(sizeStream)
 
     const onVmCreation = nameLabel !== undefined ? vm => vm.set_name_label(nameLabel) : null
 
     const vm = await targetXapi._getOrWaitObject(await targetXapi._importVm(stream, sr, onVmCreation))
 
     return {
-      size: sizeStream.size,
       vm,
     }
   }

packages/xo-server/src/xo-mixins/backups-ng/index.js
@@ -16,8 +16,6 @@ import { handleBackupLog } from '../../_handleBackupLog.js'
 import { unboxIdsFromPattern } from '../../utils.js'
 import { waitAll } from '../../_waitAll.js'
 
-import { translateLegacyJob } from './migration.js'
-
 const log = createLogger('xo:xo-mixins:backups-ng')
 
 const parseVmBackupId = id => {
@@ -518,10 +516,5 @@ export default class BackupNg {
     )
 
     return backupsByVmByRemote
-  }
-
-  async migrateLegacyBackupJob(jobId) {
-    const [job, schedules] = await Promise.all([this._app.getJob(jobId, 'call'), this._app.getAllSchedules()])
-    await this._app.updateJob(translateLegacyJob(job, schedules), false)
   }
 }

packages/xo-server/src/api/backup-ng.js
@@ -52,16 +52,6 @@ export function getSuggestedExcludedTags() {
   return ['Continuous Replication', 'Disaster Recovery', 'XOSAN', this.config.get('xo-proxy.vmTag')]
 }
 
-export function migrateLegacyJob({ id }) {
-  return this.migrateLegacyBackupJob(id)
-}
-migrateLegacyJob.permission = 'admin'
-migrateLegacyJob.params = {
-  id: {
-    type: 'string',
-  },
-}
-
 export function deleteJob({ id }) {
   return this.deleteBackupNgJob(id)
 }

packages/xo-server/src/api/backup.js
@@ -1,99 +1,0 @@
-import archiver from 'archiver'
-import { basename } from 'path'
-import { createLogger } from '@xen-orchestra/log'
-import { format } from 'json-rpc-peer'
-import { forEach } from 'lodash'
-
-const log = createLogger('xo:backup')
-
-// ===================================================================
-
-export function list({ remote }) {
-  return this.listVmBackups(remote)
-}
-
-list.permission = 'admin'
-list.params = {
-  remote: { type: 'string' },
-}
-
-// -------------------------------------------------------------------
-
-export function scanDisk({ remote, disk }) {
-  return this.scanDiskBackup(remote, disk)
-}
-
-scanDisk.permission = 'admin'
-scanDisk.params = {
-  remote: { type: 'string' },
-  disk: { type: 'string' },
-}
-
-// -------------------------------------------------------------------
-
-export function scanFiles({ remote, disk, partition, path }) {
-  return this.scanFilesInDiskBackup(remote, disk, partition, path)
-}
-
-scanFiles.permission = 'admin'
-scanFiles.params = {
-  remote: { type: 'string' },
-  disk: { type: 'string' },
-  partition: { type: 'string', optional: true },
-  path: { type: 'string' },
-}
-
-// -------------------------------------------------------------------
-
-function handleFetchFiles(req, res, { remote, disk, partition, paths, format: archiveFormat }) {
-  return this.fetchFilesInDiskBackup(remote, disk, partition, paths).then(files => {
-    res.setHeader('content-disposition', 'attachment')
-    res.setHeader('content-type', 'application/octet-stream')
-
-    const nFiles = paths.length
-
-    // Send lone file directly
-    if (nFiles === 1) {
-      files[0].pipe(res)
-      return
-    }
-
-    const archive = archiver(archiveFormat)
-    archive.on('error', error => {
-      log.error(error)
-      res.end(format.error(0, error))
-    })
-
-    forEach(files, file => {
-      archive.append(file, { name: basename(file.path) })
-    })
-    archive.finalize()
-
-    archive.pipe(res)
-  })
-}
-
-export async function fetchFiles({ format = 'zip', ...params }) {
-  const fileName = params.paths.length > 1 ? `restore_${new Date().toJSON()}.${format}` : basename(params.paths[0])
-
-  return this.registerHttpRequest(
-    handleFetchFiles,
-    { ...params, format },
-    {
-      suffix: '/' + encodeURIComponent(fileName),
-    }
-  ).then(url => ({ $getFrom: url }))
-}
-
-fetchFiles.permission = 'admin'
-fetchFiles.params = {
-  remote: { type: 'string' },
-  disk: { type: 'string' },
-  format: { type: 'string', optional: true },
-  partition: { type: 'string', optional: true },
-  paths: {
-    type: 'array',
-    items: { type: 'string' },
-    minLength: 1,
-  },
-}

packages/xo-server/src/api/remote.js
@@ -31,17 +31,6 @@ test.description = 'Performs a read/write matching test on a remote point'
 test.params = {
   id: { type: 'string' },
 }
-
-export async function list({ id }) {
-  return this.listRemoteBackups(id)
-}
-
-list.permission = 'admin'
-list.description = 'Lists the files found in a remote point'
-list.params = {
-  id: { type: 'string' },
-}
-
 export function create(props) {
   return this.createRemote(props)
 }

packages/xo-server/src/lvm.js
@@ -1,27 +1,0 @@
-import execa from 'execa'
-import splitLines from 'split-lines'
-import { createParser } from 'parse-pairs'
-
-// ===================================================================
-
-const parse = createParser({
-  keyTransform: key => key.slice(5).toLowerCase(),
-})
-const makeFunction = command => async (fields, ...args) => {
-  const { stdout } = await execa(command, [
-    '--noheading',
-    '--nosuffix',
-    '--nameprefixes',
-    '--unbuffered',
-    '--units',
-    'b',
-    '-o',
-    String(fields),
-    ...args,
-  ])
-
-  return splitLines(stdout).map(Array.isArray(fields) ? parse : line => parse(line)[fields])
-}
-
-export const lvs = makeFunction('lvs')
-export const pvs = makeFunction('pvs')

packages/xo-server/src/size-stream.js
@@ -1,12 +1,0 @@
-import through2 from 'through2'
-
-const createSizeStream = () => {
-  const wrapper = through2((chunk, enc, cb) => {
-    wrapper.size += chunk.length
-    cb(null, chunk)
-  })
-  wrapper.size = 0
-  return wrapper
-}
-
-export { createSizeStream as default }

packages/xo-server/src/xo-mixins/backups-ng/migration.js
@@ -1,129 +1,0 @@
-import assert from 'assert'
-
-const createOr = children => (children.length === 1 ? children[0] : { __or: children })
-
-const methods = {
-  'vm.deltaCopy': (job, { _reportWhen: reportWhen, retention = 1, sr, vms }, schedule) => ({
-    mode: 'delta',
-    settings: {
-      '': reportWhen === undefined ? undefined : { reportWhen },
-      [schedule.id]: {
-        copyRetention: retention,
-        vmTimeout: job.timeout,
-      },
-    },
-    srs: { id: sr },
-    userId: job.userId,
-    vms,
-  }),
-  'vm.rollingDeltaBackup': (job, { _reportWhen: reportWhen, depth = 1, retention = depth, remote, vms }, schedule) => ({
-    mode: 'delta',
-    remotes: { id: remote },
-    settings: {
-      '': reportWhen === undefined ? undefined : { reportWhen },
-      [schedule.id]: {
-        exportRetention: retention,
-        vmTimeout: job.timeout,
-      },
-    },
-    vms,
-  }),
-  'vm.rollingDrCopy': (
-    job,
-    { _reportWhen: reportWhen, deleteOldBackupsFirst, depth = 1, retention = depth, sr, vms },
-    schedule
-  ) => ({
-    mode: 'full',
-    settings: {
-      '': reportWhen === undefined ? undefined : { reportWhen },
-      [schedule.id]: {
-        deleteFirst: deleteOldBackupsFirst,
-        copyRetention: retention,
-        vmTimeout: job.timeout,
-      },
-    },
-    srs: { id: sr },
-    vms,
-  }),
-  'vm.rollingBackup': (
-    job,
-    { _reportWhen: reportWhen, compress, depth = 1, retention = depth, remoteId, vms },
-    schedule
-  ) => ({
-    compression: compress ? 'native' : undefined,
-    mode: 'full',
-    remotes: { id: remoteId },
-    settings: {
-      '': reportWhen === undefined ? undefined : { reportWhen },
-      [schedule.id]: {
-        exportRetention: retention,
-        vmTimeout: job.timeout,
-      },
-    },
-    vms,
-  }),
-  'vm.rollingSnapshot': (job, { _reportWhen: reportWhen, depth = 1, retention = depth, vms }, schedule) => ({
-    mode: 'full',
-    settings: {
-      '': reportWhen === undefined ? undefined : { reportWhen },
-      [schedule.id]: {
-        snapshotRetention: retention,
-        vmTimeout: job.timeout,
-      },
-    },
-    vms,
-  }),
-}
-
-const parseParamsVector = vector => {
-  assert.strictEqual(vector.type, 'crossProduct')
-  const { items } = vector
-  assert.strictEqual(items.length, 2)
-
-  let vms, params
-  if (items[1].type === 'map') {
-    ;[params, vms] = items
-
-    vms = vms.collection
-    assert.strictEqual(vms.type, 'fetchObjects')
-    vms = vms.pattern
-  } else {
-    ;[vms, params] = items
-
-    assert.strictEqual(vms.type, 'set')
-    vms = vms.values
-    if (vms.length !== 0) {
-      assert.deepStrictEqual(Object.keys(vms[0]), ['id'])
-      vms = { id: createOr(vms.map(_ => _.id)) }
-    }
-  }
-
-  assert.strictEqual(params.type, 'set')
-  params = params.values
-  assert.strictEqual(params.length, 1)
-  params = params[0]
-
-  return { ...params, vms }
-}
-
-export const translateLegacyJob = (job, schedules) => {
-  const { id } = job
-  let method, schedule
-  if (
-    id.includes(':') ||
-    job.type !== 'call' ||
-    (method = methods[job.method]) === undefined ||
-    (schedule = schedules.find(_ => _.jobId === id)) === undefined ||
-    schedule.id.includes(':')
-  ) {
-    throw new Error(`cannot convert job ${job.id}`)
-  }
-  const params = parseParamsVector(job.paramsVector)
-  return {
-    id,
-    name: params.tag || job.name,
-    type: 'backup',
-    userId: job.userId,
-    ...method(job, params, schedule),
-  }
-}

commit b495c2b60b9bde4c241f33fb0c8acaeafad412db

fix(xo-server/Xapi#importDeltaVm): remove transferSize in result
Not necessary and broken since bdb0ca836

packages/xo-server/src/api/test.js
@@ -68,10 +68,9 @@ export async function copyVm({ vm, sr }) {
     console.log('export delta VM...')
     const input = await srcXapi.exportDeltaVm(vm)
     console.log('import delta VM...')
-    const { transferSize, vm: copyVm } = await tgtXapi.importDeltaVm(input, {
+    const { vm: copyVm } = await tgtXapi.importDeltaVm(input, {
       srId: sr,
     })
-    console.log('transfered size:', transferSize)
     await tgtXapi.VM_destroy(copyVm.$ref)
   }
 }

packages/xo-server/src/xapi/index.js
@@ -793,7 +793,6 @@ export default class Xapi extends XapiBase {
     })
 
     const { streams } = delta
-    let transferSize = 0
 
     await Promise.all([
       // Import VDI contents.
@@ -802,12 +801,7 @@ export default class Xapi extends XapiBase {
           if (typeof stream === 'function') {
             stream = await stream()
           }
-          const sizeStream = stream.pipe(createSizeStream()).once('finish', () => {
-            transferSize += sizeStream.size
-          })
-          sizeStream.task = stream.task
-          sizeStream.length = stream.length
-          await this._importVdiContent(vdi, sizeStream, VDI_FORMAT_VHD)
+          await this._importVdiContent(vdi, stream, VDI_FORMAT_VHD)
         }
       }),
 
@@ -851,7 +845,7 @@ export default class Xapi extends XapiBase {
       ),
     ])
 
-    return { transferSize, vm }
+    return { vm }
   }
 
   async _migrateVmWithStorageMotion(