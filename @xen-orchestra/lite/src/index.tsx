9c38f5b327390660cc3214018f65864534a8b0fd
Pierre Donias <pierre.donias@gmail.com>
2021-04-29 14:22:19 +0200

    feat(xo-lite): styled-components

--- @xen-orchestra/lite/src/index.tsx
+++ @xen-orchestra/lite/src/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './style.css'
 import App from './App/index'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: Arial, Verdana, Helvetica, Ubuntu, sans-serif;
+  }
+`
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
