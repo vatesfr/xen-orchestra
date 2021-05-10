c64d63599c111c7dbdee40e9e2f55f707ae94904
mathieuRA <contact@mathieu-raisin.fr>
2021-05-10 15:48:24 +0200

    Sort

---
+++ @xen-orchestra/lite/src/App/Pools/PoolNetwork.tsx
@@ -0,0 +1,86 @@
+import React from 'react'
+import styled from 'styled-components'
+import { FormattedMessage } from 'react-intl'
+import { Map } from 'immutable'
+import { withState } from 'reaclette'
+
+import { Network, ObjectsByType, Pif } from '../../libs/xapi'
+
+interface ParentState {
+  objectsByType: ObjectsByType
+}
+
+interface State {}
+
+interface Props {
+  poolId: string
+}
+
+interface ParentEffects {}
+
+interface Effects {}
+
+interface Computed {
+  managementPIFs?: Map<string, Pif>
+  networks?: Map<string, Network>
+  objectsFetched: boolean
+  PIFs?: Map<string, Pif>
+}
+
+const Table = styled.table`
+  border: 1px solid #333;
+  td  {
+    border: 1px solid #333;
+  }
+  thead {
+    background-color: #333;
+    color: #fff;
+  }
+`
+
+const PoolNetwork = withState<State, Props, Effects, Computed, ParentState, ParentEffects>(
+  {
+    computed: {
+      managementPIFs: state => state.PIFs?.filter(pif => pif.management),
+      networks: (state, props) =>
+        state.objectsFetched
+          ? state.objectsByType.get('network')?.filter(network => network.$pool.$id === props.poolId)
+          : undefined,
+      objectsFetched: state => state.objectsByType !== undefined,
+      PIFs: state =>
+        state.objectsByType.get('PIF')?.filter(pif => state.networks?.find(network => network.$ref === pif.network)),
+    },
+  },
+  ({ state }) => (
+    <Table>
+      <thead>
+        <tr>
+          <td>
+            <FormattedMessage id='device' />
+          </td>
+          <td>
+            <FormattedMessage id='DNS' />
+          </td>
+          <td>
+            <FormattedMessage id='gateway' />
+          </td>
+          <td>
+            <FormattedMessage id='IP' />
+          </td>
+        </tr>
+      </thead>
+      <tbody>
+        {state.managementPIFs?.valueSeq().map(pif => (
+          <tr key={pif.$id}>
+            <td>{pif.device}</td>
+            <td>{pif.DNS}</td>
+            <td>{pif.gateway}</td>
+            <td>{pif.IP}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  )
+)
+
+export default PoolNetwork
