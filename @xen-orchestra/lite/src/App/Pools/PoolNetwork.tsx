a4b9548fa3a246d975714160887457006ccdacdc
mathieuRA <contact@mathieu-raisin.fr>
2021-05-10 15:25:55 +0200

    feat(xo-lite/Table): creation of Table component

--- @xen-orchestra/lite/src/App/Pools/PoolNetwork.tsx
+++ @xen-orchestra/lite/src/App/Pools/PoolNetwork.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import styled from 'styled-components'
+import { FormattedMessage } from 'react-intl'
 import { Map } from 'immutable'
 import { withState } from 'reaclette'
 
+import Table, { TableColumn } from '../../components/Table'
 import { Network, ObjectsByType, Pif } from '../../libs/xapi'
-import { FormattedMessage } from 'react-intl'
 
 interface ParentState {
   objectsByType: ObjectsByType
@@ -21,25 +21,35 @@ interface ParentEffects {}
 interface Effects {}
 
 interface Computed {
+  managementPIFs?: Map<string, Pif>
   networks?: Map<string, Network>
   objectsFetched: boolean
   PIFs?: Map<string, Pif>
 }
 
-const Table = styled.table`
-  border: 1px solid #333;
-  td  {
-    border: 1px solid #333;
-  }
-  thead {
-    background-color: #333;
-    color: #fff;
-  }
-`
+const COLUMNS: TableColumn[] = [
+  {
+    name: <FormattedMessage id='device' />,
+    itemRenderer: (pif: Pif) => pif.device,
+  },
+  {
+    name: <FormattedMessage id='DNS' />,
+    itemRenderer: (pif: Pif) => pif.DNS,
+  },
+  {
+    name: <FormattedMessage id='gateway' />,
+    itemRenderer: (pif: Pif) => pif.gateway,
+  },
+  {
+    name: <FormattedMessage id='IP' />,
+    itemRenderer: (pif: Pif) => pif.IP,
+  },
+]
 
 const PoolNetwork = withState<State, Props, Effects, Computed, ParentState, ParentEffects>(
   {
     computed: {
+      managementPIFs: state => state.PIFs?.filter(pif => pif.management),
       networks: (state, props) =>
         state.objectsFetched
           ? state.objectsByType.get('network')?.filter(network => network.$pool.$id === props.poolId)
@@ -49,39 +59,7 @@ const PoolNetwork = withState<State, Props, Effects, Computed, ParentState, Pare
         state.objectsByType.get('PIF')?.filter(pif => state.networks?.find(network => network.$ref === pif.network)),
     },
   },
-  ({ state }) => (
-    <Table>
-      <thead>
-        <tr>
-          <td>
-            <FormattedMessage id='device' />
-          </td>
-          <td>
-            <FormattedMessage id='DNS' />
-          </td>
-          <td>
-            <FormattedMessage id='gateway' />
-          </td>
-          <td>
-            <FormattedMessage id='IP' />
-          </td>
-        </tr>
-      </thead>
-      <tbody>
-        {state.PIFs?.valueSeq().map(
-          pif =>
-            pif.management && (
-              <tr key={pif.$id}>
-                <td>{pif.device}</td>
-                <td>{pif.DNS}</td>
-                <td>{pif.gateway}</td>
-                <td>{pif.IP}</td>
-              </tr>
-            )
-        )}
-      </tbody>
-    </Table>
-  )
+  ({ state }) => <Table collections={state.managementPIFs} columns={COLUMNS} />
 )
 
 export default PoolNetwork
---
+++ @xen-orchestra/lite/src/components/Table.tsx
@@ -0,0 +1,90 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Dictionary, isEmpty, map } from 'lodash'
+import { FormattedMessage } from 'react-intl'
+import { Map } from 'immutable'
+import { withState } from 'reaclette'
+
+type Collections = Dictionary<unknown> | unknown[]
+
+export type TableColumn = {
+  name: JSX.Element
+  itemRenderer: { (item: any): string | JSX.Element }
+}
+
+interface ParentState {}
+
+interface State {}
+
+interface Props {
+  collections: Collections | Map<string, unknown> | undefined
+  columns: TableColumn[]
+}
+
+interface ParentEffects {}
+
+interface Effects {}
+
+interface Computed {
+  collections: Collections
+}
+
+const _Table = styled.table`
+  thead  {
+    background-color: #2d83c3;
+    color: #fff;
+  }
+  td,
+  th  {
+    padding: 1em;
+  }
+  tbody tr:nth-child(even) {
+    background-color: #2d83c324;
+  }
+  tbody tr:hover {
+    background-color: #4749494f;
+    transition: 0.4s;
+  }
+`
+
+const Table = withState<State, Props, Effects, Computed, ParentState, ParentEffects>(
+  {
+    computed: {
+      collections: (_, props) => {
+        const { collections } = props
+        if (collections === undefined) {
+          return []
+        }
+        if (Map.isMap(collections)) {
+          return collections.toArray().map(item => item[1])
+        }
+        return collections
+      },
+    },
+  },
+  ({ columns, state }) =>
+    !isEmpty(state.collections) ? (
+      <_Table>
+        <thead>
+          <tr>
+            {columns.map((col, index) => (
+              <th key={index}>{col.name}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {map(state.collections, (item, index) => (
+            <tr key={index}>
+              {columns.map((col, index) => (
+                <td key={index}>{col.itemRenderer(item)}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </_Table>
+    ) : (
+      <FormattedMessage id='noData' />
+    )
+)
+
+export default Table
