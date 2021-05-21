84ec7fe50d145adeac9a40b8cdc2fab0b6c27f20
Pierre Donias <pierre.donias@gmail.com>
2021-05-21 15:11:35 +0200

    chore(xo-lite): fix config and xen-api

--- @xen-orchestra/lite/.babelrc.js
+++ @xen-orchestra/lite/.babelrc.js
@@ -1,3 +1,5 @@
-const config = (module.exports = require('../../@xen-orchestra/babel-config')(require('./package.json')))
-
-config.presets.find(_ => _[0] === '@babel/preset-env')[1].targets.esmodules = true
+module.exports = require('../../@xen-orchestra/babel-config')(require('./package.json'), {
+  '@babel/preset-env': {
+    exclude: ['@babel/plugin-transform-regenerator'],
+  },
+})
