f35c865348b767b814b7986e68781d2e8d05cf98
Julien Fontanet <julien.fontanet@isonoe.net>
2021-05-21 14:19:50 +0200

    feat(xo-web): SSH key input monospaced

--- packages/xo-web/src/common/xo/new-ssh-key-modal/index.js
+++ packages/xo-web/src/common/xo/new-ssh-key-modal/index.js
@@ -37,7 +37,12 @@ export default class NewSshKeyModalBody extends BaseComponent {
           <SingleLineRow>
             <Col size={4}>{_('key')}</Col>
             <Col size={8}>
-              <textarea className='form-control' onChange={this._onKeyChange} rows={10} value={key || ''} />
+              <textarea
+                className='form-control text-monospace'
+                onChange={this._onKeyChange}
+                rows={10}
+                value={key || ''}
+              />
             </Col>
           </SingleLineRow>
         </div>
