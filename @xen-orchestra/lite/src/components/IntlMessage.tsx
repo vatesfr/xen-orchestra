3269069f36795743eb56f3810865fb4514e0f52a
mathieuRA <contact@mathieu-raisin.fr>
2021-06-16 09:52:35 +0200

    add comment

--- @xen-orchestra/lite/src/components/IntlMessage.tsx
+++ @xen-orchestra/lite/src/components/IntlMessage.tsx
@@ -2,6 +2,9 @@ import React, { ElementType, ReactElement, ReactNode } from 'react'
 import { FormattedMessage, MessageDescriptor } from 'react-intl'
 import intlMessage from '../lang/en.json'
 
+// Extends FormattedMessage not working: "FormattedMessage refers to a value, but is being used as a type here"
+// https://stackoverflow.com/questions/62059408/reactjs-and-typescript-refers-to-a-value-but-is-being-used-as-a-type-here-ts
+// InstanceType<typeof FormattedMessage> not working: "Type [...] does not satisfy the constraint abstract new (...args: any) => any."
 // See https://formatjs.io/docs/react-intl/components/#formattedmessage
 interface Props extends MessageDescriptor {
   children?: (chunks: ReactElement) => ReactElement
