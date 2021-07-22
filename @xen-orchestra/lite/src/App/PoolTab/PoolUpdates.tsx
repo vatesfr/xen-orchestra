97bb499f0986d4ac37c38f2fd64af3983e031cbd
mathieuRA <contact@mathieu-raisin.fr>
2021-07-22 11:22:06 +0200

    fix

--- @xen-orchestra/lite/src/App/PoolTab/PoolUpdates.tsx
+++ @xen-orchestra/lite/src/App/PoolTab/PoolUpdates.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { Map } from 'immutable'
 import { withState } from 'reaclette'
 
 import IntlMessage from '../../components/IntlMessage'
 import Table, { Column } from '../../components/Table'
-import XapiConnection, { Host, ObjectsByType, PoolUpdate } from '../../libs/xapi'
+import XapiConnection, { ObjectsByType, PoolUpdate } from '../../libs/xapi'
 import { formatSize } from '../../helpers/format'
 
 const COLUMN: Column<PoolUpdate>[] = [
@@ -39,7 +38,7 @@ interface ParentState {
 interface State {}
 
 interface Props {
-  poolId: string
+  hostRef: string
 }
 
 interface ParentEffects {}
@@ -47,47 +46,47 @@ interface ParentEffects {}
 interface Effects {}
 
 interface Computed {
-  availableUpdates?: PoolUpdate[]
-  hosts?: Map<string, Host>
+  availableUpdates?: PoolUpdate[] | JSX.Element
 }
 
 const PoolUpdates = withState<State, Props, Effects, Computed, ParentState, ParentEffects>(
   {
     computed: {
-      availableUpdates: async function (state) {
-        const promises: Promise<unknown>[] = []
-        state.hosts?.forEach(({ $ref }) =>
-          promises.push(state.xapi.call('host.call_plugin', $ref, 'updater.py', 'check_update', {}))
-        )
-
-        let poolUpdates: Map<string, PoolUpdate> = Map()
-        ;(await Promise.all(promises)).forEach(stringifiedPoolUpdates => {
-          if (typeof stringifiedPoolUpdates !== 'string') return
-          JSON.parse(stringifiedPoolUpdates).forEach((update: PoolUpdate | string) => {
-            // update variable can be a string that contain: "The updater plugin is busy"
-            if (typeof update !== 'string' && !poolUpdates.has(update.name)) {
-              poolUpdates = poolUpdates.set(update.name, update)
-            }
-          })
-        })
-        return poolUpdates
-          .map(update => ({ id: update.name, ...update }))
-          .valueSeq()
-          .toArray()
-      },
-      hosts: function (state) {
-        return state.objectsByType.get('host')
+      availableUpdates: async function (state, { hostRef }) {
+        const error = <IntlMessage id='errorOccurred' />
+        try {
+          const stringifiedPoolUpdates = await state.xapi.call(
+            'host.call_plugin',
+            hostRef,
+            'updater.py',
+            'check_update',
+            {}
+          )
+          if (typeof stringifiedPoolUpdates !== 'string') {
+            return error
+          }
+          return JSON.parse(stringifiedPoolUpdates)
+        } catch (err) {
+          return error
+        }
       },
     },
   },
-  ({ state: { availableUpdates } }) => (
-    <>
-      {availableUpdates !== undefined && availableUpdates.length !== 0 && (
-        <IntlMessage id='availableUpdates' values={{ nUpdates: availableUpdates.length }} />
-      )}
-      <Table collection={availableUpdates} columns={COLUMN} placeholder={<IntlMessage id='noUpdatesAvailable' />} />
-    </>
-  )
+  ({ state: { availableUpdates } }) =>
+    availableUpdates !== undefined ? (
+      Array.isArray(availableUpdates) ? (
+        <>
+          {availableUpdates.length !== 0 && (
+            <IntlMessage id='availableUpdates' values={{ nUpdates: availableUpdates.length }} />
+          )}
+          <Table collection={availableUpdates} columns={COLUMN} placeholder={<IntlMessage id='noUpdatesAvailable' />} />
+        </>
+      ) : (
+        availableUpdates
+      )
+    ) : (
+      <IntlMessage id='loading' />
+    )
 )
 
 export default PoolUpdates
--- @xen-orchestra/lite/src/App/PoolTab/index.tsx
+++ @xen-orchestra/lite/src/App/PoolTab/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Map } from 'immutable'
 import { withState } from 'reaclette'
 
 import PoolNetworks from './PoolNetworks'
 import PoolUpdates from './PoolUpdates'
 
 import IntlMessage from '../../components/IntlMessage'
-import { ObjectsByType, Pool } from '../../libs/xapi'
+import { Host, ObjectsByType, Pool } from '../../libs/xapi'
 
 interface ParentState {
   objectsFetched: boolean
@@ -22,12 +23,14 @@ interface ParentEffects {}
 interface Effects {}
 
 interface Computed {
+  hosts?: Map<string, Host>
   pool?: Pool
 }
 
 const PoolTab = withState<State, Props, Effects, Computed, ParentState, ParentEffects>(
   {
     computed: {
+      hosts: state => (state.objectsFetched ? state.objectsByType?.get('host') : undefined),
       pool: state => (state.objectsFetched ? state.objectsByType?.get('pool')?.first() : undefined),
     },
   },
@@ -35,7 +38,12 @@ const PoolTab = withState<State, Props, Effects, Computed, ParentState, ParentEf
     state.pool !== undefined ? (
       <>
         <PoolNetworks poolId={state.pool.$id} />
-        <PoolUpdates poolId={state.pool.$id} />
+        {state.hosts?.valueSeq().map(host => (
+          <div key={host.$id}>
+            <p>{host.name_label}</p>
+            <PoolUpdates hostRef={host.$ref} />
+          </div>
+        ))}
       </>
     ) : (
       <IntlMessage id='loading' />
