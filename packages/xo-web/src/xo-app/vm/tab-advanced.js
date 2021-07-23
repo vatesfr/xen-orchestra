c482f18572082ab781dd6f8aa280f48446441e74
Julien Fontanet <julien.fontanet@isonoe.net>
2021-07-23 09:49:32 +0200

    chore(xo-web/vm/tab-advanced): shutdown is a valid operation

--- packages/xo-web/src/xo-app/vm/tab-advanced.js
+++ packages/xo-web/src/xo-app/vm/tab-advanced.js
@@ -68,8 +68,10 @@ const STOP_OPERATIONS = [
   'hard_reboot',
   'hard_shutdown',
   'pause',
-  'shutdown',
   'suspend',
+
+  // Even though it's not recognized by `xe (as of 2021-08), it's a valid operation
+  'shutdown',
 ]
 
 const forceReboot = vm => restartVm(vm, true)
