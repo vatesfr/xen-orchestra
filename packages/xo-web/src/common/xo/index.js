cd81cf0fd98f6078e69bf1d463e81340db889c83
mathieuRA <contact@mathieu-raisin.fr>
2021-08-23 15:48:35 +0200

    fix

--- packages/xo-web/src/common/xo/index.js
+++ packages/xo-web/src/common/xo/index.js
@@ -301,10 +301,11 @@ export const subscribeCurrentUser = createSubscription(() => xo.refreshUser())
 
 export const subscribeAcls = createSubscription(() => _call('acl.get'))
 
-export const subscribeHvSupportedVersions =
-  xoaPlans.CURRENT !== xoaPlans.SOURCES && xoaPlans.CURRENT !== xoaPlans.UNKNOWN
-    ? createSubscription(() => _call('xoa.getHVSupportedVersions'))
-    : noop
+export const subscribeHvSupportedVersions = createSubscription(() => {
+  if (xoaPlans.CURRENT !== xoaPlans.SOURCES && xoaPlans.CURRENT !== xoaPlans.UNKNOWN) {
+    return _call('xoa.getHVSupportedVersions')
+  }
+})
 
 export const subscribeJobs = createSubscription(() => _call('job.getAll'))
 
