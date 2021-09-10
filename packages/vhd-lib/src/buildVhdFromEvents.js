af3529e9c54d7dd32821b90728be6cc8f102eb4b
Julien Fontanet <julien.fontanet@isonoe.net>
2021-09-10 10:21:57 +0200

    format with Prettier

--- packages/vhd-lib/src/buildVhdFromEvents.js
+++ packages/vhd-lib/src/buildVhdFromEvents.js
@@ -1,129 +1,128 @@
 import assert from 'assert'
-import {gzip, ungzip} from 'node-gzip'
+import { gzip, ungzip } from 'node-gzip'
 import { off } from 'process'
 
 import { fuHeader } from './_structs'
 
 class VhdOutputStreamCreator {
+  constructor(outputStream) {
+    this.currentOffset = 0
+    this.outputStream = outputStream
+  }
 
-    constructor(outputStream){
-        this.currentOffset = 0
-        this.outputStream = outputStream
-    }
+  async writeFooter(footerData) {
+    assert(footerData.length <= 512, `footer  must be 512 bytes of  less ${footerData.length}`)
+    await this.write(footerData)
+    await this.pad(512 - footerData.length)
+  }
 
-    async writeFooter(footerData){
-        assert(footerData.length <= 512, `footer  must be 512 bytes of  less ${footerData.length}`)
-        await this.write(footerData)
-        await this.pad(512 - footerData.length)
-    }
-    
-    async writeHeader(headerData){
-        assert(headerData.length <= 1024, `headerData must be 1024 bytes of  less ${headerData.length}`)
-        await this.write(headerData)
-        await this.pad(1024 - headerData.length)
-    }
+  async writeHeader(headerData) {
+    assert(headerData.length <= 1024, `headerData must be 1024 bytes of  less ${headerData.length}`)
+    await this.write(headerData)
+    await this.pad(1024 - headerData.length)
+  }
 
-    async write(data, offset = null ){
-        if(null === offset){
-            offset = this.currentOffset
-        }
-        
-        assert(offset >= this.currentOffset, `try to write a block in an already handled part ${offset} ${this.currentOffset}`)
-        assert(this.outputStream.writable)
-        this.pad(offset - this.currentOffset)
-        this.currentOffset += data.length
-
-        await this.outputStream.write(data)
+  async write(data, offset = null) {
+    if (null === offset) {
+      offset = this.currentOffset
     }
 
-    async pad(length, fill = 0){
-        if(0 === length){
-            return ;
-        }
-        await this.write(Buffer.alloc(length, fill))
+    assert(
+      offset >= this.currentOffset,
+      `try to write a block in an already handled part ${offset} ${this.currentOffset}`
+    )
+    assert(this.outputStream.writable)
+    this.pad(offset - this.currentOffset)
+    this.currentOffset += data.length
+
+    await this.outputStream.write(data)
+  }
+
+  async pad(length, fill = 0) {
+    if (0 === length) {
+      return
     }
-    async end(){
-        console.log('end',this.currentOffset)
-        await this.outputStream.end()
-    }
+    await this.write(Buffer.alloc(length, fill))
+  }
+  async end() {
+    console.log('end', this.currentOffset)
+    await this.outputStream.end()
+  }
 }
 
-
-export function eventsToStream(outputStream, eventEmitter){ 
-    return new Promise((resolve, reject)=>{
-
-        const creator = new VhdOutputStreamCreator(outputStream)
-        let header = null
-        let footerBuf = null
-        eventEmitter.on('footer', (footer)=>{
-            footerBuf = footer
-            creator.writeFooter(footer)
-        })
-    
-        eventEmitter.on('header', async (bufHeader)=>{ 
-            header = fuHeader.unpack(bufHeader)
-            await creator.writeHeader(bufHeader)
-        })
-    
-        eventEmitter.on('bat', async (bat)=>{ 
-            assert(!!header, 'header must be before BAT')
-            await creator.write(bat, header.tableOffset)
-        })
-        eventEmitter.on('block', async (block, offset)=>{ 
-            await creator.write(block, offset)
-        })
-        eventEmitter.on('parentLocator', async (parentLocator, offset)=>{ 
-            await creator.write(parentLocator, offset)
-        })
-        
-        eventEmitter.on('end', async ()=>{
-            assert(!!footerBuf, 'footer must be before end')
-            await creator.writeFooter(footerBuf)
-            await creator.end()
-            resolve()
-        }) 
-
-        eventEmitter.on('error', (error)=>{
-            reject(error)
-        }) 
+export function eventsToStream(outputStream, eventEmitter) {
+  return new Promise((resolve, reject) => {
+    const creator = new VhdOutputStreamCreator(outputStream)
+    let header = null
+    let footerBuf = null
+    eventEmitter.on('footer', footer => {
+      footerBuf = footer
+      creator.writeFooter(footer)
     })
+
+    eventEmitter.on('header', async bufHeader => {
+      header = fuHeader.unpack(bufHeader)
+      await creator.writeHeader(bufHeader)
+    })
+
+    eventEmitter.on('bat', async bat => {
+      assert(!!header, 'header must be before BAT')
+      await creator.write(bat, header.tableOffset)
+    })
+    eventEmitter.on('block', async (block, offset) => {
+      await creator.write(block, offset)
+    })
+    eventEmitter.on('parentLocator', async (parentLocator, offset) => {
+      await creator.write(parentLocator, offset)
+    })
+
+    eventEmitter.on('end', async () => {
+      assert(!!footerBuf, 'footer must be before end')
+      await creator.writeFooter(footerBuf)
+      await creator.end()
+      resolve()
+    })
+
+    eventEmitter.on('error', error => {
+      reject(error)
+    })
+  })
 }
 
+export function eventsToS3(s3, eventEmitter, bucket, dir) {
+  return new Promise((resolve, reject) => {
+    const saveBlock = async (data, offset, type) => {
+      const stringOffset = '' + offset
+      const fileMask = '000000000000'
+      const fileName = fileMask.substring(0, fileMask.length - stringOffset.length) + stringOffset + '/' + type
+      console.log('send ', fileName, data.length)
+      await s3.putObject({
+        Bucket: bucket,
+        Body: data,
+        Key: dir + fileName,
+      })
+      console.log('sent')
+    }
 
-export function eventsToS3(s3, eventEmitter, bucket, dir){ 
-    return new Promise((resolve, reject)=>{
-        const saveBlock = async (data, offset, type) => {
-            const stringOffset = ''+offset
-            const fileMask = '000000000000'
-            const fileName = fileMask.substring(0,fileMask.length - stringOffset.length)+ stringOffset +'/'+type
-            console.log('send ',fileName, data.length)
-            await s3.putObject({
-                Bucket: bucket,
-                Body: data,
-                Key: dir + fileName
-            })
-            console.log('sent')
-        }
-
-        eventEmitter.on('footer', async (footer, offset)=>{
-            await saveBlock(footer, offset, 'footer')
-        })
-    
-        eventEmitter.on('header', async (header, offset)=>{ 
-            await saveBlock(header, offset, 'header')
-        })
-    
-        eventEmitter.on('bat',async (bat, offset)=>{ 
-            await saveBlock(bat, offset, 'bat')
-        })
-        eventEmitter.on('block', async (block, offset)=>{ 
-            await saveBlock(block, offset, 'block')
-        })
-        eventEmitter.on('parentLocator', async(parentLocator, offset)=>{ 
-            await saveBlock(parentLocator, offset, 'parentLocator')
-        })
-        eventEmitter.on('end',()=>{
-            resolve()
-        })
+    eventEmitter.on('footer', async (footer, offset) => {
+      await saveBlock(footer, offset, 'footer')
     })
+
+    eventEmitter.on('header', async (header, offset) => {
+      await saveBlock(header, offset, 'header')
+    })
+
+    eventEmitter.on('bat', async (bat, offset) => {
+      await saveBlock(bat, offset, 'bat')
+    })
+    eventEmitter.on('block', async (block, offset) => {
+      await saveBlock(block, offset, 'block')
+    })
+    eventEmitter.on('parentLocator', async (parentLocator, offset) => {
+      await saveBlock(parentLocator, offset, 'parentLocator')
+    })
+    eventEmitter.on('end', () => {
+      resolve()
+    })
+  })
 }
--- packages/vhd-lib/src/vhdparser.js
+++ packages/vhd-lib/src/vhdparser.js
@@ -1,178 +1,174 @@
 import assert from 'assert'
-import  EventEmitter from 'events'
+import EventEmitter from 'events'
 import emitAsync from '@xen-orchestra/emit-async'
-import { readChunk } from '@vates/read-chunk' 
+import { readChunk } from '@vates/read-chunk'
 import { SECTOR_SIZE, BLOCK_UNUSED } from './_constants'
 import { fuHeader } from './_structs'
 
-
 const cappedBufferConcat = (buffers, maxSize) => {
-    let buffer = Buffer.concat(buffers)
-    if(buffer.length > maxSize){
-        buffer = buffer.slice(buffer.length - maxSize)
-    }
-    return buffer
+  let buffer = Buffer.concat(buffers)
+  if (buffer.length > maxSize) {
+    buffer = buffer.slice(buffer.length - maxSize)
+  }
+  return buffer
 }
 
 class Parser extends EventEmitter {
-    parse(){
-        throw new TypeError('Not implemented');
-    }
+  parse() {
+    throw new TypeError('Not implemented')
+  }
 }
 
 export class S3VhdParser extends Parser {
-    constructor(s3, bucket, prefix) {
-        super()
-        this.s3 = s3
-        this.bucket = bucket
-        this.prefix = prefix
-    }
+  constructor(s3, bucket, prefix) {
+    super()
+    this.s3 = s3
+    this.bucket = bucket
+    this.prefix = prefix
+  }
 
-    getFile(fileId){
-        return this.s3.getObject({
-            Bucket: this.bucket,
-            Key: fileId
-        })
+  getFile(fileId) {
+    return this.s3.getObject({
+      Bucket: this.bucket,
+      Key: fileId,
+    })
+  }
+  async parse() {
+    const params = {
+      Bucket: this.bucket,
+      Prefix: this.prefix,
     }
-    async parse(){
-        const params = {
-            Bucket: this.bucket,
-            Prefix: this.prefix
-        }
-        do {
-            const { NextContinuationToken, Contents } = await this.s3.listObjectsV2(params)
-            params.ContinuationToken = NextContinuationToken
+    do {
+      const { NextContinuationToken, Contents } = await this.s3.listObjectsV2(params)
+      params.ContinuationToken = NextContinuationToken
 
-            for(const {Key} of Contents){
-                const path = Key.split('/')
-                const type = path.pop()
-                const offset = parseInt(path.pop())
-                console.log(Key)
-                const fileContent = await this.getFile(Key)
-                this.emit(type, fileContent.Body, offset)
-            }
-        }while(!!params.ContinuationToken)
-        this.emit('end')
-    }
+      for (const { Key } of Contents) {
+        const path = Key.split('/')
+        const type = path.pop()
+        const offset = parseInt(path.pop())
+        console.log(Key)
+        const fileContent = await this.getFile(Key)
+        this.emit(type, fileContent.Body, offset)
+      }
+    } while (!!params.ContinuationToken)
+    this.emit('end')
+  }
 }
 
-export class InputStreamVhdParser extends Parser{
-    constructor(stream) {
-        super()
-        this.emitAsync = emitAsync
-        this.stream = stream
+export class InputStreamVhdParser extends Parser {
+  constructor(stream) {
+    super()
+    this.emitAsync = emitAsync
+    this.stream = stream
+  }
+
+  async parse() {
+    const stream = this.stream
+    const bufFooter = await readChunk(stream, SECTOR_SIZE)
+    await this.emitAsync('footer', bufFooter, 0)
+
+    const bufHeader = await readChunk(stream, 2 * SECTOR_SIZE)
+    const header = fuHeader.unpack(bufHeader)
+
+    await this.emitAsync('header', bufHeader, 512)
+
+    const blockBitmapSize = Math.ceil(header.blockSize / SECTOR_SIZE / 8 / SECTOR_SIZE) * SECTOR_SIZE
+    const blockSize = header.blockSize
+    const blockAndBitmapSize = blockBitmapSize + blockSize
+
+    const batOffset = header.tableOffset
+    const batSize = Math.max(1, Math.ceil((header.maxTableEntries * 4) / SECTOR_SIZE)) * SECTOR_SIZE
+    /**
+     * the norm allows the BAT to be after some blocks or parent locator
+     * we do not handle this case for now since we need the BAT to order the blocks/parent locator
+     *
+     * also there can be some free space between header and the start of BAT
+     */
+
+    await readChunk(stream, batOffset - 1024 - 512)
+
+    const bat = await readChunk(stream, batSize)
+
+    await this.emitAsync('bat', bat, batOffset)
+
+    /**
+     * blocks and parent locators  can be intervined
+     *
+     * we build a sorted index since we read the stream sequentially and
+     * we need to know what is the the next element to read and its size
+     * (parent locator size can vary)
+     */
+
+    const index = []
+    for (let blockCounter = 0; blockCounter < header.maxTableEntries; blockCounter++) {
+      let batEntrySector = bat.readUInt32BE(blockCounter * 4)
+      // unallocated block, no need to export it
+      if (batEntrySector === BLOCK_UNUSED) {
+        continue
+      }
+      const batEntryBytes = batEntrySector * SECTOR_SIZE
+      // ensure the block is not before the bat
+      assert.ok(batEntryBytes >= batOffset + batSize)
+
+      index.push({
+        type: 'block',
+        offset: batEntryBytes,
+        size: blockAndBitmapSize,
+      })
     }
 
-    async parse(){
-        const stream = this.stream
-        const bufFooter = await readChunk(stream, SECTOR_SIZE)
-        await this.emitAsync('footer', bufFooter, 0)
+    for (const parentLocatorEntry of header.parentLocatorEntry) {
+      // empty parent locator entry, does not exist in the content
+      if (0 === parentLocatorEntry.platformDataSpace) {
+        continue
+      }
+      assert.ok(parentLocatorEntry.platformDataOffset * SECTOR_SIZE >= batOffset + batSize)
 
-        const bufHeader = await readChunk(stream, 2 * SECTOR_SIZE)
-        const header = fuHeader.unpack(bufHeader)
-
-        await this.emitAsync('header', bufHeader, 512)
-
-        const blockBitmapSize = Math.ceil(header.blockSize / SECTOR_SIZE / 8 / SECTOR_SIZE) * SECTOR_SIZE
-        const blockSize = header.blockSize
-        const blockAndBitmapSize = blockBitmapSize + blockSize
-
-        const batOffset = header.tableOffset
-        const batSize = Math.max(1, Math.ceil(header.maxTableEntries * 4 / SECTOR_SIZE)) * SECTOR_SIZE
-        /**
-         * the norm allows the BAT to be after some blocks or parent locator
-         * we do not handle this case for now since we need the BAT to order the blocks/parent locator
-         * 
-         * also there can be some free space between header and the start of BAT
-         */
-        
-        await readChunk(stream, batOffset - 1024 - 512)
-
-        const bat = await readChunk(stream, batSize)
-
-        await this.emitAsync('bat', bat, batOffset) 
-
-        /**
-         * blocks and parent locators  can be intervined
-         * 
-         * we build a sorted index since we read the stream sequentially and
-         * we need to know what is the the next element to read and its size
-         * (parent locator size can vary)
-         */
-
-        const index = []
-        for(let blockCounter = 0; blockCounter < header.maxTableEntries; blockCounter ++){
-            let batEntrySector = bat.readUInt32BE(blockCounter*4)
-            // unallocated block, no need to export it
-            if(batEntrySector === BLOCK_UNUSED){
-                continue
-            }
-            const batEntryBytes = batEntrySector * SECTOR_SIZE
-            // ensure the block is not before the bat
-            assert.ok(batEntryBytes >= batOffset + batSize)
-            
-            index.push({
-                type: 'block',
-                offset: batEntryBytes, 
-                size: blockAndBitmapSize
-            }) 
-        }
-
-        for(const parentLocatorEntry of header.parentLocatorEntry){
-            // empty parent locator entry, does not exist in the content
-            if(0 === parentLocatorEntry.platformDataSpace){
-                continue
-            }
-            assert.ok(parentLocatorEntry.platformDataOffset * SECTOR_SIZE >= batOffset + batSize)
-
-            index.push({
-                type: 'parentLocator',
-                offset: parentLocatorEntry.platformDataOffset * SECTOR_SIZE, 
-                size: parentLocatorEntry.platformDataSpace * SECTOR_SIZE
-            }) 
-        }
-
-        index.sort((a, b)=> a.offset - b.offset)
-
-        for(const {type, offset, size} of index){
-            const nextObject = await readChunk(stream, size)
-            await this.emitAsync(type, nextObject, offset)
-        }
-        /**
-         * the second footer is at filesize - 512 , there can be empty spaces between last block 
-         * and the start of the footer
-         * 
-         * we read till the end of the stream, and use the last 512 bytes as the footer
-         */
-        const bufFooterEnd = await this.readLastSector(stream)
-        assert(bufFooter.equals(bufFooterEnd), 'footer1 !== footer2')
-
-        await this.emitAsync('end')
-
+      index.push({
+        type: 'parentLocator',
+        offset: parentLocatorEntry.platformDataOffset * SECTOR_SIZE,
+        size: parentLocatorEntry.platformDataSpace * SECTOR_SIZE,
+      })
     }
 
-    readLastSector(){
-        return new Promise(resolve=>{
-            let bufFooterEnd = Buffer.alloc(0)
-            let size = 0
-            this.stream.on('data', chunk => {
-                if(chunk){
-                    size+= chunk.length
-                    bufFooterEnd = cappedBufferConcat([bufFooterEnd, chunk], SECTOR_SIZE)
-                }
-            })
-    
-            this.stream.on('end' , chunk => {
-                if(chunk){
-                    size+= chunk.length
-                    bufFooterEnd = cappedBufferConcat([bufFooterEnd, chunk], SECTOR_SIZE)
-                }
-                resolve(bufFooterEnd)
-            })
-        })
+    index.sort((a, b) => a.offset - b.offset)
+
+    for (const { type, offset, size } of index) {
+      const nextObject = await readChunk(stream, size)
+      await this.emitAsync(type, nextObject, offset)
     }
+    /**
+     * the second footer is at filesize - 512 , there can be empty spaces between last block
+     * and the start of the footer
+     *
+     * we read till the end of the stream, and use the last 512 bytes as the footer
+     */
+    const bufFooterEnd = await this.readLastSector(stream)
+    assert(bufFooter.equals(bufFooterEnd), 'footer1 !== footer2')
+
+    await this.emitAsync('end')
+  }
+
+  readLastSector() {
+    return new Promise(resolve => {
+      let bufFooterEnd = Buffer.alloc(0)
+      let size = 0
+      this.stream.on('data', chunk => {
+        if (chunk) {
+          size += chunk.length
+          bufFooterEnd = cappedBufferConcat([bufFooterEnd, chunk], SECTOR_SIZE)
+        }
+      })
+
+      this.stream.on('end', chunk => {
+        if (chunk) {
+          size += chunk.length
+          bufFooterEnd = cappedBufferConcat([bufFooterEnd, chunk], SECTOR_SIZE)
+        }
+        resolve(bufFooterEnd)
+      })
+    })
+  }
 }
 
- 
-
-export default {InputStreamVhdParser}
+export default { InputStreamVhdParser }
