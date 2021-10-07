7193170660fb453590f9840e6e2a32e1c7a69944
Florent Beauchamp <flo850@free.fr>
2021-10-07 15:09:47 +0200

    rename parent locator id, fix comments

--- packages/vhd-lib/src/Vhd/VhdAbstract.js
+++ packages/vhd-lib/src/Vhd/VhdAbstract.js
@@ -169,29 +169,31 @@ export class VhdAbstract {
     }
   }
 
-  async readParentLocator(parentLocatorId) {
-    assert(parentLocatorId >= 0, 'parent Locator id must be a positive number')
-    assert(parentLocatorId < 8, 'parent Locator id  must be less than 8')
-
-    const data = await this._readParentLocatorData(parentLocatorId)
-    // offset and size are only used internally, do not expose them
+  async readParentLocator(id) {
+    assert(id >= 0, 'parent Locator id must be a positive number')
+    assert(id < 8, 'parent Locator id  must be less than 8')
+    const data = await this._readParentLocatorData(id)
+    // offset is storage specific, don't expose it
+    const {platformCode, platformDataSpace, platformDataLength} = this.header.parentLocatorEntry[parentLocatorId]
     return {
-      platformCode: this.header.parentLocatorEntry[parentLocatorId].platformCode,
-      parentLocatorId,
+      platformCode,
+      platformDataSpace,
+      platformDataLength,
+      id,
       data,
     }
   }
 
   async setUniqueParentLocator(fileNameString) {
     await this.writeParentLocator({
-      parentLocatorId: 0,
+      id: 0,
       code: PLATFORM_W2KU,
       data: Buffer.from(fileNameString, 'utf16le'),
     })
 
     for (let i = 1; i < 8; i++) {
       await this.writeParentLocator({
-        parentLocatorId: i,
+        id: i,
         code: PLATFORM_NONE,
         data: Buffer.alloc(0),
       })
