483bfd1b20bf7e876b7d7538e116e8a767226305
Florent Beauchamp <flo850@free.fr>
2021-10-15 11:29:37 +0200

    fix ebase

--- packages/vhd-lib/src/index.js
+++ packages/vhd-lib/src/index.js
@@ -9,7 +9,6 @@ export { default as createVhdStreamWithLength } from './createVhdStreamWithLengt
 export { default as mergeVhd } from './merge'
 export { default as peekFooterFromVhdStream } from './peekFooterFromVhdStream'
 export { openVhd } from './openVhd'
-export { openVhd } from './openVhd'
 export { VhdAlias } from './Vhd/VhdAlias'
 export { VhdDirectory } from './Vhd/VhdDirectory'
 export { VhdFile } from './Vhd/VhdFile'
