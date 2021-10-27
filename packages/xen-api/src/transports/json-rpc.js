9b52ab2c20b1c2c6392115a9d40b7be52652f9d7
Florent Beauchamp <flo850@free.fr>
2021-10-27 16:00:42 +0200

    fix: avoid mutating parameters, remove uneeded changelog

--- packages/xen-api/src/transports/json-rpc.js
+++ packages/xen-api/src/transports/json-rpc.js
@@ -8,13 +8,11 @@ import UnsupportedTransport from './_UnsupportedTransport'
 
 // https://github.com/xenserver/xenadmin/blob/0df39a9d83cd82713f32d24704852a0fd57b8a64/XenModel/XenAPI/Session.cs#L403-L433
 export default ({ secureOptions, url, httpProxy }) => {
-  if (httpProxy !== undefined) {
-    secureOptions.agent = new ProxyAgent(httpProxy)
-  }
   return (method, args) =>
     httpRequestPlus
       .post(url, {
         ...secureOptions,
+        agent: httpProxy !== undefined ? new ProxyAgent(httpProxy) : undefined,
         body: format.request(0, method, args),
         headers: {
           Accept: 'application/json',
