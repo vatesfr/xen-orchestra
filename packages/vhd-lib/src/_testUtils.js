62736613112dac17ff3484454fe759b371441617
Florent Beauchamp <flo850@free.fr>
2021-10-28 17:54:10 +0200

    remove function needed later

--- packages/vhd-lib/src/_testUtils.js
+++ packages/vhd-lib/src/_testUtils.js
@@ -41,50 +41,6 @@ export async function convertFromVmdkToRaw(vmdkName, rawName) {
   await convert(VMDK, vmdkName, RAW, rawName)
 }
 
-export async function createRandomVhdFile(path, sizeMB) {
-  const rawFileName = `${path}.temp.raw`
-  await createRandomFile(rawFileName, sizeMB)
-  const vhdFileName = `${path}`
-  await convertFromRawToVhd(rawFileName, vhdFileName)
-}
-
-export async function createRandomVhdDirectory(path, sizeMB) {
-  fs.mkdir(path)
-  const rawFileName = `${path}/temp.raw`
-  await createRandomFile(rawFileName, sizeMB)
-  const vhdFileName = `${path}/vhd`
-  await convertFromRawToVhd(rawFileName, vhdFileName)
-
-  const srcVhd = await fs.open(vhdFileName, 'r')
-
-  const footer = Buffer.alloc(512)
-  await fs.read(srcVhd, footer, 0, footer.length, 0)
-  await fs.writeFile(path + '/footer', footer)
-
-  const header = Buffer.alloc(1024)
-  await fs.read(srcVhd, header, 0, header.length, 512)
-  await fs.writeFile(path + '/header', header)
-
-  await fs.close(srcVhd)
-
-  // a BAT , with at most 512 blocks of 2MB
-  const bat = Buffer.alloc(512, 1)
-  await fs.writeFile(path + '/bat', bat)
-
-  // copy blocks
-  const srcRaw = await fs.open(rawFileName, 'r')
-  const blockDataSize = 512 * 4096
-  const bitmap = Buffer.alloc(4096)
-  await fs.mkdir(path + '/blocks/')
-  await fs.mkdir(path + '/blocks/1/')
-  for (let i = 0, offset = 0; i < sizeMB; i++, offset += blockDataSize) {
-    const blockData = Buffer.alloc(blockDataSize)
-    await fs.read(srcRaw, blockData, offset)
-    await fs.writeFile(path + '/blocks/1/' + i, Buffer.concat([bitmap, blockData]))
-  }
-  await fs.close(srcRaw)
-}
-
 export async function recoverRawContent(vhdName, rawName, originalSize) {
   await checkFile(vhdName)
   await convertFromVhdToRaw(vhdName, rawName)
