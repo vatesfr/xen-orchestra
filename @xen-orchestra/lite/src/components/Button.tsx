5a9abf9ee522acb6083eca039f5dd43165673d34
mathieuRA <contact@mathieu-raisin.fr>
2021-10-29 14:13:22 +0200

    fullwidth prop

--- @xen-orchestra/lite/src/components/Button.tsx
+++ @xen-orchestra/lite/src/components/Button.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Button as MuiButton, ButtonProps, Theme } from '@mui/material'
-import { SxProps } from '@mui/system'
+import { Button as MuiButton, ButtonProps } from '@mui/material'
 import { withState } from 'reaclette'
 
 interface ParentState {}
@@ -15,14 +14,10 @@ interface Effects {}
 
 interface Computed {}
 
-const MUI_CSS_BUTTON: SxProps<Theme> = {
-  width: '100%',
-}
-
 const Button = withState<State, Props, Effects, Computed, ParentState, ParentEffects>(
   {},
   ({ children, color = 'secondary', effects, resetState, state, variant = 'contained', ...props }) => (
-    <MuiButton color={color} sx={MUI_CSS_BUTTON} variant={variant} {...props}>
+    <MuiButton color={color} fullWidth variant={variant} {...props}>
       {children}
     </MuiButton>
   )
