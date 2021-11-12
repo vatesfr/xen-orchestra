d6d7e87fe5cfc09236b5f6e9c8a60305876cd6aa
Florent Beauchamp <flo850@free.fr>
2021-11-12 11:24:00 +0100

    fix: remove root need for openVhd.integ.spec.js and merge.integ.spec.js

--- packages/vhd-lib/src/merge.integ.spec.js
+++ packages/vhd-lib/src/merge.integ.spec.js
@@ -26,53 +26,53 @@ afterEach(async () => {
 
 test('coalesce works in normal cases', async () => {
   const mbOfRandom = 5
-  const randomFileName = `${tempDir}/randomfile`
-  const random2FileName = `${tempDir}/randomfile2`
-  const smallRandomFileName = `${tempDir}/small_randomfile`
-  const parentFileName = `${tempDir}/parent.vhd`
-  const child1FileName = `${tempDir}/child1.vhd`
-  const child2FileName = `${tempDir}/child2.vhd`
-  const recoveredFileName = `${tempDir}/recovered`
-  await createRandomFile(randomFileName, mbOfRandom)
-  await createRandomFile(smallRandomFileName, Math.ceil(mbOfRandom / 2))
-  await execa('qemu-img', ['create', '-fvpc', parentFileName, mbOfRandom + 1 + 'M'])
-  await checkFile(parentFileName)
-  await convertFromRawToVhd(randomFileName, child1FileName)
-  const handler = getHandler({ url: 'file://' })
-  await execa('vhd-util', ['snapshot', '-n', child2FileName, '-p', child1FileName])
-  const vhd = new VhdFile(handler, child2FileName)
+  const randomFilePath = `${tempDir}/randomfile`
+  const random2FilePath = `${tempDir}/randomfile2`
+  const smallRandomFilePath = `${tempDir}/small_randomfile`
+  const parentFilePath = `${tempDir}/parent.vhd`
+  const child1FilePath = `${tempDir}/child1.vhd`
+  const child2FilePath = `${tempDir}/child2.vhd`
+  const recoveredFilePath = `${tempDir}/recovered`
+  await createRandomFile(randomFilePath, mbOfRandom)
+  await createRandomFile(smallRandomFilePath, Math.ceil(mbOfRandom / 2))
+  await execa('qemu-img', ['create', '-fvpc', parentFilePath, mbOfRandom + 1 + 'M'])
+  await checkFile(parentFilePath)
+  await convertFromRawToVhd(randomFilePath, child1FilePath)
+  const handler = getHandler({ url: `file://${tempDir}/` })
+  await execa('vhd-util', ['snapshot', '-n', child2FilePath, '-p', child1FilePath])
+  const vhd = new VhdFile(handler, 'child2.vhd')
   await vhd.readHeaderAndFooter()
   await vhd.readBlockAllocationTable()
   vhd.footer.creatorApplication = 'xoa'
   await vhd.writeFooter()
 
-  const originalSize = await handler._getSize(randomFileName)
-  await checkFile(child1FileName)
-  await chainVhd(handler, parentFileName, handler, child1FileName, true)
-  await checkFile(child1FileName)
-  await chainVhd(handler, child1FileName, handler, child2FileName, true)
-  await checkFile(child2FileName)
-  const smallRandom = await fs.readFile(smallRandomFileName)
-  const newVhd = new VhdFile(handler, child2FileName)
+  const originalSize = await handler._getSize('randomfile')
+  await checkFile(child1FilePath)
+  await chainVhd(handler, 'parent.vhd', handler, 'child1.vhd', true)
+  await checkFile(child1FilePath)
+  await chainVhd(handler, 'child1.vhd', handler, 'child2.vhd', true)
+  await checkFile(child2FilePath)
+  const smallRandom = await fs.readFile(smallRandomFilePath)
+  const newVhd = new VhdFile(handler, 'child2.vhd')
   await newVhd.readHeaderAndFooter()
   await newVhd.readBlockAllocationTable()
   await newVhd.writeData(5, smallRandom)
-  await checkFile(child2FileName)
-  await checkFile(child1FileName)
-  await checkFile(parentFileName)
-  await vhdMerge(handler, parentFileName, handler, child1FileName)
-  await checkFile(parentFileName)
-  await chainVhd(handler, parentFileName, handler, child2FileName, true)
-  await checkFile(child2FileName)
-  await vhdMerge(handler, parentFileName, handler, child2FileName)
-  await checkFile(parentFileName)
-  await recoverRawContent(parentFileName, recoveredFileName, originalSize)
-  await execa('cp', [randomFileName, random2FileName])
-  const fd = await fs.open(random2FileName, 'r+')
+  await checkFile(child2FilePath)
+  await checkFile(child1FilePath)
+  await checkFile(parentFilePath)
+  await vhdMerge(handler, 'parent.vhd', handler, 'child1.vhd')
+  await checkFile(parentFilePath)
+  await chainVhd(handler, 'parent.vhd', handler, 'child2.vhd', true)
+  await checkFile(child2FilePath)
+  await vhdMerge(handler, 'parent.vhd', handler, 'child2.vhd')
+  await checkFile(parentFilePath)
+  await recoverRawContent(parentFilePath, recoveredFilePath, originalSize)
+  await execa('cp', [randomFilePath, random2FilePath])
+  const fd = await fs.open(random2FilePath, 'r+')
   try {
     await fs.write(fd, smallRandom, 0, smallRandom.length, 5 * SECTOR_SIZE)
   } finally {
     await fs.close(fd)
   }
-  expect(await fs.readFile(recoveredFileName)).toEqual(await fs.readFile(random2FileName))
+  expect(await fs.readFile(recoveredFilePath)).toEqual(await fs.readFile(random2FilePath))
 })
