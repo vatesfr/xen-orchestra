57dd6ebfba63a8b87e6014e1d354994070fdc9b1
Florent BEAUCHAMP <flo850@free.fr>
2021-11-22 15:50:30 +0100

    chore(vhd-lib): use openVhd for chain and checkChain (#5997)

--- packages/vhd-lib/src/checkChain.js
+++ packages/vhd-lib/src/checkChain.js
@@ -1,16 +1,14 @@
-import { VhdFile } from '.'
+import { openVhd } from '.'
 import resolveRelativeFromFile from './_resolveRelativeFromFile'
 import { DISK_TYPE_DYNAMIC } from './_constants'
+import { Disposable } from 'promise-toolbox'
 
 export default async function checkChain(handler, path) {
-  while (true) {
-    const vhd = new VhdFile(handler, path)
-    await vhd.readHeaderAndFooter()
-
-    if (vhd.footer.diskType === DISK_TYPE_DYNAMIC) {
-      break
-    }
-
-    path = resolveRelativeFromFile(path, vhd.header.parentUnicodeName)
-  }
+  await Disposable.use(function* () {
+    let vhd
+    do {
+      vhd = yield openVhd(handler, path)
+      path = resolveRelativeFromFile(path, vhd.header.parentUnicodeName)
+    } while (vhd.footer.diskType !== DISK_TYPE_DYNAMIC)
+  })
 }
