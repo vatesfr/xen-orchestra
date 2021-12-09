f6dc7b79801390c961b3b0761f2bdddca18b23aa
Florent Beauchamp <flo850@free.fr>
2021-12-09 13:52:03 +0100

    feat(vhd-lib): make a function to create e block generator from a vhd stream

---
+++ packages/vhd-lib/src/parserVhdToBlocks.js
@@ -0,0 +1,32 @@
+import { computeBlockBitmapSize, sectorsRoundUpNoZero } from './Vhd/_utils'
+import { vhdStreamGenerator } from './_vhdStreamGenerator'
+import { SECTOR_SIZE } from './_constants'
+
+async function* filteredGenerator(iterator, filter) {
+  for await (const item of iterator) {
+    if (filter(item) === true) {
+      yield item
+    }
+  }
+}
+
+export async function parseVhdToBlocks(inputStream) {
+  const sourceIterator = vhdStreamGenerator(inputStream)
+  const sourceWithoutParentLocatorIterator = filteredGenerator(sourceIterator, ({ type }) => type !== 'parentLocator')
+
+  const footer = sourceWithoutParentLocatorIterator.next().value
+  const header = sourceWithoutParentLocatorIterator.next().value
+  // without parent locator we have footer -header-bat-blocks-footer
+
+  const { blockCount } = sourceWithoutParentLocatorIterator.next().value
+  const blockGenerator = filteredGenerator(sourceIterator, ({ type }) => type === 'block')
+
+  return {
+    // each block have data + a bitmap
+    blockSizeBytes: header.blockSize + sectorsRoundUpNoZero(computeBlockBitmapSize(header.blockSize)) * SECTOR_SIZE,
+    blockCount,
+    capacityBytes: footer.currentSize,
+    geometry: footer.diskGeometry,
+    blockGenerator,
+  }
+}
