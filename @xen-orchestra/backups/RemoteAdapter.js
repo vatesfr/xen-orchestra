vatesfr/xen-orchestra

commit 7e302fd1cb93c182a1b93f9e631c0afa30e39356

feat(backups/RemoteAdapter): new method deleteVmBackups()
It's usually best to delete multiple backups at once instead of one by one because it allows some optimizations, for instance when merging unused VHDs.

This was already possible in private methods but not exposed in the public API.

@xen-orchestra/backups/RemoteAdapter.js
@@ -3,6 +3,7 @@ const Disposable = require('promise-toolbox/Disposable.js')
 const fromCallback = require('promise-toolbox/fromCallback.js')
 const fromEvent = require('promise-toolbox/fromEvent.js')
 const pDefer = require('promise-toolbox/defer.js')
+const groupBy = require('lodash/groupBy.js')
 const { dirname, join, normalize, resolve } = require('path')
 const { createLogger } = require('@xen-orchestra/log')
 const { Constants, createVhdDirectoryFromStream, openVhd, VhdAbstract, VhdSynthetic } = require('vhd-lib')
@@ -243,17 +244,14 @@ class RemoteAdapter {
     )
   }
 
-  async deleteVmBackup(filename) {
-    const metadata = JSON.parse(String(await this._handler.readFile(filename)))
-    metadata._filename = filename
+  deleteVmBackup(file) {
+    return this.deleteVmBackups([file])
+  }
 
-    if (metadata.mode === 'delta') {
-      await this.deleteDeltaVmBackups([metadata])
-    } else if (metadata.mode === 'full') {
-      await this.deleteFullVmBackups([metadata])
-    } else {
-      throw new Error(`no deleter for backup mode ${metadata.mode}`)
-    }
+  async deleteVmBackups(files) {
+    const metadatas = groupBy(await asyncMap(files, file => this.readVmBackupMetadata(file)), 'mode')
+
+    await Promise.all([this.deleteDeltaVmBackups(metadatas.delta), this.deleteFullVmBackups(metadatas.full)])
   }
 
   getDisk = Disposable.factory(this.getDisk)

commit fb331c0a2cf251bccea13e06cfb1cf01070a4512

fix(backups/RemoteAdapter#deleteVmBackups): dont delete undefined
Fixes https://xcp-ng.org/forum/topic/5331/backup-smart-mode-broken/6
Introduced by 7e302fd1c

@xen-orchestra/backups/RemoteAdapter.js
@@ -249,9 +249,12 @@ class RemoteAdapter {
   }
 
   async deleteVmBackups(files) {
-    const metadatas = groupBy(await asyncMap(files, file => this.readVmBackupMetadata(file)), 'mode')
+    const { delta, full } = groupBy(await asyncMap(files, file => this.readVmBackupMetadata(file)), 'mode')
 
-    await Promise.all([this.deleteDeltaVmBackups(metadatas.delta), this.deleteFullVmBackups(metadatas.full)])
+    await Promise.all([
+      delta !== undefined && this.deleteDeltaVmBackups(delta),
+      full !== undefined && this.deleteFullVmBackups(full),
+    ])
   }
 
   getDisk = Disposable.factory(this.getDisk)