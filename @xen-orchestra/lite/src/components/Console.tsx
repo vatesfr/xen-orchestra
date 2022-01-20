834182693ef0e8e307485b71a3dc77a1a900bc29
mathieuRA <contact@mathieu-raisin.fr>
2022-01-20 14:36:57 +0100

    Wip feat(lite/console): handle case that host have self signed certificate

--- @xen-orchestra/lite/src/components/Console.tsx
+++ @xen-orchestra/lite/src/components/Console.tsx
@@ -5,7 +5,7 @@ import { fibonacci } from 'iterable-backoff'
 import { withState } from 'reaclette'
 
 import IntlMessage from './IntlMessage'
-import { confirm } from './Modal'
+import { alert, confirm } from './Modal'
 
 import XapiConnection, { ObjectsByType, Vm } from '../libs/xapi'
 
@@ -22,6 +22,7 @@ interface State {
   rfb: any
   rfbConnected: boolean
   timeout?: NodeJS.Timeout
+  url?: URL
 }
 
 interface Props {
@@ -35,7 +36,7 @@ interface ParentEffects {}
 interface Effects {
   _connect: () => Promise<void>
   _handleConnect: () => void
-  _handleDisconnect: () => void
+  _handleDisconnect: () => Promise<void>
   sendCtrlAltDel: () => void
 }
 
@@ -45,6 +46,14 @@ interface PropsStyledConsole {
   scale: number
   visible: boolean
 }
+
+enum Protocols {
+  https = 'https:',
+  http = 'http:',
+  ws = 'ws:',
+  wss = 'wss:',
+}
+
 const StyledConsole = styled.div<PropsStyledConsole>`
   height: ${props => props.scale}%;
   margin: auto;
@@ -60,6 +69,7 @@ const Console = withState<State, Props, Effects, Computed, ParentState, ParentEf
       rfb: undefined,
       rfbConnected: false,
       timeout: undefined,
+      url: undefined,
     }),
     effects: {
       initialize: function () {
@@ -68,9 +78,41 @@ const Console = withState<State, Props, Effects, Computed, ParentState, ParentEf
       _handleConnect: function () {
         this.state.rfbConnected = true
       },
-      _handleDisconnect: function () {
+      _handleDisconnect: async function () {
         this.state.rfbConnected = false
-        this.effects._connect()
+        let reconnect = true
+
+        const protocol = window.location.protocol
+        if (protocol === Protocols.https) {
+          try {
+            await fetch(`${protocol}//${this.state.url?.host}`)
+          } catch (error) {
+            console.error(error)
+            try {
+              await confirm({
+                icon: 'exclamation-triangle',
+                message: (
+                  <div>
+                    <p>
+                      The host seems unreachable. Please verify the host is alive and in case the host use self signed
+                      certificat, ensure your browser support it.
+                    </p>
+                    <a href={`${protocol}//${this.state.url?.host}`} target='_blank' rel='noopener noreferrer'>
+                      {this.state.url?.host}
+                    </a>
+                  </div>
+                ),
+                title: <IntlMessage id='connectionError' />,
+              })
+            } catch {
+              reconnect = false
+            }
+          }
+        }
+
+        if (reconnect) {
+          this.effects._connect()
+        }
       },
       _connect: async function () {
         const { vmId } = this.props
@@ -97,11 +139,11 @@ const Console = withState<State, Props, Effects, Computed, ParentState, ParentEf
               throw new Error('Not connected to XAPI')
             }
 
-            const url = new URL(consoles[0].location)
-            url.protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:'
-            url.searchParams.set('session_id', xapi.sessionId)
+            this.state.url = new URL(consoles[0].location)
+            this.state.url.protocol = window.location.protocol === Protocols.https ? Protocols.wss : Protocols.ws
+            this.state.url.searchParams.set('session_id', xapi.sessionId)
 
-            this.state.rfb = new RFB(this.state.container.current, url, {
+            this.state.rfb = new RFB(this.state.container.current, this.state.url, {
               wsProtocols: ['binary'],
             })
             this.state.rfb.addEventListener('connect', this.effects._handleConnect)
