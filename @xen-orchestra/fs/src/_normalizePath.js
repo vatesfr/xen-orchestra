43236f25ad2adf87f1dc0337fd2834ddde3aa28b
Julien Fontanet <julien.fontanet@isonoe.net>
2022-03-14 12:41:07 +0100

    chore(fs/_normalizePath): comment more behaviors

--- @xen-orchestra/fs/src/_normalizePath.js
+++ @xen-orchestra/fs/src/_normalizePath.js
@@ -5,5 +5,7 @@ const { resolve } = path.posix
 // normalize the path:
 // - does not contains `.` or `..`  (cannot escape root dir)
 // - always starts with `/`
+// - no trailing slash (expect for root)
+// - no duplicate slashes
 const normalizePath = path => resolve('/', path)
 export { normalizePath as default }
