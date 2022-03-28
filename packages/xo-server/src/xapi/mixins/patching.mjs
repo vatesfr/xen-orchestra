vatesfr/xen-orchestra

commit dc27317d0b2dd6c038a3b89191fe6daed0f06024

feat(xo-server/rollingPoolUpdate): fail if some hosts are not running
Fixes zammad#795.

packages/xo-server/src/xapi/mixins/patching.mjs
@@ -8,13 +8,14 @@ import unzip from 'unzipper'
 import { createLogger } from '@xen-orchestra/log'
 import { decorateWith } from '@vates/decorate-with'
 import { defer as deferrable } from 'golike-defer'
+import { incorrectState } from 'xo-common/api-errors'
 import { timeout } from 'promise-toolbox'
 
 import ensureArray from '../../_ensureArray.mjs'
 import { debounceWithKey } from '../../_pDebounceWithKey.mjs'
 import { forEach, mapFilter, parseXml } from '../../utils.mjs'
 
-import { extractOpaqueRef, parseDateTime, useUpdateSystem } from '../utils.mjs'
+import { extractOpaqueRef, isHostRunning, parseDateTime, useUpdateSystem } from '../utils.mjs'
 
 // TOC -------------------------------------------------------------------------
 
@@ -485,6 +486,20 @@ export default {
     }
 
     const hosts = filter(this.objects.all, { $type: 'host' })
+
+    {
+      const deadHost = hosts.find(_ => !isHostRunning(_))
+      if (deadHost !== undefined) {
+        // reflect the interface of an XO host object
+        throw incorrectState({
+          actual: 'Halted',
+          expected: 'Running',
+          object: deadHost.$id,
+          property: 'power_state',
+        })
+      }
+    }
+
     await Promise.all(hosts.map(host => host.$call('assert_can_evacuate')))
 
     log.debug('Install patches')

commit 8d6ba861184f8d8a2fa856b2953a6e78d54460a1

fix(xo-server): xo-common/api-error.js import
Introduced by dc27317d0

packages/xo-server/src/xapi/mixins/patching.mjs
@@ -8,7 +8,7 @@ import unzip from 'unzipper'
 import { createLogger } from '@xen-orchestra/log'
 import { decorateWith } from '@vates/decorate-with'
 import { defer as deferrable } from 'golike-defer'
-import { incorrectState } from 'xo-common/api-errors'
+import { incorrectState } from 'xo-common/api-errors.js'
 import { timeout } from 'promise-toolbox'
 
 import ensureArray from '../../_ensureArray.mjs'