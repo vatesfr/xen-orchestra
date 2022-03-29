b3f27810ab759db74869cdd0790bcdaa3eb9e85d
Thierry Goettelmann <thierry@byscripts.info>
2022-03-29 09:46:24 +0200

    fix(backups/_backupType): XVA sum files are not detected

--- @xen-orchestra/backups/_backupType.js
+++ @xen-orchestra/backups/_backupType.js
@@ -3,4 +3,4 @@
 exports.isMetadataFile = filename => filename.endsWith('.json')
 exports.isVhdFile = filename => filename.endsWith('.vhd')
 exports.isXvaFile = filename => filename.endsWith('.xva')
-exports.isXvaSumFile = filename => filename.endsWith('.xva.cheksum')
+exports.isXvaSumFile = filename => filename.endsWith('.xva.checksum')
