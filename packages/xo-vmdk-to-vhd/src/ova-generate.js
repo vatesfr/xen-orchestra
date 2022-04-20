vatesfr/xen-orchestra

commit 4b9db257fda4cc3ce71392d146abbc4e30e441a2

feat: initial support for OVA VM export (#6006)
Co-authored-by: Florent Beauchamp <flo850@free.fr>

packages/xo-vmdk-to-vhd/src/ova-generate.js
@@ -1,0 +1,194 @@
+import tar from 'tar-stream'
+import { computeVmdkLength, vhdToVMDKIterator } from '.'
+import { fromCallback } from 'promise-toolbox'
+
+// WE MIGHT WANT TO HAVE A LOOK HERE: https://opennodecloud.com/howto/2013/12/25/howto-ON-ovf-reference.html
+
+/**
+ *
+ * @param writeStream
+ * @param vmName
+ * @param vmDescription
+ * @param disks [{name, fileName, capacityMB, getStream}]
+ * @param nics [{name, networkName}]
+ * @param vmMemoryMB
+ * @param cpuCount
+ * @returns readStream
+ */
+export async function writeOvaOn(
+  writeStream,
+  { vmName, vmDescription = '', disks = [], firmware = 'bios', nics = [], vmMemoryMB = 64, cpuCount = 1 }
+) {
+  const ovf = createOvf(vmName, vmDescription, disks, nics, vmMemoryMB, cpuCount, firmware)
+  const pack = tar.pack()
+  const pipe = pack.pipe(writeStream)
+  await fromCallback.call(pack, pack.entry, { name: `${vmName}.ovf` }, Buffer.from(ovf, 'utf8'))
+
+  async function writeDisk(entry, blockIterator) {
+    for await (const block of blockIterator) {
+      entry.write(block)
+    }
+  }
+
+  // https://github.com/mafintosh/tar-stream/issues/24#issuecomment-558358268
+  async function pushDisk(disk) {
+    const size = await computeVmdkLength(disk.name, await disk.getStream())
+    disk.fileSize = size
+    const blockIterator = await vhdToVMDKIterator(disk.name, await disk.getStream())
+
+    return new Promise((resolve, reject) => {
+      const entry = pack.entry({ name: `${disk.name}.vmdk`, size: size }, err => {
+        if (err == null) {
+          return resolve()
+        } else return reject(err)
+      })
+      return writeDisk(entry, blockIterator).then(
+        () => entry.end(),
+        e => reject(e)
+      )
+    })
+  }
+
+  for (const disk of disks) {
+    await pushDisk(disk)
+  }
+  pack.finalize()
+  return pipe
+}
+
+function createDiskSections(disks) {
+  const fileReferences = []
+  const diskFragments = []
+  const diskItems = []
+  for (let i = 0; i < disks.length; i++) {
+    const disk = disks[i]
+    const diskId = `vmdisk${i + 1}`
+    fileReferences.push(`    <File ovf:href="${disk.fileName}" ovf:id="file${i + 1}"/>`)
+    diskFragments.push(
+      `    <Disk ovf:capacity="${
+        disk.capacityMB
+      }" ovf:capacityAllocationUnits="byte * 2^20" ovf:diskId="${diskId}" ovf:fileRef="file${
+        i + 1
+      }" ovf:format="http://www.vmware.com/interfaces/specifications/vmdk.html#streamOptimized" />`
+    )
+    diskItems.push(`
+      <Item>
+        <rasd:AddressOnParent>${i}</rasd:AddressOnParent>
+        <rasd:ElementName>Hard Disk ${i + 1}</rasd:ElementName>
+        <rasd:HostResource>ovf:/disk/${diskId}</rasd:HostResource>
+        <rasd:InstanceID>${diskId}</rasd:InstanceID>
+        <rasd:Parent>4</rasd:Parent>
+        <rasd:ResourceType>17</rasd:ResourceType>
+      </Item>
+    `)
+  }
+  return {
+    fileReferences: fileReferences.join('\n'),
+    diskFragments: diskFragments.join('\n'),
+    diskItems: diskItems.join('\n'),
+  }
+}
+
+function createNicsSection(nics) {
+  const networks = new Set()
+  const nicItems = []
+  for (let i = 0; i < nics.length; i++) {
+    const nic = nics[i]
+    networks.add(nic.networkName)
+    const text = `
+      <Item>
+        <rasd:AddressOnParent>${i}</rasd:AddressOnParent>
+        <rasd:AutomaticAllocation>true</rasd:AutomaticAllocation>
+        <rasd:Connection>${nic.networkName}</rasd:Connection>
+        <rasd:Description>PCNet32 ethernet adapter on "${nic.networkName}"</rasd:Description>
+        <rasd:ElementName>Connection to ${nic.networkName}</rasd:ElementName>
+        <rasd:InstanceID>${'nic' + i}</rasd:InstanceID>
+        <rasd:ResourceSubType>PCNet32</rasd:ResourceSubType>
+        <rasd:Address>${nic.macAddress}</rasd:Address>
+        <rasd:ResourceType>10</rasd:ResourceType>
+      </Item>
+`
+    nicItems.push(text)
+  }
+  const networksElements = []
+  for (const network of networks) {
+    networksElements.push(`    <Network ovf:name="${network}"/>`)
+  }
+  return { nicsSection: nicItems.join('\n'), networksSection: networksElements.join('\n') }
+}
+
+function createOvf(vmName, vmDescription, disks = [], nics = [], vmMemoryMB = 64, cpuCount = 1, firmware = 'bios') {
+  const diskSection = createDiskSections(disks)
+  const networkSection = createNicsSection(nics)
+  let id = 1
+  const nextId = () => id++
+
+  return `<?xml version="1.0" encoding="UTF-8"?>
+<!--Generated by Xen Orchestra-->
+<ovf:Envelope xmlns="http://schemas.dmtf.org/ovf/envelope/1" xmlns:ovf="http://schemas.dmtf.org/ovf/envelope/1"
+    xmlns:rasd="http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_ResourceAllocationSettingData"
+    xmlns:vssd="http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_VirtualSystemSettingData">
+  <References>
+${diskSection.fileReferences}
+  </References>
+  <DiskSection>
+    <Info>Virtual disk information</Info>
+${diskSection.diskFragments}
+  </DiskSection>
+  <NetworkSection>
+    <Info>The list of logical networks</Info>
+${networkSection.networksSection}
+  </NetworkSection>
+  <VirtualSystem ovf:id="${vmName}">
+    <Info>A virtual machine</Info>
+    <Name>${vmName}</Name>
+    <OperatingSystemSection ovf:id="${nextId()}">
+      <Info>The kind of installed guest operating system</Info>
+   </OperatingSystemSection>
+    <VirtualHardwareSection>
+      <Info>Virtual hardware requirements</Info>
+      <System>
+        <vssd:ElementName>Virtual Hardware Family</vssd:ElementName>
+        <vssd:InstanceID>0</vssd:InstanceID>
+        <vssd:VirtualSystemIdentifier>${vmName}</vssd:VirtualSystemIdentifier>
+        <vssd:VirtualSystemType>vmx-11</vssd:VirtualSystemType>
+      </System>
+      <Item>
+        <rasd:AllocationUnits>hertz * 10^6</rasd:AllocationUnits>
+        <rasd:Description>Number of Virtual CPUs</rasd:Description>
+        <rasd:ElementName>${cpuCount} virtual CPU(s)</rasd:ElementName>
+        <rasd:InstanceID>1</rasd:InstanceID>
+        <rasd:ResourceType>3</rasd:ResourceType>
+        <rasd:VirtualQuantity>${cpuCount}</rasd:VirtualQuantity>
+      </Item>
+      <Item>
+        <rasd:AllocationUnits>byte * 2^20</rasd:AllocationUnits>
+        <rasd:Description>Memory Size</rasd:Description>
+        <rasd:ElementName>${vmMemoryMB}MB of memory</rasd:ElementName>
+        <rasd:InstanceID>2</rasd:InstanceID>
+        <rasd:ResourceType>4</rasd:ResourceType>
+        <rasd:VirtualQuantity>${vmMemoryMB}</rasd:VirtualQuantity>
+      </Item>
+      <Item>
+        <rasd:Address>0</rasd:Address>
+        <rasd:Description>IDE Controller</rasd:Description>
+        <rasd:ElementName>VirtualIDEController 0</rasd:ElementName>
+        <rasd:InstanceID>4</rasd:InstanceID>
+        <rasd:ResourceType>5</rasd:ResourceType>
+      </Item>
+      <Item ovf:required="false">
+        <rasd:AutomaticAllocation>false</rasd:AutomaticAllocation>
+        <rasd:ElementName>VirtualVideoCard</rasd:ElementName>
+        <rasd:InstanceID>5</rasd:InstanceID>
+        <rasd:ResourceType>24</rasd:ResourceType>
+      </Item>
+${diskSection.diskItems}
+${networkSection.nicsSection}
+    </VirtualHardwareSection>
+    <AnnotationSection ovf:required="false">
+      <Info>A human-readable annotation</Info>
+      <Annotation>${vmDescription}</Annotation>
+    </AnnotationSection>
+  </VirtualSystem>
+</ovf:Envelope>`
+}

docker/Dockerfile
@@ -4,7 +4,7 @@ FROM ubuntu:xenial
 # https://qastack.fr/programming/25899912/how-to-install-nvm-in-docker
 
 RUN apt-get update
-RUN apt-get install -y curl qemu-utils blktap-utils vmdk-stream-converter git
+RUN apt-get install -y curl qemu-utils blktap-utils vmdk-stream-converter git libxml2-utils
 ENV NVM_DIR /usr/local/nvm
 RUN mkdir -p /usr/local/nvm
 RUN cd /usr/local/nvm

packages/xo-server/src/api/vm.mjs
@@ -6,8 +6,8 @@ import getStream from 'get-stream'
 import hrp from 'http-request-plus'
 import { createLogger } from '@xen-orchestra/log'
 import { defer } from 'golike-defer'
-import { FAIL_ON_QUEUE } from 'limit-concurrency-decorator'
 import { format } from 'json-rpc-peer'
+import { FAIL_ON_QUEUE } from 'limit-concurrency-decorator'
 import { ignoreErrors } from 'promise-toolbox'
 import { invalidParameters, noSuchObject, operationFailed, unauthorized } from 'xo-common/api-errors.js'
 import { Ref } from 'xen-api'
@@ -1004,10 +1004,11 @@ revert.resolve = {
 
 // -------------------------------------------------------------------
 
-async function handleExport(req, res, { xapi, vmRef, compress }) {
-  const stream = await xapi.VM_export(FAIL_ON_QUEUE, vmRef, {
-    compress,
-  })
+async function handleExport(req, res, { xapi, vmRef, compress, format = 'xva' }) {
+  // @todo : should we put back the handleExportFAIL_ON_QUEUE ?
+  const stream =
+    format === 'ova' ? await xapi.exportVmOva(vmRef) : await xapi.VM_export(FAIL_ON_QUEUE, vmRef, { compress })
+
   res.on('close', () => stream.cancel())
   // Remove the filename as it is already part of the URL.
   stream.headers['content-disposition'] = 'attachment'
@@ -1017,7 +1018,7 @@ async function handleExport(req, res, { xapi, vmRef, compress }) {
 }
 
 // TODO: integrate in xapi.js
-async function export_({ vm, compress }) {
+async function export_({ vm, compress, format = 'xva' }) {
   if (vm.power_state === 'Running') {
     await checkPermissionOnSrs.call(this, vm)
   }
@@ -1026,11 +1027,12 @@ async function export_({ vm, compress }) {
     xapi: this.getXapi(vm),
     vmRef: vm._xapiRef,
     compress,
+    format,
   }
 
   return {
     $getFrom: await this.registerHttpRequest(handleExport, data, {
-      suffix: '/' + encodeURIComponent(`${safeDateFormat(new Date())} - ${vm.name_label}.xva`),
+      suffix: '/' + encodeURIComponent(`${safeDateFormat(new Date())} - ${vm.name_label}.${format}`),
     }),
   }
 }
@@ -1038,6 +1040,7 @@ async function export_({ vm, compress }) {
 export_.params = {
   vm: { type: 'string' },
   compress: { type: ['boolean', 'string'], optional: true },
+  format: { enum: ['xva', 'ova'], optional: true },
 }
 
 export_.resolve = {
@@ -1060,6 +1063,7 @@ async function handleVmImport(req, res, { data, srId, type, xapi }) {
   // Timeout seems to be broken in Node 4.
   // See https://github.com/nodejs/node/issues/3319
   req.setTimeout(43200000) // 12 hours
+
   // expect "multipart/form-data; boundary=something"
   const contentType = req.headers['content-type']
   const vm = await (contentType !== undefined && contentType.startsWith('multipart/form-data')

packages/xo-server/src/xapi/index.mjs
@@ -20,7 +20,7 @@ import semver from 'semver'
 import tarStream from 'tar-stream'
 import uniq from 'lodash/uniq.js'
 import { asyncMap } from '@xen-orchestra/async-map'
-import { vmdkToVhd, vhdToVMDK } from 'xo-vmdk-to-vhd'
+import { vmdkToVhd, vhdToVMDK, writeOvaOn } from 'xo-vmdk-to-vhd'
 import { cancelable, CancelToken, fromEvents, ignoreErrors, pCatch, pRetry } from 'promise-toolbox'
 import { createLogger } from '@xen-orchestra/log'
 import { decorateWith } from '@vates/decorate-with'
@@ -517,6 +517,76 @@ export default class Xapi extends XapiBase {
     }
 
     return console
+  }
+
+  @cancelable
+  async exportVmOva($cancelToken, vmRef) {
+    const vm = this.getObject(vmRef)
+    const useSnapshot = isVmRunning(vm)
+    let exportedVm
+    if (useSnapshot) {
+      const snapshotRef = await this.VM_snapshot(vmRef, {
+        name_label: vm.name_label,
+        cancelToken: $cancelToken,
+      })
+      exportedVm = this.getObject(snapshotRef)
+    } else {
+      exportedVm = vm
+    }
+
+    const collectedDisks = []
+    for (const blockDevice of exportedVm.$VBDs) {
+      if (blockDevice.type === 'Disk') {
+        const vdi = blockDevice.$VDI
+        collectedDisks.push({
+          getStream: () => {
+            return this.exportVdiContent($cancelToken, vdi, VDI_FORMAT_VHD)
+          },
+          name: vdi.name_label,
+          fileName: vdi.name_label + '.vmdk',
+          description: vdi.name_description,
+          capacityMB: Math.ceil(vdi.virtual_size / 1024 / 1024),
+        })
+      }
+    }
+    const nics = []
+    for (const vif of exportedVm.$VIFs) {
+      nics.push({
+        macAddress: vif.MAC_autogenerated ? '' : vif.MAC,
+        networkName: this.getObject(vif.network).name_label,
+      })
+    }
+
+    const writeStream = new PassThrough()
+    writeStream.task = this.task_create('VM OVA export', exportedVm.name_label)
+    writeOvaOn(writeStream, {
+      disks: collectedDisks,
+      vmName: exportedVm.name_label,
+      vmDescription: exportedVm.name_description,
+      cpuCount: exportedVm.VCPUs_at_startup,
+      vmMemoryMB: Math.ceil(exportedVm.memory_dynamic_max / 1024 / 1024),
+      firmware: exportedVm.HVM_boot_params.firmware,
+      nics,
+    })
+
+    writeStream.statusCode = 200
+    writeStream.headers = { 'content-type': 'application/ova' }
+    writeStream.statusMessage = 'OK'
+
+    let destroyed = false
+    const destroySnapshot = () => {
+      if (useSnapshot && !destroyed) {
+        destroyed = true
+        this.VM_destroy(exportedVm.$ref)::ignoreErrors()
+      }
+    }
+    writeStream.cancel = () => {
+      destroySnapshot()
+      return writeStream.destroy()
+    }
+    writeStream.once('end', destroySnapshot)
+    writeStream.once('error', destroySnapshot)
+    return writeStream
   }
 
   // Create a snapshot (if necessary) of the VM and returns a delta export
@@ -1670,8 +1740,11 @@ export default class Xapi extends XapiBase {
     if (base !== undefined) {
       params.base = base
     }
-    const vhdResult = await this.VDI_exportContent(vdi.$ref, params)
-    const vmdkStream = await vhdToVMDK(filename, vhdResult)
+    let vhdResult
+    const vmdkStream = await vhdToVMDK(`${vdi.name_label}.vmdk`, async () => {
+      vhdResult = await this.VDI_exportContent(vdi.$ref, params)
+      return vhdResult
+    })
     // callers expect the stream to be an HTTP response.
     vmdkStream.headers = {
       ...vhdResult.headers,

packages/xo-vmdk-to-vhd/src/index.js
@@ -1,9 +1,10 @@
 import asyncIteratorToStream from 'async-iterator-to-stream'
 import createReadableSparseStream from 'vhd-lib/createReadableSparseStream.js'
-import { parseOVAFile, ParsableFile } from './ova'
+import { parseOVAFile, ParsableFile } from './ova-read'
 import VMDKDirectParser from './vmdk-read'
 import { generateVmdkData } from './vmdk-generate'
-import { parseVhdToBlocks } from './parseVhdToBlocks.js'
+import { writeOvaOn } from './ova-generate'
+import { parseVhdToBlocks } from './parseVhdToBlocks'
 
 export { default as readVmdkGrainTable, readCapacityAndGrainTable } from './vmdk-read-table'
 
@@ -26,15 +27,43 @@ async function vmdkToVhd(vmdkReadStream, grainLogicalAddressList, grainFileOffse
   )
 }
 
+export async function computeVmdkLength(diskName, vhdReadStream) {
+  let length = 0
+  for await (const b of await vhdToVMDKIterator(diskName, vhdReadStream)) {
+    length += b.length
+  }
+  return length
+}
+
 /**
- *
+ * @param diskName
+ * @param vhdReadStreamGetter an async function whose call brings a fresh VHD readStream.
+ * We need to read the VHD twice when generating OVA files to get the VMDK file length.
+ * @param withLength if true, the returned VMDK stream will have its `length` field set. The VHD stream will be entirely read to compute it.
+ * @returns a readable stream representing a VMDK file
+ */
+export async function vhdToVMDK(diskName, vhdReadStreamGetter, withLength = false) {
+  let length
+  if (withLength) {
+    length = await computeVmdkLength(diskName, await vhdReadStreamGetter())
+  }
+  const iterable = await vhdToVMDKIterator(diskName, await vhdReadStreamGetter())
+  const stream = await asyncIteratorToStream(iterable)
+  if (withLength) {
+    stream.length = length
+  }
+  return stream
+}
+
+/**
+ * the returned stream will have its length set IIF compress === false
  * @param diskName
  * @param vhdReadStream
  * @returns a readable stream representing a VMDK file
  */
-async function vhdToVMDK(diskName, vhdReadStream) {
+export async function vhdToVMDKIterator(diskName, vhdReadStream) {
   const { blockSize, blocks, diskSize, geometry } = await parseVhdToBlocks(vhdReadStream)
-  return asyncIteratorToStream(generateVmdkData(diskName, diskSize, blockSize, blocks, geometry))
+  return generateVmdkData(diskName, diskSize, blockSize, blocks, geometry)
 }
 
-export { ParsableFile, parseOVAFile, vmdkToVhd, vhdToVMDK }
+export { ParsableFile, parseOVAFile, vmdkToVhd, writeOvaOn }

packages/xo-vmdk-to-vhd/src/ova-generate.integ.spec.js
@@ -1,0 +1,91 @@
+/* eslint-env jest */
+import { validateXMLWithXSD } from 'validate-with-xmllint'
+import { createReadStream, createWriteStream, readFile } from 'fs-extra'
+import execa from 'execa'
+import path from 'path'
+import { pFromCallback, fromEvent } from 'promise-toolbox'
+import tmp from 'tmp'
+import rimraf from 'rimraf'
+import { writeOvaOn } from './ova-generate'
+import { parseOVF } from './ova-read'
+
+const initialDir = process.cwd()
+jest.setTimeout(100000)
+beforeEach(async () => {
+  const dir = await pFromCallback(cb => tmp.dir(cb))
+  process.chdir(dir)
+})
+
+afterEach(async () => {
+  const tmpDir = process.cwd()
+  process.chdir(initialDir)
+  await pFromCallback(cb => rimraf(tmpDir, cb))
+})
+
+test('An ova file is generated correctly', async () => {
+  const inputRawFileName1 = 'random-data1.raw'
+  const inputRawFileName2 = 'random-data2.raw'
+  const vhdFileName1 = 'random-data1.vhd'
+  const vhdFileName2 = 'random-data2.vhd'
+  const ovaFileName1 = 'random-disk1.ova'
+  const dataSize = 100 * 1024 * 1024 // this number is an integer head/cylinder/count equation solution
+  try {
+    await execa('base64 /dev/urandom | head -c ' + dataSize + ' > ' + inputRawFileName1, [], { shell: true })
+    await execa('base64 /dev/urandom | head -c ' + dataSize + ' > ' + inputRawFileName2, [], { shell: true })
+    await execa('qemu-img', ['convert', '-fraw', '-Ovpc', inputRawFileName1, vhdFileName1])
+    await execa('qemu-img', ['convert', '-fraw', '-Ovpc', inputRawFileName2, vhdFileName2])
+    const destination = await createWriteStream(ovaFileName1)
+    const diskName1 = 'disk1'
+    const diskName2 = 'disk2'
+    const vmdkDiskName1 = `${diskName1}.vmdk`
+    const vmdkDiskName2 = `${diskName2}.vmdk`
+    const pipe = await writeOvaOn(destination, {
+      vmName: 'vm1',
+      vmDescription: 'desc',
+      vmMemoryMB: 100,
+      cpuCount: 3,
+      nics: [{ name: 'eth12', networkName: 'BigLan' }],
+      disks: [
+        {
+          name: diskName1,
+          fileName: 'diskName1.vmdk',
+          capacityMB: Math.ceil((dataSize / 1024) * 1024),
+          getStream: async () => {
+            return createReadStream(vhdFileName1)
+          },
+        },
+        {
+          name: diskName2,
+          fileName: 'diskName1.vmdk',
+          capacityMB: Math.ceil((dataSize / 1024) * 1024),
+          getStream: async () => {
+            return createReadStream(vhdFileName2)
+          },
+        },
+      ],
+    })
+    await fromEvent(pipe, 'finish')
+    await execa('tar', ['xf', ovaFileName1, 'vm1.ovf'])
+    const xml = await readFile('vm1.ovf', { encoding: 'utf-8' })
+
+    try {
+      await validateXMLWithXSD(xml, path.join(__dirname, 'ova-schema', 'dsp8023_1.1.1.xsd'))
+      await execa('tar', ['xf', ovaFileName1, vmdkDiskName1])
+      await execa('tar', ['xf', ovaFileName1, vmdkDiskName2])
+      await execa('qemu-img', ['check', vmdkDiskName1])
+      await execa('qemu-img', ['check', vmdkDiskName2])
+      await execa('qemu-img', ['compare', inputRawFileName1, vmdkDiskName1])
+      await execa('qemu-img', ['compare', inputRawFileName2, vmdkDiskName2])
+      await parseOVF({ read: () => xml }, s => s)
+    } catch (e) {
+      e.xml = xml
+      // console.log({ xml })
+      throw e
+    }
+  } catch (error) {
+    console.error(error.stdout)
+    console.error(error.stderr)
+    console.error(error.message)
+    throw error
+  }
+})

packages/xo-vmdk-to-vhd/src/ova-read.js
@@ -141,7 +141,7 @@ const filterDisks = disks => {
   }
 }
 
-async function parseOVF(fileFragment, stringDeserializer) {
+export async function parseOVF(fileFragment, stringDeserializer) {
   const xmlString = stringDeserializer(await fileFragment.read(), 'utf-8')
   return new Promise((resolve, reject) =>
     xml2js.parseString(

packages/xo-vmdk-to-vhd/src/ova-schema/CIM_BootConfigSetting.xsd
@@ -1,0 +1,144 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<xs:schema xmlns:cim="http://schemas.dmtf.org/wbem/wscim/1/common" xmlns:class="http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_BootConfigSetting" xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" elementFormDefault="qualified" targetNamespace="http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_BootConfigSetting">
+    <xs:import namespace="http://schemas.dmtf.org/wbem/wscim/1/common" schemaLocation="http://schemas.dmtf.org/wbem/wscim/1/common.xsd"/>
+    <xs:element name="InstanceID" type="cim:cimString"/>
+    <xs:element name="ElementName" type="cim:cimString"/>
+    <xs:element name="ChangeableType" nillable="true">
+        <xs:complexType>
+            <xs:simpleContent>
+                <xs:restriction base="cim:cimUnsignedShort">
+                    <xs:enumeration value="0"/>
+                    <xs:enumeration value="1"/>
+                    <xs:enumeration value="2"/>
+                    <xs:enumeration value="3"/>
+                    <xs:anyAttribute namespace="##any" processContents="lax"/>
+                </xs:restriction>
+            </xs:simpleContent>
+        </xs:complexType>
+    </xs:element>
+    <xs:element name="ConfigurationName" nillable="true" type="cim:cimString"/>
+    <xs:element name="ComponentSetting" nillable="true" type="xs:anyType"/>
+    <xs:element name="SoID" nillable="true" type="cim:cimString"/>
+    <xs:element name="SoOrgID" nillable="true" type="cim:cimString"/>
+    <xs:element name="Caption" nillable="true">
+        <xs:complexType>
+            <xs:simpleContent>
+                <xs:restriction base="cim:cimString">
+                    <xs:maxLength value="64"/>
+                    <xs:anyAttribute namespace="##any" processContents="lax"/>
+                </xs:restriction>
+            </xs:simpleContent>
+        </xs:complexType>
+    </xs:element>
+    <xs:element name="Description" nillable="true" type="cim:cimString"/>
+    <xs:element name="Generation" nillable="true" type="cim:cimUnsignedLong"/>
+    <xs:element name="ChangeBootOrder_INPUT">
+        <xs:complexType>
+            <xs:sequence>
+                <xs:element maxOccurs="unbounded" minOccurs="0" name="Source" nillable="true" type="cim:cimReference"/>
+            </xs:sequence>
+        </xs:complexType>
+    </xs:element>
+    <xs:element name="ChangeBootOrder_OUTPUT">
+        <xs:complexType>
+            <xs:sequence>
+                <xs:element name="Job" nillable="true" type="cim:cimReference"/>
+                <xs:element name="ReturnValue" nillable="true">
+                    <xs:complexType>
+                        <xs:simpleContent>
+                            <xs:restriction base="cim:cimAnySimpleType">
+                                <xs:simpleType>
+                                    <xs:union>
+                                        <xs:simpleType>
+                                            <xs:restriction base="xs:unsignedInt">
+                                                <xs:enumeration value="0"/>
+                                                <xs:enumeration value="1"/>
+                                                <xs:enumeration value="2"/>
+                                                <xs:enumeration value="3"/>
+                                                <xs:enumeration value="4"/>
+                                                <xs:enumeration value="5"/>
+                                                <xs:enumeration value="6"/>
+                                            </xs:restriction>
+                                        </xs:simpleType>
+                                        <xs:simpleType>
+                                            <xs:restriction base="xs:unsignedInt">
+                                                <xs:minInclusive value="7"/>
+                                                <xs:maxInclusive value="32767"/>
+                                            </xs:restriction>
+                                        </xs:simpleType>
+                                        <xs:simpleType>
+                                            <xs:restriction base="xs:unsignedInt">
+                                                <xs:minInclusive value="32768"/>
+                                                <xs:maxInclusive value="65535"/>
+                                            </xs:restriction>
+                                        </xs:simpleType>
+                                    </xs:union>
+                                </xs:simpleType>
+                                <xs:anyAttribute namespace="##any" processContents="lax"/>
+                            </xs:restriction>
+                        </xs:simpleContent>
+                    </xs:complexType>
+                </xs:element>
+            </xs:sequence>
+        </xs:complexType>
+    </xs:element>
+    <xs:element name="ValidateSettings_INPUT">
+        <xs:complexType/>
+    </xs:element>
+    <xs:element name="ValidateSettings_OUTPUT">
+        <xs:complexType>
+            <xs:sequence>
+                <xs:element name="ReturnValue" nillable="true">
+                    <xs:complexType>
+                        <xs:simpleContent>
+                            <xs:restriction base="cim:cimAnySimpleType">
+                                <xs:simpleType>
+                                    <xs:union>
+                                        <xs:simpleType>
+                                            <xs:restriction base="xs:unsignedInt">
+                                                <xs:enumeration value="0"/>
+                                                <xs:enumeration value="1"/>
+                                                <xs:enumeration value="2"/>
+                                                <xs:enumeration value="3"/>
+                                            </xs:restriction>
+                                        </xs:simpleType>
+                                        <xs:simpleType>
+                                            <xs:restriction base="xs:unsignedInt">
+                                                <xs:minInclusive value="4"/>
+                                                <xs:maxInclusive value="32767"/>
+                                            </xs:restriction>
+                                        </xs:simpleType>
+                                        <xs:simpleType>
+                                            <xs:restriction base="xs:unsignedInt">
+                                                <xs:minInclusive value="32768"/>
+                                                <xs:maxInclusive value="65535"/>
+                                            </xs:restriction>
+                                        </xs:simpleType>
+                                    </xs:union>
+                                </xs:simpleType>
+                                <xs:anyAttribute namespace="##any" processContents="lax"/>
+                            </xs:restriction>
+                        </xs:simpleContent>
+                    </xs:complexType>
+                </xs:element>
+            </xs:sequence>
+        </xs:complexType>
+    </xs:element>
+    <xs:element name="CIM_BootConfigSetting" type="class:CIM_BootConfigSetting_Type"/>
+    <xs:complexType name="CIM_BootConfigSetting_Type">
+        <xs:sequence>
+            <xs:element minOccurs="0" ref="class:Caption"/>
+            <xs:element minOccurs="0" ref="class:ChangeableType"/>
+            <xs:element maxOccurs="unbounded" minOccurs="0" ref="class:ComponentSetting"/>
+            <xs:element minOccurs="0" ref="class:ConfigurationName"/>
+            <xs:element minOccurs="0" ref="class:Description"/>
+            <xs:element ref="class:ElementName"/>
+            <xs:element minOccurs="0" ref="class:Generation"/>
+            <xs:element ref="class:InstanceID"/>
+            <xs:element minOccurs="0" ref="class:SoID"/>
+            <xs:element minOccurs="0" ref="class:SoOrgID"/>
+            <xs:any maxOccurs="unbounded" minOccurs="0" namespace="##other" processContents="lax"/>
+        </xs:sequence>
+        <xs:anyAttribute namespace="##any" processContents="lax"/>
+    </xs:complexType>
+</xs:schema>

commit 6da0aa376f48e3fe61c8d7d5310f5493abe0caef

fix(xo-vmdk-to-vhd/createNicsSection): remove incorrect XML node
Introduced by 4b9db257f

See https://github.com/vatesfr/xen-orchestra/pull/6195#discussion_r853177122

packages/xo-vmdk-to-vhd/src/ova-generate.js
@@ -104,7 +104,6 @@ function createNicsSection(nics) {
         <rasd:ElementName>Connection to ${nic.networkName}</rasd:ElementName>
         <rasd:InstanceID>${'nic' + i}</rasd:InstanceID>
         <rasd:ResourceSubType>PCNet32</rasd:ResourceSubType>
-        <rasd:Address>${nic.macAddress}</rasd:Address>
         <rasd:ResourceType>10</rasd:ResourceType>
       </Item>
 `