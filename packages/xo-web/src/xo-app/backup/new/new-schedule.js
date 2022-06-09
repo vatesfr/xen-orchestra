809a2657d79d55b19da4eaa0336fb9c47d01473f
mathieuRA <contact@mathieu-raisin.fr>
2022-06-09 15:30:12 +0200

    add missing file

--- packages/xo-web/src/xo-app/backup/new/new-schedule.js
+++ packages/xo-web/src/xo-app/backup/new/new-schedule.js
@@ -146,6 +146,7 @@ const New = decorate([
               getXoaPlan().value < ENTERPRISE.value ? _('healthCheckAvailableEnterpriseUser') : undefined
             )}
           </label>
+          {console.log(schedule)}
           {schedule.healthCheckVmsWithTags !== undefined && (
             <div className='mb-2'>
               <strong>{_('vmsTags')}</strong>
