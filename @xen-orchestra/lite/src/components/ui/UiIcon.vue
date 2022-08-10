949f3377c5ec0d9de2c4ea9c7a3bc7cd7894583f
Thierry Goettelmann <thierry@byscripts.info>
2022-08-10 08:37:26 +0200

    feat(xo-lite/component): UI Icon utility component (#6353)

---
+++ @xen-orchestra/lite/src/components/ui/UiIcon.vue
@@ -0,0 +1,28 @@
+<template>
+  <FontAwesomeIcon
+    v-if="icon !== undefined"
+    :icon="icon"
+    :spin="busy"
+    class="ui-icon"
+    :fixed-width="fixedWidth"
+  />
+</template>
+
+<script lang="ts" setup>
+import { computed } from "vue";
+import type { IconDefinition } from "@fortawesome/fontawesome-common-types";
+import { faSpinner } from "@fortawesome/pro-solid-svg-icons";
+
+const props = withDefaults(
+  defineProps<{
+    busy?: boolean;
+    icon?: IconDefinition;
+    fixedWidth?: boolean;
+  }>(),
+  { fixedWidth: true }
+);
+
+const icon = computed(() => (props.busy ? faSpinner : props.icon));
+</script>
+
+<style lang="postcss" scoped></style>
