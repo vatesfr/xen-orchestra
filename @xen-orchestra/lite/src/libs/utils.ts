22ab42b2b4ac82ec136b396a0c8715ad1e6c0ead
mathieuRA <contact@mathieu-raisin.fr>
2022-08-18 11:44:24 +0200

    code enhancement

--- @xen-orchestra/lite/src/libs/utils.ts
+++ @xen-orchestra/lite/src/libs/utils.ts
@@ -1,5 +1,9 @@
 import { utcParse } from "d3-time-format";
+<<<<<<< HEAD
 import humanFormat from "human-format";
+=======
+import { find, forEach, size } from "lodash";
+>>>>>>> code enhancement
 import { round } from "lodash-es";
 import { find, forEach, isEqual, size, sum } from "lodash-es";
 import { type ComputedGetter, type Ref, computed, ref, watchEffect } from "vue";
