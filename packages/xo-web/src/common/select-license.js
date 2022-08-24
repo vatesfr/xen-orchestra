12ea38ae34010e4e6b241290a3d4a38442f16c1b
Rajaa <rajaa@pc-55.home>
2022-08-24 14:48:03 +0200

    fix

--- packages/xo-web/src/common/select-license.js
+++ packages/xo-web/src/common/select-license.js
@@ -4,7 +4,6 @@ import decorate from 'apply-decorators'
 import ActionButton from 'action-button'
 import React from 'react'
 import { map } from 'lodash'
-import { get } from '@xen-orchestra/defined'
 import { getLicenses } from 'xo'
 import { injectIntl } from 'react-intl'
 import { injectState, provideState } from 'reaclette'
@@ -27,48 +26,51 @@ const SelectLicense = decorate([
     },
   }),
   injectState,
-  ({ state: { license = 'none', licenses }, effects, intl: { formatTime }, ...props }) => licenseError !== undefined 
-    ? <span><em className='text-danger'>{_('getLicensesError')}</em></span>
-    : (
-    <form className='form-inline'>
-      <select className='form-control' name='license' onChange={effects.linkState}>
-        {_('selectLicense', message => (
-          <option key='none' value='none'>
-            {message}
-          </option>
-        ))}
-        {map(licenses, license =>
-          _(
-            'expiresOn',
-            {
-              date:
-                license.expires !== undefined
-                  ? formatTime(license.expires, {
-                      day: 'numeric',
-                      month: 'numeric',
-                      year: 'numeric',
-                    })
-                  : '',
-            },
-            message => (
-              <option key={license.id} value={license.id}>
-                {license.id.slice(-4)} {license.expires ? `(${message})` : ''}
-              </option>
+  ({ state: { license = 'none', licenses }, effects, intl: { formatTime }, ...props }) =>
+    licenses.licenseError !== undefined ? (
+      <span>
+        <em className='text-danger'>{_('getLicensesError')}</em>
+      </span>
+    ) : (
+      <form className='form-inline'>
+        <select className='form-control' name='license' onChange={effects.linkState}>
+          {_('selectLicense', message => (
+            <option key='none' value='none'>
+              {message}
+            </option>
+          ))}
+          {map(licenses, license =>
+            _(
+              'expiresOn',
+              {
+                date:
+                  license.expires !== undefined
+                    ? formatTime(license.expires, {
+                        day: 'numeric',
+                        month: 'numeric',
+                        year: 'numeric',
+                      })
+                    : '',
+              },
+              message => (
+                <option key={license.id} value={license.id}>
+                  {license.id.slice(-4)} {license.expires ? `(${message})` : ''}
+                </option>
+              )
             )
-          )
-        )}
-      </select>
-      <ActionButton
-        btnStyle='primary'
-        className='ml-1'
-        disabled={license === 'none'}
-        handler={props.onChange}
-        handlerParam={license}
-        icon='connect'
-      >
-        {_('bindLicense')}
-      </ActionButton>
-    </form>
-  ),
+          )}
+        </select>
+        <ActionButton
+          btnStyle='primary'
+          className='ml-1'
+          disabled={license === 'none'}
+          handler={props.onChange}
+          handlerParam={license}
+          icon='connect'
+        >
+          {_('bindLicense')}
+        </ActionButton>
+      </form>
+    ),
 ])
 export default SelectLicense
