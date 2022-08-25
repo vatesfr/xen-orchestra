51c3fc90611b5bcc13b209de9d5442f8a9d1bf5e
mathieuRA <contact@mathieu-raisin.fr>
2022-08-25 10:48:33 +0200

    feat(lite): alert when unreachable hosts

--- @xen-orchestra/lite/src/App.vue
+++ @xen-orchestra/lite/src/App.vue
@@ -17,13 +17,14 @@
 </template>
 
 <script lang="ts" setup>
-import { watchEffect } from "vue";
+import { ref, watchEffect } from "vue";
 import favicon from "@/assets/favicon.svg";
 import AppHeader from "@/components/AppHeader.vue";
 import AppLogin from "@/components/AppLogin.vue";
 import AppTooltips from "@/components/AppTooltips.vue";
 import InfraPoolList from "@/components/infra/InfraPoolList.vue";
 import { useChartTheme } from "@/composables/chart-theme.composable";
+import { useHostStore } from "@/stores/host.store";
 import { useXenApiStore } from "@/stores/xen-api.store";
 
 let link: HTMLLinkElement | null = document.querySelector("link[rel~='icon']");
@@ -40,7 +41,10 @@ if (window.localStorage?.getItem("colorMode") !== "light") {
   document.documentElement.classList.add("dark");
 }
 
+const unreachableHosts = ref();
+
 const xenApiStore = useXenApiStore();
+const hostStore = useHostStore();
 
 watchEffect(() => {
   if (xenApiStore.isConnected) {
@@ -49,6 +53,41 @@ watchEffect(() => {
 });
 
 useChartTheme();
+watchEffect(async () => {
+  const _unreachableHosts = [];
+  for (const key in hostStore.allRecords) {
+    const host = hostStore.allRecords[key];
+
+    const url = new URL("http://localhost");
+    url.protocol = window.location.protocol;
+    url.hostname = host.address;
+
+    try {
+      await fetch(url, {
+        mode: "no-cors",
+      });
+    } catch (_) {
+      _unreachableHosts.push(url);
+    }
+  }
+  unreachableHosts.value = _unreachableHosts;
+});
+
+watchEffect(() => {
+  if (unreachableHosts.value?.length > 0) {
+    // TODO: Replace with modal component when available.
+    let string = "The following hosts are unreachable. ";
+    if (window.location.protocol === "https:") {
+      string +=
+        "You may need to allow self-signed SSL certificates in your browser.";
+    }
+    string += "\n\n";
+    unreachableHosts.value.forEach((url: URL) => {
+      string += url.href + "\n";
+    });
+    alert(string);
+  }
+});
 </script>
 
 <style lang="postcss">
