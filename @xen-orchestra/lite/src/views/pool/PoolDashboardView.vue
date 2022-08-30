8246db30cb3aa806da02100c28fc0f9385ee96a3
Thierry Goettelmann <thierry@byscripts.info>
2022-08-30 10:55:01 +0200

    chore(lite): remove fake cards from Pool Dashboard (#6385)

--- @xen-orchestra/lite/src/views/pool/PoolDashboardView.vue
+++ @xen-orchestra/lite/src/views/pool/PoolDashboardView.vue
@@ -1,27 +1,11 @@
 <template>
   <div class="pool-dashboard-view">
     <PoolDashboardStatus class="item" />
-
-    <UiCard style="min-width: 40rem">
-      <UiTitle type="h4">Storage usage</UiTitle>
-      <ProgressBar :value="65" style="margin: 1rem 0" />
-      <ProgressBar :value="50" style="margin: 1rem 0" />
-      <ProgressBar :value="40" style="margin: 1rem 0" />
-      <ProgressBar :value="22" style="margin: 1rem 0" />
-    </UiCard>
-
-    <UiCard>
-      <UiBadge>38/64 GB</UiBadge>
-    </UiCard>
   </div>
 </template>
 
 <script lang="ts" setup>
-import ProgressBar from "@/components/ProgressBar.vue";
 import PoolDashboardStatus from "@/components/pool/dashboard/PoolDashboardStatus.vue";
-import UiBadge from "@/components/ui/UiBadge.vue";
-import UiCard from "@/components/ui/UiCard.vue";
-import UiTitle from "@/components/ui/UiTitle.vue";
 </script>
 
 <style lang="postcss" scoped>
