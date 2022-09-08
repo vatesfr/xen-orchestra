a994dd9d584c3290e0f322be85d6fbd6a8e52080
Thierry Goettelmann <thierry@byscripts.info>
2022-09-08 10:52:36 +0200

    feat(lite/component): Linear Chart (#6376)

--- @xen-orchestra/lite/src/types/human-format.d.ts
+++ @xen-orchestra/lite/src/types/human-format.d.ts
@@ -1,16 +1,3 @@
 declare module "human-format" {
-  type Options = {
-    decimals?: number;
-    maxDecimals?: number;
-    prefix?: string;
-    scale?: string;
-    separator?: string;
-    unit?: string;
-  };
-
-  function humanFormat(value: number, opts?: Options): number;
-  function bytes(value: number): number;
-
-  humanFormat.bytes = bytes;
-  export default humanFormat;
+  function bytes(value: number): string;
 }
