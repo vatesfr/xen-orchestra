6496df9423738d46d5d113b284f00dc3385513e6
mathieuRA <contact@mathieu-raisin.fr>
2022-09-12 16:11:23 +0200

    manage stats at the top level

--- @xen-orchestra/lite/src/components/pool/dashboard/cpuUsage/HostsCpuUsage.vue
+++ @xen-orchestra/lite/src/components/pool/dashboard/cpuUsage/HostsCpuUsage.vue
@@ -8,21 +8,17 @@
 </template>
 
 <script lang="ts" setup>
-import { differenceBy } from "lodash";
-import { computed, onMounted, watch } from "vue";
+import { type ComputedRef, computed, inject } from "vue";
 import UsageBar from "@/components/UsageBar.vue";
-import useFetchStats from "@/composables/fetch-stats.composable";
 import { getAvgCpuUsage } from "@/libs/utils";
-import { GRANULARITY, type HostStats } from "@/libs/xapi-stats";
-import type { XenApiHost } from "@/libs/xen-api";
-import { useHostStore } from "@/stores/host.store";
+import type { HostStats } from "@/libs/xapi-stats";
 
-const { register, unregister, stats } = useFetchStats<XenApiHost, HostStats>(
-  "host",
-  GRANULARITY.Seconds
-);
-
-const hostStore = useHostStore();
+const stats: ComputedRef<
+  {
+    name: string;
+    stats?: HostStats;
+  }[]
+> = inject<any>("hostStats", []);
 
 const data = computed<{ label: string; value: number }[]>(() => {
   const result: { label: string; value: number }[] = [];
@@ -44,20 +40,5 @@ const data = computed<{ label: string; value: number }[]>(() => {
     });
   });
   return result;
-});
-
-watch(
-  () => hostStore.allRecords,
-  (hosts, previousHosts) => {
-    // Host turned On
-    differenceBy(hosts, previousHosts ?? [], "uuid").forEach(register);
-
-    // Host turned Off
-    differenceBy(previousHosts, hosts, "uuid").forEach(unregister);
-  }
-);
-
-onMounted(() => {
-  hostStore.allRecords.forEach(register);
 });
 </script>
--- @xen-orchestra/lite/src/views/pool/PoolDashboardView.vue
+++ @xen-orchestra/lite/src/views/pool/PoolDashboardView.vue
@@ -6,8 +6,64 @@
 </template>
 
 <script lang="ts" setup>
+import { differenceBy } from "lodash-es";
+import { computed, onMounted, provide, readonly, watch } from "vue";
+import ProgressBar from "@/components/ProgressBar.vue";
 import PoolDashboardCpuUsage from "@/components/pool/dashboard/PoolDashboardCpuUsage.vue";
 import PoolDashboardStatus from "@/components/pool/dashboard/PoolDashboardStatus.vue";
+import UiBadge from "@/components/ui/UiBadge.vue";
+import UiCard from "@/components/ui/UiCard.vue";
+import UiTitle from "@/components/ui/UiTitle.vue";
+import useFetchStats from "@/composables/fetch-stats.composable";
+import { GRANULARITY, type HostStats, type VmStats } from "@/libs/xapi-stats";
+import type { XenApiHost, XenApiVm } from "@/libs/xen-api";
+import { useHostStore } from "@/stores/host.store";
+import { useVmStore } from "@/stores/vm.store";
+
+const hostStore = useHostStore();
+const vmStore = useVmStore();
+
+const {
+  register: hostRegister,
+  unregister: hostUnregister,
+  stats: hostStats,
+} = useFetchStats<XenApiHost, HostStats>("host", GRANULARITY.Seconds);
+const {
+  register: vmRegister,
+  unregister: vmUnregister,
+  stats: vmStats,
+} = useFetchStats<XenApiVm, VmStats>("vm", GRANULARITY.Seconds);
+
+const runningVms = computed(() =>
+  vmStore.allRecords.filter((vm) => vm.power_state === "Running")
+);
+
+provide("hostStats", readonly(hostStats));
+provide("vmStats", readonly(vmStats));
+
+watch(
+  () => hostStore.allRecords,
+  (hosts, previousHosts) => {
+    // turned On
+    differenceBy(hosts, previousHosts ?? [], "uuid").forEach(hostRegister);
+
+    // turned Off
+    differenceBy(previousHosts, hosts, "uuid").forEach(hostUnregister);
+  }
+);
+
+watch(runningVms, (vms, previousVms) => {
+  // turned On
+  differenceBy(vms, previousVms ?? [], "uuid").forEach(vmRegister);
+
+  // turned Off
+  differenceBy(previousVms, vms, "uuid").forEach(vmUnregister);
+});
+
+onMounted(() => {
+  hostStore.allRecords.forEach(hostRegister);
+  runningVms.value.forEach(vmRegister);
+});
 </script>
 
 <style lang="postcss" scoped>
