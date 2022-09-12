d1b845f6e99e8d91fb5bf2bf277effa8011a372a
Thierry Goettelmann <thierry@byscripts.info>
2022-09-12 15:29:10 +0200

    feat(lite): tooltips (#6412)

---
+++ @xen-orchestra/lite/src/components/AppTooltips.vue
@@ -0,0 +1,19 @@
+<template>
+  <AppTooltip
+    v-for="tooltip in tooltips"
+    :key="tooltip.target"
+    :options="tooltip.options"
+    :target="tooltip.target"
+  />
+</template>
+
+<script lang="ts" setup>
+import { storeToRefs } from "pinia";
+import AppTooltip from "@/components/AppTooltip.vue";
+import { useTooltipStore } from "@/stores/tooltip.store";
+
+const tooltipStore = useTooltipStore();
+const { tooltips } = storeToRefs(tooltipStore);
+</script>
+
+<style scoped></style>
