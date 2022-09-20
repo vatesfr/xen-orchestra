47dd5ca8fb4119c78522ba51ec9a1a0e8a08e97f
Thierry Goettelmann <thierry@byscripts.info>
2022-09-20 13:37:14 +0200

    feat(lite/component): Menu component (#6354)
    * feat(lite/component): Menu component
    
    * feat(lite/component): Add disabled prop to AppMenu
    
    * feat(lite/component): Add custom placement to AppMenu + Fix trigger color
    
    * feat(lite/component): Update VmsActionBar to use new AppMenu (#6357)
    
    * fix(lite/menu): Doesn't teleport the root menu if no trigger
    
    * Don't disable a menu item having a submenu
    
    * i18n

--- @xen-orchestra/lite/src/components/AccountButton.vue
+++ @xen-orchestra/lite/src/components/AccountButton.vue
@@ -1,24 +1,67 @@
 <template>
-  <button class="account-button">
-    <FontAwesomeIcon class="user-icon" :icon="faCircleUser" />
-    <FontAwesomeIcon class="dropdown-icon" :icon="faAngleDown" />
-  </button>
+  <AppMenu placement="bottom-end" shadow>
+    <template #trigger="{ open, isOpen }">
+      <button :class="{ active: isOpen }" class="account-button" @click="open">
+        <UiIcon :icon="faCircleUser" class="user-icon" />
+        <UiIcon :icon="faAngleDown" class="dropdown-icon" />
+      </button>
+    </template>
+    <MenuItem :icon="faGear">{{ $t("settings") }}</MenuItem>
+    <MenuItem :icon="faMessage" @click="openFeedbackUrl">
+      {{ $t("send-us-feedback") }}
+    </MenuItem>
+    <MenuItem
+      :icon="faArrowRightFromBracket"
+      class="menu-item-logout"
+      @click="logout"
+    >
+      {{ $t("log-out") }}
+    </MenuItem>
+  </AppMenu>
 </template>
 
 <script lang="ts" setup>
-import { faAngleDown, faCircleUser } from "@fortawesome/free-solid-svg-icons";
+import { nextTick } from "vue";
+import { useRouter } from "vue-router";
+import {
+  faAngleDown,
+  faArrowRightFromBracket,
+  faCircleUser,
+  faGear,
+  faMessage,
+} from "@fortawesome/free-solid-svg-icons";
+import AppMenu from "@/components/menu/AppMenu.vue";
+import MenuItem from "@/components/menu/MenuItem.vue";
+import UiIcon from "@/components/ui/UiIcon.vue";
+import { useXenApiStore } from "@/stores/xen-api.store";
+
+const router = useRouter();
+
+const logout = () => {
+  const xenApiStore = useXenApiStore();
+  xenApiStore.disconnect();
+  nextTick(() => router.push({ name: "home" }));
+};
+
+const openFeedbackUrl = () => {
+  window.open(
+    "https://xcp-ng.org/forum/topic/4731/xen-orchestra-lite",
+    "_blank",
+    "noopener"
+  );
+};
 </script>
 
 <style scoped>
 .account-button {
-  padding: 1rem;
   display: flex;
   align-items: center;
-  gap: 0.8rem;
-  border-radius: 0.8rem;
-  border: none;
+  padding: 1rem;
   color: var(--color-blue-scale-100);
+  border: none;
+  border-radius: 0.8rem;
   background-color: var(--background-color-secondary);
+  gap: 0.8rem;
 
   &:disabled {
     color: var(--color-blue-scale-400);
@@ -26,12 +69,15 @@ import { faAngleDown, faCircleUser } from "@fortawesome/free-solid-svg-icons";
 
   &:not(:disabled) {
     cursor: pointer;
+
     &:hover,
-    &:active {
+    &:active,
+    &.active {
       background-color: var(--background-color-primary);
     }
 
-    &:active {
+    &:active,
+    &.active {
       color: var(--color-extra-blue-base);
     }
   }
@@ -43,5 +89,9 @@ import { faAngleDown, faCircleUser } from "@fortawesome/free-solid-svg-icons";
 
 .dropdown-icon {
   font-size: 1.6rem;
+}
+
+.menu-item-logout {
+  color: var(--color-red-vates-base);
 }
 </style>
