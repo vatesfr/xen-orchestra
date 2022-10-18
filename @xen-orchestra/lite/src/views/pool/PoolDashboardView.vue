9963568368668fd8cd734e5e8cce1ade3978af8b
Mathieu <70369997+MathieuRA@users.noreply.github.com>
2022-10-18 11:02:07 +0200

    feat(lite/pool/dashboard): top 5 CPU usage (#6370)

--- @xen-orchestra/lite/src/views/pool/PoolDashboardView.vue
+++ @xen-orchestra/lite/src/views/pool/PoolDashboardView.vue
@@ -2,12 +2,65 @@
   <div class="pool-dashboard-view">
     <PoolDashboardStatus class="item" />
     <PoolDashboardStorageUsage class="item" />
+    <PoolDashboardCpuUsage class="item" />
   </div>
 </template>
 
 <script lang="ts" setup>
+import { differenceBy } from "lodash-es";
+import { computed, onMounted, provide, readonly, watch } from "vue";
+import PoolDashboardCpuUsage from "@/components/pool/dashboard/PoolDashboardCpuUsage.vue";
 import PoolDashboardStatus from "@/components/pool/dashboard/PoolDashboardStatus.vue";
 import PoolDashboardStorageUsage from "@/components/pool/dashboard/PoolDashboardStorageUsage.vue";
+import useFetchStats from "@/composables/fetch-stats.composable";
+import { isHostRunning } from "@/libs/utils";
+import { GRANULARITY, type HostStats, type VmStats } from "@/libs/xapi-stats";
+import type { XenApiHost, XenApiVm } from "@/libs/xen-api";
+import { useHostStore } from "@/stores/host.store";
+import { useVmStore } from "@/stores/vm.store";
+
+const hostStore = useHostStore();
+const vmStore = useVmStore();
+
+const {
+  register: hostRegister,
+  unregister: hostUnregister,
+  stats: hostStats,
+} = useFetchStats<XenApiHost, HostStats>("host", GRANULARITY.Seconds);
+const {
+  register: vmRegister,
+  unregister: vmUnregister,
+  stats: vmStats,
+} = useFetchStats<XenApiVm, VmStats>("vm", GRANULARITY.Seconds);
+
+const runningHosts = computed(() => hostStore.allRecords.filter(isHostRunning));
+const runningVms = computed(() =>
+  vmStore.allRecords.filter((vm) => vm.power_state === "Running")
+);
+
+provide("hostStats", readonly(hostStats));
+provide("vmStats", readonly(vmStats));
+
+watch(runningHosts, (hosts, previousHosts) => {
+  // turned On
+  differenceBy(hosts, previousHosts ?? [], "uuid").forEach(hostRegister);
+
+  // turned Off
+  differenceBy(previousHosts, hosts, "uuid").forEach(hostUnregister);
+});
+
+watch(runningVms, (vms, previousVms) => {
+  // turned On
+  differenceBy(vms, previousVms ?? [], "uuid").forEach(vmRegister);
+
+  // turned Off
+  differenceBy(previousVms, vms, "uuid").forEach(vmUnregister);
+});
+
+onMounted(() => {
+  runningHosts.value.forEach(hostRegister);
+  runningVms.value.forEach(vmRegister);
+});
 </script>
 
 <style lang="postcss" scoped>
