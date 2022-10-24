21c9020235e3440a020caf88e9e8fc9f68f07468
ggunullu <gabrokh@pm.me>
2022-10-24 20:30:45 +0200

    test(async-each): from Jest to test

--- @vates/async-each/index.test.js
+++ @vates/async-each/index.test.js
@@ -1,6 +1,8 @@
 'use strict'
 
-/* eslint-env jest */
+const { describe, it, beforeEach } = require('test')
+const { spy, assert } = require('sinon')
+const { rejects } = require('tap')
 
 const { asyncEach } = require('./')
 
@@ -34,12 +36,18 @@ describe('asyncEach', () => {
       })
 
       it('works', async () => {
-        const iteratee = jest.fn(async () => {})
+        const iteratee = spy(async () => {})
 
         await asyncEach.call(thisArg, iterable, iteratee, { concurrency: 1 })
 
-        expect(iteratee.mock.instances).toEqual(Array.from(values, () => thisArg))
-        expect(iteratee.mock.calls).toEqual(Array.from(values, (value, index) => [value, index, iterable]))
+        assert.match(
+          iteratee.thisValues,
+          Array.from(values, () => thisArg)
+        )
+        assert.match(
+          iteratee.args,
+          Array.from(values, (value, index) => [value, index, iterable])
+        )
       })
       ;[1, 2, 4].forEach(concurrency => {
         it('respects a concurrency of ' + concurrency, async () => {
@@ -49,7 +57,7 @@ describe('asyncEach', () => {
             values,
             async () => {
               ++running
-              expect(running).toBeLessThanOrEqual(concurrency)
+              assert.match(running <= concurrency, true)
               await randomDelay()
               --running
             },
@@ -60,41 +68,42 @@ describe('asyncEach', () => {
 
       it('stops on first error when stopOnError is true', async () => {
         const error = new Error()
-        const iteratee = jest.fn((_, i) => {
+        const iteratee = spy((_, i) => {
           if (i === 1) {
             throw error
           }
         })
 
-        expect(await rejectionOf(asyncEach(iterable, iteratee, { concurrency: 1, stopOnError: true }))).toBe(error)
-        expect(iteratee).toHaveBeenCalledTimes(2)
+        assert.match(await rejectionOf(asyncEach(iterable, iteratee, { concurrency: 1, stopOnError: true })), error)
+        assert.calledTwice(iteratee)
       })
 
       it('rejects AggregateError when stopOnError is false', async () => {
         const errors = []
-        const iteratee = jest.fn(() => {
+        const iteratee = spy(() => {
           const error = new Error()
           errors.push(error)
           throw error
         })
 
         const error = await rejectionOf(asyncEach(iterable, iteratee, { stopOnError: false }))
-        expect(error.errors).toEqual(errors)
-        expect(iteratee.mock.calls).toEqual(Array.from(values, (value, index) => [value, index, iterable]))
+        assert.match(error.errors, errors)
+        assert.match(
+          iteratee.args,
+          Array.from(values, (value, index) => [value, index, iterable])
+        )
       })
 
       it('can be interrupted with an AbortSignal', async () => {
         const ac = new AbortController()
-        const iteratee = jest.fn((_, i) => {
+        const iteratee = spy((_, i) => {
           if (i === 1) {
             ac.abort()
           }
         })
 
-        await expect(asyncEach(iterable, iteratee, { concurrency: 1, signal: ac.signal })).rejects.toThrow(
-          'asyncEach aborted'
-        )
-        expect(iteratee).toHaveBeenCalledTimes(2)
+        await rejects(asyncEach(iterable, iteratee, { concurrency: 1, signal: ac.signal }), 'asyncEach aborted')
+        assert.calledTwice(iteratee)
       })
     })
   )
