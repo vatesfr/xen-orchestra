14e205ab699e563a1159ed87bdac7c1d02052f03
Florent Beauchamp <flo850@free.fr>
2022-10-24 17:54:20 +0200

    fix(vhd-cli): sync handler

--- packages/vhd-cli/commands/merge.js
+++ packages/vhd-cli/commands/merge.js
@@ -1,28 +1,30 @@
 'use strict'
 
 const { Bar } = require('cli-progress')
-const { getHandler } = require('@xen-orchestra/fs')
+const { getSyncedHandler } = require('@xen-orchestra/fs')
 const { mergeVhdChain } = require('vhd-lib/merge')
 const { resolve } = require('path')
+const { Disposable } = require('promise-toolbox')
 
 module.exports = async function merge(args) {
   if (args.length < 2 || args.some(_ => _ === '-h' || _ === '--help')) {
     return `Usage: ${this.command} <child VHD> <parent VHD>`
   }
 
-  const handler = getHandler({ url: 'file:///' })
-  let bar
-  await mergeVhdChain(handler, [resolve(args[1]), resolve(args[0])], {
-    onProgress({ done, total }) {
-      if (bar === undefined) {
-        bar = new Bar({
-          format: 'merging [{bar}] {percentage}% | ETA: {eta}s | {value}/{total}',
-        })
-        bar.start(total, done)
-      } else {
-        bar.update(done)
-      }
-    },
+  await Disposable.use(getSyncedHandler({ url: 'file:///' }), async handler => {
+    let bar
+    await mergeVhdChain(handler, [resolve(args[1]), resolve(args[0])], {
+      onProgress({ done, total }) {
+        if (bar === undefined) {
+          bar = new Bar({
+            format: 'merging [{bar}] {percentage}% | ETA: {eta}s | {value}/{total}',
+          })
+          bar.start(total, done)
+        } else {
+          bar.update(done)
+        }
+      },
+    })
+    bar.stop()
   })
-  bar.stop()
 }
