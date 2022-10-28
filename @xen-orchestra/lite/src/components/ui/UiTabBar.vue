54c289524c6b392748fc48259006e7eb8001c97d
Thierry Goettelmann <thierry@byscripts.info>
2022-10-28 11:08:32 +0200

    feat(lite): UiTabBar, UiTab, RouterTab
    `TabBar` and `TabBarItem` was only able to handle router links.
    
    The wrapper `TabBar` is now `UiTabBar`.
    
    `TabBarItem` is now `RouterTab` to handle router links.
    
    `UiTab` has been created to handle any other scenario with `@click` event, `tag` and `active` props.
    
    Possibility to use `disabled` prop directly on `UiTabBar` to disable all children items.

---
+++ @xen-orchestra/lite/src/components/ui/UiTabBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="ui-tab-bar">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed, provide } from "vue";
+
+const props = defineProps<{
+  disabled?: boolean;
+}>();
+
+provide(
+  "isTabBarDisabled",
+  computed(() => props.disabled)
+);
+</script>
+
+<style lang="postcss" scoped>
+.ui-tab-bar {
+  display: flex;
+  align-items: stretch;
+  height: 6.5rem;
+  border-bottom: 1px solid var(--color-blue-scale-400);
+  background-color: var(--background-color-primary);
+}
+</style>
