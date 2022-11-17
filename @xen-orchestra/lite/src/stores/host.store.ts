eb157f059852f9356ab3660eb62d46e61ab2c22d
mathieuRA <contact@mathieu-raisin.fr>
2022-11-17 16:36:31 +0100

    add button to reload the page

--- @xen-orchestra/lite/src/stores/host.store.ts
+++ @xen-orchestra/lite/src/stores/host.store.ts
@@ -1,10 +1,10 @@
-import { defineStore } from "pinia";
 import { computed } from "vue";
 import { sortRecordsByNameLabel } from "@/libs/utils";
 import type { GRANULARITY } from "@/libs/xapi-stats";
 import type { XenApiHost } from "@/libs/xen-api";
 import { createRecordContext } from "@/stores/index";
 import { useXenApiStore } from "@/stores/xen-api.store";
+import { defineStore } from "pinia";
 
 export const useHostStore = defineStore("host", () => {
   const xenApiStore = useXenApiStore();
