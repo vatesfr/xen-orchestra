3a9b722fa977fdbc102874324c1d6eb8c7196596
Thierry Goettelmann <thierry@byscripts.info>
2022-11-21 17:51:04 +0100

    feat(lite): Remove ProgressBar in favor of new UiProgressBar. Update UsageBar.
    ProgressBar component handled too much logic (a progress bar + a circle icon + a label + a badge)
    
    Since at some point we need a simple progress bar, all the additional logic should be handled by UsageBar.
    
    - Move usage-specific logic from `ProgressBar` to `UsageBar`
    - Removed `ProgressBar` component
    - Created `ui/UiProgressBar` component containing only the bar itself
    - Updated `UsageBar` to use `UiProgressBar` then adapting its style

--- @xen-orchestra/lite/src/components/UsageBar.vue
+++ @xen-orchestra/lite/src/components/UsageBar.vue
@@ -3,25 +3,31 @@
     <div class="header">
       <slot name="header" />
     </div>
-    <template v-if="data !== undefined">
-      <ProgressBar
+    <div v-if="data !== undefined">
+      <div
         v-for="item in computedData.sortedArray"
         :key="item.id"
-        :value="item.value"
-        :label="item.label"
-        :badge-label="item.badgeLabel"
-      />
-      <div class="footer">
-        <slot name="footer" :total-percent="computedData.totalPercentUsage" />
+        class="progress-item"
+      >
+        <UiProgressBar :value="item.value" color="custom" />
+        <div class="badge">
+          <span class="circle" />
+          {{ item.label }}
+          <UiBadge v-if="item.badgeLabel">{{ item.badgeLabel }}</UiBadge>
+        </div>
       </div>
-    </template>
+      <div class="footer">
+        <slot :total-percent="computedData.totalPercentUsage" name="footer" />
+      </div>
+    </div>
     <UiSpinner v-else class="spinner" />
   </div>
 </template>
 
 <script lang="ts" setup>
+import UiBadge from "@/components/ui/UiBadge.vue";
+import UiProgressBar from "@/components/ui/UiProgressBar.vue";
 import { computed } from "vue";
-import ProgressBar from "@/components/ProgressBar.vue";
 import UiSpinner from "@/components/ui/UiSpinner.vue";
 
 interface Data {
@@ -33,7 +39,7 @@ interface Data {
 }
 
 interface Props {
-  data?: Array<Data>;
+  data?: Data[];
   nItems?: number;
 }
 
@@ -84,23 +90,36 @@ const computedData = computed(() => {
   width: 40px;
   height: 40px;
 }
-</style>
 
-<style>
-.progress-bar-component:nth-of-type(2) .progress-bar-fill,
-.progress-bar-component:nth-of-type(2) .circle {
-  background-color: var(--color-extra-blue-d60);
+.badge {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 0.5rem;
 }
-.progress-bar-component:nth-of-type(3) .progress-bar-fill,
-.progress-bar-component:nth-of-type(3) .circle {
-  background-color: var(--color-extra-blue-d40);
+
+.progress-item {
+  --progress-bar-height: 1.2rem;
+  --progress-bar-color: var(--color-extra-blue-l20);
 }
-.progress-bar-component:nth-of-type(4) .progress-bar-fill,
-.progress-bar-component:nth-of-type(4) .circle {
-  background-color: var(--color-extra-blue-d20);
+
+.progress-item:nth-child(1) {
+  --progress-bar-color: var(--color-extra-blue-d60);
 }
-.progress-bar-component .progress-bar-fill,
-.progress-bar-component .circle {
-  background-color: var(--color-extra-blue-l20);
+
+.progress-item:nth-child(2) {
+  --progress-bar-color: var(--color-extra-blue-d40);
+}
+
+.progress-item:nth-child(3) {
+  --progress-bar-color: var(--color-extra-blue-d20);
+}
+
+.circle {
+  display: inline-block;
+  width: 1rem;
+  height: 1rem;
+  border-radius: 0.5rem;
+  background-color: var(--progress-bar-color);
 }
 </style>
---
+++ @xen-orchestra/lite/src/components/ui/UiProgressBar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="ui-progress-bar" :class="`color-${color}`">
+    <div class="fill" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { Color } from "@/types";
+import { computed } from "vue";
+
+const props = withDefaults(
+  defineProps<{
+    color: Color | "custom";
+    value: number;
+    maxValue?: number;
+  }>(),
+  { color: "info", maxValue: 100 }
+);
+
+const progressWithUnit = computed(() => {
+  const progress = Math.round((props.value / props.maxValue) * 100);
+  return `${progress}%`;
+});
+</script>
+
+<style lang="postcss" scoped>
+.ui-progress-bar {
+  overflow: hidden;
+  height: var(--progress-bar-height, 0.4rem);
+  margin: 1rem 0;
+  border-radius: 0.4rem;
+  background-color: var(--background-color-extra-blue);
+
+  &.color-info {
+    --progress-bar-color: var(--color-extra-blue-base);
+  }
+
+  &.color-success {
+    --progress-bar-color: var(--color-green-infra-base);
+  }
+
+  &.color-warning {
+    --progress-bar-color: var(--color-orange-world-base);
+  }
+
+  &.color-error {
+    --progress-bar-color: var(--color-red-vates-base);
+  }
+}
+
+.fill {
+  width: v-bind(progressWithUnit);
+  height: var(--progress-bar-height, 0.4rem);
+  transition: width 1s ease-in-out;
+  background-color: var(--progress-bar-color);
+}
+</style>
