647995428c99185f9f17f96c997a142df19869bd
Mathieu <70369997+MathieuRA@users.noreply.github.com>
2022-11-24 15:57:11 +0100

    feat(lite/pool/dashboard): top 5 RAM usage (#6419)

--- @xen-orchestra/lite/src/components/pool/dashboard/ramUsage/VmsRamUsage.vue
+++ @xen-orchestra/lite/src/components/pool/dashboard/ramUsage/VmsRamUsage.vue
@@ -1,7 +1,7 @@
 <template>
   <UsageBar :data="statFetched ? data : undefined" :n-items="N_ITEMS">
     <template #header>
-      <span>{{ $t("hosts") }}</span>
+      <span>{{ $t("vms") }}</span>
       <span>{{ $t("top-#", { n: N_ITEMS }) }}</span>
     </template>
   </UsageBar>
@@ -11,42 +11,42 @@
 import { type ComputedRef, computed, inject } from "vue";
 import UsageBar from "@/components/UsageBar.vue";
 import type { Stat } from "@/composables/fetch-stats.composable";
-import { getAvgCpuUsage } from "@/libs/utils";
-import type { HostStats } from "@/libs/xapi-stats";
+import { formatSize, parseRamUsage } from "@/libs/utils";
+import type { VmStats } from "@/libs/xapi-stats";
 import { N_ITEMS } from "@/views/pool/PoolDashboardView.vue";
 
-const stats = inject<ComputedRef<Stat<HostStats>[]>>(
-  "hostStats",
+const stats = inject<ComputedRef<Stat<VmStats>[]>>(
+  "vmStats",
   computed(() => [])
 );
 
-const data = computed<{ id: string; label: string; value: number }[]>(() => {
-  const result: { id: string; label: string; value: number }[] = [];
+const data = computed(() => {
+  const result: {
+    id: string;
+    label: string;
+    value: number;
+    badgeLabel: string;
+  }[] = [];
 
   stats.value.forEach((stat) => {
     if (stat.stats === undefined) {
       return;
     }
 
-    const avgCpuUsage = getAvgCpuUsage(stat.stats.cpus);
-
-    if (avgCpuUsage === undefined) {
-      return;
-    }
-
+    const { percentUsed, total, used } = parseRamUsage(stat.stats);
     result.push({
       id: stat.id,
       label: stat.name,
-      value: avgCpuUsage,
+      value: percentUsed,
+      badgeLabel: `${formatSize(used)}/${formatSize(total)}`,
     });
   });
-
   return result;
 });
 
-const statFetched: ComputedRef<boolean> = computed(() =>
-  statFetched.value
-    ? true
-    : stats.value.length > 0 && stats.value.length === data.value.length
+const statFetched: ComputedRef<boolean> = computed(
+  () =>
+    statFetched.value ||
+    (stats.value.length > 0 && stats.value.length === data.value.length)
 );
 </script>
