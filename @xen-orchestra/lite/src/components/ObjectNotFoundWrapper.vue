9af37486fadd055528f2ed476286de60dd3a9d3d
mathieuRA <contact@mathieu-raisin.fr>
2022-12-13 10:41:12 +0100

    add loader

--- @xen-orchestra/lite/src/components/ObjectNotFoundWrapper.vue
+++ @xen-orchestra/lite/src/components/ObjectNotFoundWrapper.vue
@@ -1,9 +1,13 @@
 <template>
-  <ObjectNotFoundView :id="id" v-if="isRecordNotFound" />
+  <div class="wrapper-spinner" v-if="!store.isReady">
+    <UiSpinner class="spinner" />
+  </div>
+  <ObjectNotFoundView :id="id" v-else-if="isRecordNotFound" />
   <slot v-else />
 </template>
 <script lang="ts" setup>
 import ObjectNotFoundView from "@/views/ObjectNotFoundView.vue";
+import UiSpinner from "@/components/ui/UiSpinner.vue";
 import { useHostStore } from "@/stores/host.store";
 import { useVmStore } from "@/stores/vm.store";
 import { computed } from "vue";
@@ -26,3 +30,18 @@ const isRecordNotFound = computed(
   () => store.isReady && !store.hasRecordByUuid(id.value)
 );
 </script>
+
+<style scoped>
+.wrapper-spinner {
+  display: flex;
+  height: 100%;
+}
+
+.spinner {
+  color: var(--color-extra-blue-base);
+  display: flex;
+  margin: auto;
+  width: 10rem;
+  height: 10rem;
+}
+</style>
