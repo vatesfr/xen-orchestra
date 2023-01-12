59123ae44040a7bd50aceaf6e75eef6c72152089
Thierry Goettelmann <thierry@byscripts.info>
2023-01-12 11:24:09 +0100

    Stories implementation

---
+++ @xen-orchestra/lite/src/components/component-story/StoryEventParams.vue
@@ -0,0 +1,34 @@
+<template>
+  <StoryParamsTable>
+    <thead>
+      <tr>
+        <th>Event</th>
+        <th>Type</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="param in params" :key="param.name">
+        <th class="name">
+          {{ param.getFullName() }}
+        </th>
+        <td>
+          <CodeHighlight :code="param.getTypeLabel()" />
+        </td>
+        <td>
+          {{ param.getHelp() }}
+        </td>
+      </tr>
+    </tbody>
+  </StoryParamsTable>
+</template>
+
+<script lang="ts" setup>
+import CodeHighlight from "@/components/CodeHighlight.vue";
+import StoryParamsTable from "@/components/component-story/StoryParamsTable.vue";
+import type { EventParam } from "@/libs/story/story-param";
+
+defineProps<{
+  params: EventParam[];
+}>();
+</script>
---
+++ @xen-orchestra/lite/src/components/component-story/StoryMenu.vue
@@ -0,0 +1,33 @@
+<template>
+  <RouterLink :to="{ name: 'story-home' }">
+    <UiTitle type="h4">Stories</UiTitle>
+  </RouterLink>
+  <ul class="links">
+    <li v-for="route in routes" :key="route.name">
+      <RouterLink class="link" :to="route">
+        {{ route.meta.storyTitle }}
+      </RouterLink>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts" setup>
+import { useRouter } from "vue-router";
+import UiTitle from "@/components/ui/UiTitle.vue";
+
+const { getRoutes } = useRouter();
+
+const routes = getRoutes().filter((route) => route.meta.isStory);
+</script>
+
+<style lang="postcss" scoped>
+.links {
+  padding: 1rem;
+}
+.link {
+  display: inline-block;
+  padding: 0.5rem 1rem;
+  text-decoration: none;
+  font-size: 1.6rem;
+}
+</style>
