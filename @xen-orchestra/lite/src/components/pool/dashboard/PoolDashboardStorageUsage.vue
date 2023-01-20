52e58656e3726492f6bf244029b1983a7a5460e3
mathieuRA <contact@mathieu-raisin.fr>
2023-01-20 10:24:32 +0100

    fix i18n

--- @xen-orchestra/lite/src/components/pool/dashboard/PoolDashboardStorageUsage.vue
+++ @xen-orchestra/lite/src/components/pool/dashboard/PoolDashboardStorageUsage.vue
@@ -23,7 +23,9 @@ import UsageBar from "@/components/UsageBar.vue";
 import UiCard from "@/components/ui/UiCard.vue";
 import { useSrStore } from "@/stores/storage.store";
 import { N_ITEMS } from "@/views/pool/PoolDashboardView.vue";
+
 const srStore = useSrStore();
+
 const data = computed<{
   result: { id: string; label: string; value: number }[];
   maxSize: number;
@@ -32,17 +34,22 @@ const data = computed<{
   const result: { id: string; label: string; value: number }[] = [];
   let maxSize = 0;
   let usedSize = 0;
+
   srStore.allRecords.forEach(
     ({ name_label, physical_size, physical_utilisation, uuid }) => {
       if (physical_size < 0 || physical_utilisation < 0) {
         return;
       }
+
       maxSize += physical_size;
       usedSize += physical_utilisation;
+
       const percent = (physical_utilisation / physical_size) * 100;
+
       if (isNaN(percent)) {
         return;
       }
+
       result.push({
         id: uuid,
         label: name_label,
