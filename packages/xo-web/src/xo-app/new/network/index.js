fe93b2a1198504a7a5b8c60a8e200427f6ad0db1
ggunullu <gabrokh@pm.me>
2023-01-30 17:19:37 +0100

    rename labels

--- packages/xo-web/src/xo-app/new/network/index.js
+++ packages/xo-web/src/xo-app/new/network/index.js
@@ -435,8 +435,8 @@ const NewNetwork = decorate([
                               name='nbd'
                               onChange={effects.onChangeNbd}
                               options={[
-                                { label: _('noNbdConnection'), value: false },
-                                { label: _('nbdConnection'), value: true },
+                                { labelId: _('noNbdConnection'), value: false },
+                                { labelId: _('nbdConnection'), value: true },
                               ]}
                               value={nbd}
                             />
