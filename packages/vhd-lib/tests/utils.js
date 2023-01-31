a4a828a71c1f705969d411fa5ca65e4a3bfe9f60
ggunullu <gabrokh@pm.me>
2023-01-31 14:03:51 +0100

    first try with qemu check

--- packages/vhd-lib/tests/utils.js
+++ packages/vhd-lib/tests/utils.js
@@ -21,7 +21,8 @@ async function createRandomFile(name, sizeMB) {
 exports.createRandomFile = createRandomFile
 
 async function checkFile(vhdName) {
-  await execa('vhd-util', ['check', '-p', '-b', '-t', '-n', vhdName])
+  // await execa('vhd-util', ['check', '-p', '-b', '-t', '-n', vhdName])
+  await execa('qemu-img', ['check', vhdName])
 }
 exports.checkFile = checkFile
 
