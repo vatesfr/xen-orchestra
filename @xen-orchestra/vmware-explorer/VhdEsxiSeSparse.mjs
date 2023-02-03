vatesfr/xen-orchestra

commit 9fa15d9c841d7f32c1e2cf8cc37612be578790c7

feat(xo-server): import VM from ESXi  (#6595)

@xen-orchestra/vmware-explorer/VhdEsxiSeSparse.mjs
@@ -1,0 +1,243 @@
+import _computeGeometryForSize from 'vhd-lib/_computeGeometryForSize.js'
+import { createFooter, createHeader } from 'vhd-lib/_createFooterHeader.js'
+import { FOOTER_SIZE } from 'vhd-lib/_constants.js'
+import { notEqual, strictEqual } from 'node:assert'
+import { unpackFooter, unpackHeader } from 'vhd-lib/Vhd/_utils.js'
+import { VhdAbstract } from 'vhd-lib'
+
+// from https://github.com/qemu/qemu/commit/98eb9733f4cf2eeab6d12db7e758665d2fd5367b#
+
+function readInt64(buffer, index) {
+  const n = buffer.readBigInt64LE(index * 8 /* size of an int64 in bytes */)
+  if (n > Number.MAX_SAFE_INTEGER) {
+    throw new Error(`can't handle ${n} ${Number.MAX_SAFE_INTEGER} ${n & 0x00000000ffffffffn}`)
+  }
+  return +n
+}
+
+export default class VhdEsxiSeSparse extends VhdAbstract {
+  #esxi
+  #datastore
+  #parentVhd
+  #path
+  #lookMissingBlockInParent
+
+  #header
+  #footer
+
+  #grainDirectory
+  // as we will read all grain with data with load everything in memory
+  // in theory , that can be 512MB of data for a 2TB fully allocated
+  // but our use case is to transfer a relatively small diff
+  // and random access is expensive in HTTP, and migration is a one time cors
+  // so let's go with naive approach, and future me will have to handle a more
+  // clever approach if necessary
+  // grain at zero won't be stored
+
+  #grainMap = new Map()
+
+  #grainSize
+  #grainTableSize
+  #grainTableOffset
+  #grainOffset
+
+  static async open(esxi, datastore, path, parentVhd, opts) {
+    const vhd = new VhdEsxiSeSparse(esxi, datastore, path, parentVhd, opts)
+    await vhd.readHeaderAndFooter()
+    return vhd
+  }
+  constructor(esxi, datastore, path, parentVhd, { lookMissingBlockInParent = true } = {}) {
+    super()
+    this.#esxi = esxi
+    this.#path = path
+    this.#datastore = datastore
+    this.#parentVhd = parentVhd
+    this.#lookMissingBlockInParent = lookMissingBlockInParent
+  }
+
+  get header() {
+    return this.#header
+  }
+
+  get footer() {
+    return this.#footer
+  }
+
+  async #readGrain(start, length = 4 * 1024) {
+    return (await this.#esxi.download(this.#datastore, this.#path, `${start}-${start + length - 1}`)).buffer()
+  }
+
+  containsBlock(blockId) {
+    notEqual(this.#grainDirectory, undefined, "bat must be loaded to use contain blocks'")
+
+    // a grain table is 4096 entries of 4KB
+    // a grain table cover 8 vhd blocks
+    // grain table always exists in sespars
+
+    // depending on the paramters we also look into the parent data
+    return (
+      this.#grainDirectory.readInt32LE(blockId * 4) !== 0 ||
+      (this.#lookMissingBlockInParent && this.#parentVhd.containsBlock(blockId))
+    )
+  }
+
+  async #read(start, end) {
+    return (await this.#esxi.download(this.#datastore, this.#path, `${start}-${end}`)).buffer()
+  }
+
+  async readHeaderAndFooter() {
+    const buffer = await this.#read(0, 2048)
+    strictEqual(buffer.readBigInt64LE(0), 0xcafebaben)
+    for (let i = 0; i < 2048 / 8; i++) {
+      console.log(i, '> ', buffer.readBigInt64LE(8 * i).toString(16), buffer.readBigInt64LE(8 * i))
+    }
+
+    strictEqual(readInt64(buffer, 1), 0x200000001) // version 2.1
+
+    const capacity = readInt64(buffer, 2)
+    const grain_size = readInt64(buffer, 3)
+    const grain_table_size = readInt64(buffer, 4)
+    const flags = readInt64(buffer, 5)
+
+    const grain_dir_offset = readInt64(buffer, 16)
+    const grain_dir_size = readInt64(buffer, 17)
+    const grain_tables_offset = readInt64(buffer, 18)
+    const grain_tables_size = readInt64(buffer, 19)
+    this.#grainOffset = readInt64(buffer, 24)
+
+    console.log({
+      capacity,
+      grain_size,
+      grain_table_size,
+      flags,
+      grain_dir_offset,
+      grain_dir_size,
+      grain_tables_offset,
+      grain_tables_size,
+      grainSize: this.#grainSize,
+    })
+
+    this.#grainSize = grain_size * 512 // 8 sectors / 4KB default
+    this.#grainTableOffset = grain_tables_offset * 512
+    this.#grainTableSize = grain_tables_size * 512
+
+    const size = capacity * grain_size * 512
+    this.#header = unpackHeader(createHeader(Math.ceil(size / (4096 * 512))))
+    const geometry = _computeGeometryForSize(size)
+    const actualSize = geometry.actualSize
+    this.#footer = unpackFooter(
+      createFooter(actualSize, Math.floor(Date.now() / 1000), geometry, FOOTER_SIZE, this.#parentVhd.footer.diskType)
+    )
+  }
+
+  async readBlockAllocationTable() {
+    console.log('READ BLOCK ALLOCATION', this.#grainTableSize)
+    const CHUNK_SIZE = 64 * 512
+
+    strictEqual(this.#grainTableSize % CHUNK_SIZE, 0)
+
+    console.log(' will read ', this.#grainTableSize / CHUNK_SIZE, 'table')
+    for (let chunkIndex = 0, grainIndex = 0; chunkIndex < this.#grainTableSize / CHUNK_SIZE; chunkIndex++) {
+      process.stdin.write('.')
+      const start = chunkIndex * CHUNK_SIZE + this.#grainTableOffset
+      const end = start + 4096 * 8 - 1
+      const buffer = await this.#read(start, end)
+      for (let indexInChunk = 0; indexInChunk < 4096; indexInChunk++) {
+        const entry = buffer.readBigInt64LE(indexInChunk * 8)
+        switch (entry) {
+          case 0n: // not allocated, go to parent
+            break
+          case 1n: // unmapped
+            break
+        }
+        if (entry > 3n) {
+          const intIndex = +(((entry & 0x0fff000000000000n) >> 48n) | ((entry & 0x0000ffffffffffffn) << 12n))
+          let pos = intIndex * this.#grainSize + CHUNK_SIZE * chunkIndex + this.#grainOffset
+          console.log({ indexInChunk, grainIndex, intIndex, pos })
+          this.#grainMap.set(grainIndex)
+          grainIndex++
+        }
+      }
+    }
+    console.log('found', this.#grainMap.size)
+
+    // read grain directory and the grain tables
+    const nbBlocks = this.header.maxTableEntries
+    this.#grainDirectory = await this.#read(2048 /* header length */, 2048 + nbBlocks * 4 - 1)
+  }
+
+  // we're lucky : a grain address can address exacty a full block
+  async readBlock(blockId) {
+    notEqual(this.#grainDirectory, undefined, 'grainDirectory is not loaded')
+    const sectorOffset = this.#grainDirectory.readInt32LE(blockId * 4)
+
+    const buffer = (await this.#parentVhd.readBlock(blockId)).buffer
+
+    if (sectorOffset === 0) {
+      strictEqual(this.#lookMissingBlockInParent, true, "shouldn't have empty block in a delta alone")
+      return {
+        id: blockId,
+        bitmap: buffer.slice(0, 512),
+        data: buffer.slice(512),
+        buffer,
+      }
+    }
+    const offset = sectorOffset * 512
+
+    const graintable = await this.#read(offset, offset + 4096 * 4 /* grain table length */ - 1)
+
+    strictEqual(graintable.length, 4096 * 4)
+    // we have no guaranty that data are order or contiguous
+    // let's construct ranges to limit the number of queries
+    let rangeStart, offsetStart, offsetEnd, lastOffset
+
+    const changeRange = async (index, offset) => {
+      if (offsetStart !== undefined) {
+        // if there was a
+        if (offset === offsetEnd) {
+          offsetEnd++
+          return
+        }
+        const grains = await this.#read(offsetStart * 512, offsetEnd * 512 - 1)
+        grains.copy(buffer, (rangeStart + 1) /* block bitmap */ * 512)
+      }
+      if (offset) {
+        // we're at the beginning of a range present in the file
+        rangeStart = index
+        offsetStart = offset
+        offsetEnd = offset + 1
+      } else {
+        // we're at the beginning of a range from the parent or empty
+        rangeStart = undefined
+        offsetStart = undefined
+        offsetEnd = undefined
+      }
+    }
+
+    for (let i = 0; i < graintable.length / 4; i++) {
+      const grainOffset = graintable.readInt32LE(i * 4)
+      if (grainOffset === 0) {
+        await changeRange()
+        // from parent
+        continue
+      }
+      if (grainOffset === 1) {
+        await changeRange()
+        // this is a emptied grain, no data, don't look into parent
+        buffer.fill(0, (i + 1) /* block bitmap */ * 512)
+      }
+
+      if (grainOffset > 1) {
+        // non empty grain
+        await changeRange(i, grainOffset)
+      }
+    }
+    await changeRange()
+    return {
+      id: blockId,
+      bitmap: buffer.slice(0, 512),
+      data: buffer.slice(512),
+      buffer,
+    }
+  }
+}

@xen-orchestra/vmware-explorer/index.mjs
@@ -1,0 +1,16 @@
+import Esxi from './esxi.mjs'
+const host = '10.10.0.62'
+const user = 'root'
+const password = 'vateslab'
+const sslVerify = false
+
+console.log(Esxi)
+const esxi = new Esxi(host, user, password, sslVerify)
+console.log(esxi)
+esxi.on('ready', async function () {
+  //const metadata = await esxi.getTransferableVmMetadata('4')
+  //console.log('metadata', metadata)
+
+  const res = await esxi.powerOn(9)
+  console.log(res)
+})

@xen-orchestra/vmware-explorer/parsers/vmsd.mjs
@@ -1,0 +1,109 @@
+// the vmsd file contain the snapshot history of the VM , and their chaining
+
+function set(obj, keyPath, val) {
+  const [key, ...other] = keyPath
+  // key like snapshot0->snapshot9 are grouped in an array snapshots[]
+  const match = key.match(/^(.+)([0-9])$/)
+  if (match) {
+    // an array
+    let [, label, index] = match
+    // I like my array names in plural form
+    label += 's'
+    if (!obj[label]) {
+      // create array if not exists
+      obj[label] = []
+    }
+    if (other.length) {
+      // it contains objects
+      if (!obj[label][index]) {
+        // and this object is not already initialized
+        obj[label][parseInt(index)] = {}
+      }
+      set(obj[label][index], other, val)
+    } else {
+      // it contains a scalar value
+      obj[label][index] = val
+    }
+  } else {
+    if (other.length) {
+      // an object
+      if (!obj[key]) {
+        // first time
+        obj[key] = {}
+      }
+      set(obj[key], other, val)
+    } else {
+      // a scalar
+      obj[key] = val
+    }
+  }
+}
+
+export default function parseVmsd(text) {
+  const parsed = {}
+  text.split('\n').forEach(line => {
+    const [key, val] = line.split(' = ')
+    if (!key.startsWith('snapshot')) {
+      // we only look for the snapshot part of the file
+      return
+    }
+    // remove the " around value
+    set(parsed, key.split('.'), val?.substring(1, val.length - 1))
+  })
+  if (parsed.snapshot?.current == undefined) {
+    return
+  }
+
+  return {
+    lastUID: parsed.snapshot.current,
+    current: parsed.snapshot.current,
+    numSnapshots: parsed.snapshot.numSnapshots,
+    snapshots: Object.values(parsed.snapshots) || [],
+  }
+}
+
+/** file content example
+ * .encoding = "UTF-8"
+snapshot.lastUID = "4"
+snapshot.current = "4"
+snapshot0.uid = "1"
+snapshot0.filename = "test flo-Snapshot1.vmsn"
+snapshot0.displayName = "SNAPSHOT POST INSTALL"
+snapshot0.description = "blablabla"
+snapshot0.createTimeHigh = "388745"
+snapshot0.createTimeLow = "1991180826"
+snapshot0.numDisks = "1"
+snapshot0.disk0.fileName = "test flo_0.vmdk"
+snapshot0.disk0.node = "scsi0:0"
+snapshot.numSnapshots = "4"
+snapshot1.uid = "2"
+snapshot1.filename = "test flo-Snapshot2.vmsn"
+snapshot1.parent = "1"
+snapshot1.displayName = "SECOND"
+snapshot1.description = "small"
+snapshot1.createTimeHigh = "388801"
+snapshot1.createTimeLow = "-814770620"
+snapshot1.numDisks = "1"
+snapshot1.disk0.fileName = "test flo_0-000001.vmdk"
+snapshot1.disk0.node = "scsi0:0"
+snapshot2.uid = "3"
+snapshot2.filename = "test flo-Snapshot3.vmsn"
+snapshot2.parent = "2"
+snapshot2.displayName = "third"
+snapshot2.type = "1"
+snapshot2.createTimeHigh = "388802"
+snapshot2.createTimeLow = "96936632"
+snapshot2.numDisks = "1"
+snapshot2.disk0.fileName = "test flo_0-000002.vmdk"
+snapshot2.disk0.node = "scsi0:0"
+snapshot3.uid = "4"
+snapshot3.filename = "test flo-Snapshot4.vmsn"
+snapshot3.parent = "3"
+snapshot3.displayName = "from cli"
+snapshot3.type = "1"
+snapshot3.createTimeHigh = "388804"
+snapshot3.createTimeLow = "1541463919"
+snapshot3.numDisks = "1"
+snapshot3.disk0.fileName = "test flo_0-000003.vmdk"
+snapshot3.disk0.node = "scsi0:0"
+ */

@xen-orchestra/vmware-explorer/.npmignore
@@ -1,0 +1,1 @@
+../../scripts/npmignore

@xen-orchestra/vmware-explorer/VhdEsxiCowd.mjs
@@ -1,0 +1,162 @@
+import _computeGeometryForSize from 'vhd-lib/_computeGeometryForSize.js'
+import { createFooter, createHeader } from 'vhd-lib/_createFooterHeader.js'
+import { FOOTER_SIZE } from 'vhd-lib/_constants.js'
+import { notEqual, strictEqual } from 'node:assert'
+import { unpackFooter, unpackHeader } from 'vhd-lib/Vhd/_utils.js'
+import { VhdAbstract } from 'vhd-lib'
+
+export default class VhdEsxiCowd extends VhdAbstract {
+  #esxi
+  #datastore
+  #parentVhd
+  #path
+  #lookMissingBlockInParent
+
+  #header
+  #footer
+
+  #grainDirectory
+
+  static async open(esxi, datastore, path, parentVhd, opts) {
+    const vhd = new VhdEsxiCowd(esxi, datastore, path, parentVhd, opts)
+    await vhd.readHeaderAndFooter()
+    return vhd
+  }
+  constructor(esxi, datastore, path, parentVhd, { lookMissingBlockInParent = true } = {}) {
+    super()
+    this.#esxi = esxi
+    this.#path = path
+    this.#datastore = datastore
+    this.#parentVhd = parentVhd
+    this.#lookMissingBlockInParent = lookMissingBlockInParent
+  }
+
+  get header() {
+    return this.#header
+  }
+
+  get footer() {
+    return this.#footer
+  }
+
+  containsBlock(blockId) {
+    notEqual(this.#grainDirectory, undefined, "bat must be loaded to use contain blocks'")
+    // only check if a grain table exist for on of the sector of the block
+    // the great news is that a grain size has 4096 entries of 512B = 2M
+    // and a vhd block is also 2M
+    // so we only need to check if a grain table exists (it's not created without data)
+
+    // depending on the paramters we also look into the parent data
+    return (
+      this.#grainDirectory.readInt32LE(blockId * 4) !== 0 ||
+      (this.#lookMissingBlockInParent && this.#parentVhd.containsBlock(blockId))
+    )
+  }
+
+  async #read(start, length) {
+    return (await this.#esxi.download(this.#datastore, this.#path, `${start}-${start + length - 1}`)).buffer()
+  }
+
+  async readHeaderAndFooter() {
+    const buffer = await this.#read(0, 2048)
+
+    strictEqual(buffer.slice(0, 4).toString('ascii'), 'COWD')
+    strictEqual(buffer.readInt32LE(4), 1) // version
+    strictEqual(buffer.readInt32LE(8), 3) // flags
+    const numSectors = buffer.readInt32LE(12)
+    const grainSize = buffer.readInt32LE(16)
+    strictEqual(grainSize, 1) // 1 grain should be 1 sector long
+    strictEqual(buffer.readInt32LE(20), 4) // grain directory position in sectors
+
+    const nbGrainDirectoryEntries = buffer.readInt32LE(24)
+    strictEqual(nbGrainDirectoryEntries, Math.ceil(numSectors / 4096))
+    const size = numSectors * 512
+    // a grain directory entry contains the address of a grain table
+    // a grain table can adresses at most 4096 grain of 512 Bytes of data
+    this.#header = unpackHeader(createHeader(Math.ceil(size / (4096 * 512))))
+
+    const geometry = _computeGeometryForSize(size)
+    const actualSize = geometry.actualSize
+    this.#footer = unpackFooter(
+      createFooter(actualSize, Math.floor(Date.now() / 1000), geometry, FOOTER_SIZE, this.#parentVhd.footer.diskType)
+    )
+  }
+
+  async readBlockAllocationTable() {
+    const nbBlocks = this.header.maxTableEntries
+    this.#grainDirectory = await this.#read(2048 /* header length */, nbBlocks * 4)
+  }
+
+  // we're lucky : a grain address can address exacty a full block
+  async readBlock(blockId) {
+    notEqual(this.#grainDirectory, undefined, 'grainDirectory is not loaded')
+    const sectorOffset = this.#grainDirectory.readInt32LE(blockId * 4)
+
+    const buffer = (await this.#parentVhd.readBlock(blockId)).buffer
+
+    if (sectorOffset === 0) {
+      strictEqual(this.#lookMissingBlockInParent, true, "shouldn't have empty block in a delta alone")
+      return {
+        id: blockId,
+        bitmap: buffer.slice(0, 512),
+        data: buffer.slice(512),
+        buffer,
+      }
+    }
+    const offset = sectorOffset * 512
+
+    const graintable = await this.#read(offset, 4096 * 4 /* grain table length */)
+
+    strictEqual(graintable.length, 4096 * 4)
+    // we have no guaranty that data are ordered or contiguous
+    // let's construct ranges to limit the number of queries
+    let rangeStart, offsetStart, offsetEnd
+
+    const changeRange = async (index, offset) => {
+      if (offsetStart !== undefined) {
+        // if there was already a branch
+        if (offset === offsetEnd) {
+          offsetEnd++
+          return
+        }
+        const grains = await this.#read(offsetStart * 512, (offsetEnd - offsetStart) * 512)
+        grains.copy(buffer, (rangeStart + 1) /* block bitmap */ * 512)
+      }
+      // start a new range
+      if (offset) {
+        // we're at the beginning of a range present in the file
+        rangeStart = index
+        offsetStart = offset
+        offsetEnd = offset + 1
+      } else {
+        // we're at the beginning of a range from the parent or empty
+        rangeStart = undefined
+        offsetStart = undefined
+        offsetEnd = undefined
+      }
+    }
+
+    for (let i = 0; i < graintable.length / 4; i++) {
+      const grainOffset = graintable.readInt32LE(i * 4)
+      if (grainOffset === 0) {
+        // the content from parent : it is already in buffer
+        await changeRange()
+      } else if (grainOffset === 1) {
+        await changeRange()
+        // this is a emptied grain, no data, don't look into parent
+        buffer.fill(0, (i + 1) /* block bitmap */ * 512)
+      } else if (grainOffset > 1) {
+        // non empty grain, read from file
+        await changeRange(i, grainOffset)
+      }
+    }
+    // close last range
+    await changeRange()
+    return {
+      id: blockId,
+      bitmap: buffer.slice(0, 512),
+      data: buffer.slice(512),
+      buffer,
+    }
+  }
+}

@xen-orchestra/vmware-explorer/VhdEsxiRaw.mjs
@@ -1,0 +1,126 @@
+import _computeGeometryForSize from 'vhd-lib/_computeGeometryForSize.js'
+import { createFooter, createHeader } from 'vhd-lib/_createFooterHeader.js'
+import { DISK_TYPES, FOOTER_SIZE } from 'vhd-lib/_constants.js'
+import { readChunk } from '@vates/read-chunk'
+import { Task } from '@vates/task'
+import { unpackFooter, unpackHeader } from 'vhd-lib/Vhd/_utils.js'
+import { VhdAbstract } from 'vhd-lib'
+import assert from 'node:assert'
+
+/* eslint-disable no-console */
+
+// create a thin VHD from a raw disk
+const VHD_BLOCK_LENGTH = 2 * 1024 * 1024
+export default class VhdEsxiRaw extends VhdAbstract {
+  #esxi
+  #datastore
+  #path
+  #thin
+
+  #bat
+  #header
+  #footer
+
+  static async open(esxi, datastore, path, opts) {
+    const vhd = new VhdEsxiRaw(esxi, datastore, path, opts)
+    await vhd.readHeaderAndFooter()
+    return vhd
+  }
+
+  get header() {
+    return this.#header
+  }
+
+  get footer() {
+    return this.#footer
+  }
+
+  constructor(esxi, datastore, path, { thin = true } = {}) {
+    super()
+    this.#esxi = esxi
+    this.#path = path
+    this.#datastore = datastore
+    this.#thin = thin
+  }
+
+  async readHeaderAndFooter() {
+    const res = await this.#esxi.download(this.#datastore, this.#path)
+    const length = res.headers.get('content-length')
+
+    this.#header = unpackHeader(createHeader(length / VHD_BLOCK_LENGTH))
+    const geometry = _computeGeometryForSize(length)
+    const actualSize = geometry.actualSize
+
+    this.#footer = unpackFooter(
+      createFooter(actualSize, Math.floor(Date.now() / 1000), geometry, FOOTER_SIZE, DISK_TYPES.DYNAMIC)
+    )
+  }
+
+  containsBlock(blockId) {
+    if (!this.#thin) {
+      return true
+    }
+    assert.notEqual(this.#bat, undefined, 'bat is not loaded')
+    return this.#bat.has(blockId)
+  }
+
+  async readBlock(blockId) {
+    const start = blockId * VHD_BLOCK_LENGTH
+    const end = (blockId + 1) * VHD_BLOCK_LENGTH - 1
+
+    const data = await (await this.#esxi.download(this.#datastore, this.#path, `${start}-${end}`)).buffer()
+
+    const bitmap = Buffer.alloc(512, 255)
+    return {
+      id: blockId,
+      bitmap,
+      data,
+      buffer: Buffer.concat([bitmap, data]),
+    }
+  }
+
+  // this will read all the disk once to check which block contains data, it can take a long time to execute depending on the network speed
+  async readBlockAllocationTable() {
+    if (!this.#thin) {
+      // fast path : is we do not use thin mode, the BAT is full
+      return
+    }
+    const res = await this.#esxi.download(this.#datastore, this.#path)
+    const length = res.headers.get('content-length')
+    const stream = res.body
+    const empty = Buffer.alloc(VHD_BLOCK_LENGTH, 0)
+    let pos = 0
+    this.#bat = new Set()
+    let nextChunkLength = Math.min(VHD_BLOCK_LENGTH, length)
+    Task.set('total', length / VHD_BLOCK_LENGTH)
+    const progress = setInterval(() => {
+      Task.set('progress', Math.round((pos * 100) / length))
+      console.log('reading blocks', pos / VHD_BLOCK_LENGTH, '/', length / VHD_BLOCK_LENGTH)
+    }, 30 * 1000)
+
+    while (nextChunkLength > 0) {
+      try {
+        const chunk = await readChunk(stream, nextChunkLength)
+        let isEmpty
+        if (nextChunkLength === VHD_BLOCK_LENGTH) {
+          isEmpty = empty.equals(chunk)
+        } else {
+          // last block can be smaller
+          isEmpty = Buffer.alloc(nextChunkLength, 0).equals(chunk)
+        }
+        if (!isEmpty) {
+          this.#bat.add(pos / VHD_BLOCK_LENGTH)
+        }
+        pos += VHD_BLOCK_LENGTH
+        nextChunkLength = Math.min(VHD_BLOCK_LENGTH, length - pos)
+      } catch (error) {
+        clearInterval(progress)
+        throw error
+      }
+    }
+    console.log('BAT reading done, remaining  ', this.#bat.size, '/', Math.ceil(length / VHD_BLOCK_LENGTH))
+    clearInterval(progress)
+  }
+}
+
+/* eslint-enable no-console */

commit bf51b945c5348ba76459e603be87416d3415b264

chore(vmware-explorer): fix lint issues
Introduced by 9fa15d9c8

@xen-orchestra/vmware-explorer/VhdEsxiSeSparse.mjs
@@ -88,34 +88,15 @@ export default class VhdEsxiSeSparse extends VhdAbstract {
   async readHeaderAndFooter() {
     const buffer = await this.#read(0, 2048)
     strictEqual(buffer.readBigInt64LE(0), 0xcafebaben)
-    for (let i = 0; i < 2048 / 8; i++) {
-      console.log(i, '> ', buffer.readBigInt64LE(8 * i).toString(16), buffer.readBigInt64LE(8 * i))
-    }
 
     strictEqual(readInt64(buffer, 1), 0x200000001) // version 2.1
 
     const capacity = readInt64(buffer, 2)
     const grain_size = readInt64(buffer, 3)
-    const grain_table_size = readInt64(buffer, 4)
-    const flags = readInt64(buffer, 5)
 
-    const grain_dir_offset = readInt64(buffer, 16)
-    const grain_dir_size = readInt64(buffer, 17)
     const grain_tables_offset = readInt64(buffer, 18)
     const grain_tables_size = readInt64(buffer, 19)
     this.#grainOffset = readInt64(buffer, 24)
-
-    console.log({
-      capacity,
-      grain_size,
-      grain_table_size,
-      flags,
-      grain_dir_offset,
-      grain_dir_size,
-      grain_tables_offset,
-      grain_tables_size,
-      grainSize: this.#grainSize,
-    })
 
     this.#grainSize = grain_size * 512 // 8 sectors / 4KB default
     this.#grainTableOffset = grain_tables_offset * 512
@@ -131,12 +112,10 @@ export default class VhdEsxiSeSparse extends VhdAbstract {
   }
 
   async readBlockAllocationTable() {
-    console.log('READ BLOCK ALLOCATION', this.#grainTableSize)
     const CHUNK_SIZE = 64 * 512
 
     strictEqual(this.#grainTableSize % CHUNK_SIZE, 0)
 
-    console.log(' will read ', this.#grainTableSize / CHUNK_SIZE, 'table')
     for (let chunkIndex = 0, grainIndex = 0; chunkIndex < this.#grainTableSize / CHUNK_SIZE; chunkIndex++) {
       process.stdin.write('.')
       const start = chunkIndex * CHUNK_SIZE + this.#grainTableOffset
@@ -151,15 +130,11 @@ export default class VhdEsxiSeSparse extends VhdAbstract {
             break
         }
         if (entry > 3n) {
-          const intIndex = +(((entry & 0x0fff000000000000n) >> 48n) | ((entry & 0x0000ffffffffffffn) << 12n))
-          let pos = intIndex * this.#grainSize + CHUNK_SIZE * chunkIndex + this.#grainOffset
-          console.log({ indexInChunk, grainIndex, intIndex, pos })
           this.#grainMap.set(grainIndex)
           grainIndex++
         }
       }
     }
-    console.log('found', this.#grainMap.size)
 
     // read grain directory and the grain tables
     const nbBlocks = this.header.maxTableEntries
@@ -189,7 +164,7 @@ export default class VhdEsxiSeSparse extends VhdAbstract {
     strictEqual(graintable.length, 4096 * 4)
     // we have no guaranty that data are order or contiguous
     // let's construct ranges to limit the number of queries
-    let rangeStart, offsetStart, offsetEnd, lastOffset
+    let rangeStart, offsetStart, offsetEnd
 
     const changeRange = async (index, offset) => {
       if (offsetStart !== undefined) {

@xen-orchestra/vmware-explorer/index.mjs
@@ -1,16 +1,0 @@
-import Esxi from './esxi.mjs'
-const host = '10.10.0.62'
-const user = 'root'
-const password = 'vateslab'
-const sslVerify = false
-
-console.log(Esxi)
-const esxi = new Esxi(host, user, password, sslVerify)
-console.log(esxi)
-esxi.on('ready', async function () {
-  //const metadata = await esxi.getTransferableVmMetadata('4')
-  //console.log('metadata', metadata)
-
-  const res = await esxi.powerOn(9)
-  console.log(res)
-})

@xen-orchestra/vmware-explorer/parsers/vmsd.mjs
@@ -50,7 +50,7 @@ export default function parseVmsd(text) {
     // remove the " around value
     set(parsed, key.split('.'), val?.substring(1, val.length - 1))
   })
-  if (parsed.snapshot?.current == undefined) {
+  if (parsed.snapshot?.current === undefined) {
     return
   }
 