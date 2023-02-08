b42bce198d1e606c3160744758c1b40840386a69
mathieuRA <contact@mathieu-raisin.fr>
2023-02-08 16:30:29 +0100

    fix(xo-web/home/pool): hide pro icon support for non XCP-ng pool

--- packages/xo-web/src/xo-app/home/pool-item.js
+++ packages/xo-web/src/xo-app/home/pool-item.js
@@ -87,9 +87,14 @@ export default class PoolItem extends Component {
     return icon(tooltip)
   }
 
+  _isXcpngPool() {
+    return Object.values(this.props.poolHosts)[0].productBrand === 'XCP-ng'
+  }
+
   render() {
     const { item: pool, expandAll, isAdmin, selected, hostMetrics, poolHosts, nSrs, nVms } = this.props
     const { missingPatchCount } = this.state
+    const isXcpngPool = this._isXcpngPool()
 
     return (
       <div className={styles.item}>
@@ -102,7 +107,7 @@ export default class PoolItem extends Component {
                 <Ellipsis>
                   <Text value={pool.name_label} onChange={this._setNameLabel} useLongClick />
                 </Ellipsis>
-                {isAdmin && <span className='ml-1'>{this._getPoolLicenseIcon()}</span>}
+                {isAdmin && isXcpngPool && <span className='ml-1'>{this._getPoolLicenseIcon()}</span>}
                 &nbsp;&nbsp;
                 {missingPatchCount > 0 && (
                   <span>
