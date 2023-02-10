vatesfr/xen-orchestra

commit 711b722118bfd795a003848272bb6482e14d39e6

fix(xo-server-backup-reports/test): don't swallow errors (#5491)
See #5486

packages/xo-server-backup-reports/src/index.js
@@ -193,7 +193,13 @@ const toMarkdown = parts => {
 class BackupReportsXoPlugin {
   constructor(xo) {
     this._xo = xo
-    this._report = this._report.bind(this)
+    this._eventListener = async (...args) => {
+      try {
+        this._report(...args)
+      } catch (error) {
+        logger.warn(error)
+      }
+    }
   }
 
   configure({ toMails, toXmpp }) {
@@ -202,7 +208,7 @@ class BackupReportsXoPlugin {
   }
 
   load() {
-    this._xo.on('job:terminated', this._report)
+    this._xo.on('job:terminated', this._eventListener)
   }
 
   test({ runId }) {
@@ -210,50 +216,46 @@ class BackupReportsXoPlugin {
   }
 
   unload() {
-    this._xo.removeListener('job:terminated', this._report)
+    this._xo.removeListener('job:terminated', this._eventListener)
   }
 
   async _report(runJobId, { type, status } = {}, force) {
     const xo = this._xo
-    try {
-      if (type === 'call') {
-        return this._legacyVmHandler(status)
-      }
-
-      const log = await xo.getBackupNgLogs(runJobId)
-      if (log === undefined) {
-        throw new Error(`no log found with runId=${JSON.stringify(runJobId)}`)
-      }
-
-      const reportWhen = log.data.reportWhen
-      if (
-        !force &&
-        (reportWhen === 'never' ||
-          // Handle improper value introduced by:
-          // https://github.com/vatesfr/xen-orchestra/commit/753ee994f2948bbaca9d3161eaab82329a682773#diff-9c044ab8a42ed6576ea927a64c1ec3ebR105
-          reportWhen === 'Never' ||
-          (reportWhen === 'failure' && log.status === 'success'))
-      ) {
-        return
-      }
-
-      const [job, schedule] = await Promise.all([
-        await xo.getJob(log.jobId),
-        await xo.getSchedule(log.scheduleId).catch(error => {
-          logger.warn(error)
-        }),
-      ])
-
-      if (job.type === 'backup') {
-        return this._ngVmHandler(log, job, schedule, force)
-      } else if (job.type === 'metadataBackup') {
-        return this._metadataHandler(log, job, schedule, force)
-      }
-
-      throw new Error(`Unknown backup job type: ${job.type}`)
-    } catch (error) {
-      logger.warn(error)
+    if (type === 'call') {
+      return this._legacyVmHandler(status)
     }
+
+    const log = await xo.getBackupNgLogs(runJobId)
+    if (log === undefined) {
+      throw new Error(`no log found with runId=${JSON.stringify(runJobId)}`)
+    }
+
+    const reportWhen = log.data.reportWhen
+    if (
+      !force &&
+      (reportWhen === 'never' ||
+        // Handle improper value introduced by:
+        // https://github.com/vatesfr/xen-orchestra/commit/753ee994f2948bbaca9d3161eaab82329a682773#diff-9c044ab8a42ed6576ea927a64c1ec3ebR105
+        reportWhen === 'Never' ||
+        (reportWhen === 'failure' && log.status === 'success'))
+    ) {
+      return
+    }
+
+    const [job, schedule] = await Promise.all([
+      await xo.getJob(log.jobId),
+      await xo.getSchedule(log.scheduleId).catch(error => {
+        logger.warn(error)
+      }),
+    ])
+
+    if (job.type === 'backup') {
+      return this._ngVmHandler(log, job, schedule, force)
+    } else if (job.type === 'metadataBackup') {
+      return this._metadataHandler(log, job, schedule, force)
+    }
+
+    throw new Error(`Unknown backup job type: ${job.type}`)
   }
 
   async _metadataHandler(log, { name: jobName }, schedule, force) {

commit 31f850c19c2c2ad5054c292d0f22e9237869dc04

fix(xo-server-transport-email): log async errors
Introduced by 711b722

packages/xo-server-backup-reports/src/index.js
@@ -195,7 +195,7 @@ class BackupReportsXoPlugin {
     this._xo = xo
     this._eventListener = async (...args) => {
       try {
-        this._report(...args)
+        await this._report(...args)
       } catch (error) {
         logger.warn(error)
       }