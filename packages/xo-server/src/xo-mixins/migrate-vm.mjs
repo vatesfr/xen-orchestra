7d0cc771a334b2bd4baa1f439c90a0052c9bb2b9
Florent Beauchamp <flo850@free.fr>
2023-02-24 16:31:43 +0100

    fix(xo-server): revert changes on blocked operation during warm migrations

--- packages/xo-server/src/xo-mixins/migrate-vm.mjs
+++ packages/xo-server/src/xo-mixins/migrate-vm.mjs
@@ -71,8 +71,11 @@ export default class MigrateVm {
     // make it so it can't be restarted by error
     const message =
       'This VM has been migrated somewhere else and might not be up to date, check twice before starting it.'
-    await Promise.all([['start', 'start_on'].map(op => sourceVm.update_blocked_operations(op, message))])
-    // run the transfer again to transfer the changed parts
+    await sourceVm.update_blocked_operations({
+      start: message,
+      start_on: message,
+    }) // run the transfer again to transfer the changed parts
+
     // since the source is stopped, there won't be any new change after
     backup = this.#createWarmBackup(sourceVmId, srId, jobId)
     await backup.run()
@@ -99,8 +102,8 @@ export default class MigrateVm {
     const targetVm = app.getXapiObject(targets[0])
 
     // new vm is ready to start
-    // delta replication writer has set this as blocked
-    await Promise.all([['start', 'start_on'].map(op => targetVm.update_blocked_operations(op, null))])
+    // delta replication writer has set this as blocked=
+    await targetVm.update_blocked_operations({ start: null, start_on: null })
 
     if (startDestVm) {
       // boot it
