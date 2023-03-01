831201550b51e10a26ea0c3d7bd4d183af9ad5a7
Florent Beauchamp <flo850@free.fr>
2023-03-01 17:12:11 +0100

    feat(xo-server): use dcpath

--- @xen-orchestra/vmware-explorer/esxi.mjs
+++ @xen-orchestra/vmware-explorer/esxi.mjs
@@ -1,7 +1,7 @@
 import { Client } from 'node-vsphere-soap'
 import { dirname } from 'node:path'
 import { EventEmitter } from 'node:events'
-import { strictEqual } from 'node:assert'
+import { strictEqual, notStrictEqual } from 'node:assert'
 import fetch from 'node-fetch'
 
 import parseVmdk from './parsers/vmdk.mjs'
@@ -11,12 +11,13 @@ import parseVmx from './parsers/vmx.mjs'
 export default class Esxi extends EventEmitter {
   #client
   #cookies
+  #dcPath
   #host
   #user
   #password
   #ready = false
 
-  constructor(host, user, password, sslVerify = true) {
+  constructor(host, user, password, sslVerify) {
     super()
     this.#host = host
     this.#user = user
@@ -25,8 +26,10 @@ export default class Esxi extends EventEmitter {
     //
     // we need to find a fix for this, maybe forking the library
     this.#client = new Client(host, user, password, sslVerify)
-    this.#client.once('ready', () => {
+    this.#client.once('ready', async () => {
       this.#ready = true
+      const res = await this.search('Datacenter', ['name'])
+      this.#dcPath = Object.keys(res)[0]
       this.emit('ready')
     })
     this.#client.on('error', err => {
@@ -51,7 +54,8 @@ export default class Esxi extends EventEmitter {
 
   async download(dataStore, path, range) {
     strictEqual(this.#ready, true)
-    const url = `https://${this.#host}/folder/${path}?dsName=${dataStore}`
+    notStrictEqual(this.#dcPath, undefined)
+    const url = `https://${this.#host}/folder/${path}?dcPath=${this.#dcPath}&dsName=${dataStore}`
     const headers = {}
     if (this.#cookies) {
       headers.cookie = this.#cookies
