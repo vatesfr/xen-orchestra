9423d1810447f73340f61225a940a8ed1d5e9b1a
Florent Beauchamp <flo850@free.fr>
2023-03-17 15:36:17 +0100

    feat(@xen-orchestra/vmware-explorer): better error message when trying to read unsupported delta files

--- @xen-orchestra/vmware-explorer/openDeltaVmdkAsVhd.mjs
+++ @xen-orchestra/vmware-explorer/openDeltaVmdkAsVhd.mjs
@@ -4,13 +4,15 @@ import VhdEsxiCowd from './VhdEsxiCowd.mjs'
 export default async function openDeltaVmdkasVhd(esxi, datastore, path, parentVhd, opts) {
   let vhd
   if (path.endsWith('-sesparse.vmdk')) {
-    throw new Error(`sesparse Vmdk reading is not functionnal yet ${path}`)
+    throw new Error(
+      `sesparse Vmdk reading is not functionnal yet ${path}. For now, this VM can only be migrated if it don't have any snapshot and if it is halted.`
+    )
     // vhd = new VhdEsxiSeSparse(esxi, datastore, path, parentVhd, opts)
   } else {
     if (path.endsWith('-delta.vmdk')) {
       vhd = new VhdEsxiCowd(esxi, datastore, path, parentVhd, opts)
     } else {
-      throw new Error(`Vmdk ${path} does not seems to be a delta vmdk`)
+      throw new Error(`Vmdk ${path} does not seems to be a delta vmdk.`)
     }
   }
   await vhd.readHeaderAndFooter()
