633deed89258bf83eb47a923d913209903fffd25
mathieuRA <contact@mathieu-raisin.fr>
2023-03-24 14:20:54 +0100

    fix(lite/treeview): fix host highlighting

--- @xen-orchestra/lite/src/views/host/HostRootView.vue
+++ @xen-orchestra/lite/src/views/host/HostRootView.vue
@@ -6,4 +6,17 @@
 
 <script lang="ts" setup>
 import ObjectNotFoundWrapper from "@/components/ObjectNotFoundWrapper.vue";
+import { useRoute } from "vue-router";
+import { watchEffect } from "vue";
+import { useHostStore } from "@/stores/host.store";
+import { useUiStore } from "@/stores/ui.store";
+
+const route = useRoute();
+const hostStore = useHostStore();
+const uiStore = useUiStore();
+
+watchEffect(() => {
+  const host = hostStore.getRecordByUuid(route.params.uuid as string);
+  uiStore.currentHostOpaqueRef = host?.$ref;
+});
 </script>
