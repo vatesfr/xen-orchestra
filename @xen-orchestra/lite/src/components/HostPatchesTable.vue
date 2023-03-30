6e379d32710fdb42381058e6d5660e83f9c1838d
Thierry <thierry@byscripts.info>
2023-03-30 08:47:26 +0200

    Sort host patches and add a tooltip

--- @xen-orchestra/lite/src/components/HostPatchesTable.vue
+++ @xen-orchestra/lite/src/components/HostPatchesTable.vue
@@ -1,7 +1,7 @@
 <template>
   <UiCardSpinner v-if="!areSomeLoaded" />
   <UiTable v-else class="hosts-patches-table">
-    <tr v-for="[patchId, patch] in patches" :key="patchId">
+    <tr v-for="patch in sortedPatches" :key="patch.$id">
       <th>{{ patch.name }}</th>
       <td>
         <div class="version">
@@ -9,9 +9,15 @@
           <template v-if="hasMultipleHosts">
             <UiSpinner v-if="!areAllLoaded" />
             <UiCounter
-              class="counter"
               v-else
+              v-tooltip="{
+                placement: 'left',
+                content: $t('n-hosts-awaiting-patch', {
+                  n: patch.$hostRefs.length,
+                }),
+              }"
               :value="patch.$hostRefs.length"
+              class="counter"
               color="error"
             />
           </template>
@@ -26,6 +32,7 @@ import UiCardSpinner from "@/components/ui/UiCardSpinner.vue";
 import UiCounter from "@/components/ui/UiCounter.vue";
 import UiSpinner from "@/components/ui/UiSpinner.vue";
 import UiTable from "@/components/ui/UiTable.vue";
+import { vTooltip } from "@/directives/tooltip.directive";
 import type { XenApiHost, XenApiPatch } from "@/libs/xen-api";
 import { computed } from "vue";
 
@@ -45,6 +52,20 @@ const areSomeLoaded = computed(
 );
 
 const hasMultipleHosts = computed(() => props.hostsLoadedStatus.size > 1);
+
+const sortedPatches = computed<
+  (XenApiPatch & { $hostRefs: XenApiHost["$ref"][]; $id: string })[]
+>(() =>
+  Array.from(props.patches.entries())
+    .map(([$id, patch]) => ({
+      $id,
+      ...patch,
+    }))
+    .sort(
+      (leftPatch, rightPatch) =>
+        leftPatch.changelog.date - rightPatch.changelog.date
+    )
+);
 </script>
 
 <style lang="postcss" scoped>
