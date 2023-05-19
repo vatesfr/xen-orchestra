841a98727a2e1ef4b2c2ab762123188e9bdacd29
Thierry <thierry@byscripts.info>
2023-05-19 09:05:52 +0200

    feat(lite/bulk-action): add copy VM

---
+++ @xen-orchestra/lite/src/components/vm/VmActionItems/VmActionCopyItem.vue
@@ -0,0 +1,54 @@
+<template>
+  <MenuItem
+    :icon="faCopy"
+    :disabled="!areAllSelectedVmsHalted"
+    :busy="areSomeSelectedVmsCloning"
+    @click="handleCopy"
+  >
+    {{ $t("copy") }}
+  </MenuItem>
+</template>
+
+<script setup lang="ts">
+import MenuItem from "@/components/menu/MenuItem.vue";
+import { isOperationsPending } from "@/libs/utils";
+import { POWER_STATE, VM_OPERATION, type XenApiVm } from "@/libs/xen-api";
+import { useVmStore } from "@/stores/vm.store";
+import { useXenApiStore } from "@/stores/xen-api.store";
+import { faCopy } from "@fortawesome/free-solid-svg-icons";
+import { computed } from "vue";
+
+const props = defineProps<{
+  selectedRefs: string[];
+}>();
+
+const { getByOpaqueRef } = useVmStore().subscribe();
+
+const selectedVms = computed(() =>
+  props.selectedRefs
+    .map((vmRef) => getByOpaqueRef(vmRef))
+    .filter((vm): vm is XenApiVm => vm !== undefined)
+);
+
+const areAllSelectedVmsHalted = computed(() =>
+  selectedVms.value.every(
+    (selectedVm) => selectedVm.power_state === POWER_STATE.HALTED
+  )
+);
+
+const areSomeSelectedVmsCloning = computed(() =>
+  selectedVms.value.some((vm) => isOperationsPending(vm, VM_OPERATION.CLONE))
+);
+
+const handleCopy = async () => {
+  const xapiStore = useXenApiStore();
+
+  const vmRefsToClone = Object.fromEntries(
+    selectedVms.value.map((vm) => [vm.$ref, `${vm.name_label} (COPY)`])
+  );
+
+  await xapiStore.getXapi().vm.clone(vmRefsToClone);
+};
+</script>
+
+<style scoped lang="postcss"></style>
