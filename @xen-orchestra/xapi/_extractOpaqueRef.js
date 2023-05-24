cf7393992cbe5e4f3db938eb07e17556bc460908
Julien Fontanet <julien.fontanet@isonoe.net>
2023-05-24 12:05:56 +0200

    chore(xapi/extractOpaqueRef): named function for better stacktraces

--- @xen-orchestra/xapi/_extractOpaqueRef.js
+++ @xen-orchestra/xapi/_extractOpaqueRef.js
@@ -2,7 +2,7 @@
 
 const OPAQUE_REF_RE = /OpaqueRef:[0-9a-z-]+/
 
-module.exports = str => {
+module.exports = function extractOpaqueRef(str) {
   const matches = OPAQUE_REF_RE.exec(str)
   if (!matches) {
     throw new Error('no opaque ref found')
