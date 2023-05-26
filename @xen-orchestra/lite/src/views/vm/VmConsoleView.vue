871565b8561766ef7529c551387ce270fcb496c7
Thierry <thierry@byscripts.info>
2023-05-26 11:26:01 +0200

    feat(lite/console): rework vm console page

--- @xen-orchestra/lite/src/views/vm/VmConsoleView.vue
+++ @xen-orchestra/lite/src/views/vm/VmConsoleView.vue
@@ -1,15 +1,22 @@
 <template>
-  <div v-if="!isReady">Loading...</div>
-  <div v-else-if="!isVmRunning">Console is only available for running VMs.</div>
-  <RemoteConsole
-    v-else-if="vm && vmConsole"
-    :is-console-available="!isOperationsPending(vm, STOP_OPERATIONS)"
-    :location="vmConsole.location"
-  />
+  <div class="vm-console-view">
+    <UiSpinner v-if="!isReady" class="spinner" />
+    <div v-else-if="!isVmRunning" class="not-running">
+      <div><img alt="" src="@/assets/monitor.svg" /></div>
+      {{ $t("power-on-for-console") }}
+    </div>
+    <RemoteConsole
+      v-else-if="vm && vmConsole"
+      :is-console-available="!isOperationsPending(vm, STOP_OPERATIONS)"
+      :location="vmConsole.location"
+      class="remote-console"
+    />
+  </div>
 </template>
 
 <script lang="ts" setup>
 import RemoteConsole from "@/components/RemoteConsole.vue";
+import UiSpinner from "@/components/ui/UiSpinner.vue";
 import { isOperationsPending } from "@/libs/utils";
 import { POWER_STATE, VM_OPERATION, type XenApiVm } from "@/libs/xen-api";
 import { useConsoleStore } from "@/stores/console.store";
@@ -56,3 +63,37 @@ const vmConsole = computed(() => {
   return getConsoleByOpaqueRef(consoleOpaqueRef);
 });
 </script>
+
+<style lang="postcss" scoped>
+.vm-console-view {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: calc(100% - 14.5rem);
+}
+
+.spinner {
+  color: var(--color-extra-blue-base);
+  display: flex;
+  margin: auto;
+  width: 10rem;
+  height: 10rem;
+}
+
+.remote-console {
+  flex: 1;
+  max-width: 100%;
+  height: 100%;
+}
+
+.not-running {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+  gap: 4rem;
+  color: var(--color-extra-blue-base);
+  font-size: 3.6rem;
+}
+</style>
