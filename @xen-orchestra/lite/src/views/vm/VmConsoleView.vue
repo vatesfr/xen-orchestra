e67a2ad667d9055a38eac5b0ba076b6f128b3df4
Thierry <thierry@byscripts.info>
2023-05-30 08:41:00 +0200

    feat(lite): open console in new window

--- @xen-orchestra/lite/src/views/vm/VmConsoleView.vue
+++ @xen-orchestra/lite/src/views/vm/VmConsoleView.vue
@@ -1,28 +1,43 @@
 <template>
-  <div class="vm-console-view">
+  <div :class="{ 'no-ui': !uiStore.hasUi }" class="vm-console-view">
     <div v-if="hasError">{{ $t("error-occurred") }}</div>
     <UiSpinner v-else-if="!isReady" class="spinner" />
     <div v-else-if="!isVmRunning" class="not-running">
       <div><img alt="" src="@/assets/monitor.svg" /></div>
       {{ $t("power-on-for-console") }}
     </div>
-    <RemoteConsole
-      v-else-if="vm && vmConsole"
-      :is-console-available="isConsoleAvailable"
-      :location="vmConsole.location"
-      class="remote-console"
-    />
+    <template v-else-if="vm && vmConsole">
+      <RemoteConsole
+        :is-console-available="isConsoleAvailable"
+        :location="vmConsole.location"
+        class="remote-console"
+      />
+      <div class="open-in-new-window">
+        <RouterLink
+          v-if="uiStore.hasUi"
+          :to="{ query: { ui: '0' } }"
+          class="link"
+          target="_blank"
+        >
+          <UiIcon :icon="faArrowUpRightFromSquare" />
+          {{ $t("open-in-new-window") }}
+        </RouterLink>
+      </div>
+    </template>
   </div>
 </template>
 
 <script lang="ts" setup>
 import RemoteConsole from "@/components/RemoteConsole.vue";
+import UiIcon from "@/components/ui/icon/UiIcon.vue";
 import UiSpinner from "@/components/ui/UiSpinner.vue";
 import { isOperationsPending } from "@/libs/utils";
 import { POWER_STATE, VM_OPERATION, type XenApiVm } from "@/libs/xen-api";
 import { useConsoleStore } from "@/stores/console.store";
 import { usePageTitleStore } from "@/stores/page-title.store";
+import { useUiStore } from "@/stores/ui.store";
 import { useVmStore } from "@/stores/vm.store";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 import { computed } from "vue";
 import { useI18n } from "vue-i18n";
 import { useRoute } from "vue-router";
@@ -40,6 +55,7 @@ const STOP_OPERATIONS = [
 usePageTitleStore().setTitle(useI18n().t("console"));
 
 const route = useRoute();
+const uiStore = useUiStore();
 
 const {
   isReady: isVmReady,
@@ -85,6 +101,10 @@ const isConsoleAvailable = computed(
   align-items: center;
   justify-content: center;
   height: calc(100% - 14.5rem);
+
+  &.no-ui {
+    height: 100%;
+  }
 }
 
 .spinner {
@@ -111,5 +131,31 @@ const isConsoleAvailable = computed(
   gap: 4rem;
   color: var(--color-extra-blue-base);
   font-size: 3.6rem;
+}
+
+.open-in-new-window {
+  position: absolute;
+  top: 0;
+  right: 0;
+  overflow: hidden;
+
+  & > .link {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    background-color: var(--color-extra-blue-base);
+    color: var(--color-blue-scale-500);
+    text-decoration: none;
+    padding: 1.5rem;
+    font-size: 1.6rem;
+    border-radius: 0 0 0 0.8rem;
+    white-space: nowrap;
+    transform: translateX(calc(100% - 4.5rem));
+    transition: transform 0.2s ease-in-out;
+
+    &:hover {
+      transform: translateX(0);
+    }
+  }
 }
 </style>
