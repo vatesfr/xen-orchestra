e484c8a8e7b4f4ee8ad39300cddfd5e0141e561e
mathieuRA <contact@mathieu-raisin.fr>
2023-06-16 16:29:47 +0200

    fix(xo-web/vm/stats): avoid request duplications

--- packages/xo-web/src/xo-app/vm/tab-stats.js
+++ packages/xo-web/src/xo-app/vm/tab-stats.js
@@ -4,7 +4,7 @@ import Icon from 'icon'
 import React from 'react'
 import Tooltip from 'tooltip'
 import { Container, Row, Col } from 'grid'
-import { DEFAULT_GRANULARITY, fetchStats, SelectGranularity } from 'stats'
+import { DEFAULT_GRANULARITY, fetchStats, INTERVAL_BY_GRANULARITY, SelectGranularity } from 'stats'
 import { Toggle } from 'form'
 import { CpuLineChart, MemoryLineChart, VifLineChart, XvdLineChart } from 'xo-line-chart'
 
@@ -12,39 +12,49 @@ export default class VmStats extends Component {
   state = {
     granularity: DEFAULT_GRANULARITY,
     useCombinedValues: false,
+    statsIsPending: false,
   }
 
-  loop(vm = this.props.vm) {
+  fetchVmStats = () => {
+    if (this.state.statsIsPending) {
+      return
+    }
+
+    const vm = this.props.vm
+
     if (this.cancel) {
-      this.cancel()
+      return
     }
 
     if (vm.power_state !== 'Running') {
       return
     }
 
-    let cancelled = false
-    this.cancel = () => {
-      cancelled = true
+    if (this.props.statsOverview?.interval === INTERVAL_BY_GRANULARITY[this.state.granularity.granularity]) {
+      console.log('get stats from parent component. granularity: %s', this.state.granularity.granularity)
+      this.setState({
+        stats: this.props.statsOverview,
+        selectStatsLoading: false,
+      })
+      return
     }
 
-    fetchStats(vm, 'vm', this.state.granularity).then(stats => {
-      if (cancelled) {
-        return
-      }
-      this.cancel = null
-
-      clearTimeout(this.timeout)
-      this.setState(
-        {
-          stats,
-          selectStatsLoading: false,
-        },
-        () => {
-          this.timeout = setTimeout(this.loop, stats.interval * 1000)
-        }
-      )
+    console.log('get stats from xo-server. granularity: %s', this.state.granularity.granularity)
+    this.setState({
+      statsIsPending: true,
     })
+    fetchStats(vm, 'vm', this.state.granularity).then(stats => {
+      this.setState({
+        stats,
+        selectStatsLoading: false,
+        statsIsPending: false,
+      })
+    })
+  }
+
+  loop() {
+    this.fetchVmStats()
+    this.interval = setInterval(this.fetchVmStats, INTERVAL_BY_GRANULARITY[this.state.granularity.granularity] * 1000)
   }
   loop = ::this.loop
 
@@ -53,7 +63,7 @@ export default class VmStats extends Component {
   }
 
   componentWillUnmount() {
-    clearTimeout(this.timeout)
+    clearInterval(this.interval)
   }
 
   componentWillReceiveProps(props) {
@@ -70,7 +80,7 @@ export default class VmStats extends Component {
   }
 
   handleSelectStats(granularity) {
-    clearTimeout(this.timeout)
+    clearInterval(this.interval)
 
     this.setState(
       {
