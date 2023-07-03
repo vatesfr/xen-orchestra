e6956fe300628857c767f5ec53e81990e67b44ac
Thierry <thierry@byscripts.info>
2023-07-03 17:03:40 +0200

    feat(lite/component): created UiResources + UiResource

---
+++ @xen-orchestra/lite/src/components/ui/resources/UiResource.vue
@@ -0,0 +1,50 @@
+<template>
+  <li class="ui-resource">
+    <UiIcon :icon="icon" class="icon" />
+    <div class="separator" />
+    <div class="label">{{ label }}</div>
+    <div class="count">{{ count }}</div>
+  </li>
+</template>
+
+<script lang="ts" setup>
+import UiIcon from "@/components/ui/icon/UiIcon.vue";
+import type { IconDefinition } from "@fortawesome/fontawesome-common-types";
+
+defineProps<{
+  icon: IconDefinition;
+  label: string;
+  count: string | number;
+}>();
+</script>
+
+<style lang="postcss" scoped>
+.ui-resource {
+  display: flex;
+  align-items: center;
+}
+
+.icon {
+  color: var(--color-extra-blue-base);
+  font-size: 3.2rem;
+}
+
+.separator {
+  height: 4.5rem;
+  width: 0;
+  border-left: 0.1rem solid var(--color-extra-blue-base);
+  background-color: var(--color-extra-blue-base);
+  margin: 0 1.5rem;
+}
+
+.label {
+  font-size: 1.6rem;
+  font-weight: 700;
+}
+
+.count {
+  font-size: 1.4rem;
+  font-weight: 400;
+  margin-left: 2rem;
+}
+</style>
---
+++ @xen-orchestra/lite/src/components/ui/resources/UiResources.vue
@@ -0,0 +1,13 @@
+<template>
+  <ul class="ui-resources">
+    <slot />
+  </ul>
+</template>
+
+<style lang="postcss" scoped>
+.ui-resources {
+  display: flex;
+  gap: 1rem 5.4rem;
+  flex-wrap: wrap;
+}
+</style>
