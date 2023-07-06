9e0b5575a46faebef362cef033a8f964ba60a88d
Thierry Goettelmann <thierry@byscripts.info>
2023-07-06 15:55:05 +0200

    feat(lite/component): new component FormSection (#6926)
    Can take a `collapsible` prop in conjunction of a `collapsed` prop.
    If `collapsible` is set to `true`, the style is changed and clicking the section
    header will toggle the content visibility.
    Collapse status updates are sent via the `update:collapsed` event.
    This allows to use `collapsed` as a model, e.g.:
    `<FormSection collapsible v-model:collapsed="collapsed">`

---
+++ @xen-orchestra/lite/src/components/form/FormSection.vue
@@ -0,0 +1,92 @@
+<template>
+  <div :class="{ collapsible }" class="form-section">
+    <fieldset class="fieldset">
+      <legend class="legend" @click="toggleCollapse">
+        {{ label }}
+        <UiIcon :icon="icon" class="collapse-icon" />
+      </legend>
+      <div v-if="!isCollapsed" class="content">
+        <slot />
+      </div>
+    </fieldset>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import UiIcon from "@/components/ui/icon/UiIcon.vue";
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { useVModel, whenever } from "@vueuse/core";
+import { computed } from "vue";
+
+const props = defineProps<{
+  label: string;
+  collapsible?: boolean;
+  collapsed?: boolean;
+}>();
+
+const emit = defineEmits<{
+  (event: "update:collapsed", value: boolean): void;
+}>();
+
+const isCollapsed = useVModel(props, "collapsed", emit);
+
+const toggleCollapse = () => {
+  if (props.collapsible) {
+    isCollapsed.value = !isCollapsed.value;
+  }
+};
+
+const icon = computed(() => {
+  if (!props.collapsible) {
+    return undefined;
+  }
+
+  return isCollapsed.value ? faChevronDown : faChevronUp;
+});
+
+whenever(
+  () => !props.collapsible,
+  () => (isCollapsed.value = false)
+);
+</script>
+
+<style lang="postcss" scoped>
+.collapsible {
+  padding: 1rem 1.5rem;
+  background-color: var(--background-color-extra-blue);
+  border-radius: 0.8rem;
+}
+
+.fieldset {
+  border: none;
+  margin: 0;
+  padding: 0;
+}
+
+.legend {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: var(--color-extra-blue-base);
+  border: none;
+  border-bottom: 1px solid var(--color-extra-blue-base);
+  width: 100%;
+  font-size: 2rem;
+  font-weight: 500;
+  padding-bottom: 1rem;
+
+  .collapsible & {
+    color: var(--color-blue-scale-100);
+    padding-bottom: 0;
+    cursor: pointer;
+  }
+}
+
+.content {
+  padding: 1.5rem 0;
+}
+
+.collapse-icon {
+  color: var(--color-extra-blue-base);
+}
+</style>
