ff432e04b0857641a79377c71a77f5ff636e005e
rbarhtaoui <rajaa.barhtaoui@gmail.com>
2023-07-12 14:34:16 +0200

    feat(lite/pool/vms): export selected VMs as CSV file (#6915)

---
+++ @xen-orchestra/lite/src/libs/vm.ts
@@ -0,0 +1,32 @@
+import { saveAs } from "file-saver";
+import type { XenApiVm } from "@/libs/xen-api";
+
+function stringifyCsvValue(value: any) {
+  let res = "";
+  if (Array.isArray(value)) {
+    res = value.join(";");
+  } else if (typeof value === "object") {
+    res = JSON.stringify(value);
+  } else {
+    res = String(value);
+  }
+  return `"${res.replace(/"/g, '""')}"`;
+}
+
+export function exportVmsAsCsvFile(vms: XenApiVm[], fileName: string) {
+  const csvHeaders = Object.keys(vms[0]);
+
+  const csvRows = vms.map((vm) =>
+    csvHeaders.map((header) => stringifyCsvValue(vm[header as keyof XenApiVm]))
+  );
+
+  saveAs(
+    new Blob(
+      [[csvHeaders, ...csvRows].map((row) => row.join(",")).join("\n")],
+      {
+        type: "text/csv;charset=utf-8",
+      }
+    ),
+    fileName
+  );
+}
