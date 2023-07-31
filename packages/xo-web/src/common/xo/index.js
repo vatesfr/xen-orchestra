vatesfr/xen-orchestra

commit 52cf2d1514b9a4e44fa1c7dd768c50778d2b75fa

feat(xo-web/settings/users): auth providers can be removed

packages/xo-web/src/common/xo/index.js
@@ -2964,6 +2964,10 @@ export const deleteUsers = users =>
 export const editUser = (user, { email, password, permission }) =>
   _call('user.set', { id: resolveId(user), email, password, permission })::tap(subscribeUsers.forceRefresh)
 
+export const removeUserAuthProvider = ({ userId, authProviderId }) => {
+  _call('user.removeAuthProvider', { id: userId, authProvider: authProviderId })::tap(subscribeUsers.forceRefresh)
+}
+
 const _signOutFromEverywhereElse = () =>
   _call('token.delete', {
     pattern: {

packages/xo-web/src/xo-app/settings/users/index.js
@@ -17,7 +17,16 @@ import { get } from '@xen-orchestra/defined'
 import { injectIntl } from 'react-intl'
 import { Password, Select } from 'form'
 
-import { createUser, deleteUser, deleteUsers, editUser, removeOtp, subscribeGroups, subscribeUsers } from 'xo'
+import {
+  createUser,
+  deleteUser,
+  deleteUsers,
+  editUser,
+  removeOtp,
+  removeUserAuthProvider,
+  subscribeGroups,
+  subscribeUsers,
+} from 'xo'
 
 const permissions = {
   none: {
@@ -92,6 +101,15 @@ const USER_COLUMNS = [
               return (
                 <li key={id} className='list-group-item'>
                   <Link to={`/settings/plugins/?s=${encodeURIComponent(`name=^${plugin}$`)}`}>{shortId}</Link>
+                  <ActionButton
+                    className='pull-right'
+                    btnStyle='warning'
+                    size='small'
+                    icon='remove'
+                    handler={removeUserAuthProvider}
+                    data-userId={user.id}
+                    data-authProviderId={id}
+                  />
                 </li>
               )
             })}

commit 9986f3fb1868f72c94de097704022cb61840f03a

fix(xo-web/removeUserAuthProvider): notify on error
Introduced by 52cf2d151

packages/xo-web/src/common/xo/index.js
@@ -2965,7 +2965,11 @@ export const editUser = (user, { email, password, permission }) =>
   _call('user.set', { id: resolveId(user), email, password, permission })::tap(subscribeUsers.forceRefresh)
 
 export const removeUserAuthProvider = ({ userId, authProviderId }) => {
-  _call('user.removeAuthProvider', { id: userId, authProvider: authProviderId })::tap(subscribeUsers.forceRefresh)
+  _call('user.removeAuthProvider', { id: userId, authProvider: authProviderId })
+    ::tap(subscribeUsers.forceRefresh)
+    .catch(e => {
+      error('user.removeAuthProvider', e.message)
+    })
 }
 
 const _signOutFromEverywhereElse = () =>