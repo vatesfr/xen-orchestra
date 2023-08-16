8478d8bc7544f8709babade1f373bd40cac873f8
Thierry <thierry@byscripts.info>
2023-08-16 10:32:29 +0200

    feat(lite): new context system

---
+++ @xen-orchestra/lite/src/stories/ui-accent.story.vue
@@ -0,0 +1,25 @@
+<template>
+  <ComponentStory
+    v-slot="{ properties, settings }"
+    :params="[
+      prop('tag').str().default('span').widget(),
+      colorProp().ctx(),
+      slot(),
+      setting('defaultSlotContent')
+        .preset('Example content for default slot')
+        .widget(text())
+        .help('Content for default slot'),
+    ]"
+  >
+    <UiAccent v-bind="properties">
+      {{ settings.defaultSlotContent }}
+    </UiAccent>
+  </ComponentStory>
+</template>
+
+<script lang="ts" setup>
+import ComponentStory from "@/components/component-story/ComponentStory.vue";
+import UiAccent from "@/components/ui/UiAccent.vue";
+import { prop, slot, setting, colorProp } from "@/libs/story/story-param";
+import { text } from "@/libs/story/story-widget";
+</script>
