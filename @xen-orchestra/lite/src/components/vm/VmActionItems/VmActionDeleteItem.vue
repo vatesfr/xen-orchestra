ca145d8b56e9cd3b84b52fff62c4309e04df9398
Thierry <thierry@byscripts.info>
2023-08-21 10:56:44 +0200

    feat(lite): new modal system

--- @xen-orchestra/lite/src/components/vm/VmActionItems/VmActionDeleteItem.vue
+++ @xen-orchestra/lite/src/components/vm/VmActionItems/VmActionDeleteItem.vue
@@ -73,6 +73,10 @@ const isDisabled = computed(
   () => vms.value.length === 0 || areSomeVmsInExecution.value
 );
 
+const isDisabled = computed(
+  () => vms.value.length === 0 || areSomeVmsInExecution.value
+);
+
 const deleteVms = async () => {
   await xenApi.vm.delete(props.vmRefs);
   closeDeleteModal();
