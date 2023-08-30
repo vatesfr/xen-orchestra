5b1cc7415ece9a1d245cca50c07250c645f3426e
Pierre Donias <pierre.donias@gmail.com>
2023-08-30 09:56:08 +0200

    fix(xo-server/normalizeVmNetworks): always assume multiple space-delimited IPs (#6990)
    See https://xcp-ng.org/forum/topic/7625

--- packages/xo-server/src/_normalizeVmNetworks.spec.mjs
+++ packages/xo-server/src/_normalizeVmNetworks.spec.mjs
@@ -19,10 +19,10 @@ tap.test('normalizeVmNetworks', async t => {
       '0/ipv4/1': '127.0.0.5',
 
       // any key are allowed
-      '0/ipv4/foo bar': '127.0.0.3',
+      '0/ipv4/foo bar': '127.0.0.3 127.0.0.6',
 
       // ipv6 protocol is supported as well
-      '0/ipv6/0': '::1',
+      '0/ipv6/0': '::1 ::2',
 
       // empty addresses are ignored
       '0/ipv4/3': '   ',
@@ -36,7 +36,9 @@ tap.test('normalizeVmNetworks', async t => {
       '0/ipv4/2': '127.0.0.5',
       '0/ipv4/3': '127.0.0.4',
       '0/ipv4/4': '127.0.0.3',
+      '0/ipv4/5': '127.0.0.6',
       '0/ipv6/0': '::1',
+      '0/ipv6/1': '::2',
       '1/ipv4/0': '127.0.0.1',
     }
   )
