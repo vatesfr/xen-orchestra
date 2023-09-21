ad94646c02021de912c012b865552116f7af09ca
Julien Fontanet <julien.fontanet@isonoe.net>
2023-09-21 11:16:50 +0200

    Update openVhd.js

--- packages/vhd-lib/openVhd.js
+++ packages/vhd-lib/openVhd.js
@@ -6,6 +6,10 @@ const { VhdFile } = require('./Vhd/VhdFile.js')
 
 exports.openVhd = async function openVhd(handler, path, opts) {
   const resolved = await resolveVhdAlias(handler, path)
+
+  // VHD files can't be encrypted since we can't modify part of a file during merge
+  //
+  // Skip trying to open it if the remote is encrypted
   if (!handler.isEncrypted) {
     try {
       return await VhdFile.open(handler, resolved, opts)
@@ -15,6 +19,6 @@ exports.openVhd = async function openVhd(handler, path, opts) {
       }
     }
   }
-  // vhd file can't be encrypted, since we can't modify part of a file during merge
+
   return await VhdDirectory.open(handler, resolved, opts)
 }
