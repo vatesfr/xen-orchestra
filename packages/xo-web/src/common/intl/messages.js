55f627ed838f1c1d2a0f9f4c6fa41961e1fe1ab8
Julien Fontanet <julien.fontanet@isonoe.net>
2023-09-28 15:37:45 +0200

    chore: fix formatting
    Introduced by 869f7ffab

--- packages/xo-web/src/common/intl/messages.js
+++ packages/xo-web/src/common/intl/messages.js
@@ -241,7 +241,8 @@ const messages = {
   closeTunnel: 'Close tunnel',
   createSupportTicket: 'Create a support ticket',
   restartXoServer: 'Restart XO Server',
-  restartXoServerConfirm: 'Restarting XO Server will interrupt any backup job or XO task that is currently running. Xen Orchestra will also be unavailable for a few seconds. Are you sure you want to restart XO Server?',
+  restartXoServerConfirm:
+    'Restarting XO Server will interrupt any backup job or XO task that is currently running. Xen Orchestra will also be unavailable for a few seconds. Are you sure you want to restart XO Server?',
   openTunnel: 'Open tunnel',
   supportCommunity: 'The XOA check and the support tunnel are available in XOA.',
   supportTunnel: 'Support tunnel',
