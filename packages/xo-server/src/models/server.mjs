df91772f5c5ac0be39f8ac70c386900eff3ae720
Julien Fontanet <julien.fontanet@isonoe.net>
2023-10-23 16:11:38 +0200

    chore(xo-server/server): use builtin (un)serialize

--- packages/xo-server/src/models/server.mjs
+++ packages/xo-server/src/models/server.mjs
@@ -1,11 +1,35 @@
 import Collection from '../collection/redis.mjs'
-import { forEach, serializeError } from '../utils.mjs'
+import { serializeError } from '../utils.mjs'
 
 import { parseProp } from './utils.mjs'
 
 // ===================================================================
 
 export class Servers extends Collection {
+  _serialize(server) {
+    server.allowUnauthorized = server.allowUnauthorized ? 'true' : undefined
+    server.enabled = server.enabled ? 'true' : undefined
+    const { error } = server
+    server.error = error != null ? JSON.stringify(serializeError(error)) : undefined
+    server.readOnly = server.readOnly ? 'true' : undefined
+  }
+
+  _unserialize(server) {
+    server.allowUnauthorized = server.allowUnauthorized === 'true'
+    server.enabled = server.enabled === 'true'
+    if (server.error) {
+      server.error = parseProp('server', server, 'error', '')
+    } else {
+      delete server.error
+    }
+    server.readOnly = server.readOnly === 'true'
+
+    // see https://github.com/vatesfr/xen-orchestra/issues/6656
+    if (server.httpProxy === '') {
+      delete server.httpProxy
+    }
+  }
+
   async create(params) {
     const { host } = params
 
@@ -14,39 +38,5 @@ export class Servers extends Collection {
     }
 
     return /* await */ this.add(params)
-  }
-
-  async get(properties) {
-    const servers = await super.get(properties)
-
-    // Deserializes
-    forEach(servers, server => {
-      server.allowUnauthorized = server.allowUnauthorized === 'true'
-      server.enabled = server.enabled === 'true'
-      if (server.error) {
-        server.error = parseProp('server', server, 'error', '')
-      } else {
-        delete server.error
-      }
-      server.readOnly = server.readOnly === 'true'
-
-      // see https://github.com/vatesfr/xen-orchestra/issues/6656
-      if (server.httpProxy === '') {
-        delete server.httpProxy
-      }
-    })
-
-    return servers
-  }
-
-  _update(servers) {
-    servers.forEach(server => {
-      server.allowUnauthorized = server.allowUnauthorized ? 'true' : undefined
-      server.enabled = server.enabled ? 'true' : undefined
-      const { error } = server
-      server.error = error != null ? JSON.stringify(serializeError(error)) : undefined
-      server.readOnly = server.readOnly ? 'true' : undefined
-    })
-    return super._update(servers)
   }
 }
