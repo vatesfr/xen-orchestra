1ad054725e971c312862934d0fc3ba37c5a84879
mathieuRA <contact@mathieu-raisin.fr>
2023-10-23 16:10:21 +0200

    use getXoaPlan instead

--- packages/xo-web/src/xo-app/xostor/index.js
+++ packages/xo-web/src/xo-app/xostor/index.js
@@ -6,6 +6,7 @@ import React from 'react'
 import { injectState, provideState } from 'reaclette'
 import { Container, Col, Row } from 'grid'
 import { TryXoa } from 'utils'
+import { getXoaPlan, SOURCES } from 'xoa-plans'
 
 import NewXostorForm from './new-xostor-form'
 import XostorList from './xostor-list'
@@ -32,7 +33,14 @@ const Xostor = decorate([
   injectState,
   ({ effects, state }) => (
     <Page header={HEADER}>
-      {process.env.XOA_PLAN < 5 ? (
+      {getXoaPlan() === SOURCES ? (
+        <Container>
+          <h2 className='text-info'>{_('xostorCommunity')}</h2>
+          <p>
+            <TryXoa page='xosan' />
+          </p>
+        </Container>
+      ) : (
         <Container>
           <XostorList />
           <Row className='mb-1'>
@@ -47,13 +55,6 @@ const Xostor = decorate([
             </Col>
           </Row>
           {state.showNewXostorForm && <NewXostorForm />}
-        </Container>
-      ) : (
-        <Container>
-          <h2 className='text-info'>{_('xostorCommunity')}</h2>
-          <p>
-            <TryXoa page='xosan' />
-          </p>
         </Container>
       )}
     </Page>
