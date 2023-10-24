vatesfr/xen-orchestra

commit 8834af65f736c3f308f20a13fd0e0290f1c67818

feat(xo-server/xo-web/VM/new): VTPM creation (#7077)
See #7066
See #6802
See #7085

packages/xo-web/src/xo-app/new-vm/index.js
@@ -346,6 +346,7 @@ export default class NewVm extends BaseComponent {
         seqStart: 1,
         share: this._getResourceSet()?.shareByDefault ?? false,
         tags: [],
+        createVtpm: this._templateNeedsVtpm(),
       },
       callback
     )
@@ -493,6 +494,7 @@ export default class NewVm extends BaseComponent {
       bootAfterCreate: state.bootAfterCreate,
       copyHostBiosStrings:
         state.hvmBootFirmware !== 'uefi' && !this._templateHasBiosStrings() && state.copyHostBiosStrings,
+      createVtpm: state.createVtpm,
       destroyCloudConfigVdiAfterBoot: state.destroyCloudConfigVdiAfterBoot,
       secureBoot: state.secureBoot,
       share: state.share,
@@ -599,6 +601,7 @@ export default class NewVm extends BaseComponent {
       }),
       // settings
       secureBoot: template.secureBoot,
+      createVtpm: this._templateNeedsVtpm(),
     })
 
     if (this._isCoreOs()) {
@@ -748,6 +751,8 @@ export default class NewVm extends BaseComponent {
     template => template && template.virtualizationMode === 'hvm'
   )
 
+  _templateNeedsVtpm = () => this.props.template?.platform?.vtpm === 'true'
+
   // On change -------------------------------------------------------------------
 
   _onChangeSshKeys = keys => this._setState({ sshKeys: map(keys, key => key.id) })
@@ -881,7 +886,12 @@ export default class NewVm extends BaseComponent {
 
   _getRedirectionUrl = id => (this.state.state.multipleVms ? '/home' : `/vms/${id}`)
 
-  _handleBootFirmware = value => this._setState({ hvmBootFirmware: value, secureBoot: false })
+  _handleBootFirmware = value =>
+    this._setState({
+      hvmBootFirmware: value,
+      secureBoot: false,
+      createVtpm: value === 'uefi' ? this._templateNeedsVtpm() : false,
+    })
 
   // MAIN ------------------------------------------------------------------------
 
@@ -1531,6 +1541,7 @@ export default class NewVm extends BaseComponent {
       cpuCap,
       cpusMax,
       cpuWeight,
+      createVtpm,
       destroyCloudConfigVdiAfterBoot,
       hvmBootFirmware,
       installMethod,
@@ -1564,6 +1575,8 @@ export default class NewVm extends BaseComponent {
           {_('copyHostBiosStrings')}
         </label>
       ) : null
+
+    const isVtpmSupported = pool.vtpmSupported
 
     return (
       <Section icon='new-vm-advanced' title='newVmAdvancedPanel' done={this._isAdvancedDone()}>
@@ -1768,6 +1781,23 @@ export default class NewVm extends BaseComponent {
             <SectionContent>
               <Item label={_('secureBoot')}>
                 <Toggle onChange={this._toggleState('secureBoot')} value={secureBoot} />
+              </Item>
+              <Item label={_('enableVtpm')} className='d-inline-flex'>
+                <Tooltip content={!isVtpmSupported ? _('vtpmNotSupported') : undefined}>
+                  <Toggle onChange={this._toggleState('createVtpm')} value={createVtpm} disabled={!isVtpmSupported} />
+                </Tooltip>
+                {/* FIXME: link to VTPM documentation when ready */}
+                {/* &nbsp;
+                <Tooltip content={_('seeVtpmDocumentation')}>
+                  <a className='text-info align-self-center' style={{ cursor: 'pointer' }} href='#'>
+                    <Icon icon='info' />
+                  </a>
+                </Tooltip> */}
+                {!createVtpm && this._templateNeedsVtpm() && (
+                  <span className='align-self-center text-warning ml-1'>
+                    <Icon icon='alarm' /> {_('warningVtpmRequired')}
+                  </span>
+                )}
               </Item>
             </SectionContent>
           ),

packages/xo-server/src/api/vm.mjs
@@ -238,6 +238,11 @@ export const create = defer(async function ($defer, params) {
     await this.allocIpAddresses(vif.$id, concat(vif.ipv4_allowed, vif.ipv6_allowed)).catch(() => xapi.deleteVif(vif))
   }
 
+  if (params.createVtpm) {
+    const vtpmRef = await xapi.VTPM_create({ VM: xapiVm.$ref })
+    $defer.onFailure(() => xapi.call('VTPM.destroy', vtpmRef))
+  }
+
   if (params.bootAfterCreate) {
     startVmAndDestroyCloudConfigVdi(xapi, xapiVm, cloudConfigVdiUuid, params)
   }
@@ -256,6 +261,11 @@ create.params = {
   cloudConfig: {
     type: 'string',
     optional: true,
+  },
+
+  createVtpm: {
+    type: 'boolean',
+    default: false,
   },
 
   networkConfig: {

packages/xo-web/src/common/intl/messages.js
@@ -1582,6 +1582,7 @@ const messages = {
   createVmModalWarningMessage:
     "You're about to use a large amount of resources available on the resource set. Are you sure you want to continue?",
   copyHostBiosStrings: 'Copy host BIOS strings to VM',
+  enableVtpm: 'Enable VTPM',
   newVmCreateNewVmOn: 'Create a new VM on {select}',
   newVmCreateNewVmNoPermission: 'You have no permission to create a VM',
   newVmInfoPanel: 'Info',
@@ -1655,6 +1656,7 @@ const messages = {
   vmBootFirmwareIsUefi: 'The boot firmware is UEFI',
   destroyCloudConfigVdiAfterBoot: 'Destroy cloud config drive after first boot',
   vtpmNotSupported: 'VTPM is only supported on pools running XCP-ng/XS 8.3 or later.',
+  warningVtpmRequired: 'This template requires a VTPM, if you proceed, the VM will likely not be able to boot.',
 
   // ----- Self -----
   resourceSets: 'Resource sets',

packages/xo-web/src/index.scss
@@ -100,6 +100,14 @@ $select-input-height: 40px; // Bootstrap input height
   color: #333;
 }
 
+.d-inline-flex {
+  display: inline-flex;
+}
+
+.align-self-center {
+  align-self: center;
+}
+
 // COLORS ======================================================================
 
 .xo-status-running {

commit 77d97983199b69fbd673c469125ca81a1280893c

fix(xo-web/vtpm): fix various an error has occured (#7122)
Introduced by 8834af65f736c3f308f20a13fd0e0290f1c67818
Introduced by 1a1dd0531dbea87fdde25a30fec40db88a5082df

Fix `an error has occurred` in the VM advanced tab and on the VM creation form
if the user does not have pool permission.

packages/xo-web/src/common/xo/index.js
@@ -1667,14 +1667,22 @@ export const migrateVms = vms =>
     )
   }, noop)
 
-export const createVm = args => _call('vm.create', args)
+export const createVm = async args => {
+  try {
+    return await _call('vm.create', args)
+  } catch (err) {
+    handlePoolDoesNotSupportVtpmError(err)
+    throw error
+  }
+}
 
-export const createVms = (args, nameLabels, cloudConfigs) =>
-  confirm({
+export const createVms = async (args, nameLabels, cloudConfigs) => {
+  await confirm({
     title: _('newVmCreateVms'),
     body: _('newVmCreateVmsConfirm', { nbVms: nameLabels.length }),
-  }).then(() =>
-    Promise.all(
+  })
+  try {
+    return await Promise.all(
       map(
         nameLabels,
         (
@@ -1688,7 +1696,11 @@ export const createVms = (args, nameLabels, cloudConfigs) =>
           })
       )
     )
-  )
+  } catch (error) {
+    handlePoolDoesNotSupportVtpmError(error)
+    throw error
+  }
+}
 
 export const getCloudInitConfig = template => _call('vm.getCloudInitConfig', { template })
 
@@ -2156,8 +2168,26 @@ export const deleteAclRule = ({ protocol = undefined, port = undefined, ipRange 
   })
 
 // VTPM -----------------------------------------------------------
+const handlePoolDoesNotSupportVtpmError = err => {
+  if (
+    incorrectState.is(err, {
+      property: 'restrictions.restrict_vtpm',
+      expected: 'false',
+    })
+  ) {
+    console.error(err)
+    throw new Error('This pool does not support VTPM')
+  }
+}
 
-export const createVtpm = vm => _call('vtpm.create', { id: resolveId(vm) })
+export const createVtpm = async vm => {
+  try {
+    return await _call('vtpm.create', { id: resolveId(vm) })
+  } catch (err) {
+    handlePoolDoesNotSupportVtpmError(err)
+    throw err
+  }
+}
 export const deleteVtpm = vtpm => _call('vtpm.destroy', { id: resolveId(vtpm) })
 
 // Network -----------------------------------------------------------

packages/xo-web/src/xo-app/new-vm/index.js
@@ -1576,7 +1576,7 @@ export default class NewVm extends BaseComponent {
         </label>
       ) : null
 
-    const isVtpmSupported = pool.vtpmSupported
+    const isVtpmSupported = pool?.vtpmSupported ?? true
 
     return (
       <Section icon='new-vm-advanced' title='newVmAdvancedPanel' done={this._isAdvancedDone()}>

packages/xo-web/src/xo-app/vm/tab-advanced.js
@@ -457,7 +457,7 @@ export default class TabAdvanced extends Component {
     () => this.props.vm,
     () => this.props.pool,
     (vm, pool) => {
-      if (!pool.vtpmSupported) {
+      if (pool?.vtpmSupported === false) {
         return _('vtpmNotSupported')
       }
       if (vm.boot.firmware !== 'uefi') {