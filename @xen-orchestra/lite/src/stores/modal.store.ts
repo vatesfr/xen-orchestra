6d881a9625e2c2e9457467a87595516a4b0ee2ea
Thierry Goettelmann <thierry@byscripts.info>
2023-11-09 10:55:07 +0100

    add `void` to ignored promise result

--- @xen-orchestra/lite/src/stores/modal.store.ts
+++ @xen-orchestra/lite/src/stores/modal.store.ts
@@ -32,7 +32,7 @@ export const useModalStore = defineStore("modal", () => {
         isBusy.value = true;
         const result = await payload;
         await approveEvent.trigger(result);
-        close();
+        void close();
       } finally {
         isBusy.value = false;
       }
@@ -42,7 +42,7 @@ export const useModalStore = defineStore("modal", () => {
       try {
         isBusy.value = true;
         await declineEvent.trigger(undefined);
-        close();
+        void close();
       } finally {
         isBusy.value = false;
       }
