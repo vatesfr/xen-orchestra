8fbd2e80fa4e3e4db413cfbe29b0fbfef027dc46
mathieuRA <contact@mathieu-raisin.fr>
2023-11-09 10:50:05 +0100

    Thierry's feedback

--- @xen-orchestra/lite/src/stores/modal.store.ts
+++ @xen-orchestra/lite/src/stores/modal.store.ts
@@ -12,12 +12,6 @@ import {
 
 export const useModalStore = defineStore("modal", () => {
   const modals = ref(new Map<symbol, ModalController>());
-  const closeEvent = createEventHook();
-
-  const close = (id: symbol) => {
-    modals.value.delete(id);
-    closeEvent.trigger(undefined);
-  };
 
   const open = <T>(loader: AsyncComponentLoader, props: object) => {
     const id = Symbol();
@@ -26,13 +20,19 @@ export const useModalStore = defineStore("modal", () => {
 
     const approveEvent = createEventHook<T>();
     const declineEvent = createEventHook();
+    const closeEvent = createEventHook();
+
+    const close = async () => {
+      await closeEvent.trigger(undefined);
+      modals.value.delete(id);
+    };
 
     const approve = async (payload: any) => {
       try {
         isBusy.value = true;
         const result = await payload;
         await approveEvent.trigger(result);
-        close(id);
+        close();
       } finally {
         isBusy.value = false;
       }
@@ -42,7 +42,7 @@ export const useModalStore = defineStore("modal", () => {
       try {
         isBusy.value = true;
         await declineEvent.trigger(undefined);
-        close(id);
+        close();
       } finally {
         isBusy.value = false;
       }
@@ -70,7 +70,6 @@ export const useModalStore = defineStore("modal", () => {
 
   return {
     open,
-    close,
     modals: computed(() => modals.value.values()),
   };
 });
