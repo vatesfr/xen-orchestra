83141989f0c2f18dbb442b783c6ff1cb43674adc
Mathieu <70369997+MathieuRA@users.noreply.github.com>
2023-11-09 10:57:24 +0100

    feat(xolite/modals): add onClose event (#7167)

--- @xen-orchestra/lite/src/stores/modal.store.ts
+++ @xen-orchestra/lite/src/stores/modal.store.ts
@@ -13,10 +13,6 @@ import {
 export const useModalStore = defineStore("modal", () => {
   const modals = ref(new Map<symbol, ModalController>());
 
-  const close = (id: symbol) => {
-    modals.value.delete(id);
-  };
-
   const open = <T>(loader: AsyncComponentLoader, props: object) => {
     const id = Symbol();
     const isBusy = ref(false);
@@ -24,13 +20,19 @@ export const useModalStore = defineStore("modal", () => {
 
     const approveEvent = createEventHook<T>();
     const declineEvent = createEventHook();
+    const closeEvent = createEventHook();
+
+    const close = async () => {
+      await closeEvent.trigger(undefined);
+      modals.value.delete(id);
+    };
 
     const approve = async (payload: any) => {
       try {
         isBusy.value = true;
         const result = await payload;
         await approveEvent.trigger(result);
-        close(id);
+        void close();
       } finally {
         isBusy.value = false;
       }
@@ -40,7 +42,7 @@ export const useModalStore = defineStore("modal", () => {
       try {
         isBusy.value = true;
         await declineEvent.trigger(undefined);
-        close(id);
+        void close();
       } finally {
         isBusy.value = false;
       }
@@ -61,13 +63,13 @@ export const useModalStore = defineStore("modal", () => {
     return {
       onApprove: approveEvent.on,
       onDecline: declineEvent.on,
+      onClose: closeEvent.on,
       id,
     };
   };
 
   return {
     open,
-    close,
     modals: computed(() => modals.value.values()),
   };
 });
