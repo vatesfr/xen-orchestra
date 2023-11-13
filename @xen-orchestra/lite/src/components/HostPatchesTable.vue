4d3b8009ab9999759522611050921a31130b4b7b
Thierry <thierry@byscripts.info>
2023-11-13 17:44:00 +0100

    move hosts patches with no changelog at bottom

--- @xen-orchestra/lite/src/components/HostPatchesTable.vue
+++ @xen-orchestra/lite/src/components/HostPatchesTable.vue
@@ -45,8 +45,10 @@ const props = defineProps<{
 
 const sortedPatches = computed(() =>
   [...props.patches].sort((patch1, patch2) => {
-    if (patch1.changelog == null || patch2.changelog == null) {
-      return 0;
+    if (patch1.changelog == null) {
+      return 1;
+    } else if (patch2.changelog == null) {
+      return -1;
     }
 
     return patch1.changelog.date - patch2.changelog.date;
