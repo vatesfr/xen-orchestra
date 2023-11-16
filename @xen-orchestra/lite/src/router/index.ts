5101ff29e12a8b95d4b8dd87838cf9e6f5ba3abb
mathieuRA <contact@mathieu-raisin.fr>
2023-11-16 11:05:35 +0100

    feat(lite/nav): keep query param 'master' during navigation

--- @xen-orchestra/lite/src/router/index.ts
+++ @xen-orchestra/lite/src/router/index.ts
@@ -1,8 +1,49 @@
+import { getFirst } from "@/libs/utils";
 import pool from "@/router/pool";
 import story from "@/router/story";
 import vm from "@/router/vm";
 import HomeView from "@/views/HomeView.vue";
+import type { Router } from "vue-router";
 import { createRouter, createWebHashHistory } from "vue-router";
+
+function preserveMasterQueryParam(router: Router) {
+  let keepMaster = true;
+
+  router.beforeEach((to, from, next) => {
+    const previousMasterQueryParam = getFirst(from.query["master"])?.trim();
+    const currentMasterQueryParam = getFirst(to.query["master"])?.trim();
+
+    if (previousMasterQueryParam == undefined) {
+      keepMaster = true;
+      next();
+      return;
+    }
+
+    // Needed to keep master query param between navigation
+    if (currentMasterQueryParam == undefined && keepMaster) {
+      const nav = router.resolve({
+        path: to.path,
+        query: { ...to.query, master: previousMasterQueryParam },
+      });
+      next(nav);
+      return;
+    }
+
+    // If user remove the master query value
+    if (currentMasterQueryParam === "") {
+      delete to.query.master;
+      keepMaster = false;
+      const nav = router.resolve({
+        path: to.path,
+        query: to.query,
+      });
+      next(nav);
+      return;
+    }
+
+    next();
+  });
+}
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -38,5 +79,7 @@ const router = createRouter({
     },
   ],
 });
+
+preserveMasterQueryParam(router);
 
 export default router;
