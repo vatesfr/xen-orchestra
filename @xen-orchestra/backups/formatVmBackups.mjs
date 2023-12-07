cec8237a470f578e9de6a3b984b5f81a19b5ff13
Florent Beauchamp <florent.beauchamp@vates.tech>
2023-12-07 13:48:59 +0000

    feat(xo-web): show if an incremental backup is a delta or a full

--- @xen-orchestra/backups/formatVmBackups.mjs
+++ @xen-orchestra/backups/formatVmBackups.mjs
@@ -25,6 +25,8 @@ function formatVmBackup(backup) {
       name_description: backup.vm.name_description,
       name_label: backup.vm.name_label,
     },
+    differencingVhds: Object.values(backup.isVhdDifferencing).filter(t => t).length,
+    dynamicVhds: Object.values(backup.isVhdDifferencing).filter(t => !t).length,
   }
 }
 
