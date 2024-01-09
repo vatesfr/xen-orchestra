88805c3e29e8bc2b64caec160197f5312f294347
Pizzosaure <melissafranca3@gmail.com>
2024-01-09 15:58:11 +0100

    feat(xo-web/pool/patches): disable rolling pool update button

--- packages/xo-web/src/xo-app/pool/tab-patches.js
+++ packages/xo-web/src/xo-app/pool/tab-patches.js
@@ -12,12 +12,16 @@ import { getXoaPlan, ENTERPRISE } from 'xoa-plans'
 import {
   installAllPatchesOnPool,
   installPatches,
+  isSrShared,
+  isSrWritable,
   rollingPoolUpdate,
   subscribeCurrentUser,
   subscribeHostMissingPatches,
 } from 'xo'
+import filter from 'lodash/filter.js'
 import isEmpty from 'lodash/isEmpty.js'
 import size from 'lodash/size.js'
+import some from 'lodash/some.js'
 
 const ROLLING_POOL_UPDATES_AVAILABLE = getXoaPlan().value >= ENTERPRISE.value
 
@@ -173,8 +177,32 @@ const INSTALLED_PATCH_COLUMNS = [
       poolId => host => host.$pool === poolId
     )
   ),
+  runningVms: createGetObjectsOfType('VM').filter(
+    createSelector(
+      (_, props) => props.pool.id,
+      poolId => vm => vm.$pool === poolId && vm.power_state === 'Running'
+    )
+  ),
+  vbds: createGetObjectsOfType('VBD'),
+  vdis: createGetObjectsOfType('VDI'),
+  srs: createGetObjectsOfType('SR'),
 })
 export default class TabPatches extends Component {
+  nVmsRunningOnLocalStorage = createSelector(
+    () => this.props.runningVms,
+    () => this.props.vbds,
+    () => this.props.vdis,
+    () => this.props.srs,
+    (runningVms, vbds, vdis, srs) =>
+      filter(runningVms, vm =>
+        some(vm.$VBDs, vbdId => {
+          const vbd = vbds[vbdId]
+          const vdi = vdis[vbd?.VDI]
+          const sr = srs[vdi?.$SR]
+          return !isSrShared(sr) && isSrWritable(sr)
+        })
+      ).length
+  )
   render() {
     const {
       hostPatches,
@@ -197,12 +225,18 @@ export default class TabPatches extends Component {
               {ROLLING_POOL_UPDATES_AVAILABLE && (
                 <TabButton
                   btnStyle='primary'
-                  disabled={isEmpty(missingPatches) || isSingleHost}
+                  disabled={isEmpty(missingPatches) || this.nVmsRunningOnLocalStorage() > 0}
                   handler={rollingPoolUpdate}
                   handlerParam={pool.id}
                   icon='pool-rolling-update'
                   labelId='rollingPoolUpdate'
-                  tooltip={isSingleHost ? _('multiHostPoolUpdate') : undefined}
+                  tooltip={
+                    this.nVmsRunningOnLocalStorage() > 0
+                      ? _('nVmsRunningOnLocalStorage', {
+                          nVms: this.nVmsRunningOnLocalStorage(),
+                        })
+                      : null
+                  }
                 />
               )}
               <TabButton
