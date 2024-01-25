b4952e0e456e570cdc0d6621f704250c82c8d95a
Pierre Donias <pierre.donias@gmail.com>
2024-01-25 16:13:40 +0100

    chore(web): use relative asset URLs
    Since @xen-orchestra/web can be served on different route than /, this change
    ensures that assets get fetched with relative URLs

--- @xen-orchestra/web/vite.config.ts
+++ @xen-orchestra/web/vite.config.ts
@@ -6,6 +6,7 @@ import vueRouter from 'unplugin-vue-router/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [vueRouter(), vue()],
   resolve: {
     alias: {
