2bc8f270490b227555eb55c2925b725262f9c653
Thierry <thierry@byscripts.info>
2024-01-31 11:47:24 +0100

    change router to webhash history

--- @xen-orchestra/web/src/main.ts
+++ @xen-orchestra/web/src/main.ts
@@ -2,13 +2,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 // eslint-disable-next-line import/no-unresolved -- https://github.com/posva/unplugin-vue-router/issues/232
-import { createRouter, createWebHistory } from 'vue-router/auto'
+import { createRouter, createWebHashHistory } from 'vue-router/auto'
 import '@xen-orchestra/web-core/assets/css/base.pcss'
 
 const app = createApp(App)
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
 })
 
 app.use(createPinia())
