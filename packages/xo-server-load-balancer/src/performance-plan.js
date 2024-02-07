vatesfr/xen-orchestra

commit d949112921408af81ed26cd8eff75549c635d944

fix(load-balancer): bad comparison to evaluate migration in perf plan (#7288)
Memory is compared to CPU usage to migrate VM in performance plan context.

This condition can cause unwanted migrations.

packages/xo-server-load-balancer/src/performance-plan.js
@@ -178,7 +178,7 @@ export default class PerformancePlan extends Plan {
       const state = this._getThresholdState(exceededAverages)
       if (
         destinationAverages.cpu + vmAverages.cpu >= this._thresholds.cpu.low ||
-        destinationAverages.memoryFree - vmAverages.memory <= this._thresholds.cpu.high ||
+        destinationAverages.memoryFree - vmAverages.memory <= this._thresholds.memory.high ||
         (!state.cpu &&
           !state.memory &&
           (exceededAverages.cpu - vmAverages.cpu < destinationAverages.cpu + vmAverages.cpu ||

commit 83a94eefd60680bf52830c46f1e0510d1316c65e

fix(xo-server-load-balancer): error during optimize (#7362)
Introduced by d949112

Fixes #7359

packages/xo-server-load-balancer/src/performance-plan.js
@@ -178,7 +178,7 @@ export default class PerformancePlan extends Plan {
       const state = this._getThresholdState(exceededAverages)
       if (
         destinationAverages.cpu + vmAverages.cpu >= this._thresholds.cpu.low ||
-        destinationAverages.memoryFree - vmAverages.memory <= this._thresholds.memory.high ||
+        destinationAverages.memoryFree - vmAverages.memory <= this._thresholds.memoryFree.high ||
         (!state.cpu &&
           !state.memory &&
           (exceededAverages.cpu - vmAverages.cpu < destinationAverages.cpu + vmAverages.cpu ||