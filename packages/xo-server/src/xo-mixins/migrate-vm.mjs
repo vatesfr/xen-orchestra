vatesfr/xen-orchestra

commit 2d047c4fef356104ccbf51bffecc293c36b33beb

feat(vmware/import): use xva to load base disk

packages/xo-server/src/xo-mixins/migrate-vm.mjs
@@ -4,12 +4,13 @@ import { fromEvent } from 'promise-toolbox'
 import { createRunner } from '@xen-orchestra/backups/Backup.mjs'
 import { Task } from '@xen-orchestra/mixins/Tasks.mjs'
 import { v4 as generateUuid } from 'uuid'
-import { VDI_FORMAT_RAW, VDI_FORMAT_VHD } from '@xen-orchestra/xapi'
+import { VDI_FORMAT_VHD } from '@xen-orchestra/xapi'
 import asyncMapSettled from '@xen-orchestra/async-map/legacy.js'
 import Esxi from '@xen-orchestra/vmware-explorer/esxi.mjs'
 import openDeltaVmdkasVhd from '@xen-orchestra/vmware-explorer/openDeltaVmdkAsVhd.mjs'
 import OTHER_CONFIG_TEMPLATE from '../xapi/other-config-template.mjs'
 import VhdEsxiRaw from '@xen-orchestra/vmware-explorer/VhdEsxiRaw.mjs'
+import { importVdi as importVdiThroughXva } from '@xen-orchestra/xva/importVdi.mjs'
 
 export default class MigrateVm {
   constructor(app) {
@@ -169,7 +170,7 @@ export default class MigrateVm {
   @decorateWith(deferrable)
   async migrationfromEsxi(
     $defer,
-    { host, user, password, sslVerify, sr: srId, network: networkId, vm: vmId, thin, stopSource }
+    { host, user, password, sslVerify, sr: srId, network: networkId, vm: vmId, stopSource }
   ) {
     const app = this._app
     const esxi = await this.#connectToEsxi(host, user, password, sslVerify)
@@ -220,7 +221,7 @@ export default class MigrateVm {
           xapi.VIF_create(
             {
               device: vifDevices[i],
-              network: xapi.getObject(networkId).$ref,
+              network: app.getXapiObject(networkId).$ref,
               VM: vm.$ref,
             },
             {
@@ -231,29 +232,13 @@ export default class MigrateVm {
       )
       return vm
     })
-
     $defer.onFailure.call(xapi, 'VM_destroy', vm.$ref)
 
     const vhds = await Promise.all(
       Object.keys(chainsByNodes).map(async (node, userdevice) =>
         Task.run({ properties: { name: `Cold import of disks ${node}` } }, async () => {
           const chainByNode = chainsByNodes[node]
-          const vdi = await xapi._getOrWaitObject(
-            await xapi.VDI_create({
-              name_description: 'fromESXI' + chainByNode[0].descriptionLabel,
-              name_label: '[ESXI]' + chainByNode[0].nameLabel,
-              SR: sr.$ref,
-              virtual_size: chainByNode[0].capacity,
-            })
-          )
-          // it can fail before the vdi is connected to the vm
-
-          $defer.onFailure.call(xapi, 'VDI_destroy', vdi.$ref)
-
-          await xapi.VBD_create({
-            VDI: vdi.$ref,
-            VM: vm.$ref,
-          })
+          let vdi
           let parentVhd, vhd
           // if the VM is running we'll transfer everything before the last , which is an active disk
           //  the esxi api does not allow us to read an active disk
@@ -262,27 +247,37 @@ export default class MigrateVm {
           for (let diskIndex = 0; diskIndex < nbColdDisks; diskIndex++) {
             // the first one  is a RAW disk ( full )
             const disk = chainByNode[diskIndex]
-            const { fileName, path, datastore, isFull } = disk
+            const { capacity, descriptionLabel, fileName, nameLabel, path, datastore, isFull } = disk
             if (isFull) {
-              vhd = await VhdEsxiRaw.open(esxi, datastore, path + '/' + fileName, { thin })
-              await vhd.readBlockAllocationTable()
+              vhd = await VhdEsxiRaw.open(esxi, datastore, path + '/' + fileName)
+              // we don't need to read the BAT with the importVdiThroughXva process
+              const vdiMetadata = {
+                name_description: 'fromESXI' + descriptionLabel,
+                name_label: '[ESXI]' + nameLabel,
+                SR: sr.$ref,
+                virtual_size: capacity,
+              }
+              vdi = await importVdiThroughXva(vdiMetadata, vhd, xapi, sr)
+              // it can fail before the vdi is connected to the vm
+              $defer.onFailure.call(xapi, 'VDI_destroy', vdi.$ref)
+              await xapi.VBD_create({
+                VDI: vdi.$ref,
+                VM: vm.$ref,
+              })
             } else {
-              vhd = await openDeltaVmdkasVhd(esxi, datastore, path + '/' + fileName, parentVhd)
+              vhd = await openDeltaVmdkasVhd(esxi, datastore, path + '/' + fileName, parentVhd, {
+                lookMissingBlockInParent: false,
+              })
             }
+            vhd.label = fileName
             parentVhd = vhd
           }
-          // it can be empty if the VM don't have a snapshot and is running
-          if (vhd !== undefined) {
-            if (thin) {
-              const stream = vhd.stream()
-              await vdi.$importContent(stream, { format: VDI_FORMAT_VHD })
-            } else {
-              // no transformation when there is no snapshot in thick mode
-              const stream = await vhd.rawContent()
-              await vdi.$importContent(stream, { format: VDI_FORMAT_RAW })
-            }
+          if (nbColdDisks > 1 /* got a cold snapshot chain */) {
+            // it can be empty if the VM don't have a snapshot and is running
+            const stream = vhd.stream()
+            await vdi.$importContent(stream, { format: VDI_FORMAT_VHD })
           }
-          return { vdi, vhd }
+          return vhd
         })
       )
     )
@@ -296,15 +291,28 @@ export default class MigrateVm {
           await Task.run({ properties: { name: `Transfering deltas of ${userdevice}` } }, async () => {
             const chainByNode = chainsByNodes[node]
             const disk = chainByNode[chainByNode.length - 1]
-            const { fileName, path, datastore, isFull } = disk
-            const { vdi, vhd: parentVhd } = vhds[userdevice]
+            const { capacity, descriptionLabel, fileName, nameLabel, path, datastore, isFull } = disk
+            let { vdi, vhd: parentVhd } = vhds[userdevice]
             let vhd
             if (vdi === undefined) {
               throw new Error(`Can't import delta of a running VM without its parent vdi`)
             }
             if (isFull) {
-              vhd = await VhdEsxiRaw.open(esxi, datastore, path + '/' + fileName, { thin })
-              await vhd.readBlockAllocationTable()
+              vhd = await VhdEsxiRaw.open(esxi, datastore, path + '/' + fileName, { thin: false })
+              // we don't need to read the BAT with the importVdiThroughXva process
+              const vdiMetadata = {
+                name_description: 'fromESXI' + descriptionLabel,
+                name_label: '[ESXI]' + nameLabel,
+                SR: sr.$ref,
+                virtual_size: capacity,
+              }
+              vdi = await importVdiThroughXva(vdiMetadata, vhd, xapi, sr)
+              // it can fail before the vdi is connected to the vm
+              $defer.onFailure.call(xapi, 'VDI_destroy', vdi.$ref)
+              await xapi.VBD_create({
+                VDI: vdi.$ref,
+                VM: vm.$ref,
+              })
             } else {
               if (parentVhd === undefined) {
                 throw new Error(`Can't import delta of a running VM without its parent VHD`)

@xen-orchestra/xva/.npmignore
@@ -1,0 +1,1 @@
+../../scripts/npmignore

@xen-orchestra/xva/_isNotEmptyRef.mjs
@@ -1,0 +1,5 @@
+export function isNotEmptyRef(val) {
+  const EMPTY = 'OpaqueRef:NULL'
+  const PREFIX = 'OpaqueRef:'
+  return val !== EMPTY && typeof val === 'string' && val.startsWith(PREFIX)
+}

@xen-orchestra/xva/_toOvaXml.mjs
@@ -1,0 +1,42 @@
+// from package xml-escape
+function escape(string) {
+  if (string === null || string === undefined) return
+  if (typeof string === 'number') {
+    return string
+  }
+  const map = {
+    '>': '&gt;',
+    '<': '&lt;',
+    "'": '&apos;',
+    '"': '&quot;',
+    '&': '&amp;',
+  }
+
+  const pattern = '([&"<>\'])'
+  return string.replace(new RegExp(pattern, 'g'), function (str, item) {
+    return map[item]
+  })
+}
+
+function formatDate(d) {
+  return d.toISOString().replaceAll('-', '').replace('.000Z', 'Z')
+}
+
+export default function toOvaXml(obj) {
+  if (Array.isArray(obj)) {
+    return `<value><array><data>${obj.map(val => toOvaXml(val)).join('')}</data></array></value>`
+  }
+
+  if (typeof obj === 'object') {
+    if (obj instanceof Date) {
+      return `<value><dateTime.iso8601>${escape(formatDate(obj))}</dateTime.iso8601></value>`
+    }
+    return `<value><struct>${Object.entries(obj)
+      .map(([key, value]) => `<member><name>${escape(key)}</name>${toOvaXml(value)}</member>`)
+      .join('')}</struct></value>`
+  }
+  if (typeof obj === 'boolean') {
+    return `<value><boolean>${obj ? 1 : 0}</boolean></value>`
+  }
+  return `<value>${escape(obj)}</value>`
+}

@xen-orchestra/xva/_writeDisk.mjs
@@ -1,0 +1,39 @@
+import { fromCallback } from 'promise-toolbox'
+import { readChunkStrict } from '@vates/read-chunk'
+import { XXHash64 } from 'xxhash'
+
+async function writeBlock(pack, data, name) {
+  await fromCallback.call(pack, pack.entry, { name }, data)
+  const hasher = new XXHash64(0)
+  hasher.update(data)
+  // weirdly, ocaml and xxhash return the bytes in reverse order to each other
+  const hash = hasher.digest().reverse().toString('hex').toUpperCase()
+  await fromCallback.call(pack, pack.entry, { name: `${name}.xxhash` }, Buffer.from(hash, 'utf8'))
+}
+export default async function addDisk(pack, vhd, basePath) {
+  let counter = 0
+  let written
+  const chunk_length = 1024 * 1024
+  const empty = Buffer.alloc(chunk_length, 0)
+  const stream = await vhd.rawContent()
+  let lastBlockLength
+  const diskSize = vhd.footer.currentSize
+  let remaining = diskSize
+  while (remaining > 0) {
+    const data = await readChunkStrict(stream, Math.min(chunk_length, remaining))
+    lastBlockLength = data.length
+    remaining -= lastBlockLength
+
+    if (counter === 0 || !data.equals(empty)) {
+      written = true
+      await writeBlock(pack, data, `${basePath}/${('' + counter).padStart(8, '0')}`)
+    } else {
+      written = false
+    }
+    counter++
+  }
+  if (!written) {
+    // last block must be present
+    writeBlock(pack, empty.slice(0, lastBlockLength), `${basePath}/${counter}`)
+  }
+}

@xen-orchestra/xva/_writeOvaXml.mjs
@@ -1,0 +1,154 @@
+import assert from 'node:assert'
+
+import { fromCallback } from 'promise-toolbox'
+import { v4 as uuid } from 'uuid'
+import defaultsDeep from 'lodash.defaultsdeep'
+
+import { DEFAULT_VBD } from './templates/vbd.mjs'
+import { DEFAULT_VDI } from './templates/vdi.mjs'
+import { DEFAULT_VIF } from './templates/vif.mjs'
+import { DEFAULT_VM } from './templates/vm.mjs'
+import toOvaXml from './_toOvaXml.mjs'
+
+export default async function writeOvaXml(
+  pack,
+  { memory, networks, nCpus, firmware, vdis, vhds, ...vmSnapshot },
+  { sr, network }
+) {
+  let refId = 0
+  function nextRef() {
+    return 'Ref:' + String(refId++).padStart(3, '0')
+  }
+  const data = {
+    version: {
+      hostname: 'localhost',
+      date: '2022-01-01',
+      product_version: '8.2.1',
+      product_brand: 'XCP-ng',
+      build_number: 'release/yangtze/master/58',
+      xapi_major: 1,
+      xapi_minor: 20,
+      export_vsn: 2,
+    },
+    objects: [],
+  }
+  const vm = defaultsDeep(
+    {
+      id: nextRef(),
+      // you can pass a full snapshot and nothing more to do
+      snapshot: vmSnapshot,
+    },
+    {
+      // some data need a little more work to be usable
+      // if they are not already in vm
+      snapshot: {
+        HVM_boot_params: {
+          firmware,
+        },
+        memory_static_max: memory,
+        memory_static_min: memory,
+        memory_dynamic_max: memory,
+        memory_dynamic_min: memory,
+        other_config: {
+          mac_seed: uuid(),
+        },
+        uuid: uuid(),
+        VCPUs_at_startup: nCpus,
+        VCPUs_max: nCpus,
+      },
+    },
+    DEFAULT_VM
+  )
+
+  data.objects.push(vm)
+  const srObj = defaultsDeep(
+    {
+      class: 'SR',
+      id: nextRef(),
+      snapshot: sr,
+    },
+    {
+      snapshot: {
+        VDIs: [],
+      },
+    }
+  )
+
+  data.objects.push(srObj)
+  assert.strictEqual(vhds.length, vdis.length)
+  for (let index = 0; index < vhds.length; index++) {
+    const userdevice = index + 1
+    const vhd = vhds[index]
+    const vdi = defaultsDeep(
+      {
+        id: nextRef(),
+        // overwrite SR from an opaqref to a ref:
+        snapshot: { ...vdis[index], SR: srObj.id },
+      },
+      {
+        snapshot: {
+          uuid: uuid(),
+        },
+      },
+      DEFAULT_VDI
+    )
+
+    data.objects.push(vdi)
+    srObj.snapshot.VDIs.push(vdi.id)
+    vhd.ref = vdi.id
+
+    const vbd = defaultsDeep(
+      {
+        id: nextRef(),
+        snapshot: {
+          device: `xvd${String.fromCharCode('a'.charCodeAt(0) + index)}`,
+          uuid: uuid(),
+          userdevice,
+          VM: vm.id,
+          VDI: vdi.id,
+        },
+      },
+      DEFAULT_VBD
+    )
+    data.objects.push(vbd)
+    vdi.snapshot.vbds.push(vbd.id)
+    vm.snapshot.VBDs.push(vbd.id)
+  }
+
+  if (network && networks?.length) {
+    const networkObj = defaultsDeep(
+      {
+        class: 'network',
+        id: nextRef(),
+        snapshot: network,
+      },
+      {
+        snapshot: {
+          vifs: [],
+        },
+      }
+    )
+    data.objects.push(networkObj)
+    let vifIndex = 0
+    for (const sourceNetwork of networks) {
+      const vif = defaultsDeep(
+        {
+          id: nextRef(),
+          snapshot: {
+            device: ++vifIndex,
+            MAC: sourceNetwork.macAddress,
+            MAC_autogenerated: sourceNetwork.isGenerated,
+            uuid: uuid(),
+            VM: vm.id,
+            network: networkObj.id,
+          },
+        },
+        DEFAULT_VIF
+      )
+      data.objects.push(vif)
+      networkObj.snapshot.vifs.push(vif.id)
+    }
+  }
+  const xml = toOvaXml(data)
+  await fromCallback.call(pack, pack.entry, { name: `ova.xml` }, xml)
+}

@xen-orchestra/xva/importVdi.mjs
@@ -1,0 +1,32 @@
+import { isNotEmptyRef } from './_isNotEmptyRef.mjs'
+import { importVm } from './importVm.mjs'
+
+export async function importVdi(vdi, vhd, xapi, sr) {
+  // create a fake VM
+  const vmRef = await importVm(
+    {
+      name_label: `[xva-disp-import]${vdi.name_label}`,
+      memory: 1024 * 1024 * 32,
+      nCpus: 1,
+      firmware: 'bios',
+      vdis: [vdi],
+      vhds: [vhd],
+    },
+    xapi,
+    sr
+  )
+  // wait for the VM to be loaded if necessary
+  xapi.getObject(vmRef, undefined) ?? (await xapi.waitObject(vmRef))
+
+  const vbdRefs = await xapi.getField('VM', vmRef, 'VBDs')
+  // get the disk
+  const disks = { __proto__: null }
+  ;(await xapi.getRecords('VBD', vbdRefs)).forEach(vbd => {
+    if (vbd.type === 'Disk' && isNotEmptyRef(vbd.VDI)) {
+      disks[vbd.VDI] = true
+    }
+  })
+  // destroy the VM and VBD
+  await xapi.call('VM.destroy', vmRef)
+  return await xapi.getRecord('VDI', Object.keys(disks)[0])
+}

@xen-orchestra/xva/importVm.mjs
@@ -1,0 +1,33 @@
+import tar from 'tar-stream'
+
+import writeOvaXml from './_writeOvaXml.mjs'
+import writeDisk from './_writeDisk.mjs'
+
+export async function importVm(vm, xapi, sr, network) {
+  const pack = tar.pack()
+  const taskRef = await xapi.task_create('VM import')
+  const query = {
+    sr_id: sr.$ref,
+  }
+
+  const promise = xapi
+    .putResource(pack, '/import/', {
+      query,
+      task: taskRef,
+    })
+    .catch(err => console.error(err))
+
+  await writeOvaXml(pack, vm, { sr, network })
+  for (const vhd of vm.vhds) {
+    await writeDisk(pack, vhd, vhd.ref)
+  }
+  pack.finalize()
+  const str = await promise
+  const matches = /OpaqueRef:[0-9a-z-]+/.exec(str)
+  if (!matches) {
+    const error = new Error('no opaque ref found')
+    error.haystack = str
+    throw error
+  }
+  return matches[0]
+}

commit 398407ea9cf16dd48e99fb99de701e3f0963a9ac

fix(xo-server/import): error during import of snapshot of running VM
introduced by 2d047c4fef356104ccbf51bffecc293c36b33beb

packages/xo-server/src/xo-mixins/migrate-vm.mjs
@@ -280,7 +280,7 @@ export default class MigrateVm {
             const stream = vhd.stream()
             await vdi.$importContent(stream, { format: VDI_FORMAT_VHD })
           }
-          return vhd
+          return { vdi, vhd }
         })
       )
     )