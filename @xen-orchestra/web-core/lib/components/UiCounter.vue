4671848cbdca645be8704f891ca3c6ab98522364
OlivierFL <66562640+OlivierFL@users.noreply.github.com>
2024-03-11 09:26:23 +0100

    feedback
    Co-authored-by: Thierry Goettelmann <thierry@byscripts.info>

--- @xen-orchestra/web-core/lib/components/UiCounter.vue
+++ @xen-orchestra/web-core/lib/components/UiCounter.vue
@@ -14,26 +14,9 @@ defineProps<{
 </script>
 
 <style lang="postcss" scoped>
+/* COLOR VARIANTS */
 .ui-counter {
-  font-weight: 600;
-  font-size: 1rem;
-  display: inline-flex;
-  align-items: center;
-  color: var(--color-grey-600);
-  border-radius: 9rem;
-  background-color: var(--background-color);
-  min-width: var(--size);
-  padding: 0 0.6rem;
-  text-transform: lowercase;
-  --size: 1.5rem;
   --background-color: var(--color-grey-300);
-
-  &.large {
-    --size: 2.4rem;
-    font-size: 1.6rem;
-    font-weight: 500;
-    padding: 0 0.8rem;
-  }
 
   &.info {
     --background-color: var(--color-purple-base);
@@ -54,5 +37,36 @@ defineProps<{
   &.black {
     --background-color: var(--color-grey-100);
   }
+}
+
+/* SIZE VARIANTS */
+.ui-counter {
+  & {
+    --font-size: 1rem;
+    --font-weight: 600;
+    --min-width: 1.5rem;
+    --padding: 0 0.6rem;
+  }
+
+  &.large {
+    --font-size: 1.6rem;
+    --font-weight: 500;
+    --min-width: 2.4rem;
+    --padding: 0 0.8rem;
+  }
+}
+
+/* IMPLEMENTATION */
+.ui-counter {
+  display: inline-flex;
+  align-items: center;
+  color: var(--color-grey-600);
+  border-radius: 9rem;
+  text-transform: lowercase;
+  background-color: var(--background-color);
+  font-size: var(--font-size);
+  font-weight: var(--font-weight);
+  min-width: var(--min-width);
+  padding: var(--padding);
 }
 </style>
