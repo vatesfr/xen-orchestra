51f95b3c8590492164be38a77ad2c7bf5dc42451
Thierry Goettelmann <thierry@byscripts.info>
2024-03-12 11:18:20 +0100

    feat(web-core): new ButtonIcon component (#7447)

---
+++ @xen-orchestra/lite/src/stories/web-core/button/button-icon.story.vue
@@ -0,0 +1,26 @@
+<template>
+  <ComponentStory
+    v-slot="{ properties }"
+    :params="[
+      iconProp().preset(faEllipsis),
+      colorProp(),
+      prop('size')
+        .default('medium')
+        .str()
+        .widget(choice('small', 'medium', 'large')),
+      prop('disabled').bool().widget(),
+      prop('active').bool().widget(),
+      prop('dot').bool().widget(),
+    ]"
+  >
+    <ButtonIcon v-bind="properties" />
+  </ComponentStory>
+</template>
+
+<script lang="ts" setup>
+import ComponentStory from '@/components/component-story/ComponentStory.vue'
+import { colorProp, iconProp, prop } from '@/libs/story/story-param'
+import { choice } from '@/libs/story/story-widget'
+import ButtonIcon from '@core/components/button/ButtonIcon.vue'
+import { faEllipsis } from '@fortawesome/free-solid-svg-icons'
+</script>
