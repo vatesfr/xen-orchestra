ec974cc3be4a089b179db4cfdb4b2fff1a95ab8e
b-Nollet <bastien.nollet@vates.tech>
2024-03-12 16:55:04 +0100

    other suggested changes

--- @xen-orchestra/xapi/network.mjs
+++ @xen-orchestra/xapi/network.mjs
@@ -1,37 +1,46 @@
 import { asyncEach } from '@vates/async-each'
+import { createLogger } from '@xen-orchestra/log'
+
+const { warn } = createLogger('xo:xapi:network')
 
 export default class Network {
   async setMtu(network, mtu) {
     await network.set_MTU(mtu)
 
     // The MTU will not be updated for VIFs of paused VM, but we can't unplug/replug VIF on a paused VM
-    const pluggedVIFs = network.$VIFs.filter(vif => vif.currently_attached && vif.$VM.power_state === 'Running')
-    const pluggedPIFs = network.$PIFs.filter(pif => pif.currently_attached)
+    const pluggedVifsRefs = network.$VIFs
+      .filter(vif => vif.currently_attached && vif.$VM.power_state === 'Running')
+      .map(vif => vif.$ref)
+    const pluggedPifsRefs = network.$PIFs.filter(pif => pif.currently_attached).map(pif => pif.$ref)
 
     let errorOccurred = false
 
     for (const { method, networkInterfaces } of [
       {
         method: 'VIF.unplug',
-        networkInterfaces: pluggedVIFs,
-      }, // unplugging PIFs before re-plugging seems unnecessary
+        networkInterfaces: pluggedVifsRefs,
+      },
+      {
+        method: 'PIF.unplug',
+        networkInterfaces: pluggedPifsRefs,
+      }, // unplugging PIFs may be unnecessary
       {
         method: 'PIF.plug',
-        networkInterfaces: pluggedPIFs,
+        networkInterfaces: pluggedPifsRefs,
       },
       {
         method: 'VIF.plug',
-        networkInterfaces: pluggedVIFs,
+        networkInterfaces: pluggedVifsRefs,
       },
     ]) {
       try {
-        await asyncEach(networkInterfaces, networkInterface => this.callAsync(method, networkInterface.$ref), {
-          concurrency: 20,
+        await asyncEach(networkInterfaces, networkInterface => this.callAsync(method, networkInterface), {
           stopOnError: false,
         })
       } catch (error) {
         // report an error occured, but continue the re-plugging process
         errorOccurred = true
+        warn(error)
       }
     }
 
