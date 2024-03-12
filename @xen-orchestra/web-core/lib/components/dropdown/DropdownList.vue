a1def028f044ca0f838ed2e15945b1c030757fd4
Thierry Goettelmann <thierry@byscripts.info>
2024-03-12 10:54:39 +0100

    feat(web-core): new dropdown components (#7420)
    New `DropdownList`, `DropdownTitle` and `DropdownItem` components, based on Design System.

---
+++ @xen-orchestra/web-core/lib/components/dropdown/DropdownList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="dropdown-list">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { IK_DROPDOWN_CHECKBOX } from '@core/utils/injection-keys.util'
+import { computed, provide } from 'vue'
+
+const props = defineProps<{
+  checkbox?: boolean
+}>()
+
+provide(
+  IK_DROPDOWN_CHECKBOX,
+  computed(() => props.checkbox)
+)
+</script>
+
+<style lang="postcss" scoped>
+.dropdown-list {
+  display: flex;
+  flex-direction: column;
+  padding: 0.4rem 0;
+  gap: 0.2rem;
+  border-radius: 0.4rem;
+  background: var(--background-color-primary);
+  box-shadow: var(--shadow-300);
+}
+</style>
--- @xen-orchestra/web-core/lib/utils/injection-keys.util.ts
+++ @xen-orchestra/web-core/lib/utils/injection-keys.util.ts
@@ -7,3 +7,5 @@ export const IK_TREE_ITEM_TOGGLE = Symbol('IK_TREE_ITEM_TOGGLE') as InjectionKey
 export const IK_TREE_ITEM_EXPANDED = Symbol('IK_TREE_ITEM_EXPANDED') as InjectionKey<Ref<boolean>>
 
 export const IK_TREE_LIST_DEPTH = Symbol('IK_TREE_LIST_DEPTH') as InjectionKey<number>
+
+export const IK_DROPDOWN_CHECKBOX = Symbol('IK_DROPDOWN_CHECKBOX') as InjectionKey<ComputedRef<boolean>>
