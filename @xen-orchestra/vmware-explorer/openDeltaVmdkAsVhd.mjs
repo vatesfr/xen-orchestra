098a3ad90f532d0f66db2c137a65d0fa88cdabfe
Florent Beauchamp <florent.beauchamp@vates.tech>
2024-03-20 15:54:57 +0000

    feat(vmware-explorer): Use NFS datastore from XO Remote to bypass VMFS6 lock
    - Migrate VM to NFS on VMWare side
    - mount NFS in XO , named [VMWARE]{datastoreName}
    - launch import and it will read through the direct NFS instead of using the soap access layer

--- @xen-orchestra/vmware-explorer/openDeltaVmdkAsVhd.mjs
+++ @xen-orchestra/vmware-explorer/openDeltaVmdkAsVhd.mjs
@@ -1,13 +1,15 @@
 import VHDEsxiSeSparse from './VhdEsxiSeSparse.mjs'
 import VhdEsxiCowd from './VhdEsxiCowd.mjs'
+import { openDatastore } from './_openDatastore.mjs'
 
-export default async function openDeltaVmdkasVhd(esxi, datastore, path, parentVhd, opts) {
+export default async function openDeltaVmdkasVhd(datastoreName, path, parentVhd, opts) {
   let vhd
+  const datastore = openDatastore(datastoreName, opts)
   if (path.endsWith('-sesparse.vmdk')) {
-    vhd = new VHDEsxiSeSparse(esxi, datastore, path, parentVhd, opts)
+    vhd = new VHDEsxiSeSparse(datastore, path, parentVhd, opts)
   } else {
     if (path.endsWith('-delta.vmdk')) {
-      vhd = new VhdEsxiCowd(esxi, datastore, path, parentVhd, opts)
+      vhd = new VhdEsxiCowd(datastore, path, parentVhd, opts)
     } else {
       throw new Error(`Vmdk ${path} does not seems to be a delta vmdk.`)
     }
