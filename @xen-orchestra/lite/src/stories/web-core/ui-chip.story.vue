9667593b42f2331bec664bfa1d7756f2f5d8cdb4
Olivier Floch <olivier.floch@vates.fr>
2024-03-21 09:32:57 +0100

    feedback

--- @xen-orchestra/lite/src/stories/web-core/ui-chip.story.vue
+++ @xen-orchestra/lite/src/stories/web-core/ui-chip.story.vue
@@ -18,5 +18,5 @@
 <script lang="ts" setup>
 import ComponentStory from '@/components/component-story/ComponentStory.vue'
 import { colorProp, event, iconProp, prop, setting, slot } from '@/libs/story/story-param'
-import UiChip from '@core/components/UiChip.vue'
+import UiChip from '@core/components/chip/UiChip.vue'
 </script>
