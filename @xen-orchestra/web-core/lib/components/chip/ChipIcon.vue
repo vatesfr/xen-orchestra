7f2c84d88e63d225aeb2e67624cbf8aac629a297
OlivierFL <66562640+OlivierFL@users.noreply.github.com>
2024-03-25 10:04:22 +0100

    feat(web-core): add new chip component (#7485)

---
+++ @xen-orchestra/web-core/lib/components/chip/ChipIcon.vue
@@ -0,0 +1,29 @@
+<template>
+  <UiIcon :class="{ disabled }" :color :icon class="chip-icon" fixed-width />
+</template>
+
+<script lang="ts" setup>
+import UiIcon from '@core/components/icon/UiIcon.vue'
+import type { Color } from '@core/types/color.type'
+import type { IconDefinition } from '@fortawesome/fontawesome-common-types'
+
+defineProps<{
+  color: Color
+  icon?: IconDefinition
+  disabled?: boolean
+}>()
+</script>
+
+<style lang="postcss" scoped>
+.chip-icon {
+  color: var(--color-grey-200);
+
+  &.disabled {
+    color: var(--color-grey-600);
+
+    &.info {
+      color: var(--color-grey-400);
+    }
+  }
+}
+</style>
