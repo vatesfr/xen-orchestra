2fc393b907ea60eeac7e7242f8a2aec80a307d80
Julien Fontanet <julien.fontanet@isonoe.net>
2024-03-25 13:26:34 +0100

    fix(xen-api/examples/export-vdi): proxy variable

--- packages/xen-api/examples/export-vdi.mjs
+++ packages/xen-api/examples/export-vdi.mjs
@@ -14,6 +14,7 @@ import { createOutputStream, formatProgress, pipeline, resolveRecord, throttle }
 
 defer(async ($defer, rawArgs) => {
   const {
+    proxy,
     raw,
     throttle: bps,
     _: args,
