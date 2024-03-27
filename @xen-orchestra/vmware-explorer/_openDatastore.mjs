ef7560cb65a1efe3a04d02f918c5610c3234fb80
Florent BEAUCHAMP <florent.beauchamp@vates.fr>
2024-03-27 17:26:57 +0100

    feat(vmware-explorer): log datastore used

--- @xen-orchestra/vmware-explorer/_openDatastore.mjs
+++ @xen-orchestra/vmware-explorer/_openDatastore.mjs
@@ -1,10 +1,21 @@
 import { DatastoreSoapEsxi } from './DatastoreSoapEsxi.mjs'
 import { DatastoreXoRemote } from './DatastoreXoRemote.mjs'
+import { createLogger } from '@xen-orchestra/log'
+
+const { info } = createLogger('xo:vmware-explorer:openDatastore')
 
 export function openDatastore(dataStoreName, { esxi, dataStoreToHandlers = {}, ...otherOptions }) {
   const handler = dataStoreToHandlers[dataStoreName]
   if (handler === undefined) {
+    info(`use Soap api to read datastore ${dataStoreName}`, {
+      dataStoreName,
+      dataStoreToHandlers: Object.keys(dataStoreToHandlers),
+    })
     return new DatastoreSoapEsxi(dataStoreName, { esxi, ...otherOptions })
   }
+  info(`use Xo remote to read  datastore ${dataStoreName}`, {
+    dataStoreName,
+    dataStoreToHandlers: Object.keys(dataStoreToHandlers),
+  })
   return new DatastoreXoRemote(dataStoreName, { handler, ...otherOptions })
 }
