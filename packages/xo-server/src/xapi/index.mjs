35a05757ad70f44d87ec2693ecce77d8ac111ed7
b-Nollet <bastien.nollet@vates.tech>
2024-03-27 11:47:41 +0100

    feat(xo-server): HA setup

--- packages/xo-server/src/xapi/index.mjs
+++ packages/xo-server/src/xapi/index.mjs
@@ -149,6 +149,18 @@ export default class Xapi extends XapiBase {
 
   // =================================================================
 
+  async disableHa() {
+    await this.call('pool.disable_ha')
+  }
+
+  // =================================================================
+
+  async enableHa(heartbeatSrs, configuration) {
+    await this.call('pool.enable_ha', heartbeatSrs, configuration)
+  }
+
+  // =================================================================
+
   async joinPool(masterAddress, masterUsername, masterPassword, force = false) {
     try {
       await this.call(force ? 'pool.join_force' : 'pool.join', masterAddress, masterUsername, masterPassword)
