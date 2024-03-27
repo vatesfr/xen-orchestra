ae780c9bb541f58d0cadd9b02b9c489fc4f2a8ae
OlivierFL <66562640+OlivierFL@users.noreply.github.com>
2024-03-27 09:42:27 +0100

    feat(lite): disable navigation to WiP views via tabs (#7482)

--- @xen-orchestra/lite/src/components/pool/PoolTabBar.vue
+++ @xen-orchestra/lite/src/components/pool/PoolTabBar.vue
@@ -3,25 +3,25 @@
     <RouterTab :to="{ name: 'pool.dashboard', params: { uuid: pool?.uuid } }">
       {{ $t('dashboard') }}
     </RouterTab>
-    <RouterTab :to="{ name: 'pool.alarms', params: { uuid: pool?.uuid } }">
+    <RouterTab :to="{ name: 'pool.alarms', params: { uuid: pool?.uuid } }" disabled>
       {{ $t('alarms') }}
     </RouterTab>
-    <RouterTab :to="{ name: 'pool.stats', params: { uuid: pool?.uuid } }">
+    <RouterTab :to="{ name: 'pool.stats', params: { uuid: pool?.uuid } }" disabled>
       {{ $t('stats') }}
     </RouterTab>
-    <RouterTab :to="{ name: 'pool.system', params: { uuid: pool?.uuid } }">
+    <RouterTab :to="{ name: 'pool.system', params: { uuid: pool?.uuid } }" disabled>
       {{ $t('system') }}
     </RouterTab>
-    <RouterTab :to="{ name: 'pool.network', params: { uuid: pool?.uuid } }">
+    <RouterTab :to="{ name: 'pool.network', params: { uuid: pool?.uuid } }" disabled>
       {{ $t('network') }}
     </RouterTab>
-    <RouterTab :to="{ name: 'pool.storage', params: { uuid: pool?.uuid } }">
+    <RouterTab :to="{ name: 'pool.storage', params: { uuid: pool?.uuid } }" disabled>
       {{ $t('storage') }}
     </RouterTab>
     <RouterTab :to="{ name: 'pool.tasks', params: { uuid: pool?.uuid } }">
       {{ $t('tasks') }}
     </RouterTab>
-    <RouterTab :to="{ name: 'pool.hosts', params: { uuid: pool?.uuid } }">
+    <RouterTab :to="{ name: 'pool.hosts', params: { uuid: pool?.uuid } }" disabled>
       {{ $t('hosts') }}
     </RouterTab>
     <RouterTab :to="{ name: 'pool.vms', params: { uuid: pool?.uuid } }">
