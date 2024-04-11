vatesfr/xen-orchestra

commit c96c2535686bf420be306a9d8ef34bb38eea91b4

feat(xo-web/templates): allow bypassing blocked operation when deleting protected template (#7493)
See Zammad#21638

packages/xo-web/src/common/intl/messages.js
@@ -1519,7 +1519,8 @@ const messages = {
   deleteDefaultTemplatesTitle: 'Delete default template{nDefaultTemplates, plural, one {} other {s}}',
   deleteDefaultTemplatesMessage:
     'You are attempting to delete {nDefaultTemplates, number} default template{nDefaultTemplates, plural, one {} other {s}}. Do you want to continue?',
-
+  deleteProtectedTemplatesTitle:'Delete protected template{nProtectedTemplates, plural, one {} other {s}}',
+  deleteProtectedTemplatesMessage: 'You are attempting to delete {nProtectedTemplates, plural, one {a} other {nProtectedTemplates}} template{nProtectedTemplates, plural, one {} other {s}} protected from accidental deletion. Do you want to continue?',
   // ----- Dashboard -----
   poolPanel: 'Pool{pools, plural, one {} other {s}}',
   hostPanel: 'Host{hosts, plural, one {} other {s}}',

packages/xo-server/src/api/vm.mjs
@@ -425,6 +425,7 @@ const delete_ = defer(async function (
     delete_disks, // eslint-disable-line camelcase
     force,
     forceDeleteDefaultTemplate,
+    forceBlockedOperation,
     vm,
 
     deleteDisks = delete_disks,
@@ -468,7 +469,12 @@ const delete_ = defer(async function (
     }
   })
 
-  return xapi.VM_destroy(vm._xapiRef, { deleteDisks, force, forceDeleteDefaultTemplate })
+  return xapi.VM_destroy(vm._xapiRef, {
+    deleteDisks,
+    force,
+    forceDeleteDefaultTemplate,
+    bypassBlockedOperation: forceBlockedOperation,
+  })
 })
 
 delete_.params = {
@@ -485,6 +491,11 @@ delete_.params = {
   },
 
   forceDeleteDefaultTemplate: {
+    optional: true,
+    type: 'boolean',
+  },
+
+  forceBlockedOperation: {
     optional: true,
     type: 'boolean',
   },

packages/xo-web/src/common/xo/index.js
@@ -1573,17 +1573,21 @@ export const deleteTemplates = templates =>
     body: _('templateDeleteModalBody', { templates: templates.length }),
   }).then(async () => {
     const defaultTemplates = []
+    const protectedTemplates = []
     let nErrors = 0
+    const isDefaultTemplateError = error =>
+      incorrectState.is(error, {
+        expected: false,
+        property: 'isDefaultTemplate',
+      })
+
     await Promise.all(
       map(resolveIds(templates), id =>
         _call('vm.delete', { id }).catch(reason => {
-          if (
-            incorrectState.is(reason, {
-              expected: false,
-              property: 'isDefaultTemplate',
-            })
-          ) {
+          if (isDefaultTemplateError(reason)) {
             defaultTemplates.push(id)
+          } else if (forbiddenOperation.is(reason)) {
+            protectedTemplates.push(id)
           } else {
             nErrors++
           }
@@ -1591,42 +1595,56 @@ export const deleteTemplates = templates =>
       )
     )
 
-    const nDefaultTemplates = defaultTemplates.length
-    if (nDefaultTemplates === 0 && nErrors === 0) {
-      return
+    const nProtectedTemplates = protectedTemplates.length
+    let forceBlockedOperation = false
+
+    if (nProtectedTemplates !== 0) {
+      await confirm({
+        title: _('deleteProtectedTemplatesTitle', { nProtectedTemplates }),
+        body: _('deleteProtectedTemplatesMessage', { nProtectedTemplates }),
+      })
+      forceBlockedOperation = true
+      await Promise.all(
+        protectedTemplates.map(id =>
+          _call('vm.delete', {
+            id,
+            forceBlockedOperation,
+          }).catch(reason => {
+            if (isDefaultTemplateError(reason)) {
+              defaultTemplates.push(id)
+            } else {
+              nErrors++
+            }
+          })
+        )
+      )
     }
 
-    const showError = () =>
+    const nDefaultTemplates = defaultTemplates.length
+    if (nDefaultTemplates !== 0) {
+      await confirm({
+        title: _('deleteDefaultTemplatesTitle', { nDefaultTemplates }),
+        body: _('deleteDefaultTemplatesMessage', { nDefaultTemplates }),
+      })
+      await Promise.all(
+        defaultTemplates.map(id =>
+          _call('vm.delete', {
+            id,
+            forceDeleteDefaultTemplate: true,
+            forceBlockedOperation,
+          }).catch(() => {
+            nErrors++
+          })
+        )
+      )
+    }
+
+    if (nErrors !== 0) {
       error(
         _('failedToDeleteTemplatesTitle', { nTemplates: nErrors }),
         _('failedToDeleteTemplatesMessage', { nTemplates: nErrors })
       )
-
-    return nDefaultTemplates === 0
-      ? showError()
-      : confirm({
-          title: _('deleteDefaultTemplatesTitle', { nDefaultTemplates }),
-          body: _('deleteDefaultTemplatesMessage', { nDefaultTemplates }),
-        })
-          .then(
-            () =>
-              Promise.all(
-                map(defaultTemplates, id =>
-                  _call('vm.delete', {
-                    id,
-                    forceDeleteDefaultTemplate: true,
-                  }).catch(() => {
-                    nErrors++
-                  })
-                )
-              ),
-            noop
-          )
-          .then(() => {
-            if (nErrors !== 0) {
-              showError()
-            }
-          }, noop)
+    }
   }, noop)
 
 export const snapshotVm = async (vm, name, saveMemory, description) => {

commit ec1e0b8701d1506a31ff10274e1ff61683e028c5

fix(xo-web): fix Prettier formatting issues
Introduced by c96c2535686bf420be306a9d8ef34bb38eea91b4, a4c775d4efe9d4c2a83cef1acfb1eda9bfb2e630 & 44f6d2f0e3f01c1686694b13077768c3e3b01f32

packages/xo-web/src/common/intl/messages.js
@@ -1526,8 +1526,9 @@ const messages = {
   deleteDefaultTemplatesTitle: 'Delete default template{nDefaultTemplates, plural, one {} other {s}}',
   deleteDefaultTemplatesMessage:
     'You are attempting to delete {nDefaultTemplates, number} default template{nDefaultTemplates, plural, one {} other {s}}. Do you want to continue?',
-  deleteProtectedTemplatesTitle:'Delete protected template{nProtectedTemplates, plural, one {} other {s}}',
-  deleteProtectedTemplatesMessage: 'You are attempting to delete {nProtectedTemplates, plural, one {a} other {nProtectedTemplates}} template{nProtectedTemplates, plural, one {} other {s}} protected from accidental deletion. Do you want to continue?',
+  deleteProtectedTemplatesTitle: 'Delete protected template{nProtectedTemplates, plural, one {} other {s}}',
+  deleteProtectedTemplatesMessage:
+    'You are attempting to delete {nProtectedTemplates, plural, one {a} other {nProtectedTemplates}} template{nProtectedTemplates, plural, one {} other {s}} protected from accidental deletion. Do you want to continue?',
   // ----- Dashboard -----
   poolPanel: 'Pool{pools, plural, one {} other {s}}',
   hostPanel: 'Host{hosts, plural, one {} other {s}}',
@@ -2584,7 +2585,8 @@ const messages = {
   poolAlreadyHasXostor: 'Pool already has a XOSTOR',
   poolNotRecentEnough: 'Not recent enough. Current version: {version}',
   replication: 'Replication',
-  rpuNoLongerAvailableIfXostor: 'As long as a XOSTOR storage is present in the pool, Rolling Pool Update will not be available',
+  rpuNoLongerAvailableIfXostor:
+    'As long as a XOSTOR storage is present in the pool, Rolling Pool Update will not be available',
   selectDisks: 'Select disk(s)…',
   selectedDiskTypeIncompatibleXostor: 'Only disks of type "Disk" and "Raid" are accepted. Selected disk type: {type}.',
   storage: 'Storage',

packages/xo-web/src/xo-app/hub/recipes/recipe-form.js
@@ -146,7 +146,7 @@ export default decorate([
             .filter(version => !version.prerelease)
             .map(({ tag_name }) => ({
               label: tag_name,
-              value: tag_name.slice(1)
+              value: tag_name.slice(1),
             }))
           return versionList
         } else {