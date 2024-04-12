1b9556ccc3ea5a08fdc16e28b4b6e3f7601228f7
Olivier Floch <olivier.floch@vates.fr>
2024-04-12 09:59:59 +0200

    feat(web-core): add new ComplexIcon component

---
+++ @xen-orchestra/lite/src/stories/web-core/icon/complex-icon.story.vue
@@ -0,0 +1,22 @@
+<template>
+  <ComponentStory
+    v-slot="{ properties }"
+    :params="[
+      prop('size').enum('small', 'medium', 'large').default('medium').widget(),
+      slot().help('Meant to receive two <UiIcon> components. The second icon component is the stacked icon'),
+    ]"
+  >
+    <ComplexIcon v-bind="properties">
+      <UiIcon :icon="faRotateRight" />
+      <UiIcon :icon="faLightbulb" />
+    </ComplexIcon>
+  </ComponentStory>
+</template>
+
+<script lang="ts" setup>
+import ComponentStory from '@/components/component-story/ComponentStory.vue'
+import { prop, slot } from '@/libs/story/story-param'
+import ComplexIcon from '@core/components/icon/ComplexIcon.vue'
+import UiIcon from '@core/components/icon/UiIcon.vue'
+import { faLightbulb, faRotateRight } from '@fortawesome/free-solid-svg-icons'
+</script>
