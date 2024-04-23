afdc4568510e3cbf31128819aef89b42b6054cbe
Thierry <thierry@byscripts.info>
2024-04-23 16:33:16 +0200

    feat(web-stack): add i18n + keyboard shortcuts

--- @xen-orchestra/web/src/main.ts
+++ @xen-orchestra/web/src/main.ts
@@ -1,8 +1,9 @@
-import { createApp } from 'vue'
+import i18n from '@core/i18n'
 import { createPinia } from 'pinia'
-import App from './App.vue'
+import { createApp } from 'vue'
 // eslint-disable-next-line import/no-unresolved -- https://github.com/posva/unplugin-vue-router/issues/232
 import { createRouter, createWebHashHistory } from 'vue-router/auto'
+import App from './App.vue'
 import '@xen-orchestra/web-core/assets/css/base.pcss'
 
 const app = createApp(App)
@@ -11,6 +12,7 @@ const router = createRouter({
   history: createWebHashHistory(),
 })
 
+app.use(i18n)
 app.use(createPinia())
 app.use(router)
 
