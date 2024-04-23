8eb4dbb6837d67a02507a06bae93e57260f31d5e
Thierry Goettelmann <thierry@byscripts.info>
2024-04-23 16:41:54 +0200

    feat(web-stack): add i18n + keyboard shortcuts (#7590)
    - Move i18n configuration from Lite to Core
    - Add keyboard shortcuts (toggle lang / theme) to XO 6
    - Add locales to Core
    - Import Core locales into Lite and XO 6

--- @xen-orchestra/web/src/App.vue
+++ @xen-orchestra/web/src/App.vue
@@ -1,3 +1,31 @@
 <template>
   <RouterView />
 </template>
+
+<script lang="ts" setup>
+import { useUiStore } from '@core/stores/ui.store'
+import { useActiveElement, useMagicKeys, whenever } from '@vueuse/core'
+import { logicAnd } from '@vueuse/math'
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+const uiStore = useUiStore()
+
+if (import.meta.env.DEV) {
+  const { locale } = useI18n()
+  const activeElement = useActiveElement()
+  const { D, L } = useMagicKeys()
+
+  const canToggle = computed(() => {
+    if (activeElement.value == null) {
+      return true
+    }
+
+    return !['INPUT', 'TEXTAREA'].includes(activeElement.value.tagName)
+  })
+
+  whenever(logicAnd(D, canToggle), () => (uiStore.colorMode = uiStore.colorMode === 'dark' ? 'light' : 'dark'))
+
+  whenever(logicAnd(L, canToggle), () => (locale.value = locale.value === 'en' ? 'fr' : 'en'))
+}
+</script>
--- @xen-orchestra/web/vite.config.ts
+++ @xen-orchestra/web/vite.config.ts
@@ -1,13 +1,20 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import vueI18n from '@intlify/unplugin-vue-i18n/vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+import { resolve } from 'path'
 import vueRouter from 'unplugin-vue-router/vite'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [vueRouter(), vue()],
+  plugins: [
+    vueRouter(),
+    vue(),
+    vueI18n({
+      include: [resolve(__dirname, 'src/locales/**'), resolve(__dirname, '../web-core/lib/locales/**')],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
