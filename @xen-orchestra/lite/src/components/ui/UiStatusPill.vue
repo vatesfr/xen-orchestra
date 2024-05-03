07f7bdfc3700b27186b39b4d0db5a7cd62324ce0
Paloma Tejeda <palomatejeda81@gmail.com>
2024-05-03 10:23:57 +0200

    feat(xo6/core): Add status pill UI component for backup issues

---
+++ @xen-orchestra/lite/src/components/ui/UiStatusPill.vue
@@ -0,0 +1,41 @@
+<template>
+  <UiIcon :icon class="icon" :class="props.state" />
+</template>
+
+<script lang="ts" setup>
+import UiIcon from '@/components/ui/icon/UiIcon.vue'
+import { computed } from 'vue'
+import { faCheckCircle, faCircleMinus, faCircleXmark } from '@fortawesome/free-solid-svg-icons'
+import type { Color } from '@/types'
+
+const props = defineProps<{
+  state: Color
+}>()
+
+const icon = computed(() => {
+  switch (props.state) {
+    case 'error':
+      return faCircleXmark
+    case 'warning':
+      return faCircleMinus
+    case 'success':
+      return faCheckCircle
+  }
+})
+</script>
+
+<style scoped lang="postcss">
+.icon {
+  color: var(--icon-color);
+
+  &.error {
+    --icon-color: var(--color-red-base);
+  }
+  &.warning {
+    --icon-color: var(--color-orange-base);
+  }
+  &.success {
+    --icon-color: var(--color-green-base);
+  }
+}
+</style>
