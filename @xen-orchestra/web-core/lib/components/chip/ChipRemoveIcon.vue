57e6bfe8e937259bf8840c923555c5040044acc2
Olivier Floch <olivier.floch@vates.fr>
2024-05-06 11:11:48 +0200

    feedback

--- @xen-orchestra/web-core/lib/components/chip/ChipRemoveIcon.vue
+++ @xen-orchestra/web-core/lib/components/chip/ChipRemoveIcon.vue
@@ -1,6 +1,6 @@
 <!-- v1.1.0 -->
 <template>
-  <ButtonIcon v-if="!disabled" :color :disabled :icon="faXmark" class="chip-remove-icon" size="small" />
+  <ButtonIcon :color :icon="faXmark" class="chip-remove-icon" size="small" />
 </template>
 
 <script lang="ts" setup>
@@ -10,18 +10,5 @@ import { faXmark } from '@fortawesome/free-solid-svg-icons'
 
 defineProps<{
   color: Color
-  disabled?: boolean
 }>()
 </script>
-
-<style lang="postcss" scoped>
-.chip-remove-icon {
-  &.disabled {
-    color: var(--color-grey-600);
-
-    &.info {
-      color: var(--color-grey-400);
-    }
-  }
-}
-</style>
