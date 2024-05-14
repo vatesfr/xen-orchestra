5184097a76e074a194de9e4dcc209721d515fda9
Paloma <94314524+P4l0m4@users.noreply.github.com>
2024-05-14 10:11:32 +0200

    Update @xen-orchestra/lite/src/components/pool/dashboard/ArrowCollapseButton.vue
    Co-authored-by: Thierry Goettelmann <thierry@byscripts.info>

--- @xen-orchestra/lite/src/components/pool/dashboard/ArrowCollapseButton.vue
+++ @xen-orchestra/lite/src/components/pool/dashboard/ArrowCollapseButton.vue
@@ -1,19 +1,16 @@
 <template>
   <button class="description-toggle-button">
-    <UiIcon v-if="!isCollapsedRef" :icon="faAngleDown" class="description-toggle-button__icon" />
-    <UiIcon v-else :icon="faAngleUp" class="description-toggle-button__icon" />
+    <UiIcon :icon="collapsed ? faAngleUp : faAngleDown" class="description-toggle-button__icon" />
   </button>
 </template>
+
 <script lang="ts" setup>
 import UiIcon from '@core/components/icon/UiIcon.vue'
 import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons'
-import { computed } from 'vue'
 
-const props = defineProps<{
-  isCollapsed: boolean
+defineProps<{
+  collapsed: boolean
 }>()
-
-const isCollapsedRef = computed(() => props.isCollapsed)
 </script>
 <style lang="postcss" scoped>
 .description-toggle-button {
