80e16b7fd93809e17f37e2afb6928fcfa93b4122
Paloma Tejeda <palomatejeda81@gmail.com>
2024-05-14 13:16:57 +0200

    add props structure

--- @xen-orchestra/web-core/lib/components/DonutChart.vue
+++ @xen-orchestra/web-core/lib/components/DonutChart.vue
@@ -1,12 +1,33 @@
 <template>
   <div class="donut-chart">
-    <div id="porcion1" class="recorte"><div class="quesito ios" data-rel="21"></div></div>
-    <div id="porcion2" class="recorte"><div class="quesito mac" data-rel="39"></div></div>
-    <div id="porcion3" class="recorte"><div class="quesito win" data-rel="31"></div></div>
-    <div id="porcionFin" class="recorte"><div class="quesito linux" data-rel="9"></div></div>
+    <div id="segment1" class="cut"><div class="colored-part"></div></div>
+    <div id="segment2" class="cut"><div class="colored-part"></div></div>
+    <div id="segment3" class="cut"><div class="colored-part"></div></div>
+    <div id="segmentFin" class="cut"><div class="colored-part"></div></div>
     <span class="donut-hole"></span>
   </div>
 </template>
+<script setup lang="ts">
+type chartFigure = {
+  value: number
+  color: string
+  legend: string
+}
+
+interface Props {
+  chartFigures: chartFigure[]
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  chartFigures: () => [
+    {
+      value: 100,
+      color: 'unknown',
+      legend: 'Unknown',
+    },
+  ],
+})
+</script>
 <style lang="postcss" scoped>
 .donut-chart {
   position: relative;
@@ -24,33 +45,15 @@
   margin: auto;
   border-radius: 50%;
 }
-p.center-date {
-  background: #394264;
-  position: absolute;
-  text-align: center;
-  font-size: 28px;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  width: 130px;
-  height: 130px;
-  margin: auto;
-  border-radius: 50%;
-  line-height: 35px;
-  padding: 15% 0 0;
-}
-.center-date span.scnd-font-color {
-  line-height: 0;
-}
-.recorte {
+
+.cut {
   border-radius: 50%;
   clip: rect(0px, 200px, 200px, 100px);
   height: 100%;
   position: absolute;
   width: 100%;
 }
-.quesito {
+.colored-part {
   border-radius: 50%;
   clip: rect(0px, 100px, 200px, 0px);
   height: 100%;
@@ -59,46 +62,33 @@ p.center-date {
   font-family: monospace;
   font-size: 1.5rem;
 }
-#porcion1 {
+#segment1 {
   transform: rotate(0deg);
 }
 
-#porcion1 .quesito {
+#segment1 .colored-part {
   background-color: #e64c65;
   transform: rotate(76deg);
 }
-#porcion2 {
+#segment2 {
   transform: rotate(76deg);
 }
-#porcion2 .quesito {
+#segment2 .colored-part {
   background-color: #11a8ab;
   transform: rotate(140deg);
 }
-#porcion3 {
+#segment3 {
   transform: rotate(215deg);
 }
-#porcion3 .quesito {
+#segment3 .colored-part {
   background-color: #4fc4f6;
   transform: rotate(113deg);
 }
-#porcionFin {
+#segmentFin {
   transform: rotate(-32deg);
 }
-#porcionFin .quesito {
+#segmentFin .colored-part {
   background-color: #fcb150;
   transform: rotate(32deg);
-}
-.nota-final {
-  clear: both;
-  color: #4fc4f6;
-  font-size: 1rem;
-  padding: 2rem 0;
-}
-.nota-final strong {
-  color: #e64c65;
-}
-.nota-final a {
-  color: #fcb150;
-  font-size: inherit;
 }
 </style>
