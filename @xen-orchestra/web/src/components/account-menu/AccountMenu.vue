111108041926af4436e996a16eef2139383eb121
Olivier Floch <olivier.floch@vates.fr>
2024-05-15 12:35:37 +0200

    feat(xo-6/account-menu): add menu items (links to doc and support)

--- @xen-orchestra/web/src/components/account-menu/AccountMenu.vue
+++ @xen-orchestra/web/src/components/account-menu/AccountMenu.vue
@@ -3,6 +3,26 @@
     <template #trigger="{ isOpen, open }">
       <AccountMenuTrigger :active="isOpen" @click="open($event)" />
     </template>
+    <MenuItem :icon="faBook">
+      <a
+        class="link typo p2-medium"
+        href="https://docs.xcp-ng.org?utm_campaign=xo6&utm_term=xcpdoc"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        {{ $t('documentation-name', { name: 'XCP-ng' }) }}
+      </a>
+    </MenuItem>
+    <MenuItem :icon="faHeadset">
+      <a
+        class="link typo p2-medium"
+        href="https://vates.tech/pricing-and-support?utm_campaign=xo6&utm_term=pricing"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        {{ $t('support-name', { name: 'XCP-ng' }) }}
+      </a>
+    </MenuItem>
     <MenuItem :icon="faArrowRightFromBracket" class="logout" @click="logout()">
       {{ $t('core.log-out') }}
     </MenuItem>
@@ -13,7 +33,7 @@
 import AccountMenuTrigger from '@/components/account-menu/AccountMenuTrigger.vue'
 import MenuItem from '@core/components/menu/MenuItem.vue'
 import MenuList from '@core/components/menu/MenuList.vue'
-import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+import { faArrowRightFromBracket, faBook, faHeadset } from '@fortawesome/free-solid-svg-icons'
 
 defineProps<{
   disabled?: boolean
@@ -24,6 +44,15 @@ const logout = () => window.location.assign('/signout')
 </script>
 
 <style lang="postcss" scoped>
+.link {
+  text-decoration: none;
+  color: var(--color-grey-100);
+  /* Make the link take the height of the MenuItem component */
+  padding-block: 1.15rem;
+  /* Make the link take the available width in the MenuItem component */
+  flex-grow: 1;
+}
+
 .logout {
   color: var(--color-red-base);
 }
