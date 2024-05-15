e01503e0227e0a18a734b263881197147889141d
Paloma Tejeda <palomatejeda81@gmail.com>
2024-05-15 11:14:23 +0200

    donut chart now works with conic gradient, has legends, can have a title and an icon

--- @xen-orchestra/web-core/lib/components/DonutChart.vue
+++ @xen-orchestra/web-core/lib/components/DonutChart.vue
@@ -1,94 +1,162 @@
 <template>
-  <div class="donut-chart">
-    <div id="segment1" class="cut"><div class="colored-part"></div></div>
-    <div id="segment2" class="cut"><div class="colored-part"></div></div>
-    <div id="segment3" class="cut"><div class="colored-part"></div></div>
-    <div id="segmentFin" class="cut"><div class="colored-part"></div></div>
-    <span class="donut-hole"></span>
+  <div class="wrapper">
+    <div class="donut-chart" :style="gradientStyle">
+      <span class="donut-hole"><UiIcon v-if="icon" :icon="icon" class="chart-icon" /></span>
+    </div>
+
+    <ul class="chart-legends">
+      <h3 v-if="title" class="chart-title">{{ title }}</h3>
+      <li class="legend" v-for="legend in chartFigures">
+        <span class="legend-dot" :class="legend.color"></span><span class="legend-text">{{ legend.legend }}</span
+        ><span class="legend-value">{{ legend.value }}</span>
+      </li>
+    </ul>
   </div>
 </template>
+
 <script setup lang="ts">
-type chartFigure = {
+import { computed } from 'vue'
+import UiIcon from '@core/components/icon/UiIcon.vue'
+import type { IconDefinition } from '@fortawesome/fontawesome-common-types'
+
+type ChartFigure = {
   value: number
   color: string
   legend: string
 }
 
 interface Props {
-  chartFigures: chartFigure[]
+  chartFigures: ChartFigure[]
+  title?: string
+  icon?: IconDefinition
 }
 
 const props = withDefaults(defineProps<Props>(), {
   chartFigures: () => [
     {
-      value: 100,
+      value: 1,
       color: 'unknown',
       legend: 'Unknown',
     },
   ],
 })
+
+const totalValue = props.chartFigures.reduce((acc, chartFigure) => acc + chartFigure.value, 0)
+
+function convertColorToVar(color: string) {
+  switch (color) {
+    case 'info':
+      return 'var(--color-purple-base)'
+    case 'success':
+      return 'var(--color-green-base)'
+    case 'warning':
+      return 'var(--color-orange-base)'
+    case 'error':
+      return 'var(--color-red-base)'
+    case 'unknown':
+      return 'var(--background-color-purple-10)'
+    default:
+      return 'var(--background-color-primary)'
+  }
+}
+
+const gradientStyle = computed(() => {
+  let cumulativePercent = 0
+  let gradientStr = 'background: conic-gradient('
+  props.chartFigures.forEach((figure, index) => {
+    const nextPercent = (figure.value / totalValue) * 100
+    if (index > 0) {
+      gradientStr += `, ${convertColorToVar(figure.color)} ${cumulativePercent}% ${cumulativePercent + nextPercent}%`
+    } else {
+      gradientStr += `${convertColorToVar(figure.color)} 0% ${nextPercent}%`
+    }
+    cumulativePercent += nextPercent
+  })
+  gradientStr += ')'
+  return gradientStr
+})
 </script>
 <style lang="postcss" scoped>
-.donut-chart {
-  position: relative;
-  width: 200px;
-  height: 200px;
-  margin: 0 auto 2rem;
-  border-radius: 100%;
-}
-.donut-hole {
-  width: 70%;
-  height: 70%;
-  position: absolute;
-  background-color: var(--background-color-primary);
-  inset: 0;
-  margin: auto;
-  border-radius: 50%;
+.wrapper {
+  display: flex;
+  align-items: center;
+  gap: 2.4rem;
+
+  & .donut-chart {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    & .donut-hole {
+      width: 80%;
+      height: 80%;
+      background-color: var(--background-color-primary);
+      border-radius: 50%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      & .chart-icon {
+        font-size: 2.4rem;
+      }
+    }
+  }
+
+  & .chart-legends {
+    display: flex;
+    flex-direction: column;
+    gap: 0.8rem;
+
+    & .chart-title {
+      font-size: 1.3rem;
+      font-weight: 600;
+      color: var(--color-grey-200);
+    }
+
+    & .legend {
+      display: flex;
+      align-items: center;
+      gap: 0.8rem;
+
+      & .legend-dot {
+        width: 0.8rem;
+        height: 0.8rem;
+        min-width: 0.8rem;
+        border-radius: 50%;
+      }
+
+      & .legend-text {
+        font-size: 1.2rem;
+        font-weight: 400;
+        color: var(--color-grey-000);
+        width: 100%;
+      }
+
+      & .legend-value {
+        font-weight: 600;
+        font-size: 1.3rem;
+        color: var(--color-grey-300);
+      }
+    }
+  }
 }
 
-.cut {
-  border-radius: 50%;
-  clip: rect(0px, 200px, 200px, 100px);
-  height: 100%;
-  position: absolute;
-  width: 100%;
+.info {
+  background-color: var(--color-purple-base);
 }
-.colored-part {
-  border-radius: 50%;
-  clip: rect(0px, 100px, 200px, 0px);
-  height: 100%;
-  position: absolute;
-  width: 100%;
-  font-family: monospace;
-  font-size: 1.5rem;
+.success {
+  background-color: var(--color-green-base);
 }
-#segment1 {
-  transform: rotate(0deg);
+.warning {
+  background-color: var(--color-orange-base);
 }
-
-#segment1 .colored-part {
-  background-color: #e64c65;
-  transform: rotate(76deg);
+.error {
+  background-color: var(--color-red-base);
 }
-#segment2 {
-  transform: rotate(76deg);
-}
-#segment2 .colored-part {
-  background-color: #11a8ab;
-  transform: rotate(140deg);
-}
-#segment3 {
-  transform: rotate(215deg);
-}
-#segment3 .colored-part {
-  background-color: #4fc4f6;
-  transform: rotate(113deg);
-}
-#segmentFin {
-  transform: rotate(-32deg);
-}
-#segmentFin .colored-part {
-  background-color: #fcb150;
-  transform: rotate(32deg);
+.unknown {
+  background-color: var(--background-color-purple-10);
 }
 </style>
