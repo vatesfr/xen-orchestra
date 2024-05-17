543edf69aabf00419dafb3e6a444554b0e2772f2
Paloma Tejeda <palomatejeda81@gmail.com>
2024-05-17 11:32:11 +0200

    fix code, donut hole is transparent, chart is alone, removed unused package

--- @xen-orchestra/web-core/lib/components/DonutChart.vue
+++ @xen-orchestra/web-core/lib/components/DonutChart.vue
@@ -1,16 +1,29 @@
 <template>
+  <!-- v1.1 -->
   <div class="wrapper">
     <div class="donut-chart" :style="gradientStyle">
-      <span class="donut-hole"><UiIcon v-if="icon" :icon="icon" class="chart-icon" /></span>
+      <svg
+        id="donut-clip"
+        clipPathUnits="objectBoundingBox"
+        width="100"
+        height="100"
+        viewBox="0 0 100 100"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <defs>
+          <mask id="mask">
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M50 100C77.6143 100 100 77.6143 100 50C100 22.3857 77.6143 0 50 0C22.3857 0 0 22.3857 0 50C0 77.6143 22.3857 100 50 100ZM50.5 88C71.2104 88 88 71.2109 88 50.5C88 29.7891 71.2104 13 50.5 13C29.7896 13 13 29.7891 13 50.5C13 71.2109 29.7896 88 50.5 88Z"
+              fill="#D9D9D9"
+            />
+          </mask>
+        </defs>
+      </svg>
     </div>
-
-    <ul class="chart-legends">
-      <h3 v-if="title" class="chart-title">{{ title }}</h3>
-      <li class="legend" v-for="legend in chartFigures" :key="legend.legend">
-        <span class="legend-dot" :class="legend.color"></span><span class="legend-text">{{ legend.legend }}</span
-        ><span class="legend-value">{{ legend.value }}</span>
-      </li>
-    </ul>
+    <UiIcon :icon="icon" class="chart-icon" />
   </div>
 </template>
 
@@ -18,30 +31,27 @@
 import { computed } from 'vue'
 import UiIcon from '@core/components/icon/UiIcon.vue'
 import type { IconDefinition } from '@fortawesome/fontawesome-common-types'
+import type { Color } from '@core/types/color.type.ts'
 
 type ChartFigure = {
   value: number
-  color: string
-  legend: string
+  color: Color | 'unknown'
 }
 
 interface Props {
-  chartFigures: ChartFigure[]
-  title?: string
+  segments: ChartFigure[]
+  maxValue?: number
   icon?: IconDefinition
 }
 
-const props = withDefaults(defineProps<Props>(), {
-  chartFigures: () => [
-    {
-      value: 1,
-      color: 'unknown',
-      legend: 'Unknown',
-    },
-  ],
-})
+const props = defineProps<Props>()
 
-const totalValue = props.chartFigures.reduce((acc, chartFigure) => acc + chartFigure.value, 0)
+const totalValue = computed(() => {
+  const sumOfValues = props.segments.reduce((acc, segment) => acc + segment.value, 0)
+  return props.maxValue !== undefined && props.maxValue >= sumOfValues
+    ? props.maxValue
+    : props.segments.reduce((acc, segment) => acc + segment.value, 0)
+})
 
 function convertColorToVar(color: string) {
   switch (color) {
@@ -62,101 +72,33 @@ function convertColorToVar(color: string) {
 
 const gradientStyle = computed(() => {
   let cumulativePercent = 0
-  let gradientStr = 'background: conic-gradient('
-  props.chartFigures.forEach((figure, index) => {
-    const nextPercent = (figure.value / totalValue) * 100
-    if (index > 0) {
-      gradientStr += `, ${convertColorToVar(figure.color)} ${cumulativePercent}% ${cumulativePercent + nextPercent}%`
-    } else {
-      gradientStr += `${convertColorToVar(figure.color)} 0% ${nextPercent}%`
-    }
-    cumulativePercent += nextPercent
+  const parts = props.segments.map(figure => {
+    const currentPercent = cumulativePercent
+    cumulativePercent += (figure.value / totalValue.value) * 100
+    return `${convertColorToVar(figure.color)} ${currentPercent}% ${cumulativePercent}%`
   })
-  gradientStr += ')'
-  return gradientStr
+  return { background: `conic-gradient(${parts.join(', ')}` }
 })
 </script>
 <style lang="postcss" scoped>
 .wrapper {
-  display: flex;
-  align-items: center;
-  gap: 2.4rem;
+  position: relative;
+  width: fit-content;
+  border-radius: 50%;
+  overflow: hidden;
 
   & .donut-chart {
     width: 100px;
     height: 100px;
-    border-radius: 50%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    & .donut-hole {
-      width: 80%;
-      height: 80%;
-      background-color: var(--background-color-primary);
-      border-radius: 50%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-
-      & .chart-icon {
-        font-size: 2.4rem;
-      }
-    }
+    -webkit-mask-image: url(#mask);
+    mask-image: url(#mask);
   }
 
-  & .chart-legends {
-    display: flex;
-    flex-direction: column;
-    gap: 0.8rem;
-
-    & .chart-title {
-      font-size: 1.3rem;
-      font-weight: 600;
-      color: var(--color-grey-200);
-    }
-
-    & .legend {
-      display: flex;
-      align-items: center;
-      gap: 0.8rem;
-
-      & .legend-dot {
-        width: 0.8rem;
-        height: 0.8rem;
-        min-width: 0.8rem;
-        border-radius: 50%;
-      }
-
-      & .legend-text {
-        font-size: 1.2rem;
-        font-weight: 400;
-        color: var(--color-grey-000);
-        width: 100%;
-      }
-
-      & .legend-value {
-        font-weight: 600;
-        font-size: 1.3rem;
-        color: var(--color-grey-300);
-      }
-    }
+  & .chart-icon {
+    font-size: 2.4rem;
+    position: absolute;
+    inset: 0;
+    margin: auto;
   }
-}
-
-.info {
-  background-color: var(--color-purple-base);
-}
-.success {
-  background-color: var(--color-green-base);
-}
-.warning {
-  background-color: var(--color-orange-base);
-}
-.error {
-  background-color: var(--color-red-base);
-}
-.unknown {
-  background-color: var(--background-color-purple-10);
 }
 </style>
