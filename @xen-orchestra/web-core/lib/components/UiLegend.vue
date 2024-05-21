4e72d76e0bda98ef975ed175b3a586d1535adb0c
Paloma Tejeda <palomatejeda81@gmail.com>
2024-05-21 11:49:57 +0200

    update of code to match interface, now legend component is single and has an info icon

---
+++ @xen-orchestra/web-core/lib/components/UiLegend.vue
@@ -0,0 +1,71 @@
+<template>
+  <li class="legend">
+    <span class="ellipsis" :class="legend.color"></span><span class="label">{{ legend.label }}</span
+    ><UiIcon :icon="faCircleInfo" color="info" class="ui-icon" />
+
+    <span class="value">{{ legend.value }}</span>
+    <span class="unit">{{ legend.unit }}</span>
+  </li>
+</template>
+<script setup lang="ts">
+import type { Color } from '@core/types/color.type.ts'
+import UiIcon from '@core/components/icon/UiIcon.vue'
+import { faCircleInfo } from '@fortawesome/free-solid-svg-icons'
+
+defineProps<{
+  legend: {
+    value: number
+    color: Color | 'unknown'
+    label: string
+    unit?: string
+  }
+}>()
+</script>
+<style lang="postcss" scoped>
+.legend {
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
+
+  & .ellipsis {
+    width: 0.8rem;
+    height: 0.8rem;
+    min-width: 0.8rem;
+    border-radius: 50%;
+
+    &.info {
+      background-color: var(--color-purple-base);
+    }
+    &.success {
+      background-color: var(--color-green-base);
+    }
+    &.warning {
+      background-color: var(--color-orange-base);
+    }
+    &.error {
+      background-color: var(--color-red-base);
+    }
+    &.unknown {
+      background-color: var(--background-color-purple-10);
+    }
+  }
+
+  & .label {
+    font-size: 1.3rem;
+    font-weight: 400;
+    color: var(--color-grey-000);
+  }
+
+  & .ui-icon {
+    font-size: 1.2rem;
+    margin-right: auto;
+  }
+
+  & .value,
+  & .unit {
+    font-weight: 600;
+    font-size: 1.3rem;
+    color: var(--color-grey-300);
+  }
+}
+</style>
