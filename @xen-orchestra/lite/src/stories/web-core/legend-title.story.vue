e14f6f6b84547f548af492d15a24ae8fe2be1ff2
Paloma Tejeda <palomatejeda81@gmail.com>
2024-05-21 19:37:21 +0200

    fix component naming, classes for typo, creation of a story and possibility to show the icon

---
+++ @xen-orchestra/lite/src/stories/web-core/legend-title.story.vue
@@ -0,0 +1,14 @@
+<template>
+  <ComponentStory
+    v-slot="{ properties, settings }"
+    :params="[prop('title').type('string').required().widget(), prop('hasIcon').bool().widget().default(false)]"
+  >
+    <LegendTitle v-bind="properties">{{ settings.defaultSlot }}</LegendTitle>
+  </ComponentStory>
+</template>
+
+<script lang="ts" setup>
+import ComponentStory from '@/components/component-story/ComponentStory.vue'
+import { prop } from '@/libs/story/story-param'
+import LegendTitle from '@core/components/LegendTitle.vue'
+</script>
---
+++ @xen-orchestra/web-core/lib/components/LegendTitle.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- Legend title v1.0.1 -->
+  <span v-if="title" class="legend-title typo c3-semi-bold"
+    >{{ title }} <UiIcon :icon="hasIcon !== false ? faFaceSmile : undefined" color="info" class="ui-icon"
+  /></span>
+</template>
+<script setup lang="ts">
+import { faFaceSmile } from '@fortawesome/free-solid-svg-icons'
+import UiIcon from '@core/components/icon/UiIcon.vue'
+defineProps({
+  title: String,
+  hasIcon: {
+    type: Boolean,
+    default: false,
+  },
+})
+</script>
+<style lang="postcss" scoped>
+.legend-title {
+  color: var(--color-grey-200);
+  display: flex;
+  gap: 0.8rem;
+  align-items: center;
+
+  & .ui-icon {
+    font-size: 1.2rem;
+  }
+}
+</style>
