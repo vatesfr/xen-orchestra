d0ec3522554bf4a0912e3e9ab1a66fa295fb5c7a
Paloma <94314524+P4l0m4@users.noreply.github.com>
2024-05-22 11:08:23 +0200

    Update @xen-orchestra/web-core/lib/components/StatusPill.vue
    Co-authored-by: Thierry Goettelmann <thierry@byscripts.info>

--- @xen-orchestra/web-core/lib/components/StatusPill.vue
+++ @xen-orchestra/web-core/lib/components/StatusPill.vue
@@ -8,18 +8,20 @@ import { computed } from 'vue'
 import { faCheckCircle, faCircleMinus, faCircleXmark } from '@fortawesome/free-solid-svg-icons'
 import type { Color } from '@core/types/color.type'
 
-const props = defineProps<{
+type Props = {
   state: 'success' | 'partial' | 'failure'
-}>()
+}
 
-const states = {
+const props = defineProps<Props>()
+
+const states: Record<Props['state'], { icon: IconDefinition; color: Color }> = {
   success: { icon: faCheckCircle, color: 'success' },
   partial: { icon: faCircleMinus, color: 'warning' },
   failure: { icon: faCircleXmark, color: 'error' },
 }
 
 const icon = computed(() => states[props.state].icon)
-const color = computed(() => states[props.state].color as Color)
+const color = computed(() => states[props.state].color)
 </script>
 
 <style lang="postcss" scoped>
