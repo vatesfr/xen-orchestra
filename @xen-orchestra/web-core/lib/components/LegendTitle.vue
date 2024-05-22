f51513debbd3582ef34e057bd2757b7146d2ffc5
Paloma <94314524+P4l0m4@users.noreply.github.com>
2024-05-22 11:28:11 +0200

    Update @xen-orchestra/web-core/lib/components/LegendTitle.vue
    Co-authored-by: Thierry Goettelmann <thierry@byscripts.info>

--- @xen-orchestra/web-core/lib/components/LegendTitle.vue
+++ @xen-orchestra/web-core/lib/components/LegendTitle.vue
@@ -1,27 +1,29 @@
+<!-- v1.0 -->
 <template>
-  <!-- Legend title v1.0.1 -->
-  <span v-if="title" class="legend-title typo c3-semi-bold"
-    >{{ title }} <UiIcon :icon="hasIcon !== false ? faFaceSmile : undefined" color="info" class="ui-icon"
-  /></span>
+  <div class="legend-title typo c3-semi-bold">
+    {{ title }}
+    <UiIcon :icon color="info" />
+  </div>
 </template>
+
 <script setup lang="ts">
-import { faFaceSmile } from '@fortawesome/free-solid-svg-icons'
 import UiIcon from '@core/components/icon/UiIcon.vue'
-interface Props {
+
+defineProps<{
   title: string
-  hasIcon: boolean
-}
-defineProps<Props>()
+  icon?: IconDefinition
+}>()
 </script>
+
 <style lang="postcss" scoped>
 .legend-title {
   color: var(--color-grey-200);
   display: flex;
   gap: 0.8rem;
   align-items: center;
+}
 
-  & .ui-icon {
-    font-size: 1.2rem;
-  }
+.ui-icon {
+  font-size: 1.6rem;
 }
 </style>
