4c8d3f7ced85b91e861ce55fb2a4d6777e7f2907
Paloma Tejeda <palomatejeda81@gmail.com>
2024-05-22 10:21:42 +0200

    add component with story

---
+++ @xen-orchestra/lite/src/stories/web-core/card-numbers.story.vue
@@ -0,0 +1,31 @@
+<template>
+  <ComponentStory
+    v-slot="{ properties }"
+    :params="[
+      prop('label').type('string').default('Label').widget(),
+      prop('value').type('number').default(0).widget(),
+      prop('unit').type('string').default('GB').widget(),
+      prop('size').type('string').enum('small', 'medium').default('small').widget(),
+    ]"
+    :presets="presets"
+  >
+    <CardNumbers v-bind="properties" />
+  </ComponentStory>
+</template>
+
+<script lang="ts" setup>
+import ComponentStory from '@/components/component-story/ComponentStory.vue'
+import { prop } from '@/libs/story/story-param'
+import CardNumbers from '@core/components/CardNumbers.vue'
+
+const presets = {
+  'Default Values': {
+    props: {
+      label: 'Label',
+      value: 0,
+      unit: 'GB',
+      size: 'small',
+    },
+  },
+}
+</script>
---
+++ @xen-orchestra/web-core/lib/components/CardNumbers.vue
@@ -0,0 +1,54 @@
+<template>
+  <!-- v1.0.1 -->
+  <div class="card-numbers" :class="{ column: size === 'medium' }">
+    <span
+      class="label typo c2-semi-bold"
+      :class="{ 'c3-semi-bold': size === 'medium', 'grey-300': size === 'medium' }"
+      >{{ label }}</span
+    >
+    <div class="value typo c2-semi-bold" :class="{ 'h3-semi-bold': size === 'medium' }">
+      {{ value }}<span class="unit typo p2-medium">{{ unit }}</span>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+interface CardNumbersProps {
+  label: string
+  value: number
+  unit?: string
+  size?: 'small' | 'medium'
+}
+
+defineProps<CardNumbersProps>()
+</script>
+<style lang="postcss" scoped>
+.card-numbers {
+  display: flex;
+  gap: 0.8rem;
+  justify-content: space-between;
+
+  & .label {
+    color: var(--color-grey-100);
+  }
+
+  & .value {
+    display: flex;
+    gap: 0.2rem;
+    align-items: center;
+
+    & .value,
+    & .unit {
+      color: var(--color-grey-100);
+    }
+  }
+
+  & .grey-300 {
+    color: var(--color-grey-300);
+  }
+}
+
+.column {
+  flex-direction: column;
+  gap: 0.4rem;
+}
+</style>
