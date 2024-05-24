307628974a97d95b6bfa132a194eb0a59ec60093
Olivier Floch <olivier.floch@vates.fr>
2024-05-24 09:38:42 +0200

    fix(xo-6/tree-view): show halted VMs attached to a pool

--- @xen-orchestra/web/src/components/infra/InfraPoolItem.vue
+++ @xen-orchestra/web/src/components/infra/InfraPoolItem.vue
@@ -3,11 +3,13 @@
     <TreeItemLabel :icon="faCity" :route="`/pool/${pool.id}`">{{ pool.name_label }}</TreeItemLabel>
     <template #sublist>
       <InfraHostList :pool-id="pool.id" />
+      <InfraHostLessVmList :pool-id="pool.id" />
     </template>
   </TreeItem>
 </template>
 
 <script lang="ts" setup>
+import InfraHostLessVmList from '@/components/infra/InfraHostLessVmList.vue'
 import InfraHostList from '@/components/infra/InfraHostList.vue'
 import type { Pool } from '@/types/pool.type'
 import TreeItem from '@core/components/tree/TreeItem.vue'
