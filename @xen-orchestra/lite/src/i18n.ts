136ea23199abd8ad10392cbeeb403493ea0bfdc4
OlivierFL <66562640+OlivierFL@users.noreply.github.com>
2024-05-28 09:11:17 +0200

    feat(xo-lite): add german translation (#7686)
    See #7284

--- @xen-orchestra/lite/src/i18n.ts
+++ @xen-orchestra/lite/src/i18n.ts
@@ -17,6 +17,10 @@ export const locales: Locales = {
     code: 'fr',
     name: 'Français',
   },
+  de: {
+    code: 'de',
+    name: 'Deutsch',
+  },
 }
 
 export default createI18n({
@@ -67,6 +71,48 @@ export default createI18n({
       },
     },
     fr: {
+      date_short: {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+      },
+      date_medium: {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+      },
+      date_long: {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      },
+      datetime_short: {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+      },
+      datetime_medium: {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+      },
+      datetime_long: {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+      },
+      time: {
+        hour: '2-digit',
+        minute: '2-digit',
+      },
+    },
+    de: {
       date_short: {
         year: 'numeric',
         month: 'numeric',
