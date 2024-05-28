ff41bee0183b3cf00f4659574cc8021774d8cf10
Paloma Tejeda <palomatejeda81@gmail.com>
2024-05-28 16:07:42 +0200

    refactor code, adjusted color variables and class system, changed version number, added tooltip

--- @xen-orchestra/web-core/lib/components/UiLegend.vue
+++ @xen-orchestra/web-core/lib/components/UiLegend.vue
@@ -1,75 +1,97 @@
-<!-- v1.0.1 -->
+<!-- v1.0 -->
 <template>
   <li class="legend">
-    <span class="ellipsis" :class="legend.color"></span><span class="label typo p3-regular">{{ legend.label }}</span
-    ><UiIcon :icon="legend.info ? faCircleInfo : undefined" color="info" class="ui-icon" />
+    <span class="circle" :class="circleColorClass"></span><span class="label typo p3-regular">{{ label }}</span
+    ><UiIcon
+      v-tooltip="{
+        placement: 'top',
+        content: tooltip ? $t(tooltip) : '',
+      }"
+      :icon="tooltip ? faCircleInfo : undefined"
+      color="info"
+      class="ui-icon"
+    />
 
-    <span class="value typo c3-semi-bold">{{ legend.value }}</span>
-    <span class="unit typo c3-semi-bold">{{ legend.unit }}</span>
+    <span class="value-and-unit typo c3-semi-bold">{{ value }} {{ unit }}</span>
   </li>
 </template>
+
 <script setup lang="ts">
-import type { Color } from '@core/types/color.type.ts'
 import UiIcon from '@core/components/icon/UiIcon.vue'
 import { faCircleInfo } from '@fortawesome/free-solid-svg-icons'
+import { vTooltip } from '@core/directives/tooltip.directive'
+import { computed } from 'vue'
 
-defineProps<{
-  legend: {
-    value: number
-    color: Color | 'dark-blue' | 'disabled'
-    label: string
-    unit?: string
-    info?: boolean
-  }
+const props = defineProps<{
+  value?: number
+  state: 'success' | 'warning' | 'error' | 'disabled' | 'dark-blue' | 'default'
+  label: string
+  unit?: string
+  tooltip?: string
 }>()
+
+const circleColorClass = computed(() => {
+  switch (props.state) {
+    case 'success':
+      return 'success'
+    case 'warning':
+      return 'warning'
+    case 'error':
+      return 'error'
+    case 'disabled':
+      return 'disabled'
+    case 'dark-blue':
+      return 'dark-blue'
+    default:
+      return 'default'
+  }
+})
 </script>
+
 <style lang="postcss" scoped>
 .legend {
   display: flex;
   align-items: center;
   gap: 0.8rem;
+}
 
-  & .ellipsis {
-    width: 0.8rem;
-    height: 0.8rem;
-    min-width: 0.8rem;
-    border-radius: 50%;
+.circle {
+  width: 0.8rem;
+  height: 0.8rem;
+  min-width: 0.8rem;
+  border-radius: 50%;
+  background-color: var(--label-color);
 
-    &.default {
-      background-color: var(--color-purple-base);
-    }
-    &.success {
-      background-color: var(--color-green-base);
-    }
-    &.warning {
-      background-color: var(--color-orange-base);
-    }
-    &.error {
-      background-color: var(--color-red-base);
-    }
-    &.disabled {
-      background-color: var(--color-grey-300);
-    }
-    &.dark-blue {
-      background-color: var(--color-grey-100);
-    }
+  &.success {
+    --label-color: var(--color-green-base);
   }
-
-  & .label {
-    color: var(--color-grey-000);
+  &.warning {
+    --label-color: var(--color-orange-base);
   }
-
-  & .ui-icon {
-    font-size: 1.2rem;
+  &.error {
+    --label-color: var(--color-red-base);
   }
-
-  & .value,
-  & .unit {
-    color: var(--color-grey-300);
+  &.default {
+    --label-color: var(--color-purple-base);
   }
-
-  & .value {
-    margin-left: auto;
+  &.disabled {
+    --label-color: var(--color-grey-300);
   }
+  &.dark-blue {
+    --label-color: var(--color-grey-100);
+  }
+}
+
+.label {
+  color: var(--color-grey-000);
+}
+
+.ui-icon {
+  font-size: 1.2rem;
+}
+
+.value-and-unit {
+  color: var(--color-grey-300);
+  margin-left: auto;
 }
 </style>
