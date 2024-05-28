b0b55c75322d319d75364fbac103e35d4234af7b
Paloma Tejeda <palomatejeda81@gmail.com>
2024-05-28 10:37:43 +0200

    refactor code

--- @xen-orchestra/web-core/lib/components/CardNumbers.vue
+++ @xen-orchestra/web-core/lib/components/CardNumbers.vue
@@ -1,17 +1,19 @@
 <!-- v1.0 -->
 <template>
-  <div class="card-numbers" :class="{ column: size === 'medium' }">
+  <div class="card-numbers" :class="size">
     <span class="label typo" :class="labelFontClass">{{ label }}</span>
     <div class="values">
-      <div v-if="size === 'small' && max" class="value typo c2-semi-bold">{{ valueAsPercentage }}%</div>
+      <span v-if="size === 'small' && max" class="value typo c2-semi-bold">{{ valueAsPercentage }}%</span>
       <div class="value typo" :class="valueFontClass">
         {{ value }}<span class="unit typo" :class="unitFontClass">{{ unit }}</span>
       </div>
     </div>
   </div>
 </template>
+
 <script setup lang="ts">
 import { computed } from 'vue'
+
 interface CardNumbersProps {
   label: string
   value: number
@@ -20,61 +22,65 @@ interface CardNumbersProps {
   max?: number
 }
 
-const props = withDefaults(defineProps<CardNumbersProps>(), {
-  size: 'small',
-})
+const props = defineProps<CardNumbersProps>()
 
-const labelFontClass = computed(() => {
-  return [
-    props.size === 'medium' ? 'c3-semi-bold' : 'c2-semi-bold',
-    {
-      'grey-300': props.size === 'medium',
-    },
-  ]
-})
+const labelFontClass = computed(() => (props.size === 'medium' ? 'c3-semi-bold' : 'c2-semi-bold'))
 
 const valueFontClass = computed(() => (props.size === 'medium' ? 'h3-semi-bold' : 'c2-semi-bold'))
 
 const unitFontClass = computed(() => (props.size === 'medium' ? 'p2-medium' : 'c2-semi-bold'))
 
 const valueAsPercentage = computed(() => {
-  return props.max ? ((props.value / props.max) * 100).toFixed(1) : null
+  if (props.max === undefined) {
+    return
+  }
+
+  return ((props.value / props.max) * 100).toFixed(1)
 })
 </script>
+
 <style lang="postcss" scoped>
+/* COLOR VARIANTS */
+.card-numbers {
+  &.small {
+    --label-color: var(--color-grey-100);
+  }
+
+  &.medium {
+    --label-color: var(--color-grey-300);
+  }
+}
+
+/* IMPLEMENTATION */
 .card-numbers {
   display: flex;
   gap: 0.8rem;
   justify-content: space-between;
 
-  & .label {
-    color: var(--color-grey-100);
-  }
-
-  & .values {
-    display: flex;
+  &.medium {
     flex-direction: column;
-    gap: 0.8rem;
-
-    & .value {
-      display: flex;
-      gap: 0.2rem;
-      align-items: center;
-
-      & .value,
-      & .unit {
-        color: var(--color-grey-100);
-      }
-    }
-
-    & .grey-300 {
-      color: var(--color-grey-300);
-    }
+    gap: 0.4rem;
   }
 }
 
-.column {
+.label {
+  color: var(--label-color);
+}
+
+.values {
+  display: flex;
   flex-direction: column;
-  gap: 0.4rem;
+  gap: 0.8rem;
+}
+
+.value {
+  color: var(--color-grey-100);
+  display: flex;
+  gap: 0.2rem;
+  align-items: center;
+}
+
+.unit {
+  color: var(--color-grey-100);
 }
 </style>
