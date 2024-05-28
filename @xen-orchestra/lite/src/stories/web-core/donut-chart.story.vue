d6312d1c37d6ef6e102d2f2a694b4b8601cf4e0c
Paloma Tejeda <palomatejeda81@gmail.com>
2024-05-28 20:54:34 +0200

    refactor code, donut is now fully SVG based

---
+++ @xen-orchestra/lite/src/stories/web-core/donut-chart.story.vue
@@ -0,0 +1,25 @@
+<template>
+  <ComponentStory
+    v-slot="{ properties }"
+    :params="[
+      prop('segments').type('array').required().preset(segments).widget(),
+      iconProp(),
+      prop('maxValue').type('number').widget().help('maxValue must be greater than the sum of the segments'),
+    ]"
+  >
+    <DonutChart v-bind="properties" />
+  </ComponentStory>
+</template>
+
+<script lang="ts" setup>
+import ComponentStory from '@/components/component-story/ComponentStory.vue'
+import { prop, iconProp } from '@/libs/story/story-param'
+import DonutChart from '@core/components/DonutChart.vue'
+
+const segments = [
+  { value: 16, color: 'success' },
+  { value: 22, color: 'warning' },
+  { value: 35, color: 'error' },
+  { value: 12, color: 'unknown' },
+]
+</script>
--- @xen-orchestra/web-core/lib/components/DonutChart.vue
+++ @xen-orchestra/web-core/lib/components/DonutChart.vue
@@ -1,104 +1,102 @@
+<!-- v1.1 -->
 <template>
-  <!-- v1.1 -->
   <div class="wrapper">
-    <div class="donut-chart" :style="gradientStyle">
-      <svg
-        id="donut-clip"
-        clipPathUnits="objectBoundingBox"
-        width="100"
-        height="100"
-        viewBox="0 0 100 100"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <defs>
-          <mask id="mask">
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M50 100C77.6143 100 100 77.6143 100 50C100 22.3857 77.6143 0 50 0C22.3857 0 0 22.3857 0 50C0 77.6143 22.3857 100 50 100ZM50.5 88C71.2104 88 88 71.2109 88 50.5C88 29.7891 71.2104 13 50.5 13C29.7896 13 13 29.7891 13 50.5C13 71.2109 29.7896 88 50.5 88Z"
-              fill="#D9D9D9"
-            />
-          </mask>
-        </defs>
-      </svg>
-    </div>
-    <UiIcon :icon="icon" class="chart-icon" />
+    <svg class="donut-chart" viewBox="0 0 100 100">
+      <circle class="segment max-value-segment" cx="50" cy="50" r="40" />
+      <circle
+        v-for="(segment, index) in computedSegments"
+        :key="index"
+        :class="segment.color"
+        :stroke-dasharray="`${segment.percent} ${circumference - segment.percent}`"
+        :stroke-dashoffset="segment.offset"
+        class="segment"
+        cx="50"
+        cy="50"
+        r="40"
+      />
+      <UiIcon :icon height="24" width="24" x="38" y="38" />
+    </svg>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
 import UiIcon from '@core/components/icon/UiIcon.vue'
 import type { IconDefinition } from '@fortawesome/fontawesome-common-types'
-import type { Color } from '@core/types/color.type.ts'
+import { computed } from 'vue'
 
 type ChartFigure = {
   value: number
-  color: Color | 'unknown'
+  color: 'success' | 'warning' | 'error' | 'unknown'
 }
 
-interface Props {
+type Segment = {
+  color: 'success' | 'warning' | 'error' | 'unknown'
+  percent: number
+  offset: number
+}
+
+const props = defineProps<{
   segments: ChartFigure[]
   maxValue?: number
   icon?: IconDefinition
-}
+}>()
 
-const props = defineProps<Props>()
+const circumference = Math.PI * 80
 
 const totalValue = computed(() => {
   const sumOfValues = props.segments.reduce((acc, segment) => acc + segment.value, 0)
-  return props.maxValue !== undefined && props.maxValue >= sumOfValues
-    ? props.maxValue
-    : props.segments.reduce((acc, segment) => acc + segment.value, 0)
+  return props.maxValue !== undefined && props.maxValue >= sumOfValues ? props.maxValue : sumOfValues
 })
 
-function convertColorToVar(color: string) {
-  switch (color) {
-    case 'info':
-      return 'var(--color-purple-base)'
-    case 'success':
-      return 'var(--color-green-base)'
-    case 'warning':
-      return 'var(--color-orange-base)'
-    case 'error':
-      return 'var(--color-red-base)'
-    case 'unknown':
-      return 'var(--background-color-purple-10)'
-    default:
-      return 'var(--background-color-primary)'
-  }
-}
-
-const gradientStyle = computed(() => {
-  let cumulativePercent = 0
-  const parts = props.segments.map(figure => {
-    const currentPercent = cumulativePercent
-    cumulativePercent += (figure.value / totalValue.value) * 100
-    return `${convertColorToVar(figure.color)} ${currentPercent}% ${cumulativePercent}%`
+const computedSegments = computed<Segment[]>(() => {
+  let nextOffset = circumference / 4
+  return props.segments.map(segment => {
+    const percent = (segment.value / totalValue.value) * circumference
+    const currentSegment = {
+      color: segment.color,
+      percent,
+      offset: nextOffset,
+    }
+    nextOffset -= percent
+    return currentSegment
   })
-  return { background: `conic-gradient(${parts.join(', ')}` }
 })
 </script>
-<style lang="postcss" scoped>
+
+<style scoped lang="postcss">
 .wrapper {
   position: relative;
   width: fit-content;
-  border-radius: 50%;
-  overflow: hidden;
+}
 
-  & .donut-chart {
-    width: 100px;
-    height: 100px;
-    -webkit-mask-image: url(#mask);
-    mask-image: url(#mask);
+.donut-chart {
+  width: 100px;
+  height: 100px;
+}
+
+.segment {
+  stroke: var(--stroke-color);
+  stroke-width: 10;
+  fill: transparent;
+
+  &.max-value-segment {
+    --stroke-color: var(--color-grey-100);
   }
 
-  & .chart-icon {
-    font-size: 2.4rem;
-    position: absolute;
-    inset: 0;
-    margin: auto;
+  &.success {
+    --stroke-color: var(--color-green-base);
+  }
+
+  &.warning {
+    --stroke-color: var(--color-orange-base);
+  }
+
+  &.error {
+    --stroke-color: var(--color-red-base);
+  }
+
+  &.unknown {
+    --stroke-color: var(--color-grey-400);
   }
 }
 </style>
