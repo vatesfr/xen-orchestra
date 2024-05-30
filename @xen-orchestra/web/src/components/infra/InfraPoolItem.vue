b98d0ce607b2b4d273ba558bf291431d4349aa76
Olivier Floch <olivier.floch@vates.fr>
2024-05-30 09:06:49 +0200

    feedback

--- @xen-orchestra/web/src/components/infra/InfraPoolItem.vue
+++ @xen-orchestra/web/src/components/infra/InfraPoolItem.vue
@@ -3,14 +3,14 @@
     <TreeItemLabel :icon="faCity" :route="`/pool/${pool.id}`">{{ pool.name_label }}</TreeItemLabel>
     <template #sublist>
       <InfraHostList :pool-id="pool.id" />
-      <InfraHostLessVmList :pool-id="pool.id" />
+      <InfraVmList :container-id="pool.id" for="pool" />
     </template>
   </TreeItem>
 </template>
 
 <script lang="ts" setup>
-import InfraHostLessVmList from '@/components/infra/InfraHostLessVmList.vue'
 import InfraHostList from '@/components/infra/InfraHostList.vue'
+import InfraVmList from '@/components/infra/InfraVmList.vue'
 import type { Pool } from '@/types/pool.type'
 import TreeItem from '@core/components/tree/TreeItem.vue'
 import TreeItemLabel from '@core/components/tree/TreeItemLabel.vue'
