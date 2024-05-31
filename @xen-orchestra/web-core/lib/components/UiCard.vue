51c5d158c37eae96ac55aa4a1818610228f482d3
Paloma Tejeda <palomatejeda81@gmail.com>
2024-05-31 16:50:13 +0200

    add card ui component

---
+++ @xen-orchestra/web-core/lib/components/UiCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="ui-card" :style="{ flexDirection: direction, padding: padding ? '2.4rem' : 0 }">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+type Props = {
+  direction: 'column' | 'row'
+  padding: boolean
+}
+
+defineProps<Props>()
+</script>
+
+<style lang="postcss" scoped>
+.ui-card {
+  display: flex;
+  border-radius: 0.8rem;
+  box-shadow: var(--shadow-200);
+  background-color: var(--background-color-primary);
+  border: 1px solid var(--color-grey-500);
+  gap: 2.4rem;
+  width: 100%;
+  height: 100%;
+}
+</style>
