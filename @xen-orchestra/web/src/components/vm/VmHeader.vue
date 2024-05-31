3dd5a6ba13df8c8e3c02eb139d322e2275048491
OlivierFL <66562640+OlivierFL@users.noreply.github.com>
2024-05-31 14:23:24 +0200

    feat(xo-6/vm): add console view (#7693)

---
+++ @xen-orchestra/web/src/components/vm/VmHeader.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="vm-header">
+    <ObjectIcon :state size="medium" type="vm" />
+    <h4 v-tooltip class="typo h4-medium title">{{ name }}</h4>
+  </div>
+  <TabList>
+    <TabItem disabled>Dashboard</TabItem>
+    <TabItem active>Console</TabItem>
+    <TabItem disabled>Alarms</TabItem>
+    <TabItem disabled>Stats</TabItem>
+    <TabItem disabled>System</TabItem>
+    <TabItem disabled>Network</TabItem>
+    <TabItem disabled>Storage</TabItem>
+    <TabItem disabled>Tasks</TabItem>
+  </TabList>
+</template>
+
+<script lang="ts" setup>
+import ObjectIcon from '@core/components/icon/ObjectIcon.vue'
+import TabItem from '@core/components/tab/TabItem.vue'
+import TabList from '@core/components/tab/TabList.vue'
+import type { VmState } from '@core/types/object-icon.type'
+import { vTooltip } from '@core/directives/tooltip.directive'
+
+defineProps<{
+  name?: string
+  state?: VmState
+}>()
+</script>
+
+<style lang="postcss" scoped>
+.vm-header {
+  padding: 1.6rem;
+  display: flex;
+  gap: 1.6rem;
+  align-items: center;
+  border-bottom: 0.1rem solid var(--color-grey-500);
+  background-color: var(--background-color-primary);
+}
+
+.title {
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+</style>
--- @xen-orchestra/web/src/utils/rest-api-config.util.ts
+++ @xen-orchestra/web/src/utils/rest-api-config.util.ts
@@ -3,7 +3,7 @@ import type { XoObjectType } from '@/types/xo-object.type'
 export const restApiConfig: Record<XoObjectType, { path: string; fields: string }> = {
   VM: {
     path: 'vms',
-    fields: 'id,name_label,power_state,$container',
+    fields: 'id,name_label,power_state,$container,other',
   },
   host: {
     path: 'hosts',
