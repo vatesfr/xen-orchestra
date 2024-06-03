1bba37adb1d0dfa0a9233aeb35554b9c407abd23
Paloma <94314524+P4l0m4@users.noreply.github.com>
2024-06-03 09:34:43 +0200

    feat(xo-core): add card number ui component (#7666)

---
+++ @xen-orchestra/lite/src/stories/web-core/card-numbers.story.vue
@@ -0,0 +1,20 @@
+<template>
+  <ComponentStory
+    v-slot="{ properties }"
+    :params="[
+      prop('label').str().required().preset('Label').widget(),
+      prop('value').num().required().preset(0).widget(),
+      prop('size').str().required().enum('small', 'medium').preset('small').widget(),
+      prop('unit').str().preset('GB').widget(),
+      prop('max').num().widget(),
+    ]"
+  >
+    <CardNumbers v-bind="properties" />
+  </ComponentStory>
+</template>
+
+<script lang="ts" setup>
+import ComponentStory from '@/components/component-story/ComponentStory.vue'
+import { prop } from '@/libs/story/story-param'
+import CardNumbers from '@core/components/CardNumbers.vue'
+</script>
