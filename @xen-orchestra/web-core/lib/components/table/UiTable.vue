f6672a643f249b5e9256e134dfcace1e6c90536c
Mathieu <70369997+MathieuRA@users.noreply.github.com>
2024-06-03 15:53:47 +0200

    feat(XO6/core): creation of the interactive UiTable (#7677)

---
+++ @xen-orchestra/web-core/lib/components/table/UiTable.vue
@@ -0,0 +1,58 @@
+<template>
+  <table :class="{ 'vertical-border': verticalBorder }" class="ui-table typo p2-regular">
+    <slot />
+  </table>
+</template>
+
+<script lang="ts" setup>
+import { provide } from 'vue'
+
+const props = defineProps<{
+  name?: string
+  verticalBorder?: boolean
+}>()
+
+provide('tableName', props.name)
+</script>
+
+<style lang="postcss" scoped>
+.ui-table {
+  width: 100%;
+  border-spacing: 0;
+  background-color: var(--background-color-primary);
+  line-height: 2.4rem;
+  color: var(--color-grey-200);
+
+  :deep(th),
+  :deep(td) {
+    padding: 1rem;
+    border-top: 0.1rem solid var(--color-grey-500);
+    text-align: left;
+  }
+
+  :deep(th) {
+    font-weight: 700;
+  }
+
+  :deep(thead) {
+    th,
+    td {
+      color: var(--color-purple-base);
+      font-size: 1.4rem;
+      font-weight: 400;
+      text-transform: uppercase;
+    }
+  }
+
+  &.vertical-border {
+    :deep(th),
+    :deep(td) {
+      border-right: 0.1rem solid var(--color-grey-500);
+
+      &:last-child {
+        border-right: none;
+      }
+    }
+  }
+}
+</style>
