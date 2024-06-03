8c4fcc43d2892d581bd18758f19f122d49605b29
Paloma <94314524+P4l0m4@users.noreply.github.com>
2024-06-03 09:22:56 +0200

    feat(xo-core): add legend UI component (#7675)

---
+++ @xen-orchestra/lite/src/stories/web-core/ui-legend.story.vue
@@ -0,0 +1,27 @@
+<template>
+  <ComponentStory
+    v-slot="{ properties, settings }"
+    :params="[
+      prop('color')
+        .type('LegendColor')
+        .required()
+        .preset('default')
+        .enum('default', 'success', 'warning', 'error', 'disabled', 'dark-blue')
+        .widget(),
+      prop('value').num().widget().preset(4),
+      prop('unit').str().preset('GB').widget(),
+      prop('tooltip').str().widget(),
+      slot(),
+      setting('slot').preset('Legend label').widget(text()),
+    ]"
+  >
+    <UiLegend v-bind="properties">{{ settings.slot }}</UiLegend>
+  </ComponentStory>
+</template>
+
+<script lang="ts" setup>
+import ComponentStory from '@/components/component-story/ComponentStory.vue'
+import { prop, slot, setting } from '@/libs/story/story-param'
+import UiLegend from '@core/components/UiLegend.vue'
+import { text } from '@/libs/story/story-widget'
+</script>
---
+++ @xen-orchestra/web-core/lib/components/UiLegend.vue
@@ -0,0 +1,68 @@
+<!-- v1.0 -->
+<template>
+  <li :class="color" class="ui-legend">
+    <UiIcon :icon="faCircle" class="circle-icon" />
+    <span class="label typo p3-regular"><slot /></span>
+    <UiIcon v-if="tooltip" v-tooltip="tooltip" :icon="faCircleInfo" class="tooltip-icon" color="info" />
+
+    <span class="value-and-unit typo c3-semi-bold">{{ value }} {{ unit }}</span>
+  </li>
+</template>
+
+<script setup lang="ts">
+import UiIcon from '@core/components/icon/UiIcon.vue'
+import { faCircleInfo, faCircle } from '@fortawesome/free-solid-svg-icons'
+import { vTooltip } from '@core/directives/tooltip.directive'
+
+type LegendColor = 'default' | 'success' | 'warning' | 'error' | 'disabled' | 'dark-blue'
+
+defineProps<{
+  color: LegendColor
+  value?: number
+  unit?: string
+  tooltip?: string
+}>()
+</script>
+
+<style lang="postcss" scoped>
+/* COLOR VARIANTS */
+.ui-legend {
+  &.success {
+    --circle-color: var(--color-green-base);
+  }
+  &.warning {
+    --circle-color: var(--color-orange-base);
+  }
+  &.error {
+    --circle-color: var(--color-red-base);
+  }
+  &.default {
+    --circle-color: var(--color-purple-base);
+  }
+  &.disabled {
+    --circle-color: var(--color-grey-300);
+  }
+  &.dark-blue {
+    --circle-color: var(--color-grey-100);
+  }
+}
+/* IMPLEMENTATION */
+.ui-legend {
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
+}
+.circle-icon {
+  font-size: 0.8rem;
+  color: var(--circle-color);
+}
+.tooltip-icon {
+  font-size: 1.2rem;
+}
+.label {
+  color: var(--color-grey-000);
+}
+.value-and-unit {
+  color: var(--color-grey-300);
+}
+</style>
