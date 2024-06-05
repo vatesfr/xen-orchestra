1c28f47c913ea67aef481e6668f9848cd371918d
Olivier Floch <olivier.floch@vates.fr>
2024-06-05 08:46:59 +0200

    feat(xo-core): add Input component

---
+++ @xen-orchestra/web-core/lib/components/input/UiInput.vue
@@ -0,0 +1,119 @@
+<!-- WIP -->
+<template>
+  <div class="ui-input">
+    <UiIcon class="left-icon" :icon="leftIcon" />
+    <input :id v-model="model" type="search" :disabled v-bind="$attrs" class="typo p1-regular" />
+    <UiIcon v-if="!disabled && model" class="right-icon" :icon="faXmark" @click="model = undefined" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import UiIcon from '@core/components/icon/UiIcon.vue'
+import type { IconDefinition } from '@fortawesome/fontawesome-common-types'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
+
+defineProps<{
+  id: string
+  disabled?: boolean
+  leftIcon?: IconDefinition
+}>()
+
+const model = defineModel<string>()
+</script>
+
+<style lang="postcss" scoped>
+/* COLOR VARIANTS */
+input {
+  & {
+    --border-color: var(--color-grey-500);
+    --background-color: var(--background-color-primary);
+  }
+
+  &:is(:hover, .hover, :focus-visible) {
+    --border-color: var(--color-purple-d20);
+    --background-color: var(--background-color-primary);
+  }
+
+  &:is(:focus, .active) {
+    --border-color: var(--color-purple-base);
+    --background-color: var(--background-color-primary);
+  }
+
+  &:is(:active, .pressed) {
+    --border-color: var(--color-purple-d40);
+    --background-color: var(--background-color-primary);
+  }
+
+  &:is(:disabled, .disabled) {
+    --border-color: var(--color-grey-500);
+    --background-color: var(--background-color-secondary);
+  }
+}
+
+/* BORDER VARIANTS */
+input {
+  --border-width: 0.1rem;
+
+  &:is(:hover, .hover, :focus-visible) {
+    --border-width: 0.1rem;
+  }
+
+  &:is(:focus, .active) {
+    --border-width: 0.2rem;
+  }
+
+  &:is(:active, .pressed) {
+    --border-width: 0.1rem;
+  }
+
+  &:is(:disabled, .disabled) {
+    --border-width: 0.1rem;
+  }
+}
+
+/* IMPLEMENTATION */
+input {
+  border: var(--border-width) solid var(--border-color);
+  border-radius: 0.8rem;
+  outline: none;
+  width: 100%;
+  height: 4rem;
+  padding: 0.8rem 4.8rem;
+  color: var(--color-grey-000);
+  background-color: var(--background-color);
+
+  &:is(:disabled, .disabled) {
+    cursor: not-allowed;
+  }
+
+  &::placeholder {
+    color: var(--color-grey-300);
+  }
+
+  &[type='search']::-webkit-search-cancel-button {
+    display: none;
+  }
+}
+
+.ui-input {
+  position: relative;
+}
+
+.left-icon,
+.right-icon {
+  position: absolute;
+  inset-block: 1.2rem;
+  color: var(--color-grey-400);
+}
+
+.left-icon {
+  inset-inline-start: 1.6rem;
+  pointer-events: none;
+  z-index: 1;
+}
+
+.right-icon {
+  inset-inline-end: 1.6rem;
+  cursor: pointer;
+}
+</style>
