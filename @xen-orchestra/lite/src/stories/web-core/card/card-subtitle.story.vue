05c4975b4f98bf8ea6fd55e65e61632e367b9c87
Paloma Tejeda <palomatejeda81@gmail.com>
2024-06-05 17:40:15 +0200

    add card subtitle ui component for web core

---
+++ @xen-orchestra/lite/src/stories/web-core/card/card-subtitle.story.vue
@@ -0,0 +1,22 @@
+<template>
+  <ComponentStory
+    v-slot="{ settings }"
+    :params="[
+      slot(),
+      setting('defaultSlotContent').preset('Legend subtitle').widget(text()),
+      setting('rightSlotContent').preset('Info').widget(text()),
+    ]"
+  >
+    <CardSubtitle
+      ><template #left>{{ settings.defaultSlotContent }}</template>
+      <template #right>{{ settings.rightSlotContent }}</template>
+    </CardSubtitle>
+  </ComponentStory>
+</template>
+
+<script lang="ts" setup>
+import ComponentStory from '@/components/component-story/ComponentStory.vue'
+import { prop, slot, setting } from '@/libs/story/story-param'
+import { text } from '@/libs/story/story-widget'
+import CardSubtitle from '@core/components/card/CardSubtitle.vue'
+</script>
---
+++ @xen-orchestra/web-core/lib/components/card/CardSubtitle.vue
@@ -0,0 +1,16 @@
+<!-- v1.0 -->
+<template>
+  <div class="card-subtitle">
+    <span class="typo h7-semi-bold"><slot name="left" /></span>
+    <span class="typo p3-medium"><slot name="right" /></span>
+  </div>
+</template>
+<style lang="postcss" scoped>
+.card-subtitle {
+  color: var(--color-purple-base);
+  border-bottom: 1px solid var(--color-purple-base);
+  display: flex;
+  justify-content: space-between;
+  gap: 0.8rem;
+}
+</style>
