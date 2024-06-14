ffb23e34e40b4cf7472dbbd9962aa0baa0eebb32
P4l0m4 <palomatejeda81@gmail.com>
2024-06-14 10:30:06 +0200

    fix code to match comments

--- @xen-orchestra/lite/src/stories/web-core/card/card-title.story.vue
+++ @xen-orchestra/lite/src/stories/web-core/card/card-title.story.vue
@@ -6,27 +6,28 @@
       slot('info').help('Meant to receive an info text'),
       slot('link').help('Meant to receive a tertiary button'),
       slot('description').help('Meant to receive a description text, will be displayed under the title'),
-      setting('defaultSlotContent').preset('Legend title').widget(text()),
+      setting('defaultSlotContent').preset('Title').widget(text()),
       setting('infoSlotContent').preset('Info').widget(text()),
       setting('descriptionSlotContent').preset('Description').widget(text()),
     ]"
   >
     <CardTitle v-bind="properties"
-      >Title<UiCounter :value="13" color="danger" size="medium" />
+      >{{ settings.defaultSlotContent }}<UiCounter :value="3" color="danger" size="medium" />
       <template #info
-        ><UiButton level="tertiary" size="extra-small" :right-icon="faAngleRight">See all</UiButton></template
+        >{{ settings.infoSlotContent
+        }}<UiButton level="tertiary" size="extra-small" :right-icon="faAngleRight">See all</UiButton></template
       >
-      <template #description>{{ settings.descriptionSlotContent }} </template>
+      <template #description>{{ settings.descriptionSlotContent }}</template>
     </CardTitle>
   </ComponentStory>
 </template>
 
 <script lang="ts" setup>
 import ComponentStory from '@/components/component-story/ComponentStory.vue'
-import { prop, slot, setting } from '@/libs/story/story-param'
+import { slot, setting } from '@/libs/story/story-param'
 import { text } from '@/libs/story/story-widget'
 import CardTitle from '@core/components/card/CardTitle.vue'
 import UiButton from '@core/components/button/UiButton.vue'
-import UiIcon from '@core/components/icon/UiIcon.vue'
+import UiCounter from '@core/components/UiCounter.vue'
 import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
 </script>
