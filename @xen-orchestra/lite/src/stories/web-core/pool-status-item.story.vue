71972cfef96c807835e6a19f0192a7c802dd2ed3
P4l0m4 <palomatejeda81@gmail.com>
2024-06-21 15:37:50 +0200

    feat(xo-core): add pool status item component and story

---
+++ @xen-orchestra/lite/src/stories/web-core/pool-status-item.story.vue
@@ -0,0 +1,37 @@
+<template>
+  <ComponentStory
+    v-slot="{ properties }"
+    :params="[
+      iconProp(),
+      prop('title').type('string').required().preset('Card title').widget(),
+      prop('titleLink').type('string').required().preset('/hello').widget(),
+      prop('titleLinkLabel').type('string').widget(),
+      prop('segments').type('array').required().preset(segments).widget(),
+      prop('legends').type('array').required().preset(legends).widget(),
+      prop('totalLabel').type('string').widget(),
+      prop('totalValue').type('number').required().preset(80).widget(),
+    ]"
+  >
+    <PoolStatusItem v-bind="properties" />
+  </ComponentStory>
+</template>
+
+<script lang="ts" setup>
+import ComponentStory from '@/components/component-story/ComponentStory.vue'
+import { prop, iconProp } from '@/libs/story/story-param'
+import PoolStatusItem from '@core/components/PoolStatusItem.vue'
+
+const segments = [
+  { value: 16, color: 'success' },
+  { value: 22, color: 'warning' },
+  { value: 35, color: 'error' },
+  { value: 12, color: 'unknown' },
+]
+
+const legends = [
+  { color: 'success', value: 16, unit: 'GB', label: 'Label' },
+  { color: 'warning', value: 22, unit: 'GB', label: 'Label' },
+  { color: 'error', value: 35, unit: 'GB', label: 'Label' },
+  { color: 'unknown', value: 12, unit: 'GB', tooltip: 'Tooltip for unknown', label: 'Label' },
+]
+</script>
---
+++ @xen-orchestra/web-core/lib/components/PoolStatusItem.vue
@@ -0,0 +1,103 @@
+<template>
+  <UiCard>
+    <CardTitle>
+      {{ title }}
+      <template #info>
+        <RouterLink :to="titleLink">
+          <UiButton level="tertiary" size="extra-small" :right-icon="faAngleRight">{{ titleLinkLabel }}</UiButton>
+        </RouterLink>
+      </template>
+    </CardTitle>
+    <div class="chart-and-legends">
+      <DonutChart :segments :icon />
+      <ul class="legends">
+        <UiLegend
+          v-for="(legend, index) in legends"
+          :key="index"
+          :color="legend.color"
+          :value="legend.value"
+          :unit="legend.unit"
+          :tooltip="legend.tooltip"
+        >
+          {{ legend.label }}
+        </UiLegend>
+      </ul>
+    </div>
+    <div class="total">
+      <span class="typo c2-semi-bold">{{ totalLabel }}</span
+      ><span class="typo h3-semi-bold">{{ totalValue }}</span>
+    </div>
+  </UiCard>
+</template>
+
+<script lang="ts" setup>
+import UiButton from '@core/components/button/UiButton.vue'
+import CardTitle from '@core/components/card/CardTitle.vue'
+import DonutChart from '@core/components/DonutChart.vue'
+import UiCard from '@core/components/UiCard.vue'
+import UiLegend from '@core/components/UiLegend.vue'
+import type { IconDefinition } from '@fortawesome/fontawesome-common-types'
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
+
+type LegendColor = 'default' | 'success' | 'warning' | 'error' | 'disabled' | 'dark-blue'
+
+type Segment = {
+  value: number
+  color: 'success' | 'warning' | 'error' | 'unknown'
+}
+
+type Legend = {
+  color: LegendColor
+  value?: number
+  unit?: string
+  tooltip?: string
+  label: string
+}
+
+// defineProps<{
+//   title: string
+//   titleLink: string
+//   titleLinkLabel: string
+//   segments: Segment[]
+//   totalValue: number
+//   totalLabel: string
+//   icon?: IconDefinition
+//   legends: Legend[]
+// }>()
+interface Props {
+  title: string
+  titleLink: string
+  titleLinkLabel?: string
+  segments: Segment[]
+  totalValue: number
+  totalLabel?: string
+  icon?: IconDefinition
+  legends: Legend[]
+}
+
+withDefaults(defineProps<Props>(), {
+  titleLinkLabel: 'See all',
+  totalLabel: 'Total',
+})
+</script>
+
+<style lang="postcss" scoped>
+.chart-and-legends {
+  display: flex;
+  align-items: center;
+  gap: 3.2rem;
+}
+
+.legends {
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+}
+
+.total {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1rem;
+}
+</style>
