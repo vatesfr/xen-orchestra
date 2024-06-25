ed6bf6a28f358b813945925ab4031a2120c11b68
P4l0m4 <palomatejeda81@gmail.com>
2024-06-25 14:59:53 +0200

    refactor story

--- @xen-orchestra/lite/src/stories/web-core/alarm-line.story.vue
+++ @xen-orchestra/lite/src/stories/web-core/alarm-line.story.vue
@@ -4,14 +4,18 @@
     :params="[
       prop('alarmType').type('string').preset('VM').required().widget(),
       prop('alarmLevel').type('number').preset(0.8).required().widget(),
-      prop('alarmTriggerLevel').type('number').preset(0.8).required().widget(),
-      prop('alarmCls').type('string').preset('on SR').required().widget(),
+      prop('alarmTrigger-level').type('number').preset(0.8).required().widget(),
       slot(),
       setting('defaultSlotContent').preset('Description content').widget(text()),
     ]"
   >
     <UiTable>
-      <AlarmLine v-bind="properties">
+      <AlarmLine
+        :alarm-level="properties.alarmLevel"
+        :alarm-type="properties.alarmType"
+        :alarm-trigger-level="properties.alarmTriggerLevel"
+        :alarm-cls="properties.alarmCls"
+      >
         {{ settings.defaultSlotContent }}
         <template #objectLink>
           <ObjectLink :type="rawTypeToType(alarm.cls)" :uuid="alarm.obj_uuid" />
@@ -26,7 +30,7 @@
   </ComponentStory>
 </template>
 
-<script lang="ts" setup>
+<script lang="ts" setup generic="T extends RawObjectType">
 import ComponentStory from '@/components/component-story/ComponentStory.vue'
 import UiTable from '@/components/ui/UiTable.vue'
 import AlarmLine from '@core/components/AlarmLine.vue'
@@ -37,11 +41,22 @@ import { text } from '@/libs/story/story-widget'
 import { vTooltip } from '@core/directives/tooltip.directive'
 import { parseDateTime } from '@/libs/utils'
 import { rawTypeToType } from '@/libs/xen-api/xen-api.utils'
-// import type { XenApiAlarm } from '@/types/xen-api'
+import type { RawObjectType, RecordUuid, RecordRef } from '@/libs/xen-api/xen-api.types'
+import type { XenApiAlarm } from '@/types/xen-api'
 
-const alarm = {
+const createRecordRef = <T,>(value: string): T => value as unknown as T
+
+const alarm: XenApiAlarm<RawObjectType> = {
+  $ref: createRecordRef<RecordRef<'message'>>('message-ref'),
+  uuid: createRecordRef<RecordUuid<'message'>>('message-uuid'),
   cls: 'VM',
-  obj_uuid: '1234',
+  obj_uuid: createRecordRef<RecordUuid<'vm'>>('1234'),
   timestamp: new Date().toISOString(),
+  level: 0.8,
+  triggerLevel: 0.8,
+  type: 'cpu_usage',
+  body: 'Description content',
+  name: 'Alarm name',
+  priority: 0,
 }
 </script>
