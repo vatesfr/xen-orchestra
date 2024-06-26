vatesfr/xen-orchestra

commit 800655a9a324c662115b0f25f7d1fbf1bde5fff4

fix(xo-server/rest-api): host logs are in tar+gzip
Fixes #7703

packages/xo-server/src/xo-mixins/rest-api.mjs
@@ -257,13 +257,13 @@ export default class RestApi {
           await pipeline(response.body, compressMaybe(req, res))
         },
 
-        async 'logs.tar'(req, res) {
+        async 'logs.tgz'(req, res) {
           const host = req.xapiObject
 
           const response = await host.$xapi.getResource('/host_logs_download', { host })
 
-          res.setHeader('content-type', 'application/x-tar')
-          await pipeline(response.body, compressMaybe(req, res))
+          res.setHeader('content-type', 'application/gzip')
+          await pipeline(response.body, (req, res))
         },
 
         async missing_patches(req, res) {

packages/xo-web/src/xo-app/host/tab-advanced.js
@@ -172,7 +172,7 @@ const downloadLogs = async uuid => {
     title: _('hostDownloadLogs'),
     body: _('hostDownloadLogsContainEntireHostLogs'),
   })
-  window.open(`./rest/v0/hosts/${uuid}/logs.tar`)
+  window.open(`./rest/v0/hosts/${uuid}/logs.tgz`)
 }
 
 const forceReboot = host => restartHost(host, true)

commit 93833062314a58831a6778b9dc5d961f6bc6070d

fix(xo-server/rest-api): remove incorrect comma operator
Introduced by 800655a9a

Fortunately this did not have any impact.

packages/xo-server/src/xo-mixins/rest-api.mjs
@@ -263,7 +263,7 @@ export default class RestApi {
           const response = await host.$xapi.getResource('/host_logs_download', { host })
 
           res.setHeader('content-type', 'application/gzip')
-          await pipeline(response.body, (req, res))
+          await pipeline(response.body, res)
         },
 
         async missing_patches(req, res) {