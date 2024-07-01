9d272da9ba887da545b8aa741755eebafc00875c
P4l0m4 <palomatejeda81@gmail.com>
2024-07-01 11:21:18 +0200

    feat(xo-web): add pool dashboard status component

---
+++ @xen-orchestra/web/src/components/pool/dashboard/PoolDashboardStatus.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- eslint-disable vue/html-self-closing -->
+  <div class="pool-dashboard-status"></div>
+  <!-- eslint-enable vue/html-self-closing -->
+</template>
+
+<style lang="postcss" scoped>
+.pool-dashboard-status {
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+
+  @media (--desktop) {
+    flex-direction: row;
+  }
+}
+</style>
--- @xen-orchestra/web/src/pages/index.vue
+++ @xen-orchestra/web/src/pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <!-- eslint-disable @intlify/vue-i18n/no-raw-text -->
-  Welcome to XO 6
-  <!-- eslint-enable @intlify/vue-i18n/no-raw-text -->
+  <PoolDashboardStatus />
 </template>
+
+<script lang="ts" setup>
+import PoolDashboardStatus from '@/components/pool/dashboard/PoolDashboardStatus.vue'
+</script>
