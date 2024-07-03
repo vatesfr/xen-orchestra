381fd20b2128d21ea96a252b94f4ebd45cb61333
P4l0m4 <palomatejeda81@gmail.com>
2024-07-03 10:10:40 +0200

    feat(xo-web): add host status to xo6 dashboard

---
+++ @xen-orchestra/web/src/components/pool/dashboard/HostStatus.vue
@@ -0,0 +1,62 @@
+<template v-if="isReady">
+  <UiCard>
+    <CardTitle>{{ $t('hosts-status') }}</CardTitle>
+    <DonutWithLegends :segments="segments" :icon />
+    <CardNumbers label="Total" :value="host.length" size="small" class="right" />
+  </UiCard>
+</template>
+
+<script lang="ts" setup>
+import DonutWithLegends from '@/components/DonutWithLegends.vue'
+import { useHostStore } from '@/stores/xo-rest-api/host.store'
+import type { Host } from '@/types/host.type'
+import type { LegendColor } from '@core/types/ui-legend.type'
+import CardTitle from '@core/components/card/CardTitle.vue'
+import CardNumbers from '@core/components/CardNumbers.vue'
+import UiCard from '@core/components/UiCard.vue'
+import { faServer } from '@fortawesome/free-solid-svg-icons'
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+const { records: host, isReady } = useHostStore().subscribe()
+
+const runningHostsCount = computed(() => {
+  if (!isReady.value) return 0
+  return host.value.filter((host: Host) => host.power_state === 'Running').length
+})
+const inactiveHostsCount = computed(() => {
+  if (!isReady.value) return 0
+  return host.value.filter((host: Host) => host.power_state === 'Halted').length
+})
+const unknownHostsCount = computed(() => {
+  if (!isReady.value) return 0
+  return host.value.filter((host: Host) => host.power_state !== 'Running' && host.power_state !== 'Halted').length
+})
+const segments = computed(() => [
+  {
+    label: t('hosts-running-status'),
+    value: runningHostsCount.value,
+    color: 'success' as LegendColor,
+  },
+  {
+    label: t('hosts-halted-status'),
+    value: inactiveHostsCount.value,
+    color: 'dark-blue' as LegendColor,
+    tooltip: t('hosts-halted-status-tooltip'),
+  },
+  {
+    label: t('hosts-unknown-status'),
+    value: unknownHostsCount.value,
+    color: 'disabled' as LegendColor,
+    tooltip: t('hosts-unknown-status-tooltip'),
+  },
+])
+const icon = faServer
+</script>
+
+<style lang="postcss" scoped>
+.right {
+  margin-left: auto;
+}
+</style>
