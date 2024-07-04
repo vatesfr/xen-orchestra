b135c6fa70c3647990ad553be738d08dcea1456b
Florent Beauchamp <florent.beauchamp@vates.tech>
2024-07-04 06:55:43 +0000

    fix(backups): Cannot destructure property 'other_config' of 'undefined'
    introduced by a85f44d699a3e581fe654cfd1118a28fd85e9945

--- @xen-orchestra/backups/_runners/_writers/IncrementalXapiWriter.mjs
+++ @xen-orchestra/backups/_runners/_writers/IncrementalXapiWriter.mjs
@@ -20,6 +20,7 @@ export class IncrementalXapiWriter extends MixinXapiWriter(AbstractIncrementalWr
     // @todo : this seems similare to decorateVmMetadata
 
     const replicatedVdis = sr.$VDIs
+      .filter(_ => !!_)
       .filter(({ other_config }) => {
         // REPLICATED_TO_SR_UUID is not used here since we are already filtering from sr.$VDIs
         return baseUuidToSrcVdi.has(other_config?.[COPY_OF])
@@ -103,10 +104,12 @@ export class IncrementalXapiWriter extends MixinXapiWriter(AbstractIncrementalWr
       .filter(_ => !!_)
     // @todo use index ?
 
-    const replicatedVdis = sr.$VDIs.filter(({ other_config }) => {
-      // REPLICATED_TO_SR_UUID is not used here since we are already filtering from sr.$VDIs
-      return sourceVdiUuids.includes(other_config?.[COPY_OF])
-    })
+    const replicatedVdis = sr.$VDIs
+      .filter(_ => !!_) // some VDIs are undefined
+      .filter(({ other_config }) => {
+        // REPLICATED_TO_SR_UUID is not used here since we are already filtering from sr.$VDIs
+        return sourceVdiUuids.includes(other_config?.[COPY_OF])
+      })
 
     Object.values(backup.vdis).forEach(vdi => {
       vdi.other_config[COPY_OF] = vdi.uuid
