8094940c9e17ae5d137bbe2ad7a6a71ccae64bc0
P4l0m4 <palomatejeda81@gmail.com>
2024-07-08 16:13:02 +0200

    feat(xo-web): add storage repository component to xo6 dashboard

---
+++ @xen-orchestra/web/src/components/pool/dashboard/StorageRepository.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="storage-repository">
+    <CardTitle>
+      <!-- {{ $t('storage-repository') }} -->
+      <!-- <template #description>{{ $t('for-replication') }}</template> -->
+    </CardTitle>
+    <!-- <StackedBarWithLegends :segments="segments" :max-value="50" /> -->
+    <div class="numbers">
+      <CardNumbers :value="41.5" unit="G.B" label="Used for backup" size="medium" />
+      <CardNumbers :value="16.5" unit="G.B" label="Available" size="medium" />
+      <CardNumbers :value="30" unit="G.B" label="Total" size="medium" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import CardTitle from '@core/components/card/CardTitle.vue'
+import CardNumbers from '@core/components/CardNumbers.vue'
+// import StackedBarWithLegends from '@core/components/stacked-bar/StackedBarWithLegends.vue'
+
+// const segments = [
+//   { label: 'XO Replications', value: 10.5, color: 'default', unit: 'G.B' },
+//   { label: 'Other', value: 6.5, color: 'warning', unit: 'G.B' },
+// ]
+</script>
+
+<style scoped lang="scss">
+.storage-repository {
+  display: flex;
+  flex-direction: column;
+  gap: 2.4rem;
+  width: 100%;
+}
+
+.numbers {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
