4a70b935b0fa1fc9d86742eec89aa303fe87ee50
P4l0m4 <palomatejeda81@gmail.com>
2024-07-08 16:09:02 +0200

    feat(xo-web): add dashboard storage to xo6

---
+++ @xen-orchestra/web/src/components/pool/dashboard/DashboardStorage.vue
@@ -0,0 +1,22 @@
+<template>
+  <UiCard :horizontal="isDesktop">
+    <!-- <BackupRepository /> -->
+    <UiSeparator />
+    <StorageRepository />
+    <UiSeparator />
+    <S3BackupRepository />
+  </UiCard>
+</template>
+
+<script setup lang="ts">
+// import BackupRepository from '@/components/pool/dashboard/BackupRepository.vue'
+import S3BackupRepository from '@/components/pool/dashboard/S3BackupRepository.vue'
+import StorageRepository from '@/components/pool/dashboard/StorageRepository.vue'
+import UiCard from '@core/components/UiCard.vue'
+import UiSeparator from '@core/components/UiSeparator.vue'
+import { useUiStore } from '@core/stores/ui.store'
+import { storeToRefs } from 'pinia'
+
+const uiStore = useUiStore()
+const { isDesktop } = storeToRefs(uiStore)
+</script>
--- @xen-orchestra/web/src/pages/index.vue
+++ @xen-orchestra/web/src/pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <!-- eslint-disable @intlify/vue-i18n/no-raw-text -->
-  Welcome to XO 6
-  <!-- eslint-enable @intlify/vue-i18n/no-raw-text -->
+  <DashboardStorage />
 </template>
+
+<script setup lang="ts">
+import DashboardStorage from '@/components/pool/dashboard/DashboardStorage.vue'
+</script>
