474ede53736f0fb84bb14f704f89309692c4e6a6
P4l0m4 <palomatejeda81@gmail.com>
2024-07-11 15:18:17 +0200

    update

--- @xen-orchestra/web/src/components/pool/dashboard/DashboardPatches.vue
+++ @xen-orchestra/web/src/components/pool/dashboard/DashboardPatches.vue
@@ -40,13 +40,28 @@
     >
       <LegendTitle>{{ $t('hosts') }}</LegendTitle>
     </DonutWithLegends>
+
+    <pre>{{ data }}</pre>
   </UiCard>
 </template>
 
 <script setup lang="ts">
 import DonutWithLegends from '@/components/DonutWithLegends.vue'
+// import { useDashboardStore } from '@/stores/xo-rest-api/dashboard.store'
 import CardTitle from '@core/components/card/CardTitle.vue'
 import LegendTitle from '@core/components/LegendTitle.vue'
 import UiCard from '@core/components/UiCard.vue'
 import UiSeparator from '@core/components/UiSeparator.vue'
+import { useFetch } from '@vueuse/core'
+
+// const { records: dashboard, isReady } = useDashboardStore().subscribe()
+
+const { data } = useFetch<string>('/rest/v0/dashboard').get().json<{
+  nHosts: number
+  nPools: number
+  missingPatches?: {
+    nHostsWithMissingPatches: number
+    nPoolsWithMissingPatches: number
+  }
+}>()
 </script>
