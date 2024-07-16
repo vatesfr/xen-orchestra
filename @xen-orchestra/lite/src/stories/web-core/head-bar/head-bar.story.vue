d2507025a99af6f86a873d4b67da5329db7a3e71
OlivierFL <66562640+OlivierFL@users.noreply.github.com>
2024-07-16 10:43:45 +0200

    feat(xo-core): add HeadBar component (#7806)

---
+++ @xen-orchestra/lite/src/stories/web-core/head-bar/head-bar.story.vue
@@ -0,0 +1,44 @@
+<template>
+  <ComponentStory
+    v-slot="{ properties, settings }"
+    :params="[
+      iconProp('icon'),
+      slot().help('Meant to display the label of the object'),
+      slot('icon').help('Meant to receive an ObjectIcon or UiIcon component'),
+      slot('status').help('Meant to display the status of the object, e.g., migration progress for a VM'),
+      slot('actions').help('Meant to receive UiButton or ButtonIcon components that will trigger actions'),
+      setting('default').preset('Pool name').widget(),
+      setting('showDemoButtons').widget(boolean()),
+      setting('showStatusSlotDemoContent').widget(boolean()),
+    ]"
+  >
+    <HeadBar v-bind="properties">
+      {{ settings.default }}
+      <template v-if="settings.showStatusSlotDemoContent" #status>
+        <UiSpinner class="spinner" />
+        migrating... (34%)
+      </template>
+      <template v-if="settings.showDemoButtons" #actions>
+        <UiButton size="small" :left-icon="faPlus">New VM</UiButton>
+        <UiButton size="small" :left-icon="faPowerOff">Change state</UiButton>
+      </template>
+    </HeadBar>
+  </ComponentStory>
+</template>
+
+<script lang="ts" setup>
+import ComponentStory from '@/components/component-story/ComponentStory.vue'
+import { iconProp, setting, slot } from '@/libs/story/story-param'
+import { boolean } from '@/libs/story/story-widget'
+import UiButton from '@core/components/button/UiButton.vue'
+import HeadBar from '@core/components/head-bar/HeadBar.vue'
+import UiSpinner from '@core/components/UiSpinner.vue'
+import { faPlus, faPowerOff } from '@fortawesome/free-solid-svg-icons'
+</script>
+
+<style lang="scss" scoped>
+.spinner {
+  font-size: 2rem;
+  color: var(--color-purple-base);
+}
+</style>
