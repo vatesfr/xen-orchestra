bc1c2ae30a264a235238adb95fe8d071d939525b
Olivier Floch <olivier.floch@vates.fr>
2024-07-22 18:05:08 +0200

    feat(xo-6/host): add VMs table list view

---
+++ @xen-orchestra/web/src/components/host/HostHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <HeadBar>
+    {{ host.name_label }}
+    <template #icon>
+      <ObjectIcon type="host" :state="host.power_state.toLocaleLowerCase() as HostState" />
+    </template>
+  </HeadBar>
+  <TabList>
+    <TabItem disabled>{{ $t('dashboard') }}</TabItem>
+    <TabItem disabled>{{ $t('console') }}</TabItem>
+    <TabItem disabled>{{ $t('alarms') }}</TabItem>
+    <TabItem disabled>{{ $t('stats') }}</TabItem>
+    <TabItem disabled>{{ $t('system') }}</TabItem>
+    <TabItem disabled>{{ $t('network') }}</TabItem>
+    <TabItem disabled>{{ $t('storage') }}</TabItem>
+    <TabItem disabled>{{ $t('tasks') }}</TabItem>
+    <RouterLink v-slot="{ isActive, href }" :to="`/host/${host.id}/vms`" custom>
+      <TabItem :active="isActive" :href tag="a">
+        {{ $t('vms') }}
+      </TabItem>
+    </RouterLink>
+  </TabList>
+</template>
+
+<script lang="ts" setup>
+import type { Host } from '@/types/host.type'
+import type { HostState } from '@core/types/object-icon.type'
+import HeadBar from '@core/components/head-bar/HeadBar.vue'
+import ObjectIcon from '@core/components/icon/ObjectIcon.vue'
+import TabItem from '@core/components/tab/TabItem.vue'
+import TabList from '@core/components/tab/TabList.vue'
+
+defineProps<{
+  host: Host
+}>()
+</script>
--- @xen-orchestra/web/src/pages/host/[id].vue
+++ @xen-orchestra/web/src/pages/host/[id].vue
@@ -1,7 +1,24 @@
 <template>
-  <ComingSoonHero type="page" />
+  <LoadingHero v-if="!isReady" type="page" />
+  <ObjectNotFoundHero v-else-if="!host" :id="route.params.id" />
+  <RouterView v-else v-slot="{ Component }">
+    <HostHeader :host />
+    <component :is="Component" :host />
+  </RouterView>
 </template>
 
 <script setup lang="ts">
-import ComingSoonHero from '@core/components/state-hero/ComingSoonHero.vue'
+import HostHeader from '@/components/host/HostHeader.vue'
+import { useHostStore } from '@/stores/xo-rest-api/host.store'
+import type { RecordId } from '@/types/xo-object.type'
+import LoadingHero from '@core/components/state-hero/LoadingHero.vue'
+import ObjectNotFoundHero from '@core/components/state-hero/ObjectNotFoundHero.vue'
+import { computed } from 'vue'
+import { useRoute } from 'vue-router/auto'
+
+const route = useRoute<'/host/[id]'>()
+
+const { isReady, get } = useHostStore().subscribe()
+
+const host = computed(() => get(route.params.id as RecordId<'host'>))
 </script>
