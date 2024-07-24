36174a26190ba3a6ba3937ac97618331a6cd667c
Olivier Floch <olivier.floch@vates.fr>
2024-07-24 17:43:41 +0200

    feat(xo-6/vm): use HeadBar component instead of custom integration

--- @xen-orchestra/web/src/pages/vm/[id].vue
+++ @xen-orchestra/web/src/pages/vm/[id].vue
@@ -2,7 +2,7 @@
   <LoadingHero v-if="!isReady" type="page" />
   <ObjectNotFoundHero v-else-if="!vm" :id="route.params.id" />
   <RouterView v-else v-slot="{ Component }">
-    <VmHeader :name="vm.name_label" :state="powerState" />
+    <VmHeader :vm />
     <component :is="Component" :vm />
   </RouterView>
 </template>
@@ -10,8 +10,7 @@
 <script lang="ts" setup>
 import VmHeader from '@/components/vm/VmHeader.vue'
 import { useVmStore } from '@/stores/xo-rest-api/vm.store'
-import type { XoVm } from '@/types/xo/vm.type'
-import type { VmState } from '@core/types/object-icon.type'
+import type { RecordId } from '@/types/xo-object.type'
 import LoadingHero from '@core/components/state-hero/LoadingHero.vue'
 import ObjectNotFoundHero from '@core/components/state-hero/ObjectNotFoundHero.vue'
 import { computed } from 'vue'
@@ -21,7 +20,5 @@ const route = useRoute<'/vm/[id]'>()
 
 const { isReady, get: getVm } = useVmStore().subscribe()
 
-const vm = computed(() => getVm(route.params.id as XoVm['id']))
-
-const powerState = computed(() => vm.value?.power_state.toLocaleLowerCase() as VmState | undefined)
+const vm = computed(() => getVm(route.params.id as RecordId<'VM'>))
 </script>
