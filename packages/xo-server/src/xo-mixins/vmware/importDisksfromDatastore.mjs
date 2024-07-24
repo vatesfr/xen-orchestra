vatesfr/xen-orchestra

commit 24d584e9056379b03f8440935b24519e02dd94f1

fix(xo-server): dispose stream properly after v2v import (#7655)

packages/xo-server/src/xo-mixins/vmware/importDisksfromDatastore.mjs
@@ -4,8 +4,9 @@ import openDeltaVmdkasVhd from '@xen-orchestra/vmware-explorer/openDeltaVmdkAsVh
 import VhdEsxiRaw from '@xen-orchestra/vmware-explorer/VhdEsxiRaw.mjs'
 import { importVdi as importVdiThroughXva } from '@xen-orchestra/xva/importVdi.mjs'
 import { defer } from 'golike-defer'
+import { Disposable } from 'promise-toolbox'
 
-async function _importDiskChain(
+const _importDiskChain = Disposable.factory(async function* _importDiskChain(
   $defer,
   { esxi, dataStoreToHandlers, sr, vm, chainByNode, vdi, parentVhd, userdevice }
 ) {
@@ -19,7 +20,7 @@ async function _importDiskChain(
     const disk = chainByNode[diskIndex]
     const { fileName, path, datastore: datastoreName, isFull } = disk
     if (isFull) {
-      vhd = await VhdEsxiRaw.open(datastoreName, path + '/' + fileName, {
+      vhd = yield VhdEsxiRaw.open(datastoreName, path + '/' + fileName, {
         thin: false,
         esxi,
         dataStoreToHandlers,
@@ -28,7 +29,7 @@ async function _importDiskChain(
       if (parentVhd === undefined) {
         throw new Error(`Can't import delta of a running VM without its parent VHD`)
       }
-      vhd = await openDeltaVmdkasVhd(datastoreName, path + '/' + fileName, parentVhd, {
+      vhd = yield openDeltaVmdkasVhd(datastoreName, path + '/' + fileName, parentVhd, {
         lookMissingBlockInParent: isFullImport, // only look to missing block on full import
         esxi,
         dataStoreToHandlers,
@@ -66,7 +67,7 @@ async function _importDiskChain(
     await vdi.$importContent(stream, { format: VDI_FORMAT_VHD })
   }
   return { vdi, vhd }
-}
+})
 
 const importDiskChain = defer(_importDiskChain)
 

@xen-orchestra/vmware-explorer/VhdEsxiCowd.mjs
@@ -21,7 +21,7 @@ export default class VhdEsxiCowd extends VhdAbstract {
     const datastore = openDatastore(datastoreName, opts)
     const vhd = new VhdEsxiCowd(datastore, path, parentVhd, opts)
     await vhd.readHeaderAndFooter()
-    return vhd
+    return { value: vhd, dispose: () => {} }
   }
   constructor(datastore, path, parentVhd, { lookMissingBlockInParent = true } = {}) {
     super()

@xen-orchestra/vmware-explorer/VhdEsxiRaw.mjs
@@ -29,7 +29,10 @@ export default class VhdEsxiRaw extends VhdAbstract {
     const datastore = openDatastore(datastoreName, opts)
     const vhd = new VhdEsxiRaw(datastore, path, opts)
     await vhd.readHeaderAndFooter()
-    return vhd
+    return {
+      value: vhd,
+      dispose: () => vhd.dispose(),
+    }
   }
 
   get header() {
@@ -203,6 +206,9 @@ export default class VhdEsxiRaw extends VhdAbstract {
       stream.length = this.footer.currentSize
       return stream
     })
+  }
+  async dispose() {
+    await this.#stream?.destroy()
   }
 }
 

@xen-orchestra/vmware-explorer/VhdEsxiSeSparse.mjs
@@ -77,7 +77,10 @@ export default class VhdEsxiSeSparse extends VhdAbstract {
     const datastore = openDatastore(datastoreName, opts)
     const vhd = new VhdEsxiSeSparse(datastore, path, parentVhd, opts)
     await vhd.readHeaderAndFooter()
-    return vhd
+    return {
+      value: vhd,
+      dispose: () => vhd.dispose(),
+    }
   }
 
   get path() {
@@ -133,8 +136,7 @@ export default class VhdEsxiSeSparse extends VhdAbstract {
       // no stream
       if (this.#stream === undefined) {
         const end = this.footer.currentSize
-        const res = await this.#datastore.getStream(this.#path, start, end)
-        this.#stream = res.body
+        this.#stream = await this.#datastore.getStream(this.#path, start, end)
         this.#streamOffset = start
       }
 
@@ -251,10 +253,10 @@ export default class VhdEsxiSeSparse extends VhdAbstract {
   async readBlock(blockId) {
     let changed = false
     const grainOffsets = this.#grainIndex.get(blockId) // may be undefined if the child contains block and lookMissingBlockInParent=true
-
     // negative value indicate that it's not an offset
     // SE_SPARSE_GRAIN_NON_ALLOCATED means we have to look into the parent data
-    const isLocallyFull = !grainOffsets.some(value => value === -SE_SPARSE_GRAIN_NON_ALLOCATED)
+    const isLocallyFull =
+      grainOffsets !== undefined && !grainOffsets.some(value => value === -SE_SPARSE_GRAIN_NON_ALLOCATED)
 
     let parentBuffer, parentBlock
     // don't read from parent is current block is already completly described
@@ -302,5 +304,9 @@ export default class VhdEsxiSeSparse extends VhdAbstract {
           buffer: parentBuffer,
         }
       : parentBlock
+  }
+
+  async dispose() {
+    await this.#stream?.destroy()
   }
 }

@xen-orchestra/vmware-explorer/openDeltaVmdkAsVhd.mjs
@@ -1,20 +1,15 @@
 import VHDEsxiSeSparse from './VhdEsxiSeSparse.mjs'
 import VhdEsxiCowd from './VhdEsxiCowd.mjs'
-import { openDatastore } from './_openDatastore.mjs'
 
 export default async function openDeltaVmdkasVhd(datastoreName, path, parentVhd, opts) {
-  let vhd
-  const datastore = openDatastore(datastoreName, opts)
+  let disposableVhd
   if (path.endsWith('-sesparse.vmdk')) {
-    vhd = new VHDEsxiSeSparse(datastore, path, parentVhd, opts)
+    disposableVhd = await VHDEsxiSeSparse.open(datastoreName, path, parentVhd, opts)
+  } else if (path.endsWith('-delta.vmdk')) {
+    disposableVhd = await VhdEsxiCowd.open(datastoreName, path, parentVhd, opts)
   } else {
-    if (path.endsWith('-delta.vmdk')) {
-      vhd = new VhdEsxiCowd(datastore, path, parentVhd, opts)
-    } else {
-      throw new Error(`Vmdk ${path} does not seems to be a delta vmdk.`)
-    }
+    throw new Error(`Vmdk ${path} does not seems to be a delta vmdk.`)
   }
-  await vhd.readHeaderAndFooter()
-  await vhd.readBlockAllocationTable()
-  return vhd
+  await disposableVhd.value.readBlockAllocationTable()
+  return disposableVhd
 }

commit bc1a3ae0682899e09ba0d31e59eadb69bac47f54

fix(v2v): warm migration failing
vhd are disposable and can't be used directly
introduced by 24d584e9056379b03f8440935b24519e02dd94f1

packages/xo-server/src/xo-mixins/vmware/importDisksfromDatastore.mjs
@@ -76,8 +76,9 @@ export const importDisksFromDatastore = async function importDisksFromDatastore(
     Object.keys(chainsByNodes).map(async (node, userdevice) =>
       Task.run({ properties: { name: `Cold import of disks ${node}` } }, async () => {
         const chainByNode = chainsByNodes[node]
-        const { vdi, vhd: parentVhd } = vhds[userdevice] ?? {}
-        return importDiskChain($defer, { esxi, dataStoreToHandlers, vm, chainByNode, userdevice, sr, parentVhd, vdi })
+        return Disposable.use(vhds[userdevice] ?? {}, ({ vdi, vhd: parentVhd }) =>
+          importDiskChain($defer, { esxi, dataStoreToHandlers, vm, chainByNode, userdevice, sr, parentVhd, vdi })
+        )
       })
     )
   )