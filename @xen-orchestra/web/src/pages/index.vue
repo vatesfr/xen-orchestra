2017ec26ad31137b17345611f22f7c33c46f1a9a
Paloma <94314524+P4l0m4@users.noreply.github.com>
2024-07-26 10:33:39 +0200

    feat(xo-web): add vms status for xo6 dashboard (#7795)

--- @xen-orchestra/web/src/pages/index.vue
+++ @xen-orchestra/web/src/pages/index.vue
@@ -1,5 +1,27 @@
 <template>
-  <!-- eslint-disable @intlify/vue-i18n/no-raw-text -->
-  Welcome to XO 6
-  <!-- eslint-enable @intlify/vue-i18n/no-raw-text -->
+  <div class="site-dashboard">
+    <VmsStatus class="vms-status" />
+  </div>
 </template>
+
+<script lang="ts" setup>
+import VmsStatus from '@/components/site/dashboard/VmsStatus.vue'
+</script>
+
+<style lang="postcss" scoped>
+.site-dashboard {
+  display: grid;
+  margin: 0.8rem;
+  gap: 0.8rem;
+  grid-template-columns: repeat(8, 1fr);
+  grid-template-areas:
+    'pools-status pools-status hosts-status hosts-status vms-status vms-status resources resources'
+    'backups backups backups backups backup-issues backup-issues backup-issues backup-issues'
+    'repositories repositories repositories repositories repositories repositories repositories repositories'
+    'alarms alarms alarms alarms alarms alarms alarms patches';
+}
+
+.vms-status {
+  grid-area: vms-status;
+}
+</style>
