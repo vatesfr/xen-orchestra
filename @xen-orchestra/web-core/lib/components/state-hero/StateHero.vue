974f9fe5213e485ed3cdedfe81e5ca46bf84f437
Thierry <thierry@byscripts.info>
2024-07-26 09:13:32 +0200

    feat(web-core): update heroes

--- @xen-orchestra/web-core/lib/components/state-hero/StateHero.vue
+++ @xen-orchestra/web-core/lib/components/state-hero/StateHero.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="state-hero">
+  <div :class="type" class="state-hero">
     <UiSpinner v-if="busy" class="spinner" />
     <img v-else-if="imageSrc" :src="imageSrc" alt="" class="image" />
-    <p v-if="$slots.default" class="typo h2-black text">
+    <p v-if="$slots.default" :class="typoClass" class="typo text">
       <slot />
     </p>
   </div>
@@ -12,10 +12,15 @@
 import UiSpinner from '@core/components/UiSpinner.vue'
 import { computed } from 'vue'
 
+export type StateHeroType = 'page' | 'card'
+
 const props = defineProps<{
+  type: StateHeroType
   busy?: boolean
   image?: 'no-result' | 'under-construction' // TODO: 'offline' | 'no-data' |  'not-found' | 'all-good' | 'all-done' | 'error'
 }>()
+
+const typoClass = computed(() => (props.type === 'page' ? 'h2-black' : 'h4-medium'))
 
 const imageSrc = computed(() => {
   try {
@@ -28,26 +33,39 @@ const imageSrc = computed(() => {
 
 <style lang="postcss" scoped>
 .state-hero {
+  &.page {
+    --image-width: 90%;
+    --spinner-size: 10rem;
+    --gap: 8.2rem;
+  }
+
+  &.card {
+    --image-width: 70%;
+    --spinner-size: 6rem;
+    --gap: 2rem;
+  }
+}
+
+.state-hero {
   flex: 1;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  gap: 4.2rem;
+  gap: var(--gap);
 }
 
 .image {
-  width: 90%;
+  width: var(--image-width);
   max-width: 55rem;
 }
 
 .spinner {
   color: var(--color-purple-base);
-  font-size: 10rem;
+  font-size: var(--spinner-size);
 }
 
 .text {
   color: var(--color-purple-base);
-  margin-top: 4rem;
 }
 </style>
