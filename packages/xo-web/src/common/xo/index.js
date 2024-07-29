fad659ec006fcd86f58d8a78aeaa324afd40db25
mathieuRA <contact@mathieu-raisin.fr>
2024-07-29 15:29:43 +0200

    remove unrelated changes

--- packages/xo-web/src/common/xo/index.js
+++ packages/xo-web/src/common/xo/index.js
@@ -2457,7 +2457,9 @@ export const migrateVdi = (vdi, sr, resourceSet) =>
     sr_id: resolveId(sr),
   })
 
-export const setCbt = (vdi, cbt) => _call('vdi.set', { id: resolveId(vdi), cbt })
+  export const setCbt = (vdi, cbt) =>
+    _call('vdi.set', { id: resolveId(vdi), cbt })
+  
 
 // VBD ---------------------------------------------------------------
 
