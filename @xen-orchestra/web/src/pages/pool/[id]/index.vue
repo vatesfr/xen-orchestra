124d96f31a1c2ff856469a2bbb424a2e416d0a9a
OlivierFL <66562640+OlivierFL@users.noreply.github.com>
2024-07-30 14:57:27 +0200

    fix(xo-6): add comments in host and pool pages to fix Vite build (#7878)
    The components `/host/[id]/index.vue` and `pool/[id]/index.vue` only use
    `definePage()` macro.
    The macro is removed during the build, leaving an empty script tag that causes
    the build to fail.
    This PR addresses this issue by adding a comment in the components to avoid
    empty content in `<script>` tags at build time.

--- @xen-orchestra/web/src/pages/pool/[id]/index.vue
+++ @xen-orchestra/web/src/pages/pool/[id]/index.vue
@@ -1,4 +1,7 @@
 <script lang="ts" setup>
+// [DO NOT REMOVE]
+// To fix a Vite error during the build process, this comment is required.
+// The `definePage` macro is removed during the build, leaving an empty script tag that causes the build to fail.
 definePage({
   name: '/pool/:id',
   redirect: (to: any) => `/pool/${to.params.id}/hosts`,
