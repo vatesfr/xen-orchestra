1c9b2ecb2d1b2d32cae3d638c37bf726360a9352
mathieuRA <contact@mathieu-raisin.fr>
2024-08-08 15:38:50 +0200

    explicit condition

--- packages/xo-server/src/utils.mjs
+++ packages/xo-server/src/utils.mjs
@@ -339,4 +339,4 @@ export const unboxIdsFromPattern = pattern => {
 
 // -------------------------------------------------------------------
 
-export const isSrWritable = sr => sr && sr.content_type !== 'iso' && sr.size > 0
+export const isSrWritable = sr => sr !== undefined && sr.content_type !== 'iso' && sr.size > 0
