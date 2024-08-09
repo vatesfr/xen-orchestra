700149dabe2b1fa9a6f8518f77ce911c0763308a
OlivierFL <66562640+OlivierFL@users.noreply.github.com>
2024-08-09 15:56:44 +0200

    chore(xo-core): update NoVnc typescript type (#7900)
    Use type definitions from DefinitelyTyped instead of using custom declaration

--- @xen-orchestra/lite/env.d.ts
+++ @xen-orchestra/lite/env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 /// <reference types="json-rpc-2.0/dist" />
 /// <reference types="vite-plugin-pages/client" />
+/// <reference types="novnc__novnc" />
 
 declare const XO_LITE_VERSION: string
 declare const XO_LITE_GIT_HEAD: string
