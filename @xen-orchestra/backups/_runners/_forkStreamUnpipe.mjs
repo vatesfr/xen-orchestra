79545b4fd9126e7507716d814180dc5ce33fc9c4
Florent Beauchamp <florent.beauchamp@vates.tech>
2024-08-13 15:10:33 +0000

    fix(backups/forkStreamUpipe): race condition
    when a source , forked to multiple destination was failing, it was triggering
    a lot of cleanup, some running in parallel on the same stream/fork
    
    This PR ensure each fork is handled only once

--- @xen-orchestra/backups/_runners/_forkStreamUnpipe.mjs
+++ @xen-orchestra/backups/_runners/_forkStreamUnpipe.mjs
@@ -8,31 +8,53 @@ const { debug } = createLogger('xo:backups:forkStreamUnpipe')
 // in case of error in the new readable stream, it will simply be unpiped
 // from the original one
 export function forkStreamUnpipe(source) {
-  const { forks = 0 } = source
+  const { forks = 0, id = Math.round(Math.random() * 1000) } = source
   source.forks = forks + 1
+  source.id = id
 
-  debug('forking', { forks: source.forks })
+  debug('forking', { forks: source.forks, id: source.id })
 
   const fork = new PassThrough()
+  fork.id = Math.round(Math.random() * 1000)
   source.pipe(fork)
+  let stillPiped = true
+
+  function unpipeFork() {
+    source.unpipe(fork)
+    --source.forks
+    stillPiped = false
+  }
+
   finished(source, { writable: false }, error => {
-    if (error !== undefined) {
-      debug('error on original stream, destroying fork', { error })
-      fork.destroy(error)
+    if (stillPiped) {
+      debug('finished source stream, destroying fork', { error, id: source.id, forkId: fork.id })
+      unpipeFork()
+      // from https://nodejs.org/api/stream.html
+      // By default, stream.end() is called on the destination Writable stream when the source Readable stream emits 'end', so that the destination is no longer writable.
+      // One important caveat is that if the Readable stream emits an error during processing, the Writable destination is not closed automatically. If an error occurs, it will be necessary to manually close each stream in order to prevent memory leaks.
+      if (error !== undefined) {
+        debug('source errored, destroy fork')
+        fork.destroy(error)
+      }
+    } else {
+      debug('finished source stream, already handled')
     }
   })
   finished(fork, { readable: false }, error => {
-    debug('end of stream, unpiping', { error, forks: --source.forks })
-
-    source.unpipe(fork)
-
-    if (source.forks === 0) {
-      debug('no more forks, destroying original stream')
-      source.destroy(new Error('no more consumers for this stream'))
+    if (stillPiped) {
+      debug('end of forked stream, unpiping', { error, forks: source.forks, id: source.id, forkId: fork.id })
+      unpipeFork()
+      if (source.forks === 0) {
+        debug('no more forks, destroy source')
+        // this will trigger the finished(source , ) code block an all other forks
+        source.destroy(error)
+      } else {
+        // a combination of stream.unpipe, onReadable and onData may stall stream here
+        // force it to flow again since we're piping it
+        source.resume()
+      }
     } else {
-      // a combination of stream.unpipe, onReadable and onData may stall stream here
-      // force it to flow again since we're piping it
-      source.resume()
+      debug('end of forked stream already handled')
     }
   })
   return fork
