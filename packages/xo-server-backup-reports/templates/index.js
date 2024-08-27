b4f90537915f74d2e8e0718b9c8c5651249c35ca
Julien Fontanet <julien.fontanet@isonoe.net>
2024-08-27 11:28:48 +0200

    chore(xo-server-backup-reports): unify templates compilation
    It will make adding new templates easier.

---
+++ packages/xo-server-backup-reports/templates/index.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const Handlebars = require('handlebars')
+const { join, parse } = require('node:path')
+const { readdirSync, statSync, readFileSync } = require('node:fs')
+
+const helpers = require('./_helpers.js')
+
+for (const name of readdirSync(__dirname)) {
+  // ignore `.`, `..` and hidden entries
+  if (name[0] === '.') {
+    continue
+  }
+
+  const path = join(__dirname, name)
+
+  // ignore non-directories
+  if (!statSync(path).isDirectory()) {
+    continue
+  }
+
+  const template = { __proto__: null }
+
+  template.transform = require(join(path, 'transform.js'))
+
+  const handlebars = Handlebars.create()
+
+  for (const [name, fn] of Object.entries(helpers)) {
+    handlebars.registerHelper(name, fn)
+  }
+
+  const partialsPath = join(path, 'partials')
+  for (const name of readdirSync(partialsPath)) {
+    const parts = parse(name)
+    if (parts.ext === '.hbs') {
+      handlebars.registerPartial(parts.base, readFileSync(join(partialsPath, name), 'utf8'))
+    }
+  }
+
+  for (const name of readdirSync(path)) {
+    const parts = parse(name)
+    if (parts.ext === '.hbs') {
+      template['$' + parts.base] = handlebars.compile(readFileSync(join(path, name), 'utf-8').replace(/\n$/, ''))
+    }
+  }
+
+  exports[name] = template
+}
