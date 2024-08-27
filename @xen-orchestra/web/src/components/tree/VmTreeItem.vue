7c08b2e0655c83d4beb5472ba96cad757d8329e8
Olivier Floch <olivier.floch@vates.fr>
2024-08-27 16:12:05 +0200

    feedback: add tooltip on VM items, add changelog entry

--- @xen-orchestra/web/src/components/tree/VmTreeItem.vue
+++ @xen-orchestra/web/src/components/tree/VmTreeItem.vue
@@ -3,7 +3,12 @@
     <TreeItemLabel :route="`/vm/${leaf.data.id}/console`" no-indent>
       {{ leaf.data.name_label }}
       <template #icon>
-        <ObjectIcon :state="leaf.data.power_state.toLocaleLowerCase() as POWER_STATE" size="small" type="vm" />
+        <ObjectIcon
+          v-tooltip="leaf.data.power_state"
+          :state="leaf.data.power_state.toLocaleLowerCase() as POWER_STATE"
+          size="small"
+          type="vm"
+        />
       </template>
     </TreeItemLabel>
   </TreeItem>
@@ -15,6 +20,7 @@ import type { POWER_STATE } from '@core/types/power-state.type'
 import ObjectIcon from '@core/components/icon/ObjectIcon.vue'
 import TreeItem from '@core/components/tree/TreeItem.vue'
 import TreeItemLabel from '@core/components/tree/TreeItemLabel.vue'
+import { vTooltip } from '@core/directives/tooltip.directive'
 
 defineProps<{
   leaf: VmLeaf
