8356726e5d5a39fde4c29ee5806afacdae0a9b0c
mathieuRA <contact@mathieu-raisin.fr>
2024-09-18 17:29:27 +0200

    feat(web-core): new Info component

---
+++ @xen-orchestra/lite/src/stories/web-core/info/ui-info.story.vue
@@ -0,0 +1,19 @@
+<template>
+  <ComponentStory
+    v-slot="{ properties, settings }"
+    :params="[
+      prop('status').enum('info', 'success', 'warning', 'danger').required().preset('info').widget(),
+      slot(),
+      setting('defaultSlot').widget(text()).preset('message'),
+    ]"
+  >
+    <UiInfo v-bind="properties">{{ settings.defaultSlot }}</UiInfo>
+  </ComponentStory>
+</template>
+
+<script lang="ts" setup>
+import ComponentStory from '@/components/component-story/ComponentStory.vue'
+import { prop, setting, slot } from '@/libs/story/story-param'
+import { text } from '@/libs/story/story-widget'
+import UiInfo from '@core/components/info/UiInfo.vue'
+</script>
