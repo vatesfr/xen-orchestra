64572a4c2f7abd58a07d22b7be439c4d9ec6ca46
Florent Beauchamp <florent.beauchamp@vates.tech>
2024-09-20 09:05:23 +0000

    feat(backups): implement long terme retention
    long term retention sometimes called GFS ( Grand Father / Father / Son)
    is a way to promote some backup to be kept on a long time
    
    that way , the user can use the find the best equilibrium between
    storage and security
    
    This commit add the code mechanics to indentify bakcup that
    can be deleted safely. It is intended to use with a form
    that ask the suer for the number of day, week, month, and year
    for which XO will keep the most recent
    
    It extends the actual system of keeping the n most recent backup
    
    Keep in mind that the backup oromoted by week and month can be decaled

--- @xen-orchestra/backups/_getOldEntries.mjs
+++ @xen-orchestra/backups/_getOldEntries.mjs
@@ -1,4 +1,86 @@
+const LTR_DEFINITIONS = {
+  daily: {
+    makeDateFormatter: ({ firstHourOfTheDay = 0 } = {}) => {
+      return date => {
+        const copy = new Date(date)
+        copy.setHours(copy.getHours() - firstHourOfTheDay)
+        return `${copy.getFullYear()}-${copy.getMonth()}-${copy.getDate()}`
+      }
+    },
+  },
+  weekly: {
+    makeDateFormatter: ({ firstDayOfWeek = 1 /* sunday is 0 , let's use monday as default instead */ } = {}) => {
+      return date => {
+        const copy = new Date(date)
+        copy.setDate(date.getDate() - ((date.getDay() + 7 - firstDayOfWeek) % 7))
+        return `${copy.getFullYear()}-${copy.getMonth()}-${copy.getDate()}`
+      }
+    },
+    ancestor: 'daily',
+  },
+  monthly: {
+    makeDateFormatter: ({ firstDayOfMonth = 0 } = {}) => {
+      return date => {
+        const copy = new Date(date)
+        copy.setDate(copy.getDate() - firstDayOfMonth)
+        return `${copy.getFullYear()}-${copy.getMonth()}`
+      }
+    },
+    ancestor: 'weekly',
+  },
+  yearly: {
+    makeDateFormatter: () => {
+      return date => `${date.getFullYear()}`
+    },
+    ancestor: 'monthly',
+  },
+}
+
 // returns all entries but the last retention-th
-export function getOldEntries(retention, entries) {
-  return entries === undefined ? [] : retention > 0 ? entries.slice(0, -retention) : entries
+/**
+ * return the entries too old to be kept
+ *  if multiple entries are i the same time bucket : keep only the most recent one
+ *  if an entry is valid in any of the bucket OR the  minRetentionCount : keep it
+ *  if a bucket is cmpletly empty : it does not count as one, thus it may extend the retention
+ * @returns Array<Backup>
+ */
+export function getOldEntries(minRetentionCount, entries, { longTermRetention = {} } = {}) {
+  const dateBuckets = {}
+  for (const [duration, { retention, settings }] of Object.entries(longTermRetention)) {
+    if (LTR_DEFINITIONS[duration] === undefined) {
+      throw new Error(`Retention of type ${retention} is invalid`)
+    }
+    dateBuckets[duration] = {
+      remaining: retention,
+      lastMatchingBucket: null,
+      formatter: LTR_DEFINITIONS[duration].makeDateFormatter(settings),
+    }
+  }
+  const nb = entries.length
+  const oldEntries = []
+
+  for (let i = nb - 1; i >= 0; i--) {
+    const entry = entries[i]
+    const entryDate = new Date(entry.timestamp)
+    let shouldBeKept = false
+    for (const [duration, { remaining, lastMatchingBucket, formatter }] of Object.entries(dateBuckets)) {
+      if (remaining === 0) {
+        continue
+      }
+      const bucket = formatter(entryDate)
+      if (lastMatchingBucket !== bucket) {
+        shouldBeKept = true
+        dateBuckets[duration].remaining -= 1
+        dateBuckets[duration].lastMatchingBucket = bucket
+      }
+    }
+    if (i >= nb - minRetentionCount) {
+      shouldBeKept = true
+    }
+    if (!shouldBeKept) {
+      oldEntries.push(entry)
+    }
+  }
+  // we expect the entries to be in the right order
+  return oldEntries.reverse()
 }
