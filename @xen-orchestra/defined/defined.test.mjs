f335d10f4e1d1603691f1b3d90b3e11bb3d02617
Julien Fontanet <julien.fontanet@isonoe.net>
2024-09-23 17:34:04 +0200

    homogenize case titles

--- @xen-orchestra/defined/defined.test.mjs
+++ @xen-orchestra/defined/defined.test.mjs
@@ -7,22 +7,22 @@ describe('defined()', () => {
     const expected = 'foo'
     assert.deepEqual(defined(undefined, 'foo', 42), expected)
   })
-  it('should return first non undefined value in array', () => {
+  it('returns first non undefined value in array', () => {
     const expected = null
     assert.deepEqual(defined([undefined, null, 10]), expected)
   })
-  it('should return first non undefined value in arrays', () => {
+  it('returns first non undefined value in arrays', () => {
     const expected = [undefined, undefined, undefined]
     assert.deepEqual(defined([undefined, undefined, undefined], [undefined, undefined, 10]), expected)
   })
-  it('should return first non undefined value in function', () => {
+  it('returns first non undefined value in function', () => {
     const expected = 'bar'
     assert.deepEqual(
       defined(() => 'bar', 42),
       expected
     )
   })
-  it('should return undefined if only undefined values', () => {
+  it('returns undefined if only undefined values', () => {
     const expected = undefined
     assert.equal(defined(undefined, undefined), expected)
   })
