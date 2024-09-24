6fa6fbf70ae8ffefab7def7dad4b02eb2a5e2b9c
Stephane MEYER <stephane.meyer@vates.tech>
2024-09-24 10:31:19 +0200

    Changes after code review

--- @xen-orchestra/log/capture.test.js
+++ @xen-orchestra/log/capture.test.js
@@ -1,11 +1,10 @@
 'use strict'
 
+const assert = require('node:assert/strict')
 const { beforeEach, describe, it } = require('node:test')
 const { captureLogs, createCaptureTransport } = require('@xen-orchestra/log/capture')
 const { configure } = require('@xen-orchestra/log/configure')
 const { createLogger } = require('@xen-orchestra/log')
-const { dedupe } = require('@xen-orchestra/log/dedupe')
-const assert = require('node:assert/strict')
 
 describe('capture log', () => {
   const logger = createLogger('test-logger')
@@ -66,35 +65,5 @@ describe('capture log', () => {
     assert.equal(captureLog[2].message, 'logs in async functions or promise chains too')
     assert.equal(captureLog[3], undefined, 'this log will not be captured')
     assert.equal(logsTransportDefault[0].message, 'this log will not be captured')
-  })
-
-  it('should not dedup logs', () => {
-    configure(
-      createCaptureTransport([
-        {
-          transport: transportTest,
-        },
-      ])
-    )
-
-    for (let i = 0; i < 3; i++) {
-      logger.debug('this line should be logged 3 times')
-    }
-    assert.equal(logsTransportDefault.length, 3)
-  })
-  it('should dedup logs', () => {
-    configure(
-      createCaptureTransport([
-        dedupe({
-          timeout: 50, // without a defined timeout, the test will wait for 10 minutes
-          transport: transportTest,
-        }),
-      ])
-    )
-
-    for (let i = 0; i < 3; i++) {
-      logger.debug('this line should be logged only once')
-    }
-    assert.equal(logsTransportDefault.length, 1)
   })
 })
