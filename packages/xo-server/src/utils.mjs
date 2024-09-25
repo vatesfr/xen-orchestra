3c63854501df981fecb54c95ee762e60ddde7f39
Mathieu <70369997+MathieuRA@users.noreply.github.com>
2024-09-25 10:10:24 +0200

    Update packages/xo-server/src/utils.mjs

--- packages/xo-server/src/utils.mjs
+++ packages/xo-server/src/utils.mjs
@@ -346,7 +346,6 @@ export const isSrWritable = sr => sr !== undefined && sr.content_type !== 'iso' 
 export const isReplicaVm = vm => 'start' in vm.blockedOperations && vm.other['xo:backup:job'] !== undefined
 
 // -------------------------------------------------------------------
-
 export const vmContainsNoBakTag = vm => vm.tags.some(t => t.split('=', 1)[0] === 'xo:no-bak')
 
 // -------------------------------------------------------------------
