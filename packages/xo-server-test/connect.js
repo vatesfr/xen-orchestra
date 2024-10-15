5a70f943f1579561a5bf74253d96c4a8f08773c6
Stephane MEYER <stephane.meyer@vates.tech>
2024-10-15 17:00:22 +0200

    changes after code review

---
+++ packages/xo-server-test/connect.js
@@ -0,0 +1,19 @@
+import Xo from 'xo-lib'
+
+const SERVER_URL = process.env.SERVER_URL || 'http://127.0.0.1:80'
+
+/**
+ * @param params Contains {url, email, password} as the optionnal server url, and the email/password of the user
+ * @return Xo A Xo object connection
+ **/
+export async function connect({ url = SERVER_URL, email, password }) {
+  const xo = new Xo.default({ url })
+  await xo.open()
+  try {
+    await xo.signIn({ email, password })
+  } catch (err) {
+    xo.close()
+    throw err
+  }
+  return xo
+}
--- packages/xo-server-test/old-tests/issues/index.spec.js
+++ packages/xo-server-test/old-tests/issues/index.spec.js
@@ -2,8 +2,8 @@
 
 import { parseDuration } from '@vates/parse-duration'
 
-import config from '../_config.js'
-import xo from '../_xoConnection.js'
+import config from '../../_config.js'
+import xo from '../../_xoConnection.js'
 
 describe('issue', () => {
   test('4507', async () => {
