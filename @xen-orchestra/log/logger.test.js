8b412486e371f92bdfd270c93a3d745216bbefc5
Julien Fontanet <julien.fontanet@isonoe.net>
2024-10-22 14:36:49 +0200

    wip

--- @xen-orchestra/log/logger.test.js
+++ @xen-orchestra/log/logger.test.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const assert = require('node:assert/strict')
-const { beforeEach, describe, it } = require('node:test')
+const { before, beforeEach, describe, it } = require('node:test')
 const { configure } = require('@xen-orchestra/log/configure')
 const { createLogger } = require('@xen-orchestra/log')
 const { createCaptureTransport } = require('@xen-orchestra/log/capture')
@@ -20,7 +20,7 @@ describe('logger', () => {
     logsTransportFatal.push(message)
   }
 
-  beforeEach(() => {
+  before(() => {
     configure(
       createCaptureTransport([
         {
@@ -33,7 +33,9 @@ describe('logger', () => {
         },
       ])
     )
+  })
 
+  beforeEach(() => {
     logsTransportDebug.length = 0
     logsTransportFatal.length = 0
   })
@@ -41,11 +43,13 @@ describe('logger', () => {
   it('should log test with all its attributes', () => {
     const message = 'test 1 with all its attributes'
     logger.debug(message)
+
     assert.equal(logsTransportDebug[0].message, message)
     assert.equal(typeof logsTransportDebug[0].level, 'number', `level attribute should exist`)
     assert.equal(typeof logsTransportDebug[0].namespace, 'string', `namespace attribute should exist`)
     assert.equal(logsTransportDebug[0].time instanceof Date, true, 'time attribute should exist')
   })
+
   it('should log test 1 and 2', () => {
     const expected1 = {
       level: 20,
@@ -61,9 +65,11 @@ describe('logger', () => {
     }
     logger.debug(expected1.message)
     logger.debug(expected2.message)
+
     assert.equal(logsTransportDebug[0].message, expected1.message)
     assert.equal(logsTransportDebug[1].message, expected2.message)
   })
+
   it('should log tests for debug', () => {
     const expectedDebug = {
       data: undefined,
@@ -73,9 +79,11 @@ describe('logger', () => {
       time: new Date(),
     }
     logger.debug(expectedDebug.message)
+
     assert.deepEqual(logsTransportDebug[0].message, expectedDebug.message)
     assert.deepEqual(logsTransportDebug[0].level, expectedDebug.level)
   })
+
   it('should log tests for info', () => {
     const expectedInfo = {
       data: undefined,
@@ -85,9 +93,11 @@ describe('logger', () => {
       time: new Date(),
     }
     logger.info(expectedInfo.message)
+
     assert.deepEqual(logsTransportDebug[0].message, expectedInfo.message)
     assert.deepEqual(logsTransportDebug[0].level, expectedInfo.level)
   })
+
   it('should log tests for warn', () => {
     const expectedWarn = {
       data: undefined,
@@ -97,9 +107,11 @@ describe('logger', () => {
       time: new Date(),
     }
     logger.warn(expectedWarn.message)
+
     assert.deepEqual(logsTransportDebug[0].message, expectedWarn.message)
     assert.deepEqual(logsTransportDebug[0].level, expectedWarn.level)
   })
+
   it('should log tests for error', () => {
     const expectedError = {
       data: undefined,
@@ -109,10 +121,12 @@ describe('logger', () => {
       time: new Date(),
     }
     logger.error(expectedError.message)
+
     assert.deepEqual(logsTransportDebug[0].message, expectedError.message)
     assert.deepEqual(logsTransportDebug[0].level, expectedError.level)
     assert.ok(logsTransportFatal.length === 0, 'fatal logs should be empty')
   })
+
   it('should log tests for fatal', () => {
     const expectedFatal = {
       data: undefined,
@@ -122,9 +136,11 @@ describe('logger', () => {
       time: new Date(),
     }
     logger.fatal(expectedFatal.message)
+
     assert.deepEqual(logsTransportFatal[0].message, expectedFatal.message)
     assert.deepEqual(logsTransportFatal[0].level, expectedFatal.level)
   })
+
   it('should log an error on null', () => {
     const expected1 = {
       data: { level: 20, value: null },
@@ -134,8 +150,10 @@ describe('logger', () => {
       time: new Date(),
     }
     logger.debug(null)
+
     assert.deepEqual(logsTransportDebug[0].data, expected1.data)
   })
+
   it('should log an error on undefined', () => {
     const expected1 = {
       data: { level: 20, value: undefined },
@@ -145,8 +163,10 @@ describe('logger', () => {
       time: new Date(),
     }
     logger.debug(undefined)
+
     assert.deepEqual(logsTransportDebug[0].data, expected1.data)
   })
+
   it('should not dedup logs', () => {
     configure(
       createCaptureTransport([
@@ -159,8 +179,10 @@ describe('logger', () => {
     for (let i = 0; i < 3; i++) {
       logger.debug('this line should be logged 3 times')
     }
+
     assert.equal(logsTransportDebug.length, 3)
   })
+
   it('should dedup logs', () => {
     configure(
       createCaptureTransport([
@@ -174,6 +196,7 @@ describe('logger', () => {
     for (let i = 0; i < 3; i++) {
       logger.debug('this line should be logged only once')
     }
+
     assert.equal(logsTransportDebug.length, 1)
   })
 })
