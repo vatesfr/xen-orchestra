ed60f8329f18f6889d848beef790c88852c04ee2
joris K <joris.kosacki@vates.tech>
2024-10-23 09:43:31 +0200

    feat(web-core): added UiRadioButton component

---
+++ @xen-orchestra/lite/src/stories/web-core/radio-button/ui-radio-button.story.vue
@@ -0,0 +1,33 @@
+<template>
+  <ComponentStory
+    v-slot="{ properties, settings }"
+    :params="[
+      prop('accent').required().enum('brand', 'success', 'warning', 'danger').preset('brand').widget(),
+      prop('disabled').bool().widget(),
+      slot(),
+      slot('info'),
+      setting('defaultSlot').widget(text()).preset('Label'),
+      setting('info').widget(text()).preset('Lorem ipsum dolor sit amet, consectetur adipiscing elit.'),
+    ]"
+  >
+    <div>
+      <UiRadioButton v-model="selectedRadio" :value="1" v-bind="properties">{{ settings.defaultSlot }}</UiRadioButton>
+    </div>
+    <div>
+      <UiRadioButton v-model="selectedRadio" :value="2" v-bind="properties">
+        {{ settings.defaultSlot }}
+        <template v-if="settings.info" #info>{{ settings.info }}</template>
+      </UiRadioButton>
+    </div>
+  </ComponentStory>
+</template>
+
+<script lang="ts" setup>
+import ComponentStory from '@/components/component-story/ComponentStory.vue'
+import { prop, setting, slot } from '@/libs/story/story-param'
+import { text } from '@/libs/story/story-widget'
+import UiRadioButton from '@core/components/ui/radio-button/UiRadioButton.vue'
+import { ref } from 'vue'
+
+const selectedRadio = ref('')
+</script>
