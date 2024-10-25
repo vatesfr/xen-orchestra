ee0aa7b77c38c18460c76184e9115807e495d447
joris K <joris.kosacki@vates.tech>
2024-10-25 15:27:23 +0200

    feat(web-core): added UiTableActions component

---
+++ @xen-orchestra/lite/src/stories/web-core/table-actions/ui-table-actions.story.vue
@@ -0,0 +1,50 @@
+<template>
+  <ComponentStory
+    v-slot="{ properties, settings }"
+    :params="[
+      slot(),
+      slot('actions').help('Meant to receive actions item like buttons'),
+      setting('defaultSlot').widget(text()).preset('message'),
+    ]"
+  >
+    <UiTableActions v-bind="properties">
+      {{ settings.defaultSlot }}
+      <template #actions>
+        <UiButton
+          v-for="(label, index) in buttonLabels"
+          :key="index"
+          :left-icon="label.icon"
+          level="tertiary"
+          color="normal"
+          size="medium"
+        >
+          {{ label.title }}
+        </UiButton>
+      </template>
+    </UiTableActions>
+  </ComponentStory>
+</template>
+
+<script lang="ts" setup>
+import ComponentStory from '@/components/component-story/ComponentStory.vue'
+import { setting, slot } from '@/libs/story/story-param'
+import { text } from '@/libs/story/story-widget'
+import UiButton from '@core/components/button/UiButton.vue'
+import UiTableActions from '@core/components/ui/table-actions/UiTableActions.vue'
+import {
+  faArrowsAlt,
+  faBuildingCircleArrowRight,
+  faCalendarDays,
+  faCircleChevronRight,
+  faSoap,
+} from '@fortawesome/free-solid-svg-icons'
+import { ref } from 'vue'
+
+const buttonLabels = ref([
+  { title: 'Label', icon: faArrowsAlt },
+  { title: 'Label', icon: faCircleChevronRight },
+  { title: 'Label', icon: faCalendarDays },
+  { title: 'Label', icon: faBuildingCircleArrowRight },
+  { title: 'Label', icon: faSoap },
+])
+</script>
---
+++ @xen-orchestra/web-core/lib/components/ui/table-actions/UiTableActions.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="ui-table-actions">
+    <UiActionsTitle>
+      <slot />
+    </UiActionsTitle>
+    <slot name="actions" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import UiActionsTitle from '@core/components/ui/actions-title/UiActionsTitle.vue'
+
+defineSlots<{
+  actions(): any
+  default(): any
+}>()
+</script>
+
+<style scoped lang="postcss">
+.ui-table-actions {
+  display: flex;
+  align-items: center;
+  gap: 2.4rem;
+  padding-block: 0.4rem;
+  border-bottom: 0.1rem solid var(--color-neutral-border);
+}
+</style>
