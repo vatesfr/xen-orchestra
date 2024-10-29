vatesfr/xen-orchestra

commit ba3753ab1a62d1b3e02f7a8042763c8caee9798d

feat(task): add start, success & failure methods (#8061)
* feat(task): add start, success & failure methods

Allow managing the task manually instead of using `.run()` and `.runInside()`.

@vates/task/index.js
@@ -132,9 +132,13 @@ class Task {
   #end(status, result) {
     assert.equal(this.#status, PENDING)
 
-    this.#status = status
     this.#emit('end', { status, result })
     this.#onProgress = alreadyEnded
+    this.#status = status
+  }
+
+  failure(error) {
+    this.#end(FAILURE, error)
   }
 
   info(message, data) {
@@ -142,10 +146,8 @@ class Task {
   }
 
   #maybeStart() {
-    const startData = this.#startData
-    if (startData !== undefined) {
-      this.#startData = undefined
-      this.#emit('start', startData)
+    if (this.#startData !== undefined) {
+      this.start()
     }
   }
 
@@ -179,6 +181,18 @@ class Task {
     assert.equal(this.status, PENDING)
 
     this.#emit('property', { name, value })
+  }
+
+  start() {
+    const startData = this.#startData
+    assert.notEqual(startData, undefined, 'task has already started')
+
+    this.#startData = undefined
+    this.#emit('start', startData)
+  }
+
+  success(result) {
+    this.#end(SUCCESS, result)
   }
 
   warning(message, data) {

@vates/task/index.test.js
@@ -145,6 +145,36 @@ describe('Task', function () {
     })
   })
 
+  describe('#failure()', function () {
+    const error = new Error()
+
+    it('throws if the task has not started yet', function () {
+      const task = createTask()
+
+      assert.throws(() => task.failure(error), { message: 'task has not started yet' })
+    })
+
+    it('throws if the task has already finished', function () {
+      const task = createTask()
+      task.start()
+      task.success()
+
+      assert.throws(() => task.failure(error), { code: 'ERR_ASSERTION' })
+    })
+
+    it('finishes the task and mark it as failed', function () {
+      const task = createTask()
+      task.start()
+      task.failure(error)
+
+      assertEvent(task, {
+        status: 'failure',
+        result: error,
+        type: 'end',
+      })
+    })
+  })
+
   describe('.info()', function () {
     it('does nothing when run outside a task', function () {
       Task.info('foo')
@@ -240,6 +270,52 @@ describe('Task', function () {
             value,
           })
         })
+      })
+    })
+  })
+
+  describe('#start', function () {
+    it('starts the task', function () {
+      const task = createTask()
+      task.start()
+
+      assertEvent(task, { type: 'start' })
+    })
+
+    it('throws when the task has already started', function () {
+      const task = createTask()
+      task.start()
+
+      assert.throws(() => task.start(), { message: 'task has already started' })
+    })
+  })
+
+  describe('#success()', function () {
+    const result = {}
+
+    it('throws if the task has not started yet', function () {
+      const task = createTask()
+
+      assert.throws(() => task.success(result), { message: 'task has not started yet' })
+    })
+
+    it('throws if the task has already finished', function () {
+      const task = createTask()
+      task.start()
+      task.success()
+
+      assert.throws(() => task.success(result), { code: 'ERR_ASSERTION' })
+    })
+
+    it('finishes the task and mark it as successful', function () {
+      const task = createTask()
+      task.start()
+      task.success(result)
+
+      assertEvent(task, {
+        status: 'success',
+        result,
+        type: 'end',
       })
     })
   })

commit 297b9b2025a78f711a17637562abbb387ec11c5f

feat(task/{failure,success}): ensure not called during run
Introduced by ba3753ab1

@vates/task/index.js
@@ -131,6 +131,7 @@ class Task {
 
   #end(status, result) {
     assert.equal(this.#status, PENDING)
+    assert.equal(this.#running, false)
 
     this.#emit('end', { status, result })
     this.#onProgress = alreadyEnded
@@ -172,6 +173,7 @@ class Task {
       this.#running = false
       return result
     } catch (result) {
+      this.#running = false
       this.#end(FAILURE, result)
       throw result
     }

@vates/task/index.test.js
@@ -154,6 +154,13 @@ describe('Task', function () {
       assert.throws(() => task.failure(error), { message: 'task has not started yet' })
     })
 
+    it('throws if the task is running', function () {
+      const task = createTask()
+      task.runInside(noop)
+
+      assert.throws(() => task.failure(error), { code: 'ERR_ASSERTION' })
+    })
+
     it('throws if the task has already finished', function () {
       const task = createTask()
       task.start()
@@ -297,6 +304,13 @@ describe('Task', function () {
       const task = createTask()
 
       assert.throws(() => task.success(result), { message: 'task has not started yet' })
+    })
+
+    it('throws if the task is running', function () {
+      const task = createTask()
+      task.runInside(noop)
+
+      assert.throws(() => task.success(result), { code: 'ERR_ASSERTION' })
     })
 
     it('throws if the task has already finished', function () {