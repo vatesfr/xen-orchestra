81f556cd7b386516511aae60687c4a7465f1fb22
Nathanaël HANNEBERT <nathanael.hannebert@vates.tech>
2024-10-30 16:13:58 +0100

    docs(global): Docusauris, disable blog, remove default homepage

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -43,21 +43,7 @@ const config: Config = {
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
+        blog: false,
         theme: {
           customCss: './src/css/custom.css',
         },
