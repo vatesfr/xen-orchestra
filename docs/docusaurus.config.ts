5bb6888b5254855bba7ba7b3c934f2a4406c6924
Nathanaël HANNEBERT <nathanael.hannebert@vates.tech>
2024-11-05 16:46:14 +0100

    docs(global): Remove trailingSlash in uri, because I don't link it.

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   title: 'Xen Orchestra | XO Documentation',
   tagline: 'Discover how to use Xen Orchestra',
   favicon: 'img/favicon.ico',
-
+  trailingSlash: false,
   // Set the production url of your site here
   url: 'https://docs.xen-orchestra.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
