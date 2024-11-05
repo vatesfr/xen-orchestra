5caab9725b5249a74cdd600ac14ac63fce978d6c
Thomas Moraine <thomas.moraine@vates.tech>
2024-11-05 11:03:52 +0100

    docs(global): Fixed the url of the documentation in Docusaurus config with the actual production URL

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://xen-orchestra.com/',
+  url: 'https://docs.xen-orchestra.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
