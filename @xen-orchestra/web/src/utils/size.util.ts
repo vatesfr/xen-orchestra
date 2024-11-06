b729ecc875c98c9e44f29894102f58660edce762
Thierry <thierry@byscripts.info>
2024-11-06 11:37:25 +0100

    update human-format dep

--- @xen-orchestra/web/src/utils/size.util.ts
+++ @xen-orchestra/web/src/utils/size.util.ts
@@ -1,7 +1,5 @@
-// @ts-expect-error Waiting for pending fix in human-format
 import { Scale, raw } from 'human-format'
 
-// @ts-expect-error Waiting for pending fix in human-format
 const scale = Scale.create(['', 'KiB', 'MiB', 'GiB', 'TiB'], 1024)
 
 export const formatSizeRaw = (bytes: number | undefined, decimals: number) => {
