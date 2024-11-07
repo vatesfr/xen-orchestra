b41d6857c21b8f671d49affacef9c4b99c43b1eb
joris K <joris.kosacki@vates.tech>
2024-11-07 15:23:24 +0100

    feat(web-core): update UiInput component

--- @xen-orchestra/lite/src/stories/web-core/ui/input/ui-input.story.vue
+++ @xen-orchestra/lite/src/stories/web-core/ui/input/ui-input.story.vue
@@ -1,19 +1,33 @@
 <template>
   <ComponentStory
-    v-slot="{ properties }"
+    v-slot="{ properties, settings }"
     :params="[
       model().type('string').preset(''),
+      prop('accent').enum('info', 'warning', 'danger').required().preset('info').widget(),
+      prop('type').enum('text', 'number', 'password').required().preset('text').widget(),
       prop('disabled').bool().widget(),
-      iconProp().preset(faMagnifyingGlass).type('IconDefinition'),
+      prop('clearable').bool().widget(),
+      prop('href').str().widget(),
+      iconProp(),
+      iconProp('iconBefore'),
+      prop('placeholder').str().widget(),
+      prop('required').bool().widget(),
+      slot(),
+      slot('info'),
+      setting('defaultSlot').widget(text()).preset('Some label'),
+      setting('info').widget(text()).preset('Lorem ipsum dolor sit amet, consectetur adipiscing elit.'),
     ]"
   >
-    <UiInput placeholder="Enter your search" v-bind="properties" />
+    <UiInput placeholder="Enter your search" v-bind="properties">
+      {{ settings.defaultSlot }}
+      <template v-if="settings.info" #info>{{ settings.info }}</template>
+    </UiInput>
   </ComponentStory>
 </template>
 
 <script lang="ts" setup>
 import ComponentStory from '@/components/component-story/ComponentStory.vue'
-import { iconProp, model, prop } from '@/libs/story/story-param'
+import { iconProp, model, prop, setting, slot } from '@/libs/story/story-param'
+import { text } from '@/libs/story/story-widget'
 import UiInput from '@core/components/ui/input/UiInput.vue'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 </script>
