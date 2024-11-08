99add713691de0668f16f4a71fefa62eb86596cf
Sébastian Czech <czechsebastian@icloud.com>
2024-11-08 13:49:33 +0100

    feat(xo-6): remove class

--- @xen-orchestra/lite/src/stories/web-core/state-hero/vts-state-hero.story.vue
+++ @xen-orchestra/lite/src/stories/web-core/state-hero/vts-state-hero.story.vue
@@ -9,7 +9,7 @@
       setting('defaultSlot').preset('Some text').widget(),
     ]"
   >
-    <VtsStateHero v-bind="properties" class="container">
+    <VtsStateHero v-bind="properties">
       {{ settings.defaultSlot }}
     </VtsStateHero>
   </ComponentStory>
