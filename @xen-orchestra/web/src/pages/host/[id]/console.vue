1d80765f532bbd632a64306151314116c5f787d4
mathieuRA <contact@mathieu-raisin.fr>
2024-11-12 10:29:25 +0100

    add layout console for hosts

--- @xen-orchestra/web/src/pages/host/[id]/console.vue
+++ @xen-orchestra/web/src/pages/host/[id]/console.vue
@@ -1,11 +1,22 @@
 <template>
   <p v-if="!isHostConsoleAvailable" class="typo h5-medium">{{ $t('power-on-host-for-console') }}</p>
-  <VtsRemoteConsole v-else :url />
+  <VtsLayoutConsole v-else>
+    <VtsRemoteConsole :url />
+    <template #actions>
+      <VtsActionsConsole />
+      <VtsDivider type="stretch" />
+      <VtsClipboardConsole />
+    </template>
+  </VtsLayoutConsole>
 </template>
 
 <script lang="ts" setup>
 import type { XoHost } from '@/types/xo/host.type'
+import VtsActionsConsole from '@core/components/console/VtsActionsConsole.vue'
+import VtsClipboardConsole from '@core/components/console/VtsClipboardConsole.vue'
+import VtsLayoutConsole from '@core/components/console/VtsLayoutConsole.vue'
 import VtsRemoteConsole from '@core/components/console/VtsRemoteConsole.vue'
+import VtsDivider from '@core/components/divider/VtsDivider.vue'
 import { computed } from 'vue'
 
 const props = defineProps<{
