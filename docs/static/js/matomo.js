9ae8b689b4908acdbf1601f1f2328f2f8eb6b30b
Julien Fontanet <julien.fontanet@isonoe.net>
2024-11-13 09:26:18 +0100

    various fixes

--- docs/static/js/matomo.js
+++ docs/static/js/matomo.js
@@ -1,11 +1,11 @@
+'use strict'
 
-  var _paq = window._paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="https://visit.vates.tech/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '5']);
-    
-  })();
+const _paq = (window._paq = window._paq || [])
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['trackPageView'])
+_paq.push(['enableLinkTracking'])
+;(function () {
+  const u = 'https://visit.vates.tech/'
+  _paq.push(['setTrackerUrl', u + 'matomo.php'])
+  _paq.push(['setSiteId', '5'])
+})()
