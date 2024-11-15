73a0a7b47807620bb655af8d626ea34a55472176
Sébastian Czech <czechsebastian@icloud.com>
2024-11-15 15:03:14 +0100

    feat(xo-6): implement network view

---
+++ @xen-orchestra/web/src/pages/pool/[id]/networks.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="networks">
+    <UiCard class="card">
+      <PoolNetworksTable />
+      <PoolHostInternalNetworksTable />
+    </UiCard>
+    <PoolNetworksSidePanel />
+  </div>
+</template>
+
+<script setup lang="ts">
+import PoolHostInternalNetworksTable from '@/components/pool/PoolHostInternalNetworksTable.vue'
+import PoolNetworksSidePanel from '@/components/pool/PoolNetworksSidePanel.vue'
+import PoolNetworksTable from '@/components/pool/PoolNetworksTable.vue'
+import UiCard from '@core/components/ui/card/UiCard.vue'
+</script>
+
+<style scoped lang="postcss">
+.networks {
+  display: flex;
+  .card {
+    margin: 0.8rem;
+    flex: 1;
+  }
+}
+</style>
