7c68ff83cfff7cd66c98cd391692736ccf924a22
OlivierFL <66562640+OlivierFL@users.noreply.github.com>
2024-11-18 16:35:07 +0100

    feat(xo-lite/header): update user menu button (#8109)

---
+++ @xen-orchestra/lite/src/stories/web-core/ui/account-menu-button/ui-account-menu-button.story.vue
@@ -0,0 +1,17 @@
+<template>
+  <ComponentStory
+    v-slot="{ properties }"
+    :params="[
+      prop('size').type(`'small' | 'medium'`).enum('small', 'medium').required().preset('medium').widget(),
+      prop('selected').bool().widget(),
+    ]"
+  >
+    <UiAccountMenuButton v-bind="properties" />
+  </ComponentStory>
+</template>
+
+<script lang="ts" setup>
+import ComponentStory from '@/components/component-story/ComponentStory.vue'
+import { prop } from '@/libs/story/story-param'
+import UiAccountMenuButton from '@core/components/ui/account-menu-button/UiAccountMenuButton.vue'
+</script>
