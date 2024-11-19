b71b6c379679d90cd929485341a4cc8b82851d4a
Sébastian Czech <czechsebastian@icloud.com>
2024-11-19 15:24:00 +0100

    feat(xo-6): add stores

--- @xen-orchestra/web/src/pages/host/[id]/network.vue
+++ @xen-orchestra/web/src/pages/host/[id]/network.vue
@@ -9,7 +9,7 @@
           </UiButton>
         </template>
       </UiTitle>
-      <div class="table-container">
+      <div v-if="isReady" class="table-container">
         <UiQuerySearchBar class="table-query" @search="(value: string) => (filter = value)" />
         <UiTableActions>
           <UiButton size="medium" variant="tertiary" accent="info" :left-icon="faEdit" disabled>
@@ -25,14 +25,14 @@
         <UiTopBottomTable
           class="selection"
           :selected-items="selectedItems"
-          :total-items="totalItems.length"
+          :total-items="pifs.length"
           @toggle-select-all="toggleSelect"
         />
         <VtsTable vertical-border class="table">
           <thead>
             <tr>
               <ColumnTitle id="checkbox">
-                <UiCheckbox :v-model="test" accent="info" @update:model-value="toggleSelect($event)" />
+                <UiCheckbox accent="info" @update:model-value="toggleSelect($event)" />
               </ColumnTitle>
               <ColumnTitle id="network" :icon="faAlignLeft">{{ $t('network') }}</ColumnTitle>
               <ColumnTitle id="device" :icon="faAlignLeft">{{ $t('device') }}</ColumnTitle>
@@ -47,34 +47,34 @@
             </tr>
           </thead>
           <tbody>
-            <tr v-for="pif in totalItems" :key="pif.id">
+            <tr v-for="pif in pifs" :key="pif.uuid">
               <VtsCellText>
                 <UiCheckbox v-model="pif.selected" accent="info" />
               </VtsCellText>
-              <VtsCellObject>
-                <UiObjectLink :route="`/vm/${pif.id}/console`">
+              <VtsCellText>
+                <UiObjectLink>
                   <template #icon>
                     <UiComplexIcon size="medium">
                       <VtsIcon :icon="faNetworkWired" accent="current" />
                       <VtsIcon accent="success" :icon="faCircle" :overlay-icon="faCheck" />
                     </UiComplexIcon>
                   </template>
-                  {{ pif.name_label }}
+                  {{ pif.networkLabel }}
                 </UiObjectLink>
-              </VtsCellObject>
+              </VtsCellText>
               <VtsCellText>{{ pif.device }}</VtsCellText>
               <VtsCellObject>
                 <VtsIcon
-                  :accent="getStatusProps(pif.status as NetworkStatus).accent"
+                  :accent="getStatusProps(status(pif) as NetworkStatus).accent"
                   :icon="faCircle"
-                  :overlay-icon="getStatusProps(pif.status as NetworkStatus).icon"
+                  :overlay-icon="getStatusProps(status(pif) as NetworkStatus).icon"
                 />
-                <div class="pif-status">{{ pif.status }}</div>
+                <div class="pif-status">{{ getStatusProps(status(pif) as NetworkStatus).text }}</div>
               </VtsCellObject>
               <VtsCellText>{{ pif.vlan }}</VtsCellText>
               <VtsCellText>{{ pif.ip }}</VtsCellText>
               <VtsCellText>{{ pif.mac }}</VtsCellText>
-              <VtsCellText>{{ pif.ip_mode }}</VtsCellText>
+              <VtsCellText>{{ pif.mode }}</VtsCellText>
               <VtsCellText class="status-icon">
                 <UiButtonIcon :icon="faEllipsis" size="medium" accent="info" />
               </VtsCellText>
@@ -84,12 +84,12 @@
         <UiTopBottomTable
           class="selection"
           :selected-items="selectedItems"
-          :total-items="totalItems.length"
+          :total-items="pifsByHost.length"
           @toggle-select-all="toggleSelect"
         />
       </div>
     </div>
-    <UiPanel class="network-panel">
+    <UiPanel v-if="isReady" class="network-panel">
       <template #header>
         <UiButton size="medium" variant="tertiary" accent="info" :left-icon="faEdit">
           {{ $t('edit') }}
@@ -98,39 +98,93 @@
           {{ $t('delete') }}
         </UiButton>
       </template>
-      <UiCard v-for="card in cardsContent" :key="card.name" class="card">
-        <UiCardTitle>{{ card.name }}</UiCardTitle>
-        <div v-for="(value, key) in card.content" :key="key" class="card-content">
-          <p class="title typo p3-regular">{{ key }}</p>
-          <div v-if="key === 'network'">
-            <UiComplexIcon size="medium">
-              <VtsIcon :icon="faNetworkWired" accent="current" />
-              <VtsIcon accent="success" :icon="faCircle" :overlay-icon="faCheck" />
-            </UiComplexIcon>
+      <UiCard class="card">
+        <UiCardTitle>{{ $t('pif') }}</UiCardTitle>
+        <div class="card-content">
+          <p class="title typo p3-regular">{{ $t('uuid') }}</p>
+          <p class="typo p3-regular">{{ selectedPif[0].uuid }}</p>
+        </div>
+        <div class="card-content">
+          <p class="title typo p3-regular">{{ $t('network') }}</p>
+          <p class="typo p3-regular">{{ selectedPif[0].networkLabel }}</p>
+        </div>
+        <div class="card-content">
+          <p class="title typo p3-regular">{{ $t('device') }}</p>
+          <p class="typo p3-regular">{{ selectedPif[0].device }}</p>
+        </div>
+        <div class="card-content">
+          <p class="title typo p3-regular">{{ $t('pif-status') }}</p>
+          <VtsIcon
+            :accent="getStatusProps(status(selectedPif[0]) as NetworkStatus).accent"
+            :icon="faCircle"
+            :overlay-icon="getStatusProps(status(selectedPif[0]) as NetworkStatus).icon"
+          />
+          <p class="typo p3-regular">{{ getStatusProps(status(selectedPif[0]) as NetworkStatus).text }}</p>
+        </div>
+        <div class="card-content">
+          <p class="title typo p3-regular">{{ $t('physical-interface-status') }}</p>
+          <VtsIcon
+            :accent="getStatusProps(status(selectedPif[0]) as NetworkStatus).accent"
+            :icon="faCircle"
+            :overlay-icon="getStatusProps(status(selectedPif[0]) as NetworkStatus).icon"
+          />
+          <p class="typo p3-regular">{{ getStatusProps(status(selectedPif[0]) as NetworkStatus).text }}</p>
+        </div>
+        <div class="card-content">
+          <p class="title typo p3-regular">{{ $t('vlan') }}</p>
+          <p class="typo p3-regular">{{ selectedPif[0].vlan }}</p>
+        </div>
+        <div class="card-content">
+          <p class="title typo p3-regular">{{ $t('tags') }}</p>
+          <div class="tags">
+            <UiTag v-for="tag in selectedPif[0].tags" :key="tag" accent="info" variant="secondary">{{ tag }}</UiTag>
           </div>
-          <div v-if="key === 'status' || key === 'physicalStatus'" class="status-icon">
-            <VtsIcon
-              :accent="getStatusProps(value as NetworkStatus).accent"
-              :icon="faCircle"
-              :overlay-icon="getStatusProps(value as NetworkStatus).icon"
-            />
-          </div>
-          <div v-if="key === 'ip_addresses'" class="description ip typo p3-regular">
-            <div v-for="(ip, index) in value" :key="ip" class="ip-address">
-              <p>{{ ip }}</p>
-              <div class="action-buttons">
-                <UiButtonIcon :icon="faCopy" size="medium" accent="info" />
-                <UiButtonIcon v-if="index === 0" :icon="faEllipsis" size="medium" accent="info" />
-              </div>
-            </div>
-          </div>
-          <div v-else-if="key === 'tags'" class="tags">
-            <UiTag v-for="tag in value" :key="tag" accent="info" variant="secondary">{{ tag }}</UiTag>
-          </div>
-          <div v-else class="description typo p3-regular">
-            <p>{{ value }}</p>
-            <UiButtonIcon :icon="faCopy" size="medium" accent="info" />
-          </div>
+        </div>
+      </UiCard>
+      <UiCard class="card">
+        <UiCardTitle>{{ $t('network-information') }}</UiCardTitle>
+        <div class="card-content">
+          <p class="title typo p3-regular">{{ $t('ip-addresses') }}</p>
+          <p v-for="ip in selectedPif[0].allIps" :key="ip" class="ip-address typo p3-regular">{{ ip }}</p>
+        </div>
+        <div class="card-content">
+          <p class="title typo p3-regular">{{ $t('mac-addresses') }}</p>
+          <p class="typo p3-regular">{{ selectedPif[0].mac }}</p>
+        </div>
+        <div class="card-content">
+          <p class="title typo p3-regular">{{ $t('netmask') }}</p>
+          <p class="typo p3-regular">{{ selectedPif[0].netmask }}</p>
+        </div>
+        <div class="card-content">
+          <p class="title typo p3-regular">{{ $t('dns') }}</p>
+          <p class="typo p3-regular">{{ selectedPif[0].dns }}</p>
+        </div>
+        <div class="card-content">
+          <p class="title typo p3-regular">{{ $t('gateway') }}</p>
+          <p class="typo p3-regular">{{ selectedPif[0].gateway }}</p>
+        </div>
+        <div class="card-content">
+          <p class="title typo p3-regular">{{ $t('ip-mode') }}</p>
+          <p class="typo p3-regular">{{ selectedPif[0].mode }}</p>
+        </div>
+      </UiCard>
+      <UiCard class="card">
+        <UiCardTitle>{{ $t('properties') }}</UiCardTitle>
+        <div class="card-content">
+          <p class="title typo p3-regular">{{ $t('mtu') }}</p>
+          <p class="typo p3-regular">{{ selectedPif[0].mtu }}</p>
+        </div>
+        <div class="card-content">
+          <p class="title typo p3-regular">{{ $t('speed') }}</p>
+          <p class="typo p3-regular">{{ selectedPif[0].speed }} {{ $t('mbs') }}</p>
+        </div>
+        <div class="card-content">
+          <p class="title typo p3-regular">{{ $t('network-blocking-device') }}</p>
+          <p class="typo p3-regular">{{ selectedPif[0].nbd }}</p>
+        </div>
+        <div class="card-content">
+          <p class="title typo p3-regular">{{ $t('default-locking-mode') }}</p>
+          <p class="typo p3-regular">{{ selectedPif[0].defaultLockingMode }}</p>
         </div>
       </UiCard>
     </UiPanel>
@@ -138,6 +192,8 @@
 </template>
 
 <script setup lang="ts">
+import { useNetworkStore } from '@/stores/xo-rest-api/network.store'
+import type { XoPif } from '@/types/xo/pif.type'
 import VtsCellObject from '@core/components/cell-object/VtsCellObject.vue'
 import VtsCellText from '@core/components/cell-text/VtsCellText.vue'
 import VtsIcon from '@core/components/icon/VtsIcon.vue'
@@ -157,9 +213,7 @@ import UiTableActions from '@core/components/ui/table-actions/UiTableActions.vue
 import UiTag from '@core/components/ui/tag/UiTag.vue'
 import UiTitle from '@core/components/ui/title/UiTitle.vue'
 import UiTopBottomTable from '@core/components/ui/top-bottom-table/UiTopBottomTable.vue'
-// import { defineTree } from '@core/composables/tree/define-tree'
 import { useTreeFilter } from '@core/composables/tree-filter.composable'
-// import { useTree } from '@core/composables/tree.composable'
 import type { IconDefinition } from '@fortawesome/fontawesome-common-types'
 import {
   faAlignLeft,
@@ -168,7 +222,6 @@ import {
   faCaretDown,
   faCheck,
   faCircle,
-  faCopy,
   faEdit,
   faEllipsis,
   faExclamation,
@@ -176,185 +229,40 @@ import {
   faPowerOff,
   faTrash,
 } from '@fortawesome/free-solid-svg-icons'
-import { computed, ref } from 'vue'
+import { computed } from 'vue'
 
-const data = [
-  {
-    id: 'e8ec9a13-7b65-495e-2d90-224c17e02b67',
-    name_label: '[Team XCPng] - Win X centers',
-    status: 'connected',
-    device: 'eth1',
-    vlan: 100,
-    ip: '127.0.0.1',
-    mac: '00:1A:2B:3C:4D:5E',
-    ip_mode: 'DHCP',
-    selected: false,
-  },
-  {
-    id: 'e40c1da3-918b-c173-506c-e8c2c069181f',
-    name_label: 'AS-YA test VM pharpp_2023-10-30T15:03:17.071Z',
-    status: 'connected',
-    device: 'eth1',
-    vlan: 101,
-    ip: '127.0.0.1',
-    mac: '00:1A:2B:3C:4D:5E',
-    ip_mode: 'DHCP',
-    selected: false,
-  },
-  {
-    id: '989cd0ad-17f8-c198-15c6-8549da1f3b9f',
-    name_label: 'BRS - XenServer 8',
-    status: 'disconnected',
-    device: 'eth1',
-    vlan: 100,
-    ip: '127.0.0.1',
-    mac: '00:1A:2B:3C:4D:5E',
-    ip_mode: 'DHCP',
-    selected: false,
-  },
-  {
-    id: 'bfbacbd4-8a09-c769-4e7e-59c2a0f77095',
-    name_label: 'Debian Teddy',
-    status: 'other',
-    device: 'eth1',
-    vlan: null,
-    ip: '127.0.0.1',
-    mac: '00:1A:2B:3C:4D:5E',
-    ip_mode: 'DHCP',
-    selected: false,
-  },
-  {
-    id: '03a631e2-a39e-7c63-7bb3-ac7a5ef6c47e',
-    name_label: 'DML - 8.2.1 - Host 1',
-    status: 'connected',
-    device: 'eth1',
-    vlan: 100,
-    ip: '127.0.0.1',
-    mac: '00:1A:2B:3C:4D:5E',
-    ip_mode: 'Static',
-    selected: false,
-  },
-  {
-    id: 'ea57eb75-c55b-d9c9-6cb4-e0ea235eb16f',
-    name_label: 'ElasticSearch FMD',
-    status: 'disconnected',
-    device: 'eth1',
-    vlan: 102,
-    ip: '127.0.0.1',
-    mac: '00:1A:2B:3C:4D:5E',
-    ip_mode: 'DHCP',
-    selected: false,
-  },
-  {
-    id: '559b0b63-2173-b6a1-ef65-7155d56401b6',
-    name_label: 'flo-ubuntu',
-    status: 'connected',
-    device: 'eth1',
-    vlan: 100,
-    ip: '127.0.0.1',
-    mac: '00:1A:2B:3C:4D:5E',
-    ip_mode: 'DHCP',
-    selected: false,
-  },
-  {
-    id: '99e196e1-33f9-461b-93e3-d70a0eb29b3e',
-    name_label: 'Test BRS',
-    status: 'disconnected',
-    device: 'eth1',
-    vlan: 100,
-    ip: '127.0.0.1',
-    mac: '00:1A:2B:3C:4D:5E',
-    ip_mode: 'Static',
-    selected: false,
-  },
-  {
-    id: 'cd418934-1f70-22bc-688e-eb6da266aa57',
-    name_label: 'XCP-ng 8.2.1 UEFI Ext (TEe)',
-    status: 'connected',
-    device: 'eth1',
-    vlan: 100,
-    ip: '127.0.0.1',
-    mac: '00:1A:2B:3C:4D:5E',
-    ip_mode: 'DHCP',
-    selected: false,
-  },
-  {
-    id: '6d9c33fc-ebec-240d-e31b-18152911795d',
-    name_label: 'XCP-ng 8.3 A1 Lyon - BRS',
-    status: 'connected',
-    device: 'eth1',
-    vlan: 100,
-    ip: '127.0.0.1',
-    mac: '00:1A:2B:3C:4D:5E',
-    ip_mode: 'Static',
-    selected: false,
-  },
-]
-const cardsContent = [
-  {
-    name: 'PIF',
-    content: {
-      uuid: '71df26a2-678a-49c7-8232-8ebcac4987ab',
-      network: data[0].name_label,
-      device: data[0].device,
-      status: data[0].status,
-      physicalStatus: data[0].status,
-      vlan: data[0].vlan,
-      tags: ['prod', 'QA'],
-    },
-  },
-  {
-    name: 'Network Info',
-    content: {
-      ip_addresses: ['fe80::1a2b:3c4d:5f', '255.255.255.0', '255.255.255.0', '255.255.255.0', '255.255.255.0'],
-      mac_addresses: '00:1A:2B:3C:4D:5E',
-      netmask: '255.255.255.0',
-      dns: '8.8.8.8',
-      gateway: '192.168.1.1',
-      ip_mode: data[0].ip_mode,
-    },
-  },
-  {
-    name: 'Properties',
-    content: {
-      mtu: '1500',
-      speed: '1000 Mb/s',
-      network_block_device: 'Off',
-      default_locking_mode: 'True',
-    },
-  },
-]
+const { pifsByHost, isReady } = useNetworkStore().subscribe()
 
 const { filter } = useTreeFilter()
 
-// const definitions = defineTree(data, {
-//   getLabel: 'name_label',
-//   predicate,
-// })
-
-const totalItems = ref(data)
-
-// const { nodes: pifs } = useTree(definitions, { expand: false })
+const pifs = computed(() => pifsByHost.value.map(pif => ({ ...pif })))
+const selectedPif = computed(() => pifs.value as XoPif[])
 
 type NetworkStatus = 'connected' | 'disconnected' | 'other'
 type NetworkAccent = 'success' | 'warning' | 'danger'
 
-const states: Record<NetworkStatus, { icon: IconDefinition; accent: NetworkAccent }> = {
-  connected: { icon: faCheck, accent: 'success' },
-  disconnected: { icon: faCheck, accent: 'danger' },
-  other: { icon: faExclamation, accent: 'warning' },
+const states: Record<NetworkStatus, { text: string; icon: IconDefinition; accent: NetworkAccent }> = {
+  connected: { text: 'Connected', icon: faCheck, accent: 'success' },
+  disconnected: { text: 'Disconnected', icon: faCheck, accent: 'danger' },
+  other: { text: 'Disconnected from physical device', icon: faExclamation, accent: 'warning' },
+}
+const status = (pif: XoPif) => {
+  if (pif.attached && pif.carrier) {
+    return 'connected'
+  }
+  if (pif.attached && !pif.carrier) {
+    return 'other'
+  }
+  return 'disconnected'
 }
 
 const getStatusProps = (status: NetworkStatus) => states[status as NetworkStatus]
-const selectedItems = computed(() => totalItems.value.filter(item => item.selected).length)
+const selectedItems = computed(() => pifsByHost.value.filter(item => item.selected).length)
 
 const toggleSelect = (isSelected: boolean) => {
-  totalItems.value.forEach(item => {
+  pifs.value.forEach(item => {
     item.selected = isSelected
   })
-}
-const test = () => {
-  return false
 }
 </script>
 
