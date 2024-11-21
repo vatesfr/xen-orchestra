45fd76e0b5b1e83bed21e9a1b238b1ee52bb676b
Sébastian Czech <czechsebastian@icloud.com>
2024-11-21 14:30:11 +0100

    feat(xo6): feedback

---
+++ @xen-orchestra/web/src/pages/host/[id]/pifStatus.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="pif-status">
+    <VtsIcon
+      v-if="icon"
+      :accent="getStatusProps(status(pif)).accent"
+      :icon="faCircle"
+      :overlay-icon="getStatusProps(status(pif)).icon"
+    />
+    <p class="text-ellipsis" :class="{ 'typo p3-regular': card }">
+      {{ getStatusProps(status(pif)).text }}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { XoPif } from '@/types/xo/pif.type'
+import VtsIcon from '@core/components/icon/VtsIcon.vue'
+import type { IconDefinition } from '@fortawesome/fontawesome-common-types'
+import { faCheck, faCircle, faExclamation } from '@fortawesome/free-solid-svg-icons'
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+defineProps<{
+  icon?: IconDefinition
+  pif: XoPif
+  card?: boolean
+}>()
+
+const { t } = useI18n()
+
+type NetworkStatus = 'connected' | 'disconnected' | 'partial'
+type NetworkAccent = 'success' | 'warning' | 'danger'
+
+const states = computed<Record<NetworkStatus, { text: string; icon: IconDefinition; accent: NetworkAccent }>>(() => ({
+  connected: { text: t('connected'), icon: faCheck, accent: 'success' },
+  disconnected: { text: t('disconnected'), icon: faCheck, accent: 'danger' },
+  partial: { text: t('disconnected-from-physical-device'), icon: faExclamation, accent: 'warning' },
+}))
+
+const status = pif => {
+  if (pif.attached && pif.carrier) {
+    return 'connected'
+  }
+  if (pif.attached && !pif.carrier) {
+    return 'partial'
+  }
+  return 'disconnected'
+}
+
+const getStatusProps = (status: NetworkStatus) => states.value[status as NetworkStatus]
+</script>
+
+<style scoped lang="postcss">
+.pif-status {
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+}
+</style>
