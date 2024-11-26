e80b39e417a0dfe7de53e35aaf39d05e9526541a
Florent BEAUCHAMP <florent.beauchamp@vates.tech>
2024-11-26 14:39:39 +0100

    feat(backups): implement long term retention (#7999)
    * feat(backups): implement long terme retention
    
    long term retention sometimes called GFS ( Grand Father / Father / Son)
    is a way to promote some backup to be kept on a long time
    
    that way , the user can use the find the best equilibrium between
    storage and security
    
    This commit add the code mechanics to identify backup that
    can be deleted safely. It is intended to use with a form
    that ask the user for the number of day, week, month, and year
    for which XO will keep the most recent
    
    It extends the actual system of keeping the n most recent backup
    
    Keep in mind that the backup promoted by week and month can be decaled

--- @xen-orchestra/backups/_getOldEntries.mjs
+++ @xen-orchestra/backups/_getOldEntries.mjs
@@ -1,4 +1,136 @@
+import moment from 'moment-timezone'
+import assert from 'node:assert'
+
+function instantiateTimezonedDateCreator(timezone) {
+  return date => {
+    const transformed = timezone ? moment.tz(date, timezone) : moment(date)
+    assert.ok(transformed.isValid(), `date ${date} , timezone ${timezone} is invalid`)
+    return transformed
+  }
+}
+
+const LTR_DEFINITIONS = {
+  daily: {
+    makeDateFormatter: ({ firstHourOfTheDay = 0, dateCreator } = {}) => {
+      return date => {
+        const copy = dateCreator(date)
+        copy.hour(copy.hour() - firstHourOfTheDay)
+        return copy.format('YYYY-MM-DD')
+      }
+    },
+  },
+  weekly: {
+    makeDateFormatter: ({ firstDayOfWeek = 0 /* relative to timezone week start */, dateCreator } = {}) => {
+      return date => {
+        const copy = dateCreator(date)
+
+        copy.date(date.date() - firstDayOfWeek)
+        // warning, the year in term of week may different from YYYY
+        // since the computation of the first week of a year is timezone dependant
+        return copy.format('gggg-WW')
+      }
+    },
+    ancestor: 'daily',
+  },
+  monthly: {
+    makeDateFormatter: ({ firstDayOfMonth = 0, dateCreator } = {}) => {
+      return date => {
+        const copy = dateCreator(date)
+        copy.date(copy.date() - firstDayOfMonth)
+        return copy.format('YYYY-MM')
+      }
+    },
+    ancestor: 'weekly',
+  },
+  yearly: {
+    makeDateFormatter: ({ firstDayOfYear = 0, dateCreator } = {}) => {
+      return date => {
+        const copy = dateCreator(date)
+        copy.date(copy.date() - firstDayOfYear)
+        return copy.format('YYYY')
+      }
+    },
+    ancestor: 'monthly',
+  },
+}
+
 // returns all entries but the last retention-th
-export function getOldEntries(retention, entries) {
-  return entries === undefined ? [] : retention > 0 ? entries.slice(0, -retention) : entries
+/**
+ * return the entries too old to be kept
+ *  if multiple entries are i the same time bucket : keep only the most recent one
+ *  if an entry is valid in any of the bucket OR the  minRetentionCount : keep it
+ *  if a bucket is completly empty : it does not count as one, thus it may extend the retention
+ * @returns Array<Backup>
+ */
+export function getOldEntries(minRetentionCount, entries, { longTermRetention = {}, timezone } = {}) {
+  assert.strictEqual(
+    typeof minRetentionCount,
+    'number',
+    `minRetentionCount must be a number, got ${JSON.stringify(minRetentionCount)}`
+  )
+  assert.strictEqual(
+    minRetentionCount >= 0,
+    true,
+    `minRetentionCount must be a positive number, got ${JSON.stringify(minRetentionCount)}`
+  )
+  const dateBuckets = {}
+  const dateCreator = instantiateTimezonedDateCreator(timezone)
+  // only check buckets that have a retention set
+  for (const [duration, { retention, settings }] of Object.entries(longTermRetention)) {
+    assert.notStrictEqual(LTR_DEFINITIONS[duration], undefined, `Retention of type ${duration} is not defined`)
+    assert.notStrictEqual(
+      timezone,
+      undefined,
+      `timezone must defined for ltr, got ${JSON.stringify({ minRetentionCount, longTermRetention, timezone })}`
+    )
+    assert.strictEqual(
+      typeof retention,
+      'number',
+      `retention  of type ${duration} must be a number, got ${JSON.stringify(retention)}`
+    )
+    assert.strictEqual(
+      retention > 0,
+      true,
+      `retention  of type ${duration} must be a positive number, got ${JSON.stringify(retention)}`
+    )
+    dateBuckets[duration] = {
+      remaining: retention,
+      lastMatchingBucket: null,
+      formatter: LTR_DEFINITIONS[duration].makeDateFormatter({ ...settings, dateCreator }),
+    }
+  }
+  const nb = entries.length
+  const oldEntries = []
+
+  for (let i = nb - 1; i >= 0; i--) {
+    const entry = entries[i]
+    const entryDate = dateCreator(entry.timestamp)
+    let shouldBeKept = false
+    for (const [duration, { remaining, lastMatchingBucket, formatter }] of Object.entries(dateBuckets)) {
+      if (remaining === 0) {
+        continue
+      }
+      const bucket = formatter(entryDate)
+      if (lastMatchingBucket !== bucket) {
+        if (lastMatchingBucket !== null) {
+          assert.strictEqual(
+            lastMatchingBucket > bucket,
+            true,
+            `entries must be sorted in asc order ${lastMatchingBucket} ${bucket}`
+          )
+        }
+        shouldBeKept = true
+        dateBuckets[duration].remaining -= 1
+        dateBuckets[duration].lastMatchingBucket = bucket
+      }
+    }
+    if (i >= nb - minRetentionCount) {
+      shouldBeKept = true
+    }
+    if (!shouldBeKept) {
+      oldEntries.push(entry)
+    }
+  }
+  // we expect the entries to be in the right order
+  return oldEntries.reverse()
 }
---
+++ @xen-orchestra/backups/_getOldEntries.test.mjs
@@ -0,0 +1,282 @@
+import { describe, it } from 'node:test'
+import assert from 'node:assert/strict'
+
+import { getOldEntries } from './_getOldEntries.mjs'
+
+describe('_getOldEntries() should succeed', () => {
+  const tests = [
+    {
+      args: [
+        1,
+        [
+          { timestamp: 1, id: 1 },
+          { timestamp: 3, id: 2 },
+          { timestamp: 2, id: 3 },
+        ],
+      ],
+      expectedIds: [1, 2],
+      testLabel: 'should  handle number based retention ',
+    },
+
+    {
+      args: [
+        0,
+        [
+          { timestamp: +new Date('2024-09-01 00:01:00'), id: 1 }, // too old
+          { timestamp: +new Date('2024-09-01 00:00:00'), id: 2 }, // too old
+          { timestamp: +new Date('2024-09-02 00:09:00'), id: 3 }, // too old in same day
+          { timestamp: +new Date('2024-09-02 00:10:00'), id: 4 },
+          { timestamp: +new Date('2024-09-03 00:09:00'), id: 5 },
+          { timestamp: +new Date('2024-09-04 00:09:00'), id: 6 }, // too old in same day
+          { timestamp: +new Date('2024-09-04 00:10:00'), id: 7 },
+        ],
+        {
+          longTermRetention: {
+            daily: { retention: 3 },
+          },
+          timezone: 'Europe/Paris',
+        },
+      ],
+      expectedIds: [1, 2, 3, 6],
+      testLabel: 'should  handle day based retention  ',
+    },
+    {
+      args: [
+        0,
+        [
+          { timestamp: +new Date('2024-09-01 00:01:00'), id: 1 }, // week n-3 too old
+          { timestamp: +new Date('2024-09-02 00:00:00'), id: 2 }, // week n-3 too old
+          { timestamp: +new Date('2024-09-03 00:09:00'), id: 3 }, // week n-2
+          { timestamp: +new Date('2024-09-04 00:09:00'), id: 4 }, // week n-2
+          { timestamp: +new Date('2024-09-05 00:09:00'), id: 5 }, // week n-2
+          { timestamp: +new Date('2024-09-06 00:09:00'), id: 6 }, // week n-2
+          { timestamp: +new Date('2024-09-07 00:09:00'), id: 7 }, // week n-2, most recent kept
+          { timestamp: +new Date('2024-09-09 00:09:00'), id: 8 }, // week n-1, too old
+          { timestamp: +new Date('2024-09-15 00:09:00'), id: 9 }, // week n-1  kept
+          { timestamp: +new Date('2024-09-22 00:09:00'), id: 10 }, // week n  kept
+        ],
+        {
+          longTermRetention: {
+            weekly: { retention: 3 },
+          },
+          timezone: 'Europe/Paris',
+        },
+      ],
+      expectedIds: [1, 2, 3, 4, 5, 6, 8],
+      testLabel: 'should  handle week based retention  ',
+    },
+    {
+      args: [
+        0,
+        [
+          { timestamp: +new Date('2024-06-22 00:09:00'), id: 1 }, // too old
+          { timestamp: +new Date('2024-07-31 00:09:00'), id: 2 }, // first of july
+          { timestamp: +new Date('2024-08-01 00:09:00'), id: 3 }, // older of august
+          { timestamp: +new Date('2024-08-05 00:09:00'), id: 4 }, // older of august
+          { timestamp: +new Date('2024-08-07 00:09:00'), id: 5 }, // most recent of august
+          { timestamp: +new Date('2024-09-09 00:09:00'), id: 6 }, // older of september
+          { timestamp: +new Date('2024-09-15 00:09:00'), id: 7 }, // older of september
+          { timestamp: +new Date('2024-09-22 00:09:00'), id: 8 }, // most recent of september
+        ],
+        {
+          longTermRetention: {
+            monthly: { retention: 3 },
+          },
+          timezone: 'Europe/Paris',
+        },
+      ],
+      expectedIds: [1, 3, 4, 6, 7],
+      testLabel: 'should  handle month based retention',
+    },
+    {
+      args: [
+        0,
+        [
+          { timestamp: +new Date('2023-05-18 00:09:00'), id: 1 }, // too old
+          { timestamp: +new Date('2024-06-15 00:09:00'), id: 2 }, // too old in same year
+          { timestamp: +new Date('2024-07-04 00:09:00'), id: 3 }, // too old
+          { timestamp: +new Date('2024-08-12 00:09:00'), id: 4 }, // too old
+          { timestamp: +new Date('2024-09-05 00:09:00'), id: 5 }, // too old
+          { timestamp: +new Date('2024-10-02 00:09:00'), id: 6 }, // new month,
+          { timestamp: +new Date('2024-11-01 00:09:00'), id: 7 }, // new month , week reached retention
+          { timestamp: +new Date('2024-12-17 00:09:00'), id: 8 }, // new week
+          { timestamp: +new Date('2024-12-24 00:09:00'), id: 9 }, // new week/month / year  daily reach retention
+          { timestamp: +new Date('2025-01-01 00:09:00'), id: 10 }, // same day/week/month/year
+          { timestamp: +new Date('2025-01-01 00:10:00'), id: 11 }, //  new  day / week / month
+          { timestamp: +new Date('2025-12-31 00:09:00'), id: 12 }, // same day/week/month/year
+          { timestamp: +new Date('2025-12-31 00:09:00'), id: 13 }, // new month /year
+        ],
+        {
+          longTermRetention: {
+            daily: { retention: 2 },
+            weekly: { retention: 4 },
+            monthly: { retention: 5 },
+            yearly: { retention: 2 },
+          },
+          timezone: 'Europe/Paris', // use a time zone here because week definition is timezone bound
+        },
+      ],
+      expectedIds: [1, 2, 3, 4, 5, 10, 12],
+      testLabel: 'complete test  ',
+    },
+    {
+      args: [
+        0,
+        [
+          { timestamp: +new Date('2024-09-01 00:01:00'), id: 1 }, // thrid day too old
+          { timestamp: +new Date('2024-09-01 00:10:00'), id: 2 }, // thrid day
+          { timestamp: +new Date('2024-09-01 00:20:00'), id: 3 }, // second day, too old
+          { timestamp: +new Date('2024-09-02 00:22:00'), id: 4 }, // second day too old
+          { timestamp: +new Date('2024-09-03 00:20:00'), id: 5 }, // second day in NZ
+          { timestamp: +new Date('2024-09-04 00:09:00'), id: 6 }, // same day in NZ
+          { timestamp: +new Date('2024-09-04 00:10:00'), id: 7 }, // most recent
+        ],
+        {
+          longTermRetention: {
+            daily: { retention: 3 },
+          },
+          timezone: 'Pacific/Auckland', // GMT +6
+        },
+      ],
+      expectedIds: [1, 2, 3, 6],
+      testLabel: 'should  handle timezone ',
+    },
+  ]
+
+  for (const { args, expectedIds, testLabel } of tests) {
+    it(testLabel, () => {
+      const oldEntries = getOldEntries.apply(null, args)
+      assert.strictEqual(
+        oldEntries.length,
+        expectedIds.length,
+        `different length , ${JSON.stringify({ oldEntries, expectedIds })}`
+      )
+      for (let i = 0; i < expectedIds.length; i++) {
+        assert.strictEqual(
+          oldEntries[i].id,
+          expectedIds[i],
+          `different id , ${JSON.stringify({ i, expectedIds, oldEntries })}`
+        )
+      }
+    })
+  }
+})
+
+describe('_getOldEntries() should fail when called incorrectly', () => {
+  const tests = [
+    {
+      args: [
+        -1,
+        [
+          { timestamp: 1, id: 1 },
+          { timestamp: 3, id: 2 },
+          { timestamp: 2, id: 3 },
+        ],
+      ],
+      testLabel: 'negative min retention ',
+    },
+    {
+      args: [
+        { not: 'a number' },
+        [
+          { timestamp: 1, id: 1 },
+          { timestamp: 3, id: 2 },
+          { timestamp: 2, id: 3 },
+        ],
+      ],
+      testLabel: 'non number min retention ',
+    },
+    {
+      args: [
+        4,
+        [
+          { timestamp: 1, id: 1 },
+          { timestamp: 2, id: 2 },
+          { timestamp: 3, id: 3 },
+        ],
+        {
+          longTermRetention: {
+            invalidType: { retention: 3 },
+          },
+          timezone: 'Europe/Paris',
+        },
+      ],
+      testLabel: 'invalid type ltr ',
+    },
+    {
+      args: [
+        3,
+        [
+          { timestamp: 1, id: 1 },
+          { timestamp: 3, id: 2 },
+          { timestamp: 2, id: 3 },
+        ],
+        {
+          longTermRetention: {
+            daily: { retention: 0 },
+          },
+          timezone: 'Europe/Paris',
+        },
+      ],
+      testLabel: 'zero retention ltr ',
+    },
+    {
+      args: [
+        1,
+        [
+          { timestamp: 1, id: 1 },
+          { timestamp: 2, id: 2 },
+          { timestamp: 3, id: 3 },
+        ],
+        {
+          longTermRetention: {
+            daily: { retention: 'not a number' },
+          },
+          timezone: 'Europe/Paris',
+        },
+      ],
+      testLabel: 'no timezone ltr ',
+    },
+
+    {
+      args: [
+        5,
+        [
+          { timestamp: +new Date('2024-09-01'), id: 1 },
+          { timestamp: +new Date('2024-09-03'), id: 3 },
+          { timestamp: +new Date('2024-09-02'), id: 2 },
+        ],
+        {
+          longTermRetention: {
+            daily: { retention: 5 },
+          },
+          timezone: 'Europe/Paris',
+        },
+      ],
+      testLabel: 'unsorted entries ',
+    },
+
+    {
+      args: [
+        5,
+        [
+          { timestamp: 'notadate', id: 1 },
+          { timestamp: 'still not a date, but after', id: 3 },
+        ],
+        {
+          longTermRetention: {
+            daily: { retention: 5 },
+          },
+          timezone: 'Europe/Paris',
+        },
+      ],
+      testLabel: 'broken date ',
+    },
+  ]
+
+  for (const { args, testLabel } of tests) {
+    it(testLabel, () => {
+      assert.throws(() => getOldEntries.apply(null, args), { code: 'ERR_ASSERTION' })
+    })
+  }
+})
