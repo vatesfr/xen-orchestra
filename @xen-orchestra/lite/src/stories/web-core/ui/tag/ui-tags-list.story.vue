296dd02f728a113b139d200b956d7cb1ad3449c7
Sébastian Czech <czechsebastian@icloud.com>
2024-11-29 09:39:23 +0100

    feat(web-core): feedback

--- @xen-orchestra/lite/src/stories/web-core/ui/tag/ui-tags-list.story.vue
+++ @xen-orchestra/lite/src/stories/web-core/ui/tag/ui-tags-list.story.vue
@@ -1,31 +1,20 @@
 <template>
   <ComponentStory
-    v-slot="{ properties, settings }"
-    :params="[
-      iconProp(),
-      prop('accent')
-        .type('TagAccent')
-        .required()
-        .preset('info')
-        .enum('info', 'neutral', 'success', 'warning', 'danger', 'muted')
-        .widget(),
-      prop('variant').type('TagVariant').required().preset('primary').enum('primary', 'secondary').widget(),
-      slot(),
-      setting('slot').preset('Label').widget(),
-    ]"
+    v-slot="{ settings }"
+    :params="[slot().help('Meant to receive a list of UiTag components'), setting('slot').preset('Label').widget()]"
   >
     <UiTagsList>
-      <UiTag v-bind="properties">{{ settings.slot }}</UiTag>
-      <UiTag v-bind="properties">{{ settings.slot }}</UiTag>
-      <UiTag v-bind="properties">{{ settings.slot }}</UiTag>
-      <UiTag v-bind="properties">{{ settings.slot }}</UiTag>
+      <UiTag accent="info" variant="primary">{{ settings.slot }}</UiTag>
+      <UiTag accent="success" variant="primary">{{ settings.slot }}</UiTag>
+      <UiTag accent="warning" variant="secondary">{{ settings.slot }}</UiTag>
+      <UiTag accent="danger" variant="secondary">{{ settings.slot }}</UiTag>
     </UiTagsList>
   </ComponentStory>
 </template>
 
 <script lang="ts" setup>
 import ComponentStory from '@/components/component-story/ComponentStory.vue'
-import { iconProp, prop, setting, slot } from '@/libs/story/story-param'
+import { setting, slot } from '@/libs/story/story-param'
 import UiTag from '@core/components/ui/tag/UiTag.vue'
 import UiTagsList from '@core/components/ui/tag/UiTagsList.vue'
 </script>
