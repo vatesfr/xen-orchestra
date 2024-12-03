e3a0e52df161c507b2e8d9974c89525f54e5b92a
Joris-k <joris.kosacki@vates.tech>
2024-12-03 14:07:36 +0100

    feat(xo-lite): adding pif status component, some improvement and css

---
+++ @xen-orchestra/lite/src/components/pool/network/PoolNetworksPifStatus.vue
@@ -0,0 +1,24 @@
+<template>
+  <UiInfo class="text-ellipsis" :accent="statusProps.accent">
+    <span class="text-ellipsis">{{ statusProps.text }}</span>
+  </UiInfo>
+</template>
+
+<script setup lang="ts">
+import UiInfo from '@core/components/ui/info/UiInfo.vue'
+import { computed } from 'vue'
+
+const { pif } = defineProps<{
+  pif: { status: 'connected' | 'disconnected' | 'partially_connected' }
+}>()
+
+type NetworkAccent = 'success' | 'warning' | 'danger'
+
+const statusMap: Record<string, { text: string; accent: NetworkAccent }> = {
+  connected: { text: 'Connected', accent: 'success' },
+  disconnected: { text: 'Disconnected', accent: 'danger' },
+  partially_connected: { text: 'Partially Connected', accent: 'warning' },
+}
+
+const statusProps = computed(() => statusMap[pif.status])
+</script>
