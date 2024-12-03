69347e9a4b5e7fef17184f45f7a76d47ab15e827
Sebastian CZECH <sebastian.czech@vates.tech>
2024-12-03 15:45:32 +0100

    feat(web-core): create tags list component (#8165)

---
+++ @xen-orchestra/lite/src/stories/web-core/ui/tag/ui-tags-list.story.vue
@@ -0,0 +1,20 @@
+<template>
+  <ComponentStory
+    v-slot="{ settings }"
+    :params="[slot().help('Meant to receive a list of UiTag components'), setting('slot').preset('Label').widget()]"
+  >
+    <UiTagsList>
+      <UiTag accent="info" variant="primary">{{ settings.slot }}</UiTag>
+      <UiTag accent="success" variant="primary">{{ settings.slot }}</UiTag>
+      <UiTag accent="warning" variant="secondary">{{ settings.slot }}</UiTag>
+      <UiTag accent="danger" variant="secondary">{{ settings.slot }}</UiTag>
+    </UiTagsList>
+  </ComponentStory>
+</template>
+
+<script lang="ts" setup>
+import ComponentStory from '@/components/component-story/ComponentStory.vue'
+import { setting, slot } from '@/libs/story/story-param'
+import UiTag from '@core/components/ui/tag/UiTag.vue'
+import UiTagsList from '@core/components/ui/tag/UiTagsList.vue'
+</script>
---
+++ @xen-orchestra/web-core/lib/components/ui/tag/UiTagsList.vue
@@ -0,0 +1,14 @@
+<!-- v1 -->
+<template>
+  <div class="ui-tags-list">
+    <slot />
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+.ui-tags-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.4rem;
+}
+</style>
