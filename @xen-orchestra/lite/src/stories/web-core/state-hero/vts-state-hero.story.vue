b459631682f329cbf6671166bcb115d06c26755b
Sébastian Czech <czechsebastian@icloud.com>
2024-12-03 15:08:04 +0100

    feat(web-core): add no data in story

--- @xen-orchestra/lite/src/stories/web-core/state-hero/vts-state-hero.story.vue
+++ @xen-orchestra/lite/src/stories/web-core/state-hero/vts-state-hero.story.vue
@@ -4,7 +4,7 @@
     :params="[
       prop('type').required().enum('page', 'card', 'panel', 'table').preset('card').widget(),
       prop('busy').bool().widget(),
-      prop('image').enum('no-result', 'under-construction', 'no-selection', 'error').widget(),
+      prop('image').enum('no-result', 'under-construction', 'no-selection', 'error', 'no-data').widget(),
       slot(),
       setting('defaultSlot').preset('Some text').widget(),
     ]"
