6b2efe7039113b2393c5d157719486d8082587a3
Sebastian CZECH <sebastian.czech@vates.tech>
2024-12-03 14:34:03 +0100

    feat(web-core): add no selection state component (#8170)

---
+++ @xen-orchestra/lite/src/stories/web-core/state-hero/vts-no-selection-hero.story.vue
@@ -0,0 +1,14 @@
+<template>
+  <ComponentStory
+    v-slot="{ properties }"
+    :params="[prop('type').required().enum('page', 'card', 'panel').preset('card').widget()]"
+  >
+    <VtsNoSelectionHero v-bind="properties" />
+  </ComponentStory>
+</template>
+
+<script lang="ts" setup>
+import ComponentStory from '@/components/component-story/ComponentStory.vue'
+import { prop } from '@/libs/story/story-param'
+import VtsNoSelectionHero from '@core/components/state-hero/VtsNoSelectionHero.vue'
+</script>
