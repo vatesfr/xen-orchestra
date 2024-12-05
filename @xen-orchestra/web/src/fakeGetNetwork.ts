a81231290a25f89400314023bb2977f3414a782a
Sébastian Czech <czechsebastian@icloud.com>
2024-12-05 11:20:33 +0100

    feat(xo-6): add store

--- @xen-orchestra/web/src/fakeGetNetwork.ts
+++
@@ -1,8 +0,0 @@
-export const getNetwork = {
-  id: 'd9424d50-def7-9882-64d3-4f305c87fb7a',
-  name_label: 'Production Management (Only For Olivier)',
-  nbd: false,
-  tags: ['Prod', 'Delete', 'QA'],
-  href: '/rest/v0/networks/d9424d50-def7-9882-64d3-4f305c87fb7a',
-  defaultIsLocked: false,
-}
