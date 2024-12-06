a0ba86d41624cf66d340a7e8fef5a02114710a75
Joris-k <joris.kosacki@vates.tech>
2024-12-06 17:40:13 +0100

    feat(xo-lite): add host network tab and pif table / status component and pif store

--- @xen-orchestra/lite/src/views/host/HostNetworkView.vue
+++ @xen-orchestra/lite/src/views/host/HostNetworkView.vue
@@ -1,11 +1,24 @@
 <template>
-  <PageUnderConstruction />
+  <UiCard class="host-network-view">
+    <HostPifTable :pifs="pifs" :is-ready />
+  </UiCard>
 </template>
 
 <script lang="ts" setup>
-import PageUnderConstruction from '@/components/PageUnderConstruction.vue'
+import HostPifTable from '@/components/host/network/HostPifTable.vue'
 import { usePageTitleStore } from '@/stores/page-title.store'
+import { usePifStore } from '@/stores/xen-api/pif.store'
+import UiCard from '@core/components/ui/card/UiCard.vue'
 import { useI18n } from 'vue-i18n'
 
 usePageTitleStore().setTitle(useI18n().t('network'))
+
+const { currentHostPifs: pifs, isReady } = usePifStore().subscribe()
 </script>
+
+<style lang="postcss" scoped>
+.host-network-view {
+  border: solid 0.1rem var(--color-neutral-border);
+  margin: 0.8rem;
+}
+</style>
