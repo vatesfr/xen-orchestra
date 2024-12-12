2844e5a1e711f33bc76882d6dee4f1fb74a2c1ce
Joris-k <joris.kosacki@vates.tech>
2024-12-12 20:41:28 +0100

    review feedback

--- @xen-orchestra/lite/src/components/pool/network/PoolNetworksPifStatus.vue
+++ @xen-orchestra/lite/src/components/pool/network/PoolNetworksPifStatus.vue
@@ -9,11 +9,11 @@ import UiInfo from '@core/components/ui/info/UiInfo.vue'
 import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 
-type Status = 'connected' | 'disconnected' | 'partial'
+export type Status = 'connected' | 'disconnected' | 'partial'
 type Accent = 'success' | 'warning' | 'danger'
 
 const { status } = defineProps<{
-  status: Status
+  status?: Status
 }>()
 
 const { t } = useI18n()
@@ -24,5 +24,5 @@ const statusMap: Record<Status, { text: string; accent: Accent }> = {
   partial: { text: t('partially-connected'), accent: 'warning' },
 }
 
-const statusProps = computed(() => statusMap[status])
+const statusProps = computed(() => statusMap[status!])
 </script>
