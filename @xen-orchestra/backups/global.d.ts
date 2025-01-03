d87358091a012c6934702e8e0636f3e4fda0f7d4
Stéphane MEYER <stephane.meyer@vates.tech>
2025-01-03 20:47:15 +0100

    add types

--- @xen-orchestra/backups/global.d.ts
+++ @xen-orchestra/backups/global.d.ts
@@ -1,2 +1,7 @@
 declare module "limit-concurrency-decorator";
 declare module "promise-toolbox";
+declare module '@xen-orchestra/fs/abstract' {
+  export default class RemoteHandlerAbstract {
+    // Types ici
+  }
+}
---
+++ @xen-orchestra/fs/src/checksum.d.ts
@@ -0,0 +1,2 @@
+export function createChecksumStream(algorithm?: string): any;
+export function validChecksumOfReadStream(stream: any, expectedChecksum: any): any;
