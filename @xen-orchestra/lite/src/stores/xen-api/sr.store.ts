e8b869821f4893c4f1211ea8e967932ef89e07a1
Joris-K <joris.kosacki@vates.tech>
2025-01-06 10:48:59 +0100

    add list of iso dvd and display management of radio button

--- @xen-orchestra/lite/src/stores/xen-api/sr.store.ts
+++ @xen-orchestra/lite/src/stores/xen-api/sr.store.ts
@@ -1,9 +1,49 @@
+import type { XenApiSr } from '@/libs/xen-api/xen-api.types'
 import { createXapiStoreConfig } from '@/stores/xen-api/create-xapi-store-config'
+import { useVdiStore } from '@/stores/xen-api/vdi.store'
 import { createSubscribableStoreContext } from '@core/utils/create-subscribable-store-context.util'
 import { defineStore } from 'pinia'
+import { computed } from 'vue'
 
 export const useSrStore = defineStore('xen-api-sr', () => {
-  const config = createXapiStoreConfig('sr')
+  const deps = {
+    vdiStore: useVdiStore(),
+  }
 
-  return createSubscribableStoreContext(config, {})
+  const vdiContext = deps.vdiStore.getContext()
+
+  const { context: baseContext, ...configRest } = createXapiStoreConfig('sr')
+
+  const srs = computed(() => baseContext.records.value)
+  const srsName = (ref: XenApiSr['$ref']) => baseContext.getByOpaqueRef(ref)?.name_label
+
+  const getSrWithISO = computed(() => srs.value.filter(sr => sr.type === 'iso'))
+
+  console.log('getSrWithISO', getSrWithISO)
+
+  const concatVidsArray = computed(() => getSrWithISO.value.flatMap(sr => sr.VDIs || []))
+
+  const vdisGroupedBySrName = computed(() => {
+    const groupedVdis: Record<string, XenApiSr[]> = {}
+
+    concatVidsArray.value.forEach(vdiRef => {
+      const vdi = vdiContext.getByOpaqueRef(vdiRef)
+      if (vdi) {
+        const srName = srsName(vdi.SR) || 'Unknown SR'
+        if (!groupedVdis[srName]) {
+          groupedVdis[srName] = []
+        }
+        groupedVdis[srName].push(vdi)
+      }
+    })
+
+    return groupedVdis
+  })
+
+  const context = {
+    ...baseContext,
+    vdisGroupedBySrName,
+  }
+
+  return createSubscribableStoreContext({ context, ...configRest }, deps)
 })
