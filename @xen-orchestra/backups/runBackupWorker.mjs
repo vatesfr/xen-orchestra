c73670afd1f18a45b046479ce9d561fe6a2ce321
Stéphane MEYER <stephane.meyer@vates.tech>
2025-01-16 19:07:53 +0100

    configurable port

--- @xen-orchestra/backups/runBackupWorker.mjs
+++ @xen-orchestra/backups/runBackupWorker.mjs
@@ -4,13 +4,21 @@ import { fork } from 'child_process'
 const { warn } = createLogger('xo:backups:backupWorker')
 
 const PATH = new URL('_backupWorker.mjs', import.meta.url).pathname
+const DEFAULT_INSPECTOR_PORT = 9229
 
 export function runBackupWorker(params, onLog) {
   return new Promise((resolve, reject) => {
-    const inspectArg = process.execArgv.find(arg => arg.startsWith('--inspect'))
+    // run Node inspector on port+1 if --inspect or --inspect-brk
+    const inspectArg = process.execArgv.find(arg => arg.startsWith('--inspect') || arg.startsWith('--inspect-brk'))
     const execArgv = inspectArg
-      ? [inspectArg.replace(/^(--inspect)(=.*)?$/, (_, prefix) => `${prefix}=localhost:9230`)]
+      ? [
+          inspectArg.replace(/^(--inspect(-brk)?)(=([^:]+:)?(\d+))?$/, (_, prefix, brk, _fullMatch, host, port) => {
+            const basePort = port ? parseInt(port) : DEFAULT_INSPECTOR_PORT
+            return `${prefix}=${host || ''}${basePort + 1}`
+          }),
+        ]
       : []
+
     const worker = fork(PATH, [], { execArgv })
 
     worker.on('exit', (code, signal) => reject(new Error(`worker exited with code ${code} and signal ${signal}`)))
