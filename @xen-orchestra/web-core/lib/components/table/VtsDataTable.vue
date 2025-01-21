f8d6a1830abdf2031b70a25bad3ffb266b0439c5
Joris-K <joris.kosacki@vates.tech>
2025-01-21 18:22:34 +0100

    improvement

---
+++ @xen-orchestra/web-core/lib/components/table/VtsDataTable.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="vts-data-table">
+    <VtsLoadingHero :disabled="isReady" type="table">
+      <VtsTable vertical-border>
+        <thead>
+          <slot name="thead" />
+        </thead>
+        <tbody>
+          <slot name="tbody" />
+        </tbody>
+      </VtsTable>
+    </VtsLoadingHero>
+    <VtsErrorNoDataHero v-if="hasError" type="table" />
+    <VtsStateHero v-if="noDataMessage" type="table" image="no-data">
+      {{ noDataMessage }}
+    </VtsStateHero>
+  </div>
+</template>
+
+<script setup lang="ts">
+import VtsErrorNoDataHero from '@core/components/state-hero/VtsErrorNoDataHero.vue'
+import VtsLoadingHero from '@core/components/state-hero/VtsLoadingHero.vue'
+import VtsStateHero from '@core/components/state-hero/VtsStateHero.vue'
+import VtsTable from '@core/components/table/VtsTable.vue'
+
+defineProps<{
+  isReady?: boolean
+  hasError?: boolean
+  noDataMessage?: string
+}>()
+defineSlots<{
+  thead(): any
+  tbody(): any
+}>()
+</script>
