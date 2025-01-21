b598c9190640412ca648d8ae1d238253938eac62
Joris-K <joris.kosacki@vates.tech>
2025-01-21 17:35:08 +0100

    implement new component vtsDataTable

--- @xen-orchestra/lite/src/stores/xen-api/pool.store.ts
+++ @xen-orchestra/lite/src/stores/xen-api/pool.store.ts
@@ -1,4 +1,4 @@
-import type { XenApiPool } from '@/libs/xen-api/xen-api.types'
+import type { XenApiPif, XenApiPool } from '@/libs/xen-api/xen-api.types'
 import { createXapiStoreConfig } from '@/stores/xen-api/create-xapi-store-config'
 import { createSubscribableStoreContext } from '@core/utils/create-subscribable-store-context.util'
 import { defineStore } from 'pinia'
@@ -9,9 +9,12 @@ export const usePoolStore = defineStore('xen-api-pool', () => {
 
   const pool = computed<XenApiPool | undefined>(() => baseContext.records.value[0])
 
+  const isPoolMaster = (hostRef: XenApiPif['host']) => pool.value?.master === hostRef
+
   const context = {
     ...baseContext,
     pool,
+    isPoolMaster,
   }
 
   return createSubscribableStoreContext({ context, ...configRest }, {})
