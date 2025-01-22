42f205e4738b5f40264ae5f1662796336d3372c1
Sebastian CZECH <sebastian.czech@vates.tech>
2025-01-22 14:55:01 +0100

    feat(xo-6): switch to language based on xo5 cookie (#8242)

--- @xen-orchestra/web/src/App.vue
+++ @xen-orchestra/web/src/App.vue
@@ -8,13 +8,20 @@
 <script lang="ts" setup>
 import AppLayout from '@/layouts/AppLayout.vue'
 import VtsTooltipList from '@core/components/tooltip-list/VtsTooltipList.vue'
+import { locales } from '@core/i18n'
 import { useUiStore } from '@core/stores/ui.store'
 import { useActiveElement, useMagicKeys, whenever } from '@vueuse/core'
+import { useCookies } from '@vueuse/integrations/useCookies'
 import { logicAnd } from '@vueuse/math'
 import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 
 const uiStore = useUiStore()
+const { locale } = useI18n()
+const { get } = useCookies()
+
+const cookieLang = get('lang')
+locale.value = cookieLang && locales[cookieLang] ? cookieLang : 'en'
 
 if (import.meta.env.DEV) {
   const { locale } = useI18n()
