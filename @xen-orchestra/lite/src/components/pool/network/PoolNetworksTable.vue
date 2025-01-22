1fef24af2bce8619601715712419f7106ba08eef
Joris-K <joris.kosacki@vates.tech>
2025-01-22 17:40:57 +0100

    review feedback

--- @xen-orchestra/lite/src/components/pool/network/PoolNetworksTable.vue
+++ @xen-orchestra/lite/src/components/pool/network/PoolNetworksTable.vue
@@ -15,47 +15,47 @@
         </UiDropdownButton>
       </template>
     </UiTitle>
-    <div class="table-actions">
-      <UiQuerySearchBar class="table-query" @search="value => (searchQuery = value)" />
-      <UiTableActions :title="t('table-actions')">
-        <UiButton
-          v-tooltip="$t('coming-soon')"
-          disabled
-          :left-icon="faEdit"
-          variant="tertiary"
-          accent="info"
-          size="medium"
-        >
-          {{ $t('edit') }}
-        </UiButton>
-        <UiButton
-          v-tooltip="$t('coming-soon')"
-          disabled
-          :left-icon="faCopy"
-          variant="tertiary"
-          accent="info"
-          size="medium"
-        >
-          {{ $t('copy-info-json') }}
-        </UiButton>
-        <UiButton
-          v-tooltip="$t('coming-soon')"
-          disabled
-          :left-icon="faTrash"
-          variant="tertiary"
-          accent="danger"
-          size="medium"
-        >
-          {{ $t('delete') }}
-        </UiButton>
-      </UiTableActions>
-      <UiTopBottomTable
-        :selected-items="selected.length"
-        :total-items="networkUuids.length"
-        @toggle-select-all="toggleSelect"
-      />
-    </div>
-    <div class="table-container">
+    <div class="container">
+      <div class="table-actions">
+        <UiQuerySearchBar @search="value => (searchQuery = value)" />
+        <UiTableActions :title="t('table-actions')">
+          <UiButton
+            v-tooltip="$t('coming-soon')"
+            disabled
+            :left-icon="faEdit"
+            variant="tertiary"
+            accent="info"
+            size="medium"
+          >
+            {{ $t('edit') }}
+          </UiButton>
+          <UiButton
+            v-tooltip="$t('coming-soon')"
+            disabled
+            :left-icon="faCopy"
+            variant="tertiary"
+            accent="info"
+            size="medium"
+          >
+            {{ $t('copy-info-json') }}
+          </UiButton>
+          <UiButton
+            v-tooltip="$t('coming-soon')"
+            disabled
+            :left-icon="faTrash"
+            variant="tertiary"
+            accent="danger"
+            size="medium"
+          >
+            {{ $t('delete') }}
+          </UiButton>
+        </UiTableActions>
+        <UiTopBottomTable
+          :selected-items="selected.length"
+          :total-items="networkUuids.length"
+          @toggle-select-all="toggleSelect"
+        />
+      </div>
       <VtsDataTable
         :is-ready
         :has-error
@@ -67,11 +67,11 @@
               <th v-if="column.id === 'checkbox'" class="checkbox">
                 <UiCheckbox :v-model="areAllSelected" accent="info" @update:model-value="toggleSelect" />
               </th>
-              <th v-else-if="column.id === 'more'" class="more">
-                <UiButtonIcon size="small" accent="info" :icon="getHeaderIcon(column.id)" />
+              <th v-else-if="column.id === 'more'" v-tooltip="$t('coming-soon')" class="more">
+                <UiButtonIcon size="small" accent="info" :icon="faEllipsis" />
                 {{ column.label }}
               </th>
-              <ColumnTitle v-else :icon="getHeaderIcon(column.id)">
+              <ColumnTitle v-else :icon="headerIcon[column.id]">
                 <span class="text-ellipsis">{{ column.label }}</span>
               </ColumnTitle>
             </template>
@@ -84,9 +84,19 @@
             :class="{ selected: selectedNetworkId === row.id }"
             @click="selectedNetworkId = row.id"
           >
-            <td v-for="column of row.visibleColumns" :key="column.id" class="typo p2-regular">
+            <td
+              v-for="column of row.visibleColumns"
+              :key="column.id"
+              class="typo p2-regular"
+              :class="{ checkbox: column.id === 'checkbox' }"
+            >
               <UiCheckbox v-if="column.id === 'checkbox'" v-model="selected" accent="info" :value="row.id" />
-              <VtsIcon v-else-if="column.id === 'more'" accent="info" :icon="faEllipsis" />
+              <VtsIcon
+                v-else-if="column.id === 'more'"
+                v-tooltip="$t('coming-soon')"
+                accent="info"
+                :icon="faEllipsis"
+              />
               <VtsConnectionStatus v-else-if="column.id === 'status'" :status="column.value" />
               <div v-else v-tooltip="{ placement: 'bottom-end' }" class="text-ellipsis">
                 {{ column.value }}
@@ -109,9 +119,10 @@
 
 <script setup lang="ts">
 import useMultiSelect from '@/composables/multi-select.composable'
-import type { XenApiNetwork, XenApiPif } from '@/libs/xen-api/xen-api.types'
+import type { XenApiNetwork } from '@/libs/xen-api/xen-api.types'
+import { useNetworkStore } from '@/stores/xen-api/network.store'
 import { usePifStore } from '@/stores/xen-api/pif.store'
-import VtsConnectionStatus, { type ConnectionStatus } from '@core/components/connection-status/VtsConnectionStatus.vue'
+import VtsConnectionStatus from '@core/components/connection-status/VtsConnectionStatus.vue'
 import VtsIcon from '@core/components/icon/VtsIcon.vue'
 import VtsStateHero from '@core/components/state-hero/VtsStateHero.vue'
 import ColumnTitle from '@core/components/table/ColumnTitle.vue'
@@ -142,28 +153,25 @@ import {
 import { computed, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 
-const { networks } = defineProps<{
-  networks: XenApiNetwork[]
-  isReady: boolean
-  hasError: boolean
-}>()
+const { networksWithPifs: networks, isReady, hasError } = useNetworkStore().subscribe()
+const { records: pifs } = usePifStore().subscribe()
 
 const { t } = useI18n()
 const searchQuery = ref('')
 const selectedNetworkId = useRouteQuery('id')
-const { records: pifs } = usePifStore().subscribe()
 
 const filteredNetworks = computed(() => {
   const searchTerm = searchQuery.value.trim().toLocaleLowerCase()
   if (!searchTerm) {
-    return networks
+    return networks.value
   }
-  return networks.filter(network =>
+
+  return networks.value.filter(network =>
     Object.values(network).some(value => String(value).toLocaleLowerCase().includes(searchTerm))
   )
 })
 
-const networkUuids = computed(() => networks.map(network => network.uuid))
+const networkUuids = computed(() => networks.value.map(network => network.uuid))
 
 const { selected, areAllSelected } = useMultiSelect(networkUuids)
 
@@ -178,23 +186,23 @@ const getNetworkVlan = (network: XenApiNetwork) => {
   }
 }
 
-const pifsStatus = (PIFs: XenApiPif[]): ConnectionStatus => {
-  if (PIFs.length === 0) {
+const getNetworkStatus = (network: XenApiNetwork) => {
+  const networkPIFs = pifs.value.filter(pif => network.PIFs?.includes(pif.$ref))
+
+  if (networkPIFs.length === 0) {
     return 'disconnected'
   }
-  const currentlyAttached = PIFs.map(PIF => PIF.currently_attached)
+
+  const currentlyAttached = networkPIFs.map(pif => pif.currently_attached)
   if (currentlyAttached.every(Boolean)) {
     return 'connected'
   }
+
   if (currentlyAttached.some(Boolean)) {
     return 'partially-connected'
   }
-  return 'disconnected'
-}
 
-const getNetworkStatus = (network: XenApiNetwork) => {
-  const networkPIFs = pifs.value.filter(pif => network.PIFs?.includes(pif.$ref))
-  return pifsStatus(networkPIFs)
+  return 'disconnected'
 }
 
 const getLockingMode = (lockingMode: string) => (lockingMode === 'disabled' ? t('disabled') : t('unlocked'))
@@ -203,11 +211,11 @@ const { visibleColumns, rows } = useTable('networks', filteredNetworks, {
   rowId: record => record.uuid,
   columns: define => [
     define('checkbox', () => '', { label: '', isHideable: false }),
-    define('name_label', record => record.name_label, { label: t('name') }),
-    define('name_description', record => record.name_description, { label: t('description') }),
+    define('name_label', { label: t('name') }),
+    define('name_description', { label: t('description') }),
     define('status', record => getNetworkStatus(record), { label: t('pifs-status') }),
     define('vlan', record => getNetworkVlan(record), { label: t('vlan') }),
-    define('MTU', record => record.MTU, { label: t('mtu') }),
+    define('MTU', { label: t('mtu') }),
     define('default_locking_mode', record => getLockingMode(record.default_locking_mode), {
       label: t('default-locking-mode'),
     }),
@@ -215,7 +223,8 @@ const { visibleColumns, rows } = useTable('networks', filteredNetworks, {
   ],
 })
 
-type NetworkHeader = 'name_label' | 'name_description' | 'status' | 'vlan' | 'MTU' | 'default_locking_mode' | 'more'
+type NetworkHeader = 'name_label' | 'name_description' | 'status' | 'vlan' | 'MTU' | 'default_locking_mode'
+
 const headerIcon: Record<NetworkHeader, IconDefinition> = {
   name_label: faAlignLeft,
   name_description: faAlignLeft,
@@ -223,15 +232,13 @@ const headerIcon: Record<NetworkHeader, IconDefinition> = {
   vlan: faAlignLeft,
   MTU: faHashtag,
   default_locking_mode: faCaretDown,
-  more: faEllipsis,
 }
-
-const getHeaderIcon = (status: NetworkHeader) => headerIcon[status]
 </script>
 
 <style scoped lang="postcss">
 .pool-networks-table,
-.table-actions {
+.table-actions,
+.container {
   display: flex;
   flex-direction: column;
 }
@@ -239,34 +246,19 @@ const getHeaderIcon = (status: NetworkHeader) => headerIcon[status]
 .pool-networks-table {
   gap: 2.4rem;
 
+  .container,
   .table-actions {
     gap: 0.8rem;
   }
 
-  .table-container {
-    overflow-x: auto;
-
-    .checkbox,
-    .more {
-      width: 4.8rem;
-    }
-
-    ::deep(.col-status) {
-      width: 2rem;
-    }
+  .checkbox,
+  .more {
+    width: 4.8rem;
   }
 
-  tbody tr:hover {
-    cursor: pointer;
-    background-color: var(--color-info-background-hover);
-  }
-
-  tr:last-child {
-    border-bottom: 0.1rem solid var(--color-neutral-border);
-  }
-
-  .selected {
-    background-color: var(--color-info-background-selected);
+  .checkbox {
+    text-align: center;
+    line-height: 1;
   }
 }
 </style>
--- @xen-orchestra/lite/src/views/pool/PoolNetworkView.vue
+++ @xen-orchestra/lite/src/views/pool/PoolNetworkView.vue
@@ -1,34 +1,33 @@
 <template>
-  <UiCard class="pool-network-view">
-    <div class="content">
-      <PoolNetworksTable :networks="networksWithPifs" :is-ready :has-error />
-      <PoolHostInternalNetworkTable :networks="networksWithoutPifs" :is-ready :has-error />
-    </div>
-  </UiCard>
+  <div class="pool-network-view">
+    <UiCard class="container">
+      <PoolNetworksTable />
+      <PoolHostInternalNetworksTable />
+    </UiCard>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import PoolHostInternalNetworkTable from '@/components/pool/network/PoolHostInternalNetworkTable.vue'
+import PoolHostInternalNetworksTable from '@/components/pool/network/PoolHostInternalNetworksTable.vue'
 import PoolNetworksTable from '@/components/pool/network/PoolNetworksTable.vue'
 import UiCard from '@/components/ui/UiCard.vue'
 import { usePageTitleStore } from '@/stores/page-title.store'
-import { useNetworkStore } from '@/stores/xen-api/network.store'
 import { useI18n } from 'vue-i18n'
 
 usePageTitleStore().setTitle(useI18n().t('network'))
-
-const { networksWithPifs, networksWithoutPifs, isReady, hasError } = useNetworkStore().subscribe()
 </script>
 
 <style lang="postcss" scoped>
 .pool-network-view {
-  border: solid 0.1rem var(--color-neutral-border);
-  margin: 0.8rem;
+  display: flex;
 
-  .content {
+  .container {
     display: flex;
     flex-direction: column;
     gap: 4rem;
+    margin: 0.8rem;
+    border: solid 0.1rem var(--color-neutral-border);
+    border-radius: 0.8rem;
   }
 }
 </style>
