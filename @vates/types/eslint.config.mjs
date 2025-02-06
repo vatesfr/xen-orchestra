bb87cd2e48c2470192e4a766c9fa26bc84ae728d
Mathieu <70369997+MathieuRA@users.noreply.github.com>
2025-02-06 17:08:09 +0100

    fix eslint ignore dist folder

--- @vates/types/eslint.config.mjs
+++ @vates/types/eslint.config.mjs
@@ -4,5 +4,5 @@ import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, {
-  ignores: ['./dist/**/*'],
+  ignores: ['dist'],
 })
