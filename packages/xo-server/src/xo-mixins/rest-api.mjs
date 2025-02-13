a0b1ac93881d1be3a51a760c4582e86ed88622c9
MlssFrncJrg <119158464+MelissaFrncJrg@users.noreply.github.com>
2025-02-13 12:36:46 +0100

    feat(xo-server/rest-api): ability to add user in a group (#8334)

--- packages/xo-server/src/xo-mixins/rest-api.mjs
+++ packages/xo-server/src/xo-mixins/rest-api.mjs
@@ -1543,6 +1543,22 @@ export default class RestApi {
       })
     )
 
+    api.put(
+      '/:collection(groups)/:id/users/:userId',
+      wrap(async (req, res) => {
+        const { id, userId } = req.params
+        const group = await app.getGroup(id)
+
+        if (group.provider !== undefined) {
+          return res.status(403).json({ message: 'cannot add user to synchronized group' })
+        }
+
+        await app.addUserToGroup(userId, id)
+
+        res.sendStatus(204)
+      }, true)
+    )
+
     api.delete(
       '/:collection(groups)/:id',
       wrap(async (req, res) => {
