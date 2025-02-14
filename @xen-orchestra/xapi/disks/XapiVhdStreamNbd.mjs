7ebee3ab6d684e357d596760e38ef5552a7ad231
Florent BEAUCHAMP <florent.beauchamp@vates.tech>
2025-02-14 14:46:22 +0000

    feat(@xen-orchestra/xapi): use disk classes

---
+++ @xen-orchestra/xapi/disks/XapiVhdStreamNbd.mjs
@@ -0,0 +1,66 @@
+// @ts-check
+/**
+ * @typedef {import('../../disk-transform/src/Disk.mjs').DiskBlock} DiskBlock
+ * @typedef {import('../../disk-transform/src/Disk.mjs').Disk} Disk
+ * @typedef {import('@vates/nbd-client/multi.mjs')} MultiNbdClient
+ */
+ 
+import { connectNbdClientIfPossible } from './utils.mjs'
+import { XapiVhdStreamSource } from './XapiVhdStreamSource.mjs'
+
+export class XapiVhdStreamNbdSource extends XapiVhdStreamSource {
+
+  /** @type {MultiNbdClient|undefined} */
+  #nbdClient
+  /** @type {number } */
+  #nbdConcurrency
+
+  /**
+   * @param {Object} params
+   * @param {string} params.vdiRef
+   * @param {string|undefined} params.baseRef
+   * @param {any} params.xapi
+   * @param {number} params.nbdConcurrency
+   */
+  constructor({ vdiRef, baseRef, xapi, nbdConcurrency = 4 }) {
+    super({ vdiRef, baseRef, xapi })
+    this.#nbdConcurrency = nbdConcurrency
+  }
+
+  /** @returns {Promise<void>} */
+  async init(){
+    await super.init()
+    const client = await connectNbdClientIfPossible(this.xapi, this.ref, this.#nbdConcurrency)
+    if(client === undefined){
+      throw new Error(`can't connect to any nbd client`)
+    }
+    this.#nbdClient = client
+    // we won't use the stream anymore
+    await super.close()
+  }
+  /**
+   * @param {number} index
+   * @returns {Promise<DiskBlock>}
+   */
+  async readBlock(index) {
+    if(this.#nbdClient === undefined){
+      throw new Error(`Can't use the nbd client of a XapiVhdStreamNbdSource before init`)
+    }
+    const data = await this.#nbdClient.readBlock(index, this.getBlockSize())
+    return { index, data }
+  }
+  /**
+   * @returns { AsyncGenerator<DiskBlock>}
+   */
+  async *buildDiskBlockGenerator() {
+    for (const { index } of this.blocks) {
+      yield this.readBlock(index)
+    }
+  }
+
+  /** @returns {Promise<void>} */
+  async close() {
+    await super.close()
+    await this.#nbdClient?.disconnect()
+  }
+}
