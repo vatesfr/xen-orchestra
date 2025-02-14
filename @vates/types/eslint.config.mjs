753066d33b3db7571230ed1c898396fd500d6891
mathieuRA <contact@mathieu-raisin.fr>
2025-02-14 12:26:13 +0100

    Julien's feedback

--- @vates/types/eslint.config.mjs
+++ @vates/types/eslint.config.mjs
@@ -3,4 +3,6 @@
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended)
+export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, {
+  ignores: ['dist'],
+})
