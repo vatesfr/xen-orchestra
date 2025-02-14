5cd9c0c790dd6442f9249e2bb4220fed8f1b8f69
Olivier Floch <olivier.floch@vates.fr>
2025-02-14 16:15:42 +0100

    feat(xo-core): add UiCharacterLimit component

---
+++ @xen-orchestra/web-core/lib/components/ui/character-limit/UiCharacterLimit.vue
@@ -0,0 +1,35 @@
+<!-- v1 -->
+<template>
+  <span class="ui-character-limit" :class="classes">
+    {{ $t('core.character-limit', { count, max }) }}
+  </span>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+const { count, max } = defineProps<{
+  count: number
+  max: number
+}>()
+
+const isTooLong = computed(() => count > max)
+
+const classes = computed(() => {
+  if (isTooLong.value) {
+    return ['has-error', 'typo-body-bold-small']
+  }
+
+  return 'typo-body-regular-small'
+})
+</script>
+
+<style lang="postcss" scoped>
+.ui-character-limit {
+  color: var(--color-neutral-txt-secondary);
+
+  &.has-error {
+    color: var(--color-danger-txt-base);
+  }
+}
+</style>
