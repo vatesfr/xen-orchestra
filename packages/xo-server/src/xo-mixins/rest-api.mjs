c8342672c355d79fc572c724a0c57e7b1a5ee9aa
mathieuRA <contact@mathieu-raisin.fr>
2025-02-18 10:43:36 +0100

    fix openapi spec with braneded type

--- packages/xo-server/src/xo-mixins/rest-api.mjs
+++ packages/xo-server/src/xo-mixins/rest-api.mjs
@@ -639,7 +639,6 @@ export default class RestApi {
         return object
       }
       function getObjects(filter, limit) {
-        console.log(filter)
         return Object.values(
           app.getObjects({
             filter: every(this.isCorrectType, filter),
@@ -775,7 +774,7 @@ export default class RestApi {
           )
         }
 
-        for (const collection of ['vms', 'vm-controllers', 'vm-snapshots', 'vm-templates']) {
+        for (const collection of ['vm-controllers', 'vm-snapshots', 'vm-templates']) {
           collections[collection].routes.vdis = vdis
         }
       }
