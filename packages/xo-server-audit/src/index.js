2aa7c012f181067eb810eb93b46c50ad319eb9b1
Julien Fontanet <julien.fontanet@isonoe.net>
2025-02-18 09:11:44 +0100

    fix(xo-server-audit): ignore host.getMdadmHealth
    Introduced by c41ae0527
    
    Related to zammad#34890

--- packages/xo-server-audit/src/index.js
+++ packages/xo-server-audit/src/index.js
@@ -31,6 +31,7 @@ const DEFAULT_BLOCKED_LIST = {
   'cloudConfig.getAllNetworkConfigs': true,
   'group.getAll': true,
   'host.getIpmiSensors': true,
+  'host.getMdadmHealth': true,
   'host.getSchedulerGranularity': true,
   'host.isHostServerTimeConsistent': true,
   'host.isHyperThreadingEnabled': true,
