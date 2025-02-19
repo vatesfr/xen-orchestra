2459869e36397420e7b2a6f1031969b36850a04e
Sébastian Czech <czechsebastian@icloud.com>
2025-02-19 13:59:58 +0100

    Implement vee validate lib

--- @xen-orchestra/web/src/validation/vm-create.schema.ts
+++ @xen-orchestra/web/src/validation/vm-create.schema.ts
@@ -1,7 +1,7 @@
 import { toTypedSchema } from '@vee-validate/yup'
 import * as yup from 'yup'
 
-export const formSchema = (maxRam: number) =>
+export const createVMSchema = (maxRam: number, maxVcpu: number) =>
   toTypedSchema(
     yup.object({
       vm_name: yup.string().required('VM Name is required'),
@@ -10,6 +10,10 @@ export const formSchema = (maxRam: number) =>
         .min(1, 'RAM must be at least 1')
         .max(maxRam, `RAM cannot exceed ${maxRam} GB`)
         .required('RAM is required'),
-      vCpu: yup.number().min(1, 'vCPU must be at least 1').required('vCPU is required'),
+      vCpu: yup
+        .number()
+        .min(1, 'vCPU must be at least 1')
+        .max(maxVcpu, `vCpu cannot exceed ${maxVcpu}`)
+        .required('vCPU is required'),
     })
   )
