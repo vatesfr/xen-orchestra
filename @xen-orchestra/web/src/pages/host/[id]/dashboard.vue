80d7b3451b5c37825de613aef45a97573fb8d47a
Olivier Floch <olivier.floch@vates.fr>
2025-03-03 18:32:14 +0100

    feat(xo-6/host): frontend integration

--- @xen-orchestra/web/src/pages/host/[id]/dashboard.vue
+++ @xen-orchestra/web/src/pages/host/[id]/dashboard.vue
@@ -1,5 +1,95 @@
 <template>
-  <div class="dashboard" />
+  <div class="dashboard">
+    <!-- Quick Info -->
+    <VtsQuickInfoCard class="quick-info" :is-ready>
+      <VtsQuickInfoColumn>
+        <VtsQuickInfoRow :title="$t('state')">
+          <template #value>
+            <VtsIcon :accent="powerState.accent" :icon="powerState.icon" />
+            {{ powerState.text }}
+          </template>
+        </VtsQuickInfoRow>
+        <VtsQuickInfoRow :title="$t('ip-address')" :value="host.address" />
+        <VtsQuickInfoRow :title="$t('started')" :value="relativeStartTime" />
+        <VtsQuickInfoRow v-if="isMaster">
+          <template #title>
+            <VtsIcon v-tooltip="$t('master')" accent="info" :icon="faCircle" :overlay-icon="faStar" />
+          </template>
+          <template #value>
+            {{ $t('is-primary-host', { name: host.name_label }) }}
+          </template>
+        </VtsQuickInfoRow>
+      </VtsQuickInfoColumn>
+      <VtsQuickInfoColumn>
+        <VtsQuickInfoRow :title="$t('id')" :value="host.id" />
+        <VtsQuickInfoRow :title="$t('description')" :value="host.name_description" />
+        <VtsQuickInfoRow :title="$t('version')" :value="host.version" />
+        <VtsQuickInfoRow :title="$t('hardware')">
+          <template #value>
+            {{ host.bios_strings['system-manufacturer'] }}
+            {{ `(${host.bios_strings['system-product-name']})` }}
+          </template>
+        </VtsQuickInfoRow>
+      </VtsQuickInfoColumn>
+      <VtsQuickInfoColumn>
+        <VtsQuickInfoRow :title="$t('cores-with-sockets')">
+          <template #value>{{ host.cpus.cores }} {{ `(${host.cpus.sockets})` }}</template>
+        </VtsQuickInfoRow>
+        <VtsQuickInfoRow :title="$t('ram')">
+          <template #value>{{ `${ram?.value} ${ram?.prefix}` }}</template>
+        </VtsQuickInfoRow>
+        <VtsQuickInfoRow v-if="host.tags.length" :title="$t('tags')">
+          <template #value>
+            <UiTagsList>
+              <UiTag v-for="tag in host.tags" :key="tag" accent="neutral" variant="secondary">{{ tag }}</UiTag>
+            </UiTagsList>
+          </template>
+        </VtsQuickInfoRow>
+      </VtsQuickInfoColumn>
+    </VtsQuickInfoCard>
+    <!-- VMs Status -->
+    <UiCard class="vms-status">
+      <UiCardTitle>{{ $t('vms-status') }}</UiCardTitle>
+      <VtsLoadingHero :disabled="isReady" type="card">
+        <VtsDonutChartWithLegend :segments="vmsSegments" />
+        <UiCardNumbers class="total" :label="t('total')" :value="hostVms.length" size="small" />
+      </VtsLoadingHero>
+    </UiCard>
+    <!-- CPU provisioning -->
+    <UiCard class="cpu-provisioning">
+      <UiCardTitle>{{ $t('cpu-provisioning') }}</UiCardTitle>
+      <VtsLoadingHero :disabled="isReady" type="card">
+        <!-- TODO: use Progress Bar -->
+        <!-- <VtsDonutChartWithLegend :segments="vmsSegments" /> -->
+        <div class="total">
+          <UiCardNumbers :label="t('vcpus-used')" :value="cpuProvisioning.used" size="medium" />
+          <UiCardNumbers :label="t('total-cpus')" :value="cpuProvisioning.total" size="medium" />
+        </div>
+      </VtsLoadingHero>
+    </UiCard>
+    <!-- RAM usage -->
+    <UiCard class="ram-usage">
+      <UiCardTitle>{{ $t('ram-usage') }}</UiCardTitle>
+      <VtsLoadingHero :disabled="isReady" type="card">
+        <!-- TODO: use Progress Bar -->
+        <!-- <VtsDonutChartWithLegend :segments="vmsSegments" /> -->
+        <div class="total">
+          <UiCardNumbers
+            :label="t('total-used')"
+            :unit="ramUsage.used?.prefix"
+            :value="ramUsage.used?.value"
+            size="medium"
+          />
+          <UiCardNumbers
+            :label="t('total-free')"
+            :unit="ramUsage.free?.prefix"
+            :value="ramUsage.free?.value"
+            size="medium"
+          />
+        </div>
+      </VtsLoadingHero>
+    </UiCard>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -7,13 +97,25 @@ import { useHostStore } from '@/stores/xo-rest-api/host.store'
 import { useVmStore } from '@/stores/xo-rest-api/vm.store'
 import { type XoHost } from '@/types/xo/host.type'
 import type { POWER_STATE } from '@core/types/power-state.type'
-import { type DonutChartWithLegendProps } from '@core/components/donut-chart-with-legend/VtsDonutChartWithLegend.vue'
-import { type IconAccent } from '@core/components/icon/VtsIcon.vue'
+import VtsDonutChartWithLegend, {
+  type DonutChartWithLegendProps,
+} from '@core/components/donut-chart-with-legend/VtsDonutChartWithLegend.vue'
+import VtsIcon, { type IconAccent } from '@core/components/icon/VtsIcon.vue'
+import VtsQuickInfoCard from '@core/components/quick-info-card/VtsQuickInfoCard.vue'
+import VtsQuickInfoColumn from '@core/components/quick-info-column/VtsQuickInfoColumn.vue'
+import VtsQuickInfoRow from '@core/components/quick-info-row/VtsQuickInfoRow.vue'
+import VtsLoadingHero from '@core/components/state-hero/VtsLoadingHero.vue'
+import UiCard from '@core/components/ui/card/UiCard.vue'
+import UiCardNumbers from '@core/components/ui/card-numbers/UiCardNumbers.vue'
+import UiCardTitle from '@core/components/ui/card-title/UiCardTitle.vue'
+import UiTag from '@core/components/ui/tag/UiTag.vue'
+import UiTagsList from '@core/components/ui/tag/UiTagsList.vue'
 import useRelativeTime from '@core/composables/relative-time.composable'
+import { vTooltip } from '@core/directives/tooltip.directive'
 import { formatSizeRaw } from '@core/utils/size.util'
 import { parseDateTime } from '@core/utils/time.util'
 import type { IconDefinition } from '@fortawesome/fontawesome-common-types'
-import { faPlay, faStop } from '@fortawesome/free-solid-svg-icons'
+import { faCircle, faPlay, faStar, faStop } from '@fortawesome/free-solid-svg-icons'
 import { useNow } from '@vueuse/core'
 import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
@@ -128,3 +230,48 @@ const ramUsage = computed(() => {
   }
 })
 </script>
+
+<style lang="postcss" scoped>
+.dashboard {
+  display: grid;
+  margin: 0.8rem;
+  gap: 0.8rem;
+  grid-template-columns: repeat(8, 1fr);
+  grid-template-areas:
+    'quick-info quick-info quick-info quick-info quick-info quick-info quick-info quick-info'
+    'vms-status vms-status cpu-provisioning cpu-provisioning cpu-provisioning ram-usage ram-usage ram-usage'
+    'cpu-usage cpu-usage memory-usage memory-usage network-usage network-usage load-average load-average';
+
+  .quick-info {
+    grid-area: quick-info;
+  }
+
+  .vms-status {
+    grid-area: vms-status;
+
+    .total {
+      margin-inline-start: auto;
+    }
+  }
+
+  .cpu-provisioning {
+    grid-area: cpu-provisioning;
+
+    .total {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      margin-block-start: auto;
+    }
+  }
+
+  .ram-usage {
+    grid-area: ram-usage;
+
+    .total {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      margin-block-start: auto;
+    }
+  }
+}
+</style>
