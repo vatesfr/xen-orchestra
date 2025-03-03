20a5726e6eaade67bea2bf97984c5fb2f2dc1ad1
Sebastian CZECH <sebastian.czech@vates.tech>
2025-03-03 16:19:09 +0100

    feat(web-core): circle progress bar component

--- @xen-orchestra/web/src/pages/dev/index.vue
+++ @xen-orchestra/web/src/pages/dev/index.vue
@@ -1,1 +1,21 @@
-<template>Dev</template>
+<template>
+  <div class="circle">
+    <UiCircleProgressBar variant="primary" accent="info" size="extra-small" :value="25" :max-value="100" />
+    <UiCircleProgressBar variant="primary" accent="warning" size="small" :value="25" :max-value="100" />
+    <UiCircleProgressBar variant="primary" accent="danger" size="medium" :value="50" :max-value="100" />
+    <UiCircleProgressBar variant="primary" accent="info" size="large" :value="100" :max-value="100" />
+    <UiCircleProgressBar variant="primary" accent="success" size="large" :value="50" :max-value="100" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import UiCircleProgressBar from '@core/components/ui/circle-progress-bar/UiCircleProgressBar.vue'
+</script>
+
+<style lang="postcss" scoped>
+.circle {
+  padding: 20rem;
+  display: flex;
+  gap: 10rem;
+}
+</style>
