07e6f765ba066d0a85d52893444f8d78385a1094
Olivier Floch <olivier.floch@vates.fr>
2025-03-03 15:34:00 +0100

    feat(xo-6/host): fetch host data needed for dashboard view, add properties in types

--- @xen-orchestra/web/src/types/xo/host.type.ts
+++ @xen-orchestra/web/src/types/xo/host.type.ts
@@ -26,4 +26,19 @@ export type XoHost = {
   controlDomain: string
   power_state: HOST_POWER_STATE
   residentVms: XoVm['id'][]
+  startTime: number
+  version: string
+  bios_strings: {
+    'system-manufacturer': string
+    'system-product-name': string
+  }
+  cpus: {
+    cores: number
+    sockets: number
+  }
+  memory: {
+    usage: number
+    size: number
+  }
+  tags: string[]
 }
