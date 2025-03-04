a1e299167ee1fdfa591c168b750453221550fc49
Sebastian CZECH <sebastian.czech@vates.tech>
2025-03-04 07:53:53 +0100

    centrer text

--- @xen-orchestra/web/src/pages/dev/index.vue
+++ @xen-orchestra/web/src/pages/dev/index.vue
@@ -3,7 +3,7 @@
     <UiCircleProgressBar variant="primary" accent="info" size="extra-small" :value="25" :max-value="100" />
     <UiCircleProgressBar variant="primary" accent="warning" size="small" :value="25" :max-value="100" />
     <UiCircleProgressBar variant="primary" accent="danger" size="medium" :value="50" :max-value="100" />
-    <UiCircleProgressBar variant="primary" accent="info" size="large" :value="100" :max-value="100" />
+    <UiCircleProgressBar variant="primary" accent="info" size="large" :value="99" :max-value="100" />
     <UiCircleProgressBar variant="primary" accent="success" size="large" :value="50" :max-value="100" />
   </div>
 </template>
