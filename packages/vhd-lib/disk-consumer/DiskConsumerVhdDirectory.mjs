9d1db2984b1cb33d63bdb4bb63e849b138ec6098
Florent BEAUCHAMP <florent.beauchamp@vates.tech>
2025-03-06 20:51:09 +0000

    feat(vhd-lib):  mplement Disk classes handling vhds

---
+++ packages/vhd-lib/disk-consumer/DiskConsumerVhdDirectory.mjs
@@ -0,0 +1,68 @@
+// @ts-check
+
+/**
+ * @typedef {import('@xen-orchestra/disk-transform').FileAccessor} FileAccessor
+ * @typedef {import('@xen-orchestra/disk-transform').Disk} Disk
+ */
+
+import { BaseVhd, FULL_BLOCK_BITMAP } from './BaseVhd.mjs'
+import { dirname } from 'node:path'
+import { v4 as uuidv4 } from 'uuid'
+import { asyncEach } from '@vates/async-each'
+import { VhdDirectory, VhdAbstract } from 'vhd-lib'
+import { unpackFooter, unpackHeader } from 'vhd-lib/Vhd/_utils.js'
+
+/**
+ * @typedef {Object} VhdRemoteTarget
+ * @property {FileAccessor} handler
+ * @property {string} path
+ * @property {string} compression
+ * @property {number} concurrency
+ * @property {string} flags
+ * @property {(path: string) => Promise<void>} validator
+ */
+
+/**
+ * @extends {BaseVhd}
+ */
+export class DiskConsumerVhdDirectory extends BaseVhd {
+  /** @type {VhdRemoteTarget} */
+  #target
+
+  /**
+   * @param {Disk} source
+   * @param {VhdRemoteTarget} target
+   */
+  constructor(source, target) {
+    super(source)
+    this.#target = target
+  }
+
+  /**
+   * @returns {Promise<void>}
+   */
+  async write() {
+    const { handler, path, compression, flags, validator, concurrency } = this.#target
+    const dataPath = `${dirname(path)}/data/${uuidv4()}.vhd`
+    try {
+      await handler.mktree(dataPath)
+      const vhd = new VhdDirectory(handler, dataPath, { flags, compression })
+      vhd.footer = unpackFooter(this.computeVhdFooter())
+      vhd.header = unpackHeader(this.computeVhdHeader())
+      await asyncEach(
+        this.source.diskBlocks(),
+        async ({ index, data }) => {
+          await vhd.writeEntireBlock({ id: index, buffer: Buffer.concat([FULL_BLOCK_BITMAP, data]) })
+        },
+        { concurrency }
+      )
+      await Promise.all([vhd.writeFooter(), vhd.writeHeader(), vhd.writeBlockAllocationTable()])
+      await validator(dataPath)
+      await VhdAbstract.createAlias(handler, path, dataPath)
+    } catch (err) {
+      await handler.rmtree(dataPath).catch(() => {}) // data
+      await handler.unlink(path).catch(() => {}) // alias
+      throw err
+    }
+  }
+}
