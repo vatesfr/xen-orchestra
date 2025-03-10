2a3cedef7584f2c9113cfeb4ad98fe1e83698e14
Thierry <thierry@byscripts.info>
2025-03-10 09:31:18 +0100

    feat(web-core): recreate UiDropdown component from DS

---
+++ @xen-orchestra/lite/src/stories/web-core/ui/dropdown/ui-dropdown-list.story.vue
@@ -0,0 +1,17 @@
+<template>
+  <ComponentStory
+    v-slot="{ properties }"
+    :params="[slot().help('This slot is meant to receive a list of <DropdownItem> and <DropdownTitle>')]"
+  >
+    <UiDropdownList v-bind="properties">
+      <UiDropdown v-for="n in 5" :key="n" accent="brand">Item label</UiDropdown>
+    </UiDropdownList>
+  </ComponentStory>
+</template>
+
+<script lang="ts" setup>
+import ComponentStory from '@/components/component-story/ComponentStory.vue'
+import { slot } from '@/libs/story/story-param'
+import UiDropdown from '@core/components/ui/dropdown/UiDropdown.vue'
+import UiDropdownList from '@core/components/ui/dropdown/UiDropdownList.vue'
+</script>
--- @xen-orchestra/web-core/lib/utils/injection-keys.util.ts
+++ @xen-orchestra/web-core/lib/utils/injection-keys.util.ts
@@ -11,8 +11,6 @@ export const IK_TREE_ITEM_EXPANDED = Symbol('IK_TREE_ITEM_EXPANDED') as Injectio
 
 export const IK_TREE_LIST_DEPTH = Symbol('IK_TREE_LIST_DEPTH') as InjectionKey<number>
 
-export const IK_DROPDOWN_CHECKBOX = Symbol('IK_DROPDOWN_CHECKBOX') as InjectionKey<ComputedRef<boolean>>
-
 export const IK_MENU_HORIZONTAL = Symbol('IK_MENU_HORIZONTAL') as InjectionKey<ComputedRef<boolean>>
 
 export const IK_CLOSE_MENU = Symbol('IK_CLOSE_MENU') as InjectionKey<() => void>
