7645b365e98c9c68bdbd389c3533d79003551054
Florent BEAUCHAMP <florent.beauchamp@vates.tech>
2025-03-13 16:25:38 +0000

    try to configure jeager

--- packages/xo-server/src/instrumentation.js
+++ packages/xo-server/src/instrumentation.js
@@ -1,21 +1,26 @@
-/*instrumentation.js*/
-// Require dependencies
-const { NodeSDK } = require('@opentelemetry/sdk-node');
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-node');
-const {
-  getNodeAutoInstrumentations,
-} = require('@opentelemetry/auto-instrumentations-node');
-const {
-  PeriodicExportingMetricReader,
-  ConsoleMetricExporter,
-} = require('@opentelemetry/sdk-metrics');
+const { NodeSDK } = require('@opentelemetry/sdk-node')
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node')
+const { Resource } = require('@opentelemetry/resources')
 
+const { ATTR_SERVICE_NAME } = require('@opentelemetry/semantic-conventions')
+const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api')
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO)
+
+const { ZipkinExporter } = require('@opentelemetry/exporter-zipkin')
+
+// Add your zipkin url (`http://localhost:9411/api/v2/spans` is used as
+// default) and application name to the Zipkin options.
+// You can also define your custom headers which will be added automatically.
+const options = {
+  url: 'http://10.1.5.158:9411/api/v2/spans',
+}
+const exporter = new ZipkinExporter(options)
 const sdk = new NodeSDK({
-  traceExporter: new ConsoleSpanExporter(),
-  metricReader: new PeriodicExportingMetricReader({
-    exporter: new ConsoleMetricExporter(),
-  }),
+  traceExporter: exporter,
   instrumentations: [getNodeAutoInstrumentations()],
-});
+  resource: new Resource({
+    [ATTR_SERVICE_NAME]: 'XO',
+  }),
+})
 
-sdk.start();
+sdk.start()
