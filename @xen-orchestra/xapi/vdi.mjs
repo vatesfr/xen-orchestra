vatesfr/xen-orchestra

commit 2a2c966126c08717c43890ac2b30b1741c70bf4d

fix(backups): disable CBT when CBT is unusable (#8313)

@xen-orchestra/xapi/vdi.mjs
@@ -158,6 +158,39 @@ class Vdi {
     return Buffer.from(encoded, 'base64')
   }
 
+  /**
+   * will disable CBT on the VDI, all its ancestor and will purge
+   * snapshots of type cbt_metadata of this chain
+   *
+   * @param {OpaqueRef} vdiRef
+   */
+  async disableCbtOnChain(vdiRef) {
+    const smConfig = await this.getField('VDI', vdiRef, 'sm_config')
+    if (smConfig['vhd-parent']) {
+      const parentRef = await this.call('VDI.get_by_uuid', smConfig['vhd-parent'])
+      await this.VDI_disableCbtOnChain(parentRef)
+    }
+    const snapshotRefs = await this.getField('VDI', vdiRef, 'snapshots')
+    for (const snapshotRef of snapshotRefs) {
+      const type = await this.getField('VDI', snapshotRef, 'type')
+      if (type === 'cbt_metadata') {
+        try {
+          await this.VDI_destroy(snapshotRef)
+        } catch (err) {
+          warn('couldn t destroy snapshot', { err, vdiRef, snapshotRef })
+        }
+      }
+      /**
+       * xapi can't disable CBT on a snapshot OPERATION_NOT_ALLOWED(VDI is a snapshot)
+       */
+    }
+    try {
+      await this.callAsync('VDI.disable_cbt', vdiRef)
+    } catch (err) {
+      warn('couldn t disable cbt on disk', { err, vdiRef })
+    }
+  }
+
   async disconnectFromControlDomain(vdiRef) {
     let vbdRefs
     try {
@@ -206,11 +239,12 @@ class Vdi {
       vdi: ref,
     }
 
-    const [cbt_enabled, size, uuid, vdiName] = await Promise.all([
+    const [cbt_enabled, size, snapshotOf, uuid, vdiName] = await Promise.all([
       this.getField('VDI', ref, 'cbt_enabled').catch(() => {
         /* on XS < 7.3 cbt is not supported */
       }),
       this.getField('VDI', ref, 'virtual_size'),
+      this.getField('VDI', ref, 'snapshot_of'),
       this.getField('VDI', ref, 'uuid'),
       this.getField('VDI', ref, 'name_label'),
     ])
@@ -275,8 +309,10 @@ class Vdi {
     // a CBT export can only work if we have a NBD client and changed blocks
     if (changedBlocks === undefined || nbdClient === undefined) {
       if (baseParentType === 'cbt_metadata') {
+        await this.VDI_disableCbtOnChain(snapshotOf)
         const e = new Error(`can't create a stream from a metadata VDI, fall back to a base `)
         e.code = 'VDI_CANT_DO_DELTA'
+        // CBT is not usable: reset it
         throw e
       }
 

packages/xo-server/src/api/vdi.mjs
@@ -67,8 +67,11 @@ export const set = defer(async function ($defer, params) {
     await xapi.resizeVdi(ref, size)
   }
   if ('cbt' in params) {
-    const method = params.cbt ? 'VDI.enable_cbt' : 'VDI.disable_cbt'
-    await xapi.callAsync(method, ref)
+    if (params.cbt) {
+      await xapi.callAsync('VDI.enable_cbt', ref)
+    } else {
+      await xapi.VDI_disableCbtOnChain(ref)
+    }
   }
 
   // Other fields.

commit bb554a0ddd28cd3cf45bdb26a84b0eb808e2578b

fix(@xen-orchestra/xapi): remove automatic disabling of cbt chain on export failure (#8446)
introduced by 2a2c966
zammad#35683

@xen-orchestra/xapi/vdi.mjs
@@ -239,12 +239,11 @@ class Vdi {
       vdi: ref,
     }
 
-    const [cbt_enabled, size, snapshotOf, uuid, vdiName] = await Promise.all([
+    const [cbt_enabled, size, uuid, vdiName] = await Promise.all([
       this.getField('VDI', ref, 'cbt_enabled').catch(() => {
         /* on XS < 7.3 cbt is not supported */
       }),
       this.getField('VDI', ref, 'virtual_size'),
-      this.getField('VDI', ref, 'snapshot_of'),
       this.getField('VDI', ref, 'uuid'),
       this.getField('VDI', ref, 'name_label'),
     ])
@@ -309,10 +308,8 @@ class Vdi {
     // a CBT export can only work if we have a NBD client and changed blocks
     if (changedBlocks === undefined || nbdClient === undefined) {
       if (baseParentType === 'cbt_metadata') {
-        await this.VDI_disableCbtOnChain(snapshotOf)
         const e = new Error(`can't create a stream from a metadata VDI, fall back to a base `)
         e.code = 'VDI_CANT_DO_DELTA'
-        // CBT is not usable: reset it
         throw e
       }
 