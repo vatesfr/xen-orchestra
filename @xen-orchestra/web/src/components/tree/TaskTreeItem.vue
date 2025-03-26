7eb696211a5b7468f4e777bf7c799b9e9e705196
Sebastian CZECH <sebastian.czech@vates.tech>
2025-03-26 11:33:49 +0100

    wip create treeview

---
+++ @xen-orchestra/web/src/components/tree/TaskTreeItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <VtsTreeItem :expanded="branch.isExpanded">
+    <UiTreeItemLabel :icon="faTasks" :route="`/task/${branch.id}`" @toggle="branch.toggleExpand()">
+      <UiTaskItem :task="branch.data" :user />
+    </UiTreeItemLabel>
+    <template v-if="branch.hasChildren" #sublist>
+      <VtsTreeList>
+        <TaskTreeItem v-for="child in branch.rawChildren" :key="child.id" :branch="child" />
+      </VtsTreeList>
+    </template>
+  </VtsTreeItem>
+</template>
+
+<script lang="ts" setup>
+import { useUserStore } from '@/stores/xo-rest-api/user.store.ts'
+import type { TaskBranch } from '@/types/tree.type.ts'
+import VtsTreeItem from '@core/components/tree/VtsTreeItem.vue'
+import VtsTreeList from '@core/components/tree/VtsTreeList.vue'
+import UiTaskItem from '@core/components/ui/task-item/UiTaskItem.vue'
+import UiTreeItemLabel from '@core/components/ui/tree-item-label/UiTreeItemLabel.vue'
+import { faTasks } from '@fortawesome/free-solid-svg-icons'
+import { computed, defineProps } from 'vue'
+
+const { branch } = defineProps<{
+  branch: TaskBranch
+}>()
+
+// console.log('Branch', branch)
+
+const { records } = useUserStore().subscribe()
+
+const user = computed(() => {
+  const userId = branch.data.properties.userId
+  if (!userId) return
+  const user = records.value.find(user => user.id === userId)
+  return user ? user?.name || user.email : 'Not Found'
+})
+</script>
--- @xen-orchestra/web/src/pages/dev/index.vue
+++ @xen-orchestra/web/src/pages/dev/index.vue
@@ -1,46 +1,20 @@
 <template>
-  <UiTaskItem :task />
+  <VtsTreeList v-if="!isReady">
+    <VtsTreeLoadingItem v-for="i in 5" :key="i" :icon="faTasks" />
+  </VtsTreeList>
+  <NoResults v-else-if="tasks.length === 0" />
+  <div v-else>
+    <TaskTreeItem v-for="task in tasks" :key="task" :branch="task" />
+  </div>
 </template>
 
 <script setup lang="ts">
-import UiTaskItem from '@core/components/ui/task-item/UiTaskItem.vue'
+import NoResults from '@/components/NoResults.vue'
+import TaskTreeItem from '@/components/tree/TaskTreeItem.vue'
+import { useTaskTree } from '@/composables/task-tree.composable.ts'
+import VtsTreeList from '@core/components/tree/VtsTreeList.vue'
+import VtsTreeLoadingItem from '@core/components/tree/VtsTreeLoadingItem.vue'
+import { faTasks } from '@fortawesome/free-solid-svg-icons'
 
-const task = {
-  // success
-  id: '0m8eavoye',
-  properties: {
-    name: 'REST: create_vm pool',
-    objectId: 'b7569d99-30f8-178a-7d94-801de3e29b5b',
-    userId: '260f7f80-b257-4c9f-885a-17ef5dbe71a9',
-  },
-  start: 1742290670102,
-  status: 'success',
-  updatedAt: 1742290672161,
-  end: 1743088671000,
-  // result: '4bf5c1db-dad2-6ded-4c34-a8f43fc24000',
-  result: {
-    message: 'no object with UUID or opaque ref: 0cb9edfd-aa75-a8a4-0ec1-5833c7217f98',
-    name: 'Error',
-    stack: 'stack example',
-  },
-}
-
-// const taskError = {
-//   id: '0m7vtsw1s',
-//   properties: {
-//     name: 'REST: create_vm pool',
-//     objectId: 'b7569d99-30f8-178a-7d94-801de3e29b5b',
-//     userId: '260f7f80-b257-4c9f-885a-17ef5dbe71a9',
-//   },
-//   start: 1741173594688,
-//   status: 'failure',
-//   updatedAt: 1741173603988,
-//   end: 1741173603988,
-//   result: {
-//     message: 'no object with UUID or opaque ref: 0cb9edfd-aa75-a8a4-0ec1-5833c7217f98',
-//     name: 'Error',
-//     stack:
-//       'Error: no object with UUID or opaque ref: 0cb9edfd-aa75-a8a4-0ec1-5833c7217f98\n    at Xapi.apply (file:///Users/sebastian/Documents/GitHub/xen-orchestra/packages/xen-api/index.mjs:725:11)\n    at Xapi.getObject (file:///Users/sebastian/Documents/GitHub/xen-orchestra/packages/xo-server/src/xapi/index.mjs:100:26)\n    at map (file:///Users/sebastian/Documents/GitHub/xen-orchestra/packages/xo-server/src/xapi/mixins/vm.mjs:302:29)\n    at Array.map (<anonymous>)\n    at Xapi.createVm (file:///Users/sebastian/Documents/GitHub/xen-orchestra/packages/xo-server/src/xapi/mixins/vm.mjs:294:23)\n    at file:///Users/sebastian/Documents/GitHub/xen-orchestra/packages/xo-server/src/xo-mixins/rest-api.mjs:795:26\n    at Task.runInside (/Users/sebastian/Documents/GitHub/xen-orchestra/@vates/task/index.js:175:22)\n    at Task.run (/Users/sebastian/Documents/GitHub/xen-orchestra/@vates/task/index.js:159:20)',
-//   },
-// }
+const { tasks, isReady } = useTaskTree()
 </script>
