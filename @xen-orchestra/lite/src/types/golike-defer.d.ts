198cfe3dadfcaa4aac9f155f600221923bcda3cb
Joris-K <joris.kosacki@vates.tech>
2025-03-27 14:55:02 +0100

    review changelog

--- @xen-orchestra/lite/src/types/golike-defer.d.ts
+++ @xen-orchestra/lite/src/types/golike-defer.d.ts
@@ -1,4 +1,6 @@
 declare module 'golike-defer' {
+  type Cb = () => Promise<unknown> | unknown
+
   export type Defer = ((cb: Cb) => void) & {
     onFailure: (cb: Cb) => void
     onSuccess: (cb: Cb) => void
