859517e185a92c66c39b9962a8a23379ac4da25d
Sebastian CZECH <sebastian.czech@vates.tech>
2025-04-01 09:06:03 +0200

    Feedback

--- @xen-orchestra/web-core/lib/components/ui/circle-progress-bar/UiCircleProgressBar.vue
+++ @xen-orchestra/web-core/lib/components/ui/circle-progress-bar/UiCircleProgressBar.vue
@@ -1,33 +1,41 @@
 <!-- v4 -->
 <template>
-  <div class="ui-circle-progress-bar">
+  <div class="ui-circle-progress-bar" :class="className">
     <svg
       :width="circleSize"
       :height="circleSize"
       :viewBox="`0 0 ${circleSize} ${circleSize}`"
       xmlns="http://www.w3.org/2000/svg"
-      class="circle"
     >
       <circle :r="radius" :cx="circleSize / 2" :cy="circleSize / 2" fill="transparent" class="background" />
-      <circle :r="radius" :cx="circleSize / 2" :cy="circleSize / 2" fill="transparent" class="foreground fill" />
+      <circle
+        :r="radius"
+        :cx="circleSize / 2"
+        :cy="circleSize / 2"
+        fill="transparent"
+        class="fill"
+        :class="{ success: isCompleteWithSuccess }"
+      />
     </svg>
     <div v-if="size !== 'extra-small'" class="overlay">
-      <VtsIcon v-if="isComplete" :icon class="icon" :accent="iconAccent" />
-      <span v-else class="text" :class="fontClass">{{ percentValue }}</span>
+      <VtsIcon v-if="isComplete" class="icon" :icon :accent="iconAccent" />
+      <span v-else>{{ percentValue }}</span>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import VtsIcon from '@core/components/icon/VtsIcon.vue'
+import { toVariants } from '@core/utils/to-variants.util.ts'
 import { faCheck, faExclamation } from '@fortawesome/free-solid-svg-icons'
+import { useClamp, useMax } from '@vueuse/math'
 import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 
 const {
   accent,
   size,
-  value,
+  value: _value,
   maxValue = 100,
 } = defineProps<{
   accent: ProgressCircleAccent
@@ -48,13 +56,6 @@ const circleSizeMap = {
   large: 164,
 }
 
-const iconSizeMap = {
-  'extra-small': undefined,
-  small: '1.6rem',
-  medium: '3.2rem',
-  large: '4.8rem',
-}
-
 const strokeWidthMap = {
   'extra-small': 2,
   small: 4,
@@ -71,33 +72,16 @@ const fontClassesMap = {
 
 const circleSize = computed(() => circleSizeMap[size])
 const fontClass = computed(() => fontClassesMap[size])
-const iconSize = computed(() => iconSizeMap[size])
 const strokeWidth = computed(() => strokeWidthMap[size])
 
 const radius = computed(() => (circleSize.value - strokeWidth.value) / 2)
 const circumference = computed(() => 2 * Math.PI * radius.value)
 
-const isComplete = computed(() => value >= maxValue)
+const value = useMax(0, () => _value)
+const valuePercent = useClamp(() => (value.value / maxValue) * 100 || 0, 0, 100)
 
+const isComplete = computed(() => valuePercent.value >= 100)
 const isCompleteWithSuccess = computed(() => isComplete.value && accent === 'info')
-
-const strokeColorMap = {
-  info: 'var(--color-info-item-base)',
-  success: 'var(--color-success-item-base)',
-  warning: 'var(--color-warning-item-base)',
-  danger: 'var(--color-danger-item-base)',
-}
-const strokeColor = computed(() => {
-  if (isCompleteWithSuccess.value) {
-    return strokeColorMap.success
-  }
-  return strokeColorMap[accent]
-})
-
-const backgroundStrokeColor = computed(() => `var(--color-${accent}-background-selected)`)
-
-const iconAccent = computed(() => (isCompleteWithSuccess.value ? 'success' : accent))
-const valuePercent = computed(() => Math.round((value / maxValue) * 100))
 
 const dashOffset = computed(() => {
   if (valuePercent.value > 100) return
@@ -106,7 +90,16 @@ const dashOffset = computed(() => {
 
 const percentValue = computed(() => n(valuePercent.value / 100, 'percent'))
 
+const iconAccent = computed(() => (isCompleteWithSuccess.value ? 'success' : accent))
 const icon = computed(() => (accent === 'warning' || accent === 'danger' ? faExclamation : faCheck))
+
+const className = computed(() => [
+  fontClass.value,
+  toVariants({
+    accent,
+    size,
+  }),
+])
 </script>
 
 <style lang="postcss" scoped>
@@ -116,21 +109,95 @@ const icon = computed(() => (accent === 'warning' || accent === 'danger' ? faExc
   align-items: center;
   justify-content: center;
 
-  .background {
-    stroke: v-bind(backgroundStrokeColor);
-    stroke-width: v-bind(strokeWidth);
+  /* ACCENT */
+
+  &.accent--info {
+    color: var(--color-info-item-base);
+
+    .fill {
+      stroke: var(--color-info-item-base);
+    }
+
+    .background {
+      stroke: var(--color-info-background-selected);
+    }
+
+    .success {
+      stroke: var(--color-success-item-base);
+    }
   }
 
-  .foreground {
-    stroke-width: v-bind(strokeWidth);
+  &.accent--warning {
+    color: var(--color-warning-item-base);
+
+    .fill {
+      stroke: var(--color-warning-item-base);
+    }
+
+    .background {
+      stroke: var(--color-warning-background-selected);
+    }
+  }
+
+  &.accent--danger {
+    color: var(--color-danger-item-base);
+
+    .fill {
+      stroke: var(--color-danger-item-base);
+    }
+
+    .background {
+      stroke: var(--color-danger-background-selected);
+    }
+  }
+
+  /* SIZE */
+
+  &.size--extra-small {
+    .background,
+    .fill {
+      stroke-width: 2;
+    }
+  }
+
+  &.size--small {
+    .icon {
+      font-size: 1.6rem;
+    }
+
+    .background,
+    .fill {
+      stroke-width: 4;
+    }
+  }
+
+  &.size--medium {
+    .icon {
+      font-size: 3.2rem;
+    }
+
+    .background,
+    .fill {
+      stroke-width: 6;
+    }
+  }
+
+  &.size--large {
+    .icon {
+      font-size: 4.8rem;
+    }
+
+    .background,
+    .fill {
+      stroke-width: 16;
+    }
+  }
+
+  .fill {
     stroke-linecap: butt;
     transition: stroke-dashoffset 0.3s ease;
     transform: rotate(-90deg);
     transform-origin: center;
-  }
-
-  .fill {
-    stroke: v-bind(strokeColor);
     stroke-dasharray: v-bind(circumference);
     stroke-dashoffset: v-bind(dashOffset);
   }
@@ -140,14 +207,6 @@ const icon = computed(() => (accent === 'warning' || accent === 'danger' ? faExc
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-  }
-
-  .text {
-    color: v-bind(strokeColor);
-  }
-
-  .icon {
-    font-size: v-bind(iconSize);
   }
 }
 </style>
