929079ddd1d0bcdda2d15fbfae4a78d7fb1f8ab2
Sebastian CZECH <sebastian.czech@vates.tech>
2025-04-02 10:53:29 +0200

    fix(x06-vm): display sr name in select

--- @xen-orchestra/web/src/utils/xo-api-definition.util.ts
+++ @xen-orchestra/web/src/utils/xo-api-definition.util.ts
@@ -16,7 +16,7 @@ export const xoApiDefinition = {
   pool: {
     type: 'collection',
     path: 'pools',
-    fields: 'id,name_label,master,default_SR',
+    fields: 'id,name_label,master,default_SR,haSrs',
     handler: (record: XoPool) => record,
   },
   host: {
