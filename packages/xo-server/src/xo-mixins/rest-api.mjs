1783b197de469be522cb5a1e70e7223b1070f2bb
mathieuRA <contact@mathieu-raisin.fr>
2025-04-07 10:37:46 +0200

    feat(@xen-orchestra/rest-api): expose get messages and get message

--- packages/xo-server/src/xo-mixins/rest-api.mjs
+++ packages/xo-server/src/xo-mixins/rest-api.mjs
@@ -606,6 +606,7 @@ export default class RestApi {
     const swaggerEndpoints = {
       alarms: {},
       docs: {},
+      messages: {},
       vifs: {},
       vms: {
         actions: {
@@ -1036,26 +1037,6 @@ export default class RestApi {
       },
     }
     collections.dashboard = {}
-    collections.messages = {
-      getObject(id) {
-        const message = app.getObject(id, 'message')
-        if (isAlarm(message)) {
-          throw noSuchObject(id, 'message')
-        }
-
-        return message
-      },
-      getObjects(filter, limit) {
-        return handleArray(
-          Object.values(
-            app.getObjects({
-              filter: every(keepNonAlarmMessages, filter),
-              limit,
-            })
-          )
-        )
-      },
-    }
 
     // normalize collections
     for (const id of Object.keys(collections)) {
