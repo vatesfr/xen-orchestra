04d6ce10f40c24546aed1d089dbf9708e4780d2d
Sebastian CZECH <sebastian.czech@vates.tech>
2025-04-16 16:09:03 +0200

    add logo in header

--- @xen-orchestra/web/src/components/main/MainHeader.vue
+++ @xen-orchestra/web/src/components/main/MainHeader.vue
@@ -1,29 +1,34 @@
 <template>
-  <UiHeadBar :icon="faCity" />
+  <UiHeadBar>
+    <LogoTextOnly class="logo" />
+  </UiHeadBar>
+
   <TabList>
     <RouterLink v-slot="{ isActive, href }" to="/dashboard" custom>
       <TabItem :active="isActive" :href tag="a">
         {{ $t('dashboard') }}
       </TabItem>
     </RouterLink>
-    <TabItem disabled>{{ $t('alarms') }}</TabItem>
-    <TabItem disabled>{{ $t('stats') }}</TabItem>
-    <TabItem disabled>{{ $t('system') }}</TabItem>
-    <TabItem disabled>{{ $t('network') }}</TabItem>
-    <TabItem disabled>{{ $t('storage') }}</TabItem>
     <RouterLink v-slot="{ isActive, href }" to="/tasks" custom>
       <TabItem :active="isActive" :href tag="a">
         {{ $t('tasks') }}
       </TabItem>
     </RouterLink>
+    <TabItem disabled>{{ $t('pools') }}</TabItem>
     <TabItem disabled>{{ $t('hosts') }}</TabItem>
-    <TabItem disabled>{{ $t('vms') }}</TabItem>
+    <TabItem disabled>{{ $t('vms', 0) }}</TabItem>
   </TabList>
 </template>
 
 <script lang="ts" setup>
+import LogoTextOnly from '@/components/LogoTextOnly.vue'
 import TabItem from '@core/components/tab/TabItem.vue'
 import TabList from '@core/components/tab/TabList.vue'
 import UiHeadBar from '@core/components/ui/head-bar/UiHeadBar.vue'
-import { faCity } from '@fortawesome/free-solid-svg-icons'
 </script>
+
+<style lang="postcss" scoped>
+.logo {
+  height: 1.6rem;
+}
+</style>
