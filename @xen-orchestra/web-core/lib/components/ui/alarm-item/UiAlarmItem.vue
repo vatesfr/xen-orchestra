601f509ff199f06a68483f0ac540bd8c936dbb2a
unel <enee.prog@gmail.com>
2025-04-17 15:43:11 +0200

    feat(web-core/alarm-item): creating component

---
+++ @xen-orchestra/web-core/lib/components/ui/alarm-item/UiAlarmItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <UiButtonIcon :icon="faAngleRight" size="small" accent="brand" />
+  <div class="typo-body-regular">
+    {{ label }}
+  </div>
+  <div class="typo-body-regular value">
+    {{ value }}
+  </div>
+  <div class="typo-body-regular-small object">
+    {{ $t('on-object') }}
+    <slot name="objectLink" />
+  </div>
+  <div class="typo-body-regular-small time-ago">
+    {{ timeAgo }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
+import { useTimeAgo } from '@vueuse/core'
+import { computed } from 'vue'
+import UiButtonIcon from '../button-icon/UiButtonIcon.vue'
+
+const { date } = defineProps<{
+  label: string
+  value: string
+  date: Date | number | string
+}>()
+
+defineSlots<{
+  objectLink: any
+}>()
+
+const timeAgo = computed(() => useTimeAgo(date))
+</script>
+
+<style scoped lang="postcss">
+.value {
+  color: var(--color-danger-txt-base);
+}
+</style>
