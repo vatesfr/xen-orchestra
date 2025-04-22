c56fee7883c46567860f0baa8f4771dae7e65cdf
Sebastian CZECH <sebastian.czech@vates.tech>
2025-04-22 16:50:55 +0200

    feat(xo6): add host system page

---
+++ @xen-orchestra/web-core/lib/components/status-mode/VtsStatusMode.vue
@@ -0,0 +1,35 @@
+<template>
+  <UiInfo :accent="currentStatus.accent">
+    {{ currentStatus.text }}
+  </UiInfo>
+</template>
+
+<script setup lang="ts">
+import UiInfo, { type InfoAccent } from '@core/components/ui/info/UiInfo.vue'
+import { computed, type ComputedRef } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+export type Status = 'enabled' | 'disabled'
+type StatusesMap = Record<Status, { text: string; accent: InfoAccent }>
+
+const { status } = defineProps<{
+  status: string | boolean
+}>()
+
+const { t } = useI18n()
+
+const getStatus = computed(() => {
+  if (status === 'Running' || status === true || (typeof status === 'string' && status.trim() !== '')) {
+    return 'enabled'
+  } else {
+    return 'disabled'
+  }
+})
+
+const statuses: ComputedRef<StatusesMap> = computed(() => ({
+  enabled: { text: t('enabled'), accent: 'success' },
+  disabled: { text: t('disabled'), accent: 'muted' },
+}))
+
+const currentStatus = computed(() => statuses.value[getStatus.value])
+</script>
