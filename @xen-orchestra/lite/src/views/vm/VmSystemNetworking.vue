970ca37e25b1bd34bd226e5f193ec1d29d372255
unel <enee.prog@gmail.com>
2025-04-28 10:36:01 +0200

    feat(xo-lite/vm system): add systemNetworing component

---
+++ @xen-orchestra/lite/src/views/vm/VmSystemNetworking.vue
@@ -0,0 +1,19 @@
+<template>
+  <UiCard class="vm-system-networking">
+    <UiTitle>
+      {{ $t('Networking') }}
+    </UiTitle>
+    <VtsQuickInfoRow :label="$t('nic-type')" :value="vm?.uuid" />
+  </UiCard>
+</template>
+
+<script setup lang="ts">
+import type { XenApiVm } from '@/libs/xen-api/xen-api.types'
+import VtsQuickInfoRow from '@core/components/quick-info-row/VtsQuickInfoRow.vue'
+import UiCard from '@core/components/ui/card/UiCard.vue'
+import UiTitle from '@core/components/ui/title/UiTitle.vue'
+
+const { vm } = defineProps<{ vm: XenApiVm | undefined }>()
+</script>
+
+/* not found: vm?.nicType */
