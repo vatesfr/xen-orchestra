8eca9f2f35275bceaabd5b8cb7e3e9ead89dfcd2
unel <enee.prog@gmail.com>
2025-04-29 13:37:51 +0200

    feat(xo-lite/vm system): fix data

--- @xen-orchestra/lite/src/views/vm/VmGraphics.vue
+++ @xen-orchestra/lite/src/views/vm/VmGraphics.vue
@@ -5,31 +5,29 @@
     </UiTitle>
     <VtsQuickInfoRow :label="$t('vga')">
       <template #value>
-        <!--
- <UiInfo :accent="vm.vga === 'std' ? 'muted' : 'success'">
-          {{ vm.vga === 'std' ? $t('disabled') : $t('enabled') }}
+        <UiInfo :accent="vga === 'std' ? 'muted' : 'success'">
+          {{ vga === 'std' ? $t('disabled') : $t('enabled') }}
         </UiInfo>
--->
       </template>
     </VtsQuickInfoRow>
     <VtsQuickInfoRow :label="$t('video-ram')">
       <template v-if="VideoRamValue" #value>
-        {{ VideoRamValue + $t('bytes.mi') }}
+        {{ `${VideoRamValue} ${$t('bytes.mi')}` }}
       </template>
     </VtsQuickInfoRow>
   </UiCard>
 </template>
 
 <script setup lang="ts">
+import type { XenApiVm } from '@/libs/xen-api/xen-api.types'
 import VtsQuickInfoRow from '@core/components/quick-info-row/VtsQuickInfoRow.vue'
 import UiCard from '@core/components/ui/card/UiCard.vue'
+import UiInfo from '@core/components/ui/info/UiInfo.vue'
 import UiTitle from '@core/components/ui/title/UiTitle.vue'
+import { formatSizeRaw } from '@core/utils/size.util'
 
-// const { vm } = defineProps<{ vm: XenApiVm | undefined }>()
+const { vm } = defineProps<{ vm: XenApiVm | undefined }>()
 
-const VideoRamValue = 0 // vm.videoram ? formatSizeRaw(Number(vm.videoram), 0)?.value : null
-
-// not found
-// videoram
-// vga
+const VideoRamValue = formatSizeRaw(Number(vm?.platform.videoram ?? 4), 0)?.value
+const vga = vm?.platform.vga ?? 'cirrus'
 </script>
