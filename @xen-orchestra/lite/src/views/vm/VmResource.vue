61475d010938bd7546044adb69e705b93646d896
unel <enee.prog@gmail.com>
2025-04-29 10:26:56 +0200

    feat(xo-lite/vm system): find more data for vmResource

--- @xen-orchestra/lite/src/views/vm/VmResource.vue
+++ @xen-orchestra/lite/src/views/vm/VmResource.vue
@@ -4,23 +4,28 @@
       {{ $t('resource-management') }}
     </UiTitle>
     <template v-for="(value, key) in generalInfo" :key>
-      <VtsQuickInfoRow :label="$t(key)" :value="String(value)" />
+      <VtsQuickInfoRow :label="$t(key)" :value="value" />
     </template>
   </UiCard>
 </template>
 
 <script setup lang="ts">
 import type { XenApiVm } from '@/libs/xen-api/xen-api.types'
+import { useVmMetricsStore } from '@/stores/xen-api/vm-metrics.store'
 import VtsQuickInfoRow from '@core/components/quick-info-row/VtsQuickInfoRow.vue'
 import UiCard from '@core/components/ui/card/UiCard.vue'
 import UiTitle from '@core/components/ui/title/UiTitle.vue'
 import { formatSizeRaw } from '@core/utils/size.util'
 import { useI18n } from 'vue-i18n'
 
+const { vm } = defineProps<{ vm: XenApiVm | undefined }>()
+
+const { getByOpaqueRef: getMetricsByOpaqueRef } = useVmMetricsStore().subscribe()
+
 type GeneralInfo = {
   'cpu-mask': string
-  'CPU-weight': number
-  'cpu-cap': number
+  'CPU-weight': string
+  'cpu-cap': string
   'minimum-CPU-limit': string
   'maximum-CPU-limit': string
   'vm-limit-topology': string
@@ -31,8 +36,6 @@ type GeneralInfo = {
   GPUs: string
 }
 
-const { vm } = defineProps<{ vm: XenApiVm | undefined }>()
-
 const { t } = useI18n()
 
 const staticMinMemoryFormated = formatSizeRaw(vm?.memory_static_min, 0)
@@ -40,19 +43,36 @@ const staticMaxMemoryFormated = formatSizeRaw(vm?.memory_static_max, 0)
 const dynamicMinMemoryFormated = formatSizeRaw(vm?.memory_dynamic_min, 0)
 const dynamicMaxMemoryFormated = formatSizeRaw(vm?.memory_dynamic_max, 0)
 
+// @see packages/xo-server/src/xapi-object-to-xo.mjs
+const { major, minor } = (vm?.guest_metrics as any | undefined)?.PV_drivers_version ?? {}
+const xenTools = !(
+  !vm?.power_state ||
+  !vm?.metrics ||
+  vm?.guest_metrics === undefined ||
+  major === undefined ||
+  minor === undefined
+)
+const VmMetrics = vm?.metrics ? getMetricsByOpaqueRef(vm?.metrics) : undefined
+const maxCPU =
+  vm && vm.power_state && vm.metrics && xenTools && VmMetrics ? VmMetrics.VCPUs_number : +(vm?.VCPUs_at_startup ?? 0)
+
 const generalInfo: GeneralInfo = {
-  'cpu-cap': /* vm.cpuCap ? vm.cpuCap : 0  XEN_DEFAULT_CPU_CAP */ 0,
-  'cpu-mask': /* vm.cpuMask ? vm.cpuMask.join(', ') : '' */ '',
-  'CPU-weight': /* vm.cpuWeight ? vm.cpuWeight : 256 /* XEN_DEFAULT_CPU_WEIGHT */ 256,
+  'cpu-cap': vm?.VCPUs_params.cap ? String(+vm?.VCPUs_params.cap) : '0' /* XEN_DEFAULT_CPU_CAP */,
+  'cpu-mask': vm?.VCPUs_params.mask
+    ? vm?.VCPUs_params.mask
+        .split(',')
+        .map(toPositive => +toPositive)
+        .join(', ')
+    : '',
+  'CPU-weight': vm?.VCPUs_params.weight ? String(+vm?.VCPUs_params.weight) : '256' /* XEN_DEFAULT_CPU_WEIGHT */,
   'minimum-CPU-limit': 'no data',
-  'maximum-CPU-limit': vm?.VCPUs_max + ' ' + t('CPUs'),
-  'vm-limit-topology':
-    /* vm.coresPerSocket
+  'maximum-CPU-limit': `${maxCPU} ${t('CPUs')}`,
+  'vm-limit-topology': vm?.platform['cores-per-socket']
     ? t('sockets-with-cores-per-socket', {
-        nSockets: vm.CPUs.max / vm.coresPerSocket,
-        nCores: vm.coresPerSocket,
+        nSockets: maxCPU / Number(vm?.platform['cores-per-socket'] ?? 0),
+        nCores: vm?.platform['cores-per-socket'] ?? 0,
       })
-    : '' */ '',
+    : '',
   'minimum-static-memory': `${staticMinMemoryFormated?.value} ${staticMinMemoryFormated?.prefix}`,
   'maximum-static-memory': `${staticMaxMemoryFormated?.value} ${staticMaxMemoryFormated?.prefix}`,
   'minimum-dynamic-memory': `${dynamicMinMemoryFormated?.value} ${dynamicMinMemoryFormated?.prefix}`,
