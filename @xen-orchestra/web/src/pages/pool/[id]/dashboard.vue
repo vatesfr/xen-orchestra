6215002ecb356e4f88f89353a9e3b2c19cf1be5c
unel <enee.prog@gmail.com>
2025-05-02 15:11:19 +0200

    feat(xo6): create pool/dashboard page and statusComponent

---
+++ @xen-orchestra/web/src/pages/pool/[id]/dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="host-dashboard">
+    <div class="dashboard-row">
+      <div><Status :pool /></div>
+      <div />
+      <div />
+    </div>
+    <div class="dashboard-column">
+      <div />
+      <div />
+      <div />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Status from '@/components/pool/dashboard/status.vue'
+import type { XoPool } from '@/types/xo/pool.type'
+
+const { pool } = defineProps<{
+  pool: XoPool
+}>()
+</script>
