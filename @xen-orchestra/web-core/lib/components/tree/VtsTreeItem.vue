115b987583aadf504061fa9e4a6ae96efbb055a7
unel <enee.prog@gmail.com>
2025-05-02 10:42:48 +0200

    feat(xo6/sidebar): auto close sidebar when clic on element

--- @xen-orchestra/web-core/lib/components/tree/VtsTreeItem.vue
+++ @xen-orchestra/web-core/lib/components/tree/VtsTreeItem.vue
@@ -1,11 +1,12 @@
 <template>
-  <li class="vts-tree-item">
+  <li class="vts-tree-item" @click="handleClick">
     <slot />
     <slot v-if="expanded" name="sublist" />
   </li>
 </template>
 
 <script lang="ts" setup>
+import { useSidebarStore } from '@core/stores/sidebar.store'
 import { IK_TREE_ITEM_EXPANDED, IK_TREE_ITEM_HAS_CHILDREN } from '@core/utils/injection-keys.util'
 import { onBeforeMount, onBeforeUpdate, provide, ref, toRef, useSlots } from 'vue'
 
@@ -18,11 +19,18 @@ defineSlots<{
   sublist?(): any
 }>()
 
+const sidebar = useSidebarStore()
 const hasChildren = ref(false)
 
 const updateHasChildren = () => {
   const { sublist } = useSlots()
   hasChildren.value = sublist !== undefined
+}
+
+const handleClick = () => {
+  if (!hasChildren.value) {
+    sidebar.toggleExpand()
+  }
 }
 
 onBeforeMount(() => updateHasChildren())
