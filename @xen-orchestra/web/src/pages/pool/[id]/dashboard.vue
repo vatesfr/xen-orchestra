c8cde70a49d62e6e5667aa8a8d35a5b28461169c
unel <enee.prog@gmail.com>
2025-05-05 13:08:34 +0200

    feat(xo6): add pool dashboard ram usage

--- @xen-orchestra/web/src/pages/pool/[id]/dashboard.vue
+++ @xen-orchestra/web/src/pages/pool/[id]/dashboard.vue
@@ -2,18 +2,21 @@
   <div class="host-dashboard">
     <div class="dashboard-row">
       <div><Status :pool /></div>
-      <div />
-      <div />
+      <div class="alarms-box"><Alarms /></div>
+      <div><Patch /></div>
     </div>
     <div class="dashboard-column">
       <div />
-      <div />
+      <div><RamUsage :pool /></div>
       <div />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import Alarms from '@/components/pool/dashboard/alarms.vue'
+import Patch from '@/components/pool/dashboard/patch.vue'
+import RamUsage from '@/components/pool/dashboard/ramUsage.vue'
 import Status from '@/components/pool/dashboard/status.vue'
 import type { XoPool } from '@/types/xo/pool.type'
 
@@ -21,3 +24,14 @@ const { pool } = defineProps<{
   pool: XoPool
 }>()
 </script>
+
+<style scoped lang="postcss">
+.dashboard-row {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  .alarms-box {
+    flex-shrink: 1;
+  }
+}
+</style>
