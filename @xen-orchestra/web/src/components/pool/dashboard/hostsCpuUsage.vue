c6f6ba3be122ff7c013e1836837c3f8d27c8b560
unel <enee.prog@gmail.com>
2025-05-05 16:04:07 +0200

    feat(xo6): add cpu usage componnent

---
+++ @xen-orchestra/web/src/components/pool/dashboard/hostsCpuUsage.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="hosts-cpu-usage">
+    <VtsLoadingHero v-if="!areHostReady && !areVmsReady" type="card" />
+    <template v-else>
+      <UiProgressBar
+        v-for="cpuUsage in cpuUsages.hosts.splice(0, 5)"
+        :key="cpuUsage.id"
+        class="progressBar"
+        :value="cpuUsage.used ?? 0"
+        :max="cpuUsage.total"
+        :legend="`${cpuUsage.name} ${cpuUsage.total} ${$t('vcpus', cpuUsages.cpuUsageTotal)}`"
+      />
+      <div class="total">
+        <UiCardNumbers
+          :label="$t('total-used')"
+          :unit="$t('vcpus', cpuUsages.cpuUsageTotal)"
+          :value="cpuUsages.cpuUsageTotal"
+          size="medium"
+        />
+        <UiCardNumbers
+          :label="$t('total-free')"
+          :unit="$t('vcpus', cpuUsages.cpuFreeTotal)"
+          :value="cpuUsages.cpuFreeTotal"
+          size="medium"
+        />
+      </div>
+    </template>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useHostStore } from '@/stores/xo-rest-api/host.store'
+import { useVmStore } from '@/stores/xo-rest-api/vm.store'
+import type { XoHost } from '@/types/xo/host.type'
+import type { XoVm } from '@/types/xo/vm.type'
+import VtsLoadingHero from '@core/components/state-hero/VtsLoadingHero.vue'
+import UiCardNumbers from '@core/components/ui/card-numbers/UiCardNumbers.vue'
+import UiProgressBar from '@core/components/ui/progress-bar/UiProgressBar.vue'
+import { computed } from 'vue'
+
+const { hosts } = defineProps<{
+  hosts: XoHost[]
+  vms: XoVm[]
+}>()
+
+const { isReady: areHostReady } = useHostStore().subscribe()
+const { vmsByHost, isReady: areVmsReady } = useVmStore().subscribe()
+
+const cpuUsages = computed(() => {
+  let cpuUsageTotal = 0
+  let cpuFreeTotal = 0
+  return {
+    hosts: hosts.map(host => {
+      const vms = vmsByHost.value.get(host.id)
+      const vmsCpuNumber = vms?.reduce((cpuUsed, vm) => cpuUsed + vm.CPUs.number, 0) ?? 0
+      cpuUsageTotal += vmsCpuNumber
+      cpuFreeTotal += host.cpus.cores - vmsCpuNumber
+      return {
+        total: host.cpus.cores,
+        used: vmsCpuNumber,
+        free: host.cpus.cores - vmsCpuNumber,
+        id: host.id,
+        name: host.name_label,
+      }
+    }),
+    cpuUsageTotal,
+    cpuFreeTotal,
+  }
+})
+</script>
+
+<style scoped lang="postcss">
+.hosts-cpu-usage {
+  .total {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    margin-block-start: auto;
+  }
+}
+</style>
--- @xen-orchestra/web/src/components/pool/dashboard/vmsCpuUsage.vue
+++ @xen-orchestra/web/src/components/pool/dashboard/vmsCpuUsage.vue
@@ -1,26 +1,26 @@
 <template>
-  <div class="vm-ram-usage">
+  <div class="vm-cpu-usage">
     <VtsLoadingHero v-if="!isReady" type="card" />
     <template v-else>
       <UiProgressBar
-        v-for="ramUsage in ramUsages.vm.splice(0, 5)"
-        :key="ramUsage.id"
+        v-for="cpuUsage in cpuUsages.vm.splice(0, 5)"
+        :key="cpuUsage.id"
         class="progressBar"
-        :value="ramUsage.used?.value ?? 0"
-        :max="ramUsage.total?.value"
-        :legend="ramUsage.name + ramUsage.used?.prefix"
+        :value="cpuUsage.used ?? 0"
+        :max="cpuUsage.total"
+        :legend="`${cpuUsage.name} ${cpuUsage.total} ${$t('vcpus', cpuUsages.cpuUsageTotal)}`"
       />
       <div class="total">
         <UiCardNumbers
           :label="$t('total-used')"
-          :unit="ramUsages.ramUsageTotal?.prefix"
-          :value="ramUsages.ramUsageTotal?.value"
+          :unit="$t('vcpus', cpuUsages.cpuUsageTotal)"
+          :value="cpuUsages.cpuUsageTotal"
           size="medium"
         />
         <UiCardNumbers
           :label="$t('total-free')"
-          :unit="ramUsages.ramFreeTotal?.prefix"
-          :value="ramUsages.ramFreeTotal?.value"
+          :unit="$t('vcpus', cpuUsages.cpuFreeTotal)"
+          :value="cpuUsages.cpuFreeTotal"
           size="medium"
         />
       </div>
@@ -34,7 +34,6 @@ import type { XoVm } from '@/types/xo/vm.type'
 import VtsLoadingHero from '@core/components/state-hero/VtsLoadingHero.vue'
 import UiCardNumbers from '@core/components/ui/card-numbers/UiCardNumbers.vue'
 import UiProgressBar from '@core/components/ui/progress-bar/UiProgressBar.vue'
-import { formatSizeRaw } from '@core/utils/size.util'
 import { computed } from 'vue'
 
 const { vms } = defineProps<{
@@ -43,29 +42,29 @@ const { vms } = defineProps<{
 
 const { isReady } = useHostStore().subscribe()
 
-const ramUsages = computed(() => {
-  let ramUsageTotal = 0
-  let ramFreeTotal = 0
+const cpuUsages = computed(() => {
+  let cpuUsageTotal = 0
+  let cpuFreeTotal = 0
   return {
     vm: vms.map(vm => {
-      ramUsageTotal += vm.memory.dynamic[0]
-      ramFreeTotal += vm.memory.size - vm.memory.dynamic[0]
+      cpuUsageTotal += vm.CPUs.number
+      cpuFreeTotal += vm.CPUs.max - vm.CPUs.number
       return {
-        total: formatSizeRaw(vm.memory.size, 0),
-        used: formatSizeRaw(vm.memory.dynamic[0], 0),
-        free: formatSizeRaw(vm.memory.size - vm.memory.dynamic[0], 0),
+        total: vm.CPUs.max,
+        used: vm.CPUs.number,
+        free: vm.CPUs.max - vm.CPUs.number,
         id: vm.id,
         name: vm.name_label,
       }
     }),
-    ramUsageTotal: formatSizeRaw(ramUsageTotal, 0),
-    ramFreeTotal: formatSizeRaw(ramFreeTotal, 0),
+    cpuUsageTotal,
+    cpuFreeTotal,
   }
 })
 </script>
 
 <style scoped lang="postcss">
-.vm-ram-usage {
+.vm-cpu-usage {
   .total {
     display: grid;
     grid-template-columns: 1fr 1fr;
