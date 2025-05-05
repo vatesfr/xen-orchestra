1a3f6ba4b02c9f8b0fe90f0b62ed9184c39d1692
unel <enee.prog@gmail.com>
2025-05-05 13:08:34 +0200

    feat(xo6): add pool dashboard ram usage

---
+++ @xen-orchestra/web/src/components/vm/VmDashboadMultiRamUsage.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="vm-ram-usage">
+    <VtsLoadingHero v-if="!isReady" type="card" />
+    <template v-else>
+      <UiProgressBar
+        v-for="ramUsage in ramUsages.vm"
+        :key="ramUsage.id"
+        class="progressBar"
+        :value="ramUsage.used?.value ?? 0"
+        :max="ramUsage.total?.value"
+        :legend="ramUsage.name + ramUsage.used?.prefix"
+      />
+      <div class="total">
+        <UiCardNumbers
+          :label="$t('total-used')"
+          :unit="ramUsages.ramUsageTotal?.prefix"
+          :value="ramUsages.ramUsageTotal?.value"
+          size="medium"
+        />
+        <UiCardNumbers
+          :label="$t('total-free')"
+          :unit="ramUsages.ramFreeTotal?.prefix"
+          :value="ramUsages.ramFreeTotal?.value"
+          size="medium"
+        />
+      </div>
+    </template>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useHostStore } from '@/stores/xo-rest-api/host.store'
+import type { XoVm } from '@/types/xo/vm.type'
+import VtsLoadingHero from '@core/components/state-hero/VtsLoadingHero.vue'
+import UiCardNumbers from '@core/components/ui/card-numbers/UiCardNumbers.vue'
+import UiProgressBar from '@core/components/ui/progress-bar/UiProgressBar.vue'
+import { formatSizeRaw } from '@core/utils/size.util'
+import { computed } from 'vue'
+
+const { vms } = defineProps<{
+  vms: XoVm[]
+}>()
+
+const { isReady } = useHostStore().subscribe()
+
+const ramUsages = computed(() => {
+  let ramUsageTotal = 0
+  let ramFreeTotal = 0
+  return {
+    vm: vms.map(vm => {
+      ramUsageTotal += vm.memory.dynamic[0]
+      ramFreeTotal += vm.memory.size
+      return {
+        total: formatSizeRaw(vm.memory.size, 0),
+        used: formatSizeRaw(vm.memory.dynamic[0], 0),
+        free: formatSizeRaw(vm.memory.size - vm.memory.dynamic[0], 0),
+        id: vm.id,
+        name: vm.name_label,
+      }
+    }),
+    ramUsageTotal: formatSizeRaw(ramUsageTotal, 0),
+    ramFreeTotal: formatSizeRaw(ramFreeTotal, 0),
+  }
+})
+</script>
+
+<style scoped lang="postcss">
+.vm-ram-usage {
+  .total {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    margin-block-start: auto;
+  }
+}
+</style>
