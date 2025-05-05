2b59c1ea497545449e9ecd3f6ba877141df7e5bb
unel <enee.prog@gmail.com>
2025-05-05 15:04:57 +0200

    feat(xo6): add Pool dashboard cpu component

---
+++ @xen-orchestra/web/src/components/pool/dashboard/PoolDashboardCpu.vue
@@ -0,0 +1,64 @@
+<template>
+  <UiCard class="host-dashboard-cpu-provisioning">
+    <UiCardTitle>{{ $t('cpu-provisioning') }}</UiCardTitle>
+    <VtsLoadingHero v-if="!isReady" type="card" />
+    <template v-else>
+      <UiProgressBar :max="pool.cpus.cores" :legend="$t('vcpus')" :value="cpuProvisioning.used" />
+      <div class="total">
+        <UiCardNumbers :label="$t('vcpus-used')" :value="cpuProvisioning.used" size="medium" />
+        <UiCardNumbers :label="$t('total-cpus')" :value="cpuProvisioning.total" size="medium" />
+      </div>
+    </template>
+  </UiCard>
+</template>
+
+<script setup lang="ts">
+import { useHostStore } from '@/stores/xo-rest-api/host.store'
+import { usePoolStore } from '@/stores/xo-rest-api/pool.store'
+import { useVmStore } from '@/stores/xo-rest-api/vm.store'
+import type { XoPool } from '@/types/xo/pool.type'
+import VtsLoadingHero from '@core/components/state-hero/VtsLoadingHero.vue'
+import UiCard from '@core/components/ui/card/UiCard.vue'
+import UiCardNumbers from '@core/components/ui/card-numbers/UiCardNumbers.vue'
+import UiCardTitle from '@core/components/ui/card-title/UiCardTitle.vue'
+import UiProgressBar from '@core/components/ui/progress-bar/UiProgressBar.vue'
+import { computed } from 'vue'
+
+const { pool } = defineProps<{
+  pool: XoPool
+}>()
+
+const { isReady: isPoolReady } = usePoolStore().subscribe()
+const { isReady: isHostReady } = useHostStore().subscribe()
+const { vmsByHost, isReady: areVmsReady, hostLessVmsByPool } = useVmStore().subscribe()
+const { hostsByPool, isReady: areHostReady } = useHostStore().subscribe()
+
+const isReady = computed(() => isPoolReady.value && areHostReady.value && isHostReady.value && areVmsReady.value)
+const hosts = computed(() => (hostsByPool.value.get(pool.id) ?? []).sort((a, b) => b.memory.usage - a.memory.usage))
+// no memory.usage for vm
+const vms = computed(() =>
+  [
+    ...hosts.value.flatMap(host => vmsByHost.value.get(host.id) ?? []),
+    ...(hostLessVmsByPool.value.get(pool.id) ?? []),
+  ].sort((a, b) => b.memory.dynamic[0] - a.memory.dynamic[0])
+)
+const cpuProvisioning = computed(() => {
+  const totalHostCpus = pool.cpus.cores
+  const totalVcpus = vms.value.reduce((acc, vm) => acc + (vm.CPUs?.number ?? 0), 0)
+
+  return {
+    total: totalHostCpus,
+    used: totalVcpus,
+  }
+})
+</script>
+
+<style lang="postcss" scoped>
+.host-dashboard-cpu-provisioning {
+  .total {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    margin-block-start: auto;
+  }
+}
+</style>
