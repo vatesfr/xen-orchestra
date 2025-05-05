621fb983a7456bd53e8a2d540e8b155fc1f5f865
unel <enee.prog@gmail.com>
2025-05-05 13:08:34 +0200

    feat(xo6): add pool dashboard ram usage

---
+++ @xen-orchestra/web/src/components/pool/dashboard/ramUsage.vue
@@ -0,0 +1,60 @@
+<template>
+  <UiCard class="pool-dashboard-ram-usage">
+    <UiCardTitle>
+      {{ $t('ram-usage') }}
+    </UiCardTitle>
+    <UiCardSubtitle>
+      {{ $t('host') }}
+      <template #info>
+        {{ $t('top-5') }}
+      </template>
+    </UiCardSubtitle>
+    <VtsLoadingHero v-if="!isReady" type="card" />
+    <template v-else>
+      <HostDashboadMultiRamUsage :hosts="hosts.slice(0, 5)" />
+    </template>
+    <UiCardSubtitle>
+      {{ $t('vms', vms.length) }}
+      <template #info>
+        {{ $t('top-5') }}
+      </template>
+    </UiCardSubtitle>
+    <VtsLoadingHero v-if="!isReady" type="card" />
+    <template v-else>
+      <VmDashboadMultiRamUsage :vms="vms.slice(0, 5)" />
+    </template>
+  </UiCard>
+</template>
+
+<script lang="ts" setup>
+import HostDashboadMultiRamUsage from '@/components/host/dashboard/HostDashboadMultiRamUsage.vue'
+import VmDashboadMultiRamUsage from '@/components/vm/VmDashboadMultiRamUsage.vue'
+import { useHostStore } from '@/stores/xo-rest-api/host.store'
+import { usePoolStore } from '@/stores/xo-rest-api/pool.store'
+import { useVmStore } from '@/stores/xo-rest-api/vm.store'
+import type { XoPool } from '@/types/xo/pool.type'
+import VtsLoadingHero from '@core/components/state-hero/VtsLoadingHero.vue'
+import UiCard from '@core/components/ui/card/UiCard.vue'
+import UiCardSubtitle from '@core/components/ui/card-subtitle/UiCardSubtitle.vue'
+import UiCardTitle from '@core/components/ui/card-title/UiCardTitle.vue'
+import { computed } from 'vue'
+
+const { pool } = defineProps<{
+  pool: XoPool
+}>()
+
+const { isReady: isPoolReady } = usePoolStore().subscribe()
+const { isReady: isHostReady } = useHostStore().subscribe()
+const { vmsByHost, isReady: areVmsReady, hostLessVmsByPool } = useVmStore().subscribe()
+const { hostsByPool, isReady: areHostReady } = useHostStore().subscribe()
+
+const isReady = computed(() => isPoolReady.value && areHostReady.value && isHostReady.value && areVmsReady.value)
+const hosts = computed(() => (hostsByPool.value.get(pool.id) ?? []).sort((a, b) => b.memory.usage - a.memory.usage))
+// no memory.usage for vm
+const vms = computed(() =>
+  [
+    ...hosts.value.flatMap(host => vmsByHost.value.get(host.id) ?? []),
+    ...(hostLessVmsByPool.value.get(pool.id) ?? []),
+  ].sort((a, b) => b.memory.dynamic[0] - a.memory.dynamic[0])
+)
+</script>
