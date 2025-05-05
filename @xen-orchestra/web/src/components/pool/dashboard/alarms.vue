79422d6392fe1578938c72ffd0bab7afa64c4ded
unel <enee.prog@gmail.com>
2025-05-05 13:08:34 +0200

    feat(xo6): add pool dashboard ram usage

---
+++ @xen-orchestra/web/src/components/pool/dashboard/alarms.vue
@@ -0,0 +1,17 @@
+<template>
+  <UiCard class="pool-dashboard-alarms">
+    <UiCardTitle>{{ $t('alarms') }}</UiCardTitle>
+  </UiCard>
+</template>
+
+<script setup lang="ts">
+// import type { XoPool } from '@/types/xo/pool.type'
+import UiCard from '@core/components/ui/card/UiCard.vue'
+import UiCardTitle from '@core/components/ui/card-title/UiCardTitle.vue'
+
+// const { pool } = defineProps<{
+//   pool: XoPool
+// }>()
+
+// useAlarmStore().subscribe()
+</script>
---
+++ @xen-orchestra/web/src/components/pool/dashboard/patch.vue
@@ -0,0 +1,10 @@
+<template>
+  <UiCard class="pool-dashboard-alarms">
+    <UiCardTitle>{{ $t('patches') }}</UiCardTitle>
+  </UiCard>
+</template>
+
+<script setup lang="ts">
+import UiCard from '@core/components/ui/card/UiCard.vue'
+import UiCardTitle from '@core/components/ui/card-title/UiCardTitle.vue'
+</script>
