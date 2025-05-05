2a5de674356d4da97016a79f5d79f45914976791
Sebastian CZECH <sebastian.czech@vates.tech>
2025-05-05 15:40:02 +0200

    feedback

--- @xen-orchestra/web/src/pages/vm/new.vue
+++ @xen-orchestra/web/src/pages/vm/new.vue
@@ -412,7 +412,7 @@ import { useVdiStore } from '@/stores/xo-rest-api/vdi.store'
 import { useVifStore } from '@/stores/xo-rest-api/vif.store'
 import { useVmTemplateStore } from '@/stores/xo-rest-api/vm-template.store'
 import type { XoNetwork } from '@/types/xo/network.type.ts'
-import type { NetworkInterface, Vdi, VmState } from '@/types/xo/new-vm.type'
+import type { NetworkInterface, Vdi, VmState, Vif } from '@/types/xo/new-vm.type'
 import type { XoVdi } from '@/types/xo/vdi.type.ts'
 import type { XoVmTemplate } from '@/types/xo/vm-template.type'
 import type { Branded } from '@core/types/utility.type'
@@ -806,47 +806,58 @@ const modifiedExistingVdis = computed(() => {
   }, [])
 })
 
+const vifsToSend = computed(() => {
+  const result: Vif[] = []
+
+  if (vmState.new_vm_template === undefined) {
+    return
+  }
+
+  for (const vifId of vmState.new_vm_template.VIFs) {
+    const vif = getVif(vifId)
+    if (!vif) {
+      continue
+    }
+
+    const matchedInterface = vmState.networkInterfaces.find(networkInterface => networkInterface.id === vif.id)
+
+    if (!matchedInterface) {
+      result.push({
+        device: vif.device,
+        destroy: true,
+      })
+      continue
+    }
+
+    const networkChanged = vif.$network !== matchedInterface.interface
+    const macChanged = vif.MAC !== matchedInterface.macAddress
+
+    if (networkChanged || macChanged) {
+      result.push({
+        device: vif.device,
+        network: matchedInterface.interface,
+        mac: matchedInterface.macAddress,
+      })
+    }
+  }
+
+  for (const networkInterface of vmState.networkInterfaces) {
+    if (!networkInterface.id) {
+      result.push({
+        network: networkInterface.interface,
+        mac: networkInterface.macAddress?.trim() === '' ? ' ' : networkInterface.macAddress,
+      })
+    }
+  }
+
+  return result
+})
+
 const vmData = computed(() => {
   const vdisToSend = [...vmState.vdis, ...modifiedExistingVdis.value].map(vdi => ({
     ...vdi,
     ...(vdi.size && { size: giBToBytes(vdi.size) }),
   }))
-
-  function vifsToSend() {
-    const result = []
-    if (vmState.new_vm_template === undefined) return
-
-    for (const id of vmState.new_vm_template.VIFs) {
-      const vif = getVif(id)
-      if (!vif) continue
-
-      const matchedInterface = vmState.networkInterfaces.find(ni => ni.id === vif.id)
-
-      if (!matchedInterface) {
-        result.push({
-          device: vif.device,
-          destroy: true,
-        })
-      } else if (vif.$network !== matchedInterface.interface || vif.MAC !== matchedInterface.macAddress) {
-        result.push({
-          network: matchedInterface.interface,
-          mac: matchedInterface.macAddress,
-          device: vif.device,
-        })
-      }
-    }
-
-    for (const ni of vmState.networkInterfaces) {
-      if (!ni.id) {
-        result.push({
-          network: ni.interface,
-          mac: ni.macAddress?.trim() === '' ? ' ' : ni.macAddress,
-        })
-      }
-    }
-
-    return result
-  }
 
   const optionalFields = Object.assign(
     {},
@@ -874,7 +885,7 @@ const vmData = computed(() => {
     name_label: vmState.name,
     template: vmState.new_vm_template?.uuid,
     // Todo: Handle in case we have less networks interfaces than templates vifs
-    vifs: vifsToSend(),
+    vifs: vifsToSend.value,
     ...optionalFields,
   }
 })
