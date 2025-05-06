d133d7c122b38d034b7fd1c53280dcd832e1f305
unel <enee.prog@gmail.com>
2025-05-06 17:03:08 +0200

    fix(xo6/pool-dashboard): add placeholder for alarms and patches

--- @xen-orchestra/web/src/components/pool/dashboard/PoolDashboardalarms.vue
+++ @xen-orchestra/web/src/components/pool/dashboard/PoolDashboardalarms.vue
@@ -1,11 +1,13 @@
 <template>
   <UiCard class="pool-dashboard-alarms">
     <UiCardTitle>{{ $t('alarms') }}</UiCardTitle>
+    <VtsComingSoonHero type="card" />
   </UiCard>
 </template>
 
 <script setup lang="ts">
 // import type { XoPool } from '@/types/xo/pool.type'
+import VtsComingSoonHero from '@core/components/state-hero/VtsComingSoonHero.vue'
 import UiCard from '@core/components/ui/card/UiCard.vue'
 import UiCardTitle from '@core/components/ui/card-title/UiCardTitle.vue'
 
@@ -15,3 +17,9 @@ import UiCardTitle from '@core/components/ui/card-title/UiCardTitle.vue'
 
 // useAlarmStore().subscribe()
 </script>
+
+<style lang="postcss" scoped>
+.pool-dashboard-alarms {
+  height: 100%;
+}
+</style>
