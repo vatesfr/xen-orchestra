fb7f21f6ffe2aad0940a9492112118bc31cc13ab
unel <enee.prog@gmail.com>
2025-05-06 16:48:17 +0200

    fix(xo6/pool-dashboard): fix for match with figma

--- @xen-orchestra/web/src/components/pool/dashboard/hostsCpuUsage.vue
+++ @xen-orchestra/web/src/components/pool/dashboard/hostsCpuUsage.vue
@@ -10,20 +10,6 @@
         :max="cpuUsage.total"
         :legend="cpuUsage.name"
       />
-      <div class="total">
-        <UiCardNumbers
-          :label="$t('total-used')"
-          :unit="$t('vcpus', cpuUsages.cpuUsageTotal)"
-          :value="cpuUsages.cpuUsageTotal"
-          size="medium"
-        />
-        <UiCardNumbers
-          :label="$t('total-free')"
-          :unit="$t('vcpus', cpuUsages.cpuFreeTotal)"
-          :value="cpuUsages.cpuFreeTotal"
-          size="medium"
-        />
-      </div>
     </template>
   </div>
 </template>
@@ -34,7 +20,6 @@ import { useVmStore } from '@/stores/xo-rest-api/vm.store'
 import type { XoHost } from '@/types/xo/host.type'
 import type { XoVm } from '@/types/xo/vm.type'
 import VtsLoadingHero from '@core/components/state-hero/VtsLoadingHero.vue'
-import UiCardNumbers from '@core/components/ui/card-numbers/UiCardNumbers.vue'
 import UiProgressBar from '@core/components/ui/progress-bar/UiProgressBar.vue'
 import { computed } from 'vue'
 
@@ -47,22 +32,15 @@ const { isReady: areHostReady } = useHostStore().subscribe()
 const { vmsByHost, isReady: areVmsReady } = useVmStore().subscribe()
 
 const cpuUsages = computed(() => {
-  let cpuUsageTotal = 0
-  let cpuFreeTotal = 0
   return {
     hosts: hosts
       .map(host => {
-        const vms = vmsByHost.value.get(host.id)
-        const vmsCpuNumber = vms?.reduce((cpuUsed, vm) => cpuUsed + vm.CPUs.number, 0) ?? 0
-        const cpuFree = host.cpus.cores - vmsCpuNumber
-
-        cpuUsageTotal += vmsCpuNumber
-        cpuFreeTotal += cpuFree
+        const vmsCpuNumber = vmsByHost.value.get(host.id)?.reduce((cpuUsed, vm) => cpuUsed + vm.CPUs.number, 0) ?? 0
 
         return {
           total: host.cpus.cores,
           used: vmsCpuNumber,
-          free: cpuFree,
+          free: host.cpus.cores - vmsCpuNumber,
           id: host.id,
           name: host.name_label,
         }
@@ -72,18 +50,6 @@ const cpuUsages = computed(() => {
           // reproduce calcul in progress bar.
           b.used / (b.total > 1 ? b.total : 1) - a.used / (a.total > 1 ? a.total : 1)
       ),
-    cpuUsageTotal,
-    cpuFreeTotal,
   }
 })
 </script>
-
-<style scoped lang="postcss">
-.hosts-cpu-usage {
-  .total {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    margin-block-start: auto;
-  }
-}
-</style>
