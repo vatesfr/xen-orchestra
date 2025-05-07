d44364d4d5a8b8a2cc915c4129eb4585894e5f6b
Sebastian CZECH <sebastian.czech@vates.tech>
2025-05-07 14:08:15 +0200

    update

--- @xen-orchestra/web/src/stores/xo-rest-api/user.store.ts
+++
@@ -1,9 +0,0 @@
-import { createXoStoreConfig } from '@/utils/create-xo-store-config.util'
-import { createSubscribableStoreContext } from '@core/utils/create-subscribable-store-context.util'
-import { defineStore } from 'pinia'
-
-export const useUserStore = defineStore('user', () => {
-  const config = createXoStoreConfig('user')
-
-  return createSubscribableStoreContext(config, {})
-})
