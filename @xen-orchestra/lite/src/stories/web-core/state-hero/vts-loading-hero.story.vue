4d741e5dd09d894cc30b4ea9af382ea76a7488fa
Sebastian CZECH <sebastian.czech@vates.tech>
2025-05-07 13:47:01 +0200

    feedback

--- @xen-orchestra/lite/src/stories/web-core/state-hero/vts-loading-hero.story.vue
+++ @xen-orchestra/lite/src/stories/web-core/state-hero/vts-loading-hero.story.vue
@@ -5,13 +5,14 @@
       prop('type').required().enum('page', 'card', 'table', 'panel').preset('card').widget(),
       slot('title').help('Meant to receive title'),
       slot('text').help('Meant to receive text'),
-      setting('title').preset('Please wait').widget(text()),
-      setting('text').preset('This is a loading text').widget(text()),
+      setting('title').preset('').widget(text()),
+      setting('text').preset('').widget(text()),
     ]"
+    :presets
   >
     <VtsLoadingHero v-bind="properties">
-      <template #title> {{ settings.title }}</template>
-      <template #text> {{ settings.text }}</template>
+      <template v-if="settings.title" #title>{{ settings.title }}</template>
+      <template v-if="settings.text" #text>{{ settings.text }}</template>
     </VtsLoadingHero>
   </ComponentStory>
 </template>
@@ -21,4 +22,19 @@ import ComponentStory from '@/components/component-story/ComponentStory.vue'
 import { prop, setting, slot } from '@/libs/story/story-param'
 import { text } from '@/libs/story/story-widget.ts'
 import VtsLoadingHero from '@core/components/state-hero/VtsLoadingHero.vue'
+
+const presets = {
+  'with text': {
+    settings: {
+      title: 'Please wait',
+      text: 'This is a loading text',
+    },
+  },
+  'without text': {
+    settings: {
+      title: '',
+      text: '',
+    },
+  },
+}
 </script>
--- @xen-orchestra/web-core/lib/components/state-hero/VtsLoadingHero.vue
+++ @xen-orchestra/web-core/lib/components/state-hero/VtsLoadingHero.vue
@@ -3,8 +3,8 @@
     <VtsStateHero :type busy>
       {{ $t('loading-in-progress') }}
     </VtsStateHero>
-    <div class="content">
-      <div v-if="slots.title" class="title" :class="typoClass">
+    <div v-if="slots.title || slots.text" class="content">
+      <div v-if="slots.title" class="title" :class="className">
         <slot name="title" />
       </div>
       <div v-if="slots.text" class="text typo-body-bold">
@@ -18,16 +18,16 @@
 import VtsStateHero, { type StateHeroType } from '@core/components/state-hero/VtsStateHero.vue'
 import { computed } from 'vue'
 
-const props = defineProps<{
+const { type } = defineProps<{
   type: StateHeroType
 }>()
 
 const slots = defineSlots<{
-  title(): string
-  text(): string
+  title?(): any
+  text?(): any
 }>()
 
-const typoClass = computed(() => (props.type === 'page' ? 'typo-h1' : 'typo-h2'))
+const className = computed(() => (type === 'page' ? 'typo-h1' : 'typo-h2'))
 </script>
 
 <style lang="postcss" scoped>
@@ -35,14 +35,17 @@ const typoClass = computed(() => (props.type === 'page' ? 'typo-h1' : 'typo-h2')
   display: flex;
   flex-direction: column;
   gap: 1.6rem;
+
   .content {
     display: flex;
     flex-direction: column;
     gap: 2.4rem;
     text-align: center;
+
     .title {
       color: var(--color-neutral-txt-primary);
     }
+
     .text {
       color: var(--color-neutral-txt-secondary);
     }
