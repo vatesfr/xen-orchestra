7be66cabef922fd7d94de40cb60e4b11cb6a7d6d
unel <enee.prog@gmail.com>
2025-05-14 13:45:08 +0200

    feat(xo-web): creat poolLicensing component

---
+++ @xen-orchestra/web/src/components/pool/system/PoolLicensing.vue
@@ -0,0 +1,17 @@
+<template>
+  <UiCard class="pool-licensing">
+    <UiTitle>
+      {{ $t('licensing') }}
+    </UiTitle>
+    <VtsStateHero type="card" image="under-construction" />
+  </UiCard>
+</template>
+
+<script setup lang="ts">
+import type { XoPool } from '@/types/xo/pool.type'
+import VtsStateHero from '@core/components/state-hero/VtsStateHero.vue'
+import UiCard from '@core/components/ui/card/UiCard.vue'
+import UiTitle from '@core/components/ui/title/UiTitle.vue'
+
+defineProps<{ pool: XoPool }>()
+</script>
