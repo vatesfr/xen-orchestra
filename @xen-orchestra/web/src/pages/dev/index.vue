ff9f7b7f2895da225430d4081ed660b735671b09
unel <enee.prog@gmail.com>
2025-05-19 14:48:30 +0200

    feat(xo-core): create uiTaskItem from uiQuikTask

--- @xen-orchestra/web/src/pages/dev/index.vue
+++ @xen-orchestra/web/src/pages/dev/index.vue
@@ -1,1 +1,29 @@
-<template>Dev</template>
+<template>
+  <ul>
+    <UiTaskItem :task="task as any" />
+    <UiTaskItem :task="task as any" />
+  </ul>
+</template>
+
+<script lang="ts" setup>
+import UiTaskItem from '@core/components/ui/task-item/UiTaskItem.vue'
+
+const task2 = {
+  id: '12345',
+  name: "c'est cassé",
+  status: 'failure',
+  tag: 'cassé',
+  end: Date.now(),
+  start: Date.now() - 31_536_000_000,
+}
+
+const task = {
+  id: '12345',
+  name: "c'est cassé",
+  status: 'failure',
+  tag: 'cassé',
+  end: Date.now(),
+  start: Date.now() - 31_536_000_000,
+  subtasks: [task2, task2],
+}
+</script>
