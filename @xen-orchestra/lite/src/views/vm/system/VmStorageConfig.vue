14ce24fa8ba22112c9faaa2e0720bf4ca29ee324
Enée DI IORIO <86972822+UnelDev@users.noreply.github.com>
2025-05-20 10:40:17 +0200

    feat(xo-lite/vm): create system page (#8547)

---
+++ @xen-orchestra/lite/src/views/vm/system/VmStorageConfig.vue
@@ -0,0 +1,23 @@
+<template>
+  <UiCard>
+    <UiTitle>
+      {{ $t('storage-configuration') }}
+    </UiTitle>
+    <VtsQuickInfoRow :label="$t('suspend-storage-repository')" :value="srRepo?.uuid ?? $t('none')" />
+  </UiCard>
+</template>
+
+<script setup lang="ts">
+import type { XenApiVm } from '@/libs/xen-api/xen-api.types'
+import { useSrStore } from '@/stores/xen-api/sr.store'
+import VtsQuickInfoRow from '@core/components/quick-info-row/VtsQuickInfoRow.vue'
+import UiCard from '@core/components/ui/card/UiCard.vue'
+import UiTitle from '@core/components/ui/title/UiTitle.vue'
+import { computed } from 'vue'
+
+const { vm } = defineProps<{ vm: XenApiVm }>()
+
+const { getByOpaqueRef } = useSrStore().subscribe()
+
+const srRepo = computed(() => getByOpaqueRef(vm.suspend_SR))
+</script>
