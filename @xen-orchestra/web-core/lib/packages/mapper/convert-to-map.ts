53c75b8bbeef4d42abeca7384f96323f2e8642c1
Thierry <thierry@byscripts.info>
2025-05-22 08:39:05 +0200

    feedback: fix import error

--- @xen-orchestra/web-core/lib/packages/mapper/convert-to-map.ts
+++ @xen-orchestra/web-core/lib/packages/mapper/convert-to-map.ts
@@ -1,12 +1,8 @@
-import type { Mapping } from './types.ts'
+import type { AnyMapping } from './types.ts'
 import { toValue } from 'vue'
 
-export function convertToMap<TFrom, TTo>(_mapping: Mapping<TFrom, TTo>): Map<TFrom, TTo> {
+export function convertToMap(_mapping: AnyMapping): Map<unknown, unknown> {
   const mapping = toValue(_mapping)
 
-  return mapping instanceof Map
-    ? mapping
-    : Array.isArray(mapping)
-      ? new Map<TFrom, TTo>(mapping)
-      : new Map<TFrom, TTo>(Object.entries(mapping) as [TFrom, TTo][])
+  return mapping instanceof Map ? mapping : Array.isArray(mapping) ? new Map(mapping) : new Map(Object.entries(mapping))
 }
