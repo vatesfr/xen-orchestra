547b6144a700d9d381af620a987566caeace8d57
Thierry Goettelmann <thierry@byscripts.info>
2025-05-22 17:57:40 +0200

    Add missing component to index.ts

--- @xen-orchestra/web-core/lib/packages/icon/index.ts
+++ @xen-orchestra/web-core/lib/packages/icon/index.ts
@@ -2,6 +2,7 @@ export * from './define-icon.ts'
 export * from './define-icon-pack.ts'
 export * from './types.ts'
 
+export { default as DisplayIcon } from './DisplayIcon.vue'
 export { default as DisplayIconAny } from './DisplayIconAny.vue'
 export { default as DisplayIconSingle } from './DisplayIconSingle.vue'
 export { default as DisplayIconStack } from './DisplayIconStack.vue'
