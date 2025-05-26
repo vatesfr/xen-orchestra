10c3371f0923017603189040b322c1f0a5680c7c
unel <enee.prog@gmail.com>
2025-05-26 17:30:09 +0200

    feat(xo6): update poolTable

--- @xen-orchestra/web/src/types/xo/server.type.ts
+++ @xen-orchestra/web/src/types/xo/server.type.ts
@@ -1,13 +1,18 @@
+import type { Branded } from '@core/types/utility.type'
 import type { XoPool } from './pool.type'
 
 export type XoServer = {
-  label: string
-  host: string
-  username: string
-  readOnly: boolean
   allowUnauthorized: boolean
-  poolId: XoPool['id']
+  enabled: boolean
+  error?: Record<string, unknown>
+  host: string
   httpProxy?: string
+  id: Branded<'server'>
+  label?: string
+  poolId?: XoPool['id']
+  poolNameDescription?: string
   poolNameLabel?: string
+  readOnly: boolean
   status: 'connected' | 'disconnected' | 'connecting'
+  username: string
 }
