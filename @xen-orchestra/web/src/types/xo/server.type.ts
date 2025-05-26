46442d4c0502eee94807a63fdbebef92e299f447
unel <enee.prog@gmail.com>
2025-05-26 10:39:52 +0200

    feat(xo6): add poolsTable in site

--- @xen-orchestra/web/src/types/xo/server.type.ts
+++ @xen-orchestra/web/src/types/xo/server.type.ts
@@ -1,13 +1,13 @@
-import type { Branded } from '@core/types/utility.type'
 import type { XoPool } from './pool.type'
 
 export type XoServer = {
-  id: Branded<'server'>
   label: string
   host: string
-  httpProxy?: string
   username: string
   readOnly: boolean
   allowUnauthorized: boolean
-  poolId?: XoPool['id']
+  poolId: XoPool['id']
+  httpProxy?: string
+  poolNameLabel?: string
+  status: 'connected' | 'disconnected' | 'connecting'
 }
