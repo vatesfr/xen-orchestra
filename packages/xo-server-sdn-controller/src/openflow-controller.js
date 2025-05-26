e6491e088ecb3ba57d6b062bbb3b7a74a4442467
Florent BEAUCHAMP <florent.beauchamp@vates.tech>
2025-05-26 07:57:07 +0000

    cleanup

--- packages/xo-server-sdn-controller/src/openflow-controller.js
+++ packages/xo-server-sdn-controller/src/openflow-controller.js
@@ -1,15 +1,17 @@
+import { createLogger } from '@xen-orchestra/log'
 import { OpenFlowPlugin } from './openflow-plugin'
 import { OpenFlowChannel } from './protocol/openflow-channel'
 
+const log = createLogger('xo:sdn-controller:openflowcontroller')
 export async function instantiateController(host, tlsHelper) {
   try {
     const controller = new OpenFlowPlugin(host)
     await controller.check(host)
-    console.log('USE OPENFLOW')
+    log.info('use xapi plugin')
     return controller
   } catch (error) {
     if (error.code === 'XENAPI_MISSING_PLUGIN') {
-      console.log('FALL BACK TO DIRECT',{error})
+      log.info('use direct poenflow channel')
       return new OpenFlowChannel(host, tlsHelper)
     } else {
       throw error
