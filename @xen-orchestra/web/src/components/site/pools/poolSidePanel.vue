8678ee38a4f9c1c08f632c082de55fb27cd5a3c5
unel <enee.prog@gmail.com>
2025-05-27 14:47:44 +0200

    feat(xo6/uiCardNumber): add connection in poolSidePanel

--- @xen-orchestra/web/src/components/site/pools/poolSidePanel.vue
+++ @xen-orchestra/web/src/components/site/pools/poolSidePanel.vue
@@ -1,5 +1,6 @@
 <template>
-  <UiPanel>
+  <VtsLoadingHero v-if="!isPoolReady && !isHostready" type="panel" />
+  <UiPanel v-else>
     <template #header>
       <UiButton
         v-tooltip="$t('coming-soon')"
@@ -32,7 +33,14 @@
           <!-- Pool -->
           <VtsCardRowKeyValue>
             <template #key>{{ $t('pool') }}</template>
-            <template #value>{{ server.poolNameLabel }}</template>
+            <template #value>
+              <UiLink :icon="faCity" size="small" :to="`/pool/${server.poolId}/`">
+                {{ server.poolNameLabel }}
+              </UiLink>
+            </template>
+            <template #addons>
+              <VtsCopyButton :value="server.poolNameLabel ?? ''" />
+            </template>
           </VtsCardRowKeyValue>
           <!-- ID -->
           <VtsCardRowKeyValue>
@@ -53,55 +61,122 @@
           <!-- tag -->
           <VtsCardRowKeyValue>
             <template #key>{{ $t('tags') }}</template>
-            <template #value>{{ server. }}</template>
-          </VtsCardRowKeyValue>
-          <!-- ENABLED -->
-          <VtsCardRowKeyValue>
-            <template #key>{{ $t('enabled') }}</template>
-            <template #value>{{ server.enabled ? $t('yes') : $t('no') }}</template>
-          </VtsCardRowKeyValue>
-          <!-- READ ONLY -->
-          <VtsCardRowKeyValue>
-            <template #key>{{ $t('read-only') }}</template>
-            <template #value>{{ server.readOnly ? $t('yes') : $t('no') }}</template>
-          </VtsCardRowKeyValue>
-          <!-- POOL NAME -->
-          <VtsCardRowKeyValue v-if="server.poolNameLabel">
-            <template #key>{{ $t('pool') }}</template>
-            <template #value>{{ server.poolNameLabel }}</template>
-            <template #addons>
-              <VtsCopyButton :value="server.poolNameLabel" />
+            <template v-if="(pool?.tags.length ?? 0) > 0" #value>
+              <div class="tags">
+                <UiTag v-for="tag in pool?.tags" :key="tag" accent="info" variant="primary">{{ tag }}</UiTag>
+              </div>
             </template>
-          </VtsCardRowKeyValue>
-          <!-- POOL DESCRIPTION -->
-          <VtsCardRowKeyValue v-if="server.poolNameDescription">
-            <template #key>{{ $t('description') }}</template>
-            <template #value>{{ server.poolNameDescription }}</template>
             <template #addons>
-              <VtsCopyButton :value="server.poolNameDescription" />
+              <VtsCopyButton :value="pool?.tags.join(', ')!" />
             </template>
           </VtsCardRowKeyValue>
         </div>
+      </UiCard>
+      <UiCard class="card-container">
+        <UiCardTitle class="typo-body-bold text-ellipsis">
+          {{ $t('connection') }}
+        </UiCardTitle>
+        <VtsCardRowKeyValue>
+          <template #key>{{ $t('status') }}</template>
+          <template #value>
+            <UiInfo :accent="connectionStatus.accent">
+              {{ connectionStatus.text }}
+            </UiInfo>
+          </template>
+        </VtsCardRowKeyValue>
+        <VtsCardRowKeyValue>
+          <template #key>{{ $t('primary-host') }}</template>
+          <template v-if="primaryHost" #value>
+            <UiLink :icon="faServer" size="small" :to="`/host/${primaryHost.id}/`">
+              {{ primaryHost.name_label }}
+            </UiLink>
+          </template>
+          <template #addons>
+            <VtsCopyButton :value="primaryHost?.name_label ?? ''" />
+          </template>
+        </VtsCardRowKeyValue>
+        <VtsCardRowKeyValue>
+          <template #key>{{ $t('ip-address') }}</template>
+          <template #value> {{ server.host }}</template>
+          <template #addons>
+            <VtsCopyButton :value="server.host" />
+          </template>
+        </VtsCardRowKeyValue>
+        <VtsCardRowKeyValue>
+          <template #key>{{ $t('proxy-url') }}</template>
+          <template #value> {{ server.httpProxy }}</template>
+          <template #addons>
+            <VtsCopyButton :value="server.httpProxy ?? ''" />
+          </template>
+        </VtsCardRowKeyValue>
+        <VtsCardRowKeyValue>
+          <template #key>{{ $t('username') }}</template>
+          <template #value> {{ server.username }}</template>
+          <template #addons>
+            <VtsCopyButton :value="server.username" />
+          </template>
+        </VtsCardRowKeyValue>
+        <VtsCardRowKeyValue>
+          <template #key>{{ $t('read-only') }}</template>
+          <template #value>
+            <VtsEnabledState :enabled="server.readOnly" />
+          </template>
+        </VtsCardRowKeyValue>
+        <VtsCardRowKeyValue />
       </UiCard>
     </template>
   </UiPanel>
 </template>
 
 <script setup lang="ts">
+import { useHostStore } from '@/stores/xo-rest-api/host.store'
+import { usePoolStore } from '@/stores/xo-rest-api/pool.store'
 import type { XoServer } from '@/types/xo/server.type'
 import VtsCardRowKeyValue from '@core/components/card/VtsCardRowKeyValue.vue'
 import VtsCopyButton from '@core/components/copy-button/VtsCopyButton.vue'
+import VtsEnabledState from '@core/components/enabled-state/VtsEnabledState.vue'
+import VtsLoadingHero from '@core/components/state-hero/VtsLoadingHero.vue'
 import UiButton from '@core/components/ui/button/UiButton.vue'
 import UiButtonIcon from '@core/components/ui/button-icon/UiButtonIcon.vue'
 import UiCard from '@core/components/ui/card/UiCard.vue'
 import UiCardTitle from '@core/components/ui/card-title/UiCardTitle.vue'
+import UiInfo, { type InfoAccent } from '@core/components/ui/info/UiInfo.vue'
+import UiLink from '@core/components/ui/link/UiLink.vue'
 import UiPanel from '@core/components/ui/panel/UiPanel.vue'
+import UiTag from '@core/components/ui/tag/UiTag.vue'
 import { vTooltip } from '@core/directives/tooltip.directive'
-import { faEdit, faEllipsis, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faCity, faEdit, faEllipsis, faServer, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { computed, type ComputedRef } from 'vue'
+import { useI18n } from 'vue-i18n'
 
 const { server } = defineProps<{
   server: XoServer
 }>()
+
+const { t } = useI18n()
+
+const { isReady: isPoolReady, get: getpool } = usePoolStore().subscribe()
+const { isReady: isHostready, getMasterHostByPoolId } = useHostStore().subscribe()
+const pool = computed(() => (server.poolId ? getpool(server.poolId) : undefined))
+
+const connectionStatus: ComputedRef<{ accent: InfoAccent; text: string }> = computed(() => {
+  if (server.error) {
+    return { accent: 'danger', text: t('error') }
+  }
+
+  switch (server.status) {
+    case 'disconnected':
+      return { accent: 'muted', text: t('disconnected') }
+    case 'connected':
+      return { accent: 'success', text: t('connected') }
+    case 'connecting':
+      return { accent: 'info', text: t('connecting') }
+    default:
+      return { accent: 'muted', text: t('unknown') }
+  }
+})
+
+const primaryHost = computed(() => (server.poolId ? getMasterHostByPoolId(server.poolId) : undefined))
 </script>
 
 <style scoped lang="postcss">
@@ -115,8 +190,8 @@ const { server } = defineProps<{
     flex-direction: column;
     gap: 0.4rem;
 
-    .value:empty::before {
-      content: '-';
+    .tags {
+      display: flex;
     }
   }
 }
--- @xen-orchestra/web/src/pages/pools.vue
+++ @xen-orchestra/web/src/pages/pools.vue
@@ -1,9 +1,13 @@
 <template>
   <VtsNoDataHero v-if="!records.length" type="page" />
   <div v-else class="pools">
-    <UiCard v-if="true" class="container">
+    <UiCard v-if="isReady" class="container">
       <PoolsTable :servers="records" />
     </UiCard>
+    <UiPanel v-else>
+      <VtsNoSelectionHero type="panel" />
+    </UiPanel>
+    <PoolSidePanel v-if="selectedServer" :server="selectedServer" />
     <UiPanel v-else>
       <VtsNoSelectionHero type="panel" />
     </UiPanel>
@@ -11,12 +15,26 @@
 </template>
 
 <script setup lang="ts">
+import PoolSidePanel from '@/components/site/pools/poolSidePanel.vue'
 import PoolsTable from '@/components/site/pools/PoolsTable.vue'
 import { useServerStore } from '@/stores/xo-rest-api/server.store'
+import type { XoServer } from '@/types/xo/server.type'
 import VtsNoDataHero from '@core/components/state-hero/VtsNoDataHero.vue'
 import VtsNoSelectionHero from '@core/components/state-hero/VtsNoSelectionHero.vue'
 import UiCard from '@core/components/ui/card/UiCard.vue'
 import UiPanel from '@core/components/ui/panel/UiPanel.vue'
+import { useRouteQuery } from '@core/composables/route-query.composable'
 
-const { records } = useServerStore().subscribe()
+const { records, isReady, get } = useServerStore().subscribe()
+
+const selectedServer = useRouteQuery<XoServer | undefined>('id', {
+  toData: id => get(id as XoServer['id']),
+  toQuery: network => network?.id ?? '',
+})
 </script>
+
+<style lang="postcss" scoped>
+.pools {
+  display: flex;
+}
+</style>
