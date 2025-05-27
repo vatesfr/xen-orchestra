1c83ebc3b4a136d04af810a3c820448adf9a20ed
Olivier Floch <olivier.floch@vates.fr>
2025-05-27 16:00:01 +0200

    fix(xo-6/vm): add required comment in vm index.vue file to prevent build fail

--- @xen-orchestra/web/src/pages/vm/[id]/index.vue
+++ @xen-orchestra/web/src/pages/vm/[id]/index.vue
@@ -1,4 +1,7 @@
 <script lang="ts" setup>
+// [DO NOT REMOVE]
+// To fix a Vite error during the build process, this comment is required.
+// The `definePage` macro is removed during the build, leaving an empty script tag that causes the build to fail.
 definePage({
   name: '/vm/:id',
   redirect: (to: any) => `/vm/${to.params.id}/console`,
