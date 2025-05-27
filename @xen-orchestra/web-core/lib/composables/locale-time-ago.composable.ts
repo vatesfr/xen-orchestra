698801f6a2fd8a336f4541bcff92d74a1937051d
Joris-K <joris.kosacki@vates.tech>
2025-05-27 16:39:07 +0200

    review feedback

--- @xen-orchestra/web-core/lib/composables/locale-time-ago.composable.ts
+++ @xen-orchestra/web-core/lib/composables/locale-time-ago.composable.ts
@@ -1,40 +1,53 @@
-import type { UseTimeAgoMessages, UseTimeAgoUnitNamesDefault } from '@vueuse/core'
-import { useTimeAgo } from '@vueuse/core'
+import { useTimestamp } from '@vueuse/core'
+import { computed, type MaybeRefOrGetter, toValue } from 'vue'
+import { useI18n } from 'vue-i18n'
 
-export function useLocaleTimeAgo(date: Date, t: (key: string, params?: any) => string) {
-  const pf = (past: boolean) => (past ? '.past' : '.future')
-  const messages: UseTimeAgoMessages<UseTimeAgoUnitNamesDefault> = {
-    justNow: t('time-ago.just-now'),
-    past: n => n,
-    future: n => n,
-    month: (n, past) =>
-      n === 1
-        ? past
-          ? t('time-ago.last-month')
-          : t('time-ago.next-month')
-        : t('time-ago.month' + pf(past), { count: n }),
-    year: (n, past) =>
-      n === 1
-        ? past
-          ? t('time-ago.last-year')
-          : t('time-ago.next-year')
-        : t('time-ago.year' + pf(past), { count: n }),
-    day: (n, past) =>
-      n === 1 ? (past ? t('time-ago.yesterday') : t('time-ago.tomorrow')) : t('time-ago.day' + pf(past), { count: n }),
-    week: (n, past) =>
-      n === 1
-        ? past
-          ? t('time-ago.last-week')
-          : t('time-ago.next-week')
-        : t('time-ago.week' + pf(past), { count: n }),
-    hour: (n, past) => t('time-ago.hour' + pf(past), { count: n }),
-    minute: (n, past) => t('time-ago.minute' + pf(past), { count: n }),
-    second: (n, past) => t('time-ago.second' + pf(past), { count: n }),
-    invalid: t(''),
-  }
+export enum SECONDS {
+  MINUTE = 60,
+  HOUR = SECONDS.MINUTE * 60,
+  DAY = SECONDS.HOUR * 24,
+  WEEK = SECONDS.DAY * 7,
+  MONTH = SECONDS.DAY * 30,
+  YEAR = SECONDS.DAY * 365,
+}
 
-  return useTimeAgo(date, {
-    fullDateFormatter: date => date.toLocaleString(),
-    messages,
+export type TimeThreshold = [number, Intl.RelativeTimeFormatUnit]
+
+const timeThresholds: TimeThreshold[] = [
+  [SECONDS.MINUTE, 'second'],
+  [SECONDS.HOUR, 'minute'],
+  [SECONDS.DAY, 'hour'],
+  [SECONDS.WEEK, 'day'],
+  [SECONDS.MONTH, 'week'],
+  [SECONDS.YEAR, 'month'],
+  [Infinity, 'year'],
+]
+
+export function useTimeAgo(referenceDate: MaybeRefOrGetter<Date | number | string>) {
+  const { locale } = useI18n()
+
+  const formatter = computed(() => new Intl.RelativeTimeFormat(locale.value, { numeric: 'auto' }))
+
+  const now = useTimestamp({ interval: 1000 })
+
+  const referenceTime = computed(() => new Date(toValue(referenceDate)).getTime())
+
+  const distance = computed(() => {
+    const diff = Math.trunc((referenceTime.value - now.value) / 1000)
+
+    if (Math.abs(diff) < 60) {
+      return Math.trunc(diff / 10) * 10 || 0 // Avoid recomputing when the value changes from 0 to -0
+    }
+
+    return timeThresholds.reduce(
+      (acc, [threshold]) => (Math.abs(acc) < threshold ? acc : Math.trunc(acc / threshold) * threshold),
+      diff
+    )
+  })
+
+  return computed(() => {
+    const index = timeThresholds.findIndex(([threshold]) => Math.abs(distance.value) < threshold)
+    const divisor = index > 0 ? timeThresholds[index - 1][0] : 1
+    return formatter.value.format(Math.trunc(distance.value / divisor), timeThresholds[index][1])
   })
 }
