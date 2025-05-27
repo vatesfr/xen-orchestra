9ed68a603e1a911bec4b01a537bd42036ce33daf
Joris-K <joris.kosacki@vates.tech>
2025-05-27 16:14:36 +0200

    fix issue with value formatter

--- @xen-orchestra/lite/src/components/vm/dashboard/VmDashboardVdiUsageChart.vue
+++ @xen-orchestra/lite/src/components/vm/dashboard/VmDashboardVdiUsageChart.vue
@@ -36,34 +36,51 @@ const VtsLinearChart = defineAsyncComponent(() => import('@core/components/linea
 
 const { t } = useI18n()
 
-const vdiUsage = computed<LinearChartData>(() => {
+const vdiUsage = computed((): LinearChartData => {
   const { stats, timestampStart } = data
 
-  const { xvds } = stats ?? {}
+  const xvds = stats?.xvds
 
-  if (xvds === undefined) {
+  if (!xvds) {
     return []
   }
 
-  const addVdiData = (type: 'r' | 'w') => ({
-    label: type === 'r' ? t('vdi-read') : t('vdi-write'),
+  const addVdiData = (type: 'r' | 'w') => {
+    const xvdsArrays = Object.values(xvds[type])
 
-    data: Object.values(xvds[type])[0].map((_, index) => ({
-      timestamp:
-        (timestampStart -
-          RRD_STEP_FROM_STRING.hours * (Object.values(xvds[type])[0].length - 1) +
-          index * RRD_STEP_FROM_STRING.hours) *
-        1000,
-      value: Object.values(xvds[type]).reduce((sum, values) => sum + values[index], 0),
-    })),
-  })
+    if (xvdsArrays.length === 0) {
+      return {
+        label: '',
+        data: [],
+      }
+    }
+
+    const data = Array.from({ length: xvdsArrays[0].length }, (_, idx) => {
+      const timestamp =
+        (timestampStart - RRD_STEP_FROM_STRING.hours * (xvdsArrays[0].length - 1) + idx * RRD_STEP_FROM_STRING.hours) *
+        1000
+
+      const value = xvdsArrays.reduce((sum, arr) => sum + (arr[idx] ?? 0), 0)
+
+      return {
+        timestamp,
+        // Sometimes we got infinity values in the result, we need to replace it with null
+        value: Number.isFinite(value) ? value : null,
+      }
+    })
+
+    return {
+      label: type === 'r' ? t('vdi-read') : t('vdi-write'),
+      data,
+    }
+  }
 
   return [addVdiData('r'), addVdiData('w')]
 })
 
 const maxValue = computed(() => {
   const values = vdiUsage.value.reduce(
-    (acc, series) => [...acc, ...series.data.map(item => item.value)],
+    (acc, series) => [...acc, ...series.data.map(item => item.value ?? 0)],
     [] as number[]
   )
 
@@ -71,11 +88,16 @@ const maxValue = computed(() => {
     return 100
   }
 
-  const maxUsage = Math.max(...values)
+  const maxUsage = Math.max(...values) * 1.2
 
   return Math.ceil(maxUsage / 100) * 100
 })
-const byteFormatter = (value: number) => {
+
+const byteFormatter = (value: number | null) => {
+  if (value === null) {
+    return ''
+  }
+
   const result = formatSizeRaw(value, 1)
 
   return `${result?.value}${result?.prefix}`
