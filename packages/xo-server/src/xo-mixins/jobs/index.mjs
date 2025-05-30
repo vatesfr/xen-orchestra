e5e3060b7b13d304e3cda2222bd60bfe97b3a319
b-Nollet <bastien.nollet@vates.tech>
2025-05-30 09:46:41 +0200

    chore(xo-server): remove unused value

--- packages/xo-server/src/xo-mixins/jobs/index.mjs
+++ packages/xo-server/src/xo-mixins/jobs/index.mjs
@@ -276,7 +276,7 @@ export default class Jobs {
       runs[runJobId] = { cancel }
       $defer(() => delete runs[runJobId])
 
-      const status = await executor({
+      await executor({
         app,
         cancelToken: token,
         connection,
@@ -296,7 +296,7 @@ export default class Jobs {
         true
       )
 
-      app.emit('job:terminated', runJobId, { status, type })
+      app.emit('job:terminated', runJobId, { type })
     } catch (error) {
       await logger.error(
         `The execution of ${id} has failed.`,
