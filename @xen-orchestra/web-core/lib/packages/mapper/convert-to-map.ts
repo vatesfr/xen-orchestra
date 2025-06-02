9c55d48b3eb82cae5b351e58f5ab104bc731589c
Thierry <thierry@byscripts.info>
2025-06-02 14:36:41 +0200

    remove unnecessary code

--- @xen-orchestra/web-core/lib/packages/mapper/convert-to-map.ts
+++ @xen-orchestra/web-core/lib/packages/mapper/convert-to-map.ts
@@ -1,8 +1,5 @@
 import type { AnyMapping } from './types.ts'
-import { toValue } from 'vue'
 
-export function convertToMap(_mapping: AnyMapping): Map<unknown, unknown> {
-  const mapping = toValue(_mapping)
-
+export function convertToMap(mapping: AnyMapping): Map<unknown, unknown> {
   return mapping instanceof Map ? mapping : Array.isArray(mapping) ? new Map(mapping) : new Map(Object.entries(mapping))
 }
