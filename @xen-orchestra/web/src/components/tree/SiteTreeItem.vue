ecb7fe33600c35f3ea74315513e9c19e519a5e24
Olivier Floch <olivier.floch@vates.fr>
2025-06-02 17:58:48 +0200

    feat(xo-6/treeview): introduce site level in treeview

---
+++ @xen-orchestra/web/src/components/tree/SiteTreeItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <VtsTreeItem :expanded="branch.isExpanded">
+    <UiTreeItemLabel :icon="faSatellite" route="/" @toggle="branch.toggleExpand()">
+      {{ branch.data.name_label }}
+    </UiTreeItemLabel>
+    <template v-if="branch.hasChildren" #sublist>
+      <PoolTreeList :branches="branch.children" />
+    </template>
+  </VtsTreeItem>
+</template>
+
+<script lang="ts" setup>
+import PoolTreeList from '@/components/tree/PoolTreeList.vue'
+import type { SiteBranch } from '@/types/tree.type'
+import VtsTreeItem from '@core/components/tree/VtsTreeItem.vue'
+import UiTreeItemLabel from '@core/components/ui/tree-item-label/UiTreeItemLabel.vue'
+import { faSatellite } from '@fortawesome/free-solid-svg-icons'
+
+defineProps<{
+  branch: SiteBranch
+}>()
+</script>
