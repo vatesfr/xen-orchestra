a69a8568ce36397c3cf485e839137b3c9296fef3
unel <enee.prog@gmail.com>
2025-06-04 11:22:33 +0200

    feat(xo6/connectPool): create page

---
+++ @xen-orchestra/web/src/pages/(site)/connectPool.vue
@@ -0,0 +1,9 @@
+<template>
+  <ConnectPoolHeader />
+  <ConnectionCard />
+</template>
+
+<script setup lang="ts">
+import ConnectionCard from '@/components/site/connectPool/ConnectionCard.vue'
+import ConnectPoolHeader from '@/components/site/connectPool/ConnectPoolHeader.vue'
+</script>
