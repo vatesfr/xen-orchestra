a9cc796c838af014a49e89d47852d68cfc5ed7a7
unel <enee.prog@gmail.com>
2025-06-04 16:36:31 +0200

    feat(xo6/connectPool): move page

--- @xen-orchestra/web/src/pages/(site)/connectPool.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <ConnectPoolHeader />
-  <ConnectionCard />
-</template>
-
-<script setup lang="ts">
-import ConnectionCard from '@/components/site/connectPool/ConnectionCard.vue'
-import ConnectPoolHeader from '@/components/site/connectPool/ConnectPoolHeader.vue'
-</script>
