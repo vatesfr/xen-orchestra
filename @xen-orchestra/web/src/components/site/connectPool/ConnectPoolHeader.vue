2b533ff216777bcbb0dcf86745fd9193180db9be
unel <enee.prog@gmail.com>
2025-06-04 11:22:33 +0200

    feat(xo6/connectPool): create page

---
+++ @xen-orchestra/web/src/components/site/connectPool/ConnectPoolHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  <UiHeadBar :icon="faLink">
+    {{ $t('connect-pool') }}
+  </UiHeadBar>
+</template>
+
+<script setup lang="ts">
+import UiHeadBar from '@core/components/ui/head-bar/UiHeadBar.vue'
+import { faLink } from '@fortawesome/free-solid-svg-icons'
+</script>
