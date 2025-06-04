122690d94408aa4d9e4d524e7a8f88a6d53ea0c7
unel <enee.prog@gmail.com>
2025-06-04 11:22:33 +0200

    feat(xo6/connectPool): create page

---
+++ @xen-orchestra/web/src/components/site/connectPool/ConnectionCard.vue
@@ -0,0 +1,78 @@
+<template>
+  <UiCard class="pool-connection-card">
+    <div class="input-content">
+      <UiTitle class="primary-host-title">{{ $t('primary-host') }}</UiTitle>
+      <VtsInputWrapper :label="$t('ip-address')" required>
+        <UiInput v-model="ip" accent="brand" />
+        <UiInfo accent="info" wrap>
+          {{ $t('pool-connection-ip-info') }}
+        </UiInfo>
+      </VtsInputWrapper>
+      <VtsInputWrapper :label="$t('proxy-url')"> <UiInput v-model="proxy" accent="brand" /> </VtsInputWrapper>
+      <VtsInputWrapper :label="$t('username')" required>
+        <UiInput v-model="username" accent="brand" />
+        <UiInfo accent="info" wrap>
+          {{ $t('root-by-default') }}
+        </UiInfo>
+      </VtsInputWrapper>
+      <VtsInputWrapper :label="$t('password')" required>
+        <UiInput v-model="password" accent="brand" />
+      </VtsInputWrapper>
+    </div>
+    <UiTitle>{{ $t('option') }}</UiTitle>
+    <div class="checkbox-wrapper">
+      <UiCheckbox v-model="readOnly" accent="brand">{{ $t('read-only') }}</UiCheckbox>
+      <UiCheckbox v-model="selfSigned" accent="brand">{{ $t('accept-self-signed-certificates') }}</UiCheckbox>
+    </div>
+    <div class="button-warper">
+      <UiButton accent="brand" size="medium" variant="secondary">{{ $t('cancel') }}</UiButton>
+      <UiButton accent="brand" size="medium" variant="primary">{{ $t('connect') }}</UiButton>
+    </div>
+  </UiCard>
+</template>
+
+<script setup lang="ts">
+import VtsInputWrapper from '@core/components/input-wrapper/VtsInputWrapper.vue'
+import UiButton from '@core/components/ui/button/UiButton.vue'
+import UiCard from '@core/components/ui/card/UiCard.vue'
+import UiCheckbox from '@core/components/ui/checkbox/UiCheckbox.vue'
+import UiInfo from '@core/components/ui/info/UiInfo.vue'
+import UiInput from '@core/components/ui/input/UiInput.vue'
+import UiTitle from '@core/components/ui/title/UiTitle.vue'
+import { ref } from 'vue'
+const ip = ref()
+const proxy = ref()
+const username = ref()
+const password = ref()
+const readOnly = ref(false)
+const selfSigned = ref(false)
+</script>
+
+<style lang="postcss" scoped>
+.pool-connection-card {
+  margin: 0.8rem;
+  display: flex;
+  gap: 4rem;
+  .input-content {
+    display: grid;
+    grid-template-columns: 40rem 40rem;
+    gap: 1.6rem 8rem;
+
+    .primary-host-title {
+      grid-column: 1 / 3;
+    }
+  }
+
+  .checkbox-wrapper {
+    display: flex;
+    gap: 8rem;
+  }
+
+  .button-warper {
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    gap: 2.4rem;
+  }
+}
+</style>
