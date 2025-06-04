03b66bd7e2a7e75dfd18a128830dcde974ab779b
unel <enee.prog@gmail.com>
2025-06-04 17:15:15 +0200

    feat(xo6/connectPool): add form

--- @xen-orchestra/web/src/components/pool/connect/ConnectionCard.vue
+++ @xen-orchestra/web/src/components/pool/connect/ConnectionCard.vue
@@ -1,35 +1,37 @@
 <template>
-  <UiCard class="pool-connection-card">
-    <div class="input-warpper">
-      <UiTitle class="primary-host-title">{{ $t('primary-host') }}</UiTitle>
-      <div class="input-content">
-        <VtsInputWrapper :label="$t('ip-address')">
-          <UiInput v-model="ip" accent="brand" required />
-          <UiInfo accent="info" wrap>
-            {{ $t('pool-connection-ip-info') }}
-          </UiInfo>
-        </VtsInputWrapper>
-        <VtsInputWrapper :label="$t('proxy-url')"> <UiInput v-model="proxy" accent="brand" /> </VtsInputWrapper>
-        <VtsInputWrapper :label="$t('username')">
-          <UiInput v-model="username" accent="brand" required />
-          <UiInfo accent="info" wrap>
-            {{ $t('root-by-default') }}
-          </UiInfo>
-        </VtsInputWrapper>
-        <VtsInputWrapper :label="$t('password')">
-          <UiInput v-model="password" accent="brand" required />
-        </VtsInputWrapper>
+  <UiCard>
+    <form class="pool-connection-card">
+      <div class="input-warpper">
+        <UiTitle class="primary-host-title">{{ $t('primary-host') }}</UiTitle>
+        <div class="input-content">
+          <VtsInputWrapper :label="$t('ip-address')">
+            <UiInput v-model="ip" accent="brand" required />
+            <UiInfo accent="info" wrap>
+              {{ $t('pool-connection-ip-info') }}
+            </UiInfo>
+          </VtsInputWrapper>
+          <VtsInputWrapper :label="$t('proxy-url')"> <UiInput v-model="proxy" accent="brand" /> </VtsInputWrapper>
+          <VtsInputWrapper :label="$t('username')">
+            <UiInput v-model="username" accent="brand" required />
+            <UiInfo accent="info" wrap>
+              {{ $t('root-by-default') }}
+            </UiInfo>
+          </VtsInputWrapper>
+          <VtsInputWrapper :label="$t('password')">
+            <UiInput v-model="password" accent="brand" required />
+          </VtsInputWrapper>
+        </div>
       </div>
-    </div>
-    <UiTitle>{{ $t('option') }}</UiTitle>
-    <div class="checkbox-wrapper">
-      <UiCheckbox v-model="readOnly" accent="brand">{{ $t('read-only') }}</UiCheckbox>
-      <UiCheckbox v-model="selfSigned" accent="brand">{{ $t('accept-self-signed-certificates') }}</UiCheckbox>
-    </div>
-    <div class="button-warper">
-      <UiButton accent="brand" size="medium" variant="secondary">{{ $t('cancel') }}</UiButton>
-      <UiButton accent="brand" size="medium" variant="primary">{{ $t('connect') }}</UiButton>
-    </div>
+      <UiTitle>{{ $t('option') }}</UiTitle>
+      <div class="checkbox-wrapper">
+        <UiCheckbox v-model="readOnly" accent="brand">{{ $t('read-only') }}</UiCheckbox>
+        <UiCheckbox v-model="selfSigned" accent="brand">{{ $t('accept-self-signed-certificates') }}</UiCheckbox>
+      </div>
+      <div class="button-warper">
+        <UiButton accent="brand" size="medium" variant="secondary">{{ $t('cancel') }}</UiButton>
+        <UiButton type="submit" accent="brand" size="medium" variant="primary">{{ $t('connect') }}</UiButton>
+      </div>
+    </form>
   </UiCard>
 </template>
 
@@ -54,6 +56,7 @@ const selfSigned = ref(false)
 .pool-connection-card {
   margin: 0.8rem;
   display: flex;
+  flex-direction: column;
   gap: 4rem;
   .input-content {
     display: grid;
