7a59d9e37cae7d3c734f97bcc8a7b14ef5036539
Olivier Floch <olivier.floch@vates.fr>
2025-06-06 13:58:53 +0200

    wip: implement connection to server

---
+++ @xen-orchestra/web/src/pages/pool/connect/success.vue
@@ -0,0 +1,17 @@
+<template>
+  <!--  <VtsStateHero type="page" image="all-good" :title="$t('connect-to-ip', { ip: props.host })"> -->
+  <!--    <UiAlert accent="success" /> -->
+  <!--  </VtsStateHero> -->
+  <div>{{ t('here') }}</div>
+</template>
+
+<script setup lang="ts">
+// import VtsStateHero from '@core/components/state-hero/VtsStateHero.vue'
+// import UiAlert from '@core/components/ui/alert/UiAlert.vue'
+// const props = defineProps<any>()
+
+// console.log(props)
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+</script>
--- @xen-orchestra/web/src/types/xo/server.type.ts
+++ @xen-orchestra/web/src/types/xo/server.type.ts
@@ -1,13 +1,18 @@
-import type { Branded } from '@core/types/utility.type'
-import type { XoPool } from './pool.type'
+import type { XoPool } from '@/types/xo/pool.type.ts'
+import type { Branded } from '@core/types/utility.type.ts'
 
 export type XoServer = {
-  id: Branded<'server'>
-  label: string
+  allowUnauthorized: boolean
+  enabled: boolean
+  error?: Record<string, unknown>
   host: string
   httpProxy?: string
-  username: string
-  readOnly: boolean
-  allowUnauthorized: boolean
+  id: Branded<'server'>
+  label?: string
   poolId?: XoPool['id']
+  poolNameDescription?: string
+  poolNameLabel?: string
+  readOnly: boolean
+  status: 'connected' | 'disconnected' | 'connecting'
+  username: string
 }
