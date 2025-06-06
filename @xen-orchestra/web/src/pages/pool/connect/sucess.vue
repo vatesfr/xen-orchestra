0355c7bf519232c82054171f1da91988c01428b6
unel <enee.prog@gmail.com>
2025-06-06 10:42:42 +0200

    feat(xo6/connectPool): add success pages

---
+++ @xen-orchestra/web/src/pages/pool/connect/sucess.vue
@@ -0,0 +1,13 @@
+<template>
+  <VtsStateHero type="page" image="all-good" :title="$t('connect-to-ip', { ip: props.host })">
+    <UiAlert accent="success" />
+  </VtsStateHero>
+</template>
+
+<script setup lang="ts">
+import VtsStateHero from '@core/components/state-hero/VtsStateHero.vue'
+import UiAlert from '@core/components/ui/alert/UiAlert.vue'
+const props = defineProps<any>()
+
+// console.log(props)
+</script>
