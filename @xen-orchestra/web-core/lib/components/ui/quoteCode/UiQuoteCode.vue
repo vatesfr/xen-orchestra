4a0d38d9afba227947d92c2cbc17b7569df6ecf9
unel <enee.prog@gmail.com>
2025-06-11 17:27:58 +0200

    feat(xo6/uiQuoteCode): create uiQuoteCode

---
+++ @xen-orchestra/web-core/lib/components/ui/quoteCode/UiQuoteCode.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="ui-quote-code">
+    <slot name="title" />
+    <div class="code-container">
+      {{ code }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+type quoteCodeAccent = 'brand' | 'error'
+
+defineProps<{
+  code: string
+  accent: quoteCodeAccent
+}>()
+</script>
+
+<style lang="postcss" scoped>
+.label {
+  color: var(--color-neutral-txt-primary);
+  display: flex;
+  justify-content: space-between;
+}
+
+.code-container {
+  border-inline-start: 0.2rem solid var(--color-brand-item-base);
+  background-color: var(--color-neutral-background-disabled);
+  padding: 0.8rem 1.2rem;
+  border-radius: 0.4rem;
+}
+</style>
--- @xen-orchestra/web/src/pages/dev/index.vue
+++ @xen-orchestra/web/src/pages/dev/index.vue
@@ -1,1 +1,7 @@
-<template>Dev</template>
+<template>
+  <UiQuoteCode accent="brand" size="medium" code="coucouc coucou coucou" label="code" />
+</template>
+
+<script setup lang="ts">
+import UiQuoteCode from '@core/components/ui/quoteCode/UiQuoteCode.vue'
+</script>
