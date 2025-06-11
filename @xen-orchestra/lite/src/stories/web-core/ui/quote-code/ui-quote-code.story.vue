effda7ab8576f0c06f53edbe2d0694f988a06c5a
unel <enee.prog@gmail.com>
2025-06-11 17:27:58 +0200

    feat(xo6/uiQuoteCode): create uiQuoteCode

---
+++ @xen-orchestra/lite/src/stories/web-core/ui/quote-code/ui-quote-code.story.vue
@@ -0,0 +1,22 @@
+<template>
+  <ComponentStory
+    v-slot="{ properties }"
+    :params="[
+      prop('code').preset(`{component: UiQuoteCode}`).type('string').widget(),
+      prop('accent').preset('brand').enum('brand', 'error').widget(),
+      slot('title'),
+    ]"
+  >
+    <UiQuoteCode v-bind="properties">
+      <template #title>
+        <div>hello world</div>
+      </template>
+    </UiQuoteCode>
+  </ComponentStory>
+</template>
+
+<script setup lang="ts">
+import ComponentStory from '@/components/component-story/ComponentStory.vue'
+import { prop, slot } from '@/libs/story/story-param'
+import UiQuoteCode from '@core/components/ui/quoteCode/UiQuoteCode.vue'
+</script>
