a1a9cd93cc98dd52c532541dc141cfae7bdcb050
unel <enee.prog@gmail.com>
2025-06-12 09:51:29 +0200

    feat(xo6/uiQuoteCode): add return button, add style

--- @xen-orchestra/web/src/pages/pool/connect/error.vue
+++ @xen-orchestra/web/src/pages/pool/connect/error.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="error-on-connection">
-    <VtsStateHero image="error" type="page" no-background>
+    <VtsStateHero image="error" type="table" no-background>
       <div class="content">
         <h1>{{ t('unable-to-connect-to', { ip }) }}</h1>
         <UiAlert accent="danger">{{ t('pool-connection-error') }}</UiAlert>
+        <UiButton variant="secondary" accent="brand" size="medium" @click="goBack"> {{ t('go-back') }}</UiButton>
       </div>
     </VtsStateHero>
   </div>
@@ -12,14 +13,24 @@
 <script setup lang="ts">
 import VtsStateHero from '@core/components/state-hero/VtsStateHero.vue'
 import UiAlert from '@core/components/ui/alert/UiAlert.vue'
+import UiButton from '@core/components/ui/button/UiButton.vue'
 import { useI18n } from 'vue-i18n'
+import { useRouter } from 'vue-router'
 
 defineProps<{
   ip: string
-  errror: string
+  errrorJson?: string
 }>()
 
 const { t } = useI18n()
+const router = useRouter()
+
+function goBack() {
+  // do not allow the return to this page without argument.
+  router.replace({
+    name: '/pool/connect/',
+  })
+}
 </script>
 
 <style lang="postcss" scoped>
@@ -32,6 +43,11 @@ const { t } = useI18n()
     flex-direction: column;
     align-items: center;
     gap: 4rem;
+    width: min-content;
+
+    h1 {
+      text-wrap: nowrap;
+    }
   }
 }
 </style>
