1925c046388cc4b1f05161a860d5ae3807cf0e3f
unel <enee.prog@gmail.com>
2025-06-16 16:39:19 +0200

    review

--- @xen-orchestra/web/src/pages/dev/index.vue
+++ @xen-orchestra/web/src/pages/dev/index.vue
@@ -1,7 +1,1 @@
-<template>
-  <UiQuoteCode accent="brand" size="medium" code="coucouc coucou coucou" label="code" />
-</template>
-
-<script setup lang="ts">
-import UiQuoteCode from '@core/components/ui/quoteCode/UiQuoteCode.vue'
-</script>
+<template>dev</template>
