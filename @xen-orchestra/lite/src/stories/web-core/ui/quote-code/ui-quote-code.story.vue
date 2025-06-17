5b055cb6349a0ff708e9dd98d6593ab4da0aedbe
unel <enee.prog@gmail.com>
2025-06-17 14:19:21 +0200

    review

--- @xen-orchestra/lite/src/stories/web-core/ui/quote-code/ui-quote-code.story.vue
+++ @xen-orchestra/lite/src/stories/web-core/ui/quote-code/ui-quote-code.story.vue
@@ -1,18 +1,21 @@
 <template>
   <ComponentStory
-    v-slot="{ properties }"
+    v-slot="{ properties, settings }"
     :params="[
-      prop('code').preset(`{component: UiQuoteCode}`).type('string').widget(),
+      prop('code').preset(`[Code error here]`).type('string').widget(),
       prop('accent').preset('brand').enum('brand', 'error').widget(),
-      slot('title'),
+      prop('size').enum('small', 'medium').required().preset('small').widget(),
+      slot().help('Meant to display the label'),
+      slot('actions').help('Meant to receive UiButton or ButtonIcon (or other) components that will trigger actions'),
+      setting('default').preset('Label').widget(),
+      setting('showDemoButtons').widget(boolean()),
     ]"
   >
     <UiQuoteCode v-bind="properties">
-      <template #title>
-        <span class="title">
-          <div>hello world</div>
-          <VtsCopyButton :value="properties.code" />
-        </span>
+      {{ settings.default }}
+
+      <template v-if="settings.showDemoButtons" #actions>
+        <VtsCopyButton :value="properties.code" />
       </template>
     </UiQuoteCode>
   </ComponentStory>
@@ -20,14 +23,8 @@
 
 <script setup lang="ts">
 import ComponentStory from '@/components/component-story/ComponentStory.vue'
-import { prop, slot } from '@/libs/story/story-param'
+import { prop, setting, slot } from '@/libs/story/story-param'
+import { boolean } from '@/libs/story/story-widget.ts'
 import VtsCopyButton from '@core/components/copy-button/VtsCopyButton.vue'
 import UiQuoteCode from '@core/components/ui/quoteCode/UiQuoteCode.vue'
 </script>
-
-<style lang="postcss" scoped>
-.title {
-  display: flex;
-  justify-content: space-between;
-}
-</style>
