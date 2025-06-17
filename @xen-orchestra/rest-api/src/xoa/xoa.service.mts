vatesfr/xen-orchestra

commit 0c1947aba26b65986e8c5f7d8cd91967fd645166

feat(@xen-orchestra/rest-api): expose dashboard endpoint (#8580)

@xen-orchestra/rest-api/src/xoa/xoa.service.mts
@@ -1,0 +1,506 @@
+import groupBy from 'lodash/groupBy.js'
+import semver from 'semver'
+import {
+  AnyXoVdi,
+  AnyXoVm,
+  BACKUP_TYPE,
+  XoBackupJob,
+  XoHost,
+  XoPool,
+  XoSchedule,
+  XoSr,
+  XoVbd,
+  XoVm,
+} from '@vates/types'
+import { asyncEach } from '@vates/async-each'
+import { createLogger } from '@xen-orchestra/log'
+import { createPredicate } from 'value-matcher'
+import { extractIdsFromSimplePattern } from '@xen-orchestra/backups/extractIdsFromSimplePattern.mjs'
+import { noSuchObject } from 'xo-common/api-errors.js'
+import { parse } from 'xo-remote-parser'
+
+import { type AsyncCacheEntry, getFromAsyncCache } from '../helpers/cache.helper.mjs'
+import { DashboardBackupRepositoriesSizeInfo, DashboardBackupsInfo, XoaDashboard } from './xoa.type.mjs'
+import { isReplicaVm, isSrWritable, vmContainsNoBakTag } from '../helpers/utils.helper.mjs'
+import type { MaybePromise } from '../helpers/helper.type.mjs'
+import { RestApi } from '../rest-api/rest-api.mjs'
+
+const log = createLogger('xo:rest-api:xoa-service')
+
+type DashboardAsyncCache = {
+  backupRepositories: MaybePromise<DashboardBackupRepositoriesSizeInfo>
+  backups: MaybePromise<DashboardBackupsInfo>
+}
+
+export class XoaService {
+  #restApi: RestApi
+  #dashboardAsyncCache = new Map<
+    keyof DashboardAsyncCache,
+    AsyncCacheEntry<DashboardAsyncCache[keyof DashboardAsyncCache]>
+  >()
+  #dashboardCacheOpts: { timeout?: number; expiresIn?: number }
+
+  constructor(restApi: RestApi) {
+    this.#restApi = restApi
+    this.#dashboardCacheOpts = {
+      timeout: this.#restApi.xoApp.config.getOptionalDuration('rest-api.dashboardCacheTimeout'),
+      expiresIn: this.#restApi.xoApp.config.getOptionalDuration('rest-api.dashboardCacheExpiresIn'),
+    }
+  }
+
+  async #getBackupRepositoriesSizeInfo(): Promise<
+    (DashboardBackupRepositoriesSizeInfo & { isExpired?: true }) | undefined
+  > {
+    const brResult = await getFromAsyncCache<DashboardAsyncCache['backupRepositories']>(
+      this.#dashboardAsyncCache as Map<
+        'backupRepositories',
+        AsyncCacheEntry<DashboardAsyncCache['backupRepositories']>
+      >,
+      'backupRepositories',
+      async () => {
+        const xoApp = this.#restApi.xoApp
+
+        const s3Brsize: DashboardBackupRepositoriesSizeInfo['s3']['size'] = { backups: 0 }
+        const otherBrSize: DashboardBackupRepositoriesSizeInfo['other']['size'] = {
+          available: 0,
+          backups: 0,
+          other: 0,
+          total: 0,
+          used: 0,
+        }
+
+        const backupRepositories = await xoApp.getAllRemotes()
+        const backupRepositoriesInfo = await xoApp.getAllRemotesInfo()
+        for (const backupRepository of backupRepositories) {
+          const { type } = parse(backupRepository.url)
+          const backupRepositoryInfo = backupRepositoriesInfo[backupRepository.id]
+
+          if (!backupRepository.enabled || backupRepositoryInfo === undefined) {
+            continue
+          }
+
+          const totalBackupSize = await xoApp.getTotalBackupSizeOnRemote(backupRepository.id)
+
+          const { available, size, used } = backupRepositoryInfo
+
+          const isS3 = type === 's3'
+          const target = isS3 ? s3Brsize : otherBrSize
+
+          target.backups += totalBackupSize.onDisk
+          if (!isS3) {
+            const _target = target as DashboardBackupRepositoriesSizeInfo['other']['size']
+            _target.available += available ?? 0
+            _target.other += used - totalBackupSize.onDisk
+            _target.total += size ?? 0
+            _target.used += used
+          }
+        }
+
+        return { s3: { size: s3Brsize }, other: { size: otherBrSize } }
+      },
+      this.#dashboardCacheOpts
+    )
+
+    if (brResult?.value !== undefined) {
+      return { ...brResult.value, isExpired: brResult.isExpired }
+    }
+  }
+
+  #getNumberOfPools() {
+    const pools = this.#restApi.getObjectsByType<XoPool>('pool')
+    return Object.keys(pools).length
+  }
+
+  #getNumberOfHosts() {
+    const hosts = this.#restApi.getObjectsByType<XoHost>('host')
+    return Object.keys(hosts).length
+  }
+
+  #getResourcesOverview(): XoaDashboard['resourcesOverview'] {
+    const pools = Object.values(this.#restApi.getObjectsByType<XoPool>('pool'))
+    const hosts = Object.values(this.#restApi.getObjectsByType<XoHost>('host'))
+    const writableSrs = Object.values(
+      this.#restApi.getObjectsByType<XoSr>('SR', {
+        filter: isSrWritable,
+      })
+    )
+
+    const maxLenght = Math.max(hosts.length, writableSrs.length)
+
+    const resourcesOverview = { nCpus: 0, memorySize: 0, srSize: 0 }
+    for (let index = 0; index < maxLenght; index++) {
+      const pool = pools[index]
+      const host = hosts[index]
+      const sr = writableSrs[index]
+
+      if (pool !== undefined) {
+        resourcesOverview.nCpus += pool.cpus.cores ?? 0
+      }
+      if (host !== undefined) {
+        resourcesOverview.memorySize += host.memory.size
+      }
+      if (sr !== undefined) {
+        resourcesOverview.srSize += sr.size
+      }
+    }
+
+    return resourcesOverview
+  }
+
+  async #getPoolsStatus(): Promise<XoaDashboard['poolsStatus']> {
+    const servers = await this.#restApi.xoApp.getAllXenServers()
+    const pools = this.#restApi.getObjectsByType<XoPool>('pool')
+
+    let nConnectedServers = 0
+    let nUnreachableServers = 0
+    let nUnknownServers = 0
+    servers.forEach(server => {
+      // it may happen that some servers are marked as "connected", but no pool matches "server.pool"
+      // so they are counted as `nUnknownServers`
+      if (server.status === 'connected' && server.poolId !== undefined && pools[server.poolId] !== undefined) {
+        nConnectedServers++
+        return
+      }
+
+      if (
+        server.status === 'disconnected' &&
+        server.error !== undefined &&
+        server.error.connectedServerId === undefined
+      ) {
+        nUnreachableServers++
+        return
+      }
+
+      if (server.status === 'disconnected') {
+        return
+      }
+
+      nUnknownServers++
+    })
+
+    return {
+      connected: nConnectedServers,
+      unreachable: nUnreachableServers,
+      unknown: nUnknownServers,
+    }
+  }
+
+  async #getNumberOfEolHosts(): Promise<XoaDashboard['nHostsEol']> {
+    const getHVSupportedVersions = this.#restApi.xoApp.getHVSupportedVersions
+
+    if (getHVSupportedVersions === undefined) {
+      return
+    }
+
+    const hvSupportedVersions = await getHVSupportedVersions()
+
+    const hosts = this.#restApi.getObjectsByType<XoHost>('host')
+    let nHostsEol = 0
+
+    for (const hostId in hosts) {
+      const host = hosts[hostId as XoHost['id']]
+      if (!semver.satisfies(host.version, hvSupportedVersions[host.productBrand])) {
+        nHostsEol++
+      }
+    }
+
+    return nHostsEol
+  }
+
+  async #getMissingPatchesInfo(): Promise<XoaDashboard['missingPatches']> {
+    if (!(await this.#restApi.xoApp.hasFeatureAuthorization('LIST_MISSING_PATCHES'))) {
+      return {
+        hasAuthorization: false,
+      }
+    }
+
+    const hosts = Object.values(this.#restApi.getObjectsByType<XoHost>('host'))
+    const poolsWithMissingPatches = new Set()
+    let nHostsWithMissingPatches = 0
+    let nHostsFailed = 0
+
+    await asyncEach(hosts, async (host: XoHost) => {
+      const xapi = this.#restApi.xoApp.getXapi(host)
+
+      try {
+        const patches = await xapi.listMissingPatches(host.id)
+
+        if (patches.length > 0) {
+          nHostsWithMissingPatches++
+          poolsWithMissingPatches.add(host.$pool)
+        }
+      } catch (err) {
+        log.error('listMissingPatches failed', err)
+        nHostsFailed++
+      }
+    })
+
+    return {
+      hasAuthorization: true,
+      nHostsFailed,
+      nHostsWithMissingPatches,
+      nPoolsWithMissingPatches: poolsWithMissingPatches.size,
+    }
+  }
+
+  #isReplicaVmInVdb(vbds: XoVbd['id'][]): boolean {
+    for (const vbd of vbds) {
+      try {
+        const vdbObject = this.#restApi.getObject<XoVbd>(vbd)
+        const { VM } = vdbObject
+        const vmObject = this.#restApi.getObject<AnyXoVm>(VM)
+        return isReplicaVm(vmObject)
+      } catch (err) {
+        if (!noSuchObject.is(err)) {
+          throw err
+        }
+      }
+    }
+    return false
+  }
+
+  #calculateReplicatedSize(vdiId: AnyXoVdi['id'], cache: Set<AnyXoVdi['id']>): number {
+    if (cache.has(vdiId)) {
+      return 0
+    }
+
+    let vdiObject: AnyXoVdi
+    try {
+      vdiObject = this.#restApi.getObject<AnyXoVdi>(vdiId)
+      cache.add(vdiId)
+    } catch (err) {
+      if (!noSuchObject.is(err)) {
+        throw err
+      }
+      return 0
+    }
+
+    const { parent, usage, $VBDs } = vdiObject
+    const replicaUsage = this.#isReplicaVmInVdb($VBDs) && usage ? usage : 0
+    const parentUsage = parent ? this.#calculateReplicatedSize(parent, cache) : 0
+
+    return replicaUsage + parentUsage
+  }
+
+  #getStorageRepositoriesSizeInfo() {
+    const writableSrs = this.#restApi.getObjectsByType<XoSr>('SR', {
+      filter: isSrWritable,
+    })
+
+    let replicated = 0
+    let total = 0
+    let used = 0
+
+    for (const srId in writableSrs) {
+      const sr = writableSrs[srId as XoSr['id']]
+
+      const cache = new Set<AnyXoVdi['id']>()
+      const { VDIs } = sr
+
+      replicated += VDIs.reduce((total, vdi) => total + this.#calculateReplicatedSize(vdi, cache), 0)
+      total += sr.size
+      used += sr.physical_usage
+    }
+
+    return {
+      size: { available: total - used, other: used - replicated, replicated, total, used },
+    }
+  }
+
+  async #getbackupsInfo(): Promise<(DashboardBackupsInfo & { isExpired?: true }) | undefined> {
+    const vmIdsProtected = new Set<XoVm['id']>()
+    const vmIdsUnprotected = new Set<XoVm['id']>()
+    const nonReplicaVms = Object.values(this.#restApi.getObjectsByType<XoVm>('VM', { filter: vm => !isReplicaVm(vm) }))
+    const restApi = this.#restApi
+    const xoApp = restApi.xoApp
+    function _extractVmIdsFromBackupJob(job: XoBackupJob) {
+      let vmIds: XoVm['id'][]
+      try {
+        vmIds = extractIdsFromSimplePattern(job.vms)
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      } catch (_) {
+        const predicate = createPredicate(job.vms)
+        vmIds = nonReplicaVms.filter(predicate).map(vm => vm.id)
+      }
+      return vmIds
+    }
+    function _processVmsProtection(job: XoBackupJob, isProtected: boolean) {
+      if (job.type !== BACKUP_TYPE.backup) {
+        return
+      }
+
+      _extractVmIdsFromBackupJob(job).forEach(vmId => {
+        _updateVmProtection(vmId, isProtected)
+      })
+    }
+    function _updateVmProtection(vmId: XoVm['id'], isProtected: boolean) {
+      if (vmIdsProtected.has(vmId) || !xoApp.hasObject(vmId, 'VM')) {
+        return
+      }
+
+      const vm = restApi.getObject<XoVm>(vmId, 'VM')
+      if (vmContainsNoBakTag(vm)) {
+        return
+      }
+
+      if (isProtected) {
+        vmIdsProtected.add(vmId)
+        vmIdsUnprotected.delete(vmId)
+      } else {
+        vmIdsUnprotected.add(vmId)
+      }
+    }
+    async function _jobHasAtLeastOneScheduleEnabled(job: XoBackupJob) {
+      for (const maybeScheduleId in job.settings) {
+        if (maybeScheduleId === '') {
+          continue
+        }
+
+        try {
+          const schedule = await xoApp.getSchedule(maybeScheduleId as XoSchedule['id'])
+          if (schedule.enabled) {
+            return true
+          }
+        } catch (error) {
+          if (!noSuchObject.is(error, { id: maybeScheduleId, type: 'schedule' })) {
+            console.error(error)
+          }
+          continue
+        }
+      }
+      return false
+    }
+
+    const backupsResult = await getFromAsyncCache<DashboardAsyncCache['backups']>(
+      this.#dashboardAsyncCache as Map<'backups', AsyncCacheEntry<DashboardAsyncCache['backups']>>,
+      'backups',
+      async () => {
+        const [logs, jobs] = await Promise.all([
+          xoApp.getBackupNgLogsSorted({
+            filter: log => log.message === 'backup' || log.message === 'metadata',
+          }),
+          Promise.all([
+            xoApp.getAllJobs('backup'),
+            xoApp.getAllJobs('mirrorBackup'),
+            xoApp.getAllJobs('metadataBackup'),
+          ]).then(jobs => jobs.flat(1)) as Promise<XoBackupJob[]>,
+        ])
+        const logsByJob = groupBy(logs, 'jobId')
+
+        let disabledJobs = 0
+        let failedJobs = 0
+        let skippedJobs = 0
+        let successfulJobs = 0
+        const backupJobIssues: DashboardBackupsInfo['issues'] = []
+
+        for (const job of jobs) {
+          if (!(await _jobHasAtLeastOneScheduleEnabled(job))) {
+            _processVmsProtection(job, false)
+            disabledJobs++
+            continue
+          }
+
+          // Get only the last 3 runs
+          const jobLogs = logsByJob[job.id]?.slice(-3).reverse()
+          if (jobLogs === undefined || jobLogs.length === 0) {
+            _processVmsProtection(job, false)
+            continue
+          }
+
+          if (job.type === BACKUP_TYPE.backup) {
+            const lastJobLog = jobLogs[0]
+            const { tasks, status } = lastJobLog
+
+            if (tasks === undefined) {
+              _processVmsProtection(job, status === 'success')
+            } else {
+              // @TODO: remove as when logs are correctly typed
+              ;(tasks as unknown as { data: { id: XoVm['id'] }; status: string }[]).forEach(task => {
+                _updateVmProtection(task.data.id, task.status === 'success')
+              })
+            }
+          }
+
+          const failedLog = jobLogs.find(log => log.status !== 'success')
+          if (failedLog !== undefined) {
+            const { status } = failedLog
+            if (status === 'failure' || status === 'interrupted') {
+              failedJobs++
+            } else if (status === 'skipped') {
+              skippedJobs++
+            }
+            backupJobIssues.push({
+              // @TODO: remove as when logs are correctly typed
+              logs: jobLogs.map(log => log.status) as DashboardBackupsInfo['issues'][number]['logs'],
+              name: job.name,
+              type: job.type,
+              uuid: job.id,
+            })
+          } else {
+            successfulJobs++
+          }
+        }
+
+        const nVmsProtected = vmIdsProtected.size
+        const nVmsUnprotected = vmIdsUnprotected.size
+        const nVmsNotInJob = nonReplicaVms.length - (nVmsProtected + nVmsUnprotected)
+
+        return {
+          jobs: {
+            disabled: disabledJobs,
+            failed: failedJobs,
+            skipped: skippedJobs,
+            successful: successfulJobs,
+            total: jobs.length,
+          },
+          issues: backupJobIssues,
+          vmsProtection: {
+            protected: nVmsProtected,
+            unprotected: nVmsUnprotected,
+            notInJob: nVmsNotInJob,
+          },
+        }
+      },
+      this.#dashboardCacheOpts
+    )
+
+    if (backupsResult?.value !== undefined) {
+      return { ...backupsResult.value, isExpired: backupsResult.isExpired }
+    }
+  }
+
+  async getDashboard() {
+    const nPools = this.#getNumberOfPools()
+    const nHosts = this.#getNumberOfHosts()
+    const resourcesOverview = this.#getResourcesOverview()
+    const storageRepositories = this.#getStorageRepositoriesSizeInfo()
+
+    const poolsStatus = await this.#getPoolsStatus()
+    const missingPatches = await this.#getMissingPatchesInfo()
+    const backupRepositories = await this.#getBackupRepositoriesSizeInfo().catch(err => {
+      log.error('#getBackupRepositoriesSizeInfo failed', err)
+      // explicitly return undefined because typescript understand it as void instead of undefined
+      return undefined
+    })
+    const nHostsEol = await this.#getNumberOfEolHosts().catch(err => {
+      log.err('#getNumberOfEolHosts failed', err)
+      return undefined
+    })
+    const backups = await this.#getbackupsInfo().catch(err => {
+      log.error('#getbackupsInfo failed', err)
+      return undefined
+    })
+
+    return {
+      nPools,
+      nHosts,
+      backupRepositories,
+      resourcesOverview,
+      poolsStatus,
+      nHostsEol,
+      missingPatches,
+      storageRepositories,
+      backups,
+    }
+  }
+}

@vates/types/src/common.mts
@@ -649,6 +649,10 @@ export const OPAQUE_REF = { EMPTY: 'OpaqueRef:NULL' } as const
 
 export type OPAQUE_REF_NULL = (typeof OPAQUE_REF)['EMPTY']
 
+export const BACKUP_TYPE = { backup: 'backup', metadata: 'metadataBackup', mirror: 'mirrorBackup' } as const
+
+export type BACKUP_TYPE = (typeof BACKUP_TYPE)[keyof typeof BACKUP_TYPE]
+
 // ----- XAPI Stats
 
 type XapiStatsResponse<T> = {

@vates/types/src/lib/xen-orchestra-xapi.mts
@@ -1,0 +1,61 @@
+import { WrappedXenApiRecord, XenApiNetworkWrapped, XenApiRecord } from '../xen-api.mjs'
+import type { XoHost, XoNetwork, XoPif } from '../xo.mjs'
+
+type XcpPatches = {
+  changelog?: {
+    author: string
+    date: number
+    description: string
+  }
+  description: string
+  license: string
+  name: string
+  release: string
+  size: number
+  url: string
+  version: string
+}
+type XsPatches = {
+  conflicts?: string[]
+  date: string
+  description: string
+  documentationUrl?: string
+  guidances: string
+  name: string
+  id?: string
+  paid?: boolean
+  requirements?: string[]
+  upgrade?: boolean
+  url?: string
+  uuid?: string
+}
+
+export interface Xapi {
+  call: <ReturnType>(...args: unknown[]) => Promise<ReturnType>
+  callAsync: <ReturnType>(...args: unknown[]) => Promise<ReturnType>
+
+  getField<T extends XenApiRecord, K extends keyof T>(
+    type: Extract<WrappedXenApiRecord, T>['$type'],
+    ref: T['$ref'],
+    field: K
+  ): Promise<T[K]>
+  createNetwork(
+    params:
+      | {
+          name: string
+          description?: string
+          mtu?: number
+        }
+      | {
+          name: string
+          description?: string
+          pifId: XoPif['id']
+          mtu?: number
+          /* between 0 and 4094 */
+          vlan: number
+        }
+  ): Promise<XenApiNetworkWrapped>
+  deleteNetwork(id: XoNetwork['id']): Promise<void>
+  listMissingPatches(host: XoHost['id']): Promise<XcpPatches[] | XsPatches[]>
+  pool_emergencyShutdown(): Promise<void>
+}

@vates/types/src/xen-api.mts
@@ -64,7 +64,7 @@ import type {
   VTPM_OPERATIONS,
   VUSB_OPERATIONS,
 } from './common.mjs'
-import type { XoNetwork, XoPif } from './xo.mjs'
+import type { Xapi } from './lib/xen-orchestra-xapi.mjs'
 
 // types automatically generated by the @ByScripts script.
 // https://github.com/vatesfr/xen-orchestra/tree/wip-xapi-types-generator/%40xen-orchestra/xapi-generator
@@ -84,30 +84,7 @@ type WrapperXenApi<T, Type extends string, Fn = { (): void }> = T & {
     name_label?: string
     unplugVusbs?: boolean
   }): Promise<XenApiVm['$ref']>
-  $xapi: {
-    call: <ReturnType>(...args: unknown[]) => Promise<ReturnType>
-    callAsync: <ReturnType>(...args: unknown[]) => Promise<ReturnType>
-    getField<T extends XenApiRecord, K extends keyof T>(type: Type, ref: T['$ref'], field: K): Promise<T[K]>
-
-    createNetwork(
-      params:
-        | {
-            name: string
-            description?: string
-            mtu?: number
-          }
-        | {
-            name: string
-            description?: string
-            pifId: XoPif['id']
-            mtu?: number
-            /* between 0 and 4094 */
-            vlan: number
-          }
-    ): Promise<XenApiNetworkWrapped>
-    deleteNetwork(id: XoNetwork['id']): Promise<void>
-    pool_emergencyShutdown(): Promise<void>
-  }
+  $xapi: Xapi
 }
 
 export interface XenApiSession {

@vates/types/src/xo.mts
@@ -1,6 +1,7 @@
 // Types based on xapi-object-to-xo
 
 import type {
+  BACKUP_TYPE,
   Branded,
   DOMAIN_TYPE,
   HOST_ALLOWED_OPERATIONS,
@@ -129,6 +130,17 @@ export type XoAlarm = Omit<XoMessage, '$object' | 'body'> & {
     uuid: XapiXoRecord['uuid']
     href?: string
   }
+}
+
+export type XoBackupRepository = {
+  benchmarks?: { readRate: number; timestamp: number; writeRate: number }[]
+  enabled: boolean
+  error?: Record<string, unknown>
+  id: Branded<'backup-repository'>
+  name: string
+  options?: string
+  proxy?: XoProxy['id']
+  url: string
 }
 
 export type XoGroup = {
@@ -335,15 +347,65 @@ export type XoPool = BaseXapiXo & {
   zstdSupported: boolean
 }
 
-export type XoJob = {
+export type XoProxy = {
+  id: Branded<'proxy'>
+}
+
+type BaseXoJob = {
   id: Branded<'job'>
 }
+// @TODO: create type for complex matcher
+export type XoBackupJob = BaseXoJob & {
+  compression?: 'native' | 'zstd' | ''
+  proxy?: XoProxy['id']
+  mode: 'full' | 'delta'
+  name?: string
+  remotes?: {
+    id: XoBackupRepository['id'] | { __or: XoBackupRepository['id'][] }
+  }
+  vms?: {
+    id: XoVm['id'] | { __or: XoVm['id'][] } | Record<string, unknown>
+  }
+  srs: {
+    id: XoSr['id'] | { __or: XoSr['id'][] }
+  }
+  type: BACKUP_TYPE
+  settings: {
+    '': {
+      cbtDestroySnapshotData?: boolean
+      concurrency?: number
+      longTermRetention?: {
+        daily?: { retention: number; settings: Record<string, unknown> }
+        weekly?: { retention: number; settings: Record<string, unknown> }
+        monthly?: { retention: number; settings: Record<string, unknown> }
+        yearly?: { retention: number; settings: Record<string, unknown> }
+      }
+      maxExportRate?: number
+      nbdConcurrency?: number
+      nRetriesVmBackupFailures?: number
+      preferNbd?: boolean
+      timezone?: string
+      [key: string]: unknown
+    }
+    [key: XoSchedule['id']]: {
+      exportRetention?: number
+      healthCheckSr?: XoSr['id']
+      healthCheckVmsWithTags?: string[]
+      fullInterval?: number
+      copyRetention?: number
+      snapshotRetention?: number
+      cbtDestroySnapshotData?: boolean
+      [key: string]: unknown
+    }
+  }
+}
+export type XoJob = BaseXoJob & {}
 
 export type XoSchedule = {
   cron: string
-  enable: boolean
+  enabled: boolean
   id: Branded<'schedule'>
-  jobId: XoJob['id']
+  jobId: (XoJob | XoBackupJob)['id']
   name?: string
   timezone?: string
 }
@@ -370,7 +432,7 @@ export type XoSr = BaseXapiXo & {
 
   $container: XoPool['id'] | XoHost['id']
 
-  VDIs: XoVdi['id'][]
+  VDIs: AnyXoVdi['id'][]
 
   allocationStrategy: 'thin' | 'thick' | 'unknown'
   content_type: string
@@ -380,9 +442,9 @@ export type XoSr = BaseXapiXo & {
   name_description: string
   name_label: string
   other_config: Record<string, string>
-  physical_usage: number | null
+  physical_usage: number
   shared: boolean
-  size: number | null
+  size: number
   sm_config: Record<string, string>
   SR_type: string
   tags: string[]
@@ -410,8 +472,8 @@ export type XoVbd = BaseXapiXo & {
   position: string
   read_only: boolean
   type: 'VBD'
-  VDI: XoVdi['id']
-  VM: XoVm['id']
+  VDI: AnyXoVdi['id']
+  VM: AnyXoVm['id']
 }
 
 type BaseXoVdi = BaseXapiXo & {
@@ -540,6 +602,12 @@ export type XapiXoRecord =
   | XoVmTemplate
   | XoVtpm
 
-export type NonXapiXoRecord = XoGroup | XoJob | XoSchedule | XoServer | XoUser
+export type NonXapiXoRecord = XoGroup | XoProxy | XoJob | XoBackupRepository | XoSchedule | XoServer | XoUser
 
 export type XoRecord = XapiXoRecord | NonXapiXoRecord
+
+export type AnyXoVm = XoVm | XoVmSnapshot | XoVmTemplate | XoVmController
+
+export type AnyXoVdi = XoVdi | XoVdiSnapshot | XoVdiUnmanaged
+
+export type AnyXoJob = XoJob | XoBackupJob

@xen-orchestra/rest-api/src/helpers/cache.helper.mts
@@ -1,0 +1,78 @@
+import { MaybePromise } from './helper.type.mjs'
+
+export type AsyncCacheEntry<T> = {
+  current: T | Promise<T>
+  expires?: number
+  previous?: T | Promise<T>
+}
+
+/**
+ *
+ * If the value is cached and not expired, it will return
+ * the cached value immediately. If expired or not present, it will invoke the provided
+ * function to fetch the value, cache it, and return it.
+ *
+ * The function also handles timeout for fetching the value, ensuring that if fetching
+ * takes too long, it resolves to `undefined` or returns the expired value based on the
+ * cache's state.
+ *
+ */
+export async function getFromAsyncCache<T>(
+  cache: Map<string, AsyncCacheEntry<MaybePromise<T>>>,
+  key: string,
+  fn: () => Promise<T>,
+  { expiresIn = 60000, timeout = 5000, forceRefresh = false } = {}
+): Promise<{ value: T | undefined; isExpired?: true } | undefined> {
+  if (forceRefresh) {
+    cache.delete(key)
+  }
+
+  const { current, expires } = cache.get(key) ?? {}
+  if (current === undefined || (expires ?? 0) < Date.now()) {
+    const _promise = fn()
+
+    const promise = _promise.then(result => {
+      cache.set(key, {
+        current: result,
+        expires: Date.now() + expiresIn,
+        previous: undefined,
+      })
+
+      return result
+    })
+
+    cache.set(key, {
+      current: promise,
+      previous: current,
+      expires: undefined,
+    })
+  }
+
+  let timeoutId
+  const timeoutPromise = new Promise<never>(
+    (resolve, reject) =>
+      (timeoutId = setTimeout(() => reject(new Error('Promise timed out', { cause: 'ERR_TIMEOUT' })), timeout))
+  )
+
+  const result = {} as {
+    value: T | undefined
+    isExpired?: true
+  }
+
+  try {
+    result.value = await Promise.race([timeoutPromise, cache.get(key)!.current])
+  } catch (error) {
+    if (error instanceof Error && error.cause !== 'ERR_TIMEOUT') {
+      throw error
+    }
+
+    result.value = await cache.get(key)!.previous
+    if (result.value !== undefined) {
+      result.isExpired = true
+    }
+  } finally {
+    clearTimeout(timeoutId)
+  }
+
+  return result
+}

commit cd338fd54b75a8a5b0b329243ea7d214ac7cc3f8

fix(rest-api/XoaService): log.err → log.error
Introduced by #8580

@xen-orchestra/rest-api/src/xoa/xoa.service.mts
@@ -483,7 +483,7 @@ export class XoaService {
       return undefined
     })
     const nHostsEol = await this.#getNumberOfEolHosts().catch(err => {
-      log.err('#getNumberOfEolHosts failed', err)
+      log.error('#getNumberOfEolHosts failed', err)
       return undefined
     })
     const backups = await this.#getbackupsInfo().catch(err => {