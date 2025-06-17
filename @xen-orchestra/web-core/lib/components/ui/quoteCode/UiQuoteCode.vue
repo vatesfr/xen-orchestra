2796f03667c4ba39cc155b4002ee168214d9ff0a
unel <enee.prog@gmail.com>
2025-06-17 14:22:47 +0200

    review

--- @xen-orchestra/web-core/lib/components/ui/quoteCode/UiQuoteCode.vue
+++ @xen-orchestra/web-core/lib/components/ui/quoteCode/UiQuoteCode.vue
@@ -1,39 +1,87 @@
 <template>
   <div class="ui-quote-code">
-    <slot name="title" />
-    <div class="code-container" :class="accent === 'brand' ? 'color-brand' : 'color-error'">
+    <div :class="classNames" class="typo-body-regular label-container">
+      <div class="label">
+        <slot />
+      </div>
+      <div v-if="slots.actions" class="actions">
+        <slot name="actions" />
+      </div>
+    </div>
+    <div :class="classNames" class="code-container">
       {{ code }}
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-type quoteCodeAccent = 'brand' | 'error'
+import { toVariants } from '@core/utils/to-variants.util.ts'
+import { computed } from 'vue'
 
-defineProps<{
+type quoteCodeAccent = 'brand' | 'error'
+type quoteCodeSize = 'small' | 'medium'
+
+const { size, accent } = defineProps<{
   code: string
+  size: quoteCodeSize
   accent: quoteCodeAccent
 }>()
+
+const slots = defineSlots<{
+  default(): any
+  actions?(): any
+}>()
+
+const fontClasses = {
+  small: 'typo-body-bold-small',
+  medium: 'typo-body-bold',
+}
+
+const classNames = computed(() => {
+  return [
+    fontClasses[size],
+    toVariants({
+      size,
+      accent,
+    }),
+  ]
+})
 </script>
 
 <style lang="postcss" scoped>
-.label {
-  color: var(--color-neutral-txt-primary);
+.ui-quote-code {
   display: flex;
-  justify-content: space-between;
-}
+  flex-direction: column;
+  gap: 0.8rem;
 
-.code-container {
-  background-color: var(--color-neutral-background-disabled);
-  padding: 0.8rem 1.2rem;
-  border-radius: 0.4rem;
+  .label-container {
+    display: flex;
+    justify-content: space-between;
 
-  &.color-brand {
-    border-inline-start: 0.2rem solid var(--color-brand-item-base);
+    .label {
+      color: var(--color-neutral-txt-secondary);
+    }
+
+    .actions {
+      display: flex;
+      gap: 0.8rem;
+      align-items: center;
+    }
   }
 
-  &.color-error {
-    border-inline-start: 0.2rem solid var(--color-danger-item-base);
+  .code-container {
+    background-color: var(--color-neutral-background-disabled);
+    padding-block: 0.8rem;
+    padding-inline: 1.2rem;
+    border-radius: 0.4rem;
+
+    &.accent--brand {
+      border-inline-start: 0.2rem solid var(--color-brand-item-base);
+    }
+
+    &.accent--error {
+      border-inline-start: 0.2rem solid var(--color-danger-item-base);
+    }
   }
 }
 </style>
