23b0cec6469e77b8b27af61c00c10e96d4e6555f
Pierre Brunet <pierre.brunet@vates.tech>
2025-06-17 12:51:08 +0200

    fix: add multiple callbacks and tests

--- @xen-orchestra/fs/src/utils.js
+++ @xen-orchestra/fs/src/utils.js
@@ -1,19 +1,45 @@
+import { createLogger } from '@xen-orchestra/log'
 import { TimeoutError } from 'promise-toolbox'
+const { info, warn } = createLogger('xo:fs:abstract')
 
-export function withTimeout(fn, timeout) {
+export function withTimeout(fn, timeout, { onSuccess, onTimeout, onSuccessAfterTimeout, onFailureAfterTimeout } = {}) {
+  if (typeof fn.then !== 'undefined') {
+    throw new Error('Function needs to be asynchronous.')
+  }
+
   return function (...args) {
     let timeoutHandle
-    const timeoutPromise = new Promise((_resolve, reject) => {
-      timeoutHandle = setTimeout(() => reject(new TimeoutError()), timeout)
+    let didTimeout = false
+    const timeoutError = new TimeoutError('Async call timeout limit reached')
+
+    return new Promise((resolve, reject) => {
+      timeoutHandle = setTimeout(() => {
+        didTimeout = true
+        onTimeout?.()
+        if (onSuccessAfterTimeout === undefined && onFailureAfterTimeout === undefined) {
+          reject(timeoutError)
+        }
+      }, timeout)
+      fn.apply(this, args)
+        .then(result => {
+          clearTimeout(timeoutHandle)
+          if (didTimeout) {
+            info('Success after timeout:\n', result)
+            onSuccessAfterTimeout?.(result)
+            reject(timeoutError)
+          } else {
+            onSuccess?.(result)
+            resolve(result)
+          }
+        })
+        .catch(error => {
+          clearTimeout(timeoutHandle)
+          if (didTimeout) {
+            warn('Failure after timeout:\n', error)
+            onFailureAfterTimeout?.(error)
+          }
+          reject(error)
+        })
     })
-    return Promise.race([Promise.resolve().then(() => fn.apply(this, args)), timeoutPromise])
-      .then(result => {
-        clearTimeout(timeoutHandle)
-        return result
-      })
-      .catch(err => {
-        clearTimeout(timeoutHandle)
-        throw err
-      })
   }
 }
--- @xen-orchestra/fs/src/utils.test.js
+++ @xen-orchestra/fs/src/utils.test.js
@@ -1,19 +1,88 @@
-import { describe, it } from 'node:test'
+import { describe, it, beforeEach, afterEach, mock } from 'node:test'
 import { strict as assert } from 'assert'
 import sinon from 'sinon'
-import { withTimeout } from './utils'
 import { TimeoutError } from 'promise-toolbox'
+import { withTimeout } from './utils'
 
 const TIMEOUT = 6e6
 
-const clock = sinon.useFakeTimers()
-
 describe('withTimeout()', () => {
+  let clock
+
+  beforeEach(() => {
+    clock = sinon.useFakeTimers()
+  })
+
+  afterEach(() => {
+    clock.restore()
+    mock.reset()
+  })
+
   it(`throws in case of timeout`, async () => {
     let promiseFct = () => new Promise(() => {})
     promiseFct = withTimeout(promiseFct, TIMEOUT)
     const promise = promiseFct()
     clock.tick(TIMEOUT)
     await assert.rejects(promise, TimeoutError)
+  })
+
+  it(`can success after timeout()`, async () => {
+    let promiseFct = () =>
+      new Promise(resolve => {
+        setTimeout(() => resolve('done'), 5000)
+      })
+    const mockMethod = mock.fn(() => {})
+    promiseFct = withTimeout(promiseFct, 2000, { onSuccessAfterTimeout: mockMethod })
+
+    const promise = promiseFct()
+    clock.tick(6000)
+    await assert.rejects(promise, TimeoutError)
+    assert.strictEqual(mockMethod.mock.callCount(), 1)
+    assert.strictEqual(mockMethod.mock.calls[0].arguments[0], 'done')
+  })
+
+  it(`can fail after timeout()`, async () => {
+    const testError = new Error('test')
+    let promiseFct = () =>
+      new Promise((_resolve, reject) => {
+        setTimeout(() => {
+          reject(testError)
+        }, 5000)
+      })
+
+    const mockMethod = mock.fn(() => {})
+    promiseFct = withTimeout(promiseFct, 2000, { onFailureAfterTimeout: mockMethod })
+    const promise = promiseFct()
+    clock.tick(TIMEOUT)
+    await assert.rejects(promise, Error)
+
+    assert.strictEqual(mockMethod.mock.callCount(), 1)
+    assert.strictEqual(mockMethod.mock.calls[0].arguments[0], testError)
+  })
+
+  it(`can success before timeout()`, async () => {
+    let promiseFct = () =>
+      new Promise(resolve => {
+        setTimeout(() => resolve('done'), 1000)
+      })
+    promiseFct = withTimeout(promiseFct, 2000)
+
+    const promise = promiseFct()
+    clock.tick(1000)
+    const result = await promise
+    assert.strictEqual(result, 'done')
+  })
+
+  it(`can fail before timeout()`, async () => {
+    const testError = new Error('test')
+    let promiseFct = () =>
+      new Promise((_resolve, reject) => {
+        setTimeout(() => reject(testError), 1000)
+      })
+    promiseFct = withTimeout(promiseFct, 2000)
+
+    const promise = promiseFct()
+    clock.tick(1000)
+    assert.rejects(promise, testError)
   })
 })
