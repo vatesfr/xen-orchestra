a2440e831a02623233312c8f7dae8518aa004db0
unel <enee.prog@gmail.com>
2025-06-18 09:19:06 +0200

    review

--- @xen-orchestra/lite/src/stories/web-core/ui/quote-code/ui-quote-code.story.vue
+++ @xen-orchestra/lite/src/stories/web-core/ui/quote-code/ui-quote-code.story.vue
@@ -2,18 +2,17 @@
   <ComponentStory
     v-slot="{ properties, settings }"
     :params="[
-      prop('code').preset(`[Code error here]`).type('string').widget(),
-      prop('accent').preset('brand').enum('brand', 'error').widget(),
-      prop('size').enum('small', 'medium').required().preset('small').widget(),
-      slot().help('Meant to display the label'),
+      prop('label').preset('Label').type('string').widget(),
+      prop('accent').type('QuoteCodeAccent').enum('brand', 'danger').required().preset('brand').widget(),
+      prop('size').type('QuoteCodeSize').enum('small', 'medium').required().preset('small').widget(),
+      slot().help('Meant to display the code'),
       slot('actions').help('Meant to receive UiButton or ButtonIcon (or other) components that will trigger actions'),
-      setting('default').preset('Label').widget(),
+      setting('default').preset('[Error code here]').widget(),
       setting('showDemoButtons').widget(boolean()),
     ]"
   >
     <UiQuoteCode v-bind="properties">
       {{ settings.default }}
-
       <template v-if="settings.showDemoButtons" #actions>
         <VtsCopyButton :value="properties.code" />
       </template>
