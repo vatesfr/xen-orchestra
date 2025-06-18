4b81b4f755aed9fdb6c77f805324f67cf452dcb8
unel <enee.prog@gmail.com>
2025-06-18 09:19:06 +0200

    review

--- @xen-orchestra/web-core/lib/components/ui/quoteCode/UiQuoteCode.vue
+++ @xen-orchestra/web-core/lib/components/ui/quoteCode/UiQuoteCode.vue
@@ -1,30 +1,31 @@
 <template>
-  <div class="ui-quote-code">
-    <div :class="classNames" class="typo-body-regular label-container">
-      <div class="label">
-        <slot />
+  <div class="ui-quote-code" :class="className">
+    <div class="label-container">
+      <div :class="fontClasses.labelClass" class="label">
+        {{ label }}
       </div>
       <div v-if="slots.actions" class="actions">
         <slot name="actions" />
       </div>
     </div>
-    <div :class="classNames" class="code-container">
-      {{ code }}
+    <div :class="fontClasses.codeClass" class="code-container">
+      <slot />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { useMapper } from '@core/packages/mapper'
 import { toVariants } from '@core/utils/to-variants.util.ts'
 import { computed } from 'vue'
 
-type quoteCodeAccent = 'brand' | 'error'
-type quoteCodeSize = 'small' | 'medium'
+type QuoteCodeAccent = 'brand' | 'danger'
+type QuoteCodeSize = 'small' | 'medium'
 
 const { size, accent } = defineProps<{
-  code: string
-  size: quoteCodeSize
-  accent: quoteCodeAccent
+  label: string
+  size: QuoteCodeSize
+  accent: QuoteCodeAccent
 }>()
 
 const slots = defineSlots<{
@@ -32,27 +33,31 @@ const slots = defineSlots<{
   actions?(): any
 }>()
 
-const fontClasses = {
-  small: 'typo-body-bold-small',
-  medium: 'typo-body-bold',
+const mapping = {
+  small: {
+    labelClass: 'typo-body-regular-small',
+    codeClass: 'typo-form-value-small',
+  },
+  medium: {
+    labelClass: 'typo-body-regular',
+    codeClass: 'typo-form-value',
+  },
 }
 
-const classNames = computed(() => {
-  return [
-    fontClasses[size],
-    toVariants({
-      size,
-      accent,
-    }),
-  ]
-})
+const fontClasses = useMapper(() => size, mapping, 'medium')
+
+const className = computed(() =>
+  toVariants({
+    accent,
+  })
+)
 </script>
 
 <style lang="postcss" scoped>
 .ui-quote-code {
   display: flex;
   flex-direction: column;
-  gap: 0.8rem;
+  gap: 0.4rem;
 
   .label-container {
     display: flex;
@@ -71,16 +76,20 @@ const classNames = computed(() => {
 
   .code-container {
     background-color: var(--color-neutral-background-disabled);
-    padding-block: 0.8rem;
-    padding-inline: 1.2rem;
+    padding: 0.8rem 1.2rem;
     border-radius: 0.4rem;
+    border-inline-start: 0.2rem solid;
+  }
 
-    &.accent--brand {
-      border-inline-start: 0.2rem solid var(--color-brand-item-base);
+  &.accent--brand {
+    .code-container {
+      border-inline-start-color: var(--color-brand-item-base);
     }
+  }
 
-    &.accent--error {
-      border-inline-start: 0.2rem solid var(--color-danger-item-base);
+  &.accent--danger {
+    .code-container {
+      border-inline-start-color: var(--color-danger-item-base);
     }
   }
 }
