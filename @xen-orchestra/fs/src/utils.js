295563f755e2c224627d59c6b30b6d2d9fa3aacf
Pierre Brunet <pierre.brunet@vates.tech>
2025-06-19 10:14:36 +0200

    fix: add doc and handle typeError

--- @xen-orchestra/fs/src/utils.js
+++ @xen-orchestra/fs/src/utils.js
@@ -3,10 +3,42 @@ import { TimeoutError } from 'promise-toolbox'
 
 const { info, warn } = createLogger('xo:fs:abstract')
 
+/**
+ * Wraps an asynchronous function with timeout functionality.
+ *
+ * @template T
+ * @param {(...args: any[]) => Promise<T>} fn - The asynchronous function to wrap
+ * @param {Object} [options] - Configuration options
+ * @param {(result: T) => void} [options.onSuccessAfterTimeout] - Callback invoked if original promise resolves after timeout
+ * @param {(error: any) => void} [options.onFailureAfterTimeout] - Callback invoked if original promise rejects after timeout
+ * @returns {(...args: any[]) => Promise<T>} A new function that will enforce the timeout
+ * @throws {TypeError} If fn is not a function or timeout is not a positive number
+ *
+ * @example
+ * // Basic usage
+ * const fnWithTimeout = withTimeout(asyncFunc, 1000)
+ * try {
+ *   const result = await fnWithTimeout()
+ * } catch (error) {
+ *   if (error instanceof TimeoutError) {
+ *     // Handle timeout
+ *   }
+ * }
+ *
+ * @example
+ * // With callbacks
+ * const fnWithTimeout = withTimeout(asyncFunc, 1000, {
+ *   onSuccessAfterTimeout: (result) => console.log('Late success', result),
+ *   onFailureAfterTimeout: (error) => console.log('Late failure', error)
+ * })
+ */
 export function withTimeout(fn, timeout, { onSuccessAfterTimeout, onFailureAfterTimeout } = {}) {
   return function (...args) {
     let timeoutHandle
     let didTimeout = false
+    if (typeof fn !== 'function') {
+      throw new TypeError('First argument must be a function')
+    }
     const timeoutError = new TimeoutError('Async call timeout limit reached')
 
     return new Promise((resolve, reject) => {
@@ -14,6 +46,8 @@ export function withTimeout(fn, timeout, { onSuccessAfterTimeout, onFailureAfter
         didTimeout = true
         reject(timeoutError)
       }, timeout)
+
+      // if fn is synchronous and throw an error, it will throws it here
       const promise = fn.apply(this, args)
       if (promise?.then === undefined) {
         throw new Error('Function needs to be asynchronous.')
