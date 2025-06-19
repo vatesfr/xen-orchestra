vatesfr/xen-orchestra

commit 36e594a3ca59cd51c3690610749bd53a0c68c53d

feat(xo-6/vm): add dashboard view (#8585)

@xen-orchestra/web-core/lib/utils/size.util.ts
@@ -1,6 +1,6 @@
-import { parse, Scale, raw } from 'human-format'
+import { parse, raw, Scale } from 'human-format'
 
-const scale = Scale.create(['', 'KiB', 'MiB', 'GiB', 'TiB'], 1024)
+const scale = Scale.create(['B', 'KiB', 'MiB', 'GiB', 'TiB'], 1024)
 
 export const formatSizeRaw = (bytes: number | undefined, decimals: number) => {
   if (bytes === undefined) {

@vates/types/src/xo.mts
@@ -75,7 +75,7 @@ type BaseXoVm = BaseXapiXo & {
   isNestedVirtEnabled: boolean
   hasVendorDevice: boolean
   high_availability: string
-  installTime?: number
+  installTime?: number | null
   isFirmwareSupported: boolean
   memory: {
     dynamic: number[]
@@ -101,7 +101,7 @@ type BaseXoVm = BaseXapiXo & {
   secureBoot: boolean
   snapshots: XoVmSnapshot['id'][]
   startDelay: number
-  startTime?: number
+  startTime?: number | null
   suspendSr?: XoSr['id']
   tags: string[]
   vga?: string

@xen-orchestra/web-core/lib/composables/local-time-ago.composable.ts
@@ -1,0 +1,53 @@
+import { useTimestamp } from '@vueuse/core'
+import { computed, type MaybeRefOrGetter, toValue } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+export enum SECONDS {
+  MINUTE = 60,
+  HOUR = SECONDS.MINUTE * 60,
+  DAY = SECONDS.HOUR * 24,
+  WEEK = SECONDS.DAY * 7,
+  MONTH = SECONDS.DAY * 30,
+  YEAR = SECONDS.DAY * 365,
+}
+
+export type TimeThreshold = [number, Intl.RelativeTimeFormatUnit]
+
+const timeThresholds: TimeThreshold[] = [
+  [SECONDS.MINUTE, 'second'],
+  [SECONDS.HOUR, 'minute'],
+  [SECONDS.DAY, 'hour'],
+  [SECONDS.WEEK, 'day'],
+  [SECONDS.MONTH, 'week'],
+  [SECONDS.YEAR, 'month'],
+  [Infinity, 'year'],
+]
+
+export function useTimeAgo(referenceDate: MaybeRefOrGetter<Date | number | string>) {
+  const { locale } = useI18n()
+
+  const formatter = computed(() => new Intl.RelativeTimeFormat(locale.value, { numeric: 'auto' }))
+
+  const now = useTimestamp({ interval: 1000 })
+
+  const referenceTime = computed(() => new Date(toValue(referenceDate)).getTime())
+
+  const distance = computed(() => {
+    const diff = Math.trunc((referenceTime.value - now.value) / 1000)
+
+    if (Math.abs(diff) < 60) {
+      return Math.trunc(diff / 10) * 10 || 0 // Avoid recomputing when the value changes from 0 to -0
+    }
+
+    return timeThresholds.reduce(
+      (acc, [threshold]) => (Math.abs(acc) < threshold ? acc : Math.trunc(acc / threshold) * threshold),
+      diff
+    )
+  })
+
+  return computed(() => {
+    const index = timeThresholds.findIndex(([threshold]) => Math.abs(distance.value) < threshold)
+    const divisor = index > 0 ? timeThresholds[index - 1][0] : 1
+    return formatter.value.format(Math.trunc(distance.value / divisor), timeThresholds[index][1])
+  })
+}

@xen-orchestra/web/src/components/tree/VmTreeItem.vue
@@ -1,6 +1,6 @@
 <template>
   <VtsTreeItem expanded>
-    <UiTreeItemLabel :route="`/vm/${leaf.data.id}/`" no-indent>
+    <UiTreeItemLabel :route="`/vm/${leaf.data.id}`" no-indent>
       {{ leaf.data.name_label }}
       <template #icon>
         <UiObjectIcon

@xen-orchestra/web/src/components/vm/VmHeader.vue
@@ -6,7 +6,11 @@
     </template>
   </UiHeadBar>
   <TabList>
-    <TabItem disabled>{{ t('dashboard') }}</TabItem>
+    <RouterLink v-slot="{ isActive, href }" :to="`/vm/${vm.id}/dashboard`" custom>
+      <TabItem :active="isActive" :href tag="a">
+        {{ t('dashboard') }}
+      </TabItem>
+    </RouterLink>
     <RouterLink v-slot="{ isActive, href }" :to="`/vm/${vm.id}/console`" custom>
       <TabItem :active="isActive" :href tag="a">
         {{ t('console') }}

@xen-orchestra/web/src/components/vm/dashboard/VmDashboardCpuUsageChart.vue
@@ -1,0 +1,77 @@
+<template>
+  <UiCard>
+    <UiCardTitle>
+      {{ t('cpu-usage') }}
+      <template #description>{{ t('last-week') }}</template>
+    </UiCardTitle>
+    <VtsLoadingHero v-if="loading" type="card" />
+    <VtsErrorNoDataHero v-else-if="error" type="card" />
+    <VtsNoDataHero v-else-if="cpuUsage.length === 0" type="card" />
+    <VtsLinearChart v-else :data="cpuUsage" :max-value :value-formatter />
+  </UiCard>
+</template>
+
+<script lang="ts" setup>
+import type { XoVmStats } from '@/types/xo/vm-stats.type.ts'
+import type { LinearChartData, ValueFormatter } from '@core/types/chart.ts'
+import VtsErrorNoDataHero from '@core/components/state-hero/VtsErrorNoDataHero.vue'
+import VtsLoadingHero from '@core/components/state-hero/VtsLoadingHero.vue'
+import VtsNoDataHero from '@core/components/state-hero/VtsNoDataHero.vue'
+import UiCard from '@core/components/ui/card/UiCard.vue'
+import UiCardTitle from '@core/components/ui/card-title/UiCardTitle.vue'
+import { computed, defineAsyncComponent } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+const { data } = defineProps<{
+  data: XoVmStats | null
+  loading: boolean
+  error?: string
+}>()
+
+const VtsLinearChart = defineAsyncComponent(() => import('@core/components/linear-chart/VtsLinearChart.vue'))
+
+const { t, n } = useI18n()
+
+const cpuUsage = computed<LinearChartData>(() => {
+  if (!data?.stats.cpus) {
+    return []
+  }
+
+  const cpus = Object.values(data.stats.cpus)
+
+  const result = new Map<number, { timestamp: number; value: number }>()
+
+  const timestampStart = data.endTimestamp - data.interval * (cpus[0].length - 1)
+
+  for (let hourIndex = 0; hourIndex < cpus[0].length; hourIndex++) {
+    const timestamp = (timestampStart + hourIndex * data.interval) * 1000
+
+    const cpuUsageSum = cpus.reduce((total, cpu) => total + cpu[hourIndex], 0)
+
+    result.set(timestamp, {
+      timestamp,
+      value: Math.round(cpuUsageSum / cpus.length),
+    })
+  }
+  return [
+    {
+      label: t('stacked-cpu-usage'),
+      data: Array.from(result.values()),
+    },
+  ]
+})
+
+const maxValue = computed(() => {
+  const values = cpuUsage.value[0]?.data.map(item => item.value) ?? []
+
+  if (values.length === 0) {
+    return 100
+  }
+
+  const maxCpuUsage = Math.max(...values)
+
+  return Math.ceil(maxCpuUsage / 100) * 100
+})
+
+const valueFormatter: ValueFormatter = value => n(value / 100, 'percent')
+</script>

@xen-orchestra/web/src/components/vm/dashboard/VmDashboardNetworkUsageChart.vue
@@ -1,0 +1,88 @@
+<template>
+  <UiCard>
+    <UiCardTitle>
+      {{ t('network-throughput') }}
+      <template #description>{{ t('last-week') }}</template>
+    </UiCardTitle>
+    <VtsLoadingHero v-if="loading" type="card" />
+    <VtsErrorNoDataHero v-else-if="error" type="card" />
+    <VtsNoDataHero v-else-if="networkUsage.length === 0" type="card" />
+    <VtsLinearChart v-else :data="networkUsage" :max-value :value-formatter="byteFormatter" />
+  </UiCard>
+</template>
+
+<script lang="ts" setup>
+import type { XoVmStats } from '@/types/xo/vm-stats.type.ts'
+import type { LinearChartData } from '@core/types/chart.ts'
+import VtsErrorNoDataHero from '@core/components/state-hero/VtsErrorNoDataHero.vue'
+import VtsLoadingHero from '@core/components/state-hero/VtsLoadingHero.vue'
+import VtsNoDataHero from '@core/components/state-hero/VtsNoDataHero.vue'
+import UiCard from '@core/components/ui/card/UiCard.vue'
+import UiCardTitle from '@core/components/ui/card-title/UiCardTitle.vue'
+import { formatSizeRaw } from '@core/utils/size.util.ts'
+import { computed, defineAsyncComponent } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+const { data } = defineProps<{
+  data: XoVmStats | null
+  loading: boolean
+  error?: string
+}>()
+
+const VtsLinearChart = defineAsyncComponent(() => import('@core/components/linear-chart/VtsLinearChart.vue'))
+
+const { t } = useI18n()
+
+const networkUsage = computed<LinearChartData>(() => {
+  if (!data?.stats.vifs) {
+    return []
+  }
+
+  const rxArrays = Object.values(data.stats.vifs.rx)
+
+  const timestamps = Array.from(
+    { length: rxArrays[0].length },
+    (_, i) => data.endTimestamp * 1000 - (rxArrays[0].length - 1 - i) * data.interval * 1000
+  )
+
+  const rxSeries = [
+    {
+      label: t('network-upload'),
+      data: timestamps.map((timestamp, index) => ({
+        timestamp,
+        value: Object.values(data.stats.vifs.rx).reduce((sum, values) => sum + values[index], 0),
+      })),
+    },
+  ]
+
+  const txSeries = [
+    {
+      label: t('network-download'),
+      data: timestamps.map((timestamp, index) => ({
+        timestamp,
+        value: Object.values(data.stats.vifs.tx).reduce((sum, values) => sum + values[index], 0),
+      })),
+    },
+  ]
+
+  return [...rxSeries, ...txSeries]
+})
+
+const maxValue = computed(() => {
+  const values = networkUsage.value.flatMap(series => series.data.map(item => item.value))
+
+  if (values.length === 0) {
+    return 100
+  }
+
+  const maxUsage = Math.max(...values)
+
+  return Math.ceil(maxUsage / 50) * 50
+})
+
+const byteFormatter = (value: number) => {
+  const result = formatSizeRaw(value, 1)
+
+  return `${result?.value}${result?.prefix}`
+}
+</script>

@xen-orchestra/web/src/components/vm/dashboard/VmDashboardQuickInfo.vue
@@ -1,0 +1,135 @@
+<template>
+  <VtsQuickInfoCard class="vm-dashboard-quick-info" :loading="!isReady">
+    <VtsQuickInfoColumn>
+      <VtsQuickInfoRow :label="t('state')">
+        <template #value>
+          <span class="power-state">
+            <VtsIcon :accent="powerState.accent" :icon="powerState.icon" />
+            {{ powerState.text }}
+          </span>
+        </template>
+      </VtsQuickInfoRow>
+      <VtsQuickInfoRow :label="t('ip-address')" :value="vm.mainIpAddress" />
+      <VtsQuickInfoRow :label="t('created-on')" :value="installDateFormatted" />
+      <VtsQuickInfoRow :label="t('started')" :value="relativeStartTime" />
+      <VtsQuickInfoRow :label="t('host')">
+        <template #value>
+          <span v-if="host" class="host-name">
+            <UiLink :icon="faServer" :to="`/host/${host.id}`" size="medium">
+              {{ host.name_label }}
+            </UiLink>
+          </span>
+          <span v-else>
+            {{ t('none') }}
+          </span>
+        </template>
+      </VtsQuickInfoRow>
+    </VtsQuickInfoColumn>
+    <VtsQuickInfoColumn>
+      <VtsQuickInfoRow :label="t('uuid')" :value="vm.id" />
+      <VtsQuickInfoRow :label="t('description')" :value="vm.name_description" />
+      <VtsQuickInfoRow :label="t('os-name')" :value="vm.os_version?.name" />
+      <VtsQuickInfoRow :label="t('virtualization-type')" :value="virtualizationType" />
+    </VtsQuickInfoColumn>
+    <VtsQuickInfoColumn>
+      <VtsQuickInfoRow :label="t('vcpus')" :value="String(vm.CPUs.number)" />
+      <VtsQuickInfoRow :label="t('ram')" :value="`${ram?.value} ${ram?.prefix}`" />
+      <VtsQuickInfoRow :label="t('tags')">
+        <template #value>
+          <UiTagsList v-if="vm.tags.length > 0">
+            <UiTag v-for="tag in vm.tags" :key="tag" accent="info" variant="secondary">
+              {{ tag }}
+            </UiTag>
+          </UiTagsList>
+        </template>
+      </VtsQuickInfoRow>
+    </VtsQuickInfoColumn>
+  </VtsQuickInfoCard>
+</template>
+
+<script lang="ts" setup>
+import { useVmStore } from '@/stores/xo-rest-api/vm.store.ts'
+import { VM_POWER_STATE, type XoVm } from '@/types/xo/vm.type.ts'
+import VtsIcon from '@core/components/icon/VtsIcon.vue'
+import VtsQuickInfoCard from '@core/components/quick-info-card/VtsQuickInfoCard.vue'
+import VtsQuickInfoColumn from '@core/components/quick-info-column/VtsQuickInfoColumn.vue'
+import VtsQuickInfoRow from '@core/components/quick-info-row/VtsQuickInfoRow.vue'
+import UiLink from '@core/components/ui/link/UiLink.vue'
+import UiTag from '@core/components/ui/tag/UiTag.vue'
+import UiTagsList from '@core/components/ui/tag/UiTagsList.vue'
+import { useTimeAgo } from '@core/composables/local-time-ago.composable.ts'
+import { useMapper } from '@core/packages/mapper'
+import { formatSizeRaw } from '@core/utils/size.util.ts'
+import { parseDateTime } from '@core/utils/time.util.ts'
+import { faMoon, faPause, faPlay, faServer, faStop } from '@fortawesome/free-solid-svg-icons'
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+const { vm } = defineProps<{
+  vm: XoVm
+}>()
+
+const { t, locale } = useI18n()
+
+const { isReady, getVmHost } = useVmStore().subscribe()
+
+const host = computed(() => getVmHost(vm))
+
+// TODO add this to icon when new component is available
+// const hostPowerState = computed(() => {
+//   if (host.value === undefined) {
+//     return
+//   }
+//
+//   return host.value.power_state === 'Running' ? 'running' : 'halted'
+// })
+
+// TODO as above, add this to icon when new component is available
+const powerState = useMapper(
+  () => vm.power_state,
+  {
+    [VM_POWER_STATE.RUNNING]: { icon: faPlay, accent: 'success', text: t('vm-status.running') },
+    [VM_POWER_STATE.HALTED]: { icon: faStop, accent: 'danger', text: t('vm-status.halted') },
+    [VM_POWER_STATE.PAUSED]: { icon: faPause, accent: 'brand', text: t('vm-status.paused') },
+    [VM_POWER_STATE.SUSPENDED]: { icon: faMoon, accent: 'info', text: t('vm-status.suspended') },
+  },
+  VM_POWER_STATE.RUNNING
+)
+
+const ram = computed(() => formatSizeRaw(vm.memory.size, 1))
+
+const vmTimeAgo = useTimeAgo(() => new Date(parseDateTime((vm.startTime ?? 0) * 1000)))
+
+const relativeStartTime = computed(() => {
+  if (!vm.startTime || vm.power_state === VM_POWER_STATE.HALTED) {
+    return t('not-running')
+  }
+
+  return vmTimeAgo.value
+})
+
+const installDateFormatted = computed(() => {
+  if (!vm.installTime) {
+    return t('unknown')
+  }
+
+  return new Intl.DateTimeFormat(locale.value, { dateStyle: 'long' }).format(
+    new Date(parseDateTime(vm.installTime * 1000))
+  )
+})
+
+const virtualizationType = computed(() =>
+  vm.virtualizationMode === 'hvm' && vm.pvDriversDetected ? 'pvhvm' : vm.virtualizationMode
+)
+</script>
+
+<style lang="postcss" scoped>
+.vm-dashboard-quick-info {
+  .power-state,
+  .host-name {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+  }
+}
+</style>

commit a8723e361987445f138f57b14e1301c85ad33fd7

fix(xo6/site): fix issue with prefix in site dashboard (#8721)
Introduced by #8585

@xen-orchestra/web-core/lib/utils/size.util.ts
@@ -1,13 +1,15 @@
 import { parse, raw, Scale } from 'human-format'
 
-const scale = Scale.create(['B', 'KiB', 'MiB', 'GiB', 'TiB'], 1024)
+const scale = Scale.create(['', 'KiB', 'MiB', 'GiB', 'TiB'], 1024)
 
 export const formatSizeRaw = (bytes: number | undefined, decimals: number) => {
   if (bytes === undefined) {
     return undefined
   }
 
-  return raw(bytes, { maxDecimals: decimals, scale })
+  const result = raw(bytes, { maxDecimals: decimals, scale })
+
+  return { ...result, prefix: result.prefix === '' ? 'B' : result.prefix }
 }
 
 export const formatSizeParse = (size: number | undefined, unit?: string) => {