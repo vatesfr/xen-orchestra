vatesfr/xen-orchestra

commit 0776fc16161c141627fa3bc802e6b7bfc4573c52

Feat fs safeguards (#8718)
Add safety functions in abstract instead of s3 and azure. A new function "withTimeout" allows to handle timeout and success/failure after timeout.
It should help for better debug and it can be used in several situations.

@xen-orchestra/fs/src/abstract.js
@@ -3,13 +3,14 @@ import getStream from 'get-stream'
 import { asyncEach } from '@vates/async-each'
 import { coalesceCalls } from '@vates/coalesce-calls'
 import { createLogger } from '@xen-orchestra/log'
-import { fromCallback, fromEvent, ignoreErrors, timeout } from 'promise-toolbox'
+import { fromCallback, fromEvent, ignoreErrors, pRetry } from 'promise-toolbox'
 import { limitConcurrency } from 'limit-concurrency-decorator'
 import { parse } from 'xo-remote-parser'
 import { pipeline } from 'stream'
 import { randomBytes, randomUUID } from 'crypto'
 import { synchronized } from 'decorator-synchronized'
 
+import { withTimeout } from './utils'
 import { basename, dirname, normalize as normalizePath } from './path'
 import { createChecksumStream, validChecksumOfReadStream } from './checksum'
 import { DEFAULT_ENCRYPTION_ALGORITHM, UNENCRYPTED_ALGORITHM, _getEncryptor } from './_encryptor'
@@ -27,6 +28,58 @@ const DEFAULT_MAX_PARALLEL_OPERATIONS = 10
 
 const ENCRYPTION_DESC_FILENAME = 'encryption.json'
 const ENCRYPTION_METADATA_FILENAME = 'metadata.json'
+
+const WITH_LIMIT = [
+  'closeFile',
+  'copy',
+  'getInfo',
+  'getSizeOnDisk',
+  'list',
+  'mkdir',
+  'openFile',
+  'outputFile',
+  'read',
+  'readFile',
+  'rename',
+  'rmdir',
+  'truncate',
+  'unlink',
+  'write',
+  'writeFile',
+]
+
+const WITH_RETRY = [
+  '_closeFile',
+  '_copy',
+  '_getInfo',
+  '_getSize',
+  '_list',
+  '_mkdir',
+  '_openFile',
+  '_outputFile',
+  '_read',
+  '_readFile',
+  '_rename',
+  '_rmdir',
+  '_truncate',
+  '_unlink',
+  '_write',
+  '_writeFile',
+]
+
+const WITH_TIMEOUT = [
+  '_closeFile',
+  '_createReadStream',
+  '_getInfo',
+  '_getSize',
+  '_list',
+  '_rename',
+  '_copy',
+  '_rmdir',
+  '_closeFile',
+  '_openFile',
+  '_createOutputStream',
+]
 
 const ignoreEnoent = error => {
   if (error == null || error.code !== 'ENOENT') {
@@ -93,28 +146,59 @@ export default class RemoteHandlerAbstract {
         throw new Error('Incorrect remote type')
       }
     }
-    ;({ highWaterMark: this._highWaterMark, timeout: this._timeout = DEFAULT_TIMEOUT } = options)
+    ;({
+      highWaterMark: this._highWaterMark,
+      timeout: this._timeout = DEFAULT_TIMEOUT,
+      withLimit: this._withLimit = WITH_LIMIT,
+      withTimeout: this._withTimeout = WITH_TIMEOUT,
+      withRetry: this._withRetry = WITH_RETRY,
+    } = options)
 
+    this.#applySafeGuards(options)
+  }
+
+  #conditionRetry(error) {
+    return !['EEXIST', 'EISDIR', 'ENOTEMPTY', 'ENOENT', 'ENOTDIR', 'SystemInUse'].includes(error?.code)
+  }
+
+  #applySafeGuards(options) {
     const sharedLimit = limitConcurrency(options.maxParallelOperations ?? DEFAULT_MAX_PARALLEL_OPERATIONS)
-    this.closeFile = sharedLimit(this.closeFile)
-    this.copy = sharedLimit(this.copy)
-    this.getInfo = sharedLimit(this.getInfo)
-    this.getSizeOnDisk = sharedLimit(this.getSizeOnDisk)
-    this.list = sharedLimit(this.list)
-    this.mkdir = sharedLimit(this.mkdir)
-    this.openFile = sharedLimit(this.openFile)
-    this.outputFile = sharedLimit(this.outputFile)
-    this.read = sharedLimit(this.read)
-    this.readFile = sharedLimit(this.readFile)
-    this.rename = sharedLimit(this.rename)
-    this.rmdir = sharedLimit(this.rmdir)
-    this.truncate = sharedLimit(this.truncate)
-    this.unlink = sharedLimit(this.unlink)
-    this.write = sharedLimit(this.write)
-    this.writeFile = sharedLimit(this.writeFile)
 
     this._forget = coalesceCalls(this._forget)
     this._sync = coalesceCalls(this._sync)
+
+    this._withLimit.forEach(functionName => {
+      if (this[functionName] !== undefined) {
+        this[functionName] = sharedLimit(this[functionName])
+      }
+    })
+
+    this._withTimeout.forEach(functionName => {
+      if (this[functionName] !== undefined) {
+        this[functionName] = withTimeout(this[functionName], DEFAULT_TIMEOUT)
+      }
+    })
+
+    this._withRetry.forEach(functionName => {
+      if (this[functionName] !== undefined) {
+        // adding the retry on the top level method won't
+        // cover when _functionName are called internally
+        this[functionName] = pRetry.wrap(this[functionName], {
+          delays: [100, 200, 500, 1000, 2000],
+          // these errors should not change on retry
+          when: err => this.#conditionRetry(err),
+          onRetry(error) {
+            warn('retrying method on fs ', {
+              method: functionName,
+              attemptNumber: this.attemptNumber,
+              delay: this.delay,
+              error,
+              file: this.arguments?.[0],
+            })
+          },
+        })
+      }
+    })
   }
 
   // Public members
@@ -140,11 +224,7 @@ export default class RemoteHandlerAbstract {
       file = normalizePath(file)
     }
 
-    let stream = await timeout.call(
-      this._createReadStream(file, { ...options, highWaterMark: this._highWaterMark }),
-      this._timeout
-    )
-
+    let stream = await this._createReadStream(file, { ...options, highWaterMark: this._highWaterMark })
     // detect early errors
     await fromEvent(stream, 'readable')
 
@@ -228,7 +308,7 @@ export default class RemoteHandlerAbstract {
   }
 
   async getInfo() {
-    return timeout.call(this._getInfo(), this._timeout)
+    return this._getInfo()
   }
 
   // returns the real size occupied by an unencrypted file
@@ -239,7 +319,7 @@ export default class RemoteHandlerAbstract {
   }
 
   async getSizeOnDisk(file) {
-    return timeout.call(this._getSize(typeof file === 'string' ? normalizePath(file) : file), this._timeout)
+    return this._getSize(typeof file === 'string' ? normalizePath(file) : file)
   }
 
   async __list(dir, { filter, ignoreMissing = false, prependDir = false } = {}) {
@@ -247,7 +327,7 @@ export default class RemoteHandlerAbstract {
       const virtualDir = normalizePath(dir)
       dir = normalizePath(dir)
 
-      let entries = await timeout.call(this._list(dir), this._timeout)
+      let entries = await this._list(dir)
       if (filter !== undefined) {
         entries = entries.filter(filter)
       }
@@ -293,7 +373,7 @@ export default class RemoteHandlerAbstract {
 
   async #rename(oldPath, newPath, { checksum }, createTree = true) {
     try {
-      let p = timeout.call(this._rename(oldPath, newPath), this._timeout)
+      let p = this._rename(oldPath, newPath)
       if (checksum) {
         p = Promise.all([p, this._rename(checksumFile(oldPath), checksumFile(newPath))])
       }
@@ -316,7 +396,7 @@ export default class RemoteHandlerAbstract {
     oldPath = normalizePath(oldPath)
     newPath = normalizePath(newPath)
 
-    let p = timeout.call(this._copy(oldPath, newPath), this._timeout)
+    let p = this._copy(oldPath, newPath)
     if (checksum) {
       p = Promise.all([p, this._copy(checksumFile(oldPath), checksumFile(newPath))])
     }
@@ -324,7 +404,7 @@ export default class RemoteHandlerAbstract {
   }
 
   async rmdir(dir) {
-    await timeout.call(this._rmdir(normalizePath(dir)).catch(ignoreEnoent), this._timeout)
+    await this._rmdir(normalizePath(dir)).catch(ignoreEnoent)
   }
 
   async rmtree(dir) {
@@ -473,7 +553,7 @@ export default class RemoteHandlerAbstract {
   // Methods that can be called by private methods to avoid parallel limit on public methods
 
   async __closeFile(fd) {
-    await timeout.call(this._closeFile(fd.fd), this._timeout)
+    await this._closeFile(fd.fd)
   }
 
   async __mkdir(dir, { mode } = {}) {
@@ -495,7 +575,7 @@ export default class RemoteHandlerAbstract {
     path = normalizePath(path)
 
     return {
-      fd: await timeout.call(this._openFile(path, flags), this._timeout),
+      fd: await this._openFile(path, flags),
       path,
     }
   }
@@ -588,13 +668,10 @@ export default class RemoteHandlerAbstract {
 
   async _outputStream(path, input, { dirMode, validator }) {
     const tmpPath = `${dirname(path)}/.${basename(path)}`
-    const output = await timeout.call(
-      this._createOutputStream(tmpPath, {
-        dirMode,
-        flags: 'wx',
-      }),
-      this._timeout
-    )
+    const output = this._createOutputStream(tmpPath, {
+      dirMode,
+      flags: 'wx',
+    })
     try {
       await fromCallback(pipeline, input, output)
       if (validator !== undefined) {

@xen-orchestra/fs/src/abstract.test.js
@@ -10,17 +10,37 @@ import AbstractHandler from './abstract'
 import fs from 'fs-extra'
 import tmp from 'tmp'
 
-const TIMEOUT = 10e3
+const TIMEOUT = 6e5
 
 class TestHandler extends AbstractHandler {
-  constructor(impl) {
-    super({ url: 'test://' }, { timeout: TIMEOUT })
+  constructor() {
+    const options = { timeout: TIMEOUT, withRetry: [] }
+    super({ url: 'test://' }, options)
     Object.defineProperty(this, 'isEncrypted', {
       get: () => false, // encryption is tested separately
     })
-    Object.keys(impl).forEach(method => {
-      this[`_${method}`] = impl[method]
-    })
+  }
+
+  async _closeFile() {
+    Promise(() => {})
+  }
+  async _getInfo() {
+    Promise(() => {})
+  }
+  async _getSize() {
+    Promise(() => {})
+  }
+  async _list() {
+    Promise(() => {})
+  }
+  async _openFile() {
+    Promise(() => {})
+  }
+  async _rename() {
+    Promise(() => {})
+  }
+  async _rmdir() {
+    Promise(() => {})
   }
 }
 
@@ -30,9 +50,7 @@ const clock = sinon.useFakeTimers()
 
 describe('closeFile()', () => {
   it(`throws in case of timeout`, async () => {
-    const testHandler = new TestHandler({
-      closeFile: () => new Promise(() => {}),
-    })
+    const testHandler = new TestHandler()
 
     const promise = testHandler.closeFile({ fd: undefined, path: '' })
     clock.tick(TIMEOUT)
@@ -42,9 +60,7 @@ describe('closeFile()', () => {
 
 describe('getInfo()', () => {
   it('throws in case of timeout', async () => {
-    const testHandler = new TestHandler({
-      getInfo: () => new Promise(() => {}),
-    })
+    const testHandler = new TestHandler()
 
     const promise = testHandler.getInfo()
     clock.tick(TIMEOUT)
@@ -54,9 +70,7 @@ describe('getInfo()', () => {
 
 describe('getSize()', () => {
   it(`throws in case of timeout`, async () => {
-    const testHandler = new TestHandler({
-      getSize: () => new Promise(() => {}),
-    })
+    const testHandler = new TestHandler()
 
     const promise = testHandler.getSize('')
     clock.tick(TIMEOUT)
@@ -66,9 +80,7 @@ describe('getSize()', () => {
 
 describe('list()', () => {
   it(`throws in case of timeout`, async () => {
-    const testHandler = new TestHandler({
-      list: () => new Promise(() => {}),
-    })
+    const testHandler = new TestHandler()
 
     const promise = testHandler.list('.')
     clock.tick(TIMEOUT)
@@ -78,9 +90,7 @@ describe('list()', () => {
 
 describe('openFile()', () => {
   it(`throws in case of timeout`, async () => {
-    const testHandler = new TestHandler({
-      openFile: () => new Promise(() => {}),
-    })
+    const testHandler = new TestHandler()
 
     const promise = testHandler.openFile('path')
     clock.tick(TIMEOUT)
@@ -90,9 +100,7 @@ describe('openFile()', () => {
 
 describe('rename()', () => {
   it(`throws in case of timeout`, async () => {
-    const testHandler = new TestHandler({
-      rename: () => new Promise(() => {}),
-    })
+    const testHandler = new TestHandler()
 
     const promise = testHandler.rename('oldPath', 'newPath')
     clock.tick(TIMEOUT)
@@ -102,9 +110,7 @@ describe('rename()', () => {
 
 describe('rmdir()', () => {
   it(`throws in case of timeout`, async () => {
-    const testHandler = new TestHandler({
-      rmdir: () => new Promise(() => {}),
-    })
+    const testHandler = new TestHandler()
 
     const promise = testHandler.rmdir('dir')
     clock.tick(TIMEOUT)

@xen-orchestra/fs/src/azure.js
@@ -3,7 +3,6 @@ import { createLogger } from '@xen-orchestra/log'
 import { parse } from 'xo-remote-parser'
 import { join, split } from './path'
 import RemoteHandlerAbstract from './abstract'
-import { pRetry } from 'promise-toolbox'
 import copyStreamToBuffer from './_copyStreamToBuffer'
 import { PassThrough, Transform, pipeline } from 'stream'
 
@@ -37,24 +36,6 @@ export default class AzureHandler extends RemoteHandlerAbstract {
     this.#container = parts.shift() // in azurite, container = first component after host, only lowercase allowed
     this.#dir = join(...parts)
     this.#containerClient = this.#blobServiceClient.getContainerClient(this.#container)
-    const WITH_RETRY = ['_copy', '_getSize', '_list', '_outputFile', '_read', '_rename', '_unlink', '_writeFile']
-    WITH_RETRY.forEach(functionName => {
-      if (this[functionName] !== undefined) {
-        this[functionName] = pRetry.wrap(this[functionName], {
-          delays: [100, 200, 500, 1000, 2000],
-          when: err => !['SystemInUse'].includes(err?.code),
-          onRetry(error) {
-            warn('retrying method on fs ', {
-              method: functionName,
-              attemptNumber: this.attemptNumber,
-              delay: this.delay,
-              error,
-              file: this.arguments?.[0],
-            })
-          },
-        })
-      }
-    })
   }
 
   get type() {

@xen-orchestra/fs/src/s3.js
@@ -25,7 +25,6 @@ import guessAwsRegion from './_guessAwsRegion.js'
 import RemoteHandlerAbstract from './abstract'
 import { basename, join, split } from './path'
 import { asyncEach } from '@vates/async-each'
-import { pRetry } from 'promise-toolbox'
 
 // endpoints https://docs.aws.amazon.com/general/latest/gr/s3.html
 
@@ -81,44 +80,6 @@ export default class S3Handler extends RemoteHandlerAbstract {
     const parts = split(path)
     this.#bucket = parts.shift()
     this.#dir = join(...parts)
-    const WITH_RETRY = [
-      '_closeFile',
-      '_copy',
-      '_getInfo',
-      '_getSize',
-      '_list',
-      '_mkdir',
-      '_openFile',
-      '_outputFile',
-      '_read',
-      '_readFile',
-      '_rename',
-      '_rmdir',
-      '_truncate',
-      '_unlink',
-      '_write',
-      '_writeFile',
-    ]
-    WITH_RETRY.forEach(functionName => {
-      if (this[functionName] !== undefined) {
-        // adding the retry on the top level method won't
-        // cover when _functionName are called internally
-        this[functionName] = pRetry.wrap(this[functionName], {
-          delays: [100, 200, 500, 1000, 2000],
-          // these errors should not change on retry
-          when: err => !['EEXIST', 'EISDIR', 'ENOTEMPTY', 'ENOENT', 'ENOTDIR', 'EISDIR'].includes(err?.code),
-          onRetry(error) {
-            warn('retrying method on fs ', {
-              method: functionName,
-              attemptNumber: this.attemptNumber,
-              delay: this.delay,
-              error,
-              file: this.arguments?.[0],
-            })
-          },
-        })
-      }
-    })
   }
 
   get type() {

@xen-orchestra/fs/src/utils.js
@@ -1,0 +1,77 @@
+import { createLogger } from '@xen-orchestra/log'
+import { TimeoutError } from 'promise-toolbox'
+
+const { info, warn } = createLogger('xo:fs:abstract')
+
+/**
+ * Wraps an asynchronous function with timeout functionality.
+ *
+ * @template T
+ * @param {(...args: any[]) => Promise<T>} fn - The asynchronous function to wrap
+ * @param {Object} [options] - Configuration options
+ * @param {(result: T) => void} [options.onSuccessAfterTimeout] - Callback invoked if original promise resolves after timeout
+ * @param {(error: any) => void} [options.onFailureAfterTimeout] - Callback invoked if original promise rejects after timeout
+ * @returns {(...args: any[]) => Promise<T>} A new function that will enforce the timeout
+ * @throws {TypeError} If fn is not a function or timeout is not a positive number
+ *
+ * @example
+ * // Basic usage
+ * const fnWithTimeout = withTimeout(asyncFunc, 1000)
+ * try {
+ *   const result = await fnWithTimeout()
+ * } catch (error) {
+ *   if (error instanceof TimeoutError) {
+ *     // Handle timeout
+ *   }
+ * }
+ *
+ * @example
+ * // With callbacks
+ * const fnWithTimeout = withTimeout(asyncFunc, 1000, {
+ *   onSuccessAfterTimeout: (result) => console.log('Late success', result),
+ *   onFailureAfterTimeout: (error) => console.log('Late failure', error)
+ * })
+ */
+export function withTimeout(fn, timeout, { onSuccessAfterTimeout, onFailureAfterTimeout } = {}) {
+  return function (...args) {
+    let timeoutHandle
+    let didTimeout = false
+    if (typeof fn !== 'function') {
+      throw new TypeError('First argument must be a function')
+    }
+    const timeoutError = new TimeoutError('Async call timeout limit reached')
+
+    return new Promise((resolve, reject) => {
+      timeoutHandle = setTimeout(() => {
+        didTimeout = true
+        reject(timeoutError)
+      }, timeout)
+
+      // if fn is synchronous and throws an error, it will throw it here
+      const promise = fn.apply(this, args)
+      if (promise?.then === undefined) {
+        throw new Error('Function needs to be asynchronous.')
+      }
+      promise.then(
+        result => {
+          clearTimeout(timeoutHandle)
+          if (didTimeout) {
+            info('Success after timeout:\n', result)
+            onSuccessAfterTimeout?.(result)
+          } else {
+            resolve(result)
+          }
+        },
+        error => {
+          clearTimeout(timeoutHandle)
+          if (didTimeout) {
+            warn('Failure after timeout:\n', error)
+            onFailureAfterTimeout?.(error)
+          } else {
+            reject(error)
+          }
+        }
+      )
+    })
+  }
+}

@xen-orchestra/fs/src/utils.test.js
@@ -1,0 +1,97 @@
+import { describe, it, beforeEach, afterEach, mock } from 'node:test'
+import { strict as assert } from 'assert'
+import sinon from 'sinon'
+import { TimeoutError } from 'promise-toolbox'
+import { withTimeout } from './utils'
+
+const TIMEOUT = 5000
+
+describe('withTimeout()', () => {
+  let clock
+
+  beforeEach(() => {
+    clock = sinon.useFakeTimers()
+  })
+
+  afterEach(() => {
+    clock.restore()
+    mock.reset()
+  })
+
+  it(`throws if function is synchronous`, async () => {
+    let promiseFct = () => {
+      return 'return value'
+    }
+    promiseFct = withTimeout(promiseFct, TIMEOUT)
+    const promise = promiseFct()
+    assert.rejects(promise, new Error('Function needs to be asynchronous.'))
+  })
+
+  it(`throws in case of timeout`, async () => {
+    let promiseFct = () => new Promise(() => {})
+    promiseFct = withTimeout(promiseFct, TIMEOUT)
+    const promise = promiseFct()
+    clock.tick(TIMEOUT)
+    await assert.rejects(promise, new TimeoutError())
+  })
+
+  it(`can success after timeout()`, async () => {
+    let promiseFct = () =>
+      new Promise(resolve => {
+        setTimeout(() => resolve('done'), TIMEOUT)
+      })
+    const mockMethod = mock.fn(() => {})
+    promiseFct = withTimeout(promiseFct, TIMEOUT / 2, { onSuccessAfterTimeout: mockMethod })
+
+    const promise = promiseFct()
+    clock.tick(TIMEOUT)
+    await assert.rejects(promise, new TimeoutError())
+    assert.strictEqual(mockMethod.mock.callCount(), 1)
+    assert.strictEqual(mockMethod.mock.calls[0].arguments[0], 'done')
+  })
+
+  it(`can fail after timeout()`, async () => {
+    const testError = new Error('test')
+    let promiseFct = () =>
+      new Promise((_resolve, reject) => {
+        setTimeout(() => {
+          reject(testError)
+        }, TIMEOUT)
+      })
+
+    const mockMethod = mock.fn(() => {})
+    promiseFct = withTimeout(promiseFct, TIMEOUT / 2, { onFailureAfterTimeout: mockMethod })
+    const promise = promiseFct()
+    clock.tick(TIMEOUT)
+    await assert.rejects(promise, new TimeoutError())
+
+    assert.strictEqual(mockMethod.mock.callCount(), 1)
+    assert.strictEqual(mockMethod.mock.calls[0].arguments[0], testError)
+  })
+
+  it(`can success before timeout()`, async () => {
+    let promiseFct = () =>
+      new Promise(resolve => {
+        setTimeout(() => resolve('done'), TIMEOUT)
+      })
+    promiseFct = withTimeout(promiseFct, TIMEOUT * 2)
+
+    const promise = promiseFct()
+    clock.tick(TIMEOUT)
+    const result = await promise
+    assert.strictEqual(result, 'done')
+  })
+
+  it(`can fail before timeout()`, async () => {
+    const testError = new Error('test')
+    let promiseFct = () =>
+      new Promise((_resolve, reject) => {
+        setTimeout(() => reject(testError), TIMEOUT)
+      })
+    promiseFct = withTimeout(promiseFct, TIMEOUT * 2)
+
+    const promise = promiseFct()
+    clock.tick(TIMEOUT)
+    assert.rejects(promise, testError)
+  })
+})

commit 695b0c4d908f9ab7643b913808d7db972fed6e3f

fix: re-add await for createOutputStream (#8741)
Fix issue in metadata backup which was broken due to forgotten async introduced by #8718

@xen-orchestra/fs/src/abstract.js
@@ -668,7 +668,7 @@ export default class RemoteHandlerAbstract {
 
   async _outputStream(path, input, { dirMode, validator }) {
     const tmpPath = `${dirname(path)}/.${basename(path)}`
-    const output = this._createOutputStream(tmpPath, {
+    const output = await this._createOutputStream(tmpPath, {
       dirMode,
       flags: 'wx',
     })