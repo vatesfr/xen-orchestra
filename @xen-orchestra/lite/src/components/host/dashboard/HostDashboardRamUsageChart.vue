903f7352e159fd8d78474d362e5e1bd452302bc8
Joris-K <joris.kosacki@vates.tech>
2025-07-02 09:50:15 +0200

    fix(xo-lite): change timestamp calculation

--- @xen-orchestra/lite/src/components/host/dashboard/HostDashboardRamUsageChart.vue
+++ @xen-orchestra/lite/src/components/host/dashboard/HostDashboardRamUsageChart.vue
@@ -43,11 +43,9 @@ const memoryUsage = computed<LinearChartData>(() => {
     return []
   }
 
-  const result = memoryTotal.map((total, index) => ({
-    timestamp:
-      (timestampStart - RRD_STEP_FROM_STRING.hours * (memoryTotal.length - 1) + index * RRD_STEP_FROM_STRING.hours) *
-      1000,
-    value: (total ?? NaN) - (memoryFree[index] ?? NaN),
+  const result = memoryTotal.map((total, hourIndex) => ({
+    timestamp: (timestampStart + hourIndex * RRD_STEP_FROM_STRING.hours) * 1000,
+    value: (total ?? NaN) - (memoryFree[hourIndex] ?? NaN),
   }))
 
   return [
