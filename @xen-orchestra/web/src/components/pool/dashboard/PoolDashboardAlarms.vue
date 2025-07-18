74d3cd2400a9bada23d24e14492aa2ad2c997702
Joris-K <joris.kosacki@vates.tech>
2025-07-18 17:36:22 +0200

    wip, add data in component alarm

--- @xen-orchestra/web/src/components/pool/dashboard/PoolDashboardAlarms.vue
+++ @xen-orchestra/web/src/components/pool/dashboard/PoolDashboardAlarms.vue
@@ -1,31 +1,45 @@
 <template>
   <UiCard class="pool-dashboard-alarms">
     <UiCardTitle>
-      {{ t('alarms') }} <UiCounter accent="danger" size="small" variant="primary" :value="records.length" />
+      {{ t('alarms') }}
+      <UiCounter
+        v-if="alarmRecords.length"
+        accent="danger"
+        size="small"
+        variant="primary"
+        :value="alarmRecords.length"
+      />
     </UiCardTitle>
-    <!--    TODO Change when the component is available -->
-    <ul v-if="records.length > 0">
-      <li v-for="(alarm, index) in records" :key="index">
-        <p>{{ alarm.body.name }}</p>
-        <p>{{ alarm.body.value }}</p>
-        <p>{{ alarm.object.type }}</p>
-        <p>{{ alarm.time }}</p>
-        <VtsDivider type="stretch" />
-      </li>
-    </ul>
+    <UiAlarmList>
+      <UiAlarmItem
+        v-for="alarm in alarmRecords"
+        :key="alarm.id"
+        :label="alarm.body.name"
+        :percent="Number(alarm.body.value)"
+        size="large"
+        :date="alarm.time * 1000"
+      >
+        <template #link>
+          <AlarmLink :type="alarm.object.type" :uuid="alarm.object.uuid" />
+        </template>
+      </UiAlarmItem>
+    </UiAlarmList>
   </UiCard>
 </template>
 
 <script setup lang="ts">
+import AlarmLink from '@/components/pool/dashboard/AlarmLink.vue'
 import { useAlarmStore } from '@/stores/xo-rest-api/alarm.store.ts'
-import VtsDivider from '@core/components/divider/VtsDivider.vue'
+import UiAlarmItem from '@core/components/ui/alarm-item/UiAlarmItem.vue'
+import UiAlarmList from '@core/components/ui/alarm-list/UiAlarmList.vue'
 import UiCard from '@core/components/ui/card/UiCard.vue'
 import UiCardTitle from '@core/components/ui/card-title/UiCardTitle.vue'
 import UiCounter from '@core/components/ui/counter/UiCounter.vue'
 import { useI18n } from 'vue-i18n'
 
 const { t } = useI18n()
-const { records } = useAlarmStore().subscribe()
+
+const { records: alarmRecords } = useAlarmStore().subscribe()
 </script>
 
 <style lang="postcss" scoped>
