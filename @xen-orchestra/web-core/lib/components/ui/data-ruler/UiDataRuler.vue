210415ece4be36765fed5308f70644781e029616
Joris-K <joris.kosacki@vates.tech>
2025-07-22 11:22:28 +0200

    feat(web-core): added data ruler component

---
+++ @xen-orchestra/web-core/lib/components/ui/data-ruler/UiDataRuler.vue
@@ -0,0 +1,23 @@
+<!-- v1 -->
+<template>
+  <div class="ui-data-ruler typo-body-regular-small">
+    <span>{{ t('n-percent', 0) }}</span>
+    <span>{{ t('n-percent', 50) }}</span>
+    <span>{{ t('n-percent', 100) }}</span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+</script>
+
+<style lang="postcss" scoped>
+.ui-data-ruler {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1rem;
+}
+</style>
