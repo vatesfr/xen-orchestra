0c914704db5a1c28cc92f3fac457ed78b72c8210
Joris K <43381087+J0ris-K@users.noreply.github.com>
2025-07-23 10:29:32 +0200

    feat(web-core): added data ruler component (#8812)

---
+++ @xen-orchestra/lite/src/stories/web-core/ui/data-ruler/ui-data-ruler.story.vue
@@ -0,0 +1,10 @@
+<template>
+  <ComponentStory :params="[]">
+    <UiDataRuler />
+  </ComponentStory>
+</template>
+
+<script lang="ts" setup>
+import ComponentStory from '@/components/component-story/ComponentStory.vue'
+import UiDataRuler from '@core/components/ui/data-ruler/UiDataRuler.vue'
+</script>
