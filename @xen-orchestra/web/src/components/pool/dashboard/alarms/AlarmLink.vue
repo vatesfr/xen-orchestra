27b1d889dc75595ec3d5c2e29d3b431d3582ba68
Joris-K <joris.kosacki@vates.tech>
2025-07-24 12:09:22 +0200

    fix issue with type

--- @xen-orchestra/web/src/components/pool/dashboard/alarms/AlarmLink.vue
+++ @xen-orchestra/web/src/components/pool/dashboard/alarms/AlarmLink.vue
@@ -14,6 +14,8 @@ import { useHostStore } from '@/stores/xo-rest-api/host.store.ts'
 import { useSrStore } from '@/stores/xo-rest-api/sr.store.ts'
 import { useVmControllerStore } from '@/stores/xo-rest-api/vm-controller.store.ts'
 import { useVmStore } from '@/stores/xo-rest-api/vm.store.ts'
+import type { XoVmController } from '@/types/xo/vm-controller.type.ts'
+import type { SupportedState } from '@core/types/object-icon.type.ts'
 import UiObjectIcon from '@core/components/ui/object-icon/UiObjectIcon.vue'
 import UiObjectLink from '@core/components/ui/object-link/UiObjectLink.vue'
 import type { XapiXoRecord } from '@vates/types'
@@ -33,15 +35,19 @@ const record = computed(() => {
   if (type === 'VM') {
     return vmRecords.value.find(vm => vm.id === uuid)
   }
+
   if (type === 'host') {
     return hostRecords.value.find(vm => vm.id === uuid)
   }
+
   if (type === 'VM-controller') {
     return vmControllerRecords.value.find(vm => vm.id === uuid)
   }
+
   if (type === 'SR') {
     return srRecords.value.find(vm => vm.id === uuid)
   }
+
   return undefined
 })
 
@@ -51,15 +57,35 @@ const iconType = computed(() => {
   if (type === 'VM' || type === 'VM-controller') {
     return 'vm'
   }
+
   if (type === 'host') {
     return 'host'
   }
+
+  if (type === 'SR') {
+    return 'sr'
+  }
+
   return 'vm'
 })
 
-const powerState = computed(() => record.value?.power_state.toLowerCase() ?? 'unknown')
+const powerState = computed<SupportedState<'vm' | 'host'> | 'disabled'>(() => {
+  if (!record.value || !('power_state' in record.value)) {
+    return 'disabled'
+  }
+
+  return record.value.power_state.toLowerCase() as SupportedState<'vm' | 'host'>
+})
 
 const route = computed(() => {
-  return type === 'VM-controller' ? `/host/${record?.value?.$container}` : `/${type}/${uuid}`
+  if (!record.value) {
+    return undefined
+  }
+
+  if (type === 'VM-controller') {
+    return `/host/${(record.value as XoVmController).$container}`
+  }
+
+  return `/${type}/${uuid}`
 })
 </script>
