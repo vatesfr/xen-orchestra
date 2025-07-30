d1441b8c9942e7b285a94d8d497cd2ee089b4b9b
Joris-K <joris.kosacki@vates.tech>
2025-07-30 11:49:17 +0200

    fix(xo6): fix issue with infinite loading on site dashboard

--- @xen-orchestra/web/src/components/site/dashboard/S3BackupRepository.vue
+++ @xen-orchestra/web/src/components/site/dashboard/S3BackupRepository.vue
@@ -4,14 +4,15 @@
       {{ t('s3-backup-repository') }}
       <template #description>{{ t('for-backup') }}</template>
     </UiCardTitle>
-    <VtsLoadingHero v-if="!areS3BackupRepositoriesReady" type="card" />
+    <!--    TODO change and add loading when we have isReady available -->
+    <VtsNoDataHero v-if="!areS3BackupRepositoriesReady" type="card" />
     <UiCardNumbers v-else :value="usedSize?.value" :unit="usedSize?.prefix" :label="t('used')" size="medium" />
   </div>
 </template>
 
 <script setup lang="ts">
 import type { XoDashboard } from '@/types/xo/dashboard.type.ts'
-import VtsLoadingHero from '@core/components/state-hero/VtsLoadingHero.vue'
+import VtsNoDataHero from '@core/components/state-hero/VtsNoDataHero.vue'
 import UiCardNumbers from '@core/components/ui/card-numbers/UiCardNumbers.vue'
 import UiCardTitle from '@core/components/ui/card-title/UiCardTitle.vue'
 import { formatSizeRaw } from '@core/utils/size.util'
