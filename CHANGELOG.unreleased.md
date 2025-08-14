d93c218f9d149eaeabb9355d5ed73ce9dc13dea9
Florent BEAUCHAMP <florent.beauchamp@vates.tech>
2025-08-14 06:32:40 +0000

    feat(xo-web): show vddk check results

--- CHANGELOG.unreleased.md
+++ CHANGELOG.unreleased.md
@@ -11,6 +11,9 @@
 
 > Users must be able to say: “Nice enhancement, I'm eager to test it”
 
+- [V2V] Use vddk to transfer data from vmware (PR [#840](https://github.com/vatesfr/xen-orchestra/pull/840))
+- [V2V] Show if any prerequisite on XO is missing before import (PR [#840](https://github.com/vatesfr/xen-orchestra/pull/840))
+
 ### Bug fixes
 
 > Users must be able to say: “I had this issue, happy to know it's fixed”
@@ -34,5 +37,7 @@
 - @vates/nbd-client minor
 - @vates/read-chunk patch
 - @xen-orchestra/vmware-explorer minor
+- xo-server minor
+- xo-web minor
 
 <!--packages-end-->
---
+++ packages/xo-web/src/xo-app/vm-import/esxi/esxi-check.js
@@ -0,0 +1,46 @@
+import _ from 'intl'
+import { esxiCheckInstall } from 'xo'
+import Component from 'base-component'
+import Icon from 'icon'
+import React from 'react'
+
+export class EsxiCheck extends Component {
+  state = {
+    result: {},
+  }
+  componentWillMount() {
+    esxiCheckInstall().then(result => {
+      this.setState({
+        result,
+      })
+    })
+  }
+  render() {
+    const { result } = this.state
+    if (result === undefined) {
+      return (
+        <ul>
+          <li>{_('esxiCheckingPrerequisite')}</li>
+        </ul>
+      )
+    }
+    return (
+      <div>
+        <h4>{_('esxiCheckedPrerequisite')}</h4>
+        <ul className='list-group'>
+          {Object.entries(result).map(([name, value]) => {
+            const { status, error, version, expectedVersion } = value
+            return (
+              <li key={name}>
+                <Icon icon={status} />
+                {name} :{status === 'success' && ' ok'}
+                {status === 'error' && error}
+                {version && status === 'alarm' && _('esxiCheckedPrerequisiteVersion', { version, expectedVersion })}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    )
+  }
+}
