f239ee431308d8bd654475292f04b4323713dbe8
Olivier Floch <olivier.floch@vates.fr>
2025-08-25 16:11:25 +0200

    Add missing link in table header

--- @xen-orchestra/web/src/components/site/backups/BackupJobsTable.vue
+++ @xen-orchestra/web/src/components/site/backups/BackupJobsTable.vue
@@ -2,6 +2,9 @@
   <div class="backup-jobs-table">
     <UiTitle>
       {{ t('backup-jobs') }}
+      <template #actions>
+        <UiLink size="medium" href="/#/backup/new">{{ t('configure-in-xo-5') }}</UiLink>
+      </template>
     </UiTitle>
     <div class="container">
       <div class="table-actions">
