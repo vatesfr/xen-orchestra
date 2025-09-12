e6388c40a0e2ed5aecdfa26f177b8807a3b225c1
MathieuRA <contact@mathieu-raisin.fr>
2025-09-12 10:06:38 -0400

    update XO6 URLs

--- @xen-orchestra/web/src/remote-resources/use-xo-vm-backup-job-collection.ts
+++ @xen-orchestra/web/src/remote-resources/use-xo-vm-backup-job-collection.ts
@@ -3,7 +3,7 @@ import type { XoVmBackupJob } from '@/types/xo/vm-backup-job.type.ts'
 import { defineRemoteResource } from '@core/packages/remote-resource/define-remote-resource.ts'
 
 export const useXoVmBackupJobCollection = defineRemoteResource({
-  url: '/rest/v0/backup/jobs/vm?fields=id,name,mode,type,settings,remotes,srs,vms',
+  url: '/rest/v0/backup-jobs?filter=type:backup&fields=id,name,mode,type,settings,remotes,srs,vms',
   initialData: () => [] as XoVmBackupJob[],
   state: (vmBackupJobs, context) =>
     useXoCollectionState(vmBackupJobs, {
