b83b2ab36af9885061253f6d8a28413954f5a529
Florent BEAUCHAMP <florent.beauchamp@vates.fr>
2025-09-15 06:52:06 +0200

    fix(backups): sr. can contains empty values

--- @xen-orchestra/backups/_runners/_writers/IncrementalXapiWriter.mjs
+++ @xen-orchestra/backups/_runners/_writers/IncrementalXapiWriter.mjs
@@ -26,8 +26,8 @@ export class IncrementalXapiWriter extends MixinXapiWriter(AbstractIncrementalWr
       .filter(vdi => {
         // REPLICATED_TO_SR_UUID is not used here since we are already filtering from sr.$VDIs
         return (
-          vdi.managed &&
-          !vdi.is_a_snapshot /* only look for real vdi */ &&
+          vdi?.managed &&
+          !vdi?.is_a_snapshot /* only look for real vdi */ &&
           baseUuidToSrcVdi.has(vdi?.other_config[COPY_OF])
         )
       })
