d21d35077aff5a7c3aee920d3b3c7439ca689903
unel <enee.prog@gmail.com>
2025-09-17 09:41:43 +0200

    feat(web-core/icon): add ds icon for pool and host

--- @xen-orchestra/web-core/lib/icons/index.ts
+++ @xen-orchestra/web-core/lib/icons/index.ts
@@ -3,11 +3,13 @@ import { legacyIcons } from '@core/icons/legacy-icons.ts'
 import { objectIcons } from '@core/icons/object-icons.ts'
 import { defineIconPack } from '@core/packages/icon/define-icon-pack.ts'
 import type { ICON_SYMBOL } from '@core/packages/icon/types.ts'
+import { ObjectsIcons } from './DS-icons'
 
 export const icons = defineIconPack({
   fa: faIcons,
   legacy: legacyIcons,
   object: objectIcons,
+  regular: ObjectsIcons,
 })
 
 export type IconName = Exclude<keyof typeof icons, typeof ICON_SYMBOL>
