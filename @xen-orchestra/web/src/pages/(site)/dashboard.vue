67bf9a879f745ad0afeaaf735cad3f19dcc6dec0
Joris-K <joris.kosacki@vates.tech>
2025-09-18 18:07:57 +0200

    rebase and fix

--- @xen-orchestra/web/src/pages/(site)/dashboard.vue
+++ @xen-orchestra/web/src/pages/(site)/dashboard.vue
@@ -69,15 +69,9 @@ const { alarms, hasAlarmFetchError, areAlarmsReady } = useXoAlarmCollection()
   .second-row {
     grid-template-columns: repeat(3, 1fr);
     grid-template-areas:
-      'pools-status'
-      'hosts-status'
-      'vms-status'
-      'alarms'
-      'patches'
-      'resources-overview'
-      'backups'
-      'backup-issues'
-      'repositories';
+      'alarms alarms patches'
+      'backup-jobs-status vms-protection backup-issues'
+      'backup-repository storage-repository s3-backup-repository';
   }
 
   .pools-status {
