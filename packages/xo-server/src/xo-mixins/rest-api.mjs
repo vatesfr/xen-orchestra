067ee267155d3de3bc093f8fe6ca44585b5cad9e
Mathieu <70369997+MathieuRA@users.noreply.github.com>
2025-09-19 10:13:19 +0200

    feat(@xen-orchestra/rest-api): expose POST srs/:id/vdis (#8984)

--- packages/xo-server/src/xo-mixins/rest-api.mjs
+++ packages/xo-server/src/xo-mixins/rest-api.mjs
@@ -12,7 +12,6 @@ import cloneDeep from 'lodash/cloneDeep.js'
 import path from 'node:path'
 import pick from 'lodash/pick.js'
 import * as CM from 'complex-matcher'
-import { VDI_FORMAT_RAW, VDI_FORMAT_VHD } from '@xen-orchestra/xapi'
 
 import { getUserPublicProperties, isAlarm } from '../utils.mjs'
 import { compileXoJsonSchema } from './_xoJsonSchema.mjs'
@@ -1013,23 +1012,6 @@ export default class RestApi {
           return next()
         }
         return handler(req, res, next)
-      })
-    )
-
-    api.post(
-      '/:collection(srs)/:object/vdis',
-      wrap(async (req, res) => {
-        const sr = req.xapiObject
-        req.length = ifDef(req.headers['content-length'], Number)
-
-        const { name_label, name_description, raw } = req.query
-        const vdiRef = await sr.$importVdi(req, {
-          format: raw !== undefined ? VDI_FORMAT_RAW : VDI_FORMAT_VHD,
-          name_label,
-          name_description,
-        })
-
-        res.end(await sr.$xapi.getField('VDI', vdiRef, 'uuid'))
       })
     )
 
