e5d1d4f2aa2f2fb5ea1f0819bc5c802910a665de
Florent BEAUCHAMP <florent.beauchamp@vates.fr>
2025-09-22 18:15:06 +0200

    fix(sdn-controller): properly find PIFs when create cross pool networks
    PIF detection was fixed in 137837cdab310dc1c1495a9230d47e90d3a50ae2 but breaks the cross pool private
    network cases, this fixes that properly using the center when not in the
    current pool.

--- packages/xo-server-sdn-controller/src/private-network/private-network.js
+++ packages/xo-server-sdn-controller/src/private-network/private-network.js
@@ -60,7 +60,6 @@ export class PrivateNetwork {
 
     const transportPif = await this.getTransportPif(network)
     const centerTransportPif = await this.getTransportPif(centerNetwork)
-
     const hostPif = host.$PIFs.find(pif => pif.network === transportPif.network)
     const centerPif = this.center.$PIFs.find(pif => pif.network === centerTransportPif.network)
 
