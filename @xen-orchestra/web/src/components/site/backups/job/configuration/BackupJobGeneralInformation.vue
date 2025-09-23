650f52a196bc295ae80e44367d3cd003df11dce9
unel <enee.prog@gmail.com>
2025-09-23 14:52:38 +0200

    feat(xo6/backups-job): add backups jobs configguration general information

---
+++ @xen-orchestra/web/src/components/site/backups/job/configuration/BackupJobGeneralInformation.vue
@@ -0,0 +1,50 @@
+<template>
+  <UiCard class="backupJobConfiguration">
+    <UiTitle> {{ t('general-information') }} </UiTitle>
+    <span>
+      <VtsQuickInfoColumn>
+        <VtsQuickInfoRow :label="t('name')" :value="backupJob.name" />
+      </VtsQuickInfoColumn>
+      <VtsQuickInfoColumn>
+        <VtsQuickInfoRow :label="t('id')" :value="backupJob.id" />
+      </VtsQuickInfoColumn>
+      <VtsQuickInfoColumn>
+        <VtsQuickInfoRow :label="t('mode')" :value="backupJob.mode">
+          <template #value>
+            <UiTag variant="secondary" accent="info">
+              {{ backupJob.mode }}
+            </UiTag>
+          </template></VtsQuickInfoRow
+        >
+      </VtsQuickInfoColumn>
+    </span>
+  </UiCard>
+</template>
+
+<script setup lang="ts">
+import type { XoVmBackupJob } from '@/types/xo/vm-backup-job.type'
+import VtsQuickInfoColumn from '@core/components/quick-info-column/VtsQuickInfoColumn.vue'
+import VtsQuickInfoRow from '@core/components/quick-info-row/VtsQuickInfoRow.vue'
+import UiCard from '@core/components/ui/card/UiCard.vue'
+import UiTag from '@core/components/ui/tag/UiTag.vue'
+import UiTitle from '@core/components/ui/title/UiTitle.vue'
+import { useI18n } from 'vue-i18n'
+
+const { backupJob } = defineProps<{
+  backupJob: XoVmBackupJob
+}>()
+
+const { t } = useI18n()
+</script>
+
+<style lang="postcss" scoped>
+.backupJobConfiguration {
+  width: 100%;
+  span {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    justify-content: space-between;
+  }
+}
+</style>
---
+++ @xen-orchestra/web/src/components/site/backups/job/configuration/BackupJobSettings.vue
@@ -0,0 +1,80 @@
+<template>
+  <UiCard class="backupJobSettings">
+    <UiTitle> {{ t('general-information') }} </UiTitle>
+    <span>
+      <VtsQuickInfoColumn>
+        <VtsQuickInfoRow :label="t('proxy')" :value="backupJob.proxy" />
+        <VtsQuickInfoRow :label="t('snapshot-mode')">
+          <template #value>
+            <UiTag variant="secondary" accent="info">{{ backupJob.mode }}</UiTag>
+          </template>
+        </VtsQuickInfoRow>
+        <VtsQuickInfoRow
+          :label="t('number-retries-vm-fails')"
+          :value="backupJob.settings[''].nRetriesVmBackupFailures?.toString()"
+        />
+        <VtsQuickInfoRow :label="t('timeout')" :value="backupJob.settings[''].timeout?.toString()" />
+      </VtsQuickInfoColumn>
+      <VtsQuickInfoColumn>
+        <VtsQuickInfoRow :label="t('speed-limit')" :value="backupJob.settings[''].maxExportRate?.toString()" />
+        <VtsQuickInfoRow :label="t('report-when')" :value="backupJob.settings[''].reportWhen" />
+        <VtsQuickInfoRow :label="t('report-recipients')">
+          <template #value>
+            <UiTag
+              v-for="(recipient, index) in backupJob.settings[''].reportRecipients"
+              :key="index"
+              variant="secondary"
+              accent="info"
+            >
+              {{ recipient }}
+            </UiTag>
+          </template>
+        </VtsQuickInfoRow>
+        <VtsQuickInfoRow :label="t('concurrency')" :value="backupJob.settings[''].concurrency?.toString()" />
+      </VtsQuickInfoColumn>
+      <VtsQuickInfoColumn>
+        <VtsQuickInfoRow :label="t('compression')" :value="backupJob.compression" />
+        <VtsQuickInfoRow :label="t('offline-backup')">
+          <template #value>
+            <VtsEnabledState :enabled="backupJob.settings[''].offlineBackup ?? false" />
+          </template>
+        </VtsQuickInfoRow>
+        <VtsQuickInfoRow :label="t('shorter-backup-reports')" :value="backupJob.settings[''].reportWhen" />
+        <VtsQuickInfoRow :label="t('merge-backups-synchronously')">
+          <template #value>
+            <VtsEnabledState :enabled="backupJob.settings[''].mergeBackupsSynchronously ?? false" />
+          </template>
+        </VtsQuickInfoRow>
+      </VtsQuickInfoColumn>
+    </span>
+  </UiCard>
+</template>
+
+<script setup lang="ts">
+import type { XoVmBackupJob } from '@/types/xo/vm-backup-job.type'
+import VtsEnabledState from '@core/components/enabled-state/VtsEnabledState.vue'
+import VtsQuickInfoColumn from '@core/components/quick-info-column/VtsQuickInfoColumn.vue'
+import VtsQuickInfoRow from '@core/components/quick-info-row/VtsQuickInfoRow.vue'
+import UiCard from '@core/components/ui/card/UiCard.vue'
+import UiTag from '@core/components/ui/tag/UiTag.vue'
+import UiTitle from '@core/components/ui/title/UiTitle.vue'
+import { useI18n } from 'vue-i18n'
+
+const { backupJob } = defineProps<{
+  backupJob: XoVmBackupJob
+}>()
+
+const { t } = useI18n()
+</script>
+
+<style lang="postcss" scoped>
+.backupJobSettings {
+  width: 100%;
+  span {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    justify-content: space-between;
+  }
+}
+</style>
