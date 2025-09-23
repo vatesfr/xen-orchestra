3c615198a29abef551e026faca23534af43c8949
unel <enee.prog@gmail.com>
2025-09-23 17:11:44 +0200

    feat(xo6/backups-job): add backups jobs configguration schedules

--- @xen-orchestra/web/src/components/site/backups/job/configuration/BackupJobConfiguration.vue
+++ @xen-orchestra/web/src/components/site/backups/job/configuration/BackupJobConfiguration.vue
@@ -2,17 +2,26 @@
   <span class="backup-job-configuration">
     <BackupJobGeneralInformation :backup-job />
     <BackupJobsSettings :backup-job />
+    <BackupJobSchedules :backup-jobs="backupJobsForSchedule" />
   </span>
 </template>
 
 <script setup lang="ts">
+import { useXoBackupJobCollection } from '@/remote-resources/use-xo-backup-job-collection'
 import type { XoVmBackupJob } from '@/types/xo/vm-backup-job.type'
+import { computed } from 'vue'
 import BackupJobGeneralInformation from './BackupJobGeneralInformation.vue'
+import BackupJobSchedules from './BackupJobSchedules.vue'
 import BackupJobsSettings from './BackupJobSettings.vue'
 
 const { backupJob } = defineProps<{
   backupJob: XoVmBackupJob
 }>()
+
+const { backupJobs } = useXoBackupJobCollection()
+const backupJobsForSchedule = computed(() => {
+  return backupJobs.value.filter(backup => backup.id === backupJob.id)
+})
 </script>
 
 <style lang="postcss" scoped>
