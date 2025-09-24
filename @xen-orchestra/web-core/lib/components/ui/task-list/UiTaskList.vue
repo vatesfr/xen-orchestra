baf99b23e85d959572555b32c279ba132ddc13a6
Joris-K <joris.kosacki@vates.tech>
2025-09-24 17:59:16 +0200

    feat(web-core): add task item and task list components

--- @xen-orchestra/web-core/lib/components/ui/task-list/UiTaskList.vue
+++ @xen-orchestra/web-core/lib/components/ui/task-list/UiTaskList.vue
@@ -19,7 +19,7 @@ import type { Task } from '@core/types/task.type.ts'
 
 const { tasks, depth = 0 } = defineProps<{
   tasks: Task[]
-  depth: number
+  depth?: number
 }>()
 
 const emit = defineEmits<{
