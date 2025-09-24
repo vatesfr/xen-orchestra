14002de7258e68587046275a44d68fadb8880011
Joris-K <joris.kosacki@vates.tech>
2025-09-24 17:59:16 +0200

    feat(web-core): add task item and task list components

---
+++ @xen-orchestra/web-core/lib/components/ui/task-list/UiTaskList.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+    <UiTaskItem
+      v-for="task of tasksItems"
+      :key="task.id"
+      :task="task.source"
+      :expanded="task.flags.expanded"
+      :depth="depth + 1"
+      @expand="task.toggleFlag('expanded')"
+    />
+  </ul>
+</template>
+
+<script lang="ts" setup>
+import UiTaskItem, { type Task } from '@core/components/ui/task-item/UiTaskItem.vue'
+import { useCollection } from '@core/packages/collection'
+
+const { tasks, depth = 0 } = defineProps<{
+  tasks: Task[]
+  depth?: number
+}>()
+
+const { items: tasksItems } = useCollection(() => tasks, {
+  flags: ['expanded'],
+})
+</script>
