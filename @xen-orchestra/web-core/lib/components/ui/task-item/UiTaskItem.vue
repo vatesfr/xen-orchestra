eb922e91ecf50942434953eea5f364246859fc3d
Joris-K <joris.kosacki@vates.tech>
2025-09-24 17:59:16 +0200

    feat(web-core): add task item and task list components

---
+++ @xen-orchestra/web-core/lib/components/ui/task-item/UiTaskItem.vue
@@ -0,0 +1,188 @@
+<template>
+  <li>
+    <div class="ui-task-item">
+      <div class="content">
+        <div v-for="index in depth - 1" :key="index" class="tree-line">
+          <div class="tree-line-vertical" />
+        </div>
+        <UiButtonIcon
+          v-if="hasSubTasks"
+          v-tooltip="expanded ? t('core.close') : t('core.open')"
+          accent="brand"
+          :icon="expanded ? 'fa:angle-down' : 'fa:angle-right'"
+          size="small"
+          :target-scale="{ x: 1.5, y: 2 }"
+          @click="emit('expand')"
+        />
+        <div v-else class="h-line" />
+
+        <div v-if="task.properties.name" class="typo-body-bold">
+          <UiLink :href="`#task-${task.id}`" size="medium">
+            {{ task.properties.name }}
+          </UiLink>
+        </div>
+
+        <UiTag v-if="task.properties?.type" accent="info" variant="secondary">
+          {{ task.properties.type }}
+        </UiTag>
+        <UiInfo
+          v-if="task.status === 'failure' || task.status === 'interrupted'"
+          class="typo-body-regular-small"
+          accent="danger"
+        />
+        <UiInfo v-if="task.warning?.length" class="typo-body-regular-small" accent="warning" />
+        <UiInfo v-if="task.infos?.length" class="typo-body-regular-small" accent="info" />
+        <UiCounter v-if="hasSubTasks" :value="subTasksCount" accent="muted" variant="primary" size="small" />
+      </div>
+      <div class="information typo-body-regular-small">
+        <div v-if="task.properties.userId" class="user">
+          {{ t('by') }}
+          <UiAccountMenuButton size="small" />
+          {{ task.properties?.userId }}
+        </div>
+
+        <span v-if="task.start" class="start-time">
+          {{ `${t('task.started')} ${started}` }}
+        </span>
+        <span v-if="task.end" class="end-time">
+          {{ `${t('task.estimated-end')} ${end}` }}
+        </span>
+        <div class="progress">
+          <UiCircleProgressBar
+            v-if="task.properties.progress"
+            accent="info"
+            size="small"
+            :value="task.properties.progress"
+          />
+        </div>
+        <div class="actions">
+          <div class="cancel">
+            <UiButtonIcon v-if="task.status === 'pending'" icon="fa:close" size="medium" accent="danger" />
+          </div>
+          <UiButtonIcon icon="fa:check" size="medium" accent="brand" />
+        </div>
+      </div>
+    </div>
+    <template v-if="hasSubTasks && expanded">
+      <UiTaskList :tasks="subTasks" :depth="depth" />
+    </template>
+  </li>
+</template>
+
+<script lang="ts" setup>
+import UiAccountMenuButton from '@core/components/ui/account-menu-button/UiAccountMenuButton.vue'
+import UiCounter from '@core/components/ui/counter/UiCounter.vue'
+import UiLink from '@core/components/ui/link/UiLink.vue'
+import UiTag from '@core/components/ui/tag/UiTag.vue'
+import UiTaskList from '@core/components/ui/task-list/UiTaskList.vue'
+import { vTooltip } from '@core/directives/tooltip.directive'
+import { useTimeAgo } from '@vueuse/core'
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+import UiButtonIcon from '../button-icon/UiButtonIcon.vue'
+import UiCircleProgressBar from '../circle-progress-bar/UiCircleProgressBar.vue'
+import UiInfo from '../info/UiInfo.vue'
+
+export type Task = {
+  end?: number
+  id: string
+  infos?: { data: unknown; message: string }[]
+  properties: {
+    name?: string
+    progress?: number
+    type?: string
+    userId?: string
+  }
+  start: number
+  status: 'failure' | 'interrupted' | 'pending' | 'success'
+  tasks?: Task[]
+  warning?: { data: unknown; message: string }[]
+}
+
+const { task } = defineProps<{
+  task: Task
+  expanded?: boolean
+  depth: number
+}>()
+
+const emit = defineEmits<{
+  expand: []
+}>()
+
+const { t } = useI18n()
+
+const subTasks = computed(() => task.tasks ?? [])
+const subTasksCount = computed(() => subTasks.value.length)
+const hasSubTasks = computed(() => subTasksCount.value > 0)
+
+const started = useTimeAgo(() => task.start)
+const end = useTimeAgo(() => task.end ?? 0)
+</script>
+
+<style lang="postcss" scoped>
+.ui-task-item {
+  display: flex;
+  justify-content: space-between;
+  height: 4.8rem;
+  border-bottom: 0.1rem solid var(--color-neutral-border);
+
+  .information,
+  .content {
+    display: flex;
+    align-items: center;
+    gap: 1.6rem;
+    padding: 0.4rem 1.6rem;
+  }
+
+  .tree-line {
+    flex: 0 0 1em;
+    align-self: stretch;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    .tree-line-vertical {
+      width: 0.1rem;
+      background: var(--color-brand-txt-base);
+      height: calc(100% + 0.8rem);
+    }
+  }
+
+  .h-line {
+    width: 2rem;
+  }
+
+  .user {
+    display: flex;
+    align-items: center;
+  }
+
+  .user::after {
+    content: '•';
+    margin-inline-start: 1.6rem;
+  }
+
+  .start-time + .end-time::before {
+    content: '•';
+    margin-inline-end: 1.6rem;
+  }
+
+  .typo-body-regular-small {
+    color: var(--color-neutral-txt-secondary);
+  }
+
+  .progress {
+    display: flex;
+    width: 4rem;
+  }
+
+  .actions {
+    display: flex;
+    gap: 1.6rem;
+
+    .cancel {
+      width: calc(4rem - 1.6rem);
+    }
+  }
+}
+</style>
