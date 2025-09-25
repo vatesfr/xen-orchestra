0a12adad7fb8198bca2ac9881fe951c84fcd3d3b
Joris-K <joris.kosacki@vates.tech>
2025-09-25 16:30:07 +0200

    add stories and fix issue border on list

--- @xen-orchestra/web-core/lib/components/ui/task-list/UiTaskList.vue
+++ @xen-orchestra/web-core/lib/components/ui/task-list/UiTaskList.vue
@@ -18,7 +18,7 @@ import { useCollection } from '@core/packages/collection'
 
 const { tasks, depth = 0 } = defineProps<{
   tasks: Task[]
-  depth?: number
+  depth: number
 }>()
 
 const emit = defineEmits<{
