9c0a82ba4f1ac3b3f43b6bd24f46da41500489fe
unel <enee.prog@gmail.com>
2025-09-26 15:54:26 +0200

    feat(xo6/backups-job): create backed up page

---
+++ @xen-orchestra/web/src/pages/backup/[id]/backedUpVm.vue
@@ -0,0 +1,16 @@
+<template>
+  <span class="backed-up-vm">
+    <BackedUpVmsConfiguration v-if="backupJob.type === 'backup' && backupJob.vms" :backed-up-vms="backupJob.vms" />
+    <BackedUpVmsRuns v-if="backupJob.type === 'backup' && backupJob.vms" :backed-up-vms="backupJob.vms" />
+  </span>
+</template>
+
+<script setup lang="ts">
+import BackedUpVmsConfiguration from '@/components/backups/backed-up-vms/backedUpVmsConfiguration.vue'
+import BackedUpVmsRuns from '@/components/backups/backed-up-vms/backedUpVmsRuns.vue'
+import type { XoBackupJob } from '@/remote-resources/use-xo-backup-job-collection.ts'
+
+const { backupJob } = defineProps<{
+  backupJob: XoBackupJob
+}>()
+</script>
