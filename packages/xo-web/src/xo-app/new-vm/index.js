vatesfr/xen-orchestra

commit 7bb6592be4d89c66708ac0fd4488ad9fd01413f7

feat(xo-web/new-vm): add sshkeys in cloudconfig (#8968)

packages/xo-web/src/xo-app/new-vm/index.js
@@ -771,12 +771,18 @@ export default class NewVm extends BaseComponent {
     namePattern => this._buildTemplate(namePattern)
   )
 
-  _buildTemplate = pattern =>
-    compileTemplate(pattern, {
+  _buildTemplate = pattern => {
+    const rules = {
       '{index}': (_, i) => i,
       '{name}': state => state.name_label || '',
       '%': (state, i) => (state.multipleVms ? i : '%'),
+    }
+    this.props.userSshKeys.forEach(sshKey => {
+      rules[`{sshKey:${sshKey.title}}`] = sshKey.key
     })
+
+    return compileTemplate(pattern, rules)
+  }
 
   _templateHasBiosStrings = createSelector(
     () => this.props.template,

packages/xo-web/src/common/cloud-config.js
@@ -9,6 +9,7 @@ import { alert } from './modal'
 const AVAILABLE_TEMPLATE_VARS = {
   '{name}': 'templateNameInfo',
   '{index}': 'templateIndexInfo',
+  '{sshKey:<sshTitle>}': 'templateSshInfo',
 }
 
 const showAvailableTemplateVars = () =>

packages/xo-web/src/common/intl/messages.js
@@ -1823,6 +1823,7 @@ const messages = {
   availableTemplateVarsTitle: 'Available template variables',
   templateNameInfo: 'the VM\'s name. It must not contain "_"',
   templateIndexInfo: "the VM's index, it will take 0 in case of single VM",
+  templateSshInfo: 'SSH key corresponding to the SSH title',
   templateEscape: 'Tip: escape any variable with a preceding backslash (\\)',
   coreOsDefaultTemplateError: 'Error on getting the default coreOS cloud template',
   newVmBootAfterCreate: 'Boot VM after creation',

commit d77d6d0d9fb4caed9c9550ff4b15275590a58e5c

fix(new-vm): prevent error in case of empty usersshkeys (#9015)
Introduced by #8968 
Reported in #9014

packages/xo-web/src/xo-app/new-vm/index.js
@@ -777,10 +777,9 @@ export default class NewVm extends BaseComponent {
       '{name}': state => state.name_label || '',
       '%': (state, i) => (state.multipleVms ? i : '%'),
     }
-    this.props.userSshKeys.forEach(sshKey => {
+    this.props.userSshKeys?.forEach(sshKey => {
       rules[`{sshKey:${sshKey.title}}`] = sshKey.key
     })
-
     return compileTemplate(pattern, rules)
   }
 