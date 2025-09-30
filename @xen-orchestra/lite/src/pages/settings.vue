8d2af27896a18fc4e3306b38b1da677c71057037
Enée DI IORIO <86972822+UnelDev@users.noreply.github.com>
2025-09-30 15:40:21 +0200

    fix(xo-lite/settings): fix key-value alignment (#8981)
    * feat(xo-lite/settings): fix key-value alignment
    
    Refactor page for use of more regular components and match better to Figma
    
    * review
    
    * review

--- @xen-orchestra/lite/src/pages/settings.vue
+++ @xen-orchestra/lite/src/pages/settings.vue
@@ -1,134 +1,128 @@
 <template>
-  <TitleBar icon="fa:gear">{{ t('settings') }}</TitleBar>
-  <div class="card-view">
-    <UiCard class="group about">
-      <UiCardTitle>{{ t('about') }}</UiCardTitle>
-      <div class="sections">
-        <div>
-          <h5>Xen Orchestra Lite</h5>
-          <UiKeyValueList>
-            <UiKeyValueRow>
-              <template #key>{{ t('version') }}</template>
-              <template #value>
-                {{ `v${xoLiteVersion}` }}
-                <code v-if="xoLiteGitHead">{{ `(${xoLiteGitHead.slice(0, 5)})` }}</code>
-              </template>
-            </UiKeyValueRow>
-            <UiKeyValueRow>
-              <template #key>{{ t('news') }}</template>
-              <template #value>
-                <UiLink size="medium" href="https://xen-orchestra.com/blog/">
-                  {{ t('news-name', { name: 'Xen Orchestra' }) }}
-                </UiLink>
-              </template>
-            </UiKeyValueRow>
-            <UiKeyValueRow>
-              <template #key>{{ t('community') }}</template>
-              <template #value>
-                <UiLink size="medium" href="https://xcp-ng.org/forum/category/12/xen-orchestra">
-                  {{ t('community-name', { name: 'Xen Orchestra' }) }}
-                </UiLink>
-              </template>
-            </UiKeyValueRow>
-          </UiKeyValueList>
-        </div>
-        <div>
-          <h5>XCP-ng</h5>
-          <UiKeyValueList>
-            <UiKeyValueRow>
-              <template #key>{{ t('version') }}</template>
-              <template #value>{{ `v${xcpVersion}` }}</template>
-            </UiKeyValueRow>
-            <UiKeyValueRow>
-              <template #key>{{ t('news') }}</template>
-              <template #value>
-                <UiLink size="medium" href="https://xcp-ng.org/blog/">
-                  {{ t('news-name', { name: 'XCP-ng' }) }}
-                </UiLink>
-              </template>
-            </UiKeyValueRow>
-            <UiKeyValueRow>
-              <template #key>{{ t('community') }}</template>
-              <template #value>
-                <UiLink size="medium" href="https://xcp-ng.org/forum">
-                  {{ t('community-name', { name: 'XCP-ng' }) }}
-                </UiLink>
-              </template>
-            </UiKeyValueRow>
-            <UiKeyValueRow>
-              <template #key>{{ t('documentation') }}</template>
-              <template #value>
-                <UiLink size="medium" href="https://xcp-ng.org/docs/">
-                  {{ t('documentation-name', { name: 'XCP-ng' }) }}
-                </UiLink>
-              </template>
-            </UiKeyValueRow>
-            <UiKeyValueRow>
-              <template #key>{{ t('support') }}</template>
-              <template #value>
-                <UiLink size="medium" href="https://xcp-ng.com/">
-                  {{ t('professional-support') }}
-                </UiLink>
-              </template>
-            </UiKeyValueRow>
-          </UiKeyValueList>
-        </div>
+  <UiHeadBar icon="fa:gear">{{ t('settings') }}</UiHeadBar>
+  <UiCard class="settings-page">
+    <UiTitle>{{ t('about') }}</UiTitle>
+    <VtsColumns class="about-columns">
+      <VtsQuickInfoColumn>
+        <div class="typo-h5">Xen Orchestra Lite</div>
+        <VtsQuickInfoColumn>
+          <VtsQuickInfoRow :label="t('version')">
+            <template #value>
+              {{ `v${xoLiteVersion}` }}
+              <code v-if="xoLiteGitHead">{{ `(${xoLiteGitHead.slice(0, 5)})` }}</code>
+            </template>
+          </VtsQuickInfoRow>
+          <VtsQuickInfoRow :label="t('news')">
+            <template #value>
+              <UiLink size="medium" href="https://xen-orchestra.com/blog/">
+                {{ t('news-name', { name: 'Xen Orchestra' }) }}
+              </UiLink>
+            </template>
+          </VtsQuickInfoRow>
+          <VtsQuickInfoRow :label="t('community')">
+            <template #value>
+              <UiLink size="medium" href="https://xcp-ng.org/forum/category/12/xen-orchestra">
+                {{ t('community-name', { name: 'Xen Orchestra' }) }}
+              </UiLink>
+            </template>
+          </VtsQuickInfoRow>
+          <!-- #TODO we dont have ofical documentation for xo-lite -->
+          <!--
+ <VtsQuickInfoRow :label="t('documentation')">
+            <template #value>
+              <UiLink size="medium" href="https://github.com/vatesfr/xen-orchestra/tree/master/%40xen-orchestra/lite">
+                {{ t('documentation-name', { name: 'XO Lite' }) }}
+              </UiLink>
+            </template>
+          </VtsQuickInfoRow>
+-->
+        </VtsQuickInfoColumn>
+      </VtsQuickInfoColumn>
+      <VtsQuickInfoColumn>
+        <div class="typo-h5">XCP-ng</div>
+        <VtsQuickInfoColumn>
+          <VtsQuickInfoRow :label="t('version')">
+            <template #value>{{ `v${xcpVersion}` }}</template>
+          </VtsQuickInfoRow>
+          <VtsQuickInfoRow :label="t('news')">
+            <template #value>
+              <UiLink size="medium" href="https://xcp-ng.org/blog/">
+                {{ t('news-name', { name: 'XCP-ng' }) }}
+              </UiLink>
+            </template>
+          </VtsQuickInfoRow>
+          <VtsQuickInfoRow :label="t('community')">
+            <template #value>
+              <UiLink size="medium" href="https://xcp-ng.org/forum">
+                {{ t('community-name', { name: 'XCP-ng' }) }}
+              </UiLink>
+            </template>
+          </VtsQuickInfoRow>
+          <VtsQuickInfoRow :label="t('documentation')">
+            <template #value>
+              <UiLink size="medium" href="https://xcp-ng.org/docs/">
+                {{ t('documentation-name', { name: 'XCP-ng' }) }}
+              </UiLink>
+            </template>
+          </VtsQuickInfoRow>
+          <VtsQuickInfoRow :label="t('support')">
+            <template #value>
+              <UiLink size="medium" href="https://xcp-ng.com/">
+                {{ t('professional-support') }}
+              </UiLink>
+            </template>
+          </VtsQuickInfoRow>
+        </VtsQuickInfoColumn>
+      </VtsQuickInfoColumn>
+    </VtsColumns>
+    <UiTitle>{{ t('appearance') }}</UiTitle>
+    <div class="options">
+      <div
+        v-for="colorModeOption in colorModeOptions"
+        :key="colorModeOption"
+        class="option"
+        :class="{ selected: uiStore.colorMode === colorModeOption }"
+        @click="uiStore.colorMode = colorModeOption"
+      >
+        <img v-if="colorModeOption === 'light'" src="../assets/color-mode-light.svg" :alt="t('dark-mode.disable')" />
+        <img v-else-if="colorModeOption === 'dark'" src="../assets/color-mode-dark.svg" :alt="t('dark-mode.enable')" />
+        <img v-else src="../assets/color-mode-auto.svg" :alt="t('dark-mode.auto')" />
+        <span>
+          <VtsIcon v-if="uiStore.colorMode === colorModeOption" name="fa:check" size="medium" />
+          {{ t(`theme-${colorModeOption}`) }}
+        </span>
       </div>
-    </UiCard>
-    <UiCard class="group appearance">
-      <UiCardTitle>{{ t('appearance') }}</UiCardTitle>
-      <div class="options">
-        <div
-          v-for="colorModeOption in colorModeOptions"
-          :key="colorModeOption"
-          class="option"
-          :class="{ selected: uiStore.colorMode === colorModeOption }"
-          @click="uiStore.colorMode = colorModeOption"
-        >
-          <img v-if="colorModeOption === 'light'" src="../assets/color-mode-light.svg" :alt="t('dark-mode.disable')" />
-          <img
-            v-else-if="colorModeOption === 'dark'"
-            src="../assets/color-mode-dark.svg"
-            :alt="t('dark-mode.enable')"
-          />
-          <img v-else src="../assets/color-mode-auto.svg" :alt="t('dark-mode.auto')" />
-          <span>
-            <VtsIcon v-if="uiStore.colorMode === colorModeOption" name="fa:check" size="medium" />
-            {{ t(`theme-${colorModeOption}`) }}
-          </span>
-        </div>
-      </div>
-    </UiCard>
-    <UiCard class="group">
-      <UiCardTitle>{{ t('language') }}</UiCardTitle>
-      <VtsColumns class="i18n-columns">
-        <VtsColumn>
-          <VtsSelect :id="localeSelectId" icon="fa:earth-americas" accent="brand" />
-        </VtsColumn>
-        <VtsColumn class="i18n-link-column" :size="2">
-          <UiLink size="medium" href="https://translate.vates.tech/engage/xen-orchestra/">
-            {{ t('settings.missing-translations') }}
-          </UiLink>
-        </VtsColumn>
-      </VtsColumns>
-    </UiCard>
-  </div>
+    </div>
+    <UiTitle>{{ t('language') }}</UiTitle>
+    <!-- for regular spacing using VtsQuickInfoRow even label template is not used for this -->
+    <VtsQuickInfoRow>
+      <template #label>
+        <VtsSelect :id="localeSelectId" icon="fa:earth-americas" accent="brand" />
+      </template>
+      <template #value>
+        <UiLink size="medium" href="https://translate.vates.tech/engage/xen-orchestra/">
+          {{ t('settings.missing-translations') }}
+        </UiLink>
+      </template>
+    </VtsQuickInfoRow>
+    <UiInfo accent="info">{{ t('untranslated-text-helper') }}</UiInfo>
+  </UiCard>
 </template>
 
 <script lang="ts" setup>
-import TitleBar from '@/components/TitleBar.vue'
-import UiCard from '@/components/ui/UiCard.vue'
-import UiCardTitle from '@/components/ui/UiCardTitle.vue'
-import UiKeyValueList from '@/components/ui/UiKeyValueList.vue'
-import UiKeyValueRow from '@/components/ui/UiKeyValueRow.vue'
 import { usePageTitleStore } from '@/stores/page-title.store.ts'
 import { useHostStore } from '@/stores/xen-api/host.store.ts'
 import { usePoolStore } from '@/stores/xen-api/pool.store.ts'
-import VtsColumn from '@core/components/column/VtsColumn.vue'
 import VtsColumns from '@core/components/columns/VtsColumns.vue'
 import VtsIcon from '@core/components/icon/VtsIcon.vue'
+import VtsQuickInfoColumn from '@core/components/quick-info-column/VtsQuickInfoColumn.vue'
+import VtsQuickInfoRow from '@core/components/quick-info-row/VtsQuickInfoRow.vue'
 import VtsSelect from '@core/components/select/VtsSelect.vue'
+import UiCard from '@core/components/ui/card/UiCard.vue'
+import UiHeadBar from '@core/components/ui/head-bar/UiHeadBar.vue'
+import UiInfo from '@core/components/ui/info/UiInfo.vue'
 import UiLink from '@core/components/ui/link/UiLink.vue'
+import UiTitle from '@core/components/ui/title/UiTitle.vue'
 import { locales } from '@core/i18n.ts'
 import { useFormSelect } from '@core/packages/form-select'
 import { useUiStore } from '@core/stores/ui.store.ts'
@@ -164,64 +158,41 @@ const { id: localeSelectId } = useFormSelect(availableLocales, {
 </script>
 
 <style lang="postcss" scoped>
-.card-view {
-  flex-direction: column;
-}
+.settings-page {
+  margin: 0.8rem;
 
-h5 {
-  font-size: 1.6rem;
-  font-weight: 700;
-  margin-bottom: 0.7em;
-}
-
-.about .sections {
-  display: flex;
-  gap: 2em;
-
-  div {
-    flex-grow: 1;
+  .about-columns {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(25rem, 1fr));
+    gap: 2.4rem;
   }
 
-  @media (--mobile) {
-    flex-direction: column;
-  }
-}
-
-.appearance .options {
-  display: flex;
-  gap: 25px;
-
-  .option {
+  .options {
     display: flex;
-    flex-direction: column;
-    gap: 1.6em;
+    gap: 25px;
 
-    &.selected {
-      color: var(--color-brand-txt-base);
+    .option {
+      display: flex;
+      flex-direction: column;
+      gap: 1.6em;
+
+      &.selected {
+        color: var(--color-brand-txt-base);
+
+        img {
+          outline: solid 2px var(--color-brand-txt-base);
+        }
+      }
+
+      &:not(.selected) {
+        cursor: pointer;
+      }
 
       img {
-        outline: solid 2px var(--color-brand-txt-base);
+        box-shadow: var(--shadow-100);
+        border-radius: 8px;
       }
     }
-
-    &:not(.selected) {
-      cursor: pointer;
-    }
-
-    img {
-      box-shadow: var(--shadow-100);
-      border-radius: 8px;
-    }
-  }
-}
-
-.i18n-columns {
-  padding-inline: 0;
-  gap: 2rem;
-
-  .i18n-link-column {
-    flex: 2;
-    justify-content: center;
   }
 }
 </style>
