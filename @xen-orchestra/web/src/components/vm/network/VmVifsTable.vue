885a1ad364b8e1af56aeb65aeeb1352bfae61406
Thierry <thierry@byscripts.info>
2025-10-03 21:10:44 +0200

    add missing conversions

--- @xen-orchestra/web/src/components/vm/network/VmVifsTable.vue
+++ @xen-orchestra/web/src/components/vm/network/VmVifsTable.vue
@@ -103,7 +103,7 @@
                 size="small"
               />
               <div v-else-if="column.id === 'status'" v-tooltip>
-                <VtsConnectionStatus :status="column.value" />
+                <VtsStatus :status="column.value" />
               </div>
               <div v-else-if="column.id === 'network'" class="network">
                 <!-- TODO Remove the span when the link works and the icon is fixed -->
@@ -145,10 +145,10 @@ import { useXoVifCollection } from '@/remote-resources/use-xo-vif-collection.ts'
 import { useXoVmCollection } from '@/remote-resources/use-xo-vm-collection.ts'
 import type { XoVif } from '@/types/xo/vif.type'
 import type { IconName } from '@core/icons'
-import VtsConnectionStatus from '@core/components/connection-status/VtsConnectionStatus.vue'
 import VtsDataTable from '@core/components/data-table/VtsDataTable.vue'
 import VtsIcon from '@core/components/icon/VtsIcon.vue'
 import VtsStateHero from '@core/components/state-hero/VtsStateHero.vue'
+import VtsStatus from '@core/components/status/VtsStatus.vue'
 import UiButton from '@core/components/ui/button/UiButton.vue'
 import UiButtonIcon from '@core/components/ui/button-icon/UiButtonIcon.vue'
 import UiCheckbox from '@core/components/ui/checkbox/UiCheckbox.vue'
