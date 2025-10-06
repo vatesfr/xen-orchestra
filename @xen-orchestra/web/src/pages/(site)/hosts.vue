15670f57980924640d8c07f46f817b8dd7fe6ee2
unel <enee.prog@gmail.com>
2025-10-06 11:19:09 +0200

    feat(xo6/site-host-tab-pages): create host-tab

---
+++ @xen-orchestra/web/src/pages/(site)/hosts.vue
@@ -0,0 +1,17 @@
+<template>
+  <UiCard>
+    <UiTitle>{{ t('hosts') }}</UiTitle>
+    <HostsTable :hosts />
+  </UiCard>
+</template>
+
+<script setup lang="ts">
+import HostsTable from '@/components/host/hostsTable.vue'
+import { useXoHostCollection } from '@/remote-resources/use-xo-host-collection'
+import UiCard from '@core/components/ui/card/UiCard.vue'
+import UiTitle from '@core/components/ui/title/UiTitle.vue'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+const { hosts } = useXoHostCollection()
+</script>
