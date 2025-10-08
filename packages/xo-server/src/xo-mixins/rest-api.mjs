2e4c45b207cef6d0df8f6b462779993eba3e9872
MlssFrncJrg <119158464+MelissaFrncJrg@users.noreply.github.com>
2025-10-08 14:49:44 +0200

    fix(xo-server/rest-api): fix indentation (#9081)

--- packages/xo-server/src/xo-mixins/rest-api.mjs
+++ packages/xo-server/src/xo-mixins/rest-api.mjs
@@ -346,7 +346,7 @@ export default class RestApi {
       servers: {
         routes: {
           tasks: true,
-        }
+        },
       },
       tasks: {},
     }
@@ -820,20 +820,18 @@ export default class RestApi {
         )
       )
 
-    api
-      .get(
-        '/restore',
-        wrap((req, res) => sendObjects([{ id: 'logs' }], req, res))
-      )
-    api
-      .get(
-        '/tasks/:id/actions',
-        wrap(async (req, res) => {
-          const task = await app.tasks.get(req.params.id)
+    api.get(
+      '/restore',
+      wrap((req, res) => sendObjects([{ id: 'logs' }], req, res))
+    )
+    api.get(
+      '/tasks/:id/actions',
+      wrap(async (req, res) => {
+        const task = await app.tasks.get(req.params.id)
 
-          await sendObjects(task.status === 'pending' ? [{ id: 'abort' }] : [], req, res)
-        })
-      )
+        await sendObjects(task.status === 'pending' ? [{ id: 'abort' }] : [], req, res)
+      })
+    )
 
     api.get(
       '/:collection',
