7b4589c3a90479237bf7cdec15845c9103b8adc8
Olivier Floch <olivier.floch@vates.fr>
2025-10-14 09:23:59 +0200

    feat(xo-6/pool): add storage repositories side panel

---
+++ @xen-orchestra/web/src/components/storage-repositories/panel/cards/StorageRepositoryHostsCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <UiCard class="card-container">
+    <UiCardTitle>
+      {{ t('hosts') }}
+      <UiCounter :value="hosts.length" accent="neutral" size="small" variant="primary" />
+    </UiCardTitle>
+    <UiCollapsibleList tag="ul" :total-items="hosts.length">
+      <li v-for="host in hosts" :key="host.id">
+        <UiLink size="small" icon="fa:server" :to="`/host/${host.id}`">
+          {{ host.name_label }}
+        </UiLink>
+      </li>
+    </UiCollapsibleList>
+  </UiCard>
+</template>
+
+<script lang="ts" setup>
+import type { XoHost } from '@/types/xo/host.type'
+import UiCard from '@core/components/ui/card/UiCard.vue'
+import UiCardTitle from '@core/components/ui/card-title/UiCardTitle.vue'
+import UiCollapsibleList from '@core/components/ui/collapsible-list/UiCollapsibleList.vue'
+import UiCounter from '@core/components/ui/counter/UiCounter.vue'
+import UiLink from '@core/components/ui/link/UiLink.vue'
+import { useI18n } from 'vue-i18n'
+
+const { hosts } = defineProps<{
+  hosts: XoHost[]
+}>()
+
+const { t } = useI18n()
+</script>
+
+<style scoped lang="postcss">
+.card-container {
+  gap: 1.6rem;
+}
+</style>
