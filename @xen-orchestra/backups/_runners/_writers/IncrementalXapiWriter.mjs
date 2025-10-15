8eb41ce6dd2394de5164ed5c3a15b96cddd7f976
Florent BEAUCHAMP <florent.beauchamp@vates.tech>
2025-10-15 15:02:55 +0000

    fix(backups): force  target managed  vdi for replication

--- @xen-orchestra/backups/_runners/_writers/IncrementalXapiWriter.mjs
+++ @xen-orchestra/backups/_runners/_writers/IncrementalXapiWriter.mjs
@@ -112,7 +112,7 @@ export class IncrementalXapiWriter extends MixinXapiWriter(AbstractIncrementalWr
 
     const replicatedVdis = sr.$VDIs.filter(vdi => {
       // REPLICATED_TO_SR_UUID is not used here since we are already filtering from sr.$VDIs
-      return sourceVdiUuids.includes(vdi?.other_config[COPY_OF])
+      return vdi?.managed && !vdi?.is_a_snapshot && sourceVdiUuids.includes(vdi?.other_config[COPY_OF])
     })
 
     Object.values(backup.vdis).forEach(vdi => {
