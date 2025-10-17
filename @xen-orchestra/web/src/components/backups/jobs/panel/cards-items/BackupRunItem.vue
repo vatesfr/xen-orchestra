4a67efe1f6622619a17b6f681bf5ef1f06bfffea
Joris-K <joris.kosacki@vates.tech>
2025-10-17 10:14:43 +0200

    fix(xo6): fix formatted date in backup jobs

--- @xen-orchestra/web/src/components/backups/jobs/panel/cards-items/BackupRunItem.vue
+++ @xen-orchestra/web/src/components/backups/jobs/panel/cards-items/BackupRunItem.vue
@@ -65,13 +65,13 @@ const { backupRun } = defineProps<{
   backupRun: XoBackupLog
 }>()
 
-const { t } = useI18n()
+const { t, d } = useI18n()
 
 const { schedules } = useXoScheduleCollection()
 
 const runDate = computed(() => backupRun.end ?? backupRun.start)
 
-const formattedRunDate = computed(() => new Date(runDate.value).toLocaleString())
+const formattedRunDate = computed(() => d(runDate.value, 'datetime_short'))
 
 const logContent = computed(() => {
   if (backupRun.status !== 'success' && backupRun.status !== 'pending' && backupRun.tasks && backupRun.tasks[0]) {
