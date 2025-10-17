40b1b1502a25677ae5714d9a3fb63518eb1dfbbb
unel <enee.prog@gmail.com>
2025-10-17 17:04:10 +0200

    feat(xo6/site-host-tab-pages): create hostSoftwareCard

---
+++ @xen-orchestra/web/src/components/hosts/card/HostSoftwareCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <UiCard>
+    <UiCardTitle>
+      {{ t('software') }}
+    </UiCardTitle>
+    <div class="content" />
+    <VtsCardRowKeyValue>
+      <template #key>{{ t('version') }}</template>
+      <template #value>{{ host.version }}</template>
+      <template #addons>
+        <VtsCopyButton :value="host.version" />
+      </template>
+    </VtsCardRowKeyValue>
+  </UiCard>
+</template>
+
+<script lang="ts" setup>
+import type { XoHost } from '@/types/xo/host.type'
+import VtsCardRowKeyValue from '@core/components/card/VtsCardRowKeyValue.vue'
+import VtsCopyButton from '@core/components/copy-button/VtsCopyButton.vue'
+import UiCard from '@core/components/ui/card/UiCard.vue'
+import UiCardTitle from '@core/components/ui/card-title/UiCardTitle.vue'
+import { useI18n } from 'vue-i18n'
+
+const { host } = defineProps<{
+  host: XoHost
+}>()
+
+const { t } = useI18n()
+</script>
+
+<style scoped lang="postcss">
+.content {
+  display: flex;
+  flex-direction: column;
+  gap: 0.4rem;
+}
+</style>
