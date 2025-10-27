dbab288c08d14fcbc5c579b9bf44004b464ca87c
Joris-K <joris.kosacki@vates.tech>
2025-10-27 10:11:07 +0100

    feat(xo6/site): implement vms page

--- @xen-orchestra/web/src/components/vms/panel/cards/VmInfoCard.vue
+++ @xen-orchestra/web/src/components/vms/panel/cards/VmInfoCard.vue
@@ -205,6 +205,8 @@ const { powerState, installDateFormatted, relativeStartTime, guestToolsDisplay }
 
 <style scoped lang="postcss">
 .card-container {
+  display: flex;
+  flex-direction: column;
   gap: 1.6rem;
 
   .content {
