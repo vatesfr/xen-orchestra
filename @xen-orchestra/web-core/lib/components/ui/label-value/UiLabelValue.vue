721ac7d4fde2f9c11c8945e30244abfe690931eb
unel <enee.prog@gmail.com>
2025-10-28 14:55:36 +0100

    fix(xo6/uiLabelValue): fix size of value for responsive

--- @xen-orchestra/web-core/lib/components/ui/label-value/UiLabelValue.vue
+++ @xen-orchestra/web-core/lib/components/ui/label-value/UiLabelValue.vue
@@ -1,14 +1,19 @@
 <!-- v3 -->
 <template>
   <div class="ui-label-value typo-body-regular">
-    <div class="label">
-      {{ label }}
-    </div>
-    <div class="value">
-      <div class="valueActionWrapper">
+    <div class="ui-label-value-grid-container">
+      <div class="label">
+        {{ label }}
+      </div>
+      <div class="value">
         <div class="valueAddonWraper">
           <slot name="value">
-            <div v-if="value && !Array.isArray(value)" v-tooltip="ellipsis" :class="{ 'text-ellipsis': ellipsis }">
+            <div
+              v-if="value && !Array.isArray(value)"
+              v-tooltip="ellipsis"
+              :class="ellipsis == true ? 'text-ellipsis' : 'text-wrap'"
+              class="valueText"
+            >
               {{ value }}
             </div>
             <UiTagsList
@@ -61,74 +66,74 @@ const slots = defineSlots<{
 
 <style lang="postcss" scoped>
 .ui-label-value {
-  display: grid;
-  column-gap: 1.6rem;
-  grid-template-columns: minmax(auto, 20rem) 20rem;
-  row-gap: 0.8rem;
   container-type: inline-size;
   container-name: card;
+}
 
-  @container card (max-width: 32rem) {
-    .label {
-      grid-column: 1;
-      grid-row: 1;
-    }
+.ui-label-value-grid-container {
+  display: grid;
+}
 
-    .value {
-      grid-column: 1;
-      grid-row: 2;
-      width: 100cqi;
-    }
-  }
-
-  @container card (min-width: 32rem) {
-    .label {
-      grid-column: 1;
-      grid-row: 1;
-    }
-    .value {
-      grid-column: 2;
-      grid-row: 1;
-      align-items: left;
-    }
+@container card (max-width: 32rem) {
+  .ui-label-value-grid-container {
+    grid-template-columns: 1fr;
   }
 
   .label {
-    overflow-wrap: break-word;
-    color: var(--color-neutral-txt-secondary);
+    grid-column: 1;
+    grid-row: 1;
+    width: 100cqi;
   }
 
   .value {
-    overflow-wrap: anywhere;
-    min-width: 0;
-    color: var(--color-neutral-txt-primary);
-    display: flex;
+    grid-column: 1;
+    grid-row: 2;
+    width: 100cqi;
+    border: 1px solid lime;
+  }
+}
+
+@container card (min-width: 32rem) {
+  .ui-label-value-grid-container {
+    grid-template-columns: 20rem calc(100% - 20rem);
+  }
+
+  .label {
+    grid-column: 1;
+    grid-row: 1;
+  }
+  .value {
+    grid-column: 2;
+    grid-row: 1;
     align-items: left;
-    gap: 0.8rem;
+    border: 1px solid red;
+    width: 100%;
   }
+}
 
-  .valueAddonWraper:empty::before {
-    content: '-';
-  }
-
-  .valueActionWrapper {
+.ui-label-value-grid-container {
+  .value {
     display: flex;
+    flex-direction: row;
     justify-content: space-between;
-    width: 100%;
+    .valueText {
+      width: 100%;
+      .text-wrap {
+        text-wrap: wrap;
+        overflow-wrap: anywhere;
+      }
+    }
   }
+}
 
-  .valueAddonWraper {
-    display: flex;
-    gap: 0.8rem;
-    width: 100%;
-  }
+.valueAddonWraper {
+  display: flex;
+  align-items: center;
+  overflow: hidden;
+  white-space: nowrap;
+}
 
-  .actions {
-    display: flex;
-    gap: 0.8rem;
-    margin-inline-start: auto;
-    font-size: 1.6rem;
-    align-items: flex-start;
-  }
+.valueAddonWraper:empty::before {
+  content: '-';
 }
 </style>
