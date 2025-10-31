vatesfr/xen-orchestra

commit e547447bb4151ddf48f4366ea3c8e2273e00e0d9

feat(xo-6/backups): add backup configuration page (#9008)

@xen-orchestra/web/src/components/backups/configuration/BackupJobSettings.vue
@@ -1,0 +1,95 @@
+<template>
+  <UiCard>
+    <UiTitle>{{ t('settings') }}</UiTitle>
+    <VtsColumns>
+      <VtsColumn>
+        <VtsQuickInfoRow :label="t('proxy')" :value="proxy?.name" />
+        <VtsQuickInfoRow :label="t('snapshot-mode')">
+          <template v-if="backupJob.mode" #value>
+            <UiTag variant="secondary" accent="info">{{ t(snapshotModeTranslation) }}</UiTag>
+          </template>
+        </VtsQuickInfoRow>
+        <VtsQuickInfoRow
+          :label="t('vm-backup-failure-number-of-retries')"
+          :value="
+            settings.nRetriesVmBackupFailures !== undefined ? String(settings.nRetriesVmBackupFailures) : undefined
+          "
+        />
+        <VtsQuickInfoRow :label="t('timeout')" :value="formattedTimeout" />
+      </VtsColumn>
+      <VtsColumn>
+        <VtsQuickInfoRow
+          :label="t('speed-limit')"
+          :value="maxExportRate ? `${maxExportRate.value} ${maxExportRate.prefix}` : undefined"
+        />
+        <VtsQuickInfoRow :label="t('report-when')" :value="reportWhenValueTranslation" />
+        <VtsQuickInfoRow :label="t('report-recipients')">
+          <template #value>
+            <UiTagsList v-if="settings.reportRecipients">
+              <UiTag
+                v-for="(recipient, index) in settings.reportRecipients"
+                :key="index"
+                variant="secondary"
+                accent="info"
+              >
+                {{ recipient }}
+              </UiTag>
+            </UiTagsList>
+          </template>
+        </VtsQuickInfoRow>
+        <VtsQuickInfoRow
+          :label="t('concurrency')"
+          :value="settings.concurrency !== undefined ? String(settings.concurrency) : undefined"
+        />
+      </VtsColumn>
+      <VtsColumn>
+        <VtsQuickInfoRow :label="t('compression')" :value="compression" />
+        <VtsQuickInfoRow :label="t('offline-backup')">
+          <template #value>
+            <VtsEnabledState :enabled="!!settings.offlineBackup" />
+          </template>
+        </VtsQuickInfoRow>
+        <VtsQuickInfoRow :label="t('shorter-backup-reports')">
+          <template #value>
+            <VtsEnabledState :enabled="settings.backupReportTpl ? settings.backupReportTpl === 'compactMjml' : false" />
+          </template>
+        </VtsQuickInfoRow>
+        <VtsQuickInfoRow :label="t('merge-backups-synchronously')">
+          <template #value>
+            <VtsEnabledState :enabled="!!settings.mergeBackupsSynchronously" />
+          </template>
+        </VtsQuickInfoRow>
+      </VtsColumn>
+    </VtsColumns>
+  </UiCard>
+</template>
+
+<script setup lang="ts">
+import { useXoBackupJobSettingsUtils } from '@/composables/xo-backup-job-settings.composable'
+import type { XoVmBackupJob } from '@/types/xo/vm-backup-job.type'
+import VtsColumn from '@core/components/column/VtsColumn.vue'
+import VtsColumns from '@core/components/columns/VtsColumns.vue'
+import VtsEnabledState from '@core/components/enabled-state/VtsEnabledState.vue'
+import VtsQuickInfoRow from '@core/components/quick-info-row/VtsQuickInfoRow.vue'
+import UiCard from '@core/components/ui/card/UiCard.vue'
+import UiTag from '@core/components/ui/tag/UiTag.vue'
+import UiTagsList from '@core/components/ui/tag/UiTagsList.vue'
+import UiTitle from '@core/components/ui/title/UiTitle.vue'
+import { useI18n } from 'vue-i18n'
+
+const { backupJob } = defineProps<{
+  backupJob: XoVmBackupJob
+}>()
+
+const { t } = useI18n()
+
+const {
+  maxExportRate,
+  formattedTimeout,
+  compression,
+  proxy,
+  settings,
+  reportWhenValueTranslation,
+  snapshotModeTranslation,
+} = useXoBackupJobSettingsUtils(backupJob)
+</script>

@xen-orchestra/web/src/components/backups/jobs/panel/cards/BackupJobSettingsCard.vue
@@ -106,114 +106,31 @@
 </template>
 
 <script lang="ts" setup>
+import { useXoBackupJobSettingsUtils } from '@/composables/xo-backup-job-settings.composable'
 import type { XoBackupJob } from '@/remote-resources/use-xo-backup-job-collection.ts'
-import { useXoProxyCollection } from '@/remote-resources/use-xo-proxy-collection.ts'
 import VtsCardRowKeyValue from '@core/components/card/VtsCardRowKeyValue.vue'
 import VtsStatus from '@core/components/status/VtsStatus.vue'
 import UiCard from '@core/components/ui/card/UiCard.vue'
 import UiCardTitle from '@core/components/ui/card-title/UiCardTitle.vue'
 import UiLogEntryViewer from '@core/components/ui/log-entry-viewer/UiLogEntryViewer.vue'
-import { useMapper } from '@core/packages/mapper'
-import { formatSpeedRaw } from '@core/utils/speed.util.ts'
-import { formatTimeout } from '@core/utils/time.util.ts'
-import { reactiveComputed } from '@vueuse/shared'
-import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 
 const { backupJob } = defineProps<{
   backupJob: XoBackupJob
 }>()
 
-const { t, locale } = useI18n()
+const { t } = useI18n()
 
-const { useGetProxyById } = useXoProxyCollection()
-
-type ReportWhen = 'always' | 'failure' | 'error' | 'never'
-
-const settings = reactiveComputed(() => {
-  if (!backupJob.settings['']) {
-    return {}
-  }
-
-  const {
-    preferNbd,
-    cbtDestroySnapshotData,
-    concurrency,
-    nbdConcurrency,
-    maxExportRate,
-    nRetriesVmBackupFailures,
-    hideSuccessfulItems,
-    backupReportTpl,
-    reportWhen,
-    timeout,
-    checkpointSnapshot,
-    offlineBackup,
-    offlineSnapshot,
-    mergeBackupsSynchronously,
-    timezone,
-    reportRecipients,
-    ...other
-  } = backupJob.settings['']
-
-  return {
-    compression: backupJob.compression,
-    proxy: backupJob.proxy,
-    preferNbd,
-    cbtDestroySnapshotData,
-    concurrency,
-    nbdConcurrency,
-    maxExportRate,
-    nRetriesVmBackupFailures,
-    hideSuccessfulItems,
-    backupReportTpl,
-    reportWhen,
-    timeout,
-    checkpointSnapshot,
-    offlineBackup,
-    offlineSnapshot,
-    mergeBackupsSynchronously,
-    timezone,
-    reportRecipients: reportRecipients as string[],
-    other,
-  }
-})
-
-const proxy = useGetProxyById(() => settings.proxy)
-
-const reportWhenValueTranslation = useMapper<ReportWhen, string>(
-  () => settings.reportWhen as ReportWhen | undefined,
-  {
-    always: t('report-when.always'),
-    failure: t('report-when.skipped-and-failure'),
-    error: t('report-when.error'),
-    never: t('report-when.never'),
-  },
-  'never'
-)
-
-const nbdConcurrency = computed(() => (settings.preferNbd ? (settings.nbdConcurrency ?? 1) : undefined))
-
-const cbtDestroySnapshotData = computed(() =>
-  !!settings.preferNbd && settings.cbtDestroySnapshotData !== undefined ? settings.cbtDestroySnapshotData : undefined
-)
-
-const maxExportRate = computed(() => (settings.maxExportRate ? formatSpeedRaw(settings.maxExportRate) : undefined))
-
-const formattedTimeout = computed(() =>
-  settings.timeout !== undefined ? formatTimeout(Number(settings.timeout), locale.value) : undefined
-)
-
-const compression = computed(() => {
-  if (backupJob.compression === undefined) {
-    return undefined
-  }
-
-  if (backupJob.compression === 'native') {
-    return 'GZIP'
-  }
-
-  return backupJob.compression
-})
+const {
+  cbtDestroySnapshotData,
+  compression,
+  formattedTimeout,
+  maxExportRate,
+  nbdConcurrency,
+  proxy,
+  reportWhenValueTranslation,
+  settings,
+} = useXoBackupJobSettingsUtils(backupJob)
 </script>
 
 <style scoped lang="postcss">

@xen-orchestra/lite/typed-router.d.ts
@@ -65,7 +65,6 @@ declare module 'vue-router/auto-routes' {
     '/story/web-core/select/vts-select': RouteRecordInfo<'/story/web-core/select/vts-select', '/story/web-core/select/vts-select', Record<never, never>, Record<never, never>>,
     '/story/web-core/stacked-bar-with-legend/vts-stacked-bar-with-legend': RouteRecordInfo<'/story/web-core/stacked-bar-with-legend/vts-stacked-bar-with-legend', '/story/web-core/stacked-bar-with-legend/vts-stacked-bar-with-legend', Record<never, never>, Record<never, never>>,
     '/story/web-core/state-hero/vts-state-hero': RouteRecordInfo<'/story/web-core/state-hero/vts-state-hero', '/story/web-core/state-hero/vts-state-hero', Record<never, never>, Record<never, never>>,
-    '/story/web-core/status/vts-status': RouteRecordInfo<'/story/web-core/status/vts-status', '/story/web-core/status/vts-status', Record<never, never>, Record<never, never>>,
     '/story/web-core/tab/router-tab': RouteRecordInfo<'/story/web-core/tab/router-tab', '/story/web-core/tab/router-tab', Record<never, never>, Record<never, never>>,
     '/story/web-core/tab/tab-item': RouteRecordInfo<'/story/web-core/tab/tab-item', '/story/web-core/tab/tab-item', Record<never, never>, Record<never, never>>,
     '/story/web-core/tab/tab-list': RouteRecordInfo<'/story/web-core/tab/tab-list', '/story/web-core/tab/tab-list', Record<never, never>, Record<never, never>>,

@xen-orchestra/web-core/lib/icons/fa-icons.ts
@@ -1,11 +1,11 @@
 import { defineIconPack } from '@core/packages/icon/define-icon-pack.ts'
 import {
   faBuilding,
+  faCopy,
   faFile,
   faFolderClosed,
   faFolderOpen,
   faSquareCheck,
-  faCopy,
 } from '@fortawesome/free-regular-svg-icons'
 import {
   faA,
@@ -111,6 +111,7 @@ import {
 } from '@fortawesome/free-solid-svg-icons'
 
 export const faIcons = defineIconPack({
+  a: { icon: faA },
   'align-left': { icon: faAlignLeft },
   'angle-double-left': { icon: faAngleDoubleLeft },
   'angle-double-right': { icon: faAngleDoubleRight },
@@ -129,6 +130,7 @@ export const faIcons = defineIconPack({
   book: { icon: faBook },
   building: { icon: faBuilding },
   camera: { icon: faCamera },
+  calendar: { icon: faCalendar },
   'caret-down': { icon: faCaretDown },
   'caret-up': { icon: faCaretUp },
   check: { icon: faCheck },
@@ -144,6 +146,7 @@ export const faIcons = defineIconPack({
   'circle-xmark': { icon: faCircleXmark },
   city: { icon: faCity },
   close: { icon: faClose },
+  clock: { icon: faClock },
   code: { icon: faCode },
   comments: { icon: faComments },
   copy: { icon: faCopy },
@@ -188,7 +191,6 @@ export const faIcons = defineIconPack({
   minus: { icon: faMinus },
   moon: { icon: faMoon },
   'network-wired': { icon: faNetworkWired },
-  object: { icon: faA },
   pause: { icon: faPause },
   pencil: { icon: faPencil },
   play: { icon: faPlay },

@xen-orchestra/web/src/components/backups/BackupHeader.vue
@@ -11,7 +11,11 @@
         {{ t('runs') }}
       </TabItem>
     </RouterLink>
-    <TabItem disabled>{{ t('configuration') }}</TabItem>
+    <RouterLink v-slot="{ isActive, href }" :to="`/backup/${backupJob.id}/configuration`" custom>
+      <TabItem :active="isActive" :href tag="a">
+        {{ t('configuration') }}
+      </TabItem>
+    </RouterLink>
     <RouterLink v-slot="{ isActive, href }" :to="`/backup/${backupJob.id}/backed-up-vms`" custom>
       <TabItem :active="isActive" :href tag="a" :disabled="backupJob.type !== 'backup'">
         {{ t('backed-up-vms') }}

@xen-orchestra/web/src/components/backups/backed-up-vms/BackedUpVmsTable.vue
@@ -138,7 +138,7 @@ const { visibleColumns, rows } = useTable('backed-up-vms', filteredBackedUpVms, 
 type BackupJobHeader = 'vm' | 'disk-size'
 
 const headerIcon: Record<BackupJobHeader, IconName> = {
-  vm: 'fa:object',
+  vm: 'fa:a',
   'disk-size': 'fa:hashtag',
 }
 

@xen-orchestra/web/src/components/backups/configuration/BackupJobGeneralInformation.vue
@@ -1,0 +1,48 @@
+<template>
+  <UiCard>
+    <UiTitle>{{ t('general-information') }}</UiTitle>
+    <VtsColumns>
+      <VtsColumn>
+        <VtsQuickInfoRow :label="t('name')" :value="backupJob.name" />
+      </VtsColumn>
+      <VtsColumn>
+        <VtsQuickInfoRow :label="t('id')" :value="backupJob.id" />
+      </VtsColumn>
+      <VtsColumn>
+        <VtsQuickInfoRow :label="t('mode')">
+          <template v-if="modeLabels.length > 0" #value>
+            <UiTagsList>
+              <UiTag v-for="label in modeLabels" :key="label" variant="secondary" accent="info">
+                {{ label }}
+              </UiTag>
+            </UiTagsList>
+          </template>
+        </VtsQuickInfoRow>
+      </VtsColumn>
+    </VtsColumns>
+  </UiCard>
+</template>
+
+<script setup lang="ts">
+import { useXoBackupUtils } from '@/composables/xo-backup-utils.composable'
+import type { XoVmBackupJob } from '@/types/xo/vm-backup-job.type'
+import VtsColumn from '@core/components/column/VtsColumn.vue'
+import VtsColumns from '@core/components/columns/VtsColumns.vue'
+import VtsQuickInfoRow from '@core/components/quick-info-row/VtsQuickInfoRow.vue'
+import UiCard from '@core/components/ui/card/UiCard.vue'
+import UiTag from '@core/components/ui/tag/UiTag.vue'
+import UiTagsList from '@core/components/ui/tag/UiTagsList.vue'
+import UiTitle from '@core/components/ui/title/UiTitle.vue'
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+const { backupJob } = defineProps<{
+  backupJob: XoVmBackupJob
+}>()
+
+const { t } = useI18n()
+
+const { getModeLabels } = useXoBackupUtils()
+
+const modeLabels = computed(() => getModeLabels(backupJob))
+</script>

@xen-orchestra/web/src/components/backups/configuration/BackupJobSchedulesTable.vue
@@ -1,8 +1,6 @@
 <template>
-  <div class="backup-jobs-table">
-    <UiTitle>
-      {{ t('backup-jobs') }}
-    </UiTitle>
+  <UiCard class="backup-job-schedules-table">
+    <UiTitle>{{ t('schedules') }}</UiTitle>
     <div class="container">
       <div class="table-actions">
         <UiQuerySearchBar @search="value => (searchQuery = value)" />
@@ -13,7 +11,7 @@
       <VtsDataTable
         is-ready
         :has-error
-        :no-data-message="backupJobs.length === 0 ? t('no-backup-available') : undefined"
+        :no-data-message="backupJobSchedules.length === 0 ? t('no-backup-available') : undefined"
       >
         <template #thead>
           <tr>
@@ -27,7 +25,7 @@
                 <UiButtonIcon v-tooltip="t('coming-soon')" icon="fa:ellipsis" accent="brand" disabled size="small" />
               </th>
               <th v-else>
-                <div v-tooltip class="text-ellipsis">
+                <div v-tooltip>
                   <VtsIcon size="medium" :name="headerIcon[column.id]" />
                   {{ column.label }}
                 </div>
@@ -37,17 +35,11 @@
         </template>
         <template #tbody>
           <tr
-            v-for="row of backupJobsRecords"
+            v-for="row of backupJobSchedulesRecords"
             :key="row.id"
             :class="{ selected: selectedBackupJobId === row.id }"
-            @click="selectedBackupJobId = row.id"
           >
-            <td
-              v-for="column of row.visibleColumns"
-              :key="column.id"
-              class="typo-body-regular-small"
-              :class="{ checkbox: column.id === 'checkbox' }"
-            >
+            <td v-for="column of row.visibleColumns" :key="column.id" :class="{ checkbox: column.id === 'checkbox' }">
               <div v-if="column.id === 'checkbox'" v-tooltip="t('coming-soon')">
                 <UiCheckbox disabled accent="brand" :value="row.id" />
               </div>
@@ -59,18 +51,14 @@
                 disabled
                 size="small"
               />
-              <div v-else-if="column.id === 'job-name'">
-                <UiLink size="medium" icon="object:backup-job" :to="`/backup/${row.id}/runs`" @click.stop>
-                  {{ column.value }}
+              <div v-else-if="column.id === 'schedule'">
+                <UiLink size="medium" icon="object:backup-job" :href="`/#/backup/${column.value.jobId}/edit`">
+                  {{ column.value.name }}
                 </UiLink>
               </div>
-              <UiTagsList v-else-if="column.id === 'mode'">
-                <template v-for="(backupMode, index) in column.value" :key="index">
-                  <UiTag v-if="backupMode !== undefined" variant="secondary" accent="info" class="mode">
-                    {{ backupMode }}
-                  </UiTag>
-                </template>
-              </UiTagsList>
+              <template v-else-if="column.id === 'status'">
+                <VtsEnabledState :enabled="column.value ?? false" />
+              </template>
               <template v-else-if="column.id === 'last-runs'">
                 <ul class="last-three-runs">
                   <li v-for="(status, index) in column.value" :key="index" v-tooltip="status.tooltip">
@@ -79,108 +67,118 @@
                   </li>
                 </ul>
               </template>
-              <div v-else-if="column.id === 'schedules'" class="schedules">
+              <div v-else v-tooltip class="text-ellipsis">
                 {{ column.value }}
               </div>
             </td>
           </tr>
         </template>
       </VtsDataTable>
-      <VtsStateHero v-if="searchQuery && filteredBackupJobs.length === 0" format="table" type="no-result" size="small">
+      <VtsStateHero
+        v-if="searchQuery && filteredBackupJobsSchedules.length === 0"
+        format="table"
+        type="no-result"
+        size="small"
+      >
         {{ t('no-result') }}
       </VtsStateHero>
       <UiTopBottomTable :selected-items="0" :total-items="0">
         <UiTablePagination v-bind="paginationBindings" />
       </UiTopBottomTable>
     </div>
-  </div>
+  </UiCard>
 </template>
 
 <script setup lang="ts">
 import { useXoBackupJobSchedulesUtils } from '@/composables/xo-backup-job-schedules.composable'
-import { useXoBackupUtils } from '@/composables/xo-backup-utils.composable.ts'
-import type { XoBackupJob } from '@/remote-resources/use-xo-backup-job-collection.ts'
+import { useXoBackupJobCollection } from '@/remote-resources/use-xo-backup-job-collection'
+import type { XoSchedule } from '@/types/xo/schedule.type'
 import type { IconName } from '@core/icons'
 import VtsDataTable from '@core/components/data-table/VtsDataTable.vue'
+import VtsEnabledState from '@core/components/enabled-state/VtsEnabledState.vue'
 import VtsIcon from '@core/components/icon/VtsIcon.vue'
 import VtsStateHero from '@core/components/state-hero/VtsStateHero.vue'
 import UiButtonIcon from '@core/components/ui/button-icon/UiButtonIcon.vue'
+import UiCard from '@core/components/ui/card/UiCard.vue'
 import UiCheckbox from '@core/components/ui/checkbox/UiCheckbox.vue'
 import UiLink from '@core/components/ui/link/UiLink.vue'
 import UiQuerySearchBar from '@core/components/ui/query-search-bar/UiQuerySearchBar.vue'
 import UiTablePagination from '@core/components/ui/table-pagination/UiTablePagination.vue'
-import UiTag from '@core/components/ui/tag/UiTag.vue'
-import UiTagsList from '@core/components/ui/tag/UiTagsList.vue'
 import UiTitle from '@core/components/ui/title/UiTitle.vue'
 import UiTopBottomTable from '@core/components/ui/top-bottom-table/UiTopBottomTable.vue'
-import { usePagination } from '@core/composables/pagination.composable.ts'
-import { useRouteQuery } from '@core/composables/route-query.composable.ts'
-import { useTable } from '@core/composables/table.composable.ts'
-import { vTooltip } from '@core/directives/tooltip.directive.ts'
+import { usePagination } from '@core/composables/pagination.composable'
+import { useRouteQuery } from '@core/composables/route-query.composable'
+import { useTable } from '@core/composables/table.composable'
+import { vTooltip } from '@core/directives/tooltip.directive'
 import { noop } from '@vueuse/shared'
 import { computed, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 
-const { backupJobs } = defineProps<{
-  backupJobs: XoBackupJob[]
+const { backupJobSchedules } = defineProps<{
+  backupJobSchedules: XoSchedule[]
   hasError: boolean
 }>()
 
 const { t } = useI18n()
 
-const { getModeLabels } = useXoBackupUtils()
-const { getLastThreeRunsStatuses, getTotalSchedules } = useXoBackupJobSchedulesUtils()
+const { useGetBackupJobById } = useXoBackupJobCollection()
+const { getLastThreeRunsStatuses } = useXoBackupJobSchedulesUtils()
+
 const selectedBackupJobId = useRouteQuery('id')
 
 const searchQuery = ref('')
 
-const filteredBackupJobs = computed(() => {
+const filteredBackupJobsSchedules = computed(() => {
   const searchTerm = searchQuery.value.trim().toLocaleLowerCase()
 
   if (!searchTerm) {
-    return backupJobs
+    return backupJobSchedules
   }
 
-  return backupJobs.filter(backupJob =>
-    Object.values(backupJob).some(value => String(value).toLocaleLowerCase().includes(searchTerm))
+  return backupJobSchedules.filter(schedules =>
+    Object.values(schedules).some(value => String(value).toLocaleLowerCase().includes(searchTerm))
   )
 })
 
-const { visibleColumns, rows } = useTable('backup-jobs', filteredBackupJobs, {
+const { visibleColumns, rows } = useTable('backup-job-schedules', filteredBackupJobsSchedules, {
   rowId: record => record.id,
   columns: define => [
     define('checkbox', noop, { label: '', isHideable: false }),
-    define('job-name', record => record.name, { label: t('job-name') }),
-    define('mode', record => getModeLabels(record), { label: t('mode') }),
-    define('last-runs', record => getLastThreeRunsStatuses(record), {
+    define('schedule', record => record, { label: t('schedule') }),
+    define('id', record => record.id, { label: t('id') }),
+    define('status', record => record.enabled, { label: t('status') }),
+    define('cron-pattern', record => record.cron, { label: t('cron-pattern') }),
+    define('last-runs', record => getLastThreeRunsStatuses(useGetBackupJobById(record.jobId).value), {
       label: t('last-n-runs', { n: 3 }),
     }),
-    define('schedules', record => getTotalSchedules(record), { label: t('total-schedules') }),
+    // define('next-run', () => 'comming soon', { label: t('next-run') }), // #TODO bad data
     define('more', noop, { label: '', isHideable: false }),
   ],
 })
 
-const { pageRecords: backupJobsRecords, paginationBindings } = usePagination('backups-jobs', rows)
+const { pageRecords: backupJobSchedulesRecords, paginationBindings } = usePagination('backup-job-schedules', rows)
 
-type BackupJobHeader = 'job-name' | 'mode' | 'last-runs' | 'schedules'
+type BackupJobHeader = 'schedule' | 'id' | 'status' | 'cron-pattern' | 'last-runs'
 
 const headerIcon: Record<BackupJobHeader, IconName> = {
-  'job-name': 'fa:align-left',
-  mode: 'fa:square-caret-down',
+  schedule: 'fa:a',
+  id: 'fa:hashtag',
+  status: 'fa:square-caret-down',
+  'cron-pattern': 'fa:clock',
   'last-runs': 'fa:square-caret-down',
-  schedules: 'fa:hashtag',
+  // 'next-run': 'fa:calendar',
 }
 </script>
 
-<style scoped lang="postcss">
-.backup-jobs-table,
+<style lang="postcss" scoped>
+.backup-job-schedules-table,
 .table-actions,
 .container {
   display: flex;
   flex-direction: column;
 }
 
-.backup-jobs-table {
+.backup-job-schedules-table {
   gap: 2.4rem;
 
   .container,
@@ -198,14 +196,9 @@ const headerIcon: Record<BackupJobHeader, IconName> = {
     line-height: 1;
   }
 
-  .mode {
-    max-width: fit-content;
-  }
-
   .last-three-runs {
     display: flex;
     gap: 0.8rem;
-    align-items: center;
 
     li:not(:first-child) {
       &::after {
@@ -232,10 +225,6 @@ const headerIcon: Record<BackupJobHeader, IconName> = {
       transform: scale(1.2);
       margin-inline-end: 0.3rem;
     }
-  }
-
-  .schedules {
-    text-align: right;
   }
 }
 </style>

commit 2dd451a7d933f27e550fac673029d8ab79aba70d

fix(xo-6/backup-settings): reactivity issue when using useXoBackupJobSettingsUtils (#9169)
Introduced by e547447

@xen-orchestra/web/src/components/backups/configuration/BackupJobSettings.vue
@@ -91,5 +91,5 @@ const {
   settings,
   reportWhenValueTranslation,
   snapshotModeTranslation,
-} = useXoBackupJobSettingsUtils(backupJob)
+} = useXoBackupJobSettingsUtils(() => backupJob)
 </script>

@xen-orchestra/web/src/components/backups/jobs/panel/cards/BackupJobSettingsCard.vue
@@ -130,7 +130,7 @@ const {
   proxy,
   reportWhenValueTranslation,
   settings,
-} = useXoBackupJobSettingsUtils(backupJob)
+} = useXoBackupJobSettingsUtils(() => backupJob)
 </script>
 
 <style scoped lang="postcss">