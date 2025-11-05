9aa0d29263407ce82a63f7ddecd84b0a59f0160a
mathieuRA <contact@mathieu-raisin.fr>
2025-11-05 15:02:40 +0100

    feat: redirect after VM creation

--- @xen-orchestra/web/src/jobs/vm-create.job.ts
+++ @xen-orchestra/web/src/jobs/vm-create.job.ts
@@ -1,15 +1,16 @@
 import type { XoPool } from '@/types/xo/pool.type.ts'
+import type { XoVm } from '@/types/xo/vm.type'
 import { useFetch } from '@vueuse/core'
 
-export async function createVM(payload: Record<string, any>, poolId: XoPool['id']) {
+export async function createVM(payload: Record<string, any>, poolId: XoPool['id']): Promise<XoVm['id']> {
   const { data, error } = await useFetch(`/rest/v0/pools/${poolId}/actions/create_vm?sync=true`, {
     method: 'POST',
     body: JSON.stringify(payload),
     headers: { 'Content-Type': 'application/json' },
-  }).text()
+  }).json<{ id: XoVm['id'] }>()
 
   if (error.value) {
     throw new Error(error.value.message)
   }
-  return data.value
+  return data.value!.id
 }
