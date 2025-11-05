76f85b216510d1fc11d9455604e25d15f0008c38
mathieuRA <contact@mathieu-raisin.fr>
2025-11-05 17:24:27 +0100

    feat: watch VBDs

--- @xen-orchestra/web/src/remote-resources/use-xo-vbd-collection.ts
+++ @xen-orchestra/web/src/remote-resources/use-xo-vbd-collection.ts
@@ -2,8 +2,14 @@ import { useXoCollectionState } from '@/composables/xo-collection-state/use-xo-c
 import { defineRemoteResource } from '@core/packages/remote-resource/define-remote-resource.ts'
 import type { XoVbd } from '@vates/types'
 
+const vbdFields: (keyof XoVbd)[] = ['id', 'VDI', 'is_cd_drive', 'position'] as const
+
 export const useXoVbdCollection = defineRemoteResource({
-  url: '/rest/v0/vbds?fields=id,name_label,name_description,VDI,is_cd_drive,position',
+  url: '/rest/v0/vbds?fields='.concat(vbdFields.toString()),
+  watchCollection: {
+    type: 'VBD',
+    fields: vbdFields,
+  },
   initialData: () => [] as XoVbd[],
   state: (vbds, context) =>
     useXoCollectionState(vbds, {
