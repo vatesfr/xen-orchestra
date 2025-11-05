f261ae7f3539404fb8a921e6f894a6b92f38590e
mathieuRA <contact@mathieu-raisin.fr>
2025-11-05 17:31:19 +0100

    feat: watch VIFs

--- @xen-orchestra/web/src/remote-resources/use-xo-vif-collection.ts
+++ @xen-orchestra/web/src/remote-resources/use-xo-vif-collection.ts
@@ -2,8 +2,24 @@ import { useXoCollectionState } from '@/composables/xo-collection-state/use-xo-c
 import { defineRemoteResource } from '@core/packages/remote-resource/define-remote-resource.ts'
 import type { XoVif } from '@vates/types'
 
+const vifFields: (keyof XoVif)[] = [
+  '$VM',
+  '$network',
+  'attached',
+  'device',
+  'txChecksumming',
+  'id',
+  'lockingMode',
+  'MAC',
+  'MTU',
+] as const
+
 export const useXoVifCollection = defineRemoteResource({
-  url: '/rest/v0/vifs?fields=$VM,$network,attached,device,txChecksumming,id,lockingMode,MAC,MTU',
+  url: '/rest/v0/vifs?fields='.concat(vifFields.toString()),
+  watchCollection: {
+    type: 'VIF',
+    fields: vifFields,
+  },
   initialData: () => [] as XoVif[],
   state: (vifs, context) =>
     useXoCollectionState(vifs, {
