f01110bd6ab76b70959757f14f37ee820b59993b
mathieuRA <contact@mathieu-raisin.fr>
2025-11-05 17:27:38 +0100

    feat: watch VDIs

--- @xen-orchestra/web/src/remote-resources/use-xo-vdi-collection.ts
+++ @xen-orchestra/web/src/remote-resources/use-xo-vdi-collection.ts
@@ -2,8 +2,14 @@ import { useXoCollectionState } from '@/composables/xo-collection-state/use-xo-c
 import { defineRemoteResource } from '@core/packages/remote-resource/define-remote-resource.ts'
 import type { XoVdi } from '@vates/types'
 
+const vdiFields: (keyof XoVdi)[] = ['id', 'name_label', 'name_description', '$VBDs', '$SR', 'size', '$pool'] as const
+
 export const useXoVdiCollection = defineRemoteResource({
-  url: '/rest/v0/vdis?fields=id,name_label,name_description,$VBDs,$SR,size,$pool',
+  url: '/rest/v0/vdis?fields='.concat(vdiFields.toString()),
+  watchCollection: {
+    type: 'VDI',
+    fields: vdiFields,
+  },
   initialData: () => [] as XoVdi[],
   state: (vdis, context) =>
     useXoCollectionState(vdis, {
