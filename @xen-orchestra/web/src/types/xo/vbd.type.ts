8d75ad972ed5bd0d559cff4408a6f9b346efb86f
mathieuRA <contact@mathieu-raisin.fr>
2025-11-05 17:24:27 +0100

    feat: watch VBDs

--- @xen-orchestra/web/src/types/xo/vbd.type.ts
+++ @xen-orchestra/web/src/types/xo/vbd.type.ts
@@ -3,8 +3,6 @@ import type { Branded } from '@core/types/utility.type'
 
 export type XoVbd = {
   id: Branded<'vbd'>
-  name_label: string
-  name_description: string
   VDI: XoVdi['id']
   is_cd_drive: boolean
   position: string
