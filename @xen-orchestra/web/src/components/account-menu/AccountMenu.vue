0125f00722682870ffb101eda230ed90ceae9cde
Elise-F <elise.franzini@gmail.com>
2025-11-10 17:21:16 +0100

    second feedback

--- @xen-orchestra/web/src/components/account-menu/AccountMenu.vue
+++ @xen-orchestra/web/src/components/account-menu/AccountMenu.vue
@@ -9,8 +9,8 @@
       />
     </template>
 
-    <MenuItem v-for="link in links" :key="link.href" class="icon" :icon="link.icon">
-      <UiLink size="small" class="link typo-body-bold-small" :href="link.href" :target="link.target">
+    <MenuItem v-for="(link, index) in links" :key="index">
+      <UiLink size="small" class="link typo-body-bold-small" v-bind="link.props">
         {{ link.label }}
       </UiLink>
     </MenuItem>
@@ -22,11 +22,13 @@
 </template>
 
 <script lang="ts" setup>
+import { XCP_LINKS, XO_LINKS } from '@/constants.ts'
 import type { IconName } from '@core/icons'
 import MenuItem from '@core/components/menu/MenuItem.vue'
 import MenuList from '@core/components/menu/MenuList.vue'
 import UiAccountMenuButton from '@core/components/ui/account-menu-button/UiAccountMenuButton.vue'
 import UiLink from '@core/components/ui/link/UiLink.vue'
+import { type LinkOptions } from '@core/composables/link-component.composable'
 import { vTooltip } from '@core/directives/tooltip.directive'
 import { useI18n } from 'vue-i18n'
 
@@ -39,55 +41,55 @@ const { t } = useI18n()
 // TODO: Fetch the XO 5 mount path from API when available
 const logout = () => window.location.assign('/signout')
 
-type MenuLink = {
-  icon: IconName
-  label: string
-  href: string
-  target: '_self' | '_blank'
-}
-
-const links: MenuLink[] = [
+const links: { label: string; props: LinkOptions & { icon: IconName } }[] = [
   {
-    icon: 'fa:gear',
     label: t('settings'),
-    href: '/#/settings',
-    target: '_self',
+    props: {
+      icon: 'fa:gear',
+      to: '/settings',
+    },
   },
   {
-    icon: 'fa:satellite',
-    label: t('license-name', { name: 'XOA' }),
-    href: '/#/xoa/licenses',
-    target: '_blank',
+    label: t('license-name', { name: t('xoa') }),
+    props: {
+      icon: 'fa:satellite',
+      href: '/#/xoa/licenses',
+    },
   },
   {
-    icon: 'fa:book',
-    label: t('documentation-name', { name: 'XO' }),
-    href: 'https://docs.xen-orchestra.com',
-    target: '_blank',
+    label: t('documentation-name', { name: t('xo') }),
+    props: {
+      icon: 'fa:book',
+      href: XO_LINKS.DOC,
+    },
   },
   {
-    icon: 'fa:book',
-    label: t('documentation-name', { name: 'XCP-ng' }),
-    href: 'https://docs.xcp-ng.org?utm_campaign=xo6&utm_term=xcpdoc',
-    target: '_blank',
+    label: t('documentation-name', { name: t('xcp-ng') }),
+    props: {
+      icon: 'fa:book',
+      href: XCP_LINKS.DOC,
+    },
   },
   {
-    icon: 'fa:headset',
     label: t('professional-support'),
-    href: 'https://vates.tech/pricing-and-support?utm_campaign=xo6&utm_term=pricing',
-    target: '_blank',
+    props: {
+      icon: 'fa:headset',
+      href: XCP_LINKS.SUPPORT,
+    },
   },
   {
-    icon: 'fa:code',
-    label: t('documentation-name', { name: 'REST API' }),
-    href: '/rest/v0/docs/#/',
-    target: '_blank',
+    label: t('documentation-name', { name: t('rest-api') }),
+    props: {
+      icon: 'fa:code',
+      href: '/rest/v0/docs/#/',
+    },
   },
   {
-    icon: 'fa:message',
     label: t('send-us-feedback'),
-    href: 'https://xcp-ng.org/forum/category/12/xen-orchestra',
-    target: '_blank',
+    props: {
+      icon: 'fa:message',
+      href: XO_LINKS.COMMUNITY,
+    },
   },
 ]
 </script>
@@ -101,7 +103,6 @@ const links: MenuLink[] = [
   /* Make the link take the available width in the MenuItem component */
   flex-grow: 1;
 }
-
 .icon {
   color: var(--color-brand-txt-base);
 
