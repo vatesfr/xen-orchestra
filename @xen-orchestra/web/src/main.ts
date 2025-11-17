01a248cbdcd29aa721cb076ce9a0e60d144f2db1
mathieuRA <contact@mathieu-raisin.fr>
2025-11-17 17:12:58 +0100

    feat(@xen-orchestra/web): redirect non admin to xo5

--- @xen-orchestra/web/src/main.ts
+++ @xen-orchestra/web/src/main.ts
@@ -1,4 +1,5 @@
 import i18n from '@core/i18n'
+import { useFetch } from '@vueuse/core'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
@@ -7,19 +8,35 @@ import App from './App.vue'
 import '@xen-orchestra/web-core/assets/css/base.pcss'
 import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
 
-const app = createApp(App)
+async function init() {
+  // TODO: remove when non admin users are handled in the REST API
+  if (import.meta.env.PROD) {
+    // Expect an error for no admin users.
+    // redirect to XO5 in that case
+    const { statusCode } = await useFetch('/rest/v0/users/me').json()
+    if (statusCode.value === 403) {
+      // voluntary not using `useXoRoutes` we need info BEFORE mounting anything from XO6 (UI,store, ...)
+      const { data } = await useFetch<{ xo5: string; xo6: string }>('/rest/v0/gui-routes').json()
+      window.location.href = data.value!.xo5
+    }
+  }
 
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
-})
+  const app = createApp(App)
 
-if (import.meta.hot) {
-  handleHotUpdate(router)
+  const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+  })
+
+  if (import.meta.hot) {
+    handleHotUpdate(router)
+  }
+
+  app.use(i18n)
+  app.use(createPinia())
+  app.use(router)
+
+  app.mount('#app')
 }
 
-app.use(i18n)
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+init()
