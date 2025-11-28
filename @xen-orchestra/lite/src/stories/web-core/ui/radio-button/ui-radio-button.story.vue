36eb7074778e7a65091f8937f228accaf515cd3d
Elise-F <elise.franzini@gmail.com>
2025-11-28 16:36:59 +0100

    feat(web-core): udpate Ui Radio button component and add rich props

--- @xen-orchestra/lite/src/stories/web-core/ui/radio-button/ui-radio-button.story.vue
+++ @xen-orchestra/lite/src/stories/web-core/ui/radio-button/ui-radio-button.story.vue
@@ -3,16 +3,33 @@
     v-slot="{ properties, settings }"
     :params="[
       prop('accent').required().enum('brand', 'warning', 'danger').preset('brand').widget(),
+      prop('size').enum('small', 'medium').preset('medium').widget(),
       prop('disabled').bool().widget(),
+      prop('rich').bool().widget(),
+      prop('src').str().help('Image URL for rich mode'),
+      prop('alt').str().help('Alt text for image'),
       slot().help('Meant to receive a label'),
       setting('defaultSlot').widget(text()).preset('Label'),
     ]"
   >
-    <div>
-      <UiRadioButton v-model="selectedRadio" value="1" v-bind="properties">{{ settings.defaultSlot }}</UiRadioButton>
-    </div>
-    <div>
-      <UiRadioButton v-model="selectedRadio" value="2" v-bind="properties">
+    <div class="radio">
+      <UiRadioButton
+        v-model="selectedRadio"
+        value="1"
+        v-bind="properties"
+        :src="properties.rich ? imageUrl : undefined"
+        :alt="properties.rich ? properties.alt : undefined"
+      >
+        {{ settings.defaultSlot }}
+      </UiRadioButton>
+
+      <UiRadioButton
+        v-model="selectedRadio"
+        value="2"
+        v-bind="properties"
+        :src="properties.rich ? imageUrl : undefined"
+        :alt="properties.rich ? properties.alt : undefined"
+      >
         {{ settings.defaultSlot }}
       </UiRadioButton>
     </div>
@@ -25,6 +42,15 @@ import { prop, setting, slot } from '@/libs/story/story-param'
 import { text } from '@/libs/story/story-widget'
 import UiRadioButton from '@core/components/ui/radio-button/UiRadioButton.vue'
 import { ref } from 'vue'
+import imageUrl from '../../../../assets/color-mode-dark.svg'
 
 const selectedRadio = ref('')
 </script>
+
+<style lang="postcss" scoped>
+.radio {
+  display: flex;
+  gap: 1.6rem;
+  flex-wrap: wrap;
+}
+</style>
