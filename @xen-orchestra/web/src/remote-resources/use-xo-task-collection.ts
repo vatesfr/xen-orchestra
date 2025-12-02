f4a9c3201f36be21d46236e900b7472bd20ea9a7
MathieuRA <contact@mathieu-raisin.fr>
2025-12-02 03:01:11 -0500

    feat(@xen-orchestra/rest-api): add reactivity on task store

--- @xen-orchestra/web/src/remote-resources/use-xo-task-collection.ts
+++ @xen-orchestra/web/src/remote-resources/use-xo-task-collection.ts
@@ -1,13 +1,17 @@
 import { useXoCollectionState } from '@/composables/xo-collection-state/use-xo-collection-state.ts'
 import { convertTaskToCore } from '@/utils/convert-task-to-core.util.ts'
+import { watchCollectionWrapper } from '@/utils/sse.util'
 import { defineRemoteResource } from '@core/packages/remote-resource/define-remote-resource.ts'
 import type { XoTask } from '@vates/types'
 import { computed } from 'vue'
 
 const ONE_DAY = 24 * 60 * 60 * 1000
 
+const taskFields: (keyof XoTask)[] = ['id', 'start', 'end', 'properties', 'status', 'progress', 'tasks'] as const
+
 export const useXoTaskCollection = defineRemoteResource({
-  url: '/rest/v0/tasks?fields=id,start,end,properties,status,progress,tasks',
+  url: `/rest/v0/tasks?fields=${taskFields}`,
+  watchCollection: watchCollectionWrapper({ resource: 'task', fields: taskFields }),
   initialData: () => [] as XoTask[],
   state: (tasks, context) => {
     const lastDayTasks = computed(() => {
