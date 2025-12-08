0e1bc4704e87a3f4b30867ac082a041deaee2a95
Joris-K <joris.kosacki@vates.tech>
2025-12-08 15:10:38 +0100

    first feedback

--- @xen-orchestra/web-core/lib/components/ui/task-list/UiTaskList.vue
+++ @xen-orchestra/web-core/lib/components/ui/task-list/UiTaskList.vue
@@ -18,7 +18,11 @@ import { useCollection } from '@core/packages/collection'
 
 const { tasks, depth = 0 } = defineProps<{
   tasks: Task[]
-  depth: number
+  depth?: number
+}>()
+
+const emit = defineEmits<{
+  select: [id: string]
 }>()
 
 const emit = defineEmits<{
