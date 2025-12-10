50c21f6917d1265c6ebdea1045f946d61df39bfe
unel <enee.prog@gmail.com>
2025-12-10 10:54:40 +0100

    feat(xo6/vm-dashnoard): add backupRuns and BackupArchives card

---
+++ @xen-orchestra/web/src/modules/vm/remote-resources/use-xo-vm-dashboard.ts
@@ -0,0 +1,9 @@
+import type { XoVmDashboard } from '@/types/xo/vm-dashboard.type'
+import { defineRemoteResource } from '@core/packages/remote-resource/define-remote-resource.ts'
+import type { Ref } from 'vue'
+
+export const useXoVmDashboard = defineRemoteResource({
+  url: (vmId: string) => `/rest/v0/vms/${vmId}/dashboard?ndjson=true`,
+  stream: true,
+  state: (vmDashboard: Ref<XoVmDashboard | undefined>, context) => ({ vmDashboard, hasError: context.hasError }),
+})
--- @xen-orchestra/web/src/pages/vm/[id]/dashboard.vue
+++ @xen-orchestra/web/src/pages/vm/[id]/dashboard.vue
@@ -21,10 +21,12 @@
         :has-ready="hasVmAlarmFetchError"
       />
       <template v-if="data">
-        <VmDashboardCpuUsageChart class="cpu-usage-chart" :data :error :loading="isFetching" />
-        <VmDashboardRamUsageChart class="ram-usage-chart" :data :error :loading="isFetching" />
-        <VmDashboardNetworkUsageChart class="network-usage-chart" :data :error :loading="isFetching" />
-        <VmDashboardVdiUsageChart class="vdi-usage-chart" :data :error :loading="isFetching" />
+        <VmDashboardBackupRuns class="backup-rum" :vm-id="vm.id" :vm-dashboard :error="dashboardError" />
+        <VmDashboardBackupArchives class="backup-archives" :vm-dashboard :error="dashboardError" />
+        <VmDashboardCpuUsageChart class="cpu-usage-chart" :data :error="statsError" :loading="isFetching" />
+        <VmDashboardRamUsageChart class="ram-usage-chart" :data :error="statsError" :loading="isFetching" />
+        <VmDashboardNetworkUsageChart class="network-usage-chart" :data :error="statsError" :loading="isFetching" />
+        <VmDashboardVdiUsageChart class="vdi-usage-chart" :data :error="statsError" :loading="isFetching" />
       </template>
     </template>
   </div>
@@ -32,6 +34,8 @@
 
 <script lang="ts" setup>
 import DashboardAlarms from '@/modules/alarm/components/DashboardAlarms.vue'
+import VmDashboardBackupArchives from '@/modules/vm/components/dashboard/VmDashboardBackupArchives.vue'
+import VmDashboardBackupRuns from '@/modules/vm/components/dashboard/VmDashboardBackupRuns.vue'
 import VmDashboardCpuUsageChart from '@/modules/vm/components/dashboard/VmDashboardCpuUsageChart.vue'
 import VmDashboardNetworkUsageChart from '@/modules/vm/components/dashboard/VmDashboardNetworkUsageChart.vue'
 import VmDashboardQuickInfo from '@/modules/vm/components/dashboard/VmDashboardQuickInfo.vue'
@@ -39,6 +43,7 @@ import VmDashboardRamUsageChart from '@/modules/vm/components/dashboard/VmDashbo
 import VmDashboardVdiUsageChart from '@/modules/vm/components/dashboard/VmDashboardVdiUsageChart.vue'
 import { useXoVmAlarmsCollection } from '@/modules/vm/remote-resources/use-xo-vm-alarms-collection.ts'
 import type { FrontXoVm } from '@/modules/vm/remote-resources/use-xo-vm-collection.ts'
+import { useXoVmDashboard } from '@/modules/vm/remote-resources/use-xo-vm-dashboard'
 import { useFetchStats } from '@/shared/composables/fetch-stats.composable.ts'
 import { GRANULARITY } from '@/shared/utils/rest-api-stats.ts'
 import VtsStateHero from '@core/components/state-hero/VtsStateHero.vue'
@@ -51,7 +56,9 @@ const { vm } = defineProps<{
   vm: FrontXoVm
 }>()
 
-const { data, isFetching, error } = useFetchStats('vm', () => vm.id, GRANULARITY.Hours)
+const { data, isFetching, error: statsError } = useFetchStats('vm', () => vm.id, GRANULARITY.Hours)
+
+const { vmDashboard, hasError: dashboardError } = useXoVmDashboard({}, () => vm.id)
 
 const { vmAlarms, areVmAlarmsReady, hasVmAlarmFetchError } = useXoVmAlarmsCollection({}, () => vm.id)
 
@@ -70,6 +77,7 @@ const { t } = useI18n()
   grid-template-columns: repeat(8, 1fr);
   grid-template-areas:
     'quick-info quick-info quick-info quick-info quick-info quick-info quick-info quick-info'
+    'backup-rum backup-rum backup-rum backup-rum backup-archives backup-archives backup-archives backup-archives'
     'alarms alarms alarms alarms alarms alarms alarms alarms'
     'cpu-usage-chart cpu-usage-chart ram-usage-chart ram-usage-chart network-usage-chart network-usage-chart vdi-usage-chart vdi-usage-chart'
     'offline-hero-container offline-hero-container offline-hero-container offline-hero-container offline-hero-container offline-hero-container offline-hero-container offline-hero-container';
@@ -78,6 +86,8 @@ const { t } = useI18n()
     grid-template-columns: 1fr;
     grid-template-areas:
       'quick-info'
+      'backup-rum'
+      'backup-archives'
       'alarms'
       'cpu-usage-chart'
       'ram-usage-chart'
@@ -93,6 +103,14 @@ const { t } = useI18n()
   .alarms {
     grid-area: alarms;
     height: 46.2rem;
+  }
+
+  .backup-rum {
+    grid-area: backup-rum;
+  }
+
+  .backup-archives {
+    grid-area: backup-archives;
   }
 
   .offline-hero-container {
