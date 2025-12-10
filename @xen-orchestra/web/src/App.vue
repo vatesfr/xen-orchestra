907ee7d88c25fd491b4f2a71ee2bb69e36a97fde
Mathieu <70369997+MathieuRA@users.noreply.github.com>
2025-12-10 12:52:01 +0100

    feat(@xen-orchestra/web): display modal of the first connection (#9230)

--- @xen-orchestra/web/src/App.vue
+++ @xen-orchestra/web/src/App.vue
@@ -12,8 +12,9 @@ import VtsModalList from '@core/components/modal/VtsModalList.vue'
 import VtsTooltipList from '@core/components/tooltip-list/VtsTooltipList.vue'
 import { useChartTheme } from '@core/composables/chart-theme.composable.ts'
 import { locales } from '@core/i18n'
+import { useModal } from '@core/packages/modal/use-modal'
 import { useUiStore } from '@core/stores/ui.store'
-import { useActiveElement, useMagicKeys, whenever } from '@vueuse/core'
+import { useActiveElement, useLocalStorage, useMagicKeys, whenever } from '@vueuse/core'
 import { useCookies } from '@vueuse/integrations/useCookies'
 import { logicAnd } from '@vueuse/math'
 import { computed } from 'vue'
@@ -22,6 +23,16 @@ import { useI18n } from 'vue-i18n'
 const uiStore = useUiStore()
 const { locale } = useI18n()
 const { get } = useCookies()
+
+// TODO: Remove when we considere XO6 in more advanced state
+const isFirstConnection = useLocalStorage('first-connection', true)
+const openModal = useModal({
+  component: import('@/components/modals/FirstConnection.vue'),
+  onConfirm: () => {
+    isFirstConnection.value = false
+  },
+})
+whenever(() => isFirstConnection.value, openModal, { immediate: true })
 
 const cookieLang = get('lang')
 locale.value = cookieLang && locales[cookieLang] ? cookieLang : 'en'
---
+++ @xen-orchestra/web/src/components/modals/FirstConnection.vue
@@ -0,0 +1,64 @@
+<template>
+  <VtsModal accent="info" icon="fa:satellite">
+    <template #title>
+      {{ t('welcome-to-xo6') }}
+    </template>
+    <template #content>
+      <div class="popup-content">
+        <div>
+          {{ t('popup-first-connection-introduction') }}
+          <I18nT keypath="popup-first-connection-default-interface">
+            <template #documentationLink>
+              <UiLink
+                href="https://docs.xen-orchestra.com/configuration#using-xo-5-as-the-default-interface"
+                size="medium"
+              >
+                {{ t('popup-first-connection-follow-this-guide') }}
+              </UiLink>
+            </template>
+          </I18nT>
+        </div>
+        <div>{{ t('popup-first-connection-early-release') }}</div>
+        <I18nT keypath="popup-first-connection-feedback" scope="global" tag="div">
+          <template #feedbackLink>
+            <UiLink
+              href="https://xcp-ng.org/forum/topic/11604/xo-6-dedicated-thread-for-all-your-feedback"
+              size="medium"
+            >
+              {{ t('popup-first-connection-feedback-and-impressions') }}
+            </UiLink>
+          </template>
+        </I18nT>
+        <div>{{ t('popup-first-connection-upcoming-release') }}</div>
+        <div class="xo-team">
+          {{ t('xo-team') }}
+        </div>
+      </div>
+    </template>
+    <template #buttons>
+      <VtsModalConfirmButton>{{ t('lets-go') }}</VtsModalConfirmButton>
+    </template>
+  </VtsModal>
+</template>
+
+<script lang="ts" setup>
+import VtsModal from '@core/components/modal/VtsModal.vue'
+import VtsModalConfirmButton from '@core/components/modal/VtsModalConfirmButton.vue'
+import UiLink from '@core/components/ui/link/UiLink.vue'
+
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n()
+</script>
+
+<style lang="postcss" scoped>
+.popup-content {
+  text-align: justify;
+  div {
+    margin-bottom: 1.5rem;
+  }
+  .xo-team {
+    font-style: italic;
+    font-weight: bold;
+  }
+}
+</style>
