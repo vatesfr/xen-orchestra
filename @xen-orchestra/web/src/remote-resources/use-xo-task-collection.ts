a11e3dffc97229a5eeaf39feed646b41f30c1cb5
Joris-K <joris.kosacki@vates.tech>
2025-12-15 15:47:19 +0100

    improvement of collection

--- @xen-orchestra/web/src/remote-resources/use-xo-task-collection.ts
+++ @xen-orchestra/web/src/remote-resources/use-xo-task-collection.ts
@@ -2,40 +2,56 @@ import { useXoCollectionState } from '@/composables/xo-collection-state/use-xo-c
 import { convertTaskToCore } from '@/utils/convert-task-to-core.util.ts'
 import { watchCollectionWrapper } from '@/utils/sse.util'
 import { findTaskById } from '@/utils/xo-records/task.util.ts'
+import type { ResourceContext } from '@core/packages/remote-resource/types.ts'
 import { defineRemoteResource } from '@core/packages/remote-resource/define-remote-resource.ts'
 import type { XoTask } from '@vates/types'
-import { computed } from 'vue'
+import { useSorted } from '@vueuse/core'
+import { computed, type Ref } from 'vue'
 
 const ONE_DAY = 24 * 60 * 60 * 1000
 
-const taskFields: (keyof XoTask)[] = ['id', 'start', 'end', 'properties', 'infos', 'warnings', 'status', 'progress', 'tasks', 'result'] as const
+export const taskFields: (keyof XoTask)[] = [
+  'id',
+  'start',
+  'end',
+  'properties',
+  'infos',
+  'warnings',
+  'status',
+  'progress',
+  'tasks',
+  'result',
+] as const
+
+export function createTaskCollectionState<TArgs extends any[] = []>(
+  tasks: Ref<XoTask[]>,
+  context: ResourceContext<TArgs>
+) {
+  const lastDayTasks = computed(() => {
+    const now = Date.now()
+    return tasks.value.filter(task => now - task.start < ONE_DAY).map(task => convertTaskToCore(task))
+  })
+
+  const sortedTasks = useSorted(tasks, (task1, task2) => task2.start - task1.start)
+
+  const getTaskById = (taskId: XoTask['id']) => {
+    return findTaskById(sortedTasks.value, taskId)
+  }
+
+  return {
+    ...useXoCollectionState(tasks, {
+      context,
+      baseName: 'task',
+    }),
+    lastDayTasks,
+    sortedTasks,
+    getTaskById,
+  }
+}
 
 export const useXoTaskCollection = defineRemoteResource({
-  url: `/rest/v0/tasks?fields=${taskFields}`,
+  url: `/rest/v0/tasks?fields=${taskFields.join(',')}`,
   watchCollection: watchCollectionWrapper({ resource: 'task', fields: taskFields }),
   initialData: () => [] as XoTask[],
-  state: (tasks, context) => {
-    const lastDayTasks = computed(() => {
-      const now = Date.now()
-      return tasks.value.filter(task => now - task.start < ONE_DAY).map(task => convertTaskToCore(task))
-    })
-
-    const sortedTasks = computed(() => {
-      return [...tasks.value].sort((a, b) => b.start - a.start)
-    })
-
-    const getTaskById = (taskId: XoTask['id']) => {
-      return findTaskById(sortedTasks.value, taskId)
-    }
-
-    return {
-      ...useXoCollectionState(tasks, {
-        context,
-        baseName: 'task',
-      }),
-      lastDayTasks,
-      sortedTasks,
-      getTaskById,
-    }
-  },
+  state: createTaskCollectionState,
 })
