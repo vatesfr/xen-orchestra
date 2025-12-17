53386f0558040c15df38c23b03d18b9b457719c7
Olivier Floch <olivier.floch@vates.fr>
2025-12-17 09:09:33 +0100

    change locale code to BCP style to avoid errors when using regional variations languages

--- @xen-orchestra/web-core/lib/i18n.ts
+++ @xen-orchestra/web-core/lib/i18n.ts
@@ -53,8 +53,8 @@ export const locales: Locales = {
     code: 'nl',
     name: 'Nederlands',
   },
-  pt_BR: {
-    code: 'pt_BR',
+  'pt-BR': {
+    code: 'pt-BR',
     name: 'Português (Brasil)',
   },
   da: {
@@ -65,8 +65,8 @@ export const locales: Locales = {
     code: 'ko',
     name: '한국어',
   },
-  nb_NO: {
-    code: 'nb_NO',
+  'nb-NO': {
+    code: 'nb-NO',
     name: 'Norsk (Bokmål)',
   },
   ja: {
