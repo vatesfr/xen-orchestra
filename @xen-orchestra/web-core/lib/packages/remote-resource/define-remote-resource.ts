vatesfr/xen-orchestra

commit 5d28710ab313f0f3234f8d80065740c3370c3be9

feat(@xen-orchestra/web): implement reactivity for sub-stores (#9250)

@xen-orchestra/web-core/lib/packages/remote-resource/define-remote-resource.ts
@@ -64,11 +64,13 @@ export function defineRemoteResource<
   onDataRemoved?: (data: Ref<NoInfer<TData>>, receivedData: any) => void
   stream?: boolean
   watchCollection: {
+    collectionId: string
     resource: string // reactivity only on XAPI XO record for now
     getIdentifier: (obj: unknown) => string
     handleDelete: THandleDelete
     handlePost: THandlePost
     handleWatching: THandleWatching
+    predicate?: (receivedData: TData, context: ResourceContext<TArgs> | undefined) => boolean
   }
 }): UseRemoteResource<TState, TArgs>
 
@@ -86,11 +88,13 @@ export function defineRemoteResource<
   pollingIntervalMs?: number | false
   stream?: boolean
   watchCollection?: {
+    collectionId: string
     resource: string // reactivity only on XAPI XO record for now
     getIdentifier: (obj: unknown) => string
     handleDelete: THandleDelete
     handlePost: THandlePost
     handleWatching: THandleWatching
+    predicate?: (receivedData: TData, context: ResourceContext<TArgs> | undefined) => boolean
   }
 }) {
   const cache = new Map<
@@ -131,7 +135,11 @@ export function defineRemoteResource<
 
   const onDataReceived =
     config.onDataReceived ??
-    ((data: Ref<TData>, receivedData: any) => {
+    ((data: Ref<TData>, receivedData: any, args?: ResourceContext<TArgs>) => {
+      // allow to ignore some update (like for sub collection. E.g. vms/:id/vdis)
+      if (config.watchCollection?.predicate?.(receivedData, args) === false) {
+        return
+      }
       if (data.value === undefined || (Array.isArray(data.value) && Array.isArray(receivedData))) {
         data.value = receivedData
         return
@@ -148,7 +156,12 @@ export function defineRemoteResource<
 
   const onDataRemoved =
     config.onDataRemoved ??
-    ((data: Ref<TData>, receivedData: any) => {
+    ((data: Ref<TData>, receivedData: any, args?: ResourceContext<TArgs>) => {
+      // allow to ignore some update (like for sub collection. E.g. vms/:id/vdis)
+      if (!config.watchCollection?.predicate?.(receivedData, args)) {
+        return
+      }
+
       // for now only support `onDataRemoved` when watching XapiXoRecord collection
       if (Array.isArray(data.value) && !Array.isArray(receivedData)) {
         removeData(data.value, receivedData)
@@ -244,18 +257,19 @@ export function defineRemoteResource<
     let resume: VoidFunction = execute
 
     if (config.watchCollection !== undefined) {
-      const { resource, handleDelete, handlePost, handleWatching } = config.watchCollection
+      const { collectionId, resource, handleDelete, handlePost, handleWatching } = config.watchCollection
       const { watch, unwatch } = useSseStore()
 
-      pause = () => unwatch({ resource, handleDelete })
+      pause = () => unwatch({ collectionId, resource, handleDelete })
       resume = async function () {
         await execute()
         await watch({
+          collectionId,
           handleWatching,
           handlePost,
           resource,
-          onDataReceived: receivedData => onDataReceived(data, receivedData),
-          onDataRemoved: receivedData => onDataRemoved(data, receivedData),
+          onDataReceived: receivedData => onDataReceived(data, receivedData, context),
+          onDataRemoved: receivedData => onDataRemoved(data, receivedData, context),
         })
       }
     } else if (pollingInterval !== false) {

@xen-orchestra/web-core/lib/packages/remote-resource/sse.store.ts
@@ -9,26 +9,32 @@ export type THandleWatching = (
   getConfigByResource: (resource: string) =>
     | {
         subscriptionId: string
-        events: {
-          add: (object: unknown) => void
-          update: (object: unknown) => void
-          remove: (object: unknown) => void
-        }
+        configs: Record<
+          string,
+          {
+            add: (object: unknown) => void
+            update: (object: unknown) => void
+            remove: (object: unknown) => void
+          }
+        >
       }
     | undefined
 ) => void
 
 export const useSseStore = defineStore('sse', () => {
   const sse = ref<{ id?: string; isWatching: boolean }>({ isWatching: false })
-  const configByResource: Map<
+  const configsByResource: Map<
     string,
     {
       subscriptionId: string
-      events: {
-        add: EventFn
-        update: EventFn
-        remove: EventFn
-      }
+      configs: Record<
+        string,
+        {
+          add: EventFn
+          update: EventFn
+          remove: EventFn
+        }
+      >
     }
   > = new Map()
 
@@ -36,8 +42,8 @@ export const useSseStore = defineStore('sse', () => {
     sse.value.id = id
   }
 
-  function getConfigByResource(resource: string) {
-    return configByResource.get(resource)
+  function getConfigsByResource(resource: string) {
+    return configsByResource.get(resource)
   }
 
   function initializeWatcher(handleWatching: THandleWatching) {
@@ -58,18 +64,20 @@ export const useSseStore = defineStore('sse', () => {
 
       if (!sse.value.isWatching) {
         sse.value.isWatching = true
-        handleWatching(updateSseId, getConfigByResource)
+        handleWatching(updateSseId, getConfigsByResource)
       }
     })
   }
 
   async function watch({
+    collectionId,
     resource,
     onDataReceived,
     onDataRemoved,
     handlePost,
     handleWatching,
   }: {
+    collectionId: string
     resource: string
     onDataReceived: EventFn
     onDataRemoved: EventFn
@@ -82,25 +90,50 @@ export const useSseStore = defineStore('sse', () => {
 
     const id = await handlePost(sse.value.id!)
 
-    configByResource.set(resource, {
+    const { subscriptionId, configs } = configsByResource.get(resource) ?? {}
+
+    if (subscriptionId !== undefined && id !== subscriptionId) {
+      throw new Error(`Previous subscription ID: ${subscriptionId} and new one: ${id} are not the same`)
+    }
+
+    configsByResource.set(resource, {
       subscriptionId: id,
-      events: {
-        add: onDataReceived,
-        update: onDataReceived,
-        remove: onDataRemoved,
+      configs: {
+        ...configs,
+        [collectionId]: {
+          add: onDataReceived,
+          update: onDataReceived,
+          remove: onDataRemoved,
+        },
       },
     })
   }
 
-  async function unwatch({ resource, handleDelete }: { resource: string; handleDelete: THandleDelete }) {
-    if (sse.value.id === undefined || !configByResource.has(resource)) {
+  async function unwatch({
+    collectionId,
+    resource,
+    handleDelete,
+  }: {
+    collectionId: string
+    resource: string
+    handleDelete: THandleDelete
+  }) {
+    if (sse.value.id === undefined || !configsByResource.has(resource)) {
       return
     }
 
-    const subscriptionId = configByResource.get(resource)!.subscriptionId
-    await handleDelete(sse.value.id, subscriptionId)
+    const { configs, subscriptionId } = configsByResource.get(resource)!
+    delete configs[collectionId]
 
-    configByResource.delete(resource)
+    if (Object.keys(configs).length === 0) {
+      await handleDelete(sse.value.id, subscriptionId)
+      configsByResource.delete(resource)
+    } else {
+      configsByResource.set(resource, {
+        subscriptionId,
+        configs,
+      })
+    }
   }
 
   return { watch, unwatch }

@xen-orchestra/web/src/remote-resources/use-xo-alarm-collection.ts
@@ -4,7 +4,7 @@ import { defineRemoteResource } from '@core/packages/remote-resource/define-remo
 import type { XoAlarm } from '@vates/types'
 import { useSorted } from '@vueuse/core'
 
-const alarmFields: (keyof XoAlarm)[] = ['id', 'time', 'body', 'object', 'type'] as const
+export const alarmFields: (keyof XoAlarm)[] = ['id', 'time', 'body', 'object', 'type'] as const
 
 export const useXoAlarmCollection = defineRemoteResource({
   url: `/rest/v0/alarms?fields=${alarmFields.join(',')}`,

@xen-orchestra/web/src/remote-resources/use-xo-host-alarms-collection.ts
@@ -1,12 +1,30 @@
 import { useXoCollectionState } from '@/composables/xo-collection-state/use-xo-collection-state.ts'
+import { watchCollectionWrapper } from '@/utils/sse.util'
 import { defineRemoteResource } from '@core/packages/remote-resource/define-remote-resource.ts'
 import { createDateSorter } from '@core/utils/date-sorter.utils.ts'
 import type { XoAlarm } from '@vates/types'
 import { useSorted } from '@vueuse/core'
+import { toValue } from 'vue'
+import { alarmFields } from './use-xo-alarm-collection'
 
 export const useXoHostAlarmsCollection = defineRemoteResource({
-  url: (hostId: string) => `/rest/v0/hosts/${hostId}/alarms?fields=id,time,body,object`,
+  url: (hostId: string) => `/rest/v0/hosts/${hostId}/alarms?fields=${alarmFields.join(',')}`,
   initialData: () => [] as XoAlarm[],
+  watchCollection: watchCollectionWrapper<XoAlarm>({
+    collectionId: 'hostAlarm',
+    resource: 'alarm',
+    fields: alarmFields,
+    predicate(alarm, context) {
+      if (context === undefined || context.args === undefined || Array.isArray(alarm)) {
+        return true
+      }
+
+      const [id] = context.args
+      const hostId = toValue(id)
+
+      return alarm.object.uuid === hostId
+    },
+  }),
   state: (rawHostAlarms, context) => {
     const hostAlarms = useSorted(rawHostAlarms, createDateSorter('time'))
 

@xen-orchestra/web/src/remote-resources/use-xo-vbd-collection.ts
@@ -3,7 +3,7 @@ import { watchCollectionWrapper } from '@/utils/sse.util'
 import { defineRemoteResource } from '@core/packages/remote-resource/define-remote-resource.ts'
 import type { XoVbd } from '@vates/types'
 
-const vbdFields: (keyof XoVbd)[] = ['id', 'VDI', 'is_cd_drive', 'position', 'type', 'attached', 'device'] as const
+const vbdFields: (keyof XoVbd)[] = ['id', 'VDI', 'VM', 'is_cd_drive', 'position', 'type', 'attached', 'device'] as const
 
 export const useXoVbdCollection = defineRemoteResource({
   url: `/rest/v0/vbds?fields=${vbdFields.join(',')}`,

@xen-orchestra/web/src/remote-resources/use-xo-vdi-collection.ts
@@ -3,7 +3,7 @@ import { watchCollectionWrapper } from '@/utils/sse.util'
 import { defineRemoteResource } from '@core/packages/remote-resource/define-remote-resource.ts'
 import type { XoVdi } from '@vates/types'
 
-const vdiFields: (keyof XoVdi)[] = [
+export const vdiFields: (keyof XoVdi)[] = [
   'id',
   'name_label',
   'name_description',
@@ -12,6 +12,11 @@ const vdiFields: (keyof XoVdi)[] = [
   'size',
   '$pool',
   'type',
+  'usage',
+  'tags',
+  'uuid',
+  'cbt_enabled',
+  'image_format',
 ] as const
 
 export const useXoVdiCollection = defineRemoteResource({

@xen-orchestra/web/src/remote-resources/use-xo-vm-alarms-collection.ts
@@ -1,12 +1,30 @@
 import { useXoCollectionState } from '@/composables/xo-collection-state/use-xo-collection-state.ts'
+import { watchCollectionWrapper } from '@/utils/sse.util'
 import { defineRemoteResource } from '@core/packages/remote-resource/define-remote-resource.ts'
 import { createDateSorter } from '@core/utils/date-sorter.utils.ts'
 import type { XoAlarm } from '@vates/types'
 import { useSorted } from '@vueuse/core'
+import { toValue } from 'vue'
+import { alarmFields } from './use-xo-alarm-collection'
 
 export const useXoVmAlarmsCollection = defineRemoteResource({
-  url: (vmId: string) => `/rest/v0/vms/${vmId}/alarms?fields=id,time,body,object`,
+  url: (vmId: string) => `/rest/v0/vms/${vmId}/alarms?fields=${alarmFields.join(',')}`,
   initialData: () => [] as XoAlarm[],
+  watchCollection: watchCollectionWrapper<XoAlarm>({
+    collectionId: 'vmAlarm',
+    resource: 'alarm',
+    fields: alarmFields,
+    predicate(alarm, context) {
+      if (context === undefined || context.args === undefined || Array.isArray(alarm)) {
+        return true
+      }
+
+      const [id] = context.args
+      const vmId = toValue(id)
+
+      return alarm.object.uuid === vmId
+    },
+  }),
   state: (rawVmAlarms, context) => {
     const vmAlarms = useSorted(rawVmAlarms, createDateSorter('time'))
 

@xen-orchestra/web/src/remote-resources/use-xo-vm-vdis-collection.ts
@@ -1,11 +1,32 @@
 import { useXoCollectionState } from '@/composables/xo-collection-state/use-xo-collection-state.ts'
+import { watchCollectionWrapper } from '@/utils/sse.util'
 import { defineRemoteResource } from '@core/packages/remote-resource/define-remote-resource.ts'
 import type { XoVdi } from '@vates/types'
+import { toValue } from 'vue'
+import { useXoVbdCollection } from './use-xo-vbd-collection'
+import { vdiFields } from './use-xo-vdi-collection'
 
 export const useXoVmVdisCollection = defineRemoteResource({
-  url: (vmId: string) =>
-    `/rest/v0/vms/${vmId}/vdis?fields=id,name_label,name_description,usage,size,uuid,tags,cbt_enabled,$VBDs,$SR,image_format`,
+  url: (vmId: string) => `/rest/v0/vms/${vmId}/vdis?fields=${vdiFields.join(',')}`,
   initialData: () => [] as XoVdi[],
+  watchCollection: watchCollectionWrapper<XoVdi>({
+    collectionId: 'vmVdi',
+    resource: 'VDI',
+    fields: vdiFields,
+    predicate(obj, context) {
+      if (context === undefined || context.args === undefined || Array.isArray(obj)) {
+        return true
+      }
+
+      const [id] = context.args
+      const vmId = toValue(id)
+
+      const { getVbdsByIds } = useXoVbdCollection(context)
+      const vbds = getVbdsByIds(obj.$VBDs)
+
+      return vbds.some(vbd => vbd.VM === vmId)
+    },
+  }),
   state: (vmVdis, context) => {
     return useXoCollectionState(vmVdis, {
       context,

commit bfe788e096b0560af21dc1c735a68226a72b5784

fix(@xen-orchestra/web-core): fix race condition on reactive store (#9307)
Introduced by 5d28710ab313f0f3234f8d80065740c3370c3be9

@xen-orchestra/web-core/lib/packages/remote-resource/define-remote-resource.ts
@@ -133,6 +133,25 @@ export function defineRemoteResource<
     })
   }
 
+  const bufferedEvents: ['update' | 'remove', TData][] = []
+  let isBufferEventsProcessed = false
+  function handleBuffer(data: Ref<TData[]>) {
+    while (bufferedEvents.length > 0) {
+      const buffer = bufferedEvents.shift()
+      if (buffer === undefined) {
+        continue
+      }
+
+      const type = buffer[0]
+      const event = buffer[1]
+
+      removeData(data.value, event)
+      if (type !== 'remove') {
+        data.value.push(event)
+      }
+    }
+  }
+
   const onDataReceived =
     config.onDataReceived ??
     ((data: Ref<TData>, receivedData: any, context?: ResourceContext<TArgs>) => {
@@ -140,14 +159,24 @@ export function defineRemoteResource<
       if (config.watchCollection?.predicate?.(receivedData, context) === false) {
         return
       }
+
       if (data.value === undefined || (Array.isArray(data.value) && Array.isArray(receivedData))) {
         data.value = receivedData
+
+        if (config.watchCollection !== undefined && Array.isArray(data.value)) {
+          handleBuffer(data as Ref<TData[]>)
+          isBufferEventsProcessed = true
+        }
         return
       }
 
       if (Array.isArray(data.value)) {
-        removeData(data.value, receivedData)
-        data.value.push(receivedData)
+        if (!isBufferEventsProcessed) {
+          bufferedEvents.push(['update', receivedData])
+        } else {
+          removeData(data.value, receivedData)
+          data.value.push(receivedData)
+        }
         return
       }
 
@@ -164,7 +193,11 @@ export function defineRemoteResource<
 
       // for now only support `onDataRemoved` when watching XapiXoRecord collection
       if (Array.isArray(data.value) && !Array.isArray(receivedData)) {
-        removeData(data.value, receivedData)
+        if (!isBufferEventsProcessed) {
+          bufferedEvents.push(['remove', receivedData])
+        } else {
+          removeData(data.value, receivedData)
+        }
         return
       }
 
@@ -262,7 +295,6 @@ export function defineRemoteResource<
 
       pause = () => unwatch({ collectionId, resource, handleDelete })
       resume = async function () {
-        await execute()
         await watch({
           collectionId,
           handleWatching,
@@ -271,6 +303,7 @@ export function defineRemoteResource<
           onDataReceived: receivedData => onDataReceived(data, receivedData, context),
           onDataRemoved: receivedData => onDataRemoved(data, receivedData, context),
         })
+        await execute()
       }
     } else if (pollingInterval !== false) {
       const timeoutPoll = useTimeoutPoll(execute, pollingInterval, {