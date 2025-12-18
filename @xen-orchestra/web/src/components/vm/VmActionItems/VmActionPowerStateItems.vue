8450529afad66f8025f4192c9f6bbe2c766c6a95
MelissaFrncJrg <melissafranca3@gmail.com>
2025-12-18 12:05:32 +0100

    use composables

--- @xen-orchestra/web/src/components/vm/VmActionItems/VmActionPowerStateItems.vue
+++ @xen-orchestra/web/src/components/vm/VmActionItems/VmActionPowerStateItems.vue
@@ -1,21 +1,30 @@
 <template>
-  <MenuItem icon="fa:play" :disabled="isVmRunning" @click="handleStart">{{ t('start') }}</MenuItem>
+  <MenuItem icon="fa:play" :busy="isVmBusyToStart" :disabled="!isVmHalted || !canRunVmStart" @click="start">
+    {{ t('start') }}
+  </MenuItem>
 </template>
 
 <script lang="ts" setup>
-import { vmActions } from '@/actions/vm-actions'
+import { useVmStartJob } from '@/jobs/vm-start.job'
 import MenuItem from '@core/components/menu/MenuItem.vue'
 import { VM_POWER_STATE, type XoVm } from '@vates/types'
 import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 
-const { vm } = defineProps<{ vm: XoVm }>()
+const props = defineProps<{ vm: XoVm }>()
 
 const { t } = useI18n()
 
-const isVmRunning = computed(() => vm.power_state === VM_POWER_STATE.RUNNING)
+const isVmHalted = computed(() => props.vm.power_state === VM_POWER_STATE.HALTED)
 
-const handleStart = async () => {
-  await vmActions.start(vm.id)
+const selectedVms = computed(() => [props.vm])
+
+const { run: runVmStart, canRun: canRunVmStart, isRunning: isVmStartRunning } = useVmStartJob(selectedVms)
+
+const isVmBusyToStart = computed(() => isVmStartRunning.value)
+
+const start = () => {
+  if (!canRunVmStart.value || !isVmHalted.value) return
+  void runVmStart()
 }
 </script>
