51263d25d259756bd46ded47c36d3d5705cdca67
MelissaFrncJrg <melissafranca3@gmail.com>
2025-12-19 13:22:09 +0100

    changes after review

--- @xen-orchestra/web/src/composables/vm/xo-vm-start.composable.ts
+++ @xen-orchestra/web/src/composables/vm/xo-vm-start.composable.ts
@@ -1,17 +1,23 @@
 import { vmsArg } from '@/jobs/args'
-import { startVm } from '@/jobs/vm-start.job'
 import { isVmOperatingPending } from '@/utils/xo-records/vm.util'
 import { defineJob, JobError, JobRunningError } from '@core/packages/job'
 import { VM_OPERATIONS, VM_POWER_STATE } from '@vates/types'
+import { useFetch } from '@vueuse/core'
 import { useI18n } from 'vue-i18n'
 
-export const useVmStartJob = defineJob('vm.start', [vmsArg], () => {
+export const useJobVmStart = defineJob('vm.start', [vmsArg], () => {
   const { t } = useI18n()
 
   return {
     async run(vms) {
       for (const vm of vms) {
-        await startVm(vm)
+        const { error } = await useFetch(`/rest/v0/vms/${vm.id}/actions/start?sync=false`, {
+          method: 'POST',
+        }).json()
+
+        if (error.value) {
+          throw new Error(error.value.message)
+        }
       }
     },
 
