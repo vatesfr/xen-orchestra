1d776da4110f9d63c9c1905b3c3e133e48a6e1ad
Elise-F <elise.franzini@gmail.com>
2025-12-31 16:37:55 +0100

    feat(web core): create new component UiSubtitle

---
+++ @xen-orchestra/web-core/lib/components/ui/subtitle/UiSubtitle.vue
@@ -0,0 +1,18 @@
+<!-- v1 -->
+<template>
+  <div class="ui-subtitle typo-h6">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineSlots<{
+  default(): any
+}>()
+</script>
+
+<style scoped lang="postcss">
+.ui-subtitle {
+  color: var(--color-neutral-txt-primary);
+}
+</style>
