e82d625106a57052b6331f60f8678169ff3fcc53
MlssFrncJrg <119158464+MelissaFrncJrg@users.noreply.github.com>
2026-01-07 09:55:52 +0100

    Apply suggestions from code review
    Co-authored-by: Joris K <43381087+J0ris-K@users.noreply.github.com>

--- @xen-orchestra/web/src/components/vm/actions/VmActions.vue
+++ @xen-orchestra/web/src/components/vm/actions/VmActions.vue
@@ -4,7 +4,7 @@
 
 <script lang="ts" setup>
 import type { XoVm } from '@vates/types'
-import VmStartButton from './VmStartButton.vue'
+import VmStartButton from '@/components/vm/actions/VmStartButton.vue'
 
 defineProps<{
   vm: XoVm
