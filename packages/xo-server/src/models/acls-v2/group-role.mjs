676e20277f2ca0a3bce8910e0518206a4ee9a4b8
mathieuRA <contact@mathieu-raisin.fr>
2026-01-07 14:05:31 +0100

    feat(xo-server): create DB models for ACL V2

---
+++ packages/xo-server/src/models/acls-v2/group-role.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+
+import Collection from '../../collection/redis.mjs'
+
+export class GroupRoles extends Collection {
+  /**
+   * @param {object} params
+   * @param {string} params.connection
+   * @param {string[]} params.indexes
+   * @param {string} params.namespace
+   */
+  // eslint-disable-next-line no-useless-constructor
+  constructor(params) {
+    // @ts-ignore Collection.indexes is typed with never[] instead of string[]
+    super(params)
+  }
+}
