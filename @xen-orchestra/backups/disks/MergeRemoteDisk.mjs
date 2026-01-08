87b5a5eb13c43b4e32d34fdeec5321a8075896e4
debian <simon.pacotte@vates.tech>
2026-01-08 08:24:31 -0500

    MergeRemoteDisk and RemoteDisk improvement

--- @xen-orchestra/backups/disks/MergeRemoteDisk.mjs
+++ @xen-orchestra/backups/disks/MergeRemoteDisk.mjs
@@ -1,4 +1,3 @@
-
 /**
  * @typedef {import('./RemoteDisk.mjs').RemoteDisk} RemoteDisk
  */
@@ -13,7 +12,7 @@ import { RemoteVhdDisk } from './RemoteVhdDisk.mjs'
 const { warn } = createLogger('remote-disk:merge')
 
 /**
- * 
+ *
  * @typedef {Object} MergeState
  * @property {{ uuid: number }} child
  * @property {{ uuid: number }} parent
@@ -24,224 +23,253 @@ const { warn } = createLogger('remote-disk:merge')
  */
 
 export class MergeRemoteDisk {
-    /**
-     * @type {MergeState}
-     */
-    #state
+  /**
+   * @type {MergeState}
+   */
+  #state
 
-    /**
-     * @type {string}
-     */
-    #statePath
+  /**
+   * @type {string}
+   */
+  #statePath
 
-    /**
-     * @type {boolean}
-     */
-    #isResuming
+  /**
+   * @type {boolean}
+   */
+  #isResuming
 
-    /**
-     * @type {Logger}
-     */
-    #logInfo
+  /**
+   * @type {Logger}
+   */
+  #logInfo
 
-    /**
-     * @type {number}
-     */
-    #mergeBlockConcurrency
+  /**
+   * @type {number}
+   */
+  #mergeBlockConcurrency
 
-    /**
-     * @type {Function}
-     */
-    #onProgress
+  /**
+   * @type {Function}
+   */
+  #onProgress
 
-    /**
-     * @type {number}
-     */
-    #lastStateWrittenAt
+  /**
+   * @type {boolean}
+   */
+  #removeUnused
 
-    /**
-     * @type {FileAccessor}
-     */
-    #handler
+  /**
+   * @type {number}
+   */
+  #writeStateDelay
 
-    /**
-     * @param {FileAccessor} handler
-     * @param {Object} params
-     * @param {Function} params.onProgress
-     * @param {Logger} params.logInfo
-     * @param {boolean} params.removeUnused
-     * @param {number} params.mergeBlockConcurrency
-     */
-    constructor(handler, { onProgress = () => { }, logInfo = () => { }, mergeBlockConcurrency = 2 }) {
-        this.#handler = handler
-        this.#logInfo = logInfo
-        this.#onProgress = onProgress
-        this.#mergeBlockConcurrency = mergeBlockConcurrency
+  /**
+   * @type {number}
+   */
+  #lastStateWrittenAt
 
-        this.#isResuming = false
-        this.#lastStateWrittenAt = 0
+  /**
+   * @type {FileAccessor}
+   */
+  #handler
+
+  /**
+   * @param {FileAccessor} handler
+   * @param {Object} params
+   * @param {Function} params.onProgress
+   * @param {Logger} params.logInfo
+   * @param {boolean} params.removeUnused
+   * @param {number} params.mergeBlockConcurrency
+   * @param {number} params.writeStateDelay
+   */
+  constructor(
+    handler,
+    {
+      onProgress = () => {},
+      logInfo = () => {},
+      removeUnused = false,
+      mergeBlockConcurrency = 2,
+      writeStateDelay = 10e3,
+    }
+  ) {
+    this.#handler = handler
+    this.#logInfo = logInfo
+    this.#onProgress = onProgress
+    this.#removeUnused = removeUnused
+    this.#mergeBlockConcurrency = mergeBlockConcurrency
+    this.#writeStateDelay = writeStateDelay
+
+    this.#isResuming = false
+    this.#lastStateWrittenAt = 0
+  }
+
+  /**
+   * @param {RemoteDisk} parentDisk
+   * @param {RemoteDisk} childDisk
+   */
+  async merge(parentDisk, childDisk) {
+    this.#statePath = dirname(parentDisk.getPath()) + '/.' + basename(parentDisk.getPath()) + '.merge.json'
+
+    try {
+      const mergeStateContent = await this.#handler.readFile(this.#statePath)
+      this.#state = JSON.parse(mergeStateContent)
+
+      // work-around a bug introduce in 97d94b795
+      //
+      // currentBlock could be `null` due to the JSON.stringify of a `NaN` value
+      if (this.#state.currentBlock === null) this.#state.currentBlock = 0
+      this.#isResuming = true
+    } catch (error) {
+      if (error.code !== 'ENOENT') {
+        warn('problem while checking the merge state', { error })
+      }
     }
 
-    /**
-     * @param {RemoteDisk} parentDisk
-     * @param {RemoteDisk} childDisk
-     */
-    async merge(parentDisk, childDisk) {
-        this.#statePath = dirname(parentDisk.getPath()) + '/.' + basename(parentDisk.getPath()) + '.merge.json'
+    try {
+      /* eslint-disable no-fallthrough */
+      switch (this.#state?.step ?? 'mergeBlocks') {
+        case 'mergeBlocks':
+          await this.#step_mergeBlocks(parentDisk, childDisk)
+        case 'cleanup':
+          await this.#step_cleanup(parentDisk, childDisk)
+          return this.#cleanup(parentDisk, childDisk, true)
+        default:
+          warn(`Step ${this.#state.step} is unknown`, { state: this.#state })
+      }
+      /* eslint-enable no-fallthrough */
+    } catch (error) {
+      await this.#cleanup(parentDisk, childDisk, false)
+      throw error
+    }
+  }
 
-        try {
-            const mergeStateContent = await this.#handler.readFile(this.#statePath)
-            this.#state = JSON.parse(mergeStateContent)
+  async #writeState() {
+    try {
+      await this.#handler.writeFile(this.#statePath, JSON.stringify(this.#state), { flags: 'w' })
+    } catch (err) {
+      warn('failed to write merge state', { error: err })
+    }
+  }
 
-            // work-around a bug introduce in 97d94b795
-            //
-            // currentBlock could be `null` due to the JSON.stringify of a `NaN` value
-            if (this.#state.currentBlock === null) this.#state.currentBlock = 0
-            this.#isResuming = true
-        } catch (error) {
-            if (error.code !== 'ENOENT') {
-                warn('problem while checking the merge state', { error })
-            }
-        }
+  async #writeStateThrottled() {
+    const now = Date.now()
+    if (now - this.#lastStateWrittenAt > this.#writeStateDelay) {
+      this.#lastStateWrittenAt = now
+      await this.#writeState()
+    }
+  }
 
-        /* eslint-disable no-fallthrough */
-        switch (this.#state?.step ?? 'mergeBlocks') {
-            case 'mergeBlocks':
-                await this.#step_mergeBlocks(parentDisk, childDisk)
-            case 'cleanup':
-                await this.#step_cleanup(parentDisk, childDisk)
-                return this.#cleanup(parentDisk, childDisk)
-            default:
-                warn(`Step ${this.#state.step} is unknown`, { state: this.#state })
-        }
-        /* eslint-enable no-fallthrough */
+  /**
+   * @param {RemoteDisk} parentDisk
+   * @param {RemoteDisk} childDisk
+   */
+  async #step_mergeBlocks(parentDisk, childDisk) {
+    const getMaxBlockCount = childDisk.getMaxBlockCount()
+
+    if (!this.#state) {
+      this.#state = {
+        child: { uuid: childDisk.getUuid() ?? 0 },
+        parent: { uuid: parentDisk.getUuid() ?? 0 },
+        currentBlock: 0,
+        mergedDataSize: 0,
+        step: 'mergeBlocks',
+      }
+
+      // finds first allocated block for the 2 following loops
+      while (this.#state.currentBlock < getMaxBlockCount && !childDisk.hasBlock(this.#state.currentBlock)) {
+        ++this.#state.currentBlock
+      }
+      await this.#writeState()
+    } else {
+      // If we are resuming, we don't transfer the last transfered block again.
+      this.#state.currentBlock++
     }
 
-    async #writeState() {
-        try {
-            await this.#handler.writeFile(this.#statePath, JSON.stringify(this.#state), { flags: 'w' })
-        } catch (err) {
-            warn('failed to write merge state', { error: err })
-        }
+    await this.#mergeBlocks(parentDisk, childDisk)
+    await parentDisk.flushMetadata()
+    parentDisk.mergeMetadata(childDisk)
+  }
+
+  /**
+   * @param {RemoteDisk} parentDisk
+   * @param {RemoteDisk} childDisk
+   */
+  async #mergeBlocks(parentDisk, childDisk) {
+    if (parentDisk instanceof RemoteVhdDisk || childDisk instanceof RemoteVhdDisk) {
+      this.#mergeBlockConcurrency = 1 // TODO: TEST
     }
 
-    async #writeStateThrottled() {
-        const delay = 10e3
-        const now = Date.now()
-        if (now - this.#lastStateWrittenAt > delay) {
-            this.#lastStateWrittenAt = now
-            await this.#writeState()
-        }
+    const toMerge = []
+    for (let block = this.#state.currentBlock; block < childDisk.getMaxBlockCount(); block++) {
+      if (childDisk.hasBlock(block)) {
+        toMerge.push(block)
+      }
     }
 
-    /**
-     * @param {RemoteDisk} parentDisk
-     * @param {RemoteDisk} childDisk
-     */
-    async #step_mergeBlocks(parentDisk, childDisk) {
-        const getMaxBlockCount = childDisk.getMaxBlockCount()
+    const nBlocks = toMerge.length
+    this.#onProgress({ total: nBlocks, done: 0 })
 
-        if (!this.#state) {
-            this.#state = {
-                child: { uuid: childDisk.getUuid() ?? 0 },
-                parent: { uuid: parentDisk.getUuid() ?? 0 },
-                currentBlock: 0,
-                mergedDataSize: 0,
-                step: 'mergeBlocks',
-            }
+    let counter = 0
+    await asyncEach(
+      toMerge,
+      async blockId => {
+        const blockSize = await parentDisk.writeBlock(await childDisk.readBlock(blockId))
+        this.#state.mergedDataSize += blockSize
 
-            // finds first allocated block for the 2 following loops
-            while (this.#state.currentBlock < getMaxBlockCount && !childDisk.hasBlock(this.#state.currentBlock)) {
-                ++this.#state.currentBlock
-            }
-            await this.#writeState()
-        }
+        this.#state.currentBlock = blockId
 
-        await this.#mergeBlocks(parentDisk, childDisk)
-        await parentDisk.flushMetadata()
-        parentDisk.mergeMetadata(childDisk)
+        this.#onProgress({ total: nBlocks, done: counter + 1 })
+        counter++
+        await this.#writeStateThrottled()
+      },
+      { concurrency: this.#mergeBlockConcurrency }
+    )
+
+    await this.#writeState()
+
+    this.#state.diskSize = childDisk.getSizeOnDisk()
+
+    this.#onProgress({ total: nBlocks, done: nBlocks })
+  }
+
+  /**
+   * @param {RemoteDisk} parentDisk
+   * @param {RemoteDisk} childDisk
+   */
+  async #step_cleanup(parentDisk, childDisk) {
+    assert.notEqual(this.#state, undefined)
+    this.#state.step = 'cleanup'
+    await this.#writeState()
+
+    const newPath = childDisk.getPath()
+
+    await childDisk.unlink()
+
+    try {
+      await this.#handler.rename(parentDisk.getPath(), newPath)
+    } catch (error) {
+      if (error.code === 'ENOENT' && this.#isResuming) {
+        this.#logInfo(`the parent disk was already renamed`, { parent: parentDisk.getPath(), mergeTarget: newPath })
+      } else {
+        throw error
+      }
+    }
+  }
+
+  /**
+   * @param {RemoteDisk} parentDisk
+   * @param {RemoteDisk} childDisk
+   */
+  async #cleanup(parentDisk, childDisk, cleanStateFile) {
+    const finalDiskSize = this.#state?.diskSize ?? 0
+    const mergedDataSize = this.#state?.mergedDataSize ?? 0
+    await parentDisk.close().catch(warn)
+    await childDisk.close().catch(warn)
+
+    if (cleanStateFile) {
+      await this.#handler.unlink(this.#statePath).catch(warn)
     }
 
-    /**
-     * @param {RemoteDisk} parentDisk
-     * @param {RemoteDisk} childDisk
-     */
-    async #mergeBlocks(parentDisk, childDisk) {
-        if(parentDisk instanceof RemoteVhdDisk || childDisk instanceof RemoteVhdDisk){
-            this.#mergeBlockConcurrency = 1 // TODO: TEST
-        }
-
-        const toMerge = []
-        for (const block of childDisk.getBlockIndexes()) {
-            if (childDisk.hasBlock(block)) toMerge.push(block)
-        }
-
-        const nBlocks = toMerge.length
-        this.#onProgress({ total: nBlocks, done: 0 })
-
-        const merging = new Set()
-        let counter = 0
-
-        await asyncEach(
-            toMerge,
-            async blockId => {
-                merging.add(blockId)
-
-                const blockSize = await parentDisk.writeBlock((await childDisk.readBlock(blockId)))
-                this.#state.mergedDataSize += blockSize
-
-                this.#state.currentBlock = Math.min(...merging)
-                merging.delete(blockId)
-
-                this.#onProgress({ total: nBlocks, done: counter + 1 })
-                counter++
-                await this.#writeStateThrottled()
-            },
-            { concurrency: this.#mergeBlockConcurrency }
-        )
-
-        await this.#writeState()
-        
-        this.#state.diskSize = childDisk.getSize()
-
-        this.#onProgress({ total: nBlocks, done: nBlocks })
-    }
-
-    /**
-     * @param {RemoteDisk} parentDisk
-     * @param {RemoteDisk} childDisk
-     */
-    async #step_cleanup(parentDisk, childDisk) {
-        assert.notEqual(this.#state, undefined)
-        this.#state.step = 'cleanup'
-        await this.#writeState()
-
-        const newPath = childDisk.getPath()
-
-        await childDisk.unlink()
-
-        try {
-            await this.#handler.rename(parentDisk.getPath(), newPath)
-        } catch (error) {
-            if (error.code === 'ENOENT' && this.#isResuming) {
-                this.#logInfo(`the parent disk was already renamed`, { parent: parentDisk.getPath(), mergeTarget: newPath })
-            } else {
-                throw error
-            }
-        }
-    }
-
-    /**
-     * @param {RemoteDisk} parentDisk
-     * @param {RemoteDisk} childDisk
-     */
-    async #cleanup(parentDisk, childDisk) {
-        console.log('close')
-        const finalDiskSize = this.#state?.diskSize ?? 0
-        const mergedDataSize = this.#state?.mergedDataSize ?? 0
-        await this.#handler.unlink(this.#statePath).catch(warn)
-        await parentDisk.close().catch(warn)
-        await childDisk.close().catch(warn)
-        return { mergedDataSize, finalDiskSize }
-    }
+    return { mergedDataSize, finalDiskSize }
+  }
 }
--- @xen-orchestra/backups/disks/RemoteDisk.mjs
+++ @xen-orchestra/backups/disks/RemoteDisk.mjs
@@ -4,136 +4,137 @@
  * @typedef {import('@xen-orchestra/disk-transform').DiskBlock} DiskBlock
  */
 
-import { RandomAccessDisk } from "@xen-orchestra/disk-transform"
+import { RandomAccessDisk } from '@xen-orchestra/disk-transform'
 export class RemoteDisk extends RandomAccessDisk {
-    /**
-     * Abstract
-     * @returns {Promise<void>}
-     */
-    async init() {
-        throw new Error(`init must be implemented`)
-    }
+  /**
+   * Abstract
+   * @param {boolean} force
+   * @returns {Promise<void>}
+   */
+  async init(force = false) {
+    throw new Error(`init must be implemented`)
+  }
 
-    /**
-     * Abstract
-     * @returns {Promise<void>}
-     */
-    async close() {
-        throw new Error(`close must be implemented`)
-    }
+  /**
+   * Abstract
+   * @returns {Promise<void>}
+   */
+  async close() {
+    throw new Error(`close must be implemented`)
+  }
 
-    /**
-     * Abstract
-     * @returns {number}
-     */
-    getVirtualSize() {
-        throw new Error(`getVirtualSize must be implemented`)
-    }
+  /**
+   * Abstract
+   * @returns {number}
+   */
+  getVirtualSize() {
+    throw new Error(`getVirtualSize must be implemented`)
+  }
 
-    /**
-     * Abstract
-     * @returns {number} size
-     */
-    getSize() {
-        throw new Error(`getSize must be implemented`)
-    }
+  /**
+   * Abstract
+   * @returns {number} size
+   */
+  getSizeOnDisk() {
+    throw new Error(`getSizeOnDisk must be implemented`)
+  }
 
-    /**
-     * @returns {number}
-     */
-    getBlockSize() {
-        throw new Error(`getBlockSize must be implemented`)
-    }
+  /**
+   * @returns {number}
+   */
+  getBlockSize() {
+    throw new Error(`getBlockSize must be implemented`)
+  }
 
-    /**
-     * Abstract
-     * @returns {string}
-     */
-    getPath() {
-        throw new Error(`getPath must be implemented`)
-    }
+  /**
+   * Abstract
+   * @returns {string}
+   */
+  getPath() {
+    throw new Error(`getPath must be implemented`)
+  }
 
-    /**
-     * Abstract
-     * @returns {string}
-     */
-    getUuid() {
-        throw new Error(`getUuid must be implemented`)
-    }
+  /**
+   * Abstract
+   * @returns {string}
+   */
+  getUuid() {
+    throw new Error(`getUuid must be implemented`)
+  }
 
-    /**
-     * Abstract
-     * @returns {number} getMaxBlockCount
-     */
-    getMaxBlockCount() {
-        throw new Error(`getMaxBlockCount must be implemented`)
-    }
+  /**
+   * Abstract
+   * @returns {number} getMaxBlockCount
+   */
+  getMaxBlockCount() {
+    throw new Error(`getMaxBlockCount must be implemented`)
+  }
 
-    /**
-     * Checks if the VHD contains a specific block.
-     * @param {number} index
-     * @returns {boolean}
-     */
-    hasBlock(index) {
-        throw new Error(`hasBlock must be implemented`)
-    }
+  /**
+   * Checks if the VHD contains a specific block.
+   * @param {number} index
+   * @returns {boolean}
+   */
+  hasBlock(index) {
+    throw new Error(`hasBlock must be implemented`)
+  }
 
-    /**
-     * Gets the indexes of all blocks in the VHD.
-     * @returns {Array<number>}
-     */
-    getBlockIndexes() {
-        throw new Error(`getBlockIndexes must be implemented`)
-    }
+  /**
+   * Gets the indexes of all blocks in the VHD.
+   * @returns {Array<number>}
+   */
+  getBlockIndexes() {
+    throw new Error(`getBlockIndexes must be implemented`)
+  }
 
-    /**
-     * Abstract
-     * Writes a full block.
-     * @param {DiskBlock} diskBlock
-     * @return {Promise<number>} blockSize
-     */
-    async writeBlock(diskBlock) {
-        throw new Error(`writeBlock must be implemented`)
-    }
+  /**
+   * Abstract
+   * Writes a full block.
+   * @param {DiskBlock} diskBlock
+   * @return {Promise<number>} blockSize
+   */
+  async writeBlock(diskBlock) {
+    throw new Error(`writeBlock must be implemented`)
+  }
 
-    /**
-     * Reads a specific block from the VHD.
-     * @param {number} index
-     * @returns {Promise<DiskBlock>} diskBlock
-     */
-    async readBlock(index) {
-        throw new Error(`readBlock must be implemented`)
-    }
+  /**
+   * Reads a specific block from the VHD.
+   * @param {number} index
+   * @returns {Promise<DiskBlock>} diskBlock
+   */
+  async readBlock(index) {
+    throw new Error(`readBlock must be implemented`)
+  }
 
-    /**
-     * Abstract
-     * @returns {Promise<void>}
-     */
-    async flushMetadata() {
-        throw new Error(`flushMetadata must be implemented`)
-    }
+  /**
+   * Abstract
+   * @returns {Promise<void>}
+   */
+  async flushMetadata() {
+    throw new Error(`flushMetadata must be implemented`)
+  }
 
-    /**
-     * Abstract
-     * @param {RemoteDisk} child
-     */
-    mergeMetadata(child) {
-        throw new Error(`mergeMetadata must be implemented`)
-    }
+  /**
+   * Abstract
+   * @param {RemoteDisk} child
+   */
+  mergeMetadata(child) {
+    throw new Error(`mergeMetadata must be implemented`)
+  }
 
-    /**
-     * Checks if the VHD is a differencing disk.
-     * @returns {boolean}
-     */
-    isDifferencing() {
-        throw new Error(`isDifferencing must be implemented`)
-    }
+  /**
+   * Checks if the VHD is a differencing disk.
+   * @returns {boolean}
+   */
+  isDifferencing() {
+    throw new Error(`isDifferencing must be implemented`)
+  }
 
-    /**
-     * Abstract
-     * Deletes disk
-     */
-    async unlink() { 
-        throw new Error(`unlink must be implemented`)
-    }
+  /**
+   * Abstract
+   * Deletes disk
+   */
+  async unlink() {
+    throw new Error(`unlink must be implemented`)
+  }
 }
