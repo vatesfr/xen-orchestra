18de788c2da581e89d147e89e70c714f7e7d04a3
Joris-K <joris.kosacki@vates.tech>
2026-01-09 15:33:52 +0100

    update with new info from api

---
+++ @xen-orchestra/web/src/components/modals/VmProtected.vue
@@ -0,0 +1,13 @@
+<template>
+  <VtsModal icon="fa:user-astronaut" dismissible accent="info">
+    <template #title>{{ t('what-does-protected-means?') }}</template>
+    <template #content>{{ t('what-does-protected-means-content') }}</template>
+  </VtsModal>
+</template>
+
+<script setup lang="ts">
+import VtsModal from '@core/components/modal/VtsModal.vue'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+</script>
