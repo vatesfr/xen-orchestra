1466eddaee6d96d3a0324d60dd2d7f648f5cc69b
Elise-F <elise.franzini@gmail.com>
2026-01-12 14:52:22 +0100

    feat(xo6): add vtpm support

--- @xen-orchestra/web/src/pages/vm/new.vue
+++ @xen-orchestra/web/src/pages/vm/new.vue
@@ -212,7 +212,6 @@ import UiTextarea from '@core/components/ui/text-area/UiTextarea.vue'
 import UiTitle from '@core/components/ui/title/UiTitle.vue'
 import UiToaster from '@core/components/ui/toaster/UiToaster.vue'
 import { useRouteQuery } from '@core/composables/route-query.composable'
-import { vTooltip } from '@core/directives/tooltip.directive'
 import { useFormSelect } from '@core/packages/form-select'
 import { useUiStore } from '@core/stores/ui.store'
 import type { XoPool } from '@vates/types'
@@ -765,7 +764,7 @@ const xo5Link = computed(() => {
 
 // BOOT FIRMWARE SELECTOR
 
-const bootFirmwareOptions = ['bios', 'uefi']
+const bootFirmwareOptions = ['BIOS', 'UEFI']
 
 const { id: bootFirmwareSelectId } = useFormSelect(bootFirmwareOptions, {
   model: toRef(vmState, 'bootFirmware'),
