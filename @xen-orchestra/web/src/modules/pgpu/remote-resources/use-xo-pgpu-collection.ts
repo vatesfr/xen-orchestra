4e6aff22ec4c5d16bef251ffe2e84d66393acd6b
Elise-F <elise.franzini@gmail.com>
2026-01-15 11:59:44 +0100

    tech(xo6):change of sse utils for a composable, update of the define-remote-resource file and change of calls to use the new composable

--- @xen-orchestra/web/src/modules/pgpu/remote-resources/use-xo-pgpu-collection.ts
+++ @xen-orchestra/web/src/modules/pgpu/remote-resources/use-xo-pgpu-collection.ts
@@ -1,6 +1,6 @@
 import { useXoCollectionState } from '@/shared/composables/xo-collection-state/use-xo-collection-state.ts'
 import { BASE_URL } from '@/shared/utils/fetch.util.ts'
-import { watchCollectionWrapper } from '@/shared/utils/sse.util.ts'
+import { useWatchCollection } from '@core/composables/watch-collection.composable.ts'
 import { defineRemoteResource } from '@core/packages/remote-resource/define-remote-resource.ts'
 import type { XoPgpu } from '@vates/types'
 
@@ -8,7 +8,7 @@ const pgpuFields: (keyof XoPgpu)[] = ['id', 'pci', 'type'] as const
 
 export const useXoPgpuCollection = defineRemoteResource({
   url: `${BASE_URL}/pgpus?fields=${pgpuFields.join(',')}`,
-  watchCollection: watchCollectionWrapper({ resource: 'PGPU', fields: pgpuFields }),
+  initWatchCollection: () => useWatchCollection({ resource: 'PGPU', fields: pgpuFields }),
   initialData: () => [] as XoPgpu[],
   state: (pgpus, context) =>
     useXoCollectionState(pgpus, {
