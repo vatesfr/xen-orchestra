22cbc2237c40507233a02ed7cb9d383704c27176
Joris-K <joris.kosacki@vates.tech>
2026-01-16 11:16:56 +0100

    feat(xo6): add VMs actions lifecycle

---
+++ @xen-orchestra/web/src/components/vm/actions/VmPauseButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <MenuItem v-if="canRun || isRunning" icon="fa:pause" :busy="isRunning" @click="pause">
+    {{ t('pause') }}
+  </MenuItem>
+</template>
+
+<script setup lang="ts">
+import { useVmPauseJob } from '@/jobs/vm/vm-pause.job.ts'
+import MenuItem from '@core/components/menu/MenuItem.vue'
+import type { XoVm } from '@vates/types'
+import { useI18n } from 'vue-i18n'
+
+const { vm } = defineProps<{
+  vm: XoVm
+}>()
+
+const { t } = useI18n()
+
+const { run: pause, canRun, isRunning } = useVmPauseJob(() => [vm])
+</script>
--- @xen-orchestra/web/src/jobs/vm/vm-pause.job.ts
+++ @xen-orchestra/web/src/jobs/vm/vm-pause.job.ts
@@ -1,18 +1,18 @@
-import { vmsArg } from '@/jobs/args'
+import { vmsArg } from '@/jobs/args.ts'
 import { isVmOperatingPending } from '@/utils/xo-records/vm.util'
 import { defineJob, JobError, JobRunningError } from '@core/packages/job'
 import { VM_OPERATIONS, VM_POWER_STATE, type XoVm } from '@vates/types'
 import { useFetch } from '@vueuse/core'
 import { useI18n } from 'vue-i18n'
 
-export const useVmStartJob = defineJob('vm.start', [vmsArg], () => {
+export const useVmPauseJob = defineJob('vm.pause', [vmsArg], () => {
   const { t } = useI18n()
 
   return {
     async run(vms: XoVm[]) {
       await Promise.all(
         vms.map(async vm => {
-          const { error } = await useFetch(`/rest/v0/vms/${vm.id}/actions/start?sync=true`, {
+          const { error } = await useFetch(`/rest/v0/vms/${vm.id}/actions/pause?sync=true`, {
             method: 'POST',
           })
 
@@ -23,17 +23,17 @@ export const useVmStartJob = defineJob('vm.start', [vmsArg], () => {
       )
     },
 
-    validate(isRunning, vms: XoVm[]) {
+    validate: (isRunning, vms: XoVm[]) => {
       if (!vms || vms.length === 0) {
-        throw new JobError(t('job.vm-start.missing-vms'))
+        throw new JobError(t('job.vm-pause.missing-vms'))
       }
 
-      if (isRunning || vms.some(vm => isVmOperatingPending(vm, VM_OPERATIONS.START))) {
-        throw new JobRunningError(t('job.vm-start.in-progress'))
+      if (isRunning || vms.some(vm => isVmOperatingPending(vm, VM_OPERATIONS.PAUSE))) {
+        throw new JobRunningError(t('job.vm-pause.in-progress'))
       }
 
-      if (vms.some(vm => vm.power_state !== VM_POWER_STATE.HALTED)) {
-        throw new JobError(t('job.vm-start.bad-power-state'))
+      if (vms.some(vm => vm.power_state !== VM_POWER_STATE.RUNNING)) {
+        throw new JobError(t('job.vm-pause.bad-power-state'))
       }
     },
   }
