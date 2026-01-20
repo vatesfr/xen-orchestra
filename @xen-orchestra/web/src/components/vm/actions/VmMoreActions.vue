400696158eb81fcf725a9a2c83cf4bcb49696a7c
Joris-K <joris.kosacki@vates.tech>
2026-01-20 10:14:51 +0100

    feat(xo6/vm): add more actions button to delete vm

---
+++ @xen-orchestra/web/src/components/vm/actions/VmMoreActions.vue
@@ -0,0 +1,12 @@
+<template>
+  <VmDeleteButton :vm />
+</template>
+
+<script lang="ts" setup>
+import VmDeleteButton from '@/components/vm/actions/VmDeleteButton.vue'
+import type { XoVm } from '@vates/types'
+
+defineProps<{
+  vm: XoVm
+}>()
+</script>
