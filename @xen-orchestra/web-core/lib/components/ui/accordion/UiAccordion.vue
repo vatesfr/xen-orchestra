9ebb5e26e523fd6f30e800b380c79b5688a1f127
unel <enee.prog@gmail.com>
2026-01-27 16:09:30 +0100

    feat(web-core/accordion): create accordion and addcordionItem component

---
+++ @xen-orchestra/web-core/lib/components/ui/accordion/UiAccordion.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="accondion">
+    <UiAccordionItem
+      accent="large"
+      title="test"
+      :is-expanded="expandedIndex === 0"
+      text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. "
+      @toggle="toggle(0)"
+    />
+    <UiAccordionItem
+      accent="large"
+      title="test"
+      :is-expanded="expandedIndex === 1"
+      text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. "
+      @toggle="toggle(1)"
+    />
+    <UiAccordionItem
+      accent="large"
+      title="test"
+      :is-expanded="expandedIndex === 2"
+      text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. "
+      @toggle="toggle(2)"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import UiAccordionItem from '../accordion-item/UiAccordionItem.vue'
+
+const expandedIndex = ref<number | null>(0)
+
+const toggle = (index: number) => {
+  expandedIndex.value = expandedIndex.value === index ? null : index
+}
+</script>
+
+<style scoped lang="postcss">
+.accondion {
+  display: flex;
+  flex-direction: column;
+  gap: 2.4rem;
+}
+</style>
