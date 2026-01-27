7605fab809ffb5ad35603b282f6a1a3873c6497a
Olivier Floch <olivier.floch@vates.fr>
2026-01-27 15:59:06 +0100

    use mapping to get texts by object and action in VtsActionModal

--- @xen-orchestra/web-core/lib/components/modal/VtsActionModal.vue
+++ @xen-orchestra/web-core/lib/components/modal/VtsActionModal.vue
@@ -1,15 +1,15 @@
 <template>
   <VtsModal :accent icon="fa:user-astronaut">
     <template #title>
-      <span>{{ title }}</span>
+      <span>{{ modalTexts.title }}</span>
     </template>
     <template #content>
-      <span>{{ message }}</span>
+      <span>{{ modalTexts.message }}</span>
     </template>
     <template #buttons>
       <VtsModalCancelButton>{{ t('action:go-back') }}</VtsModalCancelButton>
       <VtsModalConfirmButton>
-        {{ actionText }}
+        {{ modalTexts.action }}
       </VtsModalConfirmButton>
     </template>
   </VtsModal>
@@ -20,20 +20,53 @@ import VtsModal from '@core/components/modal/VtsModal.vue'
 import VtsModalCancelButton from '@core/components/modal/VtsModalCancelButton.vue'
 import VtsModalConfirmButton from '@core/components/modal/VtsModalConfirmButton.vue'
 import type { ModalAccent } from '@core/components/ui/modal/UiModal.vue'
-import { computed } from 'vue'
+import { useMapper } from '@core/packages/mapper/use-mapper.ts'
 import { useI18n } from 'vue-i18n'
 
+type ObjectType = 'vm'
+type XoVmActions = 'reboot' | 'shutdown' | 'force-reboot' | 'force-shutdown'
+type ActionTexts = {
+  title: string
+  message: string
+  action: string
+}
+
 const { action, object } = defineProps<{
-  action: 'reboot' | 'shutdown' | 'force-reboot' | 'force-shutdown'
+  action: XoVmActions
   accent: ModalAccent
-  object: 'vm' // add more object if needed
+  object: ObjectType
 }>()
 
 const { t } = useI18n()
 
-const title = computed(() => t(`modal:confirm-object-${action}`, { object }))
+const textMappingsByObject: Record<ObjectType, Record<XoVmActions, ActionTexts>> = {
+  vm: {
+    'force-reboot': {
+      title: t('modal:confirm-vm-force-reboot'),
+      message: t('modal:vm-force-reboot-message'),
+      action: t('modal:action:vm-force-reboot'),
+    },
+    'force-shutdown': {
+      title: t('modal:confirm-vm-force-shutdown'),
+      message: t('modal:vm-force-shutdown-message'),
+      action: t('modal:action:vm-force-shutdown'),
+    },
+    reboot: {
+      title: t('modal:confirm-vm-reboot'),
+      message: t('modal:vm-reboot-message'),
+      action: t('modal:action:vm-reboot'),
+    },
+    shutdown: {
+      title: t('modal:confirm-vm-shutdown'),
+      message: t('modal:vm-shutdown-message'),
+      action: t('modal:action:vm-shutdown'),
+    },
+  },
+}
 
-const message = computed(() => t(`modal:${object}-${action}-message`))
-
-const actionText = computed(() => t(`modal:action:${action}`, { object }))
+const modalTexts = useMapper(
+  () => action,
+  () => textMappingsByObject[object],
+  () => 'shutdown'
+)
 </script>
