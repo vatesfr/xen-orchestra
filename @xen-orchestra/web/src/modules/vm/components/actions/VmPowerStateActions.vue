9be096ddf4177b98374c3d44f8d389dc98d4af72
Joris-K <joris.kosacki@vates.tech>
2026-01-28 16:01:22 +0100

    fix after rebase

--- @xen-orchestra/web/src/modules/vm/components/actions/VmPowerStateActions.vue
+++ @xen-orchestra/web/src/modules/vm/components/actions/VmPowerStateActions.vue
@@ -1,9 +1,27 @@
 <template>
   <VmStartButton :vm />
+  <VmStartOnButton :vm />
+  <VmPauseButton :vm />
+  <VmUnpauseButton :vm />
+  <VmResumeButton :vm />
+  <VmSuspendButton :vm />
+  <VmRebootButton :vm />
+  <VmForceRebootButton :vm />
+  <VmShutdownButton :vm />
+  <VmForceShutdownButton :vm />
 </template>
 
 <script lang="ts" setup>
+import VmPauseButton from '@/modules/vm/components/actions/pause/VmPauseButton.vue'
+import VmForceRebootButton from '@/modules/vm/components/actions/reboot/VmForceRebootButton.vue'
+import VmRebootButton from '@/modules/vm/components/actions/reboot/VmRebootButton.vue'
+import VmResumeButton from '@/modules/vm/components/actions/resume/VmResumeButton.vue'
+import VmForceShutdownButton from '@/modules/vm/components/actions/shutdown/VmForceShutdownButton.vue'
+import VmShutdownButton from '@/modules/vm/components/actions/shutdown/VmShutdownButton.vue'
 import VmStartButton from '@/modules/vm/components/actions/start/VmStartButton.vue'
+import VmStartOnButton from '@/modules/vm/components/actions/start/VmStartOnButton.vue'
+import VmSuspendButton from '@/modules/vm/components/actions/suspend/VmSuspendButton.vue'
+import VmUnpauseButton from '@/modules/vm/components/actions/unpause/VmUnpauseButton.vue'
 import type { XoVm } from '@vates/types'
 
 defineProps<{
