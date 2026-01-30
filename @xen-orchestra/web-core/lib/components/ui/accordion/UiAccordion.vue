f99f6aa77ba8ab885da32944b6be40d2ae613e4c
unel <enee.prog@gmail.com>
2026-01-30 14:36:28 +0100

    feat(web-core/accordion): improve toggle

--- @xen-orchestra/web-core/lib/components/ui/accordion/UiAccordion.vue
+++ @xen-orchestra/web-core/lib/components/ui/accordion/UiAccordion.vue
@@ -1,41 +1,22 @@
 <template>
   <div class="accondion">
-    <UiAccordionItem
-      :size
-      title="test"
-      :is-expanded="expandedIndex === 0"
-      content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. "
-      @toggle="toggle(0)"
-    />
-    <UiAccordionItem
-      :size
-      title="test"
-      :is-expanded="expandedIndex === 1"
-      content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. "
-      @toggle="toggle(1)"
-    />
-    <UiAccordionItem
-      :size
-      title="test"
-      :is-expanded="expandedIndex === 2"
-      content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. "
-      disabled
-      @toggle="toggle(2)"
-    />
+    <slot />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import UiAccordionItem from '../accordion-item/UiAccordionItem.vue'
+import { ref, provide } from 'vue'
 
-defineProps<{ size: 'small' | 'large' }>()
+const expandedKey = ref<string | null>(null)
 
-const expandedIndex = ref<number | null>(0)
-
-const toggle = (index: number) => {
-  expandedIndex.value = expandedIndex.value === index ? null : index
+const toggle = (key: string) => {
+  expandedKey.value = expandedKey.value === key ? null : key
 }
+
+provide('accordion', {
+  expandedKey,
+  toggle,
+})
 </script>
 
 <style scoped lang="postcss">
