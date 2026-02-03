9df08ccf4e2b4705ecf55c2b93ffdfb0ad5fcb53
Florent BEAUCHAMP <florent.beauchamp@vates.tech>
2026-02-03 14:40:24 +0000

    better handling of vm. desynchro

--- @xen-orchestra/backups/_runners/_vmRunners/_AbstractXapi.mjs
+++ @xen-orchestra/backups/_runners/_vmRunners/_AbstractXapi.mjs
@@ -378,7 +378,7 @@ export const AbstractXapi = class AbstractXapiVmBackupRunner extends Abstract {
 
     // list and remove the snapshot were the jobs failed between
     // makesnapshot and update_other_config
-    const snapshots = this._vm.$snapshots.filter(({ name_label }) => name_label === TEMP_SNAPSHOT_NAME)
+    const snapshots = this._vm.$snapshots.filter(_ => !!_).filter(({ name_label }) => name_label === TEMP_SNAPSHOT_NAME)
     await asyncEach(snapshots, snapshot => snapshot.$destroy())
   }
 
