af446c4cf9d5db74113c1a2f29695d0026895d37
Joris-K <joris.kosacki@vates.tech>
2026-02-03 11:23:32 +0100

    feedback

--- @xen-orchestra/web/src/modules/vm/components/actions/delete/VmDeleteButton.vue
+++ @xen-orchestra/web/src/modules/vm/components/actions/delete/VmDeleteButton.vue
@@ -11,6 +11,7 @@ import MenuItem from '@core/components/menu/MenuItem.vue'
 import { useModal } from '@core/packages/modal/use-modal.ts'
 import type { XoVm } from '@vates/types'
 import { useI18n } from 'vue-i18n'
+import { useRouter } from 'vue-router'
 
 const { vm } = defineProps<{
   vm: XoVm
@@ -22,10 +23,24 @@ const { run: deleteVM, canRun, isRunning } = useXoVmDeleteJob(() => [vm])
 
 const { xo5VmAdvancedHref } = useXoVmUtils(() => vm)
 
+const router = useRouter()
+
 const openDeleteModal = useModal({
   component: import('@/modules/vm/components/modal/VmDeleteModal.vue'),
   props: { count: 1 },
-  onConfirm: () => deleteVM(),
+  onConfirm: async () => {
+    // TODO we can improve it in the future by handling errors in a better way
+    let result
+    try {
+      result = await deleteVM()
+    } catch (error) {
+      console.error('Error when deleting VM:', error)
+    }
+
+    if (result && result[0].status === 'fulfilled' && router.currentRoute.value.path.includes(`/vm/${vm.id}`)) {
+      await router.push({ name: '/pool/[id]/dashboard', params: { id: vm.$pool } })
+    }
+  },
 })
 
 const openBlockedModal = useModal({
