a05586df3930e825a1f22349d7d73aacfeef9926
Pierre Briffaux <pierre.briffaux@vates.tech>
2026-02-04 11:35:21 +0100

    feat(web-core): added changelog and button hiding if browser feature not available anymore
    Signed-off-by: Pierre Briffaux <pierre.briffaux@vates.tech>

--- @xen-orchestra/web-core/lib/components/copy-button/VtsCopyButton.vue
+++ @xen-orchestra/web-core/lib/components/copy-button/VtsCopyButton.vue
@@ -1,5 +1,12 @@
 <template>
-  <UiButtonIcon v-tooltip="copied && t('copied')" :icon size="small" accent="brand" @click="copy()" />
+  <UiButtonIcon
+    v-tooltip="!isSupported ? t('copy-unavailable-http') : copied && t('copied')"
+    :disabled="!isSupported"
+    :icon
+    size="small"
+    accent="brand"
+    @click="copy()"
+  />
 </template>
 
 <script setup lang="ts">
@@ -15,9 +22,7 @@ const { value } = defineProps<{
 
 const { t } = useI18n()
 
-const { copy, copied } = useClipboard({ source: () => value, legacy: true })
+const { copy, copied, isSupported } = useClipboard({ source: () => value, legacy: true })
 
 const icon = computed(() => (copied.value ? 'fa:check-circle' : 'fa:copy'))
-
-// TODO : Add warning if copy fails
 </script>
