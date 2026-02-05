2e7a62967e1f9691cf452000e33aca33b14f8a3d
Elise-F <elise.franzini@gmail.com>
2026-02-05 16:15:53 +0100

    feat(sidepanel task): cloud_config display update

--- @xen-orchestra/web/src/modules/task/components/list/panel/cards/TaskPropertiesCard.vue
+++ @xen-orchestra/web/src/modules/task/components/list/panel/cards/TaskPropertiesCard.vue
@@ -4,8 +4,9 @@
       {{ t('properties') }}
     </UiCardTitle>
     <div class="content">
-      <VtsRecursiveFields :fields="properties.other" />
+      <VtsRecursiveFields :fields="propertiesOtherWithoutCloudConfig" />
     </div>
+    <UiLogEntryViewer :content="cloudConfig" :label="t('cloud-config')" size="small" accent="info" />
     <UiLogEntryViewer :content="properties.other" :label="t('other-properties')" size="small" accent="info" />
   </UiCard>
 </template>
@@ -17,6 +18,7 @@ import VtsRecursiveFields from '@core/components/recursive-fields/VtsRecursiveFi
 import UiCard from '@core/components/ui/card/UiCard.vue'
 import UiCardTitle from '@core/components/ui/card-title/UiCardTitle.vue'
 import UiLogEntryViewer from '@core/components/ui/log-entry-viewer/UiLogEntryViewer.vue'
+import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 
 const { task } = defineProps<{
@@ -26,6 +28,28 @@ const { task } = defineProps<{
 const { t } = useI18n()
 
 const { properties } = useXoTaskPropertiesUtils(() => task)
+
+const cloudConfig = computed(() => {
+  const args = properties.value.other?.args as { cloud_config?: string } | undefined
+  return args?.cloud_config
+})
+
+const propertiesOtherWithoutCloudConfig = computed(() => {
+  const other = properties.value.other
+  const args = other?.args as { cloud_config?: string } | undefined
+
+  if (!args?.cloud_config) {
+    return other
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { cloud_config, ...argsCopy } = args
+
+  return {
+    ...other,
+    args: argsCopy,
+  }
+})
 </script>
 
 <style scoped lang="postcss">
