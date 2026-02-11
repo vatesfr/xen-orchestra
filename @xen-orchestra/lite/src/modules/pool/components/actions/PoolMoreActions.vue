64059d63612f5818f72fc4d2ae8f359ef35f128e
unel <enee.prog@gmail.com>
2026-02-11 16:00:30 +0100

    feat(xo6/PoolMenu): update architecture

---
+++ @xen-orchestra/lite/src/modules/pool/components/actions/PoolMoreActions.vue
@@ -0,0 +1,12 @@
+<template>
+  <PoolDownloadButton :master-host="pool.master" />
+</template>
+
+<script setup lang="ts">
+import type { XenApiPool } from '@/libs/xen-api/xen-api.types.ts'
+import PoolDownloadButton from '@/modules/pool/components/actions/download/PoolDownloadButton.vue'
+
+defineProps<{
+  pool: XenApiPool
+}>()
+</script>
