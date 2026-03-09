45c96e366bc1f74b31d6d69ac3fa282204068310
Pierre Briffaux <pierre.briffaux@vates.tech>
2026-03-09 20:10:54 +0100

    feat(xo6): add console clipboard support
    Signed-off-by: Pierre Briffaux <pierre.briffaux@vates.tech>

--- @xen-orchestra/web-core/lib/components/console/VtsClipboardConsole.vue
+++ @xen-orchestra/web-core/lib/components/console/VtsClipboardConsole.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="vts-clipboard-console">
     <UiCardTitle>{{ t('console-clipboard') }}</UiCardTitle>
-    <UiTextarea v-tooltip="t('coming-soon!')" accent="brand" disabled :model-value="modelValue" />
+    <UiTextarea accent="brand" :model-value="modelValue" @update:model-value="modelValue = $event" />
+    <UiInfo v-if="!hasGuestTools" accent="warning">
+      {{ t('no-xen-tools-detected') }}
+    </UiInfo>
     <div class="buttons-container">
-      <UiButton v-tooltip="t('coming-soon!')" accent="brand" variant="primary" size="medium" disabled>
+      <UiButton accent="brand" variant="primary" size="medium" @click="onSend">
         {{ t('action:send') }}
       </UiButton>
-      <UiButton v-tooltip="t('coming-soon!')" accent="brand" variant="secondary" size="medium" disabled>
+      <UiButton
+        v-tooltip="!isSupported ? t('copy-unavailable-http') : undefined"
+        accent="brand"
+        variant="secondary"
+        size="medium"
+        :disabled="!hasGuestTools"
+        @click="onReceive"
+      >
         {{ t('receive') }}
       </UiButton>
     </div>
@@ -16,14 +26,37 @@
 <script setup lang="ts">
 import UiButton from '@core/components/ui/button/UiButton.vue'
 import UiCardTitle from '@core/components/ui/card-title/UiCardTitle.vue'
+import UiInfo from '@core/components/ui/info/UiInfo.vue'
 import UiTextarea from '@core/components/ui/text-area/UiTextarea.vue'
 import { vTooltip } from '@core/directives/tooltip.directive'
-import { ref } from 'vue'
+import { useClipboard } from '@vueuse/core'
+import { ref, watch } from 'vue'
 import { useI18n } from 'vue-i18n'
+
+const props = defineProps<{
+  clipboardText: string
+  sendClipboard: (text: string) => void
+  hasGuestTools?: boolean | undefined
+}>()
+
+const { t } = useI18n()
 
 const modelValue = ref('')
 
-const { t } = useI18n()
+watch(
+  () => props.clipboardText,
+  text => {
+    modelValue.value = text
+  }
+)
+
+const { text, isSupported } = useClipboard({ read: true, legacy: true })
+
+const onSend = () => props.sendClipboard(modelValue.value)
+
+const onReceive = () => {
+  modelValue.value = text.value
+}
 </script>
 
 <style lang="postcss" scoped>
@@ -35,6 +68,7 @@ const { t } = useI18n()
 
   .buttons-container {
     display: flex;
+    align-items: center;
     gap: 0.8rem;
   }
 }
