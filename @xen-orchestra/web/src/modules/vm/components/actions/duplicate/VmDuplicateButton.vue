8c394ff73e90337fadd7289b5baaeabddff79e1b
Elise-F <elise.franzini@gmail.com>
2026-03-09 14:39:31 +0100

    feat(xo6): add VM duplicate action

---
+++ @xen-orchestra/web/src/modules/vm/components/actions/duplicate/VmDuplicateButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <MenuItem icon="action:duplicate" @click="handleDuplicate()">
+    {{ t('action:duplicate') }}
+  </MenuItem>
+</template>
+
+<script setup lang="ts">
+import type { FrontXoVm } from '@/modules/vm/remote-resources/use-xo-vm-collection.ts'
+import MenuItem from '@core/components/menu/MenuItem.vue'
+import { useI18n } from 'vue-i18n'
+import { useRouter } from 'vue-router'
+
+const { vm } = defineProps<{
+  vm: FrontXoVm
+}>()
+
+const { t } = useI18n()
+
+const router = useRouter()
+
+const handleDuplicate = () => {
+  router.push({ name: '/vm/[id]/duplicate', params: { id: vm.id } })
+}
+</script>
