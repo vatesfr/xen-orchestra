c1e9b83adc8bf630011c06e52b2e5d17f24d43de
Florent BEAUCHAMP <florent.beauchamp@vates.tech>
2026-03-11 09:34:09 +0100

    feat(backups): symetrical backups (#9524)

--- @xen-orchestra/backups/_runners/_vmRunners/IncrementalXapi.mjs
+++ @xen-orchestra/backups/_runners/_vmRunners/IncrementalXapi.mjs
@@ -135,16 +135,6 @@ export const IncrementalXapi = class IncrementalXapiVmBackupRunner extends Abstr
     const deltaChainLength = Math.max(
       ...lastExportedVdis.map(({ other_config }) => Number(other_config[DELTA_CHAIN_LENGTH] ?? 0))
     )
-    const fullInterval = this._settings.fullInterval
-    if (fullInterval !== 0 && fullInterval <= deltaChainLength + 1) {
-      debug('not using base VM because fullInterval reached', {
-        fullInterval,
-        deltaChainLength,
-        eq: fullInterval < deltaChainLength + 1,
-        dc1: deltaChainLength + 1,
-      })
-      return
-    }
 
     const srcVdis = keyBy(await xapi.getRecords('VDI', await this._vm.$getDisks()), '$ref')
 
@@ -173,6 +163,20 @@ export const IncrementalXapi = class IncrementalXapiVmBackupRunner extends Abstr
       debug('no base VM found')
       return
     }
+
+    // we do tafter checkbasevdis because we want the writer to know the target VM
+    // especially on replication when we alwayr update the same VM
+    const fullInterval = this._settings.fullInterval
+    if (fullInterval !== 0 && fullInterval <= deltaChainLength + 1) {
+      debug('not using base VM because fullInterval reached', {
+        fullInterval,
+        deltaChainLength,
+        eq: fullInterval < deltaChainLength + 1,
+        dc1: deltaChainLength + 1,
+      })
+      return
+    }
+
     baseUuidToSrcVdiUuid.forEach((srcVdiUuid, baseUuid) => {
       if (presentBaseVdis.has(baseUuid)) {
         debug('found base VDI', {
--- CHANGELOG.unreleased.md
+++ CHANGELOG.unreleased.md
@@ -12,6 +12,7 @@
 > Users must be able to say: "Nice enhancement, I'm eager to test it"
 
 - [MCP] Support token authentication via `XO_TOKEN` environment variable as an alternative to username/password (PR [#9577](https://github.com/vatesfr/xen-orchestra/pull/9577))
+- [Replication] Reuse the same VM as an incremental replication target (PR [#9524](https://github.com/vatesfr/xen-orchestra/pull/9524))
 
 ### Bug fixes
 
@@ -36,6 +37,7 @@
 <!--packages-start-->
 
 - @vates/generator-toolbox patch
+- @xen-orchestra/backups minor
 - @xen-orchestra/disk-transform patch
 - @xen-orchestra/mcp minor
 - xo-server minor
