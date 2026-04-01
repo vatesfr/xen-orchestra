87729a935df24a1bc8bc0356612dafa48ac2ec5b
Simon Pacotte <simon.pacotte@vates.tech>
2026-04-01 19:03:49 +0200

    fix(bakups): fix merge resume issue when child is disk chain

--- @xen-orchestra/backups/disks/MergeRemoteDisk.mjs
+++ @xen-orchestra/backups/disks/MergeRemoteDisk.mjs
@@ -10,6 +10,7 @@ import { createLogger } from '@xen-orchestra/log'
 
 import { basename, dirname } from 'path'
 import { asyncEach } from '@vates/async-each'
+import { relativeFromFile } from '@xen-orchestra/fs/path'
 
 // @ts-ignore
 const { warn } = createLogger('remote-disk:merge')
@@ -18,6 +19,7 @@ const { warn } = createLogger('remote-disk:merge')
  * @typedef {Object} MergeState
  * @property {{ uuid: string }} child
  * @property {{ uuid: string }} parent
+ * @property { string[]  | undefined} chain
  * @property {number} currentBlock
  * @property {number} mergedDataSize
  * @property {'mergeBlocks' | 'cleanup'} step
@@ -32,6 +34,7 @@ export class MergeRemoteDisk {
   #state = {
     child: { uuid: '0' },
     parent: { uuid: '0' },
+    chain: undefined,
     currentBlock: 0,
     mergedDataSize: 0,
     step: 'mergeBlocks',
@@ -204,6 +207,11 @@ export class MergeRemoteDisk {
     } else {
       this.#state.child = { uuid: childDisk.getUuid() ?? undefined }
       this.#state.parent = { uuid: parentDisk.getUuid() ?? undefined }
+      const childPaths = childDisk.getPaths()
+      this.#state.chain =
+        childPaths !== undefined
+          ? [parentDisk.getPath(), ...childPaths].map(p => relativeFromFile(this.#statePath, p))
+          : undefined
 
       // Finds first allocated block for the 2 following loops
       while (this.#state.currentBlock < getMaxBlockCount && !childDisk.hasBlock(this.#state.currentBlock)) {
