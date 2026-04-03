c6bd2bb65ce7a7ae54129983d97f7ef95cf12fd0
Simon Pacotte <simon.pacotte@vates.tech>
2026-04-03 10:35:34 +0200

    fix review PierreB & FlorentB

--- @xen-orchestra/backups/disks/RemoteDisk.mjs
+++ @xen-orchestra/backups/disks/RemoteDisk.mjs
@@ -68,7 +68,7 @@ export class RemoteDisk extends RandomAccessDisk {
   /**
    * Either returns an array of disk paths for disk chains or undefined for simple disks.
    *
-   * @returns {string[] | undefined}
+   * @returns {string[]}
    */
   getPaths() {
     throw new Error(`getPaths must be implemented`)
