2e02ddb5118df14c7b5d1abffbc47da8a85d945d
Elise-F <elise.franzini@gmail.com>
2026-04-08 17:41:40 +0200

    tech(xo6): refacto code about forms

---
+++ @xen-orchestra/web/src/modules/vm/components/form/duplicate/inputs/DuplicateVmSrSelect.vue
@@ -0,0 +1,19 @@
+<template>
+  <VtsInputWrapper :label="t('storage-repository')" :message="error">
+    <VtsSelect :id :accent="error ? 'danger' : 'brand'" />
+  </VtsInputWrapper>
+</template>
+
+<script setup lang="ts">
+import type { FormSelectId } from '@core/packages/form-select'
+import VtsInputWrapper, { type InputWrapperMessage } from '@core/components/input-wrapper/VtsInputWrapper.vue'
+import VtsSelect from '@core/components/select/VtsSelect.vue'
+import { useI18n } from 'vue-i18n'
+
+defineProps<{
+  id: FormSelectId
+  error?: InputWrapperMessage
+}>()
+
+const { t } = useI18n()
+</script>
