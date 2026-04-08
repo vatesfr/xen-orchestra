1e54408a90823cdc12d26bdf61e97ec0a3cae245
Elise-F <elise.franzini@gmail.com>
2026-04-08 17:41:40 +0200

    tech(xo6): refacto code about forms

---
+++ @xen-orchestra/web/src/modules/vm/components/form/duplicate/inputs/DuplicateVmButtonSection.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="footer">
+    <UiButton variant="secondary" accent="brand" size="medium" @click="router.back()">
+      {{ t('cancel') }}
+    </UiButton>
+    <UiButton variant="primary" accent="brand" size="medium" type="submit" :disabled="!canDuplicate" :busy="isRunning">
+      {{ t('action:duplicate') }}
+    </UiButton>
+  </div>
+</template>
+
+<script setup lang="ts">
+import UiButton from '@core/components/ui/button/UiButton.vue'
+import { useI18n } from 'vue-i18n'
+import { useRouter } from 'vue-router'
+
+defineProps<{
+  canDuplicate: boolean
+  isRunning: boolean
+}>()
+
+const { t } = useI18n()
+
+const router = useRouter()
+</script>
+
+<style scoped lang="postcss">
+.footer {
+  display: flex;
+  justify-content: center;
+  gap: 1.6rem;
+}
+</style>
---
+++ @xen-orchestra/web/src/modules/vm/components/form/duplicate/inputs/DuplicateVmCompressionModeRadioButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <UiRadioButtonGroup :label="t('compression')" accent="brand" :gap="uiStore.isSmall ? 'narrow' : 'wide'">
+    <UiRadioButton v-model="model" accent="brand" value="disabled">{{ t('disabled') }}</UiRadioButton>
+    <UiRadioButton v-model="model" accent="brand" value="gzip">
+      {{ t('duplicate-vm:compression-gzip') }}
+    </UiRadioButton>
+    <UiRadioButton v-model="model" accent="brand" value="zstd">
+      {{ t('duplicate-vm:compression-zstd') }}
+    </UiRadioButton>
+  </UiRadioButtonGroup>
+</template>
+
+<script setup lang="ts">
+import UiRadioButton from '@core/components/ui/radio-button/UiRadioButton.vue'
+import UiRadioButtonGroup from '@core/components/ui/radio-button-group/UiRadioButtonGroup.vue'
+import { useUiStore } from '@core/stores/ui.store.ts'
+import { useI18n } from 'vue-i18n'
+
+const model = defineModel<'disabled' | 'gzip' | 'zstd'>({ required: true })
+
+const { t } = useI18n()
+
+const uiStore = useUiStore()
+</script>
