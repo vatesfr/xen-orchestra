418e7b70aef79965cfcd28389339a4adaa384585
Simon Pacotte <simon.pacotte@vates.tech>
2026-04-09 15:18:16 +0200

    fix(backups): fix vhd directory merge speed regression

--- @xen-orchestra/backups/disks/RemoteVhdDisk.mjs
+++ @xen-orchestra/backups/disks/RemoteVhdDisk.mjs
@@ -16,6 +16,7 @@ import { DISK_TYPES } from 'vhd-lib/_constants.js'
 import { isVhdAlias, resolveVhdAlias } from 'vhd-lib/aliases.js'
 import { stringify } from 'uuid'
 import { dirname, join } from 'node:path'
+import { RemoteVhdDiskChain } from './RemoteVhdDiskChain.mjs'
 
 export class RemoteVhdDisk extends RemoteDisk {
   /**
@@ -255,7 +256,11 @@ export class RemoteVhdDisk extends RemoteDisk {
    * @returns {Promise<number>} blockSize
    */
   async mergeBlock(childDisk, index, isResumingMerge) {
-    if ((await this.isDirectory()) && childDisk instanceof RemoteVhdDisk && (await childDisk.isDirectory())) {
+    if (
+      (childDisk instanceof RemoteVhdDisk || childDisk instanceof RemoteVhdDiskChain) &&
+      (await this.isDirectory()) &&
+      (await childDisk.isDirectory())
+    ) {
       try {
         await this.#handler.rename(childDisk.getBlockPath(index), this.getBlockPath(index))
 
