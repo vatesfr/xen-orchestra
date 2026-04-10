7070d2789ef8fcfec9ab40560bf036beca902cdd
Joris-K <joris.kosacki@vates.tech>
2026-04-10 16:03:26 +0200

    feat(xo6): add possibility to detach VDIs

---
+++ @xen-orchestra/web/src/modules/vdi/components/modal/VdiDetachModal.vue
@@ -0,0 +1,29 @@
+<template>
+  <VtsModal accent="info" icon="status:info-picto">
+    <template #title>
+      {{ t('vdi-detach-title', { n: count }) }}
+    </template>
+    <template #content>
+      {{ t('vdi-detach-info') }}
+    </template>
+    <template #buttons>
+      <VtsModalCancelButton>{{ t('action:go-back') }}</VtsModalCancelButton>
+      <VtsModalConfirmButton>
+        {{ t(`action:detach-vdis`, { n: count }) }}
+      </VtsModalConfirmButton>
+    </template>
+  </VtsModal>
+</template>
+
+<script lang="ts" setup>
+import VtsModal from '@core/components/modal/VtsModal.vue'
+import VtsModalCancelButton from '@core/components/modal/VtsModalCancelButton.vue'
+import VtsModalConfirmButton from '@core/components/modal/VtsModalConfirmButton.vue'
+import { useI18n } from 'vue-i18n'
+
+defineProps<{
+  count: number
+}>()
+
+const { t } = useI18n()
+</script>
