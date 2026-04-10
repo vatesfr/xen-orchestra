8361c4f1a7427c07c653da5460d3d447bc69211d
Elise-F <elise.franzini@gmail.com>
2026-04-10 15:33:52 +0200

    feat(xo6): add current operation and clean code

--- @xen-orchestra/web/src/modules/vm/jobs/xo-vm-duplicate.job.ts
+++ @xen-orchestra/web/src/modules/vm/jobs/xo-vm-duplicate.job.ts
@@ -1,12 +1,11 @@
 import type { FrontXoSr } from '@/modules/storage-repository/remote-resources/use-xo-sr-collection.ts'
-import type { FrontXoTask } from '@/modules/task/remote-resources/use-xo-task-collection.ts'
 import { xoVmsArg } from '@/modules/vm/jobs/xo-vm-args.ts'
 import type { FrontXoVm } from '@/modules/vm/remote-resources/use-xo-vm-collection.ts'
 import { areVmsOperationPending } from '@/modules/vm/utils/xo-vm.util.ts'
 import { useXoTaskUtils } from '@/shared/composables/xo-task-utils.composable.ts'
 import { fetchPost } from '@/shared/utils/fetch.util.ts'
 import { defineJob, defineJobArg, JobError, JobRunningError } from '@core/packages/job'
-import { VM_OPERATIONS } from '@vates/types'
+import { VM_OPERATIONS, type XoTask } from '@vates/types'
 import { useI18n } from 'vue-i18n'
 
 export type DuplicateVmPayload =
@@ -26,7 +25,7 @@ export const useXoVmDuplicateJob = defineJob('vm.duplicate', [xoVmsArg, payloadA
     async run(vms: FrontXoVm[], payload: DuplicateVmPayload) {
       const results = await Promise.allSettled(
         vms.map(async vm => {
-          const { taskId } = await fetchPost<{ taskId: FrontXoTask['id'] }>(`vms/${vm.id}/actions/clone`, payload)
+          const { taskId } = await fetchPost<{ taskId: XoTask['id'] }>(`vms/${vm.id}/actions/clone`, payload)
           await monitorTask(taskId)
         })
       )
