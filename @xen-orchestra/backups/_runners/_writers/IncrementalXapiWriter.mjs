e28a5c7c347093fd21fdc8e356dc4f791c0fd79b
Florent BEAUCHAMP <florent.beauchamp@vates.tech>
2026-04-13 05:13:23 -0400

    fix(xapi): wait for disk to be really disconnected

--- @xen-orchestra/backups/_runners/_writers/IncrementalXapiWriter.mjs
+++ @xen-orchestra/backups/_runners/_writers/IncrementalXapiWriter.mjs
@@ -65,8 +65,9 @@ export class IncrementalXapiWriter extends MixinXapiWriter(AbstractIncrementalWr
         snapshotCandidates,
         async snapshot => {
           let diffDisk
+          let activeVdi
           try {
-            const activeVdi = sr.$xapi.getObject(snapshot.$snapshot_of)
+            activeVdi = sr.$xapi.getObject(snapshot.$snapshot_of)
             const userVbds = activeVdi.$VBDs?.filter(vbd => vbd.$VM && !vbd.$VM.is_control_domain) ?? []
             if (userVbds.length !== 1) {
               debug('checkBaseVdis, share vbd ', { ref: snapshot.$ref, userVbds })
@@ -108,6 +109,10 @@ export class IncrementalXapiWriter extends MixinXapiWriter(AbstractIncrementalWr
             return
           } finally {
             await diffDisk?.close().catch(error => debug('checkBaseVdis, error closing', error))
+            await sr.$xapi.VDI_disconnectFromControlDomain(snapshot.$ref)
+            if (activeVdi !== undefined) {
+              await sr.$xapi.VDI_disconnectFromControlDomain(activeVdi.$ref)
+            }
           }
         },
         {
