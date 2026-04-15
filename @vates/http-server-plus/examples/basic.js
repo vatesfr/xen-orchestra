7238d4567cb3b526915f1757ad71e68d714278ad
Florent BEAUCHAMP <florent.beauchamp@vates.tech>
2026-04-15 07:52:30 -0400

    normalize

--- @vates/http-server-plus/examples/basic.js
+++ @vates/http-server-plus/examples/basic.js
@@ -7,6 +7,7 @@ const httpServerPlus = require('../')
 // ===================================================================
 
 const server = httpServerPlus.create(function onRequest(request, response) {
+  // eslint-disable-next-line no-console
   console.log('New request: %s %s', request.method, request.url)
 
   response.end('Nothing to see here.')
@@ -18,8 +19,10 @@ server
     port: 8080,
   })
   .then(function (niceAddress) {
+    // eslint-disable-next-line no-console
     console.log('Listening on', niceAddress)
   })
   .catch(function (error) {
+    // eslint-disable-next-line no-console
     console.log(error.code, '- Failed to listen on', error.niceAddress)
   })
