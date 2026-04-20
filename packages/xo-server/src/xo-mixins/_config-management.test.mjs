b83e099f6296ca27ffe680111fa364518391d828
Debian <simon.pacotte@vates.tech>
2026-04-20 12:43:38 +0000

    fix linter issue

--- packages/xo-server/src/xo-mixins/_config-management.test.mjs
+++ packages/xo-server/src/xo-mixins/_config-management.test.mjs
@@ -3,11 +3,11 @@ import test from 'node:test'
 
 import ConfigManagement from './config-management.mjs'
 
-const { describe, it, before } = test
+const { describe, it } = test
 
 describe('ConfigManagement', function () {
   it('encryptCredentialDatabase forces passphrase', async function () {
-    const mockApp = { hooks: { on: () => { } }, config: { getOptional: () => false } }
+    const mockApp = { hooks: { on: () => {} }, config: { getOptional: () => false } }
     let configManagement = new ConfigManagement(mockApp)
 
     // Check that the export config works without a passphrase when encryptCredentialDatabase is false
@@ -17,9 +17,13 @@ describe('ConfigManagement', function () {
     configManagement = new ConfigManagement(mockApp)
 
     // Check that the export config throws without a passphrase when encryptCredentialDatabase is true.
-    await assert.rejects(() => configManagement.exportConfig({ compress: false, entries: [] }), { message: 'Config export requires a passphrase when credential encryption is enabled.'})
+    await assert.rejects(() => configManagement.exportConfig({ compress: false, entries: [] }), {
+      message: 'Config export requires a passphrase when credential encryption is enabled.',
+    })
 
     // Check that the export config works without a passphrase when encryptCredentialDatabase is true.
-    await assert.doesNotReject(() => configManagement.exportConfig({ compress: false, entries: [], passphrase: 'passphrase' }))
+    await assert.doesNotReject(() =>
+      configManagement.exportConfig({ compress: false, entries: [], passphrase: 'passphrase' })
+    )
   })
 })
