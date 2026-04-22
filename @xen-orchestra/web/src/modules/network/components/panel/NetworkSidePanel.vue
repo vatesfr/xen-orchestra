5531394188e13bdd294a96e0d76c245073e573fd
Olivier Floch <olivier.floch@vates.fr>
2026-04-22 14:44:03 +0200

    feat(xo-6/xo-lite): migrate side panels objects titles to VtsCardObjectTitle

--- @xen-orchestra/web/src/modules/network/components/panel/NetworkSidePanel.vue
+++ @xen-orchestra/web/src/modules/network/components/panel/NetworkSidePanel.vue
@@ -15,12 +15,8 @@
     </template>
     <template #default>
       <UiCard class="card-container">
-        <UiCardTitle v-tooltip="{ placement: 'bottom-end' }" class="typo-body-bold">
-          {{ network.name_label }}
-        </UiCardTitle>
+        <VtsCardObjectTitle :id="network.id" :label="network.name_label" icon="object:network" />
         <div class="content">
-          <!-- ID -->
-          <VtsCodeSnippet :content="network.id" copy />
           <!-- DESCRIPTION -->
           <VtsCardRowKeyValue truncate align-top>
             <template #key>{{ t('description') }}</template>
@@ -77,7 +73,7 @@ import { useNetworkDeleteModal } from '@/modules/network/composables/use-network
 import type { FrontXoNetwork } from '@/modules/network/remote-resources/use-xo-network-collection.ts'
 import { useXoPifCollection } from '@/modules/pif/remote-resources/use-xo-pif-collection.ts'
 import VtsCardRowKeyValue from '@core/components/card/VtsCardRowKeyValue.vue'
-import VtsCodeSnippet from '@core/components/code-snippet/VtsCodeSnippet.vue'
+import VtsCardObjectTitle from '@core/components/card-object-title/VtsCardObjectTitle.vue'
 import VtsCopyButton from '@core/components/copy-button/VtsCopyButton.vue'
 import VtsDeleteButton from '@core/components/delete-button/VtsDeleteButton.vue'
 import UiButtonIcon from '@core/components/ui/button-icon/UiButtonIcon.vue'
