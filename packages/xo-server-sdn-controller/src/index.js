b07da7c715a37a1e43ad0a020d6d9ac33226a124
All-Ki <k.allegrain@gmail.com>
2026-04-23 03:17:38 -0400

    feat : Added POST and DELETE rules

--- packages/xo-server-sdn-controller/src/index.js
+++ packages/xo-server-sdn-controller/src/index.js
@@ -472,7 +472,7 @@ class SDNController extends EventEmitter {
             _post: async (req, res, next) => {
               const validationErrors = []
 
-              if (!req.body.allow || typeof req.body.allow !== 'boolean') {
+              if (req.body.allow === undefined || typeof req.body.allow !== 'boolean') {
                 validationErrors.push('allow is required and must be a boolean')
               }
 
@@ -487,7 +487,9 @@ class SDNController extends EventEmitter {
               if (!req.body.protocol || typeof req.body.protocol !== 'string') {
                 validationErrors.push('protocol is required and must be a string')
               }
-
+              if (req.body.port && Number.isInteger(req.body.port) === false) {
+                validationErrors.push('port must be an integer')
+              }
               if (validationErrors.length > 0) {
                 throw invalidParameters(validationErrors)
               }
@@ -529,6 +531,9 @@ class SDNController extends EventEmitter {
               if (!req.body.protocol || typeof req.body.protocol !== 'string') {
                 validationErrors.push('protocol is required and must be a string')
               }
+              if (req.body.port && Number.isInteger(req.body.port) === false) {
+                validationErrors.push('port must be an integer')
+              }
 
               if (validationErrors.length > 0) {
                 throw invalidParameters(validationErrors)
@@ -565,7 +570,7 @@ class SDNController extends EventEmitter {
                 throw invalidParameters(validationErrors)
               }
 
-              if (!req.body.allow || typeof req.body.allow !== 'boolean') {
+              if (req.body.allow === undefined || typeof req.body.allow !== 'boolean') {
                 validationErrors.push('allow is required and must be a boolean')
               }
 
@@ -580,7 +585,7 @@ class SDNController extends EventEmitter {
               if (!req.body.protocol || typeof req.body.protocol !== 'string') {
                 validationErrors.push('protocol is required and must be a string')
               }
-              if (req.body.port && isNaN(parseInt(req.body.port))) {
+              if (req.body.port && Number.isInteger(req.body.port) === false) {
                 validationErrors.push('port must be an integer')
               }
 
@@ -624,6 +629,9 @@ class SDNController extends EventEmitter {
 
               if (!req.body.protocol || typeof req.body.protocol !== 'string') {
                 validationErrors.push('protocol is required and must be a string')
+              }
+              if (req.body.port && Number.isInteger(req.body.port) === false) {
+                validationErrors.push('port must be an integer')
               }
 
               if (validationErrors.length > 0) {
@@ -798,7 +806,6 @@ class SDNController extends EventEmitter {
       for (const pool of pools) {
         pool.update_other_config('xo:sdn-controller:of-method', of_method)
       }
-
     } catch (error) {
       log.error('Error while handling xapi connection', {
         id: xapi.pool.uuid,
@@ -887,9 +894,7 @@ class SDNController extends EventEmitter {
       const network = this._xo.getXapiObject(this._xo.getObject(networkId, 'network'))
       assert(network.$PIFs.length > 0, 'Network needs to be plugged to add a rule')
 
-      const networkRules = JSON.parse(
-        network.other_config['xo:sdn-controller:of-rules'] || '[]'
-      ).map(JSON.parse)
+      const networkRules = JSON.parse(network.other_config['xo:sdn-controller:of-rules'] || '[]').map(JSON.parse)
 
       // filter matching rule (don't compare allow and cookie)
       const matchRules = networkRules.filter(rule => {
@@ -902,15 +907,18 @@ class SDNController extends EventEmitter {
       let cookie
       if (matchRules.length !== 0) {
         // use the one in rule if matching rule is present
-        cookie = matchRules.map(rule => { return rule.cookie })[0]
-
+        cookie = matchRules.map(rule => {
+          return rule.cookie
+        })[0]
       } else {
         // generate a new cookie not in use (OpenVSwitch cookie range: 0x1 to 0xFFFF_FFFF_FFFF_FFFF)
         do {
           cookie = '0x' + randomBytes(8).toString('hex')
         } while (
           cookie === '0x0000000000000000' ||
-          networkRules.filter(rule => { return rule.cookie === cookie }).length > 0
+          networkRules.filter(rule => {
+            return rule.cookie === cookie
+          }).length > 0
         )
       }
 
@@ -984,7 +992,7 @@ class SDNController extends EventEmitter {
             host: vif.$VM.$resident_on?.uuid,
           })
         } else {
-          throw (error)
+          throw error
         }
       }
 
@@ -1032,9 +1040,7 @@ class SDNController extends EventEmitter {
       let network = this._xo.getXapiObject(this._xo.getObject(networkId, 'network'))
       assert(network.$PIFs.length > 0, 'Network needs to be plugged to delete a rule')
 
-      const networkRules = JSON.parse(
-        network.other_config['xo:sdn-controller:of-rules'] || '[]'
-      ).map(JSON.parse)
+      const networkRules = JSON.parse(network.other_config['xo:sdn-controller:of-rules'] || '[]').map(JSON.parse)
 
       // filter matching rule (don't compare allow and cookie)
       const matchRules = networkRules.filter(rule => {
@@ -1045,12 +1051,14 @@ class SDNController extends EventEmitter {
 
       // ignore processing if rule not present here
       if (matchRules.length === 0) {
-        log.info("_deleteNetworkOfRule: rule not present, ignoring", {})
+        log.info('_deleteNetworkOfRule: rule not present, ignoring', {})
         return
       }
 
       // extract the (first) matching cookie
-      const cookie = matchRules.map(rule => { return rule.cookie })[0]
+      const cookie = matchRules.map(rule => {
+        return rule.cookie
+      })[0]
 
       try {
         const host = this._xo.getXapiObject(this._xo.getObject(network.$PIFs[0].host, 'host'))
@@ -1062,7 +1070,7 @@ class SDNController extends EventEmitter {
         if (error.code === 'HOST_OFFLINE') {
           log.info('deleteNetworkOfRule: Ignoring HOST_OFFLINE', { network: networkId })
         } else {
-          throw (error)
+          throw error
         }
       }
 
@@ -1072,7 +1080,11 @@ class SDNController extends EventEmitter {
 
       const newNetworkRules = networkRules.filter(rule => {
         return (
-          rule.protocol !== protocol || rule.port !== port || rule.ipRange !== ipRange || rule.direction !== direction || rule.cookie !== cookie
+          rule.protocol !== protocol ||
+          rule.port !== port ||
+          rule.ipRange !== ipRange ||
+          rule.direction !== direction ||
+          rule.cookie !== cookie
         )
       })
       await network.update_other_config(
