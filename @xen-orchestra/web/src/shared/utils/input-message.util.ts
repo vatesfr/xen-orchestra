e4d49bb2777a02615dc3f3892f1194c8ef60769c
Elise-F <elise.franzini@gmail.com>
2026-05-21 10:42:44 +0200

    feat(xo6): update to the traffic rules form with a new validation form

--- @xen-orchestra/web/src/shared/utils/input-message.util.ts
+++
@@ -1,5 +0,0 @@
-import type { InputWrapperMessage } from '@core/components/input-wrapper/VtsInputWrapper.vue'
-
-export const danger = (content: string): InputWrapperMessage => ({ content, accent: 'danger' })
-
-export const warning = (content: string): InputWrapperMessage => ({ content, accent: 'warning' })
