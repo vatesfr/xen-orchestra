vatesfr/xen-orchestra

commit 4e5085852688ca979f9c7a288aaa9da5d43e6fa6

feat(@xen-orchestra/backups): refactor backup merge code (#9300)
this merge code leverage the new classes introduced to handle qcow2, improving our flexibility, and future uses.

this also improve the performance on "block mode" backups (s3/ azure and other storage with the option), since it reliably updates the block allocation table only at the end of the merge instead of each time a new block is detected

@xen-orchestra/backups/disks/MergeRemoteDisk.mjs
@@ -1,0 +1,317 @@
+// @ts-check
+
+/**
+ * @typedef {import('./RemoteDisk.mjs').RemoteDisk} RemoteDisk
+ * @typedef {import('@xen-orchestra/disk-transform').FileAccessor} FileAccessor
+ */
+
+import assert from 'assert'
+import { createLogger } from '@xen-orchestra/log'
+
+import { basename, dirname } from 'path'
+import { asyncEach } from '@vates/async-each'
+
+// @ts-ignore
+const { warn } = createLogger('remote-disk:merge')
+
+/**
+ * @typedef {Object} MergeState
+ * @property {{ uuid: string }} child
+ * @property {{ uuid: string }} parent
+ * @property {number} currentBlock
+ * @property {number} mergedDataSize
+ * @property {'mergeBlocks' | 'cleanup'} step
+ * @property {number} diskSize
+ */
+
+export class MergeRemoteDisk {
+  /**
+   * @type {MergeState}
+   */
+  #state = {
+    child: { uuid: '0' },
+    parent: { uuid: '0' },
+    currentBlock: 0,
+    mergedDataSize: 0,
+    step: 'mergeBlocks',
+    diskSize: 0,
+  }
+
+  /**
+   * @type {string}
+   */
+  #statePath = ''
+
+  /**
+   * @type {boolean}
+   */
+  #isResuming
+
+  /**
+   * @type {Logger | Function}
+   */
+  #logInfo
+
+  /**
+   * @type {number}
+   */
+  #mergeBlockConcurrency
+
+  /**
+   * @type {Function}
+   */
+  #onProgress
+
+  /**
+   * @type {boolean}
+   */
+  #removeUnused
+
+  /**
+   * @type {number}
+   */
+  #writeStateDelay
+
+  /**
+   * @type {number}
+   */
+  #lastStateWrittenAt
+
+  /**
+   * @type {FileAccessor}
+   */
+  #handler
+
+  /**
+   * @param {FileAccessor} handler
+   * @param {Object} params
+   * @param {Function} params.onProgress
+   * @param {Logger | Function} params.logInfo
+   * @param {boolean} params.removeUnused
+   * @param {number} params.mergeBlockConcurrency
+   * @param {number} params.writeStateDelay
+   */
+  constructor(
+    handler,
+    {
+      onProgress = () => {},
+      logInfo = () => {},
+      removeUnused = false,
+      mergeBlockConcurrency = 2,
+      writeStateDelay = 10e3,
+    }
+  ) {
+    this.#handler = handler
+    this.#logInfo = logInfo
+    this.#onProgress = onProgress
+    this.#removeUnused = removeUnused
+    this.#mergeBlockConcurrency = mergeBlockConcurrency
+    this.#writeStateDelay = writeStateDelay
+
+    this.#isResuming = false
+    this.#lastStateWrittenAt = 0
+  }
+
+  /**
+   * @param {RemoteDisk} parentDisk
+   * @returns {Promise<boolean>} isResuming
+   */
+  async isResuming(parentDisk) {
+    try {
+      await this.#handler.readFile(
+        dirname(parentDisk.getPath()) + '/.' + basename(parentDisk.getPath()) + '.merge.json'
+      )
+      return true
+    } catch (error) {
+      return false
+    }
+  }
+
+  /**
+   * @param {RemoteDisk} parentDisk
+   * @param {RemoteDisk} childDisk
+   */
+  async merge(parentDisk, childDisk) {
+    this.#statePath = dirname(parentDisk.getPath()) + '/.' + basename(parentDisk.getPath()) + '.merge.json'
+
+    try {
+      const mergeStateContent = await this.#handler.readFile(this.#statePath)
+      this.#state = JSON.parse(mergeStateContent)
+
+      // work-around a bug introduce in 97d94b795
+      //
+      // currentBlock could be `null` due to the JSON.stringify of a `NaN` value
+      if (this.#state?.currentBlock === null) this.#state.currentBlock = 0
+      this.#isResuming = true
+    } catch (error) {
+      // @ts-ignore
+      if (error.code !== 'ENOENT') {
+        warn('problem while checking the merge state', { error })
+      }
+    }
+
+    try {
+      /* eslint-disable no-fallthrough */
+      switch (this.#state?.step ?? 'mergeBlocks') {
+        case 'mergeBlocks':
+          await this.#step_mergeBlocks(parentDisk, childDisk)
+        case 'cleanup':
+          await this.#step_cleanup(parentDisk, childDisk)
+          return this.#cleanup(parentDisk, childDisk, true)
+        default:
+          warn(`Step ${this.#state?.step} is unknown`, { state: this.#state })
+      }
+      /* eslint-enable no-fallthrough */
+    } catch (error) {
+      await this.#cleanup(parentDisk, childDisk, false)
+      throw error
+    }
+  }
+
+  async #writeState() {
+    try {
+      await this.#handler.writeFile(this.#statePath, JSON.stringify(this.#state), { flags: 'w' })
+    } catch (err) {
+      warn('failed to write merge state', { error: err })
+    }
+  }
+
+  async #writeStateThrottled() {
+    const now = Date.now()
+    if (now - this.#lastStateWrittenAt > this.#writeStateDelay) {
+      this.#lastStateWrittenAt = now
+      await this.#writeState()
+    }
+  }
+
+  /**
+   * @param {RemoteDisk} parentDisk
+   * @param {RemoteDisk} childDisk
+   */
+  async #step_mergeBlocks(parentDisk, childDisk) {
+    const getMaxBlockCount = childDisk.getMaxBlockCount()
+
+    if (this.#isResuming) {
+      const alreadyMergedBlocks = []
+      for (let blockId = 0; blockId < this.#state.currentBlock; blockId++) {
+        if (childDisk.hasBlock(blockId)) {
+          alreadyMergedBlocks.push(blockId)
+        }
+      }
+
+      parentDisk.setAllocatedBlocks(alreadyMergedBlocks)
+    } else {
+      this.#state.child = { uuid: childDisk.getUuid() ?? 0 }
+      this.#state.parent = { uuid: parentDisk.getUuid() ?? 0 }
+
+      // Finds first allocated block for the 2 following loops
+      while (this.#state.currentBlock < getMaxBlockCount && !childDisk.hasBlock(this.#state.currentBlock)) {
+        ++this.#state.currentBlock
+      }
+      await this.#writeState()
+    }
+
+    await this.#mergeBlocks(parentDisk, childDisk)
+    await parentDisk.flushMetadata(childDisk)
+    parentDisk.mergeMetadata(childDisk)
+  }
+
+  /**
+   * @param {RemoteDisk} parentDisk
+   * @param {RemoteDisk} childDisk
+   */
+  async #mergeBlocks(parentDisk, childDisk) {
+    this.#mergeBlockConcurrency =
+      (await parentDisk.canMergeConcurently()) && (await childDisk.canMergeConcurently())
+        ? this.#mergeBlockConcurrency
+        : 1
+
+    const toMerge = []
+    for (let block = this.#state.currentBlock; block < childDisk.getMaxBlockCount(); block++) {
+      if (childDisk.hasBlock(block)) {
+        toMerge.push(block)
+      }
+    }
+
+    const nBlocks = toMerge.length
+    this.#onProgress({ total: nBlocks, done: 0 })
+
+    const merging = new Set()
+    let counter = 0
+    await asyncEach(
+      toMerge,
+      async blockId => {
+        merging.add(blockId)
+
+        const blockSize = await parentDisk.mergeBlock(childDisk, blockId, this.#isResuming)
+        this.#state.mergedDataSize += blockSize
+
+        this.#state.currentBlock = Math.min(...merging) - 1
+        merging.delete(blockId)
+
+        this.#onProgress({ total: nBlocks, done: counter + 1 })
+        counter++
+        await this.#writeStateThrottled()
+      },
+      { concurrency: this.#mergeBlockConcurrency }
+    )
+
+    await this.#writeState()
+
+    this.#state.diskSize = childDisk.getSizeOnDisk()
+
+    this.#onProgress({ total: nBlocks, done: nBlocks })
+  }
+
+  /**
+   * @param {RemoteDisk} parentDisk
+   * @param {RemoteDisk} childDisk
+   */
+  async #step_cleanup(parentDisk, childDisk) {
+    assert.notEqual(this.#state, undefined)
+    this.#state.step = 'cleanup'
+    await this.#writeState()
+
+    const mergeTargetPath = childDisk.getPath()
+
+    // delete intermediate children if needed
+    if (this.#removeUnused) {
+      await childDisk.unlink()
+    }
+
+    try {
+      await parentDisk.rename(mergeTargetPath)
+    } catch (error) {
+      // @ts-ignore
+      if (error.code === 'ENOENT' && this.#isResuming) {
+        // @ts-ignore
+        this.#logInfo(`the parent disk was already renamed`, {
+          parent: parentDisk.getPath(),
+          mergeTarget: mergeTargetPath,
+        })
+      } else {
+        throw error
+      }
+    }
+  }
+
+  /**
+   * @param {RemoteDisk} parentDisk
+   * @param {RemoteDisk} childDisk
+   * @param {boolean} cleanStateFile
+   *
+   * @returns {Promise<{mergedDataSize: number, finalDiskSize: number}>} result
+   */
+  async #cleanup(parentDisk, childDisk, cleanStateFile) {
+    const finalDiskSize = this.#state?.diskSize ?? 0
+    const mergedDataSize = this.#state?.mergedDataSize ?? 0
+    await parentDisk.close().catch(warn)
+    await childDisk.close().catch(warn)
+
+    if (cleanStateFile) {
+      await this.#handler.unlink(this.#statePath).catch(warn)
+    }
+
+    return { mergedDataSize, finalDiskSize }
+  }
+}

@xen-orchestra/backups/RemoteAdapter.mjs
@@ -31,7 +31,7 @@ import { listPartitions, LVM_PARTITION_TYPE_MBR, LVM_PARTITION_TYPE_GPT } from '
 import { lvs, pvs } from './_lvm.mjs'
 import { watchStreamSize } from './_watchStreamSize.mjs'
 
-import { RemoteVhd } from './disks/RemoteVhd.mjs'
+import { RemoteVhdDisk } from './disks/RemoteVhdDisk.mjs'
 import { openDiskChain } from './disks/openDiskChain.mjs'
 import { toVhdStream, writeToVhdDirectory } from 'vhd-lib/disk-consumer/index.mjs'
 import { ReadAhead } from '@xen-orchestra/disk-transform'
@@ -744,7 +744,7 @@ export class RemoteAdapter {
     if (useChain) {
       disk = await openDiskChain({ handler, path })
     } else {
-      disk = new RemoteVhd({ handler, path })
+      disk = new RemoteVhdDisk({ handler, path })
       await disk.init()
     }
     disk = new ReadAhead(disk)

@xen-orchestra/backups/_cleanVm.integ.mjs
@@ -235,7 +235,7 @@ test('it merges delta of non destroyed chain', async () => {
 
   const metadata = JSON.parse(await handler.readFile(`${rootPath}/metadata.json`))
   // size should be the size of children + grand children after the merge
-  assert.equal(metadata.size, 104960)
+  assert.equal(metadata.size, 104448)
 
   // merging is already tested in vhd-lib, don't retest it here (and theses vhd are as empty as my stomach at 12h12)
   // only check deletion
@@ -402,13 +402,12 @@ describe('tests multiple combination ', { concurrency: 1 }, () => {
 
         const metadata = JSON.parse(await handler.readFile(`${rootPath}/metadata.json`))
         // size should be the size of children + grand children + clean after the merge
-        assert.deepEqual(metadata.size, vhdMode === 'file' ? 6502400 : 6501888)
+        assert.deepEqual(metadata.size, 4404224)
 
         // broken vhd, non referenced, abandoned should be deleted ( alias and data)
         // ancestor and child should be merged
         // grand child and clean vhd should not have changed
         const survivors = await handler.list(basePath)
-        // console.log(survivors)
         if (useAlias) {
           const dataSurvivors = await handler.list(basePath + '/data')
           // the goal of the alias : do not move a full folder

@xen-orchestra/backups/_cleanVm.mjs
@@ -6,7 +6,9 @@ import { isVhdAlias, resolveVhdAlias } from 'vhd-lib/aliases.js'
 import { basename, dirname, resolve } from 'node:path'
 import { isMetadataFile, isVhdFile, isVhdSumFile, isXvaFile, isXvaSumFile } from './_backupType.mjs'
 import { limitConcurrency } from 'limit-concurrency-decorator'
-import { mergeVhdChain } from 'vhd-lib/merge.js'
+import { RemoteVhdDisk } from './disks/RemoteVhdDisk.mjs'
+import { RemoteVhdDiskChain } from './disks/RemoteVhdDiskChain.mjs'
+import { MergeRemoteDisk } from './disks/MergeRemoteDisk.mjs'
 
 import { Task } from './Task.mjs'
 import { Disposable } from 'promise-toolbox'
@@ -51,7 +53,15 @@ async function _mergeVhdChain(handler, chain, { logInfo, remove, mergeBlockConcu
     }
   }, 10e3)
   try {
-    return await mergeVhdChain(handler, chain, {
+    const parentDisk = new RemoteVhdDisk({ handler, path: chain.shift() })
+
+    const childDisks = []
+    for (const path of chain) {
+      childDisks.push(new RemoteVhdDisk({ handler, path }))
+    }
+    const childDiskChain = new RemoteVhdDiskChain({ disks: childDisks })
+
+    const mergeRemoteDisk = new MergeRemoteDisk(handler, {
       logInfo,
       mergeBlockConcurrency,
       onProgress({ done: d, total: t }) {
@@ -60,6 +70,14 @@ async function _mergeVhdChain(handler, chain, { logInfo, remove, mergeBlockConcu
       },
       removeUnused: remove,
     })
+
+    const isResumingMerge = await mergeRemoteDisk.isResuming(parentDisk)
+    await parentDisk.init({ force: isResumingMerge })
+    await childDiskChain.init({ force: isResumingMerge })
+
+    const result = await mergeRemoteDisk.merge(parentDisk, childDiskChain)
+
+    return result
   } finally {
     clearInterval(handle)
   }
@@ -521,14 +539,14 @@ export async function cleanVm(
   const metadataWithMergedVhd = {}
   const doMerge = async () => {
     await asyncMap(toMerge, async chain => {
-      const { finalVhdSize } = await limitedMergeVhdChain(handler, chain, {
+      const { finalDiskSize } = await limitedMergeVhdChain(handler, chain, {
         logInfo,
         logWarn,
         remove,
         mergeBlockConcurrency,
       })
       const metadataPath = vhdsToJSons[chain[chain.length - 1]] // all the chain should have the same metadata file
-      metadataWithMergedVhd[metadataPath] = (metadataWithMergedVhd[metadataPath] ?? 0) + finalVhdSize
+      metadataWithMergedVhd[metadataPath] = (metadataWithMergedVhd[metadataPath] ?? 0) + finalDiskSize
     })
   }
 

@xen-orchestra/backups/disks/RemoteDisk.mjs
@@ -1,0 +1,194 @@
+// @ts-check
+
+/**
+ * @typedef {import('@xen-orchestra/disk-transform').DiskBlock} DiskBlock
+ */
+
+import { RandomAccessDisk } from '@xen-orchestra/disk-transform'
+export class RemoteDisk extends RandomAccessDisk {
+  /**
+   * Abstract
+   * @param {Object} options
+   * @param {boolean} options.force
+   * @returns {Promise<void>}
+   */
+  async init(options = { force: false }) {
+    throw new Error(`init must be implemented`)
+  }
+
+  /**
+   * Abstract
+   * @returns {Promise<void>}
+   */
+  async close() {
+    throw new Error(`close must be implemented`)
+  }
+
+  /**
+   * Abstract
+   * @returns {number}
+   */
+  getVirtualSize() {
+    throw new Error(`getVirtualSize must be implemented`)
+  }
+
+  /**
+   * Abstract
+   * @returns {number} size
+   */
+  getSizeOnDisk() {
+    throw new Error(`getSizeOnDisk must be implemented`)
+  }
+
+  /**
+   * @returns {number}
+   */
+  getBlockSize() {
+    throw new Error(`getBlockSize must be implemented`)
+  }
+
+  /**
+   * Abstract
+   * @returns {string}
+   */
+  getPath() {
+    throw new Error(`getPath must be implemented`)
+  }
+
+  /**
+   * Abstract
+   * @returns {string}
+   */
+  getUuid() {
+    throw new Error(`getUuid must be implemented`)
+  }
+
+  /**
+   * Abstract
+   * @returns {Promise<boolean>} canMergeConcurently
+   */
+  async canMergeConcurently() {
+    throw new Error(`canMergeConcurently must be implemented`)
+  }
+
+  /**
+   * Abstract
+   * @returns {number} getMaxBlockCount
+   */
+  getMaxBlockCount() {
+    throw new Error(`getMaxBlockCount must be implemented`)
+  }
+
+  /**
+   * Checks if the VHD contains a specific block.
+   * @param {number} index
+   * @returns {boolean}
+   */
+  hasBlock(index) {
+    throw new Error(`hasBlock must be implemented`)
+  }
+
+  /**
+   * Gets the indexes of all blocks in the VHD.
+   * @returns {Array<number>}
+   */
+  getBlockIndexes() {
+    throw new Error(`getBlockIndexes must be implemented`)
+  }
+
+  /**
+   * Abstract
+   * Writes a full block.
+   * @param {DiskBlock} diskBlock
+   * @return {Promise<number>} blockSize
+   */
+  async writeBlock(diskBlock) {
+    throw new Error(`writeBlock must be implemented`)
+  }
+
+  /**
+   * Abstract
+   * Reads a specific block from the VHD.
+   * @param {number} index
+   * @returns {Promise<DiskBlock>} diskBlock
+   */
+  async readBlock(index) {
+    throw new Error(`readBlock must be implemented`)
+  }
+
+  /**
+   * Abstract
+   * Reads a specific block from the child disk to copy/move it to this disk.
+   * @param {RemoteDisk} childDisk
+   * @param {number} index
+   * @param {boolean} isResumingMerge
+   * @returns {Promise<number>} blockSize
+   */
+  async mergeBlock(childDisk, index, isResumingMerge) {
+    throw new Error(`mergeBlock must be implemented`)
+  }
+
+  /**
+   * Abstract
+   * Manually set the disk allocated blocks.
+   * @param {Array<number>} blockIds
+   * @returns {Promise<void>}
+   */
+  async setAllocatedBlocks(blockIds) {
+    throw new Error(`setAllocatedBlocks must be implemented`)
+  }
+
+  /**
+   * Abstract
+   * @param {RemoteDisk} childDisk
+   * @returns {Promise<void>}
+   */
+  async flushMetadata(childDisk) {
+    throw new Error(`flushMetadata must be implemented`)
+  }
+
+  /**
+   * Abstract
+   * @param {RemoteDisk} childDisk
+   */
+  mergeMetadata(childDisk) {
+    throw new Error(`mergeMetadata must be implemented`)
+  }
+
+  /**
+   * Abstract
+   * Checks if the VHD is a differencing disk.
+   * @returns {boolean}
+   */
+  isDifferencing() {
+    throw new Error(`isDifferencing must be implemented`)
+  }
+
+  /**
+   * Abstract
+   * Rename alias/disk
+   * @param {string} newPath
+   */
+  async rename(newPath) {
+    throw new Error(`rename must be implemented`)
+  }
+
+  /**
+   * Abstract
+   * Deletes alias/disk/disks
+   */
+  async unlink() {
+    throw new Error(`unlink must be implemented`)
+  }
+
+  /**
+   * @returns  {Promise<RemoteDisk>}
+   */
+  async openParent() {
+    const parent = await super.openParent()
+    if (!(parent instanceof RemoteDisk)) {
+      throw new Error('parent of a RemoteDisk must be also a RemoteDisk')
+    }
+    return parent
+  }
+}

@xen-orchestra/backups/disks/RemoteVhd.mjs
@@ -1,171 +1,0 @@
-// @ts-check
-
-/**
- * @typedef {import('@xen-orchestra/disk-transform').FileAccessor} FileAccessor
- * @typedef {import('@xen-orchestra/disk-transform').DiskBlock} DiskBlock
- * @typedef {import('@xen-orchestra/disk-transform').Disk} Disk
- * @typedef {import('vhd-lib/Vhd/VhdDirectory.js').VhdDirectory} VhdDirectory
- * @typedef {import('vhd-lib/Vhd/VhdFile.js').VhdFile} VhdFile
- */
-
-import { openVhd } from 'vhd-lib'
-import { DISK_TYPES } from 'vhd-lib/_constants.js'
-import { dirname, join } from 'node:path'
-import { RandomAccessDisk } from '@xen-orchestra/disk-transform'
-/**
- * Represents a remote VHD (Virtual Hard Disk) that extends RandomAccessDisk.
- */
-export class RemoteVhd extends RandomAccessDisk {
-  /**
-   * @type {string}
-   */
-  #path
-
-  /**
-   * @type {FileAccessor}
-   */
-  #handler
-
-  /**
-   * @type {VhdFile | VhdDirectory | undefined}
-   */
-  #vhd
-
-  /**
-   * @type {boolean | undefined}
-   */
-  #isDifferencing
-
-  /**
-   * @type {() => any}
-   */
-  #dispose = () => {}
-
-  /**
-   * @returns {string}
-   */
-  get path() {
-    return this.#path
-  }
-
-  /**
-   * @param {Object} params
-   * @param {FileAccessor} params.handler
-   * @param {string} params.path
-   */
-  constructor({ handler, path }) {
-    super()
-    // @todo : ensure this is the full path from the root of the remote
-    this.#path = path
-    this.#handler = handler
-  }
-
-  /**
-   * @returns {number}
-   */
-  getVirtualSize() {
-    if (this.#vhd === undefined) {
-      throw new Error(`can't call getvirtualsize of a RemoteVhd before init`)
-    }
-    return this.#vhd.footer.currentSize
-  }
-
-  /**
-   * @returns {number}
-   */
-  getBlockSize() {
-    return 2 * 1024 * 1024
-  }
-
-  /**
-   * Initializes the VHD.
-   * @returns {Promise<void>}
-   */
-  async init() {
-    const { value, dispose } = await openVhd(this.#handler, this.#path)
-    this.#vhd = value
-    this.#dispose = dispose
-    await this.#vhd.readBlockAllocationTable()
-    this.#isDifferencing = value.footer.diskType === DISK_TYPES.DIFFERENCING
-  }
-
-  /**
-   * Closes the VHD.
-   * @returns {Promise<void>}
-   */
-  async close() {
-    await this.#dispose()
-  }
-
-  /**
-   * Checks if the VHD contains a specific block.
-   * @param {number} index
-   * @returns {boolean}
-   */
-  hasBlock(index) {
-    if (this.#vhd === undefined) {
-      throw new Error(`can't call hasblock of a RemoteVhd before init`)
-    }
-    return this.#vhd.containsBlock(index)
-  }
-
-  /**
-   * Gets the indexes of all blocks in the VHD.
-   * @returns {Array<number>}
-   */
-  getBlockIndexes() {
-    if (this.#vhd === undefined) {
-      throw new Error(`can't call getBlockIndexes of a RemoteVhd before init`)
-    }
-    const index = []
-    for (let blockIndex = 0; blockIndex < this.#vhd.header.maxTableEntries; blockIndex++) {
-      if (this.hasBlock(blockIndex)) {
-        index.push(blockIndex)
-      }
-    }
-    return index
-  }
-
-  /**
-   * Reads a specific block from the VHD.
-   * @param {number} index
-   * @returns {Promise<DiskBlock>}
-   */
-  async readBlock(index) {
-    if (this.#vhd === undefined) {
-      throw new Error(`can't call readBlock of a RemoteVhd before init`)
-    }
-    const { data } = await this.#vhd.readBlock(index)
-    return {
-      index,
-      data,
-    }
-  }
-  /**
-   *
-   * @returns {RandomAccessDisk}
-   */
-  instantiateParent() {
-    if (this.#vhd === undefined) {
-      throw new Error(`can't call openParent of a RemoteVhd before init`)
-    }
-    const parentPath = this.#vhd.header.parentUnicodeName
-    const fullParentPath = join(dirname(this.#path), parentPath)
-    if (!parentPath) {
-      throw new Error(`Disk ${this.#path} doesn't have parents`)
-    }
-    const parent = new RemoteVhd({ handler: this.#handler, path: fullParentPath })
-    return parent
-  }
-
-  /**
-   * Checks if the VHD is a differencing disk.
-   * @returns {boolean}
-   */
-  isDifferencing() {
-    if (this.#isDifferencing === undefined) {
-      throw new Error(`can't call isDifferencing of a RemoteVhd before init`)
-    }
-    return this.#isDifferencing
-  }
-}

commit fdcc999f66bc78016285a3b785e95b44ea01db95

fix(backups): fix out_of_range -4 merge error (#9782)
Introduced by #9300

Ticket #57310

Fix OUT_OF_RANGE error occurring when resuming failed merge.

@xen-orchestra/backup-archive/src/disks/MergeRemoteDisk.mjs
@@ -142,10 +142,11 @@ export class MergeRemoteDisk {
       const mergeStateContent = await this.#handler.readFile(this.#statePath)
       this.#state = JSON.parse(mergeStateContent)
 
-      // work-around a bug introduce in 97d94b795
+      // work-around to bugs introduced in 97d94b795 and 4e50858
       //
       // currentBlock could be `null` due to the JSON.stringify of a `NaN` value
-      if (this.#state?.currentBlock === null) this.#state.currentBlock = 0
+      // could also be -1 due to being set as Math.min(...merging) - 1 if merging contains 0
+      if (this.#state?.currentBlock === null || this.#state?.currentBlock < 0) this.#state.currentBlock = 0
       this.#isResuming = true
     } catch (error) {
       // @ts-ignore
@@ -255,7 +256,7 @@ export class MergeRemoteDisk {
         const blockSize = await parentDisk.mergeBlock(childDisk, blockId, this.#isResuming)
         this.#state.mergedDataSize += blockSize
 
-        this.#state.currentBlock = Math.min(...merging) - 1
+        this.#state.currentBlock = Math.max(0, Math.min(...merging) - 1)
         merging.delete(blockId)
 
         this.#onProgress({ total: nBlocks, done: counter + 1 })

@xen-orchestra/backup-archive/src/disks/RemoteDisk.test.mjs
@@ -972,6 +972,53 @@ describe('tests MergeRemoteDisk', { concurrency: 1 }, () => {
     assert.deepEqual(parent2.getBlockIndexes(), [0, 1, 2, 3], 'all blocks from the chain should be merged')
   })
 
+  test('mergeRemoteDisk resumes when state has currentBlock = -1', async () => {
+    // If the merge fails it will try to resume the merge at the lowest merging block - 1,
+    // if block 0 was being merged at this time, the state file currentBlock will be set to -1
+    // which is not a valid block id.
+
+    const parentVhdPath = `${basePath}/parent.vhd`
+    await generateVhd(parentVhdPath, { blocks: [1] })
+    const parent = new RemoteVhdDisk({ handler, path: parentVhdPath })
+    await parent.init({ force: false })
+
+    const childVhdPath = `${basePath}/child.vhd`
+    await generateVhd(childVhdPath, { blocks: [0] })
+    const child = new RemoteVhdDisk({ handler, path: childVhdPath })
+    await child.init({ force: false })
+
+    // Fail after asyncEach completes
+    parent.flushMetadata = async () => {
+      throw new Error('simulated interruption')
+    }
+
+    const mergeRemoteDisk = new MergeRemoteDisk(handler, { writeStateDelay: 0 })
+    let threw = false
+    try {
+      await mergeRemoteDisk.merge(parent, child)
+    } catch (err) {
+      threw = true
+      assert.equal(err.message, 'simulated interruption')
+    }
+    assert.ok(threw, 'first merge attempt should throw on simulated interruption')
+
+    const stateFileName = `.parent.vhd.merge.json`
+    assert.ok((await handler.list(basePath)).includes(stateFileName), 'state file should exist after failure')
+
+    // Resume with fresh instances
+    const parent2 = new RemoteVhdDisk({ handler, path: parentVhdPath })
+    await parent2.init({ force: true })
+    const child2 = new RemoteVhdDisk({ handler, path: childVhdPath })
+    await child2.init({ force: true })
+
+    const mergeRemoteDisk2 = new MergeRemoteDisk(handler, { removeUnused: false })
+    await mergeRemoteDisk2.merge(parent2, child2)
+
+    const indexes = parent2.getBlockIndexes()
+    assert.ok(indexes.includes(0), 'block 0 from child should be merged into parent')
+    assert.ok(indexes.includes(1), 'block 1 from parent should remain')
+  })
+
   test('mergeRemoteDisk merge a bigger child into a parent', async () => {
     const parentVhdPath = `${basePath}/parent.vhd`
     await generateVhd(parentVhdPath, { blocks: [0, 1] })