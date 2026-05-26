bf135e02637208e2b1ce75fbf328def106660994
MelissaFrncJrg <melissafranca3@gmail.com>
2026-05-26 09:51:04 +0200

    display object name and not object type

--- @xen-orchestra/web/src/modules/task/composables/xo-task-name-resolver.composable.ts
+++ @xen-orchestra/web/src/modules/task/composables/xo-task-name-resolver.composable.ts
@@ -14,20 +14,24 @@ export function useXoTaskNameResolver() {
   const { getSrById } = useXoSrCollection()
 
   function resolveUuid(uuid: string): TaskNameSegment | undefined {
-    if (getVmById(uuid as XoVm['id'])) {
-      return { text: 'VM', to: { name: '/vm/[id]/dashboard', params: { id: uuid } } }
+    const vm = getVmById(uuid as XoVm['id'])
+    if (vm) {
+      return { text: vm.name_label, to: { name: '/vm/[id]/dashboard', params: { id: uuid } } }
     }
 
-    if (getHostById(uuid as XoHost['id'])) {
-      return { text: 'Host', to: { name: '/host/[id]/dashboard', params: { id: uuid } } }
+    const host = getHostById(uuid as XoHost['id'])
+    if (host) {
+      return { text: host.name_label, to: { name: '/host/[id]/dashboard', params: { id: uuid } } }
     }
 
-    if (getPoolById(uuid as XoPool['id'])) {
-      return { text: 'Pool', to: { name: '/pool/[id]/dashboard', params: { id: uuid } } }
+    const pool = getPoolById(uuid as XoPool['id'])
+    if (pool) {
+      return { text: pool.name_label, to: { name: '/pool/[id]/dashboard', params: { id: uuid } } }
     }
 
-    if (getSrById(uuid as XoSr['id'])) {
-      return { text: 'SR' }
+    const sr = getSrById(uuid as XoSr['id'])
+    if (sr) {
+      return { text: sr.name_label }
     }
 
     return undefined
