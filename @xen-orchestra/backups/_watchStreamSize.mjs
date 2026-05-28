c38f394bd885ada4a66733d4a75cad9addb6b881
Florent BEAUCHAMP <florent.beauchamp@vates.tech>
2026-05-28 03:57:49 -0400

    feat: add task progress for xva transfer on full mirror

--- @xen-orchestra/backups/_watchStreamSize.mjs
+++ @xen-orchestra/backups/_watchStreamSize.mjs
@@ -1,6 +1,11 @@
+import { Task } from '@vates/task'
 export function watchStreamSize(stream, container = { size: 0 }) {
   stream.on('data', data => {
     container.size += data.length
+    if (stream.length) {
+      // empty for xva exported by xapi
+      Task.set('progress', (100 * container.size) / stream.length)
+    }
   })
   stream.pause()
   return container
