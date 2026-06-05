86a2895b7ee3c477ad5461f3b199bb309df209f6
Alexandre Mouillard <alexandre.mouillard@vates.tech>
2026-06-05 15:48:23 +0200

    feat(xo6): sr dedicated page

---
+++ @xen-orchestra/web/src/modules/storage-repository/components/general/StorageRepositorySpace.vue
@@ -0,0 +1,46 @@
+<template>
+  <UiCard>
+    <UiTitle>
+      {{ t('space') }}
+    </UiTitle>
+
+    <div class="content">
+      <VtsProgressBar
+        noruler
+        :current="sr.physical_usage"
+        :total="sr.size"
+        :label="sr.name_label"
+        legend-type="percent"
+        class="progress"
+      />
+
+      <VtsTabularKeyValueList>
+        <VtsTabularKeyValueRow :label="t('used-space')" :value="usedSpace" />
+        <VtsTabularKeyValueRow :label="t('free-space')" :value="freeSpace" />
+        <VtsTabularKeyValueRow :label="t('total-space')" :value="totalSpace" />
+      </VtsTabularKeyValueList>
+    </div>
+  </UiCard>
+</template>
+
+<script setup lang="ts">
+import type { FrontXoSr } from '@/modules/storage-repository/remote-resources/use-xo-sr-collection.ts'
+import VtsProgressBar from '@core/components/progress-bar/VtsProgressBar.vue'
+import VtsTabularKeyValueList from '@core/components/tabular-key-value-list/VtsTabularKeyValueList.vue'
+import VtsTabularKeyValueRow from '@core/components/tabular-key-value-row/VtsTabularKeyValueRow.vue'
+import UiCard from '@core/components/ui/card/UiCard.vue'
+import UiTitle from '@core/components/ui/title/UiTitle.vue'
+import { formatSize } from '@core/utils/size.util.ts'
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+const { sr } = defineProps<{
+  sr: FrontXoSr
+}>()
+
+const { t } = useI18n()
+
+const usedSpace = computed(() => formatSize(sr.physical_usage, 2))
+const totalSpace = computed(() => formatSize(sr.size, 2))
+const freeSpace = computed(() => formatSize(sr.size - sr.physical_usage, 2))
+</script>
---
+++ @xen-orchestra/web/src/pages/sr/[id]/vdis.vue
@@ -0,0 +1,31 @@
+<template>
+  <VtsColumns extra-space-around>
+    <UiCard class="container">
+      <div class="content">
+        <!--
+          WIP: vdis
+          <VdisTable :vdis="vdis" :vm="{}" />
+        -->
+      </div>
+    </UiCard>
+  </VtsColumns>
+</template>
+
+<script setup lang="ts">
+/*
+import type { FrontXoSr } from '@/modules/storage-repository/remote-resources/use-xo-sr-collection.ts'
+import VdisTable from '@/modules/vdi/components/list/VdisTable.vue'
+import { type FrontXoVdi, useXoVdiCollection } from '@/modules/vdi/remote-resources/use-xo-vdi-collection.ts'
+*/
+import VtsColumns from '@core/components/columns/VtsColumns.vue'
+import UiCard from '@core/components/ui/card/UiCard.vue'
+/*
+const { sr } = defineProps<{
+  sr: FrontXoSr
+}>()
+
+const { useGetVdisByIds } = useXoVdiCollection()
+
+const vdis = useGetVdisByIds(() => sr.VDIs as FrontXoVdi['id'][])
+*/
+</script>
