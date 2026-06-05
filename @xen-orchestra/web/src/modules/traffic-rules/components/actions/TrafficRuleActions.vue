87948d0c481e0634a75702229500358791b216d5
sandrine-bd <sandrine.barrucand@vates.tech>
2026-06-05 17:50:09 +0200

    feat(xo6/traffic-rules): add delete button

---
+++ @xen-orchestra/web/src/modules/traffic-rules/components/actions/TrafficRuleActions.vue
@@ -0,0 +1,12 @@
+<template>
+  <TrafficRuleDeleteButton :rule />
+</template>
+
+<script lang="ts" setup>
+import TrafficRuleDeleteButton from '@/modules/traffic-rules/components/actions/delete/TrafficRuleDeleteButton.vue'
+import type { TrafficRule } from '@vates/types'
+
+defineProps<{
+  rule: TrafficRule
+}>()
+</script>
