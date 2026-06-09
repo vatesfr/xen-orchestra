22b971ee09c1a974a5ff074a945ff7060f44404c
sandrine-bd <sandrine.barrucand@vates.tech>
2026-06-09 15:56:12 +0200

    feat(xo6/vdi): add migrate on SR action

--- @xen-orchestra/web/src/modules/vdi/components/actions/VdiActions.vue
+++ @xen-orchestra/web/src/modules/vdi/components/actions/VdiActions.vue
@@ -1,6 +1,7 @@
 <template>
   <VdiImportExportMenu :vdi />
   <VbdDeleteButton v-if="vbd" :vbd :vm />
+  <VdiMigrateButton :vdi />
   <VdiDeleteButton :vdi :vm />
 </template>
 
@@ -9,6 +10,7 @@ import VbdDeleteButton from '@/modules/vbd/components/actions/delete/VbdDeleteBu
 import type { FrontXoVbd } from '@/modules/vbd/remote-resources/use-xo-vbd-collection.ts'
 import VdiDeleteButton from '@/modules/vdi/components/actions/delete/VdiDeleteButton.vue'
 import VdiImportExportMenu from '@/modules/vdi/components/actions/import-export/VdiImportExportMenu.vue'
+import VdiMigrateButton from '@/modules/vdi/components/actions/migrate/VdiMigrateButton.vue'
 import type { FrontXoVdi } from '@/modules/vdi/remote-resources/use-xo-vdi-collection.ts'
 import type { FrontXoVm } from '@/modules/vm/remote-resources/use-xo-vm-collection.ts'
 
---
+++ @xen-orchestra/web/src/modules/vdi/jobs/xo-vdi-migrate.job.ts
@@ -0,0 +1,32 @@
+import { xoVdisArg } from '@/modules/vdi/jobs/xo-vdi-args.ts'
+import type { FrontXoVdi } from '@/modules/vdi/remote-resources/use-xo-vdi-collection.ts'
+import { fetchPost } from '@/shared/utils/fetch.util.ts'
+import { defineJob, defineJobArg, JobError, JobRunningError } from '@core/packages/job'
+import { useI18n } from 'vue-i18n'
+
+const xoSrIdArg = defineJobArg<string>({
+  identify: false,
+  toArray: false,
+})
+
+export const useXoVdiMigrateJob = defineJob('vdi.migrate', [xoVdisArg, xoSrIdArg], () => {
+  const { t } = useI18n()
+
+  return {
+    async run(vdis: FrontXoVdi[], srId: string) {
+      await Promise.all(vdis.map(vdi => fetchPost(`vdis/${vdi.id}/migrate`, { sr_id: srId })))
+    },
+
+    validate: (isRunning, vdis: FrontXoVdi[] | undefined, srId: string | undefined) => {
+      if (!vdis || vdis.length === 0) {
+        throw new JobError(t('job:vdi-migrate:missing-sr'))
+      }
+      if (!srId) {
+        throw new JobError(t('destination-sr-mandatory'))
+      }
+      if (isRunning) {
+        throw new JobRunningError(t('job:delete:in-progress'))
+      }
+    },
+  }
+})
