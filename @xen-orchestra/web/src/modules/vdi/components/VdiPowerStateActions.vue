41dbca4ba1d774ec9f191341d9f131cab9ecb577
Marie Garde <marie.garde@vates.tech>
2026-06-12 09:45:47 +0200

    wip - returns from mr

--- @xen-orchestra/web/src/modules/vdi/components/VdiPowerStateActions.vue
+++ @xen-orchestra/web/src/modules/vdi/components/VdiPowerStateActions.vue
@@ -1,20 +1,21 @@
 <template>
-  <VdiConnectButton :vm :vbd />
-  <VbdConnectButton v-if="vm && vbd" :vm :vbd />
-  <VdiDisconnectButton :vm :vbd />
-  <VbdDisconnectButton v-if="vm && vbd" :vm :vbd />
+  <VbdConnectButton v-if="vm && vbd && !attachedVbd" :vm :vbd is-vdi-page />
+  <VbdDisconnectButton v-if="vm && vbd && attachedVbd" :vm :vbd is-vdi-page />
 </template>
 
 <script lang="ts" setup>
 import VbdConnectButton from '@/modules/vbd/components/actions/connect/VbdConnectButton.vue'
 import VbdDisconnectButton from '@/modules/vbd/components/actions/disconnect/VbdDisconnectButton.vue'
 import type { FrontXoVbd } from '@/modules/vbd/remote-resources/use-xo-vbd-collection.ts'
-import VdiConnectButton from '@/modules/vdi/components/actions/connect/VdiConnectButton.vue'
-import VdiDisconnectButton from '@/modules/vdi/components/actions/disconnect/VdiDisconnectButton.vue'
 import type { FrontXoVm } from '@/modules/vm/remote-resources/use-xo-vm-collection.ts'
+import { computed } from 'vue'
 
-defineProps<{
+const { vbd, vm } = defineProps<{
   vm?: FrontXoVm
   vbd?: FrontXoVbd
 }>()
+
+const attachedVbd = computed(() => {
+  return vbd && vbd.attached
+})
 </script>
