780b32a553d58aaafca5d45b8716f055ca3bc898
Alina Dvornyk <alina-dvornyk@device-49.home>
2026-06-16 14:52:42 +0200

    feat(xo6): redirect after VM and VIF deletion instead of showing 404

--- @xen-orchestra/web/src/modules/vif/composables/use-vif-delete-modal.composable.ts
+++ @xen-orchestra/web/src/modules/vif/composables/use-vif-delete-modal.composable.ts
@@ -3,20 +3,28 @@ import type { FrontXoVif } from '@/modules/vif/remote-resources/use-xo-vif-colle
 import { useModal } from '@core/packages/modal/use-modal.ts'
 import { toComputed } from '@core/utils/to-computed.util.ts'
 import type { MaybeRefOrGetter } from 'vue'
+import { useRouter } from 'vue-router'
 
 export function useVifDeleteModal(rawVifs: MaybeRefOrGetter<FrontXoVif[]>) {
   const vifs = toComputed(rawVifs)
 
   const { run, canRun, isRunning } = useXoVifDeleteJob(vifs)
 
+  const router = useRouter()
+
   const openModal = useModal(() => ({
     component: import('@/modules/vif/components/modal/VifDeleteModal.vue'),
     props: { count: vifs.value.length },
     onConfirm: async () => {
+      const vif = vifs.value[0]
+      let result
       try {
-        await run()
+        result = await run()
       } catch (error) {
         console.error('Error when deleting VIF:', error)
+      }
+      if (result && result[0].status === 'fulfilled' && router.currentRoute.value.path.includes(`/vif/${vif.id}`)) {
+        await router.push({ name: '/vm/[id]/networks', params: { id: vif.$VM } })
       }
     },
   }))
