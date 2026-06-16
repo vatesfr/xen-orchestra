aaea69cad0a45522df6476e68529d862bf00f855
sandrine-bd <sandrine.barrucand@vates.tech>
2026-06-16 13:16:24 +0200

    update CHANGELOG.unreleased.md

--- @xen-orchestra/web/src/modules/vm/components/actions/VmMoreActions.vue
+++ @xen-orchestra/web/src/modules/vm/components/actions/VmMoreActions.vue
@@ -1,10 +1,12 @@
 <template>
   <VmSnapshotButton :vm />
+  <VmExportButton :vm />
   <VmDeleteButton :vm />
 </template>
 
 <script lang="ts" setup>
 import VmDeleteButton from '@/modules/vm/components/actions/delete/VmDeleteButton.vue'
+import VmExportButton from '@/modules/vm/components/actions/export/VmExportButton.vue'
 import VmSnapshotButton from '@/modules/vm/components/actions/snapshot/VmSnapshotButton.vue'
 import type { FrontXoVm } from '@/modules/vm/remote-resources/use-xo-vm-collection.ts'
 
