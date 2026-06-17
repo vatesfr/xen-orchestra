33326a825a681060d97be92bf5f1eaff3b6a6389
Alina Dvornyk <alina-dvornyk@alina-2.home>
2026-06-17 09:17:35 +0200

    fix: apply review changes

--- @xen-orchestra/web-core/lib/tables/column-definitions/tag-column.ts
+++ @xen-orchestra/web-core/lib/tables/column-definitions/tag-column.ts
@@ -4,14 +4,9 @@ import { defineColumn } from '@core/packages/table/define-column'
 import { renderHeadCell } from '@core/tables/helpers/render-head-cell'
 import type { HeaderConfig } from '@core/tables/types.ts'
 import type { MaybeArray } from '@core/types/utility.type'
-import { toArray } from '@core/utils/to-array.utils'
 import { h } from 'vue'
 
-export const useTagColumn = defineColumn((config?: HeaderConfig & { getAccent?: (value: string) => TagAccent }) => ({
+export const useTagColumn = defineColumn((config?: HeaderConfig) => ({
   renderHead: () => renderHeadCell(config?.headerIcon ?? 'fa:square-caret-down', config?.headerLabel),
-  renderBody: (tag: MaybeArray<string>) =>
-    h(VtsTagCell, {
-      tag,
-      accent: config?.getAccent?.(toArray(tag)[0]) ?? 'info',
-    }),
+  renderBody: (tag: MaybeArray<string>, accent: TagAccent = 'info') => h(VtsTagCell, { tag, accent }),
 }))
