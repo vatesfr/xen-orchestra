7221ad48abe8ebf24fde4cd5efecffdae0b487fa
Florent BEAUCHAMP <florent.beauchamp@vates.tech>
2026-06-19 09:35:39 -0400

    fix: emit block from time to time

--- @xen-orchestra/xva/_writeDisk.mjs
+++ @xen-orchestra/xva/_writeDisk.mjs
@@ -5,6 +5,8 @@ import { XVA_DISK_CHUNK_LENGTH } from './_constants.mjs'
 
 export default async function addDisk(pack, disk, basePath) {
   let diskXvaChunk
+  let lastBlockWrittenAt = Date.now()
+  const MAX_INTERVAL_BETWEEN_BLOCKS = 60 * 1000
   if (disk.getBlockSize() < XVA_DISK_CHUNK_LENGTH) {
     diskXvaChunk = new DiskLargerBlock(disk, XVA_DISK_CHUNK_LENGTH)
   } else if (disk.getBlockSize() > XVA_DISK_CHUNK_LENGTH) {
@@ -20,6 +22,12 @@ export default async function addDisk(pack, disk, basePath) {
     let block
     if (counter === 0 || counter === nbBlocks - 1) {
       mustWriteBlock = true // first and last block must be present in xva disks
+    }
+    // write one block from time to time to ensure there is no timeout
+    // occurring while passing empty blocks
+    if (Date.now() - lastBlockWrittenAt > MAX_INTERVAL_BETWEEN_BLOCKS) {
+      mustWriteBlock = true
+      lastBlockWrittenAt = Date.now()
     }
     if (mustWriteBlock || diskXvaChunk.hasBlock(counter)) {
       block = await diskXvaChunk.readBlock(counter)
