a32d3caa2d36f5a36a587f73ca22f9d8416ed821
Sylvere <sylvere.armange@vates.tech>
2026-06-19 12:50:32 +0200

    feat(lite): add SR connect/disconnect/delete

---
+++ @xen-orchestra/lite/src/modules/storage-repository/components/actions/delete/SrDeleteButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <MenuItem
+    icon="action:delete"
+    :disabled="!canDeleteSr"
+    :busy="isDeletingSr"
+    class="delete"
+    @click="openSrDeleteModal()"
+  >
+    {{ t('action:delete') }}
+  </MenuItem>
+</template>
+
+<script lang="ts" setup>
+import type { XenApiSr } from '@/libs/xen-api/xen-api.types.ts'
+import { useSrDeleteModal } from '@/modules/storage-repository/composables/use-sr-delete-modal.composable.ts'
+import MenuItem from '@core/components/menu/MenuItem.vue'
+import { useI18n } from 'vue-i18n'
+
+const { sr } = defineProps<{
+  sr: XenApiSr
+}>()
+
+const { t } = useI18n()
+
+const { openModal: openSrDeleteModal, canRun: canDeleteSr, isRunning: isDeletingSr } = useSrDeleteModal(() => [sr])
+</script>
+
+<style lang="postcss" scoped>
+.delete {
+  color: var(--color-danger-item-base);
+}
+</style>
