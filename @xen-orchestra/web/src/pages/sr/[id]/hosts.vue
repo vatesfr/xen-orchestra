a18de0be65554a5d1f26279af731c89bf71381e5
Alexandre Mouillard <alexandre.mouillard@vates.tech>
2026-06-23 12:15:52 +0200

    Hosts subview

--- @xen-orchestra/web/src/pages/sr/[id]/hosts.vue
+++ @xen-orchestra/web/src/pages/sr/[id]/hosts.vue
@@ -1,31 +1,62 @@
 <template>
-  <VtsColumns extra-space-around>
+  <div class="hosts" :class="{ mobile: uiStore.isSmall }">
     <UiCard class="container">
-      <div class="content">
-        <!-- WIP: hosts -->
-        <HostsTable :hosts="hosts" />
-      </div>
+      <HostsTable :hosts :busy="!areHostsReady" :error="hasHostFetchError" />
     </UiCard>
-  </VtsColumns>
+    <HostSidePanel v-if="selectedHost" :host="selectedHost" @close="selectedHost = undefined" />
+    <UiPanel v-else-if="!uiStore.isSmall">
+      <VtsStateHero format="panel" type="no-selection" size="medium">
+        {{ t('select-to-see-details') }}
+      </VtsStateHero>
+    </UiPanel>
+  </div>
 </template>
 
 <script setup lang="ts">
 import HostsTable from '@/modules/host/components/list/HostsTable.vue'
-import { useXoHostCollection } from '@/modules/host/remote-resources/use-xo-host-collection.ts'
+import HostSidePanel from '@/modules/host/components/list/panel/HostSidePanel.vue'
+import { type FrontXoHost, useXoHostCollection } from '@/modules/host/remote-resources/use-xo-host-collection.ts'
 import { useXoPbdCollection } from '@/modules/pbd/remote-resources/use-xo-pbd-collection.ts'
 import type { FrontXoSr } from '@/modules/storage-repository/remote-resources/use-xo-sr-collection.ts'
-import VtsColumns from '@core/components/columns/VtsColumns.vue'
+import VtsStateHero from '@core/components/state-hero/VtsStateHero.vue'
 import UiCard from '@core/components/ui/card/UiCard.vue'
+import UiPanel from '@core/components/ui/panel/UiPanel.vue'
+import { useRouteQuery } from '@core/composables/route-query.composable.ts'
+import { useUiStore } from '@core/stores/ui.store.ts'
 import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
 
 const { sr } = defineProps<{
   sr: FrontXoSr
 }>()
 
+const uiStore = useUiStore()
+
 const { pbdsBySr } = useXoPbdCollection()
-const { getHostById } = useXoHostCollection()
+const { getHostById, hasHostFetchError, areHostsReady } = useXoHostCollection()
 
 const pbds = computed(() => pbdsBySr.value.get(sr.id))
 
 const hosts = computed(() => (pbds.value ?? []).map(pbd => getHostById(pbd.host)).filter(host => host !== undefined))
+const { t } = useI18n()
+
+const selectedHost = useRouteQuery<FrontXoHost | undefined>('id', {
+  toData: id => getHostById(id as FrontXoHost['id']),
+  toQuery: host => host?.id ?? '',
+})
 </script>
+
+<style scoped lang="postcss">
+.hosts {
+  &:not(.mobile) {
+    display: grid;
+    grid-template-columns: minmax(0, 1fr) 40rem;
+  }
+
+  .container {
+    height: fit-content;
+    gap: 4rem;
+    margin: 0.8rem;
+  }
+}
+</style>
