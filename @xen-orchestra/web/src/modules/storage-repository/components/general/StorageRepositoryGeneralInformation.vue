76f102a4d9233a0485296743523c64c4dccb1be8
Alexandre Mouillard <alexandre.mouillard@vates.tech>
2026-07-01 15:40:22 +0200

    Remove HA

--- @xen-orchestra/web/src/modules/storage-repository/components/general/StorageRepositoryGeneralInformation.vue
+++ @xen-orchestra/web/src/modules/storage-repository/components/general/StorageRepositoryGeneralInformation.vue
@@ -22,9 +22,6 @@
       <VtsTabularKeyValueRow :label="t('storage-format')" :value="sr.SR_type" />
       <VtsTabularKeyValueRow :label="t('access-mode')" :value="isSrSharedI18nValue" />
       <VtsTabularKeyValueRow :label="t('provisioning')" :value="provisioning" />
-      <VtsTabularKeyValueRow :label="t('high-availability')">
-        <template #value><VtsStatus :status="isHaSr" /></template>
-      </VtsTabularKeyValueRow>
     </VtsTabularKeyValueList>
   </UiCard>
 </template>
@@ -32,10 +29,7 @@
 <script setup lang="ts">
 import { useXoPbdUtils } from '@/modules/pbd/composables/xo-pbd-utils.composable.ts'
 import { useXoPbdCollection } from '@/modules/pbd/remote-resources/use-xo-pbd-collection.ts'
-import {
-  type FrontXoSr,
-  useXoSrCollection,
-} from '@/modules/storage-repository/remote-resources/use-xo-sr-collection.ts'
+import { type FrontXoSr } from '@/modules/storage-repository/remote-resources/use-xo-sr-collection.ts'
 import VtsStatus from '@core/components/status/VtsStatus.vue'
 import VtsTabularKeyValueList from '@core/components/tabular-key-value-list/VtsTabularKeyValueList.vue'
 import VtsTabularKeyValueRow from '@core/components/tabular-key-value-row/VtsTabularKeyValueRow.vue'
@@ -53,13 +47,10 @@ const { sr } = defineProps<{
 const { t } = useI18n()
 
 const { getPbdsByIds } = useXoPbdCollection()
-const { isHighAvailabilitySr } = useXoSrCollection()
 
 const { allPbdsConnectionStatus } = useXoPbdUtils(() => getPbdsByIds(sr.$PBDs))
 
 const isSrSharedI18nValue = computed(() => (sr.shared ? t('shared') : t('local')))
 
 const provisioning = computed(() => sr.allocationStrategy ?? t('unknown'))
-
-const isHaSr = isHighAvailabilitySr(() => sr)
 </script>
