2563269981993d429d3d83c05f7433a74d52920a
Alexandre Mouillard <alexandre.mouillard@vates.tech>
2026-07-02 10:54:18 +0200

    SR / VDI space warnings

--- @xen-orchestra/web/src/modules/storage-repository/components/general/StorageRepositorySpace.vue
+++ @xen-orchestra/web/src/modules/storage-repository/components/general/StorageRepositorySpace.vue
@@ -7,8 +7,18 @@
     <div>
       <VtsProgressBar :current="sr.physical_usage" :total="sr.size" :label="sr.name_label" legend-type="percent" />
 
+      <UiAlert v-if="srUsageExceeds80Percent" class="sr-usage-exceeded-alert" accent="warning">
+        {{ t('sr-usage-exceeds-80-percent') }}
+      </UiAlert>
+
       <VtsTabularKeyValueList>
         <VtsTabularKeyValueRow :label="t('vdis-allocated-space')" :value="vdisAllocatedSpace" />
+      </VtsTabularKeyValueList>
+      <div v-if="vdiAllocatedSpaceExceedsSr" class="vdis-allocated-space-warning">
+        <VtsIcon name="status:warning-circle" size="current" />
+        <span class="label">{{ t('vdi-allocated-space-exceeds-sr') }}</span>
+      </div>
+      <VtsTabularKeyValueList>
         <VtsTabularKeyValueRow :label="t('used-space-on-sr')" :value="usedSpace" />
         <VtsTabularKeyValueRow :label="t('free-space-on-sr')" :value="freeSpace" />
         <VtsTabularKeyValueRow :label="t('total-space-on-sr')" :value="totalSpace" />
@@ -19,9 +29,11 @@
 
 <script setup lang="ts">
 import type { FrontXoSr } from '@/modules/storage-repository/remote-resources/use-xo-sr-collection.ts'
+import VtsIcon from '@core/components/icon/VtsIcon.vue'
 import VtsProgressBar from '@core/components/progress-bar/VtsProgressBar.vue'
 import VtsTabularKeyValueList from '@core/components/tabular-key-value-list/VtsTabularKeyValueList.vue'
 import VtsTabularKeyValueRow from '@core/components/tabular-key-value-row/VtsTabularKeyValueRow.vue'
+import UiAlert from '@core/components/ui/alert/UiAlert.vue'
 import UiCard from '@core/components/ui/card/UiCard.vue'
 import UiTitle from '@core/components/ui/title/UiTitle.vue'
 import { formatSize } from '@core/utils/size.util.ts'
@@ -38,4 +50,23 @@ const vdisAllocatedSpace = computed(() => formatSize(sr.usage, 2))
 const usedSpace = computed(() => formatSize(sr.physical_usage, 2))
 const totalSpace = computed(() => formatSize(sr.size, 2))
 const freeSpace = computed(() => formatSize(sr.size - sr.physical_usage, 2))
+
+const srUsageExceeds80Percent = computed(() => sr.size > 0 && sr.physical_usage / sr.size > 0.8)
+const vdiAllocatedSpaceExceedsSr = computed(() => sr.usage > sr.size - sr.physical_usage)
 </script>
+
+<style scoped lang="postcss">
+.vdis-allocated-space-warning {
+  display: flex;
+  align-items: center;
+  margin: 1rem 0;
+
+  .label {
+    margin-left: 2rem;
+  }
+}
+
+.sr-usage-exceeded-alert {
+  margin: 2rem 0;
+}
+</style>
