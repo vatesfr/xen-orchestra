b5ddb56b2d69e31ef8e4f1b5c8848665db99900b
Marie Garde <marie.garde@vates.tech>
2026-07-03 17:04:47 +0200

    feat(xo6): add disable and enable host button

---
+++ @xen-orchestra/web/src/modules/host/components/HostMoreActions.vue
@@ -0,0 +1,14 @@
+<template>
+  <HostDisableButton :host />
+  <HostEnableButton :host />
+</template>
+
+<script lang="ts" setup>
+import HostDisableButton from '@/modules/host/components/actions/disable/HostDisableButton.vue'
+import HostEnableButton from '@/modules/host/components/actions/enable/HostEnableButton.vue'
+import type { FrontXoHost } from '@/modules/host/remote-resources/use-xo-host-collection.ts'
+
+defineProps<{
+  host: FrontXoHost
+}>()
+</script>
