fe82b480ee515cda5705537cbccbd3f201c82df9
Marie Garde <marie.garde@vates.tech>
2026-07-03 17:05:16 +0200

    Add button enable disable

---
+++ @xen-orchestra/web/src/modules/host/jobs/xo-host-enable-host.job.ts
@@ -0,0 +1,31 @@
+import { xoHostArg } from '@/modules/host/jobs/xo-host-args.jobs.ts'
+import type { FrontXoHost } from '@/modules/host/remote-resources/use-xo-host-collection.ts'
+import { useXoTaskUtils } from '@/shared/composables/xo-task-utils.composable.ts'
+import { fetchPost } from '@/shared/utils/fetch.util.ts'
+import { defineJob, JobError, JobRunningError } from '@core/packages/job'
+import { type XoTask } from '@vates/types'
+import { useI18n } from 'vue-i18n'
+
+export const useXoHostEnableJob = defineJob('host.enable', [xoHostArg], () => {
+  const { t } = useI18n()
+  const { monitorTask } = useXoTaskUtils()
+
+  return {
+    async run(host: FrontXoHost | undefined) {
+      if (host) {
+        const { taskId } = await fetchPost<{ taskId: XoTask['id'] }>(`hosts/${host.id}/actions/enable`)
+        await monitorTask(taskId)
+      }
+    },
+
+    validate: (isRunning, host: FrontXoHost | undefined) => {
+      if (!host) {
+        throw new JobError(t('job:host-enable:missing-host'))
+      }
+
+      if (isRunning) {
+        throw new JobRunningError(t('job:enable:in-progress'))
+      }
+    },
+  }
+})
