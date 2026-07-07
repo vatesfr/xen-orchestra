3db255b41d6ad4b635ec28da4caae33d552dd37d
Marie Garde <marie.garde@vates.tech>
2026-07-07 15:34:12 +0200

    wip

---
+++ @xen-orchestra/web/src/modules/host/components/actions/shutdown/HostShutdownButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <UiButton size="medium" variant="tertiary" accent="brand" left-icon="status:success-circle">
+    {{ t('action:shutdown') }}
+  </UiButton>
+</template>
+
+<script lang="ts" setup>
+import type { FrontXoHost } from '@/modules/host/remote-resources/use-xo-host-collection.ts'
+import UiButton from '@core/components/ui/button/UiButton.vue'
+import { useI18n } from 'vue-i18n'
+
+const { host } = defineProps<{
+  host: FrontXoHost
+}>()
+
+const { t } = useI18n()
+</script>
