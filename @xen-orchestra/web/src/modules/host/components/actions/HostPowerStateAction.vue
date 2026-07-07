dd466a221388b1e6ae0fe0ba98e8c45d62a0e876
Marie Garde <marie.garde@vates.tech>
2026-07-07 15:34:12 +0200

    wip

---
+++ @xen-orchestra/web/src/modules/host/components/actions/HostPowerStateAction.vue
@@ -0,0 +1,17 @@
+<template>
+  <HostShutdownButton :host />
+  <HostStartButton :host />
+</template>
+
+<script lang="ts" setup>
+import HostShutdownButton from '@/modules/host/components/actions/shutdown/HostShutdownButton.vue'
+import HostStartButton from '@/modules/host/components/actions/start/HostStartButton.vue'
+import type { FrontXoHost } from '@/modules/host/remote-resources/use-xo-host-collection.ts'
+import { useI18n } from 'vue-i18n'
+
+defineProps<{
+  host: FrontXoHost
+}>()
+
+const { t } = useI18n()
+</script>
