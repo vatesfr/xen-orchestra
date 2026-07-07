ef1f16758d334b277363ddd60984d35006e45609
Marie Garde <marie.garde@vates.tech>
2026-07-07 08:50:29 +0200

    feat(xo6): add copyall button xo and lite

---
+++ @xen-orchestra/web-core/lib/components/copy-all-menu-item/VtsCopyAllMenuItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <MenuList placement="bottom-end">
+    <template #trigger="{ open }">
+      <UiButtonIcon
+        v-tooltip="{
+          placement: 'left',
+          content: t('more-actions'),
+        }"
+        icon="action:more-actions"
+        accent="brand"
+        size="medium"
+        :disabled="!isSupported"
+        @click="open($event)"
+      />
+    </template>
+    <MenuItem icon="action:copy" @click="copy()">
+      {{ t('action:copy-all') }}
+    </MenuItem>
+  </MenuList>
+</template>
+
+<script setup lang="ts">
+import MenuItem from '@core/components/menu/MenuItem.vue'
+import MenuList from '@core/components/menu/MenuList.vue'
+import UiButtonIcon from '@core/components/ui/button-icon/UiButtonIcon.vue'
+import { vTooltip } from '@core/directives/tooltip.directive.ts'
+import { useClipboard } from '@vueuse/core'
+import { useI18n } from 'vue-i18n'
+
+const { values } = defineProps<{
+  values: string[]
+}>()
+
+const { t } = useI18n()
+
+const { copy, isSupported } = useClipboard({ source: () => values.join('\n'), legacy: true })
+</script>
