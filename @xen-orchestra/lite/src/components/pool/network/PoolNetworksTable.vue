a6c13334c75d464a71947b0cadf2bb4ffcfb7b60
Alexandre Mouillard <alexandre.mouillard@vates.tech>
2026-07-07 11:59:03 +0200

    feat(lite): add copy info into JSON action to pool networks table

--- @xen-orchestra/lite/src/components/pool/network/PoolNetworksTable.vue
+++ @xen-orchestra/lite/src/components/pool/network/PoolNetworksTable.vue
@@ -4,10 +4,48 @@
       {{ t('networks') }}
     </UiTitle>
     <div class="container">
-      <UiQuerySearchBar @search="value => (searchQuery = value)" />
+      <div class="table-actions">
+        <UiQuerySearchBar @search="value => (searchQuery = value)" />
+        <UiTableActions :title="t('table-actions')">
+          <UiButton
+            v-tooltip="t('coming-soon!')"
+            disabled
+            left-icon="fa:edit"
+            variant="tertiary"
+            accent="brand"
+            size="medium"
+          >
+            {{ t('action:edit') }}
+          </UiButton>
+          <UiButton
+            v-tooltip="copied && t('copied')"
+            :disabled="!isClipboardSupported || selectedNetworkIds.length === 0"
+            :left-icon="copied ? 'fa:check-circle' : 'action:copy'"
+            variant="tertiary"
+            accent="brand"
+            size="medium"
+            @click="copy()"
+          >
+            {{ t('action:copy-info-json') }}
+          </UiButton>
+          <UiButton
+            v-tooltip="t('coming-soon!')"
+            disabled
+            left-icon="fa:trash"
+            variant="tertiary"
+            accent="danger"
+            size="medium"
+          >
+            {{ t('action:delete') }}
+          </UiButton>
+        </UiTableActions>
+      </div>
       <VtsTable :state :pagination-bindings sticky="right">
         <thead>
           <tr>
+            <VtsHeaderCell>
+              <UiCheckbox v-model="areAllNetworksSelected" accent="brand" />
+            </VtsHeaderCell>
             <HeadCells />
           </tr>
         </thead>
@@ -17,6 +55,9 @@
             :key="network.uuid"
             :selected="selectedNetworkId === network.uuid"
           >
+            <UiTableCell>
+              <UiCheckbox v-model="selectedNetworkIds" :value="network.uuid" accent="brand" />
+            </UiTableCell>
             <BodyCells :item="network" />
           </VtsRow>
         </tbody>
@@ -26,18 +67,26 @@
 </template>
 
 <script setup lang="ts">
+import useMultiSelect from '@/composables/multi-select.composable'
 import type { XenApiNetwork } from '@/libs/xen-api/xen-api.types'
 import { useNetworkStore } from '@/stores/xen-api/network.store'
 import { usePifMetricsStore } from '@/stores/xen-api/pif-metrics.store'
 import { usePifStore } from '@/stores/xen-api/pif.store'
+import VtsHeaderCell from '@core/components/table/cells/VtsHeaderCell.vue'
 import VtsRow from '@core/components/table/VtsRow.vue'
 import VtsTable from '@core/components/table/VtsTable.vue'
+import UiButton from '@core/components/ui/button/UiButton.vue'
+import UiCheckbox from '@core/components/ui/checkbox/UiCheckbox.vue'
+import UiDropdownButton from '@core/components/ui/dropdown-button/UiDropdownButton.vue'
 import UiQuerySearchBar from '@core/components/ui/query-search-bar/UiQuerySearchBar.vue'
+import UiTableActions from '@core/components/ui/table-actions/UiTableActions.vue'
+import UiTableCell from '@core/components/ui/table-cell/UiTableCell.vue'
 import UiTitle from '@core/components/ui/title/UiTitle.vue'
 import { usePagination } from '@core/composables/pagination.composable'
 import { useRouteQuery } from '@core/composables/route-query.composable'
 import { useTableState } from '@core/composables/table-state.composable'
 import { useNetworkColumns } from '@core/tables/column-sets/network-columns'
+import { useClipboard } from '@vueuse/core'
 import { logicNot } from '@vueuse/math'
 import { computed, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
@@ -98,6 +147,26 @@ const filteredNetworks = computed(() => {
 })
 
 const { pageRecords: paginatedNetworks, paginationBindings } = usePagination('networks', filteredNetworks)
+
+const { selected: selectedNetworkIds, areAllSelected: areAllNetworksSelected } = useMultiSelect(
+  computed(() => networks.map(network => network.uuid)),
+  computed(() => paginatedNetworks.value.map(network => network.uuid))
+)
+
+const selectedNetworksAsJson = computed(() => {
+  const selectedNetworks = networks.filter(network => selectedNetworkIds.value.includes(network.uuid))
+
+  return JSON.stringify(selectedNetworks)
+})
+
+const {
+  copy,
+  copied,
+  isSupported: isClipboardSupported,
+} = useClipboard({
+  source: selectedNetworksAsJson,
+  legacy: true,
+})
 
 const state = useTableState({
   busy: logicNot(isReady),
