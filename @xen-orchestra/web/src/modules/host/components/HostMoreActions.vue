8f7d4153c5c16757c9c580659de2a8fcb8d93f44
Marie Garde <marie.garde@vates.tech>
2026-07-08 15:14:03 +0200

    wip

--- @xen-orchestra/web/src/modules/host/components/HostMoreActions.vue
+++ @xen-orchestra/web/src/modules/host/components/HostMoreActions.vue
@@ -3,12 +3,14 @@
   <HostEnableButton v-else :host />
   <VtsDivider type="stretch" />
   <HostDownloadButton :host-id="host.id" />
+  <HostForgetButton v-if="!hostIsHalted" :host />
 </template>
 
 <script lang="ts" setup>
 import HostDisableButton from '@/modules/host/components/actions/disable/HostDisableButton.vue'
 import HostDownloadButton from '@/modules/host/components/actions/download/HostDownloadButton.vue'
 import HostEnableButton from '@/modules/host/components/actions/enable/HostEnableButton.vue'
+import HostForgetButton from '@/modules/host/components/actions/forget/HostForgetButton.vue'
 import type { FrontXoHost } from '@/modules/host/remote-resources/use-xo-host-collection.ts'
 import VtsDivider from '@core/components/divider/VtsDivider.vue'
 import { HOST_POWER_STATE } from '@vates/types'
