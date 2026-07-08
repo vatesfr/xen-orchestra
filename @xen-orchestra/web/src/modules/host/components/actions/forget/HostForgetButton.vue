59984fc3d83810d58f18e0866424b8159b6fddd8
Marie Garde <marie.garde@vates.tech>
2026-07-08 16:39:40 +0200

    update button for menuitem

--- @xen-orchestra/web/src/modules/host/components/actions/forget/HostForgetButton.vue
+++ @xen-orchestra/web/src/modules/host/components/actions/forget/HostForgetButton.vue
@@ -1,22 +1,22 @@
 <template>
-  <UiButton
+  <MenuItem
     v-tooltip="!canForgetHost && forgetHostErrorMessage"
     size="medium"
     variant="tertiary"
     accent="danger"
     :disabled="!canForgetHost"
-    left-icon="action:forget"
+    icon="action:forget"
     :busy="isForgettingHost"
     @click="openForgetModal()"
   >
     {{ t('action:forget-host') }}
-  </UiButton>
+  </MenuItem>
 </template>
 
 <script lang="ts" setup>
 import { useHostForgetModal } from '@/modules/host/composables/use-host-forget-modal.composable.ts'
 import type { FrontXoHost } from '@/modules/host/remote-resources/use-xo-host-collection.ts'
-import UiButton from '@core/components/ui/button/UiButton.vue'
+import MenuItem from '@core/components/menu/MenuItem.vue'
 import { vTooltip } from '@core/directives/tooltip.directive.ts'
 import { useI18n } from 'vue-i18n'
 
