277e2f572e2b56532a72a4a959797b33b156a754
Marie Garde <marie.garde@vates.tech>
2026-07-08 15:14:03 +0200

    wip

--- @xen-orchestra/web/src/modules/host/components/HostMoreActions.vue
+++ @xen-orchestra/web/src/modules/host/components/HostMoreActions.vue
@@ -3,16 +3,22 @@
   <HostEnableButton v-else :host />
   <VtsDivider type="stretch" />
   <HostDownloadButton :host-id="host.id" />
+  <HostForgetButton v-if="!hostIsHalted" :host />
 </template>
 
 <script lang="ts" setup>
 import HostDisableButton from '@/modules/host/components/actions/disable/HostDisableButton.vue'
 import HostDownloadButton from '@/modules/host/components/actions/download/HostDownloadButton.vue'
 import HostEnableButton from '@/modules/host/components/actions/enable/HostEnableButton.vue'
+import HostForgetButton from '@/modules/host/components/actions/forget/HostForgetButton.vue'
 import type { FrontXoHost } from '@/modules/host/remote-resources/use-xo-host-collection.ts'
 import VtsDivider from '@core/components/divider/VtsDivider.vue'
+import { HOST_POWER_STATE } from '@vates/types'
+import { computed } from 'vue'
 
-defineProps<{
+const { host } = defineProps<{
   host: FrontXoHost
 }>()
+
+const hostIsHalted = computed(() => host.power_state === HOST_POWER_STATE.HALTED)
 </script>
---
+++ @xen-orchestra/web/src/modules/host/components/actions/forget/HostForgetButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <UiButton
+    v-tooltip="!canForgetHost && forgetHostErrorMessage"
+    size="medium"
+    variant="tertiary"
+    accent="danger"
+    :disabled="!canForgetHost"
+    left-icon="action:disable"
+    :busy="isForgettingHost"
+    @click="openForgetModal()"
+  >
+    {{ t('action:host-disable') }}
+  </UiButton>
+</template>
+
+<script lang="ts" setup>
+import { useHostForgetModal } from '@/modules/host/composables/use-host-forget-modal.composable.ts'
+import type { FrontXoHost } from '@/modules/host/remote-resources/use-xo-host-collection.ts'
+import UiButton from '@core/components/ui/button/UiButton.vue'
+import { vTooltip } from '@core/directives/tooltip.directive.ts'
+import { useI18n } from 'vue-i18n'
+
+const { host } = defineProps<{
+  host: FrontXoHost
+}>()
+
+const { t } = useI18n()
+
+const {
+  openModal: openForgetModal,
+  canRun: canForgetHost,
+  isRunning: isForgettingHost,
+  errorMessage: forgetHostErrorMessage,
+} = useHostForgetModal(() => host)
+</script>
