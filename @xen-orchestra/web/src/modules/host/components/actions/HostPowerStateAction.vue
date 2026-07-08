f36a17ab13b951c96ea0b2a5d0893346a5ac54b1
Marie Garde <marie.garde@vates.tech>
2026-07-08 10:44:50 +0200

    add shutdown and start button and job

--- @xen-orchestra/web/src/modules/host/components/actions/HostPowerStateAction.vue
+++ @xen-orchestra/web/src/modules/host/components/actions/HostPowerStateAction.vue
@@ -1,17 +1,20 @@
 <template>
-  <HostShutdownButton :host />
-  <HostStartButton :host />
+  <HostShutdownButton v-if="hostIsRunning" :host />
+  <HostStartButton v-if="hostIsHalted" :host />
 </template>
 
 <script lang="ts" setup>
 import HostShutdownButton from '@/modules/host/components/actions/shutdown/HostShutdownButton.vue'
 import HostStartButton from '@/modules/host/components/actions/start/HostStartButton.vue'
 import type { FrontXoHost } from '@/modules/host/remote-resources/use-xo-host-collection.ts'
-import { useI18n } from 'vue-i18n'
+import { HOST_POWER_STATE } from '@vates/types'
+import { computed } from 'vue'
 
-defineProps<{
+const { host } = defineProps<{
   host: FrontXoHost
 }>()
 
-const { t } = useI18n()
+const hostIsHalted = computed(() => host.power_state === HOST_POWER_STATE.HALTED)
+
+const hostIsRunning = computed(() => host.power_state === HOST_POWER_STATE.RUNNING)
 </script>
--- @xen-orchestra/web/src/modules/host/components/actions/start/HostStartButton.vue
+++ @xen-orchestra/web/src/modules/host/components/actions/start/HostStartButton.vue
@@ -1,36 +1,30 @@
 <template>
-  <UiButton
-    v-tooltip="!canEnableHost && enableHostErrorMessage"
-    size="medium"
-    variant="tertiary"
-    accent="brand"
-    :disabled="!canEnableHost"
-    left-icon="status:success-circle"
-    :busy="isEnablingHost"
-    @click="openEnabledStateModal()"
-  >
-    {{ t('action:host-enable') }}
+  <!--  <UiButton -->
+  <!--    v-tooltip="!canEnableHost && enableHostErrorMessage" -->
+  <!--    size="medium" -->
+  <!--    variant="tertiary" -->
+  <!--    accent="brand" -->
+  <!--    :disabled="!canEnableHost" -->
+  <!--    left-icon="status:success-circle" -->
+  <!--    :busy="isEnablingHost" -->
+  <!--    @click="openEnabledStateModal()" -->
+  <!--  > -->
+  <!--    {{ t('action:start') }} -->
+  <!--  </UiButton> -->
+
+  <UiButton size="medium" variant="tertiary" accent="brand" left-icon="fa:play">
+    {{ t('action:start') }}
   </UiButton>
 </template>
 
 <script lang="ts" setup>
-import { useHostEnabledStateToggleModal } from '@/modules/host/composables/use-host-enabled-state-toggle-modal.composable.ts'
 import type { FrontXoHost } from '@/modules/host/remote-resources/use-xo-host-collection.ts'
-import { ENABLED_STATE_ACTION } from '@/modules/host/types/enabled-state.ts'
 import UiButton from '@core/components/ui/button/UiButton.vue'
-import { vTooltip } from '@core/directives/tooltip.directive.ts'
 import { useI18n } from 'vue-i18n'
 
-const { host } = defineProps<{
+defineProps<{
   host: FrontXoHost
 }>()
 
 const { t } = useI18n()
-
-const {
-  openModal: openEnabledStateModal,
-  canRun: canEnableHost,
-  isRunning: isEnablingHost,
-  errorMessage: enableHostErrorMessage,
-} = useHostEnabledStateToggleModal(ENABLED_STATE_ACTION.ENABLE, () => host)
 </script>
