1101cbcd45823587989147ad2f1b299c9fc698b9
Alexandre Mouillard <alexandre.mouillard@vates.tech>
2026-07-09 11:08:17 +0200

    feat(xo6): sr dedicated page general tab

---
+++ @xen-orchestra/web/src/pages/sr/[id]/general.vue
@@ -0,0 +1,26 @@
+<template>
+  <VtsColumns extra-space-around>
+    <VtsColumn>
+      <StorageRepositoryGeneralInformation :sr />
+      <StorageRepositoryPbdDetails :sr />
+    </VtsColumn>
+    <VtsColumn>
+      <StorageRepositorySpace :sr />
+      <StorageRepositoryCustomFields :sr />
+    </VtsColumn>
+  </VtsColumns>
+</template>
+
+<script setup lang="ts">
+import StorageRepositoryCustomFields from '@/modules/storage-repository/components/general/StorageRepositoryCustomFields.vue'
+import StorageRepositoryGeneralInformation from '@/modules/storage-repository/components/general/StorageRepositoryGeneralInformation.vue'
+import StorageRepositoryPbdDetails from '@/modules/storage-repository/components/general/StorageRepositoryPbdDetails.vue'
+import StorageRepositorySpace from '@/modules/storage-repository/components/general/StorageRepositorySpace.vue'
+import type { FrontXoSr } from '@/modules/storage-repository/remote-resources/use-xo-sr-collection.ts'
+import VtsColumn from '@core/components/column/VtsColumn.vue'
+import VtsColumns from '@core/components/columns/VtsColumns.vue'
+
+const { sr } = defineProps<{
+  sr: FrontXoSr
+}>()
+</script>
