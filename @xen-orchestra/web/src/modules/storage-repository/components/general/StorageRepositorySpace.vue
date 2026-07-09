f9430deb14e819f08a6a56df8632fc5fec7967db
Alexandre Mouillard <alexandre.mouillard@vates.tech>
2026-07-10 00:12:07 +0200

    Use UiInfo component and normalize margins

--- @xen-orchestra/web/src/modules/storage-repository/components/general/StorageRepositorySpace.vue
+++ @xen-orchestra/web/src/modules/storage-repository/components/general/StorageRepositorySpace.vue
@@ -5,7 +5,13 @@
     </UiTitle>
 
     <div>
-      <VtsProgressBar :current="sr.physical_usage" :total="sr.size" :label="sr.name_label" legend-type="percent" />
+      <VtsProgressBar
+        class="progress-bar"
+        :current="sr.physical_usage"
+        :total="sr.size"
+        :label="sr.name_label"
+        legend-type="percent"
+      />
 
       <UiAlert v-if="srUsageWarning" class="sr-usage-exceeded-alert" accent="warning">
         {{ t('sr-usage-exceeds-80-percent') }}
@@ -14,10 +20,9 @@
       <VtsTabularKeyValueList>
         <VtsTabularKeyValueRow :label="t('vdis-allocated-space')" :value="vdisAllocatedSpace" />
       </VtsTabularKeyValueList>
-      <div v-if="vdiAllocatedSpaceWarning" class="vdis-allocated-space-warning">
-        <VtsIcon name="status:warning-circle" size="current" />
-        <span class="label">{{ t('vdi-allocated-space-exceeds-sr') }}</span>
-      </div>
+      <UiInfo v-if="vdiAllocatedSpaceWarning" class="vdis-allocated-space-warning" accent="warning" wrap>
+        {{ t('vdi-allocated-space-exceeds-sr') }}
+      </UiInfo>
       <VtsTabularKeyValueList>
         <VtsTabularKeyValueRow :label="t('used-space-on-sr')" :value="usedSpace" />
         <VtsTabularKeyValueRow :label="t('free-space-on-sr')" :value="freeSpace" />
@@ -29,12 +34,12 @@
 
 <script setup lang="ts">
 import type { FrontXoSr } from '@/modules/storage-repository/remote-resources/use-xo-sr-collection.ts'
-import VtsIcon from '@core/components/icon/VtsIcon.vue'
 import VtsProgressBar from '@core/components/progress-bar/VtsProgressBar.vue'
 import VtsTabularKeyValueList from '@core/components/tabular-key-value-list/VtsTabularKeyValueList.vue'
 import VtsTabularKeyValueRow from '@core/components/tabular-key-value-row/VtsTabularKeyValueRow.vue'
 import UiAlert from '@core/components/ui/alert/UiAlert.vue'
 import UiCard from '@core/components/ui/card/UiCard.vue'
+import UiInfo from '@core/components/ui/info/UiInfo.vue'
 import UiTitle from '@core/components/ui/title/UiTitle.vue'
 import { formatSize } from '@core/utils/size.util.ts'
 import { computed } from 'vue'
@@ -56,17 +61,12 @@ const vdiAllocatedSpaceWarning = computed(() => sr.usage > sr.size - sr.physical
 </script>
 
 <style scoped lang="postcss">
-.vdis-allocated-space-warning {
-  display: flex;
-  align-items: center;
-  margin: 1rem 0;
-
-  .label {
-    margin-left: 2rem;
-  }
+.progress-bar {
+  margin: 0 0 2rem;
 }
 
-.sr-usage-exceeded-alert {
+.sr-usage-exceeded-alert,
+.vdis-allocated-space-warning {
   margin: 2rem 0;
 }
 </style>
