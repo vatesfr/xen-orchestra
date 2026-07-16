729e2cebc17f6411fda878f9543319fa555e5c2a
Marie Garde <marie.garde@vates.tech>
2026-07-16 14:22:08 +0200

    refacto for MR

--- @xen-orchestra/web/src/modules/host/types/enabled-state.ts
+++
@@ -1,6 +0,0 @@
-export const ENABLED_STATE_ACTION = {
-  ENABLE: 'enable',
-  DISABLE: 'disable',
-} as const
-
-export type EnabledStateAction = (typeof ENABLED_STATE_ACTION)[keyof typeof ENABLED_STATE_ACTION]
