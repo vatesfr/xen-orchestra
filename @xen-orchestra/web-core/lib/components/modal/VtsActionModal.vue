41a2a04efe538739051e9a002c8881bf5e399f60
Marie Garde <marie.garde@vates.tech>
2026-07-16 15:07:19 +0200

    refacto for MR

--- @xen-orchestra/web-core/lib/components/modal/VtsActionModal.vue
+++ @xen-orchestra/web-core/lib/components/modal/VtsActionModal.vue
@@ -4,7 +4,7 @@
       <span>{{ modalTexts.title }}</span>
     </template>
     <template #content>
-      <span v-if="modalTexts.message">{{ modalTexts.message }}</span>
+      <span>{{ modalTexts.message }}</span>
     </template>
     <template #buttons>
       <VtsOverlayCancelButton @click="emit('cancel')">{{ t('action:go-back') }}</VtsOverlayCancelButton>
