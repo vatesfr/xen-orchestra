f09d967c021310499a4f189c57880f4f2944f1f7
ayoublkj <ayoub.el-kajji@vates.tech>
2026-07-20 15:36:59 +0200

    adding missing dependencies

--- @vates/app-conf/example.js
+++ @vates/app-conf/example.js
@@ -7,5 +7,6 @@ const appconf = require('./')
 // ===================================================================
 
 appconf.load('my-application').then(function (config) {
+  // eslint-disable-next-line no-console
   console.log(config)
 })
