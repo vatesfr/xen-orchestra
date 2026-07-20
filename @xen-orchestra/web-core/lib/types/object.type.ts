e41757784d5fedf9f7b59ec808ca32d0f02ca312
Marie Garde <marie.garde@vates.tech>
2026-07-20 09:03:19 +0200

    wip forget

--- @xen-orchestra/web-core/lib/types/object.type.ts
+++ @xen-orchestra/web-core/lib/types/object.type.ts
@@ -1,7 +1,7 @@
 export type ObjectType = 'vm' | 'host'
 
 export type VmActions = 'reboot' | 'shutdown' | 'force-reboot' | 'force-shutdown'
-export type HostActions = 'enable' | 'disable' | 'shutdown' | 'start'
+export type HostActions = 'enable' | 'disable' | 'shutdown' | 'start' | 'forget'
 
 export type VmBlockedOperations =
   | 'clean_shutdown'
