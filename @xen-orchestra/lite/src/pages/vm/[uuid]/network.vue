0d53d7621a9f7cadac805b2338d23a8bbc9c2355
Elise-F <elise.franzini@gmail.com>
2026-07-20 15:12:46 +0200

    feat(lite): add the “connect/disconnect” action for a VIF on a VM (#10080)

--- @xen-orchestra/lite/src/pages/vm/[uuid]/network.vue
+++ @xen-orchestra/lite/src/pages/vm/[uuid]/network.vue
@@ -1,7 +1,7 @@
 <template>
   <VtsContentSidePanel class="network">
     <UiCard class="container">
-      <VmVifsTable :vifs />
+      <VmVifsTable v-if="vm" :vifs :vm />
     </UiCard>
     <VmVifsSidePanel :vif="selectedVif" @close="selectedVif = undefined" />
   </VtsContentSidePanel>
@@ -10,7 +10,7 @@
 <script lang="ts" setup>
 import VmVifsSidePanel from '@/components/vm/network/VmVifsSidePanel.vue'
 import VmVifsTable from '@/components/vm/network/VmVifsTable.vue'
-import type { XenApiVif } from '@/libs/xen-api/xen-api.types'
+import type { XenApiVif, XenApiVm } from '@/libs/xen-api/xen-api.types.ts'
 import { usePageTitleStore } from '@/stores/page-title.store'
 import { useVifStore } from '@/stores/xen-api/vif.store'
 import { useVmStore } from '@/stores/xen-api/vm.store'
@@ -18,22 +18,21 @@ import VtsContentSidePanel from '@core/components/layout/VtsContentSidePanel.vue
 import UiCard from '@core/components/ui/card/UiCard.vue'
 import { useRouteQuery } from '@core/composables/route-query.composable'
 import { useArrayFilter } from '@vueuse/shared'
+import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { useRoute } from 'vue-router'
 
 const { records } = useVifStore().subscribe()
-const { getByOpaqueRef } = useVmStore().subscribe()
+const { getByUuid } = useVmStore().subscribe()
 
 const route = useRoute<'/vm/[uuid]/network'>()
 
 const { t } = useI18n()
 usePageTitleStore().setTitle(t('network'))
 
-const vifs = useArrayFilter(records, vif => {
-  const vm = getByOpaqueRef(vif.VM)
+const vm = computed(() => getByUuid(route.params.uuid as XenApiVm['uuid']))
 
-  return vm?.uuid === route.params.uuid
-})
+const vifs = useArrayFilter(records, vif => vif.VM === vm.value?.$ref)
 
 const selectedVif = useRouteQuery<XenApiVif | undefined>('id', {
   toData: id => vifs.value.find(vif => vif.uuid === id),
