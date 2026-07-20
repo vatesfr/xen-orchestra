2a91a7de3b7ff5ab5d0aec4c73d4c0902e22776b
Marie Garde <marie.garde@vates.tech>
2026-07-20 09:03:19 +0200

    wip forget

--- @xen-orchestra/web/src/modules/host/components/actions/forget/HostForgetButton.vue
+++ @xen-orchestra/web/src/modules/host/components/actions/forget/HostForgetButton.vue
@@ -4,20 +4,22 @@
     size="medium"
     variant="tertiary"
     accent="danger"
+    class="forget"
     :disabled="!canForgetHost"
     icon="action:forget"
     :busy="isForgettingHost"
-    @click="openForgetModal()"
+    @click="openForgetHostModal()"
   >
     {{ t('action:forget-host') }}
   </MenuItem>
 </template>
 
 <script lang="ts" setup>
-import { useHostForgetModal } from '@/modules/host/composables/use-host-forget-modal.composable.ts'
+import { useXoHostForgetJob } from '@/modules/host/jobs/xo-host-forget.job.ts'
 import type { FrontXoHost } from '@/modules/host/remote-resources/use-xo-host-collection.ts'
 import MenuItem from '@core/components/menu/MenuItem.vue'
 import { vTooltip } from '@core/directives/tooltip.directive.ts'
+import { useModal } from '@core/packages/modal/use-modal.ts'
 import { useI18n } from 'vue-i18n'
 
 const { host } = defineProps<{
@@ -27,9 +29,27 @@ const { host } = defineProps<{
 const { t } = useI18n()
 
 const {
-  openModal: openForgetModal,
+  run: forgetHost,
   canRun: canForgetHost,
   isRunning: isForgettingHost,
   errorMessage: forgetHostErrorMessage,
-} = useHostForgetModal(() => host)
+} = useXoHostForgetJob(() => host)
+
+const openForgetHostModal = useModal({
+  component: import('@core/components/modal/VtsActionModal.vue'),
+  props: {
+    accent: 'warning',
+    action: 'forget',
+    object: 'host',
+    hostName: host.name_label,
+    icon: 'status:warning-picto',
+  },
+  onConfirm: () => forgetHost(),
+})
 </script>
+
+<style lang="postcss" scoped>
+.forget {
+  color: var(--color-danger-item-base);
+}
+</style>
