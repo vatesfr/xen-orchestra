4e6b3e22414e042313abc32f73011f951b6bd552
Marie Garde <marie.garde@vates.tech>
2026-07-21 10:45:08 +0200

    add enable disable un treeview

--- @xen-orchestra/web/src/modules/host/components/actions/HostTreeActions.vue
+++ @xen-orchestra/web/src/modules/host/components/actions/HostTreeActions.vue
@@ -1,9 +1,13 @@
 <template>
   <HostDownloadButton :host-id="host.id" />
+  <HostDisableButton v-if="host.enabled" :host />
+  <HostEnableButton v-else :host />
 </template>
 
 <script setup lang="ts">
+import HostDisableButton from '@/modules/host/components/actions/disable/HostDisableButton.vue'
 import HostDownloadButton from '@/modules/host/components/actions/download/HostDownloadButton.vue'
+import HostEnableButton from '@/modules/host/components/actions/enable/HostEnableButton.vue'
 import type { FrontXoHost } from '@/modules/host/remote-resources/use-xo-host-collection.ts'
 
 defineProps<{
