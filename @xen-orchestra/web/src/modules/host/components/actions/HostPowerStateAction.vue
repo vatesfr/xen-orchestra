f74c1921ac2a74137e8e899ad7dc57a521b38bcb
Marie Garde <marie.garde@vates.tech>
2026-07-22 10:25:12 +0200

    feat(xo6): add host smart reboot

--- @xen-orchestra/web/src/modules/host/components/actions/HostPowerStateAction.vue
+++ @xen-orchestra/web/src/modules/host/components/actions/HostPowerStateAction.vue
@@ -1,6 +1,7 @@
 <template>
   <HostRebootButton v-if="hostIsRunning" :host />
   <HostForceRebootButton v-if="hostIsRunning" :host />
+  <HostSmartRebootButton v-if="hostIsRunning" :host />
   <HostShutdownButton v-if="hostIsRunning" :host />
   <HostStartButton v-if="hostIsHalted" :host />
 </template>
@@ -9,6 +10,7 @@
 import HostForceRebootButton from '@/modules/host/components/actions/force-reboot/HostForceRebootButton.vue'
 import HostRebootButton from '@/modules/host/components/actions/reboot/HostRebootButton.vue'
 import HostShutdownButton from '@/modules/host/components/actions/shutdown/HostShutdownButton.vue'
+import HostSmartRebootButton from '@/modules/host/components/actions/smart-reboot/HostSmartRebootButton.vue'
 import HostStartButton from '@/modules/host/components/actions/start/HostStartButton.vue'
 import type { FrontXoHost } from '@/modules/host/remote-resources/use-xo-host-collection.ts'
 import { HOST_POWER_STATE } from '@vates/types'
