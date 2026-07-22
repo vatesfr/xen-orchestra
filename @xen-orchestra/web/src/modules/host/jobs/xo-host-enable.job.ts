c54f4c8c8e73230c6a5600f4eed3bd4898ea75a4
Marie Garde <marie.garde@vates.tech>
2026-07-22 15:03:13 +0200

    add check pending on job

--- @xen-orchestra/web/src/modules/host/jobs/xo-host-enable.job.ts
+++ @xen-orchestra/web/src/modules/host/jobs/xo-host-enable.job.ts
@@ -1,9 +1,10 @@
 import { xoHostArg } from '@/modules/host/jobs/xo-host-args.jobs.ts'
 import type { FrontXoHost } from '@/modules/host/remote-resources/use-xo-host-collection.ts'
+import { isHostOperationPending } from '@/modules/host/utils/xo-host.util.ts'
 import { useXoTaskUtils } from '@/shared/composables/xo-task-utils.composable.ts'
 import { fetchPost } from '@/shared/utils/fetch.util.ts'
 import { defineJob, JobError, JobRunningError } from '@core/packages/job'
-import { type XoTask } from '@vates/types'
+import { HOST_ALLOWED_OPERATIONS, type XoTask } from '@vates/types'
 import { useI18n } from 'vue-i18n'
 
 export const useXoHostEnableJob = defineJob('host.enable', [xoHostArg], () => {
@@ -24,7 +25,7 @@ export const useXoHostEnableJob = defineJob('host.enable', [xoHostArg], () => {
         throw new JobError(t('job:host-enable:missing-host'))
       }
 
-      if (isRunning) {
+      if (isRunning || isHostOperationPending(host, HOST_ALLOWED_OPERATIONS.ENABLE)) {
         throw new JobRunningError(t('job:enable:in-progress'))
       }
     },
