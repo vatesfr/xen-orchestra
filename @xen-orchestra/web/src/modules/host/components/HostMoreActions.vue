d7e321465f857d59e9d1bf38db635190528dbe19
Marie Garde <marie.garde@vates.tech>
2026-07-27 10:30:39 +0200

    add check pending operation evacuate

--- @xen-orchestra/web/src/modules/host/components/HostMoreActions.vue
+++ @xen-orchestra/web/src/modules/host/components/HostMoreActions.vue
@@ -1,9 +1,9 @@
 <template>
-  <template v-if="host.enabled  || hostIsHalted">
+  <template v-if="host.enabled || hostIsHalted || isEvacuateVm">
     <HostDisableButton :host />
     <HostDisableAndEvacuateVmButton :host />
   </template>
-  <HostEnableButton v-else :host />
+  <HostEnableButton v-if="!host.enabled && !isEvacuateVm" :host />
   <HostForgetButton v-if="hostIsHalted" :host />
   <VtsDivider type="stretch" />
   <HostDownloadButton :host-id="host.id" />
@@ -16,8 +16,9 @@ import HostDownloadButton from '@/modules/host/components/actions/download/HostD
 import HostEnableButton from '@/modules/host/components/actions/enable/HostEnableButton.vue'
 import HostForgetButton from '@/modules/host/components/actions/forget/HostForgetButton.vue'
 import type { FrontXoHost } from '@/modules/host/remote-resources/use-xo-host-collection.ts'
+import { isHostOperationPending } from '@/modules/host/utils/xo-host.util.ts'
 import VtsDivider from '@core/components/divider/VtsDivider.vue'
-import { HOST_POWER_STATE } from '@vates/types'
+import { HOST_ALLOWED_OPERATIONS, HOST_POWER_STATE } from '@vates/types'
 import { computed } from 'vue'
 
 const { host } = defineProps<{
@@ -25,4 +26,6 @@ const { host } = defineProps<{
 }>()
 
 const hostIsHalted = computed(() => host.power_state === HOST_POWER_STATE.HALTED)
+
+const isEvacuateVm = computed(() => isHostOperationPending(host, HOST_ALLOWED_OPERATIONS.EVACUATE))
 </script>
