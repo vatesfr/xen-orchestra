c0e7f2f1a4895c8e82dc9c6128d5c779764e8a34
Alexandre Mouillard <alexandre.mouillard@vates.tech>
2026-07-30 15:14:35 +0200

    Remove dev routes

--- @xen-orchestra/web/src/route-map.d.ts
+++ @xen-orchestra/web/src/route-map.d.ts
@@ -143,41 +143,6 @@ declare module 'vue-router/auto-routes' {
       { id: ParamValue<false> },
       | never
     >,
-    '/dev/': RouteRecordInfo<
-      '/dev/',
-      '/dev',
-      Record<never, never>,
-      Record<never, never>,
-      | never
-    >,
-    '/dev/colors': RouteRecordInfo<
-      '/dev/colors',
-      '/dev/colors',
-      Record<never, never>,
-      Record<never, never>,
-      | never
-    >,
-    '/dev/icons/': RouteRecordInfo<
-      '/dev/icons/',
-      '/dev/icons',
-      Record<never, never>,
-      Record<never, never>,
-      | never
-    >,
-    '/dev/icons/[name]': RouteRecordInfo<
-      '/dev/icons/[name]',
-      '/dev/icons/:name',
-      { name: ParamValue<true> },
-      { name: ParamValue<false> },
-      | never
-    >,
-    '/dev/token': RouteRecordInfo<
-      '/dev/token',
-      '/dev/token',
-      Record<never, never>,
-      Record<never, never>,
-      | never
-    >,
     '/host/[id]': RouteRecordInfo<
       '/host/[id]',
       '/host/:id',
