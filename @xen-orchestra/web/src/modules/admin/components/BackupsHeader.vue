5e8d3c6c9158898803db048493e7efb14b8ba563
Marie Garde <marie.garde@vates.tech>
2026-08-06 15:32:06 +0200

    feat(xo6): create page backup repository list

---
+++ @xen-orchestra/web/src/modules/admin/components/BackupsHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <UiHeadBar icon="object:organization">
+    {{ t('backups') }}
+  </UiHeadBar>
+  <TabList>
+    <RouterLink v-slot="{ isExactActive, href }" :to="{ name: '/admin/backups/brs' }" custom>
+      <TabItem :active="isExactActive" :href tag="a">
+        {{ t('brs') }}
+      </TabItem>
+    </RouterLink>
+  </TabList>
+</template>
+
+<script lang="ts" setup>
+import TabItem from '@xen-orchestra/web-core/components/tab/TabItem.vue'
+import TabList from '@xen-orchestra/web-core/components/tab/TabList.vue'
+import UiHeadBar from '@xen-orchestra/web-core/components/ui/head-bar/UiHeadBar.vue'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+</script>
---
+++ @xen-orchestra/web/src/pages/admin/backups.vue
@@ -0,0 +1,16 @@
+<template>
+  <RouterView v-slot="{ Component }">
+    <BackupsHeader />
+    <component :is="Component" :users />
+  </RouterView>
+</template>
+
+<script lang="ts" setup>
+import BackupsHeader from '@/modules/admin/components/BackupsHeader.vue'
+import { useXoUserCollection } from '@/modules/user/remote-resources/use-xo-user-collection.ts'
+import { useDefaultTab } from '@core/composables/default-tab.composable.ts'
+
+useDefaultTab('/admin/backups', 'brs')
+
+const { users } = useXoUserCollection()
+</script>
