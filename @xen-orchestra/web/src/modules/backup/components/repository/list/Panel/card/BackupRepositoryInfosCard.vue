0ba0c66fdf52ca49e2a4c181df3a80b6cc7df13f
Marie Garde <marie.garde@vates.tech>
2026-08-07 09:56:02 +0200

    create table

---
+++ @xen-orchestra/web/src/modules/backup/components/repository/list/Panel/card/BackupRepositoryInfosCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <UiCard class="card-container">
+    <VtsCardObjectTitle :id="br.id" :label="br.name" :href />
+    <div class="content">
+      <VtsCardRowKeyValue>
+        <template #key>{{ t('status') }}</template>
+        <template #value>
+          <VtsStatus :status="br.enabled" />
+        </template>
+      </VtsCardRowKeyValue>
+      <VtsCardRowKeyValue truncate align-top>
+        <template #key>{{ t('type') }}</template>
+        <template #value>{{ br.type }}</template>
+        <template v-if="br.type" #addons>
+          <VtsCopyButton :value="br.type" />
+        </template>
+      </VtsCardRowKeyValue>
+    </div>
+  </UiCard>
+</template>
+
+<script lang="ts" setup>
+import type { FrontXoBackupRepository } from '@/modules/backup/remote-resources/use-xo-backup-repository-collection.ts'
+import VtsCardRowKeyValue from '@core/components/card/VtsCardRowKeyValue.vue'
+import VtsCardObjectTitle from '@core/components/card-object-title/VtsCardObjectTitle.vue'
+import VtsCopyButton from '@core/components/copy-button/VtsCopyButton.vue'
+import VtsStatus from '@core/components/status/VtsStatus.vue'
+import UiCard from '@core/components/ui/card/UiCard.vue'
+import { useI18n } from 'vue-i18n'
+
+const { br } = defineProps<{
+  br: FrontXoBackupRepository
+}>()
+
+const { t } = useI18n()
+</script>
