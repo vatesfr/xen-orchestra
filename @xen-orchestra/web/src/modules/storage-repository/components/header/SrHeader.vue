9719fd0e44d8be19e80a3a1a8335d6f28318ecf8
Alexandre Mouillard <alexandre.mouillard@vates.tech>
2026-08-07 14:07:52 +0200

    Refactoring

--- @xen-orchestra/web/src/modules/storage-repository/components/header/SrHeader.vue
+++ @xen-orchestra/web/src/modules/storage-repository/components/header/SrHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <SrHeaderBreadcrumbLink :sr :from-context="fromContext" />
+  <SrHeaderBreadcrumb :sr :scope />
   <UiHeadBar>
     <template #icon>
       <VtsObjectIcon type="sr" :state="srConnectionStatus" size="medium" />
@@ -9,7 +9,7 @@
   <TabList>
     <RouterLink
       v-slot="{ isActive, href }"
-      :to="{ name: '/sr/[id]/general', params: { id: sr.id }, query: contextQuery }"
+      :to="{ name: '/sr/[id]/general', params: { id: sr.id }, query: scopeQuery }"
       custom
     >
       <TabItem :active="isActive" :href tag="a">
@@ -18,7 +18,7 @@
     </RouterLink>
     <RouterLink
       v-slot="{ isActive, href }"
-      :to="{ name: '/sr/[id]/hosts', params: { id: sr.id }, query: contextQuery }"
+      :to="{ name: '/sr/[id]/hosts', params: { id: sr.id }, query: scopeQuery }"
       custom
     >
       <TabItem :active="isActive" :href tag="a">
@@ -29,24 +29,26 @@
 </template>
 
 <script setup lang="ts">
-import SrHeaderBreadcrumbLink from '@/modules/storage-repository/components/header/SrHeaderBreadcrumbLink.vue'
+import SrHeaderBreadcrumb from '@/modules/storage-repository/components/header/SrHeaderBreadcrumb.vue'
 import { useXoSrUtils } from '@/modules/storage-repository/composables/xo-sr-utils.composable.ts'
 import type { FrontXoSr } from '@/modules/storage-repository/remote-resources/use-xo-sr-collection.ts'
+import { toSrScopeQuery } from '@/modules/storage-repository/utils/sr-scope.util.ts'
+import type { SrScope } from '@core/types/storage-repository.type.ts'
 import VtsObjectIcon from '@core/components/object-icon/VtsObjectIcon.vue'
 import TabItem from '@core/components/tab/TabItem.vue'
 import TabList from '@core/components/tab/TabList.vue'
 import UiHeadBar from '@core/components/ui/head-bar/UiHeadBar.vue'
-import { SR_SCOPE_TYPE, type SrScope } from '@core/types/storage-repository.type.ts'
 import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 
-const { sr, fromContext } = defineProps<{ sr: FrontXoSr; fromContext?: SrScope }>()
+const { sr, scope } = defineProps<{ sr: FrontXoSr; scope: SrScope }>()
 
 const { t } = useI18n()
 
-const contextQuery = computed(() =>
-  fromContext?.type === SR_SCOPE_TYPE.HOST ? { from: SR_SCOPE_TYPE.HOST, host: fromContext.hostId } : {}
-)
+const scopeQuery = computed(() => toSrScopeQuery(scope))
 
-const { srConnectionStatus } = useXoSrUtils(() => sr)
+const { srConnectionStatus } = useXoSrUtils(
+  () => sr,
+  () => scope
+)
 </script>
--- @xen-orchestra/web/src/pages/sr/[id].vue
+++ @xen-orchestra/web/src/pages/sr/[id].vue
@@ -4,21 +4,20 @@
     {{ t('object-not-found', { id: route.params.id }) }}
   </VtsStateHero>
   <RouterView v-else v-slot="{ Component }">
-    <SrHeader :sr :from-context="fromContext" />
+    <SrHeader :sr :scope />
     <component :is="Component" :sr />
   </RouterView>
 </template>
 
 <script lang="ts" setup>
-import { type FrontXoHost } from '@/modules/host/remote-resources/use-xo-host-collection.ts'
-import SrHeader from '@/modules/storage-repository/components/SrHeader.vue'
+import SrHeader from '@/modules/storage-repository/components/header/SrHeader.vue'
 import {
   type FrontXoSr,
   useXoSrCollection,
 } from '@/modules/storage-repository/remote-resources/use-xo-sr-collection.ts'
+import { parseSrScopeQuery } from '@/modules/storage-repository/utils/sr-scope.util.ts'
 import VtsStateHero from '@core/components/state-hero/VtsStateHero.vue'
 import { useDefaultTab } from '@core/composables/default-tab.composable.ts'
-import { SR_SCOPE_TYPE, type SrScope } from '@core/types/storage-repository.type.ts'
 import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { useRoute } from 'vue-router'
@@ -33,9 +32,5 @@ const { areSrsReady, useGetSrById } = useXoSrCollection()
 
 const sr = useGetSrById(() => route.params.id as FrontXoSr['id'])
 
-const fromContext = computed<SrScope>(() =>
-  route.query.from === SR_SCOPE_TYPE.HOST
-    ? { type: SR_SCOPE_TYPE.HOST, hostId: route.query.host as FrontXoHost['id'] }
-    : { type: SR_SCOPE_TYPE.POOL }
-)
+const scope = computed(() => parseSrScopeQuery(route.query))
 </script>
