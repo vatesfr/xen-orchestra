4d0443a9a8567e4a23e0f5b19dd490ddeca98ed8
Alexandre Mouillard <alexandre.mouillard@vates.tech>
2026-08-12 09:20:25 +0200

    Separate actions

---
+++ @xen-orchestra/web/src/modules/vdi/components/actions/VmVdiActions.vue
@@ -0,0 +1,30 @@
+<template>
+  <VbdConnectionToggleButton v-if="vbd" :vbd :vm />
+  <VdiMigrateButton :vdi />
+  <VdiImportExportMenu :vdi />
+  <VbdDeleteButton v-if="vbd" :vbd :vm />
+  <VdiDeleteButton :vdi :vm />
+</template>
+
+<script lang="ts" setup>
+import VbdConnectionToggleButton from '@/modules/vbd/components/actions/connection/VbdConnectionToggleButton.vue'
+import VbdDeleteButton from '@/modules/vbd/components/actions/delete/VbdDeleteButton.vue'
+import { useXoVbdCollection } from '@/modules/vbd/remote-resources/use-xo-vbd-collection.ts'
+import VdiDeleteButton from '@/modules/vdi/components/actions/delete/VdiDeleteButton.vue'
+import VdiImportExportMenu from '@/modules/vdi/components/actions/import-export/VdiImportExportMenu.vue'
+import VdiMigrateButton from '@/modules/vdi/components/actions/migrate/VdiMigrateButton.vue'
+import type { FrontXoVdi } from '@/modules/vdi/remote-resources/use-xo-vdi-collection.ts'
+import type { FrontXoVm } from '@/modules/vm/remote-resources/use-xo-vm-collection.ts'
+import { computed } from 'vue'
+
+const { vdi } = defineProps<{
+  vm: FrontXoVm
+  vdi: FrontXoVdi
+}>()
+
+const { useGetVbdsByIds } = useXoVbdCollection()
+
+const vbds = useGetVbdsByIds(() => vdi.$VBDs)
+
+const vbd = computed(() => vbds.value.find(vbd => vbd.attached) ?? vbds.value[0])
+</script>
