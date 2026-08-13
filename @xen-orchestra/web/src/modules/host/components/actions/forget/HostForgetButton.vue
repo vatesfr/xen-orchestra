7b35d5cb2ed2219ae493889dff2786cad2c9708e
Marie Garde <marie.garde@vates.tech>
2026-08-13 16:05:23 +0200

    Add redirect if still on host page after forget

--- @xen-orchestra/web/src/modules/host/components/actions/forget/HostForgetButton.vue
+++ @xen-orchestra/web/src/modules/host/components/actions/forget/HostForgetButton.vue
@@ -14,10 +14,12 @@
 <script lang="ts" setup>
 import { useXoHostForgetJob } from '@/modules/host/jobs/xo-host-forget.job.ts'
 import type { FrontXoHost } from '@/modules/host/remote-resources/use-xo-host-collection.ts'
+import { useXoPoolCollection } from '@/modules/pool/remote-resources/use-xo-pool-collection.ts'
 import MenuItem from '@core/components/menu/MenuItem.vue'
 import { useActionModal } from '@core/composables/modals/use-action-modal.ts'
 import { vTooltip } from '@core/directives/tooltip.directive.ts'
 import { useI18n } from 'vue-i18n'
+import { useRoute, useRouter } from 'vue-router'
 
 const { host } = defineProps<{
   host: FrontXoHost
@@ -34,10 +36,35 @@ const {
 
 const { open: openActionModal } = useActionModal()
 
+const route = useRoute<'/host/[id]'>()
+
+const router = useRouter()
+
+const { hasPoolById } = useXoPoolCollection()
+
+async function redirectIfOnHostPage() {
+  if (route.params.id !== host.id) {
+    return
+  }
+
+  await router.push(
+    hasPoolById(host.$pool) ? { name: '/pool/[id]/hosts', params: { id: host.$pool } } : { name: '/(site)/dashboard' }
+  )
+}
+
 function forgetHost() {
   openActionModal({
     events: {
-      onConfirm: () => run(),
+      onConfirm: async () => {
+        try {
+          await run()
+        } catch (error) {
+          console.error('Error when forgetting host:', error)
+          return
+        }
+
+        await redirectIfOnHostPage()
+      },
     },
     props: {
       accent: 'danger',
