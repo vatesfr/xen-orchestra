35c96e6f423f4dcd410816e99dec12ba0c4905d9
Marie Garde <marie.garde@vates.tech>
2026-08-17 15:47:21 +0200

    form wip

---
+++ @xen-orchestra/web/src/modules/backup/components/repository/form/new/NewBackupRepositoryGeneralStep.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="new-backup-repository-general-step">
+    <BackupRepositoryFormTextInput v-bind="nameInputBindings" />
+    <BackupRepositoryFormSelect v-bind="typeSelectBindings" />
+    <BackupRepositoryFormSelect v-bind="backupFormatSelectBindings" />
+    <BackupRepositoryFormSelect v-bind="proxySelectBindings" />
+    <BackupRepositoryEncryptedCheckbox v-bind="encryptedCheckboxBindings" />
+    <BackupRepositoryFormTextInput v-if="isEncrypted" v-bind="encryptionKeyInputBindings" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import BackupRepositoryEncryptedCheckbox from '@/modules/backup/components/repository/form/new/inputs/BackupRepositoryEncryptedCheckbox.vue'
+import BackupRepositoryFormSelect from '@/modules/backup/components/repository/form/new/inputs/BackupRepositoryFormSelect.vue'
+import BackupRepositoryFormTextInput from '@/modules/backup/components/repository/form/new/inputs/BackupRepositoryFormTextInput.vue'
+import { useNewBackupRepositoryForm } from '@/modules/backup/form/new/use-new-backup-repository-form.ts'
+
+const {
+  nameInputBindings,
+  typeSelectBindings,
+  backupFormatSelectBindings,
+  proxySelectBindings,
+  encryptedCheckboxBindings,
+  encryptionKeyInputBindings,
+  isEncrypted,
+} = useNewBackupRepositoryForm()
+</script>
+
+<style lang="postcss" scoped>
+.new-backup-repository-general-step {
+  display: flex;
+  flex-direction: column;
+  gap: 2.4rem;
+}
+</style>
