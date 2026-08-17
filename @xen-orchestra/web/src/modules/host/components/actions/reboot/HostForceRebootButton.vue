1f7cb229d50febee8164b8d42ec0fc9665f85a14
Marie Garde <marie.garde@vates.tech>
2026-08-17 15:48:12 +0200

    fix import

--- @xen-orchestra/web/src/modules/host/components/actions/reboot/HostForceRebootButton.vue
+++ @xen-orchestra/web/src/modules/host/components/actions/reboot/HostForceRebootButton.vue
@@ -12,7 +12,7 @@
 
 <script lang="ts" setup>
 import { useXoHostRebootJob } from '@/modules/host/jobs/xo-host-reboot.job.ts'
-import type { FrontXoHost } from '@/modules/host/remote-resources/use-xo-host-collection.js'
+import type { FrontXoHost } from '@/modules/host/remote-resources/use-xo-host-collection.ts'
 import MenuItem from '@xen-orchestra/web-core/components/menu/MenuItem.vue'
 import { useActionModal } from '@xen-orchestra/web-core/composables/modals/use-action-modal.ts'
 import { vTooltip } from '@xen-orchestra/web-core/directives/tooltip.directive.ts'
