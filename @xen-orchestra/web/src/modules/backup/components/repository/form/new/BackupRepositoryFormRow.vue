9620841a6f170a4a0a18a09af9b4e012251caa7c
Marie Garde <marie.garde@vates.tech>
2026-08-18 15:26:40 +0200

    azure and azurite details ok

---
+++ @xen-orchestra/web/src/modules/backup/components/repository/form/new/BackupRepositoryFormRow.vue
@@ -0,0 +1,42 @@
+<template>
+  <div :class="{ wide }" class="backup-repository-form-row">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  wide?: boolean
+}>()
+
+defineSlots<{
+  default(): any
+}>()
+</script>
+
+<style lang="postcss" scoped>
+.backup-repository-form-row {
+  display: flex;
+  flex-direction: column;
+  gap: 2.4rem;
+
+  & > * {
+    width: 100%;
+    min-width: 0;
+  }
+
+  @media (--medium-or-large) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    align-items: end;
+
+    & > * {
+      width: auto;
+    }
+
+    &.wide {
+      grid-template-columns: 1fr;
+    }
+  }
+}
+</style>
---
+++ @xen-orchestra/web/src/modules/backup/components/repository/form/new/fields/BackupRepositoryAzureFields.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="backup-repository-azure-fields">
+    <BackupRepositoryFormRow>
+      <BackupRepositoryFormTextInput v-bind="hostNameInputBindings" />
+    </BackupRepositoryFormRow>
+
+    <BackupRepositoryFormRow>
+      <BackupRepositoryFormTextInput v-bind="accountNameInputBindings" />
+      <BackupRepositoryFormTextInput v-bind="keyInputBindings" />
+    </BackupRepositoryFormRow>
+
+    <BackupRepositoryFormRow>
+      <BackupRepositoryFormTextInput v-bind="containerNameInputBindings" />
+    </BackupRepositoryFormRow>
+
+    <BackupRepositoryFormRow wide>
+      <BackupRepositoryFormTextInput v-bind="pathInContainerInputBindings" />
+    </BackupRepositoryFormRow>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import BackupRepositoryFormRow from '@/modules/backup/components/repository/form/new/BackupRepositoryFormRow.vue'
+import BackupRepositoryFormTextInput from '@/modules/backup/components/repository/form/new/inputs/BackupRepositoryFormTextInput.vue'
+import type { FieldMetadata, ModelBinding } from '@core/packages/validated-form'
+
+type TextInputBindings = ModelBinding<string> & FieldMetadata & { label: string; required?: boolean }
+
+defineProps<{
+  hostNameInputBindings: TextInputBindings
+  accountNameInputBindings: TextInputBindings
+  keyInputBindings: TextInputBindings
+  containerNameInputBindings: TextInputBindings
+  pathInContainerInputBindings: TextInputBindings
+}>()
+</script>
+
+<style lang="postcss" scoped>
+.backup-repository-azure-fields {
+  display: flex;
+  flex-direction: column;
+  gap: 2.4rem;
+}
+</style>
