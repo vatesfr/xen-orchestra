ca000c4375591bba6aa03ce0f52a47ffa902ca59
Marie Garde <marie.garde@vates.tech>
2026-08-19 14:46:13 +0200

    add checkbox azurite

--- @xen-orchestra/web/src/modules/backup/components/repository/form/new/fields/BackupRepositoryAzureFields.vue
+++ @xen-orchestra/web/src/modules/backup/components/repository/form/new/fields/BackupRepositoryAzureFields.vue
@@ -2,6 +2,7 @@
   <div class="backup-repository-azure-fields">
     <BackupRepositoryFormRow>
       <BackupRepositoryFormTextInput v-bind="bindings.hostName" />
+      <BackupRepositoryFormCheckbox v-if="isAzurite" v-bind="bindings.useHttps" class="use-https" />
     </BackupRepositoryFormRow>
 
     <BackupRepositoryFormRow>
@@ -21,10 +22,14 @@
 
 <script lang="ts" setup>
 import BackupRepositoryFormRow from '@/modules/backup/components/repository/form/new/BackupRepositoryFormRow.vue'
+import BackupRepositoryFormCheckbox from '@/modules/backup/components/repository/form/new/inputs/BackupRepositoryFormCheckbox.vue'
 import BackupRepositoryFormTextInput from '@/modules/backup/components/repository/form/new/inputs/BackupRepositoryFormTextInput.vue'
 import type { AzureBackupRepositoryDetailsForm } from '@/modules/backup/form/details/use-azure-backup-repository-details-form.ts'
 
-defineProps<{ bindings: AzureBackupRepositoryDetailsForm['bindings'] }>()
+defineProps<{
+  bindings: AzureBackupRepositoryDetailsForm['bindings']
+  isAzurite: boolean
+}>()
 </script>
 
 <style lang="postcss" scoped>
@@ -32,5 +37,11 @@ defineProps<{ bindings: AzureBackupRepositoryDetailsForm['bindings'] }>()
   display: flex;
   flex-direction: column;
   gap: 2.4rem;
+
+  .use-https {
+    @media (--medium-or-large) {
+      margin-block-start: 0.4rem;
+    }
+  }
 }
 </style>
