fc2c8afcde00586d08467cffc9df0b76b0c80276
Marie Garde <marie.garde@vates.tech>
2026-08-19 14:17:05 +0200

    refacto form

--- @xen-orchestra/web/src/modules/backup/components/repository/form/new/inputs/BackupRepositoryFormTextArea.vue
+++ @xen-orchestra/web/src/modules/backup/components/repository/form/new/inputs/BackupRepositoryFormTextArea.vue
@@ -1,29 +1,15 @@
 <template>
-  <UiTextarea v-model.trim="model" :accent="errorMessage !== undefined ? 'danger' : 'brand'" :required>
+  <UiTextarea v-model.trim="model" accent="brand">
     {{ label }}
-
-    <template v-if="info !== undefined" #info>{{ info }}</template>
-    <template v-if="errorMessage !== undefined" #message>{{ errorMessage }}</template>
   </UiTextarea>
 </template>
 
 <script lang="ts" setup>
-import type { InputWrapperMessage } from '@core/components/input-wrapper/VtsInputWrapper.vue'
 import UiTextarea from '@core/components/ui/text-area/UiTextarea.vue'
-import { computed } from 'vue'
 
-const { error } = defineProps<{
+defineProps<{
   label: string
-  info?: string
-  error?: InputWrapperMessage
-  required?: boolean
 }>()
 
 const model = defineModel<string>({ required: true })
-
-const errorMessage = computed(() => {
-  const first = Array.isArray(error) ? error[0] : error
-
-  return typeof first === 'object' ? first.content : first
-})
 </script>
