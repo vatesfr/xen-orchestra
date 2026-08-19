6ede01516848c901e8f4d1fd85a5efa591a1f752
Marie Garde <marie.garde@vates.tech>
2026-08-19 14:17:05 +0200

    refacto form

--- @xen-orchestra/web/src/modules/backup/components/repository/form/new/inputs/BackupRepositoryFormCheckbox.vue
+++ @xen-orchestra/web/src/modules/backup/components/repository/form/new/inputs/BackupRepositoryFormCheckbox.vue
@@ -3,22 +3,29 @@
     <UiCheckbox v-model="model" accent="brand">
       {{ label }}
     </UiCheckbox>
-    <UiInfo v-if="warning !== undefined" accent="warning" wrap>
-      {{ warning }}
+    <UiInfo v-if="warningMessage !== undefined" accent="warning" wrap>
+      {{ warningMessage }}
     </UiInfo>
   </div>
 </template>
 
 <script lang="ts" setup>
+import type { InputWrapperMessage } from '@core/components/input-wrapper/VtsInputWrapper.vue'
 import UiCheckbox from '@core/components/ui/checkbox/UiCheckbox.vue'
 import UiInfo from '@core/components/ui/info/UiInfo.vue'
+import { computed } from 'vue'
 
-defineProps<{
+const { warning } = defineProps<{
   label: string
-  warning?: string
+  warning?: InputWrapperMessage
 }>()
 
 const model = defineModel<boolean>({ required: true })
+
+const warningMessage = computed(() => {
+  const first = Array.isArray(warning) ? warning[0] : warning
+  return typeof first === 'object' ? first.content : first
+})
 </script>
 
 <style lang="postcss" scoped>
