1cd5385df250cd18f131eb59eb4354c31e62abc4
Alexandre Mouillard <alexandre.mouillard@vates.tech>
2026-08-24 10:53:13 +0200

    Push generated routes

--- @xen-orchestra/lite/src/route-map.d.ts
+++ @xen-orchestra/lite/src/route-map.d.ts
@@ -106,6 +106,20 @@ declare module 'vue-router/auto-routes' {
       { uuid: ParamValue<false> },
       | never
     >,
+    '/network/new': RouteRecordInfo<
+      '/network/new',
+      '/network/new',
+      Record<never, never>,
+      Record<never, never>,
+      | never
+    >,
+    '/network/new-bonded': RouteRecordInfo<
+      '/network/new-bonded',
+      '/network/new-bonded',
+      Record<never, never>,
+      Record<never, never>,
+      | never
+    >,
     '/object-not-found': RouteRecordInfo<
       '/object-not-found',
       '/object-not-found',
@@ -229,6 +243,13 @@ declare module 'vue-router/auto-routes' {
     '/story/linear-chart': RouteRecordInfo<
       '/story/linear-chart',
       '/story/linear-chart',
+      Record<never, never>,
+      Record<never, never>,
+      | never
+    >,
+    '/story/overlay': RouteRecordInfo<
+      '/story/overlay',
+      '/story/overlay',
       Record<never, never>,
       Record<never, never>,
       | never
@@ -383,6 +404,27 @@ declare module 'vue-router/auto-routes' {
     '/story/web-core/object-icon/vts-object-icon': RouteRecordInfo<
       '/story/web-core/object-icon/vts-object-icon',
       '/story/web-core/object-icon/vts-object-icon',
+      Record<never, never>,
+      Record<never, never>,
+      | never
+    >,
+    '/story/web-core/overlay/vts-overlay-button': RouteRecordInfo<
+      '/story/web-core/overlay/vts-overlay-button',
+      '/story/web-core/overlay/vts-overlay-button',
+      Record<never, never>,
+      Record<never, never>,
+      | never
+    >,
+    '/story/web-core/overlay/vts-overlay-cancel-button': RouteRecordInfo<
+      '/story/web-core/overlay/vts-overlay-cancel-button',
+      '/story/web-core/overlay/vts-overlay-cancel-button',
+      Record<never, never>,
+      Record<never, never>,
+      | never
+    >,
+    '/story/web-core/overlay/vts-overlay-confirm-button': RouteRecordInfo<
+      '/story/web-core/overlay/vts-overlay-confirm-button',
+      '/story/web-core/overlay/vts-overlay-confirm-button',
       Record<never, never>,
       Record<never, never>,
       | never
@@ -765,6 +807,13 @@ declare module 'vue-router/auto-routes' {
       Record<never, never>,
       | never
     >,
+    '/story/web-core/ui/panel-card/ui-panel-card': RouteRecordInfo<
+      '/story/web-core/ui/panel-card/ui-panel-card',
+      '/story/web-core/ui/panel-card/ui-panel-card',
+      Record<never, never>,
+      Record<never, never>,
+      | never
+    >,
     '/story/web-core/ui/progress-bar/ui-progress-bar': RouteRecordInfo<
       '/story/web-core/ui/progress-bar/ui-progress-bar',
       '/story/web-core/ui/progress-bar/ui-progress-bar',
@@ -931,6 +980,7 @@ declare module 'vue-router/auto-routes' {
       | '/vm/[uuid]/stats'
       | '/vm/[uuid]/system'
       | '/vm/[uuid]/tasks'
+      | '/vm/[uuid]/vdis'
     >,
     '/vm/[uuid]/alarms': RouteRecordInfo<
       '/vm/[uuid]/alarms',
@@ -977,6 +1027,13 @@ declare module 'vue-router/auto-routes' {
     '/vm/[uuid]/tasks': RouteRecordInfo<
       '/vm/[uuid]/tasks',
       '/vm/:uuid/tasks',
+      { uuid: ParamValue<true> },
+      { uuid: ParamValue<false> },
+      | never
+    >,
+    '/vm/[uuid]/vdis': RouteRecordInfo<
+      '/vm/[uuid]/vdis',
+      '/vm/:uuid/vdis',
       { uuid: ParamValue<true> },
       { uuid: ParamValue<false> },
       | never
@@ -1072,6 +1129,18 @@ declare module 'vue-router/auto-routes' {
     'src/pages/host/[uuid]/vms.vue': {
       routes:
         | '/host/[uuid]/vms'
+      views:
+        | never
+    }
+    'src/pages/network/new.vue': {
+      routes:
+        | '/network/new'
+      views:
+        | never
+    }
+    'src/pages/network/new-bonded.vue': {
+      routes:
+        | '/network/new-bonded'
       views:
         | never
     }
@@ -1183,6 +1252,12 @@ declare module 'vue-router/auto-routes' {
     'src/stories/linear-chart.story.vue': {
       routes:
         | '/story/linear-chart'
+      views:
+        | never
+    }
+    'src/stories/overlay.story.vue': {
+      routes:
+        | '/story/overlay'
       views:
         | never
     }
@@ -1315,6 +1390,24 @@ declare module 'vue-router/auto-routes' {
     'src/stories/web-core/object-icon/vts-object-icon.story.vue': {
       routes:
         | '/story/web-core/object-icon/vts-object-icon'
+      views:
+        | never
+    }
+    'src/stories/web-core/overlay/vts-overlay-button.story.vue': {
+      routes:
+        | '/story/web-core/overlay/vts-overlay-button'
+      views:
+        | never
+    }
+    'src/stories/web-core/overlay/vts-overlay-cancel-button.story.vue': {
+      routes:
+        | '/story/web-core/overlay/vts-overlay-cancel-button'
+      views:
+        | never
+    }
+    'src/stories/web-core/overlay/vts-overlay-confirm-button.story.vue': {
+      routes:
+        | '/story/web-core/overlay/vts-overlay-confirm-button'
       views:
         | never
     }
@@ -1642,6 +1735,12 @@ declare module 'vue-router/auto-routes' {
       views:
         | never
     }
+    'src/stories/web-core/ui/panel-card/ui-panel-card.story.vue': {
+      routes:
+        | '/story/web-core/ui/panel-card/ui-panel-card'
+      views:
+        | never
+    }
     'src/stories/web-core/ui/progress-bar/ui-progress-bar.story.vue': {
       routes:
         | '/story/web-core/ui/progress-bar/ui-progress-bar'
@@ -1784,6 +1883,7 @@ declare module 'vue-router/auto-routes' {
         | '/vm/[uuid]/stats'
         | '/vm/[uuid]/system'
         | '/vm/[uuid]/tasks'
+        | '/vm/[uuid]/vdis'
       views:
         | 'default'
     }
@@ -1826,6 +1926,12 @@ declare module 'vue-router/auto-routes' {
     'src/pages/vm/[uuid]/tasks.vue': {
       routes:
         | '/vm/[uuid]/tasks'
+      views:
+        | never
+    }
+    'src/pages/vm/[uuid]/vdis.vue': {
+      routes:
+        | '/vm/[uuid]/vdis'
       views:
         | never
     }
