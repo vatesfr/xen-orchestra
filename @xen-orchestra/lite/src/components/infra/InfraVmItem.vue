3f6460c26610cd91ee7111fc779ea26d8dcc7d30
Joris-K <joris.kosacki@vates.tech>
2026-08-25 16:00:54 +0200

    feat(xo-lite): add vm tree actions and loader

--- @xen-orchestra/lite/src/components/infra/InfraVmItem.vue
+++ @xen-orchestra/lite/src/components/infra/InfraVmItem.vue
@@ -5,6 +5,21 @@
       <template #icon>
         <VtsObjectIcon size="medium" :state="vmPowerState!" type="vm" />
       </template>
+      <template #addons>
+        <UiLoader v-if="isChangingState" v-tooltip="{ placement: 'top', content: currentOperation }" />
+        <MenuList placement="bottom-start">
+          <template #trigger="{ open, isOpen }">
+            <UiButtonIcon
+              accent="brand"
+              icon="action:more-actions"
+              size="small"
+              :selected="isOpen"
+              @click="open($event)"
+            />
+          </template>
+          <VmTreeActions :vm-opaque-ref />
+        </MenuList>
+      </template>
     </UiTreeItemLabel>
   </VtsTreeItem>
 </template>
@@ -12,10 +27,16 @@
 <script lang="ts" setup>
 import type { VM_POWER_STATE } from '@/libs/xen-api/xen-api.enums.ts'
 import type { XenApiVm } from '@/libs/xen-api/xen-api.types.ts'
+import VmTreeActions from '@/modules/vm/components/actions/VmTreeActions.vue'
+import { useVmUtils } from '@/modules/vm/composables/vm-utils.composable.ts'
 import { useVmStore } from '@/stores/xen-api/vm.store.ts'
+import MenuList from '@core/components/menu/MenuList.vue'
 import VtsObjectIcon from '@core/components/object-icon/VtsObjectIcon.vue'
 import VtsTreeItem from '@core/components/tree/VtsTreeItem.vue'
+import UiButtonIcon from '@core/components/ui/button-icon/UiButtonIcon.vue'
+import UiLoader from '@core/components/ui/loader/UiLoader.vue'
 import UiTreeItemLabel from '@core/components/ui/tree-item-label/UiTreeItemLabel.vue'
+import { vTooltip } from '@core/directives/tooltip.directive.ts'
 import { useIntersectionObserver } from '@vueuse/core'
 import { computed, ref } from 'vue'
 
@@ -36,4 +57,6 @@ const { stop } = useIntersectionObserver(rootElement, ([entry]) => {
 })
 
 const vmPowerState = computed(() => vm.value?.power_state.toLowerCase() as Lowercase<VM_POWER_STATE> | undefined)
+
+const { isChangingState, currentOperation } = useVmUtils(vm)
 </script>
--- @xen-orchestra/lite/src/components/vm/VmHeader.vue
+++ @xen-orchestra/lite/src/components/vm/VmHeader.vue
@@ -2,7 +2,13 @@
   <UiHeadBar icon="object:vm">
     {{ vm?.name_label }}
     <template #icon>
-      <VtsObjectIcon size="medium" :state="toLower(vm?.power_state)" type="vm" />
+      <VtsObjectIcon
+        size="medium"
+        :state="toLower(vm?.power_state)"
+        type="vm"
+        :busy="isChangingState"
+        :busy-tooltip="currentOperation"
+      />
     </template>
     <template #actions>
       <MenuList v-if="vm !== undefined" placement="bottom-start">
@@ -40,6 +46,7 @@ import VmActionMigrateItem from '@/components/vm/VmActionItems/VmActionMigrateIt
 import VmActionPowerStateItems from '@/components/vm/VmActionItems/VmActionPowerStateItems.vue'
 import VmActionSnapshotItem from '@/components/vm/VmActionItems/VmActionSnapshotItem.vue'
 import type { XenApiVm } from '@/libs/xen-api/xen-api.types.ts'
+import { useVmUtils } from '@/modules/vm/composables/vm-utils.composable.ts'
 import { useVmStore } from '@/stores/xen-api/vm.store.ts'
 import MenuList from '@core/components/menu/MenuList.vue'
 import VtsObjectIcon from '@core/components/object-icon/VtsObjectIcon.vue'
@@ -58,4 +65,6 @@ const { getByUuid: getVmByUuid } = useVmStore().subscribe()
 const route = useRoute<'/vm/[uuid]'>()
 
 const vm = computed(() => getVmByUuid(route.params.uuid as XenApiVm['uuid']))
+
+const { isChangingState, currentOperation } = useVmUtils(vm)
 </script>
