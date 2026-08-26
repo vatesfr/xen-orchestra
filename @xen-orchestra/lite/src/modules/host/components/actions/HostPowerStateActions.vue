acb5e1ca33c64f27b8a0a0de31c75f31aede54bf
Elise-F <elise.franzini@vates.tech>
2026-08-26 16:36:33 +0200

    feat(lite): add force reboot action on host

--- @xen-orchestra/lite/src/modules/host/components/actions/HostPowerStateActions.vue
+++ @xen-orchestra/lite/src/modules/host/components/actions/HostPowerStateActions.vue
@@ -1,10 +1,20 @@
 <template>
-  <HostRebootButton :host />
+  <template v-if="isRunning">
+    <HostRebootButton :host />
+    <HostForceRebootButton :host />
+  </template>
 </template>
 
 <script lang="ts" setup>
 import type { XenApiHost } from '@/libs/xen-api/xen-api.types.ts'
+import HostForceRebootButton from '@/modules/host/components/actions/reboot/HostForceRebootButton.vue'
 import HostRebootButton from '@/modules/host/components/actions/reboot/HostRebootButton.vue'
+import { useHostMetricsStore } from '@/stores/xen-api/host-metrics.store.ts'
+import { computed } from 'vue'
 
-defineProps<{ host: XenApiHost }>()
+const { host } = defineProps<{ host: XenApiHost }>()
+
+const { isHostRunning } = useHostMetricsStore().subscribe()
+
+const isRunning = computed(() => isHostRunning(host))
 </script>
