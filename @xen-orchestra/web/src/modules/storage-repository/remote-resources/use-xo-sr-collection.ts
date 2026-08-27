4993cd4e9545e5cdfa0236df82b22b02349fdc98
Alexandre Mouillard <alexandre.mouillard@vates.tech>
2026-08-27 15:08:07 +0200

    Refactor

--- @xen-orchestra/web/src/modules/storage-repository/remote-resources/use-xo-sr-collection.ts
+++ @xen-orchestra/web/src/modules/storage-repository/remote-resources/use-xo-sr-collection.ts
@@ -10,7 +10,7 @@ import { defineRemoteResource } from '@core/packages/remote-resource/define-remo
 import { sortByNameLabel } from '@core/utils/sort-by-name-label.util.ts'
 import type { XoSr } from '@vates/types'
 import { reactify, useSorted } from '@vueuse/core'
-import { ref, watch } from 'vue'
+import { computed, ref, watch } from 'vue'
 
 export type FrontXoSr = Pick<XoSr, (typeof srFields)[number]>
 
@@ -54,12 +54,10 @@ export const useXoSrCollection = defineRemoteResource({
 
     const vdiIsosBySrName = ref<Record<FrontXoSr['name_label'], FrontXoVdi[]>>({})
     const srsByPool = ref(new Map<FrontXoPool['id'], FrontXoSr[]>())
-    const srsByHost = ref(new Map<FrontXoHost['id'], FrontXoSr[]>())
 
-    watch([sortedSrs, pbdsBySr], ([srs, pbdsBySrValue]) => {
+    watch(sortedSrs, srs => {
       const tmpVdiIsosBySrName: Record<FrontXoSr['name_label'], FrontXoVdi[]> = {}
       const tmpSrsByPool = new Map<FrontXoPool['id'], FrontXoSr[]>()
-      const tmpSrsByHost = new Map<FrontXoHost['id'], FrontXoSr[]>()
 
       srs.forEach(sr => {
         if (sr.SR_type === 'iso') {
@@ -76,15 +74,22 @@ export const useXoSrCollection = defineRemoteResource({
         }
 
         safePushInMap(tmpSrsByPool, sr.$pool, sr)
-
-        pbdsBySrValue.get(sr.id)?.forEach(pbd => {
-          safePushInMap(tmpSrsByHost, pbd.host, sr)
-        })
       })
 
       vdiIsosBySrName.value = tmpVdiIsosBySrName
       srsByPool.value = tmpSrsByPool
-      srsByHost.value = tmpSrsByHost
+    })
+
+    const srsByHost = computed(() => {
+      const tmpSrsByHost = new Map<FrontXoHost['id'], FrontXoSr[]>()
+
+      sortedSrs.value.forEach(sr => {
+        pbdsBySr.value.get(sr.id)?.forEach(pbd => {
+          safePushInMap(tmpSrsByHost, pbd.host, sr)
+        })
+      })
+
+      return tmpSrsByHost
     })
 
     const isDefaultSr = (sr: FrontXoSr) => getPoolById(sr.$pool)?.default_SR === sr.id
