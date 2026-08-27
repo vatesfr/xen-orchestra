e7715b303a573d94f761f2383b53be49dba584c4
Elise-F <elise.franzini@vates.tech>
2026-08-27 17:03:50 +0200

    feat(lite): add forget action on host

--- @xen-orchestra/lite/src/modules/host/components/HostMoreActions.vue
+++ @xen-orchestra/lite/src/modules/host/components/HostMoreActions.vue
@@ -1,10 +1,11 @@
 <template>
-  <MenuItem accent="brand" icon="action:change-state">
+  <MenuItem v-if="showChangeStateButton" icon="action:change-state" class="change-state">
     {{ t('action:change-state') }}
     <template #submenu>
       <HostPowerStateActions :host />
     </template>
   </MenuItem>
+  <HostForgetButton :host />
   <VtsDivider type="stretch" />
   <HostDownloadButton :host-opaque-ref="host.$ref" />
 </template>
@@ -12,6 +13,7 @@
 <script setup lang="ts">
 import type { XenApiHost } from '@/libs/xen-api/xen-api.types.ts'
 import HostDownloadButton from '@/modules/host/components/actions/download/HostDownloadButton.vue'
+import HostForgetButton from '@/modules/host/components/actions/forget/HostForgetButton.vue'
 import HostPowerStateActions from '@/modules/host/components/actions/HostPowerStateActions.vue'
 import VtsDivider from '@core/components/divider/VtsDivider.vue'
 import MenuItem from '@core/components/menu/MenuItem.vue'
@@ -19,7 +21,14 @@ import { useI18n } from 'vue-i18n'
 
 defineProps<{
   host: XenApiHost
+  showChangeStateButton?: boolean
 }>()
 
 const { t } = useI18n()
 </script>
+
+<style lang="postcss" scoped>
+.change-state {
+  color: var(--color-brand-txt-base);
+}
+</style>
