393ff5440bb913db3a5c81b1ad83ba6a287fd078
Sandrine Barrucand <sandrine.barrucand@vates.tech>
2026-08-31 15:43:36 +0200

    fix(web-stack): use correct color for 404

---
+++ @xen-orchestra/web-core/lib/components/state-hero/VtsNotFoundIllustration.vue
@@ -0,0 +1,9 @@
+<template>
+  <svg aria-hidden="true" viewBox="0 0 397.13 396.85">
+    <use :href="`${notFoundUrl}#not-found-illustration`" />
+  </svg>
+</template>
+
+<script lang="ts" setup>
+import notFoundUrl from '@core/assets/not-found.svg'
+</script>
