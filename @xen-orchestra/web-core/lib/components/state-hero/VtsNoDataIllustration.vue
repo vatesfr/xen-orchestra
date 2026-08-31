4ab7d961306498bd99988e61286cb70fde8a1caa
Sandrine Barrucand <sandrine.barrucand@vates.tech>
2026-08-31 16:49:48 +0200

    fix(web-stack): use correct color for no-data stars

---
+++ @xen-orchestra/web-core/lib/components/state-hero/VtsNoDataIllustration.vue
@@ -0,0 +1,9 @@
+<template>
+  <svg aria-hidden="true" viewBox="0 0 397.13 396.85">
+    <use :href="`${noDataUrl}#no-data-illustration`" />
+  </svg>
+</template>
+
+<script lang="ts" setup>
+import noDataUrl from '@core/assets/no-data.svg'
+</script>
