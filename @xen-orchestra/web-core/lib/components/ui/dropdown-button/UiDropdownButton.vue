827c8d198639b1929eb47ae8ff3279e6752a196f
Sandrine Barrucand <sandrine.barrucand@vates.tech>
2026-09-03 14:15:06 +0200

    fix: border on selected state

--- @xen-orchestra/web-core/lib/components/ui/dropdown-button/UiDropdownButton.vue
+++ @xen-orchestra/web-core/lib/components/ui/dropdown-button/UiDropdownButton.vue
@@ -62,7 +62,7 @@ const className = computed(() => toVariants({ size }))
   }
 
   &.selected:not(:disabled) {
-    border: 0.2rem solid var(--color-brand-item-base);
+    outline: 0.2rem solid var(--color-brand-item-base);
     color: var(--color-brand-txt-base);
   }
 
