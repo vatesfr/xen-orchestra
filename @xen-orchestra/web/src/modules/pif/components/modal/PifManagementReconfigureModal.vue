17fe334b280cabe098ce0a3fe05f493636457ef8
Sandrine Barrucand <sandrine.barrucand@vates.tech>
2026-09-04 09:54:43 +0200

    add device name to modal title

--- @xen-orchestra/web/src/modules/pif/components/modal/PifManagementReconfigureModal.vue
+++ @xen-orchestra/web/src/modules/pif/components/modal/PifManagementReconfigureModal.vue
@@ -1,7 +1,7 @@
 <template>
   <UiModal accent="info" icon="status:info-picto" @confirm="emit('confirm')" @dismiss="emit('cancel')">
     <template #title>
-      {{ t('modal:pif-management-reconfigure-title') }}
+      {{ t('modal:pif-management-reconfigure-title', { device }) }}
     </template>
 
     <template #content>
