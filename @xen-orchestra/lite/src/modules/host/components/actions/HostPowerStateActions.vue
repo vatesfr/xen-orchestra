9750bd57a73e7fe259ca5c21599cd20aab20ee58
Elise-F <elise.franzini@vates.tech>
2026-09-04 10:04:06 +0200

    feedback 1

--- @xen-orchestra/lite/src/modules/host/components/actions/HostPowerStateActions.vue
+++ @xen-orchestra/lite/src/modules/host/components/actions/HostPowerStateActions.vue
@@ -1,20 +1,12 @@
 <template>
-  <template v-if="isRunning">
-    <HostRebootButton :host />
-    <HostForceRebootButton :host />
-  </template>
+  <HostRebootButton :host />
+  <HostForceRebootButton :host />
 </template>
 
 <script lang="ts" setup>
 import type { XenApiHost } from '@/libs/xen-api/xen-api.types.ts'
 import HostForceRebootButton from '@/modules/host/components/actions/reboot/HostForceRebootButton.vue'
 import HostRebootButton from '@/modules/host/components/actions/reboot/HostRebootButton.vue'
-import { useHostMetricsStore } from '@/stores/xen-api/host-metrics.store.ts'
-import { computed } from 'vue'
 
-const { host } = defineProps<{ host: XenApiHost }>()
-
-const { isHostRunning } = useHostMetricsStore().subscribe()
-
-const isRunning = computed(() => isHostRunning(host))
+defineProps<{ host: XenApiHost }>()
 </script>
