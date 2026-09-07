2bbff662cb0e133e8853afc192b61d3b38f084c0
Sylvère A. <sylvere.armange@vates.tech>
2026-09-07 11:46:41 +0200

    feat(xo6): add Kubernetes objects to the TreeView

---
+++ @xen-orchestra/web/src/modules/treeview/components/KubernetesNodeTreeItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <VtsTreeItem expanded :node-id="leaf.id">
+    <UiTreeItemLabel icon="object:node" :route="`/kubernetes/${encodeURIComponent(leaf.id)}`" no-indent>
+      {{ leaf.data.name }}
+    </UiTreeItemLabel>
+  </VtsTreeItem>
+</template>
+
+<script lang="ts" setup>
+import type { KubernetesNodeLeaf } from '@/modules/treeview/types/tree.type.ts'
+import VtsTreeItem from '@core/components/tree/VtsTreeItem.vue'
+import UiTreeItemLabel from '@core/components/ui/tree-item-label/UiTreeItemLabel.vue'
+
+defineProps<{
+  leaf: KubernetesNodeLeaf
+}>()
+</script>
