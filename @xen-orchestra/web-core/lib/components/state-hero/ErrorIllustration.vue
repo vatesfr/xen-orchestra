84bf6d993dca4122405481fea78239d000681837
AlinaD-2912 <alina.dvornyk@vates.tech>
2026-09-10 10:16:08 +0200

    feat(xo6): replace images with new SVG design

---
+++ @xen-orchestra/web-core/lib/components/state-hero/ErrorIllustration.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg aria-hidden="true" viewBox="0 0 397.13 396.85">
+    <use :href />
+  </svg>
+</template>
+
+<script lang="ts" setup>
+import errorUrl from '@core/assets/error.svg'
+
+const href = `${errorUrl}#error`
+</script>
---
+++ @xen-orchestra/web-core/lib/components/state-hero/UnderConstructionIllustration.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg aria-hidden="true" viewBox="0 0 397.13 396.85">
+    <use :href />
+  </svg>
+</template>
+
+<script lang="ts" setup>
+import underConstructionUrl from '@core/assets/under-construction.svg'
+
+const href = `${underConstructionUrl}#under-construction`
+</script>
