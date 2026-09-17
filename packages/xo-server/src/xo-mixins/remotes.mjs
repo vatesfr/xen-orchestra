4e8301997da46cec8381d0d2b8442dbbb12937ef
ayoublkj <ayoub.el-kajji@vates.tech>
2026-09-17 10:39:10 +0200

    remove failures throw

--- packages/xo-server/src/xo-mixins/remotes.mjs
+++ packages/xo-server/src/xo-mixins/remotes.mjs
@@ -277,11 +277,6 @@ export default class {
         { concurrency: 2, stopOnError: false }
       )
 
-      const failures = results.filter(r => !r.success)
-      if (failures.length === results.length && results.length > 0) {
-        throw new Error('reclaim space failed for all VMs')
-      }
-
       return results
     })
   }
