54a33b58704ad90b41f07061f084452c30ddda53
Elise-F <elise.franzini@vates.tech>
2026-09-17 14:31:58 +0200

    fix rebase

--- @xen-orchestra/lite/src/modules/host/jobs/host-forget.job.ts
+++ @xen-orchestra/lite/src/modules/host/jobs/host-forget.job.ts
@@ -2,12 +2,13 @@ import { hostArg } from '@/modules/host/jobs/host-args.ts'
 import { useHostMetricsStore } from '@/stores/xen-api/host-metrics.store.ts'
 import { useXenApiStore } from '@/stores/xen-api.store.ts'
 import { defineJob, JobError, JobRunningError } from '@core/packages/job'
+import { HOST_POWER_STATE } from '@vates/types'
 import { useI18n } from 'vue-i18n'
 
 export const useHostForgetJob = defineJob('host.forget', [hostArg], () => {
   const xapi = useXenApiStore().getXapi()
   const { t } = useI18n()
-  const { isHostHalted } = useHostMetricsStore().subscribe()
+  const { getHostPowerState } = useHostMetricsStore().subscribe()
 
   return {
     run: host => xapi.host.destroy(host.$ref),
@@ -17,7 +18,9 @@ export const useHostForgetJob = defineJob('host.forget', [hostArg], () => {
         throw new JobError(t('job:host-forget:missing-host'))
       }
 
-      if (!isHostHalted(host)) {
+      const powerState = getHostPowerState(host)
+
+      if (powerState !== HOST_POWER_STATE.HALTED) {
         throw new JobError(t('job:host-forget:bad-power-state'))
       }
 
