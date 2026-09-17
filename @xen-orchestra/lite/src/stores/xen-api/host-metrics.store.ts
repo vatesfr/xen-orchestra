61fabb07d4179ec6e2e7018ee7b0706fca8e314b
Elise-F <elise.franzini@vates.tech>
2026-09-17 10:17:49 +0200

    feedback 2

--- @xen-orchestra/lite/src/stores/xen-api/host-metrics.store.ts
+++ @xen-orchestra/lite/src/stores/xen-api/host-metrics.store.ts
@@ -1,6 +1,7 @@
 import type { XenApiHost } from '@/libs/xen-api/xen-api.types.ts'
 import { createXapiStoreConfig } from '@/stores/xen-api/create-xapi-store-config.ts'
 import { createSubscribableStoreContext } from '@core/utils/create-subscribable-store-context.util.ts'
+import { HOST_POWER_STATE } from '@vates/types'
 import { defineStore } from 'pinia'
 
 export const useHostMetricsStore = defineStore('xen-api-host-metrics', () => {
@@ -18,19 +19,23 @@ export const useHostMetricsStore = defineStore('xen-api-host-metrics', () => {
     }
   }
 
-  const isHostRunning = (host: XenApiHost) => {
-    return baseContext.getByOpaqueRef(host.metrics)?.live === true
+  const getHostPowerState = (host: XenApiHost): HOST_POWER_STATE => {
+    const live = baseContext.getByOpaqueRef(host.metrics)?.live
+
+    if (live === undefined) {
+      return HOST_POWER_STATE.UNKNOWN
+    }
+
+    return live ? HOST_POWER_STATE.RUNNING : HOST_POWER_STATE.HALTED
   }
 
-  const isHostHalted = (host: XenApiHost) => {
-    return baseContext.getByOpaqueRef(host.metrics)?.live === false
-  }
+  const isHostRunning = (host: XenApiHost) => getHostPowerState(host) === HOST_POWER_STATE.RUNNING
 
   const context = {
     ...baseContext,
     getHostMemory,
+    getHostPowerState,
     isHostRunning,
-    isHostHalted,
   }
 
   return createSubscribableStoreContext({ context, ...configRest }, {})
