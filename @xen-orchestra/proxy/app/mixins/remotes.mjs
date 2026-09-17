c558c252559b72029a283e273518b257bf7dec35
ayoublkj <ayoub.el-kajji@vates.tech>
2026-09-17 12:29:17 +0200

    adding subtasks for proxy reclaimSpace and a timeout of 10 min

--- @xen-orchestra/proxy/app/mixins/remotes.mjs
+++ @xen-orchestra/proxy/app/mixins/remotes.mjs
@@ -7,6 +7,7 @@ import { RemoteAdapter } from '@xen-orchestra/backups/RemoteAdapter.mjs'
 import { asyncEach } from '@vates/async-each'
 import { BACKUP_DIR } from '@xen-orchestra/backups/_getVmBackupDir.mjs'
 import { noSuchObject } from 'xo-common/api-errors.js'
+import { Task } from '@vates/task'
 
 export default class Remotes {
   constructor(app) {
@@ -71,19 +72,34 @@ export default class Remotes {
                 throw noSuchObject(vmUuid, 'VM')
               }
               const vmUuids = vmUuid !== undefined ? [vmUuid] : allVms
+              Task.set('total', vmUuids.length)
+              let done = 0
 
               const results = []
               await asyncEach(
                 vmUuids,
                 async uuid => {
                   try {
-                    const { merge: didMerge, size } = await remoteAdapter.cleanVm(`${BACKUP_DIR}/${uuid}`, {
-                      remove,
-                      merge,
-                    })
+                    const { merge: didMerge, size } = await Task.run(
+                      { properties: { name: `Clean VM ${uuid}`, data: { type: 'VM', id: uuid } } },
+                      () =>
+                        remoteAdapter.cleanVm(`${BACKUP_DIR}/${uuid}`, {
+                          remove,
+                          merge,
+                          logInfo: Task.info,
+                          logWarn: Task.warning,
+                        })
+                    )
                     results.push({ vmUuid: uuid, success: true, merge: didMerge, size })
                   } catch (error) {
-                    results.push({ vmUuid: uuid, success: false, error: error.message ?? String(error) })
+                    results.push({
+                      vmUuid: uuid,
+                      success: false,
+                      error: error instanceof Error ? error.message : String(error),
+                    })
+                  } finally {
+                    done++
+                    Task.set('progress', Math.round((done / vmUuids.length) * 100))
                   }
                 },
                 { concurrency: 2, stopOnError: false }
