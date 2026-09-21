9e08eb2c9a3000ccf00df799bd1aa10d54bdbd14
Simon Pacotte <simon.pacotte@vates.tech>
2026-09-21 10:26:30 +0200

    feat(xo-web): add support for IS_XOA_BUILD var

--- @xen-orchestra/web/env.d.ts
+++ @xen-orchestra/web/env.d.ts
@@ -1,2 +1,7 @@
 /// <reference types="vite/client" />
 /// <reference types="@core/types/value-matcher" />
+
+// Set by xoa-packager, absence means build from source.
+interface ImportMetaEnv {
+  readonly VITE_XOA_BUILD?: string
+}
