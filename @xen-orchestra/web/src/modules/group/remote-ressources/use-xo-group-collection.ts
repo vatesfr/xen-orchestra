1a6306e0ad9ecfd607e9e45ba41885b07f9feef5
MathieuRA <contact@mathieu-raisin.fr>
2026-09-24 08:44:48 -0400

    feat: groups reactivity

--- @xen-orchestra/web/src/modules/group/remote-ressources/use-xo-group-collection.ts
+++ @xen-orchestra/web/src/modules/group/remote-ressources/use-xo-group-collection.ts
@@ -1,3 +1,4 @@
+import { useWatchCollection } from '@/shared/composables/watch-collection.composable.ts'
 import { useXoCollectionState } from '@/shared/composables/xo-collection-state/use-xo-collection-state.ts'
 import { BASE_URL } from '@/shared/utils/fetch.util.ts'
 import { defineRemoteResource } from '@core/packages/remote-resource/define-remote-resource.ts'
@@ -8,8 +9,9 @@ export type FrontXoGroup = Pick<XoGroup, (typeof groupFields)[number]>
 const groupFields = ['id', 'name'] as const satisfies readonly (keyof XoGroup)[]
 
 export const useXoGroupCollection = defineRemoteResource({
-  url: `${BASE_URL}/groups?fields=${groupFields.join(',')}`,
-
+  url: `${BASE_URL}/groups?fields=${groupFields.join(',')}&ndjson=true`,
+  stream: true,
+  initWatchCollection: () => useWatchCollection({ resource: 'group', fields: groupFields }),
   initialData: () => [] as FrontXoGroup[],
   state: (groups, context) => {
     return useXoCollectionState(groups, {
